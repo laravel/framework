10675404b9feca91ed185b3e1746baecfd36bc59
Taylor Otwell <taylorotwell@gmail.com>
2013-01-11 14:14:31 -0600

    added key:generate command.

---
+++ src/Illuminate/Foundation/Console/KeyGenerateCommand.php
@@ -0,0 +1,61 @@
+<?php namespace Illuminate\Foundation\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class KeyGenerateCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'key:generate';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = "Set the application key";
+
+	/**
+	 * Create a new key generator command.
+	 *
+	 * @param  Illuminate\Filesystem\Filesystem  $files
+	 * @return void
+	 */
+	public function __construct(Filesystem $files)
+	{
+		$this->files = $files;
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		$contents = $this->files->get($path = $this->laravel['path'].'/config/app.php');
+
+		$contents = str_replace('YourSecretKey!!!', $key = $this->getRandomKey(), $contents);
+
+		$this->files->put($path, $contents);
+
+		$this->info("Application key [$key] set successfully.");
+	}
+
+	/**
+	 * Generate a random key for the application.
+	 *
+	 * @return string
+	 */
+	protected function getRandomKey()
+	{
+		$pool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$()-+';
+
+		return substr(str_shuffle(str_repeat($pool, 5)), 0, 32);
+	}
+
+}
