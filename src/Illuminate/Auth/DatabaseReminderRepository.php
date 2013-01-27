0ed5cb6a687bd865b60e7ce5d2bec1444c19ab7e
Taylor Otwell <taylorotwell@gmail.com>
2013-01-27 14:04:04 -0600

    continuing work on repository.

--- src/Illuminate/Auth/DatabaseReminderRepository.php
+++ src/Illuminate/Auth/DatabaseReminderRepository.php
@@ -49,6 +49,9 @@ class DatabaseReminderRepository implements ReminderRepositoryInterface {
 	{
 		$email = $user->getReminderEmail();
 
+		// We will create a new, random token for the user so that we can e-mail them
+		// a safe link to the password reset form. Then we will insert a record in
+		// the database so that we can verify the token within the actual reset.
 		$token = $this->createNewToken($user);
 
 		$payload = array('email' => $email, 'token' => $token, 'created_at' => new DateTime);
@@ -67,7 +70,32 @@ class DatabaseReminderRepository implements ReminderRepositoryInterface {
 	{
 		$email = $user->getReminderEmail();
 
-		return $this->getTable()->where('email', $email)->where('token', $token)->exists();
+		$reminder = $this->getTable()->where('email', $email)->where('token', $token)->first();
+
+		return $reminder and $this->notExpired($reminder);
+	}
+
+	/**
+	 * Determine if the reminder has expired.
+	 *
+	 * @param  StdClass  $reminder
+	 * @return bool
+	 */
+	protected function notExpired($reminder)
+	{
+		$createdPlusHour = strtotime($reminder->created_at) + 216000;
+
+		return $createdPlusHour < $this->getCurrentTime();
+	}
+
+	/**
+	 * Get the current UNIX timestamp.
+	 *
+	 * @return int
+	 */
+	protected function getCurrentTime()
+	{
+		return time();
 	}
 
 	/**
