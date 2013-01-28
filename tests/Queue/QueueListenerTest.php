6bd1934dc8111ece852a80979c8d8205e27cacee
Taylor Otwell <taylorotwell@gmail.com>
2013-01-28 14:05:01 -0600

    added tests for listener.

---
+++ tests/Queue/QueueListenerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Mockery as m;
+
+class QueueListenerTest extends PHPUnit_Framework_TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
+	public function testRunProcessCallsProcess()
+	{
+		$process = m::mock('Symfony\Component\Process\Process[run]');
+		$process->shouldReceive('run')->once();
+		$listener = m::mock('Illuminate\Queue\Listener[memoryExceeded]');
+		$listener->shouldReceive('memoryExceeded')->once()->with(1)->andReturn(false);
+
+		$listener->runProcess($process, 1);
+	}
+
+
+	public function testListenerStopsWhenMemoryIsExceeded()
+	{
+		$process = m::mock('Symfony\Component\Process\Process[run]');
+		$process->shouldReceive('run')->once();
+		$listener = m::mock('Illuminate\Queue\Listener[memoryExceeded,stop]');
+		$listener->shouldReceive('memoryExceeded')->once()->with(1)->andReturn(true);
+		$listener->shouldReceive('stop')->once();
+
+		$listener->runProcess($process, 1);	
+	}
+
+
+	public function testMakeProcessCorrectlyFormatsCommandLine()
+	{
+		$listener = new Illuminate\Queue\Listener(__DIR__);
+		$process = $listener->makeProcess('connection', 'queue', 1, 2);
+
+		$this->assertInstanceOf('Symfony\Component\Process\Process', $process);
+		$this->assertEquals(__DIR__, $process->getWorkingDirectory());
+		$this->assertEquals('php artisan queue:work connection --queue=queue --delay=1 --memory=2 --sleep', $process->getCommandLine());
+	}
+
+}
