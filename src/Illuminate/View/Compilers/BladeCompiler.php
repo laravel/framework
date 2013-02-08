20920e0e6c3765e951ac0f615525b1c343c728c3
Taylor Otwell <taylorotwell@gmail.com>
2013-02-08 09:58:19 -0600

    escape blade by default... added three bracket echo for raw output.

--- src/Illuminate/View/Compilers/BladeCompiler.php
+++ src/Illuminate/View/Compilers/BladeCompiler.php
@@ -146,7 +146,9 @@ class BladeCompiler extends Compiler implements CompilerInterface {
 	 */
 	protected function compileEchos($value)
 	{
-		return preg_replace('/\{\{\s*(.+?)\s*\}\}/s', '<?php echo $1; ?>', $value);
+		$value = preg_replace('/\{\{\{\s*(.+?)\s*\}\}\}/s', '<?php echo $1; ?>', $value);
+
+		return preg_replace('/\{\{\s*(.+?)\s*\}\}/s', '<?php echo e($1); ?>', $value);
 	}
 
 	/**
