e867ed6e326531196c47bb26cc226f330e04b6fa
Anders Persson <anders_persson_360@yahoo.se>
2013-02-13 22:10:06 +0100

    Generate a secure random key for the application using the new Str class

--- src/Illuminate/Foundation/Console/KeyGenerateCommand.php
+++ src/Illuminate/Foundation/Console/KeyGenerateCommand.php
@@ -62,9 +62,7 @@ class KeyGenerateCommand extends Command {
 	 */
 	protected function getRandomKey()
 	{
-		$pool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$()-+';
-
-		return substr(str_shuffle(str_repeat($pool, 5)), 0, 32);
+		return Illuminate\Support\Str::secureRandom(32);
 	}
 
 }
