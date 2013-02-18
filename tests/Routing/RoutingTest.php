53bfad401dbe17f4fdaf0047d3a7104c91f40f67
Robert Clancy <robert@mercenarydesign.net>
2013-02-18 18:20:10 +1100

    another thing I missed in the test

--- tests/Routing/RoutingTest.php
+++ tests/Routing/RoutingTest.php
@@ -88,7 +88,7 @@ class RoutingTest extends PHPUnit_Framework_TestCase {
 		$router->resource('foo', 'FooController');
 		$routes = $router->getRoutes();
 
-		$this->assertEquals(8, count($routes));
+		$this->assertEquals(9, count($routes));
 
 		$router = new Router;
 		$router->resource('foo', 'FooController', array('only' => array('show', 'destroy')));
@@ -100,7 +100,7 @@ class RoutingTest extends PHPUnit_Framework_TestCase {
 		$router->resource('foo', 'FooController', array('except' => array('show', 'destroy')));
 		$routes = $router->getRoutes();
 
-		$this->assertEquals(6, count($routes));
+		$this->assertEquals(7, count($routes));
 	}
 
 
