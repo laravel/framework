f132e3e894ea5db842ef054759c03b0088f7c955
Kane Cohen <kanecohen@gmail.com>
2013-02-22 11:48:29 +0000

    Extend Str::slug for better result consistency

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -181,6 +181,10 @@ class Str {
 		// Remove all characters that are not the separator, letters, numbers, or whitespace.
 		$title = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', mb_strtolower($title));
 
+		// Convert all dashes/undescores into separator
+		$flip = ($separator == '-' ? '_' : '-');
+		$title = preg_replace('!['.preg_quote($flip).']+!u', $separator, $title);
+
 		// Replace all separator characters and whitespace by a single separator
 		$title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
 
