f1df461d28fc610238c06ef210d87f5a46434b81
Chad Miller <cmiller@redvel.net>
2013-02-24 10:19:45 -0600

    added dedicated test for namespaced controller

--- tests/Routing/RoutingControllerInspectorTest.php
+++ tests/Routing/RoutingControllerInspectorTest.php
@@ -5,6 +5,18 @@ class RoutingControllerInspectorTest extends PHPUnit_Framework_TestCase {
 	public function testMethodsAreCorrectlyDetermined()
 	{
 		$inspector = new Illuminate\Routing\Controllers\Inspector;
+		$data = $inspector->getRoutable('RoutingControllerInspectorStub', 'prefix');
+
+		$this->assertEquals(3, count($data));
+		$this->assertEquals(array('verb' => 'get', 'plain' => 'prefix', 'uri' => 'prefix'), $data['getIndex'][0]);
+		$this->assertEquals(array('verb' => 'get', 'plain' => 'prefix/index', 'uri' => 'prefix/index/{v1?}/{v2?}/{v3?}/{v4?}/{v5?}'), $data['getIndex'][1]);
+		$this->assertEquals(array('verb' => 'get', 'plain' => 'prefix/foo-bar', 'uri' => 'prefix/foo-bar/{v1?}/{v2?}/{v3?}/{v4?}/{v5?}'), $data['getFooBar'][0]);
+		$this->assertEquals(array('verb' => 'post', 'plain' => 'prefix/baz', 'uri' => 'prefix/baz/{v1?}/{v2?}/{v3?}/{v4?}/{v5?}'), $data['postBaz'][0]);
+	}
+
+	public function testMethodsAreCorrectWhenControllerIsNamespaced()
+	{
+		$inspector = new Illuminate\Routing\Controllers\Inspector;
 		$data = $inspector->getRoutable('\\RoutingControllerInspectorStub', 'prefix');
 
 		$this->assertEquals(3, count($data));
