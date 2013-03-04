f7461155015ceef7ae65557999349360707d1fc0
Kimmy Andersson <cyaspik@gmail.com>
2013-03-04 20:29:18 +0100

    Composer and test.

--- tests/Foundation/FoundationComposerTest.php
+++ tests/Foundation/FoundationComposerTest.php
@@ -16,7 +16,7 @@ class FoundationComposerTest extends PHPUnit_Framework_TestCase {
 		$files->shouldReceive('exists')->once()->with(__DIR__.'/composer.phar')->andReturn(true);
 		$process = m::mock('stdClass');
 		$composer->expects($this->once())->method('getProcess')->will($this->returnValue($process));
-		$process->shouldReceive('setCommandLine')->once()->with('php composer.phar dump-autoload --optimize');
+		$process->shouldReceive('setCommandLine')->once()->with('php \''.__DIR__.'/composer.phar\' dump-autoload --optimize');
 		$process->shouldReceive('run')->once();
 
 		$composer->dumpAutoloads();
@@ -33,6 +33,18 @@ class FoundationComposerTest extends PHPUnit_Framework_TestCase {
 		$process->shouldReceive('run')->once();
 
 		$composer->dumpAutoloads();
+	}
+
+	public function testDumpAutoloadHasCorrectPathWhenCustomPathIsSpecified()
+	{
+		$composer = $this->getMock('Illuminate\Foundation\Composer', array('getProcess'), array($files = m::mock('Illuminate\Filesystem\Filesystem'), __DIR__));
+		$files->shouldReceive('exists')->once()->with(__DIR__.'/composer.phar')->andReturn(false);
+		$process = m::mock('stdClass');
+		$composer->expects($this->once())->method('getProcess')->with('custom')->will($this->returnValue($process));
+		$process->shouldReceive('setCommandLine')->once()->with('composer dump-autoload --optimize');
+		$process->shouldReceive('run')->once();
+
+		$composer->dumpAutoloads('custom');
 	}
 
 }
