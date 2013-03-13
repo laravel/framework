ac5b414f988d5cd952a5749f87586ab9f1ac8d13
Taylor Otwell <taylorotwell@gmail.com>
2013-03-13 14:49:44 -0500

    Use path and domain in session store.

--- src/Illuminate/Session/Store.php
+++ src/Illuminate/Session/Store.php
@@ -461,12 +461,14 @@ abstract class Store implements ArrayAccess {
 	 *
 	 * @param  Illuminate\Cookie\CookieJar  $cookie
 	 * @param  string  $name
-	 * @param  int  $lifetime
+	 * @param  int     $lifetime
+	 * @param  string  $path
+	 * @param  string  $domain
 	 * @return void
 	 */
-	public function getCookie(CookieJar $cookie, $name, $lifetime)
+	public function getCookie(CookieJar $cookie, $name, $lifetime, $path, $domain)
 	{
-		return $cookie->make($name, $this->getSessionId(), $lifetime);
+		return $cookie->make($name, $this->getSessionId(), $lifetime, $path, $domain);
 	}
 
 	/**
