2b8afba8dcaf546f77155e78a85e8028d10e0612
Maxime Fabre <ehtnam6@gmail.com>
2013-03-14 17:39:40 +0100

    Rename method Str::limitWords to Str::words

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Str;
 class SupportStrTest extends PHPUnit_Framework_TestCase
 {
   /**
-   * Test the Str::limitWords method.
+   * Test the Str::words method.
    *
    * @group laravel
    */
   public function testStringCanBeLimitedByWords()
   {
-    $this->assertEquals('Taylor...', Str::limitWords('Taylor Otwell', 1));
-    $this->assertEquals('Taylor___', Str::limitWords('Taylor Otwell', 1, '___'));
-    $this->assertEquals('Taylor Otwell', Str::limitWords('Taylor Otwell', 3));
+    $this->assertEquals('Taylor...', Str::words('Taylor Otwell', 1));
+    $this->assertEquals('Taylor___', Str::words('Taylor Otwell', 1, '___'));
+    $this->assertEquals('Taylor Otwell', Str::words('Taylor Otwell', 3));
   }
 }
