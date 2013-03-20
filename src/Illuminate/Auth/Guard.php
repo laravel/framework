3c2354230874531456001fa8d268a97210ff105f
Taylor Otwell <taylorotwell@gmail.com>
2013-03-19 20:28:47 -0500

    Make user data unavailable after Auth::logout.

--- src/Illuminate/Auth/Guard.php
+++ src/Illuminate/Auth/Guard.php
@@ -51,6 +51,13 @@ class Guard {
 	protected $events;
 
 	/**
+	 * Indicates if the logout method has been called.
+	 *
+	 * @var bool
+	 */
+	protected $loggedOut = false;
+
+	/**
 	 * Create a new authentication guard.
 	 *
 	 * @param  Illuminate\Auth\UserProviderInterface  $provider
@@ -91,21 +98,34 @@ class Guard {
 	 */
 	public function user()
 	{
-		if ( ! is_null($this->user)) return $this->user;
+		if ($this->loggedOut) return;
+
+		// If we have already retrieved the user for the current request we can just
+		// return it back immediately. We do not want to pull the user data every
+		// request into the method becaue that would tremendously slow the app.
+		if ( ! is_null($this->user))
+		{
+			return $this->user;
+		}
 
 		$id = $this->session->get($this->getName());
-
-		$user = null;
 
 		// First we will try to load the user using the identifier in the session if
 		// one exists. Otherwise we will check for a "remember me" cookie in this
 		// request, and if one exists, attempt to retrieve the user using that.
+		$user = null;
+
 		if ( ! is_null($id))
 		{
 			$user = $this->provider->retrieveByID($id);
 		}
 
-		if (is_null($user) and ! is_null($recaller = $this->getRecaller()))
+		// If the user is null, but we decrypt a "recaller" cookie we can attempt to
+		// pull the user data on that cookie which serves as a remember cookie on
+		// the application. Once we have a user we can return it to the caller.
+		$recaller = $this->getRecaller();
+
+		if (is_null($user) and ! is_null($recaller))
 		{
 			$user = $this->provider->retrieveByID($recaller);
 		}
@@ -120,7 +140,10 @@ class Guard {
 	 */
 	protected function getRecaller()
 	{
-		return $this->getCookieJar()->get($this->getRecallerName());
+		if (isset($this->cookie))
+		{
+			return $this->getCookieJar()->get($this->getRecallerName());
+		}
 	}
 
 	/**
@@ -252,6 +275,8 @@ class Guard {
 		}
 
 		$this->user = null;
+
+		$this->loggedOut = true;
 	}
 
 	/**
