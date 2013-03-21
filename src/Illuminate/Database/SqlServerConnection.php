06dca8691a85bcb53382d3529987be3f3f8bb166
Jon Watson <jwatson@fh.org>
2013-03-21 11:32:50 -0700

    Changed spaces to tabs per PSR standards.

--- src/Illuminate/Database/SqlServerConnection.php
+++ src/Illuminate/Database/SqlServerConnection.php
@@ -22,40 +22,40 @@ class SqlServerConnection extends Connection {
 		return $this->withTablePrefix(new Schema\Grammars\SqlServerGrammar);
 	}
 	
-    /**
-     * Execute a Closure within a transaction.
-     * For MsSql, transactions must be called explicitly using SQL
-     * because the PDO driver does not support beginning a transaction
-     * with dblib as of this writing.
-     *
-     * @param  Closure  $callback
-     * @return mixed
-     */
-    public function transaction(Closure $callback)
-    {  
-        $this->pdo->exec("BEGIN TRAN");
+	/**
+	* Execute a Closure within a transaction.
+	* For MsSql, transactions must be called explicitly using SQL
+	* because the PDO driver does not support beginning a transaction
+	* with dblib as of this writing.
+	*
+	* @param  Closure  $callback
+	* @return mixed
+	*/
+	public function transaction(\Closure $callback)
+	{  
+		$this->pdo->exec("BEGIN TRAN");
 
-        // We'll simply execute the given callback within a try / catch block
-        // and if we catch any exception we can rollback the transaction
-        // so that none of the changes are persisted to the database.
-        try
-        {  
-            $result = $callback($this);
+		// We'll simply execute the given callback within a try / catch block
+		// and if we catch any exception we can rollback the transaction
+		// so that none of the changes are persisted to the database.
+		try
+		{  
+			$result = $callback($this);
 
-            $this->pdo->exec("COMMIT");
-        }
+			$this->pdo->exec("COMMIT");
+		}
 
-        // If we catch an exception, we will roll back so nothing gets messed
-        // up in the database. Then we'll re-throw the exception so it can
-        // be handled how the developer sees fit for their applications.
-        catch (\Exception $e)
-        {  
-            $this->pdo->exec("ROLLBACK TRAN");
+		// If we catch an exception, we will roll back so nothing gets messed
+		// up in the database. Then we'll re-throw the exception so it can
+		// be handled how the developer sees fit for their applications.
+		catch (\Exception $e)
+		{  
+			$this->pdo->exec("ROLLBACK TRAN");
 
-            throw $e;
-        }
+			throw $e;
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
 }
