13a27926dacc4c8683ead862de3fc31d8c7e1c78
Franz Liedke <franz@develophp.org>
2013-03-23 15:02:41 +0100

    Implement console command for clearing the cache.

---
+++ src/Illuminate/Cache/Console/ClearCommand.php
@@ -0,0 +1,51 @@
+<?php namespace Illuminate\Cache\Console;
+
+use Illuminate\Console\Command;
+
+class ClearCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'cache:clear';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = "Flush the entire cache.";
+
+	/**
+	 * The cache manager instance.
+	 *
+	 * @var \Illuminate\Cache\CacheManager
+	 */
+	protected $cache;
+
+	/**
+	 * Create a new cache clear command instance.
+	 *
+	 * @param  \Illuminate\Cache\CacheManager  $cache
+	 * @return void
+	 */
+	public function __construct(CacheManager $cache)
+	{
+		parent::__construct();
+
+		$this->cache = $cache;
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		$this->cache->flush();
+	}
+
+}
