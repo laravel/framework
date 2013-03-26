c222355edb06e01a9718c37676485ecab36f7d74
Taylor Otwell <taylorotwell@gmail.com>
2013-03-26 11:21:15 -0500

    Add support for "mail" driver.

--- src/Illuminate/Mail/MailServiceProvider.php
+++ src/Illuminate/Mail/MailServiceProvider.php
@@ -3,6 +3,7 @@
 use Swift_Mailer;
 use Illuminate\Support\ServiceProvider;
 use Swift_SmtpTransport as SmtpTransport;
+use Swift_MailTransport as MailTransport;
 
 class MailServiceProvider extends ServiceProvider {
 
@@ -75,6 +76,27 @@ class MailServiceProvider extends ServiceProvider {
 	 */
 	protected function registerSwiftTransport($config)
 	{
+		switch ($config['driver'])
+		{
+			case 'smtp':
+				return $this->registerSmtpTransport($config);
+
+			case 'mail':
+				return $this->registerMailTransport($config);
+
+			default:
+				throw new \InvalidArgumentException('Invalid mail driver.');
+		}
+	}
+
+	/**
+	 * Register the SMTP Swift Transport instance.
+	 *
+	 * @param  array  $config
+	 * @return void
+	 */
+	protected function registerSmtpTransport($config)
+	{
 		$this->app['swift.transport'] = $this->app->share(function($app) use ($config)
 		{
 			extract($config);
@@ -100,6 +122,20 @@ class MailServiceProvider extends ServiceProvider {
 			}
 
 			return $transport;
+		});
+	}
+
+	/**
+	 * Register the Mail Swift Transport instance.
+	 *
+	 * @param  array  $config
+	 * @return void
+	 */
+	protected function registerMailTransport($config)
+	{
+		$this->app['swift.transport'] = $this->app->share(function()
+		{
+			return MailTransport::newInstance();
 		});
 	}
 
