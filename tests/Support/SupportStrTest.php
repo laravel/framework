4b6fd8aaebca3fbbbba911176a51271ff0d467ca
Andrew <github@andrewelkins.com>
2013-03-31 17:07:00 -0700

    Adding test cases for new STR functions

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -15,4 +15,26 @@ class SupportStrTest extends PHPUnit_Framework_TestCase
     $this->assertEquals('Taylor___', Str::words('Taylor Otwell', 1, '___'));
     $this->assertEquals('Taylor Otwell', Str::words('Taylor Otwell', 3));
   }
+
+  /**
+   * Test the Str::title method.
+   *
+   * @group laravel
+   */
+  public function testStringCanBeCapitalizedUsingTitle()
+  {
+    $this->assertEquals('Ένα Πλαίσιο Για Το Web Artisans', Str::title('Ένα πλαίσιο για το web Artisans'));
+    $this->assertEquals('A Framework For Web Artisans', Str::title('A framework For web Artisans'));
+  }
+
+  /**
+   * Test the Str::asciiTitle method.
+   *
+   * @group laravel
+   */
+  public function testStringCanBeCapitalizedUsingAsciiTitle()
+  {
+    $this->assertEquals('??? ??????? ??? ?? Web Artisans', Str::asciiTitle('Ένα πλαίσιο για το web Artisans'));
+    $this->assertEquals('A Framework For Web Artisans', Str::asciiTitle('A framework For web Artisans'));
+  }
 }
