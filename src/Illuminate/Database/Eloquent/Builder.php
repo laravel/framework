6a3c81c78db052d1f832db5e21938be8f0492d37
Taylor Otwell <taylorotwell@gmail.com>
2013-04-19 19:40:28 -0500

    Code reformat.

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -115,7 +115,9 @@ class Builder {
 	 */
 	public function lists($column, $key = null)
 	{
-		$results = $this->query->lists($column, $key ?: $this->model->getKeyName());
+		$key = $key ?: $this->model->getKeyName();
+
+		$results = $this->query->lists($column, $key);
 
 		// If the model has a mutator for the requested column, we will spin through
 		// the results and mutate the values so that the mutated version of these
