e2cc3fd44649f1f15b166306425990d9a1b8c524
Jason Lewis <jason.lewis1991@gmail.com>
2013-04-22 20:34:07 +1000

    Use array language construct instead of literal.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- tests/Routing/RoutingTest.php
+++ tests/Routing/RoutingTest.php
@@ -376,7 +376,7 @@ class RoutingTest extends PHPUnit_Framework_TestCase {
 	public function testWhereAcceptsArrayOfRequirements()
 	{
 		$router = new Router;
-		$router->get('/foo/{name}/{age}', function($name, $age) { return $name.$age; })->where(['age' => '[0-9]+']);
+		$router->get('/foo/{name}/{age}', function($name, $age) { return $name.$age; })->where(array('age' => '[0-9]+'));
 		$request = Request::create('/foo/jason/22', 'GET');
 		$this->assertEquals('jason22', $router->dispatch($request)->getContent());
 	}
