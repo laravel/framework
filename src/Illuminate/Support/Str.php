cbc3eede26838230583554f705bd22603963515a
Maxime Fabre <ehtnam6@gmail.com>
2013-04-26 10:37:21 +0200

    Make Str::words use the unicode of non-breaking space

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -132,7 +132,7 @@ class Str {
 	 */
 	public static function words($value, $words = 100, $end = '...')
 	{
-		if (trim($value, '  ') == '') return '';
+		if (trim($value, chr(0xC2).chr(0xA0).' ') == '') return '';
 
 		preg_match('/^\s*+(?:\S++\s*+){1,'.$words.'}/u', $value, $matches);
 
