6334a0d14dbadfb29870650276fb86e3cb6240ff
Taylor Otwell <taylorotwell@gmail.com>
2013-04-29 14:59:51 -0500

    Remove automatic key setting in Eloquent lists, as it causes problems. Fixes #1069.

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -116,8 +116,6 @@ class Builder {
 	 */
 	public function lists($column, $key = null)
 	{
-		$key = $key ?: $this->model->getKeyName();
-
 		$results = $this->query->lists($column, $key);
 
 		// If the model has a mutator for the requested column, we will spin through
