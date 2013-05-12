d43c15b858ad6b5a69851aa914a925125944cfde
Taylor Otwell <taylorotwell@gmail.com>
2013-05-12 13:57:52 -0500

    Fix storage path hard-codes. Closes #1155.

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -619,7 +619,7 @@ class Application extends Container implements HttpKernelInterface, ResponsePrep
 	 */
 	public function isDownForMaintenance()
 	{
-		return file_exists($this['path'].'/storage/meta/down');
+		return file_exists($this['path.storage'].'/meta/down');
 	}
 
 	/**
