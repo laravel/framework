447eeb0a7d11ba1afbcf9166c907c6f746490b7e
yottaram <jstavis@gmail.com>
2013-05-23 12:09:29 -0400

    Remove .* match from key generate replacement regex

--- src/Illuminate/Foundation/Console/KeyGenerateCommand.php
+++ src/Illuminate/Foundation/Console/KeyGenerateCommand.php
@@ -44,7 +44,7 @@ class KeyGenerateCommand extends Command {
 
 		$key = $this->getRandomKey();
 
-    $contents = preg_replace("/['\"]key['\"].*=>.*,/", "'key' => '$key',", $contents);
+    $contents = preg_replace("/['\"]key['\"]\s*=>[^,]*,/", "'key' => '$key',", $contents);
 
 		$this->files->put($path, $contents);
 
