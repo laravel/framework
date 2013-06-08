2256a981c53da8fa7286f4ad194caf21a807c564
Taylor Otwell <taylorotwell@gmail.com>
2013-06-08 13:10:26 -0500

    pull pretend value from config.

--- src/Illuminate/Mail/MailServiceProvider.php
+++ src/Illuminate/Mail/MailServiceProvider.php
@@ -35,15 +35,22 @@ class MailServiceProvider extends ServiceProvider {
 
 			$mailer->setContainer($app);
 
-			$from = $app['config']['mail.from'];
-
 			// If a "from" address is set, we will set it on the mailer so that all mail
 			// messages sent by the applications will utilize the same "from" address
 			// on each one, which makes the developer's life a lot more convenient.
+			$from = $app['config']['mail.from'];
+
 			if (is_array($from) and isset($from['address']))
 			{
 				$mailer->alwaysFrom($from['address'], $from['name']);
 			}
+
+			// Here we will determine if the mailer should be in "pretend" mode for this
+			// environment, which will simply write out e-mail to the logs instead of
+			// sending it over the web, which is useful for local dev enviornments.
+			$pretend = $app['config']->get('mail.pretend', false);
+
+			$mailer->pretend($pretend);
 
 			return $mailer;
 		});
