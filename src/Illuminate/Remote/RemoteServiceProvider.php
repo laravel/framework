b5fb5d43f5c6edc91e8c5215ddc2b54342111307
Taylor Otwell <taylorotwell@gmail.com>
2013-06-12 22:13:19 -0500

    Fix a few bugs in remote setup.

--- src/Illuminate/Remote/RemoteServiceProvider.php
+++ src/Illuminate/Remote/RemoteServiceProvider.php
@@ -18,7 +18,7 @@ class RemoteServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->app->share('remote', function($app)
+		$this->app['remote'] = $this->app->share(function($app)
 		{
 			return new RemoteManager($app);
 		});
