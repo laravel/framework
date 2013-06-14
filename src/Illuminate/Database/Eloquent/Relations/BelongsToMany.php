6bf616af4e8fe1d75b5313e5fbb117dea07ece20
Taylor Otwell <taylorotwell@gmail.com>
2013-06-13 23:44:49 -0500

    Fix a bug with selecting columns when accessing many to many relation.

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -95,7 +95,12 @@ class BelongsToMany extends Relation {
 	 */
 	public function get($columns = array('*'))
 	{
-		$models = $this->query->getModels($this->getSelectColumns($columns));
+		// If we actually found models we will also eager load any relationships that
+		// have been specified as needing to be eager loaded. This will solve the
+		// n + 1 query problem for the developer and also increase performance.
+		$select = $this->getSelectColumns($columns);
+
+		$models = $this->query->addSelect($select)->getModels();
 
 		$this->hydratePivotRelation($models);
 
