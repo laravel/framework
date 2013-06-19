f4953b907b70acd1bb4248069bc730ed2fc07be9
Adam Lavin <adam@lavoaster.co.uk>
2013-06-19 23:33:41 +0200

    Added mediumText and longText to Schema BluePrint.

--- src/Illuminate/Database/Schema/Grammars/SqlServerGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/SqlServerGrammar.php
@@ -237,6 +237,28 @@ class SqlServerGrammar extends Grammar {
 	}
 
 	/**
+	 * Create the column definition for a medium text type.
+	 *
+	 * @param  \Illuminate\Support\Fluent  $column
+	 * @return string
+	 */
+	protected function typeMediumText(Fluent $column)
+	{
+		return 'nvarchar(max)';
+	}
+
+	/**
+	 * Create the column definition for a long text type.
+	 *
+	 * @param  \Illuminate\Support\Fluent  $column
+	 * @return string
+	 */
+	protected function typeLongText(Fluent $column)
+	{
+		return 'nvarchar(max)';
+	}
+
+	/**
 	 * Create the column definition for a integer type.
 	 *
 	 * @param  \Illuminate\Support\Fluent  $column
