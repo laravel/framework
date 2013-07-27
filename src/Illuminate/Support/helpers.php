bf5747649902c209c3b691e76a2543a3ae4f66ef
Clooner <reg@clooner.nl>
2013-07-27 09:06:37 -0500

    Update helpers.php
    changes e helper method to more predictable behavior.

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -499,7 +499,7 @@ if ( ! function_exists('e'))
 	 */
 	function e($value)
 	{
-		return htmlentities($value, ENT_QUOTES, 'UTF-8', false);
+		return htmlentities($value, ENT_QUOTES, 'UTF-8');
 	}
 }
 
