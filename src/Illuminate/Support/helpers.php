1a2c340fdaa5f860c5bbd608f1c90f13811dcb46
Taylor Otwell <taylorotwell@gmail.com>
2013-08-02 08:31:18 -0500

    Allow optional path into _path helpers.

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -40,11 +40,12 @@ if ( ! function_exists('app_path'))
 	/**
 	 * Get the path to the application folder.
 	 *
+	 * @param   string  $path
 	 * @return  string
 	 */
-	function app_path()
+	function app_path($path = '')
 	{
-		return app('path');
+		return app('path').($path ? '/'.$path : $path);
 	}
 }
 
@@ -402,9 +403,9 @@ if ( ! function_exists('base_path'))
 	 *
 	 * @return string
 	 */
-	function base_path()
+	function base_path($path = '')
 	{
-		return app()->make('path.base');
+		return app()->make('path.base').($path ? '/'.$path : $path);
 	}
 }
 
@@ -634,9 +635,9 @@ if ( ! function_exists('public_path'))
 	 *
 	 * @return string
 	 */
-	function public_path()
+	function public_path($path = '')
 	{
-		return app()->make('path.public');
+		return app()->make('path.public').($path ? '/'.$path : $path);
 	}
 }
 
@@ -722,9 +723,9 @@ if ( ! function_exists('storage_path'))
 	 *
 	 * @return  string
 	 */
-	function storage_path()
+	function storage_path($path = '')
 	{
-		return app('path.storage');
+		return app('path.storage').($path ? '/'.$path : $path);
 	}
 }
 
