68c87c9d3963ab8ec94043a0c5bb637cdd032204
Mike Holler <mike.holler@vgbio.com>
2013-08-13 12:13:48 -0500

    Added support for double datatype in schema builder.

--- tests/Database/DatabaseMySqlSchemaGrammarTest.php
+++ tests/Database/DatabaseMySqlSchemaGrammarTest.php
@@ -372,6 +372,28 @@ class DatabaseMySqlSchemaGrammarTest extends PHPUnit_Framework_TestCase {
 	}
 
 
+	public function testAddingDouble()
+	{
+		$blueprint = new Blueprint('users');
+		$blueprint->double('foo');
+		$statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+		$this->assertEquals(1, count($statements));
+		$this->assertEquals('alter table `users` add `foo` double not null', $statements[0]);
+	}
+
+
+	public function testAddingDoubleSpecifyingPrecision()
+	{
+		$blueprint = new Blueprint('users');
+		$blueprint->double('foo', 15, 8);
+		$statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+		$this->assertEquals(1, count($statements));
+		$this->assertEquals('alter table `users` add `foo` double(15, 8) not null', $statements[0]);
+	}
+
+
 	public function testAddingDecimal()
 	{
 		$blueprint = new Blueprint('users');
