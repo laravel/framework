2e504e48e220613377333a8e8f25c4c680a1abb9
Taylor Otwell <taylorotwell@gmail.com>
2013-08-27 10:08:20 -0500

    Work on polymorphic many-to-many relations.

---
+++ src/Illuminate/Database/Eloquent/Relations/MorphPivot.php
@@ -0,0 +1,47 @@
+<?php namespace Illuminate\Database\Eloquent\Relations;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MorphPivot extends Pivot {
+
+	/**
+	 * Set the keys for a save update query.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\Builder
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
+	protected function setKeysForSaveQuery($query)
+	{
+		$query->where($this->morphType, $this->getAttribute($this->morphType));
+
+		return parent::setKeysForSaveQuery($query);
+	}
+
+	/**
+	 * Delete the pivot model record from the database.
+	 *
+	 * @return int
+	 */
+	public function delete()
+	{
+		$query = $this->getDeleteQuery();
+
+		$query->where($this->morphType, $this->getAttribute($this->morphType));
+
+		return $query->delete();
+	}
+
+	/**
+	 * Set the morph type for the pivot.
+	 *
+	 * @param  string  $morphType
+	 * @return \Illuminate\Database\Eloquent\Relations\MorphPivot
+	 */
+	public function setMorphType($morphType)
+	{
+		$this->morphType = $morphType;
+
+		return $this;
+	}
+
+}
