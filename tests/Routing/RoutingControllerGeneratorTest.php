ff1fd4128771cb9384c6bf236664578f8f945f7d
Taylor Otwell <taylorotwell@gmail.com>
2013-09-05 22:27:58 -0500

    Normalize test results across Windows and Unix.

--- tests/Routing/RoutingControllerGeneratorTest.php
+++ tests/Routing/RoutingControllerGeneratorTest.php
@@ -15,8 +15,15 @@ class RoutingControllerGeneratorTest extends PHPUnit_Framework_TestCase {
 	{
 		$gen = new ControllerGenerator($files = m::mock('Illuminate\Filesystem\Filesystem[put]'));
 		$controller = file_get_contents(__DIR__.'/fixtures/controller.php');
-		$files->shouldReceive('put')->once()->with(__DIR__.'/FooController.php', $controller);
+		$files->shouldReceive('put')->once()->andReturnUsing(function($path, $actual)
+		{
+			$_SERVER['__controller.actual'] = $actual;
+		});
 		$gen->make('FooController', __DIR__);
+
+		$controller = preg_replace('/\s+/', '', $controller);
+		$actual = preg_replace('/\s+/', '', $_SERVER['__controller.actual']);
+		$this->assertEquals($controller, $actual);
 	}
 
 
@@ -24,8 +31,15 @@ class RoutingControllerGeneratorTest extends PHPUnit_Framework_TestCase {
 	{
 		$gen = new ControllerGenerator($files = m::mock('Illuminate\Filesystem\Filesystem[put]'));
 		$controller = file_get_contents(__DIR__.'/fixtures/only_controller.php');
-		$files->shouldReceive('put')->once()->with(__DIR__.'/FooController.php', $controller);
+		$files->shouldReceive('put')->once()->andReturnUsing(function($path, $actual)
+		{
+			$_SERVER['__controller.actual'] = $actual;
+		});
 		$gen->make('FooController', __DIR__, array('only' => array('index', 'show')));
+
+		$controller = preg_replace('/\s+/', '', $controller);
+		$actual = preg_replace('/\s+/', '', $_SERVER['__controller.actual']);
+		$this->assertEquals($controller, $actual);
 	}
 
 
@@ -33,8 +47,15 @@ class RoutingControllerGeneratorTest extends PHPUnit_Framework_TestCase {
 	{
 		$gen = new ControllerGenerator($files = m::mock('Illuminate\Filesystem\Filesystem[put]'));
 		$controller = file_get_contents(__DIR__.'/fixtures/except_controller.php');
-		$files->shouldReceive('put')->once()->with(__DIR__.'/FooController.php', $controller);
+		$files->shouldReceive('put')->once()->andReturnUsing(function($path, $actual)
+		{
+			$_SERVER['__controller.actual'] = $actual;
+		});
 		$gen->make('FooController', __DIR__, array('except' => array('index', 'show')));
+
+		$controller = preg_replace('/\s+/', '', $controller);
+		$actual = preg_replace('/\s+/', '', $_SERVER['__controller.actual']);
+		$this->assertEquals($controller, $actual);
 	}
 
 }
