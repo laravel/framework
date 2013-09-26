8a14e1f8256a81dfbad28663df7084efe4967c5a
Taylor Otwell <taylorotwell@gmail.com>
2013-09-26 11:49:05 -0500

    Redis queue driver tests.

---
+++ tests/Queue/QueueRedisQueueTest.php
@@ -0,0 +1,75 @@
+<?php
+
+use Mockery as m;
+
+class QueueRedisQueueTest extends PHPUnit_Framework_TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
+	public function testPushProperlyPushesJobOntoBeanstalkd()
+	{
+		$queue = $this->getMock('Illuminate\Queue\RedisQueue', array('getRandomId'), array($redis = m::mock('Illuminate\Redis\Database'), 'default'));
+		$queue->expects($this->once())->method('getRandomId')->will($this->returnValue('foo'));
+		$redis->shouldReceive('rpush')->once()->with('default', json_encode(array('job' => 'foo', 'data' => array('data'), 'id' => 'foo', 'attempts' => 1)));
+
+		$queue->push('foo', array('data'));
+	}
+
+
+	public function testDelayedPushProperlyPushesJobOntoBeanstalkd()
+	{
+		$queue = $this->getMock('Illuminate\Queue\RedisQueue', array('getTime', 'getRandomId'), array($redis = m::mock('Illuminate\Redis\Database'), 'default'));
+		$queue->expects($this->once())->method('getRandomId')->will($this->returnValue('foo'));
+		$queue->expects($this->once())->method('getTime')->will($this->returnValue(1));
+
+		$redis->shouldReceive('zadd')->once()->with(
+			'default:delayed',
+			2,
+			json_encode(array('job' => 'foo', 'data' => array('data'), 'id' => 'foo', 'attempts' => 1))
+		);
+
+		$queue->later(1, 'foo', array('data'));
+	}
+
+
+	public function testPopProperlyPopsJobOffOfBeanstalkd()
+	{
+		$queue = $this->getMock('Illuminate\Queue\RedisQueue', array('getTime', 'migrateAllExpiredJobs'), array($redis = m::mock('Illuminate\Redis\Database'), 'default'));
+		$queue->setContainer(m::mock('Illuminate\Container\Container'));
+		$queue->expects($this->once())->method('getTime')->will($this->returnValue(1));
+		$queue->expects($this->once())->method('migrateAllExpiredJobs')->with($this->equalTo('default'));
+		$redis->shouldReceive('lpop')->once()->with('default')->andReturn('foo');
+		$redis->shouldReceive('zadd')->once()->with('default:reserved', 61, 'foo');
+
+		$result = $queue->pop();
+
+		$this->assertInstanceOf('Illuminate\Queue\Jobs\RedisJob', $result);
+	}
+
+
+	public function testReleaseMethod()
+	{
+		$queue = $this->getMock('Illuminate\Queue\RedisQueue', array('getTime'), array($redis = m::mock('Illuminate\Redis\Database'), 'default'));
+		$queue->expects($this->once())->method('getTime')->will($this->returnValue(1));
+		$redis->shouldReceive('zadd')->once()->with('default:delayed', 2, json_encode(array('attempts' => 2)));
+
+		$queue->release('default', json_encode(array('attempts' => 1)), 1, 2);
+	}
+
+
+	public function testMigrateExpiredJobs()
+	{
+		$queue = $this->getMock('Illuminate\Queue\RedisQueue', array('getTime'), array($redis = m::mock('Illuminate\Redis\Database'), 'default'));
+		$queue->expects($this->once())->method('getTime')->will($this->returnValue(1));
+		$redis->shouldReceive('zrangebyscore')->once()->with('from', '-inf', 1)->andReturn(array('foo', 'bar'));
+		$redis->shouldReceive('zremrangebyscore')->once()->with('from', '-inf', 1);
+		$redis->shouldReceive('rpush')->once()->with('to', 'foo', 'bar');
+
+		$queue->migrateExpiredJobs('from', 'to');
+	}
+
+}
