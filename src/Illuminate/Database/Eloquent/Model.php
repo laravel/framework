ae2880b65d8a5ce7fdeb4175958be3b69e8bcf46
Taylor Otwell <taylorotwell@gmail.com>
2013-10-01 15:48:43 -0500

    upgrade mockery... return correct value on isset for eloquent mutators.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -2514,7 +2514,8 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 */
 	public function __isset($key)
 	{
-		return isset($this->attributes[$key]) or isset($this->relations[$key]);
+		return ((isset($this->attributes[$key]) or isset($this->relations[$key])) or
+			    ($this->hasGetMutator($key) and ! is_null($this->getAttributeValue($key))));
 	}
 
 	/**
