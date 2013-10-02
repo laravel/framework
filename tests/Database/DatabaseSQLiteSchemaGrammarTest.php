589e4503af0b33b3ee21bc9077ec6661c538e9af
Taylor Otwell <taylorotwell@gmail.com>
2013-10-02 11:51:10 -0500

    added double method to schema builder.

--- tests/Database/DatabaseSQLiteSchemaGrammarTest.php
+++ tests/Database/DatabaseSQLiteSchemaGrammarTest.php
@@ -297,6 +297,17 @@ class DatabaseSQLiteSchemaGrammarTest extends PHPUnit_Framework_TestCase {
 	}
 
 
+	public function testAddingDouble()
+	{
+		$blueprint = new Blueprint('users');
+		$blueprint->double('foo', 15, 8);
+		$statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+		$this->assertEquals(1, count($statements));
+		$this->assertEquals('alter table "users" add column "foo" float not null', $statements[0]);
+	}
+
+
 	public function testAddingDecimal()
 	{
 		$blueprint = new Blueprint('users');
