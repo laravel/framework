d3fbf95250b7d919e71510bfd3051a447956d97e
tjbp <github.com@tjbp.net>
2013-10-15 15:25:22 +0200

    Add auto_increment and unsigned parameter support to remaining integer types

--- src/Illuminate/Database/Schema/Blueprint.php
+++ src/Illuminate/Database/Schema/Blueprint.php
@@ -425,33 +425,39 @@ class Blueprint {
 	 * Create a new medium integer column on the table.
 	 *
 	 * @param  string  $column
+	 * @param  bool  $autoIncrement
+	 * @param  bool  $unsigned
 	 * @return \Illuminate\Support\Fluent
 	 */
-	public function mediumInteger($column)
+	public function mediumInteger($column, $autoIncrement = false, $unsigned = false)
 	{
-		return $this->addColumn('mediumInteger', $column);
+		return $this->addColumn('mediumInteger', $column, compact('autoIncrement', 'unsigned'));
 	}
 
 	/**
 	 * Create a new tiny integer column on the table.
 	 *
 	 * @param  string  $column
+	 * @param  bool  $autoIncrement
+	 * @param  bool  $unsigned
 	 * @return \Illuminate\Support\Fluent
 	 */
-	public function tinyInteger($column)
+	public function tinyInteger($column, $autoIncrement = false, $unsigned = false)
 	{
-		return $this->addColumn('tinyInteger', $column);
+		return $this->addColumn('tinyInteger', $column, compact('autoIncrement', 'unsigned'));
 	}
 
 	/**
 	 * Create a new small integer column on the table.
 	 *
 	 * @param  string  $column
+	 * @param  bool  $autoIncrement
+	 * @param  bool  $unsigned
 	 * @return \Illuminate\Support\Fluent
 	 */
-	public function smallInteger($column)
+	public function smallInteger($column, $autoIncrement = false, $unsigned = false)
 	{
-		return $this->addColumn('smallInteger', $column);
+		return $this->addColumn('smallInteger', $column, compact('autoIncrement', 'unsigned'));
 	}
 
 	/**
