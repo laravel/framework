0641a8db1b0ec64cfb2ef1949c092ac69b1a4d76
Joseph Silber <contact@josephsilber.com>
2013-10-17 14:17:39 -0400

    Add a unique index to migration column

--- src/Illuminate/Database/Migrations/DatabaseMigrationRepository.php
+++ src/Illuminate/Database/Migrations/DatabaseMigrationRepository.php
@@ -121,7 +121,7 @@ class DatabaseMigrationRepository implements MigrationRepositoryInterface {
 			// The migrations table is responsible for keeping track of which of the
 			// migrations have actually run for the application. We'll create the
 			// table to hold the migration file's path as well as the batch ID.
-			$table->string('migration');
+			$table->string('migration')->unique();
 
 			$table->integer('batch');
 		});
