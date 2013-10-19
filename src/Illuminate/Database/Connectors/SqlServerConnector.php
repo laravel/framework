6f5990373fc67a31c6ebf5e24a8c4a1381ce4eb3
fturmel <fturmel@gmail.com>
2013-10-19 15:48:07 -0400

    Update SqlServerConnector.php

--- src/Illuminate/Database/Connectors/SqlServerConnector.php
+++ src/Illuminate/Database/Connectors/SqlServerConnector.php
@@ -19,8 +19,8 @@ class SqlServerConnector extends Connector implements ConnectorInterface {
 	/**
 	 * Establish a database connection.
 	 *
-	 * @param  array  $options
-	 * @return PDO
+	 * @param  array $config
+	 * @return \PDO
 	 */
 	public function connect(array $config)
 	{
