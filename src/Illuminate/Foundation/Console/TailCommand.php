f62d1a727155073357babee95a8f679003a8b93c
Taylor Otwell <taylorotwell@gmail.com>
2013-10-20 20:24:06 -0500

    Consolidate "live" debug into tail command.

--- src/Illuminate/Foundation/Console/TailCommand.php
+++ src/Illuminate/Foundation/Console/TailCommand.php
@@ -1,6 +1,7 @@
 <?php namespace Illuminate\Foundation\Console;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
@@ -27,16 +28,11 @@ class TailCommand extends Command {
 	 */
 	public function fire()
 	{
-		$path = $this->getPath();
+		$path = $this->getPath($this->argument('connection'));
 
 		if ($path)
 		{
-			$output = $this->output;
-
-			$this->getConnection()->run('tail -f '.$path, function($line) use ($output)
-			{
-				$output->write($line);
-			});
+			$this->tailLogFile($path, $this->argument('connection'));
 		}
 		else
 		{
@@ -45,25 +41,105 @@ class TailCommand extends Command {
 	}
 
 	/**
+	 * Tail the given log file for the connection.
+	 *
+	 * @param  string  $path
+	 * @param  string  $connection
+	 * @return void
+	 */
+	protected function tailLogFile($path, $connection)
+	{
+		if (is_null($connection))
+		{
+			$this->tailLocalLogs($path);
+		}
+		else
+		{
+			$this->tailRemoteLogs($path, $connection);
+		}
+	}
+
+	/**
+	 * Tail a local log file for the application.
+	 *
+	 * @param  string  $path
+	 * @return string
+	 */
+	protected function tailLocalLogs($path)
+	{
+		$this->registerQueryLogger();
+
+		$output = $this->output;
+
+		with(new Process('tail -f '.$path))->run(function($type, $line) use ($output)
+		{
+			$output->write($line);
+		});
+	}
+
+	/**
+	 * Register a query logger for local tailing convenience.
+	 *
+	 * @return void
+	 */
+	protected function registerQueryLogger()
+	{
+		$app = $this->laravel;
+
+		$this->laravel['db']->listen(function($sql, $bindings, $time) use ($app)
+		{
+			$sql = str_replace_array('\?', $bindings, $sql);
+
+			$app['log']->debug($sql.' ['.$time.'ms]');
+		});
+	}
+
+	/**
+	 * Tail a remote log file at the given path and connection.
+	 *
+	 * @param  string  $path
+	 * @param  string  $connection
+	 * @return void
+	 */
+	protected function tailRemoteLogs($path, $connection)
+	{
+		$out = $this->output;
+
+		$this->getRemote($connection)->run('tail -f '.$path, function($line) use ($out)
+		{
+			$out->write($line);
+		});
+	}
+
+	/**
 	 * Get a connection to the remote server.
 	 *
+	 * @param  string  $connection
 	 * @return \Illuminate\Remote\Connection
 	 */
-	protected function getConnection()
+	protected function getRemote($connection)
 	{
-		return $this->laravel['remote']->connection($this->argument('connection'));
+		return $this->laravel['remote']->connection($connection);
 	}
 
 	/**
 	 * Get the path to the Laraevl log file.
 	 *
+	 * @param  string  $connection
 	 * @return string
 	 */
-	protected function getPath()
+	protected function getPath($connection)
 	{
 		if ($this->option('path')) return $this->option('path');
 
-		return $this->getRoot($this->argument('connection')).'/app/storage/logs/laravel.log';
+		if (is_null($connection))
+		{
+			return base_path().'/app/storage/logs/laravel.log';
+		}
+		else
+		{
+			return $this->getRoot($connection).'/app/storage/logs/laravel.log';
+		}
 	}
 
 	/**
@@ -85,7 +161,7 @@ class TailCommand extends Command {
 	protected function getArguments()
 	{
 		return array(
-			array('connection', InputArgument::REQUIRED, 'The remote connection name'),
+			array('connection', InputArgument::OPTIONAL, 'The remote connection name'),
 		);
 	}
 
