78a6883bc8f87dd68f42a147a2a4294add115099
Taylor Otwell <taylorotwell@gmail.com>
2013-10-25 17:23:24 -0500

    Work on cookie signing middleware.

--- tests/Cookie/CookieTest.php
+++ tests/Cookie/CookieTest.php
@@ -2,7 +2,6 @@
 
 use Mockery as m;
 use Illuminate\Cookie\CookieJar;
-use Illuminate\Encryption\Encrypter;
 use Symfony\Component\HttpFoundation\Request;
 
 class CookieTest extends PHPUnit_Framework_TestCase {
@@ -18,16 +17,14 @@ class CookieTest extends PHPUnit_Framework_TestCase {
 		$cookie = $this->getCreator();
 		$cookie->setDefaultPathAndDomain('foo', 'bar');
 		$c = $cookie->make('color', 'blue', 10, '/path', '/domain', true, false);
-		$value = $cookie->getEncrypter()->decrypt($c->getValue());
-		$this->assertEquals('blue', $value);
+		$this->assertEquals('blue', $c->getValue());
 		$this->assertFalse($c->isHttpOnly());
 		$this->assertTrue($c->isSecure());
 		$this->assertEquals('/domain', $c->getDomain());
 		$this->assertEquals('/path', $c->getPath());
 
 		$c2 = $cookie->forever('color', 'blue', '/path', '/domain', true, false);
-		$value = $cookie->getEncrypter()->decrypt($c2->getValue());
-		$this->assertEquals('blue', $value);
+		$this->assertEquals('blue', $c2->getValue());
 		$this->assertFalse($c2->isHttpOnly());
 		$this->assertTrue($c2->isSecure());
 		$this->assertEquals('/domain', $c2->getDomain());
@@ -40,8 +37,7 @@ class CookieTest extends PHPUnit_Framework_TestCase {
 		$cookie = $this->getCreator();
 		$cookie->setDefaultPathAndDomain('/path', '/domain');
 		$c = $cookie->make('color', 'blue', 10, null, null, true, false);
-		$value = $cookie->getEncrypter()->decrypt($c->getValue());
-		$this->assertEquals('blue', $value);
+		$this->assertEquals('blue', $c->getValue());
 		$this->assertFalse($c->isHttpOnly());
 		$this->assertTrue($c->isSecure());
 		$this->assertEquals('/domain', $c->getDomain());
@@ -52,24 +48,17 @@ class CookieTest extends PHPUnit_Framework_TestCase {
 	public function testCookiesAreProperlyParsed()
 	{
 		$cookie = $this->getCreator();
-		$value = $cookie->getEncrypter()->encrypt('bar');
-		$cookie->getRequest()->cookies->set('foo', $value);
+		$cookie->getRequest()->cookies->set('foo', 'bar');
 		$this->assertEquals('bar', $cookie->get('foo'));
         $this->assertEquals('zee', $cookie->get('someOtherFoo', 'zee'));
-        
-		$cookie = $this->getCreator();
-		$value = $cookie->getEncrypter()->encrypt('bar');
-		$value = str_shuffle($value);
-		$cookie->getRequest()->cookies->set('foo', $value);
-		$this->assertNull($cookie->get('foo'));
 	}
 
 	public function testQueuedCookies()
 	{
 		$cookie = $this->getCreator();
-		$this->assertEmpty($cookie->getQueuedCookies()); // better not be anything in the array yet
+		$this->assertEmpty($cookie->getQueuedCookies());
 		$cookie->queue($cookie->make('foo','bar'));
-		$this->assertArrayHasKey('foo',$cookie->getQueuedCookies());
+		$this->assertArrayHasKey('foo', $cookie->getQueuedCookies());
 	}
 
 	public function testUnqueue()
@@ -83,7 +72,7 @@ class CookieTest extends PHPUnit_Framework_TestCase {
 
 	public function getCreator()
 	{
-		return new CookieJar(Request::create('/foo', 'GET'), new Encrypter(str_repeat('a', 16)), array(
+		return new CookieJar(Request::create('/foo', 'GET'), array(
 			'path'     => '/path',
 			'domain'   => '/domain',
 			'secure'   => true,
