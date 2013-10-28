58ec2fe05f092bdd09f2e05d8a86633b97417cef
Taylor Otwell <taylorotwell@gmail.com>
2013-10-28 09:46:27 -0500

    Add get method to cookie facade.

--- src/Illuminate/Support/Facades/Cookie.php
+++ src/Illuminate/Support/Facades/Cookie.php
@@ -3,6 +3,18 @@
 class Cookie extends Facade {
 
 	/**
+	 * Retrieve a cookie from the request.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $default
+	 * @return string
+	 */
+	public static function get($key = null, $default = null)
+	{
+		return static::$app['request']->cookie($key, $default);
+	}
+
+	/**
 	 * Get the registered name of the component.
 	 *
 	 * @return string
