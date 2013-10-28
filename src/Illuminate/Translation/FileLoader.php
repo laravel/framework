5483042e8762d4f8ffc356579ddaae4f7dc3cbc9
Taylor Otwell <taylorotwell@gmail.com>
2013-10-28 15:27:42 -0500

    A new packages directory within `lang` can now override package language files.

--- src/Illuminate/Translation/FileLoader.php
+++ src/Illuminate/Translation/FileLoader.php
@@ -70,10 +70,33 @@ class FileLoader implements LoaderInterface {
 	{
 		if (isset($this->hints[$namespace]))
 		{
-			return $this->loadPath($this->hints[$namespace], $locale, $group);
+			$lines = $this->loadPath($this->hints[$namespace], $locale, $group);
+
+			return $this->loadNamespaceOverrides($lines, $locale, $group, $namespace);
 		}
 
 		return array();
+	}
+
+	/**
+	 * Load a local namespaced translation group for overrides.
+	 *
+	 * @param  array  $lines
+	 * @param  string  $locale
+	 * @param  string  $group
+	 * @param  string  $namespace
+	 * @return array
+	 */
+	protected function loadNamespaceOverrides(array $lines, $locale, $group, $namespace)
+	{
+		$file = "{$this->path}/packages/{$locale}/{$namespace}/{$group}.php";
+
+		if ($this->files->exists($file))
+		{
+			return array_replace_recursive($lines, $this->files->getRequire($file));
+		}
+
+		return $lines;
 	}
 
 	/**
