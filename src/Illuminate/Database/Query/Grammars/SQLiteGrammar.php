c51364e212979700d0f7962b8307d01a4d4f2442
Taylor Otwell <taylorotwell@gmail.com>
2013-10-29 08:22:17 -0500

    Remove special SQLite order by logic.

--- src/Illuminate/Database/Query/Grammars/SQLiteGrammar.php
+++ src/Illuminate/Database/Query/Grammars/SQLiteGrammar.php
@@ -5,24 +5,6 @@ use Illuminate\Database\Query\Builder;
 class SQLiteGrammar extends Grammar {
 
 	/**
-	 * Compile the "order by" portions of the query.
-	 *
-	 * @param  \Illuminate\Database\Query\Builder  $query
-	 * @param  array  $orders
-	 * @return string
-	 */
-	protected function compileOrders(Builder $query, $orders)
-	{
-		$me = $this;
-
-		return 'order by '.implode(', ', array_map(function($order) use ($me)
-		{
-			return $me->wrap($order['column']).' collate nocase '.$order['direction'];
-		}
-		, $orders));
-	}
-
-	/**
 	 * Compile an insert statement into SQL.
 	 *
 	 * @param  \Illuminate\Database\Query\Builder  $query
