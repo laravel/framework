f1e93bf398e02790da812565bd989b50ca92492e
Taylor Otwell <taylorotwell@gmail.com>
2013-10-30 16:51:43 -0500

    Fix class name.

--- src/Illuminate/Foundation/Providers/ConsoleSupportServiceProvider.php
+++ src/Illuminate/Foundation/Providers/ConsoleSupportServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class ConsoleSupportProvider extends ServiceProvider {
+class ConsoleSupportServiceProvider extends ServiceProvider {
 
 	/**
 	 * The provider class names.
