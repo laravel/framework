cebad54cea3ccb23625cca3d8d65307a76f32dd7
Taylor Otwell <taylorotwell@gmail.com>
2013-10-31 09:18:24 -0500

    Check is_string.

--- src/Illuminate/Translation/Translator.php
+++ src/Illuminate/Translation/Translator.php
@@ -98,7 +98,7 @@ class Translator extends NamespacedItemResolver implements TranslatorInterface {
 	{
 		$line = array_get($this->loaded[$namespace][$group][$locale], $item);
 
-		if ( ! is_null($line)) return $this->makeReplacements($line, $replace);
+		if (is_string($line)) return $this->makeReplacements($line, $replace);
 	}
 
 	/**
