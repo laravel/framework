41ee22ac08964ad3cb500e26eb88f477f326f614
Taylor Otwell <taylorotwell@gmail.com>
2013-11-01 13:56:21 -0500

    Tests for Redis cache tagging.

--- tests/Cache/CacheTaggedCacheTest.php
+++ tests/Cache/CacheTaggedCacheTest.php
@@ -1,8 +1,15 @@
 <?php
 
+use Mockery as m;
 use Illuminate\Cache\ArrayStore;
 
 class CacheTaggedCacheTest extends PHPUnit_Framework_TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
 
 	public function testSectionCanBeFlushed()
 	{
@@ -34,6 +41,42 @@ class CacheTaggedCacheTest extends PHPUnit_Framework_TestCase {
 		$store->tag('zap')->flush();
 		$this->assertNull($store->tags($tags1)->get('foo'));
 		$this->assertEquals('bar', $store->tags($tags2)->get('foo'));
+	}
+
+
+	public function testRedisCacheTagsPushForeverKeysCorrectly()
+	{
+		$store = m::mock('Illuminate\Cache\StoreInterface');
+		$tagSet = m::mock('Illuminate\Cache\TagSet', array($store, array('foo', 'bar')));
+		$tagSet->shouldReceive('getNamespace')->andReturn('foo|bar');
+		$redis = new Illuminate\Cache\RedisTaggedCache($store, $tagSet);
+		$store->shouldReceive('getPrefix')->andReturn('prefix:');
+		$store->shouldReceive('connection')->andReturn($conn = m::mock('StdClass'));
+		$conn->shouldReceive('lpush')->once()->with('prefix:foo:forever', 'prefix:'.sha1('foo|bar').':key1');
+		$conn->shouldReceive('lpush')->once()->with('prefix:bar:forever', 'prefix:'.sha1('foo|bar').':key1');
+		$store->shouldReceive('forever')->with(sha1('foo|bar').':key1', 'key1:value');
+
+		$redis->forever('key1', 'key1:value');
+	}
+
+
+	public function testRedisCacheForeverTagsCanBeFlushed()
+	{
+		$store = m::mock('Illuminate\Cache\StoreInterface');
+		$tagSet = m::mock('Illuminate\Cache\TagSet', array($store, array('foo', 'bar')));
+		$tagSet->shouldReceive('getNamespace')->andReturn('foo|bar');
+		$redis = new Illuminate\Cache\RedisTaggedCache($store, $tagSet);
+		$store->shouldReceive('getPrefix')->andReturn('prefix:');
+		$store->shouldReceive('connection')->andReturn($conn = m::mock('StdClass'));
+		$conn->shouldReceive('lrange')->once()->with('prefix:foo:forever', 0, -1)->andReturn(array('key1', 'key2'));
+		$conn->shouldReceive('lrange')->once()->with('prefix:bar:forever', 0, -1)->andReturn(array('key3'));
+		$conn->shouldReceive('del')->once()->with('key1', 'key2');
+		$conn->shouldReceive('del')->once()->with('key3');
+		$conn->shouldReceive('del')->once()->with('prefix:foo:forever');
+		$conn->shouldReceive('del')->once()->with('prefix:bar:forever');
+		$tagSet->shouldReceive('reset')->once();
+
+		$redis->flush();
 	}
 
 }
