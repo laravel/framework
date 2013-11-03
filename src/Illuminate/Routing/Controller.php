f36a4d4678ad7f690b8d30d47132bf59054842f7
Mior Muhammad Zaki <crynobone@gmail.com>
2013-11-03 21:31:43 +0800

    Add Illuminate\Routing\Controller::callAction() based on 4.0 and setupLayout from there, this allow BC to be maintained for upgrading Laravel 4.0 to 4.1 while using setupLayout().

--- src/Illuminate/Routing/Controller.php
+++ src/Illuminate/Routing/Controller.php
@@ -34,16 +34,6 @@ abstract class Controller {
 	protected $layout;
 
 	/**
-	 * Create a new Controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->setupLayout();
-	}
-
-	/**
 	 * Register a "before" filter on the controler.
 	 *
 	 * @param  \Closure|string  $name
@@ -187,6 +177,30 @@ abstract class Controller {
 	protected function setupLayout() {}
 
 	/**
+	 * Execute an action on the controller.
+	 *
+	 * @param string  $method
+	 * @param array   $parameters
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function callAction($method, $parameters)
+	{
+		$this->setupLayout();
+
+		$response = call_user_func_array(array($this, $method), $parameters);
+
+		// If no response is returned from the controller action and a layout is being
+		// used we will assume we want to just return the layout view as any nested
+		// views were probably bound on this view during this controller actions.
+		if (is_null($response) and ! is_null($this->layout))
+		{
+			$response = $this->layout;
+		}
+
+		return $response;
+	}
+
+	/**
 	 * Get the layout used by the controller.
 	 *
 	 * @return \Illuminate\View\View|null
@@ -200,7 +214,7 @@ abstract class Controller {
 	 * Handle calls to missing methods on the controller.
 	 *
 	 * @param  string  $method
-	 * @param  array  $parameters
+	 * @param  array   $parameters
 	 * @return mixed
 	 */
 	public function missingMethod($method, $parameters)
