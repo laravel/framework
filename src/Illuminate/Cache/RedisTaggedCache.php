4e8fe094179116471e64c336d8516d4dd483e6ea
Taylor Otwell <taylorotwell@gmail.com>
2013-11-04 19:51:53 -0600

    Remove extra space.

--- src/Illuminate/Cache/RedisTaggedCache.php
+++ src/Illuminate/Cache/RedisTaggedCache.php
@@ -13,7 +13,7 @@ class RedisTaggedCache extends TaggedCache {
 	{
 		$this->pushForeverKeys($namespace = $this->tags->getNamespace(), $key);
 
-		parent::forever( $key, $value);
+		parent::forever($key, $value);
 	}
 
 	/**
