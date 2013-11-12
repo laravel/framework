1c0bfba2058ec633d38927d6211d7a09d71c7b5d
Jonathan Coetzee <jon@thancoetzee.com>
2013-11-12 09:51:15 +0200

    Make password reset more efficient by storing id of the user instead of the email address

--- src/Illuminate/Auth/Reminders/DatabaseReminderRepository.php
+++ src/Illuminate/Auth/Reminders/DatabaseReminderRepository.php
@@ -58,14 +58,14 @@ class DatabaseReminderRepository implements ReminderRepositoryInterface {
 	 */
 	public function create(RemindableInterface $user)
 	{
-		$email = $user->getAuthIdentifier();
+		$id = $user->getAuthIdentifier();
 
 		// We will create a new, random token for the user so that we can e-mail them
 		// a safe link to the password reset form. Then we will insert a record in
 		// the database so that we can verify the token within the actual reset.
 		$token = $this->createNewToken($user);
 
-		$this->getTable()->insert($this->getPayload($email, $token));
+		$this->getTable()->insert($this->getPayload($id, $token));
 
 		return $token;
 	}
