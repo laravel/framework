7e19c9dcad13c740d0c0d518c86757a5c889a1b3
Taylor Otwell <taylorotwell@gmail.com>
2013-11-14 17:28:40 -0600

    Work on reminder system.

--- src/Illuminate/Support/Facades/Password.php
+++ src/Illuminate/Support/Facades/Password.php
@@ -3,6 +3,41 @@
 class Password extends Facade {
 
 	/**
+	 * Constant representing a successfully sent reminder.
+	 *
+	 * @var int
+	 */
+	const REMINDER_SENT = 1;
+
+	/**
+	 * Constant representing a successfully reset password.
+	 *
+	 * @var int
+	 */
+	const PASSWORD_RESET = 2;
+
+	/**
+	 * Constant representing the user not found response.
+	 *
+	 * @var int
+	 */
+	const INVALID_USER = 3;
+
+	/**
+	 * Constant representing an invalid password.
+	 *
+	 * @var int
+	 */
+	const INVALID_PASSWORD = 4;
+
+	/**
+	 * Constant representing an invalid token.
+	 *
+	 * @var int
+	 */
+	const INVALID_TOKEN = 5;
+
+	/**
 	 * Get the registered name of the component.
 	 *
 	 * @return string
