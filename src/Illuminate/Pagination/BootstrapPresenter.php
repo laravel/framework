940b6cfc8119fde099f6b3e06d04d5bcd4f9a192
Taylor Otwell <taylorotwell@gmail.com>
2013-11-26 14:39:47 -0600

    Abstract some pagination presenter methods for easier extension.

--- src/Illuminate/Pagination/BootstrapPresenter.php
+++ src/Illuminate/Pagination/BootstrapPresenter.php
@@ -2,19 +2,38 @@
 
 class BootstrapPresenter extends Presenter {
 
-	public function getActivePageWrapper($text) 
+	/**
+	 * Get HTML wrapper for a page link.
+	 *
+	 * @param  string  $url
+	 * @param  int  $page
+	 * @return string
+	 */
+	public function getPageLinkWrapper($url, $page)
 	{
-		return '<li class="active"><span>' . $text . '</span></li>';
+		return '<li><a href="'.$url.'">'.$page.'</a></li>';
 	}
 
-	public function getDisabledTextWrapper($text) 
+	/**
+	 * Get HTML wrapper for disabled text.
+	 *
+	 * @param  string  $text
+	 * @return string
+	 */
+	public function getDisabledTextWrapper($text)
 	{
-		return '<li class="disabled"><span>' . $text . '</span></li>';
+		return '<li class="disabled"><span>'.$text.'</span></li>';
 	}
 
-	public function getPageLinkWrapper($url, $page) 
+	/**
+	 * Get HTML wrapper for active text.
+	 *
+	 * @param  string  $text
+	 * @return string
+	 */
+	public function getActivePageWrapper($text)
 	{
-		return '<li><a href="' . $url . '">' . $page . '</a></li>';
+		return '<li class="active"><span>'.$text.'</span></li>';
 	}
 
 }
