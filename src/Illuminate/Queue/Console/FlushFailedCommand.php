534b8589fcaa85c565a4ee2a5d3a1870d85cc684
Taylor Otwell <taylorotwell@gmail.com>
2013-11-26 11:13:50 -0600

    Change how commands use stuff.

--- src/Illuminate/Queue/Console/FlushFailedCommand.php
+++ src/Illuminate/Queue/Console/FlushFailedCommand.php
@@ -3,7 +3,6 @@
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
-use Illuminate\Queue\Failed\FailedJobProviderInterface;
 
 class FlushFailedCommand extends Command {
 
@@ -22,33 +21,13 @@ class FlushFailedCommand extends Command {
 	protected $description = 'Flush all of the failed queue jobs';
 
 	/**
-	 * The failed job provider implementation.
-	 *
-	 * @var \Illuminate\Queue\Failed\FailedJobProviderInterface
-	 */
-	protected $failer;
-
-	/**
-	 * Create a new failed job lister command instance.
-	 *
-	 * @param  \Illuminate\Queue\Failed\FailedJobProviderInterface  $failer
-	 * @return void
-	 */
-	public function __construct(FailedJobProviderInterface $failer)
-	{
-		parent::__construct();
-
-		$this->failer = $failer;
-	}
-
-	/**
 	 * Execute the console command.
 	 *
 	 * @return void
 	 */
 	public function fire()
 	{
-		$this->failer->flush();
+		$this->laravel['queue.failer']->flush();
 
 		$this->info('All failed jobs deleted successfully!');
 	}
