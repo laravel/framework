e9c2852ed68599cafc0a22cbb9c81508fad5d87b
Taylor Otwell <taylorotwell@gmail.com>
2013-11-28 19:45:25 -0600

    Work on console structure.

--- src/Illuminate/Console/Application.php
+++ src/Illuminate/Console/Application.php
@@ -1,5 +1,7 @@
 <?php namespace Illuminate\Console;
 
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
@@ -45,6 +47,28 @@ class Application extends \Symfony\Component\Console\Application {
 		}
 
 		return $artisan;
+	}
+
+	/**
+	 * Run an Artisan console command by name.
+	 *
+	 * @param  string  $command
+	 * @param  array   $parameters
+	 * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+	 * @return void
+	 */
+	public function call($command, array $parameters = array(), OutputInterface $output = null)
+	{
+		$parameters['command'] = $command;
+
+		// Unless an output interface implementation was specifically passed to us we
+		// will use the "NullOutput" implementation by default to keep any writing
+		// suppressed so it doesn't leak out to the browser or any other source.
+		$output = $output ?: new NullOutput;
+
+		$input = new ArrayInput($parameters);
+
+		return $this->find($command)->run($input, $output);
 	}
 
 	/**
