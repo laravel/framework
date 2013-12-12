6ddae3b4acc71d376277bd3d33a85ff320a09aa9
Taylor Otwell <taylorotwell@gmail.com>
2013-12-12 07:23:21 -0600

    Fix version.

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -26,7 +26,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
 	 *
 	 * @var string
 	 */
-	const VERSION = '4.1.0';
+	const VERSION = '4.2-dev';
 
 	/**
 	 * Indicates if the application has "booted".
