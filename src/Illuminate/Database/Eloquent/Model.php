0075ff7f7ffe6c2a8baa03d5ddb25bd299921a1b
Taylor Otwell <taylorotwell@gmail.com>
2013-12-28 13:38:15 -0600

    cleaning up some code.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1598,25 +1598,14 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	}
 
 	/**
-         * Get a new base query builder for the model's query builder.
-         *
-         * @param  \Illuminate\Database\Query\Builder $baseQueryBuilder
-         * @return \Illuminate\Database\Eloquent\Builder|static
-         */
-        public function newQueryBuilder($baseQueryBuilder)
-        {
-                return new Builder($baseQueryBuilder);
-        }
-
-        /**
-         * Get a new query builder for the model's table.
-         *
-         * @param  bool  $excludeDeleted
-         * @return \Illuminate\Database\Eloquent\Builder|static
-         */
-        public function newQuery($excludeDeleted = true)
-        {
-                $builder = $this->newQueryBuilder($this->newBaseQueryBuilder());
+	 * Get a new query builder for the model's table.
+	 *
+	 * @param  bool  $excludeDeleted
+	 * @return \Illuminate\Database\Eloquent\Builder|static
+	 */
+	public function newQuery($excludeDeleted = true)
+	{
+		$builder = $this->newEloquentBuilder($this->newBaseQueryBuilder());
 
 		// Once we have the query builders, we will set the model instances so the
 		// builder can easily access any information it may need from the model
@@ -1639,6 +1628,17 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	public function newQueryWithDeleted()
 	{
 		return $this->newQuery(false);
+	}
+
+	/**
+	 * Create a new Eloquent query builder for the model.
+	 *
+	 * @param  \Illuminate\Database\Query\Builder $query
+	 * @return \Illuminate\Database\Eloquent\Builder|static
+	 */
+	public function newEloquentBuilder($query)
+	{
+		return new Builder($query);
 	}
 
 	/**
@@ -2794,7 +2794,7 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	public function __isset($key)
 	{
 		return ((isset($this->attributes[$key]) || isset($this->relations[$key])) ||
-			    ($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key))));
+				($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key))));
 	}
 
 	/**
