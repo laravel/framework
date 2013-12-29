b7f24687285fb655a69675567a8716be8c7eb00a
Taylor Otwell <taylorotwell@gmail.com>
2013-12-28 19:29:53 -0600

    Increment version.

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -26,7 +26,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
 	 *
 	 * @var string
 	 */
-	const VERSION = '4.1.10';
+	const VERSION = '4.1.11';
 
 	/**
 	 * Indicates if the application has "booted".
