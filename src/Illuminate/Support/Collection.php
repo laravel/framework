33c8dea9422b0cb59fa1e0e2206a921f8d6272f2
Bailey Parker <baileyparker13@gmail.com>
2014-01-05 10:47:19 -0500

    Added descending sort option to Collection::sortBy()

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -254,9 +254,10 @@ class Collection implements ArrayAccess, ArrayableInterface, Countable, Iterator
 	 *
 	 * @param  \Closure  $callback
 	 * @param  int  $options
+	 * @param  bool  $descending
 	 * @return \Illuminate\Support\Collection
 	 */
-	public function sortBy(Closure $callback, $options = SORT_REGULAR)
+	public function sortBy(Closure $callback, $options = SORT_REGULAR, $descending = false)
 	{
 		$results = array();
 
@@ -268,7 +269,14 @@ class Collection implements ArrayAccess, ArrayableInterface, Countable, Iterator
 			$results[$key] = $callback($value);
 		}
 
-		asort($results, $options);
+		if (!$descending)
+		{
+			asort($results, $options);
+		}
+		else
+		{
+			arsort($results, $options);
+		}
 
 		// Once we have sorted all of the keys in the array, we will loop through them
 		// and grab the corresponding model so we can set the underlying items list
