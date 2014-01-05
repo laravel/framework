a172036533ee3e627c1d2c68d560cf27a6b1c8ee
Graham Campbell <graham@mineuk.com>
2014-01-05 18:14:17 +0000

    Unused database use statements and @throws

--- src/Illuminate/Database/Eloquent/Relations/BelongsTo.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsTo.php
@@ -1,6 +1,5 @@
 <?php namespace Illuminate\Database\Eloquent\Relations;
 
-use LogicException;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Expression;
@@ -81,8 +80,6 @@ class BelongsTo extends Relation {
 	 *
 	 * @param  \Illuminate\Database\Eloquent\Builder  $query
 	 * @return \Illuminate\Database\Eloquent\Builder
-	 *
-	 * @throws \LogicException
 	 */
 	public function getRelationCountQuery(Builder $query)
 	{
