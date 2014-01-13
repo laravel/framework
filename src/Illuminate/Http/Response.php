908288d59611ecc1b76587e48ae7eb3d04f6c357
Taylor Otwell <taylorotwell@gmail.com>
2014-01-13 17:20:21 -0600

    Add Response trait to dry code.

--- src/Illuminate/Http/Response.php
+++ src/Illuminate/Http/Response.php
@@ -7,40 +7,14 @@ use Illuminate\Support\Contracts\RenderableInterface;
 
 class Response extends \Symfony\Component\HttpFoundation\Response {
 
+	use ResponseTrait;
+
 	/**
 	 * The original content of the response.
 	 *
 	 * @var mixed
 	 */
 	public $original;
-
-	/**
-	 * Set a header on the Response.
-	 *
-	 * @param  string  $key
-	 * @param  string  $value
-	 * @param  bool    $replace
-	 * @return \Illuminate\Http\Response
-	 */
-	public function header($key, $value, $replace = true)
-	{
-		$this->headers->set($key, $value, $replace);
-
-		return $this;
-	}
-
-	/**
-	 * Add a cookie to the response.
-	 *
-	 * @param  \Symfony\Component\HttpFoundation\Cookie  $cookie
-	 * @return \Illuminate\Http\Response
-	 */
-	public function withCookie(Cookie $cookie)
-	{
-		$this->headers->setCookie($cookie);
-
-		return $this;
-	}
 
 	/**
 	 * Set the content on the response.
