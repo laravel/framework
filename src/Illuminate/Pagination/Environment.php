488ea6402046b44bea446468034b8b502a96fdd2
Taylor Otwell <taylorotwell@gmail.com>
2014-01-13 11:07:03 -0600

    Shadow factories.

---
+++ src/Illuminate/Pagination/Environment.php
@@ -0,0 +1,23 @@
+<?php namespace Illuminate\Pagination;
+
+use Illuminate\Http\Request;
+use Illuminate\View\Factory as ViewFactory;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class Environment extends Factory {
+
+	/**
+	 * Create a new pagination factory.
+	 *
+	 * @param  \Symfony\Component\HttpFoundation\Request  $request
+	 * @param  \Illuminate\View\Factory  $view
+	 * @param  \Symfony\Component\Translation\TranslatorInterface  $trans
+	 * @param  string  $pageName
+	 * @return void
+	 */
+	public function __construct(Request $request, ViewFactory $view, TranslatorInterface $trans, $pageName = 'page')
+	{
+		parent::__construct($request, $view, $trans, $pageName);
+	}
+
+}
