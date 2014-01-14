588aee7860d0435354f3378040e7a2733c34f2a8
kylekatarnls <kylekatarnls@gmail.com>
2014-01-14 19:45:15 +0100

    Available Memcache

--- src/Illuminate/Cache/CacheManager.php
+++ src/Illuminate/Cache/CacheManager.php
@@ -50,6 +50,20 @@ class CacheManager extends Manager {
 		return $this->repository(new MemcachedStore($memcached, $this->getPrefix()));
 	}
 
+        /**
+         * Create an instance of the Memcache cache driver.
+         *
+         * @return \Illuminate\Cache\MemcacheStore
+         */
+        protected function createMemcacheDriver()
+        {
+                $servers = $this->app['config']['cache.memcache'];
+
+                $memcache = $this->app['memcache.connector']->connect($servers);
+
+                return $this->repository(new MemcacheStore($memcache, $this->getPrefix()));
+        }
+
 	/**
 	 * Create an instance of the WinCache cache driver.
 	 *
