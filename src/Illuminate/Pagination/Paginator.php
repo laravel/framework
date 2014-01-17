87c103668fbee05065908b2847552a16afaa5407
Maxime Fabre <ehtnam6@gmail.com>
2014-01-17 13:54:46 +0100

    Make Paginator decorate Collection methods

--- src/Illuminate/Pagination/Paginator.php
+++ src/Illuminate/Pagination/Paginator.php
@@ -98,6 +98,18 @@ class Paginator implements ArrayableInterface, ArrayAccess, Countable, IteratorA
 	}
 
 	/**
+	 * Call a method on the underlying Collection
+	 *
+	 * @param string $method
+	 * @param array  $arguments
+	 * @return mixed
+	 */
+	public function __call($method, $arguments)
+	{
+		return call_user_func_array(array($this->getCollection(), $method), $arguments);
+	}
+
+	/**
 	 * Setup the pagination context (current and last page).
 	 *
 	 * @return \Illuminate\Pagination\Paginator
