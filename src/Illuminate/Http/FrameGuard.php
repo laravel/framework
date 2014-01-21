13f00c8e26a792da3817f47c07e1e5f48cd84b0f
kohkimakimoto <kohki.makimoto@gmail.com>
2014-01-21 18:50:19 +0900

    Fix incorrect comment

--- src/Illuminate/Http/FrameGuard.php
+++ src/Illuminate/Http/FrameGuard.php
@@ -13,7 +13,7 @@ class FrameGuard implements HttpKernelInterface {
 	protected $app;
 
 	/**
-	 * Create a new CookieQueue instance.
+	 * Create a new FrameGuard instance.
 	 *
 	 * @param  \Symfony\Component\HttpKernel\HttpKernelInterface  $app
 	 * @return void
