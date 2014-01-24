aecf46c951eac6cf78799c83c6ca242e082a6136
Taylor Otwell <taylorotwell@gmail.com>
2014-01-24 08:39:28 -0600

    Fix boolean operators.

--- src/Illuminate/Foundation/Console/TinkerCommand.php
+++ src/Illuminate/Foundation/Console/TinkerCommand.php
@@ -118,7 +118,7 @@ class TinkerCommand extends Command {
 	 */
 	protected function supportsBoris()
 	{
-		return (extension_loaded('readline') and extension_loaded('posix') and extension_loaded('pcntl'));
+		return extension_loaded('readline') && extension_loaded('posix') && extension_loaded('pcntl');
 	}
 
 }
