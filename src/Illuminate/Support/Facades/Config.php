db720f972dca1874c58707dfe0715d78685fbb4a
Taylor Otwell <taylorotwell@gmail.com>
2014-01-28 10:44:50 -0600

    Added Config::append method.

--- src/Illuminate/Support/Facades/Config.php
+++ src/Illuminate/Support/Facades/Config.php
@@ -6,6 +6,29 @@
 class Config extends Facade {
 
 	/**
+	 * Assign high numeric IDs to a config item to force appending.
+	 *
+	 * @param  array  $array
+	 * @return array
+	 */
+	public static function append(array $array)
+	{
+		$start = 9999;
+
+		foreach ($array as $key => $value)
+		{
+			if (is_numeric($key))
+			{
+				$start++;
+
+				$array[$start] = array_pull($array, $key);
+			}
+		}
+
+		return $array;
+	}
+
+	/**
 	 * Get the registered name of the component.
 	 *
 	 * @return string
