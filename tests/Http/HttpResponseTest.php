663a586c79048051dcd394ceda9965dce60d3d99
Eric Bollens <eric@eb.io>
2014-01-30 18:50:43 -0800

    Improve HTTP\RedirectResponse test coverage

--- tests/Http/HttpResponseTest.php
+++ tests/Http/HttpResponseTest.php
@@ -67,6 +67,41 @@ class HttpResponseTest extends PHPUnit_Framework_TestCase {
 		$response->setContent($arr);
 		$this->assertTrue($arr === $response->getOriginalContent());
 	}
+	
+	
+	public function testHeaderOnRedirect()
+	{
+		$response = new RedirectResponse('foo.bar');
+		$this->assertNull($response->headers->get('foo'));
+		$response->header('foo', 'bar');
+		$this->assertEquals('bar', $response->headers->get('foo'));
+		$response->header('foo', 'baz', false);
+		$this->assertEquals('bar', $response->headers->get('foo'));
+		$response->header('foo', 'baz');
+		$this->assertEquals('baz', $response->headers->get('foo'));
+	}
+	
+	
+	public function testWithOnRedirect()	
+{
+        $response = new RedirectResponse('foo.bar');
+        $response->setRequest(Request::create('/', 'GET', array('name' => 'Taylor', 'age' => 26)));
+        $response->setSession($session = m::mock('Illuminate\Session\Store'));
+        $session->shouldReceive('flash')->twice();
+		$response->with(array('name', 'age'));
+	}
+	
+	
+	public function testWithCookieOnRedirect()
+	{
+		$response = new RedirectResponse('foo.bar');
+		$this->assertEquals(0, count($response->headers->getCookies()));
+		$this->assertEquals($response, $response->withCookie(new \Symfony\Component\HttpFoundation\Cookie('foo', 'bar')));
+		$cookies = $response->headers->getCookies();
+		$this->assertEquals(1, count($cookies));
+		$this->assertEquals('foo', $cookies[0]->getName());
+		$this->assertEquals('bar', $cookies[0]->getValue());
+	}
 
 
     public function testInputOnRedirect()
@@ -109,7 +144,23 @@ class HttpResponseTest extends PHPUnit_Framework_TestCase {
         $provider->shouldReceive('getMessageBag')->once()->andReturn(array('foo' => 'bar'));
         $response->withErrors($provider);
     }
+	
+	
+	public function testSettersGettersOnRequest()
+	{
+		$response = new RedirectResponse('foo.bar');
+		$this->assertNull($response->getRequest());
+		$this->assertNull($response->getSession());
+		
+		$request = Request::create('/', 'GET');
+		$session = m::mock('Illuminate\Session\Store');
+		$response->setRequest($request);
+		$response->setSession($session);
+		$this->assertTrue($request === $response->getRequest());
+		$this->assertTrue($session === $response->getSession());
+	}
 
+	
     public function testRedirectWithErrorsArrayConvertsToMessageBag()
     {
         $response = new RedirectResponse('foo.bar');
@@ -119,6 +170,24 @@ class HttpResponseTest extends PHPUnit_Framework_TestCase {
         $provider = array('foo' => 'bar');
         $response->withErrors($provider);
     }
+	
+	
+	public function testMagicCall()
+	{
+		$response = new RedirectResponse('foo.bar');
+        $response->setRequest(Request::create('/', 'GET', array('name' => 'Taylor', 'age' => 26)));
+        $response->setSession($session = m::mock('Illuminate\Session\Store'));
+        $session->shouldReceive('flash')->once()->with('foo', 'bar');
+		$response->withFoo('bar');
+	}
+
+
+	public function testMagicCallException()
+	{
+		$this->setExpectedException('BadMethodCallException');
+		$response = new RedirectResponse('foo.bar');
+		$response->doesNotExist('bar');
+	}
 
 }
 
