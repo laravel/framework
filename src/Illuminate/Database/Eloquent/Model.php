b4ffb051be88e0fb02e85611caa59d5db3475e8b
Matthew Hailwood <me@matthewhailwood.co.nz>
2014-02-12 12:41:31 +1300

    Adds support for lazy eager loading of polymorphic relationships from a collection. For example a report may be owned by a User, or a Team so would have
    ````
    class Report extends Eloquent {
    
        public function owner()
        {
            return $this->morphTo('owner');
        }
    
    }
    ````
    
    This would function correctly for a single model e.g. `Report::find(1)->owner`
    
    However `Report::all()->owner` would almost function correctly. It would however have the quirk of
    assuming all owners of the reports are the same type as the first model in the collection.
    This patch fixes that by splitting each collection into it's own sub collection, loading the relationship
    on that collection and the remerging the sub collections together.
    
    This patch also adds a new `polymorph` property onto polymorphic relationships to mark them as polymorphic and
    provide the required information (id column name and type column name) of the polymorphic relationships.
    
    One test had to be altered due to this change as it was mocking the belongsTo method and causing it not to return
    anything which threw an error when we tried to set a property on the returned relationship.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -597,7 +597,9 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 
 		$localKey = $localKey ?: $this->getKeyName();
 
-		return new MorphOne($instance->newQuery(), $this, $table.'.'.$type, $table.'.'.$id, $localKey);
+		$relation = new MorphOne($instance->newQuery(), $this, $table.'.'.$type, $table.'.'.$id, $localKey);
+		$relation->polymorph = array('type' => $type, 'id' => $id);
+		return $relation;
 	}
 
 	/**
@@ -668,7 +670,9 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 
 		$class = $this->$type;
 
-		return $this->belongsTo($class, $id);
+		$relation = $this->belongsTo($class, $id);
+		$relation->polymorph = array('type' => $type, 'id' => $id);
+		return $relation;
 	}
 
 	/**
@@ -733,7 +737,9 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 
 		$localKey = $localKey ?: $this->getKeyName();
 
-		return new MorphMany($instance->newQuery(), $this, $table.'.'.$type, $table.'.'.$id, $localKey);
+		$relation = new MorphMany($instance->newQuery(), $this, $table.'.'.$type, $table.'.'.$id, $localKey);
+		$relation->polymorph = array('type' => $type, 'id' => $id);
+		return $relation;
 	}
 
 	/**
