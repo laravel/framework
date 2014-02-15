270be980cb58386b687c96e4edd199d9348d4295
Josh Manders <josh@joshmanders.com>
2014-02-15 12:37:23 -0600

    Fix tests to pass

--- tests/Html/FormBuilderTest.php
+++ tests/Html/FormBuilderTest.php
@@ -298,7 +298,7 @@ class FormBuilderTest extends PHPUnit_Framework_TestCase {
 		$form3 = $this->formBuilder->checkbox('foo', 'foobar', true);
 		$form4 = $this->formBuilder->checkbox('foo', 'foobar', false, array('class' => 'span2'));
 
-		$this->assertEquals('<input name="foo" type="hidden"><input name="foo" type="checkbox">', $form1);
+		$this->assertEquals('<input name="foo" type="checkbox">', $form1);
 		$this->assertEquals('<input name="foo" type="hidden" value="0"><input name="foo" type="checkbox" value="1">', $form2);
 		$this->assertEquals('<input name="foo" type="hidden" value="0"><input checked="checked" name="foo" type="checkbox" value="foobar">', $form3);
 		$this->assertEquals('<input class="span2" name="foo" type="hidden" value="0"<input class="span2" name="foo" type="checkbox" value="foobar">', $form4);
@@ -312,7 +312,7 @@ class FormBuilderTest extends PHPUnit_Framework_TestCase {
 
 		$session->shouldReceive('getOldInput')->once()->with('check')->andReturn(null);
 		$check = $this->formBuilder->checkbox('check', 1, true);
-		$this->assertEquals('<input name="check" type="checkbox" value="1">', $check);
+		$this->assertEquals('<input name="check" type="hidden" value="0"><input name="check" type="checkbox" value="1">', $check);
 
 		$session->shouldReceive('getOldInput')->with('check.key')->andReturn('yes');
 		$check = $this->formBuilder->checkbox('check[key]', 'yes');
