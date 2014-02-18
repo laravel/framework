e8e8876cb0576fcdf7003625673b7a784314843c
Martin Fjordvald <martin@oneplus.net>
2014-02-18 16:57:34 +0800

    Add support for char type to the database scheme builder. SQLite internally uses only a single text type so any string function causes same internal type. The rest of the drivers all support char type.

--- src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
@@ -277,6 +277,17 @@ class MySqlGrammar extends Grammar {
 	}
 
 	/**
+	 * Create the column definition for a char type.
+	 *
+	 * @param  \Illuminate\Support\Fluent  $column
+	 * @return string
+	 */
+	protected function typeChar(Fluent $column)
+	{
+		return "char({$column->length})";
+	}
+
+	/**
 	 * Create the column definition for a string type.
 	 *
 	 * @param  \Illuminate\Support\Fluent  $column
