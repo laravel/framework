3afde356e7e99bef902788464decf60e2ec216b6
Phil Sturgeon <email@philsturgeon.co.uk>
2014-02-22 17:49:40 -0500

    Allow pgsql enum fields /w reserved words as names An enum with a name like `unique` would cause an SQL syntax error, so just wrapped that sucker up in double-quotes to make it work.

--- src/Illuminate/Database/Schema/Grammars/PostgresGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/PostgresGrammar.php
@@ -393,7 +393,7 @@ class PostgresGrammar extends Grammar {
 	{
 		$allowed = array_map(function($a) { return "'".$a."'"; }, $column->allowed);
 
-		return "varchar(255) check ({$column->name} in (".implode(', ', $allowed)."))";
+		return "varchar(255) check (\"{$column->name}\" in (".implode(', ', $allowed)."))";
 	}
 
 	/**
