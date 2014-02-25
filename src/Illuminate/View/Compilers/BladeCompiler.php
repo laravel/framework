bdaf8e0cb8aa33bf605fef84e2c70c2e50c369dc
Graham Campbell <graham@mineuk.com>
2014-02-25 15:41:10 +0000

    Spaces to tabs

--- src/Illuminate/View/Compilers/BladeCompiler.php
+++ src/Illuminate/View/Compilers/BladeCompiler.php
@@ -4,375 +4,375 @@ use Closure;
 
 class BladeCompiler extends Compiler implements CompilerInterface {
 
-    /**
-     * All of the registered extensions.
-     *
-     * @var array
-     */
-    protected $extensions = array();
+	/**
+	 * All of the registered extensions.
+	 *
+	 * @var array
+	 */
+	protected $extensions = array();
 
-    /**
-     * Array of opening and closing tags for echos.
-     *
-     * @var array
-     */
-    protected $contentTags = array('{{', '}}');
+	/**
+	 * Array of opening and closing tags for echos.
+	 *
+	 * @var array
+	 */
+	protected $contentTags = array('{{', '}}');
 
-    /**
-     * Array of opening and closing tags for escaped echos.
-     *
-     * @var array
-     */
-    protected $escapedTags = array('{{{', '}}}');
+	/**
+	 * Array of opening and closing tags for escaped echos.
+	 *
+	 * @var array
+	 */
+	protected $escapedTags = array('{{{', '}}}');
 
-    /**
-     * Array of footer lines to be added to template
-     *
-     * @var array
-     */
-    protected $footer;
+	/**
+	 * Array of footer lines to be added to template
+	 *
+	 * @var array
+	 */
+	protected $footer;
 
-    /**
-     * Compile the view at the given path.
-     *
-     * @param  string  $path
-     * @return void
-     */
-    public function compile($path)
-    {
-        $this->footer = array();
-        $contents = $this->compileString($this->files->get($path));
+	/**
+	 * Compile the view at the given path.
+	 *
+	 * @param  string  $path
+	 * @return void
+	 */
+	public function compile($path)
+	{
+		$this->footer = array();
+		$contents = $this->compileString($this->files->get($path));
 
-        if ( ! is_null($this->cachePath))
-        {
-            $this->files->put($this->getCompiledPath($path), $contents);
-        }
-    }
+		if ( ! is_null($this->cachePath))
+		{
+			$this->files->put($this->getCompiledPath($path), $contents);
+		}
+	}
 
-    /**
-     * For internal use only
-     *
-     *
-     * @param string $methodName
-     * @param string $expression
-     * @return string
-     */
-    public function __compileMethod($methodName, $expression)
-    {
-        return $this->$methodName($expression);
-    }
+	/**
+	 * For internal use only
+	 *
+	 *
+	 * @param string $methodName
+	 * @param string $expression
+	 * @return string
+	 */
+	public function __compileMethod($methodName, $expression)
+	{
+		return $this->$methodName($expression);
+	}
 
-    /**
-     * Compile the given Blade template contents.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    public function compileString($value)
-    {
-        $tokens = token_get_all($value);
+	/**
+	 * Compile the given Blade template contents.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	public function compileString($value)
+	{
+		$tokens = token_get_all($value);
 
-        $result = '';
-        $footer = array();
+		$result = '';
+		$footer = array();
 
-        foreach($tokens as $token)
-        {
-            if( is_array($token) )
-            {
-                list($t_id, $t_content, $t_no) = $token;
-                if( $t_id == T_INLINE_HTML ) {
-                    $t_content = $this->compileExtensions($t_content);
+		foreach($tokens as $token)
+		{
+			if( is_array($token) )
+			{
+				list($t_id, $t_content, $t_no) = $token;
+				if( $t_id == T_INLINE_HTML ) {
+					$t_content = $this->compileExtensions($t_content);
 
-                    $obj = $this;
-                    $t_content = preg_replace_callback(
-                        '/\B # we shouldnt have words before
-                        @(\w+) # control word should start with @
-                        ([ \t]*) # and we can have spaces afterwards
-                        (\( ( (?>[^()]+) | (?3) )* \))? # and optional expression within brackets
-                        /x',
-                        function($match) use($obj)
-                        {
-                            if( method_exists($obj, $method = 'compile' . ucfirst($match[1])) )
-                            {
-                                $match[0] = $obj->__compileMethod($method, @$match[3]);
-                            }
+					$obj = $this;
+					$t_content = preg_replace_callback(
+						'/\B # we shouldnt have words before
+						@(\w+) # control word should start with @
+						([ \t]*) # and we can have spaces afterwards
+						(\( ( (?>[^()]+) | (?3) )* \))? # and optional expression within brackets
+						/x',
+						function($match) use($obj)
+						{
+							if( method_exists($obj, $method = 'compile' . ucfirst($match[1])) )
+							{
+								$match[0] = $obj->__compileMethod($method, @$match[3]);
+							}
 
-                            return $match[0] . (@$match[3]?'':$match[2]);
-                        }, $t_content);
+							return $match[0] . (@$match[3]?'':$match[2]);
+						}, $t_content);
 
-                    $t_content = $this->compileComments($t_content);
-                    $t_content = $this->compileEchos($t_content);
-                }
+					$t_content = $this->compileComments($t_content);
+					$t_content = $this->compileEchos($t_content);
+				}
 
-                $result .= $t_content;
-            }
-            else
-            {
-                $result .= $token;
-            }
-        }
+				$result .= $t_content;
+			}
+			else
+			{
+				$result .= $token;
+			}
+		}
 
-        if( count($this->footer) )
-            $result = ltrim($result, PHP_EOL) . PHP_EOL . implode(PHP_EOL, array_reverse($this->footer));
+		if( count($this->footer) )
+			$result = ltrim($result, PHP_EOL) . PHP_EOL . implode(PHP_EOL, array_reverse($this->footer));
 
-        return $result;
-    }
+		return $result;
+	}
 
-    protected function compileEach($expr)
-    {
-        return "<?php echo \$__env->renderEach{$expr}; ?>";
-    }
+	protected function compileEach($expr)
+	{
+		return "<?php echo \$__env->renderEach{$expr}; ?>";
+	}
 
-    protected function compileYield($expr)
-    {
-        return "<?php echo \$__env->yieldContent{$expr}; ?>";
-    }
+	protected function compileYield($expr)
+	{
+		return "<?php echo \$__env->yieldContent{$expr}; ?>";
+	}
 
-    protected function compileShow($expr)
-    {
-        return "<?php echo \$__env->yieldSection(); ?>";
-    }
+	protected function compileShow($expr)
+	{
+		return "<?php echo \$__env->yieldSection(); ?>";
+	}
 
-    protected function compileSection($expr)
-    {
-        return "<?php \$__env->startSection{$expr}; ?>";
-    }
+	protected function compileSection($expr)
+	{
+		return "<?php \$__env->startSection{$expr}; ?>";
+	}
 
-    protected function compileAppend($expr)
-    {
-        return "<?php \$__env->appendSection(); ?>";
-    }
+	protected function compileAppend($expr)
+	{
+		return "<?php \$__env->appendSection(); ?>";
+	}
 
-    protected function compileStop($expr)
-    {
-        return "<?php \$__env->stopSection(); ?>";
-    }
+	protected function compileStop($expr)
+	{
+		return "<?php \$__env->stopSection(); ?>";
+	}
 
-    protected function compileOverwrite($expr)
-    {
-        return "<?php \$__env->stopSection(true); ?>";
-    }
+	protected function compileOverwrite($expr)
+	{
+		return "<?php \$__env->stopSection(true); ?>";
+	}
 
-    protected function compileUnless($expr)
-    {
-        return "<?php if ( ! $expr): ?>";
-    }
+	protected function compileUnless($expr)
+	{
+		return "<?php if ( ! $expr): ?>";
+	}
 
-    protected function compileEndunless($expr)
-    {
-        return "<?php endif; ?>";
-    }
+	protected function compileEndunless($expr)
+	{
+		return "<?php endif; ?>";
+	}
 
-    protected function compileLang($expr)
-    {
-        return "<?php echo \\Illuminate\\Support\\Facades\\Lang::get$expr; ?>";
-    }
+	protected function compileLang($expr)
+	{
+		return "<?php echo \\Illuminate\\Support\\Facades\\Lang::get$expr; ?>";
+	}
 
-    protected function compileChoice($expr)
-    {
-        return "<?php echo \\Illuminate\\Support\\Facades\\Lang::choice$expr; ?>";
-    }
+	protected function compileChoice($expr)
+	{
+		return "<?php echo \\Illuminate\\Support\\Facades\\Lang::choice$expr; ?>";
+	}
 
-    protected function compileElse($expr)
-    {
-        return "<?php else: ?>";
-    }
+	protected function compileElse($expr)
+	{
+		return "<?php else: ?>";
+	}
 
-    protected function compileForeach($expr)
-    {
-        return "<?php foreach{$expr}: ?>";
-    }
+	protected function compileForeach($expr)
+	{
+		return "<?php foreach{$expr}: ?>";
+	}
 
-    protected function compileIf($expr)
-    {
-        return "<?php if{$expr}: ?>";
-    }
+	protected function compileIf($expr)
+	{
+		return "<?php if{$expr}: ?>";
+	}
 
-    protected function compileElseif($expr)
-    {
-        return "<?php elseif{$expr}: ?>";
-    }
+	protected function compileElseif($expr)
+	{
+		return "<?php elseif{$expr}: ?>";
+	}
 
-    protected function compileWhile($expr)
-    {
-        return "<?php while{$expr}: ?>";
-    }
+	protected function compileWhile($expr)
+	{
+		return "<?php while{$expr}: ?>";
+	}
 
-    protected function compileEndwhile($expr)
-    {
-        return "<?php endwhile; ?>";
-    }
+	protected function compileEndwhile($expr)
+	{
+		return "<?php endwhile; ?>";
+	}
 
-    protected function compileEndforeach($expr)
-    {
-        return "<?php endforeach; ?>";
-    }
+	protected function compileEndforeach($expr)
+	{
+		return "<?php endforeach; ?>";
+	}
 
-    protected function compileEndif($expr)
-    {
-        return "<?php endif; ?>";
-    }
+	protected function compileEndif($expr)
+	{
+		return "<?php endif; ?>";
+	}
 
-    protected function compileExtends($expr)
-    {
-        // remove outer brackets so that (a,b) becomes a,b
-        if( starts_with($expr, '(') )
-            $expr = substr($expr, 1, -1);
-        $data = "<?php echo \$__env->make($expr, array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>";
+	protected function compileExtends($expr)
+	{
+		// remove outer brackets so that (a,b) becomes a,b
+		if( starts_with($expr, '(') )
+			$expr = substr($expr, 1, -1);
+		$data = "<?php echo \$__env->make($expr, array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>";
 
-        $this->footer[] = $data;
+		$this->footer[] = $data;
 
-        return '';
-    }
+		return '';
+	}
 
-    protected function compileInclude($expr)
-    {
-        // remove outer brackets so that (a,b) becomes a,b
-        if( starts_with($expr, '(') )
-            $expr = substr($expr, 1, -1);
+	protected function compileInclude($expr)
+	{
+		// remove outer brackets so that (a,b) becomes a,b
+		if( starts_with($expr, '(') )
+			$expr = substr($expr, 1, -1);
 
-        return "<?php echo \$__env->make($expr, array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>";
-    }
+		return "<?php echo \$__env->make($expr, array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>";
+	}
 
-    /**
-     * Register a custom Blade compiler.
-     *
-     * @param  Closure  $compiler
-     * @return void
-     */
-    public function extend(Closure $compiler)
-    {
-        $this->extensions[] = $compiler;
-    }
+	/**
+	 * Register a custom Blade compiler.
+	 *
+	 * @param  Closure  $compiler
+	 * @return void
+	 */
+	public function extend(Closure $compiler)
+	{
+		$this->extensions[] = $compiler;
+	}
 
-    /**
-     * Execute the user defined extensions.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    protected function compileExtensions($value)
-    {
-        foreach ($this->extensions as $compiler)
-        {
-            $value = call_user_func($compiler, $value, $this);
-        }
+	/**
+	 * Execute the user defined extensions.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	protected function compileExtensions($value)
+	{
+		foreach ($this->extensions as $compiler)
+		{
+			$value = call_user_func($compiler, $value, $this);
+		}
 
-        return $value;
-    }
+		return $value;
+	}
 
-    /**
-     * Compile Blade comments into valid PHP.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    protected function compileComments($value)
-    {
-        // match {{--comment--}}
-        $pattern = sprintf('/%s--((.|\s)*?)--%s/', $this->contentTags[0], $this->contentTags[1]);
+	/**
+	 * Compile Blade comments into valid PHP.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	protected function compileComments($value)
+	{
+		// match {{--comment--}}
+		$pattern = sprintf('/%s--((.|\s)*?)--%s/', $this->contentTags[0], $this->contentTags[1]);
 
-        return preg_replace($pattern, '<?php /*$1*/ ?>', $value);
-    }
+		return preg_replace($pattern, '<?php /*$1*/ ?>', $value);
+	}
 
-    /**
-     * Compile Blade echos into valid PHP.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    protected function compileEchos($value)
-    {
-        $difference = strlen($this->contentTags[0]) - strlen($this->escapedTags[0]);
+	/**
+	 * Compile Blade echos into valid PHP.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	protected function compileEchos($value)
+	{
+		$difference = strlen($this->contentTags[0]) - strlen($this->escapedTags[0]);
 
-        if ($difference > 0)
-        {
-            return $this->compileEscapedEchos($this->compileRegularEchos($value));
-        }
+		if ($difference > 0)
+		{
+			return $this->compileEscapedEchos($this->compileRegularEchos($value));
+		}
 
-        return $this->compileRegularEchos($this->compileEscapedEchos($value));
-    }
+		return $this->compileRegularEchos($this->compileEscapedEchos($value));
+	}
 
-    /**
-     * Compile the "regular" echo statements.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    protected function compileRegularEchos($value)
-    {
-        $me = $this;
+	/**
+	 * Compile the "regular" echo statements.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	protected function compileRegularEchos($value)
+	{
+		$me = $this;
 
-        // match @{{ content }}
-        $pattern = sprintf('/(@)?%s\s*(.+?)\s*%s/s', $this->contentTags[0], $this->contentTags[1]);
+		// match @{{ content }}
+		$pattern = sprintf('/(@)?%s\s*(.+?)\s*%s/s', $this->contentTags[0], $this->contentTags[1]);
 
-        $callback = function($matches) use ($me)
-        {
-            return $matches[1] ? substr($matches[0], 1) : '<?php echo '.$me->compileEchoDefaults($matches[2]).'; ?>';
-        };
+		$callback = function($matches) use ($me)
+		{
+			return $matches[1] ? substr($matches[0], 1) : '<?php echo '.$me->compileEchoDefaults($matches[2]).'; ?>';
+		};
 
-        return preg_replace_callback($pattern, $callback, $value);
-    }
+		return preg_replace_callback($pattern, $callback, $value);
+	}
 
-    /**
-     * Compile the escaped echo statements.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    protected function compileEscapedEchos($value)
-    {
-        $me = $this;
+	/**
+	 * Compile the escaped echo statements.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	protected function compileEscapedEchos($value)
+	{
+		$me = $this;
 
-        // match {{{ content }}}
-        $pattern = sprintf('/%s\s*(.+?)\s*%s/s', $this->escapedTags[0], $this->escapedTags[1]);
+		// match {{{ content }}}
+		$pattern = sprintf('/%s\s*(.+?)\s*%s/s', $this->escapedTags[0], $this->escapedTags[1]);
 
-        $callback = function($matches) use ($me)
-        {
-            return '<?php echo e('.$me->compileEchoDefaults($matches[1]).'); ?>';
-        };
+		$callback = function($matches) use ($me)
+		{
+			return '<?php echo e('.$me->compileEchoDefaults($matches[1]).'); ?>';
+		};
 
-        return preg_replace_callback($pattern, $callback, $value);
-    }
+		return preg_replace_callback($pattern, $callback, $value);
+	}
 
-    /**
-     * Compile the default values for the echo statement.
-     *
-     * @param  string  $value
-     * @return string
-     */
-    public function compileEchoDefaults($value)
-    {
-        // match {{ $a or $b }}
-        return preg_replace('/^(?=\$)(.+?)(?:\s+or\s+)(.+?)$/s', 'isset($1) ? $1 : $2', $value);
-    }
+	/**
+	 * Compile the default values for the echo statement.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	public function compileEchoDefaults($value)
+	{
+		// match {{ $a or $b }}
+		return preg_replace('/^(?=\$)(.+?)(?:\s+or\s+)(.+?)$/s', 'isset($1) ? $1 : $2', $value);
+	}
 
-    /**
-     * Sets the content tags used for the compiler.
-     *
-     * @param  string  $openTag
-     * @param  string  $closeTag
-     * @param  bool    $escaped
-     * @return void
-     */
-    public function setContentTags($openTag, $closeTag, $escaped = false)
-    {
-        $property = ($escaped === true) ? 'escapedTags' : 'contentTags';
+	/**
+	 * Sets the content tags used for the compiler.
+	 *
+	 * @param  string  $openTag
+	 * @param  string  $closeTag
+	 * @param  bool    $escaped
+	 * @return void
+	 */
+	public function setContentTags($openTag, $closeTag, $escaped = false)
+	{
+		$property = ($escaped === true) ? 'escapedTags' : 'contentTags';
 
-        $this->{$property} = array(preg_quote($openTag), preg_quote($closeTag));
-    }
+		$this->{$property} = array(preg_quote($openTag), preg_quote($closeTag));
+	}
 
-    /**
-     * Sets the escaped content tags used for the compiler.
-     *
-     * @param  string  $openTag
-     * @param  string  $closeTag
-     * @return void
-     */
-    public function setEscapedContentTags($openTag, $closeTag)
-    {
-        $this->setContentTags($openTag, $closeTag, true);
-    }
+	/**
+	 * Sets the escaped content tags used for the compiler.
+	 *
+	 * @param  string  $openTag
+	 * @param  string  $closeTag
+	 * @return void
+	 */
+	public function setEscapedContentTags($openTag, $closeTag)
+	{
+		$this->setContentTags($openTag, $closeTag, true);
+	}
 
 	/**
 	* Gets the content tags used for the compiler.
