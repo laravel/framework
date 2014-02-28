ca9a822b78af3dcb5439939f550ed022b2580df1
Taylor Otwell <taylorotwell@gmail.com>
2014-02-28 11:04:41 -0600

    Working on global scope system with traits.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -157,13 +157,6 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	public $exists = false;
 
 	/**
-	 * Indicates if the model should soft delete.
-	 *
-	 * @var bool
-	 */
-	protected $softDelete = false;
-
-	/**
 	 * Indicates whether attributes are snake cased on arrays.
 	 *
 	 * @var bool
@@ -190,6 +183,13 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 * @var array
 	 */
 	protected static $booted = array();
+
+	/**
+	 * The array of global scopes on the model.
+	 *
+	 * @var array
+	 */
+	protected static $globalScopes = array();
 
 	/**
 	 * Indicates if all mass assignment is enabled.
@@ -225,13 +225,6 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 * @var string
 	 */
 	const UPDATED_AT = 'updated_at';
-
-	/**
-	 * The name of the "deleted at" column.
-	 *
-	 * @var string
-	 */
-	const DELETED_AT = 'deleted_at';
 
 	/**
 	 * Create a new Eloquent model instance.
@@ -290,6 +283,70 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 				static::$mutatorCache[$class][] = lcfirst($matches[1]);
 			}
 		}
+
+		static::bootTraits();
+	}
+
+	/**
+	 * Boot all of the bootable traits on the model.
+	 *
+	 * @return void
+	 */
+	protected static function bootTraits()
+	{
+		foreach (class_uses(get_called_class()) as $trait)
+		{
+			if (method_exists(get_called_class(), $method = 'boot'.class_basename($trait)))
+			{
+				forward_static_call([get_called_class(), $method]);
+			}
+		}
+	}
+
+	/**
+	 * Register a new global scope on the model.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\ScopeInterface  $scope
+	 * @return void
+	 */
+	public static function addGlobalScope(ScopeInterface $scope)
+	{
+		static::$globalScopes[get_called_class()][get_class($scope)] = $scope;
+	}
+
+	/**
+	 * Deteremine if a model has a global scope.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\ScopeInterface  $scope
+	 * @return bool
+	 */
+	public static function hasGlobalScope($scope)
+	{
+		return ! is_null(static::getGlobalScope($scope));
+	}
+
+	/**
+	 * Get a global scope registered with the modal.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\ScopeInterface  $scope
+	 * @return \Illuminate\Database\Eloquent\ScopeInterface|null
+	 */
+	public static function getGlobalScope($scope)
+	{
+		return array_first(static::$globalScopes[get_called_class()], function($key, $value) use ($scope)
+		{
+			return $scope instanceof $registered;
+		});
+	}
+
+	/**
+	 * Get the global scopes for this class instance.
+	 *
+	 * @return array
+	 */
+	public function getGlobalScopes()
+	{
+		return array_get(static::$globalScopes, get_class($this), []);
 	}
 
 	/**
@@ -954,73 +1011,13 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	}
 
 	/**
-	 * Force a hard delete on a soft deleted model.
-	 *
-	 * @return void
-	 */
-	public function forceDelete()
-	{
-		$softDelete = $this->softDelete;
-
-		// We will temporarily disable false delete to allow us to perform the real
-		// delete operation against the model. We will then restore the deleting
-		// state to what this was prior to this given hard deleting operation.
-		$this->softDelete = false;
-
-		$this->delete();
-
-		$this->softDelete = $softDelete;
-	}
-
-	/**
 	 * Perform the actual delete query on this model instance.
 	 *
 	 * @return void
 	 */
 	protected function performDeleteOnModel()
 	{
-		$query = $this->newQuery()->where($this->getKeyName(), $this->getKey());
-
-		if ($this->softDelete)
-		{
-			$this->{static::DELETED_AT} = $time = $this->freshTimestamp();
-
-			$query->update(array(static::DELETED_AT => $this->fromDateTime($time)));
-		}
-		else
-		{
-			$query->delete();
-		}
-	}
-
-	/**
-	 * Restore a soft-deleted model instance.
-	 *
-	 * @return bool|null
-	 */
-	public function restore()
-	{
-		if ($this->softDelete)
-		{
-			// If the restoring event does not return false, we will proceed with this
-			// restore operation. Otherwise, we bail out so the developer will stop
-			// the restore totally. We will clear the deleted timestamp and save.
-			if ($this->fireModelEvent('restoring') === false)
-			{
-				return false;
-			}
-
-			$this->{static::DELETED_AT} = null;
-
-			// Once we have saved the model, we will fire the "restored" event so this
-			// developer will do anything they need to after a restore operation is
-			// totally finished. Then we will return the result of the save call.
-			$result = $this->save();
-
-			$this->fireModelEvent('restored', false);
-
-			return $result;
-		}
+		$this->newQuery()->where($this->getKeyName(), $this->getKey())->delete();
 	}
 
 	/**
@@ -1109,28 +1106,6 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	public static function deleted($callback)
 	{
 		static::registerModelEvent('deleted', $callback);
-	}
-
-	/**
-	 * Register a restoring model event with the dispatcher.
-	 *
-	 * @param  \Closure|string  $callback
-	 * @return void
-	 */
-	public static function restoring($callback)
-	{
-		static::registerModelEvent('restoring', $callback);
-	}
-
-	/**
-	 * Register a restored model event with the dispatcher.
-	 *
-	 * @param  \Closure|string  $callback
-	 * @return void
-	 */
-	public static function restored($callback)
-	{
-		static::registerModelEvent('restored', $callback);
 	}
 
 	/**
@@ -1275,7 +1250,7 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 */
 	public function save(array $options = array())
 	{
-		$query = $this->newQueryWithDeleted();
+		$query = $this->newQueryWithoutScopes();
 
 		// If the "saving" event returns false we'll bail out of the save and return
 		// false, indicating that the save failed. This gives an opportunities to
@@ -1572,26 +1547,6 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	}
 
 	/**
-	 * Get the name of the "deleted at" column.
-	 *
-	 * @return string
-	 */
-	public function getDeletedAtColumn()
-	{
-		return static::DELETED_AT;
-	}
-
-	/**
-	 * Get the fully qualified "deleted at" column.
-	 *
-	 * @return string
-	 */
-	public function getQualifiedDeletedAtColumn()
-	{
-		return $this->getTable().'.'.$this->getDeletedAtColumn();
-	}
-
-	/**
 	 * Get a fresh timestamp for the model.
 	 *
 	 * @return \Carbon\Carbon
@@ -1617,31 +1572,73 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 * @param  bool  $excludeDeleted
 	 * @return \Illuminate\Database\Eloquent\Builder|static
 	 */
-	public function newQuery($excludeDeleted = true)
+	public function newQuery()
 	{
-		$builder = $this->newEloquentBuilder($this->newBaseQueryBuilder());
+		$builder = $this->newEloquentBuilder(
+			$this->newBaseQueryBuilder()
+		);
 
 		// Once we have the query builders, we will set the model instances so the
 		// builder can easily access any information it may need from the model
 		// while it is constructing and executing various queries against it.
 		$builder->setModel($this)->with($this->with);
 
-		if ($excludeDeleted && $this->softDelete)
+		return $this->applyGlobalScopes($builder);
+	}
+
+	/**
+	 * Get a new quer instance without a given scope.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\ScopeInterface  $scope
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
+	public function newQueryWithoutScope($scope)
+	{
+		$this->getGlobalScope($scope)->remove($builder = $this->newQuery(), $this);
+
+		return $builder;
+	}
+
+	/**
+	 * Get a new query builder that doesn't have any global scopes.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Builder|static
+	 */
+	public function newQueryWithoutScopes()
+	{
+		return $this->removeGlobalScopes($this->newQuery());
+	}
+
+	/**
+	 * Apply all of the global scopes to an Eloquent builder.
+	 *
+	 * @param  \Illuminate\Database\Eloquent\Builder  $builder
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
+	public function applyGlobalScopes($builder)
+	{
+		foreach ($this->getGlobalScopes() as $scope)
 		{
-			$builder->whereNull($this->getQualifiedDeletedAtColumn());
+			$scope->apply($builder, $this);
 		}
 
 		return $builder;
 	}
 
 	/**
-	 * Get a new query builder that includes soft deletes.
+	 * Remove all of the global scopes from an Elouqent builder.
 	 *
-	 * @return \Illuminate\Database\Eloquent\Builder|static
+	 * @param  \Illuminate\Database\Eloquent\Builder  $builder
+	 * @return void
 	 */
-	public function newQueryWithDeleted()
+	public function removeGlobalScopes($builder)
 	{
-		return $this->newQuery(false);
+		foreach ($this->getGlobalScopes() as $scope)
+		{
+			$scope->remove($builder, $this);
+		}
+
+		return $builder;
 	}
 
 	/**
@@ -1653,40 +1650,6 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	public function newEloquentBuilder($query)
 	{
 		return new Builder($query);
-	}
-
-	/**
-	 * Determine if the model instance has been soft-deleted.
-	 *
-	 * @return bool
-	 */
-	public function trashed()
-	{
-		return $this->softDelete && ! is_null($this->{static::DELETED_AT});
-	}
-
-	/**
-	 * Get a new query builder that includes soft deletes.
-	 *
-	 * @return \Illuminate\Database\Eloquent\Builder|static
-	 */
-	public static function withTrashed()
-	{
-		return with(new static)->newQueryWithDeleted();
-	}
-
-	/**
-	 * Get a new query builder that only includes soft deletes.
-	 *
-	 * @return \Illuminate\Database\Eloquent\Builder|static
-	 */
-	public static function onlyTrashed()
-	{
-		$instance = new static;
-
-		$column = $instance->getQualifiedDeletedAtColumn();
-
-		return $instance->newQueryWithDeleted()->whereNotNull($column);
 	}
 
 	/**
@@ -1789,27 +1752,6 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	public function usesTimestamps()
 	{
 		return $this->timestamps;
-	}
-
-	/**
-	 * Determine if the model instance uses soft deletes.
-	 *
-	 * @return bool
-	 */
-	public function isSoftDeleting()
-	{
-		return $this->softDelete;
-	}
-
-	/**
-	 * Set the soft deleting property on the model.
-	 *
-	 * @param  bool  $enabled
-	 * @return void
-	 */
-	public function setSoftDeleting($enabled)
-	{
-		$this->softDelete = $enabled;
 	}
 
 	/**
@@ -2390,7 +2332,7 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 */
 	public function getDates()
 	{
-		$defaults = array(static::CREATED_AT, static::UPDATED_AT, static::DELETED_AT);
+		$defaults = array(static::CREATED_AT, static::UPDATED_AT);
 
 		return array_merge($this->dates, $defaults);
 	}
---
+++ tests/Database/DatabaseSoftDeletingScopeTest.php
@@ -0,0 +1,119 @@
+<?php
+
+use Mockery as m;
+
+class DatabaseSoftDeletingScopeTest extends PHPUnit_Framework_TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
+	public function testApplyingScopeToABuilder()
+	{
+		$scope = m::mock('Illuminate\Database\Eloquent\SoftDeletingScope[extend]');
+		$builder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$builder->shouldReceive('getModel')->once()->andReturn($model = m::mock('StdClass'));
+		$model->shouldReceive('getQualifiedDeletedAtColumn')->once()->andReturn('table.deleted_at');
+		$builder->shouldReceive('whereNull')->once()->with('table.deleted_at');
+		$scope->shouldReceive('extend')->once();
+
+		$scope->apply($builder);
+	}
+
+
+	public function testScopeCanRemoveDeletedAtConstraints()
+	{
+		$scope = new Illuminate\Database\Eloquent\SoftDeletingScope;
+		$builder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$builder->shouldReceive('getModel')->andReturn($model = m::mock('StdClass'));
+		$model->shouldReceive('getQualifiedDeletedAtColumn')->andReturn('table.deleted_at');
+		$builder->shouldReceive('getQuery')->andReturn($query = m::mock('StdClass'));
+		$query->wheres = [['type' => 'Null', 'column' => 'foo'], ['type' => 'Null', 'column' => 'table.deleted_at']];
+		$scope->remove($builder);
+
+		$this->assertEquals($query->wheres, [['type' => 'Null', 'column' => 'foo']]);
+	}
+
+
+	public function testForceDeleteExtension()
+	{
+		$builder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$builder->shouldDeferMissing();
+		$scope = new Illuminate\Database\Eloquent\SoftDeletingScope;
+		$scope->extend($builder);
+		$callback = $builder->getMacro('forceDelete');
+		$givenBuilder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$givenBuilder->shouldReceive('getQuery')->andReturn($query = m::mock('StdClass'));
+		$query->shouldReceive('delete')->once();
+
+		$callback($givenBuilder);
+	}
+
+
+	public function testRestoreExtension()
+	{
+		$builder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$builder->shouldDeferMissing();
+		$scope = new Illuminate\Database\Eloquent\SoftDeletingScope;
+		$scope->extend($builder);
+		$callback = $builder->getMacro('restore');
+		$givenBuilder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$givenBuilder->shouldReceive('withTrashed')->once();
+		$givenBuilder->shouldReceive('getModel')->once()->andReturn($model = m::mock('StdClass'));
+		$model->shouldReceive('getDeletedAtColumn')->once()->andReturn('deleted_at');
+		$givenBuilder->shouldReceive('update')->once()->with(['deleted_at' => null]);
+
+		$callback($givenBuilder);
+	}
+
+
+	public function testWithTrashedExtension()
+	{
+		$builder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$builder->shouldDeferMissing();
+		$scope = m::mock('Illuminate\Database\Eloquent\SoftDeletingScope[remove]');
+		$scope->extend($builder);
+		$callback = $builder->getMacro('withTrashed');
+		$givenBuilder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$scope->shouldReceive('remove')->once()->with($givenBuilder);
+		$result = $callback($givenBuilder);
+
+		$this->assertEquals($givenBuilder, $result);
+	}
+
+
+	public function testOnlyTrashedExtension()
+	{
+		$builder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$builder->shouldDeferMissing();
+		$scope = m::mock('Illuminate\Database\Eloquent\SoftDeletingScope[remove]');
+		$scope->extend($builder);
+		$callback = $builder->getMacro('onlyTrashed');
+		$givenBuilder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$scope->shouldReceive('remove')->once()->with($givenBuilder);
+		$givenBuilder->shouldReceive('getQuery')->andReturn($query = m::mock('StdClass'));
+		$givenBuilder->shouldReceive('getModel')->andReturn($model = m::mock('StdClass'));
+		$model->shouldReceive('getQualifiedDeletedAtColumn')->andReturn('table.deleted_at');
+		$query->shouldReceive('whereNotNull')->once()->with('table.deleted_at');
+		$result = $callback($givenBuilder);
+
+		$this->assertEquals($givenBuilder, $result);
+	}
+
+}
+
+
+class DatabaseSoftDeletingScopeBuilderStub {
+	public $extensions = [];
+	public $onDelete;
+	public function extend($name, $callback)
+	{
+		$this->extensions[$name] = $callback;
+	}
+	public function onDelete($callback)
+	{
+		$this->onDelete = $callback;
+	}
+}
