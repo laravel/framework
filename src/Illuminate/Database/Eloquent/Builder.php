bc1c6d8f5456c9774b6a05187878e13109317452
Taylor Otwell <taylorotwell@gmail.com>
2014-02-28 12:50:09 -0600

    One line.

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -601,9 +601,7 @@ class Builder {
 		}
 		else
 		{
-			call_user_func_array(
-				array($this->query, 'where'), func_get_args()
-			);
+			call_user_func_array(array($this->query, 'where'), func_get_args());
 		}
 
 		return $this;
