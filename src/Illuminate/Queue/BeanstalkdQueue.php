c64bc7b44752be3005499f5d7e099f0fdbfe1c75
Jakob Murko <jakob@qy.si>
2014-03-03 15:49:12 +0100

    Typo -.-

--- src/Illuminate/Queue/BeanstalkdQueue.php
+++ src/Illuminate/Queue/BeanstalkdQueue.php
@@ -64,7 +64,7 @@ class BeanstalkdQueue extends Queue implements QueueInterface {
 	 */
 	public function pushRaw($payload, $queue = null, array $options = array())
 	{
-		return $this->pheanstalk->useTube($this->getQueue($queue))->put($payload, Pheanstalk::DEFAULT_PRIORITY, PheanstalkInterface::DEFAULT_DELAY, $this->ttr);
+		return $this->pheanstalk->useTube($this->getQueue($queue))->put($payload, Pheanstalk::DEFAULT_PRIORITY, Pheanstalk::DEFAULT_DELAY, $this->ttr);
 	}
 
 	/**
