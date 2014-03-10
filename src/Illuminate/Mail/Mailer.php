7f11e7678a07a9776fb390e9c622e86980b06968
trunghieule <letrunghieu.cse09@gmail.com>
2014-03-10 10:00:36 +0700

    Allow logging email subject and email body when setting email pretending mode to true.

--- src/Illuminate/Mail/Mailer.php
+++ src/Illuminate/Mail/Mailer.php
@@ -317,7 +317,7 @@ class Mailer {
 	{
 		$emails = implode(', ', array_keys((array) $message->getTo()));
 
-		$this->logger->info("Pretending to mail message to: {$emails}");
+		$this->logger->info("Pretending to mail message to: {$emails} [Subject: {$message->getSubject()}] [Body: {$message->getBody()}]");
 	}
 
 	/**
