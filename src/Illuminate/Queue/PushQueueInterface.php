9519d398b6ecb4b62a38133891cedccb5f88fe2b
David Carrithers <dcarrith@gmail.com>
2014-03-11 09:47:27 -0400

    Added the marshal() method to the PushQueueInterface since classes extending PushQueue really should implement it.

--- src/Illuminate/Queue/PushQueueInterface.php
+++ src/Illuminate/Queue/PushQueueInterface.php
@@ -3,11 +3,19 @@
 interface PushQueueInterface {
 
 	/**
+	 * Marshal a push queue request and fire the job.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function marshal();
+
+	/**
 	 * Subscribe a queue to the endpoint url
 	 *
 	 * @param string  $queue
 	 * @param string  $endpoint
 	 * @param array   $options
+	 *
 	 * @return array
 	 */
 	public function subscribe($queue, $endpoint, array $options = array());
@@ -17,6 +25,7 @@ interface PushQueueInterface {
 	 *
 	 * @param string  $queue
 	 * @param string  $endpoint
+	 *
 	 * @return array
 	 */
 	public function unsubscribe($queue, $endpoint);
