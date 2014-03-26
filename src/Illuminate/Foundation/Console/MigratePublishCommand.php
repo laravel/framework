db9e3831457ef26a66fbb60978d9a839c19dedd3
atrakeur <atrakeur@gmail.com>
2014-03-26 20:00:26 +0100

    Support of workbench packages in migration:publish
    migration:publish now support publishing migrations from packages inside the workbench folder.

--- src/Illuminate/Foundation/Console/MigratePublishCommand.php
+++ src/Illuminate/Foundation/Console/MigratePublishCommand.php
@@ -37,14 +37,21 @@ class MigratePublishCommand extends Command {
 	}
 
 	/**
-	 * Get the path to the source files.
+	 * Get the path to the source files (Including vendor and workbench)
 	 *
 	 * @return string
 	 */
 	protected function getSourcePath()
 	{
-		$vendor = $this->laravel['path.base'].'/vendor';
+		$workbench = $this->laravel['path.base'].'/workbench';
+		if (file_exists($workbench)) {
+			$path = $workbench.'/'.$this->argument('package').'/src/migrations';
+			if (file_exists($path)) {
+				return $path;
+			}
+		}
 
+		$vendor = $this->laravel['path.base'].'/vendor';
 		return $vendor.'/'.$this->argument('package').'/src/migrations';
 	}
 
