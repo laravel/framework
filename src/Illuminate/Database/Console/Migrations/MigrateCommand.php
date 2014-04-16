29b0f63a0f8bd29d66ccd70805c68f10ec9c9f16
Mohammad Gufran <dogabhai@gmail.com>
2014-04-17 00:53:43 +0530

    fixed indentation width

--- src/Illuminate/Database/Console/Migrations/MigrateCommand.php
+++ src/Illuminate/Database/Console/Migrations/MigrateCommand.php
@@ -55,7 +55,7 @@ class MigrateCommand extends BaseCommand {
 	{
 		if( ! $this->confirmToProceed()) return;
 
-        $this->prepareDatabase();
+		$this->prepareDatabase();
 
 		// The pretend option can be used for "simulating" the migration and grabbing
 		// the SQL queries that would fire if the migration were to be run against
