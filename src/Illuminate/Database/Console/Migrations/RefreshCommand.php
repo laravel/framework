5035816d0e6a0999d5b55f3ec17dec6033d24148
Taylor Otwell <taylorotwell@gmail.com>
2014-04-19 13:01:24 -0500

    Add hyphen.

--- src/Illuminate/Database/Console/Migrations/RefreshCommand.php
+++ src/Illuminate/Database/Console/Migrations/RefreshCommand.php
@@ -32,7 +32,7 @@ class RefreshCommand extends Command {
 
 		// The refresh command is essentially just a brief aggregate of a few other of
 		// the migration commands and just provides a convenient wrapper to execute
-		// them in succession. We'll also see if we need to reseed the database.
+		// them in succession. We'll also see if we need to re-seed the database.
 		$this->call('migrate', array('--database' => $database));
 
 		if ($this->needsSeeding())
