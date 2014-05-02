2340a43acb9501f8282f2d2f06ecaf616747fb27
Taylor Otwell <taylorotwell@gmail.com>
2014-05-02 09:27:54 -0500

    Two new traits for de-cluttering default models.

---
+++ src/Illuminate/Auth/RemindableTrait.php
@@ -0,0 +1,15 @@
+<?php namespace Illuminate\Auth;
+
+trait RemindableTrait {
+
+	/**
+	 * Get the e-mail address where password reminders are sent.
+	 *
+	 * @return string
+	 */
+	public function getReminderEmail()
+	{
+		return $this->email;
+	}
+
+}
