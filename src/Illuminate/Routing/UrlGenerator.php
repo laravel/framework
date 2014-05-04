09469c7beea10bfba516d27aad12a051e13634e8
Charles Peterson <artistan@gmail.com>
2014-05-04 14:19:58 -0500

    Allow urlencoded characters in route parameters
    encode the parameters when creating routes, not the full url! allows slashes in parameters

--- src/Illuminate/Routing/UrlGenerator.php
+++ src/Illuminate/Routing/UrlGenerator.php
@@ -20,38 +20,6 @@ class UrlGenerator {
 	protected $request;
 
 	/**
-	 * The force URL root.
-	 *
-	 * @var string
-	 */
-	protected $forcedRoot;
-
-	/**
-	 * The forced schema for URLs.
-	 *
-	 * @var string
-	 */
-	protected $forceSchema;
-
-	/**
-	 * Characters that should not be URL encoded.
-	 *
-	 * @var array
-	 */
-	protected $dontEncode = array(
-		'%2F' => '/',
-		'%40' => '@',
-		'%3A' => ':',
-		'%3B' => ';',
-		'%2C' => ',',
-		'%3D' => '=',
-		'%2B' => '+',
-		'%21' => '!',
-		'%2A' => '*',
-		'%7C' => '|',
-	);
-
-	/**
 	 * Create a new URL Generator instance.
 	 *
 	 * @param  \Illuminate\Routing\RouteCollection  $routes
@@ -189,23 +157,12 @@ class UrlGenerator {
 	{
 		if (is_null($secure))
 		{
-			return $this->forceSchema ?: $this->request->getScheme().'://';
+			return $this->request->getScheme().'://';
 		}
 		else
 		{
 			return $secure ? 'https://' : 'http://';
 		}
-	}
-
-	/**
-	 * Force the schema for URLs.
-	 *
-	 * @param  string  $schema
-	 * @return void
-	 */
-	public function forceSchema($schema)
-	{
-		$this->forceSchema = $schema.'://';
 	}
 
 	/**
@@ -246,13 +203,44 @@ class UrlGenerator {
 	protected function toRoute($route, array $parameters, $absolute)
 	{
 		$domain = $this->getRouteDomain($route, $parameters);
-
-		$uri = strtr(rawurlencode($this->trimUrl(
-			$root = $this->replaceRoot($route, $domain, $parameters),
-			$this->replaceRouteParameters($route->uri(), $parameters)
-		)), $this->dontEncode).$this->getRouteQueryString($parameters);
-
+		$parameters = array_map(array($this,'encode'), $parameters);
+		
+		$uri = $this->trimUrl(
+				$root = $this->replaceRoot($route, $domain, $parameters),
+				$this->replaceRouteParameters($route->uri(), $parameters)
+			).$this->getRouteQueryString(array_map(array($this,'decode'), $parameters));
+		
 		return $absolute ? $uri : '/'.ltrim(str_replace($root, '', $uri), '/');
+	}
+
+	/**
+	* encode callback
+	* don't decode multi-dimensional arrays, they are query string params
+	*
+	* @param $param
+	* @return string
+	*/
+	function encode($param){
+		if(!is_array($param)){
+			return rawurlencode($param);
+		}
+		
+		return $param;
+	}
+	
+	/**
+	* decode callback
+	* don't decode multi-dimensional arrays, they are query string params
+	* 
+	* @param $param
+	* @return string
+	*/
+	function decode(&$param){
+		if(!is_array($param) && !is_object($param)){
+			return rawurldecode($param);
+		}
+		
+		return $param;
 	}
 
 	/**
@@ -464,25 +452,11 @@ class UrlGenerator {
 	 */
 	protected function getRootUrl($scheme, $root = null)
 	{
-		if (is_null($root))
-		{
-			$root = $this->forcedRoot ?: $this->request->root();
-		}
+		$root = $root ?: $this->request->root();
 
 		$start = starts_with($root, 'http://') ? 'http://' : 'https://';
 
 		return preg_replace('~'.$start.'~', $scheme, $root, 1);
-	}
-
-	/**
-	 * Set the forced root URL.
-	 *
-	 * @param  string  $root
-	 * @return void
-	 */
-	public function forceRootUrl($root)
-	{
-		$this->forcedRoot = $root;
 	}
 
 	/**
