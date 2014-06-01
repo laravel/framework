b60a8bf6990935c97b16e4baa0b2e5ca5c72046b
Taylor Otwell <taylorotwell@gmail.com>
2014-05-31 20:08:40 -0500

    Use isset instead of array_key_exists.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -2205,7 +2205,7 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 		// formatting while accessing attributes vs. arraying / JSONing a model.
 		foreach ($this->getDates() as $key)
 		{
-			if ( ! array_key_exists($key, $attributes)) continue;
+			if ( ! isset($attributes[$key])) continue;
 
 			$attributes[$key] = (string) $this->asDateTime($attributes[$key]);
 		}
