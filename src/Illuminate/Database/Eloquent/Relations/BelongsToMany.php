121687e97d501fe8fe79c619fc56efcdcaee2ce2
Daniel Albornoz <dalbornoz89@gmail.com>
2014-06-02 19:47:55 -0300

    [Bug Fix] $touch should be true by default in updateExistingPivot
    Needed to be consistent with the other methods where $touch is already true by default, and also with the docs: http://laravel.com/docs/eloquent#working-with-pivot-tables

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -673,7 +673,7 @@ class BelongsToMany extends Relation {
 	 * @param  bool   $touch
 	 * @return void
 	 */
-	public function updateExistingPivot($id, array $attributes, $touch)
+	public function updateExistingPivot($id, array $attributes, $touch = true)
 	{
 		if (in_array($this->updatedAt(), $this->pivotColumns))
 		{
