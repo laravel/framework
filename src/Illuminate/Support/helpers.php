2d6b1cf29c29eb1fc86b26211793e623702ace63
Lucas Michot <lucas@semalead.com>
2014-06-09 15:56:46 +0200

    Change code alignment for str_limit function

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -933,18 +933,18 @@ if ( ! function_exists('str_is'))
 
 if ( ! function_exists('str_limit'))
 {
-		/**
-		 * Limit the number of characters in a string.
-		 *
-		 * @param  string  $value
-		 * @param  int     $limit
-		 * @param  string  $end
-		 * @return string
-		 */
-		function str_limit($value, $limit = 100, $end = '...')
-		{
-				return Str::limit($value, $limit, $end);
-		}
+	/**
+	 * Limit the number of characters in a string.
+	 *
+	 * @param  string $value
+	 * @param  int    $limit
+	 * @param  string $end
+	 * @return string
+	 */
+	function str_limit($value, $limit = 100, $end = '...')
+	{
+		return Str::limit($value, $limit, $end);
+	}
 }
 
 if ( ! function_exists('str_plural'))
