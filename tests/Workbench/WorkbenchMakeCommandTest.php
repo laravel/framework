fea60e730a142ca7e0fe74f9227e5dde031c9289
trunghieule <letrunghieu.cse09@gmail.com>
2014-06-09 23:38:38 +0700

    #4606 improve `workbench` command error message

---
+++ tests/Workbench/WorkbenchMakeCommandTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Workbench\Console\WorkbenchMakeCommand;
+use \Illuminate\Filesystem\Filesystem;
+use Mockery as m;
+
+class WorkbenchMakeCommandTest extends PHPUnit_Framework_TestCase {
+
+	protected function tearDown() {
+		parent::tearDown();
+		m::close();
+	}
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error_Warning
+	 * @expectedExceptionMessage chdir(): No such file or directory (errno 2)
+	 */
+	public function testCreatePackageSuccess() {
+		$creator = $this->getMock('\Illuminate\Workbench\PackageCreator', [], [new Filesystem()]);
+		$creator->expects($this->once())->method('create')->will($this->returnValue('foo'));
+		$this->generateAppStub($creator)->call('workbench', ['package' => 'vendor/package']);
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Package name must respect the vendor/name format. Supplied name was "package"
+	 */
+	public function testCreateWorkbenchWithInvalidPackageName() {
+		$creator = $this->getMock('\Illuminate\Workbench\PackageCreator', [], [new Filesystem()]);
+		$creator->expects($this->never())->method('create');
+		$this->generateAppStub($creator)->call('workbench', ['package' => 'package']);
+	}
+
+	private function generateAppStub($creator) {
+		$app = new \Illuminate\Console\Application();
+		$command = m::mock(new WorkbenchMakeCommand($creator));
+		$app->add($command);
+		return $app;
+	}
+
+}
