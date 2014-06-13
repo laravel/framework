a647c1bf82c1f1011ac2cd39b0aadbc3062b1e4b
Taylor Otwell <taylorotwell@gmail.com>
2014-06-13 10:54:11 -0500

    Cast found session to object.

--- src/Illuminate/Session/DatabaseSessionHandler.php
+++ src/Illuminate/Session/DatabaseSessionHandler.php
@@ -59,7 +59,7 @@ class DatabaseSessionHandler implements \SessionHandlerInterface, ExistenceAware
 	 */
 	public function read($sessionId)
 	{
-		$session = $this->getQuery()->find($sessionId);
+		$session = (object) $this->getQuery()->find($sessionId);
 
 		if ( ! is_null($session))
 		{
