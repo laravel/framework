9357f5ea41c2b7854d209ee8eca9f3b57fb4de0f
Lucas Michot <lucas@semalead.com>
2014-06-19 19:06:57 +0200

    tabs and alignments in tests

--- tests/Support/SupportClassLoaderTest.php
+++ tests/Support/SupportClassLoaderTest.php
@@ -10,7 +10,7 @@ class SupportClassLoaderTest extends PHPUnit_Framework_TestCase {
 		$php53Class      = 'Foo\Bar\Baz\Bat';
 		$prefixed53Class = '\Foo\Bar\Baz\Bat';
 		$php52Class      = 'Foo_Bar_Baz_Bat';
-		$expected = 'Foo'.DIRECTORY_SEPARATOR.'Bar'.DIRECTORY_SEPARATOR.'Baz'.DIRECTORY_SEPARATOR.'Bat.php';
+		$expected        = 'Foo'.DIRECTORY_SEPARATOR.'Bar'.DIRECTORY_SEPARATOR.'Baz'.DIRECTORY_SEPARATOR.'Bat.php';
 
 		$this->assertEquals($expected, ClassLoader::normalizeClass($php53Class));
 		$this->assertEquals($expected, ClassLoader::normalizeClass($prefixed53Class));
