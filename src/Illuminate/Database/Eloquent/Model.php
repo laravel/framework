7c9c0b6ed0091e23c9683d75d06e99a245d37914
Suhayb El Wardany <me@suw.me>
2014-06-24 04:44:58 +0200

    return void if nothing was updated on the model
    Signed-off-by: Suhayb El Wardany <me@suw.me>

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1414,7 +1414,7 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 * Perform a model update operation.
 	 *
 	 * @param  \Illuminate\Database\Eloquent\Builder  $query
-	 * @return bool
+	 * @return bool|null
 	 */
 	protected function performUpdate(Builder $query)
 	{
@@ -1449,11 +1449,9 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 
 				$this->fireModelEvent('updated', false);
 			}
-			
+
 			return true;
 		}
-
-		return false;
 	}
 
 	/**
