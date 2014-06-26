d2c6aefbb57bff1f36e7d597665d58fc89738bee
Joel (work) <joel@kalani.com>
2014-06-26 06:58:04 -1000

    code cleanup

--- src/Illuminate/Queue/BeanstalkdQueue.php
+++ src/Illuminate/Queue/BeanstalkdQueue.php
@@ -1,7 +1,7 @@
 <?php namespace Illuminate\Queue;
 
-use Pheanstalk\Job as PheanstalkJob;
 use Pheanstalk\Pheanstalk;
+use Pheanstalk\Job as PheanstalkJob;
 use Illuminate\Queue\Jobs\BeanstalkdJob;
 
 class BeanstalkdQueue extends Queue implements QueueInterface {
