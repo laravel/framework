edccb7c6cd8cb668b7303cdbcf73e0d0a6235d03
Joel (work) <joel@kalani.com>
2014-06-26 06:49:47 -1000

    make code a little cleaner

--- src/Illuminate/Queue/BeanstalkdQueue.php
+++ src/Illuminate/Queue/BeanstalkdQueue.php
@@ -1,7 +1,7 @@
 <?php namespace Illuminate\Queue;
 
-use Pheanstalk\Job as Pheanstalk_Job;
-use Pheanstalk\Pheanstalk as Pheanstalk;
+use Pheanstalk\Job as PheanstalkJob;
+use Pheanstalk\Pheanstalk;
 use Illuminate\Queue\Jobs\BeanstalkdJob;
 
 class BeanstalkdQueue extends Queue implements QueueInterface {
@@ -100,7 +100,7 @@ class BeanstalkdQueue extends Queue implements QueueInterface {
 
 		$job = $this->pheanstalk->watchOnly($queue)->reserve(0);
 
-		if ($job instanceof Pheanstalk_Job)
+		if ($job instanceof PheanstalkJob)
 		{
 			return new BeanstalkdJob($this->container, $this->pheanstalk, $job, $queue);
 		}
