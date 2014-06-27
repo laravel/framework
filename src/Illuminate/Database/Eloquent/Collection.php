42129dab248babeed9698dae3b1a3f9bee2d59e6
Joseph Silber <contact@josephsilber.com>
2014-06-27 16:47:55 -0400

    Not passing anything to getDictionary

--- src/Illuminate/Database/Eloquent/Collection.php
+++ src/Illuminate/Database/Eloquent/Collection.php
@@ -202,7 +202,7 @@ class Collection extends BaseCollection {
 	 */
 	public function only($keys)
 	{
-		$dictionary = array_only($this->getDictionary($this), $keys);
+		$dictionary = array_only($this->getDictionary(), $keys);
 
 		return new static(array_values($dictionary));
 	}
@@ -215,7 +215,7 @@ class Collection extends BaseCollection {
 	 */
 	public function except($keys)
 	{
-	    $dictionary = array_except($this->getDictionary($this), $keys);
+	    $dictionary = array_except($this->getDictionary(), $keys);
 
 	    return new static(array_values($dictionary));
 	}
