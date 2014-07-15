8875c1b355af61b08b8ba5b9d800382c95f74576
Graham Campbell <graham@mineuk.com>
2014-07-15 12:10:34 +0100

    Fixed queue docblocks

--- src/Illuminate/Queue/Jobs/BeanstalkdJob.php
+++ src/Illuminate/Queue/Jobs/BeanstalkdJob.php
@@ -24,8 +24,8 @@ class BeanstalkdJob extends Job {
 	 * Create a new job instance.
 	 *
 	 * @param  \Illuminate\Container\Container  $container
-	 * @param  Pheanstalk  $pheanstalk
-	 * @param  Pheanstalk_Job  $job
+	 * @param  \Pheanstalk_Pheanstalk  $pheanstalk
+	 * @param  \Pheanstalk_Job  $job
 	 * @param  string  $queue
 	 * @return void
 	 */
