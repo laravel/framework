70090ee332380d1a05257a211308d8d7da6ff3cc
Lucas Michot <lucas@semalead.com>
2014-07-17 22:57:05 +0200

    Remove useless $defer = false

--- src/Illuminate/Workbench/WorkbenchServiceProvider.php
+++ src/Illuminate/Workbench/WorkbenchServiceProvider.php
@@ -6,13 +6,6 @@ use Illuminate\Workbench\Console\WorkbenchMakeCommand;
 class WorkbenchServiceProvider extends ServiceProvider {
 
 	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
 	 * Register the service provider.
 	 *
 	 * @return void
