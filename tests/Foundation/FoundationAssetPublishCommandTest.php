68423c2ea67652eff109a2cb30bd3c2cf66b7243
Ryan Nielson <ryan.nielson@gmail.com>
2014-07-23 22:29:58 -0300

    Added base Publisher abstract class

--- tests/Foundation/FoundationAssetPublishCommandTest.php
+++ tests/Foundation/FoundationAssetPublishCommandTest.php
@@ -13,6 +13,7 @@ class FoundationAssetPublishCommandTest extends PHPUnit_Framework_TestCase {
 	public function testCommandCallsPublisherWithProperPackageName()
 	{
 		$command = new Illuminate\Foundation\Console\AssetPublishCommand($pub = m::mock('Illuminate\Foundation\Publishing\AssetPublisher'));
+		$pub->shouldReceive('alreadyPublished')->andReturn(false);
 		$pub->shouldReceive('publishPackage')->once()->with('foo');
 		$command->run(new Symfony\Component\Console\Input\ArrayInput(array('package' => 'foo')), new Symfony\Component\Console\Output\NullOutput);
 	}
