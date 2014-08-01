8767ae63973b439d7f249fe0d3233b949d0e69fc
Ibrahim AshShohail <ibra.sho@gmail.com>
2014-08-02 01:18:38 +0300

    Added Model::reload() method

--- tests/Database/DatabaseEloquentModelTest.php
+++ tests/Database/DatabaseEloquentModelTest.php
@@ -114,7 +114,18 @@ class DatabaseEloquentModelTest extends PHPUnit_Framework_TestCase {
 	}
 
 
-	public function testDestroyMethodCallsQueryBuilderCorrectly()
+    public function testModelReloadData()
+    {
+        $model = EloquentModelReloadStub::find(1);
+        $model->foo = 1;
+        $this->assertTrue($model->isDirty('foo'));
+        $model->reload();
+        $this->assertEquals('bar', $model->foo);
+        $this->assertFalse($model->isDirty('foo'));
+    }
+
+
+    public function testDestroyMethodCallsQueryBuilderCorrectly()
 	{
 		$result = EloquentModelDestroyStub::destroy(1, 2, 3);
 	}
@@ -982,6 +993,19 @@ class EloquentModelFindManyStub extends Illuminate\Database\Eloquent\Model {
 		$mock->shouldReceive('find')->once()->with(array(1, 2), array('*'))->andReturn('foo');
 		return $mock;
 	}
+}
+
+class EloquentModelReloadStub extends Illuminate\Database\Eloquent\Model {
+    protected $attributes = array('foo' => 'bar');
+    protected $original = array('foo' => 'bar');
+    public $exists = true;
+    public function getKey() { return 1; }
+    public function newQuery()
+    {
+        $mock = m::mock('Illuminate\Database\Eloquent\Builder');
+        $mock->shouldReceive('find')->once()->with(1, array('*'))->andReturn($this);
+        return $mock;
+    }
 }
 
 class EloquentModelWithStub extends Illuminate\Database\Eloquent\Model {
