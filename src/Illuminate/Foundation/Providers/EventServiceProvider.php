788937574ab295087af300f1455651dce8091e83
Taylor Otwell <taylorotwell@gmail.com>
2014-08-11 15:16:24 -0500

    Add a base event service provider.

---
+++ src/Illuminate/Foundation/Providers/EventServiceProvider.php
@@ -0,0 +1,18 @@
+<?php namespace Illuminate\Foundation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider {
+
+	/**
+	 * Set the paths to be scanned for events by "event:cache".
+	 *
+	 * @param  array  $paths
+	 * @return void
+	 */
+	public function scan(array $paths)
+	{
+		$this->app['config']->set('app.events.scan', $paths);
+	}
+
+}
