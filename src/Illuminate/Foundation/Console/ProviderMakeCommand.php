39f8f0ba2782f93d9149ac22df6908b4801ea147
Taylor Otwell <taylorotwell@gmail.com>
2014-08-12 15:47:31 -0500

    Working on provider generator.

---
+++ src/Illuminate/Foundation/Console/ProviderMakeCommand.php
@@ -0,0 +1,102 @@
+<?php namespace Illuminate\Foundation\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ProviderMakeCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'provider:make';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Create a new service provider class';
+
+	/**
+	 * The filesystem instance.
+	 *
+	 * @var \Illuminate\Filesystem\Filesystem
+	 */
+	protected $files;
+
+	/**
+	 * Create a new provider creator command instance.
+	 *
+	 * @param  \Illuminate\Filesystem\Filesystem  $files
+	 * @return void
+	 */
+	public function __construct(Filesystem $files)
+	{
+		parent::__construct();
+
+		$this->files = $files;
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		$path = $this->getPath($name = $this->argument('name'));
+
+		if ($this->files->exists($path))
+		{
+			return $this->error('Provider already exists!');
+		}
+
+		$this->files->put(
+			$path, $this->buildProviderClass($name)
+		);
+
+		$this->info('Provider created successfully.');
+
+		$this->call('dump-autoload');
+	}
+
+	/**
+	 * Build the provider class with the given name.
+	 *
+	 * @param  string  $name
+	 * @return string
+	 */
+	protected function buildProviderClass($name)
+	{
+		$stub = $this->files->get(__DIR__.'/stubs/provider.stub');
+
+		return str_replace('{{class}}', $name, $stub);
+	}
+
+	/**
+	 * Get the provider class path.
+	 *
+	 * @param  string  $name
+	 * @return string
+	 */
+	protected function getPath($name)
+	{
+		return $this->laravel['path.src'].'/Providers/'.$name.'.php';
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+			array('name', InputArgument::REQUIRED, 'The name of the provider class'),
+		);
+	}
+
+}
