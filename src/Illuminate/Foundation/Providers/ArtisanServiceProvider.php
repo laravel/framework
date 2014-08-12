a4e2eaf11e66607b6db821e817dd6e2c80424a5b
Taylor Otwell <taylorotwell@gmail.com>
2014-08-12 07:33:49 -0500

    Get rid of a bunch of one-off service providers.

--- src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
+++ src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
@@ -2,10 +2,22 @@
 
 use Illuminate\Foundation\Artisan;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Console\UpCommand;
+use Illuminate\Foundation\Console\DownCommand;
 use Illuminate\Foundation\Console\TailCommand;
+use Illuminate\Foundation\Console\ServeCommand;
+use Illuminate\Foundation\Console\TinkerCommand;
 use Illuminate\Foundation\Console\ChangesCommand;
+use Illuminate\Foundation\Console\OptimizeCommand;
+use Illuminate\Foundation\Console\RouteListCommand;
 use Illuminate\Foundation\Console\EventCacheCommand;
+use Illuminate\Foundation\Console\RouteCacheCommand;
+use Illuminate\Foundation\Console\RouteClearCommand;
+use Illuminate\Foundation\Console\CommandMakeCommand;
 use Illuminate\Foundation\Console\EnvironmentCommand;
+use Illuminate\Foundation\Console\KeyGenerateCommand;
+use Illuminate\Foundation\Console\RequestMakeCommand;
+use Illuminate\Foundation\Console\ClearCompiledCommand;
 
 class ArtisanServiceProvider extends ServiceProvider {
 
@@ -22,10 +34,22 @@ class ArtisanServiceProvider extends ServiceProvider {
 	 * @var array
 	 */
 	protected $commands = [
-		'Tail',
 		'Changes',
+		'ClearCompiled',
+		'CommandMake',
+		'Down',
 		'Environment',
 		'EventCache',
+		'KeyGenerate',
+		'Optimize',
+		'RequestMake',
+		'RouteCache',
+		'RouteClear',
+		'RouteList',
+		'Serve',
+		'Tail',
+		'Tinker',
+		'Up',
 	];
 
 	/**
@@ -49,21 +73,12 @@ class ArtisanServiceProvider extends ServiceProvider {
 		}
 
 		$this->commands(
-			'command.tail', 'command.changes', 'command.environment', 'command.event.cache'
+			'command.tail', 'command.changes', 'command.environment', 'command.event.cache',
+			'command.route.cache', 'command.route.clear', 'command.route.list',
+			'command.request.make', 'command.tinker', 'command.command.make',
+			'command.key.generate', 'command.down', 'command.up', 'command.clear-compiled',
+			'command.optimize', 'command.serve'
 		);
-	}
-
-	/**
-	 * Register the command.
-	 *
-	 * @return void
-	 */
-	protected function registerTailCommand()
-	{
-		$this->app->bindShared('command.tail', function($app)
-		{
-			return new TailCommand;
-		});
 	}
 
 	/**
@@ -76,6 +91,45 @@ class ArtisanServiceProvider extends ServiceProvider {
 		$this->app->bindShared('command.changes', function($app)
 		{
 			return new ChangesCommand;
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerClearCompiledCommand()
+	{
+		$this->app->bindShared('command.clear-compiled', function()
+		{
+			return new ClearCompiledCommand;
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerCommandMakeCommand()
+	{
+		$this->app->bindShared('command.command.make', function($app)
+		{
+			return new CommandMakeCommand($app['files']);
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerDownCommand()
+	{
+		$this->app->bindShared('command.down', function($app)
+		{
+			return new DownCommand;
 		});
 	}
 
@@ -106,6 +160,136 @@ class ArtisanServiceProvider extends ServiceProvider {
 	}
 
 	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerKeyGenerateCommand()
+	{
+		$this->app->bindShared('command.key.generate', function($app)
+		{
+			return new KeyGenerateCommand($app['files']);
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerOptimizeCommand()
+	{
+		$this->app->bindShared('command.optimize', function($app)
+		{
+			return new OptimizeCommand($app['composer']);
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerRequestMakeCommand()
+	{
+		$this->app->bindShared('command.request.make', function($app)
+		{
+			return new RequestMakeCommand($app['files']);
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerRouteCacheCommand()
+	{
+		$this->app->bindShared('command.route.cache', function($app)
+		{
+			return new RouteCacheCommand($app['files']);
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerRouteClearCommand()
+	{
+		$this->app->bindShared('command.route.clear', function($app)
+		{
+			return new RouteClearCommand($app['files']);
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerRouteListCommand()
+	{
+		$this->app->bindShared('command.route.list', function($app)
+		{
+			return new RouteListCommand($app['router']);
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerServerCommand()
+	{
+		$this->app->bindShared('command.serve', function()
+		{
+			return new ServeCommand;
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerTailCommand()
+	{
+		$this->app->bindShared('command.tail', function($app)
+		{
+			return new TailCommand;
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerTinkerCommand()
+	{
+		$this->app->bindShared('command.tinker', function()
+		{
+			return new TinkerCommand;
+		});
+	}
+
+	/**
+	 * Register the command.
+	 *
+	 * @return void
+	 */
+	protected function registerUpCommand()
+	{
+		$this->app->bindShared('command.up', function($app)
+		{
+			return new UpCommand;
+		});
+	}
+
+	/**
 	 * Get the services provided by the provider.
 	 *
 	 * @return array
@@ -114,7 +298,11 @@ class ArtisanServiceProvider extends ServiceProvider {
 	{
 		return [
 			'artisan', 'command.changes', 'command.tail', 'command.environment',
-			'command.event.cache',
+			'command.event.cache', 'command.route.cache', 'command.route.clear',
+			'command.route.list', 'command.request.make', 'command.tinker',
+			'command.command.make', 'command.key.generate', 'command.down',
+			'command.up', 'command.clear-compiled', 'command.optimize',
+			'command.serve',
 		];
 	}
 
