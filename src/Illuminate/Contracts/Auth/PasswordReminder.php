bcd8b83372e1689f0c909e4904186154da1a7115
Taylor Otwell <taylorotwell@gmail.com>
2014-08-22 16:01:18 -0500

    Tweaking password reminder controller and contract.

--- src/Illuminate/Contracts/Auth/PasswordReminder.php
+++ src/Illuminate/Contracts/Auth/PasswordReminder.php
@@ -5,6 +5,41 @@ use Closure;
 interface PasswordReminder {
 
 	/**
+	 * Constant representing a successfully sent reminder.
+	 *
+	 * @var int
+	 */
+	const REMINDER_SENT = 'reminders.sent';
+
+	/**
+	 * Constant representing a successfully reset password.
+	 *
+	 * @var int
+	 */
+	const PASSWORD_RESET = 'reminders.reset';
+
+	/**
+	 * Constant representing the user not found response.
+	 *
+	 * @var int
+	 */
+	const INVALID_USER = 'reminders.user';
+
+	/**
+	 * Constant representing an invalid password.
+	 *
+	 * @var int
+	 */
+	const INVALID_PASSWORD = 'reminders.password';
+
+	/**
+	 * Constant representing an invalid token.
+	 *
+	 * @var int
+	 */
+	const INVALID_TOKEN = 'reminders.token';
+
+	/**
 	 * Send a password reminder to a user.
 	 *
 	 * @param  array     $credentials
