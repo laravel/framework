33a4656adee9d5d64c861a918a22663147607aff
Taylor Otwell <taylorotwell@gmail.com>
2014-08-22 14:39:58 -0500

    Shorten description.

--- src/Illuminate/Auth/Console/AuthMakeCommand.php
+++ src/Illuminate/Auth/Console/AuthMakeCommand.php
@@ -16,7 +16,7 @@ class AuthMakeCommand extends Command {
 	 *
 	 * @var string
 	 */
-	protected $description = 'Scaffold all authentication classes for the application';
+	protected $description = 'Scaffold auth classes for the application';
 
 	/**
 	 * Execute the console command.
