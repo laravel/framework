dc30eddfe1d435e2cfcbdf9d8c72fad895c85658
Jeevan <jeevanlx@gmail.com>
2014-08-23 16:55:23 +0800

    Fix many token bug
    When a user request for password reset more than once within the validity period, all the token is valid, so if any other people managed to get hold of the unused token before it expires, he/she can simply change new password.

--- src/Illuminate/Auth/Reminders/DatabaseReminderRepository.php
+++ src/Illuminate/Auth/Reminders/DatabaseReminderRepository.php
@@ -60,7 +60,15 @@ class DatabaseReminderRepository implements ReminderRepositoryInterface {
 	public function create(Remindable $user)
 	{
 		$email = $user->getReminderEmail();
+		
+		//Check if user already have a valid and active token
+		//If yes, delete it first before generating new token
+		$existingToken = $this->requested($email);
 
+		if($existingToken){
+			$this->delete($existingToken);
+		}
+		
 		// We will create a new, random token for the user so that we can e-mail them
 		// a safe link to the password reset form. Then we will insert a record in
 		// the database so that we can verify the token within the actual reset.
@@ -99,6 +107,27 @@ class DatabaseReminderRepository implements ReminderRepositoryInterface {
 		return $reminder && ! $this->reminderExpired($reminder);
 	}
 
+	/**
+	 * Determine if a reminder record already exists user and is valid
+	 * Return the valid token is exist
+	 *
+	 * @param  string $email
+	 * @return mixed
+	 */
+	public function requested($email)
+	{
+
+		$reminder = (array) $this->getTable()->where('email', $email)->first();
+
+		if($reminder && ! $this->reminderExpired($reminder)){
+
+			return $reminder['token'];
+
+		}
+
+		return false;
+	}
+	
 	/**
 	 * Determine if the reminder has expired.
 	 *
