00cbca2ac3f77aa974c570300064c985d25b3e75
Dayle Rees <thepunkfan@gmail.com>
2014-08-26 14:52:28 +0200

    Top of Europe.

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -7,6 +7,8 @@ class Inspiring {
 	/**
 	 * Get an inspiring quote.
 	 *
+	 * Taylor & Dayle made this commit from Jungfraujoch. (11,333 ft.)
+	 *
 	 * @return string
 	 */
 	public static function quote()
