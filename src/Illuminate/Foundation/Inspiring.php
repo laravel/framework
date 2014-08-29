378af9b610d54692c60b32d8e473f90849901b97
Taylor Otwell <taylorotwell@gmail.com>
2014-08-29 03:24:02 +0200

    Add a few more quotes.

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -16,6 +16,10 @@ class Inspiring {
 		return Collection::make([
 
 			'When there is no desire, all things are at peace. - Laozi',
+			'Simplicity is the ultimate sophistication. - Leonardo da Vinci',
+			'Simplicity is the essence of happiness. - Cedric Bledsoe',
+			'Smile, breathe, and go slowly. - Thich Nhat Hanh',
+			'Simplicity is an aquired taste. - Katharine Gerould',
 
 		])->random();
 	}
