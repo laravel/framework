def2787c9cc6a5821b16ecaec902c775da1cd569
Taylor Otwell <taylorotwell@gmail.com>
2014-09-07 22:15:03 -0500

    Reference interface.

--- src/Illuminate/Queue/Connectors/BeanstalkdConnector.php
+++ src/Illuminate/Queue/Connectors/BeanstalkdConnector.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Queue\BeanstalkdQueue;
 use Pheanstalk_Pheanstalk as Pheanstalk;
+use Pheanstalk_PheanstalkInterface as PheanstalkInterface;
 
 class BeanstalkdConnector implements ConnectorInterface {
 
@@ -13,7 +14,7 @@ class BeanstalkdConnector implements ConnectorInterface {
 	 */
 	public function connect(array $config)
 	{
-		$pheanstalk = new Pheanstalk($config['host'], array_get($config, 'port', Pheanstalk::DEFAULT_PORT));
+		$pheanstalk = new Pheanstalk($config['host'], array_get($config, 'port', PheanstalkInterface::DEFAULT_PORT));
 
 		return new BeanstalkdQueue(
 			$pheanstalk, $config['queue'], array_get($config, 'ttr', Pheanstalk::DEFAULT_TTR)
