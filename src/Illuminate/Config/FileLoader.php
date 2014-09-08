b8558ce8bd8140a5ba0d1a4734336ce3ce2901d4
Taylor Otwell <taylorotwell@gmail.com>
2014-09-08 16:26:40 -0500

    Shorten up some lines.

--- src/Illuminate/Config/FileLoader.php
+++ src/Illuminate/Config/FileLoader.php
@@ -159,7 +159,9 @@ class FileLoader implements LoaderInterface {
 
 		if ($this->files->exists($path = $this->defaultPath.'/'.$file))
 		{
-			$items = array_replace_recursive($items, $this->getRequire($path));
+			$items = array_replace_recursive(
+				$items, $this->getRequire($path)
+			);
 		}
 
 		// Once we have merged the regular package configuration we need to look for
@@ -169,7 +171,9 @@ class FileLoader implements LoaderInterface {
 
 		if ($this->files->exists($path))
 		{
-			$items = array_replace_recursive($items, $this->getRequire($path));
+			$items = array_replace_recursive(
+				$items, $this->getRequire($path)
+			);
 		}
 
 		return $items;
