131e95f183628339cd2d53bb5cced8e1ccdb310d
Taylor Otwell <taylorotwell@gmail.com>
2014-09-24 15:12:28 -0500

    Added an old input helper.

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -224,6 +224,21 @@ if ( ! function_exists('info'))
 	}
 }
 
+if ( ! function_exists('old'))
+{
+	/**
+	 * Retrieve an old input item.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $default
+	 * @return mixed
+	 */
+	function old($key = null, $default = null)
+	{
+		return app('request')->old($key, $default);
+	}
+}
+
 if ( ! function_exists('patch'))
 {
 	/**
