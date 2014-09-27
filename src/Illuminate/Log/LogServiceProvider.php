d7d7c876fc92a89fa4a98dd3bdab29b5039fafe4
Taylor Otwell <taylorotwell@gmail.com>
2014-09-26 20:55:40 -0500

    Adding note.

--- src/Illuminate/Log/LogServiceProvider.php
+++ src/Illuminate/Log/LogServiceProvider.php
@@ -23,6 +23,9 @@ class LogServiceProvider extends ServiceProvider {
 			new Logger($this->app['env']), $this->app['events']
 		);
 
+		// Once we have an instance of the logger we'll bind it as an instance into
+		// the container so that it is available for resolution. We'll also bind
+		// the PSR Logger interface to resolve to this Monolog implementation.
 		$this->app->instance('log', $logger);
 
 		$this->app->bind('Psr\Log\LoggerInterface', function($app)
