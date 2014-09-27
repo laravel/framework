c35c9957a276960254274929bc966ec00a0ee18f
Lucas Michot <lucas@semalead.com>
2014-09-27 18:58:44 +0200

    Replace spaces with tabs

--- tests/Database/DatabaseQueryBuilderMemoryLeakTest.php
+++ tests/Database/DatabaseQueryBuilderMemoryLeakTest.php
@@ -5,58 +5,58 @@ use Illuminate\Database\Query\Builder;
 
 class DatabaseJoinMemoryLeakTest extends PHPUnit_Framework_TestCase {
 
-    public function tearDown()
-    {
-        m::close();
-    }
+	public function tearDown()
+	{
+		m::close();
+	}
 
-    public function testItDoesNotLeakMemoryOnNewQuery()
-    {
-        $builderMain = $this->getBuilder();
+	public function testItDoesNotLeakMemoryOnNewQuery()
+	{
+		$builderMain = $this->getBuilder();
 
-        $this->runMemoryTest(function() use($builderMain){
-            $builder = $builderMain->newQuery();
-            $builder->select('*')->from('users');
+		$this->runMemoryTest(function() use($builderMain){
+			$builder = $builderMain->newQuery();
+			$builder->select('*')->from('users');
 
-        });
-    }
+		});
+	}
 
-    public function testItDoesNotLeakMemoryOnNewQueryWithJoin()
-    {
-        $builderMain = $this->getBuilder();
+	public function testItDoesNotLeakMemoryOnNewQueryWithJoin()
+	{
+		$builderMain = $this->getBuilder();
 
-        $this->runMemoryTest(function() use($builderMain){
-            $builder = $builderMain->newQuery();
-            $builder->select('*')->join('new', 'col', '=', 'col2')->from('users');
+		$this->runMemoryTest(function() use($builderMain){
+			$builder = $builderMain->newQuery();
+			$builder->select('*')->join('new', 'col', '=', 'col2')->from('users');
 
-        });
-    }
+		});
+	}
 
-    protected function runMemoryTest(\Closure $callback)
-    {
-        $i = 5;
+	protected function runMemoryTest(\Closure $callback)
+	{
+		$i = 5;
 
-        $last = null;
+		$last = null;
 
-	    gc_collect_cycles();
+		gc_collect_cycles();
 
-        while($i--)
-        {
-            $callback();
+		while($i--)
+		{
+			$callback();
 
-            $prev = $last;
-            $last = memory_get_usage();
-        }
+			$prev = $last;
+			$last = memory_get_usage();
+		}
 
-        $this->assertEquals($prev, $last);
-    }
+		$this->assertEquals($prev, $last);
+	}
 
 
-    protected function getBuilder()
-    {
-        $grammar = new Illuminate\Database\Query\Grammars\SqlServerGrammar;
-        $processor = m::mock('Illuminate\Database\Query\Processors\Processor');
-        return new Builder(m::mock('Illuminate\Database\ConnectionInterface'), $grammar, $processor);
-    }
+	protected function getBuilder()
+	{
+		$grammar = new Illuminate\Database\Query\Grammars\SqlServerGrammar;
+		$processor = m::mock('Illuminate\Database\Query\Processors\Processor');
+		return new Builder(m::mock('Illuminate\Database\ConnectionInterface'), $grammar, $processor);
+	}
 
 }
