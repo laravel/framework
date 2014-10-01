35db411d4c2194130d83d50552c2f4a7136d512a
acasar <anze.casar@gmail.com>
2014-10-02 00:11:11 +0200

    Remove hardcoded app folder
    Allows changing app folder name

--- src/Illuminate/Console/AppNamespaceDetectorTrait.php
+++ src/Illuminate/Console/AppNamespaceDetectorTrait.php
@@ -8,13 +8,14 @@ trait AppNamespaceDetectorTrait {
 	 * @param  string  $namespacePath
 	 * @return string
 	 */
-	protected function getAppNamespace($namespacePath = 'app')
+	protected function getAppNamespace()
 	{
 		$composer = (array) json_decode(file_get_contents(base_path().'/composer.json', true));
 
 		foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path)
 		{
-			if ($path == "{$namespacePath}/") return $namespace;
+			$composerPath = realpath(base_path().'/'.$path);
+			if (app_path() == $composerPath) return $namespace;
 		}
 
 		throw new \RuntimeException("Unable to detect application namespace.");
