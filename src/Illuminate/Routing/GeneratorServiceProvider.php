8de4258a54c79f22fba467dc76a6d5fa63d0ad91
Taylor Otwell <taylorotwell@gmail.com>
2014-10-06 15:53:52 -0500

    Middleware make command.

--- src/Illuminate/Routing/GeneratorServiceProvider.php
+++ src/Illuminate/Routing/GeneratorServiceProvider.php
@@ -1,7 +1,7 @@
 <?php namespace Illuminate\Routing;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\Console\FilterMakeCommand;
+use Illuminate\Routing\Console\MiddlewareMakeCommand;
 use Illuminate\Routing\Console\ControllerMakeCommand;
 
 class GeneratorServiceProvider extends ServiceProvider {
@@ -22,9 +22,9 @@ class GeneratorServiceProvider extends ServiceProvider {
 	{
 		$this->registerControllerGenerator();
 
-		$this->registerFilterGenerator();
+		$this->registerMiddlewareGenerator();
 
-		$this->commands('command.controller.make', 'command.filter.make');
+		$this->commands('command.controller.make', 'command.middleware.make');
 	}
 
 	/**
@@ -41,15 +41,15 @@ class GeneratorServiceProvider extends ServiceProvider {
 	}
 
 	/**
-	 * Register the filter generator command.
+	 * Register the middleware generator command.
 	 *
 	 * @return void
 	 */
-	protected function registerFilterGenerator()
+	protected function registerMiddlewareGenerator()
 	{
-		$this->app->bindShared('command.filter.make', function($app)
+		$this->app->bindShared('command.middleware.make', function($app)
 		{
-			return new FilterMakeCommand($app['files']);
+			return new MiddlewareMakeCommand($app['files']);
 		});
 	}
 
@@ -61,7 +61,7 @@ class GeneratorServiceProvider extends ServiceProvider {
 	public function provides()
 	{
 		return array(
-			'command.controller.make', 'command.filter.make',
+			'command.controller.make', 'command.middleware.make',
 		);
 	}
 
