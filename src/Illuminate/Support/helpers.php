5122ea0079e99c3b40f1a9623650ca0d39b034e7
Taylor Otwell <taylorotwell@gmail.com>
2014-10-08 16:36:13 -0500

    Compiling more things. Smarter configuration merging.

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -342,6 +342,41 @@ if ( ! function_exists('class_uses_recursive'))
 	}
 }
 
+if ( ! function_exists('config_merge'))
+{
+	/**
+	 * Sensibly merge configuration arrays.
+	 *
+	 * @param  dynamic array
+	 * @return array
+	 */
+	function config_merge()
+	{
+		$result = [];
+
+		foreach (func_get_args() as $arg)
+		{
+			foreach ($arg as $key => $value)
+			{
+				if (is_numeric($key))
+				{
+					$result[] = $value;
+				}
+				elseif (array_key_exists($key, $result) && is_array($result[$key]) && is_array($value))
+				{
+					$result[$key] = config_merge($result[$key], $value);
+				}
+				else
+				{
+					$result[$key] = $value;
+				}
+			}
+		}
+
+		return $result;
+	}
+}
+
 if ( ! function_exists('data_get'))
 {
 	/**
