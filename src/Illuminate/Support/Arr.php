deab7222b0977b0d9d56b2c2f98a814207b699cb
James French <james2037@gmail.com>
2014-10-08 10:17:40 -0700

    Arr::pluck uses data_get helper
    This has the added benefit of allowing "dot"-syntax in Collection::lists

--- src/Illuminate/Support/Arr.php
+++ src/Illuminate/Support/Arr.php
@@ -253,7 +253,7 @@ class Arr {
 
 		foreach ($array as $item)
 		{
-			$itemValue = is_object($item) && !($item instanceof ArrayAccess) ? $item->{$value} : $item[$value];
+			$itemValue = data_get($item, $value);
 
 			// If the key is "null", we will just append the value to the array and keep
 			// looping. Otherwise we will key the array using the value of the key we
@@ -264,7 +264,7 @@ class Arr {
 			}
 			else
 			{
-				$itemKey = is_object($item) && !($item instanceof ArrayAccess) ? $item->{$key} : $item[$key];
+				$itemKey = data_get($item, $key);
 
 				$results[$itemKey] = $itemValue;
 			}
