ff57a40f9012f98d5d61a667e57f6e87719c38ac
vlakoff <vlakoff@gmail.com>
2014-10-09 23:58:08 +0200

    Support all Redirect::to() parameters in redirect() helper

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -304,14 +304,16 @@ if ( ! function_exists('redirect'))
 	 * Get an instance of the redirector.
 	 *
 	 * @param  string|null  $to
-	 * @param  int  $status
+	 * @param  int     $status
+	 * @param  array   $headers
+	 * @param  bool    $secure
 	 * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
 	 */
-	function redirect($to = null, $status = 302)
+	function redirect($to = null, $status = 302, $headers = array(), $secure = null)
 	{
 		if ( ! is_null($to))
 		{
-			return app('redirect')->to($to, $status);
+			return app('redirect')->to($to, $status, $headers, $secure);
 		}
 		else
 		{
