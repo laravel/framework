774c2ec33d521c9e61c0a8d87fe622acf1e9fabd
Taylor Otwell <taylorotwell@gmail.com>
2014-10-13 18:57:41 -0500

    Add ability to auto scan routes and events in the local environment.

--- src/Illuminate/Foundation/Support/Providers/EventServiceProvider.php
+++ src/Illuminate/Foundation/Support/Providers/EventServiceProvider.php
@@ -1,9 +1,20 @@
 <?php namespace Illuminate\Foundation\Support\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Events\Annotations\Scanner;
+use Illuminate\Console\AppNamespaceDetectorTrait;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 
 class EventServiceProvider extends ServiceProvider {
+
+	use AppNamespaceDetectorTrait;
+
+	/**
+	 * Determines if we will auto-scan in the local environment.
+	 *
+	 * @var bool
+	 */
+	protected $scanWhenLocal = true;
 
 	/**
 	 * Register the application's event listeners.
@@ -13,9 +24,14 @@ class EventServiceProvider extends ServiceProvider {
 	 */
 	public function boot(DispatcherContract $events)
 	{
+		if ($this->app->environment('local') && $this->scanWhenLocal)
+		{
+			$this->scanEvents();
+		}
+
 		if ($this->app->eventsAreScanned())
 		{
-			require $this->app->getScannedEventsPath();
+			$this->loadScannedEvents();
 		}
 
 		foreach ($this->listen as $event => $listeners)
@@ -25,6 +41,32 @@ class EventServiceProvider extends ServiceProvider {
 				$events->listen($event, $listener);
 			}
 		}
+	}
+
+	/**
+	 * Load the scanned events for the application.
+	 *
+	 * @return void
+	 */
+	protected function loadScannedEvents()
+	{
+		$events = app('Illuminate\Contracts\Events\Dispatcher');
+
+		require $this->app->getScannedEventsPath();
+	}
+
+	/**
+	 * Scan the events for the application.
+	 *
+	 * @return void
+	 */
+	protected function scanEvents()
+	{
+		$scanner = new Scanner(app_path(), $this->getAppNamespace());
+
+		file_put_contents(
+			$this->app->getScannedEventsPath(), '<?php '.$scanner->getEventDefinitions()
+		);
 	}
 
 	/**
