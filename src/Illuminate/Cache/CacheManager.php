c5a0c1a7d7220660f999eed3af7075dc8aa8a66b
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-13 12:09:23 +0530

    Allows developer to specify connection to use for cache

--- src/Illuminate/Cache/CacheManager.php
+++ src/Illuminate/Cache/CacheManager.php
@@ -89,7 +89,11 @@ class CacheManager extends Manager {
 	{
 		$redis = $this->app['redis'];
 
-		return $this->repository(new RedisStore($redis, $this->getPrefix()));
+		// We allow the developer to specify which connection should be used
+		// to store the cached items.
+		$connection = $this->app['config']['cache.connection'];
+
+		return $this->repository(new RedisStore($redis, $this->getPrefix(), $connection));
 	}
 
 	/**
