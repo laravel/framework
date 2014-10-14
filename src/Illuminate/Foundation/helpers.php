0a744dc8d7b4434472b64804a3cb83459a9e942a
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-10-14 17:26:41 -0400

    Add Elixir versioned file finder

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -478,3 +478,30 @@ if ( ! function_exists('view'))
 		return $factory->make($view, $data, $mergeData);
 	}
 }
+
+if ( ! function_exists('elixir'))
+{
+       /**
+	* Get the path to a versioned Elixir file.
+	*
+	* @param  string  $file
+	* @return string
+	*/
+	function elixir($file)
+	{
+		static $manifest = null;
+	
+		if (is_null($manifest))
+		{
+		    $manifest = json_decode(file_get_contents(public_path().'/build/rev-manifest.json'), true);
+		}
+	
+		if (isset($manifest[$file]))
+		{
+		    return '/build/'.$manifest[$file];
+		}
+		
+		throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
+	}
+
+}
