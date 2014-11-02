9a690b813f4ca48bd46b5e5be5359824d2a1236a
Lucas Michot <lucas@semalead.com>
2014-11-02 22:56:50 +0100

    add str_ascii function

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -570,6 +570,20 @@ if ( ! function_exists('starts_with'))
 	}
 }
 
+if ( ! function_exists('str_ascii'))
+{
+	/**
+	 * Transliterate a UTF-8 value to ASCII.
+	 *
+	 * @param  string  $value
+	 * @return string
+	 */
+	function str_ascii($value)
+	{
+		return Str::ascii($value);
+	}
+}
+
 if ( ! function_exists('str_contains'))
 {
 	/**
