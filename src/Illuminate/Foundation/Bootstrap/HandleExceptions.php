c55ddb2d1edc087f1c5564acd9dc02555083575d
Taylor Otwell <taylorotwell@gmail.com>
2014-11-03 20:21:50 -0600

    Cleaning up code.

--- src/Illuminate/Foundation/Bootstrap/HandleExceptions.php
+++ src/Illuminate/Foundation/Bootstrap/HandleExceptions.php
@@ -61,12 +61,16 @@ class HandleExceptions {
 	/**
 	 * Handle an uncaught exception from the application.
 	 *
+	 * Note: Most exceptions can be handled via the try / catch block in
+	 * the HTTP and Console kernels. But, fatal error exceptions must
+	 * be handled differently since they are not normal exceptions.
+	 *
 	 * @param  \Exception  $e
 	 * @return void
 	 */
 	public function handleException($e)
 	{
-		$this->app['Illuminate\Contracts\Debug\ExceptionHandler']->report($e);
+		$this->getExceptionHandler()->report($e);
 
 		if ($this->app->runningInConsole())
 		{
