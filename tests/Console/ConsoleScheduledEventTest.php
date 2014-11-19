46432fec1a9b37a422c944cfce70a7589f644dad
Taylor Otwell <taylorotwell@gmail.com>
2014-11-18 23:10:53 -0600

    Working on a few more tests.

--- tests/Console/ConsoleScheduledEventTest.php
+++ tests/Console/ConsoleScheduledEventTest.php
@@ -1,16 +1,42 @@
 <?php
 
+use Mockery as m;
 use Illuminate\Console\Scheduling\Event;
 
 class ConsoleScheduledEventTest extends PHPUnit_Framework_TestCase {
 
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
 	public function testBasicCronCompilation()
 	{
+		$app = m::mock('Illuminate\Foundation\Application[isDownForMaintenance,environment]');
+		$app->shouldReceive('isDownForMaintenance')->andReturn(false);
+		$app->shouldReceive('environment')->andReturn('production');
+
 		$event = new Event('php foo');
 		$this->assertEquals('* * * * * *', $event->getExpression());
+		$this->assertTrue($event->isDue($app));
+		$this->assertFalse($event->skip(function() { return true; })->isDue($app));
+
+		$event = new Event('php foo');
+		$this->assertEquals('* * * * * *', $event->getExpression());
+		$this->assertFalse($event->environments('local')->isDue($app));
+
+		$event = new Event('php foo');
 		$this->assertEquals('0 0 * * * *', $event->daily()->getExpression());
+		$this->assertFalse($event->when(function() { return true; })->isDue($app));
+
+		$event = new Event('php foo');
 		$this->assertEquals('*/5 * * * * *', $event->everyFiveMinutes()->getExpression());
+
+		$event = new Event('php foo');
 		$this->assertEquals('*/5 * * * 3 *', $event->everyFiveMinutes()->wednesdays()->getExpression());
+
+		$event = new Event('php foo');
 		$this->assertEquals('0 * * * * *', $event->everyFiveMinutes()->hourly()->getExpression());
 	}
 
