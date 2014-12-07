98b5676f843ccca62449a01b617058407bc032bf
Kennedy Tedesco <kennedyt.tw@gmail.com>
2014-12-07 12:59:49 -0200

    [5.0] Minor fixes (Console, Config and Cookie)

--- src/Illuminate/Console/Application.php
+++ src/Illuminate/Console/Application.php
@@ -4,7 +4,6 @@ use Illuminate\Contracts\Events\Dispatcher;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Application as SymfonyApplication;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Illuminate\Contracts\Console\Application as ApplicationContract;
