021fdea7f6fda3810f62f1b61872a4ecdbe333dc
Jalen Lyu <jalen.lyu@gmail.com>
2014-12-09 00:37:17 +0800

    add useWritePdo for select query

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -181,6 +181,13 @@ class Builder {
 	);
 
 	/**
+	 * Whether use write pdo for select.
+	 *
+	 * @var bool
+	 */
+	protected $useWritePdo = false;
+
+	/**
 	 * Create a new query builder instance.
 	 *
 	 * @param  \Illuminate\Database\ConnectionInterface  $connection
@@ -1356,6 +1363,10 @@ class Builder {
 	 */
 	protected function runSelect()
 	{
+		if ($this->useWritePdo) {
+			return $this->connection->select($this->toSql(), $this->getBindings(), false);
+		}
+
 		return $this->connection->select($this->toSql(), $this->getBindings());
 	}
 
@@ -2090,6 +2101,13 @@ class Builder {
 	public function getGrammar()
 	{
 		return $this->grammar;
+	}
+
+	public function useWritePdo()
+	{
+		$this->useWritePdo = true;
+
+		return $this;
 	}
 
 	/**
