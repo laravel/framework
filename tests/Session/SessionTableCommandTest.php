670d5b9b2844036c3fe376e70b476a97a655c09a
Jason Walton <jwalton512@gmail.com>
2014-12-08 11:39:06 -0700

    Fix composer autload dumping for commands

---
+++ tests/Session/SessionTableCommandTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Session\Console\SessionTableCommand;
+use Illuminate\Container\Container;
+use Mockery as m;
+
+class SessionTableCommandTest extends PHPUnit_Framework_TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
+	public function testCreateMakesMigration()
+	{
+		$command = new SessionTableCommandTestStub(
+			$files = m::mock('Illuminate\Filesystem\Filesystem'),
+			$composer = m::mock('Illuminate\Foundation\Composer')
+		);
+		$creator = m::mock('Illuminate\Database\Migrations\MigrationCreator')->shouldIgnoreMissing();
+
+		$app = new Container();
+		$app['path.database'] = __DIR__;
+		$app['migration.creator'] = $creator;
+		$command->setLaravel($app);
+		$path = __DIR__ . '/migrations';
+		$creator->shouldReceive('create')->once()->with('create_session_table', $path)->andReturn($path);
+		$files->shouldReceive('get')->once()->andReturn('foo');
+		$files->shouldReceive('put')->once()->with($path, 'foo');
+		$composer->shouldReceive('dumpAutoloads')->once();
+
+		$this->runCommand($command);
+	}
+
+
+	protected function runCommand($command, $input = array())
+	{
+		return $command->run(new Symfony\Component\Console\Input\ArrayInput($input), new Symfony\Component\Console\Output\NullOutput);
+	}
+
+}
+
+class SessionTableCommandTestStub extends SessionTableCommand {
+
+	public function call($command, array $arguments = array())
+	{
+		//
+	}
+
+}
