ea809d7ed9e8f347690476302744dbc5583d47a5
Taylor Otwell <taylorotwell@gmail.com>
2014-12-16 09:35:50 -0600

    Fix code to not be a breaking chage.

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -1702,7 +1702,13 @@ class Builder {
 	 */
 	public function exists()
 	{
-		return $this->limit(1)->count() > 0;
+		$limit = $this->limit;
+
+		$result = $this->limit(1)->count() > 0;
+
+		$this->limit($limit);
+
+		return $result;
 	}
 
 	/**
