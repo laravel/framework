4f5ce2978a5ba0372701ebbb28a0aa07d995c3c6
Taylor Otwell <taylorotwell@gmail.com>
2014-12-17 22:24:35 -0600

    Add a cache of snake, camel, and studly cased words.

--- tests/Support/SupportHelpersTest.php
+++ tests/Support/SupportHelpersTest.php
@@ -207,6 +207,7 @@ class SupportHelpersTest extends PHPUnit_Framework_TestCase {
 	public function testSnakeCase()
 	{
 		$this->assertEquals('foo_bar', snake_case('fooBar'));
+		$this->assertEquals('foo_bar', snake_case('fooBar')); // test cache
 	}
 
 
@@ -214,6 +215,7 @@ class SupportHelpersTest extends PHPUnit_Framework_TestCase {
 	{
 		$this->assertEquals('fooBar', camel_case('FooBar'));
 		$this->assertEquals('fooBar', camel_case('foo_bar'));
+		$this->assertEquals('fooBar', camel_case('foo_bar')); // test cache
 		$this->assertEquals('fooBarBaz', camel_case('Foo-barBaz'));
 		$this->assertEquals('fooBarBaz', camel_case('foo-bar_baz'));
 	}
@@ -223,6 +225,7 @@ class SupportHelpersTest extends PHPUnit_Framework_TestCase {
 	{
 		$this->assertEquals('FooBar', studly_case('fooBar'));
 		$this->assertEquals('FooBar', studly_case('foo_bar'));
+		$this->assertEquals('FooBar', studly_case('foo_bar')); // test cache
 		$this->assertEquals('FooBarBaz', studly_case('foo-barBaz'));
 		$this->assertEquals('FooBarBaz', studly_case('foo-bar_baz'));
 	}
