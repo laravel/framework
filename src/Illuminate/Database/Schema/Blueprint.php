2a4db20c70504a813a82109d6cf9c2b9e9444547
Lucas Michot <lucas@semalead.com>
2014-12-27 20:58:21 +0100

    Remove useless $allowed parameter

--- src/Illuminate/Database/Schema/Blueprint.php
+++ src/Illuminate/Database/Schema/Blueprint.php
@@ -577,10 +577,9 @@ class Blueprint {
 	 * Create a new json column on the table.
 	 *
 	 * @param  string  $column
-	 * @param  array   $allowed
 	 * @return \Illuminate\Support\Fluent
 	 */
-	public function json($column, array $allowed)
+	public function json($column)
 	{
 		return $this->addColumn('json', $column);
 	}
