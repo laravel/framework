54cfc2ab797b12e3cd4c0966d2afb819ffe8dcc1
bigsinoos <pcfeeler@gmail.com>
2015-01-01 10:45:03 +0330

    Add utf8 support to the slug method, and a friendly method for that.

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -254,24 +254,39 @@ class Str {
 	 *
 	 * @param  string  $title
 	 * @param  string  $separator
+	 * @param  bool	   $utf8
 	 * @return string
 	 */
-	public static function slug($title, $separator = '-')
+	public static function slug($title, $separator = '-', $utf8 = false)
 	{
-		$title = static::ascii($title);
+		$title = $utf8 ? $title : static::ascii($title);
 
 		// Convert all dashes/underscores into separator
 		$flip = $separator == '-' ? '_' : '-';
 
 		$title = preg_replace('!['.preg_quote($flip).']+!u', $separator, $title);
 
+		$title = $utf8 ? strtolower($title) : mb_strtolower($title);
+
 		// Remove all characters that are not the separator, letters, numbers, or whitespace.
-		$title = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', mb_strtolower($title));
+		$title = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', $title);
 
 		// Replace all separator characters and whitespace by a single separator
 		$title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
 
 		return trim($title, $separator);
+	}
+
+	/**
+	 * Generate a utf8-compatible URL friendly "slug" from a given string.
+	 *
+	 * @param $title
+	 * @param string $separator
+	 * @return string
+	 */
+	public static function lightSlug($title, $separator = '-')
+	{
+		return static::slug($title, $separator, true);
 	}
 
 	/**
