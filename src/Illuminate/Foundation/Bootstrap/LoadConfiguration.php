1de41ba73bf7f6e16419ac2293506dfdab5d3434
Franz Liedke <franz@develophp.org>
2015-01-11 20:31:52 +0100

    Remove hardcoded cache filename

--- src/Illuminate/Foundation/Bootstrap/LoadConfiguration.php
+++ src/Illuminate/Foundation/Bootstrap/LoadConfiguration.php
@@ -20,7 +20,7 @@ class LoadConfiguration {
 		// First we will see if we have a cache configuration file. If we do, we'll load
 		// the configuration items from that file so that it is very quick. Otherwise
 		// we will need to spin through every configuration file and load them all.
-		if (file_exists($cached = storage_path('framework/config.php')))
+		if (file_exists($cached = $app->getCachedConfigPath()))
 		{
 			$items = require $cached;
 
