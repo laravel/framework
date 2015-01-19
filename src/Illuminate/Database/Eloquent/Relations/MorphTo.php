e1ea6df0ca10217e76f54de46a54ee824d300817
jongwoo Yoo <dbwhddn10@gmail.com>
2015-01-19 20:10:11 +0900

    add getMorphType method in MorphTo

--- src/Illuminate/Database/Eloquent/Relations/MorphTo.php
+++ src/Illuminate/Database/Eloquent/Relations/MorphTo.php
@@ -195,6 +195,16 @@ class MorphTo extends BelongsTo {
 	}
 
 	/**
+	 * Get the foreign key "type" name.
+	 *
+	 * @return string
+	 */
+	public function getMorphType()
+	{
+		return $this->morphType;
+	}
+
+	/**
 	 * Get the dictionary used by the relationship.
 	 *
 	 * @return array
