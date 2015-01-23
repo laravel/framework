356f3bb27d608317cc7c589a15cb0a1a7ee4580c
Taylor Otwell <taylorotwell@gmail.com>
2015-01-22 20:33:32 -0600

    Bus facade.

---
+++ src/Illuminate/Support/Facades/Bus.php
@@ -0,0 +1,15 @@
+<?php namespace Illuminate\Support\Facades;
+
+/**
+ * @see \Illuminate\Bus\Dispatcher
+ */
+class Bus extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'Illuminate\Contracts\Bus\Dispatcher'; }
+
+}
