5ebd8697cd3744b5127e0fd8664cd478e47db32a
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2015-01-23 22:56:01 +0100

    Removed unused arguments

--- src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
+++ src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
@@ -230,7 +230,7 @@ class ArtisanServiceProvider extends ServiceProvider {
 	 */
 	protected function registerKeyGenerateCommand()
 	{
-		$this->app->singleton('command.key.generate', function($app)
+		$this->app->singleton('command.key.generate', function()
 		{
 			return new KeyGenerateCommand;
 		});
@@ -334,7 +334,7 @@ class ArtisanServiceProvider extends ServiceProvider {
 	 */
 	protected function registerServeCommand()
 	{
-		$this->app->singleton('command.serve', function($app)
+		$this->app->singleton('command.serve', function()
 		{
 			return new ServeCommand;
 		});
