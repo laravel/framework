11e317c6b2628bab5bd03c1861e2fc41980b2939
Taylor Otwell <taylorotwell@gmail.com>
2015-01-28 21:20:48 -0600

    allow specific service providers to be published. add force command to overwrite.

--- src/Illuminate/Foundation/Console/VendorPublishCommand.php
+++ src/Illuminate/Foundation/Console/VendorPublishCommand.php
@@ -6,6 +6,7 @@ use League\Flysystem\MountManager;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem as Flysystem;
+use Symfony\Component\Console\Input\InputOption;
 use League\Flysystem\Adapter\Local as LocalAdapter;
 
 class VendorPublishCommand extends Command {
@@ -51,7 +52,7 @@ class VendorPublishCommand extends Command {
 	 */
 	public function fire()
 	{
-		foreach (ServiceProvider::pathsToPublish() as $from => $to)
+		foreach (ServiceProvider::pathsToPublish($this->option('provider')) as $from => $to)
 		{
 			if ($this->files->isFile($from))
 			{
@@ -75,7 +76,7 @@ class VendorPublishCommand extends Command {
 	 */
 	protected function publishFile($from, $to)
 	{
-		if ($this->files->exists($to))
+		if ($this->files->exists($to) && ! $this->option('force'))
 		{
 			return;
 		}
@@ -103,9 +104,9 @@ class VendorPublishCommand extends Command {
 
 		foreach ($manager->listContents('from://') as $file)
 		{
-			if ($file['type'] === 'file' && ! $manager->has('to://'.$file['path']))
+			if ($file['type'] === 'file' && ( ! $manager->has('to://'.$file['path']) || $this->option('force')))
 			{
-				$manager->copy('from://'.$file['path'], 'to://'.$file['path']);
+				$manager->put('to://'.$file['path'], $manager->read('from://'.$file['path']));
 			}
 		}
 	}
@@ -139,6 +140,20 @@ class VendorPublishCommand extends Command {
 		$to = str_replace(base_path(), '', realpath($to));
 
 		$this->line('<info>Copied '.$type.'</info> <comment>['.$from.']</comment> <info>To</info> <comment>['.$to.']</comment>');
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('force', null, InputOption::VALUE_NONE, 'Overwrite any existing files.'),
+
+			array('provider', null, InputOption::VALUE_OPTIONAL, 'The service provider that has assets you want to publish.'),
+		);
 	}
 
 }
