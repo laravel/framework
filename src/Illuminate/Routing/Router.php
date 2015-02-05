fef793a9ab03a0e7d867fa68939e4055c321af38
Friparia <friparia@gmail.com>
2015-02-05 18:26:43 +0800

    change indent style
    change indent style from 4 spaces to tabs

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -503,10 +503,10 @@ class Router implements HttpKernelInterface, RouteFiltererInterface {
 	{
 		$group = str_replace('/', '.', $this->getLastGroupPrefix());
 
-        if (empty($group))
-        {
-		    return trim("{$prefix}{$resource}.{$method}", '.');
-        }
+		if (empty($group))
+		{
+			return trim("{$prefix}{$resource}.{$method}", '.');
+		}
 
 		return trim("{$prefix}{$group}.{$resource}.{$method}", '.');
 	}
