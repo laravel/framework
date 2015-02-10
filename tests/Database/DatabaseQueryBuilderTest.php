b86b8a5e63eb3f2b2d64d03fa26225c2bd1cc5cd
rkgrep <1@grep.su>
2015-02-10 16:47:28 +0600

    Fix addSelect behavior

--- tests/Database/DatabaseQueryBuilderTest.php
+++ tests/Database/DatabaseQueryBuilderTest.php
@@ -53,6 +53,10 @@ class DatabaseQueryBuilderTest extends PHPUnit_Framework_TestCase {
 		$builder = $this->getBuilder();
 		$builder->select('foo')->addSelect('bar')->addSelect(array('baz', 'boom'))->from('users');
 		$this->assertEquals('select "foo", "bar", "baz", "boom" from "users"', $builder->toSql());
+
+		$builder = $this->getBuilder();
+		$builder->addSelect('bar')->addSelect(array('baz', 'boom'))->from('users');
+		$this->assertEquals('select *, "bar", "baz", "boom" from "users"', $builder->toSql());
 	}
 
 
