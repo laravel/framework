a311493ca3d205931a2efe61281f5ce32dd8fcd3
Taylor Otwell <taylorotwell@gmail.com>
2015-02-16 22:53:19 -0600

    Return.

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -178,7 +178,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 	 */
 	public function afterLoadingEnvironment(Closure $callback)
 	{
-		$this->afterBootstrapping(
+		return $this->afterBootstrapping(
 			'Illuminate\Foundation\Bootstrap\DetectEnvironment', $callback
 		);
 	}
