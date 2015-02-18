ed19c6ad15af4034f3b148730a0acd015c888322
Gonzalo De Spirito <gonzalo@freshworkstudio.com>
2015-02-17 21:31:21 -0300

    Add functions to the Application contract
    Add environmentFile & detectEnvironment functions.
    
    Because it's used in Illuminate\Foundation\Bootstrap\DetectEnvironment Class.
    
    --
    Now pull request is on master branch. Sorry for that ;)

--- src/Illuminate/Contracts/Foundation/Application.php
+++ src/Illuminate/Contracts/Foundation/Application.php
@@ -1,5 +1,6 @@
 <?php namespace Illuminate\Contracts\Foundation;
 
+use Closure;
 use Illuminate\Contracts\Container\Container;
 
 interface Application extends Container {
@@ -74,5 +75,20 @@ interface Application extends Container {
 	 * @return void
 	 */
 	public function booted($callback);
+
+	/**
+	 * Get the environment file the application is using.
+	 *
+	 * @return string
+	 */
+	public function environmentFile();
+
+	/**
+	 * Detect the application's current environment.
+	 *
+	 * @param  Closure  $callback
+	 * @return string
+	 */
+	public function detectEnvironment(Closure $callback);
 
 }
