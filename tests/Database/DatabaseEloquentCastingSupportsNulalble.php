c811e8ab0b2bfa188c872af34885a4f21b9b735b
Rafael Willians <me@rwillians.com>
2015-02-19 13:53:43 -0200

    Added test to nullable support to casts.

---
+++ tests/Database/DatabaseEloquentCastingSupportsNulalble.php
@@ -0,0 +1,89 @@
+<?php
+
+use Mockery as m;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseEloquentCastingSupportsNulalble extends PHPUnit_Framework_TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function testCastingStringToInteger()
+	{
+		$model = new EloquentModelCastingStub();
+        $model->setAttribute('string_to_integer_field', '1');
+
+        $this->assert(true, is_integer($model->getAttribute('string_to_integer_field')));
+	}
+
+    public function testCastingStringToFloat()
+    {
+        $model = new EloquentModelCastingStub();
+        $model->setAttribute('string_to_float_field', '1.0');
+
+        $this->assert(true, is_float($model->getAttribute('string_to_float_field')));
+    }
+
+    public function testCastIntegerToString()
+    {
+        $model = new EloquentModelCastingStub();
+        $model->setAttribute('integer_to_string_field', 1);
+
+        $this->assert(true, is_string($model->getAttribute('integer_to_string_field')));
+    }
+
+    public function testCastFloatToString()
+    {
+        $model = new EloquentModelCastingStub();
+        $model->setAttribute('integer_to_string_field', 1.0);
+
+        $this->assert(true, is_string($model->getAttribute('float_to_string_field')));
+    }
+
+    public function testCastNullableToString()
+    {
+        $model = new EloquentModelCastingStub();
+        $model->setAttribute('nullable_to_string', null);
+
+        $this->assert(true, is_null($model->getAttribute('nullable_to_string')));
+    }
+
+    public function testCastNullableToInteger()
+    {
+        $model = new EloquentModelCastingStub();
+        $model->setAttribute('nullable_to_integer', null);
+
+        $this->assert(true, is_null($model->getAttribute('nullable_to_integer')));
+    }
+
+    public function testCastNullableToFloat()
+    {
+        $model = new EloquentModelCastingStub();
+        $model->setAttribute('nullable_to_float', null);
+
+        $this->assert(true, is_null($model->getAttribute('nullable_to_float')));
+    }
+
+
+}
+
+
+class EloquentModelCastingStub extends Model {
+
+    protected $table = 'stub';
+
+    protected $guarded = array();
+
+    protected $casts = array(
+        'string_to_integer_field' => 'integer',
+        'string_to_float_field' => 'float',
+        'integer_to_string_field' => 'string',
+        'float_to_string_field' => 'string',
+        'nullable_to_string' => 'string',
+        'nullable_to_integer' => 'integer',
+        'nullable_to_float' => 'float'
+    );
+
+}
