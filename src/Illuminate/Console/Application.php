b3e67e7908ddb65d7092d24482429e930d165ebe
Israel Ortuño <ai.ortuno@gmail.com>
2015-02-20 02:53:21 +0100

    Missing bracket

--- src/Illuminate/Console/Application.php
+++ src/Illuminate/Console/Application.php
@@ -46,7 +46,7 @@ class Application extends SymfonyApplication implements ApplicationContract {
 		$this->event = $events;
 		$this->laravel = $laravel;
 		$this->setAutoExit(false);
-		$this->setCatchExceptions(!$this->laravel->make('config')->get('app.debug');
+		$this->setCatchExceptions(!$this->laravel->make('config')->get('app.debug'));
 
 		$events->fire('artisan.start', [$this]);
 	}
