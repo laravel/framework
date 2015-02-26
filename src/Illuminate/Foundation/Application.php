3ee9f73186ff3f7980aa0e8242abc8f7e58f1736
Taylor Otwell <taylorotwell@gmail.com>
2015-02-26 16:57:38 -0600

    Increment version for release.

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -20,7 +20,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 	 *
 	 * @var string
 	 */
-	const VERSION = '5.0.8';
+	const VERSION = '5.0.9';
 
 	/**
 	 * The base path for the Laravel installation.
