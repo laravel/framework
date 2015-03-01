14f40d4896179bc990c322bf8cda7eafb863d61a
mattrandallbecker <mattrandallbecker@gmail.com>
2015-02-28 21:40:11 -0800

    Remove hard coded string from AuthenticatesAndRegistersUsers.php
    Uses trans to be able to localize the failed login message.

--- src/Illuminate/Foundation/Auth/AuthenticatesAndRegistersUsers.php
+++ src/Illuminate/Foundation/Auth/AuthenticatesAndRegistersUsers.php
@@ -95,7 +95,7 @@ trait AuthenticatesAndRegistersUsers {
 	 */
 	protected function getFailedLoginMesssage()
 	{
-		return 'These credentials do not match our records.';
+		return trans('auth.failedLoginMessage');
 	}
 
 	/**
