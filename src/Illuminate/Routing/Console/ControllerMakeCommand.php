36a5e914b319798c0f99bf8f202b2b55ea8267bc
rkgrep <1@grep.su>
2015-03-02 18:47:47 +0600

    Additional option for model hinting

--- src/Illuminate/Routing/Console/ControllerMakeCommand.php
+++ src/Illuminate/Routing/Console/ControllerMakeCommand.php
@@ -42,6 +42,50 @@ class ControllerMakeCommand extends GeneratorCommand {
 	}
 
 	/**
+	 * Build the class with the given name.
+	 *
+	 * @param  string  $name
+	 * @return string
+	 */
+	protected function buildClass($name)
+	{
+		$stub = parent::buildClass($name);
+		return $this->replaceModelName($stub);
+	}
+
+	/**
+	 * Build the controller methods with the given model name.
+	 *
+	 * @param  string  $stub
+	 * @return string
+	 */
+	protected function replaceModelName(&$stub)
+	{
+		$modelClass = $this->option('model');
+
+		if (is_null($modelClass))
+		{
+			$model = '$id';
+			$modelHint = 'int  $id';
+			$modelUse = '';
+		}
+		else
+		{
+			$basename = class_basename($modelClass);
+			$argument = strtolower($basename);
+			$model = $basename . ' ' . '$' . $argument;
+			$modelHint = $basename . '  ' . '$' . $argument;
+			$modelUse = 'use ' . $modelClass. ";\n";
+		}
+
+		$stub = str_replace('{{model}}', $model, $stub);
+		$stub = str_replace('{{modelUse}}', $modelUse, $stub);
+		$stub = str_replace('{{modelHint}}', $modelHint, $stub);
+
+		return $stub;
+	}
+
+	/**
 	 * Get the default namespace for the class.
 	 *
 	 * @param  string  $rootNamespace
@@ -61,6 +105,7 @@ class ControllerMakeCommand extends GeneratorCommand {
 	{
 		return array(
 			array('plain', null, InputOption::VALUE_NONE, 'Generate an empty controller class.'),
+			array('model', null, InputOption::VALUE_OPTIONAL, 'Optional model class for binding'),
 		);
 	}
 
