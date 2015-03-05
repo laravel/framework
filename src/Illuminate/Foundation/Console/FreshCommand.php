c816ffeb85bade31470686c9f9d1db38e584a969
Nekrasov Ilya <nekrasov.ilya90@gmail.com>
2015-03-06 00:30:03 +0300

    Delete migrations

--- src/Illuminate/Foundation/Console/FreshCommand.php
+++ src/Illuminate/Foundation/Console/FreshCommand.php
@@ -41,6 +41,9 @@ class FreshCommand extends Command {
 		$files->put(base_path('resources/assets/less/app.less'), ''.PHP_EOL);
 		$files->deleteDirectory(base_path('resources/assets/less/bootstrap'));
 
+		$files->delete(base_path('database/migrations/2014_10_12_000000_create_users_table.php'));
+		$files->delete(base_path('database/migrations/2014_10_12_100000_create_password_resets_table.php'));
+
 		$files->put(app_path('Http/routes.php'), $files->get(__DIR__.'/stubs/fresh-routes.stub'));
 		$files->put(app_path('Providers/AppServiceProvider.php'), $files->get(__DIR__.'/stubs/fresh-app-provider.stub'));
 
