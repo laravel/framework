a5da6f65d859ecbf0126fd0fdba0b7a1f93a7998
Mads Lundholm <mads@madslundholm.dk>
2015-03-06 14:47:04 +0100

    Created the RegistersUsers trait
    Instead of the AuthenticatesAndRegistersUsers, I split it up into two: AuthenticatesUsers and RegistersUsers.
    This contains only the content from the original file there are necessary for the registration.
    The redirect functions are moved to a new trait called RedirectPaths

---
+++ src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -0,0 +1,55 @@
+<?php namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Auth\Guard;
+use BlueCms\Services\Registrar;
+
+trait RegistersUsers {
+
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+    /**
+     * The registrar implementation.
+     *
+     * @var Registrar
+     */
+    protected $registrar;
+
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getRegister()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postRegister(Request $request)
+    {
+        $validator = $this->registrar->validator($request->all());
+
+        if ($validator->fails())
+        {
+            $this->throwValidationException(
+                $request, $validator
+            );
+        }
+
+        $this->auth->login($this->registrar->create($request->all()));
+
+        return redirect($this->redirectPath());
+    }
+
+
+}
