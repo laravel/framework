e50f86191d0013fb1d1936ed5ec24ae2ef8f29cb
Charlie <icharlie.osu@gmail.com>
2015-03-07 14:48:06 -0800

    Add type hinting on $parameters

--- src/Illuminate/Container/Container.php
+++ src/Illuminate/Container/Container.php
@@ -543,7 +543,7 @@ class Container implements ArrayAccess, ContainerContract {
 	 * @param  array  $parameters
 	 * @return array
 	 */
-	protected function getMethodDependencies($callback, $parameters = [])
+	protected function getMethodDependencies($callback, array $parameters = [])
 	{
 		$dependencies = [];
 
@@ -634,7 +634,7 @@ class Container implements ArrayAccess, ContainerContract {
 	 * @param  array   $parameters
 	 * @return mixed
 	 */
-	public function make($abstract, $parameters = [])
+	public function make($abstract, array $parameters = [])
 	{
 		$abstract = $this->getAlias($abstract);
 
@@ -763,7 +763,7 @@ class Container implements ArrayAccess, ContainerContract {
 	 *
 	 * @throws BindingResolutionException
 	 */
-	public function build($concrete, $parameters = [])
+	public function build($concrete, array $parameters = [])
 	{
 		// If the concrete type is actually a Closure, we will just execute it and
 		// hand back the results of the functions, which allows functions to be
@@ -824,7 +824,7 @@ class Container implements ArrayAccess, ContainerContract {
 	 * @param  array  $primitives
 	 * @return array
 	 */
-	protected function getDependencies($parameters, array $primitives = [])
+	protected function getDependencies(array $parameters, array $primitives = [])
 	{
 		$dependencies = [];
 
