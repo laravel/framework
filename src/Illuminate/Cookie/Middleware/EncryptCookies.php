04c4b03c15123ebc9a1f943ce965a29c87e5fa3d
Lucas Michot <lucas@semalead.com>
2015-03-11 01:35:12 +0100

    Add curly brackets

--- src/Illuminate/Cookie/Middleware/EncryptCookies.php
+++ src/Illuminate/Cookie/Middleware/EncryptCookies.php
@@ -87,7 +87,9 @@ class EncryptCookies implements Middleware {
 		$decrypted = array();
 
 		foreach ($cookie as $key => $value)
+		{
 			$decrypted[$key] = $this->encrypter->decrypt($value);
+		}
 
 		return $decrypted;
 	}
