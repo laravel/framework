cfba00f2dee92bbba9343004d3d1e8aa0a3e269e
Taylor Otwell <taylorotwell@gmail.com>
2015-03-18 16:45:49 -0500

    Make migration opt-in with shortcut.

--- src/Illuminate/Foundation/Console/ModelMakeCommand.php
+++ src/Illuminate/Foundation/Console/ModelMakeCommand.php
@@ -35,7 +35,7 @@ class ModelMakeCommand extends GeneratorCommand {
 	{
 		parent::fire();
 
-		if ( ! $this->option('no-migration'))
+		if ($this->option('migration'))
 		{
 			$table = str_plural(snake_case(class_basename($this->argument('name'))));
 
@@ -72,7 +72,7 @@ class ModelMakeCommand extends GeneratorCommand {
 	protected function getOptions()
 	{
 		return array(
-			array('no-migration', null, InputOption::VALUE_NONE, 'Do not create a new migration file.'),
+			array('migration', 'm', InputOption::VALUE_NONE, 'Create a new migration file for the model.'),
 		);
 	}
 
