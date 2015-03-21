855eb58cc991a399a8618d5624e049b9a731491b
Nekrasov Ilya <nekrasov.ilya90@gmail.com>
2015-03-22 02:12:47 +0300

    AppNamespaceDetectorTrait refactoring

--- src/Illuminate/Console/AppNamespaceDetectorTrait.php
+++ src/Illuminate/Console/AppNamespaceDetectorTrait.php
@@ -1,11 +1,9 @@
 <?php namespace Illuminate\Console;
 
-use RuntimeException;
-
 trait AppNamespaceDetectorTrait {
 
 	/**
-	 * Get the application namespace from the Composer file.
+	 * Get the application namespace from the Kernel object.
 	 *
 	 * @return string
 	 *
@@ -13,17 +11,10 @@ trait AppNamespaceDetectorTrait {
 	 */
 	protected function getAppNamespace()
 	{
-		$composer = json_decode(file_get_contents(base_path().'/composer.json'), true);
+		$kernelContract = app()->runningInConsole() ? 'Illuminate\Contracts\Console\Kernel' : 'Illuminate\Contracts\Http\Kernel';
+		$kernelFullClassName = get_class(app($kernelContract));	
 
-		foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path)
-		{
-			foreach ((array) $path as $pathChoice)
-			{
-				if (realpath(app_path()) == realpath(base_path().'/'.$pathChoice)) return $namespace;
-			}
-		}
-
-		throw new RuntimeException("Unable to detect application namespace.");
+		return strtok($kernelFullClassName, '\\').'\\';
 	}
 
 }
