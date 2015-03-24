1dc9dade3456027ebc804a209d0488643a7aca8d
Eric Peterson <eric@elpete.com>
2015-03-24 14:37:39 -0600

    Add a local key override option to HasManyThrough relations as an optional fifth argument whil not breaking compatibility.

--- tests/Database/DatabaseEloquentHasManyThroughTest.php
+++ tests/Database/DatabaseEloquentHasManyThroughTest.php
@@ -65,6 +65,35 @@ class DatabaseEloquentHasManyThroughTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals(0, count($models[2]->foo));
 	}
 
+	public function testModelsAreProperlyMatchedToParentsWithNonPrimaryKey()
+	{
+		$relation = $this->getRelationForNonPrimaryKey();
+
+		$result1 = new EloquentHasManyThroughModelStub;
+		$result1->country_id = 1;
+		$result2 = new EloquentHasManyThroughModelStub;
+		$result2->country_id = 2;
+		$result3 = new EloquentHasManyThroughModelStub;
+		$result3->country_id = 2;
+
+		$model1 = new EloquentHasManyThroughModelStub;
+		$model1->id = 1;
+		$model2 = new EloquentHasManyThroughModelStub;
+		$model2->id = 2;
+		$model3 = new EloquentHasManyThroughModelStub;
+		$model3->id = 3;
+
+		$relation->getRelated()->shouldReceive('newCollection')->andReturnUsing(function($array) { return new Collection($array); });
+		$models = $relation->match(array($model1, $model2, $model3), new Collection(array($result1, $result2, $result3)), 'foo');
+
+		$this->assertEquals(1, $models[0]->foo[0]->country_id);
+		$this->assertEquals(1, count($models[0]->foo));
+		$this->assertEquals(2, $models[1]->foo[0]->country_id);
+		$this->assertEquals(2, $models[1]->foo[1]->country_id);
+		$this->assertEquals(2, count($models[1]->foo));
+		$this->assertEquals(0, count($models[2]->foo));
+	}
+
 
 	public function testAllColumnsAreSelectedByDefault()
 	{
@@ -141,11 +170,17 @@ class DatabaseEloquentHasManyThroughTest extends PHPUnit_Framework_TestCase {
 
 	protected function getRelation()
 	{
-		list($builder, $country, $user, $firstKey, $secondKey) = $this->getRelationArguments();
+		list($builder, $country, $user, $firstKey, $secondKey, $overrideKey) = $this->getRelationArguments();
 
-		return new HasManyThrough($builder, $country, $user, $firstKey, $secondKey);
+		return new HasManyThrough($builder, $country, $user, $firstKey, $secondKey, $overrideKey);
 	}
 
+	protected function getRelationForNonPrimaryKey()
+	{
+		list($builder, $country, $user, $firstKey, $secondKey, $overrideKey) = $this->getRelationArgumentsForNonPrimaryKey();
+
+		return new HasManyThrough($builder, $country, $user, $firstKey, $secondKey, $overrideKey);
+	}
 
 	protected function getRelationArguments()
 	{
@@ -154,7 +189,8 @@ class DatabaseEloquentHasManyThroughTest extends PHPUnit_Framework_TestCase {
 		$builder->shouldReceive('where')->with('users.country_id', '=', 1);
 
 		$country = m::mock('Illuminate\Database\Eloquent\Model');
-		$country->shouldReceive('getKey')->andReturn(1);
+		$country->shouldReceive('getKeyName')->andReturn('id');
+		$country->shouldReceive('offsetGet')->andReturn(1);
 		$country->shouldReceive('getForeignKey')->andReturn('country_id');
 		$user = m::mock('Illuminate\Database\Eloquent\Model');
 		$user->shouldReceive('getTable')->andReturn('users');
@@ -168,7 +204,31 @@ class DatabaseEloquentHasManyThroughTest extends PHPUnit_Framework_TestCase {
 		$user->shouldReceive('getCreatedAtColumn')->andReturn('created_at');
 		$user->shouldReceive('getUpdatedAtColumn')->andReturn('updated_at');
 
-		return [$builder, $country, $user, 'country_id', 'user_id'];
+		return [$builder, $country, $user, 'country_id', 'user_id', $country->getKeyName()];
+	}
+
+	protected function getRelationArgumentsForNonPrimaryKey()
+	{
+		$builder = m::mock('Illuminate\Database\Eloquent\Builder');
+		$builder->shouldReceive('join')->once()->with('users', 'users.id', '=', 'posts.user_id');
+		$builder->shouldReceive('where')->with('users.country_id', '=', 1);
+
+		$country = m::mock('Illuminate\Database\Eloquent\Model');
+		$country->shouldReceive('offsetGet')->andReturn(1);
+		$country->shouldReceive('getForeignKey')->andReturn('country_id');
+		$user = m::mock('Illuminate\Database\Eloquent\Model');
+		$user->shouldReceive('getTable')->andReturn('users');
+		$user->shouldReceive('getQualifiedKeyName')->andReturn('users.id');
+		$post = m::mock('Illuminate\Database\Eloquent\Model');
+		$post->shouldReceive('getTable')->andReturn('posts');
+
+		$builder->shouldReceive('getModel')->andReturn($post);
+
+		$user->shouldReceive('getKey')->andReturn(1);
+		$user->shouldReceive('getCreatedAtColumn')->andReturn('created_at');
+		$user->shouldReceive('getUpdatedAtColumn')->andReturn('updated_at');
+
+		return [$builder, $country, $user, 'country_id', 'user_id', 'other_id'];
 	}
 
 }
