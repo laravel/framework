98861887ca07466e91ab400e9274e8e863ddb9f1
Scott Wilcox <scott@dor.ky>
2015-03-24 22:13:51 +0000

    Allow users to set redirectAfterLogout
    Apparently you shouldn't code early morning without coffee, @taylorotwell was correct in it not returning previously.
    
    Didn't use logoutPath because it's not really a path in the same way loginPath is.

--- src/Illuminate/Foundation/Auth/AuthenticatesAndRegistersUsers.php
+++ src/Illuminate/Foundation/Auth/AuthenticatesAndRegistersUsers.php
@@ -107,7 +107,7 @@ trait AuthenticatesAndRegistersUsers {
 	{
 		$this->auth->logout();
 
-		return redirect('/');
+		return redirect(property_exists($this, 'redirectAfterLogout') ? $this->redirectAfterLogout : '/');
 	}
 
 	/**
