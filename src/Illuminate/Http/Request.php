359af88093437f0404c6b970c4d20fad09c27d45
Malikin Sergey <malikin.sergey.1988@gmail.com>
2015-03-25 01:11:38 +0300

    miss )

--- src/Illuminate/Http/Request.php
+++ src/Illuminate/Http/Request.php
@@ -309,7 +309,7 @@ class Request extends SymfonyRequest implements ArrayAccess {
 		{
 			$value = array_get($input, $key));
 			
-			if( ! is_null($value){
+			if( ! is_null($value)){
 				array_set($results, $key, $value);
 			}
 		}
