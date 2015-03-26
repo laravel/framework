e639675e22bebe9d2901bc9bc408d750a360e74e
Justin Lau <justin@tclau.com>
2015-03-27 01:09:44 +0800

    Enhanced `Str::snake()`, passing tests: 1. Replaces spaces with delimeters; 2. Prevent duplications of delimeters.

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -317,10 +317,13 @@ class Str {
 			return static::$snakeCache[$key];
 		}
 
+
 		if ( ! ctype_lower($value))
 		{
 			$value = strtolower(preg_replace('/(.)(?=[A-Z])/', '$1'.$delimiter, $value));
 		}
+
+		$value = preg_replace("/([ { $delimiter }]+)/", $delimiter, $value);
 
 		return static::$snakeCache[$key] = $value;
 	}
