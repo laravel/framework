a1ab66d7a1335dc9bda7be569b85095f24c7305b
Will Rowe <wrowe@will-rowe.com>
2015-04-03 16:33:08 -0400

    Add reset method to migrator.

--- src/Illuminate/Database/Console/Migrations/ResetCommand.php
+++ src/Illuminate/Database/Console/Migrations/ResetCommand.php
@@ -56,19 +56,14 @@ class ResetCommand extends Command {
 
 		$pretend = $this->input->getOption('pretend');
 
-		while (true)
+		$this->migrator->reset($pretend);
+
+		// Once the migrator has run we will grab the note output and send it out to
+		// the console screen, since the migrator itself functions without having
+		// any instances of the OutputInterface contract passed into the class.
+		foreach ($this->migrator->getNotes() as $note)
 		{
-			$count = $this->migrator->rollback($pretend);
-
-			// Once the migrator has run we will grab the note output and send it out to
-			// the console screen, since the migrator itself functions without having
-			// any instances of the OutputInterface contract passed into the class.
-			foreach ($this->migrator->getNotes() as $note)
-			{
-				$this->output->writeln($note);
-			}
-
-			if ($count == 0) break;
+			$this->output->writeln($note);
 		}
 	}
 
