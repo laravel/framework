afe807fabed896206de3714d50ab50b3a095a735
Lucas Michot <lucas@semalead.com>
2015-04-07 18:02:55 +0200

    Ensure return is on a new line

--- src/Illuminate/Support/Facades/Password.php
+++ src/Illuminate/Support/Facades/Password.php
@@ -45,6 +45,9 @@ class Password extends Facade {
 	 *
 	 * @return string
 	 */
-	protected static function getFacadeAccessor() { return 'auth.password'; }
+	protected static function getFacadeAccessor()
+	{
+		return 'auth.password';
+	}
 
 }
