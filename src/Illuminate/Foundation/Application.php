97ecc93016ee2fc2650e9a463ee916e8c2922deb
Taylor Otwell <taylorotwell@gmail.com>
2015-04-17 08:54:20 -0500

    tabs

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -465,7 +465,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 		$manifestPath = $this->getCachedServicesPath();
 
 		(new ProviderRepository($this, new Filesystem, $manifestPath))
-		            ->load($this->config['app.providers']);
+					->load($this->config['app.providers']);
 	}
 
 	/**
@@ -941,14 +941,14 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 	}
 
 	/**
-     * Get the application's deferred services.
-     *
-     * @return array
-     */
-    public function getDeferredServices()
-    {
-        return $this->deferredServices;
-    }
+	 * Get the application's deferred services.
+	 *
+	 * @return array
+	 */
+	public function getDeferredServices()
+	{
+		return $this->deferredServices;
+	}
 
 	/**
 	 * Set the application's deferred services.
