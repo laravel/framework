d122d8ceb07274a8aa9cac40ec809e5f697cfff5
Taylor Otwell <taylorotwell@gmail.com>
2015-04-23 08:52:35 -0500

    fix formatting

--- src/Illuminate/Filesystem/Filesystem.php
+++ src/Illuminate/Filesystem/Filesystem.php
@@ -1,5 +1,6 @@
 <?php namespace Illuminate\Filesystem;
 
+use ErrorException;
 use FilesystemIterator;
 use Symfony\Component\Finder\Finder;
 use Illuminate\Support\Traits\Macroable;
@@ -115,11 +116,17 @@ class Filesystem {
 
 		$success = true;
 
-		foreach ($paths as $path) { 
-			try {
-				if ( ! @unlink($path)) 
-					$success = false; 
-			} catch (\ErrorException $e) {
+		foreach ($paths as $path)
+		{
+			try
+			{
+				if ( ! @unlink($path))
+				{
+					$success = false;
+				}
+			}
+			catch (ErrorException $e)
+			{
 				$success = false;
 			}
 		}
