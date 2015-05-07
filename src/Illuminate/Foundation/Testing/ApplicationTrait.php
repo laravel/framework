8efd39e5b6d5e74928a40709ee54d7ef3c591b89
Taylor Otwell <taylorotwell@gmail.com>
2015-05-06 20:56:10 -0500

    Commit application trait.

---
+++ src/Illuminate/Foundation/Testing/ApplicationTrait.php
@@ -0,0 +1,111 @@
+<?php namespace Illuminate\Foundation\Testing;
+
+use Illuminate\Http\Request;
+use InvalidArgumentException;
+use Symfony\Component\DomCrawler\Form;
+use Symfony\Component\DomCrawler\Crawler;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+use PHPUnit_Framework_ExpectationFailedException as PHPUnitException;
+
+trait ApplicationTrait
+{
+    /**
+     * The Illuminate application instance.
+     *
+     * @var \Illuminate\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * The last code returned by Artisan CLI.
+     *
+     * @var int
+     */
+    protected $code;
+
+    /**
+     * Refresh the application instance.
+     *
+     * @return void
+     */
+    protected function refreshApplication()
+    {
+        putenv('APP_ENV=testing');
+
+        $this->app = $this->createApplication();
+    }
+
+    /**
+     * Set the session to the given array.
+     *
+     * @param  array  $data
+     * @return void
+     */
+    public function session(array $data)
+    {
+        $this->startSession();
+
+        foreach ($data as $key => $value) {
+            $this->app['session']->put($key, $value);
+        }
+    }
+
+    /**
+     * Start the session for the application.
+     *
+     * @return void
+     */
+    protected function startSession()
+    {
+        if (! $this->app['session']->isStarted()) {
+            $this->app['session']->start();
+        }
+    }
+
+    /**
+     * Flush all of the current session data.
+     *
+     * @return void
+     */
+    public function flushSession()
+    {
+        $this->startSession();
+
+        $this->app['session']->flush();
+    }
+
+    /**
+     * Set the currently logged in user for the application.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  string  $driver
+     * @return void
+     */
+    public function be(UserContract $user, $driver = null)
+    {
+        $this->app['auth']->driver($driver)->setUser($user);
+    }
+
+    /**
+     * Seed a given database connection.
+     *
+     * @param  string  $class
+     * @return void
+     */
+    public function seed($class = 'DatabaseSeeder')
+    {
+        $this->artisan('db:seed', ['--class' => $class]);
+    }
+
+    /**
+     * Call artisan command and return code.
+     *
+     * @param string  $command
+     * @param array   $parameters
+     * @return int
+     */
+    public function artisan($command, $parameters = [])
+    {
+        return $this->code = $this->app['Illuminate\Contracts\Console\Kernel']->call($command, $parameters);
+    }
+}
