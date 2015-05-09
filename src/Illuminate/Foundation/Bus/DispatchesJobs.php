b2eb645ffeb0885c550a8840fbf6b2b9a81b20f9
Taylor Otwell <taylorotwell@gmail.com>
2015-05-09 17:11:16 -0500

    Change trait name.

---
+++ src/Illuminate/Foundation/Bus/DispatchesJobs.php
@@ -0,0 +1,43 @@
+<?php namespace Illuminate\Foundation\Bus;
+
+use ArrayAccess;
+
+trait DispatchesJobs {
+
+	/**
+	 * Dispatch a job to its appropriate handler.
+	 *
+	 * @param  mixed  $job
+	 * @return mixed
+	 */
+	protected function dispatch($job)
+	{
+		return app('Illuminate\Contracts\Bus\Dispatcher')->dispatch($job);
+	}
+
+	/**
+	 * Marshal a job and dispatch it to its appropriate handler.
+	 *
+	 * @param  mixed  $job
+	 * @param  array  $array
+	 * @return mixed
+	 */
+	protected function dispatchFromArray($job, array $array)
+	{
+		return app('Illuminate\Contracts\Bus\Dispatcher')->dispatchFromArray($job, $array);
+	}
+
+	/**
+	 * Marshal a job and dispatch it to its appropriate handler.
+	 *
+	 * @param  mixed  $job
+	 * @param  \ArrayAccess  $source
+	 * @param  array  $extras
+	 * @return mixed
+	 */
+	protected function dispatchFrom($job, ArrayAccess $source, $extras = [])
+	{
+		return app('Illuminate\Contracts\Bus\Dispatcher')->dispatchFrom($job, $source, $extras);
+	}
+
+}
