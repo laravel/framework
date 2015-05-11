7f5c26a8551ab25b6c31e72439b38cd272799699
Taylor Otwell <taylorotwell@gmail.com>
2015-05-10 20:55:35 -0500

    Added factory helper.

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -269,6 +269,31 @@ if ( ! function_exists('delete'))
 	}
 }
 
+if ( ! function_exists('factory'))
+{
+	/**
+	 * Create a model factory builder for a given class, name, and amount.
+	 *
+	 * @param  dynamic  class|class,name|class,amount|class,name,amount
+	 * @return \Illuminate\Database\Eloquent\FactoryBuilder
+	 */
+	function factory()
+	{
+		$factory = app('Illuminate\Database\Eloquent\Factory');
+
+		$arguments = func_get_args();
+
+		if (isset($arguments[1]) && is_string($arguments[1])) {
+			return $factory->of($arguments[0], $arguments[1])->times(isset($arguments[2]) ? $arguments[2] : 1);
+		} elseif (isset($arguments[1])) {
+			return $factory->of($arguments[0])->times($arguments[1]);
+		} else {
+			return $factory->of($arguments[0]);
+		}
+	}
+}
+
+
 if ( ! function_exists('get'))
 {
 	/**
