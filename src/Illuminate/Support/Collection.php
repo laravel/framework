66402fa18fcd5f808fca445b86b20a3a8ec139fe
Lucas Michot <lucas@semalead.com>
2015-05-15 13:20:09 +0200

    Get rid of PHPStorm warning

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -537,7 +537,7 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
 
 		foreach ($this->items as $key => $item)
 		{
-			if ($value($item, $key)) return $key;
+			if (call_user_func($value, $item, $key)) return $key;
 		}
 
 		return false;
