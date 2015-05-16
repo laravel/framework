9a20497439aea19d98dd511c607cd7db7d9214ed
Taylor Otwell <taylorotwell@gmail.com>
2015-05-15 21:15:41 -0500

    Revert "[5.1] Add keys to transform"

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -738,7 +738,7 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
 	 */
 	public function transform(callable $callback)
 	{
-		$this->items = array_map($callback, $this->items, array_keys($this->items));
+		$this->items = array_map($callback, $this->items);
 
 		return $this;
 	}
