2b213aea61a40b30b1c273b0f1648a67bc2a3e8a
J Bruni <contato@jbruni.com.br>
2015-05-22 17:11:18 -0300

    Make 'queue:table' and 'queue:failed-table' commands respect custom queue tables configuration

--- src/Illuminate/Queue/Console/TableCommand.php
+++ src/Illuminate/Queue/Console/TableCommand.php
@@ -55,7 +55,7 @@ class TableCommand extends Command {
 	{
 		$fullPath = $this->createBaseMigration();
 
-		$table = config('queue.connections.database.table', 'jobs');
+		$table = $this->laravel->make('config')->get('queue.connections.database.table', 'jobs');
 
 		$stub = str_replace('{{table}}', $table, $this->files->get(__DIR__.'/stubs/jobs.stub'));
 
