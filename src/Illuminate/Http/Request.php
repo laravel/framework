cf7c161453697a2996cbec68aba89f29162536d1
Edward Coleridge Smith <edward.smith@pod-point.com>
2015-05-25 20:16:52 +0100

    all() doesn't accept any arguments

--- src/Illuminate/Http/Request.php
+++ src/Illuminate/Http/Request.php
@@ -784,7 +784,7 @@ class Request extends SymfonyRequest implements ArrayAccess {
 
 		if (array_key_exists($key, $all))
 		{
-			return $this->all($key);
+			return $this->all();
 		}
 		elseif ( ! is_null($this->route()))
 		{
