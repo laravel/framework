c49ae7ef77915da0b05e1231783d14b782020587
Graham Campbell <graham@cachethq.io>
2015-05-28 23:39:35 +0100

    Support both guzzle 5 and guzzle 6

--- src/Illuminate/Mail/Transport/MailgunTransport.php
+++ src/Illuminate/Mail/Transport/MailgunTransport.php
@@ -2,6 +2,7 @@
 
 use Swift_Transport;
 use Swift_Mime_Message;
+use GuzzleHttp\Post\PostFile;
 use Swift_Events_EventListener;
 use GuzzleHttp\ClientInterface;
 
@@ -79,12 +80,21 @@ class MailgunTransport implements Swift_Transport {
 	 */
 	public function send(Swift_Mime_Message $message, &$failedRecipients = null)
 	{
-		return $this->client->post($this->url, ['auth' => ['api', $this->key],
-			'multipart' => [
+		$options = ['auth' => ['api', $this->key]];
+
+		if (version_compare(ClientInterface::VERSION, '6') === 1) {
+			$options['multipart'] = [
 				['name' => 'to', 'contents' => $this->getTo($message)],
 				['name' => 'message', 'contents' => (string) $message, 'filename' => 'message.mime'],
-			],
-		]);
+			];
+		} else {
+			$options['body'] = [
+				'to' => $this->getTo($message),
+				'message' => new PostFile('message', (string) $message),
+			];
+		}
+
+		return $this->client->post($this->url, $options);
 	}
 
 	/**
