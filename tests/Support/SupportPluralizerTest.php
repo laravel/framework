e5aa4730dddf79ffefb07cf3b44d8bfa0f0fb38d
Taylor Otwell <taylorotwell@gmail.com>
2015-05-29 09:33:14 -0500

    Remove a few tests we don't need.

--- tests/Support/SupportPluralizerTest.php
+++ tests/Support/SupportPluralizerTest.php
@@ -6,17 +6,12 @@ class SupportPluralizerTest extends PHPUnit_Framework_TestCase {
 	public function testBasicSingular()
 	{
 		$this->assertEquals('child', str_singular('children'));
-		$this->assertEquals('test', str_singular('tests'));
-		$this->assertEquals('deer', str_singular('deer'));
-		$this->assertEquals('criterion', str_singular('criteria'));
 	}
 
 
 	public function testBasicPlural()
 	{
 		$this->assertEquals('children', str_plural('child'));
-		$this->assertEquals('tests', str_plural('test'));
-		$this->assertEquals('deer', str_plural('deer'));
 	}
 
 
@@ -25,11 +20,6 @@ class SupportPluralizerTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('Child', str_singular('Children'));
 		$this->assertEquals('CHILD', str_singular('CHILDREN'));
 		$this->assertEquals('Test', str_singular('Tests'));
-		$this->assertEquals('TEST', str_singular('TESTS'));
-		$this->assertEquals('Deer', str_singular('Deer'));
-		$this->assertEquals('DEER', str_singular('DEER'));
-		$this->assertEquals('Criterion', str_singular('Criteria'));
-		$this->assertEquals('CRITERION', str_singular('CRITERIA'));
 	}
 
 
@@ -38,10 +28,6 @@ class SupportPluralizerTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('Children', str_plural('Child'));
 		$this->assertEquals('CHILDREN', str_plural('CHILD'));
 		$this->assertEquals('Tests', str_plural('Test'));
-		$this->assertEquals('TESTS', str_plural('TEST'));
-		$this->assertEquals('tests', str_plural('test'));
-		$this->assertEquals('Deer', str_plural('Deer'));
-		$this->assertEquals('DEER', str_plural('DEER'));
 	}
 
 
