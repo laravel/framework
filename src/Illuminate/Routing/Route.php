94345b9bbb6e83fab8ddb4b1dc927b334529f2a3
Sergiy Litvinchuk <s@ideil.com>
2015-05-31 13:24:40 +0300

    Fix. Route prefixing not working in some cases

--- src/Illuminate/Routing/Route.php
+++ src/Illuminate/Routing/Route.php
@@ -756,7 +756,9 @@ class Route {
 	 */
 	public function prefix($prefix)
 	{
-		$this->uri = trim($prefix, '/').'/'.trim($this->uri, '/');
+		$uri = rtrim($prefix, '/').'/'.ltrim($this->uri, '/');
+
+		$this->uri = trim($uri , '/');
 
 		return $this;
 	}
