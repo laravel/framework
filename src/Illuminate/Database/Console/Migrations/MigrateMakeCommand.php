8e284e24ae0f03dd96bb34f6d018c6c80c4e8c92
Rubem Mota <rubemmota89@gmail.com>
2015-05-31 14:09:35 +0100

    Added missing variable on method calling
    Added missing variable on method calling and declaration

--- src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
+++ src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
@@ -73,7 +73,7 @@ class MigrateMakeCommand extends BaseCommand {
 		// Now we are ready to write the migration out to disk. Once we've written
 		// the migration out, we will dump-autoload for the entire framework to
 		// make sure that the migrations are registered by the class loaders.
-		$this->writeMigration($name, $table, $create);
+		$this->writeMigration($name, $table, $create, $path);
 
 		$this->composer->dumpAutoloads();
 	}
@@ -86,7 +86,7 @@ class MigrateMakeCommand extends BaseCommand {
 	 * @param  bool    $create
 	 * @return string
 	 */
-	protected function writeMigration($name, $table, $create)
+	protected function writeMigration($name, $table, $create, $path)
 	{
 		$path = $this->getMigrationPath();
 
