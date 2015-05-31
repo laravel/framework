bf9a51d56d12bcab0c0a2503d9d6463c17465e1c
Rubem Mota <rubemmota89@gmail.com>
2015-05-31 14:06:44 +0100

    Added path option to migration create command
    Added option to migration create command so users can specify a custom
    directory to store migrations
    
    Added missing variable on method calling
    
    Added missing variable on method calling and declaration
    
    changed $path value to null instead NULL
    
    added missing phpdoc relative to path

--- src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
+++ src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
@@ -66,12 +66,14 @@ class MigrateMakeCommand extends BaseCommand {
 
 		$create = $this->input->getOption('create');
 
+		$path = $this->input->getOption('path');
+
 		if ( ! $table && is_string($create)) $table = $create;
 
 		// Now we are ready to write the migration out to disk. Once we've written
 		// the migration out, we will dump-autoload for the entire framework to
 		// make sure that the migrations are registered by the class loaders.
-		$this->writeMigration($name, $table, $create);
+		$this->writeMigration($name, $table, $create, $path);
 
 		$this->composer->dumpAutoloads();
 	}
@@ -82,9 +84,10 @@ class MigrateMakeCommand extends BaseCommand {
 	 * @param  string  $name
 	 * @param  string  $table
 	 * @param  bool    $create
+	 * @param  string  $path
 	 * @return string
 	 */
-	protected function writeMigration($name, $table, $create)
+	protected function writeMigration($name, $table, $create, $path)
 	{
 		$path = $this->getMigrationPath();
 
@@ -116,6 +119,8 @@ class MigrateMakeCommand extends BaseCommand {
 			array('create', null, InputOption::VALUE_OPTIONAL, 'The table to be created.'),
 
 			array('table', null, InputOption::VALUE_OPTIONAL, 'The table to migrate.'),
+
+			array('path', null, InputOption::VALUE_OPTIONAL, 'The path where migration it will be stored.'),
 		);
 	}
 
