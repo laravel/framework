8eafc9b7326dce66c786cbfeacb3baf9334fcb38
Franz Liedke <franz@develophp.org>
2015-06-03 03:48:24 +0200

    Deprecate bindShared() method

--- src/Illuminate/Container/Container.php
+++ src/Illuminate/Container/Container.php
@@ -283,6 +283,8 @@ class Container implements ArrayAccess, ContainerContract
      * @param  string    $abstract
      * @param  \Closure  $closure
      * @return void
+     *
+     * @deprecated since version 5.1
      */
     public function bindShared($abstract, Closure $closure)
     {
