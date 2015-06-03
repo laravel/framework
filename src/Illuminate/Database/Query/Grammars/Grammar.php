d68be3e9d4a18f6f2a7fa316aaa02402543362b0
Lev Nazarenko <gwae@yandex.ru>
2015-06-03 12:11:01 +0300

    Added \Illuminate\Database\Query\JoinClause::whereIn/whereNotIn/orWhereIn/orWhereNotIn methods

--- src/Illuminate/Database/Query/Grammars/Grammar.php
+++ src/Illuminate/Database/Query/Grammars/Grammar.php
@@ -177,7 +177,14 @@ class Grammar extends BaseGrammar
     {
         $first = $this->wrap($clause['first']);
 
-        $second = $clause['where'] ? '?' : $this->wrap($clause['second']);
+        $isIn = $clause['operator'] === 'in' || $clause['operator'] === 'not in';
+        if ($clause['where'] && $isIn) {
+            $second = '(' . join(',', array_fill(0, $clause['second'], '?')) . ')';
+        } elseif ($clause['where']) {
+            $second = '?';
+        } else {
+            $second = $this->wrap($clause['second']);
+        }
 
         return "{$clause['boolean']} $first {$clause['operator']} $second";
     }
