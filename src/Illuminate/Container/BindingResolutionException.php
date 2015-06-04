802e7395a815f7842ea781ec2040be1ef7a06306
Graham Campbell <graham@cachethq.io>
2015-06-04 17:52:20 +0100

    Move BindingResolutionException exception
    To be deleted in 5.2. For now, old catch blocks will still work.

--- src/Illuminate/Container/BindingResolutionException.php
+++ src/Illuminate/Container/BindingResolutionException.php
@@ -4,6 +4,9 @@ namespace Illuminate\Container;
 
 use Exception;
 
+/**
+ * @deprecated since version 5.1. Use Illuminate\Contracts\Container\BindingResolutionException.
+ */
 class BindingResolutionException extends Exception
 {
 }
---
+++ src/Illuminate/Contracts/Container/BindingResolutionException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Illuminate\Contracts\Container;
+
+use Illuminate\Container\BindingResolutionException as BaseException;
+
+class BindingResolutionException extends BaseException
+{
+}
