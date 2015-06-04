d9a17ef9a41c14afa9d7cd65e52f588a6c56c6f5
Proshanta Barman <proshanto.barman@gmail.com>
2015-06-04 22:16:15 +0600

    Fix DocBlocks and remove unnecessary use statements

--- src/Illuminate/Foundation/Auth/ResetsPasswords.php
+++ src/Illuminate/Foundation/Auth/ResetsPasswords.php
@@ -1,7 +1,6 @@
 <?php namespace Illuminate\Foundation\Auth;
 
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\PasswordBroker;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
