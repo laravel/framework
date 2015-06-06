6173267d1de51d812347c2b8d887bd132e1f4427
Taylor Otwell <taylorotwell@gmail.com>
2015-06-06 17:06:39 -0500

    improve console parser. add tests

---
+++ tests/Console/ConsoleParserTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Console\Parser;
+
+class ConsoleParserTest extends PHPUnit_Framework_TestCase
+{
+	public function testBasicParameterParsing()
+	{
+		$results = Parser::parse('command:name {argument} {--option}');
+
+		$this->assertEquals('command:name', $results[0]);
+		$this->assertEquals('argument', $results[1][0]->getName());
+		$this->assertEquals('option', $results[2][0]->getName());
+		$this->assertFalse($results[2][0]->acceptValue());
+
+		$results = Parser::parse('command:name {argument*} {--option=}');
+
+		$this->assertEquals('command:name', $results[0]);
+		$this->assertEquals('argument', $results[1][0]->getName());
+		$this->assertTrue($results[1][0]->isArray());
+		$this->assertTrue($results[1][0]->isRequired());
+		$this->assertEquals('option', $results[2][0]->getName());
+		$this->assertTrue($results[2][0]->acceptValue());
+
+		$results = Parser::parse('command:name {argument?*} {--option=*}');
+
+		$this->assertEquals('command:name', $results[0]);
+		$this->assertEquals('argument', $results[1][0]->getName());
+		$this->assertTrue($results[1][0]->isArray());
+		$this->assertFalse($results[1][0]->isRequired());
+		$this->assertEquals('option', $results[2][0]->getName());
+		$this->assertTrue($results[2][0]->acceptValue());
+		$this->assertTrue($results[2][0]->isArray());
+
+		$results = Parser::parse('command:name
+			{argument?* : The argument description.}
+			{--option=* : The option description.}');
+
+		$this->assertEquals('command:name', $results[0]);
+		$this->assertEquals('argument', $results[1][0]->getName());
+		$this->assertEquals('The argument description.', $results[1][0]->getDescription());
+		$this->assertTrue($results[1][0]->isArray());
+		$this->assertFalse($results[1][0]->isRequired());
+		$this->assertEquals('option', $results[2][0]->getName());
+		$this->assertEquals('The option description.', $results[2][0]->getDescription());
+		$this->assertTrue($results[2][0]->acceptValue());
+		$this->assertTrue($results[2][0]->isArray());
+	}
+}
