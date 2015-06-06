fc2e168eadd3d1374ed3b90550e8bcd97c885775
Taylor Otwell <taylorotwell@gmail.com>
2015-06-06 17:11:58 -0500

    fixes

--- tests/Console/ConsoleParserTest.php
+++ tests/Console/ConsoleParserTest.php
@@ -6,6 +6,10 @@ class ConsoleParserTest extends PHPUnit_Framework_TestCase
 {
 	public function testBasicParameterParsing()
 	{
+		$results = Parser::parse('command:name');
+
+		$this->assertEquals('command:name', $results[0]);
+
 		$results = Parser::parse('command:name {argument} {--option}');
 
 		$this->assertEquals('command:name', $results[0]);
@@ -29,6 +33,18 @@ class ConsoleParserTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue($results[1][0]->isArray());
 		$this->assertFalse($results[1][0]->isRequired());
 		$this->assertEquals('option', $results[2][0]->getName());
+		$this->assertTrue($results[2][0]->acceptValue());
+		$this->assertTrue($results[2][0]->isArray());
+
+		$results = Parser::parse('command:name {argument?* : The argument description.}	{--option=* : The option description.}');
+
+		$this->assertEquals('command:name', $results[0]);
+		$this->assertEquals('argument', $results[1][0]->getName());
+		$this->assertEquals('The argument description.', $results[1][0]->getDescription());
+		$this->assertTrue($results[1][0]->isArray());
+		$this->assertFalse($results[1][0]->isRequired());
+		$this->assertEquals('option', $results[2][0]->getName());
+		$this->assertEquals('The option description.', $results[2][0]->getDescription());
 		$this->assertTrue($results[2][0]->acceptValue());
 		$this->assertTrue($results[2][0]->isArray());
 
