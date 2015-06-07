de30af3b4d5aea684e637c54b11b7a445b32bd65
Tomasz Małecki <tomasz.malecki@relime.com>
2015-06-07 14:17:05 +0200

    Pass the most recent response content to the Crawler.

--- src/Illuminate/Foundation/Testing/CrawlerTrait.php
+++ src/Illuminate/Foundation/Testing/CrawlerTrait.php
@@ -137,13 +137,13 @@ trait CrawlerTrait
     {
         $uri = $this->prepareUrlForRequest($uri);
 
-        $response = $this->call($method, $uri, $parameters, $cookies, $files);
+        $this->call($method, $uri, $parameters, $cookies, $files);
 
         $this->clearInputs()->followRedirects()->assertPageLoaded($uri);
 
         $this->currentUri = $this->app->make('request')->fullUrl();
 
-        $this->crawler = new Crawler($response->getContent(), $uri);
+        $this->crawler = new Crawler($this->response->getContent(), $uri);
 
         return $this;
     }
