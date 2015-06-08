1e40618510846101d1190b55bc6946f6a12ea7e7
Graham Campbell <graham@cachethq.io>
2015-06-08 20:12:16 +0100

    CS fixes

--- tests/Console/ConsoleParserTest.php
+++ tests/Console/ConsoleParserTest.php
@@ -4,62 +4,62 @@ use Illuminate\Console\Parser;
 
 class ConsoleParserTest extends PHPUnit_Framework_TestCase
 {
-	public function testBasicParameterParsing()
-	{
-		$results = Parser::parse('command:name');
+    public function testBasicParameterParsing()
+    {
+        $results = Parser::parse('command:name');
 
-		$this->assertEquals('command:name', $results[0]);
+        $this->assertEquals('command:name', $results[0]);
 
-		$results = Parser::parse('command:name {argument} {--option}');
+        $results = Parser::parse('command:name {argument} {--option}');
 
-		$this->assertEquals('command:name', $results[0]);
-		$this->assertEquals('argument', $results[1][0]->getName());
-		$this->assertEquals('option', $results[2][0]->getName());
-		$this->assertFalse($results[2][0]->acceptValue());
+        $this->assertEquals('command:name', $results[0]);
+        $this->assertEquals('argument', $results[1][0]->getName());
+        $this->assertEquals('option', $results[2][0]->getName());
+        $this->assertFalse($results[2][0]->acceptValue());
 
-		$results = Parser::parse('command:name {argument*} {--option=}');
+        $results = Parser::parse('command:name {argument*} {--option=}');
 
-		$this->assertEquals('command:name', $results[0]);
-		$this->assertEquals('argument', $results[1][0]->getName());
-		$this->assertTrue($results[1][0]->isArray());
-		$this->assertTrue($results[1][0]->isRequired());
-		$this->assertEquals('option', $results[2][0]->getName());
-		$this->assertTrue($results[2][0]->acceptValue());
+        $this->assertEquals('command:name', $results[0]);
+        $this->assertEquals('argument', $results[1][0]->getName());
+        $this->assertTrue($results[1][0]->isArray());
+        $this->assertTrue($results[1][0]->isRequired());
+        $this->assertEquals('option', $results[2][0]->getName());
+        $this->assertTrue($results[2][0]->acceptValue());
 
-		$results = Parser::parse('command:name {argument?*} {--option=*}');
+        $results = Parser::parse('command:name {argument?*} {--option=*}');
 
-		$this->assertEquals('command:name', $results[0]);
-		$this->assertEquals('argument', $results[1][0]->getName());
-		$this->assertTrue($results[1][0]->isArray());
-		$this->assertFalse($results[1][0]->isRequired());
-		$this->assertEquals('option', $results[2][0]->getName());
-		$this->assertTrue($results[2][0]->acceptValue());
-		$this->assertTrue($results[2][0]->isArray());
+        $this->assertEquals('command:name', $results[0]);
+        $this->assertEquals('argument', $results[1][0]->getName());
+        $this->assertTrue($results[1][0]->isArray());
+        $this->assertFalse($results[1][0]->isRequired());
+        $this->assertEquals('option', $results[2][0]->getName());
+        $this->assertTrue($results[2][0]->acceptValue());
+        $this->assertTrue($results[2][0]->isArray());
 
-		$results = Parser::parse('command:name {argument?* : The argument description.}	{--option=* : The option description.}');
+        $results = Parser::parse('command:name {argument?* : The argument description.}    {--option=* : The option description.}');
 
-		$this->assertEquals('command:name', $results[0]);
-		$this->assertEquals('argument', $results[1][0]->getName());
-		$this->assertEquals('The argument description.', $results[1][0]->getDescription());
-		$this->assertTrue($results[1][0]->isArray());
-		$this->assertFalse($results[1][0]->isRequired());
-		$this->assertEquals('option', $results[2][0]->getName());
-		$this->assertEquals('The option description.', $results[2][0]->getDescription());
-		$this->assertTrue($results[2][0]->acceptValue());
-		$this->assertTrue($results[2][0]->isArray());
+        $this->assertEquals('command:name', $results[0]);
+        $this->assertEquals('argument', $results[1][0]->getName());
+        $this->assertEquals('The argument description.', $results[1][0]->getDescription());
+        $this->assertTrue($results[1][0]->isArray());
+        $this->assertFalse($results[1][0]->isRequired());
+        $this->assertEquals('option', $results[2][0]->getName());
+        $this->assertEquals('The option description.', $results[2][0]->getDescription());
+        $this->assertTrue($results[2][0]->acceptValue());
+        $this->assertTrue($results[2][0]->isArray());
 
-		$results = Parser::parse('command:name
-			{argument?* : The argument description.}
-			{--option=* : The option description.}');
+        $results = Parser::parse('command:name
+            {argument?* : The argument description.}
+            {--option=* : The option description.}');
 
-		$this->assertEquals('command:name', $results[0]);
-		$this->assertEquals('argument', $results[1][0]->getName());
-		$this->assertEquals('The argument description.', $results[1][0]->getDescription());
-		$this->assertTrue($results[1][0]->isArray());
-		$this->assertFalse($results[1][0]->isRequired());
-		$this->assertEquals('option', $results[2][0]->getName());
-		$this->assertEquals('The option description.', $results[2][0]->getDescription());
-		$this->assertTrue($results[2][0]->acceptValue());
-		$this->assertTrue($results[2][0]->isArray());
-	}
+        $this->assertEquals('command:name', $results[0]);
+        $this->assertEquals('argument', $results[1][0]->getName());
+        $this->assertEquals('The argument description.', $results[1][0]->getDescription());
+        $this->assertTrue($results[1][0]->isArray());
+        $this->assertFalse($results[1][0]->isRequired());
+        $this->assertEquals('option', $results[2][0]->getName());
+        $this->assertEquals('The option description.', $results[2][0]->getDescription());
+        $this->assertTrue($results[2][0]->acceptValue());
+        $this->assertTrue($results[2][0]->isArray());
+    }
 }
