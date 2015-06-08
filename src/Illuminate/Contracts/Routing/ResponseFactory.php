55808b81e23d35844f85782272fbe09a4e652091
Graham Campbell <graham@cachethq.io>
2015-06-08 11:23:01 +0100

    Fixed the phpdoc in the response factory contract

--- src/Illuminate/Contracts/Routing/ResponseFactory.php
+++ src/Illuminate/Contracts/Routing/ResponseFactory.php
@@ -8,9 +8,9 @@ interface ResponseFactory
      * Return a new response from the application.
      *
      * @param  string  $content
-     * @param  int     $status
-     * @param  array   $headers
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\Response
      */
     public function make($content = '', $status = 200, array $headers = []);
 
@@ -18,10 +18,10 @@ interface ResponseFactory
      * Return a new view response from the application.
      *
      * @param  string  $view
-     * @param  array   $data
-     * @param  int     $status
-     * @param  array   $headers
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  array  $data
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\Response
      */
     public function view($view, $data = [], $status = 200, array $headers = []);
 
@@ -29,10 +29,10 @@ interface ResponseFactory
      * Return a new JSON response from the application.
      *
      * @param  string|array  $data
-     * @param  int    $status
+     * @param  int  $status
      * @param  array  $headers
-     * @param  int    $options
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  int  $options
+     * @return \Illuminate\Http\JsonResponse
      */
     public function json($data = [], $status = 200, array $headers = [], $options = 0);
 
@@ -41,10 +41,10 @@ interface ResponseFactory
      *
      * @param  string  $callback
      * @param  string|array  $data
-     * @param  int    $status
+     * @param  int  $status
      * @param  array  $headers
-     * @param  int    $options
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  int  $options
+     * @return \Illuminate\Http\JsonResponse
      */
     public function jsonp($callback, $data = [], $status = 200, array $headers = [], $options = 0);
 
@@ -52,8 +52,8 @@ interface ResponseFactory
      * Return a new streamed response from the application.
      *
      * @param  \Closure  $callback
-     * @param  int      $status
-     * @param  array    $headers
+     * @param  int  $status
+     * @param  array  $headers
      * @return \Symfony\Component\HttpFoundation\StreamedResponse
      */
     public function stream($callback, $status = 200, array $headers = []);
@@ -63,8 +63,8 @@ interface ResponseFactory
      *
      * @param  \SplFileInfo|string  $file
      * @param  string  $name
-     * @param  array   $headers
-     * @param  null|string  $disposition
+     * @param  array  $headers
+     * @param  string|null  $disposition
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
     public function download($file, $name = null, array $headers = [], $disposition = 'attachment');
@@ -73,10 +73,10 @@ interface ResponseFactory
      * Create a new redirect response to the given path.
      *
      * @param  string  $path
-     * @param  int     $status
-     * @param  array   $headers
-     * @param  bool    $secure
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function redirectTo($path, $status = 302, $headers = [], $secure = null);
 
@@ -84,10 +84,10 @@ interface ResponseFactory
      * Create a new redirect response to a named route.
      *
      * @param  string  $route
-     * @param  array   $parameters
-     * @param  int     $status
-     * @param  array   $headers
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  array  $parameters
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function redirectToRoute($route, $parameters = [], $status = 302, $headers = []);
 
@@ -95,10 +95,10 @@ interface ResponseFactory
      * Create a new redirect response to a controller action.
      *
      * @param  string  $action
-     * @param  array   $parameters
-     * @param  int     $status
-     * @param  array   $headers
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  array  $parameters
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function redirectToAction($action, $parameters = [], $status = 302, $headers = []);
 
@@ -106,10 +106,10 @@ interface ResponseFactory
      * Create a new redirect response, while putting the current URL in the session.
      *
      * @param  string  $path
-     * @param  int     $status
-     * @param  array   $headers
-     * @param  bool    $secure
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function redirectGuest($path, $status = 302, $headers = [], $secure = null);
 
@@ -117,10 +117,10 @@ interface ResponseFactory
      * Create a new redirect response to the previously intended location.
      *
      * @param  string  $default
-     * @param  int     $status
-     * @param  array   $headers
-     * @param  bool    $secure
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function redirectToIntended($default = '/', $status = 302, $headers = [], $secure = null);
 }
