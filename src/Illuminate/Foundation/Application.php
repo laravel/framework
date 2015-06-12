ed5663baf21128496ff0e3187468d22aeb49672d
Taylor Otwell <taylorotwell@gmail.com>
2015-06-12 09:42:36 -0500

    a few changes'

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -252,7 +252,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      */
     public function setBasePath($basePath)
     {
-        $this->basePath = rtrim($basePath, DIRECTORY_SEPARATOR);
+        $this->basePath = rtrim($basePath, '\/');
 
         $this->bindPathsInContainer();
 
