c3cbde0aa28d49d20c5d5076ac63e13e53555f48
Laurence Ioannou <github@theshiftexchange.com>
2015-06-21 19:17:15 +1000

    Update Event.php

--- src/Illuminate/Console/Scheduling/Event.php
+++ src/Illuminate/Console/Scheduling/Event.php
@@ -124,7 +124,7 @@ class Event
      */
     public function run(Container $container)
     {
-        if ((count($this->afterCallbacks) > 0) || (count($this->beforeCallbacks) > 0)) {
+        if (count($this->afterCallbacks) > 0 || count($this->beforeCallbacks) > 0) {
             $this->runCommandInForeground($container);
         } else {
             $this->runCommandInBackground();
