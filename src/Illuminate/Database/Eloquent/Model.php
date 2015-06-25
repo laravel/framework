dd31d25426a5e39ef7f80875281754c5d7cf6abd
Harry <harrygrumbar@hotmail.com>
2015-06-25 11:46:14 +0100

    Tidy up unnecessary elseifs
    All the conditinal checks `return` from the method so there is no need to have chained `elseifs`. Changed to separate `ifs` and removed the last check as it's implied if all the other checks are not satisfied.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -2832,25 +2832,24 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
         // If this value is an integer, we will assume it is a UNIX timestamp's value
         // and format a Carbon object from this timestamp. This allows flexibility
         // when defining your date fields as they might be UNIX timestamps here.
-        elseif (is_numeric($value)) {
+        if (is_numeric($value)) {
             return Carbon::createFromTimestamp($value);
         }
 
         // If the value is in simply year, month, day format, we will instantiate the
         // Carbon instances from that format. Again, this provides for simple date
         // fields on the database, while still supporting Carbonized conversion.
-        elseif (preg_match('/^(\d{4})-(\d{2})-(\d{2})$/', $value)) {
+        if (preg_match('/^(\d{4})-(\d{2})-(\d{2})$/', $value)) {
             return Carbon::createFromFormat('Y-m-d', $value)->startOfDay();
         }
 
         // Finally, we will just assume this date is in the format used by default on
         // the database connection and use that format to create the Carbon object
         // that is returned back out to the developers after we convert it here.
-        elseif (!$value instanceof DateTime) {
-            $format = $this->getDateFormat();
+        $format = $this->getDateFormat();
 
-            return Carbon::createFromFormat($format, $value);
-        }
+        return Carbon::createFromFormat($format, $value);
+        
     }
 
     /**
