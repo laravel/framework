2c357bc2d2e325e5ceecfcd3244a20745c5c5ef1
Rob Sanchez <rob@robsanchez.com>
2015-06-28 08:19:41 -0500

    csrf_field helper returns a View Expression instead of string

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -204,7 +204,7 @@ if (!function_exists('csrf_field')) {
      */
     function csrf_field()
     {
-        return '<input type="hidden" name="_token" value="'.csrf_token().'">';
+        return new Illuminate\View\Expression('<input type="hidden" name="_token" value="'.csrf_token().'">');
     }
 }
 
