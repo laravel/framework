1549885347b4f22ad648b66e5b02354290e274c8
Taylor Otwell <taylorotwell@gmail.com>
2015-06-29 13:23:28 -0500

    change visibililty

--- src/Illuminate/Pagination/BootstrapThreeNextPreviousButtonRendererTrait.php
+++ src/Illuminate/Pagination/BootstrapThreeNextPreviousButtonRendererTrait.php
@@ -10,7 +10,7 @@ trait BootstrapThreeNextPreviousButtonRendererTrait
      * @param  string  $text
      * @return string
      */
-    protected function getPreviousButton($text = '&laquo;')
+    public function getPreviousButton($text = '&laquo;')
     {
         // If the current page is less than or equal to one, it means we can't go any
         // further back in the pages, so we will render a disabled previous button
@@ -32,7 +32,7 @@ trait BootstrapThreeNextPreviousButtonRendererTrait
      * @param  string  $text
      * @return string
      */
-    protected function getNextButton($text = '&raquo;')
+    public function getNextButton($text = '&raquo;')
     {
         // If the current page is greater than or equal to the last page, it means we
         // can't go any further into the pages, as we're already on this last page
