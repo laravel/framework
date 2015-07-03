1112e7ae242ee541a95d28592c9ddb607ee8d5a3
Taylor Otwell <taylorotwell@gmail.com>
2015-07-02 21:31:36 -0500

    Organize some trait code... allow for post authentication hook.

--- src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
+++ src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
@@ -36,22 +36,24 @@ trait AuthenticatesUsers
             $this->loginUsername() => 'required', 'password' => 'required',
         ]);
 
-        $throttles = in_array(
-            ThrottlesLogins::class, class_uses_recursive(get_class($this))
-        );
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        $throttles = $this->isUsingThrottlesLoginsTrait();
 
         if ($throttles && $this->hasTooManyLoginAttempts($request)) {
             return $this->sendLockoutResponse($request);
         }
 
-        if (Auth::attempt($this->getCredentials($request), $request->has('remember'))) {
-            if ($throttles) {
-                $this->clearLoginAttempts($request);
-            }
+        $credentials = $this->getCredentials($request);
 
-            return redirect()->intended($this->redirectPath());
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            return $this->handleUserWasAuthenticated($request, $throttles);
         }
 
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
         if ($throttles) {
             $this->incrementLoginAttempts($request);
         }
@@ -61,6 +63,26 @@ trait AuthenticatesUsers
             ->withErrors([
                 $this->loginUsername() => $this->getFailedLoginMessage(),
             ]);
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  bool  $throttles
+     * @return \Illuminate\Http\Response
+     */
+    protected function handleUserWasAuthenticated(Request $request, $throttles)
+    {
+        if ($throttles) {
+            $this->clearLoginAttempts($request);
+        }
+
+        if (method_exists($this, 'authenticated')) {
+            return $this->authenticated($request, Auth::user());
+        }
+
+        return redirect()->intended($this->redirectPath());
     }
 
     /**
@@ -114,5 +136,17 @@ trait AuthenticatesUsers
     public function loginUsername()
     {
         return property_exists($this, 'username') ? $this->username : 'email';
+    }
+
+    /**
+     * Determine if the class is using the ThrottlesLogins trait.
+     *
+     * @return bool
+     */
+    protected function isUsingThrottlesLoginsTrait()
+    {
+        return in_array(
+            ThrottlesLogins::class, class_uses_recursive(get_class($this))
+        );
     }
 }
