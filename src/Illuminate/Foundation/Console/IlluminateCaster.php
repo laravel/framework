ae0e42fb7ff008e48abd4baa7427bba3232ab9fe
Graham Campbell <graham@alt-three.com>
2015-07-06 16:26:14 +0100

    Cleanup the caster

--- src/Illuminate/Foundation/Console/IlluminateCaster.php
+++ src/Illuminate/Foundation/Console/IlluminateCaster.php
@@ -6,7 +6,6 @@ use Exception;
 use Illuminate\Support\Collection;
 use Illuminate\Foundation\Application;
 use Illuminate\Database\Eloquent\Model;
-use Symfony\Component\VarDumper\Cloner\Stub;
 use Symfony\Component\VarDumper\Caster\Caster;
 
 class IlluminateCaster
@@ -37,41 +36,33 @@ class IlluminateCaster
      * Get an array representing the properties of an application.
      *
      * @param  \Illuminate\Foundation\Application  $app
-     * @param  array  $a
-     * @param  Stub  $stub
-     * @param  bool  $isNested
-     * @param  int  $filter
      * @return array
      */
-    public static function castApplication(Application $app, array $a, Stub $stub, $isNested, $filter = 0)
+    public static function castApplication(Application $app)
     {
-        $a = [];
+        $result = [];
 
         foreach (self::$appProperties as $property) {
             try {
                 $val = $app->$property();
                 if (!is_null($val)) {
-                    $a[Caster::PREFIX_VIRTUAL . $property] = $val;
+                    $result[Caster::PREFIX_VIRTUAL . $property] = $val;
                 }
             } catch (Exception $e) {
                 //
             }
         }
 
-        return $a;
+        return $result;
     }
 
     /**
      * Get an array representing the properties of a collection.
      *
      * @param  \Illuminate\Support\Collection  $value
-     * @param  array  $a
-     * @param  Stub  $stub
-     * @param  bool  $isNested
-     * @param  int  $filter
      * @return array
      */
-    public static function castCollection(Collection $coll, array $a, Stub $stub, $isNested, $filter = 0)
+    public static function castCollection(Collection $collection)
     {
         return [
             Caster::PREFIX_VIRTUAL.'all' => $coll->all(),
@@ -82,23 +73,20 @@ class IlluminateCaster
      * Get an array representing the properties of a model.
      *
      * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @param  array  $a
-     * @param  Stub  $stub
-     * @param  bool  $isNested
-     * @param  int  $filter
      * @return array
      */
-    public static function castModel(Model $model, array $a, Stub $stub, $isNested, $filter = 0)
+    public static function castModel(Model $model)
     {
         $attributes = array_merge($model->getAttributes(), $model->getRelations());
         $visible = array_flip($model->getVisible() ?: array_diff(array_keys($attributes), $model->getHidden()));
         $attributes = array_intersect_key($attributes, $visible);
 
-        $a = [];
+        $result = [];
+
         foreach ($attributes as $key => $value) {
-            $a[(isset($visible[$key]) ? Caster::PREFIX_VIRTUAL : Caster::PREFIX_PROTECTED).$key] = $value;
+            $result[(isset($visible[$key]) ? Caster::PREFIX_VIRTUAL : Caster::PREFIX_PROTECTED).$key] = $value;
         }
 
-        return $a;
+        return $result;
     }
 }
