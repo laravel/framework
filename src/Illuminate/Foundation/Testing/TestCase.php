b5a2161f5c01d24b64d659e33fe92e3db6010cc3
Taylor Otwell <taylorotwell@gmail.com>
2015-07-10 18:52:59 -0500

    Remove old code that was never documented or used.

--- src/Illuminate/Foundation/Testing/TestCase.php
+++ src/Illuminate/Foundation/Testing/TestCase.php
@@ -10,13 +10,6 @@ abstract class TestCase extends PHPUnit_Framework_TestCase
     use ApplicationTrait, AssertionsTrait, CrawlerTrait;
 
     /**
-     * The Eloquent factory instance.
-     *
-     * @var \Illuminate\Database\Eloquent\Factory
-     */
-    protected $factory;
-
-    /**
      * The callbacks that should be run before the application is destroyed.
      *
      * @var array
@@ -41,10 +34,6 @@ abstract class TestCase extends PHPUnit_Framework_TestCase
     {
         if (!$this->app) {
             $this->refreshApplication();
-        }
-
-        if (!$this->factory) {
-            $this->factory = $this->app->make('Illuminate\Database\Eloquent\Factory');
         }
     }
 
