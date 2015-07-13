9ccc23f6a26eb3bb3c662aeef3c9335ce4790422
Denis Pshenov <morepleasure@gmail.com>
2015-07-13 19:54:01 +0700

    Fix docblock @return type in AuthManager's callCustomCreator

--- src/Illuminate/Auth/AuthManager.php
+++ src/Illuminate/Auth/AuthManager.php
@@ -39,7 +39,7 @@ class AuthManager extends Manager
      * Call a custom driver creator.
      *
      * @param  string  $driver
-     * @return \Illuminate\Auth\Guard
+     * @return \Illuminate\Contracts\Auth\Guard
      */
     protected function callCustomCreator($driver)
     {
