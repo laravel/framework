6cc6545ee9fffb177ff59f010415878b5c3ed993
Graham Campbell <graham@alt-three.com>
2015-07-17 15:40:14 +0100

    Deal with throwables better

--- src/Illuminate/Contracts/Debug/ExceptionHandler.php
+++ src/Illuminate/Contracts/Debug/ExceptionHandler.php
@@ -9,26 +9,26 @@ interface ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Throwable  $e
+     * @param  \Exception  $e
      * @return void
      */
-    public function report($e);
+    public function report(Exception $e);
 
     /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $e
+     * @param  \Exception  $e
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function render($request, $e);
+    public function render($request, Exception $e);
 
     /**
      * Render an exception to the console.
      *
      * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @param  \Throwable  $e
+     * @param  \Exception  $e
      * @return void
      */
-    public function renderForConsole($output, $e);
+    public function renderForConsole($output, Exception $e);
 }
