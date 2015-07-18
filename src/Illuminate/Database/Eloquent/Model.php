d247cba338bbdcda072cb995f882935c3c221c72
Dean Taub <deantaub@gmail.com>
2015-07-18 15:07:53 -0400

    Fix typo in with() docblock.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -704,7 +704,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
     }
 
     /**
-     * Being querying a model with eager loading.
+     * Begin querying a model with eager loading.
      *
      * @param  array|string  $relations
      * @return \Illuminate\Database\Eloquent\Builder|static
