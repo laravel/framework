f93e6488df9e6018ee29a0787d13f26161ec5756
Michael <michael@votemike.co.uk>
2015-07-30 11:05:58 +0100

    Fixing docblock arguments

--- src/Illuminate/Queue/Console/FailedTableCommand.php
+++ src/Illuminate/Queue/Console/FailedTableCommand.php
@@ -38,6 +38,7 @@ class FailedTableCommand extends Command
      * Create a new failed queue jobs table command instance.
      *
      * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @param  \Illuminate\Foundation\Composer    $composer
      * @return void
      */
     public function __construct(Filesystem $files, Composer $composer)
