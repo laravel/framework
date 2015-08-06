3fe5a1c4782aff49f773f9ddc824f19115039d70
Taylor Otwell <taylorotwell@gmail.com>
2015-08-06 08:05:18 -0500

    rename method.

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -90,7 +90,7 @@ class Handler implements ExceptionHandlerContract
         if ($this->isHttpException($e)) {
             return $this->toIlluminateResponse($this->renderHttpException($e), $e);
         } else {
-            return $this->toIlluminateResponse($this->getBasicHttpExceptionDisplayer($e), $e);
+            return $this->toIlluminateResponse($this->convertExceptionToResponse($e), $e);
         }
     }
 
@@ -135,17 +135,17 @@ class Handler implements ExceptionHandlerContract
         if (view()->exists("errors.{$status}")) {
             return response()->view("errors.{$status}", ['exception' => $e], $status);
         } else {
-            return $this->getBasicHttpExceptionDisplayer($e);
+            return $this->convertExceptionToResponse($e);
         }
     }
 
     /**
-     * Get generic HTTP exception displayer/view.
+     * Convert the given exception into a Response instance.
      *
      * @param  \Exception  $e
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    protected function getBasicHttpExceptionDisplayer(Exception $e)
+    protected function convertExceptionToResponse(Exception $e)
     {
         return (new SymfonyDisplayer(config('app.debug')))->createResponse($e);
     }
