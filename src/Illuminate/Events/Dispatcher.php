07aaf10c8bfa819b7d0b0ce97a00b2146a049eeb
Taylor Otwell <taylorotwell@gmail.com>
2015-08-08 08:06:30 -0500

    spacing

--- src/Illuminate/Events/Dispatcher.php
+++ src/Illuminate/Events/Dispatcher.php
@@ -254,6 +254,7 @@ class Dispatcher implements DispatcherContract
     {
         if ($this->queueResolver) {
             $connection = $event instanceof ShouldBroadcastNow ? 'sync' : null;
+
             $queue = method_exists($event, 'onQueue') ? $event->onQueue() : null;
 
             $this->resolveQueue()->connection($connection)->pushOn($queue, 'Illuminate\Broadcasting\BroadcastEvent', [
