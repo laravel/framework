b72fd02b8f009f04dcf279fac2ecb3c4a07c5079
Lucas Michot <lucas@semalead.com>
2015-08-28 18:30:51 +0200

    Replace array_except with Arr::except

--- src/Illuminate/Auth/Passwords/PasswordBroker.php
+++ src/Illuminate/Auth/Passwords/PasswordBroker.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Auth\Passwords;
 
 use Closure;
+use Illuminate\Support\Arr;
 use UnexpectedValueException;
 use Illuminate\Contracts\Auth\UserProvider;
 use Illuminate\Contracts\Mail\Mailer as MailerContract;
@@ -229,7 +230,7 @@ class PasswordBroker implements PasswordBrokerContract
      */
     public function getUser(array $credentials)
     {
-        $credentials = array_except($credentials, ['token']);
+        $credentials = Arr::except($credentials, ['token']);
 
         $user = $this->users->retrieveByCredentials($credentials);
 
