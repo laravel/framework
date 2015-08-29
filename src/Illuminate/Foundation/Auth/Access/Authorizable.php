9ab3da688d54100f9758e91840a679f5111625d5
Taylor Otwell <taylorotwell@gmail.com>
2015-08-29 16:48:05 -0500

    Rename HasAbilities to Authorizable.

--- src/Illuminate/Foundation/Auth/Access/Authorizable.php
+++ src/Illuminate/Foundation/Auth/Access/Authorizable.php
@@ -4,7 +4,7 @@ namespace Illuminate\Foundation\Auth\Access;
 
 use Illuminate\Contracts\Auth\Access\Gate;
 
-trait HasAbilities
+trait Authorizable
 {
     /**
      * Determine if the entity has a given ability.
@@ -28,5 +28,17 @@ trait HasAbilities
     public function cant($ability, $arguments = [])
     {
         return ! $this->can($ability, $arguments);
+    }
+
+    /**
+     * Determine if the entity does not have a given ability.
+     *
+     * @param  string  $ability
+     * @param  array|mixed  $arguments
+     * @return bool
+     */
+    public function cannot($ability, $arguments = [])
+    {
+        return $this->cant($ability, $arguments);
     }
 }
