3624a71cafd904fe77d2d02435df932393738eaa
Taylor Otwell <taylorotwell@gmail.com>
2015-08-29 16:49:50 -0500

    Add an authorizable contract.

---
+++ src/Illuminate/Contracts/Auth/Access/Authorizable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Illuminate\Contracts\Auth\Access;
+
+interface Authorizable
+{
+    /**
+     * Determine if the entity has a given ability.
+     *
+     * @param  string  $ability
+     * @param  array|mixed  $arguments
+     * @return bool
+     */
+    public function can($ability, $arguments = []);
+}
