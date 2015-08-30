414faa8e2552e92e3db74385310c8305ff0d73e0
Taylor Otwell <taylorotwell@gmail.com>
2015-08-29 20:26:58 -0500

    Write a base service provider for registering policies.

---
+++ src/Illuminate/Foundation/Support/Providers/AuthServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Illuminate\Foundation\Support\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [];
+
+    /**
+     * Register the application's policies.
+     *
+     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+     * @return void
+     */
+    public function registerPolicies(GateContract $gate)
+    {
+        foreach ($this->policies as $key => $value) {
+            $gate->policy($key, $value);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        //
+    }
+}
