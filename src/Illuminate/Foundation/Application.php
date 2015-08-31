635ca9c9768c3e985865b56012316d09355cf4a6
Patryk Knysak <knysak.patryk@gmail.com>
2015-08-31 19:15:57 +0200

    added function to determine if application is in production environment

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -462,6 +462,16 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
     }
 
     /**
+     * Determine if application is in production environment.
+     *
+     * @return bool
+     */
+    public function isProduction()
+    {
+        return $this['env'] == 'prod' || $this['env'] == 'production';
+    }
+
+    /**
      * Detect the application's current environment.
      *
      * @param  \Closure  $callback
