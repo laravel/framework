772f928f7e99476dc5169652aca35d6b13ffdff1
Franz Liedke <franz@develophp.org>
2015-09-03 09:38:00 +0200

    Add two methods to the ConnectionInterface

--- src/Illuminate/Database/ConnectionInterface.php
+++ src/Illuminate/Database/ConnectionInterface.php
@@ -146,4 +146,18 @@ interface ConnectionInterface
      * @return array
      */
     public function pretend(Closure $callback);
+    
+    /**
+     * Get the current PDO connection.
+     *
+     * @return \PDO
+     */
+    public function getPdo();
+    
+    /**
+     * Get the table prefix for the connection.
+     *
+     * @return string
+     */
+    public function getTablePrefix();
 }
