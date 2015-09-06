79b6bfc4f5bc7ab40649c7df4de30a78771b717b
alnutile <alfrednutile@gmail.com>
2015-09-06 09:13:55 -0400

    Adding files to make php artisan make:test FooTest work
    Fix the failing StyleCI.io line
    
    Fix missing line and merge issue
    
    Fix the failing StyleCI.io line
    
    One more fix
    
    Removing namespace line from stub files
    
    One more fix on the new line
    
    Final updates
    
    Yet another try
    
    Stuck in a timeloop
    
    Stuck in a timeloop
    
    Stuck in a timeloop...
    
    Still
    
    Remove slashed
    
    Remove .idea and remove back slash
    
    Update stubs to match the existings ones more
    
    Added extra break

---
+++ src/Illuminate/Foundation/Console/TestMakeCommand.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Illuminate\Foundation\Console;
+
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+class TestMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Test class for example FooTest';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Test';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        if ($this->option('database_migration')) {
+            return __DIR__.'/stubs/test.database_migration.stub';
+        }
+
+        return __DIR__.'/stubs/test.stub';
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        $name = str_replace($this->laravel->getNamespace(), '', $name);
+
+        return $this->laravel['path.base'].'/tests/'.str_replace('\\', '/', $name).'.php';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace;
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['database_migration', 'd', InputOption::VALUE_NONE, 'Add DatabaseMigration Trait to your Test'],
+        ];
+    }
+}
