02e06712bd0d93076264acbff641830c9c3f4feb
jwdeitch <jwdeitch@users.noreply.github.com>
2015-09-10 07:12:35 -0400

    Update Model.php

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -680,7 +680,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
             return $model;
         }
 
-        $model->{$model->getKeyName()} = $id
+        $model->{$model->getKeyName()} = $id;
         
         return $model;
     }
