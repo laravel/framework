3f3ce7ba6937005406fe9b4da0d67ed51dc91d48
crynobone <crynobone@gmail.com>
2015-09-11 11:30:10 +0800

    Add Illuminate\Foundation\Testing\HttpException.
    This would help testing failing routes, at the moment we have to use the following structure:
    
    ```php
    /**
     * You shall not pass!
     *
     * @expectedException PHPUnit_Framework_ExpectationFailedException
     * @expectedMessage Received status code [403]
     */
    public function testUnauthorizedRoute()
    {
        $this->visit('/test');
    }
    ```
    
    With this changes, we can make it more readable"-ish" it with.
    
    ```php
    /**
     * You shall not pass!
     *
     * @expectedException Illuminate\Foundation\Testing\HttpException
     * @expectedMessage Received status code [403]
     */
    public function testUnauthorizedRoute()
    {
        $this->visit('/test');
    }
    ```

---
+++ src/Illuminate/Foundation/Testing/HttpException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Illuminate\Foundation\Testing;
+
+use PHPUnit_Framework_ExpectationFailedException;
+
+class HttpException extends PHPUnit_Framework_ExpectationFailedException
+{
+    //
+}
