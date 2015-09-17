393a07e3157ec22adb5a7d4c489e2d28bdf78dff
Taylor Otwell <taylorotwell@gmail.com>
2015-09-17 15:28:11 -0500

    add files

---
+++ src/Illuminate/Database/DetectsLostConnections.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Illuminate\Database;
+
+use Exception;
+use Illuminate\Support\Str;
+
+trait DetectsLostConnections
+{
+    /**
+     * Determine if the given exception was caused by a lost connection.
+     *
+     * @param  \Exception  $e
+     * @return bool
+     */
+    protected function causedByLostConnection(Exception $e)
+    {
+        $message = $e->getMessage();
+
+        return Str::contains($message, [
+            'server has gone away',
+            'no connection to the server',
+            'Lost connection',
+            'is dead or not enabled',
+        ]);
+    }
+}
