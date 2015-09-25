ebbae988f5697b0a456f27c0c865515b4c0ab51a
Lucas Michot <lucas@semalead.com>
2015-09-25 16:08:19 +0200

    Use proper assertion

--- tests/Cache/CacheArrayStoreTest.php
+++ tests/Cache/CacheArrayStoreTest.php
@@ -55,6 +55,6 @@ class CacheArrayStoreTest extends PHPUnit_Framework_TestCase
     public function testCacheKey()
     {
         $store = new ArrayStore;
-        $this->assertEquals('', $store->getPrefix());
+        $this->assertEmpty($store->getPrefix());
     }
 }
