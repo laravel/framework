34e5e2082a901e1227db5ef695a74745fd650d7c
Taylor Otwell <taylorotwell@gmail.com>
2015-09-26 12:47:34 -0500

    a few code formatting updates

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -685,6 +685,20 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
     }
 
     /**
+     * Refresh the current model with the current attributes from the database.
+     *
+     * @return void
+     */
+    protected function refresh()
+    {
+        $fresh = $this->fresh();
+
+        $this->setRawAttributes($fresh->getAttributes());
+
+        $this->setRelations($fresh->getRelations());
+    }
+
+    /**
      * Reload a fresh model instance from the database.
      *
      * @param  array  $with
@@ -1623,23 +1637,12 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
 
         // Newly created models might be missing optional fields that have defaults set
         // by the database. To ensure that those attributes return the correct value
-        // and not null, we refresh the model with the updates from the database.
+        // and not null, we refresh the model with the updates from this database.
         $this->refresh();
 
         $this->fireModelEvent('created', false);
 
         return true;
-    }
-
-    /**
-     * Refresh the current model with the current attributes from the database.
-     * @return void
-     */
-    protected function refresh()
-    {
-        $fresh = $this->fresh();
-        $this->setRawAttributes($fresh->getAttributes());
-        $this->setRelations($fresh->getRelations());
     }
 
     /**
