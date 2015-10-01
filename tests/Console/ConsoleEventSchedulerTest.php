4dc224cc444f68ea9144f20cad4ceda25fcc835d
David Rodrigues <david.proweb@gmail.com>
2015-10-01 08:53:40 -0300

    Code style: using variable interpolation where possible;

--- tests/Console/ConsoleEventSchedulerTest.php
+++ tests/Console/ConsoleEventSchedulerTest.php
@@ -27,9 +27,9 @@ class ConsoleEventSchedulerTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('path/to/command', $events[0]->command);
         $this->assertEquals('path/to/command -f --foo="bar"', $events[1]->command);
         $this->assertEquals('path/to/command -f', $events[2]->command);
-        $this->assertEquals('path/to/command --foo='.$escape.'bar'.$escape, $events[3]->command);
-        $this->assertEquals('path/to/command -f --foo='.$escape.'bar'.$escape, $events[4]->command);
-        $this->assertEquals('path/to/command --title='.$escape.'A '.$escapeReal.'real'.$escapeReal.' test'.$escape, $events[5]->command);
+        $this->assertEquals("path/to/command --foo={$escape}bar{$escape}", $events[3]->command);
+        $this->assertEquals("path/to/command -f --foo={$escape}bar{$escape}", $events[4]->command);
+        $this->assertEquals("path/to/command --title={$escape}A {$escapeReal}real{$escapeReal} test{$escape}", $events[5]->command);
     }
 
     public function testCommandCreatesNewArtisanCommand()
