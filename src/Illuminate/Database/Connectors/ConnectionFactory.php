66cb0ce3359745672b25343f01b64b7de66bfc56
premsaurav <premsaurav@gmail.com>
2015-10-14 02:22:53 +0530

    handling multiple database slave

--- src/Illuminate/Database/Connectors/ConnectionFactory.php
+++ src/Illuminate/Database/Connectors/ConnectionFactory.php
@@ -100,7 +100,7 @@ class ConnectionFactory
         
         // Check if we have multiple slave hosts
         // in which case select one at randon
-        if(isset($readConfig['host']) && is_array($readConfig['host'])){
+        if(isset($readConfig['host']) && is_array($readConfig['host'])) {
             $readConfig['host'] = sizeof($readConfig['host']) > 1 ?
                 $readConfig['host'][array_rand($readConfig['host'])] : $readConfig['host'][0];
         }
