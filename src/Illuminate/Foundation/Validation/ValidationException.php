45fc321523e5f31fbe96edc8d4fd8170386e20a3
Taylor Otwell <taylorotwell@gmail.com>
2015-10-14 08:30:56 -0500

    Throw validation exception from validates request trait.
    Let the exception by handled in the exception handler like the
    authorization exceptions are. Easier testing if you let exceptions
    bubble out.

---
+++ src/Illuminate/Foundation/Validation/ValidationException.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Illuminate\Foundation\Validation;
+
+use Exception;
+
+class ValidationException extends Exception
+{
+	/**
+	 * The validator instance.
+	 *
+	 * @var \Illuminate\Validation\Validator
+	 */
+	public $validator;
+
+	/**
+	 * The recommended response to send to the client.
+	 *
+	 * @var \Illuminate\Http\Response|null
+	 */
+	public $response;
+
+	/**
+	 * Create a new exception instance.
+	 *
+	 * @param  \Illuminate\Validation\Validator  $validator
+	 * @param  \Illuminate\Http\Response  $response
+	 * @return void
+	 */
+	public function __construct($validator, $response = null)
+	{
+		parent::__construct("The given data failed to pass validation.");
+
+		$this->response = $response;
+		$this->validator = $validator;
+	}
+}
