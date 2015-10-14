acfdc3e5b90a43eeede56f496356009fa87bdee8
Joseph Silber <contact@josephsilber.com>
2015-10-14 13:07:19 -0400

    Apply StyleCI fixes

--- src/Illuminate/Foundation/Validation/ValidationException.php
+++ src/Illuminate/Foundation/Validation/ValidationException.php
@@ -6,32 +6,32 @@ use Exception;
 
 class ValidationException extends Exception
 {
-	/**
-	 * The validator instance.
-	 *
-	 * @var \Illuminate\Validation\Validator
-	 */
-	public $validator;
+    /**
+     * The validator instance.
+     *
+     * @var \Illuminate\Validation\Validator
+     */
+    public $validator;
 
-	/**
-	 * The recommended response to send to the client.
-	 *
-	 * @var \Illuminate\Http\Response|null
-	 */
-	public $response;
+    /**
+     * The recommended response to send to the client.
+     *
+     * @var \Illuminate\Http\Response|null
+     */
+    public $response;
 
-	/**
-	 * Create a new exception instance.
-	 *
-	 * @param  \Illuminate\Validation\Validator  $validator
-	 * @param  \Illuminate\Http\Response  $response
-	 * @return void
-	 */
-	public function __construct($validator, $response = null)
-	{
-		parent::__construct("The given data failed to pass validation.");
+    /**
+     * Create a new exception instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @param  \Illuminate\Http\Response  $response
+     * @return void
+     */
+    public function __construct($validator, $response = null)
+    {
+        parent::__construct('The given data failed to pass validation.');
 
-		$this->response = $response;
-		$this->validator = $validator;
-	}
+        $this->response = $response;
+        $this->validator = $validator;
+    }
 }
