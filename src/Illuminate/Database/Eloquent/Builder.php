7b6a9f61ab2229462220c1654ef2d49c4f7d265f
Lucas Michot <lucas@semalead.com>
2015-10-15 23:51:22 +0200

    Directly call create a new \Illuminate\Support\Collection

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -5,6 +5,7 @@ namespace Illuminate\Database\Eloquent;
 use Closure;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -254,7 +255,7 @@ class Builder
             }
         }
 
-        return collect($results);
+        return new Collection($results);
     }
 
     /**
