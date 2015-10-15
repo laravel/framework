77ce10bfb056084bbb3ef699c9601b55540c5278
Taylor Otwell <taylorotwell@gmail.com>
2015-10-15 09:46:17 -0500

    code cleanup

--- src/Illuminate/Support/Facades/Facade.php
+++ src/Illuminate/Support/Facades/Facade.php
@@ -201,9 +201,11 @@ abstract class Facade
     public static function __callStatic($method, $args)
     {
         $instance = static::getFacadeRoot();
-        if(!$instance){
-            throw new \RuntimeException('Facade root not found.');
-        } 
+
+        if (! $instance) {
+            throw new RuntimeException('A facade root has not been set.');
+        }
+
         switch (count($args)) {
             case 0:
                 return $instance->$method();
