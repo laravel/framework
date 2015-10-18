132e5f93671820cf110fd5c2d63a7a5d316f4c4c
Roman Kinyakin <1@grep.su>
2015-10-18 16:20:45 +0600

    Remove unused imports

--- src/Illuminate/Foundation/Testing/InteractsWithPages.php
+++ src/Illuminate/Foundation/Testing/InteractsWithPages.php
@@ -4,7 +4,6 @@ namespace Illuminate\Foundation\Testing;
 
 use Exception;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
 use InvalidArgumentException;
 use Symfony\Component\DomCrawler\Form;
 use Symfony\Component\DomCrawler\Crawler;
