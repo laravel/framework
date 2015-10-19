91d15c46c093e0ea1d8b56e74f6e6ef848e90fe5
Taylor Otwell <taylorotwell@gmail.com>
2015-10-18 19:48:49 -0500

    Moving around some testing code.

--- src/Illuminate/Foundation/Testing/InteractsWithContainer.php
+++ src/Illuminate/Foundation/Testing/InteractsWithContainer.php
@@ -5,25 +5,6 @@ namespace Illuminate\Foundation\Testing;
 trait InteractsWithContainer
 {
     /**
-     * The Illuminate application instance.
-     *
-     * @var \Illuminate\Foundation\Application
-     */
-    protected $app;
-
-    /**
-     * Refresh the application instance.
-     *
-     * @return void
-     */
-    protected function refreshApplication()
-    {
-        putenv('APP_ENV=testing');
-
-        $this->app = $this->createApplication();
-    }
-
-    /**
      * Register an instance of an object in the container.
      *
      * @param  string  $abstract
