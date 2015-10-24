11bfa3f0e492c1122bb54d16d5b3acf24634ff9d
Jonathan Reinink <jonathan@reinink.ca>
2015-10-24 10:30:52 -0400

    Add user() helper function to get the currently authenticated user.

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -710,6 +710,18 @@ if (! function_exists('url')) {
     }
 }
 
+if (! function_exists('user')) {
+    /**
+     * Get the currently authenticated user.
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    function user()
+    {
+        return app('auth')->user();
+    }
+}
+
 if (! function_exists('view')) {
     /**
      * Get the evaluated view contents for the given view.
