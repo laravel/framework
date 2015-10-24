f54835308f53b2638060b42b956aa089c58e1552
Christoph Menke <christoph@menke.co>
2015-10-24 13:24:57 +0200

    elixir helper: optionally supply build folder
    equivalent to `elixir.config.versioning.buildFolder` configuration in gulpfile.js

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -261,20 +261,21 @@ if (! function_exists('elixir')) {
      * Get the path to a versioned Elixir file.
      *
      * @param  string  $file
+     * @param  string  $buildFolder
      * @return string
      *
      * @throws \InvalidArgumentException
      */
-    function elixir($file)
+    function elixir($file, $buildFolder = 'build')
     {
         static $manifest = null;
 
         if (is_null($manifest)) {
-            $manifest = json_decode(file_get_contents(public_path('build/rev-manifest.json')), true);
+            $manifest = json_decode(file_get_contents(public_path("{$buildFolder}/rev-manifest.json")), true);
         }
 
         if (isset($manifest[$file])) {
-            return '/build/'.$manifest[$file];
+            return "/{$buildFolder}/{$manifest[$file]}";
         }
 
         throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
