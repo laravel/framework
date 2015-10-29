b643cc5ade0043db7ca94277b140e8ec3d492e00
Taylor Otwell <taylorotwell@gmail.com>
2015-10-29 10:21:43 -0500

    make type validation more intuitive on validator.

--- tests/Validation/ValidationValidatorTest.php
+++ tests/Validation/ValidationValidatorTest.php
@@ -1762,6 +1762,34 @@ class ValidationValidatorTest extends PHPUnit_Framework_TestCase
         $this->assertFalse($v->passes());
     }
 
+
+    public function testThatPrimitiveTypesAreImplicitAndMustBeCorrectIfDataIsPresent()
+    {
+        $trans = $this->getRealTranslator();
+        $v = new Validator($trans, ['foo' => 'string'], ['foo' => 'numeric']);
+        $this->assertTrue($v->fails());
+
+
+        $trans = $this->getRealTranslator();
+        $v = new Validator($trans, [], ['foo' => 'numeric']);
+        $this->assertTrue($v->passes());
+
+
+        $trans = $this->getRealTranslator();
+        $v = new Validator($trans, ['foo' => null], ['foo' => 'numeric']);
+        $this->assertTrue($v->fails());
+
+
+        $trans = $this->getRealTranslator();
+        $v = new Validator($trans, ['foo' => ''], ['foo' => 'array']);
+        $this->assertTrue($v->fails());
+
+        $trans = $this->getRealTranslator();
+        $v = new Validator($trans, [], ['foo' => 'array']);
+        $this->assertTrue($v->passes());
+    }
+
+
     protected function getTranslator()
     {
         return m::mock('Symfony\Component\Translation\TranslatorInterface');
