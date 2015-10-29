cd8b70a16c5a84629a90f3546e5a835b2269ca26
Taylor Otwell <taylorotwell@gmail.com>
2015-10-29 16:30:50 -0500

    code cleanup

--- src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
@@ -54,10 +54,8 @@ class MySqlGrammar extends Grammar
     {
         $columns = implode(', ', $this->getColumns($blueprint));
 
-        $sql = 'create';
-        if ($blueprint->temporary === true) {
-            $sql .= ' temporary';
-        }
+        $sql = $blueprint->temporary ? 'create temporary' : 'create';
+
         $sql .= ' table '.$this->wrapTable($blueprint)." ($columns)";
 
         // Once we have the primary SQL, we can add the encoding option to the SQL for
