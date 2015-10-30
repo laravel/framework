d1d2b460297ac307475834b7bead74d20ae7d363
Taylor Otwell <taylorotwell@gmail.com>
2015-10-29 20:22:43 -0500

    another test for comfort

--- tests/Validation/ValidationValidatorTest.php
+++ tests/Validation/ValidationValidatorTest.php
@@ -1780,6 +1780,10 @@ class ValidationValidatorTest extends PHPUnit_Framework_TestCase
         $v = new Validator($trans, ['foo' => [['name' => 'first'], ['name' => 'second']]],
             ['foo' => 'Array', 'foo.*.name' => 'Required|Numeric']);
         $this->assertFalse($v->passes());
+
+        $v = new Validator($trans, ['foo' => [['name' => 'first', 'votes' => [1, 2]], ['name' => 'second', 'votes' => ['something', 2]]]],
+            ['foo' => 'Array', 'foo.*.name' => 'Required|String', 'foo.*.votes.*' => 'Required|Integer']);
+        $this->assertFalse($v->passes());
     }
 
     public function testValidateEachWithNonIndexedArray()
