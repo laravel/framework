ad06a21d546b524dbc66a4aff96ddd37f7932379
Colin Viebrock <c.viebrock@tippingcanoe.com>
2015-11-02 13:49:26 -0600

    Allow migrations to be "batched" individually

--- src/Illuminate/Database/Migrations/Migrator.php
+++ src/Illuminate/Database/Migrations/Migrator.php
@@ -65,9 +65,10 @@ class Migrator
      *
      * @param  string  $path
      * @param  bool    $pretend
+     * @param  bool    $step
      * @return void
      */
-    public function run($path, $pretend = false)
+    public function run($path, $pretend = false, $step = false)
     {
         $this->notes = [];
 
@@ -82,7 +83,7 @@ class Migrator
 
         $this->requireFiles($path, $migrations);
 
-        $this->runMigrationList($migrations, $pretend);
+        $this->runMigrationList($migrations, $pretend, $step);
     }
 
     /**
@@ -90,9 +91,10 @@ class Migrator
      *
      * @param  array  $migrations
      * @param  bool   $pretend
+     * @param  bool   $step
      * @return void
      */
-    public function runMigrationList($migrations, $pretend = false)
+    public function runMigrationList($migrations, $pretend = false, $step = false)
     {
         // First we will just make sure that there are any migrations to run. If there
         // aren't, we will just make a note of it to the developer so they're aware
@@ -110,6 +112,13 @@ class Migrator
         // that the migration was run so we don't repeat it next time we execute.
         foreach ($migrations as $file) {
             $this->runUp($file, $batch, $pretend);
+
+            // If we are stepping through the migrations, then we will increment the
+            // batch value for each individual migration that is run. That way we
+            // can run "artisan migrate:rollback" and undo them one at a time.
+            if ($step) {
+                $batch++;
+            }
         }
     }
 
