feda3d802f2984d7d913319a16e96fac7520c907
Roman Kinyakin <1@grep.su>
2015-11-05 18:56:15 +0600

    Light optimization

--- src/Illuminate/Support/Arr.php
+++ src/Illuminate/Support/Arr.php
@@ -315,18 +315,16 @@ class Arr
 
         list($value, $key) = static::explodePluckParameters($value, $key);
 
-        foreach ($array as $item) {
-            $itemValue = data_get($item, $value);
-
-            // If the key is "null", we will just append the value to the array and keep
-            // looping. Otherwise we will key the array using the value of the key we
-            // received from the developer. Then we'll return the final array form.
-            if (is_null($key)) {
-                $results[] = $itemValue;
-            } else {
-                $itemKey = data_get($item, $key);
-
-                $results[$itemKey] = $itemValue;
+        // If the key is "null", we will just append the value to the array and keep
+        // looping. Otherwise we will key the array using the value of the key we
+        // received from the developer. Then we'll return the final array form.
+        if (is_null($key)) {
+            foreach ($array as $item) {
+                $results[] = data_get($item, $value);
+            }
+        } else {
+            foreach ($array as $item) {
+                $results[data_get($item, $key)] = data_get($item, $value);
             }
         }
 
