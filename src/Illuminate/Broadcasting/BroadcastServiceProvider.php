b22fff738a6b62b1217d158882a75439dbd29f6e
Lucas Michot <lucas@semalead.com>
2015-11-06 17:07:42 +0100

    Use ::class notation in Bradcasting

--- src/Illuminate/Broadcasting/BroadcastServiceProvider.php
+++ src/Illuminate/Broadcasting/BroadcastServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Illuminate\Broadcasting;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Broadcasting\Factory as BroadcastingFactory;
+use Illuminate\Contracts\Broadcasting\Broadcaster as BroadcasterContract;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
@@ -20,16 +22,16 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('Illuminate\Broadcasting\BroadcastManager', function ($app) {
+        $this->app->singleton(BroadcastManager::class, function ($app) {
             return new BroadcastManager($app);
         });
 
-        $this->app->singleton('Illuminate\Contracts\Broadcasting\Broadcaster', function ($app) {
-            return $app->make('Illuminate\Broadcasting\BroadcastManager')->connection();
+        $this->app->singleton(BroadcasterContract::class, function ($app) {
+            return $app->make(BroadcastManager::class)->connection();
         });
 
         $this->app->alias(
-            'Illuminate\Broadcasting\BroadcastManager', 'Illuminate\Contracts\Broadcasting\Factory'
+            BroadcastManager::class, BroadcastingFactory::class
         );
     }
 
@@ -41,9 +43,9 @@ class BroadcastServiceProvider extends ServiceProvider
     public function provides()
     {
         return [
-            'Illuminate\Broadcasting\BroadcastManager',
-            'Illuminate\Contracts\Broadcasting\Factory',
-            'Illuminate\Contracts\Broadcasting\Broadcaster',
+            BroadcastManager::class,
+            BroadcastingFactory::class,
+            BroadcasterContract::class,
         ];
     }
 }
