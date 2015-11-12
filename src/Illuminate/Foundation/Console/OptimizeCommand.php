eb4280c0045dc61b6cd9d18fe7530552b8de5d3d
Graham Campbell <graham@alt-three.com>
2015-11-12 10:07:20 +0000

    Restored getTraverser method for BC

--- src/Illuminate/Foundation/Console/OptimizeCommand.php
+++ src/Illuminate/Foundation/Console/OptimizeCommand.php
@@ -111,11 +111,26 @@ class OptimizeCommand extends Command
             return (new Factory)->create(['skip' => true]);
         }
 
+        // Class Preloader 2.x
+        return new ClassPreloader(new PrettyPrinter, new Parser(new Lexer), $this->getTraverser());
+    }
+
+    /**
+     * Get the node traverser used by the command.
+     *
+     * Note that this method is only called if we're using Class Preloader 2.x.
+     *
+     * @return \ClassPreloader\Parser\NodeTraverser
+     */
+    protected function getTraverser()
+    {
         $traverser = new NodeTraverser;
+
         $traverser->addVisitor(new DirVisitor(true));
+
         $traverser->addVisitor(new FileVisitor(true));
 
-        return new ClassPreloader(new PrettyPrinter, new Parser(new Lexer), $traverser);
+        return $traverser;
     }
 
     /**
