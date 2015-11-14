0d8531df5ed32fb077a46935d8be83a60fff6566
crynobone <crynobone@gmail.com>
2015-11-14 14:51:17 +0800

    Add old class for deprecation purpose.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Illuminate/Foundation/Composer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Illuminate\Foundation;
+
+/**
+ * @deprecated since version 5.2. Use Illuminate\Foundation\Support\Composer.
+ */
+class Composer extends Support\Composer
+{
+    //
+}
