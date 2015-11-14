113ea22f45e69524b3c3e53da31cfbddc7d9b41b
Jacob Bennett <me@jakebennett.net>
2015-11-14 00:40:45 -0600

    Add new Queue console commands to allow for releasing of multiple failed jobs

---
+++ src/Illuminate/Queue/Console/RetryAllCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Illuminate\Queue\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
+
+class RetryAllCommand extends Command
+{
+    use ConfirmableTrait;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'queue:retry-all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Release all failed-jobs onto the queue.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if (! $this->confirmToProceed()) {
+            return;
+        }
+
+        $failed = $this->laravel['queue.failer']->all();
+
+        if (! empty($failed)) {
+            collect($failed)->each(function($value) {
+                $this->call('queue:retry', ['id' => $value->id]);
+            });
+        } else {
+            $this->error('No failed jobs.');
+        }
+    }
+}
