4a27f6ce92e92d4babce642a06e1d4c79b62e835
Graham Campbell <graham@alt-three.com>
2015-11-16 18:10:13 +0000

    Fixed database tests

--- tests/Database/DatabaseMySqlSchemaGrammarTest.php
+++ tests/Database/DatabaseMySqlSchemaGrammarTest.php
@@ -555,7 +555,7 @@ class DatabaseMySqlSchemaGrammarTest extends PHPUnit_Framework_TestCase
         $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
 
         $this->assertEquals(1, count($statements));
-        $this->assertEquals('alter table `users` add `foo` timestamp default 0 not null', $statements[0]);
+        $this->assertEquals('alter table `users` add `foo` timestamp not null', $statements[0]);
     }
 
     public function testAddingTimeStampWithDefault()
@@ -595,7 +595,7 @@ class DatabaseMySqlSchemaGrammarTest extends PHPUnit_Framework_TestCase
         $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
 
         $this->assertEquals(1, count($statements));
-        $this->assertEquals('alter table `users` add `created_at` timestamp default 0 not null, add `updated_at` timestamp default 0 not null', $statements[0]);
+        $this->assertEquals('alter table `users` add `created_at` timestamp not null, add `updated_at` timestamp not null', $statements[0]);
     }
 
     public function testAddingTimeStampsTz()
