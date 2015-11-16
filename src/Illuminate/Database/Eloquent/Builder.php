21780ed045e9c5ffc46048bc81b68c8ffb46ec04
Graham Campbell <graham@alt-three.com>
2015-11-16 15:56:35 +0000

    Fixed pluck and lists phpdoc

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -236,7 +236,7 @@ class Builder
      * Get an array with the values of a given column.
      *
      * @param  string  $column
-     * @param  string  $key
+     * @param  string|null  $key
      * @return \Illuminate\Support\Collection
      */
     public function lists($column, $key = null)
