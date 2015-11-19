846ed8e650ef0fe25bbf7042f0b1fe2824dcb1cc
Ryan Verner <ryan.verner@gmail.com>
2015-11-19 18:19:29 +1100

    Changes type casting to Swift's toString() method, enabling Exceptions

--- src/Illuminate/Mail/Transport/MandrillTransport.php
+++ src/Illuminate/Mail/Transport/MandrillTransport.php
@@ -59,7 +59,7 @@ class MandrillTransport implements Swift_Transport {
 		$client->post('https://mandrillapp.com/api/1.0/messages/send-raw.json', [
 			'body' => [
 				'key' => $this->key,
-				'raw_message' => (string) $message,
+				'raw_message' => $message->toString(),
 				'async' => false,
 			],
 		]);
