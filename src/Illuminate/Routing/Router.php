1cde8e0cc9107ca827d6e6f69607bc114b62a0b5
Aaron Costello <aaronjcos@gmail.com>
2015-11-20 19:46:32 +0000

    CS Fix

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -118,8 +118,10 @@ class Router implements RegistrarContract
         $this->events = $events;
         $this->routes = new RouteCollection;
         $this->container = $container ?: new Container;
-        
-        $this->bind('_missing', function($v) { return explode('/', $v); });
+
+        $this->bind('_missing', function ($v) { 
+            return explode('/', $v);
+        });
     }
 
     /**
