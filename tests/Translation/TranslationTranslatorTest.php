a98397e0ed691b21701fd172707345539a7deee0
crynobone <crynobone@gmail.com>
2015-11-28 21:59:02 +0800

    [5.1] Fix when using Lang::hasForLocale('foo'); without setting the second parameter.
    Added additional tests to ensure that locale set to current app locale when the variable is set to NULL.
    
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/Translation/TranslationTranslatorTest.php
+++ tests/Translation/TranslationTranslatorTest.php
@@ -26,6 +26,16 @@ class TranslationTranslatorTest extends PHPUnit_Framework_TestCase
         $t = $this->getMock('Illuminate\Translation\Translator', ['get'], [$this->getLoader(), 'en']);
         $t->expects($this->once())->method('get')->with($this->equalTo('foo'), $this->equalTo([]), $this->equalTo('bar'), false)->will($this->returnValue('foo'));
         $this->assertFalse($t->hasForLocale('foo', 'bar'));
+
+        $t = $this->getMock('Illuminate\Translation\Translator', ['load', 'getLine'], [$this->getLoader(), 'en']);
+        $t->expects($this->once())->method('load')->with($this->equalTo('*'), $this->equalTo('foo'), $this->equalTo('en'))->will($this->returnValue(null));
+        $t->expects($this->once())->method('getLine')->with($this->equalTo('*'), $this->equalTo('foo'), $this->equalTo('en'), null, $this->equalTo([]))->will($this->returnValue('bar'));
+        $this->assertTrue($t->hasForLocale('foo'));
+
+        $t = $this->getMock('Illuminate\Translation\Translator', ['load', 'getLine'], [$this->getLoader(), 'en']);
+        $t->expects($this->once())->method('load')->with($this->equalTo('*'), $this->equalTo('foo'), $this->equalTo('en'))->will($this->returnValue(null));
+        $t->expects($this->once())->method('getLine')->with($this->equalTo('*'), $this->equalTo('foo'), $this->equalTo('en'), null, $this->equalTo([]))->will($this->returnValue('foo'));
+        $this->assertFalse($t->hasForLocale('foo'));
     }
 
     public function testGetMethodProperlyLoadsAndRetrievesItem()
