c659d98d14be053ca384a9cf63cb3ba9a64adc87
Taylor Otwell <taylorotwell@gmail.com>
2015-11-30 11:37:44 -0600

    Clean up command bus and unify to self handling commands.

--- src/Illuminate/Contracts/Bus/Dispatcher.php
+++ src/Illuminate/Contracts/Bus/Dispatcher.php
@@ -8,32 +8,13 @@ use ArrayAccess;
 interface Dispatcher
 {
     /**
-     * Marshal a command and dispatch it to its appropriate handler.
-     *
-     * @param  mixed  $command
-     * @param  array  $array
-     * @return mixed
-     */
-    public function dispatchFromArray($command, array $array);
-
-    /**
-     * Marshal a command and dispatch it to its appropriate handler.
-     *
-     * @param  mixed  $command
-     * @param  \ArrayAccess  $source
-     * @param  array  $extras
-     * @return mixed
-     */
-    public function dispatchFrom($command, ArrayAccess $source, array $extras = []);
-
-    /**
      * Dispatch a command to its appropriate handler.
      *
      * @param  mixed  $command
      * @param  \Closure|null  $afterResolving
      * @return mixed
      */
-    public function dispatch($command, Closure $afterResolving = null);
+    public function dispatch($command);
 
     /**
      * Dispatch a command to its appropriate handler in the current process.
@@ -42,7 +23,7 @@ interface Dispatcher
      * @param  \Closure|null  $afterResolving
      * @return mixed
      */
-    public function dispatchNow($command, Closure $afterResolving = null);
+    public function dispatchNow($command);
 
     /**
      * Set the pipes commands should be piped through before dispatching.
