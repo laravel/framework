3634140182762fceb9f0656530db06d1ebcae121
Lucas Michot <lucas@semalead.com>
2015-11-30 20:28:35 +0100

    Remove a useless isset

--- src/Illuminate/Bus/Dispatcher.php
+++ src/Illuminate/Bus/Dispatcher.php
@@ -254,7 +254,7 @@ class Dispatcher implements DispatcherContract, QueueingDispatcher, HandlerResol
      */
     protected function pushCommandToQueue($queue, $command)
     {
-        if (isset($command->queue) && isset($command->delay)) {
+        if (isset($command->queue, $command->delay)) {
             return $queue->laterOn($command->queue, $command->delay, $command);
         }
 
