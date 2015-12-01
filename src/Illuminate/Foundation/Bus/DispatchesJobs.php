7b486bd7df9ddd8649a2d61fc3aec2460b4909d0
Taylor Otwell <taylorotwell@gmail.com>
2015-12-01 09:39:25 -0600

    removing bus optimizes and methods.

--- src/Illuminate/Foundation/Bus/DispatchesJobs.php
+++ src/Illuminate/Foundation/Bus/DispatchesJobs.php
@@ -17,29 +17,4 @@ trait DispatchesJobs
     {
         return app(Dispatcher::class)->dispatch($job);
     }
-
-    /**
-     * Marshal a job and dispatch it to its appropriate handler.
-     *
-     * @param  mixed  $job
-     * @param  array  $array
-     * @return mixed
-     */
-    protected function dispatchFromArray($job, array $array)
-    {
-        return app(Dispatcher::class)->dispatchFromArray($job, $array);
-    }
-
-    /**
-     * Marshal a job and dispatch it to its appropriate handler.
-     *
-     * @param  mixed  $job
-     * @param  \ArrayAccess  $source
-     * @param  array  $extras
-     * @return mixed
-     */
-    protected function dispatchFrom($job, ArrayAccess $source, $extras = [])
-    {
-        return app(Dispatcher::class)->dispatchFrom($job, $source, $extras);
-    }
 }
