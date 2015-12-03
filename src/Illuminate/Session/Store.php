2a323f7accbab522bd92c29e0adbe014e37372ec
Lucas Michot <lucas@semalead.com>
2015-12-03 15:50:39 +0100

    Call forget only once

--- src/Illuminate/Session/Store.php
+++ src/Illuminate/Session/Store.php
@@ -303,9 +303,7 @@ class Store implements SessionInterface
      */
     public function ageFlashData()
     {
-        foreach ($this->get('flash.old', []) as $old) {
-            $this->forget($old);
-        }
+        $this->forget($this->get('flash.old', []));
 
         $this->put('flash.old', $this->get('flash.new', []));
 
@@ -319,9 +317,7 @@ class Store implements SessionInterface
      */
     public function removeFlashNowData()
     {
-        foreach ($this->nowKeys as $key) {
-            $this->forget($key);
-        }
+        $this->forget($this->nowKeys);
 
         $this->nowKeys = [];
     }
@@ -546,7 +542,7 @@ class Store implements SessionInterface
     /**
      * Remove an item from the session.
      *
-     * @param  string  $key
+     * @param  string|array  $key
      * @return void
      */
     public function forget($key)
