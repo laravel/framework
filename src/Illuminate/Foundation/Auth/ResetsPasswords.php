fed25d8e70a7f0d93de871822891dc892b8493c9
Graham Campbell <graham@alt-three.com>
2015-12-04 07:21:17 -0500

    Applied fixes from StyleCI

--- src/Illuminate/Foundation/Auth/ResetsPasswords.php
+++ src/Illuminate/Foundation/Auth/ResetsPasswords.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Password;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 trait ResetsPasswords
 {
