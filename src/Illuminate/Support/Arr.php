106708ec63cdfbbfaed021d04a7a84118842c546
Antério Vieira <anteriovieira@gmail.com>
2015-12-04 13:02:44 -0200

    Fixed in the breakage of the block

--- src/Illuminate/Support/Arr.php
+++ src/Illuminate/Support/Arr.php
@@ -206,7 +206,7 @@ class Arr
                 if (isset($array[$part]) && is_array($array[$part])) {
                     $array = &$array[$part];
                 } else {
-                    break(2);
+                    continue 2;
                 }
             }
 
