121f22916379c1359c839d4cba80062cf558b9a1
Taylor Otwell <taylorotwell@gmail.com>
2015-12-08 08:38:44 -0600

    fix formatting

--- src/Illuminate/Auth/Guard.php
+++ src/Illuminate/Auth/Guard.php
@@ -571,18 +571,9 @@ class Guard implements GuardContract
     {
         $this->session->remove($this->getName());
 
-        $this->forgetRecallerCookieIfExists();
-    }
-
-    /**
-     * Enqueue recaller cookie for deletion if the cookie exists.
-     *
-     * @return void
-     */
-    protected function forgetRecallerCookieIfExists()
-    {
         if (! is_null($this->getRecaller())) {
             $recaller = $this->getRecallerName();
+
             $this->getCookieJar()->queue($this->getCookieJar()->forget($recaller));
         }
     }
