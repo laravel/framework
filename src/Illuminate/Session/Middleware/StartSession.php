afa89abf5301236b896f51ea336f1721c89640b0
Graham Campbell <graham@alt-three.com>
2015-12-08 15:10:00 +0000

    Use random_int rather than mt_rand

--- src/Illuminate/Session/Middleware/StartSession.php
+++ src/Illuminate/Session/Middleware/StartSession.php
@@ -159,7 +159,7 @@ class StartSession
      */
     protected function configHitsLottery(array $config)
     {
-        return mt_rand(1, $config['lottery'][1]) <= $config['lottery'][0];
+        return random_int(1, $config['lottery'][1]) <= $config['lottery'][0];
     }
 
     /**
