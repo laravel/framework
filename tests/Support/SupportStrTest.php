3da8e57fa6c9ab41d1cdbe96f39993451ed0be81
Graham Campbell <graham@alt-three.com>
2015-12-08 14:46:01 +0000

    Tweaked test

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -120,7 +120,7 @@ class SupportStrTest extends PHPUnit_Framework_TestCase
 
     public function testQuickRandom()
     {
-        $randomInteger = mt_rand(1, 100);
+        $randomInteger = random_int(1, 100);
         $this->assertEquals($randomInteger, strlen(Str::quickRandom($randomInteger)));
         $this->assertInternalType('string', Str::quickRandom());
         $this->assertEquals(16, strlen(Str::quickRandom()));
@@ -129,7 +129,7 @@ class SupportStrTest extends PHPUnit_Framework_TestCase
     public function testRandom()
     {
         $this->assertEquals(16, strlen(Str::random()));
-        $randomInteger = mt_rand(1, 100);
+        $randomInteger = random_int(1, 100);
         $this->assertEquals($randomInteger, strlen(Str::random($randomInteger)));
         $this->assertInternalType('string', Str::random());
     }
