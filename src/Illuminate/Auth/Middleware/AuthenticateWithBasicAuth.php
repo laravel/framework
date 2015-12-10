f7803c36ecf3df32c68d6a4b82fd2065431a3189
Lucas Michot <lucas@semalead.com>
2015-12-10 14:30:29 +0100

    Switch to \Illuminate\Contracts\Auth\StatefulGuard

--- src/Illuminate/Auth/Middleware/AuthenticateWithBasicAuth.php
+++ src/Illuminate/Auth/Middleware/AuthenticateWithBasicAuth.php
@@ -3,24 +3,24 @@
 namespace Illuminate\Auth\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\StatefulGuard;
 
 class AuthenticateWithBasicAuth
 {
     /**
      * The guard instance.
      *
-     * @var \Illuminate\Contracts\Auth\Guard
+     * @var \Illuminate\Contracts\Auth\StatefulGuard
      */
     protected $auth;
 
     /**
      * Create a new middleware instance.
      *
-     * @param  \Illuminate\Contracts\Auth\Guard  $auth
+     * @param  \Illuminate\Contracts\Auth\StatefulGuard  $auth
      * @return void
      */
-    public function __construct(Guard $auth)
+    public function __construct(StatefulGuard $auth)
     {
         $this->auth = $auth;
     }
