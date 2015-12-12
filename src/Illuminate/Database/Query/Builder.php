cbe61576566a2f96c7c338e6deabe44085c84d47
mnabialek <gitre@nabialek.org>
2015-12-12 21:34:28 +0100

    Allow to pass array into selectRaw

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -237,12 +237,15 @@ class Builder
     /**
      * Add a new "raw" select expression to the query.
      *
-     * @param  string  $expression
+     * @param  string|array $expression
      * @param  array   $bindings
      * @return \Illuminate\Database\Query\Builder|static
      */
     public function selectRaw($expression, array $bindings = [])
     {
+        if (is_array($expression)) {
+            $expression = implode(', ', $expression);
+        }
         $this->addSelect(new Expression($expression));
 
         if ($bindings) {
