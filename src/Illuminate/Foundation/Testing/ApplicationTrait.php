91dc68963c8a48e847942f2fde534116bf72c5d2
Alex House <alex@tripiq.eu>
2015-12-16 10:57:01 +0000

    Add a testing method to ensure events are not fired

--- src/Illuminate/Foundation/Testing/ApplicationTrait.php
+++ src/Illuminate/Foundation/Testing/ApplicationTrait.php
@@ -86,6 +86,44 @@ trait ApplicationTrait
     }
 
     /**
+     * Specify a list of events that should not be fired for the given operation.
+     *
+     * These events will be mocked, so that handlers will not actually be executed.
+     *
+     * @param  array|mixed  $events
+     * @return $this
+     */
+    public function dontSeeEvents($events)
+    {
+        $events = is_array($events) ? $events : func_get_args();
+        $calledEvents = [];
+
+        $mock = Mockery::spy('Illuminate\Contracts\Events\Dispatcher');
+
+        $mock->shouldReceive('fire')->andReturnUsing(function ($called) use ($events, &$calledEvents) {
+            foreach ($events as $event) {
+                if ((is_string($called) && $called === $event) ||
+                    (is_string($called) && is_subclass_of($called, $event)) ||
+                    (is_object($called) && $called instanceof $event)) {
+                    $calledEvents[] = $event;
+                }
+            }
+        });
+
+        $this->beforeApplicationDestroyed(function () use (&$calledEvents) {
+            if (count($calledEvents) > 0) {
+                throw new Exception(
+                    'The following events were fired: ['.implode(', ', $calledEvents).']'
+                );
+            }
+        });
+
+        $this->app->instance('events', $mock);
+
+        return $this;
+    }
+
+    /**
      * Mock the event dispatcher so all events are silenced.
      *
      * @return $this
