a72ae4298222ff511ea30bfaa036ed095a82a9e8
Lucas Michot <lucas@semalead.com>
2015-12-16 19:08:05 +0100

    No need to get middlewares if they are skipped

--- src/Illuminate/Routing/ControllerDispatcher.php
+++ src/Illuminate/Routing/ControllerDispatcher.php
@@ -96,17 +96,17 @@ class ControllerDispatcher
      */
     protected function callWithinStack($instance, $route, $request, $method)
     {
-        $middleware = $this->getMiddleware($instance, $method);
-
         $shouldSkipMiddleware = $this->container->bound('middleware.disable') &&
                                 $this->container->make('middleware.disable') === true;
+
+        $middleware = $shouldSkipMiddleware ? [] : $this->getMiddleware($instance, $method);
 
         // Here we will make a stack onion instance to execute this request in, which gives
         // us the ability to define middlewares on controllers. We will return the given
         // response back out so that "after" filters can be run after the middlewares.
         return (new Pipeline($this->container))
                     ->send($request)
-                    ->through($shouldSkipMiddleware ? [] : $middleware)
+                    ->through($middleware)
                     ->then(function ($request) use ($instance, $route, $method) {
                         return $this->router->prepareResponse(
                             $request, $this->call($instance, $route, $method)
