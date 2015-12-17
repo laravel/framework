laravel/framework

commit f8a8e775c79f012806c9bef4d0302b14d9e14a1f

Clean up unneeded line.

src/Illuminate/Auth/RequestGuard.php
@@ -51,9 +51,7 @@ class RequestGuard implements Guard
             return $this->user;
         }
 
-        $user = call_user_func($this->callback, $this->request);
-
-        return $this->user = $user;
+        $this->user = call_user_func($this->callback, $this->request);
     }
 
     /**

commit 172ef6a271af0776f9f587671d0da7ec18a25d95

Fix RequestGuard::user() returning void
Broken since f8a8e775c79f012806c9bef4d0302b14d9e14a1f

src/Illuminate/Auth/RequestGuard.php
@@ -51,7 +51,7 @@ class RequestGuard implements Guard
             return $this->user;
         }
 
-        $this->user = call_user_func($this->callback, $this->request);
+        return $this->user = call_user_func($this->callback, $this->request);
     }
 
     /**