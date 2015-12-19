adf2ffda4a4ec497fb95c31d211ed8dcaeaa594c
Taylor Otwell <taylorotwell@gmail.com>
2015-12-19 08:25:38 -0600

    Clean up a few things in Eloquent.

--- src/Illuminate/Database/Eloquent/ScopeInterface.php
+++ src/Illuminate/Database/Eloquent/ScopeInterface.php
@@ -2,14 +2,6 @@
 
 namespace Illuminate\Database\Eloquent;
 
-interface ScopeInterface
+interface ScopeInterface extends Scope
 {
-    /**
-     * Apply the scope to a given Eloquent query builder.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $builder
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @return void
-     */
-    public function apply(Builder $builder, Model $model);
 }
