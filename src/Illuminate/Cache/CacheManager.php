27321e16672adbc16d2a1b71927999cb744427c7
Taylor Otwell <taylorotwell@gmail.com>
2015-12-20 09:49:12 -0600

    fix wording

--- src/Illuminate/Cache/CacheManager.php
+++ src/Illuminate/Cache/CacheManager.php
@@ -101,7 +101,7 @@ class CacheManager implements FactoryContract
             if (method_exists($this, $driverMethod)) {
                 return $this->{$driverMethod}($config);
             } else {
-                throw new InvalidArgumentException("Driver [{$config['driver']}] not supported.");
+                throw new InvalidArgumentException("Driver [{$config['driver']}] is not supported.");
             }
         }
     }
