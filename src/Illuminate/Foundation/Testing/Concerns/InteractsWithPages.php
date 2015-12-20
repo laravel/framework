4fc490b9e2dc48d37240c1ab1eda8cba23f8cea6
Dwight Watson <dwightwatson@me.com>
2015-12-20 22:29:16 +1100

    Allow asterisk wildcard in see()

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithPages.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithPages.php
@@ -178,7 +178,12 @@ trait InteractsWithPages
     {
         $method = $negate ? 'assertNotRegExp' : 'assertRegExp';
 
-        $rawPattern = preg_quote($text, '/');
+        // Asterisks are translated into zero-or-more regular expression wildcards
+        // to make it convenient to check if the strings starts with the given
+        // pattern such as "library/*", making any string check convenient.
+        $pattern = str_replace('\*', '.*', $pattern).'\z';
+
+        $rawPattern = preg_quote($pattern, '/');
 
         $escapedPattern = preg_quote(e($text), '/');
 
