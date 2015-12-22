449f8342897cb2914ea93a3ac5807c4d7c7d1d76
Claudio Ludovico <ludo237@users.noreply.github.com>
2015-12-22 10:09:36 +0100

    Add abstract keyword to User class
    I think it would be better to add the `abstract` keyword to this class since it doesn't have any implementation aside the traits. Also it shouldn't be much of a difference for the traits itself

--- src/Illuminate/Foundation/Auth/User.php
+++ src/Illuminate/Foundation/Auth/User.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements
+abstract class User extends Model implements
     AuthenticatableContract,
     AuthorizableContract,
     CanResetPasswordContract
