7f14799da9cd40c2106610cfee43e3760b08c05e
Anže Časar <acasar@users.noreply.github.com>
2015-12-24 12:45:37 +0100

    Don't extend RouteMatched event

--- src/Illuminate/Routing/Events/RouteFound.php
+++ src/Illuminate/Routing/Events/RouteFound.php
@@ -2,6 +2,32 @@
 
 namespace Illuminate\Routing\Events;
 
-class RouteFound extends RouteMatched
+class RouteFound
 {
+    /**
+     * The route instance.
+     *
+     * @var \Illuminate\Routing\Route
+     */
+    public $route;
+
+    /**
+     * The request instance.
+     *
+     * @var \Illuminate\Http\Request
+     */
+    public $request;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Illuminate\Routing\Route  $route
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    public function __construct($route, $request)
+    {
+        $this->route = $route;
+        $this->request = $request;
+    }
 }
