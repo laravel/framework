d10afee0e87513492d4783dbd611b8f783b0b470
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2015-12-28 11:10:13 -0200

    SQL Server accept Table valued Function as table name

--- src/Illuminate/Database/Query/Grammars/SqlServerGrammar.php
+++ src/Illuminate/Database/Query/Grammars/SqlServerGrammar.php
@@ -321,4 +321,22 @@ class SqlServerGrammar extends Grammar
 
         return trim("update {$table}{$joins} set $columns $where");
     }
+
+    /**
+     * Wrap a table in keyword identifiers.
+     *
+     * @param  string|\Illuminate\Database\Query\Expression  $table
+     * @return string
+     */
+    public function wrapTable($table)
+    {
+        $table = parent::wrapTable($table);
+
+        // check if the table is defined as a table-valued function
+        if (preg_match('/^(.+?)(\(.*?\))]$/', $table, $matches) === 1) {
+            $table = $matches[1].']'.$matches[2];
+        }
+
+        return $table;
+    }
 }
