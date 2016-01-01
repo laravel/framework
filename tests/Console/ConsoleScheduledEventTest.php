c198b131fc7eef6b24de294447891b6c8bcf90ff
Jędrzej Kuryło <jedrzej.kurylo@gmail.com>
2016-01-01 14:40:27 +0100

    Updated unit tests for scheduled events

--- tests/Console/ConsoleScheduledEventTest.php
+++ tests/Console/ConsoleScheduledEventTest.php
@@ -35,7 +35,8 @@ class ConsoleScheduledEventTest extends PHPUnit_Framework_TestCase
         $event = new Event('php foo');
         $this->assertEquals('* * * * * *', $event->getExpression());
         $this->assertTrue($event->isDue($app));
-        $this->assertFalse($event->skip(function () { return true; })->isDue($app));
+        $this->assertTrue($event->skip(function () { return true; })->isDue($app));
+        $this->assertFalse($event->skip(function () { return true; })->filtersPass($app));
 
         $event = new Event('php foo');
         $this->assertEquals('* * * * * *', $event->getExpression());
@@ -43,7 +44,7 @@ class ConsoleScheduledEventTest extends PHPUnit_Framework_TestCase
 
         $event = new Event('php foo');
         $this->assertEquals('* * * * * *', $event->getExpression());
-        $this->assertFalse($event->when(function () { return false; })->isDue($app));
+        $this->assertFalse($event->when(function () { return false; })->filtersPass($app));
 
         $event = new Event('php foo');
         $this->assertEquals('*/5 * * * * *', $event->everyFiveMinutes()->getExpression());
