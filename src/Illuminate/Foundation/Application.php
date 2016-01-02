ce1fc065de4e58fcf243fb7b84fe3d156e1f6ac9
Taylor Otwell <taylorotwell@gmail.com>
2016-01-02 14:27:55 -0600

    allow strict mode off

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -28,7 +28,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
 	 *
 	 * @var string
 	 */
-	const VERSION = '4.2.17';
+	const VERSION = '4.2.18';
 
 	/**
 	 * Indicates if the application has "booted".
