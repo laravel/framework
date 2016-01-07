eb0f9ce14021a04e163008da15dbe5e8240ae5ab
Lucas Michot <lucas@semalead.com>
2016-01-07 22:04:30 +0100

    Maintenance mode.

--- src/Illuminate/Foundation/Console/DownCommand.php
+++ src/Illuminate/Foundation/Console/DownCommand.php
@@ -7,11 +7,12 @@ use Illuminate\Console\Command;
 class DownCommand extends Command
 {
     /**
-     * The console command name.
+     * The console command signature.
      *
      * @var string
      */
-    protected $name = 'down';
+    protected $signature = 'down {--message? : The message for the maintenance mode. }
+            {--retry? : The number of seconds after which the request may be retried.}';
 
     /**
      * The console command description.
@@ -27,7 +28,15 @@ class DownCommand extends Command
      */
     public function fire()
     {
-        touch($this->laravel->storagePath().'/framework/down');
+        $data = [
+            'time' => time(),
+            'message' => $this->argument('message'),
+        ];
+
+        $retry = $this->argument('retry');
+        $data['retry'] = is_numeric($retry) && $retry > 0 ? (int) $retry : null;
+
+        file_put_contents($this->laravel->storagePath().'/framework/down', json_encode($data, JSON_PRETTY_PRINT));
 
         $this->comment('Application is now in maintenance mode.');
     }
