934d8b60cb107f3acd8c42a56b1d578c450a01a8
Marco <lee_ngai_man@hotmail.com>
2016-01-07 22:01:46 +0800

    enhance dirty check in eloquent model

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -3177,19 +3177,20 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      */
     public function isDirty($attributes = null)
     {
-        $dirty = $this->getDirty();
-
         if (is_null($attributes)) {
-            return count($dirty) > 0;
-        }
-
-        if (! is_array($attributes)) {
+            $attributes = $this->attributes;
+        } elseif (! is_array($attributes)) {
             $attributes = func_get_args();
         }
 
-        foreach ($attributes as $attribute) {
-            if (array_key_exists($attribute, $dirty)) {
-                return true;
+        foreach ($attributes as $key) {
+            if (isset($this->attributes[$key])) {
+                if (! array_key_exists($key, $this->original)) {
+                    return true;
+                } elseif ($value !== $this->original[$key] &&
+                                     ! $this->originalIsNumericallyEquivalent($key)) {
+                    return true;
+                }
             }
         }
 
