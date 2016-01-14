d8c78383aae095b277d98cac9dc16bbeb6817e2d
Donatas Šimkevičius <donatas.smk@gmail.com>
2016-01-14 17:00:14 +0200

    Update Arr.php
    Allow get from objects that implements ArrayAccess

--- src/Illuminate/Support/Arr.php
+++ src/Illuminate/Support/Arr.php
@@ -244,7 +244,7 @@ class Arr
         }
 
         foreach (explode('.', $key) as $segment) {
-            if (! is_array($array) || ! array_key_exists($segment, $array)) {
+            if ((! is_array($array) || ! array_key_exists($segment, $array)) && ! $array instanceof \ArrayAccess) {
                 return value($default);
             }
 
