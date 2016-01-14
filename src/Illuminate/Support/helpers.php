b460ca0863c795622d4ffc2f939ea13009c4a961
CupOfTea696 <cupoftea696@gmail.com>
2016-01-14 14:29:23 +0000

    Support d() dump helper

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -433,6 +433,21 @@ if (! function_exists('data_get')) {
     }
 }
 
+if (! function_exists('d')) {
+    /**
+     * Dump the passed variables.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function d()
+    {
+        array_map(function ($x) {
+            (new Dumper)->dump($x);
+        }, func_get_args());
+    }
+}
+
 if (! function_exists('dd')) {
     /**
      * Dump the passed variables and end the script.
