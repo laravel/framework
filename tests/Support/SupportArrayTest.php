bce321e482485521c5844376b0c7ade8ceb86a35
donnysim <donatas.smk@gmail.com>
2016-01-14 22:34:44 +0200

    Remove ArrayObject stub as we use the built in php ArrayObject class.

--- tests/Support/SupportArrayTest.php
+++ tests/Support/SupportArrayTest.php
@@ -345,33 +345,3 @@ class SupportArrTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(['products' => ['desk' => [], null => 'something']], $array);
     }
 }
-
-class ArrayObjectStub implements ArrayAccess
-{
-    protected $data;
-
-    public function __construct(array $data = [])
-    {
-        $this->data = $data;
-    }
-
-    public function offsetExists($offset)
-    {
-        return array_key_exists($offset, $this->data);
-    }
-
-    public function offsetGet($offset)
-    {
-        return $this->data[$offset];
-    }
-
-    public function offsetSet($offset, $value)
-    {
-        $this->data[$offset] = $value;
-    }
-
-    public function offsetUnset($offset)
-    {
-        unset($this->data[$offset]);
-    }
-}
