a2c7e4737ad698dcc70d11150c78212c5463f612
Leo Sjöberg <leo.sjoberg@gmail.com>
2016-01-18 17:31:02 +0200

    Display the relevant route's URI when a routing exception is thrown

--- src/Illuminate/Routing/RoutingException.php
+++ src/Illuminate/Routing/RoutingException.php
@@ -7,5 +7,13 @@ use RuntimeException;
 
 class RoutingException extends RuntimeException
 {
+    protected $route;
 
+    public function setRoute(Route $route)
+    {
+        $this->route = $route;
+        $this->message .= " Failed for route ".$route->uri();
+
+        return $this;
+    }
 }
