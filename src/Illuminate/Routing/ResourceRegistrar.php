87815d7fe39a03d32b0c431d733ee4af5032bcb4
Leo Sjöberg <leo.sjoberg@gmail.com>
2016-01-21 23:13:42 +0200

    Make resource route base singular for compatibility with singular implicit model binding

--- src/Illuminate/Routing/ResourceRegistrar.php
+++ src/Illuminate/Routing/ResourceRegistrar.php
@@ -53,7 +53,7 @@ class ResourceRegistrar
         // We need to extract the base resource from the resource name. Nested resources
         // are supported in the framework, but we need to know what name to use for a
         // place-holder on the route wildcards, which should be the base resources.
-        $base = $this->getResourceWildcard(last(explode('.', $name)));
+        $base = str_singular($this->getResourceWildcard(last(explode('.', $name))));
 
         $defaults = $this->resourceDefaults;
 
