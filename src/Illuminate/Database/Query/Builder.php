e6fc39f4a62f8fe59f9ce1f99e0d1b6f74fc639a
Austin Stierler <austin.stierler@gmail.com>
2016-01-22 20:47:33 -0800

    remove Builder's each() function (not used, and has invalid $item parameter)

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -3,7 +3,6 @@
 namespace Illuminate\Database\Query;
 
 use Closure;
-use RuntimeException;
 use BadMethodCallException;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
@@ -1609,30 +1608,6 @@ class Builder
         }
 
         return true;
-    }
-
-    /**
-     * Execute a callback over each item while chunking.
-     *
-     * @param  callable  $callback
-     * @param  int  $count
-     * @return bool
-     *
-     * @throws \RuntimeException
-     */
-    public function each(callable $callback, $count = 1000)
-    {
-        if (is_null($this->orders) && is_null($this->unionOrders)) {
-            throw new RuntimeException('You must specify an orderBy clause when using the "each" function.');
-        }
-
-        return $this->chunk($count, function ($results) use ($callback) {
-            foreach ($results as $key => $value) {
-                if ($callback($item, $key) === false) {
-                    return false;
-                }
-            }
-        });
     }
 
     /**
