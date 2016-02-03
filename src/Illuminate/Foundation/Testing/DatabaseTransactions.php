c1dc820249fc5a10cab43f95bbf29fd1d275ede2
Troy Pavlek <troy@tpavlek.me>
2016-02-03 09:33:25 -0700

    Modified DatabaseTransactions to work with multiple connections

--- src/Illuminate/Foundation/Testing/DatabaseTransactions.php
+++ src/Illuminate/Foundation/Testing/DatabaseTransactions.php
@@ -4,12 +4,36 @@ namespace Illuminate\Foundation\Testing;
 
 trait DatabaseTransactions
 {
+    /**
+     * The connections currently affected by the transaction. Indexed by connection name.
+     *
+     * @var array
+     */
+    protected $transacting = [];
+
+    /**
+     * Retrieve the names of the database connections that should be included in the transaction.
+     *
+     * The default value of empty string will cause only the default database connection to be included in the transaction.
+     *
+     * @return array
+     */
+    protected function connectionsToTransact()
+    {
+        return [''];
+    }
+
     public function beginDatabaseTransaction()
     {
-        $this->app->make('db')->beginTransaction();
+        foreach ($this->connectionsToTransact() as $name) {
+            $this->transacting[$name] = $this->app->make('db')->connection($name);
+            $this->transacting[$name]->beginConnection();
+        }
 
         $this->beforeApplicationDestroyed(function () {
-            $this->app->make('db')->rollBack();
+            foreach ($this->connectionsToTransact() as $name) {
+                $this->app->make('db')->connection($name)->rollBack();
+            }
         });
     }
 }
