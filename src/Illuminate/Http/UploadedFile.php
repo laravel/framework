7aa750e465816d2fd158085fe578f258600176ff
Taylor Otwell <taylorotwell@gmail.com>
2016-02-10 16:41:21 -0600

    add sane helper methods to uploaded file.

--- src/Illuminate/Http/UploadedFile.php
+++ src/Illuminate/Http/UploadedFile.php
@@ -11,6 +11,36 @@ class UploadedFile extends SymfonyUploadedFile
     use Macroable;
 
     /**
+     * Get the fully qualified path to the file.
+     *
+     * @return string
+     */
+    public function path()
+    {
+        return $this->getRealPath();
+    }
+
+    /**
+     * Get the file's extension.
+     *
+     * @return string
+     */
+    public function extension()
+    {
+        return $this->guessClientExtension();
+    }
+
+    /**
+     * Get a filename for the file that is the MD5 hash of the contents.
+     *
+     * @return string
+     */
+    public function hashName()
+    {
+        return md5_file($this->path()).'.'.$this->extension();
+    }
+
+    /**
      * Create a new file instance from a base instance.
      *
      * @param  \Symfony\Component\HttpFoundation\File\UploadedFile  $file
