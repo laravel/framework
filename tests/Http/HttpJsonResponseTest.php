8ed4b86d2b05d1ee1c417d94d6e871f774ca2b8a
Lucas Michot <lucas@semalead.com>
2016-02-10 03:18:43 +0100

    setData should support more implementations

--- tests/Http/HttpJsonResponseTest.php
+++ tests/Http/HttpJsonResponseTest.php
@@ -1,7 +1,34 @@
 <?php
 
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Contracts\Support\Arrayable;
+
 class HttpJsonResponseTest extends PHPUnit_Framework_TestCase
 {
+    public function testSeAndRetrieveJsonableData()
+    {
+        $response = new Illuminate\Http\JsonResponse(new JsonableObject);
+        $data = $response->getData();
+        $this->assertInstanceOf('StdClass', $data);
+        $this->assertEquals('bar', $data->foo);
+    }
+
+    public function testSeAndRetrieveJsonSerializeData()
+    {
+        $response = new Illuminate\Http\JsonResponse(new JsonSerializeObject);
+        $data = $response->getData();
+        $this->assertInstanceOf('StdClass', $data);
+        $this->assertEquals('bar', $data->foo);
+    }
+
+    public function testSeAndRetrieveArrayableData()
+    {
+        $response = new Illuminate\Http\JsonResponse(new ArrayableObject);
+        $data = $response->getData();
+        $this->assertInstanceOf('StdClass', $data);
+        $this->assertEquals('bar', $data->foo);
+    }
+
     public function testSetAndRetrieveData()
     {
         $response = new Illuminate\Http\JsonResponse(['foo' => 'bar']);
@@ -25,5 +52,29 @@ class HttpJsonResponseTest extends PHPUnit_Framework_TestCase
         $response = new Illuminate\Http\JsonResponse(['foo' => 'bar']);
         $response->setStatusCode(404);
         $this->assertSame(404, $response->getStatusCode());
+    }
+}
+
+class JsonableObject implements Jsonable
+{
+    public function toJson($options = 0)
+    {
+        return '{"foo":"bar"}';
+    }
+}
+
+class JsonSerializeObject implements JsonSerializable
+{
+    public function jsonSerialize()
+    {
+        return ['foo' => 'bar'];
+    }
+}
+
+class ArrayableObject implements Arrayable
+{
+    public function toArray()
+    {
+        return ['foo' => 'bar'];
     }
 }
