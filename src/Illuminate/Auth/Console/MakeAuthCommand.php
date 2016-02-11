8e2cb287dd400b0f9e85cf26f1cc99ad6c374a0d
Viktor Nyström <vnystrom@gmail.com>
2016-02-11 22:02:22 +0100

    change order of imports

--- src/Illuminate/Auth/Console/MakeAuthCommand.php
+++ src/Illuminate/Auth/Console/MakeAuthCommand.php
@@ -2,8 +2,8 @@
 
 namespace Illuminate\Auth\Console;
 
-use Illuminate\Console\AppNamespaceDetectorTrait;
 use Illuminate\Console\Command;
+use Illuminate\Console\AppNamespaceDetectorTrait;
 
 class MakeAuthCommand extends Command
 {
