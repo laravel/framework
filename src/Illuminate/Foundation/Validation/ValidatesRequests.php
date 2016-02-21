edda58f1851e66b0317b1fd54ba63c5348d65a9d
Joseph Silber <contact@josephsilber.com>
2016-02-20 22:24:37 -0500

    Allow calling validate with only the rules

--- src/Illuminate/Foundation/Validation/ValidatesRequests.php
+++ src/Illuminate/Foundation/Validation/ValidatesRequests.php
@@ -10,16 +10,22 @@ trait ValidatesRequests
      * Validate the given data with the given rules.
      *
      * @param  \Illuminate\Http\Request|array  $request
-     * @param  array  $rules
+     * @param  array|null  $rules
      * @param  array  $messages
      * @param  array  $customAttributes
      * @return void
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function validate($request, array $rules, array $messages = [], array $customAttributes = [])
+    public function validate($request, array $rules = null, array $messages = [], array $customAttributes = [])
     {
-        $data = $request instanceof Request ? $request->all() : $request;
+        if (is_null($rules)) {
+            $data = request()->all();
+
+            $rules = $request;
+        } else {
+            $data = $request instanceof Request ? $request->all() : $request;
+        }
 
         validator($data, $rules, $messages, $customAttributes)->validate();
     }
