5037f5bf0d52ea7e8c941a8a8fde2b5f5ed8e43f
James Guthrie <james@prodigi.ch>
2016-02-24 14:59:47 +0100

    Fix Mailer contract
    The mailer contract didn't reflect the fact that the transport returns
    the number of recipients accepted by the mail endpoint.

--- src/Illuminate/Contracts/Mail/Mailer.php
+++ src/Illuminate/Contracts/Mail/Mailer.php
@@ -19,7 +19,7 @@ interface Mailer
      * @param  string|array  $view
      * @param  array  $data
      * @param  \Closure|string  $callback
-     * @return void
+     * @return int
      */
     public function send($view, array $data, $callback);
 
