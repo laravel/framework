887b54ac9775762c8ead74ac2cedd74b5ae64b2a
hansvn <hans_vn@hotmail.com>
2016-02-24 10:10:35 +0100

    Add DateTimeImmutable as valid date to Model.php
    DateTimeImmutable behaves just like the DateTime object, so it should be considered as a valid date object when returning a Carbon date object.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -2934,6 +2934,12 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
         if ($value instanceof DateTime) {
             return Carbon::instance($value);
         }
+        
+        // If the value is a DateTimeImmutable instance, also skip the rest of these
+        // checks. Just return the DateTimeImmutable right away.
+        if($value instanceof DateTimeImmutable) {
+            return new Carbon($value->format('Y-m-d H:i:s.u'), $value->getTimeZone());
+        }
 
         // If this value is an integer, we will assume it is a UNIX timestamp's value
         // and format a Carbon object from this timestamp. This allows flexibility
