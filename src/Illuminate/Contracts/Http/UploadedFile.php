654c533b81f9b009fb181ec61769388025d3b61f
Hasyimi Bahrudin <hasyimibahrudin@gmail.com>
2016-02-24 15:50:13 +0800

    Add size(), mimeType(), and isValid() methods

--- src/Illuminate/Contracts/Http/UploadedFile.php
+++ src/Illuminate/Contracts/Http/UploadedFile.php
@@ -17,4 +17,25 @@ interface UploadedFile
      * @return string
      */
     public function extension();
+
+    /**
+     * Get the file size in bytes.
+     *
+     * @return int
+     */
+    public function size();
+
+    /**
+     * Get the file mime type.
+     *
+     * @return string
+     */
+    public function mimeType();
+
+    /**
+     * Returns whether the file was uploaded successfully.
+     *
+     * @return bool
+     */
+    public function isValid();
 }
