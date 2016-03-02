98dbd15ede309432cb32a8116100410af5606af5
Joseph Silber <contact@josephsilber.com>
2016-03-02 09:54:16 -0500

    Update last() usage

--- tests/Support/SupportArrTest.php
+++ tests/Support/SupportArrTest.php
@@ -80,10 +80,10 @@ class SupportArrTest extends PHPUnit_Framework_TestCase
     {
         $array = [100, 200, 300];
 
-        $last = Arr::last($array, function ($key, $value) { return $value < 250; });
+        $last = Arr::last($array, function ($value) { return $value < 250; });
         $this->assertEquals(200, $last);
 
-        $last = Arr::last($array, function ($key, $value) { return $key < 2; });
+        $last = Arr::last($array, function ($value, $key) { return $key < 2; });
         $this->assertEquals(200, $last);
 
         $this->assertEquals(300, Arr::last($array));
