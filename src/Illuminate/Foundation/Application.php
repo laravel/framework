057832fd33ba2fee42e52b5c72045c94b049db84
Taylor Otwell <taylorotwell@gmail.com>
2016-03-05 21:40:37 -0600

    order by length

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -277,11 +277,11 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
     {
         $this->instance('path', $this->path());
         $this->instance('path.base', $this->basePath());
-        $this->instance('path.config', $this->configPath());
-        $this->instance('path.database', $this->databasePath());
         $this->instance('path.lang', $this->langPath());
+        $this->instance('path.config', $this->configPath());
         $this->instance('path.public', $this->publicPath());
         $this->instance('path.storage', $this->storagePath());
+        $this->instance('path.database', $this->databasePath());
     }
 
     /**
