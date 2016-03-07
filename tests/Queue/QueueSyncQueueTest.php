0ad6a38b9b4710789e2c2764d34ed9b3a9f396f4
Kevin Simard <kev.simard@gmail.com>
2016-03-07 14:45:42 -0500

    [5.2] Add an event listener for the exception caught job event

--- tests/Queue/QueueSyncQueueTest.php
+++ tests/Queue/QueueSyncQueueTest.php
@@ -69,7 +69,7 @@ class QueueSyncQueueTest extends PHPUnit_Framework_TestCase
         $encrypter = new Illuminate\Encryption\Encrypter(str_repeat('c', 16));
         $container->instance('Illuminate\Contracts\Encryption\Encrypter', $encrypter);
         $events = m::mock('Illuminate\Contracts\Events\Dispatcher');
-        $events->shouldReceive('fire')->twice();
+        $events->shouldReceive('fire')->times(3);
         $container->instance('events', $events);
         $sync->setContainer($container);
         $sync->setEncrypter($encrypter);
