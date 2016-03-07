2e8a319cf07fb0d62f0ce04c764f59e87328b99d
Diego Felix <diegoflx.oliveira@gmail.com>
2016-03-07 16:21:49 -0300

    Added check for a valid key before update a model
    Suppose you had set a protected $primaryKey = "Id"; in your User model ( attention for the capitalized "I" ).
    
    When you do a User::find(1). The generated query will be "select * from `users` where `users`.`Id` = ? limit 1"
    
    in MySQL its a valid query. But, if you update the model: 
    
    $user->email = "changed@email.com";
    $user->save();
    
    No errors will be displayed, but the query executed will be: "update `users` set `name` = ?, `updated_at` = ? where `Id` is null"
    
    This occurs because in that specific scenario there's no check key and the method GetKeyForSaveQuery will return null.
    
    This code tells the user what happened and prevent a mistaken massive update.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1702,8 +1702,16 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
         if (isset($this->original[$this->getKeyName()])) {
             return $this->original[$this->getKeyName()];
         }
+        
+        $keyAttribute = $this->getAttribute($this->getKeyName());
+        
+        if (is_null($keyAttribute)) {
+            throw new Exception('Invalid primary key defined on model.');
+        }
 
-        return $this->getAttribute($this->getKeyName());
+        return $keyAttribute;
+
+Sorry my
     }
 
     /**
