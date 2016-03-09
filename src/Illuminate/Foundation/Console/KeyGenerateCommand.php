a53c71d3b47d87739da6142b6661e05220511deb
vlakoff <vlakoff@gmail.com>
2016-03-09 14:19:26 +0100

    Code style

--- src/Illuminate/Foundation/Console/KeyGenerateCommand.php
+++ src/Illuminate/Foundation/Console/KeyGenerateCommand.php
@@ -29,7 +29,9 @@ class KeyGenerateCommand extends Command
      */
     public function fire()
     {
-        $key = $this->getRandomKey($this->laravel['config']['app.cipher']);
+        $app = $this->laravel;
+
+        $key = $this->getRandomKey($app['config']['app.cipher']);
 
         if ($this->option('show')) {
             return $this->line('<comment>'.$key.'</comment>');
@@ -38,7 +40,7 @@ class KeyGenerateCommand extends Command
         $path = base_path('.env');
 
         if (file_exists($path)) {
-            $content = str_replace('APP_KEY='.$this->laravel['config']['app.key'], 'APP_KEY='.$key, file_get_contents($path));
+            $content = str_replace('APP_KEY='.$app['config']['app.key'], 'APP_KEY='.$key, file_get_contents($path));
 
             if (! Str::contains($content, 'APP_KEY')) {
                 $content = sprintf("%s\nAPP_KEY=%s\n", $content, $key);
@@ -47,7 +49,7 @@ class KeyGenerateCommand extends Command
             file_put_contents($path, $content);
         }
 
-        $this->laravel['config']['app.key'] = $key;
+        $app['config']['app.key'] = $key;
 
         $this->info("Application key [$key] set successfully.");
     }
