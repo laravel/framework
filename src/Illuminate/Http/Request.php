9c765a25bd5e5705a3a20072d81f0badb26b4052
Connor S. Parks <ConnorVG@users.noreply.github.com>
2016-03-17 21:13:26 +0000

    Return Changes in Illuminate\Htttp\Request
    I was wondering... Are they like this on purpose? If so, could you please share why? I'm interested.
    
    Cheers,

--- src/Illuminate/Http/Request.php
+++ src/Illuminate/Http/Request.php
@@ -879,9 +879,9 @@ class Request extends SymfonyRequest implements Arrayable, ArrayAccess
 
         if (is_null($route) || is_null($param)) {
             return $route;
-        } else {
-            return $route->parameter($param);
         }
+
+        return $route->parameter($param);
     }
 
     /**
@@ -1033,8 +1033,8 @@ class Request extends SymfonyRequest implements Arrayable, ArrayAccess
 
         if (array_key_exists($key, $all)) {
             return $all[$key];
-        } else {
-            return $this->route($key);
         }
+
+        return $this->route($key);
     }
 }
