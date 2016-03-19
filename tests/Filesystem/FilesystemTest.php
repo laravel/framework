6cfee89962d880a801587fd35114ada9b437c425
deadem <deadem@gmail.com>
2016-03-19 20:59:14 +0300

    codestyle fix

--- tests/Filesystem/FilesystemTest.php
+++ tests/Filesystem/FilesystemTest.php
@@ -294,6 +294,7 @@ class FilesystemTest extends PHPUnit_Framework_TestCase
     {
         if (defined('HHVM_VERSION')) {
             $this->markTestSkipped('Skip HHVM test due to bug: https://github.com/facebook/hhvm/issues/5657');
+
             return;
         }
 
