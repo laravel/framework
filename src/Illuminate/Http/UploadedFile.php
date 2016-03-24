33a590962c7fdb5fbc82456eb8aed06523678e4c
Taylor Otwell <taylorotwell@gmail.com>
2016-03-24 09:57:22 -0500

    use guessExtension

--- src/Illuminate/Http/UploadedFile.php
+++ src/Illuminate/Http/UploadedFile.php
@@ -26,7 +26,7 @@ class UploadedFile extends SymfonyUploadedFile
      */
     public function extension()
     {
-        return $this->guessClientExtension();
+        return $this->guessExtension();
     }
 
     /**
