85e7b3406a845c283b223d2c24e55a6db6d5704c
Taylor Otwell <taylorotwell@gmail.com>
2016-03-25 12:21:21 -0500

    add comment

--- src/Illuminate/Encryption/EncryptionServiceProvider.php
+++ src/Illuminate/Encryption/EncryptionServiceProvider.php
@@ -17,6 +17,9 @@ class EncryptionServiceProvider extends ServiceProvider
         $this->app->singleton('encrypter', function ($app) {
             $config = $app->make('config')->get('app');
 
+            // If the key starts with "base64:", we will need to decode the key before handing
+            // it off to the encrypter. Keys may be base-64 encoded for presentation and we
+            // want to make sure to convert them back to the raw bytes before encrypting.
             if (Str::startsWith($key = $config['key'], 'base64:')) {
                 $key = base64_decode(substr($key, 7));
             }
