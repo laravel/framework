e49ced14256da942fb4949ef7f6c044f5ac61921
scrubmx <scrub.mx@gmail.com>
2016-04-04 23:31:51 -0500

    [5.2] Update some docblocks
    Fixed the dockblocks on the Illuminate/Foundation/Testing namespace
    - Removed unused parameters
    - Added some missing descriptions
    - Updated some type hints

--- src/Illuminate/Foundation/Testing/DatabaseMigrations.php
+++ src/Illuminate/Foundation/Testing/DatabaseMigrations.php
@@ -4,6 +4,11 @@ namespace Illuminate\Foundation\Testing;
 
 trait DatabaseMigrations
 {
+    /**
+     * Run the database migrations for the application.
+     *
+     * @return void
+     */
     public function runDatabaseMigrations()
     {
         $this->artisan('migrate');
