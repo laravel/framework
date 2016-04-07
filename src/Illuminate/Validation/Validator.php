27a10879bd7a872fb464c72b23945f94488fa6a4
Taylor Otwell <taylorotwell@gmail.com>
2016-04-07 09:00:45 -0500

    Revert "[5.1] Validation rules won't throw an Exception when the input value is an array"

--- src/Illuminate/Validation/Validator.php
+++ src/Illuminate/Validation/Validator.php
@@ -1235,10 +1235,6 @@ class Validator implements ValidatorContract
      */
     protected function validateActiveUrl($attribute, $value)
     {
-        if (! is_string($value)) {
-            return false;
-        }
-
         if ($url = parse_url($value, PHP_URL_HOST)) {
             return count(dns_get_record($url, DNS_A | DNS_AAAA)) > 0;
         }
@@ -1383,7 +1379,7 @@ class Validator implements ValidatorContract
             return true;
         }
 
-        if (! is_string($value) || strtotime($value) === false) {
+        if (strtotime($value) === false) {
             return false;
         }
 
@@ -1404,10 +1400,6 @@ class Validator implements ValidatorContract
     {
         $this->requireParameterCount(1, $parameters, 'date_format');
 
-        if (! is_string($value)) {
-            return false;
-        }
-
         $parsed = date_parse_from_format($parameters[0], $value);
 
         return $parsed['error_count'] === 0 && $parsed['warning_count'] === 0;
@@ -1424,10 +1416,6 @@ class Validator implements ValidatorContract
     protected function validateBefore($attribute, $value, $parameters)
     {
         $this->requireParameterCount(1, $parameters, 'before');
-
-        if (! is_string($value)) {
-            return false;
-        }
 
         if ($format = $this->getDateFormat($attribute)) {
             return $this->validateBeforeWithFormat($format, $value, $parameters);
@@ -1466,10 +1454,6 @@ class Validator implements ValidatorContract
     protected function validateAfter($attribute, $value, $parameters)
     {
         $this->requireParameterCount(1, $parameters, 'after');
-
-        if (! is_string($value)) {
-            return false;
-        }
 
         if ($format = $this->getDateFormat($attribute)) {
             return $this->validateAfterWithFormat($format, $value, $parameters);
