1ba841b10045b1cb1da1e0028ffbaa784a80843b
Taylor Otwell <taylorotwell@gmail.com>
2016-04-07 23:24:31 -0500

    formatting and comments

--- src/Illuminate/Pipeline/Pipeline.php
+++ src/Illuminate/Pipeline/Pipeline.php
@@ -112,16 +112,24 @@ class Pipeline implements PipelineContract
     {
         return function ($stack, $pipe) {
             return function ($passable) use ($stack, $pipe) {
-                // If the pipe is an instance of a Closure, we will just call it directly but
-                // otherwise we'll resolve the pipes out of the container and call it with
-                // the appropriate method and arguments, returning the results back out.
                 if ($pipe instanceof Closure) {
+                    // If the pipe is an instance of a Closure, we will just call it directly but
+                    // otherwise we'll resolve the pipes out of the container and call it with
+                    // the appropriate method and arguments, returning the results back out.
                     return call_user_func($pipe, $passable, $stack);
                 } elseif (! is_object($pipe)) {
                     list($name, $parameters) = $this->parsePipeString($pipe);
+
+                    // If the pipe is a string we will parse the string and resolve the class out
+                    // of the dependency injection container. We can then build a callable and
+                    // execute the pipe function giving in the parameters that are required.
                     $pipe = $this->container->make($name);
+
                     $parameters = array_merge([$passable, $stack], $parameters);
                 } else {
+                    // If the pipe is already an object we'll just make a callable and pass it to
+                    // the pipe as-is. There is no need to do any extra parsing and formatting
+                    // since the object we're given was already a fully instantiated object.
                     $parameters = [$passable, $stack];
                 }
 
