d077eea613ae9475f67d82734802d8431532b57e
Sebastian Wasser <sebastian.wasser@gmail.com>
2016-04-14 20:02:36 +0200

    Fixed exception name

--- src/Illuminate/Database/Query/JsonExpression.php
+++ src/Illuminate/Database/Query/JsonExpression.php
@@ -40,7 +40,7 @@ class JsonExpression extends Expression
                 return '?';
         }
 
-        throw new \IllegalArgumentException('JSON value is of illegal type: '.$type);
+        throw new \InvalidArgumentException('JSON value is of illegal type: '.$type);
     }
 
     /**
