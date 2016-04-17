328e6528ce772f74603c5b86c174ffb269ab63b7
Nick Matthews <nick@grabble.co.uk>
2016-04-17 21:32:34 +0000

    Carrying over joins from nested queries so nested query scopes work correctly

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -724,6 +724,14 @@ class Builder
             $this->addBinding($query->getBindings(), 'where');
         }
 
+        if (count($query->joins)) {
+            foreach ($query->joins as $join) {
+                $this->joins[] = $join;
+
+                $this->addBinding($join->bindings, 'join');
+            }
+        }
+
         return $this;
     }
 
