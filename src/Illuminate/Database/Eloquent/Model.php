cbeaed9053561a31cc1cf8fa95e31f80bfe8e909
Jeroen Noten <jeroennoten@me.com>
2016-04-18 14:43:45 +0200

    Make sure unguarded() does not change unguarded state on exception (#13186)

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -2266,11 +2266,11 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
 
         static::unguard();
 
-        $result = $callback();
-
-        static::reguard();
-
-        return $result;
+        try {
+            return $callback();
+        } finally {
+            static::reguard();
+        }
     }
 
     /**
