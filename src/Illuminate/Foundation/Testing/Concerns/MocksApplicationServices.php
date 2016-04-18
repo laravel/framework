70c8fd1c56e836a93bf3fb3f6555b3cae1fd6853
Taylor Otwell <taylorotwell@gmail.com>
2016-04-18 07:53:55 -0500

    code method order

--- src/Illuminate/Foundation/Testing/Concerns/MocksApplicationServices.php
+++ src/Illuminate/Foundation/Testing/Concerns/MocksApplicationServices.php
@@ -94,6 +94,25 @@ trait MocksApplicationServices
     }
 
     /**
+     * Specify a list of observers that will not run for the given operation.
+     *
+     * @param  array|string $observers
+     * @return $this
+     */
+    public function withoutObservers($observers)
+    {
+        $observers = is_array($observers) ? $observers : [$observers];
+
+        array_map(function ($observer) {
+            $this->app->bind($observer, function () use ($observer) {
+                return $this->getMockBuilder($observer)->disableOriginalConstructor()->getMock();
+            });
+        }, $observers);
+
+        return $this;
+    }
+
+    /**
      * Filter the given events against the fired events.
      *
      * @param  array  $events
@@ -218,26 +237,5 @@ trait MocksApplicationServices
         }
 
         return false;
-    }
-
-    /**
-     * Specify a list of observers that will not run for the given operation.
-     *
-     * These observers will be mocked, so that they will not actually be executed.
-     *
-     * @param array|string $observers
-     * @return $this
-     */
-    public function withoutObservers($observers)
-    {
-        $observers = is_array($observers) ? $observers : [$observers];
-
-        array_map(function ($observer) {
-            $this->app->bind($observer, function () use ($observer) {
-                return $this->getMockBuilder($observer)->disableOriginalConstructor()->getMock();
-            });
-        }, $observers);
-
-        return $this;
     }
 }
