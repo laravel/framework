f778378f8dab4add3581696e53ad78e8e9e06bda
novusidea <m.skroch@mail.de>
2016-04-19 20:16:37 +0200

    inserted blank line after first line of code, becaus StyleCI analysis has failed.

--- src/Illuminate/Validation/Validator.php
+++ src/Illuminate/Validation/Validator.php
@@ -1458,6 +1458,7 @@ class Validator implements ValidatorContract
     protected function validateEmail($attribute, $value)
     {
         $valueParts = explode('@', $value);
+		
         return count($valueParts) == 2 && filter_var($valueParts[0].'@'.idn_to_ascii($valueParts[1]), FILTER_VALIDATE_EMAIL) !== false;
     }
 
