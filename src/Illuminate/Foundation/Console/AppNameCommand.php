aa2b37abc489f204addb5c84b999190d7e39fa79
Leonardo Alifraco <leonardo.alifraco@gmail.com>
2016-04-21 15:28:53 +0200

    Fix AppNameCommand when setting composer namespace (#13208)

--- src/Illuminate/Foundation/Console/AppNameCommand.php
+++ src/Illuminate/Foundation/Console/AppNameCommand.php
@@ -155,7 +155,7 @@ class AppNameCommand extends Command
     protected function setComposerNamespace()
     {
         $this->replaceIn(
-            $this->getComposerPath(), $this->currentRoot.'\\\\', str_replace('\\', '\\\\', $this->argument('name')).'\\\\'
+            $this->getComposerPath(), str_replace('\\', '\\\\', $this->currentRoot).'\\\\', str_replace('\\', '\\\\', $this->argument('name')).'\\\\'
         );
     }
 
