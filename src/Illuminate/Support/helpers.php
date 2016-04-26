4e3c66b56829e47e1d2e52e2ccf1b0fc49432ebd
Martin Bean <martinbean@users.noreply.github.com>
2016-04-26 11:11:20 +0100

    Prevent dd() calls from crashing production apps
    Stop dd() calls taking affect in production environment applications. Instead, a log message is raised with the calling stack trace frame passed as context.

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -507,6 +507,12 @@ if (! function_exists('dd')) {
      */
     function dd()
     {
+        if (app()->environment('production')) {
+            logger('dd() found', debug_backtrace()[1]);
+
+            return;
+        }
+
         array_map(function ($x) {
             (new Dumper)->dump($x);
         }, func_get_args());
