c34b112961ff3c6558634070aa4d99d5308f1f07
Thijs Van er Schaeghe <thijs@catlab.be>
2016-04-28 12:58:48 +0200

    Fixed isset bug for unloaded relationships

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -3460,8 +3460,11 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      */
     public function __isset($key)
     {
-        return (isset($this->attributes[$key]) || isset($this->relations[$key])) ||
-                ($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key)));
+        return (
+            isset($this->attributes[$key]) ||
+            isset($this->relations[$key]) ||
+            method_exists($this, $key)
+        ) || ($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key)));
     }
 
     /**
