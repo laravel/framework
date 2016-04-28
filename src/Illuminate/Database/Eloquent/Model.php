8b014483ab3ed1c0837bfa28d199ada72ccb98ec
Thijs Van er Schaeghe <thijs@catlab.be>
2016-04-28 14:26:22 +0200

    Codestyle

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -3461,7 +3461,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
     public function __isset($key)
     {
         return (isset($this->attributes[$key]) || isset($this->relations[$key]) || method_exists($this, $key)) ||
-            ($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key)));
+                ($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key)));
     }
 
     /**
