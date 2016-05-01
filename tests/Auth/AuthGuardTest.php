b950ab43dbe3b5dda067a8cdaab1f2fa9202e1af
vlakoff <vlakoff@gmail.com>
2016-05-01 12:32:16 +0200

    Test SessionGuard::onceUsingId() doesn't try to set user if not found

--- tests/Auth/AuthGuardTest.php
+++ tests/Auth/AuthGuardTest.php
@@ -263,8 +263,12 @@ class AuthGuardTest extends PHPUnit_Framework_TestCase
 
     public function testOnceUsingIdFailure()
     {
-        $guard = $this->getGuard();
+        list($session, $provider, $request, $cookie) = $this->getMocks();
+        $guard = m::mock('Illuminate\Auth\SessionGuard', ['default', $provider, $session])->makePartial();
+
         $guard->getProvider()->shouldReceive('retrieveById')->once()->with(11)->andReturn(null);
+        $guard->shouldNotReceive('setUser');
+
         $this->assertFalse($guard->onceUsingId(11));
     }
 
