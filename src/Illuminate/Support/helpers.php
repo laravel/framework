30c071fa8d6cf4dfbcc3dd7ebf3037141293a7e8
Amrani Houssain <amranidev@users.noreply.github.com>
2016-05-16 21:33:33 +0100

    Create clear_routes helper

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -891,3 +891,28 @@ if (! function_exists('with')) {
         return $object;
     }
 }
+
+if (!function_exists("clear_routes")) {
+    /**
+     * Clear routes file
+     *
+     * @param String $remove
+     * @return mixed
+     */
+    function clear_routes($remove)
+    {
+        $path = app_path() . '/Http/routes.php';
+
+        $lines = file($path, FILE_IGNORE_NEW_LINES);
+
+        foreach ($lines as $key => $line) {
+            if (strstr($line, $remove)) {
+                unset($lines[$key]);
+            }
+        }
+
+        $data = implode("\n", array_values($lines));
+
+        return file_put_contents($path, $data);
+    }
+}
