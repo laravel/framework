505cc31c3001097d8524b6ab4ccc7daaf1e3c063
Amrani Houssain <amranidev@users.noreply.github.com>
2016-05-16 21:38:12 +0100

    function ordered by name

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -365,6 +365,27 @@ if (! function_exists('class_uses_recursive')) {
     }
 }
 
+if (!function_exists("clear_routes")) {
+    /**
+     * Clear routes file
+     *
+     * @param String $remove
+     * @return mixed
+     */
+    function clear_routes($remove)
+    {
+        $path = app_path() . '/Http/routes.php';
+        $lines = file($path, FILE_IGNORE_NEW_LINES);
+        foreach ($lines as $key => $line) {
+            if (strstr($line, $remove)) {
+                unset($lines[$key]);
+            }
+        }
+        $data = implode("\n", array_values($lines));
+        return file_put_contents($path, $data);
+    }
+}
+
 if (! function_exists('collect')) {
     /**
      * Create a collection from the given value.
@@ -889,30 +910,5 @@ if (! function_exists('with')) {
     function with($object)
     {
         return $object;
-    }
-}
-
-if (!function_exists("clear_routes")) {
-    /**
-     * Clear routes file
-     *
-     * @param String $remove
-     * @return mixed
-     */
-    function clear_routes($remove)
-    {
-        $path = app_path() . '/Http/routes.php';
-
-        $lines = file($path, FILE_IGNORE_NEW_LINES);
-
-        foreach ($lines as $key => $line) {
-            if (strstr($line, $remove)) {
-                unset($lines[$key]);
-            }
-        }
-
-        $data = implode("\n", array_values($lines));
-
-        return file_put_contents($path, $data);
     }
 }
