1491a8d80900e8ffb7134d9e9110ea4508e8c1a2
Amrani Houssain <amranidev@users.noreply.github.com>
2016-05-16 22:22:16 +0100

    fix style

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -375,13 +375,17 @@ if (! function_exists('clear_routes')) {
     function clear_routes($remove)
     {
         $path = app_path().'/Http/routes.php';
+        
         $lines = file($path, FILE_IGNORE_NEW_LINES);
+        
         foreach ($lines as $key => $line) {
             if (strstr($line, $remove)) {
                 unset($lines[$key]);
             }
         }
+        
         $data = implode("\n", array_values($lines));
+        
         return file_put_contents($path, $data);
     }
 }
