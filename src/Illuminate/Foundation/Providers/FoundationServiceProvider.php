e78a0e42b77de699503d555f203fdb5946aa65e4
Mohamed Said <themsaid@gmail.com>
2016-05-18 19:32:31 +0300

    Fixes #13453 by firing the RouteMatched event to be able to deal with FormRequests (#13474)
    initisalize FormRequest regardless of the RouteMatched event

--- src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
+++ src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
@@ -5,7 +5,6 @@ namespace Illuminate\Foundation\Providers;
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Routing\Events\RouteMatched;
 use Symfony\Component\HttpFoundation\Request;
 use Illuminate\Contracts\Validation\ValidatesWhenResolved;
 
@@ -42,12 +41,10 @@ class FoundationServiceProvider extends ServiceProvider
             $resolved->validate();
         });
 
-        $this->app['events']->listen(RouteMatched::class, function () {
-            $this->app->resolving(function (FormRequest $request, $app) {
-                $this->initializeRequest($request, $app['request']);
+        $this->app->resolving(function (FormRequest $request, $app) {
+            $this->initializeRequest($request, $app['request']);
 
-                $request->setContainer($app)->setRedirector($app->make(Redirector::class));
-            });
+            $request->setContainer($app)->setRedirector($app->make(Redirector::class));
         });
     }
 
