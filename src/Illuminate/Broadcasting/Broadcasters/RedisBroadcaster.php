237cacdb9012fecc168513a85253fcf04cb7bfc9
Thiery Laverdure <thiery@spacestud.io>
2016-05-21 01:22:49 -0400

    Adding support for new Broadcast Support Provider

--- src/Illuminate/Broadcasting/Broadcasters/RedisBroadcaster.php
+++ src/Illuminate/Broadcasting/Broadcasters/RedisBroadcaster.php
@@ -2,8 +2,10 @@
 
 namespace Illuminate\Broadcasting\Broadcasters;
 
+use Illuminate\Support\Str;
 use Illuminate\Contracts\Broadcasting\Broadcaster;
 use Illuminate\Contracts\Redis\Database as RedisDatabase;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class RedisBroadcaster implements Broadcaster
 {
@@ -20,6 +22,13 @@ class RedisBroadcaster implements Broadcaster
      * @var string
      */
     protected $connection;
+
+    /**
+     * The registered channel authenticators.
+     *
+     * @var array
+     */
+    protected $channels = [];
 
     /**
      * Create a new broadcaster instance.
@@ -46,5 +55,80 @@ class RedisBroadcaster implements Broadcaster
         foreach ($channels as $channel) {
             $connection->publish($channel, $payload);
         }
+    }
+
+    /**
+     * Register a channel authenticator.
+     *
+     * @param  string  $channel
+     * @param  callable  $callback
+     * @return $this
+     */
+    public function auth($channel, callable $callback)
+    {
+        $this->channels[$channel] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Authenticate the incoming request for a given channel.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function check($request)
+    {
+        $channel = str_replace(['private-', 'presence-'], '', $request->channel_name);
+
+        foreach ($this->channels as $pattern => $callback) {
+            if (!Str::is($pattern, $channel)) {
+                continue;
+            }
+
+            $parameters = $this->extractAuthParameters($pattern, $channel);
+
+            if ($result = $callback($request->user(), ...$parameters)) {
+                return $this->validAuthenticationResponse($request, $result);
+            }
+        }
+
+        throw new HttpException(403);
+    }
+
+    /**
+     * Return the valid Pusher authentication response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $result
+     * @return mixed
+     */
+    protected function validAuthenticationResponse($request, $result)
+    {
+        return is_array($result) ? $result : json_encode($result);
+    }
+
+    /**
+     * Extract the parameters from the given pattern and channel.
+     *
+     * @param  string  $pattern
+     * @param  string  $channel
+     * @return array
+     */
+    protected function extractAuthParameters($pattern, $channel)
+    {
+        if (!Str::contains($pattern, '*')) {
+            return [];
+        }
+
+        $pattern = str_replace('\*', '([^\.]+)', preg_quote($pattern));
+
+        if (preg_match('/^'.$pattern.'/', $channel, $keys)) {
+            array_shift($keys);
+
+            return $keys;
+        }
+
+        return [];
     }
 }
