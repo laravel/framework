79fc0323695c57fab24cd4161c183c526fff0403
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-05-31 17:42:24 +0200

    Allow any arrayable item in Collection::whereIn (#13793)

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -263,12 +263,14 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
      * Filter items by the given key value pair.
      *
      * @param  string  $key
-     * @param  array  $values
+     * @param  mixed  $values
      * @param  bool  $strict
      * @return static
      */
-    public function whereIn($key, array $values, $strict = false)
+    public function whereIn($key, $values, $strict = false)
     {
+        $values = $this->getArrayableItems($values);
+
         return $this->filter(function ($item) use ($key, $values, $strict) {
             return in_array(data_get($item, $key), $values, $strict);
         });
@@ -278,10 +280,10 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
      * Filter items by the given key value pair using strict comparison.
      *
      * @param  string  $key
-     * @param  array  $values
+     * @param  mixed  $values
      * @return static
      */
-    public function whereInStrict($key, array $values)
+    public function whereInStrict($key, $values)
     {
         return $this->whereIn($key, $values, true);
     }
