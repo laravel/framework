a65386fe4aec6f1efd71b5ec2b1e5e8439f925c4
Taylor Otwell <taylorotwell@gmail.com>
2016-06-01 13:21:58 -0500

    refactor pagination to use views. not presenters.

--- src/Illuminate/Pagination/AbstractPaginator.php
+++ src/Illuminate/Pagination/AbstractPaginator.php
@@ -74,11 +74,11 @@ abstract class AbstractPaginator implements Htmlable
     protected static $currentPageResolver;
 
     /**
-     * The default presenter resolver.
+     * The view factory resolver callback.
      *
      * @var \Closure
      */
-    protected static $presenterResolver;
+    protected static $viewFactoryResolver;
 
     /**
      * Determine if the given value is a valid page number.
@@ -271,6 +271,16 @@ abstract class AbstractPaginator implements Htmlable
     }
 
     /**
+     * Determine if the paginator is on the first page.
+     *
+     * @return bool
+     */
+    public function onFirstPage()
+    {
+        return $this->currentPage() <= 1;
+    }
+
+    /**
      * Get the current page.
      *
      * @return int
@@ -344,14 +354,24 @@ abstract class AbstractPaginator implements Htmlable
     }
 
     /**
-     * Set the default Presenter resolver.
+     * Get an instance of the view factory from the resolver.
+     *
+     * @return \Illuminate\Contracts\View\Factory
+     */
+    public static function viewFactory()
+    {
+        return call_user_func(static::$viewFactoryResolver);
+    }
+
+    /**
+     * Set the view factory resolver callback.
      *
      * @param  \Closure  $resolver
      * @return void
      */
-    public static function presenter(Closure $resolver)
+    public static function viewFactoryResolver(Closure $resolver)
     {
-        static::$presenterResolver = $resolver;
+        static::$viewFactoryResolver = $resolver;
     }
 
     /**
--- src/Illuminate/Pagination/PaginationServiceProvider.php
+++ src/Illuminate/Pagination/PaginationServiceProvider.php
@@ -7,12 +7,26 @@ use Illuminate\Support\ServiceProvider;
 class PaginationServiceProvider extends ServiceProvider
 {
     /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'pagination');
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
      */
     public function register()
     {
+        Paginator::viewFactoryResolver(function () {
+            return $this->app['view'];
+        });
+
         Paginator::currentPathResolver(function () {
             return $this->app['request']->url();
         });
