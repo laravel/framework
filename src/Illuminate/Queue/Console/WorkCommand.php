25b4d930c8962362e68da99d4fb915cffe02cc8c
Taylor Otwell <taylorotwell@gmail.com>
2016-05-31 21:27:23 -0500

    Show actual handler class names in queue console output.

--- src/Illuminate/Queue/Console/WorkCommand.php
+++ src/Illuminate/Queue/Console/WorkCommand.php
@@ -136,9 +136,9 @@ class WorkCommand extends Command
     protected function writeOutput(Job $job, $failed)
     {
         if ($failed) {
-            $this->output->writeln('<error>['.Carbon::now()->format('Y-m-d H:i:s').'] Failed:</error> '.$job->getName());
+            $this->output->writeln('<error>['.Carbon::now()->format('Y-m-d H:i:s').'] Failed:</error> '.$job->resolveName());
         } else {
-            $this->output->writeln('<info>['.Carbon::now()->format('Y-m-d H:i:s').'] Processed:</info> '.$job->getName());
+            $this->output->writeln('<info>['.Carbon::now()->format('Y-m-d H:i:s').'] Processed:</info> '.$job->resolveName());
         }
     }
 
--- src/Illuminate/Queue/Queue.php
+++ src/Illuminate/Queue/Queue.php
@@ -76,7 +76,7 @@ abstract class Queue
         } elseif (is_object($job)) {
             return json_encode([
                 'job' => 'Illuminate\Queue\CallQueuedHandler@call',
-                'data' => ['command' => serialize(clone $job)],
+                'data' => ['commandName' => get_class($job), 'command' => serialize(clone $job)],
             ]);
         }
 
