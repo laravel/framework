e9407012352b194330b03233143839941e5bf802
iv4art <iv4art@ya.ru>
2016-06-07 09:45:30 +0300

    Adding JSON_UNESCAPED_UNICODE to the method `asJson` in the `Model` for correct JSON encoding of non-ASCII chars

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -3011,7 +3011,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      */
     protected function asJson($value)
     {
-        return json_encode($value);
+        return json_encode($value, JSON_UNESCAPED_UNICODE);
     }
 
     /**
