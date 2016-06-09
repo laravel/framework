c35287ac389c33e443713fbcf28b27e05fc85b0b
Marvin Schröder <schroeder@mediameets.de>
2016-06-09 14:05:10 +0200

    Adding support for StreamOptions

--- src/Illuminate/Mail/TransportManager.php
+++ src/Illuminate/Mail/TransportManager.php
@@ -44,6 +44,10 @@ class TransportManager extends Manager
 
             $transport->setPassword($config['password']);
         }
+        
+        if (isset($config['stream'])) {
+            $transport->setStreamOptions($config['stream']);
+        }
 
         return $transport;
     }
