bb594556eef784b642f6ed460a09739c1b513b90
Benjamin Schanzel <b.schanzel@gmail.com>
2016-06-11 13:25:52 +0200

    [5.1] Yet another attempt to fix Swift Mailer related SSL failures (Issue #4573) (#13583)
    * Issue #4573
        - close mailer connection after sending message
        - fix return statements and related phpdoc
    
    * Minor
        Fixed code style according to StyleCI
    
    * Revert return type fixes
    
    * Minor
        fixed code style according to StyleCI
    
    * Minor
        StyleCI still not happy... :-(
    
    * - Reverted removal of Mailer::forceReconnection() method
    - wrap try-finally around swift->send(); transport->stop();
    
    * Minor: remove temporary result variable
    
    * Use private field instead of public getter for swift mailer

--- src/Illuminate/Mail/Mailer.php
+++ src/Illuminate/Mail/Mailer.php
@@ -156,8 +156,6 @@ class Mailer implements MailerContract, MailQueueContract
      */
     public function send($view, array $data, $callback)
     {
-        $this->forceReconnection();
-
         // First we need to parse the view, which could either be a string or an array
         // containing both an HTML and plain text versions of the view which should
         // be used when sending an e-mail. We will extract both of them out here.
@@ -392,7 +390,11 @@ class Mailer implements MailerContract, MailQueueContract
         }
 
         if (! $this->pretending) {
-            return $this->swift->send($message, $this->failedRecipients);
+            try {
+                return $this->swift->send($message, $this->failedRecipients);
+            } finally {
+                $this->swift->getTransport()->stop();
+            }
         } elseif (isset($this->logger)) {
             $this->logMessage($message);
         }
