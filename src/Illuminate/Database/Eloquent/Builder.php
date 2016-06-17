22618413e9dfa9fe48aa3b38e7868cdf287381bd
Mark Beech <mbeech@mark-beech.co.uk>
2016-06-17 16:03:23 +0100

    Add without() method to remove specified eager loads

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -1032,6 +1032,23 @@ class Builder
     }
 
     /**
+     * Remove any relationships that have been set to be eager loaded.
+     *
+     * @param  mixed  $relations
+     * @return $this
+     */
+    public function without($relations)
+    {
+        if (is_string($relations)) {
+            $relations = func_get_args();
+        }
+
+        $this->eagerLoad = array_diff_key($this->eagerLoad, array_flip($relations));
+
+        return $this;
+    }
+
+    /**
      * Add subselect queries to count the relations.
      *
      * @param  mixed  $relations
