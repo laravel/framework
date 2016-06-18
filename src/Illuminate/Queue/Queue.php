d1d794802b517ce00b2c06d0e08cfee333d64ccf
Zhang Jianfeng <zelome@me.com>
2016-06-18 19:14:33 +0800

    modify $job type from string to mixed

--- src/Illuminate/Queue/Queue.php
+++ src/Illuminate/Queue/Queue.php
@@ -64,7 +64,7 @@ abstract class Queue
     /**
      * Create a payload string from the given job and data.
      *
-     * @param  string  $job
+     * @param  mixed  $job
      * @param  mixed   $data
      * @param  string  $queue
      * @return string
