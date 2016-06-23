c8b727aa1cb61f9a6eed3f513390d97c047cacb7
Rafael Mello Campanari <rafael@veridu.com>
2016-06-23 02:03:52 -0300

    PostgresProcessor | convert to tabs

--- src/Illuminate/Database/Query/Processors/PostgresProcessor.php
+++ src/Illuminate/Database/Query/Processors/PostgresProcessor.php
@@ -6,40 +6,40 @@ use Illuminate\Database\Query\Builder;
 
 class PostgresProcessor extends Processor
 {
-    /**
-     * Process an "insert get ID" query.
-     *
-     * @param  \Illuminate\Database\Query\Builder  $query
-     * @param  string  $sql
-     * @param  array   $values
-     * @param  string  $sequence
-     * @return int
-     */
-    public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
-    {
-        $result = $query->getConnection()->selectFromWriteConnection($sql, $values)[0];
+	/**
+	 * Process an "insert get ID" query.
+	 *
+	 * @param  \Illuminate\Database\Query\Builder  $query
+	 * @param  string  $sql
+	 * @param  array   $values
+	 * @param  string  $sequence
+	 * @return int
+	 */
+	public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
+	{
+		$result = $query->getConnection()->selectFromWriteConnection($sql, $values)[0];
 
-        $sequence = $sequence ?: 'id';
-        
-        $id = is_object($result) ? $result->$sequence : $result[$sequence];
+		$sequence = $sequence ?: 'id';
+		
+		$id = is_object($result) ? $result->$sequence : $result[$sequence];
 
-        return is_numeric($id) ? (int) $id : $id;
-    }
+		return is_numeric($id) ? (int) $id : $id;
+	}
 
-    /**
-     * Process the results of a column listing query.
-     *
-     * @param  array  $results
-     * @return array
-     */
-    public function processColumnListing($results)
-    {
-        $mapping = function ($r) {
-            $r = (object) $r;
+	/**
+	 * Process the results of a column listing query.
+	 *
+	 * @param  array  $results
+	 * @return array
+	 */
+	public function processColumnListing($results)
+	{
+		$mapping = function ($r) {
+			$r = (object) $r;
 
-            return $r->column_name;
-        };
+			return $r->column_name;
+		};
 
-        return array_map($mapping, $results);
-    }
+		return array_map($mapping, $results);
+	}
 }
