4c26db72fe3e14c8fa5848922082801a5f65150a
Taylor Otwell <taylorotwell@gmail.com>
2016-06-29 10:00:43 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Container/ContainerTest.php
+++ tests/Container/ContainerTest.php
@@ -212,7 +212,7 @@ class ContainerContainerTest extends PHPUnit_Framework_TestCase
 
             return $obj;
         });
-        
+
         $this->assertEquals('foo', $container->make('foo')->foo);
         $this->assertEquals('baz', $container->make('foo')->bar);
         $this->assertEquals('foo', $container->make('foo')->baz);
