e980a70d5fae0c418e33e8856c361b1b153edc54
CupOfTea696 <cupoftea696@gmail.com>
2016-07-02 19:26:00 +0100

    Fallback parameter for UrlGenerator::previous

--- tests/Routing/RoutingUrlGeneratorTest.php
+++ tests/Routing/RoutingUrlGeneratorTest.php
@@ -369,6 +369,10 @@ class RoutingUrlGeneratorTest extends PHPUnit_Framework_TestCase
 
         $url->getRequest()->headers->remove('referer');
         $this->assertEquals($url->to('/'), $url->previous());
+        
+        $this->assertEquals($url->to('/foo'), $url->previous(function () use ($url) {
+            return $url->to('/foo');
+        }));
     }
 }
 
