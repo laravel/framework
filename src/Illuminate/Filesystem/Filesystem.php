ccd8c4a88fde52f35025d0beda5209866ea89ee2
Pedro Henrique Vieira Rodrigues <vrodrigues.pedro@gmail.com>
2016-07-14 13:01:44 -0300

    Filesystem::get with offset and limit

--- src/Illuminate/Filesystem/Filesystem.php
+++ src/Illuminate/Filesystem/Filesystem.php
@@ -42,6 +42,7 @@ class Filesystem
                 $params[] = $offset;
                 $params[] = $limit;
             }
+            
             return $lock ? $this->sharedGet($path, $offset, $limit) : call_user_func_array('file_get_contents', $params);
         }
 
