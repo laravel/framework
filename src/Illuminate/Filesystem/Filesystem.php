d9ebf3c0066d5e6e0d83dd05b5ea9c5f6a2e4237
BePsvPT <freedom@csie.io>
2016-07-15 11:47:28 +0800

    Improve Filesystem sharedGet performance

--- src/Illuminate/Filesystem/Filesystem.php
+++ src/Illuminate/Filesystem/Filesystem.php
@@ -51,14 +51,16 @@ class Filesystem
     {
         $contents = '';
 
-        $handle = fopen($path, 'r');
+        $handle = fopen($path, 'rb');
 
         if ($handle) {
             try {
                 if (flock($handle, LOCK_SH)) {
-                    while (! feof($handle)) {
-                        $contents .= fread($handle, 1048576);
-                    }
+                    clearstatcache(true, $path);
+
+                    $contents = fread($handle, $this->size($path) ?: 1);
+
+                    flock($handle, LOCK_UN);
                 }
             } finally {
                 fclose($handle);
