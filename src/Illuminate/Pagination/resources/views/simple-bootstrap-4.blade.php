433a80224c56e9edd54153655738caec45822e99
kawax <kawaxbiz@gmail.com>
2016-07-16 23:02:40 +0900

    fix bootstrap4

--- src/Illuminate/Pagination/resources/views/simple-bootstrap-4.blade.php
+++ src/Illuminate/Pagination/resources/views/simple-bootstrap-4.blade.php
@@ -1,7 +1,7 @@
 <ul class="pagination">
     <!-- Previous Page Link -->
     @if ($paginator->onFirstPage())
-        <li class="page-item disabled"><span>&laquo;</span></li>
+        <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
     @else
         <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
     @endif
@@ -10,6 +10,6 @@
     @if ($paginator->hasMorePages())
         <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
     @else
-        <li class="page-item disabled"><span>&raquo;</span></li>
+        <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
     @endif
 </ul>
