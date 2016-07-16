431598df4198bf038b80a94f0db9fa3a4bbdd8de
kawax <kawaxbiz@gmail.com>
2016-07-16 23:56:51 +0900

    fix bootstrap4 (#14335)

--- src/Illuminate/Pagination/resources/views/bootstrap-4.blade.php
+++ src/Illuminate/Pagination/resources/views/bootstrap-4.blade.php
@@ -1,7 +1,7 @@
 <ul class="pagination">
     <!-- Previous Page Link -->
     @if ($paginator->onFirstPage())
-        <li class="page-item disabled"><span>&laquo;</span></li>
+        <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
     @else
         <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
     @endif
@@ -10,14 +10,14 @@
     @foreach ($elements as $element)
         <!-- "Three Dots" Separator -->
         @if (is_string($element))
-            <li class="page-item disabled"><span>{{ $element }}</span></li>
+            <li class="page-item disabled"><span class="page-link">{{ $element }}</span></li>
         @endif
 
         <!-- Array Of Links -->
         @if (is_array($element))
             @foreach ($element as $page => $url)
                 @if ($page == $paginator->currentPage())
-                    <li class="page-item active"><span>{{ $page }}</span></li>
+                    <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
                 @else
                     <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                 @endif
@@ -29,6 +29,6 @@
     @if ($paginator->hasMorePages())
         <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
     @else
-        <li class="page-item disabled"><span>&raquo;</span></li>
+        <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
     @endif
 </ul>
