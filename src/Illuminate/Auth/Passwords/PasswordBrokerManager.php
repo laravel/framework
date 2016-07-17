34e540dbac08266fc63704eddb6c35d2fb1d3ba9
Joseph Silber <contact@josephsilber.com>
2016-07-17 14:47:08 -0400

    Use spread operators (#14348)

--- src/Illuminate/Auth/Passwords/PasswordBrokerManager.php
+++ src/Illuminate/Auth/Passwords/PasswordBrokerManager.php
@@ -138,6 +138,6 @@ class PasswordBrokerManager implements FactoryContract
      */
     public function __call($method, $parameters)
     {
-        return call_user_func_array([$this->broker(), $method], $parameters);
+        return $this->broker()->{$method}(...$parameters);
     }
 }
