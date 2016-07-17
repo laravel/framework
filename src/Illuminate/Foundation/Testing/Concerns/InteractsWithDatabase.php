299a5c6f49f8ea3b3ac72d163c42fabf0dc32c5d
Dries Vints <dries.vints@gmail.com>
2016-07-17 13:55:20 +0200

    Add new count test helpers

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithDatabase.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithDatabase.php
@@ -77,6 +77,126 @@ trait InteractsWithDatabase
     }
 
     /**
+     * Assert that a given count matches the amount of rows in the database for a given where condition.
+     *
+     * @param  string  $table
+     * @param  int  $expectedCount
+     * @param  array  $data
+     * @param  string  $connection
+     * @return $this
+     */
+    protected function countInDatabase($table, $expectedCount, array $data = [], $connection = null)
+    {
+        $database = $this->app->make('db');
+
+        $connection = $connection ?: $database->getDefaultConnection();
+
+        $count = $database->connection($connection)->table($table)->where($data)->count();
+
+        $this->assertCount($expectedCount, $count, sprintf(
+            'Expected count does not match the amount of rows in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
+
+    /**
+     * Assert that a given count is greater than the amount of rows in the database for a given where condition.
+     *
+     * @param  string  $table
+     * @param  int  $expectedCount
+     * @param  array  $data
+     * @param  string  $connection
+     * @return $this
+     */
+    protected function greaterCountInDatabase($table, $expectedCount, array $data = [], $connection = null)
+    {
+        $database = $this->app->make('db');
+
+        $connection = $connection ?: $database->getDefaultConnection();
+
+        $count = $database->connection($connection)->table($table)->where($data)->count();
+
+        $this->assertGreaterThan($expectedCount, $count, sprintf(
+            'Expected count is lower or equal than the amount of rows in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
+
+    /**
+     * Assert that a given count is greater or equal than the amount of rows in the database for a given where condition.
+     *
+     * @param  string  $table
+     * @param  int  $expectedCount
+     * @param  array  $data
+     * @param  string  $connection
+     * @return $this
+     */
+    protected function greaterOrEqualCountInDatabase($table, $expectedCount, array $data = [], $connection = null)
+    {
+        $database = $this->app->make('db');
+
+        $connection = $connection ?: $database->getDefaultConnection();
+
+        $count = $database->connection($connection)->table($table)->where($data)->count();
+
+        $this->assertGreaterThanOrEqual($expectedCount, $count, sprintf(
+            'Expected count is lower than the amount of rows in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
+
+    /**
+     * Assert that a given count is lower than the amount of rows in the database for a given where condition.
+     *
+     * @param  string  $table
+     * @param  int  $expectedCount
+     * @param  array  $data
+     * @param  string  $connection
+     * @return $this
+     */
+    protected function lowerCountInDatabase($table, $expectedCount, array $data = [], $connection = null)
+    {
+        $database = $this->app->make('db');
+
+        $connection = $connection ?: $database->getDefaultConnection();
+
+        $count = $database->connection($connection)->table($table)->where($data)->count();
+
+        $this->assertLessThan($expectedCount, $count, sprintf(
+            'Expected count is greater or equal than the amount of rows in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
+
+    /**
+     * Assert that a given count is lower or equal than the amount of rows in the database for a given where condition.
+     *
+     * @param  string  $table
+     * @param  int  $expectedCount
+     * @param  array  $data
+     * @param  string  $connection
+     * @return $this
+     */
+    protected function lowerOrEqualCountInDatabase($table, $expectedCount, array $data = [], $connection = null)
+    {
+        $database = $this->app->make('db');
+
+        $connection = $connection ?: $database->getDefaultConnection();
+
+        $count = $database->connection($connection)->table($table)->where($data)->count();
+
+        $this->assertLessThanOrEqual($expectedCount, $count, sprintf(
+            'Expected count is greater than the amount of rows in database table [%s] that matched attributes [%s].', $table, json_encode($data)
+        ));
+
+        return $this;
+    }
+
+    /**
      * Seed a given database connection.
      *
      * @param  string  $class
