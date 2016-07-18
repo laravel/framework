eed9a583708e44b5588f0611b445a2d2ea7d6e7d
Taylor Otwell <taylorotwell@gmail.com>
2016-07-18 14:50:48 -0500

    add more expressive methods for getting all args or options

--- src/Illuminate/Console/Command.php
+++ src/Illuminate/Console/Command.php
@@ -228,6 +228,16 @@ class Command extends SymfonyCommand
     }
 
     /**
+     * Get all of the arguments passed to the command.
+     *
+     * @return array
+     */
+    public function arguments()
+    {
+        return $this->argument();
+    }
+
+    /**
      * Determine if the given option is present.
      *
      * @param  string  $name
@@ -251,6 +261,16 @@ class Command extends SymfonyCommand
         }
 
         return $this->input->getOption($key);
+    }
+
+    /**
+     * Get all of the options passed to the command.
+     *
+     * @return array
+     */
+    public function options()
+    {
+        return $this->option();
     }
 
     /**
