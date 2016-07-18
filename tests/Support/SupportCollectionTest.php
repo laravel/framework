d202fc0181a87221620d7dae11cf606fe97b3da1
Taylor Otwell <taylorotwell@gmail.com>
2016-07-18 08:44:13 -0500

    working on code

--- tests/Support/SupportCollectionTest.php
+++ tests/Support/SupportCollectionTest.php
@@ -1325,7 +1325,7 @@ class SupportCollectionTest extends PHPUnit_Framework_TestCase
     public function testMode()
     {
         $collection = new Collection([1, 2, 3, 4, 4, 5]);
-        $this->assertEquals(new Collection([4]), $collection->mode());
+        $this->assertEquals([4], $collection->mode());
     }
 
     public function testModeValueByKey()
@@ -1336,13 +1336,13 @@ class SupportCollectionTest extends PHPUnit_Framework_TestCase
             (object) ['foo' => 2],
             (object) ['foo' => 4],
         ]);
-        $this->assertEquals(new Collection([1]), $collection->mode('foo'));
+        $this->assertEquals([1], $collection->mode('foo'));
     }
 
     public function testWithMultipleModeValues()
     {
         $collection = new Collection([1, 2, 2, 1]);
-        $this->assertEquals(new Collection([1, 2]), $collection->mode());
+        $this->assertEquals([1, 2], $collection->mode());
     }
 }
 
