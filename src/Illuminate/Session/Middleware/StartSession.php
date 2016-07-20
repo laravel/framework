9070676dff33827cb480e009693846a79a79b7ad
Jeroen van Oort <jvanoort@simplexis.nl>
2016-07-20 15:11:42 +0200

    moved up collectGarbage call to run it even when the response throws an exception (#14386)

--- src/Illuminate/Session/Middleware/StartSession.php
+++ src/Illuminate/Session/Middleware/StartSession.php
@@ -57,6 +57,8 @@ class StartSession
             $session = $this->startSession($request);
 
             $request->setSession($session);
+
+            $this->collectGarbage($session);
         }
 
         $response = $next($request);
@@ -66,8 +68,6 @@ class StartSession
         // add the session identifier cookie to the application response headers now.
         if ($this->sessionConfigured()) {
             $this->storeCurrentUrl($request, $session);
-
-            $this->collectGarbage($session);
 
             $this->addCookieToResponse($response, $session);
         }
