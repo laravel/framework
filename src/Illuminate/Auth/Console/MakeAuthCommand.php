9b7c93c0a8b48a565858d2788955ef5b92a42622
vlakoff <vlakoff@gmail.com>
2016-07-25 21:18:15 +0200

    Graceful degradation for logging out when JavaScript is disabled

--- src/Illuminate/Auth/Console/MakeAuthCommand.php
+++ src/Illuminate/Auth/Console/MakeAuthCommand.php
@@ -30,6 +30,7 @@ class MakeAuthCommand extends Command
      */
     protected $views = [
         'auth/login.stub' => 'auth/login.blade.php',
+        'auth/logout.stub' => 'auth/logout.blade.php',
         'auth/register.stub' => 'auth/register.blade.php',
         'auth/passwords/email.stub' => 'auth/passwords/email.blade.php',
         'auth/passwords/reset.stub' => 'auth/passwords/reset.blade.php',
