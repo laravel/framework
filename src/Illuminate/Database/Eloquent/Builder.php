9fede1938f726539aaf79f766d05f573139d89af
Mohamed Said <themsaid@gmail.com>
2016-07-26 05:22:10 +0300

    fix #14468 by fixing another issue with PR 14188 (#14476)

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -490,7 +490,7 @@ class Builder
 
         $total = $query->getCountForPagination();
 
-        $results = $total ? $this->forPage($page, $perPage)->get($columns) : [];
+        $results = $total ? $this->forPage($page, $perPage)->get($columns) : new Collection();
 
         return new LengthAwarePaginator($results, $total, $perPage, $page, [
             'path' => Paginator::resolveCurrentPath(),
