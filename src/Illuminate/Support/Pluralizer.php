51fcca46c54ea8a5cf4d944ddc6bbbae656bfd78
Saeed Prez <saeed.sarvi@gmail.com>
2016-07-28 05:17:26 +0200

    [5.2] Update Str::plural() to use intval() (#14502)
    * Update Str::plural() to use intval()
    
    A lot of the time when you use str_plural() you get the $count value from a form request which returns the number as a string and therefore this plural won't work as expected. This small change should make sure the $count is in integer format.
    
    Thanks to @barryvdh for suggesting to do this change deeper in the code.
    
    * Changed to type casting
    
    Using `(int) $count` instead of `intval($count)` as requested.

--- src/Illuminate/Support/Pluralizer.php
+++ src/Illuminate/Support/Pluralizer.php
@@ -50,7 +50,7 @@ class Pluralizer
      */
     public static function plural($value, $count = 2)
     {
-        if ($count === 1 || static::uncountable($value)) {
+        if ((int) $count === 1 || static::uncountable($value)) {
             return $value;
         }
 
