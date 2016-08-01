7d0694139eda81114ac552fe0ea9e440825dc889
Brandon Shar <bshar@thatsus.com>
2016-08-01 11:20:58 -0400

    good change in arguments for contains closure in 5.3

--- src/Illuminate/Database/Eloquent/Collection.php
+++ src/Illuminate/Database/Eloquent/Collection.php
@@ -124,7 +124,7 @@ class Collection extends BaseCollection implements QueueableCollection
     {
         $result = parent::map($callback);
 
-        return $result->contains(function ($_, $item) {
+        return $result->contains(function ($item) {
             return ! ($item instanceof Model);
         }) ? $result->toBase() : $result;
     }
