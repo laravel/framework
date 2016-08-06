12336ad5f4dcf57027dda83a20c852ecb16816e5
Darwin Luague <darwinluague9001@gmail.com>
2016-08-06 22:08:21 +0800

    [5.3] Fix undefined variable: request (#14659)
    Injected Illuminate\Http\Request in sendResetFailedResponse() method.

--- src/Illuminate/Foundation/Auth/ResetsPasswords.php
+++ src/Illuminate/Foundation/Auth/ResetsPasswords.php
@@ -54,7 +54,7 @@ trait ResetsPasswords
         // redirect them back to where they came from with their error message.
         return $response == Password::PASSWORD_RESET
                     ? $this->sendResetResponse($response)
-                    : $this->sendResetFailedResponse($response);
+                    : $this->sendResetFailedResponse($request, $response);
     }
 
     /**
@@ -102,10 +102,11 @@ trait ResetsPasswords
     /**
      * Get the response for a failed password reset.
      *
+     * @param  \Illuminate\Http\Request
      * @param  string  $response
      * @return \Illuminate\Http\Response
      */
-    protected function sendResetFailedResponse($response)
+    protected function sendResetFailedResponse(Request $request, $response)
     {
         return redirect()->back()
                     ->withInput($request->only('email'))
