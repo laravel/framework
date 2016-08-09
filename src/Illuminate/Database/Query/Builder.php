de85823eb7463ab788e6bb5f9bf284f23a7e6d7f
Martin Zurowietz <martin@zurowietz.de>
2016-08-09 10:38:29 +0200

    Support column aliases in chunkById
    References #14499

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -1783,11 +1783,14 @@ class Builder
      * @param  int  $count
      * @param  callable  $callback
      * @param  string  $column
+     * @param  string  $alias  Alias of the ID column if there are multiple columns with the same name. The alias must be defined in a select statement.
      * @return bool
      */
-    public function chunkById($count, callable $callback, $column = 'id')
+    public function chunkById($count, callable $callback, $column = 'id', $alias = null)
     {
         $lastId = null;
+
+        $alias = is_null($alias) ? $column : $alias;
 
         $results = $this->forPageAfterId($count, 0, $column)->get();
 
@@ -1796,7 +1799,7 @@ class Builder
                 return false;
             }
 
-            $lastId = last($results)->{$column};
+            $lastId = last($results)->{$alias};
 
             $results = $this->forPageAfterId($count, $lastId, $column)->get();
         }
