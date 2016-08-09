625c2083c65891458e111a726145ef53b74ca027
Taylor Otwell <taylorotwell@gmail.com>
2016-08-09 15:45:55 -0500

    add files

---
+++ src/Illuminate/Broadcasting/PendingBroadcast.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Illuminate\Broadcasting;
+
+use Illuminate\Contracts\Events\Dispatcher;
+
+class PendingBroadcast
+{
+    /**
+     * The event dispatcher implementation.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The event instance.
+     *
+     * @var mixed
+     */
+    protected $event;
+
+    /**
+     * Create a new pending broadcast instance.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @param
+     */
+    public function __construct(Dispatcher $events, $event)
+    {
+        $this->event = $event;
+        $this->events = $events;
+    }
+
+    /**
+     * Broadcast the event to all listeners.
+     *
+     * @return void
+     */
+    public function all()
+    {
+        $this->events->fire($event->broadcastToEveryone());
+    }
+
+    /**
+     * Broadcast the event to everyone except the current user.
+     *
+     * @return void
+     */
+    public function others()
+    {
+        $this->events->fire($event->dontBroadcastToCurrentUser());
+    }
+}
