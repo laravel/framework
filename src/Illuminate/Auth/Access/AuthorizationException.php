9057a211f3a6965911af08ddcfbc2507afd54cd1
jwdeitch <jwdeitch@users.noreply.github.com>
2016-08-10 13:33:35 -0400

    add proper exception code

--- src/Illuminate/Auth/Access/AuthorizationException.php
+++ src/Illuminate/Auth/Access/AuthorizationException.php
@@ -6,5 +6,8 @@ use Exception;
 
 class AuthorizationException extends Exception
 {
-    //
+    public function __construct($message = 'Unauthorized')
+    {
+        parent::__construct($message, 401);
+    }
 }
