584fa77d4b8ba153ad48bdda10b97997d05a6bee
Oskar Danielsson <oskar.danielsson@hotmail.com>
2016-08-10 14:54:21 +0200

    Improved method description (#14736)

--- src/Illuminate/Http/Request.php
+++ src/Illuminate/Http/Request.php
@@ -338,7 +338,7 @@ class Request extends SymfonyRequest implements Arrayable, ArrayAccess
     }
 
     /**
-     * Get a subset of the items from the input data.
+     * Get a subset containing the provided keys with values from the input data.
      *
      * @param  array|mixed  $keys
      * @return array
