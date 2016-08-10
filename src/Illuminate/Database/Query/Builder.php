31a1e5dee1adca007e2f0b334f7cf3e23240b7af
Taylor Otwell <taylorotwell@gmail.com>
2016-08-10 07:23:17 -0500

    Applied fixes from StyleCI (#14738)

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -2152,7 +2152,7 @@ class Builder
     public function increment($column, $amount = 1, array $extra = [])
     {
         if (! is_numeric($amount)) {
-            throw new InvalidArgumentException("Non-numeric value passed to increment method.");
+            throw new InvalidArgumentException('Non-numeric value passed to increment method.');
         }
 
         $wrapped = $this->grammar->wrap($column);
@@ -2173,7 +2173,7 @@ class Builder
     public function decrement($column, $amount = 1, array $extra = [])
     {
         if (! is_numeric($amount)) {
-            throw new InvalidArgumentException("Non-numeric value passed to decrement method.");
+            throw new InvalidArgumentException('Non-numeric value passed to decrement method.');
         }
 
         $wrapped = $this->grammar->wrap($column);
