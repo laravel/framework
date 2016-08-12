af8d4e2db24f16152759e19e86560f79b36153b8
Graham Campbell <graham@alt-three.com>
2016-08-12 19:26:25 +0100

    Avoid call_user_func_array in __call functions (#14791)

--- src/Illuminate/Filesystem/FilesystemManager.php
+++ src/Illuminate/Filesystem/FilesystemManager.php
@@ -325,6 +325,6 @@ class FilesystemManager implements FactoryContract
      */
     public function __call($method, $parameters)
     {
-        return call_user_func_array([$this->disk(), $method], $parameters);
+        return $this->disk()->$method(...$parameters);
     }
 }
