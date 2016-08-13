cb43683ca1b6483c8349ddf0d36a82801a86b7f5
Mohamed Said <themohamedsaid@gmail.com>
2016-08-13 11:10:12 +0200

    Fire events on channel level and add event before sending

--- tests/Notifications/NotificationChannelManagerTest.php
+++ tests/Notifications/NotificationChannelManagerTest.php
@@ -20,10 +20,27 @@ class NotificationChannelManagerTest extends PHPUnit_Framework_TestCase
         Container::setInstance($container);
         $manager = Mockery::mock(ChannelManager::class.'[driver]', [$container]);
         $manager->shouldReceive('driver')->andReturn($driver = Mockery::mock());
+        $events->shouldReceive('until')->with(Mockery::type(Illuminate\Notifications\Events\NotificationSending::class))->andReturn(true);
         $driver->shouldReceive('send')->once();
         $events->shouldReceive('fire')->with(Mockery::type(Illuminate\Notifications\Events\NotificationSent::class));
 
         $manager->send([new NotificationChannelManagerTestNotifiable], new NotificationChannelManagerTestNotification);
+    }
+
+    public function testNotificationNotSentOnHalt()
+    {
+        $container = new Container;
+        $container->instance('config', ['app.name' => 'Name', 'app.logo' => 'Logo']);
+        $container->instance('events', $events = Mockery::mock());
+        Container::setInstance($container);
+        $manager = Mockery::mock(ChannelManager::class.'[driver]', [$container]);
+        $events->shouldReceive('until')->once()->with(Mockery::type(Illuminate\Notifications\Events\NotificationSending::class))->andReturn(false);
+        $events->shouldReceive('until')->with(Mockery::type(Illuminate\Notifications\Events\NotificationSending::class))->andReturn(true);
+        $manager->shouldReceive('driver')->once()->andReturn($driver = Mockery::mock());
+        $driver->shouldReceive('send')->once();
+        $events->shouldReceive('fire')->with(Mockery::type(Illuminate\Notifications\Events\NotificationSent::class));
+
+        $manager->send([new NotificationChannelManagerTestNotifiable], new NotificationChannelManagerTestNotificationWithTwoChannels);
     }
 
     public function testNotificationCanBeQueued()
@@ -49,6 +66,19 @@ class NotificationChannelManagerTestNotification extends Notification
     public function via()
     {
         return ['test'];
+    }
+
+    public function message()
+    {
+        return $this->line('test')->action('Text', 'url');
+    }
+}
+
+class NotificationChannelManagerTestNotificationWithTwoChannels extends Notification
+{
+    public function via()
+    {
+        return ['test', 'test2'];
     }
 
     public function message()
