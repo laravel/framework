d5a5eb85a36abdce700b4bb975aebd8893f44560
Taylor Otwell <taylorotwell@gmail.com>
2016-08-16 14:51:45 -0500

    attachments on mail notification messages

--- src/Illuminate/Notifications/Messages/MailMessage.php
+++ src/Illuminate/Notifications/Messages/MailMessage.php
@@ -12,6 +12,20 @@ class MailMessage extends SimpleMessage
     public $view = 'notifications::email';
 
     /**
+     * The attachments for the message.
+     *
+     * @var array
+     */
+    public $attachments = [];
+
+    /**
+     * The raw attachments for the message.
+     *
+     * @var array
+     */
+    public $rawAttachments = [];
+
+    /**
      * Set the view for the mail message.
      *
      * @param  string  $view
@@ -20,6 +34,35 @@ class MailMessage extends SimpleMessage
     public function view($view)
     {
         $this->view = $view;
+
+        return $this;
+    }
+
+    /**
+     * Attach a file to the message.
+     *
+     * @param  string  $file
+     * @param  array  $options
+     * @return $this
+     */
+    public function attach($file, array $options = [])
+    {
+        $this->attachments[] = compact('file', 'options');
+
+        return $this;
+    }
+
+    /**
+     * Attach in-memory data as an attachment.
+     *
+     * @param  string  $data
+     * @param  string  $name
+     * @param  array  $options
+     * @return $this
+     */
+    public function attachData($data, $name, array $options = [])
+    {
+        $this->rawAttachments[] = compact('data', 'name', 'options');
 
         return $this;
     }
