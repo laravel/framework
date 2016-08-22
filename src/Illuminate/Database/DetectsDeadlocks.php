1fc9a6b4e20e935e4b66b3c8a7bc5012d2207211
Sara Bine <sara@sarabine.com>
2016-08-21 18:55:26 -0600

    Add option to automatically retry deadlocked transactions (#5380)

---
+++ src/Illuminate/Database/DetectsDeadlocks.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Illuminate\Database;
+
+use Exception;
+use Illuminate\Support\Str;
+
+trait DetectsDeadlocks
+{
+    /**
+     * Determine if the given exception was caused by a deadlock.
+     *
+     * @param  \Exception  $e
+     * @return bool
+     */
+    protected function causedByDeadlock(Exception $e)
+    {
+        $message = $e->getMessage();
+
+        return Str::contains($message, [
+            'Deadlock found when trying to get lock',
+            'deadlock detected',
+            'The database file is locked',
+            'A table in the database is locked',
+            'has been chosen as the deadlock victim',
+        ]);
+    }
+}
