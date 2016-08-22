4bd521736f8dc6ad2a4ce4c7c23ddedc5e66221e
Giuseppe Menti <mentifg@gmail.com>
2016-08-22 13:46:22 -0300

    Remove unnecessary "else" conditions

--- src/Illuminate/Auth/AuthManager.php
+++ src/Illuminate/Auth/AuthManager.php
@@ -88,15 +88,15 @@ class AuthManager implements FactoryContract
 
         if (isset($this->customCreators[$config['driver']])) {
             return $this->callCustomCreator($name, $config);
-        } else {
-            $driverMethod = 'create'.ucfirst($config['driver']).'Driver';
-
-            if (method_exists($this, $driverMethod)) {
-                return $this->{$driverMethod}($name, $config);
-            } else {
-                throw new InvalidArgumentException("Auth guard driver [{$name}] is not defined.");
-            }
         }
+
+        $driverMethod = 'create'.ucfirst($config['driver']).'Driver';
+
+        if (method_exists($this, $driverMethod)) {
+            return $this->{$driverMethod}($name, $config);
+        }
+
+        throw new InvalidArgumentException("Auth guard driver [{$name}] is not defined.");
     }
 
     /**
