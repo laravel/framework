39a39b513e7aa6db1e7267ed058e5fbf27ac71a4
Mohamed Said <themsaid@gmail.com>
2016-08-23 19:31:57 +0200

    Check if exact key exists before assuming the dot notation represents segments (#14976)

--- src/Illuminate/Support/Arr.php
+++ src/Illuminate/Support/Arr.php
@@ -288,6 +288,10 @@ class Arr
         foreach ($keys as $key) {
             $subKeyArray = $array;
 
+            if (static::exists($array, $key)) {
+                continue;
+            }
+
             foreach (explode('.', $key) as $segment) {
                 if (static::accessible($subKeyArray) && static::exists($subKeyArray, $segment)) {
                     $subKeyArray = $subKeyArray[$segment];
