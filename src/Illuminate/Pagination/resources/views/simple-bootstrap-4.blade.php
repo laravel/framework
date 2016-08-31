79095c602d3d13b279b5ef3083eb05b239e9e6bb
Gary Green <holegary@gmail.com>
2016-08-31 15:58:29 +0100

    Hide empty paginators. (#15125)

--- src/Illuminate/Pagination/resources/views/simple-bootstrap-4.blade.php
+++ src/Illuminate/Pagination/resources/views/simple-bootstrap-4.blade.php
@@ -1,15 +1,17 @@
-<ul class="pagination">
-    <!-- Previous Page Link -->
-    @if ($paginator->onFirstPage())
-        <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
-    @else
-        <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
-    @endif
+@if ($paginator->count() > 1)
+    <ul class="pagination">
+        <!-- Previous Page Link -->
+        @if ($paginator->onFirstPage())
+            <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
+        @else
+            <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
+        @endif
 
-    <!-- Next Page Link -->
-    @if ($paginator->hasMorePages())
-        <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
-    @else
-        <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
-    @endif
-</ul>
+        <!-- Next Page Link -->
+        @if ($paginator->hasMorePages())
+            <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
+        @else
+            <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
+        @endif
+    </ul>
+@endif
