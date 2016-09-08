5deab59e89b85e09b2bd1642e4efe55e933805ca
Taylor Otwell <taylorotwell@gmail.com>
2016-09-07 20:36:00 -0500

    add fakes for bus, events, mail, queue, notifications

--- src/Illuminate/Support/Facades/Bus.php
+++ src/Illuminate/Support/Facades/Bus.php
@@ -2,11 +2,23 @@
 
 namespace Illuminate\Support\Facades;
 
+use Illuminate\Support\Testing\Fakes\BusFake;
+
 /**
  * @see \Illuminate\Contracts\Bus\Dispatcher
  */
 class Bus extends Facade
 {
+    /**
+     * Replace the bound instance with a fake.
+     *
+     * @return void
+     */
+    public static function fake()
+    {
+        static::swap(new BusFake);
+    }
+
     /**
      * Get the registered name of the component.
      *
