adb6854b03ec8ec336746a5e239ec337bc14f28d
Christopher L Bray <chris@brayniverse.com>
2016-09-12 18:23:01 +0100

    import by length

--- src/Illuminate/Foundation/Auth/RegistersUsers.php
+++ src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -2,10 +2,10 @@
 
 namespace Illuminate\Foundation\Auth;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 trait RegistersUsers
 {
