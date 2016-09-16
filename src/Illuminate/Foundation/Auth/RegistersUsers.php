bcb4b83dc51435f09e91a55929298f0540c0a65f
Taylor Otwell <taylor@laravel.com>
2016-09-16 09:59:28 -0700

    Applied fixes from StyleCI (#15468)

--- src/Illuminate/Foundation/Auth/RegistersUsers.php
+++ src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -5,7 +5,6 @@ namespace Illuminate\Foundation\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Contracts\Auth\Authenticatable;
 
 trait RegistersUsers
 {
