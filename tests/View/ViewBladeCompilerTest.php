2d640d2b3e609974bfc4cd6934bacfdac360575a
Elf Sundae <elf.sundae@gmail.com>
2016-09-18 18:38:07 +0800

    Add multi-line @forelse test

--- tests/View/ViewBladeCompilerTest.php
+++ tests/View/ViewBladeCompilerTest.php
@@ -528,6 +528,28 @@ empty
         $this->assertEquals($expected, $compiler->compileString($string));
     }
 
+    public function testForelseStatementsAreCompiledWithMultipleLine()
+    {
+        $compiler = new BladeCompiler($this->getFiles(), __DIR__);
+        $string = '@forelse ([
+foo,
+bar,
+] as $label)
+breeze
+@empty
+empty
+@endforelse';
+        $expected = '<?php $__empty_1 = true; $__currentLoopData = [
+foo,
+bar,
+]; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $label): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); $__empty_1 = false; ?>
+breeze
+<?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); if ($__empty_1): ?>
+empty
+<?php endif; ?>';
+        $this->assertEquals($expected, $compiler->compileString($string));
+    }
+
     public function testNestedForelseStatementsAreCompiled()
     {
         $compiler = new BladeCompiler($this->getFiles(), __DIR__);
