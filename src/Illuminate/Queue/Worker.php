1819c2d3236a7a9df91c5157d5d9bae1d35f5bcb
Taylor Otwell <taylorotwell@gmail.com>
2016-09-21 13:12:49 -0500

    sleep for 1 second at least in maintenance mode

--- src/Illuminate/Queue/Worker.php
+++ src/Illuminate/Queue/Worker.php
@@ -90,7 +90,11 @@ class Worker
      */
     protected function daemonShouldRun()
     {
-        if ($this->manager->isDownForMaintenance() || $this->events->until('illuminate.queue.looping') === false) {
+        if ($this->manager->isDownForMaintenance() ||
+            $this->events->until('illuminate.queue.looping') === false) {
+            // If the application is down for maintenance or doesn't want the queues to run
+            // we will sleep for one second just in case the developer has it set to not
+            // sleep at all. This just prevents CPU from maxing out in this situation.
             $this->sleep(1);
 
             return false;
