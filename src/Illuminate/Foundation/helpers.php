80310cd01f33de70c5ca039a5cb7cd0fff11731d
alex <ilovephp@spaces.ru>
2016-09-23 13:55:09 +0300

    Add short trans variant
    Add short trans variant

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -757,6 +757,23 @@ if (! function_exists('storage_path')) {
     }
 }
 
+if (! function_exists('t')) {
+     
+     /**
+     * Short function trans() .
+     *
+     * @param  string  $id
+     * @param  array   $parameters
+     * @param  string  $domain
+     * @param  string  $locale
+     * @return \Symfony\Component\Translation\TranslatorInterface|string
+     */
+    function t($id = null, $parameters = [], $domain = 'messages', $locale = null)
+    {
+       return trans($id,$parameters,$domain,$locale);
+    }
+}
+
 if (! function_exists('trans')) {
     /**
      * Translate the given message.
