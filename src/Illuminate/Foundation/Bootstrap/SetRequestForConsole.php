920f023e1bd11de1a6a2a0e923a0cce7481110d9
Julio Foulquie <jfoulquie@gmail.com>
2016-09-26 10:19:48 -0300

    Change server variables when bootstrapping the app request from artisan.
    This change fixes #14139

--- src/Illuminate/Foundation/Bootstrap/SetRequestForConsole.php
+++ src/Illuminate/Foundation/Bootstrap/SetRequestForConsole.php
@@ -16,7 +16,11 @@ class SetRequestForConsole
     public function bootstrap(Application $app)
     {
         $url = $app->make('config')->get('app.url', 'http://localhost');
-
+        $urlParts = parse_url($url);
+        if (isset($urlParts['path']) && mb_strlen($urlParts['path']) > 1) {
+            $_SERVER['SCRIPT_NAME'] = $urlParts['path'].DIRECTORY_SEPARATOR.'index.php';
+            $_SERVER['SCRIPT_FILENAME'] = getenv('PWD').DIRECTORY_SEPARATOR.$urlParts['path'].DIRECTORY_SEPARATOR.'index.php';
+        }
         $app->instance('request', Request::create($url, 'GET', [], [], [], $_SERVER));
     }
 }
