acda08d150e078d77a283c28260d1228390fa269
chengguoqiang <chengguoqiang2008@163.com>
2016-09-29 18:15:44 +0800

    Update ValidationValidatorTest.php

--- tests/Validation/ValidationValidatorTest.php
+++ tests/Validation/ValidationValidatorTest.php
@@ -284,6 +284,11 @@ class ValidationValidatorTest extends PHPUnit_Framework_TestCase
         $file = new File(__FILE__, false);
         $v = new Validator($trans, ['name' => $file], ['name' => 'Required']);
         $this->assertTrue($v->passes());
+        
+        $file = new File(__FILE__, false);
+        $foo = new File(__FILE__, false);
+        $v = new Validator($trans, ['name' => [$file, $foo]], ['name.0' => 'Required', 'name.1' => 'Required']);
+        $this->assertTrue($v->passes());
     }
 
     public function testValidateRequiredWith()
