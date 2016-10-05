ca01fc76294d0a854e05b8225a98c0e3e63e0531
Hendrik Grobler <hendrikg@miway.co.za>
2016-10-05 14:24:04 +0200

    Add translations support to make:auth with default 'en' support * Update stub views to use blade translation function and new keys * Add 'en' labels file with keys and strings * Add lang option to make:auth command (defaults to 'en') * Export labels language file as part of make:auth command

--- src/Illuminate/Auth/Console/MakeAuthCommand.php
+++ src/Illuminate/Auth/Console/MakeAuthCommand.php
@@ -4,6 +4,7 @@ namespace Illuminate\Auth\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Console\AppNamespaceDetectorTrait;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 
 class MakeAuthCommand extends Command
 {
@@ -14,7 +15,7 @@ class MakeAuthCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'make:auth {--views : Only scaffold the authentication views}';
+    protected $signature = 'make:auth {--views : Only scaffold the authentication views} {--lang=en : The language of the labels}';
 
     /**
      * The console command description.
@@ -44,9 +45,15 @@ class MakeAuthCommand extends Command
      */
     public function fire()
     {
+        $language = strtolower($this->option('lang'));
+
+        $this->checkIfLanguageIsSupported($language);
+
         $this->createDirectories();
 
         $this->exportViews();
+
+        $this->exportLanguageFile($language);
 
         if (! $this->option('views')) {
             file_put_contents(
@@ -78,6 +85,10 @@ class MakeAuthCommand extends Command
         if (! is_dir(base_path('resources/views/auth/passwords'))) {
             mkdir(base_path('resources/views/auth/passwords'), 0755, true);
         }
+
+        if (! is_dir(base_path('resources/lang'))) {
+            mkdir(base_path('resources/lang'), 0755, true);
+        }
     }
 
     /**
@@ -91,6 +102,59 @@ class MakeAuthCommand extends Command
             copy(
                 __DIR__.'/stubs/make/views/'.$key,
                 base_path('resources/views/'.$value)
+            );
+        }
+    }
+
+    /**
+     * Validate the language option
+     *
+     * @param string $language
+     */
+    protected function checkIfLanguageIsSupported($language)
+    {
+        $supportedLanguages = $this->getSupportedLanguages();
+        if (!in_array($language, $supportedLanguages)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Language %s is not supported. Supported languages: %s',
+                    $language,
+                    implode($supportedLanguages, ', ')
+                )
+            );
+        }
+    }
+
+    /**
+     * Get a list of languages found in resources/lang folder
+     *
+     * @return array
+     */
+    protected function getSupportedLanguages()
+    {
+        $languageDirectory = __DIR__.'/stubs/make/resources/lang/';
+        $supportedLanguageDirectoryPaths = glob($languageDirectory.'*', GLOB_ONLYDIR);
+
+        return array_map(function($path) use ($languageDirectory, $supportedLanguageDirectoryPaths){
+            return str_replace($languageDirectory, '', $path);
+        }, $supportedLanguageDirectoryPaths);
+    }
+
+    /**
+     * Export the language translation file for the specified language.
+     *
+     * @param string $language
+     */
+    protected function exportLanguageFile($language)
+    {
+        if (! is_dir(base_path('resources/lang/'.$language))) {
+            mkdir(base_path('resources/lang/'.$language), 0755, true);
+        }
+
+        if (!file_exists(base_path('resources/lang/' . $language . '/labels.php'))) {
+            copy(
+                __DIR__.'/stubs/make/resources/lang/' . $language . '/labels.php',
+                base_path('resources/lang/' . $language . '/labels.php')
             );
         }
     }
---
+++ src/Illuminate/Auth/Console/stubs/make/resources/lang/en/labels.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'auth' => [
+        'logout' => 'Logout',
+        'email_address' => 'E-Mail Address',
+        'password' => 'Password',
+        'password_confirm' => 'Confirm Password',
+        'remember_me' => 'Remember Me',
+        'login' => 'Login',
+        'password_reset' => 'Forgot Your Password?',
+        'register' => 'Register',
+        'reset_password' => 'Reset Password',
+        'send_password_reset' => 'Send Password Reset Link'
+    ],
+    'home' => [
+        'heading' => 'Dashboard',
+        'logged_in' => 'You are logged in!'
+    ]
+];
