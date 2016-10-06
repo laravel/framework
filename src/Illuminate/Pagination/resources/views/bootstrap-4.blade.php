34ad9726ad9de2069f3761bf3c75d00f9771f563
Thomas Praxl <thomas@macrominds.de>
2016-10-06 20:06:11 +0200

    Hide developer hints in html (#15786)
    Turn html comments into blade comments so that the resulting html is cleaner.

--- src/Illuminate/Pagination/resources/views/bootstrap-4.blade.php
+++ src/Illuminate/Pagination/resources/views/bootstrap-4.blade.php
@@ -1,20 +1,20 @@
 @if ($paginator->hasPages())
     <ul class="pagination">
-        <!-- Previous Page Link -->
+        {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
             <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
         @else
             <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
         @endif
 
-        <!-- Pagination Elements -->
+        {{-- Pagination Elements --}}
         @foreach ($elements as $element)
-            <!-- "Three Dots" Separator -->
+            {{-- "Three Dots" Separator --}}
             @if (is_string($element))
                 <li class="page-item disabled"><span class="page-link">{{ $element }}</span></li>
             @endif
 
-            <!-- Array Of Links -->
+            {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
@@ -26,7 +26,7 @@
             @endif
         @endforeach
 
-        <!-- Next Page Link -->
+        {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
         @else
