c446044dfb0b1fd5b10748042308711d0f347925
Georg Thomassen <GeorgThomassen@users.noreply.github.com>
2016-10-08 16:11:14 +0200

    Update ThrottleRequests.php
    removed @return from DocBlock for the constructor method

--- src/Illuminate/Routing/Middleware/ThrottleRequests.php
+++ src/Illuminate/Routing/Middleware/ThrottleRequests.php
@@ -20,7 +20,6 @@ class ThrottleRequests
      * Create a new request throttler.
      *
      * @param  \Illuminate\Cache\RateLimiter  $limiter
-     * @return void
      */
     public function __construct(RateLimiter $limiter)
     {
