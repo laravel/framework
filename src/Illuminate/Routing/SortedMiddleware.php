9cc5334d00824441ccce5e9d2979723e41b2fc05
Taylor Otwell <taylorotwell@gmail.com>
2016-10-14 15:10:15 -0500

    comments

--- src/Illuminate/Routing/SortedMiddleware.php
+++ src/Illuminate/Routing/SortedMiddleware.php
@@ -25,6 +25,8 @@ class SortedMiddleware extends Collection
     /**
      * Sort the middlewares by the given priority map.
      *
+     * Each call to this method makes one discrete middleware movement if necessary.
+     *
      * @param  array  $priorityMap
      * @param  array  $middlewares
      * @return array
@@ -43,10 +45,19 @@ class SortedMiddleware extends Collection
             if (in_array($stripped, $priorityMap)) {
                 $priorityIndex = array_search($stripped, $priorityMap);
 
+                // This middleware is in the priority map. If we have encountered another middleware
+                // that was also in the priority map and was at a lower priority than the current
+                // middleware, we will move this middleware to be above the previous encounter.
                 if (isset($lastPriorityIndex) && $priorityIndex < $lastPriorityIndex) {
                     return $this->sortMiddleware(
-                        $priorityMap, array_values($this->spliceMiddleware($middlewares, $index, $lastIndex))
+                        $priorityMap, array_values(
+                            $this->moveMiddleware($middlewares, $index, $lastIndex)
+                        )
                     );
+
+                // This middlewarwe is in the priority map, but this is the first middleware we have
+                // encountered from the map thus far. We'll save its current index plus its index
+                // from the priority map so we can compare against them on the next iterations.
                 } else {
                     $lastIndex = $index;
                     $lastPriorityIndex = $priorityIndex;
@@ -65,7 +76,7 @@ class SortedMiddleware extends Collection
      * @param  int  $to
      * @return array
      */
-    protected function spliceMiddleware($middlewares, $from, $to)
+    protected function moveMiddleware($middlewares, $from, $to)
     {
         array_splice($middlewares, $to, 0, $middlewares[$from]);
 
