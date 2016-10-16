730079aa8f62b6daa3e16ac14724df6acd0d684b
Till Krüss <till@kruss.io>
2016-10-16 16:55:06 -0700

    Decrement only in one place

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -617,17 +617,13 @@ if (! function_exists('retry')) {
      */
     function retry($times, callable $callback, $sleep = 0)
     {
-        $times--;
-
         beginning:
         try {
             return $callback();
         } catch (Exception $e) {
-            if (! $times) {
+            if (! --$times) {
                 throw $e;
             }
-
-            $times--;
 
             if ($sleep) {
                 usleep($sleep * 1000);
