baaad854b883b0bf896b82fbb6bfc5fdcb3728c2
Mohamed Said <themsaid@gmail.com>
2016-10-17 22:34:48 +0300

    Fix Eloquent find or create/update on multiple connections (#15958)

--- tests/Database/DatabaseEloquentIntegrationTest.php
+++ tests/Database/DatabaseEloquentIntegrationTest.php
@@ -251,6 +251,75 @@ class DatabaseEloquentIntegrationTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Abigail Otwell', $user3->name);
     }
 
+    public function testUpdateOrCreate()
+    {
+        $user1 = EloquentTestUser::create(['email' => 'taylorotwell@gmail.com']);
+
+        $user2 = EloquentTestUser::updateOrCreate(
+            ['email' => 'taylorotwell@gmail.com'],
+            ['name' => 'Taylor Otwell']
+        );
+
+        $this->assertEquals($user1->id, $user2->id);
+        $this->assertEquals('taylorotwell@gmail.com', $user2->email);
+        $this->assertEquals('Taylor Otwell', $user2->name);
+
+        $user3 = EloquentTestUser::updateOrCreate(
+            ['email' => 'themsaid@gmail.com'],
+            ['name' => 'Mohamed Said']
+        );
+
+        $this->assertEquals('Mohamed Said', $user3->name);
+        $this->assertEquals(EloquentTestUser::count(), 2);
+    }
+
+    public function testUpdateOrCreateOnDifferentConnection()
+    {
+        EloquentTestUser::create(['email' => 'taylorotwell@gmail.com']);
+
+        EloquentTestUser::on('second_connection')->updateOrCreate(
+            ['email' => 'taylorotwell@gmail.com'],
+            ['name' => 'Taylor Otwell']
+        );
+
+        EloquentTestUser::on('second_connection')->updateOrCreate(
+            ['email' => 'themsaid@gmail.com'],
+            ['name' => 'Mohamed Said']
+        );
+
+        $this->assertEquals(EloquentTestUser::count(), 1);
+        $this->assertEquals(EloquentTestUser::on('second_connection')->count(), 2);
+    }
+
+    public function testCheckAndCreateMethodsOnMultiConnections()
+    {
+        EloquentTestUser::create(['id' => 1, 'email' => 'taylorotwell@gmail.com']);
+        EloquentTestUser::on('second_connection')->find(
+            EloquentTestUser::on('second_connection')->insert(['id' => 2, 'email' => 'themsaid@gmail.com'])
+        );
+
+        $user1 = EloquentTestUser::on('second_connection')->findOrNew(1);
+        $user2 = EloquentTestUser::on('second_connection')->findOrNew(2);
+        $this->assertFalse($user1->exists);
+        $this->assertTrue($user2->exists);
+        $this->assertEquals('second_connection', $user1->getConnectionName());
+        $this->assertEquals('second_connection', $user2->getConnectionName());
+
+        $user1 = EloquentTestUser::on('second_connection')->firstOrNew(['email' => 'taylorotwell@gmail.com']);
+        $user2 = EloquentTestUser::on('second_connection')->firstOrNew(['email' => 'themsaid@gmail.com']);
+        $this->assertFalse($user1->exists);
+        $this->assertTrue($user2->exists);
+        $this->assertEquals('second_connection', $user1->getConnectionName());
+        $this->assertEquals('second_connection', $user2->getConnectionName());
+
+        $this->assertEquals(1, EloquentTestUser::on('second_connection')->count());
+        $user1 = EloquentTestUser::on('second_connection')->firstOrCreate(['email' => 'taylorotwell@gmail.com']);
+        $user2 = EloquentTestUser::on('second_connection')->firstOrCreate(['email' => 'themsaid@gmail.com']);
+        $this->assertEquals('second_connection', $user1->getConnectionName());
+        $this->assertEquals('second_connection', $user2->getConnectionName());
+        $this->assertEquals(2, EloquentTestUser::on('second_connection')->count());
+    }
+
     public function testPluck()
     {
         EloquentTestUser::create(['id' => 1, 'email' => 'taylorotwell@gmail.com']);
