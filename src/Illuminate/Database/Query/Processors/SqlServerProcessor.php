b9efeffed3e12853d64d3f20afe2a81bb160a277
vlakoff <vlakoff@gmail.com>
2016-10-17 17:46:58 +0200

    Same as parent

--- src/Illuminate/Database/Query/Processors/SqlServerProcessor.php
+++ src/Illuminate/Database/Query/Processors/SqlServerProcessor.php
@@ -9,22 +9,6 @@ use Illuminate\Database\Query\Builder;
 class SqlServerProcessor extends Processor
 {
     /**
-     * Process an "insert get ID" query.
-     *
-     * @param  \Illuminate\Database\Query\Builder  $query
-     * @param  string  $sql
-     * @param  array   $values
-     * @param  string  $sequence
-     * @return int
-     */
-    public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
-    {
-        $query->getConnection()->insert($sql, $values);
-
-        return $this->processLastInsertId($query, $sequence);
-    }
-
-    /**
      * Process a "last insert ID" query.
      *
      * @param  \Illuminate\Database\Query\Builder  $query
