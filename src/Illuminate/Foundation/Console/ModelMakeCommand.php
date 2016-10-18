ab9b1469ee42118904fcf8132370719ee4483418
thunder87 <thunder87@users.noreply.github.com>
2016-10-18 15:59:07 +0200

    Updated naming convention
    The latest addition would create the controllers with snake case

--- src/Illuminate/Foundation/Console/ModelMakeCommand.php
+++ src/Illuminate/Foundation/Console/ModelMakeCommand.php
@@ -44,7 +44,7 @@ class ModelMakeCommand extends GeneratorCommand
             }
 
             if ($this->option('controller')) {
-                $controller = Str::camel(class_basename($this->argument('name')));
+                $controller = studly_case(class_basename($this->argument('name')));
 
                 $this->call('make:controller', ['name' => "{$controller}Controller", '--resource' => true]);
             }
