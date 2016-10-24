20e22e32de2755ff530549fc0d03d51395732eb6
Taylor Otwell <taylorotwell@gmail.com>
2016-10-24 08:32:58 -0500

    change method name

--- src/Illuminate/Foundation/Auth/ResetsPasswords.php
+++ src/Illuminate/Foundation/Auth/ResetsPasswords.php
@@ -35,7 +35,7 @@ trait ResetsPasswords
      */
     public function reset(Request $request)
     {
-        $this->validate($request, $this->getResetValidationRules());
+        $this->validate($request, $this->rules());
 
         // Here we will attempt to reset the user's password. If it is successful we
         // will update the password on an actual user model and persist it to the
@@ -59,7 +59,7 @@ trait ResetsPasswords
      *
      * @return array
      */
-    protected function getResetValidationRules()
+    protected function rules()
     {
         return [
             'token' => 'required', 'email' => 'required|email',
