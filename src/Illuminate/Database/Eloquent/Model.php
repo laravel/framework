e95166054fbae03d22f8e17cc4ef9c4b63d727f6
Jonathan Pickett <jonathanpickett@me.com>
2016-10-24 12:07:33 -0700

    Restore support for using snake case relationship names on model

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -2629,18 +2629,20 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      */
     public function getRelationValue($key)
     {
+        $camelKey = camel_case($key);
+
         // If the key already exists in the relationships array, it just means the
         // relationship has already been loaded, so we'll just return it out of
         // here because there is no need to query within the relations twice.
-        if ($this->relationLoaded($key)) {
-            return $this->relations[$key];
+        if ($this->relationLoaded($camelKey)) {
+            return $this->relations[$camelKey];
         }
 
         // If the "attribute" exists as a method on the model, we will just assume
         // it is a relationship and will load and return results from the query
         // and hydrate the relationship's value on the "relationships" array.
-        if (method_exists($this, $key)) {
-            return $this->getRelationshipFromMethod($key);
+        if (method_exists($this, $camelKey)) {
+            return $this->getRelationshipFromMethod($camelKey);
         }
     }
 
