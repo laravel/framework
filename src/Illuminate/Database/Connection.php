49d710eaa5ff1451f0cffc1cd125f48ba57d550e
Mohamed Said <themohamedsaid@gmail.com>
2016-10-25 15:31:40 +0200

    affect only MySQL connections

--- src/Illuminate/Database/Connection.php
+++ src/Illuminate/Database/Connection.php
@@ -406,7 +406,7 @@ class Connection implements ConnectionInterface
         foreach ($bindings as $key => $value) {
             $statement->bindValue(
                 is_string($key) ? $key : $key + 1, $value,
-                is_int($value) || is_float($value) ? PDO::PARAM_INT : PDO::PARAM_STR
+                is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR
             );
         }
     }
