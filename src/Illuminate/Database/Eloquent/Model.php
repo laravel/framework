18d281a4f8b880a0f0b5ecb07b9eaa8fa7e3715e
Dylan Page <genpage@digitalocean.com>
2016-10-27 12:31:51 -0400

    Backport PHP7 __isset() fix from 5.3 branch

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -3077,8 +3077,7 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 */
 	public function __isset($key)
 	{
-		return ((isset($this->attributes[$key]) || isset($this->relations[$key])) ||
-				($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key))));
+		return ! is_null($this->getAttribute($key));
 	}
 
 	/**
