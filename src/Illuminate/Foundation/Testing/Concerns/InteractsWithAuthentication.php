474540f6904289d20812471ebb22316d47f5a771
Taylor Otwell <taylorotwell@gmail.com>
2016-10-28 16:59:51 -0500

    initial progress on cleaning up testing

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithAuthentication.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithAuthentication.php
@@ -2,8 +2,38 @@
 
 namespace Illuminate\Foundation\Testing\Concerns;
 
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+
 trait InteractsWithAuthentication
 {
+    /**
+     * Set the currently logged in user for the application.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  string|null  $driver
+     * @return $this
+     */
+    public function actingAs(UserContract $user, $driver = null)
+    {
+        $this->be($user, $driver);
+
+        return $this;
+    }
+
+    /**
+     * Set the currently logged in user for the application.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  string|null  $driver
+     * @return void
+     */
+    public function be(UserContract $user, $driver = null)
+    {
+        $this->app['auth']->guard($driver)->setUser($user);
+
+        $this->app['auth']->shouldUse($driver);
+    }
+
     /**
      * Assert that the user is authenticated.
      *
