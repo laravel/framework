d6196246a2214741dc0c888ae7b5364c7e0a9b50
Alan Reed <aarreedd@users.noreply.github.com>
2016-10-30 12:57:58 -0400

    Add "base64:" prefix support to env() helper (laravel/internals#266)

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -426,7 +426,7 @@ if (! function_exists('encrypt')) {
 
 if (! function_exists('env')) {
     /**
-     * Gets the value of an environment variable. Supports boolean, empty and null.
+     * Gets the value of an environment variable. Supports boolean, empty, null and base64 prefix.
      *
      * @param  string  $key
      * @param  mixed   $default
@@ -453,6 +453,10 @@ if (! function_exists('env')) {
             case 'null':
             case '(null)':
                 return;
+        }
+
+        if (Str::startsWith($value, 'base64:')) {
+            return base64_decode(substr($value, 7));
         }
 
         if (strlen($value) > 1 && Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
