c0140720909bfca7e57d7a76196a20a24787d8ca
Alfred Nutile <alfrednutile@gmail.com>
2016-11-03 07:44:24 -0400

    Space maybe

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -164,7 +164,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
     /**
      * Set an option on the casts encoding.
      * @link http://php.net/manual/en/json.constants.php
-     * 
+     *
      * @var string
      */
     protected $json_encode_option = 0;
