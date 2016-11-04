f0b59135430c56d5ea9a70e8da50ddfadf1131bf
Taylor Otwell <taylorotwell@gmail.com>
2016-11-04 10:08:56 -0500

    version

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -28,7 +28,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
 	 *
 	 * @var string
 	 */
-	const VERSION = '4.2.20';
+	const VERSION = '4.2.21';
 
 	/**
 	 * Indicates if the application has "booted".
