c9c5e77142db6aa74dd75e9dccaf04012fdba2eb
Taylor Otwell <taylorotwell@gmail.com>
2016-11-07 16:09:46 -0600

    tweak config name

--- src/Illuminate/Filesystem/FilesystemAdapter.php
+++ src/Illuminate/Filesystem/FilesystemAdapter.php
@@ -287,15 +287,16 @@ class FilesystemAdapter implements FilesystemContract, CloudFilesystemContract
 
             return $adapter->getClient()->getObjectUrl($adapter->getBucket(), $path);
         } elseif ($adapter instanceof LocalAdapter) {
-            $flysystemConfig = $this->driver->getConfig();
+            $config = $this->driver->getConfig();
 
-            if ($flysystemConfig->has('url_root')) {
-                return $flysystemConfig->get('url_root').'/'.$path;
+            if ($config->has('url')) {
+                return $config->get('url').'/'.$path;
             }
 
             $path = '/storage/'.$path;
 
-            return Str::contains($path, '/storage/public') ? Str::replaceFirst('/public', '', $path) : $path;
+            return Str::contains($path, '/storage/public') ?
+                        Str::replaceFirst('/public', '', $path) : $path;
         } else {
             throw new RuntimeException('This driver does not support retrieving URLs.');
         }
