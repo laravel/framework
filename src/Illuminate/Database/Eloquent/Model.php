8b09371c3db64cf42e4ef1f54ebf93528365320a
Taylor Otwell <taylor@laravel.com>
2016-11-11 19:39:26 -0600

    Revert "[4.2] Expand on PHP7 compatiblity with __isset() fix from 5.3 branch"

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -3077,7 +3077,8 @@ abstract class Model implements ArrayAccess, ArrayableInterface, JsonableInterfa
 	 */
 	public function __isset($key)
 	{
-		return ! is_null($this->getAttribute($key));
+		return ((isset($this->attributes[$key]) || isset($this->relations[$key])) ||
+				($this->hasGetMutator($key) && ! is_null($this->getAttributeValue($key))));
 	}
 
 	/**
