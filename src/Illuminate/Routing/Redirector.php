5c56c2db255a490faaabd33b89214e3833e67f07
Andrew <browner12@gmail.com>
2016-11-15 22:21:36 -0600

    fix docblock

--- src/Illuminate/Routing/Redirector.php
+++ src/Illuminate/Routing/Redirector.php
@@ -46,9 +46,9 @@ class Redirector
     /**
      * Create a new redirect response to the previous location.
      *
-     * @param  int   $status
-     * @param  array $headers
-     * @param  bool  $fallback
+     * @param  int    $status
+     * @param  array  $headers
+     * @param  string $fallback
      * @return \Illuminate\Http\RedirectResponse
      */
     public function back($status = 302, $headers = [], $fallback = false)
