7c86931916fae25189149e07c684d9f1fb06112a
restored18 <restored18@gmail.com>
2016-11-17 16:03:01 -0600

    Added findBy and findByOrFail methods

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -188,6 +188,21 @@ class Builder
 
         return $this->get($columns);
     }
+    
+    /**
+     * Find a model by a single column
+     * 
+     * @param  string $column
+     * @param  mixed  $value
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static[]|static|null
+     */
+    public function findBy($column, $value, $columns = ['*'])
+    {
+        $this->query->where($column, '=', $value);
+
+        return $this->first($columns);
+    }
 
     /**
      * Find a model by its primary key or throw an exception.
@@ -211,6 +226,26 @@ class Builder
         }
 
         throw (new ModelNotFoundException)->setModel(get_class($this->model), $id);
+    }
+    
+    /**
+     * Find a model by a single column
+     * 
+     * @param  string $column
+     * @param  mixed  $value
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static[]|static|null
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function findByOrFail($column, $value, $columns = ['*'])
+    {
+        $result = $this->findBy($column, $value, $columns);
+        if(! is_null($result)) {
+            return $result;
+        }
+
+        throw (new ModelNotFoundException)->setModle(get_class($this->model), $id);
     }
 
     /**
