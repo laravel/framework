3b8123b1ca10d518bc4a6429fac8a3ff54ad551c
Bert-Jan Verwer <mail.bertjan@gmail.com>
2016-11-28 19:32:05 +0100

    Merged into one method and renamed to chmod.

--- tests/Filesystem/FilesystemTest.php
+++ tests/Filesystem/FilesystemTest.php
@@ -32,21 +32,21 @@ class FilesystemTest extends PHPUnit_Framework_TestCase
         $this->assertStringEqualsFile($this->tempDir.'/file.txt', 'Hello World');
     }
 
-    public function testSetUnixMode()
+    public function testSetChmod()
     {
         file_put_contents($this->tempDir.'/file.txt', 'Hello World');
         $files = new Filesystem();
-        $files->setUnixMode($this->tempDir.'/file.txt', 0755);
+        $files->chmod($this->tempDir.'/file.txt', 0755);
         $filePermisson = substr(sprintf('%o', fileperms($this->tempDir.'/file.txt')), -4);
         $this->assertEquals('0755', $filePermisson);
     }
 
-    public function testGetUnixMode()
+    public function testGetChmod()
     {
         file_put_contents($this->tempDir.'/file.txt', 'Hello World');
         chmod($this->tempDir.'/file.txt', 0755);
         $files = new Filesystem();
-        $filePermisson = $files->getUnixMode($this->tempDir.'/file.txt');
+        $filePermisson = $files->chmod($this->tempDir.'/file.txt');
         $this->assertEquals('0755', $filePermisson);
     }
 
