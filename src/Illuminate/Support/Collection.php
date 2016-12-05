6cc3068768ce8eff105569657c21e66a540d9c8b
Duilio Palacios <i@duilio.me>
2016-12-05 10:21:18 +0000

    Preserve keys in the partition method

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -737,8 +737,8 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
 
         $callback = $this->valueRetriever($callback);
 
-        foreach ($this->items as $item) {
-            $partitions[(int) ! $callback($item)][] = $item;
+        foreach ($this->items as $key => $item) {
+            $partitions[(int) ! $callback($item)][$key] = $item;
         }
 
         return $partitions;
