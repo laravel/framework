10a8281f136006331f16adb8229a66e43f4fdbd5
Duilio Palacios <i@duilio.me>
2016-12-06 16:06:03 +0000

    Improve database testing methods

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithDatabase.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithDatabase.php
@@ -2,6 +2,9 @@
 
 namespace Illuminate\Foundation\Testing\Concerns;
 
+use PHPUnit_Framework_Constraint_Not as ReverseConstraint;
+use Illuminate\Foundation\Testing\Constraints\HasInDatabase;
+
 trait InteractsWithDatabase
 {
     /**
@@ -10,19 +13,18 @@ trait InteractsWithDatabase
      * @param  string  $table
      * @param  array  $data
      * @param  string  $connection
+     * @param  bool  $reverse
      * @return $this
      */
-    protected function seeInDatabase($table, array $data, $connection = null)
+    protected function seeInDatabase($table, array $data, $connection = null, $reverse = false)
     {
-        $database = $this->app->make('db');
+        $constraint = new HasInDatabase($data, $this->getConnection($connection));
 
-        $connection = $connection ?: $database->getDefaultConnection();
+        if ($reverse) {
+            $constraint = new ReverseConstraint($constraint);
+        }
 
-        $count = $database->connection($connection)->table($table)->where($data)->count();
-
-        $this->assertGreaterThan(0, $count, sprintf(
-            'Unable to find row in database table [%s] that matched attributes [%s].', $table, json_encode($data)
-        ));
+        $this->assertThat($table, $constraint);
 
         return $this;
     }
@@ -35,19 +37,50 @@ trait InteractsWithDatabase
      * @param  string  $connection
      * @return $this
      */
+    protected function missingFromDatabase($table, array $data, $connection = null)
+    {
+        return $this->notSeeInDatabase($table, $data, $connection);
+    }
+
+    /**
+     * Assert that a given where condition does not exist in the database.
+     *
+     * @param  string  $table
+     * @param  array  $data
+     * @param  string  $connection
+     * @return $this
+     */
     protected function dontSeeInDatabase($table, array $data, $connection = null)
+    {
+        return $this->notSeeInDatabase($table, $data, $connection);
+    }
+
+    /**
+     * Assert that a given where condition does not exist in the database.
+     *
+     * @param  string  $table
+     * @param  array  $data
+     * @param  string  $connection
+     * @return $this
+     */
+    protected function notSeeInDatabase($table, array $data, $connection = null)
+    {
+        return $this->seeInDatabase($table, $data, $connection, true);
+    }
+
+    /**
+     * Get the database connection.
+     *
+     * @param  string|null  $connection
+     * @return \Illuminate\Database\Collection
+     */
+    protected function getConnection($connection = null)
     {
         $database = $this->app->make('db');
 
         $connection = $connection ?: $database->getDefaultConnection();
 
-        $count = $database->connection($connection)->table($table)->where($data)->count();
-
-        $this->assertEquals(0, $count, sprintf(
-            'Found unexpected records in database table [%s] that matched attributes [%s].', $table, json_encode($data)
-        ));
-
-        return $this;
+        return $database->connection($connection);
     }
 
     /**
