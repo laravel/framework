71bb4e419b8d8f1fc3bae5c98c75b3c5ee3fe191
Rohit Subedi <rohitsubedi@gmail.com>
2016-12-07 14:57:43 +0700

    Fix date_format validation for all formats

--- src/Illuminate/Validation/Validator.php
+++ src/Illuminate/Validation/Validator.php
@@ -1834,10 +1834,10 @@ class Validator implements ValidatorContract
         if (! is_string($value) && ! is_numeric($value)) {
             return false;
         }
+        
+        $date = DateTime::createFromFormat($parameters[0], $value);
 
-        $parsed = date_parse_from_format($parameters[0], $value);
-
-        return $parsed['error_count'] === 0 && $parsed['warning_count'] === 0;
+        return $date && $date->format($parameters[0]) === $value;
     }
 
     /**
