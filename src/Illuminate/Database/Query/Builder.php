53f97a014da380dc85fb4b0d826475e562d78dcc
Taylor Otwell <taylorotwell@gmail.com>
2016-12-07 11:11:40 -0600

    clone query for chunking

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -1847,7 +1847,9 @@ class Builder
         $lastId = 0;
 
         do {
-            $results = $this->forPageAfterId($count, $lastId, $column)->get();
+            $clone = clone $this;
+
+            $results = $clone->forPageAfterId($count, $lastId, $column)->get();
 
             $countResults = $results->count();
 
