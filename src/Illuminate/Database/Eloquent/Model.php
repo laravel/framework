42c0e1449d15e818b5b04924f19ea7c735abc013
Fernando Henrique Bandeira <fernando@agenciaget.com.br>
2016-12-09 09:05:22 -0200

    timestamps

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1715,11 +1715,11 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
     {
         $time = $this->freshTimestamp();
 
-        if (! $this->isDirty(static::UPDATED_AT)) {
+        if (! $this->isDirty($this->getUpdatedAtColumn())) {
             $this->setUpdatedAt($time);
         }
 
-        if (! $this->exists && ! $this->isDirty(static::CREATED_AT)) {
+        if (! $this->exists && ! $this->isDirty($this->getCreatedAtColumn())) {
             $this->setCreatedAt($time);
         }
     }
@@ -1732,7 +1732,8 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      */
     public function setCreatedAt($value)
     {
-        $this->{static::CREATED_AT} = $value;
+        $column = $this->getCreatedAtColumn();
+        $this->{$column} = $value;
 
         return $this;
     }
@@ -1745,7 +1746,8 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      */
     public function setUpdatedAt($value)
     {
-        $this->{static::UPDATED_AT} = $value;
+        $column = $this->getUpdatedAtColumn();
+        $this->{$column} = $value;
 
         return $this;
     }
@@ -2935,7 +2937,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
      */
     public function getDates()
     {
-        $defaults = [static::CREATED_AT, static::UPDATED_AT];
+        $defaults = [$this->getCreatedAtColumn(), $this->getUpdatedAtColumn()];
 
         return $this->timestamps ? array_merge($this->dates, $defaults) : $this->dates;
     }
