2db47dcc1c75b0b16c4643f7b6ccd72299b31edd
vlakoff <vlakoff@gmail.com>
2016-12-12 22:44:53 +0100

    Revert changes to startsWith()

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -403,7 +403,7 @@ class Str
     public static function startsWith($haystack, $needles)
     {
         foreach ((array) $needles as $needle) {
-            if ($needle != '' && strpos((string) $haystack, (string) $needle) === 0) {
+            if ($needle != '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
                 return true;
             }
         }
