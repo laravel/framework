ed0296790bf2088775ea2dd6efe921c53b33b317
Joseph Silber <contact@josephsilber.com>
2016-12-14 09:51:14 -0500

    Add operator support to the "contains" method (#16791)

--- src/Illuminate/Database/Eloquent/Collection.php
+++ src/Illuminate/Database/Eloquent/Collection.php
@@ -65,17 +65,14 @@ class Collection extends BaseCollection implements QueueableCollection
      * Determine if a key exists in the collection.
      *
      * @param  mixed  $key
+     * @param  mixed  $operator
      * @param  mixed  $value
      * @return bool
      */
-    public function contains($key, $value = null)
+    public function contains($key, $operator = null, $value = null)
     {
-        if (func_num_args() == 2) {
-            return parent::contains($key, $value);
-        }
-
-        if ($this->useAsCallable($key)) {
-            return parent::contains($key);
+        if (func_num_args() > 1 || $this->useAsCallable($key)) {
+            return parent::contains(...func_get_args());
         }
 
         $key = $key instanceof Model ? $key->getKey() : $key;
