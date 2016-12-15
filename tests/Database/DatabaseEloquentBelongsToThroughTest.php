506a27d5e53dc6e72411fd4a019cea9741dbf551
Peter Scopes <peter.scopes@randomstorm.com>
2016-12-15 08:00:31 +0000

    Removed mocked unit tests

--- tests/Database/DatabaseEloquentBelongsToThroughTest.php
+++ tests/Database/DatabaseEloquentBelongsToThroughTest.php
@@ -118,101 +118,6 @@ class DatabaseEloquentBelongsToThroughTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Unit test relation is properly initialized.
-     */
-    public function testRelationIsProperlyInitialized()
-    {
-        $relation = $this->getRelation();
-        $model = m::mock('Illuminate\Database\Eloquent\Model');
-        $model->shouldReceive('setRelation')->once()->with('foo', null);
-
-        $models = $relation->initRelation([$model], 'foo');
-
-        $this->assertEquals([$model], $models);
-    }
-
-    /**
-     * Unit test eager constraints are properly added.
-     */
-    public function testEagerConstraintsAreProperlyAdded()
-    {
-        $relation = $this->getRelation();
-        $relation->getQuery()->shouldReceive('whereIn')->once()->with('countries.id', [1, 2]);
-        $model1 = new EloquentBelongsToThroughModelStub;
-        $model1->id = 1;
-        $model2 = new EloquentBelongsToThroughModelStub;
-        $model2->id = 2;
-        $relation->addEagerConstraints([$model1, $model2]);
-    }
-
-    /**
-     * Unit test models are properly matched to their parents.
-     */
-    public function testModelsAreProperlyMatchedToParents()
-    {
-        $relation = $this->getRelation();
-
-        // Countries
-        $results1 = new EloquentBelongsToThroughModelStub;
-        $results1->user_id = 1;
-        $results2 = new EloquentBelongsToThroughModelStub;
-        $results2->user_id = 2;
-
-        // Posts
-        $model1 = new EloquentBelongsToThroughModelStub;
-        $model1->user_id = 1;
-        $model2 = new EloquentBelongsToThroughModelStub;
-        $model2->user_id = 2;
-
-        $models = $relation->match([$model1, $model2], new Collection([$results1, $results2]), 'foo');
-
-        $this->assertEquals(1, $models[0]->foo->user_id);
-        $this->assertEquals(2, $models[1]->foo->user_id);
-    }
-
-    /**
-     * Creates a new BelongsToThrough relationship of a Post having a Country through a User.
-     *
-     * @return BelongsToThrough
-     */
-    protected function getRelation()
-    {
-        list($builder, $country, $user, $farParentKey, $parentKey) = $this->getRelationArguments();
-
-        return new BelongsToThrough($builder, $country, $user, $farParentKey, $parentKey);
-    }
-
-    protected function getRelationArguments()
-    {
-        $builder = m::mock('Illuminate\Database\Eloquent\Builder');
-        $builder->shouldReceive('join')->once()->with('users', 'users.post_id', '=', 'posts.id');
-        $builder->shouldReceive('join')->once()->with('countries', 'countries.user_id', '=', 'users.id');
-        $builder->shouldReceive('where')->with('countries.id', '=', 1);
-        $builder->shouldReceive('whereNotNull')->with('countries.id');
-
-        $country = m::mock('Illuminate\Database\Eloquent\Model');
-        $country->shouldReceive('getTable')->andReturn('countries');
-        $country->shouldReceive('getQualifiedKeyName')->andReturn('countries.id');
-        $country->shouldReceive('getKey')->andReturn(1);
-        $country->shouldReceive('getKeyName')->andReturn('id');
-
-        $user = m::mock('Illuminate\Database\Eloquent\Model');
-        $user->shouldReceive('getTable')->andReturn('users');
-        $user->shouldReceive('getQualifiedKeyName')->andReturn('users.id');
-
-        $post = m::mock('Illuminate\Database\Eloquent\Model');
-        $post->shouldReceive('getQualifiedKeyName')->andReturn('posts.id');
-
-        $builder->shouldReceive('getModel')->andReturn($post);
-
-        $user->shouldReceive('getKey')->andReturn(1);
-        $user->shouldReceive('getCreatedAtColumn')->andReturn('created_at');
-        $user->shouldReceive('getUpdatedAtColumn')->andReturn('updated_at');
-
-        return [$builder, $country, $user, 'user_id', 'post_id'];
-    }
-
-    /**
      * Helpers...
      */
 
@@ -251,11 +156,6 @@ class DatabaseEloquentBelongsToThroughTest extends PHPUnit_Framework_TestCase
 
         return $columns;
     }
-}
-
-// Stub for unit tests
-class EloquentBelongsToThroughModelStub extends Eloquent
-{
 }
 
 // Stubs for integration tests
--- tests/Database/DatabaseEloquentHasOneThroughTest.php
+++ tests/Database/DatabaseEloquentHasOneThroughTest.php
@@ -118,97 +118,6 @@ class DatabaseEloquentHasOneThroughTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * Unit test relation is properly initialized.
-     */
-    public function testRelationIsProperlyInitialized()
-    {
-        $relation = $this->getRelation();
-        $model = m::mock('Illuminate\Database\Eloquent\Model');
-        $model->shouldReceive('setRelation')->once()->with('foo', null);
-
-        $models = $relation->initRelation([$model], 'foo');
-
-        $this->assertEquals([$model], $models);
-    }
-
-    /**
-     * Unit test eager constraints are properly added.
-     */
-    public function testEagerConstraintsAreProperlyAdded()
-    {
-        $relation = $this->getRelation();
-        $relation->getQuery()->shouldReceive('whereIn')->once()->with('accounts.supplier_id', [1, 2]);
-        $model1 = new EloquentHasOneThroughModelStub;
-        $model1->id = 1;
-        $model2 = new EloquentHasOneThroughModelStub;
-        $model2->id = 2;
-        $relation->addEagerConstraints([$model1, $model2]);
-    }
-
-    /**
-     * Unit test models are properly matched to parents.
-     */
-    public function testModelsAreProperlyMatchedToParents()
-    {
-        $relation = $this->getRelation();
-
-        // Account Histories
-        $results1 = new EloquentHasOneThroughModelStub;
-        $results1->supplier_id = 1;
-        $results2 = new EloquentHasOneThroughModelStub;
-        $results2->supplier_id = 2;
-
-        // Suppliers
-        $model1 = new EloquentHasOneThroughModelStub;
-        $model1->id = 1;
-        $model2 = new EloquentHasOneThroughModelStub;
-        $model2->id = 2;
-
-        $models = $relation->match([$model1, $model2], new Collection([$results1, $results2]), 'foo');
-
-        $this->assertEquals(1, $models[0]->foo->supplier_id);
-        $this->assertEquals(2, $models[1]->foo->supplier_id);
-    }
-
-    /**
-     * Creates a new HasOneThrough relationship of a Supplier having an Account History through an Account.
-     *
-     * @return HasOneThrough
-     */
-    protected function getRelation()
-    {
-        list($builder, $supplier, $account, $parentForeignKey, $relatedForeignKey) = $this->getRelationArguments();
-
-        return new HasOneThrough($builder, $supplier, $account, $parentForeignKey, $relatedForeignKey);
-    }
-
-    protected function getRelationArguments()
-    {
-        $builder = m::mock('Illuminate\Database\Eloquent\Builder');
-        $builder->shouldReceive('join')->once()->with('accounts', 'accounts.id', '=', 'account_histories.account_id');
-        $builder->shouldReceive('where')->with('accounts.supplier_id', '=', 1);
-        $builder->shouldReceive('whereNotNull')->with('accounts.supplier_id');
-
-        // Suppliers
-        $supplier = m::mock('Illuminate\Database\Eloquent\Model');
-        $supplier->shouldReceive('getKey')->andReturn(1);
-        $supplier->shouldReceive('getKeyName')->andReturn('id');
-
-        // Accounts
-        $account = m::mock('Illuminate\Database\Eloquent\Model');
-        $account->shouldReceive('getTable')->andReturn('accounts');
-        $account->shouldReceive('getQualifiedKeyName')->andReturn('accounts.id');
-
-        // Account Histories
-        $accountHistory = m::mock('Illuminate\Database\Eloquent\Model');
-        $accountHistory->shouldReceive('getTable')->andReturn('account_histories');
-
-        $builder->shouldReceive('getModel')->andReturn($accountHistory);
-
-        return [$builder, $supplier, $account, 'supplier_id', 'account_id'];
-    }
-
-    /**
      * Helpers...
      */
 
@@ -247,11 +156,6 @@ class DatabaseEloquentHasOneThroughTest extends PHPUnit_Framework_TestCase
 
         return $columns;
     }
-}
-
-// Stub for unit tests
-class EloquentHasOneThroughModelStub extends Eloquent
-{
 }
 
 // Stubs for integration tests
