032acd81ba23c1edf62b3df48c74a0ff02ded8d5
Taylor Otwell <taylor@laravel.com>
2016-12-19 07:28:22 -0600

    remove comment

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -1089,8 +1089,6 @@ class Builder
             // the resulting column. This allows multiple counts on the same relationship name.
             $segments = explode(' ', $name);
 
-            // We want to forget the alias in the case the last loop set an alias, but the current
-            // loop does not set a new alias.
             unset($alias);
 
             if (count($segments) == 3 && Str::lower($segments[1]) == 'as') {
