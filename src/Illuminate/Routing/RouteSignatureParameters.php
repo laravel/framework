0f7985c888abb0a1824e87b32ab3d8feaca5fecf
Taylor Otwell <taylor@laravel.com>
2016-12-21 14:39:30 -0600

    Extract signature parameter extraction.

---
+++ src/Illuminate/Routing/RouteSignatureParameters.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Illuminate\Routing;
+
+use ReflectionMethod;
+use ReflectionFunction;
+
+class RouteSignatureParameters
+{
+    /**
+     * Extract the route action's signature parameters.
+     *
+     * @param  array  $action
+     * @param  string  $subClass
+     * @return array
+     */
+    public static function fromAction(array $action, $subClass = null)
+    {
+        $parameters = is_string($action['uses'])
+                        ? static::fromClassMethodString($action['uses'])
+                        : (new ReflectionFunction($action['uses']))->getParameters();
+
+        return is_null($subClass) ? $parameters : array_filter($parameters, function ($p) use ($subClass) {
+            return $p->getClass() && $p->getClass()->isSubclassOf($subClass);
+        });
+    }
+
+    /**
+     * Get the parameters for the given class / method by string.
+     *
+     * @param  string  $uses
+     * @return array
+     */
+    protected static function fromClassMethodString($uses)
+    {
+        list($class, $method) = explode('@', $uses);
+
+        return (new ReflectionMethod($class, $method))->getParameters();
+    }
+}
