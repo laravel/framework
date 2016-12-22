21218c09cbd54f7c086eaab595da77cd77245482
Taylor Otwell <taylor@laravel.com>
2016-12-21 20:38:04 -0600

    tweak check

--- src/Illuminate/Routing/Middleware/ThrottleRequests.php
+++ src/Illuminate/Routing/Middleware/ThrottleRequests.php
@@ -121,10 +121,10 @@ class ThrottleRequests
      */
     protected function calculateRemainingAttempts($key, $maxAttempts, $retryAfter = null)
     {
-        if (! is_null($retryAfter)) {
-            return 0;
+        if (is_null($retryAfter)) {
+            return $this->limiter->retriesLeft($key, $maxAttempts);
         }
 
-        return $this->limiter->retriesLeft($key, $maxAttempts);
+        return 0;
     }
 }
