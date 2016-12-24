35b25e7a9cd10929e853a3c21c898e101328a7a0
Taylor Otwell <taylor@laravel.com>
2016-12-24 13:04:36 -0600

    Simplify setting recipients on mailables. Write tests.

--- tests/Mail/MailMailableTest.php
+++ tests/Mail/MailMailableTest.php
@@ -4,6 +4,36 @@ use Illuminate\Mail\Mailable;
 
 class MailMailableTest extends PHPUnit_Framework_TestCase
 {
+    public function testMailableSetsRecipientsCorrectly()
+    {
+        $mailable = new WelcomeMailableStub;
+        $mailable->to('taylor@laravel.com');
+        $this->assertEquals([['name' => null, 'address' => 'taylor@laravel.com']], $mailable->to);
+
+        $mailable = new WelcomeMailableStub;
+        $mailable->to(['taylor@laravel.com']);
+        $this->assertEquals([['name' => null, 'address' => 'taylor@laravel.com']], $mailable->to);
+
+        $mailable = new WelcomeMailableStub;
+        $mailable->to([['name' => 'Taylor Otwell', 'email' => 'taylor@laravel.com']]);;
+        $this->assertEquals([['name' => 'Taylor Otwell', 'address' => 'taylor@laravel.com']], $mailable->to);
+
+        $mailable = new WelcomeMailableStub;
+        $mailable->to(new MailableTestUserStub);
+        $this->assertEquals([['name' => 'Taylor Otwell', 'address' => 'taylor@laravel.com']], $mailable->to);
+
+        $mailable = new WelcomeMailableStub;
+        $mailable->to(collect([new MailableTestUserStub]));
+        $this->assertEquals([['name' => 'Taylor Otwell', 'address' => 'taylor@laravel.com']], $mailable->to);
+
+        $mailable = new WelcomeMailableStub;
+        $mailable->to(collect([new MailableTestUserStub, new MailableTestUserStub]));
+        $this->assertEquals([
+            ['name' => 'Taylor Otwell', 'address' => 'taylor@laravel.com'],
+            ['name' => 'Taylor Otwell', 'address' => 'taylor@laravel.com']
+        ], $mailable->to);
+    }
+
     public function testMailableBuildsViewData()
     {
         $mailable = new WelcomeMailableStub;
@@ -36,4 +66,10 @@ class WelcomeMailableStub extends Mailable
         $this->with('first_name', 'Taylor')
             ->withLastName('Otwell');
     }
+}
+
+class MailableTestUserStub
+{
+    public $name = 'Taylor Otwell';
+    public $email = 'taylor@laravel.com';
 }
