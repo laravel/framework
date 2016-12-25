9e6d1a0cd8d9f25095bb490ba45c720b963756f9
Taylor Otwell <taylor@laravel.com>
2016-12-25 09:00:03 -0600

    spelling

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -576,7 +576,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
 
         // If the application has already booted, we will call this boot method on
         // the provider class so it has an opportunity to do its boot logic and
-        // will be ready for any usage by the developer's application logic.
+        // will be ready for any usage by this developer's application logic.
         if ($this->booted) {
             $this->bootProvider($provider);
         }
