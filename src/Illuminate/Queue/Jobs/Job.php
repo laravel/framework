52288c0a953d18a8423ef4de78640f6ae6da46be
Taylor Otwell <taylor@laravel.com>
2016-12-29 10:25:58 -0600

    Fix for PHP 5.6.

--- src/Illuminate/Queue/Jobs/Job.php
+++ src/Illuminate/Queue/Jobs/Job.php
@@ -59,7 +59,7 @@ abstract class Job
 
         list($class, $method) = JobName::parse($payload['job']);
 
-        ($this->instance = $this->resolve($class))->{$method}($this, $payload['data']);
+        with($this->instance = $this->resolve($class))->{$method}($this, $payload['data']);
     }
 
     /**
