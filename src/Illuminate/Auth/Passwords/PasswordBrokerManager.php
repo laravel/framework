38f4a25369ee843bc36bb58163c2f48231a0a877
Till Krüss <till@kruss.io>
2016-12-29 21:34:41 -0800

    Crypto-based password resets

--- src/Illuminate/Auth/Passwords/PasswordBrokerManager.php
+++ src/Illuminate/Auth/Passwords/PasswordBrokerManager.php
@@ -2,7 +2,6 @@
 
 namespace Illuminate\Auth\Passwords;
 
-use Illuminate\Support\Str;
 use InvalidArgumentException;
 use Illuminate\Contracts\Auth\PasswordBrokerFactory as FactoryContract;
 
@@ -64,36 +63,9 @@ class PasswordBrokerManager implements FactoryContract
             throw new InvalidArgumentException("Password resetter [{$name}] is not defined.");
         }
 
-        // The password broker uses a token repository to validate tokens and send user
-        // password e-mails, as well as validating that password reset process as an
-        // aggregate service of sorts providing a convenient interface for resets.
         return new PasswordBroker(
-            $this->createTokenRepository($config),
-            $this->app['auth']->createUserProvider($config['provider'])
-        );
-    }
-
-    /**
-     * Create a token repository instance based on the given configuration.
-     *
-     * @param  array  $config
-     * @return \Illuminate\Auth\Passwords\TokenRepositoryInterface
-     */
-    protected function createTokenRepository(array $config)
-    {
-        $key = $this->app['config']['app.key'];
-
-        if (Str::startsWith($key, 'base64:')) {
-            $key = base64_decode(substr($key, 7));
-        }
-
-        $connection = isset($config['connection']) ? $config['connection'] : null;
-
-        return new DatabaseTokenRepository(
-            $this->app['db']->connection($connection),
-            $this->app['hash'],
-            $config['table'],
-            $key,
+            $this->app,
+            $this->app['auth']->createUserProvider($config['provider']),
             $config['expire']
         );
     }
