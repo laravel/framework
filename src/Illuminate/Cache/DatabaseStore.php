58705515a4592f4ae56e31b98f7322310d662089
Taylor Otwell <taylorotwell@gmail.com>
2016-12-31 20:31:48 -0600

    fix spacing

--- src/Illuminate/Cache/DatabaseStore.php
+++ src/Illuminate/Cache/DatabaseStore.php
@@ -166,6 +166,7 @@ class DatabaseStore implements Store
             }
 
             $current = $this->encrypter->decrypt($cache->value);
+
             $new = $callback((int) $current, $value);
 
             if (! is_numeric($current)) {
