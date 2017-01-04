725c77bbf975bb8613e10433bfc08681774ba2f9
Taylor Otwell <taylor@laravel.com>
2017-01-04 13:48:58 -0600

    extract column rename compilation

--- src/Illuminate/Database/Schema/Grammars/Grammar.php
+++ src/Illuminate/Database/Schema/Grammars/Grammar.php
@@ -34,48 +34,7 @@ abstract class Grammar extends BaseGrammar
      */
     public function compileRenameColumn(Blueprint $blueprint, Fluent $command, Connection $connection)
     {
-        $schema = $connection->getDoctrineSchemaManager();
-
-        $table = $this->getTablePrefix().$blueprint->getTable();
-
-        $column = $connection->getDoctrineColumn($table, $command->from);
-
-        $tableDiff = $this->getRenamedDiff($blueprint, $command, $column, $schema);
-
-        return (array) $schema->getDatabasePlatform()->getAlterTableSQL($tableDiff);
-    }
-
-    /**
-     * Get a new column instance with the new column name.
-     *
-     * @param  \Illuminate\Database\Schema\Blueprint  $blueprint
-     * @param  \Illuminate\Support\Fluent  $command
-     * @param  \Doctrine\DBAL\Schema\Column  $column
-     * @param  \Doctrine\DBAL\Schema\AbstractSchemaManager  $schema
-     * @return \Doctrine\DBAL\Schema\TableDiff
-     */
-    protected function getRenamedDiff(Blueprint $blueprint, Fluent $command, Column $column, SchemaManager $schema)
-    {
-        $tableDiff = $this->getDoctrineTableDiff($blueprint, $schema);
-
-        return $this->setRenamedColumns($tableDiff, $command, $column);
-    }
-
-    /**
-     * Set the renamed columns on the table diff.
-     *
-     * @param  \Doctrine\DBAL\Schema\TableDiff  $tableDiff
-     * @param  \Illuminate\Support\Fluent  $command
-     * @param  \Doctrine\DBAL\Schema\Column  $column
-     * @return \Doctrine\DBAL\Schema\TableDiff
-     */
-    protected function setRenamedColumns(TableDiff $tableDiff, Fluent $command, Column $column)
-    {
-        $newColumn = new Column($command->to, $column->getType(), $column->toArray());
-
-        $tableDiff->renamedColumns = [$command->from => $newColumn];
-
-        return $tableDiff;
+        return RenameColumn::compile($this, $blueprint, $command, $connection);
     }
 
     /**
@@ -263,7 +222,7 @@ abstract class Grammar extends BaseGrammar
      * @param  \Doctrine\DBAL\Schema\AbstractSchemaManager  $schema
      * @return \Doctrine\DBAL\Schema\TableDiff
      */
-    protected function getDoctrineTableDiff(Blueprint $blueprint, SchemaManager $schema)
+    public function getDoctrineTableDiff(Blueprint $blueprint, SchemaManager $schema)
     {
         $table = $this->getTablePrefix().$blueprint->getTable();
 
