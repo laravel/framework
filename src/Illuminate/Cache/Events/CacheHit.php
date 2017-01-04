54b8ab6712afed91a3b5332675ee9d3ef8200578
Lucas Michot <lucas@semalead.com>
2017-01-04 08:27:43 +0100

    Factorize cache events.

--- src/Illuminate/Cache/Events/CacheHit.php
+++ src/Illuminate/Cache/Events/CacheHit.php
@@ -2,28 +2,14 @@
 
 namespace Illuminate\Cache\Events;
 
-class CacheHit extends CacheEvent
+class CacheHit extends AbstractCacheEvent
 {
-    /**
-     * The key that was hit.
-     *
-     * @var string
-     */
-    public $key;
-
     /**
      * The value that was retrieved.
      *
      * @var mixed
      */
     public $value;
-
-    /**
-     * The tags that were assigned to the key.
-     *
-     * @var array
-     */
-    public $tags;
 
     /**
      * Create a new event instance.
@@ -35,8 +21,7 @@ class CacheHit extends CacheEvent
      */
     public function __construct($key, $value, array $tags = [])
     {
-        $this->key = $key;
-        $this->tags = $tags;
+        parent::__construct($key, $tags);
         $this->value = $value;
     }
 }
--- src/Illuminate/Cache/Events/CacheMissed.php
+++ src/Illuminate/Cache/Events/CacheMissed.php
@@ -2,32 +2,7 @@
 
 namespace Illuminate\Cache\Events;
 
-class CacheMissed extends CacheEvent
+class CacheMissed extends AbstractCacheEvent
 {
-    /**
-     * The key that was missed.
-     *
-     * @var string
-     */
-    public $key;
-
-    /**
-     * The tags that were assigned to the key.
-     *
-     * @var array
-     */
-    public $tags;
-
-    /**
-     * Create a new event instance.
-     *
-     * @param  string  $key
-     * @param  array  $tags
-     * @return void
-     */
-    public function __construct($key, array $tags = [])
-    {
-        $this->key = $key;
-        $this->tags = $tags;
-    }
+    //
 }
