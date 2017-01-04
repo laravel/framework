f35b048f7cb091ac6c0628ecf0ab65cfc8c37db8
Alex Bowers <bowersbros@gmail.com>
2017-01-04 13:54:36 +0000

    [5.4] Extract blade tests verbatim (#17115)
    * Extract blade tests verbatim
    
    * Styling Fix
    
    * Styling Fix

---
+++ tests/Blade/BladeExpressionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Illuminate\View\Compilers\BladeCompiler;
+
+class BladeExpressionTest extends TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testExpressionsOnTheSameLine()
+    {
+        $compiler = new BladeCompiler($this->getFiles(), __DIR__);
+        $this->assertEquals('<?php echo app(\'translator\')->get(foo(bar(baz(qux(breeze()))))); ?> space () <?php echo app(\'translator\')->get(foo(bar)); ?>', $compiler->compileString('@lang(foo(bar(baz(qux(breeze()))))) space () @lang(foo(bar))'));
+    }
+
+    public function testExpressionWithinHTML()
+    {
+        $compiler = new BladeCompiler($this->getFiles(), __DIR__);
+        $this->assertEquals('<html <?php echo e($foo); ?>>', $compiler->compileString('<html {{ $foo }}>'));
+        $this->assertEquals('<html<?php echo e($foo); ?>>', $compiler->compileString('<html{{ $foo }}>'));
+        $this->assertEquals('<html <?php echo e($foo); ?> <?php echo app(\'translator\')->get(\'foo\'); ?>>', $compiler->compileString('<html {{ $foo }} @lang(\'foo\')>'));
+    }
+
+    protected function getFiles()
+    {
+        return m::mock('Illuminate\Filesystem\Filesystem');
+    }
+}
