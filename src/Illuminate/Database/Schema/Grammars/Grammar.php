b7ecece0da1b55ec2e3e9080553c7851ae95d1d9
Taylor Otwell <taylor@laravel.com>
2017-01-04 14:47:32 -0600

    Apply fixes from StyleCI (#17141)

--- src/Illuminate/Database/Schema/Grammars/Grammar.php
+++ src/Illuminate/Database/Schema/Grammars/Grammar.php
@@ -62,7 +62,7 @@ abstract class Grammar extends BaseGrammar
         // We need to prepare several of the elements of the foreign key definition
         // before we can create the SQL, such as wrapping the tables and convert
         // an array of columns to comma-delimited strings for the SQL queries.
-        $sql = sprintf("alter table %s add constraint %s ",
+        $sql = sprintf('alter table %s add constraint %s ',
             $this->wrapTable($blueprint),
             $this->wrap($command->index)
         );
@@ -70,7 +70,7 @@ abstract class Grammar extends BaseGrammar
         // Once we have the initial portion of the SQL statement we will add on the
         // key name, table name, and referenced columns. These will complete the
         // main portion of the SQL statement and this SQL will almost be done.
-        $sql .= sprintf("foreign key (%s) references %s (%s)",
+        $sql .= sprintf('foreign key (%s) references %s (%s)',
             $this->columnize($command->columns),
             $this->wrapTable($command->on),
             $this->columnize((array) $command->references)
