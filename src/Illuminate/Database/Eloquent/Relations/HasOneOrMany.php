332ea7c61f3aebcd2376e9ca4c6b5a5e873cead1
Taylor Otwell <taylor@laravel.com>
2017-01-06 15:25:44 -0600

    Apply fixes from StyleCI (#17185)

--- src/Illuminate/Database/Eloquent/Relations/HasOneOrMany.php
+++ src/Illuminate/Database/Eloquent/Relations/HasOneOrMany.php
@@ -4,7 +4,6 @@ namespace Illuminate\Database\Eloquent\Relations;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Eloquent\Collection;
 
 abstract class HasOneOrMany extends Relation
