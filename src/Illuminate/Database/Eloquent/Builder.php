a8a171128a889a33ddd8c8c8fafab1930de00255
Taylor Otwell <taylor@laravel.com>
2017-01-06 10:04:27 -0600

    Adjust method location.

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -995,33 +995,6 @@ class Builder
     }
 
     /**
-     * Apply the given scope on the current builder instance.
-     *
-     * @param  callable $scope
-     * @param  array $parameters
-     * @return mixed
-     */
-    protected function callScope(callable $scope, $parameters = [])
-    {
-        array_unshift($parameters, $this);
-
-        $query = $this->getQuery();
-
-        // We will keep track of how many wheres are on the query before running the
-        // scope so that we can properly group the added scope constraints in the
-        // query as their own isolated nested where statement and avoid issues.
-        $originalWhereCount = count($query->wheres);
-
-        $result = $scope(...array_values($parameters)) ?: $this;
-
-        if ($this->shouldNestWheresForScope($query, $originalWhereCount)) {
-            $this->nestWheresForScope($query, $originalWhereCount);
-        }
-
-        return $result;
-    }
-
-    /**
      * Apply the scopes to the Eloquent builder instance and return it.
      *
      * @return \Illuminate\Database\Eloquent\Builder|static
@@ -1048,15 +1021,30 @@ class Builder
     }
 
     /**
-     * Determine if the scope added after the given offset should be nested.
+     * Apply the given scope on the current builder instance.
      *
-     * @param  \Illuminate\Database\Query\Builder  $query
-     * @param  int  $originalWhereCount
-     * @return bool
+     * @param  callable $scope
+     * @param  array $parameters
+     * @return mixed
      */
-    protected function shouldNestWheresForScope(QueryBuilder $query, $originalWhereCount)
+    protected function callScope(callable $scope, $parameters = [])
     {
-        return count($query->wheres) > $originalWhereCount;
+        array_unshift($parameters, $this);
+
+        $query = $this->getQuery();
+
+        // We will keep track of how many wheres are on the query before running the
+        // scope so that we can properly group the added scope constraints in the
+        // query as their own isolated nested where statement and avoid issues.
+        $originalWhereCount = count($query->wheres);
+
+        $result = $scope(...array_values($parameters)) ?: $this;
+
+        if (count($query->wheres) > $originalWhereCount) {
+            $this->nestWheresForScope($query, $originalWhereCount);
+        }
+
+        return $result;
     }
 
     /**
