f352597897dc5221341966bd6f70b0b3bbb6be31
Till Krüss <till@kruss.io>
2017-01-07 20:56:33 -0800

    Normalize PhpRedis GET results

--- src/Illuminate/Redis/Connections/PhpRedisConnection.php
+++ src/Illuminate/Redis/Connections/PhpRedisConnection.php
@@ -18,6 +18,32 @@ class PhpRedisConnection extends Connection
     }
 
     /**
+     * Returns the value of the given key.
+     *
+     * @param  string  $key
+     * @return string|null
+     */
+    public function get($key)
+    {
+        $result = $this->client->get($key);
+
+        return $result !== false ? $result : null;
+    }
+
+    /**
+     * Get the values of all the given keys.
+     *
+     * @param  array  $keys
+     * @return array
+     */
+    public function mget(array $keys)
+    {
+        return array_map(function ($value) {
+            return $value !== false ? $value : null;
+        }, $this->client->mget($keys));
+    }
+
+    /**
      * Set the string value in argument as value of the key.
      *
      * @param string  $key
