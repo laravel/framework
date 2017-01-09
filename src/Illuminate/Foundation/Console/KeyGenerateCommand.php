58e4b1dcc07ff9d976b367ed00bf39904394c8a7
Till Krüss <till@kruss.io>
2017-01-09 13:57:57 -0800

    Make #17151 easier to read (#17226)

--- src/Illuminate/Foundation/Console/KeyGenerateCommand.php
+++ src/Illuminate/Foundation/Console/KeyGenerateCommand.php
@@ -53,7 +53,7 @@ class KeyGenerateCommand extends Command
     {
         file_put_contents($this->laravel->environmentFilePath(), preg_replace(
             $this->keyReplacementPattern(),
-            'APP_KEY='.$key,
+            "APP_KEY={$key}",
             file_get_contents($this->laravel->environmentFilePath())
         ));
     }
@@ -77,8 +77,8 @@ class KeyGenerateCommand extends Command
      */
     protected function keyReplacementPattern()
     {
-        $escaped = preg_quote('='.$this->laravel['config']['app.key'], '/');
+        $escaped = preg_quote($this->laravel['config']['app.key'], '/');
 
-        return "/^APP_KEY{$escaped}/m";
+        return "/^APP_KEY={$escaped}/m";
     }
 }
