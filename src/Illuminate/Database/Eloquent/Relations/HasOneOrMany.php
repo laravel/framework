6eea2827ec2b16e59eb87239bd83ba2e8c974df3
Lucas Michot <lucasmichot@users.noreply.github.com>
2017-01-09 16:21:58 +0100

    Remove useless use (#17223)

--- src/Illuminate/Database/Eloquent/Relations/HasOneOrMany.php
+++ src/Illuminate/Database/Eloquent/Relations/HasOneOrMany.php
@@ -268,7 +268,7 @@ abstract class HasOneOrMany extends Relation
         // Here we will set the raw attributes to avoid hitting the "fill" method so
         // that we do not have to worry about a mass accessor rules blocking sets
         // on the models. Otherwise, some of these attributes will not get set.
-        return tap($this->related->newInstance($attributes), function ($instance) use ($attributes) {
+        return tap($this->related->newInstance($attributes), function ($instance) {
             $instance->setAttribute($this->getForeignKeyName(), $this->getParentKey());
 
             $instance->save();
