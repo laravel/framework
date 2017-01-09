8b79055731a412c7da93875c019c403076834808
Taylor Otwell <taylor@laravel.com>
2017-01-09 17:03:15 -0600

    Work on base migration command.

--- src/Illuminate/Database/Console/Migrations/BaseCommand.php
+++ src/Illuminate/Database/Console/Migrations/BaseCommand.php
@@ -7,25 +7,15 @@ use Illuminate\Console\Command;
 class BaseCommand extends Command
 {
     /**
-     * Get the path to the migration directory.
-     *
-     * @return string
-     */
-    protected function getMigrationPath()
-    {
-        return $this->laravel->databasePath().DIRECTORY_SEPARATOR.'migrations';
-    }
-
-    /**
      * Get all of the migration paths.
      *
      * @return array
      */
     protected function getMigrationPaths()
     {
-        // Here, we will check to see if a path option has been defined. If it has
-        // we will use the path relative to the root of this installation folder
-        // so that migrations may be run for any path within the applications.
+        // Here, we will check to see if a path option has been defined. If it has we will
+        // use the path relative to the root of the installation folder so our database
+        // migrations may be run for any customized path from within the application.
         if ($this->input->hasOption('path') && $this->option('path')) {
             return [$this->laravel->basePath().'/'.$this->option('path')];
         }
@@ -33,5 +23,15 @@ class BaseCommand extends Command
         return array_merge(
             [$this->getMigrationPath()], $this->migrator->paths()
         );
+    }
+
+    /**
+     * Get the path to the migration directory.
+     *
+     * @return string
+     */
+    protected function getMigrationPath()
+    {
+        return $this->laravel->databasePath().DIRECTORY_SEPARATOR.'migrations';
     }
 }
