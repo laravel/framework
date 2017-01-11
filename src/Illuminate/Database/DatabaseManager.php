ce6e0cc6f14ae10df5c8a98be8373c05dc78ae3c
trey <chengnan@wifi.com>
2017-01-11 17:57:27 +0800

    fix bug

--- src/Illuminate/Database/DatabaseManager.php
+++ src/Illuminate/Database/DatabaseManager.php
@@ -59,13 +59,13 @@ class DatabaseManager implements ConnectionResolverInterface
      */
     public function connection($name = null)
     {
-        list($name, $type) = $this->parseConnectionName($name);
+        list($database, $type) = $this->parseConnectionName($name);
 
         // If we haven't created this connection, we'll create it based on the config
         // provided in the application. Once we've created the connections we will
         // set the "fetch mode" for PDO which determines the query return types.
         if (! isset($this->connections[$name])) {
-            $connection = $this->makeConnection($name);
+            $connection = $this->makeConnection($database);
 
             $this->setPdoForType($connection, $type);
 
