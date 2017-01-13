8e04ae4e6ae8ced8913af2c4bc7ceaa85f909d57
Jonathan Reinink <jonathan@reinink.ca>
2017-01-13 09:39:50 -0500

    Show seed output prior to running, instead of after.

--- src/Illuminate/Database/Seeder.php
+++ src/Illuminate/Database/Seeder.php
@@ -36,11 +36,11 @@ abstract class Seeder
      */
     public function call($class)
     {
-        $this->resolve($class)->run();
-
         if (isset($this->command)) {
-            $this->command->getOutput()->writeln("<info>Seeded:</info> $class");
+            $this->command->getOutput()->writeln("<info>Seeding:</info> $class");
         }
+
+        $this->resolve($class)->run();
     }
 
     /**
