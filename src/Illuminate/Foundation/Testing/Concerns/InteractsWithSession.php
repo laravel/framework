bdc1f66cd58981dd3e2ea8c21c39a619d44df7ef
Taylor Otwell <taylor@laravel.com>
2017-01-16 15:58:18 -0600

    Add interactsWithSession.

---
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithSession.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Illuminate\Foundation\Testing\Concerns;
+
+use PHPUnit_Framework_Assert as PHPUnit;
+
+trait InteractsWithSession
+{
+    /**
+     * Set the session to the given array.
+     *
+     * @param  array  $data
+     * @return $this
+     */
+    public function withSession(array $data)
+    {
+        $this->session($data);
+
+        return $this;
+    }
+
+    /**
+     * Set the session to the given array.
+     *
+     * @param  array  $data
+     * @return $this
+     */
+    public function session(array $data)
+    {
+        $this->startSession();
+
+        foreach ($data as $key => $value) {
+            $this->app['session']->put($key, $value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Start the session for the application.
+     *
+     * @return $this
+     */
+    protected function startSession()
+    {
+        if (! $this->app['session']->isStarted()) {
+            $this->app['session']->start();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Flush all of the current session data.
+     *
+     * @return $this
+     */
+    public function flushSession()
+    {
+        $this->startSession();
+
+        $this->app['session']->flush();
+
+        return $this;
+    }
+}
