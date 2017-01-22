e5ff11cb401fb83fe3b14de2c292e5501c6a4264
Vincent Klaiber <vinkla@users.noreply.github.com>
2017-01-22 17:29:16 +0100

    [5.4] Use namespaced phpunit (#17470)
    * [5.4] Use namespaced phpunit
    
    * Fix coding standard

--- src/Illuminate/Foundation/Testing/TestCase.php
+++ src/Illuminate/Foundation/Testing/TestCase.php
@@ -3,12 +3,12 @@
 namespace Illuminate\Foundation\Testing;
 
 use Mockery;
-use PHPUnit_Framework_TestCase;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Console\Application as Artisan;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends PHPUnit_Framework_TestCase
+abstract class TestCase extends BaseTestCase
 {
     use Concerns\InteractsWithContainer,
         Concerns\MakesHttpRequests,
