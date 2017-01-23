0e94a4d36f3d022138b01ab284fb007a12adb71d
Taylor Otwell <taylor@laravel.com>
2017-01-23 15:41:17 -0600

    tweak extension points

--- src/Illuminate/Queue/RedisQueue.php
+++ src/Illuminate/Queue/RedisQueue.php
@@ -39,13 +39,6 @@ class RedisQueue extends Queue implements QueueContract
     protected $retryAfter = 60;
 
     /**
-     * The last delayed payload sent by the queue.
-     *
-     * @var string
-     */
-    protected $lastDelayedPayload;
-
-    /**
      * Create a new Redis queue instance.
      *
      * @param  \Illuminate\Contracts\Redis\Factory  $redis
@@ -80,7 +73,7 @@ class RedisQueue extends Queue implements QueueContract
     /**
      * Push a new job onto the queue.
      *
-     * @param  string  $job
+     * @param  object|string  $job
      * @param  mixed   $data
      * @param  string  $queue
      * @return mixed
@@ -109,15 +102,26 @@ class RedisQueue extends Queue implements QueueContract
      * Push a new job onto the queue after a delay.
      *
      * @param  \DateTime|int  $delay
-     * @param  string  $job
+     * @param  object|string  $job
      * @param  mixed   $data
      * @param  string  $queue
      * @return mixed
      */
     public function later($delay, $job, $data = '', $queue = null)
     {
-        $this->lastDelayedPayload = $payload = $this->createPayload($job, $data);
+        return $this->laterRaw($delay, $this->createPayload($job, $data), $queue);
+    }
 
+    /**
+     * Push a raw job onto the queue after a delay.
+     *
+     * @param  \DateTime|int  $delay
+     * @param  string  $payload
+     * @param  string  $queue
+     * @return mixed
+     */
+    protected function laterRaw($delay, $payload, $queue = null)
+    {
         $this->getConnection()->zadd(
             $this->getQueue($queue).':delayed', $this->availableAt($delay), $payload
         );
