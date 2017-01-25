f0d4b07e0b31112e8f7ee67b0d5ca664adb96df0
Eric Chow <yateric@gmail.com>
2017-01-25 21:16:00 +0800

    fix bug with delete reset token (#17524)

--- tests/Auth/AuthDatabaseTokenRepositoryTest.php
+++ tests/Auth/AuthDatabaseTokenRepositoryTest.php
@@ -88,10 +88,12 @@ class AuthDatabaseTokenRepositoryTest extends TestCase
     {
         $repo = $this->getRepo();
         $repo->getConnection()->shouldReceive('table')->once()->with('table')->andReturn($query = m::mock('StdClass'));
-        $query->shouldReceive('where')->once()->with('token', 'token')->andReturn($query);
+        $query->shouldReceive('where')->once()->with('email', 'email')->andReturn($query);
         $query->shouldReceive('delete')->once();
+        $user = m::mock('Illuminate\Contracts\Auth\CanResetPassword');
+        $user->shouldReceive('getEmailForPasswordReset')->andReturn('email');
 
-        $repo->delete('token');
+        $repo->delete($user);
     }
 
     public function testDeleteExpiredMethodDeletesExpiredTokens()
