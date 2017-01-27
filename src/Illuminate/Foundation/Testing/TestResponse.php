d279ea7955a867c1a944caa2ec9808c963b7e6a8
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-01-27 21:22:48 +0200

    Add method to dump the content from the response (#17600)

--- src/Illuminate/Foundation/Testing/TestResponse.php
+++ src/Illuminate/Foundation/Testing/TestResponse.php
@@ -382,4 +382,22 @@ class TestResponse extends Response
     {
         return app('session.store');
     }
+
+    /**
+     * Dump the content from the response.
+     *
+     * @return void
+     */
+    public function dump()
+    {
+        $content = $this->getContent();
+
+        $json = json_decode($content);
+
+        if (json_last_error() === JSON_ERROR_NONE) {
+            $content = $json;
+        }
+
+        dd($content);
+    }
 }
