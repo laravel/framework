c60945a44db56567d4cece7a6099d14fd780f5f8
Michael Tintiuc <michaeltintiuc@users.noreply.github.com>
2017-01-27 22:03:36 +0300

    Update CompilesLoops.php

--- src/Illuminate/View/Compilers/Concerns/CompilesLoops.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesLoops.php
@@ -98,7 +98,7 @@ trait CompilesLoops
     {
         if ($expression) {
             preg_match('/\(\s*(\d+)\s*\)$/', $expression, $matches);
-            
+
             return $matches ? '<?php break '.max(1, $matches[1]).'; ?>' : "<?php if{$expression} break; ?>";
         }
 
@@ -115,7 +115,7 @@ trait CompilesLoops
     {
         if ($expression) {
             preg_match('/\(\s*(\d+)\s*\)$/', $expression, $matches);
-            
+
             return $matches ? '<?php continue '.max(1, $matches[1]).'; ?>' : "<?php if{$expression} continue; ?>";
         }
 
