646656e1afee546aeaa1b8178fd567e738913a2a
Benjamin R. Matthews <benjamin@benjaminrmatthews.com>
2017-01-27 09:55:32 -0500

    Adds Thomas Edison quote to list of inspirations

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -27,6 +27,7 @@ class Inspiring
             'He who is contented is rich. - Laozi',
             'Very little is needed to make a happy life. - Marcus Antoninus',
             'It is quality rather than quantity that matters. - Lucius Annaeus Seneca',
+            'Genius is one percent inspiration and ninety-nine percent perspiration. - Thomas Edison',
 
         ])->random();
     }
