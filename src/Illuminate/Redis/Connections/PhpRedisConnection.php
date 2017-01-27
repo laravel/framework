bc4742d10a144d0c319fe1d932cae3f1d2b2fefd
Till Krüss <till@kruss.io>
2017-01-27 05:52:26 -0800

    [5.4] PhpRedis method compatibility (#17488)
    * Added LREM, SPOP, ZADD methods
    
    * Unpack array for various methods
    
    * Added SET method
    
    * Formatting
    
    * Unpack `*scan`
    
    * Added EVALSHA

--- src/Illuminate/Redis/Connections/PhpRedisConnection.php
+++ src/Illuminate/Redis/Connections/PhpRedisConnection.php
@@ -61,6 +61,83 @@ class PhpRedisConnection extends Connection
     }
 
     /**
+     * Set the string value in argument as value of the key.
+     *
+     * @param string  $key
+     * @param mixed  $value
+     * @param string|null  $expireResolution
+     * @param int|null  $expireTTL
+     * @param string|null  $flag
+     * @return bool
+     */
+    public function set($key, $value, $expireResolution = null, $expireTTL = null, $flag = null)
+    {
+        return $this->command(
+            'set',
+            $key,
+            $value,
+            $expireResolution ? [$expireResolution, $flag => $expireTTL] : null
+        );
+    }
+
+    /**
+     * Removes the first count occurences of the value element from the list.
+     *
+     * @param  string  $key
+     * @param  int  $count
+     * @param  $value  $value
+     * @return int|false
+     */
+    public function lrem($key, $count, $value)
+    {
+        return $this->command('lrem', $key, $value, $count);
+    }
+
+    /**
+     * Removes and returns a random element from the set value at key.
+     *
+     * @param  string  $key
+     * @param  int|null  $count
+     * @return mixed|false
+     */
+    public function spop($key, $count = null)
+    {
+        return $this->command('spop', $key, $count);
+    }
+
+    /**
+     * Add one or more members to a sorted set or update its score if it already exists.
+     *
+     * @param  string  $key
+     * @param  array  $membersAndScoresDictionary
+     * @return int
+     */
+    public function zadd($key, array $membersAndScoresDictionary)
+    {
+        $arguments = [];
+
+        foreach ($membersAndScoresDictionary as $member => $score) {
+            $arguments[] = $score;
+            $arguments[] = $member;
+        }
+
+        return $this->command('zadd', ...$arguments);
+    }
+
+    /**
+     * Evaluate a LUA script serverside, from the SHA1 hash of the script instead of the script itself.
+     *
+     * @param  string  $script
+     * @param  int  $numkeys
+     * @param  mixed  $arguments
+     * @return mixed
+     */
+    public function evalsha($script, $numkeys, ...$arguments)
+    {
+        return $this->command('evalsha', [$this->script('load', $script), $arguments, $parameters]);
+    }
+
+    /**
      * Subscribe to a set of given channels for messages.
      *
      * @param  array|string  $channels
@@ -84,6 +161,17 @@ class PhpRedisConnection extends Connection
     {
         if ($method == 'eval') {
             return $this->proxyToEval($parameters);
+        }
+
+        $arrayMethods = [
+            'hdel', 'hstrlen',
+            'lpush', 'rpush',
+            'scan', 'hscan', 'sscan', 'zscan',
+            'sadd', 'srem', 'sdiff', 'sinter', 'sunion', 'sdiffstore', 'sinterstore', 'sunionstore',
+        ];
+
+        if (is_array($parameters) && in_array($method, $arrayMethods)) {
+            $this->command($method, ...$parameters);
         }
 
         return parent::__call($method, $parameters);
