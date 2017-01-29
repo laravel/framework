d53541c5816c0f9a67c252350dc83695ae304e09
balping <balping314@gmail.com>
2017-01-29 21:56:58 +0000

    Update pluck documentation to accept array (#17649)
    * Update docblock of Collection::pluck to accept array
    
    Since Arr::pluck accepts it as well: https://github.com/laravel/framework/blob/01be2355d40bcacaaf06ae6b21d1d687d0d07dd2/src/Illuminate/Support/Arr.php#L344
    
    * Add test where pluck is called with an array
    
    Just to enhance the documentation function of tests.

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -614,7 +614,7 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
     /**
      * Get the values of a given key.
      *
-     * @param  string  $value
+     * @param  string|array  $value
      * @param  string|null  $key
      * @return static
      */
