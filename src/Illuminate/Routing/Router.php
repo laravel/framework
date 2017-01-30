6f68e4a39e600ae36e2018c811f279113abb159d
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-01-30 19:44:48 +0100

    Use a Route group for auth routes

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -994,19 +994,21 @@ class Router implements RegistrarContract, BindingRegistrar
     public function auth()
     {
         // Authentication Routes...
-        $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-        $this->post('login', 'Auth\LoginController@login');
-        $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+        $this->group(['namespace' => 'Auth'], function () {
+        $this->get('login', 'LoginController@showLoginForm')->name('login');
+        $this->post('login', 'LoginController@login');
+        $this->post('logout', 'LoginController@logout')->name('logout');
 
         // Registration Routes...
-        $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-        $this->post('register', 'Auth\RegisterController@register');
+        $this->get('register', 'RegisterController@showRegistrationForm')->name('register');
+        $this->post('register', 'RegisterController@register');
 
         // Password Reset Routes...
-        $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-        $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-        $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-        $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+        $this->get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        $this->post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
+        $this->get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+        $this->post('password/reset', 'ResetPasswordController@reset');
+        });
     }
 
     /**
