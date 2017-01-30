7a3798b8183d39c7fef6cd258d9aea1d8866d86f
vlakoff <vlakoff@gmail.com>
2017-01-30 03:55:36 +0100

    Low-hanging optimizations in Eloquent models

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -358,7 +358,7 @@ trait HasAttributes
      */
     protected function getAttributeFromArray($key)
     {
-        if (array_key_exists($key, $this->attributes)) {
+        if (isset($this->attributes[$key])) {
             return $this->attributes[$key];
         }
     }
@@ -779,7 +779,7 @@ trait HasAttributes
      */
     public function hasCast($key, $types = null)
     {
-        if (array_key_exists($key, $this->getCasts())) {
+        if (isset($this->getCasts()[$key])) {
             return $types ? in_array($this->getCastType($key), (array) $types, true) : true;
         }
 
