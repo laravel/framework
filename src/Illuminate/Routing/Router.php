44fe5e08230ed3e36ee0bc31c95f50255e49fd00
kslimani <karim@playmedia.fr>
2017-02-01 17:15:08 +0100

    [5.4] Use route() helper instead of url() in Auth (#17718)
    * Add route name to password reset request form submit.
    
    * Use route names in Auth instead of url function.

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -1004,7 +1004,7 @@ class Router implements RegistrarContract, BindingRegistrar
 
         // Password Reset Routes...
         $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-        $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+        $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
         $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
         $this->post('password/reset', 'Auth\ResetPasswordController@reset');
     }
