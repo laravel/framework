35db1eb5593917c44f27cad0a6bd235e3569aa07
Mohamed Said <themohamedsaid@gmail.com>
2017-02-01 15:52:33 +0200

    added force option to listener

--- src/Illuminate/Queue/Console/ListenCommand.php
+++ src/Illuminate/Queue/Console/ListenCommand.php
@@ -17,6 +17,7 @@ class ListenCommand extends Command
                             {connection? : The name of connection}
                             {--queue= : The queue to listen on}
                             {--delay=0 : Amount of time to delay failed jobs}
+                            {--force : Force the worker to run even in maintenance mode}
                             {--memory=128 : The memory limit in megabytes}
                             {--sleep=3 : Number of seconds to sleep when no job is available}
                             {--timeout=60 : The number of seconds a child process can run}
