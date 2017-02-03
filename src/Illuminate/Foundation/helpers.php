029ddc750a1e170f17402edfb0d1a1b91be23e4f
Marco Hillger <marco@seofood.de>
2017-02-04 00:13:52 +0100

    fixed cs

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -562,32 +562,32 @@ if (! function_exists('mix')) {
     {
         static $manifest;
 
-        if ( $manifestDir && ! starts_with($manifestDir, '/')) {
+        if ($manifestDir && ! starts_with($manifestDir, '/')) {
             $manifestDir = "/{$manifestDir}";
         }
 
-        if ( ! $manifest) {
-            if ( ! file_exists($manifestPath = public_path($manifestDir . '/mix-manifest.json'))) {
+        if (! $manifest) {
+            if (! file_exists($manifestPath = public_path($manifestDir.'/mix-manifest.json'))) {
                 throw new Exception('The Mix manifest does not exist.');
             }
 
             $manifest = json_decode(file_get_contents($manifestPath), true);
         }
 
-        if ( ! starts_with($path, '/')) {
+        if (! starts_with($path, '/')) {
             $path = "/{$path}";
         }
 
-        if ( ! array_key_exists($path, $manifest)) {
+        if (! array_key_exists($path, $manifest)) {
             throw new Exception(
-                "Unable to locate Mix file: {$path}. Please check your " .
+                "Unable to locate Mix file: {$path}. Please check your ".
                 'webpack.mix.js output paths and try again.'
             );
         }
 
-        return file_exists(public_path($manifestDir . '/hot'))
+        return file_exists(public_path($manifestDir.'/hot'))
             ? new HtmlString("http://localhost:8080{$manifest[$path]}")
-            : new HtmlString($manifestDir . $manifest[$path]);
+            : new HtmlString($manifestDir.$manifest[$path]);
     }
 }
 
