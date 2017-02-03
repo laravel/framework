aeab9029455c8f90ce207485bb3ebb092546b930
Marco Hillger <marco@seofood.de>
2017-02-03 18:42:37 +0100

    allow multiple manifest files for mix helper

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -552,38 +552,42 @@ if (! function_exists('mix')) {
     /**
      * Get the path to a versioned Mix file.
      *
-     * @param  string  $path
+     * @param string $path
+     * @param string $manifestDir
      * @return \Illuminate\Support\HtmlString
      *
      * @throws \Exception
      */
-    function mix($path)
+    function mix($path, $manifestDir = '')
     {
         static $manifest;
-        static $shouldHotReload;
 
-        if (! $manifest) {
-            if (! file_exists($manifestPath = public_path('mix-manifest.json'))) {
+        if ( $manifestDir && ! starts_with($manifestDir, '/')) {
+            $manifestDir = "/{$manifestDir}";
+        }
+
+        if ( ! $manifest) {
+            if ( ! file_exists($manifestPath = public_path($manifestDir . '/mix-manifest.json'))) {
                 throw new Exception('The Mix manifest does not exist.');
             }
 
             $manifest = json_decode(file_get_contents($manifestPath), true);
         }
 
-        if (! starts_with($path, '/')) {
+        if ( ! starts_with($path, '/')) {
             $path = "/{$path}";
         }
 
-        if (! array_key_exists($path, $manifest)) {
+        if ( ! array_key_exists($path, $manifest)) {
             throw new Exception(
-                "Unable to locate Mix file: {$path}. Please check your ".
+                "Unable to locate Mix file: {$path}. Please check your " .
                 'webpack.mix.js output paths and try again.'
             );
         }
 
-        return $shouldHotReload = file_exists(public_path('hot'))
-                    ? new HtmlString("http://localhost:8080{$manifest[$path]}")
-                    : new HtmlString($manifest[$path]);
+        return file_exists(public_path($manifestDir . '/hot'))
+            ? new HtmlString("http://localhost:8080{$manifest[$path]}")
+            : new HtmlString($manifestDir . $manifest[$path]);
     }
 }
 
