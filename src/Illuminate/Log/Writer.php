2c02e78b89203bd14b817b6feefcce1ab2d6bf23
Fernando Henrique Bandeira <fernando.bandeira@outlook.com>
2017-02-06 11:52:52 -0200

    Adding facility to Log:writer Syslog (#17789)

--- src/Illuminate/Log/Writer.php
+++ src/Illuminate/Log/Writer.php
@@ -239,11 +239,12 @@ class Writer implements LogContract, PsrLoggerInterface
      *
      * @param  string  $name
      * @param  string  $level
+     * @param  mixed  $facility
      * @return \Psr\Log\LoggerInterface
      */
-    public function useSyslog($name = 'laravel', $level = 'debug')
+    public function useSyslog($name = 'laravel', $level = 'debug', $facility = LOG_USER)
     {
-        return $this->monolog->pushHandler(new SyslogHandler($name, LOG_USER, $level));
+        return $this->monolog->pushHandler(new SyslogHandler($name, $facility, $level));
     }
 
     /**
