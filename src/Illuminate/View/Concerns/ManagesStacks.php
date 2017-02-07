67379c475510946c6d60b600c8915b1ee3e1f0cd
Taylor Otwell <taylor@laravel.com>
2017-02-07 09:07:43 -0600

    update method

--- src/Illuminate/View/Concerns/ManagesStacks.php
+++ src/Illuminate/View/Concerns/ManagesStacks.php
@@ -63,6 +63,26 @@ trait ManagesStacks
     }
 
     /**
+     * Append content to a given push section.
+     *
+     * @param  string  $section
+     * @param  string  $content
+     * @return void
+     */
+    protected function extendPush($section, $content)
+    {
+        if (! isset($this->pushes[$section])) {
+            $this->pushes[$section] = [];
+        }
+
+        if (! isset($this->pushes[$section][$this->renderCount])) {
+            $this->pushes[$section][$this->renderCount] = $content;
+        } else {
+            $this->pushes[$section][$this->renderCount] .= $content;
+        }
+    }
+
+    /**
      * Start prepending content into a push section.
      *
      * @param  string  $section
@@ -89,32 +109,12 @@ trait ManagesStacks
     public function stopPrepend()
     {
         if (empty($this->pushStack)) {
-            throw new InvalidArgumentException('Cannot end a prepend stack without first starting one.');
+            throw new InvalidArgumentException('Cannot end a prepend operation without first starting one.');
         }
 
         return tap(array_pop($this->pushStack), function ($last) {
             $this->extendPrepend($last, ob_get_clean());
         });
-    }
-
-    /**
-     * Append content to a given push section.
-     *
-     * @param  string  $section
-     * @param  string  $content
-     * @return void
-     */
-    protected function extendPush($section, $content)
-    {
-        if (! isset($this->pushes[$section])) {
-            $this->pushes[$section] = [];
-        }
-
-        if (! isset($this->pushes[$section][$this->renderCount])) {
-            $this->pushes[$section][$this->renderCount] = $content;
-        } else {
-            $this->pushes[$section][$this->renderCount] .= $content;
-        }
     }
 
     /**
@@ -171,6 +171,7 @@ trait ManagesStacks
     public function flushStacks()
     {
         $this->pushes = [];
+        $this->prepends = [];
         $this->pushStack = [];
     }
 }
