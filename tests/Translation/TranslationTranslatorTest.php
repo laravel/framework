7e6a534c4562cc547ff9541b5b4097bda181c0e2
Mohamed Said <themohamedsaid@gmail.com>
2017-02-08 16:27:08 +0200

    Bring the pluralization rules back

--- tests/Translation/TranslationTranslatorTest.php
+++ tests/Translation/TranslationTranslatorTest.php
@@ -78,7 +78,7 @@ class TranslationTranslatorTest extends TestCase
         $t = $this->getMockBuilder('Illuminate\Translation\Translator')->setMethods(['get'])->setConstructorArgs([$this->getLoader(), 'en'])->getMock();
         $t->expects($this->once())->method('get')->with($this->equalTo('foo'), $this->equalTo(['replace']), $this->equalTo('en'))->will($this->returnValue('line'));
         $t->setSelector($selector = m::mock('Illuminate\Translation\MessageSelector'));
-        $selector->shouldReceive('choose')->once()->with('line', 10)->andReturn('choiced');
+        $selector->shouldReceive('choose')->once()->with('line', 10, 'en')->andReturn('choiced');
 
         $t->choice('foo', 10, ['replace']);
     }
@@ -88,7 +88,7 @@ class TranslationTranslatorTest extends TestCase
         $t = $this->getMockBuilder('Illuminate\Translation\Translator')->setMethods(['get'])->setConstructorArgs([$this->getLoader(), 'en'])->getMock();
         $t->expects($this->exactly(2))->method('get')->with($this->equalTo('foo'), $this->equalTo(['replace']), $this->equalTo('en'))->will($this->returnValue('line'));
         $t->setSelector($selector = m::mock('Illuminate\Translation\MessageSelector'));
-        $selector->shouldReceive('choose')->twice()->with('line', 3)->andReturn('choiced');
+        $selector->shouldReceive('choose')->twice()->with('line', 3, 'en')->andReturn('choiced');
 
         $values = ['foo', 'bar', 'baz'];
         $t->choice('foo', $values, ['replace']);
