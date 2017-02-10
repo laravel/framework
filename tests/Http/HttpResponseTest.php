60bfbab6db46ca94e9bdb89c6173a5fc8d5ba699
Lucas Michot <lucas@semalead.com>
2017-02-10 13:58:47 +0100

    Fix CS.

--- tests/Http/HttpResponseTest.php
+++ tests/Http/HttpResponseTest.php
@@ -2,10 +2,10 @@
 
 namespace Illuminate\Tests\Http;
 
-use Illuminate\Http\JsonResponse;
 use Mockery as m;
 use Illuminate\Http\Request;
 use PHPUnit\Framework\TestCase;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Contracts\Support\Jsonable;
 
