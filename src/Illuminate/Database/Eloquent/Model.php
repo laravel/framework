8e3c0315e0fa66653d46560f8124224d57d6fa23
Kennedy Tedesco <kennedyt.tw@gmail.com>
2017-02-12 12:42:51 -0200

    [5.5] Remove usages of the with() helper (#17888)
    * [5.5] Remove usages of the with() helper
    
    Now that Laravel 5.5 requires PHP 7 the usage of `with()` becomes
    unnecessary.
    
    * Bring back the helper

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -694,7 +694,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
         // We will actually pull the models from the database table and call delete on
         // each of them individually so that their events get fired properly with a
         // correct set of attributes in case the developers wants to check these.
-        $key = with($instance = new static)->getKeyName();
+        $key = ($instance = new static)->getKeyName();
 
         foreach ($instance->whereIn($key, $ids)->get() as $model) {
             if ($model->delete()) {
