5fd0fb81c8c724abd79ed879108d13f73509c641
Peter Scopes <peter.scopes@randomstorm.com>
2017-02-14 08:37:21 +0000

    Reverting breaking change

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -149,7 +149,7 @@ class BelongsToMany extends Relation
      * @param  \Illuminate\Database\Eloquent\Builder|null  $query
      * @return $this
      */
-    protected function performJoin(Builder $query = null)
+    protected function performJoin($query = null)
     {
         $query = $query ?: $this->query;
 
