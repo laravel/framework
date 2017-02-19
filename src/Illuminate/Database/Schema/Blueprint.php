a2b61ba74220c59402710079f9cd7d56cd2a67ce
andres2050 <andres.2050@hotmail.com>
2017-02-19 16:28:43 -0500

    method fastForeign for Blueprint
    This method creates a standard foreign key passing the name of the table and the foreign key name if required, this method returns the created column.

--- src/Illuminate/Database/Schema/Blueprint.php
+++ src/Illuminate/Database/Schema/Blueprint.php
@@ -393,6 +393,23 @@ class Blueprint
     {
         return $this->indexCommand('foreign', $columns, $name);
     }
+    
+    /**
+     * Specify a table to create a standard foreign key for the table.
+     *
+     * @param  string  $table
+     * @param  string  $name
+     * @return \Illuminate\Support\Fluent
+     */
+    public function fastForeign($table, $name = null){
+
+        $column_name = substr($table, 0, -1) . "_id";
+
+        $column = $this->integer($column_name)->unsigned();
+        $this->foreign($column_name, $name)->references('id')->on($table);
+
+        return $column;
+    }
 
     /**
      * Create a new auto-incrementing integer (4-byte) column on the table.
