f924b1be8c7ddf081d4fa93af782d93e046a065c
Grzegorz Rajchman <mrliptontea@griego.pl>
2017-02-21 12:54:15 +0000

    Reformat tests for SqsJob and SqsQueue

--- tests/Queue/QueueSqsQueueTest.php
+++ tests/Queue/QueueSqsQueueTest.php
@@ -4,7 +4,6 @@ namespace Illuminate\Tests\Queue;
 
 use Aws\Result;
 use Mockery as m;
-use Aws\Sqs\SqsClient;
 use PHPUnit\Framework\TestCase;
 
 class QueueSqsQueueTest extends TestCase
@@ -16,7 +15,6 @@ class QueueSqsQueueTest extends TestCase
 
     public function setUp()
     {
-
         // Use Mockery to mock the SqsClient
         $this->sqs = m::mock('Aws\Sqs\SqsClient');
 
@@ -25,8 +23,8 @@ class QueueSqsQueueTest extends TestCase
         $this->baseUrl = 'https://sqs.someregion.amazonaws.com';
 
         // This is how the modified getQueue builds the queueUrl
-        $this->prefix = $this->baseUrl.'/'.$this->account.'/';
-        $this->queueUrl = $this->prefix.$this->queueName;
+        $this->prefix = $this->baseUrl . '/' . $this->account . '/';
+        $this->queueUrl = $this->prefix . $this->queueName;
 
         $this->mockedJob = 'foo';
         $this->mockedData = ['data'];
@@ -35,17 +33,24 @@ class QueueSqsQueueTest extends TestCase
         $this->mockedMessageId = 'e3cd03ee-59a3-4ad8-b0aa-ee2e3808ac81';
         $this->mockedReceiptHandle = '0NNAq8PwvXuWv5gMtS9DJ8qEdyiUwbAjpp45w2m6M4SJ1Y+PxCh7R930NRB8ylSacEmoSnW18bgd4nK\/O6ctE+VFVul4eD23mA07vVoSnPI4F\/voI1eNCp6Iax0ktGmhlNVzBwaZHEr91BRtqTRM3QKd2ASF8u+IQaSwyl\/DGK+P1+dqUOodvOVtExJwdyDLy1glZVgm85Yw9Jf5yZEEErqRwzYz\/qSigdvW4sm2l7e4phRol\/+IjMtovOyH\/ukueYdlVbQ4OshQLENhUKe7RNN5i6bE\/e5x9bnPhfj2gbM';
 
-        $this->mockedSendMessageResponseModel = new Result(['Body' => $this->mockedPayload,
-                                          'MD5OfBody' => md5($this->mockedPayload),
-                                          'ReceiptHandle' => $this->mockedReceiptHandle,
-                                          'MessageId' => $this->mockedMessageId,
-                                          'Attributes' => ['ApproximateReceiveCount' => 1], ]);
+        $this->mockedSendMessageResponseModel = new Result([
+            'Body' => $this->mockedPayload,
+            'MD5OfBody' => md5($this->mockedPayload),
+            'ReceiptHandle' => $this->mockedReceiptHandle,
+            'MessageId' => $this->mockedMessageId,
+            'Attributes' => ['ApproximateReceiveCount' => 1],
+        ]);
 
-        $this->mockedReceiveMessageResponseModel = new Result(['Messages' => [0 => [
-                                                'Body' => $this->mockedPayload,
-                                                     'MD5OfBody' => md5($this->mockedPayload),
-                                                      'ReceiptHandle' => $this->mockedReceiptHandle,
-                                                     'MessageId' => $this->mockedMessageId, ]]]);
+        $this->mockedReceiveMessageResponseModel = new Result([
+            'Messages' => [
+                0 => [
+                    'Body' => $this->mockedPayload,
+                    'MD5OfBody' => md5($this->mockedPayload),
+                    'ReceiptHandle' => $this->mockedReceiptHandle,
+                    'MessageId' => $this->mockedMessageId,
+                ],
+            ],
+        ]);
     }
 
     public function testPopProperlyPopsJobOffOfSqs()
@@ -95,7 +100,7 @@ class QueueSqsQueueTest extends TestCase
     {
         $queue = new \Illuminate\Queue\SqsQueue($this->sqs, $this->queueName, $this->prefix);
         $this->assertEquals($this->queueUrl, $queue->getQueue(null));
-        $queueUrl = $this->baseUrl.'/'.$this->account.'/test';
+        $queueUrl = $this->baseUrl . '/' . $this->account . '/test';
         $this->assertEquals($queueUrl, $queue->getQueue('test'));
     }
 
@@ -103,7 +108,7 @@ class QueueSqsQueueTest extends TestCase
     {
         $queue = new \Illuminate\Queue\SqsQueue($this->sqs, $this->queueUrl);
         $this->assertEquals($this->queueUrl, $queue->getQueue(null));
-        $queueUrl = $this->baseUrl.'/'.$this->account.'/test';
+        $queueUrl = $this->baseUrl . '/' . $this->account . '/test';
         $this->assertEquals($queueUrl, $queue->getQueue($queueUrl));
     }
 }
