504bcffdb0b8ca23c5382067633318a699868ab5
Taylor Otwell <taylor@laravel.com>
2017-03-01 15:44:04 -0600

    [5.4] Storage fake and fake files. (#18178)
    This allows you to fake a storage disk which replaces it with a local
    disk pointing into the storage/framework/testing directory. This also
    includes a way to generate “Fake” uploaded files, especially images, by
    doing: UploadedFile::fake()->image(‘name.jpg’, width, height)… you may
    also spoof the size of the image by doing ->size(300) where 300 is the
    number of kilobytes the file will report as its size.

--- src/Illuminate/Support/Facades/Storage.php
+++ src/Illuminate/Support/Facades/Storage.php
@@ -2,11 +2,28 @@
 
 namespace Illuminate\Support\Facades;
 
+use Illuminate\Filesystem\Filesystem;
+
 /**
  * @see \Illuminate\Filesystem\FilesystemManager
  */
 class Storage extends Facade
 {
+    /**
+     * Replace the given disk with a local, testing disk.
+     *
+     * @param  string  $disk
+     * @return void
+     */
+    public static function fake($disk)
+    {
+        (new Filesystem)->cleanDirectory(
+            $root = storage_path('framework/testing/disks/'.$disk)
+        );
+
+        static::set($disk, self::createLocalDriver(['root' => $root]));
+    }
+
     /**
      * Get the registered name of the component.
      *
