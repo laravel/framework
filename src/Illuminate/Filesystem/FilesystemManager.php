5edf99780a768fd00d9e86a8f9e3b305983336b8
Taylor Otwell <taylor@laravel.com>
2017-03-01 15:44:04 -0600

    [5.4] Storage fake and fake files. (#18178)
    This allows you to fake a storage disk which replaces it with a local
    disk pointing into the storage/framework/testing directory. This also
    includes a way to generate “Fake” uploaded files, especially images, by
    doing: UploadedFile::fake()->image(‘name.jpg’, width, height)… you may
    also spoof the size of the image by doing ->size(300) where 300 is the
    number of kilobytes the file will report as its size.

--- src/Illuminate/Filesystem/FilesystemManager.php
+++ src/Illuminate/Filesystem/FilesystemManager.php
@@ -272,6 +272,18 @@ class FilesystemManager implements FactoryContract
     }
 
     /**
+     * Set the given disk instance.
+     *
+     * @param  string  $name
+     * @param  mixed  $disk
+     * @return void
+     */
+    public function set($name, $disk)
+    {
+        $this->disks[$name] = $disk;
+    }
+
+    /**
      * Get the filesystem connection configuration.
      *
      * @param  string  $name
