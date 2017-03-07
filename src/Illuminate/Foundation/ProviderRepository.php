ef3374a8363d88cc3e61f7c50cb5300e31d19b59
Graham Campbell <graham@alt-three.com>
2017-03-07 20:01:21 +0000

    Fixed phpdoc

--- src/Illuminate/Foundation/ProviderRepository.php
+++ src/Illuminate/Foundation/ProviderRepository.php
@@ -181,7 +181,8 @@ class ProviderRepository
      *
      * @param  array  $manifest
      * @return array
-     * @throws Exception
+     *
+     * @throws \Exception
      */
     public function writeManifest($manifest)
     {
