777fdc46630978a26ddc605d728c4df91a81d920
Jari Pekkala <jaripekkala@Jaris-MacBook-Pro.local>
2017-03-17 16:49:57 +0200

    return PendingResourceRegistration when registering resources which registeres them on destruction possibly limiting/excluding methods

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -245,7 +245,7 @@ class Router implements RegistrarContract, BindingRegistrar
      * @param  string  $name
      * @param  string  $controller
      * @param  array  $options
-     * @return void
+     * @return \Illuminate\Routing\PendingResourceRegistration
      */
     public function resource($name, $controller, array $options = [])
     {
@@ -255,7 +255,7 @@ class Router implements RegistrarContract, BindingRegistrar
             $registrar = new ResourceRegistrar($this);
         }
 
-        $registrar->register($name, $controller, $options);
+        return $registrar->lazy($name, $controller, $options);
     }
 
     /**
