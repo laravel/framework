7f2047adad9d065c2fa409ddc78912210be34b68
Ali Borhani <aliborhani1@gmail.com>
2017-03-19 19:30:45 +0330

    make php-redis connection more compatible with predis (pipeline, transaction, executeRaw)

--- src/Illuminate/Redis/Connections/PhpRedisConnection.php
+++ src/Illuminate/Redis/Connections/PhpRedisConnection.php
@@ -111,6 +111,47 @@ class PhpRedisConnection extends Connection
     }
 
     /**
+     * Execute commands in a pipeline
+     *
+     * @param  callable  $callback
+     * @return array|\Redis
+     */
+    public function pipeline(callable $callback = null)
+    {
+        $pipeline = $this->client()->pipeline();
+
+        return is_null($callback)
+            ? $pipeline
+            : tap($pipeline, $callback)->exec();
+    }
+
+    /**
+     * Execute commands in a transaction
+     *
+     * @param  callable  $callback
+     * @return array|\Redis
+     */
+    public function transaction(callable $callback = null)
+    {
+        $transaction = $this->client()->multi();
+
+        return is_null($callback)
+            ? $transaction
+            : tap($transaction, $callback)->exec();
+    }
+
+    /**
+     * Execute a raw command
+     *
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function executeRaw(array $parameters)
+    {
+        return $this->command("rawCommand", $parameters);
+    }
+
+    /**
      * Evaluate a LUA script serverside, from the SHA1 hash of the script instead of the script itself.
      *
      * @param  string  $script
