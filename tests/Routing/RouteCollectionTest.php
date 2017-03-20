4254df357db81b1ad1d72e7d5bab88e6ce00d032
Jari Pekkala <jaripekkala@Jaris-MacBook-Pro.local>
2017-03-20 11:18:52 +0200

    add method to remove existing routes from the collection

--- tests/Routing/RouteCollectionTest.php
+++ tests/Routing/RouteCollectionTest.php
@@ -165,4 +165,17 @@ class RouteCollectionTest extends TestCase
         ];
         $this->assertEquals($allRoutes, $this->routeCollection->getRoutes());
     }
+
+    public function testRouteCollectionCanRemoveRoute()
+    {
+        $route = new Route('GET', 'foo', [
+            'uses' => 'FooController@index',
+            'as' => 'foo_index',
+        ]);
+
+        $this->routeCollection->add($route);
+        $this->routeCollection->remove($route);
+
+        $this->assertCount(0, $this->routeCollection);
+    }
 }
