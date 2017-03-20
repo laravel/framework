ea99965f7943e4c23ab2df83682c660ad9a5cb7a
Clayton Stone <devcircus@outlook.com>
2017-03-20 14:47:01 -0500

    fix issue with case in authorizeResource

--- src/Illuminate/Foundation/Auth/Access/AuthorizesRequests.php
+++ src/Illuminate/Foundation/Auth/Access/AuthorizesRequests.php
@@ -81,7 +81,7 @@ trait AuthorizesRequests
      */
     public function authorizeResource($model, $parameter = null, array $options = [], $request = null)
     {
-        $parameter = $parameter ?: strtolower(class_basename($model));
+        $parameter = $parameter ?: lcfirst(class_basename($model));
 
         $middleware = [];
 
