1097693adf8b5c17a340ac699a541c9cc99f9c7d
Taylor Otwell <taylor@laravel.com>
2017-03-24 13:58:54 -0500

    remove comment

--- tests/Routing/RoutingRouteTest.php
+++ tests/Routing/RoutingRouteTest.php
@@ -700,9 +700,6 @@ class RoutingRouteTest extends TestCase
         $this->assertEquals('tayloralt', $router->dispatch(Request::create('foo/TAYLOR', 'GET'))->getContent());
     }
 
-    /**
-     * @group shit
-     */
     public function testModelBindingWithCompoundParameterName()
     {
         $router = $this->getRouter();
