0f9ea292c6be071837b52537aa2ced91925a0152
Alejandro Fernandez <alejandro.fernandez.rueda@gmail.com>
2017-03-26 20:53:15 +0200

    Applied CI Styles Fix

--- src/Illuminate/Http/TransformableResponse.php
+++ src/Illuminate/Http/TransformableResponse.php
@@ -27,7 +27,7 @@ abstract class TransformableResponse extends JsonResponse
 
         parent::__construct($data, $status, $headers, $options);
     }
-    
+
     /**
      * Transforms the response data.
      *
@@ -40,7 +40,7 @@ abstract class TransformableResponse extends JsonResponse
 
         return $this->applyRules($data, $rules);
     }
-    
+
     /**
      * Resolve array rules generating a new rule for every item in the array
      * spcified with the '*' symbol.
@@ -58,45 +58,45 @@ abstract class TransformableResponse extends JsonResponse
                 $gatheredRules = array_keys(
                     ValidationData::initializeAndGatherData($rule, $data)
                 );
-                
+
                 return array_merge(
                     $parsedRules,
                     $this->sanitizeArrayGatheredRules($rule, $gatheredRules, $rules[$rule])
                 );
             }
-            
+
             $parsedRules[$rule] = $rules[$rule];
-            
+
             return $parsedRules;
         }, []);
     }
-    
+
     /**
      * Sanitize gathered array rules removing those that don't appear into
      * orginal rules.
      *
      * @param  string   $rule
      * @param  array    $gatheredRules
-     * @param  boolean  $valueForValidOnes
+     * @param  bool  $valueForValidOnes
      * @return array
      */
     protected function sanitizeArrayGatheredRules($rule, array $gatheredRules, $valueForValidOnes)
     {
-        $pattern = '/' . str_replace('.*.', '\.([0-9])+\.', $rule) . '/';
-        
+        $pattern = '/'.str_replace('.*.', '\.([0-9])+\.', $rule).'/';
+
         return array_reduce($gatheredRules,
             function ($validRules, $rule) use ($pattern, $valueForValidOnes) {
                 preg_match($pattern, $rule, $matches);
-                
+
                 if ($matches) {
                     $validRules[$matches[0]] = $valueForValidOnes;
                 }
-                
+
                 return $validRules;
             },
         []);
     }
-    
+
     /**
      * Apply rules to the initial given data.
      *
@@ -114,7 +114,7 @@ abstract class TransformableResponse extends JsonResponse
 
         return $data;
     }
-    
+
     /**
      * Apply rules over the fields that must be displayed.
      *
@@ -135,12 +135,12 @@ abstract class TransformableResponse extends JsonResponse
                 if ($value = Arr::get($data, $rule)) {
                     Arr::set($transformedData, $rule, $value);
                 }
-                
+
                 return $transformedData;
             },
         []);
     }
-    
+
     /**
      * Apply rules over the fields that must be hidden.
      *
@@ -151,24 +151,24 @@ abstract class TransformableResponse extends JsonResponse
     protected function applyRulesToHideFields(array $data, array $rules)
     {
         $applicableRules = array_filter($rules, function ($rule) {
-            return !$rule;
+            return ! $rule;
         });
 
         if (empty($applicableRules)) {
             return $data;
         }
-        
+
         return array_reduce(array_keys($applicableRules),
             function ($transformedData, $rule) {
                 if (Arr::has($transformedData, $rule)) {
                     Arr::forget($transformedData, $rule);
                 }
-                
+
                 return $transformedData;
             },
         $data);
     }
-    
+
     /**
      * Define the rules that will apply to this transformer
      * response.
