2b9df18d8b6e65de6ed40f58c5a1b51b8d8162f0
Joseph Silber <contact@josephsilber.com>
2017-03-27 20:38:22 -0400

    Add "flatMap" to collection higher order proxies

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -32,8 +32,8 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
      * @var array
      */
     protected static $proxies = [
-        'contains', 'each', 'every', 'filter', 'first', 'map',
-        'partition', 'reject', 'sortBy', 'sortByDesc', 'sum',
+        'contains', 'each', 'every', 'filter', 'first', 'flatMap',
+        'map', 'partition', 'reject', 'sortBy', 'sortByDesc', 'sum',
     ];
 
     /**
