463d2517190d0276f3ef1437f6d178bb8f266b1e
Jari Pekkala <jari.pekkala@outlook.com>
2017-03-30 16:01:28 +0300

    don't hold the prefixed resources

--- src/Illuminate/Routing/ResourceRegistrar.php
+++ src/Illuminate/Routing/ResourceRegistrar.php
@@ -128,7 +128,7 @@ class ResourceRegistrar
         // are supported in the framework, but we need to know what name to use for a
         // place-holder on the route parameters, which should be the base resources.
         $callback = function ($me) use ($name, $controller, $options) {
-            $me->resource($name, $controller, $options);
+            $me->resource($name, $controller, $options)->register();
         };
 
         return $this->router->group(compact('prefix'), $callback);
