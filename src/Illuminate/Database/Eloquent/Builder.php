427198ce0f27c8dbdf33fc42c798957bd15427ab
Jonathan <jonnywilliamson@gmail.com>
2017-04-02 18:04:36 -0400

    Revert overly broad return type (#18625)
    Revert an overly broad return type hint, to a more specific one. Helps developer work flow by not breaking IDE autocomplete on chaining.

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -253,7 +253,7 @@ class Builder
      *
      * @param  mixed  $id
      * @param  array  $columns
-     * @return mixed
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static[]|static|null
      */
     public function find($id, $columns = ['*'])
     {
