e180e20aa479525b34f77b9cf348148d329a4d2c
Taylor Otwell <taylor@laravel.com>
2017-04-06 09:28:50 -0500

    clean up formatting

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -982,29 +982,22 @@ trait HasAttributes
             return false;
         }
 
-        if ($current === $original = $this->getOriginal($key)) {
+        $original = $this->getOriginal($key);
+
+        if ($current === $original) {
             return true;
-        }
-
-        // When check rich this check and current attribute value not equals with original, we should skip next steps
-        // if current is null
-        if (is_null($current)) {
+        } elseif (is_null($current)) {
             return false;
+        } elseif ($this->isDateAttribute($key)) {
+            return $this->fromDateTime($current) ===
+                   $this->fromDateTime($original);
+        } elseif ($this->hasCast($key)) {
+            return $this->castAttribute($key, $current) ===
+                   $this->castAttribute($key, $original);
         }
 
-        if ($this->isDateAttribute($key)) {
-            return $this->fromDateTime($current) === $this->fromDateTime($original);
-        }
-
-        if ($this->hasCast($key)) {
-            return $this->castAttribute($key, $current) === $this->castAttribute($key, $original);
-        }
-
-        // This method checks if the two values are numerically equivalent even if they
-        // are different types. This is in case the two values are not the same type
-        // we can do a fair comparison of the two values to know if this is dirty.
         return is_numeric($current) && is_numeric($original)
-            && strcmp((string) $current, (string) $original) === 0;
+                && strcmp((string) $current, (string) $original) === 0;
     }
 
     /**
