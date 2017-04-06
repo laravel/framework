34fd458d370a39335810c1f040fad04af418fed4
Taylor Otwell <taylor@laravel.com>
2017-04-06 17:13:03 -0500

    cleaning

--- src/Illuminate/Foundation/Console/Presets/React.php
+++ src/Illuminate/Foundation/Console/Presets/React.php
@@ -13,31 +13,92 @@ class React
      */
     public static function install()
     {
-        if (file_exists(base_path('package.json'))) {
-            $packages = json_decode(file_get_contents(base_path('package.json')), true);
+        static::updatePackages();
+        static::updateWebpackConfiguration();
+        static::updateBootstrapping();
+        static::updateComponent();
+        static::removeNodeModules();
+    }
 
-            unset($packages['devDependencies']['vue']);
-
-            $packages['devDependencies']['babel-preset-react'] = '^6.23.0';
-            $packages['devDependencies']['react'] = '^15.4.2';
-            $packages['devDependencies']['react-dom'] = '^15.4.2';
-
-            ksort($packages['devDependencies']);
-
-            file_put_contents(base_path('package.json'), json_encode($packages, JSON_PRETTY_PRINT));
+    /**
+     * Update the "package.json" file.
+     *
+     * @return void
+     */
+    protected static function updatePackages()
+    {
+        if (! file_exists(base_path('package.json'))) {
+            return;
         }
 
-        copy(__DIR__.'/react-stubs/app.js', resource_path('assets/js/app.js'));
-        copy(__DIR__.'/react-stubs/bootstrap.js', resource_path('assets/js/bootstrap.js'));
+        $packages = json_decode(file_get_contents(base_path('package.json')), true);
 
-        $filesystem = new Filesystem;
-        $filesystem->delete(resource_path('assets/js/components/Example.vue'));
+        unset($packages['devDependencies']['vue']);
 
-        copy(__DIR__.'/react-stubs/Example.js', resource_path('assets/js/components/Example.js'));
+        $packages['devDependencies'] += [
+            'babel-preset-react' => '^6.23.0',
+            'react' => '^15.4.2',
+            'react-dom' => '^15.4.2',
+        ];
 
+        ksort($packages['devDependencies']);
+
+        file_put_contents(
+            base_path('package.json'),
+            json_encode($packages, JSON_PRETTY_PRINT)
+        );
+    }
+
+    /**
+     * Update the Webpack configuration.
+     *
+     * @return void
+     */
+    protected static function updateWebpackConfiguration()
+    {
         copy(__DIR__.'/react-stubs/webpack.mix.js', base_path('webpack.mix.js'));
+    }
 
-        $filesystem->deleteDirectory(base_path('node_modules'));
-        $filesystem->delete(base_path('yarn.lock'));
+    /**
+     * Update the example component.
+     *
+     * @return void
+     */
+    protected static function updateComponent()
+    {
+        (new Filesystem)->delete(
+            resource_path('assets/js/components/Example.vue')
+        );
+
+        copy(
+            __DIR__.'/react-stubs/Example.js',
+            resource_path('assets/js/components/Example.js')
+        );
+    }
+
+    /**
+     * Update the bootstrapping files.
+     *
+     * @return void
+     */
+    protected static function updateBootstrapping()
+    {
+        copy(__DIR__.'/react-stubs/app.js', resource_path('assets/js/app.js'));
+
+        copy(__DIR__.'/react-stubs/bootstrap.js', resource_path('assets/js/bootstrap.js'));
+    }
+
+    /**
+     * Remove the installed Node modules.
+     *
+     * @return void
+     */
+    protected static function removeNodeModules()
+    {
+        tap(new Filesystem, function ($files) {
+            $files->deleteDirectory(base_path('node_modules'));
+
+            $files->delete(base_path('yarn.lock'));
+        });
     }
 }
