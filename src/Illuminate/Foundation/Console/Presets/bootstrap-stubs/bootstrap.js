d6c7abe5e651cda813831afa5943791334361cd7
Taylor Otwell <taylor@laravel.com>
2017-04-06 21:13:30 -0500

    update axios calls

--- src/Illuminate/Foundation/Console/Presets/bootstrap-stubs/bootstrap.js
+++ src/Illuminate/Foundation/Console/Presets/bootstrap-stubs/bootstrap.js
@@ -19,10 +19,8 @@ require('bootstrap-sass');
 
 window.axios = require('axios');
 
-window.axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.Laravel.csrfToken,
-    'X-Requested-With': 'XMLHttpRequest'
-};
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
