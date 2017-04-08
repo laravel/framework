9494cd5ecd32d1e1d4ee9e0963ae5574f26e6a7c
Laurence <github@theshiftexchange.com>
2017-04-08 12:21:28 +0000

    change tokenmismatch

---
+++ src/Illuminate/Foundation/Exceptions/views/419.blade.php
@@ -0,0 +1,9 @@
+@extends('errors::layout')
+
+@section('title', 'Logged out')
+
+@section('message')
+    You have been logged out due to inactivity.
+    <br/><br/>
+    Please log in and try again.
+@stop
