b27d4d481fa8ba1ddd6654b70de8106de1a22646
Mohamed Said <themohamedsaid@gmail.com>
2017-04-10 19:18:47 +0200

    fix style

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -593,7 +593,7 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
     public function has($key)
     {
         $keys = is_array($key) ? $key : func_get_args();
-        
+
         foreach ($keys as $value) {
             if (! $this->offsetExists($value)) {
                 return false;
