33e4a6d829503d89e977787b856dc8300d4b9fdf
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-04-10 16:32:55 +0200

    cs

--- src/Illuminate/Support/ServiceProvider.php
+++ src/Illuminate/Support/ServiceProvider.php
@@ -82,11 +82,11 @@ abstract class ServiceProvider
     protected function loadViewsFrom($path, $namespace)
     {
         $viewPaths = $this->app['config']->get('view.paths', [$this->app->resourcePath().'/views']);
-         foreach ($viewPaths as $viewPath) {
-             if (is_dir($appPath = rtrim($viewPath, '/').'/vendor/'.$namespace)) {
-                 $this->app['view']->addNamespace($namespace, $appPath);
-             }
-          }
+       foreach ($viewPaths as $viewPath) {
+           if (is_dir($appPath = rtrim($viewPath, '/').'/vendor/'.$namespace)) {
+               $this->app['view']->addNamespace($namespace, $appPath);
+           }
+        }
 
         $this->app['view']->addNamespace($namespace, $path);
     }
