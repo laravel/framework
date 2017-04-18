ea6cc47639ed2d839b3a57b33ebf172a0ecf5db8
Michael Dyrynda <michael@dyrynda.com.au>
2017-04-18 12:24:31 +0930

    Add markdown helper, refactor mailable markdown
    Introduce a markdown helper that returns a Parsedown instance if no
    arguments are passed to it, otherwise return a `HtmlString` of the
    parsed Markdown input.

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -917,3 +917,22 @@ if (! function_exists('view')) {
         return $factory->make($view, $data, $mergeData);
     }
 }
+
+if (! function_exists('markdown')) {
+    /**
+     * Parse a Markdown formatted string into HTML.
+     *
+     * @param  null|string  $input
+     * @return \Parsedown|\Illuminate\Support\HtmlString
+     */
+    function markdown($input = null)
+    {
+        $parsedown = app(Parsedown::class);
+
+        if (func_num_args() == 0) {
+            return $parsedown;
+        }
+
+        return new HtmlString($parsedown->text($input));
+    }
+}
