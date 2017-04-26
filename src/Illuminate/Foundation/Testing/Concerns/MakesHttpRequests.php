006a35764aff9ae5b1e9a1468bac3bf7703db367
Ricky Delorier <rdelorier@gmail.com>
2017-04-26 19:34:42 -0400

    fix style

--- src/Illuminate/Foundation/Testing/Concerns/MakesHttpRequests.php
+++ src/Illuminate/Foundation/Testing/Concerns/MakesHttpRequests.php
@@ -307,7 +307,7 @@ trait MakesHttpRequests
 
             if (is_array($value)) {
                 $files[$key] = $this->extractFilesFromDataArray($value);
-                
+
                 unset($data[$key]);
             }
         }
