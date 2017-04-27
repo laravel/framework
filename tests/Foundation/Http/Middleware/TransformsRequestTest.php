fdb464a26a30c4f27c1995182b80b6d603e2913d
Damian Mąsior <initx@users.noreply.github.com>
2017-04-27 14:38:42 +0200

    [5.4] Add tests to recently updated middleware (#18953)
    * test transform request middleware
    
    * rename test method
    
    * style ci fight
    
    * style ci update

---
+++ tests/Foundation/Http/Middleware/TransformsRequestTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Illuminate\Tests\Foundation\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\Middleware\TransformsRequest;
+
+class TransformsRequestTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLowerAgeAndAddBeer()
+    {
+        $middleware = new ManipulateInput();
+        $request = new Request(
+            [
+                'name' => 'Damian',
+                'beers' => 4,
+            ],
+            ['age' => 28]
+        );
+
+        $middleware->handle($request, function (Request $request) {
+            $this->assertEquals('Damian', $request->get('name'));
+            $this->assertEquals(27, $request->get('age'));
+            $this->assertEquals(5, $request->get('beers'));
+        });
+    }
+
+    public function testAjaxLowerAgeAndAddBeer()
+    {
+        $middleware = new ManipulateInput();
+        $request = new Request(
+            [
+                'name' => 'Damian',
+                'beers' => 4,
+            ],
+            [],
+            [],
+            [],
+            [],
+            ['CONTENT_TYPE' => '/json'],
+            json_encode(['age' => 28])
+        );
+
+        $middleware->handle($request, function (Request $request) {
+            $this->assertEquals('Damian', $request->input('name'));
+            $this->assertEquals(27, $request->input('age'));
+            $this->assertEquals(5, $request->input('beers'));
+        });
+    }
+}
+
+class ManipulateInput extends TransformsRequest
+{
+    protected function transform($key, $value)
+    {
+        if ($key === 'beers') {
+            $value++;
+        }
+        if ($key === 'age') {
+            $value--;
+        }
+
+        return $value;
+    }
+}
