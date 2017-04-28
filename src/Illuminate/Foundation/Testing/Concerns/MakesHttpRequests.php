a102a9fc9e25d8f35eaad711e5954092955a3aa1
Richard Delorier <rdelorier@gmail.com>
2017-04-28 17:54:14 -0400

    [5.4] Set data key when testing file uploads in nested array (#18954)
    * Remove data key when testing file uploads in nested array
    
    * fix style
    
    * Replace value instead of unset

--- src/Illuminate/Foundation/Testing/Concerns/MakesHttpRequests.php
+++ src/Illuminate/Foundation/Testing/Concerns/MakesHttpRequests.php
@@ -307,6 +307,8 @@ trait MakesHttpRequests
 
             if (is_array($value)) {
                 $files[$key] = $this->extractFilesFromDataArray($value);
+
+                $data[$key] = $value;
             }
         }
 
