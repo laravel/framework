0d667f8ee1f9533e1423fb73016c1050be4ae17f
Ganeko Guereta <ganeko.guereta@gmail.com>
2017-05-02 14:57:00 +0200

    [5.4] Minor documentation fix (#19031)

--- src/Illuminate/Events/Dispatcher.php
+++ src/Illuminate/Events/Dispatcher.php
@@ -333,7 +333,7 @@ class Dispatcher implements DispatcherContract
      *
      * @param  string|\Closure  $listener
      * @param  bool  $wildcard
-     * @return mixed
+     * @return \Closure
      */
     public function makeListener($listener, $wildcard = false)
     {
