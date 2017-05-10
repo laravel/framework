f4ba43d09f05f3ed23d811c5dae5ca2d01781d49
Julien H <cotcotquedec@users.noreply.github.com>
2017-05-10 15:41:44 +0200

    Better encode with addslashes

--- src/Illuminate/Validation/Rules/Unique.php
+++ src/Illuminate/Validation/Rules/Unique.php
@@ -173,7 +173,7 @@ class Unique
         return rtrim(sprintf('unique:%s,%s,%s,%s,%s',
             $this->table,
             $this->column,
-            $this->ignore ? '"'.str_replace('"', '\"', $this->ignore).'"' : 'NULL',
+            $this->ignore ? '"'.addslashes($this->ignore).'"' : 'NULL',
             $this->idColumn,
             $this->formatWheres()
         ), ',');
