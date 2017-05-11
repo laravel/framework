8f1a966925e28cf32274727013c7efa72868c96a
Jordan Pittman <jordan@cryptica.me>
2017-05-10 23:06:55 -0400

    Move empty key detection above starts_with check

--- src/Illuminate/Encryption/EncryptionServiceProvider.php
+++ src/Illuminate/Encryption/EncryptionServiceProvider.php
@@ -18,17 +18,19 @@ class EncryptionServiceProvider extends ServiceProvider
         $this->app->singleton('encrypter', function ($app) {
             $config = $app->make('config')->get('app');
 
-            // If the key starts with "base64:", we will need to decode the key before handing
-            // it off to the encrypter. Keys may be base-64 encoded for presentation and we
-            // want to make sure to convert them back to the raw bytes before encrypting.
-            if (Str::startsWith($key = $config['key'], 'base64:')) {
-                $key = base64_decode(substr($key, 7));
-            }
+            $key = $config['key'];
 
             if (is_null($key) || $key === '') {
                 throw new RuntimeException(
                     'The application encryption key is missing. Run php artisan key:generate to generate it.'
                 );
+            }
+
+            // If the key starts with "base64:", we will need to decode the key before handing
+            // it off to the encrypter. Keys may be base-64 encoded for presentation and we
+            // want to make sure to convert them back to the raw bytes before encrypting.
+            if (Str::startsWith($key, 'base64:')) {
+                $key = base64_decode(substr($key, 7));
             }
 
             return new Encrypter($key, $config['cipher']);
