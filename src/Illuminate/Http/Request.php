51960fa0e18933c4e1b5426e3d7d38411cf0e8cb
Михаил Шатилов <mihail.shatilov@gmail.com>
2017-05-11 09:44:29 +0300

    __isset must return true in Request.php
    The __get method does not return an exception so any property can be obtained

--- src/Illuminate/Http/Request.php
+++ src/Illuminate/Http/Request.php
@@ -575,7 +575,7 @@ class Request extends SymfonyRequest implements Arrayable, ArrayAccess
      */
     public function __isset($key)
     {
-        return ! is_null($this->__get($key));
+        return true;
     }
 
     /**
