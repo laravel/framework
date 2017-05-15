bdf399f9e5b8c81f9588f4aa3f2b47df6b8e01d9
Vincent Gabriel <vadimg88@gmail.com>
2017-05-15 08:47:38 -0700

    support php 5.6

--- src/Illuminate/View/Concerns/ManagesLayouts.php
+++ src/Illuminate/View/Concerns/ManagesLayouts.php
@@ -191,7 +191,7 @@ trait ManagesLayouts
      */
     public function getSection($name, $default=null)
     {
-        return $this->getSections()[$name] ?? $default;
+        return isset($this->getSections()[$name]) ? $this->getSections()[$name] : $default;
     }
 
     /**
