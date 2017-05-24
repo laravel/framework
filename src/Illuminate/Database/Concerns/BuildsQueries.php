609d1e2b773057b3cb7b5d4a6e07e299c2eaf6c9
Justin Reasoner <knoxxbox@gmail.com>
2017-05-23 20:46:02 -0400

    Make page number available in chunk closure (#19316)
    * Make page number available in chunk closure
    
    * Fix styleci
    
    Missing space

--- src/Illuminate/Database/Concerns/BuildsQueries.php
+++ src/Illuminate/Database/Concerns/BuildsQueries.php
@@ -32,7 +32,7 @@ trait BuildsQueries
             // On each chunk result set, we will pass them to the callback and then let the
             // developer take care of everything within the callback, which allows us to
             // keep the memory low for spinning through large result sets for working.
-            if ($callback($results) === false) {
+            if ($callback($results, $page) === false) {
                 return false;
             }
 
