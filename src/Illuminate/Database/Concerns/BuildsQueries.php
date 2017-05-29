3956b561e825b978ed5db3870d88e240ab5b0f38
Kevin Pohl <pohl@new-data-services.de>
2017-05-29 23:00:20 +0200

    fixed BuildsQueries when docblock

--- src/Illuminate/Database/Concerns/BuildsQueries.php
+++ src/Illuminate/Database/Concerns/BuildsQueries.php
@@ -79,8 +79,8 @@ trait BuildsQueries
      * Apply the callback's query changes if the given "value" is true.
      *
      * @param  mixed  $value
-     * @param  \Closure  $callback
-     * @param  \Closure  $default
+     * @param  callable|\Closure  $callback
+     * @param  callable|\Closure  $default
      * @return mixed
      */
     public function when($value, $callback, $default = null)
