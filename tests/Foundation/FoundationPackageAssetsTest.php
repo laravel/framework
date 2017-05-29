11006d82153c9dea3386566e32c5feec8298995b
Dries Vints <dries.vints@gmail.com>
2017-05-30 00:23:42 +0200

    Remove mockery call in test

--- tests/Foundation/FoundationPackageAssetsTest.php
+++ tests/Foundation/FoundationPackageAssetsTest.php
@@ -2,18 +2,12 @@
 
 namespace Illuminate\Tests\Foundation;
 
-use Mockery as m;
 use PHPUnit\Framework\TestCase;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Foundation\PackageAssetLoader;
 
 class FoundationPackageAssetsTest extends TestCase
 {
-    public function tearDown()
-    {
-        m::close();
-    }
-
     public function testAssetLoading()
     {
         $assetLoader = new PackageAssetLoader(new Filesystem, __DIR__ . '/fixtures/vendor');
