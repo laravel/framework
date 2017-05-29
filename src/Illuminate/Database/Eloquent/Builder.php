868eb090092216359072b7ee69fcd37c60fa4af1
Taylor Otwell <taylorotwell@gmail.com>
2017-05-28 19:18:11 -0500

    formatting

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -924,7 +924,8 @@ class Builder
         // We will keep track of how many wheres are on the query before running the
         // scope so that we can properly group the added scope constraints in the
         // query as their own isolated nested where statement and avoid issues.
-        $originalWhereCount = is_null($query->wheres) ? 0 : count($query->wheres);
+        $originalWhereCount = is_null($query->wheres)
+                    ? 0 : count($query->wheres);
 
         $result = $scope(...array_values($parameters)) ?: $this;
 
