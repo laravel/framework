7fba1b4a3dd3b9e82f3883258dc51ca928a22842
Hakuno <hakuno@protonmail.com>
2017-06-07 13:17:08 -0300

    StyleCI again
    One new line

--- src/Illuminate/Database/Connectors/ConnectionFactory.php
+++ src/Illuminate/Database/Connectors/ConnectionFactory.php
@@ -138,6 +138,7 @@ class ConnectionFactory
     protected function getReadWriteConfig(array $config, $type)
     {
         $randomConfig = mt_rand(0, count($config[$type]) - 1);
+
         return isset($config[$type][0])
                         ? $config[$type][$randomConfig]
                         : $config[$type];
