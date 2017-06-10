d1fd9e3fc362e12b011abc611c801a8480d70fe7
Ahmet ATAY <ahmet.atay@hotmail.com>
2017-06-10 17:42:41 +0300

    method type fix

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -88,7 +88,7 @@ class Str
      * @param  bool $upperCase
      * @return string
      */
-    function first($value, $upperCase = false)
+    public static function first($value, $upperCase = false)
     {
         $letters  = '';
         $value    = (string) preg_replace(['/[!@#$%^&*\/]/', '/\s+/'], ['', ' '], trim($value));
