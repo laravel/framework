a51fcf54b65aa885f5441a3f262e853a9e547479
Fernando Henrique Bandeira <fernando.bandeira@outlook.com>
2017-06-20 11:48:10 -0300

    Fix test

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -46,7 +46,7 @@ class SupportStrTest extends TestCase
 
     public function testStringAsciiWithSpecificLocale()
     {
-        $this->assertSame('h H sht SHT a А y Y', Str::ascii('х Х щ Щ ъ Ъ ь Ь', 'bg'));
+        $this->assertSame('h H sht SHT a A y Y', Str::ascii('х Х щ Щ ъ Ъ ь Ь', 'bg'));
         $this->assertSame('ae oe ue AE OE UE', Str::ascii('ä ö ü Ä Ö Ü', 'de'));
     }
 
