8d54d7fa3be649173745afcdf64a12989bb98d07
Rogier Verbrugge <rogier.verbrugge@me.com>
2017-06-23 16:22:07 +0200

    [5.4] Fixed duplicate // for public_path (#19731)
    * Fixed duplicate // for public_path
    
    * Changed nested ternary to ltrim

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -598,7 +598,7 @@ if (! function_exists('public_path')) {
      */
     function public_path($path = '')
     {
-        return app()->make('path.public').($path ? DIRECTORY_SEPARATOR.$path : $path);
+        return app()->make('path.public').($path ? DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR) : $path);
     }
 }
 
