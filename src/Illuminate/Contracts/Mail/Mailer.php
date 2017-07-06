dc2792690fbbbb5f55327d9e59d3045e190d27a1
Fernando Henrique Bandeira <fernando.bandeira@outlook.com>
2017-07-06 08:42:42 -0300

    Add new methods to mailer contract

--- src/Illuminate/Contracts/Mail/Mailer.php
+++ src/Illuminate/Contracts/Mail/Mailer.php
@@ -5,13 +5,75 @@ namespace Illuminate\Contracts\Mail;
 interface Mailer
 {
     /**
+     * Set the global from address and name.
+     *
+     * @param  string  $address
+     * @param  string|null  $name
+     * @return void
+     */
+    public function alwaysFrom($address, $name = null);
+
+    /**
+     * Set the global reply-to address and name.
+     *
+     * @param  string  $address
+     * @param  string|null  $name
+     * @return void
+     */
+    public function alwaysReplyTo($address, $name = null);
+
+    /**
+     * Set the global to address and name.
+     *
+     * @param  string  $address
+     * @param  string|null  $name
+     * @return void
+     */
+    public function alwaysTo($address, $name = null);
+
+    /**
+     * Begin the process of mailing a mailable class instance.
+     *
+     * @param  mixed  $users
+     * @return \Illuminate\Mail\PendingMail
+     */
+    public function to($users);
+
+    /**
+     * Begin the process of mailing a mailable class instance.
+     *
+     * @param  mixed  $users
+     * @return \Illuminate\Mail\PendingMail
+     */
+    public function bcc($users);
+
+    /**
      * Send a new message when only a raw text part.
      *
      * @param  string  $text
-     * @param  \Closure|string  $callback
-     * @return int
+     * @param  mixed  $callback
+     * @return void
      */
     public function raw($text, $callback);
+
+    /**
+     * Send a new message when only a plain part.
+     *
+     * @param  string  $view
+     * @param  array  $data
+     * @param  mixed  $callback
+     * @return void
+     */
+    public function plain($view, array $data, $callback);
+
+    /**
+     * Render the given message as a view.
+     *
+     * @param  string|array  $view
+     * @param  array  $data
+     * @return \Illuminate\View\View
+     */
+    public function render($view, array $data = []);
 
     /**
      * Send a new message using a view.
