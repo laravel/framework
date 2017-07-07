acf265af43ff045b3f0a38bda99defbce801d092
Mihail Ilinov <mihail@ilinov.eu>
2017-07-07 16:53:09 +0300

    Change option "f" to "fa" for factory
    and leaving "f" for force

--- src/Illuminate/Foundation/Console/ModelMakeCommand.php
+++ src/Illuminate/Foundation/Console/ModelMakeCommand.php
@@ -137,7 +137,7 @@ class ModelMakeCommand extends GeneratorCommand
 
             ['controller', 'c', InputOption::VALUE_NONE, 'Create a new controller for the model'],
 
-            ['factory', 'f', InputOption::VALUE_NONE, 'Create a new factory for the model'],
+            ['factory', 'fa', InputOption::VALUE_NONE, 'Create a new factory for the model'],
 
             ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the model already exists.'],
 
