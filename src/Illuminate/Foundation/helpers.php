c4841d2e154d1332f575c5a49e255d048aaf0d97
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-07-18 12:08:21 +0200

    Fix false positive

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -443,7 +443,7 @@ if (! function_exists('has_errors')) {
     function has_errors($key = null, $bag = null)
     {
         $errors = session('errors');
-        if (is_null($errors)) {
+        if (is_null($errors) && ! session('errors')->any()) {
             return false;
         } elseif ($key !== null) {
             return is_null($bag) ? $errors->has($key) : $errors->$bag->has($key);
