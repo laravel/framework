c6349e6be052e49eb15df032e110262a22807367
Taylor Otwell <taylor@laravel.com>
2017-07-18 11:48:38 -0500

    formatting

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -951,8 +951,8 @@ trait HasAttributes
      */
     public function isDirty($attributes = null)
     {
-        return $this->hasChanges($this->getDirty(),
-            is_array($attributes) ? $attributes : func_get_args()
+        return $this->hasChanges(
+            $this->getDirty(), is_array($attributes) ? $attributes : func_get_args()
         );
     }
 
@@ -975,9 +975,37 @@ trait HasAttributes
      */
     public function wasChanged($attributes = null)
     {
-        return $this->hasChanges($this->getChanges(),
-            is_array($attributes) ? $attributes : func_get_args()
+        return $this->hasChanges(
+            $this->getChanges(), is_array($attributes) ? $attributes : func_get_args()
         );
+    }
+
+    /**
+     * Determine if the given attributes were changed.
+     *
+     * @param  array  $changes
+     * @param  array|string|null  $attributes
+     * @return bool
+     */
+    protected function hasChanges($changes, $attributes = null)
+    {
+        // If no specific attributes were provided, we will just see if the dirty array
+        // already contains any attributes. If it does we will just return that this
+        // count is greater than zero. Else, we need to check specific attributes.
+        if (empty($attributes)) {
+            return count($changes) > 0;
+        }
+
+        // Here we will spin through every attribute and see if this is in the array of
+        // dirty attributes. If it is, we will return true and if we make it through
+        // all of the attributes for the entire array we will return false at end.
+        foreach (Arr::wrap($attributes) as $attribute) {
+            if (array_key_exists($attribute, $changes)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -1107,33 +1135,5 @@ trait HasAttributes
         preg_match_all('/(?<=^|;)get([^;]+?)Attribute(;|$)/', implode(';', get_class_methods($class)), $matches);
 
         return $matches[1];
-    }
-
-    /**
-     * Determine if the given attributes were changed.
-     *
-     * @param  array  $changes
-     * @param  array|string|null  $attributes
-     * @return bool
-     */
-    protected function hasChanges($changes, $attributes = null)
-    {
-        // If no specific attributes were provided, we will just see if the dirty array
-        // already contains any attributes. If it does we will just return that this
-        // count is greater than zero. Else, we need to check specific attributes.
-        if (empty($attributes)) {
-            return count($changes) > 0;
-        }
-
-        // Here we will spin through every attribute and see if this is in the array of
-        // dirty attributes. If it is, we will return true and if we make it through
-        // all of the attributes for the entire array we will return false at end.
-        foreach (Arr::wrap($attributes) as $attribute) {
-            if (array_key_exists($attribute, $changes)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
