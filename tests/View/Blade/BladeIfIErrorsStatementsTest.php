4c384aad5321e959de793b4fee7fb428774d0780
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-07-19 14:33:46 +0200

    Add tests

---
+++ tests/View/Blade/BladeIfIErrorsStatementsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Illuminate\Tests\Blade;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Illuminate\View\Compilers\BladeCompiler;
+
+class BladeIfErrorsStatementsTest extends TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testIfStatementsAreCompiled()
+    {
+        $compiler = new BladeCompiler($this->getFiles(), __DIR__);
+        $string = '@errors ($key, $bag)
+breeze
+@enderrors';
+        $expected = '<?php if(has_errors($key, $bag)): ?>
+breeze
+<?php endif; ?>';
+        $this->assertEquals($expected, $compiler->compileString($string));
+    }
+
+    protected function getFiles()
+    {
+        return m::mock('Illuminate\Filesystem\Filesystem');
+    }
+}
