01d0cecf7e523fc8dac5d1f1860cb44fcb4ab0bf
Mohamed Said <themsaid@gmail.com>
2017-07-19 23:22:12 +0200

    Revert "Apply fixes from StyleCI (#20175)"
    This reverts commit c33d9cb575fb85583d373d6fa10f9bfc62c98aa0.

--- tests/Foundation/FoundationFormRequestTest.php
+++ tests/Foundation/FoundationFormRequestTest.php
@@ -10,6 +10,7 @@ use Illuminate\Container\Container;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Validation\Factory as ValidationFactory;
 use Illuminate\Contracts\Validation\Factory as ValidationFactoryContract;
@@ -54,6 +55,7 @@ class FoundationFormRequestTest extends TestCase
     {
         $this->createRequest([], FoundationTestFormRequestForbiddenStub::class)->validate();
     }
+
 
     public function test_prepare_for_validation_runs_before_validation()
     {
