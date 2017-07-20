1890b89c9c4048429311b6d773b11c4888c5af0b
Mohamed Said <themohamedsaid@gmail.com>
2017-07-20 16:32:38 +0200

    remove duplicated broken test

--- tests/Database/DatabaseEloquentBelongsToManyTest.php
+++ tests/Database/DatabaseEloquentBelongsToManyTest.php
@@ -548,21 +548,6 @@ class DatabaseEloquentBelongsToManyTest extends TestCase
         $this->assertEquals(['attached' => [4], 'detached' => [1], 'updated' => []], $relation->sync([2, 3 => ['baz' => 'qux'], 4 => ['foo' => 'bar']]));
     }
 
-    public function testTouchMethodSyncsTimestamps()
-    {
-        $relation = $this->getRelation();
-        $relation->getRelated()->shouldReceive('getUpdatedAtColumn')->andReturn('updated_at');
-        $relation->getRelated()->shouldReceive('freshTimestampString')->andReturn('100');
-        $relation->getRelated()->shouldReceive('getQualifiedKeyName')->andReturn('table.id');
-        $relation->getQuery()->shouldReceive('select')->once()->with('table.id')->andReturn($relation->getQuery());
-        $relation->getQuery()->shouldReceive('pluck')->once()->with('id')->andReturn([1, 2, 3]);
-        $relation->getRelated()->shouldReceive('newQuery')->once()->andReturn($query = m::mock('stdClass'));
-        $query->shouldReceive('whereIn')->once()->with('id', [1, 2, 3])->andReturn($query);
-        $query->shouldReceive('update')->once()->with(['updated_at' => '100']);
-
-        $relation->touch();
-    }
-
     /**
      * @dataProvider toggleMethodListProvider
      */
