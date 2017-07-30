ae5b9564e7ce9e326f6abe74689af396f2cd7b33
ighotouch <ighotouch@gmail.com>
2017-07-30 11:39:01 +0100

    added Sendgrid transport class

---
+++ src/Illuminate/Mail/Transport/SendgridTransport.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Illuminate\Mail\Transport;
+
+use Swift_Mime_Message;
+use GuzzleHttp\ClientInterface;
+
+class SendgridTransport extends Transport
+{
+    /**
+     * Guzzle client instance.
+     *
+     * @var \GuzzleHttp\ClientInterface
+     */
+    protected $client;
+
+    /**
+     * The Sendgrid API key.
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * Create a new Sendgrid transport instance.
+     *
+     * @param  \GuzzleHttp\ClientInterface  $client
+     * @param  string  $key
+     * @return void
+     */
+    public function __construct(ClientInterface $client, $key)
+    {
+        $this->key = $key;
+        $this->client = $client;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    {
+        $this->beforeSendPerformed($message);
+
+
+        $this->client->post( 'https://api.sendgrid.com/v3/mail/send', [
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer '+$this->key
+            ],
+            'json' => [
+                "personalizations" => [
+                    [
+                        "to" => [
+                            ["email" =>$this->getTo($message)]
+                        ]
+                    ]
+                ],
+                "content" => [
+                    [
+                        "type" => "text/html",
+                        "value" =>$message->toString()
+                    ]
+                ]
+
+            ]
+
+        ]);
+
+        $this->sendPerformed($message);
+
+        return $this->numberOfRecipients($message);
+    }
+
+    /**
+     * Get all the addresses this message should be sent to.
+     *
+     * Note that Sendgrid still respects CC, BCC headers in raw message itself.
+     *
+     * @param  \Swift_Mime_Message $message
+     * @return array
+     */
+    protected function getTo(Swift_Mime_Message $message)
+    {
+        $to = [];
+
+        if ($message->getTo()) {
+            $to = array_merge($to, array_keys($message->getTo()));
+        }
+
+        if ($message->getCc()) {
+            $to = array_merge($to, array_keys($message->getCc()));
+        }
+
+        if ($message->getBcc()) {
+            $to = array_merge($to, array_keys($message->getBcc()));
+        }
+
+        return $to;
+    }
+
+    /**
+     * Get the API key being used by the transport.
+     *
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Set the API key being used by the transport.
+     *
+     * @param  string  $key
+     * @return string
+     */
+    public function setKey($key)
+    {
+        return $this->key = $key;
+    }
+}
