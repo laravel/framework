e59981477f0b710dd40708952e10f96fbf1f5361
Tim MacDonald <tim.mac7@me.com>
2017-08-01 22:58:10 +1000

    [5.4] Eloquent model 'isNot' complement method (#20354)
    * add model 'isNot' complement method
    
    * style CI fix
    
    * Update Model.php

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1002,6 +1002,17 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
     }
 
     /**
+     * Determine if two models are not the same.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return bool
+     */
+    public function isNot(Model $model)
+    {
+        return ! $this->is($model);
+    }
+
+    /**
      * Get the database connection for the model.
      *
      * @return \Illuminate\Database\Connection
