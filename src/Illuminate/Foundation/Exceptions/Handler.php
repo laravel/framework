15841e676c1f2226cb60fe558ddea007cd6b27bf
Lucas Michot <lucas@semalead.com>
2017-08-06 15:53:08 +0200

    No need to use a collection for that (#20434)

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -134,7 +134,7 @@ class Handler implements ExceptionHandlerContract
     {
         $dontReport = array_merge($this->dontReport, $this->internalDontReport);
 
-        return ! is_null(collect($dontReport)->first(function ($type) use ($e) {
+        return ! is_null(Arr::first($dontReport, function ($type) use ($e) {
             return $e instanceof $type;
         }));
     }
