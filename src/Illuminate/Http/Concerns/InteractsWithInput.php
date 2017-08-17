acf23e06650106981b420a353368147e6bb45189
Taylor Otwell <taylor@laravel.com>
2017-08-17 09:53:09 -0500

    format method

--- src/Illuminate/Http/Concerns/InteractsWithInput.php
+++ src/Illuminate/Http/Concerns/InteractsWithInput.php
@@ -143,6 +143,16 @@ trait InteractsWithInput
     }
 
     /**
+     * Get the keys for all of the input and files.
+     *
+     * @return array
+     */
+    public function keys()
+    {
+        return array_merge(array_keys($this->input()), $this->files->keys());
+    }
+
+    /**
      * Get all of the input and files for the request.
      *
      * @param  array|mixed  $keys
@@ -163,16 +173,6 @@ trait InteractsWithInput
         }
 
         return $results;
-    }
-
-    /**
-     * Get the keys for all input and files for the request.
-     *
-     * @return array
-     */
-    public function keys()
-    {
-        return array_merge(array_keys($this->input()), $this->files->keys());
     }
 
     /**
