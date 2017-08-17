d3e5ab623dbb4509ac689bd7e9b5fb9a8b9eea8b
Adam Patterson <hello@adampatterson.ca>
2017-08-17 07:42:24 -0600

    removed logging from mix()

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -570,17 +570,8 @@ if (! function_exists('mix')) {
         $manifest = $manifests[$manifestPath];
 
         if (! isset($manifest[$path])) {
-
-            $mixMessage =  "Unable to locate Mix file: {$path}. Please check your ".
-                           'webpack.mix.js output paths and try again.';
-
-            report(new Exception($mixMessage));
-
-            if (! app('config')->get('app.debug')) {
-                app('log')->info($mixMessage);
-
-                return $path;
-            }
+            report(new Exception("Unable to locate Mix file: {$path}. Please check your ".
+                           'webpack.mix.js output paths and try again.'));
         }
 
         return new HtmlString($manifestDirectory.$manifest[$path]);
