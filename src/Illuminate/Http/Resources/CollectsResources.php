d26149d51f37b1bbc6da98d0c18db3ac8df0cbed
Taylor Otwell <taylor@laravel.com>
2017-08-23 13:42:25 -0500

    wip

--- src/Illuminate/Http/Resources/CollectsResources.php
+++ src/Illuminate/Http/Resources/CollectsResources.php
@@ -4,7 +4,9 @@ namespace Illuminate\Http\Resources;
 
 use Illuminate\Support\Str;
 use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\AbstractPaginator;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 trait CollectsResources
 {
@@ -18,9 +20,25 @@ trait CollectsResources
     {
         $this->collection = $resource->mapInto($this->collects());
 
+        if ($this->containsModels()) {
+            $this->collection = new EloquentCollection($this->collection->all());
+        }
+
         return $resource instanceof AbstractPaginator
                     ? $resource->setCollection($this->collection)
                     : $this->collection;
+    }
+
+    /**
+     * Determine if the collection contains models.
+     *
+     * @return bool
+     */
+    protected function containsModels()
+    {
+        return $this->collection->contains(function ($item) {
+            return $item->resource instanceof Model;
+        });
     }
 
     /**
