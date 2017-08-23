55594aa755e80aabec9fe2061890592537d4534e
Manash Sonowal <manash149@gmail.com>
2017-08-23 15:48:22 +0530

    Update Router.php

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -203,7 +203,7 @@ class Router implements RegistrarContract, BindingRegistrar
      */
     public function any($uri, $action = null)
     {
-        $verbs = self::verbs;
+        $verbs = self::$verbs;
 
         return $this->addRoute($verbs, $uri, $action);
     }
