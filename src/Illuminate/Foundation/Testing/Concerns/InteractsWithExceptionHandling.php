1316741e30f42215d1621d2a9d466e723e2b3bb4
Duilio Palacios <duilio@styde.net>
2017-08-24 14:32:12 +0100

    Add handleExceptions to select which exceptions should be handled during a test

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithExceptionHandling.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithExceptionHandling.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Foundation\Testing\Concerns;
 
 use Exception;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Symfony\Component\Console\Application as ConsoleApplication;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -37,11 +38,41 @@ trait InteractsWithExceptionHandling
      */
     protected function withoutExceptionHandling()
     {
+        return $this->turnOffExceptionHandling();
+    }
+
+    /**
+     * @param  array  $exceptions
+     * @return $this
+     */
+    protected function handleExceptions(array $exceptions)
+    {
+        return $this->turnOffExceptionHandling($exceptions);
+    }
+
+    protected function handleValidationException()
+    {
+        return $this->handleExceptions([ValidationException::class]);
+    }
+
+    /**
+     * Disable exception handling for the test.
+     *
+     * @param  array  $except
+     * @return $this
+     */
+    protected function turnOffExceptionHandling(array $except = [])
+    {
         $this->previousExceptionHandler = app(ExceptionHandler::class);
 
-        $this->app->instance(ExceptionHandler::class, new class implements ExceptionHandler {
-            public function __construct()
+        $this->app->instance(ExceptionHandler::class, new class($this->previousExceptionHandler, $except) implements ExceptionHandler {
+            protected $except;
+            protected $previousHandler;
+
+            public function __construct($previousHandler, $except = [])
             {
+                $this->previousHandler = $previousHandler;
+                $this->except = $except;
             }
 
             public function report(Exception $e)
@@ -54,6 +85,12 @@ trait InteractsWithExceptionHandling
                     throw new NotFoundHttpException(
                         "{$request->method()} {$request->url()}", null, $e->getCode()
                     );
+                }
+
+                foreach ($this->except as $class) {
+                    if ($e instanceof $class) {
+                        return $this->previousHandler->render($request, $e);
+                    }
                 }
 
                 throw $e;
