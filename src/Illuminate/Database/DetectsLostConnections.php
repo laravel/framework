a046a2f1c246fb7aa5eb7d2b6a94c93fabb064c0
Maksim Martianov <maxmru@yandex.ru>
2017-08-25 10:42:27 +0700

    [5.4] fix for broken rabbitmq connections

--- src/Illuminate/Database/DetectsLostConnections.php
+++ src/Illuminate/Database/DetectsLostConnections.php
@@ -29,6 +29,7 @@ trait DetectsLostConnections
             'Error writing data to the connection',
             'Resource deadlock avoided',
             'Transaction() on null',
+            'Broken pipe or closed connection',
         ]);
     }
 }
