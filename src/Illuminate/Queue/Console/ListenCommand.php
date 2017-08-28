8929da5e3aa9507079d03a7e817ee0bc22463504
Marcin Nabiałek <dev@nabialek.org>
2017-08-28 23:32:02 +0200

    Make it clear that delay time is in seconds (#20801)

--- src/Illuminate/Queue/Console/ListenCommand.php
+++ src/Illuminate/Queue/Console/ListenCommand.php
@@ -15,7 +15,7 @@ class ListenCommand extends Command
      */
     protected $signature = 'queue:listen
                             {connection? : The name of connection}
-                            {--delay=0 : Amount of time to delay failed jobs}
+                            {--delay=0 : The number of seconds to delay failed jobs}
                             {--force : Force the worker to run even in maintenance mode}
                             {--memory=128 : The memory limit in megabytes}
                             {--queue= : The queue to listen on}
