4020c4b5a72863a3a7f74b7d604f67b90ac867c3
Caleb Porzio <calebporzio@gmail.com>
2017-08-29 09:29:44 -0400

    Defer loading of directive to DuskServiceProvider

--- src/Illuminate/View/Compilers/Concerns/CompilesDusk.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesDusk.php
@@ -5,21 +5,14 @@ namespace Illuminate\View\Compilers\Concerns;
 trait CompilesDusk
 {
     /**
-     * Compile dusk hooks into html "dusk" attributes.
+     * Compile dusk hooks into empty strings. This is needed
+     * because Dusk doesn't register itself in production.
      *
      * @param  string  $expression
      * @return string
      */
     protected function compileDusk($expression)
     {
-        $segments = array_map('trim', explode(',', preg_replace("/[\(\)\\\"\']/", '', $expression)));
-
-        $selector = $segments[0];
-
-        $stringifiedEnvironments = array_map(function ($environment) {
-            return "'{$environment}'";
-        }, array_merge(['testing', 'local'], array_slice($segments, 1)));
-
-        return "<?php echo app()->environment(".implode(', ', $stringifiedEnvironments).") ? 'dusk=\"{$selector}\"' : ''; ?>";
+        return '';
     }
 }
--- tests/View/Blade/BladeDuskTest.php
+++ tests/View/Blade/BladeDuskTest.php
@@ -6,25 +6,6 @@ class BladeDuskTest extends AbstractBladeTestCase
 {
     public function testDuskIsCompiled()
     {
-        $this->assertEquals(
-            "<?php echo app()->environment('testing', 'local') ? 'dusk=\"foo\"' : ''; ?>",
-            $this->compiler->compileString("@dusk('foo')")
-        );
-        $this->assertEquals(
-            "<?php echo app()->environment('testing', 'local') ? 'dusk=\"foo\"' : ''; ?>",
-            $this->compiler->compileString("@dusk(\"foo\")")
-        );
-        $this->assertEquals(
-            "<?php echo app()->environment('testing', 'local', 'staging') ? 'dusk=\"foo\"' : ''; ?>",
-            $this->compiler->compileString("@dusk('foo', 'staging')")
-        );
-        $this->assertEquals(
-            "<?php echo app()->environment('testing', 'local', 'staging') ? 'dusk=\"foo\"' : ''; ?>",
-            $this->compiler->compileString("@dusk('foo', \"staging\")")
-        );
-        $this->assertEquals(
-            "<?php echo app()->environment('testing', 'local', 'staging', 'production') ? 'dusk=\"foo\"' : ''; ?>",
-            $this->compiler->compileString("@dusk('foo', 'staging', 'production')")
-        );
+        $this->assertEquals('', $this->compiler->compileString("@dusk('foo')"));
     }
 }
