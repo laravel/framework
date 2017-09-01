b87ac8dca35f1fc6c340233d11e8b5733ec54765
Lucas Michot <lucas@semalead.com>
2017-09-01 08:30:03 +0200

    [5.5] DelegatesToResource: fix offsetExists. (#20887)
    * Fix offsetExists.
    
    * Switch argumebts

--- src/Illuminate/Http/Resources/DelegatesToResource.php
+++ src/Illuminate/Http/Resources/DelegatesToResource.php
@@ -46,7 +46,7 @@ trait DelegatesToResource
      */
     public function offsetExists($offset)
     {
-        return array_key_exists($this->resource[$offset]);
+        return array_key_exists($offset, $this->resource);
     }
 
     /**
