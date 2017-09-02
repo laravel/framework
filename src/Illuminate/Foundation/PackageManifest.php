4a6f1f2613f2ca5a1ef3792b019a769d6a269cda
Taylor Otwell <taylorotwell@gmail.com>
2017-09-02 13:33:29 +0200

    formatting

--- src/Illuminate/Foundation/PackageManifest.php
+++ src/Illuminate/Foundation/PackageManifest.php
@@ -119,9 +119,7 @@ class PackageManifest
         $this->write(collect($packages)->mapWithKeys(function ($package) {
             return [$this->format($package['name']) => $package['extra']['laravel'] ?? []];
         })->each(function ($configuration) use (&$ignore) {
-            if ($configuration['dont-discover'] ?? false) {
-                $ignore += $configuration['dont-discover'];
-            }
+            $ignore += $configuration['dont-discover'] ?? [];
         })->reject(function ($configuration, $package) use ($ignore, $ignoreAll) {
             return $ignoreAll || in_array($package, $ignore);
         })->filter()->all());
