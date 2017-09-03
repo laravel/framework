94cf0655719b6ff3af97a609f92fbd2e18a9b03b
Marcin Nabiałek <dev@nabialek.org>
2017-09-03 19:54:57 +0200

    Remove connection from Migration class

--- src/Illuminate/Database/Migrations/Migration.php
+++ src/Illuminate/Database/Migrations/Migration.php
@@ -4,20 +4,4 @@ namespace Illuminate\Database\Migrations;
 
 abstract class Migration
 {
-    /**
-     * The name of the database connection to use.
-     *
-     * @var string
-     */
-    protected $connection;
-
-    /**
-     * Get the migration connection name.
-     *
-     * @return string
-     */
-    public function getConnection()
-    {
-        return $this->connection;
-    }
 }
