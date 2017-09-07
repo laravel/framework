779a98d3b2ebed2128db1a668dcc10a65c9b01be
Taylor Otwell <taylor@laravel.com>
2017-09-07 08:50:21 -0500

    version

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -29,7 +29,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      *
      * @var string
      */
-    const VERSION = '5.5.2';
+    const VERSION = '5.5.3';
 
     /**
      * The base path for the Laravel installation.
