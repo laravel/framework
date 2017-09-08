f6901142e7ee20cd6b0b47619f9f3e06001e2e43
Chris Morrell <chris@cmorrell.com>
2017-09-08 14:42:59 -0400

    Added tests

---
+++ tests/Foundation/FoundationComposesTemplateMethodsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Illuminate\Tests\Foundation;
+
+use Illuminate\Foundation\Testing\Concerns\ComposesTemplateMethods;
+use PHPUnit\Framework\TestCase;
+
+class FoundationComposesTemplateMethodsTest extends TestCase
+{
+    use ComposesTemplateMethods, TestTraitOne, TestTraitTwo;
+
+    public function testCallsComposedSetUpMethods()
+    {
+        $this->callTraitTemplateMethods('setUp');
+
+        $this->assertTrue($this->testTraitOneSetUp);
+        $this->assertTrue($this->testTraitTwoSetUp);
+    }
+
+    public function testCallsComposedTearDownMethods()
+    {
+        $this->callTraitTemplateMethods('tearDown');
+
+        $this->assertFalse($this->testTraitOneSetUp);
+        $this->assertFalse($this->testTraitTwoSetUp);
+    }
+}
+
+trait TestTraitOne
+{
+    public $testTraitOneSetUp;
+
+    public function setUpTestTraitOne()
+    {
+        $this->testTraitOneSetUp = true;
+    }
+
+    public function tearDownTestTraitOne()
+    {
+        $this->testTraitOneSetUp = false;
+    }
+}
+
+trait TestTraitTwo
+{
+    public $testTraitTwoSetUp;
+
+    public function setUpTestTraitTwo()
+    {
+        $this->testTraitTwoSetUp = true;
+    }
+
+    public function tearDownTestTraitTwo()
+    {
+        $this->testTraitTwoSetUp = false;
+    }
+}
