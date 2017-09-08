b912ab61734746460c48d8548860e757a4bf39c4
Blake Kostner <btkostner@gmail.com>
2017-09-08 11:16:26 -0700

    add a validator service provider for registering class based rules

---
+++ src/Illuminate/Foundation/Support/Providers/ValidatorServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Illuminate\Foundation\Support\Providers;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
+
+class ValidatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Mappings for custom rules for the application.
+     *
+     * @var array
+     */
+    protected $rules = [];
+
+    /**
+     * Adds application rules to validator.
+     *
+     * @return void
+     */
+    public function bootRules()
+    {
+        foreach ($this->rules as $rule => $class) {
+            $instance = $this->app->make($class);
+
+            Validator::extend($rule, [$instance, 'passes'], $instance->message());
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->bootRules();
+    }
+}
