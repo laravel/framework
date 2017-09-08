81cc61629edb8eb63edae5c82c903b0a615f5623
n1215 <knakae1215@gmail.com>
2017-09-08 13:08:07 +0900

    [5.5] Add static factory methods to Optional class

---
+++ tests/Support/SupportOptionalTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Illuminate\Tests\Support;
+
+use Illuminate\Support\Optional;
+use PHPUnit\Framework\TestCase;
+
+class SupportOptionalTest extends TestCase
+{
+    /**
+     * @param mixed $value
+     * @dataProvider nullableValueProvider
+     */
+    public function testOf($value)
+    {
+        $this->assertEquals(new Optional($value), Optional::of($value));
+    }
+
+    public function nullableValueProvider()
+    {
+        return [
+            [null],
+            [5],
+            [new \stdClass()],
+        ];
+    }
+
+    /**
+     * @param mixed $value
+     * @dataProvider someValueProvider
+     */
+    public function testSome($value)
+    {
+        $this->assertEquals(new Optional($value), Optional::some($value));
+    }
+
+    public function someValueProvider()
+    {
+        return [
+            [5],
+            [new \stdClass()],
+        ];
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Null value passed to some() method. Use none() instead.
+     */
+    public function testSomeThrowsExceptionWhenNullGiven()
+    {
+        Optional::some(null);
+    }
+
+    public function testNone()
+    {
+        $this->assertEquals(new Optional(null), Optional::none());
+    }
+}
