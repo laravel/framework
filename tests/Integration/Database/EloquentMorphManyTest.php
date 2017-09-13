24aa2f89451f76a71bd217be85eaf0daebfb3e56
Mohamed Said <themohamedsaid@gmail.com>
2017-09-13 20:18:46 +0200

    fix style

--- tests/Integration/Database/EloquentMorphManyTest.php
+++ tests/Integration/Database/EloquentMorphManyTest.php
@@ -4,11 +4,8 @@ namespace Illuminate\Tests\Integration\Database\EloquentMorphManyTest;
 
 use Illuminate\Support\Carbon;
 use Orchestra\Testbench\TestCase;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Relations\Pivot;
 
 /**
  * @group integration
