c8716548a8999b369effc70af22834c7cb163f2d
Gökhan Güney <gokhanguneygg@gmail.com>
2017-09-22 12:37:16 +0300

    Validation message was static.

--- src/Illuminate/Validation/ValidationException.php
+++ src/Illuminate/Validation/ValidationException.php
@@ -52,7 +52,7 @@ class ValidationException extends Exception
      */
     public function __construct($validator, $response = null, $errorBag = 'default')
     {
-        parent::__construct('The given data was invalid.');
+        parent::__construct(trans('validation.message'));
 
         $this->response = $response;
         $this->errorBag = $errorBag;
