4d439f1c70358c42847c8296613642b1c81fa359
Diego SIlveira Mota <diego.smota@gmail.com>
2017-09-22 22:15:50 -0300

    Update Route.php
    Fix comments adding the redirect and view methods

--- src/Illuminate/Support/Facades/Route.php
+++ src/Illuminate/Support/Facades/Route.php
@@ -22,6 +22,8 @@ namespace Illuminate\Support\Facades;
  * @method \Illuminate\Support\Facades\Route name(string $value)
  * @method \Illuminate\Support\Facades\Route namespace(string $value)
  * @method \Illuminate\Routing\Route group(string $value)
+ * @method \Illuminate\Support\Facades\Route redirect(string $uri, string $destination, int $status = 301)
+ * @method \Illuminate\Support\Facades\Route view(string $uri, string $view, array $data = [])
  *
  * @see \Illuminate\Routing\Router
  */
