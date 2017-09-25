7be9ab2ab28766748504f86f953d83f8c02b387f
Stéphane HULARD <s.hulard@chstudio.fr>
2017-09-25 11:49:17 +0200

    Ensure user is logged.
    Check the user is logged before expecting instance type.

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithAuthentication.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithAuthentication.php
@@ -82,6 +82,11 @@ trait InteractsWithAuthentication
     {
         $expected = $this->app->make('auth')->guard($guard)->user();
 
+        $this->assertNotNull(
+            $expected,
+            'The current user is not authenticated.'
+        );
+
         $this->assertInstanceOf(
             get_class($expected), $user,
             'The currently authenticated user is not who was expected'
