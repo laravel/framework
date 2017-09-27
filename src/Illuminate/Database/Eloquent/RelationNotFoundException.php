a0879454ed09da5daabd037156e37310fdc0482a
agonzalez <agonzalez@mileyenda.com>
2017-09-27 12:05:33 +0200

    Add getRelation and getModel getters in RelationNotFoundException

--- src/Illuminate/Database/Eloquent/RelationNotFoundException.php
+++ src/Illuminate/Database/Eloquent/RelationNotFoundException.php
@@ -7,6 +7,20 @@ use RuntimeException;
 class RelationNotFoundException extends RuntimeException
 {
     /**
+     * Name of the affected Eloquent model.
+     *
+     * @var string
+     */
+    protected $model;
+
+    /**
+     * Name of the affected relation.
+     *
+     * @var string
+     */
+    protected $relation;
+
+    /**
      * Create a new exception instance.
      *
      * @param  mixed  $model
@@ -15,8 +29,31 @@ class RelationNotFoundException extends RuntimeException
      */
     public static function make($model, $relation)
     {
+        $this->model = $model;
+        $this->relation = $relation;
+
         $class = get_class($model);
 
         return new static("Call to undefined relationship [{$relation}] on model [{$class}].");
+    }
+
+    /**
+     * Get the affected Eloquent model.
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Get the affected Eloquent model IDs.
+     *
+     * @return int|array
+     */
+    public function getRelation()
+    {
+        return $this->relation;
     }
 }
