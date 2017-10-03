74f6c311ba6a5e854e043b9aa53c02ede20fc646
Charles Peterson <cpeterson@brokerbin.com>
2017-10-03 09:58:02 -0500

    withMessage magic method

--- src/Illuminate/Http/RedirectResponse.php
+++ src/Illuminate/Http/RedirectResponse.php
@@ -12,6 +12,9 @@ use Illuminate\Contracts\Support\MessageProvider;
 use Symfony\Component\HttpFoundation\File\UploadedFile as SymfonyUploadedFile;
 use Symfony\Component\HttpFoundation\RedirectResponse as BaseRedirectResponse;
 
+/**
+ * @method public \Illuminate\Http\RedirectResponse withMessage(string $message) Create and return an un-saved model instance.
+ */
 class RedirectResponse extends BaseRedirectResponse
 {
     use ResponseTrait, Macroable {
