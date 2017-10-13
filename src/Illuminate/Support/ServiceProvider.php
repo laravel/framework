7f084cd964894c62b7c11f76f1f244439a1ae4d2
Yoram <yoramdelangen@gmail.com>
2017-10-13 14:42:08 +0200

    [5.5] Check for vendor views for each paths given in config/view.php (#21636)
    * Check for vendor views for each paths given in config/view.php
    
    * CS Fix

--- src/Illuminate/Support/ServiceProvider.php
+++ src/Illuminate/Support/ServiceProvider.php
@@ -81,8 +81,10 @@ abstract class ServiceProvider
      */
     protected function loadViewsFrom($path, $namespace)
     {
-        if (is_dir($appPath = $this->app->resourcePath().'/views/vendor/'.$namespace)) {
-            $this->app['view']->addNamespace($namespace, $appPath);
+        foreach ($this->app->config['view']['paths'] as $viewPath) {
+            if (is_dir($appPath = $viewPath.'/vendor/'.$namespace)) {
+                $this->app['view']->addNamespace($namespace, $appPath);
+            }
         }
 
         $this->app['view']->addNamespace($namespace, $path);
