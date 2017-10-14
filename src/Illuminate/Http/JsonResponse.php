ee96f18905dd9e3273087c000e26e45111121568
Adam <adammalbright@gmail.com>
2017-10-13 21:40:32 -0700

    Style update

--- src/Illuminate/Http/JsonResponse.php
+++ src/Illuminate/Http/JsonResponse.php
@@ -4,9 +4,9 @@ namespace Illuminate\Http;
 
 use JsonSerializable;
 use InvalidArgumentException;
+use Illuminate\Support\Traits\Macroable;
 use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Support\Traits\Macroable;
 use Symfony\Component\HttpFoundation\JsonResponse as BaseJsonResponse;
 
 class JsonResponse extends BaseJsonResponse
