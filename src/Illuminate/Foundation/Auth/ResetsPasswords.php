23b273686e6b90ea1d8707b6096add73f025b8b1
Hafez Divandari <hafezdivandari@gmail.com>
2017-10-14 13:18:04 +0330

    Add more spaces

--- src/Illuminate/Foundation/Auth/ResetsPasswords.php
+++ src/Illuminate/Foundation/Auth/ResetsPasswords.php
@@ -116,7 +116,7 @@ trait ResetsPasswords
     /**
      * Get the response for a successful password reset.
      *
-     * @param  \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @param  string  $response
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
      */
@@ -129,7 +129,7 @@ trait ResetsPasswords
     /**
      * Get the response for a failed password reset.
      *
-     * @param  \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @param  string  $response
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
      */
