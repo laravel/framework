92e56f5c6c1ab6dffdcfa59a4179cd316d369499
Chen Alon <chen@bwebi.co.il>
2017-10-15 17:26:58 +0300

    Add translations to default email view 
    Add translation to the default email view instead of the hard-coded text in order to make it easier to use the MailMessage class without the need to publish the mail components and customize it each time.

--- src/Illuminate/Notifications/resources/views/email.blade.php
+++ src/Illuminate/Notifications/resources/views/email.blade.php
@@ -4,9 +4,9 @@
 # {{ $greeting }}
 @else
 @if ($level == 'error')
-# Whoops!
+# @lang('mail.whoops')
 @else
-# Hello!
+# @lang('mail.hello')
 @endif
 @endif
 
@@ -19,16 +19,16 @@
 {{-- Action Button --}}
 @isset($actionText)
 <?php
-    switch ($level) {
-        case 'success':
-            $color = 'green';
-            break;
-        case 'error':
-            $color = 'red';
-            break;
-        default:
-            $color = 'blue';
-    }
+switch ($level) {
+    case 'success':
+        $color = 'green';
+        break;
+    case 'error':
+        $color = 'red';
+        break;
+    default:
+        $color = 'blue';
+}
 ?>
 @component('mail::button', ['url' => $actionUrl, 'color' => $color])
 {{ $actionText }}
@@ -45,14 +45,13 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-Regards,<br>{{ config('app.name') }}
+@lang('mail.regards'),<br>{{ config('app.name') }}
 @endif
 
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+@lang('mail.copy_paste_url', ['actionText' => $actionText]): [{{ $actionUrl }}]({{ $actionUrl }})
 @endcomponent
 @endisset
 @endcomponent
