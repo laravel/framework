3423fdc9fd0a4f5e6e35c1313d62ea5e5501f01e
Paweł Mysior <pawelmysior@gmail.com>
2017-10-16 14:21:59 +0200

    Rename example component to keep consistency with laravel/laravel repo (#21685)

--- src/Illuminate/Foundation/Console/Presets/vue-stubs/app.js
+++ src/Illuminate/Foundation/Console/Presets/vue-stubs/app.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('example', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
