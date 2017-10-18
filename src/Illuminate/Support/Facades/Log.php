89465599b7a2bdd634eb3f319d0a4a053e406fca
Charles Peterson <artistan@gmail.com>
2017-10-18 13:12:02 -0500

    Log Facade methods
    docblock definition for the static facade methods

--- src/Illuminate/Support/Facades/Log.php
+++ src/Illuminate/Support/Facades/Log.php
@@ -6,6 +6,25 @@ use Psr\Log\LoggerInterface;
 
 /**
  * @see \Illuminate\Log\Writer
+ *
+ * @method static boolean debug($message, array $context = array())
+ * @method static boolean info($message, array $context = array())
+ * @method static boolean notice($message, array $context = array())
+ * @method static boolean warning($message, array $context = array())
+ * @method static boolean error($message, array $context = array())
+ * @method static boolean critical($message, array $context = array())
+ * @method static boolean alert($message, array $context = array())
+ * @method static boolean emergency($message, array $context = array())
+ * @method static void log($level, $message, array $context = array())
+ * @method static void write($level, $message, array $context = array())
+ * @method static void useFiles($path, $level = 'debug')S
+ * @method static void useDailyFiles($path, $days = 0, $level = 'debug')
+ * @method static \Psr\Log\LoggerInterface useSyslog($name = 'laravel', $level = 'debug', $facility = 8)
+ * @method static void useErrorLog($level = 'debug', $messageType = 0)
+ * @method static void listen(Closure $callback)
+ * @method static \Monolog\Logger getMonolog()
+ * @method static \Illuminate\Contracts\Events\Dispatcher getEventDispatcher()
+ * @method static void setEventDispatcher($dispatcher)
  */
 class Log extends Facade
 {
