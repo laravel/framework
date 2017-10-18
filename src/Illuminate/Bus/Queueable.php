9ae522afcadef479753164b83e10b074532b5578
Charles Peterson <cpeterson@brokerbin.com>
2017-10-18 06:58:56 -0500

    fix formatting

--- src/Illuminate/Bus/Queueable.php
+++ src/Illuminate/Bus/Queueable.php
@@ -141,7 +141,7 @@ trait Queueable
         if (! empty($this->chained)) {
             dispatch(tap(unserialize(array_shift($this->chained)), function ($next) {
                 /* @var \Illuminate\Bus\Queueable $next */
-                if (!in_array('Illuminate\Bus\Queueable', class_uses_recursive($next))) {
+                if (! in_array('Illuminate\Bus\Queueable', class_uses_recursive($next))) {
                     throw new \Exception('Trying to dispatch an object that is not Queueable');
                 }
                 // pass the chain settings on to the next job in the chain, IF this job does not have a new chain settings set...
