b1781b01469dd5e88c774510c5ddefa23b6a734e
Inani El Houssain <inanielhoussain@gmail.com>
2017-10-27 13:14:49 +0100

    Update DatabaseNotificationCollection.php (#21841)
    Improve documentation :D

--- src/Illuminate/Notifications/DatabaseNotificationCollection.php
+++ src/Illuminate/Notifications/DatabaseNotificationCollection.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Collection;
 class DatabaseNotificationCollection extends Collection
 {
     /**
-     * Mark all notification as read.
+     * Mark all notifications as read.
      *
      * @return void
      */
