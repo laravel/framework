46029cf28f3ab6d1d0f677ef6646f3b14cffb908
grashoper20 <grashoper20@yahoo.ca>
2017-10-28 10:13:49 -0500

    Paginators should be built from Contracts
    Follow up to #19328, paginators should not only be built of the container, they should use the interface. This is more correct and gives developers more flexibility in their implementation.

--- src/Illuminate/Database/Concerns/BuildsQueries.php
+++ src/Illuminate/Database/Concerns/BuildsQueries.php
@@ -3,8 +3,8 @@
 namespace Illuminate\Database\Concerns;
 
 use Illuminate\Container\Container;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 trait BuildsQueries
 {
