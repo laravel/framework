b910063202e4ea5227e2de7486bf6829e4bcfd19
Taylor Otwell <taylor@laravel.com>
2017-11-02 07:58:04 -0500

    formatting

--- src/Illuminate/Bus/Queueable.php
+++ src/Illuminate/Bus/Queueable.php
@@ -138,10 +138,12 @@ trait Queueable
         if (! empty($this->chained)) {
             dispatch(tap(unserialize(array_shift($this->chained)), function ($next) {
                 $next->chained = $this->chained;
-                $next->chainConnection = $this->chainConnection;
-                $next->chainQueue = $this->chainQueue;
+
                 $next->onConnection($next->connection ?: $this->chainConnection);
                 $next->onQueue($next->queue ?: $this->chainQueue);
+
+                $next->chainConnection = $this->chainConnection;
+                $next->chainQueue = $this->chainQueue;
             }));
         }
     }
