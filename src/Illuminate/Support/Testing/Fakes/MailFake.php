50a184039a7c9e5698c1309e6a2ec1c1fbb74a44
Thomas Ellsworth <tellsworth@rxsavingssolutions.com>
2017-11-14 16:46:36 -0600

    Update 'queue' function signature of 'MailFake.php' to match signature of 'queue' on 'Mailer.php' after 5.5 upgrade.

--- src/Illuminate/Support/Testing/Fakes/MailFake.php
+++ src/Illuminate/Support/Testing/Fakes/MailFake.php
@@ -292,12 +292,10 @@ class MailFake implements Mailer
      * Queue a new e-mail message for sending.
      *
      * @param  string|array  $view
-     * @param  array  $data
-     * @param  \Closure|string  $callback
      * @param  string|null  $queue
      * @return mixed
      */
-    public function queue($view, array $data = [], $callback = null, $queue = null)
+    public function queue($view, $queue = null)
     {
         if (! $view instanceof Mailable) {
             return;
