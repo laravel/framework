fded7b4ddcf2a4a6b5453440d834ba9b5f6bc77f
Pierre <pierre.barrau@mines-paris.org>
2017-11-16 23:36:11 +0100

    [5.5] `Model::$exists` to be set to false when force-deleting a model using SoftDeletes (#22100)
    * Model::$exists to be set to false when (force)deleting a model using SoftDeletes
    
    * Model::$exists set to false on force deletion only for soft deleteable models

--- src/Illuminate/Database/Eloquent/SoftDeletes.php
+++ src/Illuminate/Database/Eloquent/SoftDeletes.php
@@ -45,6 +45,8 @@ trait SoftDeletes
     protected function performDeleteOnModel()
     {
         if ($this->forceDeleting) {
+            $this->exists = false;
+
             return $this->newQueryWithoutScopes()->where($this->getKeyName(), $this->getKey())->forceDelete();
         }
 
