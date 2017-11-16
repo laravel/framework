4b846fe8bfa6cd9e02d049a50aaea433be63f890
Aleksey.Sanikovich <Aleksey.Sanikovich@r-stylelab.com>
2017-11-16 13:39:27 +0300

    split Authenticatable contract

---
+++ src/Illuminate/Contracts/Auth/RememberSession.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Illuminate\Contracts\Auth;
+
+interface RememberSession
+{
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken();
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setRememberToken($value);
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName();
+}
