320c210d4b5cab33d8e0a8d278385e4e0a0bbdad
Fabio Pontes <faabiopontes@gmail.com>
2017-11-28 13:56:25 -0200

    Route::view now accepts blank $view parameter - The second parameter $view now can be null - When the parameter $view is null te $uri will be searched as view - No tests were found for Route::view, don't know what to do next

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -251,10 +251,10 @@ class Router implements RegistrarContract, BindingRegistrar
      * @param  array  $data
      * @return \Illuminate\Routing\Route
      */
-    public function view($uri, $view, $data = [])
+    public function view($uri, $view = null, $data = [])
     {
         return $this->match(['GET', 'HEAD'], $uri, '\Illuminate\Routing\ViewController')
-                ->defaults('view', $view)
+                ->defaults('view', $view ? $view : $uri)
                 ->defaults('data', $data);
     }
 
