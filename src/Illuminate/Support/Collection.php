2997ed3fa2e88ef3ac6a8d17219515aae093435b
Lorand Gombos <gombos.lorand@gmail.com>
2017-12-12 22:44:10 +0200

    Use method to get the Collection items

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -391,7 +391,7 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
     public function except($keys)
     {
         if ($keys instanceof Collection) {
-            $keys = $keys->keys()->items;
+            $keys = $keys->keys()->all();
         } elseif (! is_array($keys)) {
             $keys = func_get_args();
         }
