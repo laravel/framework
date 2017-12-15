e086459337626fe57138784a611c26d0736228da
Jordan Hoff <jhoff@linkedin.com>
2017-12-15 13:28:12 -0600

    Fixes issue with multiple dont-discover packages

--- src/Illuminate/Foundation/PackageManifest.php
+++ src/Illuminate/Foundation/PackageManifest.php
@@ -119,7 +119,7 @@ class PackageManifest
         $this->write(collect($packages)->mapWithKeys(function ($package) {
             return [$this->format($package['name']) => $package['extra']['laravel'] ?? []];
         })->each(function ($configuration) use (&$ignore) {
-            $ignore += $configuration['dont-discover'] ?? [];
+            $ignore = array_merge($ignore, $configuration['dont-discover'] ?? []);
         })->reject(function ($configuration, $package) use ($ignore, $ignoreAll) {
             return $ignoreAll || in_array($package, $ignore);
         })->filter()->all());
