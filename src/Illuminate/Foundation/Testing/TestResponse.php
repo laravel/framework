bf1427f3e6473e67c71a3e6c92bc5c6bc8d01c16
Jerguš Lejko <jerguslejko@users.noreply.github.com>
2017-12-22 16:03:42 +0100

    [5.5] Allow to fetch specific key when using json helper (#22489)
    * allow to fetch specific key when using json helper
    
    * fix docblocks

--- src/Illuminate/Foundation/Testing/TestResponse.php
+++ src/Illuminate/Foundation/Testing/TestResponse.php
@@ -500,9 +500,10 @@ class TestResponse
     /**
      * Validate and return the decoded response JSON.
      *
-     * @return array
+     * @param  string|null  $key
+     * @return mixed
      */
-    public function decodeResponseJson()
+    public function decodeResponseJson($key = null)
     {
         $decodedResponse = json_decode($this->getContent(), true);
 
@@ -514,17 +515,18 @@ class TestResponse
             }
         }
 
-        return $decodedResponse;
+        return data_get($decodedResponse, $key);
     }
 
     /**
      * Validate and return the decoded response JSON.
      *
-     * @return array
+     * @param  string|null  $key
+     * @return mixed
      */
-    public function json()
+    public function json($key = null)
     {
-        return $this->decodeResponseJson();
+        return $this->decodeResponseJson($key);
     }
 
     /**
