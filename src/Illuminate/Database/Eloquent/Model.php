c7f14fbb1b263a590d4ba37a0b8313c5a99e0bfa
Paul Grimes <paul@ok200.net>
2017-12-29 14:39:57 +1100

    Updated mass assignment exception wording for clarity; nominating the key alone is a little vague

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -229,7 +229,7 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
             if ($this->isFillable($key)) {
                 $this->setAttribute($key, $value);
             } elseif ($totallyGuarded) {
-                throw new MassAssignmentException($key);
+                throw new MassAssignmentException('Model is guarded. Add key ['.$key.'] to $fillable to allow mass assignment.');
             }
         }
 
