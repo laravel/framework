b14697f59c558fc6d88174a297d260f8471b277b
Linas Pašviestis <linas.pasviestis@gmail.com>
2017-12-31 17:53:07 +0200

    [5.5] Added workaround for dd function correct output in the Chrome's network preview window (#22581)
    * Workaround for dd render in browser preview window
    
    * cs fix

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -553,6 +553,8 @@ if (! function_exists('dd')) {
      */
     function dd(...$args)
     {
+        http_response_code(500);
+
         foreach ($args as $x) {
             (new Dumper)->dump($x);
         }
