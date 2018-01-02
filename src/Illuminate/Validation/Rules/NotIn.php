5158c5ed996be1f9b4c879210a9055db7fd4595d
Marcin Nabiałek <dev@nabialek.org>
2018-01-02 07:58:19 +0100

    Reuse most of code of In rule in NoIn rule

--- src/Illuminate/Validation/Rules/NotIn.php
+++ src/Illuminate/Validation/Rules/NotIn.php
@@ -2,42 +2,10 @@
 
 namespace Illuminate\Validation\Rules;
 
-class NotIn
+class NotIn extends In
 {
     /**
      * The name of the rule.
      */
     protected $rule = 'not_in';
-
-    /**
-     * The accepted values.
-     *
-     * @var array
-     */
-    protected $values;
-
-    /**
-     * Create a new "not in" rule instance.
-     *
-     * @param  array  $values
-     * @return void
-     */
-    public function __construct(array $values)
-    {
-        $this->values = $values;
-    }
-
-    /**
-     * Convert the rule to a validation string.
-     *
-     * @return string
-     */
-    public function __toString()
-    {
-        $values = array_map(function ($value) {
-            return '"'.str_replace('"', '""', $value).'"';
-        }, $this->values);
-
-        return $this->rule.':'.implode(',', $values);
-    }
 }
