688f64ee714a711bb653b74d4e35474f655878a1
Pavinthan <pavinthan@outlook.com>
2018-01-02 17:29:11 +0530

    Default return when the key is null
    Default return when the key is null

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -154,7 +154,7 @@ if (! function_exists('array_get')) {
      */
     function array_get($array, $key, $default = null)
     {
-        return Arr::get($array, $key, $default);
+        return Arr::get($array, $key, $default) ?: $default;
     }
 }
 
