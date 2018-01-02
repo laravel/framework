42fb24849b712d30f862be31b67d11165fb954d6
Ben Cavens <BenCavens@users.noreply.github.com>
2018-01-02 23:38:34 +0100

    [5.5] Fix: verify csrf token allows to except full urls (#22619)
    * Fix: verify csrf token allows to except full urls
    
    * Update VerifyCsrfToken.php

--- src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
+++ src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
@@ -104,7 +104,7 @@ class VerifyCsrfToken
                 $except = trim($except, '/');
             }
 
-            if ($request->is($except)) {
+            if ($request->url() === $except || $request->is($except)) {
                 return true;
             }
         }
