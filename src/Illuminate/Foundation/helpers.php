c7cae859036a137c59db70d8cb129f02d1c24e33
Taylor Otwell <taylor@laravel.com>
2018-01-04 15:28:58 -0600

    [5.6] Rework Logging (#22635)
    This is a re-write / re-work of many of @phroggyy's ideas to improve logging.
    
    Implemented:
    
    Multi-driver logging support so you can have multiple logging channels per app.
    "Pipes" in @phroggyy's pull request have been re-worked as "taps". Allows customization of logging channel post-creation.
    Continued support for functionality provided by configureMonologUsing maintained using custom driver type with factory invokable that returns any PSR3 compatible logger. This allows full customization of entire logging channel.
    I decided not to implement Log::event functionality from PR because I think that would be better suited for something like a ShouldLog interface on the event itself (to complement ShouldBroadcast, etc.).
    
    Primary breaking changes:
    
    New log configuration file / settings.
    configureMonologUsing now custom driver type.
    Illuminate\Contracts\Log removed. Was literal duplication of PSR3 logging interface.
    Illuminate\Log\Writer renamed to Illuminate\Log\Logger.

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -509,7 +509,7 @@ if (! function_exists('logger')) {
      *
      * @param  string  $message
      * @param  array  $context
-     * @return \Illuminate\Contracts\Logging\Log|null
+     * @return \Illuminate\Log\LogManager|null
      */
     function logger($message = null, array $context = [])
     {
@@ -518,6 +518,19 @@ if (! function_exists('logger')) {
         }
 
         return app('log')->debug($message, $context);
+    }
+}
+
+if (! function_exists('logs')) {
+    /**
+     * Get a log driver instance.
+     *
+     * @param  string  $driver
+     * @return \Illuminate\Log\LogManager|\Psr\Log\LoggerInterface
+     */
+    function logs($driver = null)
+    {
+        return $driver ? app('log')->driver($driver) : app('log');
     }
 }
 
