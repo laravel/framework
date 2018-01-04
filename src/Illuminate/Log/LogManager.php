9b073cbf31b941923f1824c10cd75074eefdae93
Taylor Otwell <taylor@laravel.com>
2018-01-04 11:30:46 -0600

    fix call to prepare handlers

--- src/Illuminate/Log/LogManager.php
+++ src/Illuminate/Log/LogManager.php
@@ -133,7 +133,7 @@ class LogManager implements LoggerInterface
      */
     protected function createEmergencyLogger()
     {
-        return new Logger(new Monolog('laravel', $this->prepareHandlers([], [new StreamHandler(
+        return new Logger(new Monolog('laravel', $this->prepareHandlers([new StreamHandler(
                 $this->app->storagePath().'/logs/laravel.log', $this->level(['level' => 'debug'])
         )])), $this->app['events']);
     }
