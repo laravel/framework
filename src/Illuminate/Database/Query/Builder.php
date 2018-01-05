d92da4d1840792cb188b006b1adec92d2d93851a
Gabriel Caruso <carusogabriel34@gmail.com>
2018-01-04 23:51:54 -0200

    Fix typo Query/Builder::where

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -480,7 +480,7 @@ class Builder
      * Add a basic where clause to the query.
      *
      * @param  string|array|\Closure  $column
-     * @param  string|null  $operator
+     * @param  mixed   $operator
      * @param  mixed   $value
      * @param  string  $boolean
      * @return $this
