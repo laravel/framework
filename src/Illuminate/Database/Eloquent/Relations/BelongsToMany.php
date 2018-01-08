b0aa3f3d0f5af0da67eaf7db7bd70ee9c030574b
Taylor Otwell <taylor@laravel.com>
2018-01-08 16:07:08 -0600

    Apply fixes from StyleCI (#22691)

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class BelongsToMany extends Relation
