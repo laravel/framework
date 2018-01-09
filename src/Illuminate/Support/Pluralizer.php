ce88b257b657220fed5f40cd3383329ce5bc0d16
Taylor Otwell <taylor@laravel.com>
2018-01-09 13:33:24 -0600

    revert

--- src/Illuminate/Support/Pluralizer.php
+++ src/Illuminate/Support/Pluralizer.php
@@ -7,6 +7,55 @@ use Doctrine\Common\Inflector\Inflector;
 class Pluralizer
 {
     /**
+     * Uncountable word forms.
+     *
+     * @var array
+     */
+    public static $uncountable = [
+        'audio',
+        'bison',
+        'cattle',
+        'chassis',
+        'compensation',
+        'coreopsis',
+        'data',
+        'deer',
+        'education',
+        'emoji',
+        'equipment',
+        'evidence',
+        'feedback',
+        'firmware',
+        'fish',
+        'furniture',
+        'gold',
+        'hardware',
+        'information',
+        'jedi',
+        'kin',
+        'knowledge',
+        'love',
+        'metadata',
+        'money',
+        'moose',
+        'news',
+        'nutrition',
+        'offspring',
+        'plankton',
+        'pokemon',
+        'police',
+        'rain',
+        'rice',
+        'series',
+        'sheep',
+        'software',
+        'species',
+        'swine',
+        'traffic',
+        'wheat',
+    ];
+
+    /**
      * Get the plural form of an English word.
      *
      * @param  string  $value
@@ -15,11 +64,13 @@ class Pluralizer
      */
     public static function plural($value, $count = 2)
     {
-        if ((int) $count === 1) {
+        if ((int) $count === 1 || static::uncountable($value)) {
             return $value;
         }
 
-        return static::matchCase(Inflector::pluralize($value), $value);
+        $plural = Inflector::pluralize($value);
+
+        return static::matchCase($plural, $value);
     }
 
     /**
@@ -33,6 +84,17 @@ class Pluralizer
         $singular = Inflector::singularize($value);
 
         return static::matchCase($singular, $value);
+    }
+
+    /**
+     * Determine if the given value is uncountable.
+     *
+     * @param  string  $value
+     * @return bool
+     */
+    protected static function uncountable($value)
+    {
+        return in_array(strtolower($value), static::$uncountable);
     }
 
     /**
