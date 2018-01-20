c85ca451fa4d2dbf3b31d75e90ee5fa8af8000c0
Saif M <sikhlana@gmail.com>
2018-01-21 00:49:54 +0600

    Added tests.

---
+++ tests/Database/DatabaseEloquentBelongsToManyWithDefaultAttributesTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Illuminate\Tests\Database;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Database\Query\Builder;
+
+class DatabaseEloquentBelongsToManyWithDefaultAttributesTest extends TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testWithPivotValuesMethodSetsWhereConditionsForFetching()
+    {
+        $relation = $this->getMockBuilder('Illuminate\Database\Eloquent\Relations\BelongsToMany')->setMethods(['touchIfTouching'])->setConstructorArgs($this->getRelationArguments())->getMock();
+        $relation->withPivotValues(['is_admin' => 1]);
+    }
+
+    public function testWithPivotValuesMethodSetsDefaultArgumentsForInsertion()
+    {
+        $relation = $this->getMockBuilder('Illuminate\Database\Eloquent\Relations\BelongsToMany')->setMethods(['touchIfTouching'])->setConstructorArgs($this->getRelationArguments())->getMock();
+        $relation->withPivotValues(['is_admin' => 1]);
+
+        $query = m::mock('stdClass');
+        $query->shouldReceive('from')->once()->with('club_user')->andReturn($query);
+        $query->shouldReceive('insert')->once()->with([['club_id' => 1, 'user_id' => 1, 'is_admin' => 1]])->andReturn(true);
+        $relation->getQuery()->shouldReceive('getQuery')->andReturn($mockQueryBuilder = m::mock('stdClass'));
+        $mockQueryBuilder->shouldReceive('newQuery')->once()->andReturn($query);
+
+        $relation->attach(1);
+    }
+
+    public function getRelationArguments()
+    {
+        $parent = m::mock('Illuminate\Database\Eloquent\Model');
+        $parent->shouldReceive('getKey')->andReturn(1);
+        $parent->shouldReceive('getCreatedAtColumn')->andReturn('created_at');
+        $parent->shouldReceive('getUpdatedAtColumn')->andReturn('updated_at');
+        $parent->shouldReceive('getAttribute')->with('id')->andReturn(1);
+
+        $builder = m::mock('Illuminate\Database\Eloquent\Builder');
+        $related = m::mock('Illuminate\Database\Eloquent\Model');
+        $builder->shouldReceive('getModel')->andReturn($related);
+
+        $related->shouldReceive('getTable')->andReturn('users');
+        $related->shouldReceive('getKeyName')->andReturn('id');
+
+        $builder->shouldReceive('join')->once()->with('club_user', 'users.id', '=', 'club_user.user_id');
+        $builder->shouldReceive('where')->once()->with('club_user.club_id', '=', 1);
+        $builder->shouldReceive('where')->once()->with('club_user.is_admin', '=', 1, 'and');
+
+        return [$builder, $parent, 'club_user', 'club_id', 'user_id', 'id', 'id', null, false];
+    }
+}
