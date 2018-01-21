4ab34a2b39d47053816c757c331b8100764cdbd8
Priidik Vaikla <priidik.vaikla@gmail.com>
2018-01-22 01:37:32 +0200

    Add tests to FilesystemAdapter (#22875)

--- tests/Filesystem/FilesystemAdapterTest.php
+++ tests/Filesystem/FilesystemAdapterTest.php
@@ -7,6 +7,7 @@ use League\Flysystem\Filesystem;
 use League\Flysystem\Adapter\Local;
 use Illuminate\Filesystem\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 class FilesystemAdapterTest extends TestCase
 {
@@ -14,13 +15,14 @@ class FilesystemAdapterTest extends TestCase
 
     public function setUp()
     {
-        $this->filesystem = new Filesystem(new Local(__DIR__.'/tmp'));
+        $this->tempDir = __DIR__.'/tmp';
+        $this->filesystem = new Filesystem(new Local($this->tempDir));
     }
 
     public function tearDown()
     {
-        $filesystem = new Filesystem(new Local(__DIR__));
-        $filesystem->deleteDir('tmp');
+        $filesystem = new Filesystem(new Local(dirname($this->tempDir)));
+        $filesystem->deleteDir(basename($this->tempDir));
     }
 
     public function testResponse()
@@ -45,5 +47,56 @@ class FilesystemAdapterTest extends TestCase
         $response = $files->download('file.txt', 'hello.txt');
         $this->assertInstanceOf(StreamedResponse::class, $response);
         $this->assertEquals('attachment; filename="hello.txt"', $response->headers->get('content-disposition'));
+    }
+
+    public function testExists()
+    {
+        $this->filesystem->write('file.txt', 'Hello World');
+        $filesystemAdapter = new FilesystemAdapter($this->filesystem);
+        $this->assertTrue($filesystemAdapter->exists('file.txt'));
+    }
+
+    public function testPath()
+    {
+        $this->filesystem->write('file.txt', 'Hello World');
+        $filesystemAdapter = new FilesystemAdapter($this->filesystem);
+        $this->assertEquals($this->tempDir.'/file.txt', $filesystemAdapter->path('file.txt'));
+    }
+
+    public function testGet()
+    {
+        $this->filesystem->write('file.txt', 'Hello World');
+        $filesystemAdapter = new FilesystemAdapter($this->filesystem);
+        $this->assertEquals('Hello World', $filesystemAdapter->get('file.txt'));
+    }
+
+    public function testGetFileNotFound()
+    {
+        $filesystemAdapter = new FilesystemAdapter($this->filesystem);
+        $this->expectException(FileNotFoundException::class);
+        $filesystemAdapter->get('file.txt');
+    }
+
+    public function testPut()
+    {
+        $filesystemAdapter = new FilesystemAdapter($this->filesystem);
+        $filesystemAdapter->put('file.txt', 'Something inside');
+        $this->assertStringEqualsFile($this->tempDir.'/file.txt', 'Something inside');
+    }
+
+    public function testPrepend()
+    {
+        file_put_contents($this->tempDir.'/file.txt', 'World');
+        $filesystemAdapter = new FilesystemAdapter($this->filesystem);
+        $filesystemAdapter->prepend('file.txt', 'Hello ');
+        $this->assertStringEqualsFile($this->tempDir.'/file.txt', "Hello \nWorld");
+    }
+
+    public function testAppend()
+    {
+        file_put_contents($this->tempDir.'/file.txt', 'Hello ');
+        $filesystemAdapter = new FilesystemAdapter($this->filesystem);
+        $filesystemAdapter->append('file.txt', 'Moon');
+        $this->assertStringEqualsFile($this->tempDir.'/file.txt', "Hello \nMoon");
     }
 }
