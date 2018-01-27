4561f9faea6c4f16971a5fa24b70398d94bfa4ce
Mathew Tinsley <mtinsley01@gmail.com>
2018-01-27 14:23:37 -0800

    Override getAttribute in eloquent model, move relation methods to HasRelations
    Tests are passing

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -204,6 +204,35 @@ abstract class Model extends BaseModel implements QueueableEntity, UrlRoutable
     }
 
     /**
+     * Get an attribute from the model.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getAttribute($key)
+    {
+        if (! $key) {
+            return;
+        }
+
+        // If the attribute exists in the attribute array or has a "get" mutator we will
+        // get the attribute's value. Otherwise, we will proceed as if the developers
+        // are asking for a relationship's value. This covers both types of values.
+        if ($this->hasAttribute($key)) {
+            return $this->getAttributeValue($key);
+        }
+
+        // Here we will determine if the model base class itself contains this given key
+        // since we don't want to treat any of those methods as relationships because
+        // they are all intended as helper methods and none of these are relations.
+        if (method_exists(self::class, $key)) {
+            return;
+        }
+
+        return $this->getRelationValue($key);
+    }
+
+    /**
      * Fill the model with an array of attributes.
      *
      * @param  array  $attributes
