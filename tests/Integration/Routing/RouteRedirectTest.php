bdf078ce19f2218bb3cfb02d0b76efbc0284673d
Amer Anwar <amer@snso.net>
2018-02-15 12:58:29 +0300

    Fix assertRedirect making response headers matches the asserted URI form

--- tests/Integration/Routing/RouteRedirectTest.php
+++ tests/Integration/Routing/RouteRedirectTest.php
@@ -14,21 +14,22 @@ class RouteRedirectTest extends TestCase
     {
         Route::redirect('from', 'to', 301);
 
-        $response = $this->get('/from');
-        $this->assertEquals(301, $response->getStatusCode());
-        $this->assertEquals('to', $response->headers->get('Location'));
+        $this->get('/from')->assertRedirect('to');
     }
 
     public function test_route_redirect_with_params()
     {
         Route::redirect('from/{param}/{param2?}', 'to', 301);
 
-        $response = $this->get('/from/value1/value2');
-        $this->assertEquals(301, $response->getStatusCode());
-        $this->assertEquals('to', $response->headers->get('Location'));
+        $this->get('/from/value1/value2')->assertRedirect('to');
 
-        $response = $this->get('/from/value1');
-        $this->assertEquals(301, $response->getStatusCode());
-        $this->assertEquals('to', $response->headers->get('Location'));
+        $this->get('/from/value1')->assertRedirect('to');
+    }
+
+    public function test_route_redirect_to_external_location()
+    {
+        Route::redirect('from', 'https://laravel.com/');
+
+        $this->get('/from')->assertRedirect('https://laravel.com/');
     }
 }
