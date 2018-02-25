laravel/framework

commit 230eeef6113bf7a5632578508c3316583ac22534

make unserialized models reload relationships
- add new `getQueueableRelations` method to queuable interfaces
- accept the 'relations' into the `ModelIdentifier`
- implement `getQueueableRelations` method on Eloquent Collection and Model.
- extract a method on `SerializesAndRestoresModelIdentifiers` for `restoreModel`. Load the relationships in this method.
- add test

src/Illuminate/Queue/SerializesAndRestoresModelIdentifiers.php
@@ -21,7 +21,8 @@ trait SerializesAndRestoresModelIdentifiers
             return new ModelIdentifier(
                 $value->getQueueableClass(),
                 $value->getQueueableIds(),
-                $value->getQueueableConnection()
+                $value->getQueueableConnection(),
+                $value->getQueueableRelations()
             );
         }
 
@@ -29,7 +30,8 @@ trait SerializesAndRestoresModelIdentifiers
             return new ModelIdentifier(
                 get_class($value),
                 $value->getQueueableId(),
-                $value->getQueueableConnection()
+                $value->getQueueableConnection(),
+                $value->getQueueableRelations()
             );
         }
 
@@ -50,8 +52,7 @@ trait SerializesAndRestoresModelIdentifiers
 
         return is_array($value->id)
                 ? $this->restoreCollection($value)
-                : $this->getQueryForModelRestoration((new $value->class)->setConnection($value->connection))
-                    ->useWritePdo()->findOrFail($value->id);
+                : $this->restoreModel($value);
     }
 
     /**
@@ -70,6 +71,18 @@ trait SerializesAndRestoresModelIdentifiers
 
         return $this->getQueryForModelRestoration($model)->useWritePdo()
                     ->whereIn($model->getQualifiedKeyName(), $value->id)->get();
+    }
+
+    /**
+     * @param  \Illuminate\Contracts\Database\ModelIdentifier  $value
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function restoreModel($value)
+    {
+        $model = $this->getQueryForModelRestoration((new $value->class)->setConnection($value->connection))
+                      ->useWritePdo()->findOrFail($value->id);
+
+        return $model->load($value->relations);
     }
 
     /**

src/Illuminate/Contracts/Database/ModelIdentifier.php
@@ -28,17 +28,26 @@ class ModelIdentifier
     public $connection;
 
     /**
+     * The relationships loaded on the model.
+     *
+     * @var array
+     */
+    public $relations;
+
+    /**
      * Create a new model identifier.
      *
      * @param  string  $class
      * @param  mixed  $id
      * @param  mixed  $connection
+     * @param  array  $relations
      * @return void
      */
-    public function __construct($class, $id, $connection)
+    public function __construct($class, $id, $connection, $relations)
     {
         $this->id = $id;
         $this->class = $class;
         $this->connection = $connection;
+        $this->relations = $relations;
     }
 }

src/Illuminate/Contracts/Queue/QueueableCollection.php
@@ -24,4 +24,11 @@ interface QueueableCollection
      * @return string|null
      */
     public function getQueueableConnection();
+
+    /**
+     * Get the relationships of the entities being queued.
+     *
+     * @return array
+     */
+    public function getQueueableRelations();
 }

src/Illuminate/Contracts/Queue/QueueableEntity.php
@@ -17,4 +17,11 @@ interface QueueableEntity
      * @return string|null
      */
     public function getQueueableConnection();
+
+    /**
+     * Get the relationships for the entity.
+     *
+     * @return array
+     */
+    public function getQueueableRelations();
 }

src/Illuminate/Database/Eloquent/Collection.php
@@ -419,4 +419,14 @@ class Collection extends BaseCollection implements QueueableCollection
 
         return $connection;
     }
+
+    /**
+     * Get the relationships of the entities being queued.
+     *
+     * @return array
+     */
+    public function getQueueableRelations()
+    {
+        return $this->isNotEmpty() ? $this->first()->getRelations() : [];
+    }
 }

src/Illuminate/Database/Eloquent/Model.php
@@ -13,6 +13,7 @@ use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Routing\UrlRoutable;
 use Illuminate\Contracts\Queue\QueueableEntity;
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Contracts\Queue\QueueableCollection;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\ConnectionResolverInterface as Resolver;
 
@@ -1248,6 +1249,37 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
     public function getQueueableConnection()
     {
         return $this->getConnectionName();
+    }
+
+    /**
+     * Get the queueable relationships for the entity.
+     *
+     * @return array
+     */
+    public function getQueueableRelations()
+    {
+        $relations = [];
+
+        foreach ($this->getRelations() as $key => $relation){
+
+            $relations[] = $key;
+
+            if ($relation instanceof QueueableCollection)
+            {
+                foreach($relation->getQueueableRelations() as $collectionKey => $collectionValue){
+                    $relations[] = $key . '.' . $collectionKey;
+                }
+            }
+
+            if ($relation instanceof QueueableEntity)
+            {
+                foreach($relation->getQueueableRelations() as $entityKey => $entityValue){
+                    $relations[] = $key . '.' . $entityValue;
+                }
+            }
+        }
+
+        return $relations;
     }
 
     /**

tests/Integration/Queue/ModelSerializationTest.php
@@ -43,6 +43,21 @@ class ModelSerializationTest extends TestCase
             $table->increments('id');
             $table->string('email');
         });
+
+        Schema::create('orders', function ($table) {
+            $table->increments('id');
+        });
+
+        Schema::create('lines', function ($table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->unsignedInteger('product_id');
+        });
+
+        Schema::create('products', function ($table) {
+            $table->increments('id');
+
+        });
     }
 
     /**
@@ -126,11 +141,83 @@ class ModelSerializationTest extends TestCase
 
         unserialize($serialized);
     }
+
+    /**
+     * @test
+     */
+    public function it_reloads_relationships()
+    {
+        $order = Order::create();
+
+        $product1 = Product::create();
+        $product2 = Product::create();
+
+        Line::create(['order_id' => $order->id, 'product_id' => $product1->id]);
+        Line::create(['order_id' => $order->id, 'product_id' => $product2->id]);
+        Line::create(['order_id' => $order->id, 'product_id' => $product1->id]);
+
+        $order->load('lines');
+
+        $serialized = serialize(new ModelRelationSerializationTestClass($order));
+        $unSerialized = unserialize($serialized);
+
+        $this->assertEquals($unSerialized->order->getRelations(), $order->getRelations());
+    }
+
+    /**
+     * @test
+     */
+    public function it_reloads_nested_relationships()
+    {
+        $order = Order::create();
+
+        $product1 = Product::create();
+        $product2 = Product::create();
+
+        Line::create(['order_id' => $order->id, 'product_id' => $product1->id]);
+        Line::create(['order_id' => $order->id, 'product_id' => $product2->id]);
+        Line::create(['order_id' => $order->id, 'product_id' => $product1->id]);
+
+        $order->load('lines', 'lines.product');
+
+        $nestedSerialized = serialize(new ModelRelationSerializationTestClass($order));
+        $nestedUnSerialized = unserialize($nestedSerialized);
+
+        $this->assertEquals($nestedUnSerialized->order->getRelations(), $order->getRelations());
+    }
 }
 
 class ModelSerializationTestUser extends Model
 {
     public $table = 'users';
+    public $guarded = ['id'];
+    public $timestamps = false;
+}
+
+class Order extends Model
+{
+    public $guarded = ['id'];
+    public $timestamps = false;
+
+    public function lines()
+    {
+        return $this->hasMany(Line::class);
+    }
+}
+
+class Line extends Model
+{
+    public $guarded = ['id'];
+    public $timestamps = false;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
+
+class Product extends Model
+{
     public $guarded = ['id'];
     public $timestamps = false;
 }
@@ -144,5 +231,17 @@ class ModelSerializationTestClass
     public function __construct($user)
     {
         $this->user = $user;
+    }
+}
+
+class ModelRelationSerializationTestClass
+{
+    use \Illuminate\Queue\SerializesModels;
+
+    public $order;
+
+    public function __construct($order)
+    {
+        $this->order = $order;
     }
 }

tests/Queue/QueueSyncQueueTest.php
@@ -61,6 +61,11 @@ class SyncQueueTestEntity implements \Illuminate\Contracts\Queue\QueueableEntity
     {
         //
     }
+
+    public function getQueueableRelations()
+    {
+        //
+    }
 }
 
 class SyncQueueTestHandler

commit a30c03aa3143a884406af1014b5325035d409f79

fixes an issue where delayed jobs in L5.5 fail to run in L5.6 due to the new relations traits introduced in 230eeef

src/Illuminate/Queue/SerializesAndRestoresModelIdentifiers.php
@@ -82,7 +82,7 @@ trait SerializesAndRestoresModelIdentifiers
     {
         return $this->getQueryForModelRestoration(
             (new $value->class)->setConnection($value->connection), $value->id
-        )->useWritePdo()->firstOrFail()->load($value->relations);
+        )->useWritePdo()->firstOrFail()->load($value->relations ?? []);
     }
 
     /**