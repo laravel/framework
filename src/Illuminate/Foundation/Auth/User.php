75c1f2bae4f1ee20ec7a4725f846e6969170e27e
Byorun <bjoern.linkiewicz@cyber-cortex.net>
2018-03-01 19:31:25 +0100

    move Notifiable trait to "Authenticatable"
    move Notifiable trait to "Authenticatable" base class for default laravel user model since the CanResetPassword trait depends on $this->notify()

--- src/Illuminate/Foundation/Auth/User.php
+++ src/Illuminate/Foundation/Auth/User.php
@@ -9,11 +9,12 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Model implements
     AuthenticatableContract,
     AuthorizableContract,
     CanResetPasswordContract
 {
-    use Authenticatable, Authorizable, CanResetPassword;
+    use Authenticatable, Authorizable, CanResetPassword, Notifiable;
 }
