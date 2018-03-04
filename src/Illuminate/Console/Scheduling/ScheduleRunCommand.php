7339578e07fb07efb860fec57fc284ec81bdced9
Taylor Otwell <taylor@laravel.com>
2018-03-03 20:06:37 -0600

    formatting

--- src/Illuminate/Console/Scheduling/ScheduleRunCommand.php
+++ src/Illuminate/Console/Scheduling/ScheduleRunCommand.php
@@ -86,7 +86,7 @@ class ScheduleRunCommand extends Command
     /**
      * Run the given single server event.
      *
-     * @param  Event  $event
+     * @param  \Illuminate\Console\Scheduling\Event  $event
      * @return void
      */
     protected function runSingleServerEvent($event)
@@ -101,7 +101,7 @@ class ScheduleRunCommand extends Command
     /**
      * Run the given event.
      *
-     * @param  Event  $event
+     * @param  \Illuminate\Console\Scheduling\Event  $event
      * @return void
      */
     protected function runEvent($event)
