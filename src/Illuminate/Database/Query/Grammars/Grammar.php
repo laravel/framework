f448c227f2d5b60042bb8fe4c108977b34ac3a35
Mike <mike.grinspan@gmail.com>
2018-03-06 14:34:49 -0500

    Fixed typo

--- src/Illuminate/Database/Query/Grammars/Grammar.php
+++ src/Illuminate/Database/Query/Grammars/Grammar.php
@@ -330,7 +330,7 @@ class Grammar extends BaseGrammar
     {
         $between = $where['not'] ? 'not between' : 'between';
 
-        return $this->wrap($where['column']).' '.$between.' '.$this->paramater($where['value'][0]).' and '.$this->paramater($where['value'][1]);
+        return $this->wrap($where['column']).' '.$between.' '.$this->parameter($where['value'][0]).' and '.$this->parameter($where['value'][1]);
     }
 
     /**
