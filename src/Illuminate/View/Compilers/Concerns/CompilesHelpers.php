f3a167d6ea9bf6c9542363f96f45d2ff2b5cee23
Dody Agung S <dodyags@gmail.com>
2018-03-06 23:17:54 +0700

    Apply fixes from StyleCI

--- src/Illuminate/View/Compilers/Concerns/CompilesHelpers.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesHelpers.php
@@ -36,15 +36,15 @@ trait CompilesHelpers
         return "<?php dump{$arguments}; ?>";
     }
 
-    /** 
-     * Compile the hidden statements into valid PHP. 
-     * 
-     * @param  string  $expression 
-     * @return string 
-     */ 
-    protected function compileHidden($expression) 
-    { 
-        return "<?php echo hidden_field{$expression}; ?>"; 
+    /**
+     * Compile the hidden statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileHidden($expression)
+    {
+        return "<?php echo hidden_field{$expression}; ?>";
     }
 
     /**
