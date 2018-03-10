ba130c3f2ad3e32589bbf0469718f3951ee342d4
Peter Fox <peter.fox@peterfox.me>
2018-03-10 04:14:38 +0000

    Adds before, after and dateEquals to Illuminate\Validation\Rule
    Allows for the easier creation of dynamic/relative date rules for date before, after and equals validation using DateTime instances

---
+++ tests/Validation/ValidationDateComparisonRuleTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Illuminate\Tests\Validation;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\DateComparison;
+use PHPUnit\Framework\TestCase;
+
+class ValidationDateComparisonRuleTest extends TestCase
+{
+    public function testItCorrectlyFormatsAStringVersionOfTheDateEqualsRule()
+    {
+        $rule = new DateComparison(
+            Carbon::create(1977, 05, 25, 06, 52, 59)
+        );
+
+        $this->assertEquals('date_equals:1977-05-25 06:52:59', (string) $rule);
+
+        $rule = (new DateComparison(
+            Carbon::create(1977, 05, 25, 06, 52, 59)
+        ))->orEqual();
+
+        $this->assertEquals('date_equals:1977-05-25 06:52:59', (string) $rule);
+
+        $rule = Rule::dateEquals(Carbon::create(1977, 05, 25, 06, 52, 59));
+
+        $this->assertEquals('date_equals:1977-05-25 06:52:59', (string) $rule);
+    }
+
+    public function testItCorrectlyFormatsAStringVersionOfTheBeforeRule()
+    {
+        $rule = (new DateComparison(
+            Carbon::create(1977, 05, 25, 06, 52, 59)
+        ))->before();
+
+        $this->assertEquals('before:1977-05-25 06:52:59', (string) $rule);
+
+        $rule = Rule::before(Carbon::create(1977, 05, 25, 06, 52, 59));
+
+        $this->assertEquals('before:1977-05-25 06:52:59', (string) $rule);
+    }
+
+    public function testItCorrectlyFormatsAStringVersionOfTheBeforeOrEqualRule()
+    {
+        $rule = (new DateComparison(
+            Carbon::create(1977, 05, 25, 06, 52, 59)
+        ))->before()->orEqual();
+
+        $this->assertEquals('before_or_equal:1977-05-25 06:52:59', (string) $rule);
+
+        $rule = Rule::before(Carbon::create(1977, 05, 25, 06, 52, 59))
+            ->orEqual();
+
+        $this->assertEquals('before_or_equal:1977-05-25 06:52:59', (string) $rule);
+    }
+
+    public function testItCorrectlyFormatsAStringVersionOfTheAfterRule()
+    {
+        $rule = (new DateComparison(
+            Carbon::create(1977, 05, 25, 06, 52, 59)
+        ))->after();
+
+        $this->assertEquals('after:1977-05-25 06:52:59', (string) $rule);
+
+        $rule = Rule::after(Carbon::create(1977, 05, 25, 06, 52, 59));
+
+        $this->assertEquals('after:1977-05-25 06:52:59', (string) $rule);
+    }
+
+    public function testItCorrectlyFormatsAStringVersionOfTheAfterOrEqualRule()
+    {
+        $rule = (new DateComparison(
+            Carbon::create(1977, 05, 25, 06, 52, 59)
+        ))->after()->orEqual();
+
+        $this->assertEquals('after_or_equal:1977-05-25 06:52:59', (string) $rule);
+
+        $rule = Rule::after(Carbon::create(1977, 05, 25, 06, 52, 59))
+            ->orEqual();
+
+        $this->assertEquals('after_or_equal:1977-05-25 06:52:59', (string) $rule);
+    }
+}
