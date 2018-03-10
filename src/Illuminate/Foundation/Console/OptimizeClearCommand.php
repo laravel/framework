ed0e3674827675213f1879a781f03ce7f37ae775
Taylor Otwell <taylor@laravel.com>
2018-03-10 09:49:13 -0600

    Update OptimizeClearCommand.php

--- src/Illuminate/Foundation/Console/OptimizeClearCommand.php
+++ src/Illuminate/Foundation/Console/OptimizeClearCommand.php
@@ -29,8 +29,8 @@ class OptimizeClearCommand extends Command
     {
         $this->call('view:clear');
         $this->call('cache:clear');
-        $this->call('config:clear');
         $this->call('route:clear');
+        $this->call('config:clear');
         $this->call('clear-compiled');
 
         $this->info('Caches cleared successfully!');
