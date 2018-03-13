378786f86878b5cd99fc7f6d79b67c8d0f2834ab
asantibanez <santibanez.andres@gmail.com>
2018-03-13 16:45:17 -0500

    Added test helper to assert that a Job has been queued with a Chain

--- src/Illuminate/Support/Testing/Fakes/QueueFake.php
+++ src/Illuminate/Support/Testing/Fakes/QueueFake.php
@@ -35,6 +35,59 @@ class QueueFake extends QueueManager implements Queue
     }
 
     /**
+     * Assert if a job was pushed with chained jobs based on a truth-test callback.
+     *
+     * @param  string $job
+     * @param  array $expectedChain
+     * @param  callable|null $callback
+     * @return void
+     */
+    public function assertPushedWithChain($job, $expectedChain = [], $callback = null)
+    {
+        PHPUnit::assertTrue(
+            $this->pushed($job, $callback)->count() > 0,
+            "The expected [{$job}] job was not pushed."
+        );
+
+        PHPUnit::assertTrue(
+            collect($expectedChain)->count() > 0,
+            "The expected chain can not be empty."
+        );
+
+        $isExpectedChainOfObjects = collect($expectedChain)
+                ->filter(function ($job) { return is_object($job); })
+                ->count() == collect($expectedChain)->count();
+
+        if ($isExpectedChainOfObjects)
+        {
+            $chain = collect($expectedChain)->map(function ($job) { return serialize($job); })->all();
+            PHPUnit::assertTrue(
+                $this->pushed($job, $callback)
+                    ->filter(function ($job) use ($chain) {
+                        return $job->chained == $chain;
+                    })->count() > 0,
+                "The expected chain was not pushed."
+            );
+
+            return;
+        }
+
+        PHPUnit::assertTrue(
+            $this->pushed($job, $callback)
+                ->map(function ($job) {
+                    return $job->chained;
+                })->map(function ($chain) {
+                    return collect($chain)->map(function ($chainedJob) {
+                        return get_class(unserialize($chainedJob));
+                    });
+                })->filter(function ($chain) use ($expectedChain) {
+                    return $chain == collect($expectedChain);
+                })->count() > 0,
+            'The expected chain was not pushed'
+        );
+    }
+
+    /**
      * Assert if a job was pushed a number of times.
      *
      * @param  string  $job
