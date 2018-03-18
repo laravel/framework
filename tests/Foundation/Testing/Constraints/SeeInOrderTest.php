27ad8db3add28fd3f4e1a5e6448c443117be78e0
Joshua Anthony <jantho1990@gmail.com>
2018-03-18 01:29:51 -0500

    Added a separate test for assertSeeTextInOrder

--- tests/Foundation/Testing/Constraints/SeeInOrderTest.php
+++ tests/Foundation/Testing/Constraints/SeeInOrderTest.php
@@ -8,9 +8,20 @@ use Illuminate\Tests\Foundation\FoundationTestResponseTest;
 
 class SeeInOrderTest extends TestCase
 {
-    public function testAllAssertionsDetected()
+    public function testAllSeeInOrderAssertionsDetected()
     {
         $test = new FoundationTestResponseTest('testAssertSeeInOrder');
+        $test->run();
+        // If we get four assertions, that means all of the assertions
+        // were detected by PHPUnit, which is what we want to know.
+        // Doing it this way allows the test to work even though
+        // the global settings don't check for risky tests.
+        $this->assertEquals($test->getNumAssertions(), 4);
+    }
+
+    public function testAllSeeTextInOrderAssertionsDetected()
+    {
+        $test = new FoundationTestResponseTest('testAssertSeeTextInOrder');
         $test->run();
         // If we get four assertions, that means all of the assertions
         // were detected by PHPUnit, which is what we want to know.
