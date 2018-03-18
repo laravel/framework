c7f6ced520516b64fb1a1be5a20f6c6200c1e789
Joshua Anthony <jantho1990@gmail.com>
2018-03-17 23:59:11 -0500

    added SeeInOrder PHPUnit constraint to allow assertSeeInOrder and assertSeeTextInOrder to actually assert something

---
+++ src/Illuminate/Foundation/Testing/Constraints/SeeInOrder.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Illuminate\Foundation\Testing\Constraints;
+
+use Illuminate\Database\Connection;
+use PHPUnit\Framework\Constraint\Constraint;
+
+class SeeInOrder extends Constraint
+{
+    /**
+     * The value that failed. Used to display in the error message.
+     * 
+     * @var string
+     */
+    protected $failedValue;
+
+    /**
+     * The string we want to check the content of.
+     * 
+     * @var string
+     */
+    protected $content;
+
+    /**
+     * Create a new constraint instance.
+     *
+     * @param  string $content
+     * @return void
+     */
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Check if the data is found in the given table.
+     *
+     * @param  array  $values
+     * @return bool
+     */
+    public function matches($values) : bool
+    {
+        $position = 0;
+
+        foreach ($values as $value) {
+            $valuePosition = mb_strpos($this->content, $value, $position);
+
+            if ($valuePosition === false || $valuePosition < $position) {
+                $this->failedValue = $value;
+                return false;
+            }
+
+            $position = $valuePosition + mb_strlen($value);
+        }
+        return true;
+    }
+
+    /**
+     * Get the description of the failure.
+     *
+     * @param  array  $values
+     * @return string
+     */
+    public function failureDescription($values) : string
+    {
+        return sprintf(
+            'Failed asserting that \'%s\' contains "%s" in specified order.',
+            $this->content,
+            $this->failedValue
+        );
+    }
+}
