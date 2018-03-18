140321e1170f426a40c4aadb551bee2b44a18fe9
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2018-03-18 12:58:03 +0000

    Fixed phpdoc

--- src/Illuminate/Hashing/ArgonHasher.php
+++ src/Illuminate/Hashing/ArgonHasher.php
@@ -55,8 +55,8 @@ class ArgonHasher implements HasherContract
     /**
      * Hash the given value.
      *
-     * @param  string $value
-     * @param  array $options
+     * @param  string  $value
+     * @param  array  $options
      * @return string
      */
     public function make($value, array $options = [])
@@ -77,9 +77,9 @@ class ArgonHasher implements HasherContract
     /**
      * Check the given plain value against a hash.
      *
-     * @param  string $value
-     * @param  string $hashedValue
-     * @param  array $options
+     * @param  string  $value
+     * @param  string  $hashedValue
+     * @param  array  $options
      * @return bool
      */
     public function check($value, $hashedValue, array $options = [])
@@ -94,8 +94,8 @@ class ArgonHasher implements HasherContract
     /**
      * Check if the given hash has been hashed using the given options.
      *
-     * @param  string $hashedValue
-     * @param  array $options
+     * @param  string  $hashedValue
+     * @param  array  $options
      * @return bool
      */
     public function needsRehash($hashedValue, array $options = [])
