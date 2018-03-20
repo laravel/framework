be62be6a40d3ab47ec73a848349cbfcc061ad526
Johannes <schmittjoh@gmail.com>
2018-03-20 16:04:14 +0100

    make return type a bit more precise

--- src/Illuminate/Contracts/Broadcasting/ShouldBroadcast.php
+++ src/Illuminate/Contracts/Broadcasting/ShouldBroadcast.php
@@ -2,12 +2,14 @@
 
 namespace Illuminate\Contracts\Broadcasting;
 
+use Illuminate\Broadcasting\Channel;
+
 interface ShouldBroadcast
 {
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return array
+     * @return Channel|Channel[]
      */
     public function broadcastOn();
 }
