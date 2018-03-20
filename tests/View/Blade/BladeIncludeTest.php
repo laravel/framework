eda7add72e5cd550367dbd864431fc640f3bd8c7
Gary Green <holegary@gmail.com>
2018-03-20 01:46:12 +0000

    Update various blade array_ calls to use fully qualified Arr helper (#23619)
    * Update various blade array_ calls to use fully qualified Arr helper
    
    * Fix tests

--- tests/View/Blade/BladeIncludeTest.php
+++ tests/View/Blade/BladeIncludeTest.php
@@ -6,7 +6,7 @@ class BladeIncludeTest extends AbstractBladeTestCase
 {
     public function testIncludesAreCompiled()
     {
-        $this->assertEquals('<?php echo $__env->make(\'foo\', array_except(get_defined_vars(), array(\'__data\', \'__path\')))->render(); ?>', $this->compiler->compileString('@include(\'foo\')'));
-        $this->assertEquals('<?php echo $__env->make(name(foo), array_except(get_defined_vars(), array(\'__data\', \'__path\')))->render(); ?>', $this->compiler->compileString('@include(name(foo))'));
+        $this->assertEquals('<?php echo $__env->make(\'foo\', \Illuminate\Support\Arr::except(get_defined_vars(), array(\'__data\', \'__path\')))->render(); ?>', $this->compiler->compileString('@include(\'foo\')'));
+        $this->assertEquals('<?php echo $__env->make(name(foo), \Illuminate\Support\Arr::except(get_defined_vars(), array(\'__data\', \'__path\')))->render(); ?>', $this->compiler->compileString('@include(name(foo))'));
     }
 }
