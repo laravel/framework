ec8fb5596b67de0edfcc8cc7c2167d6c184f080b
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2018-03-20 21:42:46 +0100

    Simplify validators (#23635)

--- src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
+++ src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Illuminate\Foundation\Providers;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\AggregateServiceProvider;
@@ -42,7 +41,7 @@ class FoundationServiceProvider extends AggregateServiceProvider
             validator()->validate($this->all(), $rules, ...$params);
 
             return $this->only(collect($rules)->keys()->map(function ($rule) {
-                return Str::contains($rule, '.') ? explode('.', $rule)[0] : $rule;
+                return explode('.', $rule)[0];
             })->unique()->toArray());
         });
     }
