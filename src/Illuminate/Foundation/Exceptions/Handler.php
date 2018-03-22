f9162c9898c58be18f166e1832699b83602404b1
Taylor Otwell <taylor@laravel.com>
2018-03-22 11:47:22 -0700

    clean and refactor exception handler

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -106,7 +106,7 @@ class Handler implements ExceptionHandlerContract
         try {
             $logger = $this->container->make(LoggerInterface::class);
         } catch (Exception $ex) {
-            throw $e; // throw the original exception
+            throw $e;
         }
 
         $logger->error(
@@ -248,14 +248,9 @@ class Handler implements ExceptionHandlerContract
      */
     protected function invalid($request, ValidationException $exception)
     {
-        $url = $exception->redirectTo ?? url()->previous();
-
-        return redirect($url)
-                ->withInput($request->except($this->dontFlash))
-                ->withErrors(
-                    $exception->errors(),
-                    $exception->errorBag
-                );
+        return redirect($exception->redirectTo ?? url()->previous())
+                    ->withInput($request->except($this->dontFlash))
+                    ->withErrors($exception->errors(), $exception->errorBag);
     }
 
     /**
@@ -283,9 +278,7 @@ class Handler implements ExceptionHandlerContract
     protected function prepareResponse($request, Exception $e)
     {
         if (! $this->isHttpException($e) && config('app.debug')) {
-            return $this->toIlluminateResponse(
-                $this->convertExceptionToResponse($e), $e
-            );
+            return $this->toIlluminateResponse($this->convertExceptionToResponse($e), $e);
         }
 
         if (! $this->isHttpException($e)) {
@@ -305,21 +298,28 @@ class Handler implements ExceptionHandlerContract
      */
     protected function convertExceptionToResponse(Exception $e)
     {
-        $headers = $this->isHttpException($e) ? $e->getHeaders() : [];
-
-        $statusCode = $this->isHttpException($e) ? $e->getStatusCode() : 500;
-
-        try {
-            $content = config('app.debug') && class_exists(Whoops::class)
-                    ? $this->renderExceptionWithWhoops($e)
-                    : $this->renderExceptionWithSymfony($e, config('app.debug'));
-        } catch (Exception $e) {
-            $content = $content ?? $this->renderExceptionWithSymfony($e, config('app.debug'));
-        }
-
         return SymfonyResponse::create(
-            $content, $statusCode, $headers
+            $this->renderExceptionContent($e),
+            $this->isHttpException($e) ? $e->getStatusCode() : 500,
+            $this->isHttpException($e) ? $e->getHeaders() : []
         );
+    }
+
+    /**
+     * Get the response content for the given exception.
+     *
+     * @param  \Exception  $e
+     * @return string
+     */
+    protected function renderExceptionContent(Exception $e)
+    {
+        try {
+            return config('app.debug') && class_exists(Whoops::class)
+                        ? $this->renderExceptionWithWhoops($e)
+                        : $this->renderExceptionWithSymfony($e, config('app.debug'));
+        } catch (Exception $e) {
+            return $this->renderExceptionWithSymfony($e, config('app.debug'));
+        }
     }
 
     /**
@@ -340,6 +340,16 @@ class Handler implements ExceptionHandlerContract
     }
 
     /**
+     * Get the Whoops handler for the application.
+     *
+     * @return \Whoops\Handler\Handler
+     */
+    protected function whoopsHandler()
+    {
+        return (new WhoopsHandler)->forDebug();
+    }
+
+    /**
      * Render an exception to a string using Symfony.
      *
      * @param  \Exception  $e
@@ -354,36 +364,6 @@ class Handler implements ExceptionHandlerContract
     }
 
     /**
-     * Get the Whoops handler for the application.
-     *
-     * @return \Whoops\Handler\Handler
-     */
-    protected function whoopsHandler()
-    {
-        return tap(new PrettyPageHandler, function ($handler) {
-            $files = new Filesystem;
-
-            $handler->handleUnconditionally(true);
-
-            foreach (config('app.debug_blacklist', []) as $key => $secrets) {
-                foreach ($secrets as $secret) {
-                    $handler->blacklist($key, $secret);
-                }
-            }
-
-            if (config('app.editor', false)) {
-                $handler->setEditor(config('app.editor'));
-            }
-
-            $handler->setApplicationPaths(
-                array_flip(Arr::except(
-                    array_flip($files->directories(base_path())), [base_path('vendor')]
-                ))
-            );
-        });
-    }
-
-    /**
      * Render the given HttpException.
      *
      * @param  \Symfony\Component\HttpKernel\Exception\HttpException  $e
@@ -391,21 +371,29 @@ class Handler implements ExceptionHandlerContract
      */
     protected function renderHttpException(HttpException $e)
     {
-        $status = $e->getStatusCode();
+        $this->registerErrorViewPaths();
 
+        if (view()->exists($view = "errors::{$e->getStatusCode()}")) {
+            return response()->view($view, [
+                'exception' => $e, 'errors' => new ViewErrorBag,
+            ], $e->getStatusCode(), $e->getHeaders());
+        }
+
+        return $this->convertExceptionToResponse($e);
+    }
+
+    /**
+     * Register the error template hint paths.
+     *
+     * @return void
+     */
+    protected function registerErrorViewPaths()
+    {
         $paths = collect(config('view.paths'));
 
         view()->replaceNamespace('errors', $paths->map(function ($path) {
             return "{$path}/errors";
         })->push(__DIR__.'/views')->all());
-
-        if (view()->exists($view = "errors::{$status}")) {
-            return response()->view($view, [
-                'exception' => $e, 'errors' => new ViewErrorBag,
-            ], $status, $e->getHeaders());
-        }
-
-        return $this->convertExceptionToResponse($e);
     }
 
     /**
@@ -439,12 +427,10 @@ class Handler implements ExceptionHandlerContract
      */
     protected function prepareJsonResponse($request, Exception $e)
     {
-        $status = $this->isHttpException($e) ? $e->getStatusCode() : 500;
-
-        $headers = $this->isHttpException($e) ? $e->getHeaders() : [];
-
         return new JsonResponse(
-            $this->convertExceptionToArray($e), $status, $headers,
+            $this->convertExceptionToArray($e),
+            $this->isHttpException($e) ? $e->getStatusCode() : 500,
+            $this->isHttpException($e) ? $e->getHeaders() : [],
             JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
         );
     }
