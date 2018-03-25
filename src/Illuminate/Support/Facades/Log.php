76e6009797957ecccbc73ad9b9b08e2250e1e076
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-03-25 21:54:17 +0300

    [5.6] Added annotation to Support/Facades/Log.php class from Psr\Log\LoggerInterface

--- src/Illuminate/Support/Facades/Log.php
+++ src/Illuminate/Support/Facades/Log.php
@@ -5,6 +5,16 @@ namespace Illuminate\Support\Facades;
 use Psr\Log\LoggerInterface;
 
 /**
+ * @method static void emergency(string $message, array $context = array())
+ * @method static void alert(string $message, array $context = array())
+ * @method static void critical(string $message, array $context = array())
+ * @method static void error(string $message, array $context = array())
+ * @method static void warning(string $message, array $context = array())
+ * @method static void notice(string $message, array $context = array())
+ * @method static void info(string $message, array $context = array())
+ * @method static void debug(string $message, array $context = array())
+ * @method static void log($level, string $message, array $context = array())
+ *
  * @see \Illuminate\Log\Logger
  */
 class Log extends Facade
