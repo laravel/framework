3911b5fd4513e0eb7ac4883415e2c60c8af2d8ee
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-03-25 21:47:54 +0300

    [5.6] Added annotation to Support/Facades/Lang.php class from Contracts/Translation/Translator.php

--- src/Illuminate/Support/Facades/Lang.php
+++ src/Illuminate/Support/Facades/Lang.php
@@ -3,6 +3,11 @@
 namespace Illuminate\Support\Facades;
 
 /**
+ * @method static mixed trans(string $key, array $replace = [], string $locale = null)
+ * @method static string transChoice(string $key, int | array | \Countable $number, array $replace = [], string $locale = null)
+ * @method static string getLocale()
+ * @method static void setLocale(string $locale)
+ *
  * @see \Illuminate\Translation\Translator
  */
 class Lang extends Facade
