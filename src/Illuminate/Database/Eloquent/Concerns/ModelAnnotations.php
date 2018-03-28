254e14e252045d73ae44a2b1efb8014f08cecb7e
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-03-28 23:53:32 +0300

    [model type hinting improvement] Add traits for models, for better type hinting.

---
+++ src/Illuminate/Database/Eloquent/Concerns/ModelAnnotations.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Illuminate\Database\Eloquent\Concerns;
+
+/**
+ * Trait ModelAnnotation.
+ *
+ * @method $this whereKey($id)
+ * @method $this whereKeyNot($id)
+ * @method $this where(string|array|\Closure $column, string $operator = null, $value = null, string $boolean = 'and')
+ */
+trait ModelAnnotations
+{
+    //
+}
---
+++ src/Illuminate/Database/Eloquent/Concerns/ModelStaticAnnotations.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Illuminate\Database\Eloquent\Concerns;
+
+/**
+ * Trait ModelStaticAnnotations.
+ *
+ * @method static $this whereKey($id)
+ * @method static $this whereKeyNot($id)
+ * @method static $this where(string|array|\Closure $column, string $operator = null, $value = null, string $boolean = 'and')
+ */
+trait ModelStaticAnnotations
+{
+    //
+}
