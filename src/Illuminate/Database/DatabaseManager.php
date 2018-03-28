6f4e759fbe1354d06e8fef9bbd962a0b7b065d90
Ingmar Aasoja <ingmar@aasoja.ee>
2018-03-28 22:03:51 +0300

    Added MariaDb native JSON support

--- src/Illuminate/Database/DatabaseManager.php
+++ src/Illuminate/Database/DatabaseManager.php
@@ -277,7 +277,7 @@ class DatabaseManager implements ConnectionResolverInterface
      */
     public function supportedDrivers()
     {
-        return ['mysql', 'pgsql', 'sqlite', 'sqlsrv'];
+        return ['mysql', 'mariadb', 'pgsql', 'sqlite', 'sqlsrv'];
     }
 
     /**
