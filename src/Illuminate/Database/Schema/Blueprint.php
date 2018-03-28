9fdfa3c461cc2852f26daddd752c340af99ba4fc
Sam As End <4sam21@gmail.com>
2018-03-28 06:45:52 +0300

    added missing docblock

--- src/Illuminate/Database/Schema/Blueprint.php
+++ src/Illuminate/Database/Schema/Blueprint.php
@@ -391,6 +391,7 @@ class Blueprint
     /**
      * Indicate that the soft delete column should be dropped.
      *
+     * @param  string  $column
      * @return void
      */
     public function dropSoftDeletesTz($column = 'deleted_at')
