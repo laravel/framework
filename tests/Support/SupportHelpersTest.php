f5b94cc21c32a0eb2034dca8bff8500e31060f26
Bálint Szekeres <valentinxxx@gmail.com>
2018-03-29 17:55:16 +0200

    Optimize env() first and last characters check + tests (#23746)
    * env() optimize first and last characters check
    
    * added env() tests

--- tests/Support/SupportHelpersTest.php
+++ tests/Support/SupportHelpersTest.php
@@ -886,6 +886,57 @@ class SupportHelpersTest extends TestCase
             return $five + 5;
         }));
     }
+
+    public function testEnv()
+    {
+        putenv('foo=bar');
+        $this->assertEquals('bar', env('foo'));
+    }
+
+    public function testEnvWithQuotes()
+    {
+        putenv('foo="bar"');
+        $this->assertEquals('bar', env('foo'));
+    }
+
+    public function testEnvTrue()
+    {
+        putenv('foo=true');
+        $this->assertTrue(env('foo'));
+
+        putenv('foo=(true)');
+        $this->assertTrue(env('foo'));
+    }
+
+    public function testEnvFalse()
+    {
+        putenv('foo=false');
+        $this->assertFalse(env('foo'));
+
+        putenv('foo=(false)');
+        $this->assertFalse(env('foo'));
+    }
+
+    public function testEnvEmpty()
+    {
+        putenv('foo=');
+        $this->assertEquals('', env('foo'));
+
+        putenv('foo=empty');
+        $this->assertEquals('', env('foo'));
+
+        putenv('foo=(empty)');
+        $this->assertEquals('', env('foo'));
+    }
+
+    public function testEnvNull()
+    {
+        putenv('foo=null');
+        $this->assertEquals('', env('foo'));
+
+        putenv('foo=(null)');
+        $this->assertEquals('', env('foo'));
+    }
 }
 
 trait SupportTestTraitOne
