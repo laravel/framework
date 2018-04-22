68af14c2f37cc02415a353ed3d795e2f4eff75b4
Vladimir Reznichenko <kalessil@gmail.com>
2018-04-22 12:03:45 +0200

    SCA with Php Inspections (EA Ultimate): code style, as per review

--- src/Illuminate/Database/Connection.php
+++ src/Illuminate/Database/Connection.php
@@ -504,7 +504,7 @@ class Connection implements ConnectionInterface
             }
 
             $this->recordsHaveBeenModified(
-                $change = ($this->getPdo()->exec($query) !== false)
+                $change = $this->getPdo()->exec($query) !== false
             );
 
             return $change;
