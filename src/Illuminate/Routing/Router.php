91aefbe8b265954379537dcb946e3450e8f52832
Paul Redmond <paulrredmond@gmail.com>
2018-04-25 07:49:40 -0700

    Add a named route for POST password/reset (#23958)
    The reset.stub is using the GET named route (password.request) for the POST password/reset request (changed in #17718). Incorrectly using a named route from another route is confusing and could lead to unexpected results.

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -1135,7 +1135,7 @@ class Router implements RegistrarContract, BindingRegistrar
         $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
         $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
         $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-        $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+        $this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
     }
 
     /**
