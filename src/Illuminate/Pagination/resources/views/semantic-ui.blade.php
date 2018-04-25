d7cf66e4fb37a0120bf6a31712e5dd686506883e
Ng Yik Phang <ngyikp@gmail.com>
2018-04-25 22:32:32 +0800

    Improve pagination accessibility (#23962)
    - Add `role="navigation"`
    
      An alternative is to wrap `<nav>` around, but that will shift the
      HTML structure
    - Add `aria-disabled="true"` for disabled items
    - Label previous/next page items with `aria-label`
    - Hide decoration arrows from screen-readers with
      `aria-hidden="true"`
    - Mark current page with `aria-current="page"`

--- src/Illuminate/Pagination/resources/views/semantic-ui.blade.php
+++ src/Illuminate/Pagination/resources/views/semantic-ui.blade.php
@@ -1,24 +1,24 @@
 @if ($paginator->hasPages())
-    <div class="ui pagination menu">
+    <div class="ui pagination menu" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <a class="icon item disabled"> <i class="left chevron icon"></i> </a>
+            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
         @else
-            <a class="icon item" href="{{ $paginator->previousPageUrl() }}" rel="prev"> <i class="left chevron icon"></i> </a>
+            <a class="icon item" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
         @endif
 
         {{-- Pagination Elements --}}
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <a class="icon item disabled">{{ $element }}</a>
+                <a class="icon item disabled" aria-disabled="true">{{ $element }}</a>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <a class="item active" href="{{ $url }}">{{ $page }}</a>
+                        <a class="item active" href="{{ $url }}" aria-current="page">{{ $page }}</a>
                     @else
                         <a class="item" href="{{ $url }}">{{ $page }}</a>
                     @endif
@@ -28,9 +28,9 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a class="icon item" href="{{ $paginator->nextPageUrl() }}" rel="next"> <i class="right chevron icon"></i> </a>
+            <a class="icon item" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')"> <i class="right chevron icon"></i> </a>
         @else
-            <a class="icon item disabled"> <i class="right chevron icon"></i> </a>
+            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.next')"> <i class="right chevron icon"></i> </a>
         @endif
     </div>
 @endif
