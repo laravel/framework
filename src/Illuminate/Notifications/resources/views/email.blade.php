0b4a5b7c5732fc213143ff30835ed29df3e21893
Matthias Niess <mniess@gmail.com>
2018-04-25 20:21:05 +0200

    [5.6] i18n of the default notification email view (#23903)
    * i18n of the default notification email view
    
    This uses json translations through the blade directive to allow easy
    translations of mail notifications.
    
    * fix whitespace problems

--- src/Illuminate/Notifications/resources/views/email.blade.php
+++ src/Illuminate/Notifications/resources/views/email.blade.php
@@ -4,9 +4,9 @@
 # {{ $greeting }}
 @else
 @if ($level == 'error')
-# Whoops!
+# @lang('Whoops!')
 @else
-# Hello!
+# @lang('Hello!')
 @endif
 @endif
 
@@ -45,14 +45,20 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-Regards,<br>{{ config('app.name') }}
+@lang('Regards'),<br>{{ config('app.name') }}
 @endif
 
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+@lang(
+    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
+    'into your web browser: [:actionURL](:actionURL)',
+    [
+        'actionText' => $actionText,
+        'actionURL' => $actionUrl
+    ]
+)
 @endcomponent
 @endisset
 @endcomponent
