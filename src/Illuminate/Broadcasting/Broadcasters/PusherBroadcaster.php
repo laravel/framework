f92b4c38b916edabf15953b0ec57c64f39b1fc17
kevin olson <acidjazz@gmail.com>
2018-04-26 02:37:41 -0500

    ✨ Allow JSONP auth when Broadcasting with Pusher

--- src/Illuminate/Broadcasting/Broadcasters/PusherBroadcaster.php
+++ src/Illuminate/Broadcasting/Broadcasters/PusherBroadcaster.php
@@ -62,24 +62,31 @@ class PusherBroadcaster extends Broadcaster
     {
         if (Str::startsWith($request->channel_name, 'private')) {
             return $this->decodePusherResponse(
+                $request,
                 $this->pusher->socket_auth($request->channel_name, $request->socket_id)
             );
         }
 
         return $this->decodePusherResponse(
+            $request,
             $this->pusher->presence_auth(
                 $request->channel_name, $request->socket_id, $request->user()->getAuthIdentifier(), $result)
         );
     }
 
     /**
-     * Decode the given Pusher response.
+     * Decode the given Pusher response, apply the callback for JSONP support.
      *
+     * @param  mixed  $request
      * @param  mixed  $response
      * @return array
      */
-    protected function decodePusherResponse($response)
+    protected function decodePusherResponse($request, $response)
     {
+        if ($request->callback) {
+            return response()->json(json_decode($response, true))
+            ->withCallback($request->callback);
+        }
         return json_decode($response, true);
     }
 
