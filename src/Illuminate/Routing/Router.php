2409135398c7628fa6ee30941340a13e4a75a29f
I'm Liam <liamhammett@gmail.com>
2018-05-08 21:21:19 +0100

    Add the Route::viewDir(...) method

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -1217,4 +1217,31 @@ class Router implements RegistrarContract, BindingRegistrar
 
         return (new RouteRegistrar($this))->attribute($method, $parameters[0]);
     }
+
+    /**
+     * Register a new wildcard route that returns a view if it exists.
+     *
+     * @param  string  $path
+     * @param  string  $viewDirectory
+     * @param  array  $data
+     * @return \Illuminate\Routing\Route
+     */
+    public function viewDir($path, $viewDirectory = '', $data = [])
+    {
+        $path = trim($path, '/') . '/{page?}';
+
+        return $this->get($path, function ($path = '') use ($viewDirectory, $data) {
+            $viewPath = "{$viewDirectory}.{$path}";
+
+            if (view()->exists($viewPath)) {
+                return view($viewPath)->with($data);
+            }
+
+            if (view()->exists($viewPath . '.index')) {
+                return view($viewPath . '.index')->with($data);
+            }
+
+            abort(404);
+        })->where('page', '.*');
+    }
 }
