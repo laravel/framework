0b83cc1acfedfaccdc6dbc335af86a2ec61382b0
eddiclin <eddiclin@gmail.com>
2018-05-16 20:40:45 +0800

    If the expiration is null, then set it permanently when increments. (#24228)

--- src/Illuminate/Cache/FileStore.php
+++ src/Illuminate/Cache/FileStore.php
@@ -91,7 +91,7 @@ class FileStore implements Store
         $raw = $this->getPayload($key);
 
         return tap(((int) $raw['data']) + $value, function ($newValue) use ($key, $raw) {
-            $this->put($key, $newValue, $raw['time']);
+            $this->put($key, $newValue, $raw['time'] ?? 0);
         });
     }
 
