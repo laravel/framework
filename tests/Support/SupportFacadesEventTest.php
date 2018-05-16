09d34739a6e23125addfe5523cf359d27ed30def
Arjan <arjanwestdorp@gmail.com>
2018-05-16 15:38:19 +0200

    Style fix

--- tests/Support/SupportFacadesEventTest.php
+++ tests/Support/SupportFacadesEventTest.php
@@ -2,14 +2,14 @@
 
 namespace Illuminate\Tests\Support;
 
-use Illuminate\Container\Container;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Events\Dispatcher;
-use Illuminate\Support\Facades\Event;
-use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\Testing\Fakes\EventFake;
 use Mockery;
 use PHPUnit\Framework\TestCase;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Testing\Fakes\EventFake;
 
 class SupportFacadesEventTest extends TestCase
 {
