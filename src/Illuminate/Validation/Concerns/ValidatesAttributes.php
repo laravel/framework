f1f227647e3f325902eb08623c33315f0daa0d13
Jonnathan <jonnsl@hotmail.com>
2018-05-21 11:15:38 -0300

    [5.5] Validation bypass for 'before' and 'after' rules when paired with 'date_format' rule. (#24191)
    * Add more tests to Validator's before and after rules.
    
    When the rules before and/or after are used together with the date_format rules, the validator should give preference to validate a given field relative to the fixed date specified in the rule, instead of comparing to a field with the same name as the specified date.
    
    * Fix validation bypass for rules 'before' and 'after' when paired with 'date_format'.
    
    Now when when there is both a 'date_format' rule and a 'before' and/or 'after' rule,
    the first parameter will be converted to a timestamp, and only if that fails, it will
    be considered as a name for another field. This exactly the same behavior when there
    isn't a date_format rule.
    
    fixes #24191

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -147,9 +147,7 @@ trait ValidatesAttributes
         }
 
         if ($format = $this->getDateFormat($attribute)) {
-            return $this->checkDateTimeOrder(
-                $format, $value, $this->getValue($parameters[0]) ?: $parameters[0], $operator
-            );
+            return $this->checkDateTimeOrder($format, $value, $parameters[0], $operator);
         }
 
         if (! $date = $this->getDateTimestamp($parameters[0])) {
@@ -194,11 +192,13 @@ trait ValidatesAttributes
      */
     protected function checkDateTimeOrder($format, $first, $second, $operator)
     {
-        $first = $this->getDateTimeWithOptionalFormat($format, $first);
+        $firstDate = $this->getDateTimeWithOptionalFormat($format, $first);
 
-        $second = $this->getDateTimeWithOptionalFormat($format, $second);
+        if (! $secondDate = $this->getDateTimeWithOptionalFormat($format, $second)) {
+            $secondDate = $this->getDateTimeWithOptionalFormat($format, $this->getValue($second));
+        }
 
-        return ($first && $second) && ($this->compare($first, $second, $operator));
+        return ($firstDate && $secondDate) && ($this->compare($firstDate, $secondDate, $operator));
     }
 
     /**
