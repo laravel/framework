8ac740249446b3b3c912909a8b6364c12000539b
Ed Preston <epreston@prestonsoft.com>
2018-05-23 04:15:46 +1000

    add "always" to complement "sometimes"

--- src/Illuminate/Validation/Validator.php
+++ src/Illuminate/Validation/Validator.php
@@ -806,6 +806,19 @@ class Validator implements ValidatorContract
             $this->implicitAttributes, $response->implicitAttributes
         );
     }
+    
+     /**
+     * Parse the given rules and merge them into current rules.
+     *
+     * @param  array  $rules
+     * @return $this
+     */
+    public function always($rules)
+    {
+        $this->addRules($rules);
+        
+        return $this;
+    }
 
     /**
      * Add conditions to a given field based on a Closure.
