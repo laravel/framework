5bd6c713a8b0c7716236c2d0485d4f20927a4866
Ed Preston <epreston@prestonsoft.com>
2018-05-24 23:07:44 +1000

    reset doc block (#24300)
    Request to revert #24291 as discussed on that pull request.

--- src/Illuminate/Support/Facades/Validator.php
+++ src/Illuminate/Support/Facades/Validator.php
@@ -3,7 +3,7 @@
 namespace Illuminate\Support\Facades;
 
 /**
- * @method static \Illuminate\Validation\Validator make(array $data, array $rules, array $messages = [], array $customAttributes = [])
+ * @method static \Illuminate\Contracts\Validation\Validator make(array $data, array $rules, array $messages = [], array $customAttributes = [])
  * @method static void extend(string $rule, \Closure | string $extension, string $message = null)
  * @method static void extendImplicit(string $rule, \Closure | string $extension, string $message = null)
  * @method static void replacer(string $rule, \Closure | string $replacer)
