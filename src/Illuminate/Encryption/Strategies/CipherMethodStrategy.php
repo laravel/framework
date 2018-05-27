1ff8a417f65aae3e940f379d5acc0e6c10bd1011
Séra Bálint <sera.balint@e-vista.hu>
2018-05-27 18:21:38 +0200

    some more styleCI fixes

--- src/Illuminate/Encryption/Strategies/CipherMethodStrategy.php
+++ src/Illuminate/Encryption/Strategies/CipherMethodStrategy.php
@@ -3,16 +3,19 @@
  * Created by PhpStorm.
  * User: serabalint
  * Date: 2018. 05. 26.
- * Time: 19:14
+ * Time: 19:14.
  */
 
 namespace Illuminate\Encryption\Strategies;
 
-
 interface CipherMethodStrategy
 {
     public function getCipher() :string;
+
     public function getKey() :string;
+
     public function generateKey();
+
     public function supported();
+
 }
