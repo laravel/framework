255b8fc30036c3fd05544d4d6bf9b57e3fd64592
Emre Kaya <3emrekaya@gmail.com>
2018-05-27 15:42:47 +0300

    Add model option to command.

--- src/Illuminate/Foundation/Console/ObserverMakeCommand.php
+++ src/Illuminate/Foundation/Console/ObserverMakeCommand.php
@@ -3,6 +3,8 @@
 namespace Illuminate\Foundation\Console;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
 
 class ObserverMakeCommand extends GeneratorCommand
 {
@@ -28,13 +30,64 @@ class ObserverMakeCommand extends GeneratorCommand
     protected $type = 'Observer';
 
     /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        $stub = parent::buildClass($name);
+
+        $model = $this->option('model');
+
+        return $model ? $this->replaceModel($stub, $model) : $stub;
+    }
+
+    /**
      * Get the stub file for the generator.
      *
      * @return string
      */
     protected function getStub()
     {
-        return __DIR__.'/stubs/observer.stub';
+        return $this->option('model')
+                    ? __DIR__.'/stubs/observer.stub'
+                    : __DIR__.'/stubs/observer.plain.stub';
+    }
+
+    /**
+     * Replace the model for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $model
+     * @return string
+     */
+    protected function replaceModel($stub, $model)
+    {
+        $model = str_replace('/', '\\', $model);
+
+        $namespaceModel = $this->laravel->getNamespace().$model;
+
+        if (Str::startsWith($model, '\\')) {
+            $stub = str_replace('NamespacedDummyModel', trim($model, '\\'), $stub);
+        } else {
+            $stub = str_replace('NamespacedDummyModel', $namespaceModel, $stub);
+        }
+
+        $stub = str_replace(
+            "use {$namespaceModel};\nuse {$namespaceModel};", "use {$namespaceModel};", $stub
+        );
+
+        $model = class_basename(trim($model, '\\'));
+
+        $dummyModel = $model;
+
+        $stub = str_replace('DocDummyModel', Str::snake($dummyModel, ' '), $stub);
+
+        $stub = str_replace('DummyModel', $model, $stub);
+
+        return str_replace('dummyModel', Str::camel($dummyModel), $stub);
     }
 
     /**
@@ -46,5 +99,17 @@ class ObserverMakeCommand extends GeneratorCommand
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace.'\Observers';
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['model', 'm', InputOption::VALUE_OPTIONAL, 'The model that the observer applies to.'],
+        ];
     }
 }
