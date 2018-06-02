160969c5a7f735d3cefe43911abc6b9f3d666d31
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-06-02 16:58:00 +0300

    Add `defaults` method to URL Facade.

--- src/Illuminate/Support/Facades/URL.php
+++ src/Illuminate/Support/Facades/URL.php
@@ -15,6 +15,7 @@ namespace Illuminate\Support\Facades;
  * @method static string signedRoute(string $name, array $parameters = [], \DateTimeInterface|int $expiration = null)
  * @method static string temporarySignedRoute(string $name, \DateTimeInterface|int $expiration, array $parameters = [])
  * @method static string hasValidSignature(\Illuminate\Http\Request $request)
+ * @method static void defaults(array $defaults)
  *
  * @see \Illuminate\Routing\UrlGenerator
  */
