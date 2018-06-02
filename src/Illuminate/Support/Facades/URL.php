9c71c5daa808e63997ec08aec63797a33b17e8db
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-06-02 16:20:56 +0300

    Add `full` and `previous` methods to URL Facade  - https://laravel.com/docs/5.6/urls in the documentation we have sentence `Each of these methods may also be accessed via the URL facade:
    `

--- src/Illuminate/Support/Facades/URL.php
+++ src/Illuminate/Support/Facades/URL.php
@@ -4,6 +4,8 @@ namespace Illuminate\Support\Facades;
 
 /**
  * @method static string current()
+ * @method static string full()
+ * @method static string previous($fallback = false)
  * @method static string to(string $path, $extra = [], bool $secure = null)
  * @method static string secure(string $path, array $parameters = [])
  * @method static string asset(string $path, bool $secure = null)
