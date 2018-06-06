ea3b5e5300be3824cee30dfa9608d27d8432addf
有明 <youming@funcuter.org>
2018-06-06 21:18:16 +0800

    Add exit status to WorkStopping event. (#24476)

--- src/Illuminate/Queue/Events/WorkerStopping.php
+++ src/Illuminate/Queue/Events/WorkerStopping.php
@@ -4,5 +4,21 @@ namespace Illuminate\Queue\Events;
 
 class WorkerStopping
 {
-    //
+    /**
+     * The exit status.
+     *
+     * @var int
+     */
+    public $status;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  int  $status
+     * @return void
+     */
+    public function __construct($status = 0)
+    {
+        $this->status = $status;
+    }
 }
