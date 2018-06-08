26214b2c46b580a799f2748dc28698e958d3e995
mendicm <mendicm@gmail.com>
2018-06-08 08:42:57 +0200

    Fixes #20616

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Carbon;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Collection as BaseCollection;
 use Illuminate\Database\Eloquent\JsonEncodingException;
 
@@ -408,6 +409,14 @@ trait HasAttributes
 
         if (! $relation instanceof Relation) {
             throw new LogicException(get_class($this).'::'.$method.' must return a relationship instance.');
+        }
+        
+        // If the foreign key is null then the relation is not setted and we don't 
+        // need to perform any query.
+        if ($relation instanceof BelongsTo) {
+            if (is_null($this->{$relation->getForeignKey()})) {
+                return null;
+            }
         }
 
         return tap($relation->getResults(), function ($results) use ($method) {
