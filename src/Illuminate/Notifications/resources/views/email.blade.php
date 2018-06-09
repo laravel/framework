0b419c856160fadb8eccc0b3be0b760be17f28d8
Clas Heinemeyer <the.checodo@gmail.com>
2018-06-09 15:48:06 +0200

    Make email subcopy-text translatable via json

--- src/Illuminate/Notifications/resources/views/email.blade.php
+++ src/Illuminate/Notifications/resources/views/email.blade.php
@@ -52,8 +52,7 @@
 @isset($actionText)
 @component('mail::subcopy')
 @lang(
-    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser: [:actionURL](:actionURL)',
+    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\ninto your web browser: [:actionURL](:actionURL)",
     [
         'actionText' => $actionText,
         'actionURL' => $actionUrl
