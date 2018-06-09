dfa271d4417266f1631a9c07a3542d83f306926a
Dimitri König <dimitri@dimitrikoenig.net>
2018-06-09 19:53:58 +0200

    Make ResetPassword Notification translateable

--- src/Illuminate/Auth/Notifications/ResetPassword.php
+++ src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -2,6 +2,7 @@
 
 namespace Illuminate\Auth\Notifications;
 
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 
@@ -56,9 +57,10 @@ class ResetPassword extends Notification
         }
 
         return (new MailMessage)
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
-            ->line('If you did not request a password reset, no further action is required.');
+            ->subject(Lang::getFromJson('Reset Password Notification'))
+            ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your account.'))
+            ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('password.reset', $this->token, false)))
+            ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
     }
 
     /**
