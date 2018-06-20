e5e1c88c028bce745a810ecc0b4f7da9514f2d0c
Taylor Otwell <taylor@laravel.com>
2018-06-20 16:16:20 -0500

    initial pass

---
+++ src/Illuminate/Foundation/Auth/VerifiesEmails.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Http\Request;
+
+trait VerifiesEmails
+{
+    use RedirectsUsers;
+
+    /**
+     * Show the email verification notice.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        return $request->user()->hasVerifiedEmail()
+                        ? redirect($this->redirectPath())
+                        : view('auth.verify');
+    }
+
+    /**
+     * Mark the authenticated user's email address as verified.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function verify(Request $request)
+    {
+        if ($request->route('id') == $request->user()->getKey()) {
+            $request->user()->markEmailAsVerified();
+        }
+
+        return redirect($this->redirectPath());
+    }
+}
