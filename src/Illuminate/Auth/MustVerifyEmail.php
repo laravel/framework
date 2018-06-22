35c68c20922936bf1f1ae07473b1f6730a04f72c
Taylor Otwell <taylor@laravel.com>
2018-06-22 10:25:08 -0500

    [5.7] Email Verification (Framework) (#24655)
    This PR adds optional email verification to the typical "make:auth" setup. To utilize this, the end-user must only do "make:auth" and then add implements MustVerifyEmail to their User model. Everything should be automatic from there.
    
    This uses temporary signed URLs to avoid creating another "token" table, etc.
    
    Corresponding application skeleton PR will be incoming to add necessary controller, as well as middleware registration.

---
+++ src/Illuminate/Auth/MustVerifyEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Illuminate\Auth;
+
+use DateTime;
+
+trait MustVerifyEmail
+{
+    /**
+     * Determine if the user has verified their email address.
+     *
+     * @return bool
+     */
+    public function hasVerifiedEmail()
+    {
+        return ! is_null($this->email_verified_at);
+    }
+
+    /**
+     * Mark the given user's email as verified.
+     *
+     * @return void
+     */
+    public function markEmailAsVerified()
+    {
+        $this->forceFill(['email_verified_at' => new DateTime])->save();
+    }
+
+    /**
+     * Send the email verification notification.
+     *
+     * @return void
+     */
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new Notifications\VerifyEmail);
+    }
+}
