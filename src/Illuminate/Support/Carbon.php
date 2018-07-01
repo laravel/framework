aa8603adabda53b766177bfbaee92fc2cf35114b
Lucas Michot <lucas@semalead.com>
2018-07-01 23:11:43 +0200

    [5.7] Let \Illuminate\Support\Carbon support immutable datetimes.

--- src/Illuminate/Support/Carbon.php
+++ src/Illuminate/Support/Carbon.php
@@ -3,7 +3,21 @@
 namespace Illuminate\Support;
 
 use Carbon\Carbon as BaseCarbon;
+use Cake\Chronos\MutableDateTime;
 
 class Carbon extends BaseCarbon
 {
+    /**
+     * Return an immutable datetime.
+     *
+     * @return \Cake\Chronos\Chronos
+     */
+    public function immutable()
+    {
+        return MutableDateTime::createFromFormat(
+            'Y-m-d H:i:s',
+            $this->toDateTimeString(),
+            $this->getTimezone()
+        )->toImmutable();
+    }
 }
