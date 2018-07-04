e0f60349b21c60348cfaf02b9146a04cdb3edc5a
Dwight Watson <dwightwatson@me.com>
2018-07-05 09:38:49 +1000

    Add SecuresRequest middleware

---
+++ src/Illuminate/Foundation/Http/Middleware/SecuresRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Illuminate\Foundation\Http\Middleware;
+
+use Closure;
+class SecuresRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ( ! $request->secure() && config('app.https')) {
+            return redirect()->secure($request->path(), 301);
+        }
+
+        return $next($request);
+    }
+}
