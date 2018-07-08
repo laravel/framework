7ef28431cfb53a2da3829bb6596c64e06cd342c5
Nuno Maduro <enunomaduro@gmail.com>
2018-07-08 19:50:27 +0200

    Updates render method phpdocs return type

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -162,7 +162,7 @@ class Handler implements ExceptionHandlerContract
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Exception  $e
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Http\Response
      */
     public function render($request, Exception $e)
     {
