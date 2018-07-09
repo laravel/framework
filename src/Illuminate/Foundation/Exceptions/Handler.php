f8d042a976c78e929eb8a1a8a9b59d3cdd15b760
Taylor Otwell <taylor@laravel.com>
2018-07-09 08:18:51 -0500

    formatting

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -162,7 +162,7 @@ class Handler implements ExceptionHandlerContract
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Exception  $e
-     * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Http\Response
+     * @return \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\Response
      */
     public function render($request, Exception $e)
     {
