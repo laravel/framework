097d0f7a572779308ce6952613e1b4d57fa4715e
Deleu <deleugyn@gmail.com>
2018-07-17 04:28:06 -0400

    Add namespace

--- tests/Integration/Migration/fixtures/2014_10_12_000000_create_members_table.php
+++ tests/Integration/Migration/fixtures/2014_10_12_000000_create_members_table.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Illuminate\Tests\Integration\Migration\fixtures;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
