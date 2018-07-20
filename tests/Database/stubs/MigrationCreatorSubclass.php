0bd124ca5f3c7e274b5f65d521f8f9490c477e6c
Chris Halbert <christopher.halbert@gmail.com>
2018-07-20 16:45:45 -0400

    [MigrationCreatorPatch-Issue-24907] Add example of a subclass for original 5.6 file.

---
+++ tests/Database/stubs/MigrationCreatorSubclass.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Foo\Bar;
+
+use Illuminate\Database\Migrations\MigrationCreator;
+
+/**
+ * Class MigrationCreatorSubclass
+ * @package Foo\Bar
+ */
+class MigrationCreatorSubclass extends MigrationCreator
+{
+    /**
+     * Here we override the original method of Laravel 5.6's original firePostCreateHook, with no parameters.
+     */
+    protected function firePostCreateHooks()
+    {
+        // Run an arbitrary command
+        parent::firePostCreateHooks();
+    }
+}
