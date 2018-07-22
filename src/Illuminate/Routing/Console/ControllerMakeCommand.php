66f2617017d252a5078f37774701ef3d16b5324d
Taylor Otwell <taylor@laravel.com>
2018-07-22 09:41:16 -0500

    formatting

--- src/Illuminate/Routing/Console/ControllerMakeCommand.php
+++ src/Illuminate/Routing/Console/ControllerMakeCommand.php
@@ -175,13 +175,9 @@ class ControllerMakeCommand extends GeneratorCommand
     {
         return [
             ['model', 'm', InputOption::VALUE_OPTIONAL, 'Generate a resource controller for the given model.'],
-
             ['resource', 'r', InputOption::VALUE_NONE, 'Generate a resource controller class.'],
-
-            ['invokable', 'i', InputOption::VALUE_NONE, 'Generate a single action controller class.'],
-
+            ['invokable', 'i', InputOption::VALUE_NONE, 'Generate a single method, invokable controller class.'],
             ['parent', 'p', InputOption::VALUE_OPTIONAL, 'Generate a nested resource controller class.'],
-
             ['api', null, InputOption::VALUE_NONE, 'Exclude the create and edit methods from the controller.'],
         ];
     }
