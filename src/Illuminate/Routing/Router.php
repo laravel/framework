14d6477ebf5fce150dce0d4ca3822106022db120
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2018-08-01 17:44:44 +0100

    Adding fix for failed test
    This pr simply updates existing code to handle cases where group middlewares should have their parameter shared amongst it individual middleware

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -677,24 +677,23 @@ class Router implements RegistrarContract, BindingRegistrar
     {
         $middleware = collect($route->gatherMiddleware())->map(function ($name) {
             // try to see if the name contains any middleware parameters
-            if (is_string($name) && strpos($name, ':') !== FALSE) {
+            if (is_string($name) && strpos($name, ':') !== false) {
                 list($main_name, $args) = explode(':', $name);
                 // check to see if the `$main_name` is part of the middleware groups only
                 if (array_key_exists($main_name, $this->middlewareGroups)) {
-                    $group =  $this->middlewareGroups[$main_name];
+                    $groups = $this->middlewareGroups[$main_name];
                     // distribute the middleware parameters to all
                     // individual middlewares that make up the group
-                    foreach ($group as $key => $middleware) {
-                        if (!isset($this->middlewareGroups[$middleware])) {
-                            $group[$key] = "{$middleware}:{$args}";
+                    foreach ($groups as $key => $middleware) {
+                        if (! isset($this->middlewareGroups[$middleware])) {
+                            $groups[$key] = "{$middleware}:{$args}";
                         }
                     }
                     // overwrite major group middleware array with modified one
-                    $this->middlewareGroups[$main_name] = $group;
+                    $this->middlewareGroups[$main_name] = $groups;
                     $name = $main_name;
                 }
             }
-            
             return (array) MiddlewareNameResolver::resolve($name, $this->middleware, $this->middlewareGroups);
         })->flatten();
 
