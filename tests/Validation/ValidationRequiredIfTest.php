c955ce945e85eb17cef40187a5f9a8a7b2322328
Alfonso Bribiesca <alfonso@vexilo.com>
2018-08-02 15:23:50 -0500

    Required if rule test

---
+++ tests/Validation/ValidationRequiredIfTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Illuminate\Tests\Validation;
+
+use Illuminate\Validation\Rule;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Validation\Rules\RequiredIf;
+
+class ValidationRequiredIfTest extends TestCase
+{
+    public function testItClousureReturnsFormatsAStringVersionOfTheRule()
+    {
+        $rule = new RequiredIf(function () {
+            return true;
+        });
+
+        $this->assertEquals('required', (string) $rule);
+
+        $rule = new RequiredIf(function () {
+            return false;
+        });
+
+        $this->assertEquals('', (string) $rule);
+    }
+}
