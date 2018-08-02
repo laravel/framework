7661e76a29306a6e4cce719ebfba460fa85f8204
Eugene <37518271+Eugene-Melbourne@users.noreply.github.com>
2018-08-02 14:50:14 +1000

    formatting
    changed formatting to satisfy styleci.io

--- tests/Validation/ValidationAddFailureTest.php
+++ tests/Validation/ValidationAddFailureTest.php
@@ -2,23 +2,22 @@
 
 namespace Illuminate\Tests\Validation;
 
+use ReflectionMethod;
 use PHPUnit\Framework\TestCase;
 use Illuminate\Validation\Validator;
-use Illuminate\Tests\Validation\ValidationValidatorTest;
-use ReflectionMethod;
 
 class ValidationAddFailureTest extends TestCase
 {
     /**
-     * Making Validator using ValidationValidatorTest
-     * 
+     * Making Validator using ValidationValidatorTest.
+     *
      * @return Validator
      */
     public function makeValidator(): Validator
     {
         $mainTest = new ValidationValidatorTest();
-        $trans    = $mainTest->getIlluminateArrayTranslator();
-        $v        = new Validator($trans, ['foo' => ['bar' => ['baz' => '']]], ['foo.bar.baz' => 'sometimes|required']);
+        $trans = $mainTest->getIlluminateArrayTranslator();
+        $v = new Validator($trans, ['foo' => ['bar' => ['baz' => '']]], ['foo.bar.baz' => 'sometimes|required']);
 
         return $v;
     }
@@ -39,10 +38,10 @@ class ValidationAddFailureTest extends TestCase
 
     public function testAddFailureExistsAndVisibile()
     {
-        $validator   = $this->makeValidator();
+        $validator = $this->makeValidator();
         $method_name = 'addFailure';
         $this->assertTrue(method_exists($validator, $method_name));
-        $this->assertTrue(is_callable(array($validator, $method_name)));
+        $this->assertTrue(is_callable([$validator, $method_name]));
         $this->assertPublicMethod($validator, $method_name);
     }
 
@@ -52,9 +51,7 @@ class ValidationAddFailureTest extends TestCase
         $validator = $this->makeValidator();
         $validator->addFailure($attribute, 'not_in');
         $messages = json_decode($validator->messages());
-        $this->assertSame($messages->{"foo.bar.baz"}[0], "validation.required", 'initial data in messages is lost');
-        $this->assertSame($messages->{$attribute}[0], "validation.not_in", 'new data in messages was not added');
+        $this->assertSame($messages->{'foo.bar.baz'}[0], 'validation.required', 'initial data in messages is lost');
+        $this->assertSame($messages->{$attribute}[0], 'validation.not_in', 'new data in messages was not added');
     }
-
-
 }
