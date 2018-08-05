a4d00c91ed38a17ea16175704b65ced6cbb300db
Eugene <37518271+Eugene-Melbourne@users.noreply.github.com>
2018-08-06 09:28:05 +1000

    removed class reflection
    removed all of those tests that are reflecting into the class to make sure it's public 😬

--- tests/Validation/ValidationAddFailureTest.php
+++ tests/Validation/ValidationAddFailureTest.php
@@ -2,7 +2,6 @@
 
 namespace Illuminate\Tests\Validation;
 
-use ReflectionMethod;
 use PHPUnit\Framework\TestCase;
 use Illuminate\Validation\Validator;
 
@@ -22,27 +21,12 @@ class ValidationAddFailureTest extends TestCase
         return $v;
     }
 
-    /**
-     * Assert that a method has public access.
-     *
-     * @param string $class name of the class
-     * @param string $method name of the method
-     * @throws ReflectionException if $class or $method don't exist
-     * @throws PHPUnit_Framework_ExpectationFailedException if the method is not public
-     */
-    public function assertPublicMethod($class, $method)
-    {
-        $reflector = new ReflectionMethod($class, $method);
-        self::assertTrue($reflector->isPublic(), 'method is not public');
-    }
-
-    public function testAddFailureExistsAndVisibile()
+    public function testAddFailureExists()
     {
         $validator = $this->makeValidator();
         $method_name = 'addFailure';
         $this->assertTrue(method_exists($validator, $method_name));
         $this->assertTrue(is_callable([$validator, $method_name]));
-        $this->assertPublicMethod($validator, $method_name);
     }
 
     public function testAddFailureIsFunctional()
