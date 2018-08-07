944d4bc26580495825cd7d06eb98981500e2f23d
Filtration <oracrin@gmail.com>
2018-08-07 10:15:13 +0100

    Pass the collumns to the paginator count method
    At the moment, you can only select count(*) the columns you pass through do not bind to the getCountForPagination, this can speed up the count query with the correct indexes

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -741,7 +741,7 @@ class Builder
 
         $perPage = $perPage ?: $this->model->getPerPage();
 
-        $results = ($total = $this->toBase()->getCountForPagination())
+        $results = ($total = $this->toBase()->getCountForPagination($columns))
                                     ? $this->forPage($page, $perPage)->get($columns)
                                     : $this->model->newCollection();
 
