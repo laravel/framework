4d3730e56ad91f4a0c05abe63ea5f9190cc15739
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-08-11 19:01:17 -0300

    use lang directive

--- src/Illuminate/Mail/resources/views/html/message.blade.php
+++ src/Illuminate/Mail/resources/views/html/message.blade.php
@@ -21,7 +21,7 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
+            &copy; {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
         @endcomponent
     @endslot
 @endcomponent
