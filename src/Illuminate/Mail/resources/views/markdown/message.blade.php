d8433be23d5c5e5a2f1d2467add559ffbf099e7c
Lucas Leandro <lucasleandro1204@gmail.com>
2018-08-11 22:51:30 -0300

    [5.6] i18n default mail copyright (#25183)
    * use lang directive
    
    * use symbol instead

--- src/Illuminate/Mail/resources/views/markdown/message.blade.php
+++ src/Illuminate/Mail/resources/views/markdown/message.blade.php
@@ -21,7 +21,7 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            © {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
+            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
         @endcomponent
     @endslot
 @endcomponent
