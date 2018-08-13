e5231c57fbde24f192045bc0b2681968ac5de6da
Michal <m.putkowski@gmail.com>
2018-08-13 21:17:57 +0200

    remove unnecessary self-assignment

--- src/Illuminate/Http/Resources/Json/JsonResource.php
+++ src/Illuminate/Http/Resources/Json/JsonResource.php
@@ -92,7 +92,7 @@ class JsonResource implements ArrayAccess, JsonSerializable, Responsable, UrlRou
         );
 
         if (is_array($data)) {
-            $data = $data;
+            return $this->filter($data);
         } elseif ($data instanceof Arrayable || $data instanceof Collection) {
             $data = $data->toArray();
         } elseif ($data instanceof JsonSerializable) {
