c02a83062f89179d31579376ec9c832362813dff
Harish Toshniwal <haristoshniwal@gmail.com>
2018-08-17 23:49:54 +0530

    fix migration name generation when using -p flag

--- src/Illuminate/Foundation/Console/ModelMakeCommand.php
+++ src/Illuminate/Foundation/Console/ModelMakeCommand.php
@@ -83,6 +83,10 @@ class ModelMakeCommand extends GeneratorCommand
     protected function createMigration()
     {
         $table = Str::plural(Str::snake(class_basename($this->argument('name'))));
+        
+        if ($this->option('pivot')) {
+            $table = Str::singular($table);
+        }
 
         $this->call('make:migration', [
             'name' => "create_{$table}_table",
