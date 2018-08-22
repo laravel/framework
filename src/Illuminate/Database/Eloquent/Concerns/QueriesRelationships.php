d9c7093b5c66ae7bb28c8f5f947c5116df7cd4f3
mohammad sharif ahrari <msharif.ahrari@gmail.com>
2018-08-22 10:47:29 +0430

    add withSum
    add withSum to framework . in the past people try to add withMax withMin withAvg withSum and ... together and implements withCount
    i think its should be separte because in withSum withAvg and... we should determine what column but in withCount we dont 
    so i add withSum if you think its ok( i test it ) add withAvg and .... . im not pro to do that soory for my bad english

--- src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
+++ src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
@@ -226,6 +226,63 @@ trait QueriesRelationships
 
         return $this;
     }
+    /**
+     * Add subselect queries to sum the relations.
+     *
+     * @param  mixed  $relations
+     * @return $this
+     */
+    public function withSum($relations,$column)
+    {
+        if (empty($relations)) {
+            return $this;
+        }
+
+        if (is_null($this->query->columns)) {
+            $this->query->select([$this->query->from.'.*']);
+        }
+
+        $relations = is_array($relations) ? $relations : [$relations];
+
+        foreach ($this->parseWithRelations($relations) as $name => $constraints) {
+            // First we will determine if the name has been aliased using an "as" clause on the name
+            // and if it has we will extract the actual relationship name and the desired name of
+            // the resulting column. This allows multiple counts on the same relationship name.
+            $segments = explode(' ', $name);
+
+            unset($alias);
+
+            if (count($segments) == 3 && Str::lower($segments[1]) == 'as') {
+                list($name, $alias) = [$segments[0], $segments[2]];
+            }
+
+            $relation = $this->getRelationWithoutConstraints($name);
+
+            // Here we will get the relationship count query and prepare to add it to the main query
+            // as a sub-select. First, we'll get the "has" query and use that to get the relation
+            // count query. We will normalize the relation name then append _count as the name.
+            $query = $relation->getRelationExistenceSumQuery(
+                $relation->getRelated()->newQuery(), $this,$column
+            );
+
+            $query->callScope($constraints);
+
+            $query = $query->mergeConstraintsFrom($relation->getQuery())->toBase();
+
+            if (count($query->columns) > 1) {
+                $query->columns = [$query->columns[0]];
+            }
+
+            // Finally we will add the proper result column alias to the query and run the subselect
+            // statement against the query builder. Then we will return the builder instance back
+            // to the developer for further constraint chaining that needs to take place on it.
+            $column = $alias ?? Str::snake($name.'_sum');
+
+            $this->selectSub($query, $column);
+        }
+
+        return $this;
+    }
 
     /**
      * Add the "has" condition where clause to the query.
