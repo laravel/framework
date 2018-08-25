861f9b28383d60d9c0094833707032effab26604
Deleu <deleugyn@gmail.com>
2018-08-25 06:34:09 -0400

    Use Provider to cover various rules

--- tests/Validation/ValidatesAttributesTest.php
+++ tests/Validation/ValidatesAttributesTest.php
@@ -21,58 +21,67 @@ class ValidatesAttributesTest extends TestCase
         $this->translator = Mockery::spy(Translator::class);
     }
 
-    public function test_validate_bool()
+    /**
+     * @dataProvider passingValidatorProvider
+     */
+    public function test_passing_validator($value, $rules)
     {
-        $validator = new Validator($this->translator, [
-            'attr_1' => 1,
-            'attr_2' => true,
-        ], [
-            'attr_1' => 'required|bool',
-            'attr_2' => 'required|bool',
-        ]);
+        $validator = new Validator($this->translator, ['value' => $value], ['value' => $rules]);
 
         self::assertTrue($validator->passes());
     }
 
-    public function test_validate_accepted()
+    public function passingValidatorProvider()
     {
-        $validator = new Validator($this->translator, [
-            'attr_1' => 1,
-            'attr_2' => true,
-            'attr_3' => 'true',
-            'attr_4' => 'yes',
-            'attr_5' => 'on',
-        ], [
-            'attr_1' => 'required|accepted',
-            'attr_2' => 'required|accepted',
-            'attr_3' => 'required|accepted',
-            'attr_4' => 'required|accepted',
-            'attr_5' => 'required|accepted',
-        ]);
+        return [
+            'pass bool rule with true'      => [true, 'bool'],
+            'pass bool rule with false'     => [false, 'bool'],
+            'pass bool rule with 1'         => [1, 'bool'],
+            'pass bool rule with 0'         => [0, 'bool'],
+            'pass bool rule with string 1'  => ['1', 'bool'],
+            'pass bool rule with string 0'  => ['0', 'bool'],
+            'pass bool rule with empty'     => ['', 'bool'],
 
-        self::assertTrue($validator->passes());
+            'pass accepted rule with true'        => [true, 'accepted'],
+            'pass accepted rule with string true' => ['true', 'accepted'],
+            'pass accepted rule with on'          => ['on', 'accepted'],
+            'pass accepted rule with 1'           => [1, 'accepted'],
+            'pass accepted rule with yes'         => ['yes', 'accepted'],
+
+            'pass alpha rule with lowercase' => ['abcdefghijklmnopqrstuvwxyz', 'alpha'],
+            'pass alpha rule with uppercase' => ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'alpha'],
+
+            'pass max rule' => [5, 'max:10'],
+            'pass max rule at the exact limit' => [200, 'max:200'],
+
+            'pass array rule with array'       => [[1, 2, 3], 'array'],
+            'pass array rule with empty array' => [[], 'array'],
+        ];
     }
 
-    public function test_validate_alpha()
+    /**
+     * @dataProvider failingValidatorProvider
+     */
+    public function test_failing_validator($value, $rules)
     {
-        $validator = new Validator($this->translator, [
-            'attr_1' => 'abcdefghijklmnopqrstuvwxyz',
-        ], [
-            'attr_1' => 'required|alpha',
-       ]);
-
-        self::assertTrue($validator->passes());
-    }
-
-    public function test_validate_max()
-    {
-        $validator = new Validator($this->translator, [
-            'attr' => 5,
-        ], [
-            'attr' => 'int|max:4',
-        ]);
+        $validator = new Validator($this->translator, ['value' => $value], ['value' => $rules]);
 
         self::assertTrue($validator->fails());
+    }
+
+    public function failingValidatorProvider()
+    {
+        return [
+            'fail bool rule with string' => ['a', 'bool'],
+
+            'fail accept rule with string' => ['a', 'accepted'],
+            'fail accept rule with off'    => ['off', 'accepted'],
+            'fail accept rule with 0'      => [0, 'accepted'],
+
+            'fail required rule with empty'       => ['', 'required'],
+            'fail required rule with null'        => [null, 'required'],
+            'fail required rule with empty array' => [[], 'required'],
+        ];
     }
 
     public function test_validate_after_and_before_date()
