a784efcc17b1c683bb64b6e0916c4e2a6cea231c
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2018-08-29 12:55:06 +0200

    Fix relationships with global scope columns (#25368)

--- tests/Integration/Database/EloquentHasManyThroughTest.php
+++ tests/Integration/Database/EloquentHasManyThroughTest.php
@@ -45,6 +45,19 @@ class EloquentHasManyThroughTest extends DatabaseTestCase
         $this->assertEquals([$mate1->id, $mate2->id], $user->teamMates->pluck('id')->toArray());
         $this->assertEquals([$user->id], User::has('teamMates')->pluck('id')->toArray());
     }
+
+    public function test_global_scope_columns()
+    {
+        $user = User::create(['name' => str_random()]);
+
+        $team1 = Team::create(['owner_id' => $user->id]);
+
+        User::create(['name' => str_random(), 'team_id' => $team1->id]);
+
+        $teamMates = $user->teamMatesWithGlobalScope;
+
+        $this->assertEquals(['id' => 2, 'owner_id' => 1], $teamMates[0]->getAttributes());
+    }
 }
 
 class User extends Model
@@ -56,6 +69,27 @@ class User extends Model
     public function teamMates()
     {
         return $this->hasManyThrough(self::class, Team::class, 'owner_id', 'team_id');
+    }
+
+    public function teamMatesWithGlobalScope()
+    {
+        return $this->hasManyThrough(UserWithGlobalScope::class, Team::class, 'owner_id', 'team_id');
+    }
+}
+
+class UserWithGlobalScope extends Model
+{
+    public $table = 'users';
+    public $timestamps = false;
+    protected $guarded = ['id'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(function ($query) {
+            $query->select('users.id');
+        });
     }
 }
 
