4eab953451b957d4ad341fe5b63a7fde67670f59
Joseph Wensley <jwensley2@gmail.com>
2018-08-29 17:40:58 -0400

    Fix mailables always being queued for later if using Queueable trait (#25378)

--- src/Illuminate/Mail/Mailable.php
+++ src/Illuminate/Mail/Mailable.php
@@ -157,7 +157,7 @@ class Mailable implements MailableContract, Renderable
      */
     public function queue(Queue $queue)
     {
-        if (property_exists($this, 'delay')) {
+        if (isset($this->delay)) {
             return $this->later($this->delay, $queue);
         }
 
