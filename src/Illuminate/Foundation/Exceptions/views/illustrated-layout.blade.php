fee9658f508af07e3fe2f36baa664aa96dd31bae
Taylor Otwell <taylor@laravel.com>
2018-08-31 14:05:57 +0200

    wip

--- src/Illuminate/Foundation/Exceptions/views/illustrated-layout.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/illustrated-layout.blade.php
@@ -469,9 +469,11 @@
             @yield('message')
           </p>
 
-          <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
-            Go Home
-          </button>
+          <a href="/">
+            <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
+              Go Home
+            </button>
+          </a>
         </div>
       </div>
 
