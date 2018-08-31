7ecce6ca2dab634b9761542886af02623f89cafd
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2018-08-31 21:15:45 +0200

    Translate error messsages

--- src/Illuminate/Foundation/Exceptions/views/404.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/404.blade.php
@@ -1,11 +1,11 @@
 @extends('errors::illustrated-layout')
 
 @section('code', '404')
-@section('title', 'Page Not Found')
+@section('title', __('Page Not Found'))
 
 @section('image')
 <div style="background-image: url('/svg/404.svg');" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
 </div>
 @endsection
 
-@section('message', 'Sorry, the page you are looking for could not be found.')
+@section('message', __('Sorry, the page you are looking for could not be found.'))
--- src/Illuminate/Foundation/Exceptions/views/419.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/419.blade.php
@@ -1,11 +1,11 @@
 @extends('errors::illustrated-layout')
 
 @section('code', '419')
-@section('title', 'Page Expired')
+@section('title', __('Page Expired'))
 
 @section('image')
 <div style="background-image: url('/svg/403.svg');" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
 </div>
 @endsection
 
-@section('message', 'Sorry, your session has expired. Please refresh and try again.')
+@section('message', __('Sorry, your session has expired. Please refresh and try again.'))
