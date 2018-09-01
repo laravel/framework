47593e84d7c349662b3457dee16b5d0b93f71292
Mathieu TUDISCO <dev@mathieutu.ovh>
2018-09-01 15:08:04 +0200

    Move Arr class.

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -2,7 +2,7 @@
 
 namespace Illuminate\Support;
 
-/** @deprecated You should use \Illuminate\Collect\Collection now. */
+/** @deprecated You should use \Illuminate\Collect\Collection. */
 class Collection extends \Illuminate\Collect\Collection
 {
 }
