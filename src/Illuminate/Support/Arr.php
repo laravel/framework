00a0b9a765ab026b0764904ff1d1eccc836d42b0
Mathieu TUDISCO <dev@mathieutu.ovh>
2018-09-03 10:43:33 +0200

    Style

--- src/Illuminate/Support/Arr.php
+++ src/Illuminate/Support/Arr.php
@@ -2,10 +2,6 @@
 
 namespace Illuminate\Support;
 
-use ArrayAccess;
-use InvalidArgumentException;
-use Illuminate\Support\Traits\Macroable;
-
 /** @deprecated You should use \Illuminate\Collect\Arr. */
 class Arr extends \Illuminate\Collect\Arr
 {
