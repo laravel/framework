40ccbc3497198333d3c75c51729d94bb32c77d0d
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-09-03 15:54:40 +0200

    Pass authorization by default (#25417)

--- src/Illuminate/Foundation/Http/FormRequest.php
+++ src/Illuminate/Foundation/Http/FormRequest.php
@@ -150,7 +150,7 @@ class FormRequest extends Request implements ValidatesWhenResolved
             return $this->container->call([$this, 'authorize']);
         }
 
-        return false;
+        return true;
     }
 
     /**
