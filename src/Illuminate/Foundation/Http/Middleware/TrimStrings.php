d86a9ab2c2a1e71da00bbdd605b2c7a664bbc52e
Maksim <crashmybrain@gmail.com>
2018-09-05 20:04:12 +0300

    Add check for array name exception
    This patch corrects the ability to add attributes with an array type to the exceptio list on TrimStrings middleware

--- src/Illuminate/Foundation/Http/Middleware/TrimStrings.php
+++ src/Illuminate/Foundation/Http/Middleware/TrimStrings.php
@@ -28,4 +28,23 @@ class TrimStrings extends TransformsRequest
 
         return is_string($value) ? trim($value) : $value;
     }
+    
+    /**
+     * Clean the given value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function cleanValue($key, $value)
+    {
+        if (is_array($value)) {
+            if(!in_array($key, $this->except, true)){
+                return $this->cleanArray($value);
+            } else {
+                return $value;
+            }
+        }
+        return $this->transform($key, $value);
+    }
 }
