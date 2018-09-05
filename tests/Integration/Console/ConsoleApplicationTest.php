ac46ee63bd8c485b3fb23607d8be0a8ae45f1db3
Sjors <sjorsottjes@gmail.com>
2018-09-05 10:50:16 +0200

    code style

--- tests/Integration/Console/ConsoleApplicationTest.php
+++ tests/Integration/Console/ConsoleApplicationTest.php
@@ -3,9 +3,9 @@
 namespace Illuminate\Tests\Integration\Console;
 
 use Illuminate\Console\Command;
-use Illuminate\Foundation\Testing\PendingCommand;
 use Orchestra\Testbench\TestCase;
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\PendingCommand;
 
 class ConsoleApplicationTest extends TestCase
 {
