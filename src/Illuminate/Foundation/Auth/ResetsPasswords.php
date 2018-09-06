227132c3999df3ecc89670e46c4b910853089e1c
Tonko Mulder <t.mulder@jvdmedia.nl>
2018-09-06 10:08:30 +0200

    moved import of DB facade up
    Signed-off-by: Tonko Mulder <t.mulder@jvdmedia.nl>

--- src/Illuminate/Foundation/Auth/ResetsPasswords.php
+++ src/Illuminate/Foundation/Auth/ResetsPasswords.php
@@ -4,9 +4,9 @@ namespace Illuminate\Foundation\Auth;
 
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Auth\Events\PasswordReset;
 
