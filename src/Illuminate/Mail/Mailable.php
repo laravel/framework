f7f0302e66b1575c38d940eba6ddf606baf88036
Amadeusz Annissimo <annissimo@gmail.com>
2018-09-07 15:53:43 -0400

    Fixed docblock in Mail\Mailable

--- src/Illuminate/Mail/Mailable.php
+++ src/Illuminate/Mail/Mailable.php
@@ -195,7 +195,7 @@ class Mailable implements MailableContract, Renderable
     /**
      * Render the mailable into a view.
      *
-     * @return \Illuminate\View\View
+     * @return string
      */
     public function render()
     {
