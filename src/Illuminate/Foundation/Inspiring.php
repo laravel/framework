ff9166d2633199249274d692e3d1afa22ad056d6
Samet Tutkun <tutkun54@gmail.com>
2018-09-07 13:13:34 +0300

    Fixed file name

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Foundation;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Lang;
 
 class Inspiring
 {
@@ -19,19 +20,8 @@ class Inspiring
      */
     public static function quote()
     {
-        return Collection::make([
-            'When there is no desire, all things are at peace. - Laozi',
-            'Simplicity is the ultimate sophistication. - Leonardo da Vinci',
-            'Simplicity is the essence of happiness. - Cedric Bledsoe',
-            'Smile, breathe, and go slowly. - Thich Nhat Hanh',
-            'Simplicity is an acquired taste. - Katharine Gerould',
-            'Well begun is half done. - Aristotle',
-            'He who is contented is rich. - Laozi',
-            'Very little is needed to make a happy life. - Marcus Antoninus',
-            'It is quality rather than quantity that matters. - Lucius Annaeus Seneca',
-            'Genius is one percent inspiration and ninety-nine percent perspiration. - Thomas Edison',
-            'Computer science is no more about computers than astronomy is about telescopes. - Edsger Dijkstra',
-            'It always seems impossible until it is done. - Nelson Mandela',
-        ])->random();
+        return Collection::make(
+            Lang::trans('inspire.words_of_wisdom')
+        )->random();
     }
 }
