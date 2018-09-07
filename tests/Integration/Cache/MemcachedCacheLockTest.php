2d482bdea64e0934584bcb6ceb07b10aaa1fe609
Franz Liedke <franz@develophp.org>
2018-09-07 17:12:19 +0200

    Memcached cache lock tests: Test for PHP extension, not class

--- tests/Integration/Cache/MemcachedCacheLockTest.php
+++ tests/Integration/Cache/MemcachedCacheLockTest.php
@@ -2,7 +2,6 @@
 
 namespace Illuminate\Tests\Integration\Cache;
 
-use Memcached;
 use Illuminate\Support\Carbon;
 use Orchestra\Testbench\TestCase;
 use Illuminate\Support\Facades\Cache;
@@ -16,7 +15,7 @@ class MemcachedCacheLockTest extends TestCase
     {
         parent::setUp();
 
-        if (! class_exists(Memcached::class)) {
+        if (! extension_loaded('memcached')) {
             $this->markTestSkipped('Memcached module not installed');
         }
     }
