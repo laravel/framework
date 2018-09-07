458dfaf003f8e57022c04bd3fcf7c9a8e4104217
Franz Liedke <franz@develophp.org>
2018-09-07 17:10:04 +0200

    Redis cache lock tests: Use normal setup() and tearDown()

--- tests/Integration/Cache/RedisCacheLockTest.php
+++ tests/Integration/Cache/RedisCacheLockTest.php
@@ -14,31 +14,41 @@ class RedisCacheLockTest extends TestCase
 {
     use InteractsWithRedis;
 
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpRedis();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        $this->tearDownRedis();
+    }
+
     public function test_redis_locks_can_be_acquired_and_released()
     {
-        $this->ifRedisAvailable(function () {
-            Cache::store('redis')->lock('foo')->release();
-            $this->assertTrue(Cache::store('redis')->lock('foo', 10)->get());
-            $this->assertFalse(Cache::store('redis')->lock('foo', 10)->get());
-            Cache::store('redis')->lock('foo')->release();
-            $this->assertTrue(Cache::store('redis')->lock('foo', 10)->get());
-            $this->assertFalse(Cache::store('redis')->lock('foo', 10)->get());
-            Cache::store('redis')->lock('foo')->release();
-        });
+        Cache::store('redis')->lock('foo')->release();
+        $this->assertTrue(Cache::store('redis')->lock('foo', 10)->get());
+        $this->assertFalse(Cache::store('redis')->lock('foo', 10)->get());
+        Cache::store('redis')->lock('foo')->release();
+        $this->assertTrue(Cache::store('redis')->lock('foo', 10)->get());
+        $this->assertFalse(Cache::store('redis')->lock('foo', 10)->get());
+        Cache::store('redis')->lock('foo')->release();
     }
 
     public function test_redis_locks_can_block_for_seconds()
     {
-        $this->ifRedisAvailable(function () {
-            Carbon::setTestNow();
+        Carbon::setTestNow();
 
-            Cache::store('redis')->lock('foo')->release();
-            $this->assertEquals('taylor', Cache::store('redis')->lock('foo', 10)->block(1, function () {
-                return 'taylor';
-            }));
+        Cache::store('redis')->lock('foo')->release();
+        $this->assertEquals('taylor', Cache::store('redis')->lock('foo', 10)->block(1, function () {
+            return 'taylor';
+        }));
 
-            Cache::store('redis')->lock('foo')->release();
-            $this->assertTrue(Cache::store('redis')->lock('foo', 10)->block(1));
-        });
+        Cache::store('redis')->lock('foo')->release();
+        $this->assertTrue(Cache::store('redis')->lock('foo', 10)->block(1));
     }
 }
