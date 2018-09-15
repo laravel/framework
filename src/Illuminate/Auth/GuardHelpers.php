d89b9ad2047b7704420d4f904a65e2ddf649351e
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-09-15 20:53:08 +0300

    [5.8] Changed description for `authenticate` method  - Changed description for `authenticate` method, since `check` method has the same description.

--- src/Illuminate/Auth/GuardHelpers.php
+++ src/Illuminate/Auth/GuardHelpers.php
@@ -25,7 +25,7 @@ trait GuardHelpers
     protected $provider;
 
     /**
-     * Determine if the current user is authenticated.
+     * Determine if the current user is authenticated, in other case throw the error.
      *
      * @return \Illuminate\Contracts\Auth\Authenticatable
      *
