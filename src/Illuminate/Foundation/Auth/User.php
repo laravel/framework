78e5b2cd399fbab830a592e09696f2a8a03f2634
FMW <1009208036@qq.com>
2018-09-19 03:26:21 +0000

    add MustVerifyEmailContract to User model

--- src/Illuminate/Foundation/Auth/User.php
+++ src/Illuminate/Foundation/Auth/User.php
@@ -10,11 +10,13 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
 class User extends Model implements
     AuthenticatableContract,
     AuthorizableContract,
-    CanResetPasswordContract
+    CanResetPasswordContract,
+    MustVerifyEmailContract
 {
     use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail;
 }
