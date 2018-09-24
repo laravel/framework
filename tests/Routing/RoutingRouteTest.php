6690d69c6b583b10947cc1bbb425c9603b58d211
Lucas Michot <lucas@semalead.com>
2018-09-24 19:49:06 +0200

    Clean route methods.

--- tests/Routing/RoutingRouteTest.php
+++ tests/Routing/RoutingRouteTest.php
@@ -23,6 +23,19 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 
 class RoutingRouteTest extends TestCase
 {
+    public function testRouteMethodsShouldBeUppercasedAndUnique()
+    {
+        $route = new Route(['put', 'patch', 'PUT', 'POST'], 'foo', function () {
+            return 'bar';
+        });
+
+        $this->assertSame([
+            'PATCH',
+            'POST',
+            'PUT',
+        ], $route->methods());
+    }
+
     public function testBasicDispatchingOfRoutes()
     {
         $router = $this->getRouter();
