8ea41e0f008b4cd88416c752810d3733e564fc77
Antonio Pauletich <antonio.pauletich95@gmail.com>
2018-10-01 22:02:53 +0200

    Fix translation escaping

--- src/Illuminate/View/Compilers/Concerns/CompilesTranslations.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesTranslations.php
@@ -18,7 +18,7 @@ trait CompilesTranslations
             return "<?php \$__env->startTranslation{$expression}; ?>";
         }
 
-        return "<?php echo e(app('translator')->getFromJson{$expression}); ?>";
+        return "<?php echo app('translator')->getFromJson{$expression}; ?>";
     }
 
     /**
@@ -28,7 +28,7 @@ trait CompilesTranslations
      */
     protected function compileEndlang()
     {
-        return '<?php echo e($__env->renderTranslation()); ?>';
+        return '<?php echo $__env->renderTranslation(); ?>';
     }
 
     /**
@@ -39,6 +39,6 @@ trait CompilesTranslations
      */
     protected function compileChoice($expression)
     {
-        return "<?php echo e(app('translator')->choice{$expression}); ?>";
+        return "<?php echo app('translator')->choice{$expression}; ?>";
     }
 }
