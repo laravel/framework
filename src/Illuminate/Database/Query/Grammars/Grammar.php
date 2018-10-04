df2c76028bc81f1af402a11b68642de55bc4819c
bochen <robert.bochenski@lemondemon.pl>
2018-10-04 20:35:46 +0200

    Allow to extend grammar without issues;

--- src/Illuminate/Database/Query/Grammars/Grammar.php
+++ src/Illuminate/Database/Query/Grammars/Grammar.php
@@ -81,7 +81,7 @@ class Grammar extends BaseGrammar
             // To compile the query, we'll spin through each component of the query and
             // see if that component exists. If it does we'll just call the compiler
             // function for the component which is responsible for making the SQL.
-            if (! is_null($query->$component)) {
+            if (isset($query->$component) && ! is_null($query->$component)) {
                 $method = 'compile'.ucfirst($component);
 
                 $sql[$component] = $this->$method($query, $query->$component);
