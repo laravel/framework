f1e45500c5e678a86e2ed3360fdfe292fafa48d1
Marcos Felipe <omarkdev@gmail.com>
2018-10-06 23:15:24 -0300

    Adds throws PHPDoc comment

--- src/Illuminate/Container/BoundMethod.php
+++ src/Illuminate/Container/BoundMethod.php
@@ -122,8 +122,9 @@ class BoundMethod
     /**
      * Get the proper reflection instance for the given callback.
      *
-     * @param  callable|string  $callback
+     * @param  callable|string $callback
      * @return \ReflectionFunctionAbstract
+     * @throws \ReflectionException
      */
     protected static function getCallReflector($callback)
     {
