71964f873cd961dc816de88b5204d8cd4c0504d2
Tetiana Blindaruk <t.blindaruk@gmail.com>
2018-10-07 18:51:20 +0300

    [5.6] Revert email lang template changes. (#25983)
    * [5.6] Revert email lang template changes.
    I`m not sure, but probably we also should reverting `email lang template` ([#25734](https://github.com/laravel/framework/pull/25734)) changes in 5.6 version
    
    Since in prev 5.6 release we also had:
     - `Fixed translation escaping ([#25858](https://github.com/laravel/framework/pull/25858), [4c46500](https://github.com/laravel/framework/commit/4c465007bbf51d7f269871cd76b6d99de7df90bb))` for fixed translation escaping
    
    The same revert was in 5.7 (https://github.com/laravel/framework/pull/25963)
    
    * [5.6] Revert email lang template changes.
     - add `comma` after last value in array.

--- src/Illuminate/Notifications/resources/views/email.blade.php
+++ src/Illuminate/Notifications/resources/views/email.blade.php
@@ -53,12 +53,12 @@
 @component('mail::subcopy')
 @lang(
     "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-    'into your web browser: ',
+    'into your web browser: [:actionURL](:actionURL)',
     [
-        'actionText' => $actionText
+        'actionText' => $actionText,
+        'actionURL' => $actionUrl,
     ]
 )
-[{{ $actionUrl }}]({!! $actionUrl !!})
 @endcomponent
 @endisset
 @endcomponent
