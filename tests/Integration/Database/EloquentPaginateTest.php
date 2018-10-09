47183693143929500d3a3cd9d3277ff5c3f19c62
Alex Bouma <me@alexbouma.me>
2018-10-09 14:10:20 +0200

    Add test for distinct pagination on Eloquent query

--- tests/Integration/Database/EloquentPaginateTest.php
+++ tests/Integration/Database/EloquentPaginateTest.php
@@ -20,6 +20,14 @@ class EloquentPaginateTest extends DatabaseTestCase
             $table->string('title')->nullable();
             $table->timestamps();
         });
+        Schema::create('post_comments', function ($table) {
+            $table->increments('id');
+            $table->unsignedInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->string('text')->nullable();
+            $table->unsignedInteger('likes')->default(0);
+            $table->timestamps();
+        });
     }
 
     public function test_pagination_on_top_of_columns()
@@ -32,9 +40,40 @@ class EloquentPaginateTest extends DatabaseTestCase
 
         $this->assertCount(15, Post::paginate(15, ['id', 'title']));
     }
+
+    public function test_pagination_with_distinct()
+    {
+        for ($i = 1; $i <= 25; $i++) {
+            $post = Post::create([
+                'title' => 'Title '.$i,
+            ]);
+
+            for ($j = 1; $j <= 5; $j++) {
+                PostComment::create([
+                    'post_id' => $post->id,
+                    'text'    => 'Comment '.$j,
+                    'likes'   => $i + $j,
+                ]);
+            }
+        }
+
+        $paginator = Post::join('post_comments', 'post_comments.post_id', '=', 'posts.id')
+                         ->where('likes', '>', 10)
+                         ->distinct()
+                         ->select('posts.*')
+                         ->paginate(15);
+
+        $this->assertCount(15, $paginator);
+        $this->assertEquals(20, $paginator->total());
+    }
 }
 
 class Post extends Model
+{
+    protected $guarded = [];
+}
+
+class PostComment extends Model
 {
     protected $guarded = [];
 }
