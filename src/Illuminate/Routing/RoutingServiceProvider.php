6774f832b13fed5746087037939afbc15dd01c83
Lucas Michot <lucas@semalead.com>
2018-10-18 05:42:31 +0200

    Remove useless app variable (#26152)

--- src/Illuminate/Routing/RoutingServiceProvider.php
+++ src/Illuminate/Routing/RoutingServiceProvider.php
@@ -136,7 +136,7 @@ class RoutingServiceProvider extends ServiceProvider
      */
     protected function registerPsrResponse()
     {
-        $this->app->bind(ResponseInterface::class, function ($app) {
+        $this->app->bind(ResponseInterface::class, function () {
             return new PsrResponse;
         });
     }
