5892daa6e97590ea9a05dfa23b81493c8eaadc1c
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2018-10-18 05:39:29 +0200

    Replace newQuery() with newModelQuery() (#26158)

--- tests/Database/DatabaseEloquentPivotTest.php
+++ tests/Database/DatabaseEloquentPivotTest.php
@@ -110,14 +110,14 @@ class DatabaseEloquentPivotTest extends TestCase
 
     public function testDeleteMethodDeletesModelByKeys()
     {
-        $pivot = $this->getMockBuilder(Pivot::class)->setMethods(['newQuery'])->getMock();
+        $pivot = $this->getMockBuilder(Pivot::class)->setMethods(['newModelQuery'])->getMock();
         $pivot->setPivotKeys('foreign', 'other');
         $pivot->foreign = 'foreign.value';
         $pivot->other = 'other.value';
         $query = m::mock(stdClass::class);
         $query->shouldReceive('where')->once()->with(['foreign' => 'foreign.value', 'other' => 'other.value'])->andReturn($query);
         $query->shouldReceive('delete')->once()->andReturn(true);
-        $pivot->expects($this->once())->method('newQuery')->will($this->returnValue($query));
+        $pivot->expects($this->once())->method('newModelQuery')->will($this->returnValue($query));
 
         $this->assertTrue($pivot->delete());
     }
