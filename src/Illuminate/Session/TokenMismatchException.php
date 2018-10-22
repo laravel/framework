7d01155bf8c1087140323dcaad8885cca8a7e496
tomhorvat <tomhorvat@gmail.com>
2018-10-22 21:35:15 +0200

    Add message to TokenMismatchException
    When thrown, TokenMismatchException gives empty message.
    
    If form is submitted async, warning is thrown in console but message is empty and if for example axios interceptor is set to display message, it shows empty.

--- src/Illuminate/Session/TokenMismatchException.php
+++ src/Illuminate/Session/TokenMismatchException.php
@@ -6,5 +6,14 @@ use Exception;
 
 class TokenMismatchException extends Exception
 {
-    //
+    /**
+     * Create a new token mismatch exception.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->message = __('Sorry, your session has expired. Please refresh and try again.');
+    }
 }
