d1b1e55981fb23c1fdd4f9471131d78b873fdeae
André Filipe <MASNathan@users.noreply.github.com>
2018-10-22 19:26:26 +0100

    [5.7] Add uuid validation rule to validator (#26135)
    * Added uuid validation rule
    
    * Style CI requested changes
    
    * Removed dependency on ramsey/uuid
    
    * Removed unnecessary nil uuid check
    
    * Changing expression accordingly to @lucasmichot suggestion
    
    * Forced boolean return
    
    * Ups
    
    * Update ValidatesAttributes.php

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1544,6 +1544,22 @@ trait ValidatesAttributes
     }
 
     /**
+     * Validate that an attribute is a valid UUID.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function validateUuid($attribute, $value)
+    {
+        if (! is_string($value)) {
+            return false;
+        }
+
+        return preg_match('/^[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/iD', $value) > 0;
+    }
+
+    /**
      * Get the size of an attribute.
      *
      * @param  string  $attribute
