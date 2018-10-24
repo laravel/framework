3a3a450eab1cc165928e6d9da3745027dd61c37b
Reindert <reindert@myparcel.nl>
2018-10-24 14:56:07 +0200

    Remove weird spacing

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -361,7 +361,7 @@ class BelongsToMany extends Relation
      * In addition, new pivot records will receive this value.
      *
      * @param  string|array $column
-     * @param  mixed        $value
+     * @param  mixed $value
      * @return $this
      */
     public function withPivotValue($column, $value = null)
