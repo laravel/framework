bc5ca3cea10e5ef9abca35c3d76bea94963607c4
Đinh Quốc Hân <dinhquochan96@gmail.com>
2018-10-25 06:55:47 +0000

    camelCase
    Co-Authored-By: AbdelkaderBah <jr.abdelkader@gmail.com>

--- src/Illuminate/Notifications/Channels/DatabaseChannel.php
+++ src/Illuminate/Notifications/Channels/DatabaseChannel.php
@@ -55,7 +55,7 @@ class DatabaseChannel
     {
         return [
             'id' => $notification->id,
-            'type' => $notification->type_alias ?? get_class($notification),
+            'type' => $notification->typeAlias ?? get_class($notification),
             'data' => $this->getData($notifiable, $notification),
             'read_at' => null,
         ];
