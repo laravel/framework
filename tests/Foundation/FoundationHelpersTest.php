c2cbdc0951a8dddc1bb774c3d52d3f35aabf2592
Tim MacDonald <tim.mac7@me.com>
2018-11-04 00:29:09 +1100

    mix testing (#26369)

--- tests/Foundation/FoundationHelpersTest.php
+++ tests/Foundation/FoundationHelpersTest.php
@@ -65,22 +65,175 @@ class FoundationHelpersTest extends TestCase
 
     public function testMixDoesNotIncludeHost()
     {
-        $file = 'unversioned.css';
+        $manifest = $this->makeManifest();
 
+        $result = mix('/unversioned.css');
+
+        $this->assertSame('/versioned.css', $result->toHtml());
+
+        unlink($manifest);
+    }
+
+    public function testMixCachesManifestForSubsequentCalls()
+    {
+        $manifest = $this->makeManifest();
+        mix('unversioned.css');
+        unlink($manifest);
+
+        $result = mix('/unversioned.css');
+
+        $this->assertSame('/versioned.css', $result->toHtml());
+    }
+
+    public function testMixAssetMissingStartingSlashHaveItAdded()
+    {
+        $manifest = $this->makeManifest();
+
+        $result = mix('unversioned.css');
+
+        $this->assertSame('/versioned.css', $result->toHtml());
+
+        unlink($manifest);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage The Mix manifest does not exist.
+     */
+    public function testMixMissingManifestThrowsException()
+    {
+        mix('unversioned.css', 'missing');
+    }
+
+    public function testMixWithManifestDirectory()
+    {
+        mkdir($directory = __DIR__.'/mix');
+        $manifest = $this->makeManifest('mix');
+
+        $result = mix('unversioned.css', 'mix');
+
+        $this->assertSame('/mix/versioned.css', $result->toHtml());
+
+        unlink($manifest);
+        rmdir($directory);
+    }
+
+    public function testMixManifestDirectoryMissingStartingSlashHasItAdded()
+    {
+        mkdir($directory = __DIR__.'/mix');
+        $manifest = $this->makeManifest('/mix');
+
+        $result = mix('unversioned.css', 'mix');
+
+        $this->assertSame('/mix/versioned.css', $result->toHtml());
+
+        unlink($manifest);
+        rmdir($directory);
+    }
+
+    public function testMixHotModuleReloadingGetsUrlFromFileWithHttps()
+    {
+        $path = $this->makeHotModuleReloadFile('https://laravel.com/docs');
+
+        $result = mix('unversioned.css');
+
+        $this->assertSame('//laravel.com/docs/unversioned.css', $result->toHtml());
+
+        unlink($path);
+    }
+
+    public function testMixHotModuleReloadingGetsUrlFromFileWithHttp()
+    {
+        $path = $this->makeHotModuleReloadFile('http://laravel.com/docs');
+
+        $result = mix('unversioned.css');
+
+        $this->assertSame('//laravel.com/docs/unversioned.css', $result->toHtml());
+
+        unlink($path);
+    }
+
+    public function testMixHotModuleReloadingGetsUrlFromFileWithManifestDirectoryAndHttps()
+    {
+        mkdir($directory = __DIR__.'/mix');
+        $path = $this->makeHotModuleReloadFile('https://laravel.com/docs', 'mix');
+
+        $result = mix('unversioned.css', 'mix');
+
+        $this->assertSame('//laravel.com/docs/unversioned.css', $result->toHtml());
+
+        unlink($path);
+        rmdir($directory);
+    }
+
+    public function testMixHotModuleReloadingGetsUrlFromFileWithManifestDirectoryAndHttp()
+    {
+        mkdir($directory = __DIR__.'/mix');
+        $path = $this->makeHotModuleReloadFile('http://laravel.com/docs', 'mix');
+
+        $result = mix('unversioned.css', 'mix');
+
+        $this->assertSame('//laravel.com/docs/unversioned.css', $result->toHtml());
+
+        unlink($path);
+        rmdir($directory);
+    }
+
+    public function testMixHotModuleReloadingUsesLocalhostIfNoHttpScheme()
+    {
+        $path = $this->makeHotModuleReloadFile('');
+
+        $result = mix('unversioned.css');
+
+        $this->assertSame('//localhost:8080/unversioned.css', $result->toHtml());
+
+        unlink($path);
+    }
+
+    public function testMixHotModuleReloadingWithManifestDirectoryUsesLocalhostIfNoHttpScheme()
+    {
+        mkdir($directory = __DIR__.'/mix');
+        $path = $this->makeHotModuleReloadFile('', 'mix');
+
+        $result = mix('unversioned.css', 'mix');
+
+        $this->assertSame('//localhost:8080/unversioned.css', $result->toHtml());
+
+        unlink($path);
+        rmdir($directory);
+    }
+
+    protected function makeHotModuleReloadFile($url, $directory = '')
+    {
         app()->singleton('path.public', function () {
             return __DIR__;
         });
 
-        touch(public_path('mix-manifest.json'));
+        $path = public_path(str_finish($directory, '/').'hot');
 
-        file_put_contents(public_path('mix-manifest.json'), json_encode([
-            '/unversioned.css' => '/versioned.css',
-        ]));
+        // Laravel mix when run 'hot' has a new line after the
+        // url, so for consistency this "\n" is added.
+        file_put_contents($path, "{$url}\n");
 
-        $result = mix($file);
+        return $path;
+    }
 
-        $this->assertEquals('/versioned.css', $result);
+    protected function makeManifest($directory = '')
+    {
+        app()->singleton('path.public', function () {
+            return __DIR__;
+        });
 
-        unlink(public_path('mix-manifest.json'));
+        $path = public_path(str_finish($directory, '/').'mix-manifest.json');
+
+        touch($path);
+
+        // Laravel mix prints JSON pretty and with escaped
+        // slashes, so we are doing that here for consistency.
+        $content = json_encode(['/unversioned.css' => '/versioned.css'], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+        file_put_contents($path, $content);
+
+        return $path;
     }
 }
