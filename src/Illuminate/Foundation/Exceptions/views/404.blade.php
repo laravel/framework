55c6f688d5597f56fc12474cc36394c2bd581924
Matt Floyd <mattdfloyd@gmail.com>
2018-11-05 22:37:55 -0500

    wip
    wip

--- src/Illuminate/Foundation/Exceptions/views/404.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/404.blade.php
@@ -4,7 +4,7 @@
 @section('title', __('Page Not Found'))
 
 @section('image')
-<div style="background-image: url('/svg/404.svg');" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+<div style="background-image: url({{ asset('/svg/404.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
 </div>
 @endsection
 
