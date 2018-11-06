17163b46d0a1d88160dab681d699f9e694d8c1c8
Taylor Otwell <taylor@laravel.com>
2018-11-06 10:23:33 -0600

    Revert "Fix SoftDeletes Query (#26396)" (#26406)
    This reverts commit d05f93e6e2402c5ae8c33e6cb4e2fe305c712318.

--- src/Illuminate/Database/Eloquent/SoftDeletes.php
+++ src/Illuminate/Database/Eloquent/SoftDeletes.php
@@ -49,7 +49,7 @@ trait SoftDeletes
         if ($this->forceDeleting) {
             $this->exists = false;
 
-            return $this->setKeysForSaveQuery($this->newModelQuery())->forceDelete();
+            return $this->newModelQuery()->where($this->getKeyName(), $this->getKey())->forceDelete();
         }
 
         return $this->runSoftDelete();
@@ -62,7 +62,7 @@ trait SoftDeletes
      */
     protected function runSoftDelete()
     {
-        $query = $this->setKeysForSaveQuery($this->newModelQuery());
+        $query = $this->newModelQuery()->where($this->getKeyName(), $this->getKey());
 
         $time = $this->freshTimestamp();
 
