4195a640183d0ded69892ad961dceadb8d39678c
Taylor Otwell <taylor@laravel.com>
2018-11-06 16:04:45 -0500

    formatting

--- src/Illuminate/View/FileViewFinder.php
+++ src/Illuminate/View/FileViewFinder.php
@@ -53,7 +53,7 @@ class FileViewFinder implements ViewFinderInterface
     public function __construct(Filesystem $files, array $paths, array $extensions = null)
     {
         $this->files = $files;
-        $this->paths = array_map([$this, 'normalizePath'], $paths);
+        $this->paths = array_map([$this, 'resolvePath'], $paths);
 
         if (isset($extensions)) {
             $this->extensions = $extensions;
@@ -158,7 +158,7 @@ class FileViewFinder implements ViewFinderInterface
      */
     public function addLocation($location)
     {
-        $this->paths[] = $this->normalizePath($location);
+        $this->paths[] = $this->resolvePath($location);
     }
 
     /**
@@ -169,7 +169,18 @@ class FileViewFinder implements ViewFinderInterface
      */
     public function prependLocation($location)
     {
-        array_unshift($this->paths, $this->normalizePath($location));
+        array_unshift($this->paths, $this->resolvePath($location));
+    }
+
+    /**
+     * Resolve the path.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    protected function resolvePath($path)
+    {
+        return realpath($path) ?: $path;
     }
 
     /**
@@ -294,16 +305,5 @@ class FileViewFinder implements ViewFinderInterface
     public function getExtensions()
     {
         return $this->extensions;
-    }
-
-    /**
-     * Replace unnecessary relative fragments from the absolute view path.
-     *
-     * @param string $path
-     * @return string
-     */
-    protected function normalizePath($path)
-    {
-        return realpath($path) ?: $path;
     }
 }
