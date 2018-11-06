45fc124ee5642c396e9fbbf44e29b61cd9186d04
Taylor Otwell <taylor@laravel.com>
2018-11-06 10:23:15 -0600

    Revert "Fix SoftDeletes Query (#26396)"
    This reverts commit d05f93e6e2402c5ae8c33e6cb4e2fe305c712318.

--- tests/Database/DatabaseSoftDeletingTraitTest.php
+++ tests/Database/DatabaseSoftDeletingTraitTest.php
@@ -19,7 +19,7 @@ class DatabaseSoftDeletingTraitTest extends TestCase
         $model = m::mock(DatabaseSoftDeletingTraitStub::class);
         $model->shouldDeferMissing();
         $model->shouldReceive('newModelQuery')->andReturn($query = m::mock(stdClass::class));
-        $model->shouldReceive('setKeysForSaveQuery')->once()->with($query)->andReturn($query);
+        $query->shouldReceive('where')->once()->with('id', 1)->andReturn($query);
         $query->shouldReceive('update')->once()->with([
             'deleted_at' => 'date-time',
             'updated_at' => 'date-time',
