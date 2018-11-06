43c7d8aaefe65b500aa856a5cc8dc0898eb70703
Mohamed Sabil <mohamedsabil83@users.noreply.github.com>
2018-11-06 16:33:42 +0300

    support direction from language file

--- src/Illuminate/Mail/resources/views/html/layout.blade.php
+++ src/Illuminate/Mail/resources/views/html/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="{{ __('ltr') }}">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
