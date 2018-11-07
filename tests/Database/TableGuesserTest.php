36803b4273d8236965762dc55df5d92a65d1a863
Jordan Hall <jordan@hall05.co.uk>
2018-11-07 21:36:01 +0000

    Allow migration table name to be guessed without 'table' suffix

--- tests/Database/TableGuesserTest.php
+++ tests/Database/TableGuesserTest.php
@@ -25,4 +25,23 @@ class TableGuesserTest extends TestCase
         $this->assertEquals('users', $table);
         $this->assertFalse($create);
     }
+
+    public function test_migration_is_properly_parsed_without_table_suffix()
+    {
+        [$table, $create] = TableGuesser::guess('create_users');
+        $this->assertEquals('users', $table);
+        $this->assertTrue($create);
+
+        [$table, $create] = TableGuesser::guess('add_status_column_to_users');
+        $this->assertEquals('users', $table);
+        $this->assertFalse($create);
+
+        [$table, $create] = TableGuesser::guess('change_status_column_in_users');
+        $this->assertEquals('users', $table);
+        $this->assertFalse($create);
+
+        [$table, $create] = TableGuesser::guess('drop_status_column_from_users');
+        $this->assertEquals('users', $table);
+        $this->assertFalse($create);
+    }
 }
