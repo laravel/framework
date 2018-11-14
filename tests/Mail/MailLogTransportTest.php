94ae7fb9788b173825e069d8a44f2b2f7241ed4c
Taylor Otwell <taylor@laravel.com>
2018-11-14 07:46:46 -0600

    formatting. remove test

--- tests/Mail/MailLogTransportTest.php
+++ tests/Mail/MailLogTransportTest.php
@@ -11,21 +11,6 @@ use Illuminate\Mail\Transport\LogTransport;
 
 class MailLogTransportTest extends TestCase
 {
-    public function testGetLogTransportWithDefaultChannel()
-    {
-        $manager = $this->app['swift.transport'];
-
-        $transport = $manager->driver('log');
-        $this->assertInstanceOf(LogTransport::class, $transport);
-
-        $logger = $this->readAttribute($transport, 'logger');
-        $this->assertInstanceOf(LoggerInterface::class, $logger);
-
-        $this->assertInstanceOf(Logger::class, $monolog = $logger->getLogger());
-        $this->assertCount(1, $handlers = $monolog->getHandlers());
-        $this->assertInstanceOf(RotatingFileHandler::class, $handlers[0]);
-    }
-
     public function testGetLogTransportWithConfiguredChannel()
     {
         $this->app['config']->set('mail.log_channel', 'mail');
@@ -45,6 +30,5 @@ class MailLogTransportTest extends TestCase
         $this->assertInstanceOf(Logger::class, $monolog = $logger->getLogger());
         $this->assertCount(1, $handlers = $monolog->getHandlers());
         $this->assertInstanceOf(StreamHandler::class, $handler = $handlers[0]);
-        $this->assertEquals('mail.log', $handler->getUrl());
     }
 }
