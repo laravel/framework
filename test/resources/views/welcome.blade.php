db3d6eca11b953dae16a2bb0d08f814dd232462c
nellekocabrera <44804044+nellekocabrera@users.noreply.github.com>
2018-11-15 18:46:05 +0800

    Update test/resources/views/welcome.blade.php
    Added Grub Shack TItle

--- test/resources/views/welcome.blade.php
+++ test/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Grub Shack
                 </div>
 
                 <div class="links">
