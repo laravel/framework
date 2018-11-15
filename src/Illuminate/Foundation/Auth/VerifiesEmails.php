920d6b354e2979b45e414bfe36e2036ba3c9a39b
Yohanan Baruchel <yohanan@compie.co.il>
2018-11-16 00:12:03 +0200

    Style fix

--- src/Illuminate/Foundation/Auth/VerifiesEmails.php
+++ src/Illuminate/Foundation/Auth/VerifiesEmails.php
@@ -13,32 +13,32 @@ trait VerifiesEmails
     /**
      * Show the email verification notice.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function show(Request $request)
     {
         return $request->user()->hasVerifiedEmail()
-                        ? redirect($this->redirectPath())
-                        : view('auth.verify');
+            ? redirect($this->redirectPath())
+            : view('auth.verify');
     }
 
-	/**
-	 * Mark the authenticated user's email address as verified.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @return \Illuminate\Http\Response
-	 * @throws AuthorizationException
-	 */
+    /**
+     * Mark the authenticated user's email address as verified.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     * @throws AuthorizationException
+     */
     public function verify(Request $request)
     {
-		if ($request->route('id') != $request->user()->getKey()) {
-			throw new AuthorizationException();
-		}
+        if ($request->route('id') != $request->user()->getKey()) {
+            throw new AuthorizationException();
+        }
 
-		if ($request->user()->markEmailAsVerified()) {
-			event(new Verified($request->user()));
-		}
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
+        }
 
         return redirect($this->redirectPath())->with('verified', true);
     }
@@ -46,7 +46,7 @@ trait VerifiesEmails
     /**
      * Resend the email verification notification.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function resend(Request $request)
