laravel/framework

commit 76c0c1a43b3bd985c749a50fd309c7f8052e48b9

feat(TestResponse): Change implementation

src/Illuminate/Foundation/Testing/TestResponse.php
@@ -58,31 +58,42 @@ class TestResponse
     }
 
     /**
-     * Merge stdClass array and assoc array recursively.
-     * @param  array $arrayStdClass
-     * @param  array $arrayAssoc
-     * @return array
+     * JSON decode assoc and keep the empty object.
+     * @param string $json
+     * @link https://github.com/laravel/framework/issues/25769
+     * @return mixed
      */
-    public static function normalizingArraysObjects(array $arrayStdClass, array $arrayAssoc): array
+    public static function jsonDecodeKeepEmptyObject(string $json)
     {
-        $merged = $arrayStdClass;
+        $patchEmptyObject = function ($array) use (&$patchEmptyObject) {
 
-        // If arrayAssoc key has in $arrayStdClass and value is not empty, replace here.
-        // This converts stdClass to accos array and leaves the empty stdClass
-
-        foreach ($arrayAssoc as $key => $value) {
-            if (empty($value)) {
-                continue;
+            // If it is an empty class,it is reserved.
+            // Otherwise converted into an associative array.
+            if (is_object($array)) {
+                return empty((array) $array)
+                    ? $array
+                    : $patchEmptyObject((array) $array);
             }
 
-            if (is_array($value)) {
-                $value = self::normalizingArraysObjects((array) $merged[$key], $value);
+            foreach ($array as $key => $item) {
+                // We recursively deal with each of these terms.
+                if (is_array($item)
+                    || is_object($item)
+                ) {
+                    $array[$key] = $patchEmptyObject($item);
+                }
             }
 
-            $merged[$key] = $value;
+            return $array;
+        };
+
+        $stdClass = json_decode($json);
+
+        if ($stdClass === false) {
+            return $stdClass;
         }
 
-        return $merged;
+        return $patchEmptyObject($stdClass);
     }
 
     /**
@@ -713,19 +724,15 @@ class TestResponse
      */
     public function decodeResponseJson($key = null)
     {
-        $decodedResponseStdClass = (array) json_decode($this->getContent());
+        $decodedResponse = static::jsonDecodeKeepEmptyObject($this->getContent());
 
-        if (is_null($decodedResponseStdClass) || $decodedResponseStdClass === false) {
+        if (is_null($decodedResponse) || $decodedResponse === false) {
             if ($this->exception) {
                 throw $this->exception;
             } else {
                 PHPUnit::fail('Invalid JSON was returned from the route.');
             }
         }
-
-        $decodedResponseAssoc = json_decode($this->getContent(), true);
-
-        $decodedResponse = static::normalizingArraysObjects($decodedResponseStdClass, $decodedResponseAssoc);
 
         return data_get($decodedResponse, $key);
     }

commit 3c1995c60892ebc54c553a89707ee02d0353b5da

Fixes regression introduced in #26353

src/Illuminate/Foundation/Testing/TestResponse.php
@@ -736,9 +736,11 @@ class TestResponse
             }
         }
 
-        foreach ($payload as $key => $item) {
-            if (is_array($item) || is_object($item)) {
-                $payload[$key] = $this->parseJsonWhilePreservingEmptyObjects($item);
+        if (is_array($payload)) {
+            foreach ($payload as $key => $item) {
+                if (is_array($item) || is_object($item)) {
+                    $payload[$key] = $this->parseJsonWhilePreservingEmptyObjects($item);
+                }
             }
         }
 

tests/Foundation/FoundationTestResponseTest.php
@@ -298,6 +298,22 @@ class FoundationTestResponseTest extends TestCase
         ]);
     }
 
+    /**
+     * @dataProvider testJsonPrimitiveTypesSupportDataProvider
+     */
+    public function testJsonPrimitiveTypesSupport($value)
+    {
+        $response = new TestResponse(new JsonResponse($value));
+
+        $this->assertSame($value, $response->json());
+    }
+
+    public function testJsonPrimitiveTypesSupportDataProvider() {
+        // Illuminate\Foundation\TestingTestResponse thinks that `false` and
+        // `null` means "decoding failed"...
+        return [[true], ['string'], [123]];
+    }
+
     public function testAssertJsonMissingExact()
     {
         $response = TestResponse::fromBaseResponse(new Response(new JsonSerializableSingleResourceWithIntegersStub));