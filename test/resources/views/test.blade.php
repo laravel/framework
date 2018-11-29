d66b0d84502008a7618571ca8f4327f9bfa683a2
nhcsagun <nhcsagun@addu.edu.ph>
2018-11-29 18:38:12 +0800

    Test blade update
    Changed Test to Nadir Test

--- test/resources/views/test.blade.php
+++ test/resources/views/test.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Test
+                    Nadir Test
                 </div>
 
                 <div class="links">
