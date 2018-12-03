5e08fb2f45a68452846aab88f24369c274d14be3
Alexander Hofstede <alex@hofste.de>
2018-12-03 12:11:06 +0100

    Remove return statement used with function that doesn't return anything

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -215,7 +215,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      */
     public function afterLoadingEnvironment(Closure $callback)
     {
-        return $this->afterBootstrapping(
+        $this->afterBootstrapping(
             LoadEnvironmentVariables::class, $callback
         );
     }
