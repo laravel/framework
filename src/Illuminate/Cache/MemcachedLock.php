20a0d819658987d98b464ff6cd803225cd4ba00d
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2018-12-04 17:16:25 +0000

    Fixed invalid phpdoc

--- src/Illuminate/Cache/MemcachedLock.php
+++ src/Illuminate/Cache/MemcachedLock.php
@@ -17,7 +17,7 @@ class MemcachedLock extends Lock
      * @param  \Memcached  $memcached
      * @param  string  $name
      * @param  int  $seconds
-     * @param  string $owner;
+     * @param  string|null  $owner
      * @return void
      */
     public function __construct($memcached, $name, $seconds, $owner = null)
