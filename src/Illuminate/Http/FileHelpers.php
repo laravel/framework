472a029fa44633706d4a3b6195905323fde2d43f
Simon <contact@mgkprod.fr>
2018-12-12 00:20:28 +0100

    Reformatted for StyleCI

--- src/Illuminate/Http/FileHelpers.php
+++ src/Illuminate/Http/FileHelpers.php
@@ -56,10 +56,13 @@ trait FileHelpers
         }
 
         $hash = $this->hashName ?: $this->hashName = Str::random(40);
-        
+
         $extension = $this->guessExtension();
-        if ($extension) $extension = '.' . $extension;
-         
-        return $path . $hash . $extension;
+        if ($extension) {
+            $extension = '.'.$extension;
+        }
+
+        return $path.$hash.$extension;
+
     }
 }
