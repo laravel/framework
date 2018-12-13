b2c16392e3efb11eabd132a0d1f4575579f286b9
Inani El Houssain <inanielhoussain@gmail.com>
2018-12-13 13:53:32 +0000

    Update GeneratorCommand.php (#26827)

--- src/Illuminate/Console/GeneratorCommand.php
+++ src/Illuminate/Console/GeneratorCommand.php
@@ -46,6 +46,7 @@ abstract class GeneratorCommand extends Command
      * Execute the console command.
      *
      * @return bool|null
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
     public function handle()
     {
@@ -152,6 +153,7 @@ abstract class GeneratorCommand extends Command
      *
      * @param  string  $name
      * @return string
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
     protected function buildClass($name)
     {
