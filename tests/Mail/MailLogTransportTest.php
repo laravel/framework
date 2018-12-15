7c183bcd2d0a0b225c11e184c7690e9ea052bbb2
Sébastien Nikolaou <info@sebdesign.eu>
2018-12-15 16:40:14 +0200

    Test the TransportManager creates a log driver with any LoggerInterface instance (#26855)
    In addition to #26842, this test asserts that the `TransportManager` can create a log driver when the application has a different implementation of the `Psr\Log\LoggerInterface`, e.g. when the `illuminate/mail` package is used outside of Laravel.

--- tests/Mail/MailLogTransportTest.php
+++ tests/Mail/MailLogTransportTest.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Tests\Mail;
 
 use Monolog\Logger;
+use Psr\Log\NullLogger;
 use Psr\Log\LoggerInterface;
 use Orchestra\Testbench\TestCase;
 use Monolog\Handler\StreamHandler;
@@ -29,5 +30,14 @@ class MailLogTransportTest extends TestCase
         $this->assertInstanceOf(Logger::class, $monolog = $logger->getLogger());
         $this->assertCount(1, $handlers = $monolog->getHandlers());
         $this->assertInstanceOf(StreamHandler::class, $handler = $handlers[0]);
+    }
+
+    public function testGetLogTransportWithPsrLogger()
+    {
+        $logger = $this->app->instance('log', new NullLogger());
+
+        $manager = $this->app['swift.transport'];
+
+        $this->assertAttributeEquals($logger, 'logger', $manager->driver('log'));
     }
 }
