59b4b5f68ddf3366b80ce78b47e84029bd29f8a1
Alexander Dmitryuk <xakzona@bk.ru>
2018-12-18 20:45:35 +0700

    [5.7] Collection whereBetween  (#26888)
    * Add collection whereBetween
    
    *  whereBetween comments refactor

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -679,6 +679,18 @@ class Collection implements ArrayAccess, Arrayable, Countable, IteratorAggregate
     }
 
     /**
+     * Filter items where the given key between values.
+     *
+     * @param  string  $key
+     * @param  array  $values
+     * @return static
+     */
+    public function whereBetween($key, $values)
+    {
+        return $this->where($key, '>=', reset($values))->where($key, '<=', end($values));
+    }
+
+    /**
      * Filter items by the given key value pair.
      *
      * @param  string  $key
