b714370b147efda9f0d48a9fae0ae6ebb8af412e
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-12-19 14:01:19 +0000

    Allow to change the formatter for the single, daily and syslog logging (#26895)
    driver from the config file.

--- tests/Log/LogManagerTest.php
+++ tests/Log/LogManagerTest.php
@@ -8,7 +8,9 @@ use Illuminate\Log\LogManager;
 use Monolog\Logger as Monolog;
 use Orchestra\Testbench\TestCase;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogHandler;
 use Monolog\Formatter\HtmlFormatter;
+use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\NewRelicHandler;
 use Monolog\Handler\LogEntriesHandler;
 use Monolog\Formatter\NormalizerFormatter;
@@ -109,6 +111,130 @@ class LogManagerTest extends TestCase
         $formatter = $handler->getFormatter();
 
         $this->assertInstanceOf(NewRelicHandler::class, $handler);
+        $this->assertInstanceOf(HtmlFormatter::class, $formatter);
+
+        $dateFormat = new ReflectionProperty(get_class($formatter), 'dateFormat');
+        $dateFormat->setAccessible(true);
+
+        $this->assertEquals('Y/m/d--test', $dateFormat->getValue($formatter));
+    }
+
+    public function testLogManagerCreateSingleDriverWithConfiguredFormatter()
+    {
+        $config = $this->app['config'];
+        $config->set('logging.channels.defaultsingle', [
+            'driver' => 'single',
+            'name' => 'ds',
+            'path' => storage_path('logs/laravel.log'),
+        ]);
+
+        $manager = new LogManager($this->app);
+
+        // create logger with handler specified from configuration
+        $logger = $manager->channel('defaultsingle');
+        $handler = $logger->getLogger()->getHandlers()[0];
+        $formatter = $handler->getFormatter();
+
+        $this->assertInstanceOf(StreamHandler::class, $handler);
+        $this->assertInstanceOf(LineFormatter::class, $formatter);
+
+        $config->set('logging.channels.formattedsingle', [
+            'driver' => 'single',
+            'name' => 'fs',
+            'path' => storage_path('logs/laravel.log'),
+            'formatter' => HtmlFormatter::class,
+            'formatter_with' => [
+                'dateFormat' => 'Y/m/d--test',
+            ],
+        ]);
+
+        $logger = $manager->channel('formattedsingle');
+        $handler = $logger->getLogger()->getHandlers()[0];
+        $formatter = $handler->getFormatter();
+
+        $this->assertInstanceOf(StreamHandler::class, $handler);
+        $this->assertInstanceOf(HtmlFormatter::class, $formatter);
+
+        $dateFormat = new ReflectionProperty(get_class($formatter), 'dateFormat');
+        $dateFormat->setAccessible(true);
+
+        $this->assertEquals('Y/m/d--test', $dateFormat->getValue($formatter));
+    }
+
+    public function testLogManagerCreateDailyDriverWithConfiguredFormatter()
+    {
+        $config = $this->app['config'];
+        $config->set('logging.channels.defaultdaily', [
+            'driver' => 'daily',
+            'name' => 'dd',
+            'path' => storage_path('logs/laravel.log'),
+        ]);
+
+        $manager = new LogManager($this->app);
+
+        // create logger with handler specified from configuration
+        $logger = $manager->channel('defaultdaily');
+        $handler = $logger->getLogger()->getHandlers()[0];
+        $formatter = $handler->getFormatter();
+
+        $this->assertInstanceOf(StreamHandler::class, $handler);
+        $this->assertInstanceOf(LineFormatter::class, $formatter);
+
+        $config->set('logging.channels.formatteddaily', [
+            'driver' => 'daily',
+            'name' => 'fd',
+            'path' => storage_path('logs/laravel.log'),
+            'formatter' => HtmlFormatter::class,
+            'formatter_with' => [
+                'dateFormat' => 'Y/m/d--test',
+            ],
+        ]);
+
+        $logger = $manager->channel('formatteddaily');
+        $handler = $logger->getLogger()->getHandlers()[0];
+        $formatter = $handler->getFormatter();
+
+        $this->assertInstanceOf(StreamHandler::class, $handler);
+        $this->assertInstanceOf(HtmlFormatter::class, $formatter);
+
+        $dateFormat = new ReflectionProperty(get_class($formatter), 'dateFormat');
+        $dateFormat->setAccessible(true);
+
+        $this->assertEquals('Y/m/d--test', $dateFormat->getValue($formatter));
+    }
+
+    public function testLogManagerCreateSyslogDriverWithConfiguredFormatter()
+    {
+        $config = $this->app['config'];
+        $config->set('logging.channels.defaultsyslog', [
+            'driver' => 'syslog',
+            'name' => 'ds',
+        ]);
+
+        $manager = new LogManager($this->app);
+
+        // create logger with handler specified from configuration
+        $logger = $manager->channel('defaultsyslog');
+        $handler = $logger->getLogger()->getHandlers()[0];
+        $formatter = $handler->getFormatter();
+
+        $this->assertInstanceOf(SyslogHandler::class, $handler);
+        $this->assertInstanceOf(LineFormatter::class, $formatter);
+
+        $config->set('logging.channels.formattedsyslog', [
+            'driver' => 'syslog',
+            'name' => 'fs',
+            'formatter' => HtmlFormatter::class,
+            'formatter_with' => [
+                'dateFormat' => 'Y/m/d--test',
+            ],
+        ]);
+
+        $logger = $manager->channel('formattedsyslog');
+        $handler = $logger->getLogger()->getHandlers()[0];
+        $formatter = $handler->getFormatter();
+
+        $this->assertInstanceOf(SyslogHandler::class, $handler);
         $this->assertInstanceOf(HtmlFormatter::class, $formatter);
 
         $dateFormat = new ReflectionProperty(get_class($formatter), 'dateFormat');
