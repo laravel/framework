ece24b56a606474893c7ef7cdfaa8ee1e801876e
Taylor Otwell <taylor@laravel.com>
2018-12-30 19:41:05 -0500

    wip

--- src/Illuminate/Foundation/Exceptions/views/503.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/503.blade.php
@@ -1,11 +1,5 @@
-@extends('errors::illustrated-layout')
+@extends('errors::minimal')
 
 @section('code', '503')
 @section('title', __('Service Unavailable'))
-
-@section('image')
-    <div style="background-image: url({{ asset('/svg/503.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
-    </div>
-@endsection
-
-@section('message', __($exception->getMessage() ?: __('Sorry, we are doing some maintenance. Please check back soon.')))
+@section('message', __('Service Unavailable'))
