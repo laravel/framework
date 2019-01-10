8071e06506481c33044918e5eda9ddceeb2d5bba
Taylor Otwell <taylor@laravel.com>
2019-01-10 09:19:14 -0600

    formatting

--- src/Illuminate/Container/RewindableGenerator.php
+++ src/Illuminate/Container/RewindableGenerator.php
@@ -8,25 +8,37 @@ use IteratorAggregate;
 class RewindableGenerator implements Countable, IteratorAggregate
 {
     /**
+     * The generator callback.
+     *
      * @var callable
      */
-    private $generator;
+    protected $generator;
 
     /**
+     * The number of tagged services.
+     *
      * @var callable|int
      */
-    private $count;
+    protected $count;
 
     /**
-     * @param callable     $generator
-     * @param callable|int $count
+     * Create a new generator instance.
+     *
+     * @param  callable  $generator
+     * @param  callable|int  $count
+     * @return void
      */
     public function __construct(callable $generator, $count)
     {
-        $this->generator = $generator;
         $this->count = $count;
+        $this->generator = $generator;
     }
 
+    /**
+     * Get an interator from the generator.
+     *
+     * @return mixed
+     */
     public function getIterator()
     {
         $generator = $this->generator;
@@ -35,6 +47,8 @@ class RewindableGenerator implements Countable, IteratorAggregate
     }
 
     /**
+     * Get the total number of tagged services.
+     *
      * @return int
      */
     public function count()
