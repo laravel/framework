f08bcb4b5c89aa72f19fd534767ec1b2bb59198d
Wizofgoz <wizofgoz2@gmail.com>
2019-01-12 10:22:50 -0800

    Updated methods list on Crypt Facade

--- src/Illuminate/Support/Facades/Crypt.php
+++ src/Illuminate/Support/Facades/Crypt.php
@@ -5,6 +5,8 @@ namespace Illuminate\Support\Facades;
 /**
  * @method static string encrypt(string $value, bool $serialize = true)
  * @method static string decrypt(string $payload, bool $unserialize = true)
+ * @method static string generateKey(string $cipher = null)
+ * @method static bool isValid()
  *
  * @see \Illuminate\Encryption\EncryptionManager
  */
