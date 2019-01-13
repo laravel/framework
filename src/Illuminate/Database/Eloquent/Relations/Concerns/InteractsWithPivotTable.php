9d555b8e17cc1ea3770922657762190ed6609d25
Ebrahim Aboalnaga <aboalnaga@users.noreply.github.com>
2019-01-13 18:48:27 +0200

    Remove unneeded call to `parseId`.
    `parseIds` will be called from with in `newPivotStatementForId`

--- src/Illuminate/Database/Eloquent/Relations/Concerns/InteractsWithPivotTable.php
+++ src/Illuminate/Database/Eloquent/Relations/Concerns/InteractsWithPivotTable.php
@@ -188,7 +188,7 @@ trait InteractsWithPivotTable
             $attributes = $this->addTimestampsToAttachment($attributes, true);
         }
 
-        $updated = $this->newPivotStatementForId($this->parseId($id))->update(
+        $updated = $this->newPivotStatementForId($id)->update(
             $this->castAttributes($attributes)
         );
 
