5ff2a9dfaff066677885e9117ff2609be0e5f3f5
shirafuta <shirafuta.kodai@gmail.com>
2019-01-15 21:56:44 +0900

    add trusted hosts middleware

---
+++ src/Illuminate/Foundation/Http/Middleware/TrustHosts.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Illuminate\Foundation\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class TrustHosts
+{
+    /**
+     * The trusted host names.
+     * 
+     * @var array
+     */
+    protected $trustedHosts = [];
+
+    /**
+     * Gets the trusted host names.
+     *
+     * @return array
+     */
+    public function getTrustedHosts()
+    {
+        return $this->trustedHosts;
+    }
+
+    /**
+     * Sets trusted host names.
+     *
+     * @param array $trustedHosts 
+     */
+    public function setTrustedHosts($trustedHosts)
+    {
+        $this->trustedHosts = $trustedHosts;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->setTrustedHosts($this->getTrustedHosts());
+        return $next($request);
+    }
+}
---
+++ tests/Foundation/Http/Middleware/TrustHostsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Illuminate\Tests\Foundation\Http\Middleware;
+
+use Mockery as m;
+use Illuminate\Http\Request;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Http\Middleware\TrustHosts;
+
+class TrustHostsTest extends TestCase
+{
+    public function testItSetTrustHostsToRequest()
+    {
+        $hosts = ['mysites.com'];
+
+        $request = m::mock(Request::class);
+        $request->shouldReceive('setTrustedHosts')->with($hosts)->once();
+
+        $middleware = new TrustHosts();
+        $middleware->setTrustedHosts($hosts);
+        $middleware->handle($request, function(){});
+    }
+}
