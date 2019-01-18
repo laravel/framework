51c6bb8e6430dcf5479565a02ceca52884393daf
Dries Vints <dries.vints@gmail.com>
2019-01-18 11:01:44 +0100

    Remove overwritten methods on Repository interface
    These methods are already defined on the PSR contract and it's unnecessary to define them twice.

--- src/Illuminate/Contracts/Cache/Repository.php
+++ src/Illuminate/Contracts/Cache/Repository.php
@@ -8,23 +8,6 @@ use Psr\SimpleCache\CacheInterface;
 interface Repository extends CacheInterface
 {
     /**
-     * Determine if an item exists in the cache.
-     *
-     * @param  string  $key
-     * @return bool
-     */
-    public function has($key);
-
-    /**
-     * Retrieve an item from the cache by key.
-     *
-     * @param  string  $key
-     * @param  mixed   $default
-     * @return mixed
-     */
-    public function get($key, $default = null);
-
-    /**
      * Retrieve an item from the cache and delete it.
      *
      * @param  string  $key
