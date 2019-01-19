f4a6fb3fa892336e5b8fd7cdc288c2b8df335ef1
Taylor Otwell <taylor@laravel.com>
2019-01-19 08:08:09 -0600

    add integration test for memcached tagged cache operations

--- tests/Integration/Cache/MemcachedCacheLockTest.php
+++ tests/Integration/Cache/MemcachedCacheLockTest.php
@@ -10,31 +10,8 @@ use Illuminate\Support\Facades\Cache;
 /**
  * @group integration
  */
-class MemcachedCacheLockTest extends TestCase
+class MemcachedCacheLockTest extends MemcachedIntegrationTest
 {
-    public function setUp()
-    {
-        parent::setUp();
-
-        if (! extension_loaded('memcached')) {
-            $this->markTestSkipped('Memcached module not installed');
-        }
-
-        // Determine whether there is a running Memcached instance
-        $testConnection = new Memcached;
-        $testConnection->addServer(
-            env('MEMCACHED_HOST', '127.0.0.1'),
-            env('MEMCACHED_PORT', 11211)
-        );
-        $testConnection->getVersion();
-
-        if ($testConnection->getResultCode() > Memcached::RES_SUCCESS) {
-            $this->markTestSkipped('Memcached could not establish a connection');
-        }
-
-        $testConnection->quit();
-    }
-
     public function test_memcached_locks_can_be_acquired_and_released()
     {
         Cache::store('memcached')->lock('foo')->forceRelease();
---
+++ tests/Integration/Cache/MemcachedIntegrationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Cache;
+
+use Memcached;
+use Illuminate\Support\Carbon;
+use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Cache;
+
+/**
+ * @group integration
+ */
+abstract class MemcachedIntegrationTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        if (! extension_loaded('memcached')) {
+            $this->markTestSkipped('Memcached module not installed');
+        }
+
+        // Determine whether there is a running Memcached instance
+        $testConnection = new Memcached;
+
+        $testConnection->addServer(
+            env('MEMCACHED_HOST', '127.0.0.1'),
+            env('MEMCACHED_PORT', 11211)
+        );
+
+        $testConnection->getVersion();
+
+        if ($testConnection->getResultCode() > Memcached::RES_SUCCESS) {
+            $this->markTestSkipped('Memcached could not establish a connection');
+        }
+
+        $testConnection->quit();
+    }
+}
