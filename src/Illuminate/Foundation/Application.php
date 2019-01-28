52c79ecf54b6168a54730ccb6c4c9f3561732a80
Dries Vints <dries.vints@gmail.com>
2019-01-28 21:53:19 +0100

    Version

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -29,7 +29,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      *
      * @var string
      */
-    const VERSION = '5.5.44';
+    const VERSION = '5.5.45';
 
     /**
      * The base path for the Laravel installation.
