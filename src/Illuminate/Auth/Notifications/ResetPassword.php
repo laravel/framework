80c5aecb443e7a55e868b66b9e0a93b7dfec08e8
Taylor Otwell <taylor@laravel.com>
2019-01-28 08:28:03 -0600

    use json

--- src/Illuminate/Auth/Notifications/ResetPassword.php
+++ src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -60,7 +60,7 @@ class ResetPassword extends Notification
             ->subject(Lang::getFromJson('Reset Password Notification'))
             ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your account.'))
             ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('password.reset', $this->token, false)))
-            ->line(Lang::transChoice('Your password reset link will expire in :count minute.|Your password reset link will expire in :count minutes.', config('auth.passwords.users.expire')))
+            ->line(Lang::getFromJson('This password reset link will expire in :count minutes.', config('auth.passwords.users.expire')))
             ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
     }
 
