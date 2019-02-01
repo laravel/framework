182e4e14aa294b578e8e43e3c191405ed2d647b0
Justas Lavišius <jl@itcity.lt>
2019-02-01 14:56:18 +0200

    MorphTo: instantiate related class appropriate to given parent instance

--- src/Illuminate/Database/Eloquent/Relations/MorphTo.php
+++ src/Illuminate/Database/Eloquent/Relations/MorphTo.php
@@ -178,6 +178,17 @@ class MorphTo extends BelongsTo
     }
 
     /**
+     * Make a new related instance for the given model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $parent
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    protected function newRelatedInstanceFor(Model $parent)
+    {
+        return $parent->{$this->relation}()->getRelated()->newInstance();
+    }
+
+    /**
      * Associate the model instance to the given parent.
      *
      * @param  \Illuminate\Database\Eloquent\Model  $model
