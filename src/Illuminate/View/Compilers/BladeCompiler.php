db774be8588f9d30ce4577bc06fac9711e08d9cb
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2019-02-15 16:02:26 +0100

    provide a path to view in compiled view

--- src/Illuminate/View/Compilers/BladeCompiler.php
+++ src/Illuminate/View/Compilers/BladeCompiler.php
@@ -118,7 +118,8 @@ class BladeCompiler extends Compiler implements CompilerInterface
         }
 
         if (! is_null($this->cachePath)) {
-            $contents = $this->compileString($this->files->get($this->getPath()));
+            $contents = "<?php/* {$this->getPath()} */?>\n";
+            $contents .= $this->compileString($this->files->get($this->getPath()));
 
             $this->files->put($this->getCompiledPath($this->getPath()), $contents);
         }
