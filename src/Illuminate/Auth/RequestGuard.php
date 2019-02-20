859221a451b083f752734c224e2d92e425c804f8
Joël Harkes <jlharkes@gmail.com>
2019-02-20 10:48:53 +0100

    Update RequestGuard.php
    fix linting

--- src/Illuminate/Auth/RequestGuard.php
+++ src/Illuminate/Auth/RequestGuard.php
@@ -80,7 +80,7 @@ class RequestGuard implements Guard
      */
     public function setRequest(Request $request)
     {
-        if($this->request !== $request){
+        if ($this->request !== $request) {
             $this->user = null;
         }
         $this->request = $request;
