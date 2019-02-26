e7e5f81d2ac2c6d005c00eaaf5dd10c846d2ef36
Erik Gaal <me@erikgaal.nl>
2019-02-26 14:19:43 +0100

    Add test to show backwards compatibility with 'broken' behaviour

--- tests/Integration/Database/EloquentPaginateTest.php
+++ tests/Integration/Database/EloquentPaginateTest.php
@@ -48,25 +48,38 @@ class EloquentPaginateTest extends DatabaseTestCase
 
         $query = Post::query()->distinct();
 
-        $this->assertEquals(6, $query->count());
+        $this->assertEquals(6, $query->get()->count());
         $this->assertEquals(6, $query->paginate()->total());
     }
 
     public function test_pagination_with_distinct_and_select()
+    {
+        // This is the 'broken' behaviour, but this test is added to show backwards compatibility.
+        for ($i = 1; $i <= 3; $i++) {
+            Post::create(['title' => 'Hello world']);
+            Post::create(['title' => 'Goodbye world']);
+        }
+
+        $query = Post::query()->distinct()->select('title');
+
+        $this->assertEquals(2, $query->get()->count());
+        $this->assertEquals(6, $query->paginate()->total());
+    }
+
+    public function test_pagination_with_distinct_columns_and_select()
     {
         for ($i = 1; $i <= 3; $i++) {
             Post::create(['title' => 'Hello world']);
             Post::create(['title' => 'Goodbye world']);
         }
 
-        $query = Post::query()->distinct('title')
-            ->select('title');
+        $query = Post::query()->distinct('title')->select('title');
 
-        $this->assertEquals(2, $query->count());
+        $this->assertEquals(2, $query->get()->count());
         $this->assertEquals(2, $query->paginate()->total());
     }
 
-    public function test_pagination_with_distinct_and_select_and_join()
+    public function test_pagination_with_distinct_columns_and_select_and_join()
     {
         for ($i = 1; $i <= 5; $i++) {
             $user = User::create();
@@ -79,8 +92,7 @@ class EloquentPaginateTest extends DatabaseTestCase
         }
 
         $query = User::query()->join('posts', 'posts.user_id', '=', 'users.id')
-            ->distinct('users.id')
-            ->select('users.*');
+            ->distinct('users.id')->select('users.*');
 
         $this->assertEquals(5, $query->get()->count());
         $this->assertEquals(5, $query->paginate()->total());
