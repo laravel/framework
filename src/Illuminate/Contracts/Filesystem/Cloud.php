456805e9c51cdaad7efa72a8ac2b2171b7d6f10c
Donkfather <Donkfather@users.noreply.github.com>
2019-02-27 16:47:07 +0200

    add options param to url()

--- src/Illuminate/Contracts/Filesystem/Cloud.php
+++ src/Illuminate/Contracts/Filesystem/Cloud.php
@@ -8,7 +8,8 @@ interface Cloud extends Filesystem
      * Get the URL for the file at the given path.
      *
      * @param  string  $path
+     * @param  array  $options  
      * @return string
      */
-    public function url($path);
+    public function url($path, array $options = []);
 }
