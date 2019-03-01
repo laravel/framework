68b753b6730665a2e654acf5b7e6ab940b7cd1a8
Matt Ford <mat84@btinternet.com>
2019-03-01 10:53:05 +0000

    Fix typo in test

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -193,7 +193,7 @@ class SupportStrTest extends TestCase
         $this->assertTrue(Str::is($patternObject, $valueObject));
 
         // Tests the wildcard parameter
-        $this->assertFalse(Str::is('foo.*.baz', 'test.bar.bar.baz', '[^.]*'));
+        $this->assertFalse(Str::is('foo.*.baz', 'foo.bar.bar.baz', '[^.]*'));
         $this->assertTrue(Str::is('foo.*.baz', 'foo.bar.baz', '[^.]*'));
     }
 
