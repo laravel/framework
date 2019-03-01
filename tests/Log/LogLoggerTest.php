da08c970328dcc7d013e06588e8467342cc5b2f7
Yohanan Baruchel <ybaruchel@gmail.com>
2019-03-01 22:10:15 +0200

    Change logger test name

--- tests/Log/LogLoggerTest.php
+++ tests/Log/LogLoggerTest.php
@@ -26,7 +26,7 @@ class LogLoggerTest extends TestCase
         $writer->error('foo');
     }
 
-    public function testLevelIsValidated()
+    public function testLogLevelIsValidated()
     {
         $level = 'fooLevel';
 
