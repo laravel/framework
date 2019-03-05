9a90b619caa4aee52a8ab20adfe38efd54fde0a5
Ahmed Fathy <aliraqi.dev@gmail.com>
2019-03-05 12:15:24 +0200

    allow to use group option in Auth::routes()

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -1148,26 +1148,30 @@ class Router implements RegistrarContract, BindingRegistrar
      */
     public function auth(array $options = [])
     {
-        // Authentication Routes...
-        $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-        $this->post('login', 'Auth\LoginController@login');
-        $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+        $group = $options['group'] ?? [];
 
-        // Registration Routes...
-        if ($options['register'] ?? true) {
-            $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-            $this->post('register', 'Auth\RegisterController@register');
-        }
+        $this->group($group, function () {
+            // Authentication Routes...
+            $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+            $this->post('login', 'Auth\LoginController@login');
+            $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
-        // Password Reset Routes...
-        if ($options['reset'] ?? true) {
-            $this->resetPassword();
-        }
+            // Registration Routes...
+            if ($options['register'] ?? true) {
+                $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+                $this->post('register', 'Auth\RegisterController@register');
+            }
 
-        // Email Verification Routes...
-        if ($options['verify'] ?? false) {
-            $this->emailVerification();
-        }
+            // Password Reset Routes...
+            if ($options['reset'] ?? true) {
+                $this->resetPassword();
+            }
+
+            // Email Verification Routes...
+            if ($options['verify'] ?? false) {
+                $this->emailVerification();
+            }
+        });
     }
 
     /**
