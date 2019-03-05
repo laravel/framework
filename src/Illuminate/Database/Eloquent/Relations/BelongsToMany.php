af03c5c32595d2587ec80cc13d2386b98c0b2e8e
Taylor Otwell <taylor@laravel.com>
2019-03-05 07:45:48 -0600

    formatting

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -141,14 +141,39 @@ class BelongsToMany extends Relation
     public function __construct(Builder $query, Model $parent, $table, $foreignPivotKey,
                                 $relatedPivotKey, $parentKey, $relatedKey, $relationName = null)
     {
-        $this->table = $this->resolveTableName($table);
         $this->parentKey = $parentKey;
         $this->relatedKey = $relatedKey;
         $this->relationName = $relationName;
         $this->relatedPivotKey = $relatedPivotKey;
         $this->foreignPivotKey = $foreignPivotKey;
+        $this->table = $this->resolveTableName($table);
 
         parent::__construct($query, $parent);
+    }
+
+    /**
+     * Attempt to resolve the intermediate table name from the given string.
+     *
+     * @param  string  $table
+     * @return string
+     */
+    protected function resolveTableName($table)
+    {
+        if (! Str::contains($table, '\\') || ! class_exists($table)) {
+            return $table;
+        }
+
+        $model = new $table;
+
+        if (! $model instanceof Model) {
+            return $table;
+        }
+
+        if ($model instanceof Pivot) {
+            $this->using($table);
+        }
+
+        return $model->getTable();
     }
 
     /**
@@ -185,31 +210,6 @@ class BelongsToMany extends Relation
         $query->join($this->table, $key, '=', $this->getQualifiedRelatedPivotKeyName());
 
         return $this;
-    }
-
-    /**
-     * Attempt to resolve the table name from a given string.
-     *
-     * @param  string  $table
-     * @return string
-     */
-    protected function resolveTableName($table)
-    {
-        if (! class_exists($table)) {
-            return $table;
-        }
-
-        $model = new $table;
-
-        if (! ($model instanceof Model)) {
-            return $table;
-        }
-
-        if ($model instanceof Pivot) {
-            $this->using($table);
-        }
-
-        return $model->getTable();
     }
 
     /**
