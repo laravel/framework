5ffe83557fe8322f7c43934bda709b7ac0d92c7b
Vaibhav Jaimini <jaimini.v@media.net>
2019-03-06 17:23:50 +0530

    Added support for relative path in mix -- fixed minor issue

--- src/Illuminate/Foundation/Mix.php
+++ src/Illuminate/Foundation/Mix.php
@@ -64,7 +64,7 @@ class Mix
         }
 
         $html = $manifestDirectory.$manifest[$path];
-        if($relativePath && Str::startsWith($html, ['/'])){
+        if ($relativePath && Str::startsWith($html, ['/'])) {
             $html = Str::substr($html, 1);
         }
 
