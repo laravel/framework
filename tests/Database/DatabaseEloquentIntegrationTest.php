786b37f0ba0fa7636c6ebac7bfa809d8feae0353
Yuto KUBO <yuto.kubo@gmail.com>
2019-03-06 22:36:04 +0900

    backport: Add PHP 7.2 to travis #20258

--- tests/Database/DatabaseEloquentIntegrationTest.php
+++ tests/Database/DatabaseEloquentIntegrationTest.php
@@ -1005,10 +1005,12 @@ class DatabaseEloquentIntegrationTest extends PHPUnit_Framework_TestCase
         EloquentTestUser::create(['id' => 2, 'email' => 'abigailotwell@gmail.com']);
 
         $results = EloquentTestUser::forPageAfterId(15, 1);
-        $this->assertEquals(1, count($results));
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Builder', $results);
+        $this->assertEquals(2, $results->first()->id);
 
         $results = EloquentTestUser::orderBy('id', 'desc')->forPageAfterId(15, 1);
-        $this->assertEquals(1, count($results));
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Builder', $results);
+        $this->assertEquals(2, $results->first()->id);
     }
 
     public function testMorphToRelationsAcrossDatabaseConnections()
