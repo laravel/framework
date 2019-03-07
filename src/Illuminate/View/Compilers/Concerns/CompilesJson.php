e7064ca184e395f86fc924e0d58d94955c734675
Dries Vints <dries.vints@gmail.com>
2019-03-07 13:37:45 +0100

    Revert "update blade JSON directive"
    This reverts commit 590ba80d13072eec7629eb0e7a24440ba96989e2.

--- src/Illuminate/View/Compilers/Concerns/CompilesJson.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesJson.php
@@ -5,6 +5,13 @@ namespace Illuminate\View\Compilers\Concerns;
 trait CompilesJson
 {
     /**
+     * The default JSON encoding options.
+     *
+     * @var int
+     */
+    private $encodingOptions = JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT;
+
+    /**
      * Compile the JSON statement into valid PHP.
      *
      * @param  string  $expression
@@ -12,6 +19,12 @@ trait CompilesJson
      */
     protected function compileJson($expression)
     {
-        return "<?php echo json_encode(" . $this->stripParentheses($expression) . ") ?>";
+        $parts = explode(',', $this->stripParentheses($expression));
+
+        $options = isset($parts[1]) ? trim($parts[1]) : $this->encodingOptions;
+
+        $depth = isset($parts[2]) ? trim($parts[2]) : 512;
+
+        return "<?php echo json_encode($parts[0], $options, $depth) ?>";
     }
 }
--- tests/View/Blade/BladeJsonTest.php
+++ tests/View/Blade/BladeJsonTest.php
@@ -4,42 +4,18 @@ namespace Illuminate\Tests\View\Blade;
 
 class BladeJsonTest extends AbstractBladeTestCase
 {
-    public function testBasicStatementIsCompiled()
+    public function testStatementIsCompiledWithSafeDefaultEncodingOptions()
     {
         $string = 'var foo = @json($var);';
-        $expected = 'var foo = <?php echo json_encode($var) ?>;';
+        $expected = 'var foo = <?php echo json_encode($var, 15, 512) ?>;';
 
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 
-    public function testOptionsArgumentCanBeSpecified()
+    public function testEncodingOptionsCanBeOverwritten()
     {
         $string = 'var foo = @json($var, JSON_HEX_TAG);';
-        $expected = 'var foo = <?php echo json_encode($var, JSON_HEX_TAG) ?>;';
-
-        $this->assertEquals($expected, $this->compiler->compileString($string));
-    }
-
-    public function testDepthArgumentCanBeSpecified()
-    {
-        $string = 'var foo = @json($var, 0, 128);';
-        $expected = 'var foo = <?php echo json_encode($var, 0, 128) ?>;';
-
-        $this->assertEquals($expected, $this->compiler->compileString($string));
-    }
-
-    public function testValueArgumentCanContainCommas()
-    {
-        $string = 'var foo = @json(["value1", "value2", "value3"], JSON_HEX_TAG, 512);';
-        $expected = 'var foo = <?php echo json_encode(["value1", "value2", "value3"], JSON_HEX_TAG, 512) ?>;';
-
-        $this->assertEquals($expected, $this->compiler->compileString($string));
-    }
-
-    public function testValueArgumentCanContainFunctions()
-    {
-        $string = 'var foo = @json(array_merge(["value1", "value2"], ["value3", "value4"]), JSON_HEX_TAG, 512);';
-        $expected = 'var foo = <?php echo json_encode(array_merge(["value1", "value2"], ["value3", "value4"]), JSON_HEX_TAG, 512) ?>;';
+        $expected = 'var foo = <?php echo json_encode($var, JSON_HEX_TAG, 512) ?>;';
 
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
