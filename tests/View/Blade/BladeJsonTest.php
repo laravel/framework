4c3c4fa1d24120e0c6c303852677ada0b7402c5f
Andrew <browner12@gmail.com>
2019-03-11 13:36:17 -0500

    remove duplicate tests
    i had some duplicated tests in there.
    
    also rename the one test to be more consistent with the others

--- tests/View/Blade/BladeJsonTest.php
+++ tests/View/Blade/BladeJsonTest.php
@@ -12,38 +12,28 @@ class BladeJsonTest extends AbstractBladeTestCase
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 
-    public function testEncodingOptionsCanBeOverwritten()
-    {
-        $string = 'var foo = @json($var, JSON_HEX_TAG);';
-        $expected = 'var foo = <?php echo json_encode($var, JSON_HEX_TAG, 512) ?>;';
-
-        $this->assertEquals($expected, $this->compiler->compileString($string));
-    }
-
-    public function testBasicStatementIsCompiled()
-    {
-        $string = 'var foo = @json($var);';
-        $expected = 'var foo = <?php echo json_encode($var, 15, 512) ?>;';
-        $this->assertEquals($expected, $this->compiler->compileString($string));
-    }
     public function testOptionsArgumentCanBeSpecified()
     {
         $string = 'var foo = @json($var, JSON_HEX_TAG);';
         $expected = 'var foo = <?php echo json_encode($var, JSON_HEX_TAG, 512) ?>;';
+
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
+
     public function testDepthArgumentCanBeSpecified()
     {
         $string = 'var foo = @json($var, 0, 128);';
         $expected = 'var foo = <?php echo json_encode($var, 0, 128) ?>;';
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
+
     public function testValueArgumentCanContainCommas()
     {
         $string = 'var foo = @json(["value1", "value2", "value3"], JSON_HEX_TAG, 512);';
         $expected = 'var foo = <?php echo json_encode(["value1", "value2", "value3"], JSON_HEX_TAG, 512) ?>;';
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
+
     public function testValueArgumentCanContainFunctions()
     {
         $string = 'var foo = @json(array_merge(["value1", "value2"], ["value3", "value4"]), JSON_HEX_TAG, 512);';
