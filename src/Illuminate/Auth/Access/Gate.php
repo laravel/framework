203172477fc99d541a6def73449c8cacc3718ca6
raydabbah <raydabbah@gmail.com>
2019-03-12 09:39:55 -0400

    fix whitespace errors

--- src/Illuminate/Auth/Access/Gate.php
+++ src/Illuminate/Auth/Access/Gate.php
@@ -295,7 +295,7 @@ class Gate implements GateContract
         });
     }
 
-     /**
+    /**
      * Determine if any one of the given abilities should be denied for the current user.
      *
      * @param  iterable|string  $abilities
@@ -305,7 +305,7 @@ class Gate implements GateContract
     public function none($abilities, $arguments = [])
     {
         return ! $this->any($abilities, $arguments);
-    }   
+    }     
 
     /**
      * Determine if the given ability should be granted for the current user.
