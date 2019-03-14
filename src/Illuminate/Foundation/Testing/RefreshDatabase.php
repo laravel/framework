d37578fb4d7e3d92f22d6c70985797864b2ebb63
Matt Isenhower <matt@isenhower.com>
2019-03-13 20:52:03 -0700

    RefreshDatabase trait: Only refresh the database when migrations change
    This now calculates a hash based on the contents of all the migration files and uses that to determine whether the database should be refreshed.

--- src/Illuminate/Foundation/Testing/RefreshDatabase.php
+++ src/Illuminate/Foundation/Testing/RefreshDatabase.php
@@ -50,16 +50,81 @@ trait RefreshDatabase
     protected function refreshTestDatabase()
     {
         if (! RefreshDatabaseState::$migrated) {
-            $this->artisan('migrate:fresh', $this->shouldDropViews() ? [
+            // Fall back to "migrate" in case the test database was deleted
+            $command = $this->shouldRefreshTestDatabase() ? 'migrate:fresh' : 'migrate';
+
+            $this->artisan($command, $this->shouldDropViews() ? [
                 '--drop-views' => true,
             ] : []);
 
             $this->app[Kernel::class]->setArtisan(null);
 
+            $this->updateCachedMigrationHash();
+
             RefreshDatabaseState::$migrated = true;
         }
 
         $this->beginDatabaseTransaction();
+    }
+
+    /**
+     * Determine if the database should be refreshed.
+     *
+     * @return bool
+     */
+    protected function shouldRefreshTestDatabase()
+    {
+        return $this->getCachedMigrationHash() !== $this->getMigrationHash();
+    }
+
+    /**
+     * Calculate a hash based on the contents of each migration file.
+     *
+     * @return string
+     */
+    protected function getMigrationHash()
+    {
+        $migrationPath = $this->app->databasePath().DIRECTORY_SEPARATOR.'migrations';
+
+        $migrationFiles = collect($this->app['migrator']->getMigrationFiles($migrationPath));
+
+        return $migrationFiles->reduce(function ($hash, $file) {
+            return md5($hash.file_get_contents($file));
+        });
+    }
+
+    /**
+     * Get the path to the migration cache file.
+     *
+     * @return string
+     */
+    protected function getCachedMigrationPath()
+    {
+        return storage_path('framework/testing/migrationhash');
+    }
+
+    /**
+     * Get the current cached migration hash value.
+     *
+     * @return string
+     */
+    protected function getCachedMigrationHash()
+    {
+        $path = $this->getCachedMigrationPath();
+
+        if (file_exists($path)) {
+            return file_get_contents($path);
+        }
+    }
+
+    /**
+     * Update the cached migration hash value with the current migration hash.
+     *
+     * @return void
+     */
+    protected function updateCachedMigrationHash()
+    {
+        file_put_contents($this->getCachedMigrationPath(), $this->getMigrationHash());
     }
 
     /**
