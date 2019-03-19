791992e20efdf043ac3c2d989025d48d648821de
Taylor Otwell <taylor@laravel.com>
2019-03-19 07:20:36 -0700

    formatting

--- src/Illuminate/Session/Middleware/StartSession.php
+++ src/Illuminate/Session/Middleware/StartSession.php
@@ -53,11 +53,11 @@ class StartSession
 
         $this->collectGarbage($session);
 
-        $this->addCookieToResponse(
-            $response = $next($request), $session
-        );
+        $response = $next($request);
 
         $this->storeCurrentUrl($request, $session);
+
+        $this->addCookieToResponse($response, $session);
 
         // Again, if the session has been configured we will need to close out the session
         // so that the attributes may be persisted to some storage medium. We will also
