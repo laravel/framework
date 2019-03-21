2209d661b38302d7b74da932aa8adf8ffb3892eb
pepijn-de-wachter <pepijn.dewachter@gmail.com>
2019-03-21 22:12:21 +0100

    fixed docblock style

--- src/Illuminate/Database/Schema/ForeignDefinition.php
+++ src/Illuminate/Database/Schema/ForeignDefinition.php
@@ -3,12 +3,13 @@
 namespace Illuminate\Database\Schema;
 
 use Illuminate\Support\Fluent;
+
 /**
  * @method ForeignDefinition references(string $column) The name of the column on the referenced table table.
  * @method ForeignDefinition on(string $table) The name of the table which is referenced.
  * @method ForeignDefinition onDelete(string $action) The desired action to execute when a foreign row is deleted.
  * @method ForeignDefinition onUpdate(string $action) The desired action to execute when a foreign row is updated.
-*/
+ */
 class ForeignDefinition extends Fluent
 {
     //
