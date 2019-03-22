edc578f70ff7b93e5d55ff74dc77a0a3e62a87c3
pepijn-de-wachter <pepijn.dewachter@gmail.com>
2019-03-22 08:28:55 +0100

    fixed typo: forgot to update return type of actual foreign method when refactoring

--- src/Illuminate/Database/Schema/Blueprint.php
+++ src/Illuminate/Database/Schema/Blueprint.php
@@ -38,7 +38,7 @@ class Blueprint
     /**
      * The commands that should be run for the table.
      *
-     * @var \Illuminate\Support\Fluent[]
+     * @var \Illuminate\Support\Fluent[]|\Illuminate\Database\Schema\ForeignKeyDefinition[]
      */
     protected $commands = [];
 
@@ -520,7 +520,7 @@ class Blueprint
      *
      * @param  string|array  $columns
      * @param  string  $name
-     * @return \Illuminate\Database\Schema\ForeignDefinition
+     * @return \Illuminate\Database\Schema\ForeignKeyDefinition
      */
     public function foreign($columns, $name = null)
     {
