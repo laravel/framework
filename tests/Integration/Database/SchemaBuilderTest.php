0c1f39c7ddc58705b6457878008a2304d3aa8dab
jackson <stydal@protonmail.com>
2019-04-16 00:16:52 +0200

    Add a test to verify the column type and the syntax.

--- tests/Integration/Database/SchemaBuilderTest.php
+++ tests/Integration/Database/SchemaBuilderTest.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Tests\Integration\Database\SchemaTest;
 
 use Doctrine\DBAL\Types\Type;
+use Illuminate\Database\Schema\Grammars\SQLiteGrammar;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -44,6 +45,28 @@ class SchemaBuilderTest extends DatabaseTestCase
     {
         Schema::registerCustomDBALType(TinyInteger::class, TinyInteger::NAME, 'TINYINT');
 
+        Schema::create('test', function (Blueprint $table) {
+            $table->string('test_column');
+        });
+
+        $blueprint = new Blueprint('test', function (Blueprint $table) {
+            $table->tinyInteger('test_column')->change();
+        });
+
+        $expected = [
+            'CREATE TEMPORARY TABLE __temp__test AS SELECT test_column FROM test',
+            'DROP TABLE test',
+            'CREATE TABLE test (test_column TINYINT NOT NULL COLLATE BINARY)',
+            'INSERT INTO test (test_column) SELECT test_column FROM __temp__test',
+            'DROP TABLE __temp__test'
+        ];
+
+        $statements = $blueprint->toSql($this->getConnection(), new SQLiteGrammar());
+
+        $blueprint->build($this->getConnection(), new SQLiteGrammar());
+
         $this->assertArrayHasKey(TinyInteger::NAME, Type::getTypesMap());
+        $this->assertEquals('tinyinteger', Schema::getColumnType('test', 'test_column'));
+        $this->assertEquals($expected, $statements);
     }
 }
