41576bdc5fe0ecaeead70ca171639e2d923ce076
anas bouzid <bouzid.anas.1@gmail.com>
2019-04-21 17:31:26 +0100

    [5.8] convert email tables to layout tables

--- src/Illuminate/Mail/resources/views/html/subcopy.blade.php
+++ src/Illuminate/Mail/resources/views/html/subcopy.blade.php
@@ -1,4 +1,4 @@
-<table class="subcopy" width="100%" cellpadding="0" cellspacing="0">
+<table class="subcopy" width="100%" cellpadding="0" cellspacing="0" role="presentation">
     <tr>
         <td>
             {{ Illuminate\Mail\Markdown::parse($slot) }}
