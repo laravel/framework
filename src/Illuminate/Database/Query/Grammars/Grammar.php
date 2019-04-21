128ab459bd0b895227f8326df12d78d33a8b366b
David Rodrigues <david.proweb@gmail.com>
2019-04-21 13:55:04 -0300

    [5.8] Use coalesce operator

--- src/Illuminate/Database/Query/Grammars/Grammar.php
+++ src/Illuminate/Database/Query/Grammars/Grammar.php
@@ -733,9 +733,7 @@ class Grammar extends BaseGrammar
     protected function compileOrdersToArray(Builder $query, $orders)
     {
         return array_map(function ($order) {
-            return ! isset($order['sql'])
-                        ? $this->wrap($order['column']).' '.$order['direction']
-                        : $order['sql'];
+            return $order['sql'] ?? $this->wrap($order['column']).' '.$order['direction'];
         }, $orders);
     }
 
