7edf48ec748e8c435e66e278bb424aaf2877c769
Sergey Aleynik <kerbi.jam@yandex.ru>
2019-04-25 08:52:14 +0300

    Set global middleware

--- tests/Integration/Http/MiddlewareTest.php
+++ tests/Integration/Http/MiddlewareTest.php
@@ -2,6 +2,7 @@
 
 namespace Illuminate\Tests\Integration\Http;
 
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\Facades\Route;
 use Orchestra\Testbench\TestCase;
 
@@ -13,13 +14,14 @@ class MiddlewareTest extends TestCase
 
     public function test_request_in_middleware_is_shared()
     {
+        $this->app[Kernel::class]->prependMiddleware(MiddlewareStub::class);
         $this->app->singleton('service', function ($app) {
             return $app['request']->attributes->get('middleware');
         });
 
         Route::get('/', function () {
             return app('service');
-        })->middleware(MiddlewareStub::class);
+        });
 
         $response = $this->get('/');
 
