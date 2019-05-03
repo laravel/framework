3771a7950538190ec8da97a764e4c00090510403
Ricardo Sismeiro <195983+rasismeiro@users.noreply.github.com>
2019-05-03 13:02:52 +0100

    Formatting

--- tests/Console/Scheduling/EventTest.php
+++ tests/Console/Scheduling/EventTest.php
@@ -25,7 +25,8 @@ class EventTest extends TestCase
         $this->assertSame("php -i > {$quote}{$defaultOutput}{$quote} 2>&1", $event->buildCommand());
 
         $event = new Event(m::mock(EventMutex::class), 'php -i');
-        $event->after(function(){});
+        $event->after(function () {
+        });
         $event->runInBackground();
 
         $commandSeparator = ($isWindows ? '&' : ';');
