f987a6db9bd7905e87eb399fc9b408818b153439
Dries Vints <dries.vints@gmail.com>
2019-05-09 14:30:01 +0200

    Reset webpack file for none preset
    When using the none preset and previously react was chosen the webpack config file would still point to the react js compiler. Instead we should reset this file back to the JS compiler.
    
    Fixes https://github.com/laravel/framework/issues/28461

--- src/Illuminate/Foundation/Console/Presets/None.php
+++ src/Illuminate/Foundation/Console/Presets/None.php
@@ -15,6 +15,7 @@ class None extends Preset
     {
         static::updatePackages();
         static::updateBootstrapping();
+        static::updateWebpackConfiguration();
 
         tap(new Filesystem, function ($filesystem) {
             $filesystem->deleteDirectory(resource_path('js/components'));
@@ -57,5 +58,15 @@ class None extends Preset
         file_put_contents(resource_path('sass/app.scss'), ''.PHP_EOL);
         copy(__DIR__.'/none-stubs/app.js', resource_path('js/app.js'));
         copy(__DIR__.'/none-stubs/bootstrap.js', resource_path('js/bootstrap.js'));
+    }
+
+    /**
+     * Update the Webpack configuration.
+     *
+     * @return void
+     */
+    protected static function updateWebpackConfiguration()
+    {
+        copy(__DIR__.'/none-stubs/webpack.mix.js', base_path('webpack.mix.js'));
     }
 }
