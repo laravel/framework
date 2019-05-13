0df829a841466fb717431b8b3fecb18ce3c2fbf0
sedhossein <Seyedhossein77@gmail.com>
2019-05-13 22:09:40 +0430

    add php-doc
    @throws added

--- src/Illuminate/Auth/Access/Gate.php
+++ src/Illuminate/Auth/Access/Gate.php
@@ -417,6 +417,7 @@ class Gate implements GateContract
      *
      * @param  callable  $callback
      * @return bool
+     * @throws \ReflectionException
      */
     protected function callbackAllowsGuests($callback)
     {
@@ -598,6 +599,7 @@ class Gate implements GateContract
      *
      * @param  object|string  $class
      * @return mixed
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     public function resolvePolicy($class)
     {
