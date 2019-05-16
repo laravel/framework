b50fc2077b9489633f6260d7d60023f6d30591d5
4n70w4 <38257723+4n70w4@users.noreply.github.com>
2019-05-16 15:57:17 +0300

    fix exception: The filename fallback must only contain ASCII characters.

--- src/Illuminate/Filesystem/FilesystemAdapter.php
+++ src/Illuminate/Filesystem/FilesystemAdapter.php
@@ -138,7 +138,7 @@ class FilesystemAdapter implements FilesystemContract, CloudFilesystemContract
     {
         $response = new StreamedResponse;
 
-        $disposition = $response->headers->makeDisposition($disposition, $name ?? basename($path));
+        $disposition = $response->headers->makeDisposition($disposition, $name ?? basename($path), Str::ascii($name));
 
         $response->headers->replace($headers + [
             'Content-Type' => $this->mimeType($path),
