060ae1371afd8c320252617721c099d8ba27ae06
Dries Vints <dries.vints@gmail.com>
2019-05-21 14:27:50 +0200

    Consistent arranged title tag

--- src/Illuminate/Foundation/Exceptions/views/illustrated-layout.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/illustrated-layout.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>@yield('title')</title>
-
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>@yield('title')</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
