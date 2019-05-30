eff35ff6879d57e48b2878f23a6cdf613c95bb04
James Brooks <james@alt-three.com>
2019-05-30 20:24:37 +0100

    CS fixes

--- src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
+++ src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
@@ -110,7 +110,7 @@ class MigrateMakeCommand extends BaseCommand
             $name, $this->getMigrationPath(), $table, $create
         );
 
-        if (!$this->option('fullpath')) {
+        if (! $this->option('fullpath')) {
             $file = pathinfo($file, PATHINFO_FILENAME);
         }
 
