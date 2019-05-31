67ef1e631025fa0762be3c4e44bcd0193dc08b33
MohamedBenhida <contact@mohamedbenhida.com>
2019-05-31 16:05:23 +0000

    update factory helper

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -500,9 +500,11 @@ if (! function_exists('factory')) {
             return $factory->of($arguments[0], $arguments[1])->times($arguments[2] ?? null);
         } elseif (isset($arguments[1])) {
             return $factory->of($arguments[0])->times($arguments[1]);
+        } elseif (isset($arguments[0])) {
+            return $factory->of($arguments[0]);
         }
 
-        return $factory->of($arguments[0]);
+        return $factory;
     }
 }
 
