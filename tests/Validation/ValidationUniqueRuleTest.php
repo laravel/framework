53081e92c6c25f99574b9022aec79d9bf8c63801
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2019-06-06 01:49:56 +0200

    Fix database rules with WHERE clauses

--- tests/Validation/ValidationUniqueRuleTest.php
+++ tests/Validation/ValidationUniqueRuleTest.php
@@ -12,36 +12,40 @@ class ValidationUniqueRuleTest extends TestCase
     {
         $rule = new Unique('table');
         $rule->where('foo', 'bar');
-        $this->assertEquals('unique:table,NULL,NULL,id,foo,bar', (string) $rule);
+        $this->assertEquals('unique:table,NULL,NULL,id,foo,"bar"', (string) $rule);
 
         $rule = new Unique('table', 'column');
         $rule->ignore('Taylor, Otwell', 'id_column');
         $rule->where('foo', 'bar');
-        $this->assertEquals('unique:table,column,"Taylor, Otwell",id_column,foo,bar', (string) $rule);
+        $this->assertEquals('unique:table,column,"Taylor, Otwell",id_column,foo,"bar"', (string) $rule);
 
         $rule = new Unique('table', 'column');
         $rule->ignore('Taylor, Otwell"\'..-"', 'id_column');
         $rule->where('foo', 'bar');
-        $this->assertEquals('unique:table,column,"Taylor, Otwell\"\\\'..-\"",id_column,foo,bar', (string) $rule);
-        $this->assertEquals('Taylor, Otwell"\'..-"', stripslashes(str_getcsv('table,column,"Taylor, Otwell\"\\\'..-\"",id_column,foo,bar')[2]));
-        $this->assertEquals('id_column', stripslashes(str_getcsv('table,column,"Taylor, Otwell\"\\\'..-\"",id_column,foo,bar')[3]));
+        $this->assertEquals('unique:table,column,"Taylor, Otwell\"\\\'..-\"",id_column,foo,"bar"', (string) $rule);
+        $this->assertEquals('Taylor, Otwell"\'..-"', stripslashes(str_getcsv('table,column,"Taylor, Otwell\"\\\'..-\"",id_column,foo,"bar"')[2]));
+        $this->assertEquals('id_column', stripslashes(str_getcsv('table,column,"Taylor, Otwell\"\\\'..-\"",id_column,foo,"bar"')[3]));
 
         $rule = new Unique('table', 'column');
         $rule->ignore(null, 'id_column');
         $rule->where('foo', 'bar');
-        $this->assertEquals('unique:table,column,NULL,id_column,foo,bar', (string) $rule);
+        $this->assertEquals('unique:table,column,NULL,id_column,foo,"bar"', (string) $rule);
 
         $model = new EloquentModelStub(['id_column' => 1]);
 
         $rule = new Unique('table', 'column');
         $rule->ignore($model);
         $rule->where('foo', 'bar');
-        $this->assertEquals('unique:table,column,"1",id_column,foo,bar', (string) $rule);
+        $this->assertEquals('unique:table,column,"1",id_column,foo,"bar"', (string) $rule);
 
         $rule = new Unique('table', 'column');
         $rule->ignore($model, 'id_column');
         $rule->where('foo', 'bar');
-        $this->assertEquals('unique:table,column,"1",id_column,foo,bar', (string) $rule);
+        $this->assertEquals('unique:table,column,"1",id_column,foo,"bar"', (string) $rule);
+
+        $rule = new Unique('table');
+        $rule->where('foo', '"bar"');
+        $this->assertEquals('unique:table,NULL,NULL,id,foo,"""bar"""', (string) $rule);
     }
 }
 
