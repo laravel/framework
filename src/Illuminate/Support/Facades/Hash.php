7d272876dc534ee793ba5026cdac0804430175b9
antonkomarev <ell@cybercog.su>
2019-06-11 19:57:34 +0300

    Add Hash Facade methods hints

--- src/Illuminate/Support/Facades/Hash.php
+++ src/Illuminate/Support/Facades/Hash.php
@@ -3,10 +3,14 @@
 namespace Illuminate\Support\Facades;
 
 /**
+ * @method static \Illuminate\Hashing\BcryptHasher createBcryptDriver()
+ * @method static \Illuminate\Hashing\ArgonHasher createArgonDriver()
+ * @method static \Illuminate\Hashing\Argon2IdHasher createArgon2idDriver()
  * @method static array info(string $hashedValue)
  * @method static string make(string $value, array $options = [])
  * @method static bool check(string $value, string $hashedValue, array $options = [])
  * @method static bool needsRehash(string $hashedValue, array $options = [])
+ * @method static string getDefaultDriver()
  *
  * @see \Illuminate\Hashing\HashManager
  */
