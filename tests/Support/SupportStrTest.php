5b13661ebd81725938dac504b50274b187151aad
MohamedBenhida <contact@mohamedbenhida.com>
2019-06-12 11:22:26 +0000

    fix tests for Str::containsAll

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -132,9 +132,9 @@ class SupportStrTest extends TestCase
     public function testStrContainsAll()
     {
         $this->assertTrue(Str::containsAll('taylor otwell', ['taylor', 'otwell']));
-        $this->assertTrue(Str::containsAll('taylor otwell', 'taylor'));
         $this->assertTrue(Str::containsAll('taylor otwell', ['taylor']));
-        $this->assertFalse(Str::containsAll('taylor otwell', 'xxx'));
+        $this->assertTrue(Str::containsAll('taylor otwell', ['taylor']));
+        $this->assertFalse(Str::containsAll('taylor otwell', ['xxx']));
         $this->assertFalse(Str::containsAll('taylor otwell', ['taylor', 'xxx']));
     }
 
