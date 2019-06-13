987ee139963aea10cb57797cdfc46c9e68a77e5a
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2019-06-13 04:22:15 +0200

    Add "Remove whitespaces from cached views."

--- src/Illuminate/View/Compilers/BladeCompiler.php
+++ src/Illuminate/View/Compilers/BladeCompiler.php
@@ -136,10 +136,47 @@ class BladeCompiler extends Compiler implements CompilerInterface
                 $contents .= "<?php /**PATH {$this->getPath()} ENDPATH**/ ?>";
             }
 
+            if ($this->app['config']['view.removeWhitespace'] == true) {
+                $contents = $this->removeWhitespaceContent($contents);
+            }
+
             $this->files->put(
                 $this->getCompiledPath($this->getPath()), $contents
             );
         }
+    }
+
+    /**
+     * Remove whitespace from cached views
+     * ignore script , pre and textarea tags
+     * @param $contents
+     * @return mixed
+     */
+    protected function removeWhitespaceContent($contents)
+    {
+        $replace = [
+            '%# Collapse ws everywhere but in blacklisted elements.
+        (?>             # Match all whitespans other than single space.
+          [^\S]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+        | \s{2,}        # or two or more consecutive-any-whitespace.
+        ) # Note: The remaining regex consumes no text at all...
+        (?=             # Ensure we are not in a blacklist tag.
+          (?:           # Begin (unnecessary) group.
+            (?:         # Zero or more of...
+              [^<]++    # Either one or more non-"<"
+            | <         # or a < starting a non-blacklist tag.
+              (?!/?(?:textarea|pre|script)\b)
+            )*+         # (This could be "unroll-the-loop"ified.)
+          )             # End (unnecessary) group.
+          (?:           # Begin alternation group.
+            <           # Either a blacklist start tag.
+            (?>textarea|pre|script)\b
+          | \z          # or end of file.
+          )             # End alternation group.
+        )  # If we made it here, we are not in a blacklist tag.
+        %ix' => ' ',
+        ];
+        return preg_replace(array_keys($replace), array_values($replace), $contents);
     }
 
     /**
