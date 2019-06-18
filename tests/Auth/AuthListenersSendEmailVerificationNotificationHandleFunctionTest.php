6137653211d5096c79ba1f9d5e23cefdeffeeade
Kalesh Kaladharan <kalesh.13@gmail.com>
2019-06-18 17:08:21 +0530

    Test changed

--- tests/Auth/AuthListenersSendEmailVerificationNotificationHandleFunctionTest.php
+++ tests/Auth/AuthListenersSendEmailVerificationNotificationHandleFunctionTest.php
@@ -3,7 +3,6 @@
 namespace Illuminate\Tests\Auth;
 
 use PHPUnit\Framework\TestCase;
-use Illuminate\Foundation\Auth\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -18,19 +17,6 @@ class AuthListenersSendEmailVerificationNotificationHandleFunctionTest extends T
         $user = $this->getMockBuilder(MustVerifyEmail::class)->getMock();
         $user->method('hasVerifiedEmail')->willReturn(false);
         $user->expects($this->once())->method('sendEmailVerificationNotification');
-
-        $listener = new SendEmailVerificationNotification;
-
-        $listener->handle(new Registered($user));
-    }
-
-    /**
-     * @return void
-     */
-    public function testUserIsNotInstanceOfMustVerifyEmail()
-    {
-        $user = $this->getMockBuilder(User::class)->getMock();
-        $user->expects($this->never())->method('sendEmailVerificationNotification');
 
         $listener = new SendEmailVerificationNotification;
 
