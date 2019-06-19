bb2a0afef35f508f77c049c51f04087a14de0055
Taylor Otwell <taylor@laravel.com>
2019-06-19 11:40:24 -0500

    wip

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -29,7 +29,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      *
      * @var string
      */
-    const VERSION = '5.8.23';
+    const VERSION = '5.8.24';
 
     /**
      * The base path for the Laravel installation.
