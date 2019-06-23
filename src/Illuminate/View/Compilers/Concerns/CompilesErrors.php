ba1af26933f16964331409644bcfe0dfca8f13ae
therour <nizomsidiq@gmail.com>
2019-06-23 17:58:02 +0700

    Add error directive parameter for defining bag

--- src/Illuminate/View/Compilers/Concerns/CompilesErrors.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesErrors.php
@@ -12,11 +12,13 @@ trait CompilesErrors
      */
     protected function compileError($expression)
     {
-        $expression = $this->stripParentheses($expression);
+        $expression = explode(',', $this->stripParentheses($expression));
+        $bag = Arr::get($expression, 0, 'default');
+        $attribute = trim(Arr::get($expression, 1, $expression));
 
-        return '<?php if ($errors->has('.$expression.')) :
+        return '<?php if ($errors->getBag('.$bag.')->has('.$attribute.')) :
 if (isset($message)) { $messageCache = $message; }
-$message = $errors->first('.$expression.'); ?>';
+$message = $errors->getBag('.$bag.')->first('.$attribute.'); ?>';
     }
 
     /**
