1090bbb76c1c9b8bc12cb63c2f5ae00526204b91
Bill Yanelli <bill.yanelli@gmail.com>
2019-06-23 06:20:29 -0400

    Refactor repetitive variable in test

--- tests/Foundation/FoundationInteractsWithCollectionsTest.php
+++ tests/Foundation/FoundationInteractsWithCollectionsTest.php
@@ -2,6 +2,7 @@
 
 namespace Illuminate\Tests\Foundation;
 
+use Illuminate\Support\Collection;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\ExpectationFailedException;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithCollections;
@@ -10,67 +11,61 @@ class FoundationInteractsWithCollectionsTest extends TestCase
 {
     use InteractsWithCollections;
 
+    /**
+     * @var Collection
+     */
+    protected $collection;
+
+    protected function setUp(): void
+    {
+        $this->collection = collect([1, 2, 3]);
+    }
+
     public function testSeeInCollectionFindsResults()
     {
-        $collection = collect([1, 2, 3]);
-
-        $this->assertCollectionHas($collection, 3);
+        $this->assertCollectionHas($this->collection, 3);
     }
 
     public function testSeeInCollectionDoesNotFindResults()
     {
         $this->expectException(ExpectationFailedException::class);
 
-        $collection = collect([1, 2, 3]);
-
-        $this->assertCollectionHas($collection, 4);
+        $this->assertCollectionHas($this->collection, 4);
     }
 
     public function testDontSeeInCollectionDoesNotFindResults()
     {
-        $collection = collect([1, 2, 3]);
-
-        $this->assertCollectionMissing($collection, 4);
+        $this->assertCollectionMissing($this->collection, 4);
     }
 
     public function testDontSeeInCollectionFindsResults()
     {
         $this->expectException(ExpectationFailedException::class);
 
-        $collection = collect([1, 2, 3]);
-
-        $this->assertCollectionMissing($collection, 3);
+        $this->assertCollectionMissing($this->collection, 3);
     }
 
     public function testSeeInCollectionStrictFindsResults()
     {
-        $collection = collect([1, 2, 3]);
-
-        $this->assertCollectionHasStrict($collection, 3);
+        $this->assertCollectionHasStrict($this->collection, 3);
     }
 
     public function testSeeInCollectionStrictDoesNotFindResults()
     {
         $this->expectException(ExpectationFailedException::class);
 
-        $collection = collect([1, 2, 3]);
-
-        $this->assertCollectionHasStrict($collection, '3');
+        $this->assertCollectionHasStrict($this->collection, '3');
     }
 
     public function testDontSeeInCollectionStrictDoesNotFindResults()
     {
-        $collection = collect([1, 2, 3]);
-
-        $this->assertCollectionMissingStrict($collection, '3');
+        $this->assertCollectionMissingStrict($this->collection, '3');
     }
 
     public function testDontSeeInCollectionStrictFindsResults()
     {
         $this->expectException(ExpectationFailedException::class);
 
-        $collection = collect([1, 2, 3]);
-
-        $this->assertCollectionMissingStrict($collection, 3);
+        $this->assertCollectionMissingStrict($this->collection, 3);
     }
 }
