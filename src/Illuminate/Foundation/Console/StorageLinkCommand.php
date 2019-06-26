63b1cb260b7268776444c4823ab88f663a86dad9
Christian Rishøj <christian@rishoj.net>
2019-06-26 17:37:56 +0700

    optionally replace existing link

--- src/Illuminate/Foundation/Console/StorageLinkCommand.php
+++ src/Illuminate/Foundation/Console/StorageLinkCommand.php
@@ -11,7 +11,9 @@ class StorageLinkCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'storage:link';
+    protected $signature = 'storage:link
+                    {--absolute : Use an absolute pathname in the link (default is relative)}
+                    {--force : Overwrite existing link}';
 
     /**
      * The console command description.
@@ -27,12 +29,23 @@ class StorageLinkCommand extends Command
      */
     public function handle()
     {
-        if (file_exists(public_path('storage'))) {
-            return $this->error('The "public/storage" directory already exists.');
+        $publicPath = public_path('storage');
+
+        // For broken symlinks, `file_exists()` returns `false`, but `is_link()` returns `true`
+        if (is_link($publicPath) || file_exists($publicPath)) {
+            if (! $this->option('force')) {
+                return $this->error('The "public/storage" directory already exists.');
+            }
+
+            if (!unlink($publicPath)) {
+                return $this->error('Failed to remove existing "public/storage" directory.');
+            }
+
+            $this->warn('Removed existing "public/storage" directory.');
         }
 
         $this->laravel->make('files')->link(
-            storage_path('app/public'), public_path('storage')
+            storage_path('app/public'), $publicPath
         );
 
         $this->info('The [public/storage] directory has been linked.');
