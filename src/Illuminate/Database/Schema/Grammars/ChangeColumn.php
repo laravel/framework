38e1ee13536132fffc814d353ab04ec51ec102f6
Juup Schram <zakelijk@juup.org>
2019-06-27 22:59:36 +0200

    Make sure changing a database field to binary does not include collation
    This PR will fix an issue very close to https://github.com/laravel/framework/issues/25735#issuecomment-423773584. It will fix pretty much the same error but then when changing a text field in to a binary.

--- src/Illuminate/Database/Schema/Grammars/ChangeColumn.php
+++ src/Illuminate/Database/Schema/Grammars/ChangeColumn.php
@@ -121,7 +121,7 @@ class ChangeColumn
             $options['length'] = static::calculateDoctrineTextLength($fluent['type']);
         }
 
-        if ($fluent['type'] === 'json') {
+        if ($fluent['type'] === 'json' || $fluent['type'] === 'binary') {
             $options['customSchemaOptions'] = [
                 'collation' => '',
             ];
