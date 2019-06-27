1b55b289788d5c49187481e421d949fe409a27c1
Taylor Otwell <taylor@laravel.com>
2019-06-27 08:07:16 -0500

    formatting

--- src/Illuminate/Database/Connection.php
+++ src/Illuminate/Database/Connection.php
@@ -901,10 +901,7 @@ class Connection implements ConnectionInterface
                 'pdo' => $this->getPdo(),
                 'dbname' => $this->getConfig('database'),
                 'driver' => $driver->getName(),
-                // One can manually pass the serverVersion config to the database connection
-                // options. This is used by Doctrine instead of going through the regular
-                // regular platform version detection. This config was not being used.
-                'serverVersion' => $this->getConfig('serverVersion'),
+                'serverVersion' => $this->getConfig('server_version'),
             ]), $driver);
         }
 
