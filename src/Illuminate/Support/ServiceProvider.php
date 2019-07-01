780231e3a0c684370a77f3b64b447c5297c116b2
Taylor Otwell <taylor@laravel.com>
2019-07-01 09:23:22 -0500

    formatting

--- src/Illuminate/Support/ServiceProvider.php
+++ src/Illuminate/Support/ServiceProvider.php
@@ -59,9 +59,9 @@ abstract class ServiceProvider
     protected function mergeConfigFrom($path, $key)
     {
         if (! $this->app->configurationIsCached()) {
-            $config = $this->app['config']->get($key, []);
-
-            $this->app['config']->set($key, array_merge(require $path, $config));
+            $this->app['config']->set($key, array_merge(
+                require $path, $this->app['config']->get($key, [])
+            ));
         }
     }
 
