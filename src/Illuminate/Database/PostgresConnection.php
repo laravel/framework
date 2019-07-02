af0fec9c15d5d7a2093c8dfafea751bfce02ed89
exy <>
2019-07-02 13:48:43 +0200

    fix issue on postgresql boolean type

--- src/Illuminate/Database/PostgresConnection.php
+++ src/Illuminate/Database/PostgresConnection.php
@@ -2,11 +2,13 @@
 
 namespace Illuminate\Database;
 
+use DateTimeInterface;
 use Illuminate\Database\Schema\PostgresBuilder;
 use Doctrine\DBAL\Driver\PDOPgSql\Driver as DoctrineDriver;
 use Illuminate\Database\Query\Processors\PostgresProcessor;
 use Illuminate\Database\Query\Grammars\PostgresGrammar as QueryGrammar;
 use Illuminate\Database\Schema\Grammars\PostgresGrammar as SchemaGrammar;
+use PDO;
 
 class PostgresConnection extends Connection
 {
@@ -62,5 +64,53 @@ class PostgresConnection extends Connection
     protected function getDoctrineDriver()
     {
         return new DoctrineDriver;
+    }
+
+    /**
+     * Bind values to their parameters in the given statement.
+     *
+     * @param  \PDOStatement $statement
+     * @param  array  $bindings
+     * @return void
+     */
+    public function bindValues($statement, $bindings)
+    {
+        foreach ($bindings as $key => $value) {
+            $type = PDO::PARAM_STR;
+            if (is_int($value)) {
+                $type = PDO::PARAM_INT;
+            } elseif (is_null($value)) {
+                $type = PDO::PARAM_NULL;
+            } elseif (is_bool($value)) {
+                $type = PDO::PARAM_BOOL;
+            }
+            $statement->bindValue(is_string($key) ? $key : $key + 1, $value, $type);
+        }
+    }
+
+    /**
+     * Prepare the query bindings for execution.
+     *
+     * @param  array  $bindings
+     * @return array
+     */
+    public function prepareBindings(array $bindings)
+    {
+        $grammar = $this->getQueryGrammar();
+
+        foreach ($bindings as $key => $value) {
+            // We need to transform all instances of DateTimeInterface into the actual
+            // date string. Each query grammar maintains its own date string format
+            // so we'll just ask the grammar for the format to get from the date.
+            if ($value instanceof DateTimeInterface) {
+                $bindings[$key] = $value->format($grammar->getDateFormat());
+            } elseif(is_null($value)) {
+                $bindings[$key] = null;
+            } else {
+                $bindings[$key] = $value;
+            }
+        }
+
+        return $bindings;
     }
 }
