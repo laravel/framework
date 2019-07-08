ae60b2131bb9437557e237b42b737145bd6cdc33
Kirill Nesmeyanov <nesk@xakep.ru>
2019-07-08 19:17:15 +0300

    Adds a check of all admissible `Accept` headers

--- src/Illuminate/Http/Concerns/InteractsWithContentTypes.php
+++ src/Illuminate/Http/Concerns/InteractsWithContentTypes.php
@@ -53,7 +53,13 @@ trait InteractsWithContentTypes
     {
         $acceptable = $this->getAcceptableContentTypes();
 
-        return isset($acceptable[0]) && Str::contains($acceptable[0], ['/json', '+json']);
+        foreach ($acceptable as $type) {
+            if (Str::contains($type, ['/json', '+json'])) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
