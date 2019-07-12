251cff49f30de757930a7a16babf38ba53c5a395
imanghafoori <imanghafoori1@gmail.com>
2019-07-13 02:09:00 +0430

    Added tests for alias loader

--- tests/Foundation/FoundationAliasLoaderTest.php
+++ tests/Foundation/FoundationAliasLoaderTest.php
@@ -36,4 +36,46 @@ class FoundationAliasLoaderTest extends TestCase
         $loader = AliasLoader::getInstance(['foo' => 'baz']);
         $this->assertEquals(['foo2' => 'bar2', 'foo' => 'baz'], $loader->getAliases());
     }
+
+    public function testLoaderCanAliasAndLoadClasses()
+    {
+        $loader = AliasLoader::getInstance(['foo' => FoundationAliasLoaderStub::class]);
+
+        try {
+            new \foo;
+        } catch (\Error $e) {
+            $this->assertEquals("Class 'foo' not found", $e->getMessage());
+        }
+
+        $result = $loader->load('foo');
+
+        $this->assertInstanceOf(FoundationAliasLoaderStub::class, new \foo);
+        $this->assertTrue($result);
+
+        $result2 = $loader->load('bar');
+        $this->assertNull($result2);
+    }
+
+    public function testSetAlias()
+    {
+        $loader = AliasLoader::getInstance();
+        $loader->setAliases(['foo' => FoundationAliasLoaderStub::class]);
+
+        try {
+            new \foo;
+        } catch (\Error $e) {
+            $this->assertEquals("Class 'foo' not found", $e->getMessage());
+        }
+
+        $result = $loader->load('foo');
+
+        $fooObj = new \foo;
+        $this->assertInstanceOf(FoundationAliasLoaderStub::class, $fooObj);
+        $this->assertTrue($result);
+    }
+}
+
+class FoundationAliasLoaderStub
+{
+    //
 }
