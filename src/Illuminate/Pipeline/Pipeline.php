9ee4095f603a918f155b607e67446bf9c16221dd
Florent Poujol <florentpoujol@fastmail.com>
2019-07-21 20:50:19 +0200

    Wrap the middleware and the destination in a try/catch block in the base Pipeline class, but do not handle the Exception there. This allows the Routing middleware to still properly handle the exception but not wrap closure in more closure, lessening the number of lines in the stacktrace by 1 per middleware.

--- src/Illuminate/Pipeline/Pipeline.php
+++ src/Illuminate/Pipeline/Pipeline.php
@@ -3,10 +3,13 @@
 namespace Illuminate\Pipeline;
 
 use Closure;
+use Exception;
+use Throwable;
 use RuntimeException;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Support\Responsable;
+use Symfony\Component\Debug\Exception\FatalThrowableError;
 use Illuminate\Contracts\Pipeline\Pipeline as PipelineContract;
 
 class Pipeline implements PipelineContract
@@ -125,7 +128,13 @@ class Pipeline implements PipelineContract
     protected function prepareDestination(Closure $destination)
     {
         return function ($passable) use ($destination) {
-            return $destination($passable);
+            try {
+                return $destination($passable);
+            } catch (Exception $e) {
+                return $this->handleException($passable, $e);
+            } catch (Throwable $e) {
+                return $this->handleException($passable, new FatalThrowableError($e));
+            }
         };
     }
 
@@ -138,34 +147,40 @@ class Pipeline implements PipelineContract
     {
         return function ($stack, $pipe) {
             return function ($passable) use ($stack, $pipe) {
-                if (is_callable($pipe)) {
-                    // If the pipe is an instance of a Closure, we will just call it directly but
-                    // otherwise we'll resolve the pipes out of the container and call it with
-                    // the appropriate method and arguments, returning the results back out.
-                    return $pipe($passable, $stack);
-                } elseif (! is_object($pipe)) {
-                    [$name, $parameters] = $this->parsePipeString($pipe);
+                try {
+                    if (is_callable($pipe)) {
+                        // If the pipe is an instance of a Closure, we will just call it directly but
+                        // otherwise we'll resolve the pipes out of the container and call it with
+                        // the appropriate method and arguments, returning the results back out.
+                        return $pipe($passable, $stack);
+                    } elseif (! is_object($pipe)) {
+                        [$name, $parameters] = $this->parsePipeString($pipe);
 
-                    // If the pipe is a string we will parse the string and resolve the class out
-                    // of the dependency injection container. We can then build a callable and
-                    // execute the pipe function giving in the parameters that are required.
-                    $pipe = $this->getContainer()->make($name);
+                        // If the pipe is a string we will parse the string and resolve the class out
+                        // of the dependency injection container. We can then build a callable and
+                        // execute the pipe function giving in the parameters that are required.
+                        $pipe = $this->getContainer()->make($name);
 
-                    $parameters = array_merge([$passable, $stack], $parameters);
-                } else {
-                    // If the pipe is already an object we'll just make a callable and pass it to
-                    // the pipe as-is. There is no need to do any extra parsing and formatting
-                    // since the object we're given was already a fully instantiated object.
-                    $parameters = [$passable, $stack];
+                        $parameters = array_merge([$passable, $stack], $parameters);
+                    } else {
+                        // If the pipe is already an object we'll just make a callable and pass it to
+                        // the pipe as-is. There is no need to do any extra parsing and formatting
+                        // since the object we're given was already a fully instantiated object.
+                        $parameters = [$passable, $stack];
+                    }
+
+                    $response = method_exists($pipe, $this->method)
+                                    ? $pipe->{$this->method}(...$parameters)
+                                    : $pipe(...$parameters);
+
+                    return $response instanceof Responsable
+                                ? $response->toResponse($this->getContainer()->make(Request::class))
+                                : $response;
+                } catch (Exception $e) {
+                    return $this->handleException($passable, $e);
+                } catch (Throwable $e) {
+                    return $this->handleException($passable, new FatalThrowableError($e));
                 }
-
-                $response = method_exists($pipe, $this->method)
-                                ? $pipe->{$this->method}(...$parameters)
-                                : $pipe(...$parameters);
-
-                return $response instanceof Responsable
-                            ? $response->toResponse($this->getContainer()->make(Request::class))
-                            : $response;
             };
         };
     }
@@ -201,5 +216,19 @@ class Pipeline implements PipelineContract
         }
 
         return $this->container;
+    }
+
+    /**
+     * Handle the given exception.
+     *
+     * @param  mixed  $passable
+     * @param  \Exception  $e
+     * @return mixed
+     *
+     * @throws \Exception
+     */
+    protected function handleException($passable, Exception $e)
+    {
+        throw $e; // actually handled in the Routing Pipeline
     }
 }
