4a67b110f1489c0ba725b97bc58a188e1a198e5c
Jonas Staudenmeir <mail@jonas-staudenmeir.de>
2019-07-23 01:17:48 +0200

    Remove BelongsTo::update()

--- src/Illuminate/Database/Eloquent/Relations/BelongsTo.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsTo.php
@@ -194,17 +194,6 @@ class BelongsTo extends Relation
     }
 
     /**
-     * Update the parent model on the relationship.
-     *
-     * @param  array  $attributes
-     * @return mixed
-     */
-    public function update(array $attributes)
-    {
-        return $this->getResults()->fill($attributes)->save();
-    }
-
-    /**
      * Associate the model instance to the given parent.
      *
      * @param  \Illuminate\Database\Eloquent\Model|int|string  $model
