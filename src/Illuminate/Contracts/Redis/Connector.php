847b196c7bf3cadae09ce9ebb6d4a2a4664518d2
paulhenri-l <25308170+paulhenri-l@users.noreply.github.com>
2019-07-24 00:11:41 +0200

    Add support for custom drivers
    This portion of code is heavily inspired by the CacheManager extension system.

---
+++ src/Illuminate/Contracts/Redis/Connector.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Illuminate\Contracts\Redis;
+
+interface Connector
+{
+    /**
+     * Create a new clustered Predis connection.
+     *
+     * @param  array  $config
+     * @param  array  $options
+     * @return \Illuminate\Contracts\Redis\Connection
+     */
+    public function connect(array $config, array $options);
+
+    /**
+     * Create a new clustered Predis connection.
+     *
+     * @param  array  $config
+     * @param  array  $clusterOptions
+     * @param  array  $options
+     * @return \Illuminate\Contracts\Redis\Connection
+     */
+    public function connectToCluster(array $config, array $clusterOptions, array $options);
+}
