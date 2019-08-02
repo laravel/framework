86377ecb513b7e508058d0187d98463171837c79
moonhyuk <godmoon00@naver.com>
2019-08-02 11:58:55 +0900

    Fix assertJsonValidationErrors with muliple messages

--- src/Illuminate/Foundation/Testing/TestResponse.php
+++ src/Illuminate/Foundation/Testing/TestResponse.php
@@ -675,15 +675,18 @@ class TestResponse
             );
 
             if (! is_int($key)) {
+                $hasError = false;
                 foreach (Arr::wrap($jsonErrors[$key]) as $jsonErrorMessage) {
                     if (Str::contains($jsonErrorMessage, $value)) {
-                        return $this;
+                        $hasError = true;
                     }
                 }
 
-                PHPUnit::fail(
-                    "Failed to find a validation error in the response for key and message: '$key' => '$value'".PHP_EOL.PHP_EOL.$errorMessage
-                );
+                if (!$hasError) {
+                    PHPUnit::fail(
+                        "Failed to find a validation error in the response for key and message: '$key' => '$value'" . PHP_EOL . PHP_EOL . $errorMessage
+                    );
+                }
             }
         }
 
