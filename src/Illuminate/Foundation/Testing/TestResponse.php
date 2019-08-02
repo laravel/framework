9d8c11d5190e81b879f1b2bfdb4f4acfe9d94b88
moonhyuk <godmoon00@naver.com>
2019-08-02 13:11:04 +0900

    Fix coding style

--- src/Illuminate/Foundation/Testing/TestResponse.php
+++ src/Illuminate/Foundation/Testing/TestResponse.php
@@ -682,9 +682,9 @@ class TestResponse
                     }
                 }
 
-                if (!$hasError) {
+                if (! $hasError) {
                     PHPUnit::fail(
-                        "Failed to find a validation error in the response for key and message: '$key' => '$value'" . PHP_EOL . PHP_EOL . $errorMessage
+                        "Failed to find a validation error in the response for key and message: '$key' => '$value'".PHP_EOL.PHP_EOL.$errorMessage
                     );
                 }
             }
