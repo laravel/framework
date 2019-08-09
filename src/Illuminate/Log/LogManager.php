5b752e88c4613cf481a75499bd7645e21c5c3a6f
Ma <mx.ma@qq.com>
2019-08-09 12:24:32 +0800

    Remove unnecessary environment detection

--- src/Illuminate/Log/LogManager.php
+++ src/Illuminate/Log/LogManager.php
@@ -393,7 +393,7 @@ class LogManager implements LoggerInterface
      */
     protected function getFallbackChannelName()
     {
-        return $this->app->bound('env') ? $this->app->environment() : 'production';
+        return $this->app->environment();
     }
 
     /**
