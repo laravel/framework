92b672354f1712809d54c07197a40b7e722b954b
Dani <dkralj@gmail.com>
2019-08-10 23:45:36 +0200

    Make it possible to have full classnames in route

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -531,6 +531,7 @@ class Router implements RegistrarContract, BindingRegistrar
         $group = end($this->groupStack);
 
         return isset($group['namespace']) && strpos($class, '\\') !== 0
+                && strpos($class, app()->getNamespace()) !== 0
                 ? $group['namespace'].'\\'.$class : $class;
     }
 
--- src/Illuminate/Routing/UrlGenerator.php
+++ src/Illuminate/Routing/UrlGenerator.php
@@ -436,7 +436,7 @@ class UrlGenerator implements UrlGeneratorContract
             $action = '\\'.implode('@', $action);
         }
 
-        if ($this->rootNamespace && strpos($action, '\\') !== 0) {
+        if ($this->rootNamespace && strpos($action, '\\') !== 0 && strpos($action, app()->getNamespace()) !== 0) {
             return $this->rootNamespace.'\\'.$action;
         } else {
             return trim($action, '\\');
