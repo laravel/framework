cc56530121cdfa8f1a57f38304736900f007223d
Humberto Piaia <betopiaia@gmail.com>
2019-08-13 12:46:10 -0300

    Change react preset example stub to functional component
    Changed the Example.js component in react preset stub to a functional component.

--- src/Illuminate/Foundation/Console/Presets/react-stubs/Example.js
+++ src/Illuminate/Foundation/Console/Presets/react-stubs/Example.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class Example extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
 
-                            <div className="card-body">I'm an example component!</div>
-                        </div>
+                        <div className="card-body">I'm an example component!</div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default Example;
 
 if (document.getElementById('example')) {
     ReactDOM.render(<Example />, document.getElementById('example'));
