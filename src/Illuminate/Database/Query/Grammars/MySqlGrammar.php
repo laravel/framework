54acd275cdfa226d663764a024fb2e98f47ceaf6
Jared Dunham <github@djared.com>
2019-08-14 16:40:38 -0500

    Added missing MySQL compileInsertIgnore function

--- src/Illuminate/Database/Query/Grammars/MySqlGrammar.php
+++ src/Illuminate/Database/Query/Grammars/MySqlGrammar.php
@@ -53,7 +53,37 @@ class MySqlGrammar extends Grammar
 
         return $sql;
     }
-
+    
+    /**
+     * Compile an insert ignore statement into SQL.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  array  $values
+     * @return string
+     */
+    public function compileInsertIgnore(Builder $query, array $values)
+    {
+        // Essentially we will force every insert to be treated as a batch insert which
+        // simply makes creating the SQL easier for us since we can utilize the same
+        // basic routine regardless of an amount of records given to us to insert.
+        $table = $this->wrapTable($query->from);
+        
+        if (! is_array(reset($values))) {
+            $values = [$values];
+        }
+        
+        $columns = $this->columnize(array_keys(reset($values)));
+        
+        // We need to build a list of parameter place-holders of values that are bound
+        // to the query. Each insert should have the exact same amount of parameter
+        // bindings so we will loop through the record and parameterize them all.
+        $parameters = collect($values)->map(function ($record) {
+            return '('.$this->parameterize($record).')';
+        })->implode(', ');
+        
+        return "insert ignore into $table ($columns) values $parameters";
+    }
+    
     /**
      * Compile a "JSON contains" statement into SQL.
      *
