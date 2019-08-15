f1173111ee42c1585c60508093a653d1295c35f7
Jared Dunham <github@djared.com>
2019-08-14 20:55:56 -0500

    Reduced duplicated code

--- src/Illuminate/Database/Query/Grammars/PostgresGrammar.php
+++ src/Illuminate/Database/Query/Grammars/PostgresGrammar.php
@@ -204,25 +204,7 @@ class PostgresGrammar extends Grammar
      */
     public function compileInsertIgnore(Builder $query, array $values)
     {
-        // Essentially we will force every insert to be treated as a batch insert which
-        // simply makes creating the SQL easier for us since we can utilize the same
-        // basic routine regardless of an amount of records given to us to insert.
-        $table = $this->wrapTable($query->from);
-        
-        if (! is_array(reset($values))) {
-            $values = [$values];
-        }
-        
-        $columns = $this->columnize(array_keys(reset($values)));
-        
-        // We need to build a list of parameter place-holders of values that are bound
-        // to the query. Each insert should have the exact same amount of parameter
-        // bindings so we will loop through the record and parameterize them all.
-        $parameters = collect($values)->map(function ($record) {
-            return '('.$this->parameterize($record).')';
-        })->implode(', ');
-        
-        return "insert into $table ($columns) values $parameters on conflict do nothing";
+        return $this->compileInsert($query, $values).' on conflict do nothing';
     }
 
     /**
--- src/Illuminate/Database/Query/Grammars/SQLiteGrammar.php
+++ src/Illuminate/Database/Query/Grammars/SQLiteGrammar.php
@@ -187,25 +187,7 @@ class SQLiteGrammar extends Grammar
      */
     public function compileInsertIgnore(Builder $query, array $values)
     {
-        // Essentially we will force every insert to be treated as a batch insert which
-        // simply makes creating the SQL easier for us since we can utilize the same
-        // basic routine regardless of an amount of records given to us to insert.
-        $table = $this->wrapTable($query->from);
-        
-        if (! is_array(reset($values))) {
-            $values = [$values];
-        }
-        
-        $columns = $this->columnize(array_keys(reset($values)));
-        
-        // We need to build a list of parameter place-holders of values that are bound
-        // to the query. Each insert should have the exact same amount of parameter
-        // bindings so we will loop through the record and parameterize them all.
-        $parameters = collect($values)->map(function ($record) {
-            return '('.$this->parameterize($record).')';
-        })->implode(', ');
-        
-        return "insert or ignore into $table ($columns) values $parameters";
+        return substr_replace($this->compileInsert($query, $values),'insert or ignore', 0, 6);
     }
     
     /**
