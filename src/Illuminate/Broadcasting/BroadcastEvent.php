06bf064ad7962f34559d25f59d87d9e4fcb9263a
Connor Tumbleson <connor.tumbleson@gmail.com>
2019-08-20 07:45:30 -0400

    add properties (tries/timeout) to the BroadcastEvent

--- src/Illuminate/Broadcasting/BroadcastEvent.php
+++ src/Illuminate/Broadcasting/BroadcastEvent.php
@@ -22,6 +22,20 @@ class BroadcastEvent implements ShouldQueue
     public $event;
 
     /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries;
+
+    /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
+    public $timeout;
+
+    /**
      * Create a new job handler instance.
      *
      * @param  mixed  $event
