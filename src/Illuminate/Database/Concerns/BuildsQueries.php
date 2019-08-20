0b0850312ebbb80265a7fccd1eeeb4859e8a941e
Thomas Mason <thomasm789@users.noreply.github.com>
2019-08-20 11:23:33 +0100

    Update BuildsQueries.php

--- src/Illuminate/Database/Concerns/BuildsQueries.php
+++ src/Illuminate/Database/Concerns/BuildsQueries.php
@@ -23,7 +23,7 @@ trait BuildsQueries
         }
         
         $page = 1;
-
+        
         do {
             // We'll execute the query for the given page and get the results. If there are
             // no results we can just break and return from here. When there are results
