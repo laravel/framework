6524a5d7e97863d6335e75e73cbd50ca6c59122a
me-shaon <shaon.cse81@gmail.com>
2019-08-25 14:11:40 +0600

    feat: change implementation based on review

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -562,7 +562,6 @@ trait HasAttributes
      */
     public function setAttribute($key, $value)
     {
-        $key = (string) $key;
         // First we will check for the presence of a mutator for the set operation
         // which simply lets the developers tweak the attribute as it is set on
         // the model, such as "json_encoding" an listing of data for storage.
@@ -624,7 +623,7 @@ trait HasAttributes
      */
     protected function isDateAttribute($key)
     {
-        return in_array($key, $this->getDates()) ||
+        return in_array($key, $this->getDates(), true)   ||
                                     $this->isDateCastable($key);
     }
 
