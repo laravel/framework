72bae0f228855095d9b0122cabd51ccd7a345db7
Taylor Otwell <taylor@laravel.com>
2019-08-30 10:57:38 +0200

    formatting

--- src/Illuminate/Routing/RouteUrlGenerator.php
+++ src/Illuminate/Routing/RouteUrlGenerator.php
@@ -197,15 +197,11 @@ class RouteUrlGenerator
         $path = $this->replaceNamedParameters($path, $parameters);
 
         $path = preg_replace_callback('/\{.*?\}/', function ($match) use (&$parameters) {
-            // Reset numeric keys on $parameters so we can
-            // get the first non-associative element
-            // using Arr::pull($parameters, 0)
+            // Reset only the numeric keys...
             $parameters = array_merge($parameters);
 
             return (empty($parameters) && ! Str::endsWith($match[0], '?}'))
                         ? $match[0]
-                        // Use only non-associative elements, so we don't
-                        // replace parameters that were meant for query string.
                         : Arr::pull($parameters, 0);
         }, $path);
 
