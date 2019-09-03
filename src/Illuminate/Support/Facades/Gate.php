139726e392ecb4fc7114fb63f968815c1426d3f9
Hery Vandoro <vandorohery99@gmail.com>
2019-09-04 00:25:40 +0700

    Update Gate facade(inspect) (#29849)

--- src/Illuminate/Support/Facades/Gate.php
+++ src/Illuminate/Support/Facades/Gate.php
@@ -19,6 +19,7 @@ use Illuminate\Contracts\Auth\Access\Gate as GateContract;
  * @method static mixed getPolicyFor(object|string $class)
  * @method static \Illuminate\Contracts\Auth\Access\Gate forUser(\Illuminate\Contracts\Auth\Authenticatable|mixed $user)
  * @method static array abilities()
+ * @method static \Illuminate\Auth\Access\Response inspect(string $ability, array|mixed $arguments = [])
  *
  * @see \Illuminate\Contracts\Auth\Access\Gate
  */
