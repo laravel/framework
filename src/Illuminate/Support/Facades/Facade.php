dbb3b8c7f503f872d63b0fd8aecfcc60b1a19ca8
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-09-03 17:36:47 +0100

    Backport PHP 7.4 Support (#29842)
    Co-authored-by: Dries Vints <dries.vints@gmail.com>

--- src/Illuminate/Support/Facades/Facade.php
+++ src/Illuminate/Support/Facades/Facade.php
@@ -172,7 +172,9 @@ abstract class Facade
             return static::$resolvedInstance[$name];
         }
 
-        return static::$resolvedInstance[$name] = static::$app[$name];
+        if (static::$app) {
+            return static::$resolvedInstance[$name] = static::$app[$name];
+        }
     }
 
     /**
