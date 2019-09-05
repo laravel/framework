f978badd0cc882e17f53bf4dde9f8d729498bb48
Ralph M. Rivera <ralph@rahvalor.com>
2019-09-05 16:35:19 -0500

    verify() accepts key defined in getRouteKeyName()

--- src/Illuminate/Foundation/Auth/VerifiesEmails.php
+++ src/Illuminate/Foundation/Auth/VerifiesEmails.php
@@ -32,7 +32,7 @@ trait VerifiesEmails
      */
     public function verify(Request $request)
     {
-        if (! hash_equals((string) $request->route('id'), (string) $request->user()->getKey())) {
+        if (! hash_equals((string) $request->route('id'), (string) $request->user()->getRouteKey())) {
             throw new AuthorizationException;
         }
 
