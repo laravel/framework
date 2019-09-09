0bc7d177b89b15ef6dbcaf4c233f2c66e5974ca9
Sjors Ottjes <sjorsottjes@gmail.com>
2019-09-09 16:33:49 +0200

    Update InteractsWithContainer.php

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithContainer.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithContainer.php
@@ -38,7 +38,7 @@ trait InteractsWithContainer
      *
      * @param  string  $abstract
      * @param  \Closure|null  $mock
-     * @return Mockery\MockInterface
+     * @return \Mockery\MockInterface
      */
     protected function mock($abstract, Closure $mock = null)
     {
@@ -50,7 +50,7 @@ trait InteractsWithContainer
      *
      * @param  string  $abstract
      * @param  \Closure|null  $mock
-     * @return Mockery\MockInterface
+     * @return \Mockery\MockInterface
      */
     protected function spy($abstract, Closure $mock = null)
     {
