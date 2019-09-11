b59fe219697e8ea4690d71ebf94a33dc62873cbe
netpok <netpok@gmail.com>
2019-09-11 22:47:02 +0200

    Use value helper where possible (#29959)

--- src/Illuminate/Container/BoundMethod.php
+++ src/Illuminate/Container/BoundMethod.php
@@ -75,7 +75,7 @@ class BoundMethod
     protected static function callBoundMethod($container, $callback, $default)
     {
         if (! is_array($callback)) {
-            return $default instanceof Closure ? $default() : $default;
+            return value($default);
         }
 
         // Here we need to turn the array callable into a Class@method string we can use to
@@ -87,7 +87,7 @@ class BoundMethod
             return $container->callMethodBinding($method, $callback[0]);
         }
 
-        return $default instanceof Closure ? $default() : $default;
+        return value($default);
     }
 
     /**
