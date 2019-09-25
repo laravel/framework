b28ef65236b3537a3938780f672acc47385c6811
Christopher Fuchs <christopher.fuchs@ready2order.com>
2019-09-25 20:55:44 +0200

    Added check to not initialize a guard in case of rebinding 'events'
    * as Auth::guard() not only checks for an existing guard but also
    initializes a new guard in case ob absence. This can lead to side-effects
    when overbinding 'events' in another service provider

--- src/Illuminate/Auth/AuthServiceProvider.php
+++ src/Illuminate/Auth/AuthServiceProvider.php
@@ -98,6 +98,13 @@ class AuthServiceProvider extends ServiceProvider
                 return;
             }
 
+            // If no guards have been resolved yet we don't need to call setDispatcher
+            // In case 'events' is rebound in another ServiceProvider this makes sure that no guard is initialized
+            // while registering.
+            if ($app['auth']->hasResolvedGuards() === false) {
+                return;
+            }
+
             if (method_exists($guard = $app['auth']->guard(), 'setDispatcher')) {
                 $guard->setDispatcher($dispatcher);
             }
