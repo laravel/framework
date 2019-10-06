425e34182a8b7e936d2196e3bd6e9d5ec9167729
Farid Inawan <frdteknikelektro@gmail.com>
2019-10-06 21:45:30 +0700

    Use qualifyColumn instead string on BelongsToRelation query

--- src/Illuminate/Database/Eloquent/Relations/BelongsTo.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsTo.php
@@ -94,9 +94,7 @@ class BelongsTo extends Relation
             // For belongs to relationships, which are essentially the inverse of has one
             // or has many relationships, we need to actually query on the primary key
             // of the related models matching on the foreign key that's on a parent.
-            $table = $this->related->getTable();
-
-            $this->query->where($table.'.'.$this->ownerKey, '=', $this->child->{$this->foreignKey});
+            $this->query->where($this->related->qualifyColumn($this->ownerKey), '=', $this->child->{$this->foreignKey});
         }
     }
 
@@ -111,7 +109,7 @@ class BelongsTo extends Relation
         // We'll grab the primary key name of the related models since it could be set to
         // a non-standard name and not "id". We will then construct the constraint for
         // our eagerly loading query so it returns the proper models from execution.
-        $key = $this->related->getTable().'.'.$this->ownerKey;
+        $key = $this->related->qualifyColumn($this->ownerKey);
 
         $whereIn = $this->whereInMethod($this->related, $this->ownerKey);
 
@@ -262,7 +260,7 @@ class BelongsTo extends Relation
         $query->getModel()->setTable($hash);
 
         return $query->whereColumn(
-            $hash.'.'.$this->ownerKey, '=', $this->getQualifiedForeignKeyName()
+            $query->qualifyColumn($this->ownerKey), '=', $this->getQualifiedForeignKeyName()
         );
     }
 
