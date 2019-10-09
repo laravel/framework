149f927bdca0fff3165cca168ae224294a44e167
Andrew Brown <browner12@gmail.com>
2019-10-09 11:24:11 -0500

    add a timeout reset feature
    if the programmer decides to turns this on, the timer will reset every time a route using this middleware is visited and the user has provided their password for heightened security.

--- src/Illuminate/Auth/Middleware/RequirePassword.php
+++ src/Illuminate/Auth/Middleware/RequirePassword.php
@@ -50,6 +50,10 @@ class RequirePassword
                 $this->urlGenerator->route($redirectToRoute ?? 'password.confirm')
             );
         }
+        
+        if (config('auth.password_timeout_reset', false)) {
+            $request->session()->put('auth.password_confirmed_at', time());
+        }
 
         return $next($request);
     }
