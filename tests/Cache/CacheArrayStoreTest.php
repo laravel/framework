d77cacac8b894a2a8674f56f2ea3e82a9bd33725
nexxai <nexxai@gmail.com>
2019-10-15 08:37:04 -0600

    Fix typos in test names

--- tests/Cache/CacheArrayStoreTest.php
+++ tests/Cache/CacheArrayStoreTest.php
@@ -2,183 +2,183 @@
 
 namespace Illuminate\Tests\Cache;
 
-use Illuminate\Cache\ArrayStore;
 use Illuminate\Support\Carbon;
 use PHPUnit\Framework\TestCase;
+use Illuminate\Cache\ArrayStore;
 
 class CacheArrayStoreTest extends TestCase
 {
-    public function testItemsCanBeSetAndRetrieved()
-    {
-        $store = new ArrayStore;
-        $result = $store->put('foo', 'bar', 10);
-        $this->assertTrue($result);
-        $this->assertSame('bar', $store->get('foo'));
-    }
+	public function testItemsCanBeSetAndRetrieved()
+	{
+		$store = new ArrayStore();
+		$result = $store->put('foo', 'bar', 10);
+		$this->assertTrue($result);
+		$this->assertSame('bar', $store->get('foo'));
+	}
 
-    public function testMultipleItemsCanBeSetAndRetrieved()
-    {
-        $store = new ArrayStore;
-        $result = $store->put('foo', 'bar', 10);
-        $resultMany = $store->putMany([
-            'fizz'  => 'buz',
-            'quz'   => 'baz',
-        ], 10);
-        $this->assertTrue($result);
-        $this->assertTrue($resultMany);
-        $this->assertEquals([
-            'foo'   => 'bar',
-            'fizz'  => 'buz',
-            'quz'   => 'baz',
-            'norf'  => null,
-        ], $store->many(['foo', 'fizz', 'quz', 'norf']));
-    }
+	public function testMultipleItemsCanBeSetAndRetrieved()
+	{
+		$store = new ArrayStore();
+		$result = $store->put('foo', 'bar', 10);
+		$resultMany = $store->putMany([
+			'fizz' => 'buz',
+			'quz' => 'baz',
+		], 10);
+		$this->assertTrue($result);
+		$this->assertTrue($resultMany);
+		$this->assertEquals([
+			'foo' => 'bar',
+			'fizz' => 'buz',
+			'quz' => 'baz',
+			'norf' => null,
+		], $store->many(['foo', 'fizz', 'quz', 'norf']));
+	}
 
-    public function testItemsCanExpire(): void
-    {
-        Carbon::setTestNow(Carbon::now());
-        $store = new ArrayStore;
+	public function testItemsCanExpire(): void
+	{
+		Carbon::setTestNow(Carbon::now());
+		$store = new ArrayStore();
 
-        $store->put('foo', 'bar', 10);
-        Carbon::setTestNow(Carbon::now()->addSeconds(10)->addSecond());
-        $result = $store->get('foo');
+		$store->put('foo', 'bar', 10);
+		Carbon::setTestNow(Carbon::now()->addSeconds(10)->addSecond());
+		$result = $store->get('foo');
 
-        $this->assertNull($result);
-        Carbon::setTestNow(null);
-    }
+		$this->assertNull($result);
+		Carbon::setTestNow(null);
+	}
 
-    public function testStoreItemForeverProperlyStoresInArray()
-    {
-        $mock = $this->getMockBuilder(ArrayStore::class)->setMethods(['put'])->getMock();
-        $mock->expects($this->once())
-            ->method('put')->with($this->equalTo('foo'), $this->equalTo('bar'), $this->equalTo(0))
-            ->willReturn(true);
-        $result = $mock->forever('foo', 'bar');
-        $this->assertTrue($result);
-    }
+	public function testStoreItemForeverProperlyStoresInArray()
+	{
+		$mock = $this->getMockBuilder(ArrayStore::class)->setMethods(['put'])->getMock();
+		$mock->expects($this->once())
+			->method('put')->with($this->equalTo('foo'), $this->equalTo('bar'), $this->equalTo(0))
+			->willReturn(true);
+		$result = $mock->forever('foo', 'bar');
+		$this->assertTrue($result);
+	}
 
-    public function testValuesCanBeIncremented()
-    {
-        $store = new ArrayStore;
-        $store->put('foo', 1, 10);
-        $result = $store->increment('foo');
-        $this->assertEquals(2, $result);
-        $this->assertEquals(2, $store->get('foo'));
-    }
+	public function testValuesCanBeIncremented()
+	{
+		$store = new ArrayStore();
+		$store->put('foo', 1, 10);
+		$result = $store->increment('foo');
+		$this->assertEquals(2, $result);
+		$this->assertEquals(2, $store->get('foo'));
+	}
 
-    public function testNonExistingKeysCanBeIncremented()
-    {
-        $store = new ArrayStore;
-        $result = $store->increment('foo');
-        $this->assertEquals(1, $result);
-        $this->assertEquals(1, $store->get('foo'));
-    }
+	public function testNonExistingKeysCanBeIncremented()
+	{
+		$store = new ArrayStore();
+		$result = $store->increment('foo');
+		$this->assertEquals(1, $result);
+		$this->assertEquals(1, $store->get('foo'));
+	}
 
-    public function testValuesCanBeDecremented()
-    {
-        $store = new ArrayStore;
-        $store->put('foo', 1, 10);
-        $result = $store->decrement('foo');
-        $this->assertEquals(0, $result);
-        $this->assertEquals(0, $store->get('foo'));
-    }
+	public function testValuesCanBeDecremented()
+	{
+		$store = new ArrayStore();
+		$store->put('foo', 1, 10);
+		$result = $store->decrement('foo');
+		$this->assertEquals(0, $result);
+		$this->assertEquals(0, $store->get('foo'));
+	}
 
-    public function testItemsCanBeRemoved()
-    {
-        $store = new ArrayStore;
-        $store->put('foo', 'bar', 10);
-        $this->assertTrue($store->forget('foo'));
-        $this->assertNull($store->get('foo'));
-        $this->assertFalse($store->forget('foo'));
-    }
+	public function testItemsCanBeRemoved()
+	{
+		$store = new ArrayStore();
+		$store->put('foo', 'bar', 10);
+		$this->assertTrue($store->forget('foo'));
+		$this->assertNull($store->get('foo'));
+		$this->assertFalse($store->forget('foo'));
+	}
 
-    public function testItemsCanBeFlushed()
-    {
-        $store = new ArrayStore;
-        $store->put('foo', 'bar', 10);
-        $store->put('baz', 'boom', 10);
-        $result = $store->flush();
-        $this->assertTrue($result);
-        $this->assertNull($store->get('foo'));
-        $this->assertNull($store->get('baz'));
-    }
+	public function testItemsCanBeFlushed()
+	{
+		$store = new ArrayStore();
+		$store->put('foo', 'bar', 10);
+		$store->put('baz', 'boom', 10);
+		$result = $store->flush();
+		$this->assertTrue($result);
+		$this->assertNull($store->get('foo'));
+		$this->assertNull($store->get('baz'));
+	}
 
-    public function testCacheKey()
-    {
-        $store = new ArrayStore;
-        $this->assertEmpty($store->getPrefix());
-    }
+	public function testCacheKey()
+	{
+		$store = new ArrayStore();
+		$this->assertEmpty($store->getPrefix());
+	}
 
-    public function testCannotAquireLockTwice()
-    {
-        $store = new ArrayStore;
-        $lock = $store->lock('foo', 10);
+	public function testCannotAcquireLockTwice()
+	{
+		$store = new ArrayStore();
+		$lock = $store->lock('foo', 10);
 
-        $this->assertTrue($lock->acquire());
-        $this->assertFalse($lock->acquire());
-    }
+		$this->assertTrue($lock->acquire());
+		$this->assertFalse($lock->acquire());
+	}
 
-    public function testCanAquireLockAgainAfterExpiry()
-    {
-        Carbon::setTestNow(Carbon::now());
-        $store = new ArrayStore;
-        $lock = $store->lock('foo', 10);
-        $lock->acquire();
-        Carbon::setTestNow(Carbon::now()->addSeconds(10));
+	public function testCanAcquireLockAgainAfterExpiry()
+	{
+		Carbon::setTestNow(Carbon::now());
+		$store = new ArrayStore();
+		$lock = $store->lock('foo', 10);
+		$lock->acquire();
+		Carbon::setTestNow(Carbon::now()->addSeconds(10));
 
-        $this->assertTrue($lock->acquire());
-    }
+		$this->assertTrue($lock->acquire());
+	}
 
-    public function testLockExpirationLowerBoundary()
-    {
-        Carbon::setTestNow(Carbon::now());
-        $store = new ArrayStore;
-        $lock = $store->lock('foo', 10);
-        $lock->acquire();
-        Carbon::setTestNow(Carbon::now()->addSeconds(10)->subMicrosecond());
+	public function testLockExpirationLowerBoundary()
+	{
+		Carbon::setTestNow(Carbon::now());
+		$store = new ArrayStore();
+		$lock = $store->lock('foo', 10);
+		$lock->acquire();
+		Carbon::setTestNow(Carbon::now()->addSeconds(10)->subMicrosecond());
 
-        $this->assertFalse($lock->acquire());
-    }
+		$this->assertFalse($lock->acquire());
+	}
 
-    public function testLockWithNoExpirationNeverExpires()
-    {
-        Carbon::setTestNow(Carbon::now());
-        $store = new ArrayStore;
-        $lock = $store->lock('foo');
-        $lock->acquire();
-        Carbon::setTestNow(Carbon::now()->addYears(100));
+	public function testLockWithNoExpirationNeverExpires()
+	{
+		Carbon::setTestNow(Carbon::now());
+		$store = new ArrayStore();
+		$lock = $store->lock('foo');
+		$lock->acquire();
+		Carbon::setTestNow(Carbon::now()->addYears(100));
 
-        $this->assertFalse($lock->acquire());
-    }
+		$this->assertFalse($lock->acquire());
+	}
 
-    public function testCanAcquireLockAfterRelease()
-    {
-        $store = new ArrayStore;
-        $lock = $store->lock('foo', 10);
-        $lock->acquire();
+	public function testCanAcquireLockAfterRelease()
+	{
+		$store = new ArrayStore();
+		$lock = $store->lock('foo', 10);
+		$lock->acquire();
 
-        $this->assertTrue($lock->release());
-        $this->assertTrue($lock->acquire());
-    }
+		$this->assertTrue($lock->release());
+		$this->assertTrue($lock->acquire());
+	}
 
-    public function testAnotherOwnerCannotReleaseLock()
-    {
-        $store = new ArrayStore;
-        $owner = $store->lock('foo', 10);
-        $wannabeOwner = $store->lock('foo', 10);
-        $owner->acquire();
+	public function testAnotherOwnerCannotReleaseLock()
+	{
+		$store = new ArrayStore();
+		$owner = $store->lock('foo', 10);
+		$wannabeOwner = $store->lock('foo', 10);
+		$owner->acquire();
 
-        $this->assertFalse($wannabeOwner->release());
-    }
+		$this->assertFalse($wannabeOwner->release());
+	}
 
-    public function testAnotherOwnerCanForceReleaseALock()
-    {
-        $store = new ArrayStore;
-        $owner = $store->lock('foo', 10);
-        $wannabeOwner = $store->lock('foo', 10);
-        $owner->acquire();
-        $wannabeOwner->forceRelease();
+	public function testAnotherOwnerCanForceReleaseALock()
+	{
+		$store = new ArrayStore();
+		$owner = $store->lock('foo', 10);
+		$wannabeOwner = $store->lock('foo', 10);
+		$owner->acquire();
+		$wannabeOwner->forceRelease();
 
-        $this->assertTrue($wannabeOwner->acquire());
-    }
+		$this->assertTrue($wannabeOwner->acquire());
+	}
 }
--- tests/Integration/Cache/DynamoDbStoreTest.php
+++ tests/Integration/Cache/DynamoDbStoreTest.php
@@ -2,87 +2,86 @@
 
 namespace Illuminate\Tests\Integration\Cache;
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Cache;
 
 /**
  * @group integration
  */
 class DynamoDbStoreTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
+	protected function setUp(): void
+	{
+		parent::setUp();
 
-        if (! isset($_ENV['DYNAMODB_CACHE_TABLE'])) {
-            $this->markTestSkipped('DynamoDB not configured.');
-        }
-    }
+		if (!isset($_ENV['DYNAMODB_CACHE_TABLE'])) {
+			$this->markTestSkipped('DynamoDB not configured.');
+		}
+	}
 
-    public function testItemsCanBeStoredAndRetrieved()
-    {
-        Cache::driver('dynamodb')->put('name', 'Taylor', 10);
-        $this->assertSame('Taylor', Cache::driver('dynamodb')->get('name'));
+	public function testItemsCanBeStoredAndRetrieved()
+	{
+		Cache::driver('dynamodb')->put('name', 'Taylor', 10);
+		$this->assertSame('Taylor', Cache::driver('dynamodb')->get('name'));
 
-        Cache::driver('dynamodb')->put(['name' => 'Abigail', 'age' => 28], 10);
-        $this->assertSame('Abigail', Cache::driver('dynamodb')->get('name'));
-        $this->assertEquals(28, Cache::driver('dynamodb')->get('age'));
+		Cache::driver('dynamodb')->put(['name' => 'Abigail', 'age' => 28], 10);
+		$this->assertSame('Abigail', Cache::driver('dynamodb')->get('name'));
+		$this->assertEquals(28, Cache::driver('dynamodb')->get('age'));
 
-        $this->assertEquals([
-            'name' => 'Abigail',
-            'age' => 28,
-            'height' => null,
-        ], Cache::driver('dynamodb')->many(['name', 'age', 'height']));
+		$this->assertEquals([
+			'name' => 'Abigail',
+			'age' => 28,
+			'height' => null,
+		], Cache::driver('dynamodb')->many(['name', 'age', 'height']));
 
-        Cache::driver('dynamodb')->forget('name');
-        $this->assertNull(Cache::driver('dynamodb')->get('name'));
-    }
+		Cache::driver('dynamodb')->forget('name');
+		$this->assertNull(Cache::driver('dynamodb')->get('name'));
+	}
 
-    public function testItemsCanBeAtomicallyAdded()
-    {
-        $key = Str::random(6);
+	public function testItemsCanBeAtomicallyAdded()
+	{
+		$key = Str::random(6);
 
-        $this->assertTrue(Cache::driver('dynamodb')->add($key, 'Taylor', 10));
-        $this->assertFalse(Cache::driver('dynamodb')->add($key, 'Taylor', 10));
-    }
+		$this->assertTrue(Cache::driver('dynamodb')->add($key, 'Taylor', 10));
+		$this->assertFalse(Cache::driver('dynamodb')->add($key, 'Taylor', 10));
+	}
 
-    public function testItemsCanBeIncrementedAndDecremented()
-    {
-        Cache::driver('dynamodb')->put('counter', 0, 10);
-        Cache::driver('dynamodb')->increment('counter');
-        Cache::driver('dynamodb')->increment('counter', 4);
+	public function testItemsCanBeIncrementedAndDecremented()
+	{
+		Cache::driver('dynamodb')->put('counter', 0, 10);
+		Cache::driver('dynamodb')->increment('counter');
+		Cache::driver('dynamodb')->increment('counter', 4);
 
-        $this->assertEquals(5, Cache::driver('dynamodb')->get('counter'));
+		$this->assertEquals(5, Cache::driver('dynamodb')->get('counter'));
 
-        Cache::driver('dynamodb')->decrement('counter', 5);
-        $this->assertEquals(0, Cache::driver('dynamodb')->get('counter'));
-    }
+		Cache::driver('dynamodb')->decrement('counter', 5);
+		$this->assertEquals(0, Cache::driver('dynamodb')->get('counter'));
+	}
 
-    public function testLocksCanBeAquired()
-    {
-        Cache::driver('dynamodb')->lock('lock', 10)->get(function () {
-            $this->assertFalse(Cache::driver('dynamodb')->lock('lock', 10)->get());
-        });
-    }
+	public function testLocksCanBeAcquired()
+	{
+		Cache::driver('dynamodb')->lock('lock', 10)->get(function () {
+			$this->assertFalse(Cache::driver('dynamodb')->lock('lock', 10)->get());
+		});
+	}
 
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('cache.default', 'dynamodb');
+	/**
+	 * Define environment setup.
+	 *
+	 * @param \Illuminate\Foundation\Application $app
+	 */
+	protected function getEnvironmentSetUp($app)
+	{
+		$app['config']->set('cache.default', 'dynamodb');
 
-        $app['config']->set('cache.stores.dynamodb', [
-            'driver' => 'dynamodb',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => 'us-east-1',
-            'table' => env('DYNAMODB_CACHE_TABLE', 'laravel_test'),
-            'endpoint' => env('DYNAMODB_ENDPOINT'),
-        ]);
-    }
+		$app['config']->set('cache.stores.dynamodb', [
+			'driver' => 'dynamodb',
+			'key' => env('AWS_ACCESS_KEY_ID'),
+			'secret' => env('AWS_SECRET_ACCESS_KEY'),
+			'region' => 'us-east-1',
+			'table' => env('DYNAMODB_CACHE_TABLE', 'laravel_test'),
+			'endpoint' => env('DYNAMODB_ENDPOINT'),
+		]);
+	}
 }
