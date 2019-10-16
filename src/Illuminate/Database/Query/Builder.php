4ff93b75ce172258db08ae2868a49c896a3d542d
Sébastien Nikolaou <info@sebdesign.eu>
2019-10-16 22:12:08 +0300

    Determine if the value is queryable
    This method allows the query builder to check if a value is queryable and can be used to perform subqueries.
    
    Those checks were performed in 4 different places in the query builder so the repetition is now extracted to a single method.

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -228,11 +228,7 @@ class Builder
         $columns = is_array($columns) ? $columns : func_get_args();
 
         foreach ($columns as $as => $column) {
-            if (is_string($as) && (
-                $column instanceof self ||
-                $column instanceof EloquentBuilder ||
-                $column instanceof Closure
-            )) {
+            if (is_string($as) && $this->isQueryable($column)) {
                 $this->selectSub($column, $as);
             } else {
                 $this->columns[] = $column;
@@ -348,6 +344,19 @@ class Builder
     }
 
     /**
+     * Determine if the value is a query builder instance or a closure.
+     *
+     * @param  mixed  $value
+     * @return bool
+     */
+    protected function isQueryable($value)
+    {
+        return $value instanceof self ||
+               $value instanceof EloquentBuilder ||
+               $value instanceof Closure;
+    }
+
+    /**
      * Add a new select column to the query.
      *
      * @param  array|mixed  $column
@@ -358,11 +367,7 @@ class Builder
         $columns = is_array($column) ? $column : func_get_args();
 
         foreach ($columns as $as => $column) {
-            if (is_string($as) && (
-                $column instanceof self ||
-                $column instanceof EloquentBuilder ||
-                $column instanceof Closure
-            )) {
+            if (is_string($as) && $this->isQueryable($column)) {
                 if (is_null($this->columns)) {
                     $this->select($this->from.'.*');
                 }
@@ -403,9 +408,7 @@ class Builder
      */
     public function from($table, $as = null)
     {
-        if ($table instanceof self ||
-            $table instanceof EloquentBuilder ||
-            $table instanceof Closure) {
+        if ($this->isQueryable($table)) {
             return $this->fromSub($table, $as);
         }
 
@@ -890,9 +893,7 @@ class Builder
         // If the value is a query builder instance we will assume the developer wants to
         // look for any values that exists within this given query. So we will add the
         // query accordingly so that this query is properly executed when it is run.
-        if ($values instanceof self ||
-            $values instanceof EloquentBuilder ||
-            $values instanceof Closure) {
+        if ($this->isQueryable($values)) {
             [$query, $bindings] = $this->createSub($values);
 
             $values = [new Expression($query)];
@@ -1806,9 +1807,7 @@ class Builder
      */
     public function orderBy($column, $direction = 'asc')
     {
-        if ($column instanceof self ||
-            $column instanceof EloquentBuilder ||
-            $column instanceof Closure) {
+        if ($this->isQueryable($column)) {
             [$query, $bindings] = $this->createSub($column);
 
             $column = new Expression('('.$query.')');
