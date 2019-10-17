a708c656c2cfa2769faec7e783df5c45e22d1131
spawnia <benedikt@franke.tech>
2019-10-17 18:47:27 +0200

    Move class into test file

--- tests/Cache/CacheArrayStoreTest.php
+++ tests/Cache/CacheArrayStoreTest.php
@@ -205,3 +205,20 @@ class CacheArrayStoreTest extends TestCase
         $this->assertTrue($unserialized->wasSerialized);
     }
 }
+
+use Serializable;
+
+class SerializableChecker implements Serializable
+{
+    public $wasSerialized = false;
+
+    public function serialize()
+    {
+        return serialize('');
+    }
+
+    public function unserialize($serialized)
+    {
+        $this->wasSerialized = true;
+    }
+}
