051b8536566bcf92d4673fe78ef3bd3bb1d4481b
aponscat <apons@omatech.com>
2019-10-18 15:04:15 +0200

    Support last_modified parameter with Carbon::parse
    setCache last_modified requires a DateTime object and currently the Middleware passes only a String
    
    Now you can pass an integer (assumed a unix timestamp) or an arbitrary string that will be parsed by Carbon as a DateTime

--- src/Illuminate/Http/Middleware/SetCacheHeaders.php
+++ src/Illuminate/Http/Middleware/SetCacheHeaders.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Http\Middleware;
 
 use Closure;
+use Carbon\Carbon;
 
 class SetCacheHeaders
 {
@@ -31,6 +32,18 @@ class SetCacheHeaders
         if (isset($options['etag']) && $options['etag'] === true) {
             $options['etag'] = md5($response->getContent());
         }
+        
+        if (isset($options['last_modified']))
+        {
+          if (is_numeric($options['last_modified'])) 
+          {
+            $options['last_modified']=Carbon::createFromTimestamp($options['last_modified']);
+          }
+          else
+          {
+            $options['last_modified']=Carbon::parse($options['last_modified']);						
+          }
+        }        
 
         $response->setCache($options);
         $response->isNotModified($request);
