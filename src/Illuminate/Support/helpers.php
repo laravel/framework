688b96356c5d7dcb6a7116505bc19c8ebc28a4da
ginnerpeace <ginnerpeace@live.com>
2019-10-21 01:16:42 +0800

    Improve function `retry()`
    If given a value less than 1 for `$times`, function could be stuck in an infinite loop.

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -384,19 +384,17 @@ if (! function_exists('retry')) {
     function retry($times, callable $callback, $sleep = 0, $when = null)
     {
         $attempts = 0;
-        $times--;
 
         beginning:
         $attempts++;
+        $times--;
 
         try {
             return $callback($attempts);
         } catch (Exception $e) {
-            if (! $times || ($when && ! $when($e))) {
+            if ($times < 1 || ($when && ! $when($e))) {
                 throw $e;
             }
-
-            $times--;
 
             if ($sleep) {
                 usleep($sleep * 1000);
