1534427adf516588ec922ed0c54cd76d0a2d12b1
Robert Fridzema <fridzema@gmail.com>
2019-10-24 10:01:55 +0200

    Added view:cache, event:cache to optimize command
    Added missing view:cache command which is already called at the optimize:clear command.
    Added event:cache command.

--- src/Illuminate/Foundation/Console/OptimizeCommand.php
+++ src/Illuminate/Foundation/Console/OptimizeCommand.php
@@ -29,6 +29,8 @@ class OptimizeCommand extends Command
     {
         $this->call('config:cache');
         $this->call('route:cache');
+        $this->call('view:cache');
+        $this->call('event:cache');
 
         $this->info('Files cached successfully!');
     }
