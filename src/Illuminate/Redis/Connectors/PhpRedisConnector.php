12b8a6a3b7c241d497c1d076c9def85456a9ca16
davidhhuan <davidhhuan@126.com>
2019-10-28 04:16:53 +0800

    [6.x] Use isset but not !empty for changing redis database (#30420)
    * Update PhpRedisConnector.php
    
    * Fixed syntax

--- src/Illuminate/Redis/Connectors/PhpRedisConnector.php
+++ src/Illuminate/Redis/Connectors/PhpRedisConnector.php
@@ -80,8 +80,8 @@ class PhpRedisConnector implements Connector
                 $client->auth($config['password']);
             }
 
-            if (! empty($config['database'])) {
-                $client->select($config['database']);
+            if (isset($config['database'])) {
+                $client->select((int) $config['database']);
             }
 
             if (! empty($config['prefix'])) {
