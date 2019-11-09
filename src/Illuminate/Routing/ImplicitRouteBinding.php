bbe9617ec88b30985296c4a7ce3e37a914fe0e24
Loot <77028277827@yandex.ru>
2019-11-10 00:03:45 +0600

    Added declaring nested routes ability

--- src/Illuminate/Routing/ImplicitRouteBinding.php
+++ src/Illuminate/Routing/ImplicitRouteBinding.php
@@ -20,10 +20,15 @@ class ImplicitRouteBinding
     public static function resolveForRoute($container, $route)
     {
         $parameters = $route->parameters();
+        $signatureParameters = $route->signatureParameters(UrlRoutable::class);
 
-        foreach ($route->signatureParameters(UrlRoutable::class) as $parameter) {
+        foreach ($signatureParameters as $parameter) {
             if (! $parameterName = static::getParameterName($parameter->name, $parameters)) {
-                continue;
+                if (!static::isNestedParameter($parameter->name, $parameters)) {
+                    continue;
+                }
+
+                $parameterName = static::getNestedParameterName($parameter->name, $parameters);
             }
 
             $parameterValue = $parameters[$parameterName];
@@ -34,7 +39,17 @@ class ImplicitRouteBinding
 
             $instance = $container->make($parameter->getClass()->name);
 
-            if (! $model = $instance->resolveRouteBinding($parameterValue)) {
+            if (count($chunks = explode('__from__', $parameterName)) > 1)
+            {
+                $parentName = $chunks[1];
+                $parentParameter = $route->parameters()[$parentName] ?? $parentName;
+
+                $model = $instance->where($instance->getRouteKeyName(), $parameterValue)
+                    ->whereHas($parentName, function($query) use ($parentParameter) {
+                        $query->where($parentParameter->getRouteKeyName(), $parentParameter->getRouteKey());
+                    })
+                    ->first();
+            } else if (! $model = $instance->resolveRouteBinding($parameterValue)) {
                 throw (new ModelNotFoundException)->setModel(get_class($instance), [$parameterValue]);
             }
 
@@ -58,5 +73,44 @@ class ImplicitRouteBinding
         if (array_key_exists($snakedName = Str::snake($name), $parameters)) {
             return $snakedName;
         }
+    }
+
+    /**
+     * Check if parameter is nested
+     *
+     * @param string $name
+     * @return bool
+     */
+    protected static function isNestedParameter($name, $parameters)
+    {
+        return count(static::findNestedParameter(...func_get_args()));
+    }
+
+    /**
+     * Get nested parameter name
+     *
+     * @param string $name
+     * @param array $parameters
+     * @return string
+     */
+    protected static function getNestedParameterName($name, $parameters)
+    {
+        $nestedName = key(static::findNestedParameter(...func_get_args()));
+
+        return $nestedName;
+    }
+
+    /**
+     * Find parameter
+     *
+     * @param string $name
+     * @param array $parameters
+     * @return array
+     */
+    protected static function findNestedParameter($name, $parameters)
+    {
+        return array_filter($parameters, function ($parameterName) use ($name) {
+            return strpos($parameterName, $name . '__from__') !== false;
+        }, ARRAY_FILTER_USE_KEY);
     }
 }
