d52cdeb4046195d4e16370d27662fb785752819c
Dmytro Kulyk <dmytro@dkulyk.name>
2019-11-18 23:49:40 +0200

    [6.x] Changed type for `when` value (#30621)
    * Changed type for `when` value.
    
    * Update EnumeratesValues.php

--- src/Illuminate/Support/Traits/EnumeratesValues.php
+++ src/Illuminate/Support/Traits/EnumeratesValues.php
@@ -402,7 +402,7 @@ trait EnumeratesValues
     /**
      * Apply the callback if the value is truthy.
      *
-     * @param  bool  $value
+     * @param  bool|mixed  $value
      * @param  callable  $callback
      * @param  callable  $default
      * @return static|mixed
