01869bc266d6daa1a99a2d4a12f185e260b0a51a
Noah Heck <noahheck@users.noreply.github.com>
2019-11-20 07:20:25 -0700

    Update __ function doc block (#30633)
    Remove return option that won't get returned

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -902,7 +902,7 @@ if (! function_exists('__')) {
      * @param  string|null  $key
      * @param  array  $replace
      * @param  string|null  $locale
-     * @return \Illuminate\Contracts\Translation\Translator|string|array|null
+     * @return string|array|null
      */
     function __($key = null, $replace = [], $locale = null)
     {
