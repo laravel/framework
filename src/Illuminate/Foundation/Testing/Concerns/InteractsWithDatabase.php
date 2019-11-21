bc1057b2960d9e7fec84fceb49e31614ec425e15
Jason McCreary <jason@pureconcepts.net>
2019-11-21 13:36:50 -0500

    Add assertDeleted for database testing

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithDatabase.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithDatabase.php
@@ -53,6 +53,25 @@ trait InteractsWithDatabase
      * @param  \Illuminate\Database\Eloquent\Model|string  $table
      * @param  array  $data
      * @param  string|null  $connection
+     * @return $this
+     */
+    protected function assertDeleted($table, array $data = [], $connection = null)
+    {
+        if ($table instanceof Model) {
+            return $this->assertDatabaseMissing($table->getTable(), [$table->getKeyName() => $table->getKey()], $table->getConnectionName());
+        }
+
+        $this->assertDatabaseMissing($table, $data, $connection);
+
+        return $this;
+    }
+
+    /**
+     * Assert the given record has been "soft deleted".
+     *
+     * @param  \Illuminate\Database\Eloquent\Model|string  $table
+     * @param  array  $data
+     * @param  string|null  $connection
      * @param  string|null  $deletedAtColumn
      * @return $this
      */
