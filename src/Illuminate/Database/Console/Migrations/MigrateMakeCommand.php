cb4f1234ea7e8177d37c7dc803bde76603ed2cf3
hsntngr <hasantngr.53qgmail.com>
2019-11-25 10:43:24 +0300

    added --pivot option to make:migration command

--- src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
+++ src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
@@ -18,7 +18,9 @@ class MigrateMakeCommand extends BaseCommand
         {--table= : The table to migrate}
         {--path= : The location where the migration file should be created}
         {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
-        {--fullpath : Output the full path of the migration}';
+        {--fullpath : Output the full path of the migration}
+        {--pivot : Generates pivot table}
+        {--pivot-prefixes= : Field prefixes that pivot is related}';
 
     /**
      * The console command description.
@@ -72,6 +74,11 @@ class MigrateMakeCommand extends BaseCommand
 
         $create = $this->input->getOption('create') ?: false;
 
+        $pivot = [
+            'is' => $this->input->getOption('pivot') ?: false,
+            'prefixes' => $this->input->getOption('pivot-prefixes')
+        ];
+
         // If no table was given as an option but a create option is given then we
         // will use the "create" option as the table name. This allows the devs
         // to pass a table name into this option as a short-cut for creating.
@@ -91,7 +98,7 @@ class MigrateMakeCommand extends BaseCommand
         // Now we are ready to write the migration out to disk. Once we've written
         // the migration out, we will dump-autoload for the entire framework to
         // make sure that the migrations are registered by the class loaders.
-        $this->writeMigration($name, $table, $create);
+        $this->writeMigration($name, $table, $create, $pivot);
 
         $this->composer->dumpAutoloads();
     }
@@ -99,15 +106,16 @@ class MigrateMakeCommand extends BaseCommand
     /**
      * Write the migration file to disk.
      *
-     * @param  string  $name
-     * @param  string  $table
-     * @param  bool    $create
+     * @param string $name
+     * @param string $table
+     * @param bool $create
+     * @param $pivot
      * @return string
      */
-    protected function writeMigration($name, $table, $create)
+    protected function writeMigration($name, $table, $create, $pivot)
     {
         $file = $this->creator->create(
-            $name, $this->getMigrationPath(), $table, $create
+            $name, $this->getMigrationPath(), $table, $create, $pivot
         );
 
         if (! $this->option('fullpath')) {
--- src/Illuminate/Database/Migrations/MigrationCreator.php
+++ src/Illuminate/Database/Migrations/MigrationCreator.php
@@ -37,26 +37,26 @@ class MigrationCreator
     /**
      * Create a new migration at the given path.
      *
-     * @param  string  $name
-     * @param  string  $path
-     * @param  string|null  $table
-     * @param  bool    $create
+     * @param string $name
+     * @param string $path
+     * @param string|null $table
+     * @param bool $create
+     * @param array $pivot
      * @return string
      *
-     * @throws \Exception
      */
-    public function create($name, $path, $table = null, $create = false)
+    public function create($name, $path, $table = null, $create = false, $pivot = [])
     {
         $this->ensureMigrationDoesntAlreadyExist($name, $path);
 
         // First we will get the stub file for the migration, which serves as a type
         // of template for the migration. Once we have those we will populate the
         // various place-holders, save the file, and run the post create event.
-        $stub = $this->getStub($table, $create);
+        $stub = $this->getStub($table, $create, $pivot);
 
         $this->files->put(
             $path = $this->getPath($name, $path),
-            $this->populateStub($name, $stub, $table)
+            $this->populateStub($name, $stub, $table, $pivot)
         );
 
         // Next, we will fire any hooks that are supposed to fire after a migration is
@@ -95,10 +95,11 @@ class MigrationCreator
      * Get the migration stub file.
      *
      * @param  string|null  $table
-     * @param  bool    $create
+     * @param  bool  $create
+     * @param  $pivot
      * @return string
      */
-    protected function getStub($table, $create)
+    protected function getStub($table, $create, $pivot)
     {
         if (is_null($table)) {
             return $this->files->get($this->stubPath().'/blank.stub');
@@ -107,7 +108,7 @@ class MigrationCreator
         // We also have stubs for creating new tables and modifying existing tables
         // to save the developer some typing when they are creating a new tables
         // or modifying existing tables. We'll grab the appropriate stub here.
-        $stub = $create ? 'create.stub' : 'update.stub';
+        $stub = $create ? ($pivot['is'] ? 'pivot.stub' : 'create.stub') : 'update.stub';
 
         return $this->files->get($this->stubPath()."/{$stub}");
     }
@@ -118,9 +119,10 @@ class MigrationCreator
      * @param  string  $name
      * @param  string  $stub
      * @param  string|null  $table
+     * @param  array  $pivot
      * @return string
      */
-    protected function populateStub($name, $stub, $table)
+    protected function populateStub($name, $stub, $table, $pivot)
     {
         $stub = str_replace('DummyClass', $this->getClassName($name), $stub);
 
@@ -129,6 +131,19 @@ class MigrationCreator
         // or update migration from the console instead of typing it manually.
         if (! is_null($table)) {
             $stub = str_replace('DummyTable', $table, $stub);
+        }
+
+        // if it's pivot table, replace fields and table names by given prefixes
+        if ($pivot['is']) {
+            [$first, $second] = explode(
+                $pivot['prefixes'] ? ',' : '_',
+                $pivot['prefixes'] ?: $table
+            );
+            $stub = str_replace(
+                ['firstPivotId','secondPivotId','firstPivotTable','secondPivotTable'],
+                [$first.'_id', $second."_id", Str::plural($first), Str::plural($second)],
+                $stub
+            );
         }
 
         return $stub;
