fd17b307f546f449d24d4eed1725b4941a98b895
Taylor Otwell <taylorotwell@gmail.com>
2019-11-29 07:48:54 -0600

    formatting

--- src/Illuminate/Database/Eloquent/Concerns/HasTimestamps.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasTimestamps.php
@@ -39,11 +39,13 @@ trait HasTimestamps
         $time = $this->freshTimestamp();
 
         $updatedAtColumn = $this->getUpdatedAtColumn();
+
         if (! is_null($updatedAtColumn) && ! $this->isDirty($updatedAtColumn)) {
             $this->setUpdatedAt($time);
         }
 
         $createdAtColumn = $this->getCreatedAtColumn();
+
         if (! $this->exists && ! is_null($createdAtColumn) && ! $this->isDirty($createdAtColumn)) {
             $this->setCreatedAt($time);
         }
