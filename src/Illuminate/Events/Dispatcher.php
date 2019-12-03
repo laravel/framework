a6edb86ebbeb21798ac7566c9b6fd3c94929c3c6
Martin Hettiger <martin@hettiger.com>
2019-12-03 16:29:09 +0100

    Support retryAfter Option on Queued Listeners (#30743)
    * event-dispatcher: propagate retryAfter option
    
    * queued-listener-job: add retryAfter option
    
    * remove whitespace

--- src/Illuminate/Events/Dispatcher.php
+++ src/Illuminate/Events/Dispatcher.php
@@ -502,6 +502,7 @@ class Dispatcher implements DispatcherContract
     {
         return tap($job, function ($job) use ($listener) {
             $job->tries = $listener->tries ?? null;
+            $job->retryAfter = $listener->retryAfter ?? null;
             $job->timeout = $listener->timeout ?? null;
             $job->timeoutAt = method_exists($listener, 'retryUntil')
                                 ? $listener->retryUntil() : null;
