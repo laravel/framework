c75bc43833dc3c1e9bfd3e8ea011cd1dd2834cc9
Taylor Otwell <taylorotwell@gmail.com>
2019-12-10 09:49:16 -0600

    reorganize

--- src/Illuminate/Database/Eloquent/Concerns/HasRelationships.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasRelationships.php
@@ -723,30 +723,6 @@ trait HasRelationships
     }
 
     /**
-     * Duplicate the instance and unset all the loaded relations.
-     *
-     * @return $this
-     */
-    public function withoutRelations()
-    {
-        $model = clone $this;
-
-        return $model->unsetRelations();
-    }
-
-    /**
-     * Unset all the loaded relations for the instance.
-     *
-     * @return $this
-     */
-    public function unsetRelations()
-    {
-        $this->relations = [];
-
-        return $this;
-    }
-
-    /**
      * Get all the loaded relations for the instance.
      *
      * @return array
@@ -814,6 +790,30 @@ trait HasRelationships
     public function setRelations(array $relations)
     {
         $this->relations = $relations;
+
+        return $this;
+    }
+
+    /**
+     * Duplicate the instance and unset all the loaded relations.
+     *
+     * @return $this
+     */
+    public function withoutRelations()
+    {
+        $model = clone $this;
+
+        return $model->unsetRelations();
+    }
+
+    /**
+     * Unset all the loaded relations for the instance.
+     *
+     * @return $this
+     */
+    public function unsetRelations()
+    {
+        $this->relations = [];
 
         return $this;
     }
