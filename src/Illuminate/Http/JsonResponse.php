a87ba56f185d9cae2c3c9ee9e674da7d22516192
Taylor Otwell <taylor@laravel.com>
2019-12-11 13:47:34 -0600

    Apply fixes from StyleCI (#30816)

--- src/Illuminate/Http/JsonResponse.php
+++ src/Illuminate/Http/JsonResponse.php
@@ -151,7 +151,7 @@ class JsonResponse extends BaseJsonResponse
      */
     public function offsetSet($offset, $value)
     {
-        throw new LogicException("Response data may not be mutated using array access.");
+        throw new LogicException('Response data may not be mutated using array access.');
     }
 
     /**
@@ -162,6 +162,6 @@ class JsonResponse extends BaseJsonResponse
      */
     public function offsetUnset($offset)
     {
-        throw new LogicException("Response data may not be mutated using array access.");
+        throw new LogicException('Response data may not be mutated using array access.');
     }
 }
