a1e7261e8320084803c58dccf67c97c0cfad9ac0
freek <freek@spatie.be>
2019-12-11 14:29:20 +0100

    add namespace

--- src/Illuminate/Mail/Markdown.php
+++ src/Illuminate/Mail/Markdown.php
@@ -4,6 +4,7 @@ namespace Illuminate\Mail;
 
 use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Support\HtmlString;
+use Illuminate\Support\Str;
 use Parsedown;
 use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
 
