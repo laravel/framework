2206d5223606f5a24e7e3bf0ba1f25b343dfcc6b
Taylor Otwell <taylorotwell@gmail.com>
2019-12-11 08:22:36 -0600

    rename variable

--- src/Illuminate/Mail/Markdown.php
+++ src/Illuminate/Mail/Markdown.php
@@ -61,12 +61,12 @@ class Markdown
             'mail', $this->htmlComponentPaths()
         )->make($view, $data)->render();
 
-        $viewPath = Str::contains($this->theme, '::')
+        $theme = Str::contains($this->theme, '::')
             ? $this->theme
             : 'mail::themes.'.$this->theme;
 
         return new HtmlString(($inliner ?: new CssToInlineStyles)->convert(
-            $contents, $this->view->make($viewPath, $data)->render()
+            $contents, $this->view->make($theme, $data)->render()
         ));
     }
 
