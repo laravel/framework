e3b4f7f183bb41b26a90777c7114eb4867bb777c
Kenneth Keegan <kkeegan@gmail.com>
2019-12-12 06:22:45 -0800

    Change DatabaseNotification keyType to match uuid (#30823)
    The default primary key for database notifications is a UUID, but the default `keyType` (from the base model) is an `int`. This causes ids with string UUIDs to be force cast as integers in MorphTo and other relationships.
    
    https://github.com/laravel/framework/blob/f5c3c60d52eccd77274e586fc4c775374e826b0f/src/Illuminate/Database/Eloquent/Relations/Relation.php#L310-L323

--- src/Illuminate/Notifications/DatabaseNotification.php
+++ src/Illuminate/Notifications/DatabaseNotification.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class DatabaseNotification extends Model
 {
     /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
      * Indicates if the IDs are auto-incrementing.
      *
      * @var bool
