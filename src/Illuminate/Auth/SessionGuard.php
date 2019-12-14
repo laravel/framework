7e017e06a2cb442200015948f78bf9d5807b97f3
Jack Ellis <jack@jackellisweb.com>
2019-12-14 10:57:10 -0600

    Fix doc

--- src/Illuminate/Auth/SessionGuard.php
+++ src/Illuminate/Auth/SessionGuard.php
@@ -483,7 +483,7 @@ class SessionGuard implements StatefulGuard, SupportsBasicAuth
     /**
      * Log the user out of the application.
      *
-     * @param  string  $attribute
+     * @param  bool  $cycleRememberToken
      * @return void
      */
     public function logout($cycleRememberToken = true)
