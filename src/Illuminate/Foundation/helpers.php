118c281d4c5465dae248a6edfa44a371f4c3fca9
Andrew Brown <browner12@gmail.com>
2019-12-17 17:18:38 -0600

    remove factory "types"
    - simplify the global helper `factory()`. since the signature is no longer dynamic, we can be a little more explicit in our parameters
    - remove `defineAs`, `createAs`, `makeAs`, and `rawOf` which were methods specific to factory "types"
    - update the `FactoryBuilder` to remove references to the "name". we do keep one reference to the name "default" because that is how the callbacks are registered.

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -484,24 +484,21 @@ if (! function_exists('event')) {
 
 if (! function_exists('factory')) {
     /**
-     * Create a model factory builder for a given class, name, and amount.
+     * Create a model factory builder for a given class and amount.
      *
-     * @param  dynamic  class|class,name|class,amount|class,name,amount
+     * @param  string  $class
+     * @param  int  $amount
      * @return \Illuminate\Database\Eloquent\FactoryBuilder
      */
-    function factory()
+    function factory($class, $amount = null)
     {
         $factory = app(EloquentFactory::class);
 
-        $arguments = func_get_args();
-
-        if (isset($arguments[1]) && is_string($arguments[1])) {
-            return $factory->of($arguments[0], $arguments[1])->times($arguments[2] ?? null);
-        } elseif (isset($arguments[1])) {
-            return $factory->of($arguments[0])->times($arguments[1]);
+        if (isset($amount) && is_int($amount)) {
+            return $factory->of($class)->times($amount);
         }
 
-        return $factory->of($arguments[0]);
+        return $factory->of($class);
     }
 }
 
