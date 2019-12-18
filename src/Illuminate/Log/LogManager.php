61a320be0ec3d01a3a801e80310624bdc2a44b88
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-12-18 13:23:15 +0000

    Not allowed to use the storage_path helper outside of Foundation

--- src/Illuminate/Log/LogManager.php
+++ src/Illuminate/Log/LogManager.php
@@ -165,10 +165,14 @@ class LogManager implements LoggerInterface
     {
         $config = $this->configurationFor('emergency');
 
+        $handler = new StreamHandler(
+            $config['path'] ?? $this->app->storagePath().'/logs/laravel.log',
+            $this->level(['level' => 'debug'])
+        );
+
         return new Logger(
-            new Monolog('laravel', $this->prepareHandlers([
-                new StreamHandler($config['path'] ?? storage_path('logs/laravel.log'), $this->level(['level' => 'debug'])),
-            ])), $this->app['events']
+            new Monolog('laravel', $this->prepareHandlers([$handler])),
+            $this->app['events']
         );
     }
 
