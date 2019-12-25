4b420f28a77b87637ed2e36954c361c0fff6c9ca
Andrey Helldar <helldar@ai-rus.com>
2019-12-25 17:33:06 +0300

    [6.x] Removed `$value instanceof Carbon` redundancy `Illuminate\Support\Carbon extends \Carbon\Carbon implements \Carbon\CarbonInterface`

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -773,7 +773,7 @@ trait HasAttributes
         // If this value is already a Carbon instance, we shall just return it as is.
         // This prevents us having to re-instantiate a Carbon instance when we know
         // it already is one, which wouldn't be fulfilled by the DateTime check.
-        if ($value instanceof Carbon || $value instanceof CarbonInterface) {
+        if ($value instanceof CarbonInterface) {
             return Date::instance($value);
         }
 
