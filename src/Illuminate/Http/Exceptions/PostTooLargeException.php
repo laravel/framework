b074f4269311840c25dd17508a544cd5e68c7428
Taylor Otwell <taylor@laravel.com>
2019-12-26 20:18:25 -0600

    Update PostTooLargeException.php

--- src/Illuminate/Http/Exceptions/PostTooLargeException.php
+++ src/Illuminate/Http/Exceptions/PostTooLargeException.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class PostTooLargeException extends HttpException
 {
     /**
-     * Create a new post too large exception instance.
+     * Create a new "post too large" exception instance.
      *
      * @param  string|null  $message
      * @param  \Exception|null  $previous
