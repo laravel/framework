27eb546d1e80090e131c98c6d4b6edfb13b10a23
Andrey Helldar <helldar@ai-rus.com>
2019-12-29 01:34:31 +0300

    Added test for checking NULL values

--- tests/Integration/Database/EloquentModelCustomCastingTest.php
+++ tests/Integration/Database/EloquentModelCustomCastingTest.php
@@ -16,6 +16,7 @@ class EloquentModelCustomCastingTest extends DatabaseTestCase
             'field_2' => 20,
             'field_3' => '08:19:12',
             'field_4' => null,
+            'field_5' => null,
         ]);
 
         $this->assertSame(['f', 'o', 'o', 'b', 'a', 'r'], $item->toArray()['field_1']);
@@ -30,6 +31,8 @@ class EloquentModelCustomCastingTest extends DatabaseTestCase
         );
 
         $this->assertSame(null, $item->toArray()['field_4']);
+
+        $this->assertSame('foo', $item->toArray()['field_5']);
     }
 
     protected function setUp(): void
@@ -42,6 +45,7 @@ class EloquentModelCustomCastingTest extends DatabaseTestCase
             $table->integer('field_2')->nullable();
             $table->time('field_3')->nullable();
             $table->string('field_4')->nullable();
+            $table->string('field_5')->nullable();
         });
     }
 }
@@ -57,6 +61,7 @@ class TestModel extends Model
         'field_2' => NumberCast::class,
         'field_3' => TimeCast::class,
         'field_4' => NullCast::class,
+        'field_5' => NullChangedCast::class,
     ];
 
     protected $guarded = ['id'];
@@ -114,6 +119,19 @@ class NullCast implements Castable
 
     public function set($value = null)
     {
-        return $value;
+        return null;
+    }
+}
+
+class NullChangedCast implements Castable
+{
+    public function get($value = null)
+    {
+        return 'foo';
+    }
+
+    public function set($value = null)
+    {
+        return null;
     }
 }
