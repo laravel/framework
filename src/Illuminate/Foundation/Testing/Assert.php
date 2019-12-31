b2c7ecc1c1b27973034a3b97f98ae5072de9e114
Graham Campbell <graham@alt-three.com>
2019-12-31 00:59:29 +0000

    Bring in array subset code directly

--- src/Illuminate/Foundation/Testing/Assert.php
+++ src/Illuminate/Foundation/Testing/Assert.php
@@ -3,55 +3,46 @@
 namespace Illuminate\Foundation\Testing;
 
 use ArrayAccess;
-use Exception;
-use GrahamCampbell\TestBenchCore\ArraySubsetTrait;
+use Illuminate\Foundation\Testing\Constraints\ArraySubset;
 use PHPUnit\Framework\Assert as PHPUnit;
-use PHPUnit\Framework\Constraint\ArraySubset;
-use PHPUnit\Runner\Version;
+use PHPUnit\Framework\InvalidArgumentException;
 use PHPUnit\Util\InvalidArgumentHelper;
 
-if (trait_exists(ArraySubsetTrait::class)) {
+/**
+ * @internal This class is not meant to be used or overwritten outside the framework itself.
+ */
+abstract class Assert extends PHPUnit
+{
     /**
-     * @internal This class is not meant to be used or overwritten outside the framework itself.
+     * Asserts that an array has a specified subset.
+     *
+     * @param \ArrayAccess|array $subset
+     * @param \ArrayAccess|array $array
+     * @param bool               $checkForIdentity
+     * @param string             $msg
+     *
+     * @return void
      */
-    abstract class Assert extends PHPUnit
+    public static function assertArraySubset($subset, $array, bool $checkForIdentity = false, string $msg = ''): void
     {
-        use ArraySubsetTrait;
-    }
-} else {
-    /**
-     * @internal This class is not meant to be used or overwritten outside the framework itself.
-     */
-    abstract class Assert extends PHPUnit
-    {
-        /**
-         * Asserts that an array has a specified subset.
-         *
-         * This method was taken over from PHPUnit where it was deprecated. See link for more info.
-         *
-         * @param  \ArrayAccess|array  $subset
-         * @param  \ArrayAccess|array  $array
-         * @param  bool  $checkForObjectIdentity
-         * @param  string  $message
-         * @return void
-         */
-        public static function assertArraySubset($subset, $array, bool $checkForObjectIdentity = false, string $message = ''): void
-        {
-            if ((int) Version::series()[0] > 8) {
-                throw new Exception('For PHPUnit 9 support, please install graham-campbell/testbench-core:"^3.1".');
-            }
-
-            if (! (is_array($subset) || $subset instanceof ArrayAccess)) {
+        if (!(is_array($subset) || $subset instanceof ArrayAccess)) {
+            if (class_exists(InvalidArgumentException::class)) {
+                throw InvalidArgumentException::create(1, 'array or ArrayAccess');
+            } else {
                 throw InvalidArgumentHelper::factory(1, 'array or ArrayAccess');
             }
+        }
 
-            if (! (is_array($array) || $array instanceof ArrayAccess)) {
+        if (!(is_array($array) || $array instanceof ArrayAccess)) {
+            if (class_exists(InvalidArgumentException::class)) {
+                throw InvalidArgumentException::create(2, 'array or ArrayAccess');
+            } else {
                 throw InvalidArgumentHelper::factory(2, 'array or ArrayAccess');
             }
-
-            $constraint = new ArraySubset($subset, $checkForObjectIdentity);
-
-            static::assertThat($array, $constraint, $message);
         }
+
+        $constraint = new ArraySubset($subset, $checkForIdentity);
+
+        PHPUnit::assertThat($array, $constraint, $msg);
     }
 }
