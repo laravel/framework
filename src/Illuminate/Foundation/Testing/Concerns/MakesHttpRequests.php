bfd9b3528d68f37911999896cb29911786c7b920
Ahmed Ashraf <ahmed29329@gmail.com>
2020-01-01 18:31:06 +0100

    allow $paramters in make get requests

--- src/Illuminate/Foundation/Testing/Concerns/MakesHttpRequests.php
+++ src/Illuminate/Foundation/Testing/Concerns/MakesHttpRequests.php
@@ -214,14 +214,15 @@ trait MakesHttpRequests
      *
      * @param  string  $uri
      * @param  array  $headers
+     * @param  array  $parameters
      * @return \Illuminate\Foundation\Testing\TestResponse
      */
-    public function get($uri, array $headers = [])
+    public function get($uri, array $headers = [],$parameters = [])
     {
         $server = $this->transformHeadersToServerVars($headers);
         $cookies = $this->prepareCookiesForRequest();
 
-        return $this->call('GET', $uri, [], $cookies, [], $server);
+        return $this->call('GET', $uri, $parameters, $cookies, [], $server);
     }
 
     /**
