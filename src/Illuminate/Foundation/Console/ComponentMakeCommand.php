a9f74f7e7f495f34418c1158d6c1ae3e80d3cd6d
Taylor Otwell <taylorotwell@gmail.com>
2020-01-03 16:02:19 -0600

    make view from command

--- src/Illuminate/Foundation/Console/ComponentMakeCommand.php
+++ src/Illuminate/Foundation/Console/ComponentMakeCommand.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Foundation\Console;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Str;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -28,6 +29,47 @@ class ComponentMakeCommand extends GeneratorCommand
      * @var string
      */
     protected $type = 'Component';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (parent::handle() === false && ! $this->option('force')) {
+            return false;
+        }
+
+        if ($this->input->getParameterOption('--view') !== false) {
+            $this->writeView();
+        }
+    }
+
+    /**
+     * Write the view for the component.
+     *
+     * @return void
+     */
+    protected function writeView()
+    {
+        $view = $this->option('view')
+                    ? $this->option('view')
+                    : 'components.'.Str::kebab(class_basename($this->argument('name')));
+
+        $path = resource_path('views').'/'.str_replace('.', '/', $view);
+
+        if (! $this->files->isDirectory(dirname($path))) {
+            $this->files->makeDirectory(dirname($path), 0777, true, true);
+        }
+
+        file_put_contents(
+            $path.'.blade.php',
+            "<div>
+    <!-- ".Inspiring::quote()." -->
+</div>"
+        );
+    }
 
     /**
      * Build the class with the given name.
@@ -73,6 +115,7 @@ class ComponentMakeCommand extends GeneratorCommand
     protected function getOptions()
     {
         return [
+            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the component already exists'],
             ['view', null, InputOption::VALUE_OPTIONAL, 'Create a new Blade template for the component'],
         ];
     }
