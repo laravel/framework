a0d85e33c4c89061295543ae4fc25b117bb9ce4c
Taylor Otwell <taylorotwell@gmail.com>
2020-01-03 21:10:46 -0600

    add test

---
+++ tests/View/ViewComponentAttributeBagTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Illuminate\Tests\View;
+
+use Illuminate\View\Component;
+use Illuminate\View\ComponentAttributeBag;
+use PHPUnit\Framework\TestCase;
+
+class ViewComponentAttributeBagTest extends TestCase
+{
+    public function testAttributeRetrieval()
+    {
+        $bag = new ComponentAttributeBag(['class' => 'font-bold', 'name' => 'test']);
+
+        $this->assertEquals('class="mt-4 font-bold" name="test"', (string) $bag->merge(['class' => 'mt-4']));
+        $this->assertEquals('class="mt-4 font-bold"', (string) $bag->only('class')->merge(['class' => 'mt-4']));
+        $this->assertEquals('class="mt-4 font-bold"', (string) $bag->only('class')(['class' => 'mt-4']));
+        $this->assertEquals('font-bold', $bag->get('class'));
+        $this->assertEquals('bar', $bag->get('foo', 'bar'));
+        $this->assertEquals('font-bold', $bag['class']);
+    }
+}
