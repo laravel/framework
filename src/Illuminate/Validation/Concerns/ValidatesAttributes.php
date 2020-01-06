69542b9414b0a43e18802343e8cad0ccefc6cca7
Alex Bergin <alex.bergin@gmail.com>
2020-01-06 20:52:46 +0800

    create model instance to avoid multiple instances of model

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -819,8 +819,9 @@ trait ValidatesAttributes
         [$connection, $table] = Str::contains($table, '.') ? explode('.', $table, 2) : [null, $table];
 
         if (Str::contains($table, '\\') && class_exists($table) && is_a($table, Model::class, true)) {
-            $connection = $connection ?? (new $table)->getConnectionName();
-            $table = (new $table)->getTable();
+            $model = new $table;
+            $table = $model->getTable();
+            $connection = $connection ?? $model->getConnectionName();
         }
 
         return [$connection, $table];
