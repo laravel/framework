209244a87b6abab2f79564af23b5ab9c44e792a5
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-07 23:36:30 +0800

    Typehint Container\Container since lumen-framework doesn't extends Foundation\Application contract.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Testing/PendingCommand.php
+++ src/Illuminate/Testing/PendingCommand.php
@@ -22,7 +22,7 @@ class PendingCommand
     /**
      * The application instance.
      *
-     * @var \Illuminate\Contracts\Foundation\Application
+     * @var \Illuminate\Contracts\Container\Container
      */
     protected $app;
 
@@ -58,7 +58,7 @@ class PendingCommand
      * Create a new pending console command run.
      *
      * @param  \PHPUnit\Framework\TestCase  $test
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  \Illuminate\Contracts\Container\Container  $app
      * @param  string  $command
      * @param  array  $parameters
      * @return void
