678b26b1a9cd0d8a6bef85932420e67a1b20e677
Taylor Otwell <taylorotwell@gmail.com>
2020-01-14 08:09:54 -0600

    formatting

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1150,14 +1150,19 @@ abstract class Model implements Arrayable, ArrayAccess, Jsonable, JsonSerializab
             static::newQueryWithoutScopes()->findOrFail($this->getKey())->attributes
         );
 
-        $actualRelations = collect();
-        foreach ($this->relations as $key => $relation) {
-            if (! ($relation instanceof Pivot)) {
-                $actualRelations->put($key, $relation);
-            }
-        }
+        // $actualRelations = collect();
 
-        $this->load($actualRelations->keys()->toArray());
+        // foreach ($this->relations as $key => $relation) {
+        //     if (! ($relation instanceof Pivot)) {
+        //         $actualRelations->put($key, $relation);
+        //     }
+        // }
+
+        $this->load(collect($this->relations)->reject(function ($relation) {
+            return $relation instanceof Pivot;
+        })->keys()->all());
+
+        // $this->load($actualRelations->keys()->toArray());
 
         $this->syncOriginal();
 
