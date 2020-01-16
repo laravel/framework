846eb0642194779a70bb571192ff4a466e073bd1
Gustavo <gustavoocanto@gmail.com>
2020-01-16 14:30:34 +0800

    Add bindings asseror object

---
+++ src/Illuminate/View/ViewBindingsAssessor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Illuminate\View;
+
+class BindingsAssessor
+{
+    public const VIEW = 'view';
+    public const PHP_ENGINE = 'php';
+    public const FILE_ENGINE = 'file';
+    public const BLADE_ENGINE = 'blade';
+    public const VIEW_FINDER = 'view.finder';
+    public const BLADE_COMPILER = 'blade.compiler';
+    public const VIEW_ENGINE_RESOLVER = 'view.engine.resolver';
+}
