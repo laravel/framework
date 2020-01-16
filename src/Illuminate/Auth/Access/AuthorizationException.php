1875cc7362058e62e52fe23408315578859c1412
Jorge García <jorge@aprendible.com>
2020-01-16 09:59:29 -0500

    Allow to translate the default unauthorized action message

--- src/Illuminate/Auth/Access/AuthorizationException.php
+++ src/Illuminate/Auth/Access/AuthorizationException.php
@@ -23,7 +23,7 @@ class AuthorizationException extends Exception
      */
     public function __construct($message = null, $code = null, Exception $previous = null)
     {
-        parent::__construct($message ?? 'This action is unauthorized.', 0, $previous);
+        parent::__construct($message ?? __('This action is unauthorized.'), 0, $previous);
 
         $this->code = $code ?: 0;
     }
