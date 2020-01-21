709e43cb5602c92b0264752208ad92581418b718
Dries Vints <dries.vints@gmail.com>
2020-01-21 13:48:28 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Illuminate/Foundation/Console/RouteCacheCommand.php
+++ src/Illuminate/Foundation/Console/RouteCacheCommand.php
@@ -5,7 +5,6 @@ namespace Illuminate\Foundation\Console;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\Kernel as ConsoleKernelContract;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Routing\RouteCollection;
 use Illuminate\Support\Str;
 use ReflectionMethod;
 use Symfony\Component\Routing\Matcher\Dumper\CompiledUrlMatcherDumper;
