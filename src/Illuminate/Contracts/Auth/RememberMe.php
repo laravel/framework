cd9ce235d8610f179c545772fee4b63eaf76a85c
Tobias Thaden <hello@tobiasthaden.com>
2020-01-23 18:08:44 +0100

    Create RememberMe.php

---
+++ src/Illuminate/Contracts/Auth/RememberMe.php
@@ -0,0 +1,29 @@
+
+<?php
+
+namespace Illuminate\Contracts\Auth;
+
+interface RememberMe
+{
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken();
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setRememberToken($value);
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName();
+}
