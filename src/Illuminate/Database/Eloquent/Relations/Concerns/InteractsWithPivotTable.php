d3c2544b878c8ce35da9dd8eda9c9d74e839f6b8
Gege <dngege@gmail.com>
2020-01-25 12:27:17 +0100

    Fix var type

--- src/Illuminate/Database/Eloquent/Relations/Concerns/InteractsWithPivotTable.php
+++ src/Illuminate/Database/Eloquent/Relations/Concerns/InteractsWithPivotTable.php
@@ -12,7 +12,7 @@ trait InteractsWithPivotTable
     /**
      * The cached copy of the currently attached pivot models.
      *
-     * @var \Illuminate\Support\Collection
+     * @var \Illuminate\Database\Eloquent\Collection
      */
     private $currentlyAttached;
 
