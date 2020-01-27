6d9a0b1fc1be82a538e29f8a8ab17d4db69dc213
Andrew Brown <browner12@gmail.com>
2020-01-27 17:31:47 -0600

    remove temporary variable (#31250)

--- src/Illuminate/View/Engines/CompilerEngine.php
+++ src/Illuminate/View/Engines/CompilerEngine.php
@@ -61,12 +61,10 @@ class CompilerEngine extends PhpEngine
             $this->compiler->compile($path);
         }
 
-        $compiled = $this->compiler->getCompiledPath($path);
-
         // Once we have the path to the compiled file, we will evaluate the paths with
         // typical PHP just like any other templates. We also keep a stack of views
         // which have been rendered for right exception messages to be generated.
-        $results = $this->evaluatePath($compiled, $data);
+        $results = $this->evaluatePath($this->compiler->getCompiledPath($path), $data);
 
         array_pop($this->lastCompiled);
 
