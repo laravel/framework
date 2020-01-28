laravel/framework

commit 0d6349785d3cd41548e29e9dfbca3c10771b8dd5

[6.x] Redis Broadcaster: Broadcast to multiple channels at once (#31108)
* Use Lua script to broadcast to multiple channels at once

* formatting

* formatting

* add missing formatChannels

src/Illuminate/Broadcasting/Broadcasters/RedisBroadcaster.php
@@ -101,6 +101,10 @@ class RedisBroadcaster extends Broadcaster
      */
     public function broadcast(array $channels, $event, array $payload = [])
     {
+        if (empty($channels)) {
+            return;
+        }
+
         $connection = $this->redis->connection($this->connection);
 
         $payload = json_encode([
@@ -109,8 +113,23 @@ class RedisBroadcaster extends Broadcaster
             'socket' => Arr::pull($payload, 'socket'),
         ]);
 
-        foreach ($this->formatChannels($channels) as $channel) {
-            $connection->publish($channel, $payload);
-        }
+        $connection->eval($this->broadcastMultipleChannelsScript(), 0, $payload, ...$this->formatChannels($channels));
+    }
+
+    /**
+     * Get the Lua script for broadcasting to multiple channels.
+     *
+     * ARGV[1] - The payload
+     * ARGV[2...] - The channels
+     *
+     * @return string
+     */
+    protected function broadcastMultipleChannelsScript()
+    {
+        return <<<'LUA'
+for i = 2, #ARGV do
+  redis.call('publish', ARGV[i], ARGV[1])
+end
+LUA;
     }
 }

commit cb3a8eda6b5c575997944e67f0a4e9529bd09fed

Fix channel names when broadcasting via redis
This fixes a regression from #31108

src/Illuminate/Broadcasting/Broadcasters/Broadcaster.php
@@ -233,12 +233,13 @@ abstract class Broadcaster implements BroadcasterContract
      * Format the channel array into an array of strings.
      *
      * @param  array  $channels
+     * @param  string $prefix
      * @return array
      */
-    protected function formatChannels(array $channels)
+    protected function formatChannels(array $channels, string $prefix = '')
     {
-        return array_map(function ($channel) {
-            return (string) $channel;
+        return array_map(function ($channel) use ($prefix) {
+            return $prefix . (string) $channel;
         }, $channels);
     }
 

src/Illuminate/Broadcasting/Broadcasters/RedisBroadcaster.php
@@ -115,7 +115,7 @@ class RedisBroadcaster extends Broadcaster
 
         $connection->eval(
             $this->broadcastMultipleChannelsScript(),
-            0, $payload, ...$this->formatChannels($channels)
+            0, $payload, ...$this->formatChannels($channels, $this->prefix)
         );
     }
 