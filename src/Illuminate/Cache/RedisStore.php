3181782d513ecc6a90044872a385735acc038145
Dries Vints <dries.vints@gmail.com>
2020-02-04 14:04:22 +0100

    Fix infinite value for RedisStore

--- src/Illuminate/Cache/RedisStore.php
+++ src/Illuminate/Cache/RedisStore.php
@@ -292,7 +292,7 @@ class RedisStore extends TaggableStore implements LockProvider
      */
     protected function serialize($value)
     {
-        return is_numeric($value) ? $value : serialize($value);
+        return is_numeric($value) && ! in_array($value, [INF, -INF]) ? $value : serialize($value);
     }
 
     /**
