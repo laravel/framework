59c405856cbab2605370a262b5be1216e2bee2e1
Dries Vints <dries.vints@gmail.com>
2020-02-04 14:45:48 +0100

    Let Redis store NAN values

--- tests/Integration/Cache/RedisStoreTest.php
+++ tests/Integration/Cache/RedisStoreTest.php
@@ -29,6 +29,8 @@ class RedisStoreTest extends IntegrationTest
 
     public function testItCanStoreInfinite()
     {
+        Cache::store('redis')->clear();
+
         $result = Cache::store('redis')->put('foo', INF);
         $this->assertTrue($result);
         $this->assertSame(INF, Cache::store('redis')->get('foo'));
@@ -36,5 +38,14 @@ class RedisStoreTest extends IntegrationTest
         $result = Cache::store('redis')->put('bar', -INF);
         $this->assertTrue($result);
         $this->assertSame(-INF, Cache::store('redis')->get('bar'));
+    }
+
+    public function testItCanStoreNan()
+    {
+        Cache::store('redis')->clear();
+
+        $result = Cache::store('redis')->put('foo', NAN);
+        $this->assertTrue($result);
+        $this->assertNan(Cache::store('redis')->get('foo'));
     }
 }
