9a90e633ca22a4cd3aec0c2a2e65a4a13cb3f39e
Lars Moelleken <lars.moelleken@vdmg-it.com>
2020-02-07 00:39:14 +0100

    [7.x] Update external ascii package (#29947)
    * Use external ascii package also for "Str::slug()"

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -534,23 +534,9 @@ class Str
      */
     public static function slug($title, $separator = '-', $language = 'en')
     {
-        $title = $language ? static::ascii($title, $language) : $title;
+        $language = $language ?? '';
 
-        // Convert all dashes/underscores into separator
-        $flip = $separator === '-' ? '_' : '-';
-
-        $title = preg_replace('!['.preg_quote($flip).']+!u', $separator, $title);
-
-        // Replace @ with the word 'at'
-        $title = str_replace('@', $separator.'at'.$separator, $title);
-
-        // Remove all characters that are not the separator, letters, numbers, or whitespace.
-        $title = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', static::lower($title));
-
-        // Replace all separator characters and whitespace by a single separator
-        $title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
-
-        return trim($title, $separator);
+        return ASCII::to_slugify($title, $separator, $language, [], true);
     }
 
     /**
