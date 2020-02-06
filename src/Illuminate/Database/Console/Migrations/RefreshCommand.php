b4a1e84863a794de279a8e695bfe4683c8353905
Choraimy Kroonstuiver <choraimy@23g.nl>
2020-02-06 14:16:41 +0100

    Allow multiple paths to be passed to migrate fresh and migrate refresh

--- src/Illuminate/Database/Console/Migrations/RefreshCommand.php
+++ src/Illuminate/Database/Console/Migrations/RefreshCommand.php
@@ -141,7 +141,7 @@ class RefreshCommand extends Command
 
             ['force', null, InputOption::VALUE_NONE, 'Force the operation to run when in production'],
 
-            ['path', null, InputOption::VALUE_OPTIONAL, 'The path to the migrations files to be executed'],
+            ['path', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'The path(s) to the migrations files to use'],
 
             ['realpath', null, InputOption::VALUE_NONE, 'Indicate any provided migration file paths are pre-resolved absolute paths'],
 
