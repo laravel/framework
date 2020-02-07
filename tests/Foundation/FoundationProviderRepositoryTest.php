265d97dc7a27f7fdfd1ab68697a6ab0820be03fb
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-02-07 14:17:40 +0000

    [7.x] Removed old phpunit 7 code (#31392)
    * Removed old phpunit 7 code
    
    * Update composer.json
    
    Co-Authored-By: Nuno Maduro <enunomaduro@gmail.com>
    
    * Removed old work around for phpunit 7
    
    Co-authored-by: Nuno Maduro <enunomaduro@gmail.com>

--- tests/Foundation/FoundationProviderRepositoryTest.php
+++ tests/Foundation/FoundationProviderRepositoryTest.php
@@ -92,12 +92,7 @@ class FoundationProviderRepositoryTest extends TestCase
     public function testWriteManifestThrowsExceptionIfManifestDirDoesntExist()
     {
         $this->expectException(Exception::class);
-
-        if (is_callable([$this, 'expectExceptionMessageMatches'])) {
-            $this->expectExceptionMessageMatches('/^The (.*) directory must be present and writable.$/');
-        } else {
-            $this->expectExceptionMessageRegExp('/^The (.*) directory must be present and writable.$/');
-        }
+        $this->expectExceptionMessageMatches('/^The (.*) directory must be present and writable.$/');
 
         $repo = new ProviderRepository(m::mock(ApplicationContract::class), $files = m::mock(Filesystem::class), __DIR__.'/cache/services.php');
         $files->shouldReceive('replace')->never();
