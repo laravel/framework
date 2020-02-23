de178be73d5f823500f8aba32ba17871490ec797
Sjors Ottjes <sjorsottjes@gmail.com>
2020-02-23 14:45:00 +0100

    re-add method docblocks

--- src/Illuminate/Support/Facades/Http.php
+++ src/Illuminate/Support/Facades/Http.php
@@ -6,7 +6,32 @@ use Illuminate\Http\Client\Factory;
 use Illuminate\Support\Testing\Fakes\HttpFake;
 
 /**
- * @mixin \Illuminate\Http\Client\PendingRequest
+ * @method static \Illuminate\Http\Client\PendingRequest asJson()
+ * @method static \Illuminate\Http\Client\PendingRequest asForm()
+ * @method static \Illuminate\Http\Client\PendingRequest attach(string $name, string $contents, string|null $filename = null, array $headers = [])
+ * @method static \Illuminate\Http\Client\PendingRequest asMultipart()
+ * @method static \Illuminate\Http\Client\PendingRequest bodyFormat(string $format)
+ * @method static \Illuminate\Http\Client\PendingRequest contentType(string $contentType)
+ * @method static \Illuminate\Http\Client\PendingRequest acceptJson()
+ * @method static \Illuminate\Http\Client\PendingRequest accept(string $contentType)
+ * @method static \Illuminate\Http\Client\PendingRequest withHeaders(array $headers)
+ * @method static \Illuminate\Http\Client\PendingRequest withBasicAuth(string $username, string $password)
+ * @method static \Illuminate\Http\Client\PendingRequest withDigestAuth(string $username, string $password)
+ * @method static \Illuminate\Http\Client\PendingRequest withToken(string $token, string $type = 'Bearer')
+ * @method static \Illuminate\Http\Client\PendingRequest withCookies(array $cookies)
+ * @method static \Illuminate\Http\Client\PendingRequest withoutRedirecting()
+ * @method static \Illuminate\Http\Client\PendingRequest withoutVerifying()
+ * @method static \Illuminate\Http\Client\PendingRequest timeout(int $seconds)
+ * @method static \Illuminate\Http\Client\PendingRequest withOptions(array $options)
+ * @method static \Illuminate\Http\Client\PendingRequest beforeSending(callable $callback)
+ * @method static \Illuminate\Http\Client\Response get(string $url, array $query = [])
+ * @method static \Illuminate\Http\Client\Response post(string $url, array $data = [])
+ * @method static \Illuminate\Http\Client\Response patch(string $url, array $data = [])
+ * @method static \Illuminate\Http\Client\Response put(string $url, array $data = [])
+ * @method static \Illuminate\Http\Client\Response delete(string $url, array $data = [])
+ * @method static \Illuminate\Http\Client\Response send(string $method, string $url, array $options = [])
+ *
+ * @see \Illuminate\Http\Client\PendingRequest
  */
 class Http extends Facade
 {
