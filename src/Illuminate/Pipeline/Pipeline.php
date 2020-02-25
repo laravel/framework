20bde392ecb8cccedf6b1f022868da695c625843
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-02-25 07:46:39 +0000

    Fixed comment alignment

--- src/Illuminate/Pipeline/Pipeline.php
+++ src/Illuminate/Pipeline/Pipeline.php
@@ -147,8 +147,8 @@ class Pipeline implements PipelineContract
             return function ($passable) use ($stack, $pipe) {
                 try {
                     if (is_callable($pipe)) {
-                        // If the pipe is callable, we will just call it directly but
-                        // otherwise we'll resolve the pipes out of the container and call it with
+                        // If the pipe is a callable, then we will call it directly, but otherwise we
+                        // will resolve the pipes out of the dependency container and call it with
                         // the appropriate method and arguments, returning the results back out.
                         return $pipe($passable, $stack);
                     } elseif (! is_object($pipe)) {
