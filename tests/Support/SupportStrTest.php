ec30b5ecf94071058ac19e88c93126d7c0275bdf
ostap <ostap34php@gmail.com>
2020-02-28 13:46:50 +0200

    add more tests

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -129,7 +129,15 @@ class SupportStrTest extends TestCase
 
     public function testStrBetween()
     {
+        $this->assertSame('abc', Str::between('abc', '', 'c'));
+        $this->assertSame('abc', Str::between('abc', 'a', ''));
+        $this->assertSame('abc', Str::between('abc', '', ''));
+        $this->assertSame('b', Str::between('abc', 'a', 'c'));
+        $this->assertSame('b', Str::between('dddabc', 'a', 'c'));
+        $this->assertSame('b', Str::between('abcddd', 'a', 'c'));
+        $this->assertSame('b', Str::between('dddabcddd', 'a', 'c'));
         $this->assertSame('nn', Str::between('hannah', 'ha', 'ah'));
+        $this->assertSame('a]ab[b', Str::between('[a]ab[b]', '[', ']'));
         $this->assertSame('foo', Str::between('foofoobar', 'foo', 'bar'));
         $this->assertSame('bar', Str::between('foobarbar', 'foo', 'bar'));
     }
