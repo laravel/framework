33461dcbb77702d444ec0e04a55c2e7ff97521b1
Iman <imanghafoori1@gmail.com>
2020-03-01 20:48:39 +0330

    add test for view component class (#31665)

--- tests/View/ViewComponentTest.php
+++ tests/View/ViewComponentTest.php
@@ -17,6 +17,34 @@ class ViewComponentTest extends TestCase
         $this->assertEquals('world', $variables['hello']());
         $this->assertEquals('taylor', $variables['hello']('taylor'));
     }
+
+    public function testPublicMethodsWithNoArgsAreEagerlyInvokedAndNotCached()
+    {
+        $component = new TestSampleViewComponent;
+
+        $this->assertEquals(0, $component->counter);
+        $variables = $component->data();
+        $this->assertEquals(1, $component->counter);
+
+        $this->assertEquals('noArgs val', $variables['noArgs']);
+        $this->assertEquals(0, $variables['counter']);
+
+        // make sure non-public members are not invoked nor counted.
+        $this->assertEquals(1, $component->counter);
+        $this->assertArrayHasKey('publicHello', $variables);
+        $this->assertArrayNotHasKey('protectedHello', $variables);
+        $this->assertArrayNotHasKey('privateHello', $variables);
+
+        $this->assertArrayNotHasKey('protectedCounter', $variables);
+        $this->assertArrayNotHasKey('privateCounter', $variables);
+
+        // test each time we invoke data(), the non-argument methods are invoked
+        $this->assertEquals(1, $component->counter);
+        $component->data();
+        $this->assertEquals(2, $component->counter);
+        $component->data();
+        $this->assertEquals(3, $component->counter);
+    }
 }
 
 class TestViewComponent extends Component
@@ -31,5 +59,43 @@ class TestViewComponent extends Component
     public function hello($string = 'world')
     {
         return $string;
+    }
+}
+
+class TestSampleViewComponent extends Component
+{
+    public $counter = 0;
+
+    protected $protectedCounter = 0;
+
+    private $privateCounter = 0;
+
+    public function render()
+    {
+        return 'test';
+    }
+
+    public function publicHello($string = 'world')
+    {
+        $this->counter = 100;
+
+        return $string;
+    }
+
+    public function noArgs()
+    {
+        $this->counter++;
+
+        return 'noArgs val';
+    }
+
+    protected function protectedHello()
+    {
+        $this->counter++;
+    }
+
+    private function privateHello()
+    {
+        $this->counter++;
     }
 }
