17567d488b88450865f08fa0cf4137df1cb62701
innoflash <innocentmazando@gmail.com>
2020-03-02 17:05:52 +0200

    Stub publish visual selecting

--- src/Illuminate/Foundation/Console/StubPublishCommand.php
+++ src/Illuminate/Foundation/Console/StubPublishCommand.php
@@ -2,6 +2,8 @@
 
 namespace Illuminate\Foundation\Console;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 
@@ -12,7 +14,7 @@ class StubPublishCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'stub:publish';
+    protected $signature = 'stub:publish {--s|select : Allows you to select stubs to publish}';
 
     /**
      * The console command description.
@@ -28,30 +30,48 @@ class StubPublishCommand extends Command
      */
     public function handle()
     {
-        if (! is_dir($stubsPath = $this->laravel->basePath('stubs'))) {
+        if (!is_dir($stubsPath = $this->laravel->basePath('stubs'))) {
             (new Filesystem)->makeDirectory($stubsPath);
         }
 
-        $files = [
-            __DIR__.'/stubs/job.queued.stub' => $stubsPath.'/job.queued.stub',
-            __DIR__.'/stubs/job.stub' => $stubsPath.'/job.stub',
-            __DIR__.'/stubs/job.stub' => $stubsPath.'/job.stub',
-            __DIR__.'/stubs/model.pivot.stub' => $stubsPath.'/model.pivot.stub',
-            __DIR__.'/stubs/model.stub' => $stubsPath.'/model.stub',
-            __DIR__.'/stubs/test.stub' => $stubsPath.'/test.stub',
-            __DIR__.'/stubs/test.unit.stub' => $stubsPath.'/test.unit.stub',
-            realpath(__DIR__.'/../../Database/Migrations/stubs/migration.create.stub') => $stubsPath.'/migration.create.stub',
-            realpath(__DIR__.'/../../Database/Migrations/stubs/migration.stub') => $stubsPath.'/migration.stub',
-            realpath(__DIR__.'/../../Database/Migrations/stubs/migration.update.stub') => $stubsPath.'/migration.update.stub',
-            realpath(__DIR__.'/../../Routing/Console/stubs/controller.api.stub') => $stubsPath.'/controller.api.stub',
-            realpath(__DIR__.'/../../Routing/Console/stubs/controller.invokable.stub') => $stubsPath.'/controller.invokable.stub',
-            realpath(__DIR__.'/../../Routing/Console/stubs/controller.model.api.stub') => $stubsPath.'/controller.model.api.stub',
-            realpath(__DIR__.'/../../Routing/Console/stubs/controller.model.stub') => $stubsPath.'/controller.model.stub',
-            realpath(__DIR__.'/../../Routing/Console/stubs/controller.nested.api.stub') => $stubsPath.'/controller.nested.api.stub',
-            realpath(__DIR__.'/../../Routing/Console/stubs/controller.nested.stub') => $stubsPath.'/controller.nested.stub',
-            realpath(__DIR__.'/../../Routing/Console/stubs/controller.plain.stub') => $stubsPath.'/controller.plain.stub',
-            realpath(__DIR__.'/../../Routing/Console/stubs/controller.stub') => $stubsPath.'/controller.stub',
-        ];
+        $files = collect([
+            __DIR__ . '/stubs/job.queued.stub' => $stubsPath . '/job.queued.stub',
+            __DIR__ . '/stubs/job.stub' => $stubsPath . '/job.stub',
+            __DIR__ . '/stubs/job.stub' => $stubsPath . '/job.stub',
+            __DIR__ . '/stubs/model.pivot.stub' => $stubsPath . '/model.pivot.stub',
+            __DIR__ . '/stubs/model.stub' => $stubsPath . '/model.stub',
+            __DIR__ . '/stubs/test.stub' => $stubsPath . '/test.stub',
+            __DIR__ . '/stubs/test.unit.stub' => $stubsPath . '/test.unit.stub',
+            realpath(__DIR__ . '/../../Database/Migrations/stubs/migration.create.stub') => $stubsPath . '/migration.create.stub',
+            realpath(__DIR__ . '/../../Database/Migrations/stubs/migration.stub') => $stubsPath . '/migration.stub',
+            realpath(__DIR__ . '/../../Database/Migrations/stubs/migration.update.stub') => $stubsPath . '/migration.update.stub',
+            realpath(__DIR__ . '/../../Routing/Console/stubs/controller.api.stub') => $stubsPath . '/controller.api.stub',
+            realpath(__DIR__ . '/../../Routing/Console/stubs/controller.invokable.stub') => $stubsPath . '/controller.invokable.stub',
+            realpath(__DIR__ . '/../../Routing/Console/stubs/controller.model.api.stub') => $stubsPath . '/controller.model.api.stub',
+            realpath(__DIR__ . '/../../Routing/Console/stubs/controller.model.stub') => $stubsPath . '/controller.model.stub',
+            realpath(__DIR__ . '/../../Routing/Console/stubs/controller.nested.api.stub') => $stubsPath . '/controller.nested.api.stub',
+            realpath(__DIR__ . '/../../Routing/Console/stubs/controller.nested.stub') => $stubsPath . '/controller.nested.stub',
+            realpath(__DIR__ . '/../../Routing/Console/stubs/controller.plain.stub') => $stubsPath . '/controller.plain.stub',
+            realpath(__DIR__ . '/../../Routing/Console/stubs/controller.stub') => $stubsPath . '/controller.stub',
+        ]);
+
+        if ($this->option('select')) {
+            $stubsKeys = collect($files->keys());
+
+            $stubOptions = $stubsKeys->map(function ($stub, $key) {
+                return $this->getStubName($stub);
+            })->toArray();
+
+            $selectedStubNames = $this->choice('Select the stubs you want to publish', $stubOptions, 4, null, true);
+
+            $selectedStubs = collect($selectedStubNames)->map(function ($stub) {
+                return $this->getOriginalStubName($stub);
+            })->toArray();
+
+            $files = $files->filter(function ($file, $key) use ($selectedStubs) {
+                return Str::of($key)->endsWith($selectedStubs);
+            });
+        }
 
         foreach ($files as $from => $to) {
             file_put_contents($to, file_get_contents($from));
@@ -59,4 +79,37 @@ class StubPublishCommand extends Command
 
         $this->info('Stubs published successfully.');
     }
+
+    /**
+     * Formats a stub name to a more readable name to display on the console
+     *
+     * @param string $stubpath
+     * @return string
+     */
+    private function getStubName($stubpath): string
+    {
+        $nameArray = explode('/', $stubpath);
+        $stub = Arr::last($nameArray);
+
+        return Str::of($stub)
+            ->replaceLast('.stub', '')
+            ->replace('.', ' ')
+            ->ucfirst();
+    }
+
+    /**
+     * Converts the stud name into the 
+     *
+     * @param string $stubName
+     * @return string
+     */
+    private function getOriginalStubName(string $stubName): string
+    {
+        return Str::of($stubName)
+            ->lower()
+            ->replace(' ', '.')
+            ->append('.stub')
+            ->prepend('stubs/');
+    }
 }
+
