47ab302fe3a47752012e4fde8cf61149f4e5a77d
Jacob Stair <jacob.stair@gmail.com>
2020-03-06 18:20:16 -0500

    Use getTable() instead of class name

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -1530,7 +1530,7 @@ abstract class Model implements Arrayable, ArrayAccess, Jsonable, JsonSerializab
      */
     public function getForeignKey()
     {
-        return Str::snake(class_basename($this)).'_'.$this->getKeyName();
+        return Str::singular($this->getTable()).'_'.$this->getKeyName();
     }
 
     /**
