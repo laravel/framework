fa901c34a2c78941e48cc5edc5e43e81f4d6f3f8
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-03-06 10:05:42 +0000

    Added additional Str::slug tests (#31791)

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -178,6 +178,9 @@ class SupportStrTest extends TestCase
         $this->assertSame('hello_world', Str::slug('hello_world', '_'));
         $this->assertSame('user-at-host', Str::slug('user@host'));
         $this->assertSame('سلام-دنیا', Str::slug('سلام دنیا', '-', null));
+        $this->assertSame('sometext', Str::slug('some text', ''));
+        $this->assertSame('', Str::slug('', ''));
+        $this->assertSame('', Str::slug(''));
     }
 
     public function testStrStart()
