b614a93ee3432be3960af59164f9b18f27023f2e
Lucas Michot <lucas@semalead.com>
2020-03-09 23:04:25 +0100

    Fix spacing.

--- tests/Translation/TranslationMessageSelectorTest.php
+++ tests/Translation/TranslationMessageSelectorTest.php
@@ -22,49 +22,49 @@ class TranslationMessageSelectorTest extends TestCase
      */
     public function chooseTestData()
     {
-            yield ['first', 'first', 1];
-            yield ['first', 'first', 10];
-            yield ['first', 'first|second', 1];
-            yield ['second', 'first|second', 10];
-            yield ['second', 'first|second', 0];
+        yield ['first', 'first', 1];
+        yield ['first', 'first', 10];
+        yield ['first', 'first|second', 1];
+        yield ['second', 'first|second', 10];
+        yield ['second', 'first|second', 0];
 
-            yield ['first', '{0}  first|{1}second', 0];
-            yield ['first', '{1}first|{2}second', 1];
-            yield ['second', '{1}first|{2}second', 2];
-            yield ['first', '{2}first|{1}second', 2];
-            yield ['second', '{9}first|{10}second', 0];
-            yield ['first', '{9}first|{10}second', 1];
-            yield ['', '{0}|{1}second', 0];
-            yield ['', '{0}first|{1}', 1];
-            yield ['first', '{1.3}first|{2.3}second', 1.3];
-            yield ['second', '{1.3}first|{2.3}second', 2.3];
-            yield ['first
-            line', '{1}first
-            line|{2}second', 1];
-            ["first \n
-            line", "{1}first \n
-            line|{2}second", 1];
+        yield ['first', '{0}  first|{1}second', 0];
+        yield ['first', '{1}first|{2}second', 1];
+        yield ['second', '{1}first|{2}second', 2];
+        yield ['first', '{2}first|{1}second', 2];
+        yield ['second', '{9}first|{10}second', 0];
+        yield ['first', '{9}first|{10}second', 1];
+        yield ['', '{0}|{1}second', 0];
+        yield ['', '{0}first|{1}', 1];
+        yield ['first', '{1.3}first|{2.3}second', 1.3];
+        yield ['second', '{1.3}first|{2.3}second', 2.3];
+        yield ['first
+        line', '{1}first
+        line|{2}second', 1];
+        ["first \n
+        line", "{1}first \n
+        line|{2}second", 1];
 
-            yield ['first', '{0}  first|[1,9]second', 0];
-            yield ['second', '{0}first|[1,9]second', 1];
-            yield ['second', '{0}first|[1,9]second', 10];
-            yield ['first', '{0}first|[2,9]second', 1];
-            yield ['second', '[4,*]first|[1,3]second', 1];
-            yield ['first', '[4,*]first|[1,3]second', 100];
-            yield ['second', '[1,5]first|[6,10]second', 7];
-            yield ['first', '[*,4]first|[5,*]second', 1];
-            yield ['second', '[5,*]first|[*,4]second', 1];
-            yield ['second', '[5,*]first|[*,4]second', 0];
+        yield ['first', '{0}  first|[1,9]second', 0];
+        yield ['second', '{0}first|[1,9]second', 1];
+        yield ['second', '{0}first|[1,9]second', 10];
+        yield ['first', '{0}first|[2,9]second', 1];
+        yield ['second', '[4,*]first|[1,3]second', 1];
+        yield ['first', '[4,*]first|[1,3]second', 100];
+        yield ['second', '[1,5]first|[6,10]second', 7];
+        yield ['first', '[*,4]first|[5,*]second', 1];
+        yield ['second', '[5,*]first|[*,4]second', 1];
+        yield ['second', '[5,*]first|[*,4]second', 0];
 
-            yield ['first', '{0}first|[1,3]second|[4,*]third', 0];
-            yield ['second', '{0}first|[1,3]second|[4,*]third', 1];
-            yield ['third', '{0}first|[1,3]second|[4,*]third', 9];
+        yield ['first', '{0}first|[1,3]second|[4,*]third', 0];
+        yield ['second', '{0}first|[1,3]second|[4,*]third', 1];
+        yield ['third', '{0}first|[1,3]second|[4,*]third', 9];
 
-            yield ['first', 'first|second|third', 1];
-            yield ['second', 'first|second|third', 9];
-            yield ['second', 'first|second|third', 0];
+        yield ['first', 'first|second|third', 1];
+        yield ['second', 'first|second|third', 9];
+        yield ['second', 'first|second|third', 0];
 
-            yield ['first', '{0}  first | { 1 } second', 0];
-            yield ['first', '[4,*]first | [1,3]second', 100];
+        yield ['first', '{0}  first | { 1 } second', 0];
+        yield ['first', '[4,*]first | [1,3]second', 100];
     }
 }
