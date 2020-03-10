24c18182a82ee24be62d2ac1c6793c237944cda8
Mohamed Said <themohamedsaid@gmail.com>
2020-03-10 17:33:22 +0200

    bring --daemon option back

--- src/Illuminate/Queue/Console/WorkCommand.php
+++ src/Illuminate/Queue/Console/WorkCommand.php
@@ -22,6 +22,7 @@ class WorkCommand extends Command
     protected $signature = 'queue:work
                             {connection? : The name of the queue connection to work}
                             {--queue= : The names of the queues to work}
+                            {--daemon : Run the worker in daemon mode (Deprecated)}
                             {--once : Only process the next job on the queue}
                             {--stop-when-empty : Stop when the queue is empty}
                             {--delay=0 : The number of seconds to delay failed jobs}
