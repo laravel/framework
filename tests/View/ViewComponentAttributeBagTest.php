fb61ce290a25cf65dd3fc9911a4bac87d416b681
Mark Fullbrook <mark.fullbrook@universalpacking.co.uk>
2020-03-12 09:51:08 +0000

    Fix ComponentAttributeBag -  merge defaults and doesn't override template attributes

--- tests/View/ViewComponentAttributeBagTest.php
+++ tests/View/ViewComponentAttributeBagTest.php
@@ -12,9 +12,11 @@ class ViewComponentAttributeBagTest extends TestCase
         $bag = new ComponentAttributeBag(['class' => 'font-bold', 'name' => 'test']);
 
         $this->assertSame('class="mt-4 font-bold" name="test"', (string) $bag->merge(['class' => 'mt-4']));
-        $this->assertSame('class="mt-4 font-bold" name="foo"', (string) $bag->merge(['class' => 'mt-4', 'name' => 'foo']));
+        $this->assertSame('class="mt-4 font-bold" name="test"', (string) $bag->merge(['class' => 'mt-4', 'name' => 'foo']));
+        $this->assertSame('class="mt-4 font-bold" id="bar" name="test"', (string) $bag->merge(['class' => 'mt-4', 'id' => 'bar']));
         $this->assertSame('class="mt-4 font-bold" name="test"', (string) $bag(['class' => 'mt-4']));
         $this->assertSame('class="mt-4 font-bold"', (string) $bag->only('class')->merge(['class' => 'mt-4']));
+        $this->assertSame('class="italic" name="test"', (string) $bag->only('name')->merge(['class' => 'italic', 'name' => 'default']));
         $this->assertSame('class="mt-4 font-bold"', (string) $bag->merge(['class' => 'mt-4'])->only('class'));
         $this->assertSame('class="mt-4 font-bold"', (string) $bag->only('class')(['class' => 'mt-4']));
         $this->assertSame('font-bold', $bag->get('class'));
