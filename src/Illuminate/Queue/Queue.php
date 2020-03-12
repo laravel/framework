569e028efe943448ec824bbbc06b7a124acff070
Graham Campbell <graham@alt-three.com>
2020-03-12 11:34:48 +0000

    Use CallQueuedClosure::create

--- src/Illuminate/Queue/Queue.php
+++ src/Illuminate/Queue/Queue.php
@@ -88,7 +88,7 @@ abstract class Queue
     protected function createPayload($job, $queue, $data = '')
     {
         if ($job instanceof Closure) {
-            $job = new CallQueuedClosure(new SerializableClosure($job));
+            $job = new CallQueuedClosure::create($job);
         }
 
         $payload = json_encode($this->createPayloadArray($job, $queue, $data));
