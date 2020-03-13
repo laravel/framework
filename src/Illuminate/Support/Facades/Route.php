68d93642f745acb9360545eb06c07d220e65ee06
Mateus Junges <mateus@junges.dev>
2020-03-13 16:54:55 -0300

    Adds `getCurrentRoute` method to php doc for IDE Auto completion (#31955)

--- src/Illuminate/Support/Facades/Route.php
+++ src/Illuminate/Support/Facades/Route.php
@@ -35,6 +35,7 @@ namespace Illuminate\Support\Facades;
  * @method static \Illuminate\Routing\Route current()
  * @method static string|null currentRouteName()
  * @method static string|null currentRouteAction()
+ * @method static \Illuminate\Routing\Route getCurrentRoute()
  *
  * @see \Illuminate\Routing\Router
  */
