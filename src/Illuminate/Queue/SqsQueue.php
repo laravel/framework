20b85bbad6c6800630fb9303d514e4aec6c52c1d
Rob Gordijn <R.Gordijn@hotelspecials.nl>
2020-03-24 22:06:34 +0100

    Inverted to align with the rest of the method

--- src/Illuminate/Queue/SqsQueue.php
+++ src/Illuminate/Queue/SqsQueue.php
@@ -149,9 +149,9 @@ class SqsQueue extends Queue implements QueueContract
     {
         $queue = $queue ?: $this->default;
 
-        $prefix = Str::endsWith($this->prefix, ['-', '_']) === true
-            ? $this->prefix
-            : rtrim($this->prefix, '/').'/';
+        $prefix = Str::endsWith($this->prefix, ['-', '_']) === false
+            ? rtrim($this->prefix, '/').'/'
+            : $this->prefix;
 
         return filter_var($queue, FILTER_VALIDATE_URL) === false
             ? $prefix.Str::finish($queue, $this->suffix)
