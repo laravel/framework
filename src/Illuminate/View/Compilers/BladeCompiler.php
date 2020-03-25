c2d037c78625b72749f3de2ff4d39bb9547e6582
Nguyen You <tunt081295@gmail.com>
2020-03-25 20:43:58 +0700

    correct argument data type

--- src/Illuminate/View/Compilers/BladeCompiler.php
+++ src/Illuminate/View/Compilers/BladeCompiler.php
@@ -178,7 +178,7 @@ class BladeCompiler extends Compiler implements CompilerInterface
     protected function getOpenAndClosingPhpTokens($contents)
     {
         return collect(token_get_all($contents))
-            ->pluck(0)
+            ->pluck('0')
             ->filter(function ($token) {
                 return in_array($token, [T_OPEN_TAG, T_OPEN_TAG_WITH_ECHO, T_CLOSE_TAG]);
             });
