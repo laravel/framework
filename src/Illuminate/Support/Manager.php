cdf7ebc8208e41d30a4afc389328dcf33ca9069a
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-03-30 14:36:36 +0100

    [8.x] Removed deprecated code (#32092)
    * Removed deprecated code
    
    * Update WorkCommand.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Support/Manager.php
+++ src/Illuminate/Support/Manager.php
@@ -16,15 +16,6 @@ abstract class Manager
     protected $container;
 
     /**
-     * The container instance.
-     *
-     * @var \Illuminate\Contracts\Container\Container
-     *
-     * @deprecated Use the $container property instead.
-     */
-    protected $app;
-
-    /**
      * The configuration repository instance.
      *
      * @var \Illuminate\Contracts\Config\Repository
