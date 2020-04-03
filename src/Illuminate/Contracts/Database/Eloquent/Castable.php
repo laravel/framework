a951a9a70c819786f9dbf28bf57a8fda7c8df76f
Brent Roose <brent@spatie.be>
2020-04-03 14:39:11 +0200

    Support returning an instance of a caster

--- src/Illuminate/Contracts/Database/Eloquent/Castable.php
+++ src/Illuminate/Contracts/Database/Eloquent/Castable.php
@@ -7,7 +7,7 @@ interface Castable
     /**
      * Get the name of the caster class to use when casting from / to this cast target.
      *
-     * @return string
+     * @return string|\Illuminate\Contracts\Database\Eloquent\CastsAttributes|\Illuminate\Contracts\Database\Eloquent\CastsInboundAttributes
      */
     public static function castUsing();
 }
