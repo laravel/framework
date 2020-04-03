cb390336d75a347b9e4065064d7f12bcbe3994c7
Graham Campbell <graham@alt-three.com>
2020-04-03 12:14:46 +0100

    Fixed PHPUnit warnings

--- src/Illuminate/Foundation/Testing/Assert.php
+++ src/Illuminate/Foundation/Testing/Assert.php
@@ -5,6 +5,10 @@ namespace Illuminate\Foundation\Testing;
 use ArrayAccess;
 use Illuminate\Foundation\Testing\Constraints\ArraySubset;
 use PHPUnit\Framework\Assert as PHPUnit;
+use PHPUnit\Framework\Constraint\DirectoryExists;
+use PHPUnit\Framework\Constraint\FileExists;
+use PHPUnit\Framework\Constraint\LogicalNot;
+use PHPUnit\Framework\Constraint\RegularExpression;
 use PHPUnit\Framework\InvalidArgumentException;
 use PHPUnit\Runner\Version;
 use PHPUnit\Util\InvalidArgumentHelper;
@@ -46,6 +50,43 @@ if (class_exists(Version::class) && (int) Version::series()[0] >= 8) {
 
             PHPUnit::assertThat($array, $constraint, $msg);
         }
+
+        /**
+         * Asserts that a file does not exist.
+         *
+         * @param  string  $filename
+         * @param  string  $message
+         * @return void
+         */
+        public static function assertFileDoesNotExist(string $filename, string $message = ''): void
+        {
+            static::assertThat($filename, new LogicalNot(new FileExists), $message);
+        }
+
+        /**
+         * Asserts that a directory does not exist.
+         *
+         * @param  string  $directory
+         * @param  string  $message
+         * @return void
+         */
+        public static function assertDirectoryDoesNotExist(string $directory, string $message = ''): void
+        {
+            static::assertThat($directory, new LogicalNot(new DirectoryExists), $message);
+        }
+
+        /**
+         * Asserts that a string matches a given regular expression.
+         *
+         * @param  string  $pattern
+         * @param  string  $string
+         * @param  string  $message
+         * @return void
+         */
+        public static function assertMatchesRegularExpression(string $pattern, string $string, string $message = ''): void
+        {
+            static::assertThat($string, new RegularExpression($pattern), $message);
+        }
     }
 } else {
     /**
@@ -65,6 +106,43 @@ if (class_exists(Version::class) && (int) Version::series()[0] >= 8) {
         public static function assertArraySubset($subset, $array, bool $checkForIdentity = false, string $msg = ''): void
         {
             PHPUnit::assertArraySubset($subset, $array, $checkForIdentity, $msg);
+        }
+
+        /**
+         * Asserts that a file does not exist.
+         *
+         * @param  string  $filename
+         * @param  string  $message
+         * @return void
+         */
+        public static function assertFileDoesNotExist(string $filename, string $message = ''): void
+        {
+            static::assertThat($filename, new LogicalNot(new FileExists), $message);
+        }
+
+        /**
+         * Asserts that a directory does not exist.
+         *
+         * @param  string  $directory
+         * @param  string  $message
+         * @return void
+         */
+        public static function assertDirectoryDoesNotExist(string $directory, string $message = ''): void
+        {
+            static::assertThat($directory, new LogicalNot(new DirectoryExists), $message);
+        }
+
+        /**
+         * Asserts that a string matches a given regular expression.
+         *
+         * @param  string  $pattern
+         * @param  string  $string
+         * @param  string  $message
+         * @return void
+         */
+        public static function assertMatchesRegularExpression(string $pattern, string $string, string $message = ''): void
+        {
+            static::assertThat($string, new RegularExpression($pattern), $message);
         }
     }
 }
