587ae835667b73b81db219548ae44ffc0b169765
Michal Procházka <m_prochazka@utb.cz>
2020-04-09 23:43:20 +0200

    [7.x] allow store resource into postgresql bytea

--- src/Illuminate/Database/PostgresConnection.php
+++ src/Illuminate/Database/PostgresConnection.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Query\Grammars\PostgresGrammar as QueryGrammar;
 use Illuminate\Database\Query\Processors\PostgresProcessor;
 use Illuminate\Database\Schema\Grammars\PostgresGrammar as SchemaGrammar;
 use Illuminate\Database\Schema\PostgresBuilder;
+use PDO;
 
 class PostgresConnection extends Connection
 {
@@ -62,5 +63,32 @@ class PostgresConnection extends Connection
     protected function getDoctrineDriver()
     {
         return new DoctrineDriver;
+    }
+
+    /**
+     * Bind values to their parameters in the given statement.
+     *
+     * @param  \PDOStatement  $statement
+     * @param  array  $bindings
+     * @return void
+     */
+    public function bindValues($statement, $bindings)
+    {
+        foreach ($bindings as $key => $value) {
+            $pdoParam = null;
+
+            if (is_int($value)) {
+                $pdoParam = PDO::PARAM_INT;
+            } elseif (is_resource($value)) {
+                $pdoParam = PDO::PARAM_LOB;
+            } else {
+                $pdoParam = PDO::PARAM_STR;
+            }
+
+            $statement->bindValue(
+                is_string($key) ? $key : $key + 1, $value,
+                $pdoParam
+            );
+        }
     }
 }
