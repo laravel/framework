3d1ef5d04be32ce74ce6dee080dc3492000abae1
Musa <40173603+voyula@users.noreply.github.com>
2020-04-13 09:57:26 +0300

    Update SupportArrTest.php

--- tests/Support/SupportArrTest.php
+++ tests/Support/SupportArrTest.php
@@ -131,8 +131,8 @@ class SupportArrTest extends TestCase
         $this->assertTrue(Arr::exists([null], 0));
         $this->assertTrue(Arr::exists(['a' => 1], 'a'));
         $this->assertTrue(Arr::exists(['a' => null], 'a'));
-        $this->assertTrue(Arr::exists(new Collection(['a' => null]), 'a'));
 
+        $this->assertFalse(Arr::exists(new Collection(['a' => null]), 'a'));
         $this->assertFalse(Arr::exists([1], 1));
         $this->assertFalse(Arr::exists([null], 1));
         $this->assertFalse(Arr::exists(['a' => 1], 0));
