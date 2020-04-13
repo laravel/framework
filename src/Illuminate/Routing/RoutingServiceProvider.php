0eb3787a3396071d8e08fbdc66219792446faa8b
Markus Podar <markus@fischer.name>
2020-04-13 16:04:23 +0200

    Use BindingResolutionException to signal problem with container resolution (#32349)
    As part of static analysis sometimes abstracts get resolved which aren't
    resolvable => that's totally fine but it would help to use a common
    exception to signal this.
    
    I figured BindingResolutionException would be the correct one. For example,
    it's caught in cases like `\Illuminate\Foundation\Exceptions\Handler::whoopsHandler`
    and IMHO this would also benefit from a streamlined exception.

--- src/Illuminate/Routing/RoutingServiceProvider.php
+++ src/Illuminate/Routing/RoutingServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Illuminate\Routing;
 
-use Exception;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\Routing\ResponseFactory as ResponseFactoryContract;
 use Illuminate\Contracts\Routing\UrlGenerator as UrlGeneratorContract;
 use Illuminate\Contracts\View\Factory as ViewFactoryContract;
@@ -144,7 +144,7 @@ class RoutingServiceProvider extends ServiceProvider
                 return (new DiactorosFactory)->createRequest($app->make('request'));
             }
 
-            throw new Exception('Unable to resolve PSR request. Please install symfony/psr-http-message-bridge and nyholm/psr7.');
+            throw new BindingResolutionException('Unable to resolve PSR request. Please install symfony/psr-http-message-bridge and nyholm/psr7.');
         });
     }
 
@@ -164,7 +164,7 @@ class RoutingServiceProvider extends ServiceProvider
                 return new ZendPsrResponse;
             }
 
-            throw new Exception('Unable to resolve PSR response. Please install nyholm/psr7.');
+            throw new BindingResolutionException('Unable to resolve PSR response. Please install nyholm/psr7.');
         });
     }
 
