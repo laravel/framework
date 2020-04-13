86dbb2500308c7cf529daf829436ce1bced88d4c
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-04-13 20:37:30 +0100

    Depreciate the elixir function (#32366)

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -416,6 +416,8 @@ if (! function_exists('elixir')) {
      * @return string
      *
      * @throws \InvalidArgumentException
+     *
+     * @deprecated Use Laravel Mix instead.
      */
     function elixir($file, $buildDirectory = 'build')
     {
