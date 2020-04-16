c13adca24b75dcd17390d28bd82e83a0bf697a7b
Niels Mokkentstorm <nielsmokkenstorm@gmail.com>
2020-04-16 16:22:17 +0200

    style fixes

--- tests/Integration/Http/DisablesGlobalMiddlewareTest.php
+++ tests/Integration/Http/DisablesGlobalMiddlewareTest.php
@@ -2,13 +2,11 @@
 
 namespace Illuminate\Tests\Integration\Http;
 
-use Illuminate\Foundation\Http\Kernel as BaseKernel;
+use Closure;
+use Orchestra\Testbench\TestCase;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Contracts\Routing\Registrar;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Route;
-use Orchestra\Testbench\TestCase;
-use Closure;
+use Illuminate\Foundation\Http\Kernel as BaseKernel;
 
 /**
  * @group integration
@@ -28,13 +26,15 @@ class DisablesGlobalMiddlewareTest extends TestCase
 
     public function testItCanDisableGlobalMiddleware()
     {
-        $action = function () {return response('route-response');};
-        
+        $action = function () {
+            return response('route-response');
+        };
+
         $router = $this->app->make(Registrar::class);
-        
-        $router->get('foo-route', $action); 
+
+        $router->get('foo-route', $action);
         $router->get('bar-route', $action)->withoutMiddleware(DummyMiddleware::class);
-        
+
         $this->get('foo-route')->assertSee('middleware-response');
         $this->get('bar-route')->assertSee('route-response');
     }
@@ -65,6 +65,6 @@ class DummyKernel extends BaseKernel
      * @var array
      */
     protected $middleware = [
-        DummyMiddleware::class
+        DummyMiddleware::class,
     ];
 }
