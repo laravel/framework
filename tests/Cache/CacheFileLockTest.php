631000ab332411e7c1bfd3dbc7a79ea702fb5155
Roger Vilà <rogervila@me.com>
2020-04-16 09:02:17 +0200

    Update CacheFileLockTest.php

--- tests/Cache/CacheFileLockTest.php
+++ tests/Cache/CacheFileLockTest.php
@@ -4,22 +4,26 @@ namespace Illuminate\Tests\Cache;
 
 use Illuminate\Cache\FileStore;
 use Illuminate\Cache\FileLock;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Carbon;
 use PHPUnit\Framework\TestCase;
 
 class CacheFileLockTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->directory = sys_get_temp_dir();
+        $this->filesystem = new Filesystem();
+    }
+
     public function testLockCanBeAcquired()
     {
-        $name = uniqid();
         $owner = 'owner';
         $seconds = 2;
-        $directory = sys_get_temp_dir();
-        $filesystem = new Filesystem();
-        $store = new FileStore($filesystem, $directory);
-        $lock = new FileLock($store, $name, $seconds, $owner);
+        $store = new FileStore($this->filesystem, $this->directory);
+        $lock = new FileLock($store, uniqid(), $seconds, $owner);
 
         // Lock can be acquired since it does not exist
         $this->assertTrue($lock->acquire());
@@ -35,5 +39,91 @@ class CacheFileLockTest extends TestCase
 
         // Lock should be should be available after a release
         $this->assertTrue($lock->acquire());
+
+        // Release the lock at the end of the test
+        $lock->release();
+    }
+
+    public function testCannotAcquireLockTwice()
+    {
+        $store = new FileStore($this->filesystem, $this->directory);
+        $lock = $store->lock(uniqid(), 10);
+
+        $this->assertTrue($lock->acquire());
+        $this->assertFalse($lock->acquire());
+    }
+
+    public function testCanAcquireLockAgainAfterExpiry()
+    {
+        Carbon::setTestNow(Carbon::now());
+        $store = new FileStore($this->filesystem, $this->directory);
+        $lock = $store->lock(uniqid(), 10);
+        $lock->acquire();
+        Carbon::setTestNow(Carbon::now()->addSeconds(10));
+
+        $this->assertTrue($lock->acquire());
+
+        $lock->release();
+    }
+
+    public function testLockExpirationLowerBoundary()
+    {
+        Carbon::setTestNow(Carbon::now());
+        $store = new FileStore($this->filesystem, $this->directory);
+        $lock = $store->lock(uniqid(), 10);
+        $lock->acquire();
+        Carbon::setTestNow(Carbon::now()->addSeconds(10)->subMicrosecond());
+
+        $this->assertFalse($lock->acquire());
+
+        $lock->release();
+    }
+
+    public function testLockWithNoExpirationNeverExpires()
+    {
+        Carbon::setTestNow(Carbon::now());
+        $store = new FileStore($this->filesystem, $this->directory);
+        $lock = $store->lock(uniqid());
+        $lock->acquire();
+        Carbon::setTestNow(Carbon::now()->addYears(100));
+
+        $this->assertFalse($lock->acquire());
+
+        $lock->release();
+    }
+
+    public function testCanAcquireLockAfterRelease()
+    {
+        $store = new FileStore($this->filesystem, $this->directory);
+        $lock = $store->lock(uniqid(), 10);
+        $lock->acquire();
+
+        $this->assertTrue($lock->release());
+        $this->assertTrue($lock->acquire());
+
+        $lock->release();
+    }
+
+    public function testAnotherOwnerCannotReleaseLock()
+    {
+        $name = uniqid();
+        $store = new FileStore($this->filesystem, $this->directory);
+        $owner = $store->lock($name, 10);
+        $wannabeOwner = $store->lock($name, 10);
+        $owner->acquire();
+
+        $this->assertFalse($wannabeOwner->release());
+    }
+
+    public function testAnotherOwnerCanForceReleaseALock()
+    {
+        $name = uniqid();
+        $store = new FileStore($this->filesystem, $this->directory);
+        $owner = $store->lock($name, 10);
+        $wannabeOwner = $store->lock($name, 10);
+        $owner->acquire();
+        $wannabeOwner->forceRelease();
+
+        $this->assertTrue($wannabeOwner->acquire());
     }
 }
