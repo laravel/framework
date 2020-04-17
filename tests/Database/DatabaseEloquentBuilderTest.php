0946c3eb93cfe70a895bcae67d47c341d990a781
Michael Tsang <miklcct@gmail.com>
2020-04-17 12:36:35 +0800

    add getGrammar into passthru in Eloquent builder

--- tests/Database/DatabaseEloquentBuilderTest.php
+++ tests/Database/DatabaseEloquentBuilderTest.php
@@ -640,6 +640,11 @@ class DatabaseEloquentBuilderTest extends TestCase
         $builder->getQuery()->shouldReceive('raw')->once()->with('bar')->andReturn('foo');
 
         $this->assertSame('foo', $builder->raw('bar'));
+
+        $builder = $this->getBuilder();
+        $grammar = new Grammar();
+        $builder->getQuery()->shouldReceive('getGrammar')->once()->andReturn($grammar);
+        $this->assertSame($grammar, $builder->getGrammar());
     }
 
     public function testQueryScopes()
