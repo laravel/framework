bced6d3fac1c081ef4f62dd94277984fc819f9d6
Christian Rishøj <christian@rishoj.net>
2020-04-18 04:47:34 +0200

    create relative symlinks by default

--- src/Illuminate/Foundation/Console/StorageLinkCommand.php
+++ src/Illuminate/Foundation/Console/StorageLinkCommand.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Foundation\Console;
 
 use Illuminate\Console\Command;
+use Symfony\Component\HttpFoundation\Request;
 
 class StorageLinkCommand extends Command
 {
@@ -11,7 +12,7 @@ class StorageLinkCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'storage:link';
+    protected $signature = 'storage:link {--absolute : Create links using absolute paths}';
 
     /**
      * The console command description.
@@ -31,6 +32,11 @@ class StorageLinkCommand extends Command
             if (file_exists($link)) {
                 $this->error("The [$link] link already exists.");
             } else {
+                if (! $this->option('absolute') && DIRECTORY_SEPARATOR == '/') {
+                    // Utilize Symfony's Request to find the relative path
+                    $target = Request::create($link)->getRelativeUriForPath($target);
+                }
+
                 $this->laravel->make('files')->link($target, $link);
 
                 $this->info("The [$link] link has been connected to [$target].");
