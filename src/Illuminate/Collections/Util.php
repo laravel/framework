a81a9a08044261e40fd23a75e2d1744891d1db90
Taylor Otwell <taylorotwell@gmail.com>
2020-04-22 08:36:20 -0500

    add file

---
+++ src/Illuminate/Collections/Util.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Illuminate\Collections;
+
+use Closure;
+
+class Util
+{
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public static function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
+    }
+}
