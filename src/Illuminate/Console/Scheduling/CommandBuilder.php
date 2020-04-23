b9c2763ebfcd2eac29ecc900a6623f4a22281715
Taylor Otwell <taylorotwell@gmail.com>
2020-04-23 11:56:23 -0500

    pass status code to schedule finish

--- src/Illuminate/Console/Scheduling/CommandBuilder.php
+++ src/Illuminate/Console/Scheduling/CommandBuilder.php
@@ -52,11 +52,11 @@ class CommandBuilder
         $finished = Application::formatCommandString('schedule:finish').' "'.$event->mutexName().'"';
 
         if (windows_os()) {
-            return 'start /b cmd /c "('.$event->command.' & '.$finished.')'.$redirect.$output.' 2>&1"';
+            return 'start /b cmd /c "('.$event->command.' & '.$finished.' "%errorlevel%")'.$redirect.$output.' 2>&1"';
         }
 
         return $this->ensureCorrectUser($event,
-            '('.$event->command.$redirect.$output.' 2>&1 ; '.$finished.') > '
+            '('.$event->command.$redirect.$output.' 2>&1 ; '.$finished.' "$?") > '
             .ProcessUtils::escapeArgument($event->getDefaultOutput()).' 2>&1 &'
         );
     }
