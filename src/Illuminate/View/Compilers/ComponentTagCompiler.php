aed7859c659923dc7d43bcda1a8722a88bfee2ef
Josh Hanley <josh@hitsystems.com.au>
2020-04-28 13:28:32 +1000

    Add support for adding attribute bag to component

--- src/Illuminate/View/Compilers/ComponentTagCompiler.php
+++ src/Illuminate/View/Compilers/ComponentTagCompiler.php
@@ -99,18 +99,26 @@ class ComponentTagCompiler
                 (?<attributes>
                     (?:
                         \s+
-                        [\w\-:.@]+
-                        (
-                            =
+                        (?:
                             (?:
-                                \\\"[^\\\"]*\\\"
-                                |
-                                \'[^\']*\'
-                                |
-                                [^\'\\\"=<>]+
+                                \{\{\s?\\\$attributes\s?\}\}
+                            )
+                            |
+                            (?:
+                                [\w\-:.@]+
+                                (
+                                    =
+                                    (?:
+                                        \\\"[^\\\"]*\\\"
+                                        |
+                                        \'[^\']*\'
+                                        |
+                                        [^\'\\\"=<>]+
+                                    )
+                                )?
                             )
                         )
-                    ?)*
+                    )*
                     \s*
                 )
                 (?<![\/=\-])
@@ -144,17 +152,25 @@ class ComponentTagCompiler
                 (?<attributes>
                     (?:
                         \s+
-                        [\w\-:.@]+
-                        (
-                            =
+                        (?:
                             (?:
-                                \\\"[^\\\"]*\\\"
-                                |
-                                \'[^\']*\'
-                                |
-                                [^\'\\\"=<>]+
+                                \{\{\s?\\\$attributes\s?\}\}
                             )
-                        )?
+                            |
+                            (?:
+                                [\w\-:.@]+
+                                (
+                                    =
+                                    (?:
+                                        \\\"[^\\\"]*\\\"
+                                        |
+                                        \'[^\']*\'
+                                        |
+                                        [^\'\\\"=<>]+
+                                    )
+                                )?
+                            )
+                        )
                     )*
                     \s*
                 )
@@ -326,6 +342,8 @@ class ComponentTagCompiler
      */
     protected function getAttributesFromAttributeString(string $attributeString)
     {
+        $attributeString = $this->parseAttributeBag($attributeString);
+
         $attributeString = $this->parseBindAttributes($attributeString);
 
         $pattern = '/
@@ -370,6 +388,22 @@ class ComponentTagCompiler
 
             return [$attribute => $value];
         })->toArray();
+    }
+
+    /**
+     * Parse the attribute bag in a given attribute string into it's fully-qualified syntax.
+     *
+     * @param  string  $attributeString
+     * @return string
+     */
+    protected function parseAttributeBag(string $attributeString)
+    {
+        $pattern = "/
+        (?:^|\s+)     # start of the string or whitespace between attributes
+        \{\{\s?(\\\$attributes)\s?\}\} # exact match of attributes being echoed
+        /x";
+
+        return preg_replace($pattern, ' :attributes="$1"', $attributeString);
     }
 
     /**
