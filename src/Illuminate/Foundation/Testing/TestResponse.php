5408e691b7ef832cd9fce45ff56d9ecf39e02fcc
James Dickens <jdickens@intradark.com>
2020-05-04 01:36:25 +1000

    [6.x] Fix assertJsonCount not accepting falsey keys (#32655)
    * Fix assertJsonCount not accepting falsey values
    
    * Update TestResponse.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Foundation/Testing/TestResponse.php
+++ src/Illuminate/Foundation/Testing/TestResponse.php
@@ -684,7 +684,7 @@ class TestResponse implements ArrayAccess
      */
     public function assertJsonCount(int $count, $key = null)
     {
-        if ($key) {
+        if (! is_null($key)) {
             PHPUnit::assertCount(
                 $count, data_get($this->json(), $key),
                 "Failed to assert that the response count matched the expected {$count}"
