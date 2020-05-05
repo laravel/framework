9a862a1fb0122a7cf3e8220905814bdef092bd2b
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-05-05 18:17:41 -0400

    Adds (optional) array to include Eloquent Model observers.

--- src/Illuminate/Foundation/Support/Providers/EventServiceProvider.php
+++ src/Illuminate/Foundation/Support/Providers/EventServiceProvider.php
@@ -23,6 +23,13 @@ class EventServiceProvider extends ServiceProvider
     protected $subscribe = [];
 
     /**
+     * Model observers to register.
+     *
+     * @var array
+     */
+    protected $observers = [];
+
+    /**
      * Register the application's event listeners.
      *
      * @return void
@@ -39,6 +46,10 @@ class EventServiceProvider extends ServiceProvider
 
         foreach ($this->subscribe as $subscriber) {
             Event::subscribe($subscriber);
+        }
+
+        foreach ($this->observers as $model => $observers) {
+            $model::observe($observers);
         }
     }
 
