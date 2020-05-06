25730a8c1c54cdbb2bff433fc5a1f44422b23b01
Propaganistas <Propaganistas@users.noreply.github.com>
2020-05-07 00:43:30 +0200

    Allow overriding the MySQL server version for strict mode (#32708)
    * Allow overriding the MySQL server version
    
    * Update MySqlConnector.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Database/Connectors/MySqlConnector.php
+++ src/Illuminate/Database/Connectors/MySqlConnector.php
@@ -147,7 +147,7 @@ class MySqlConnector extends Connector implements ConnectorInterface
             $this->setCustomModes($connection, $config);
         } elseif (isset($config['strict'])) {
             if ($config['strict']) {
-                $connection->prepare($this->strictMode($connection))->execute();
+                $connection->prepare($this->strictMode($connection, $config))->execute();
             } else {
                 $connection->prepare("set session sql_mode='NO_ENGINE_SUBSTITUTION'")->execute();
             }
@@ -172,11 +172,14 @@ class MySqlConnector extends Connector implements ConnectorInterface
      * Get the query to enable strict mode.
      *
      * @param  \PDO  $connection
+     * @param  array  $config
      * @return string
      */
-    protected function strictMode(PDO $connection)
+    protected function strictMode(PDO $connection, $config)
     {
-        if (version_compare($connection->getAttribute(PDO::ATTR_SERVER_VERSION), '8.0.11') >= 0) {
+        $version = $config['version'] ?? $connection->getAttribute(PDO::ATTR_SERVER_VERSION);
+
+        if (version_compare($version, '8.0.11') >= 0) {
             return "set session sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'";
         }
 
