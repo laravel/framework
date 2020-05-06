effef6ee2c55080a3c8f4087dca6943c01a88b9f
Taylor Otwell <taylor@laravel.com>
2020-05-06 17:30:05 -0500

    Update MySqlConnector.php

--- src/Illuminate/Database/Connectors/MySqlConnector.php
+++ src/Illuminate/Database/Connectors/MySqlConnector.php
@@ -177,24 +177,12 @@ class MySqlConnector extends Connector implements ConnectorInterface
      */
     protected function strictMode(PDO $connection, $config)
     {
-        $version = $this->getServerVersion($connection, $config);
+        $version = $config['version'] ?? $connection->getAttribute(PDO::ATTR_SERVER_VERSION);
 
         if (version_compare($version, '8.0.11') >= 0) {
             return "set session sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'";
         }
 
         return "set session sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'";
-    }
-
-    /**
-     * Get the server version of the given connection.
-     *
-     * @param  \PDO  $connection
-     * @param  array  $config
-     * @return string
-     */
-    protected function getServerVersion(PDO $connection, array $config)
-    {
-        return $config['version'] ?? $connection->getAttribute(PDO::ATTR_SERVER_VERSION);
     }
 }
