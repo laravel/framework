c2e3c453db6f38f40331ccea88e12bfd050d7d60
Oliver Green <green2go@gmail.com>
2020-05-07 15:26:42 +0100

    Fix testing commands that do not resolve 'OutputStyle::class' from the container. (#32687)

--- src/Illuminate/Foundation/Testing/PendingCommand.php
+++ src/Illuminate/Foundation/Testing/PendingCommand.php
@@ -131,10 +131,10 @@ class PendingCommand
     {
         $this->hasExecuted = true;
 
-        $this->mockConsoleOutput();
+        $mock = $this->mockConsoleOutput();
 
         try {
-            $exitCode = $this->app[Kernel::class]->call($this->command, $this->parameters);
+            $exitCode = $this->app[Kernel::class]->call($this->command, $this->parameters, $mock);
         } catch (NoMatchingExpectationException $e) {
             if ($e->getMethodName() === 'askQuestion') {
                 $this->test->fail('Unexpected question "'.$e->getActualArguments()[0]->getQuestion().'" was asked.');
@@ -156,7 +156,7 @@ class PendingCommand
     /**
      * Mock the application's console output.
      *
-     * @return void
+     * @return \Mockery\MockInterface
      */
     protected function mockConsoleOutput()
     {
@@ -181,6 +181,8 @@ class PendingCommand
         $this->app->bind(OutputStyle::class, function () use ($mock) {
             return $mock;
         });
+
+        return $mock;
     }
 
     /**
