83a9562d205730d99d99ddcc02828225134d4690
Yongcheng Chen <yongcheng.chen@live.com>
2020-05-09 13:40:52 +1000

    fix code style

--- src/Illuminate/Database/Eloquent/Concerns/SelfMorphModel.php
+++ src/Illuminate/Database/Eloquent/Concerns/SelfMorphModel.php
@@ -4,20 +4,23 @@ namespace Illuminate\Database\Eloquent\Concerns;
 
 use Illuminate\Database\Eloquent\Model;
 
-trait SelfMorphModel {
+trait SelfMorphModel 
+{
     protected static $morphTypes = [];
 
     /**
      * register a morph type to a class
      */
-    public static function registerMorph($alias) {
+    public static function registerMorph($alias) 
+    {
         self::$morphTypes[$alias] = static::class;
     }
 
     /**
      * find the class morph type(from class name to alias)
      */
-    public static function getMorphType($className = null) {
+    public static function getMorphType($className = null) 
+    {
         $className = $className ? $className : static::class;
         return array_search($className, self::$morphTypes);
     }
@@ -25,11 +28,13 @@ trait SelfMorphModel {
     /**
      * get all morph types(alias)
      */
-    public static function getMorphTypes() {
+    public static function getMorphTypes() 
+    {
         return array_keys(self::$morphTypes);
     }
 
-    protected function newMorphInstance(array $attributes, $fillAttributes = true) {
+    protected function newMorphInstance(array $attributes, $fillAttributes = true) 
+    {
         $morphType = $attributes['morph_type'] ?? false;
         if ($morphType && isset(self::$morphTypes[$morphType])) {
             $class = self::$morphTypes[$morphType];
