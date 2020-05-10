f4fbb2ab374c4349a0e268010425053be19779eb
vladyslavstartsev <17382248+vladyslavstartsev@users.noreply.github.com>
2020-05-11 02:17:46 +0300

    made redis cache flush all only by prefix

--- src/Illuminate/Cache/RedisStore.php
+++ src/Illuminate/Cache/RedisStore.php
@@ -214,7 +214,15 @@ class RedisStore extends TaggableStore implements LockProvider
      */
     public function flush()
     {
-        $this->connection()->flushdb();
+        $luaScript = <<<'LUA'
+local keys = redis.call('keys', ARGV[1]) 
+for i=1,#keys,5000 do
+    redis.call('del', unpack(keys, i, math.min(i+4999, #keys)))
+end
+return keys
+LUA;
+
+        $this->connection()->eval( $luaScript, 0, $this->prefix . '*');
 
         return true;
     }
