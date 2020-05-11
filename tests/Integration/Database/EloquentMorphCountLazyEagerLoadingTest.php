dcf2d3320a0df3800653184bfd3568d09dd6da0a
Günther Debrauwer <debrauwer.gunther@gmail.com>
2020-05-11 20:49:59 +0200

    [7.x] Add 'loadMorph' and 'loadMorphCount' on Eloquent Model (#32760)
    * Add 'loadMorph' method to Eloquent Model
    
    * Add 'loadMorphCount' method to Eloquent model
    
    * Fix StyleCI issue
    
    Co-authored-by: GuntherDebrauwer <22586858+GuntherDebrauwer@users.noreply.github.com>

---
+++ tests/Integration/Database/EloquentMorphCountLazyEagerLoadingTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Database\EloquentMorphCountLazyEagerLoadingTest;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Tests\Integration\Database\DatabaseTestCase;
+
+/**
+ * @group integration
+ */
+class EloquentMorphCountLazyEagerLoadingTest extends DatabaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('post_id');
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+        });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('commentable_type');
+            $table->integer('commentable_id');
+        });
+
+        $post = Post::create();
+
+        tap((new Like)->post()->associate($post))->save();
+        tap((new Like)->post()->associate($post))->save();
+
+        (new Comment)->commentable()->associate($post)->save();
+    }
+
+    public function testLazyEagerLoading()
+    {
+        $comment = Comment::first();
+
+        $comment->loadMorphCount('commentable', [
+            Post::class => ['likes'],
+        ]);
+
+        $this->assertTrue($comment->relationLoaded('commentable'));
+        $this->assertEquals(2, $comment->commentable->likes_count);
+    }
+}
+
+class Comment extends Model
+{
+    public $timestamps = false;
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
+}
+
+class Post extends Model
+{
+    public $timestamps = false;
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
+
+class Like extends Model
+{
+    public $timestamps = false;
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
