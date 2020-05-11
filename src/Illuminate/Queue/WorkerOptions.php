cab1de36ad2e5e42a0111b1e5070d5c62e933f86
Taylor Otwell <taylor@laravel.com>
2020-05-11 15:04:41 -0500

    Apply fixes from StyleCI (#32762)

--- src/Illuminate/Queue/WorkerOptions.php
+++ src/Illuminate/Queue/WorkerOptions.php
@@ -5,7 +5,7 @@ namespace Illuminate\Queue;
 class WorkerOptions
 {
     /**
-     * The number of seconds to wait before retrying a job that encountered an uncaught exception
+     * The number of seconds to wait before retrying a job that encountered an uncaught exception.
      *
      * @var int
      */
