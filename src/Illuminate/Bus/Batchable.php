93e72b90b9f74e163a95609d65a5a70d6b088495
Taylor Otwell <taylorotwell@gmail.com>
2020-05-14 10:34:54 -0500

    wip

--- src/Illuminate/Bus/Batchable.php
+++ src/Illuminate/Bus/Batchable.php
@@ -16,24 +16,9 @@ trait Batchable
     /**
      * Get the batch instance for the job, if applicable.
      *
-     * The same resolved batch instance will be retunred on subsequent calls to this method.
-     *
      * @return \Illuminate\Bus\Batch|null
      */
     public function batch()
-    {
-        return $this->findBatch();
-        static $batch = null;
-
-        return $batch = $batch ?: $this->findBatch();
-    }
-
-    /**
-     * Get the batch instance for the job, if applicable.
-     *
-     * @return \Illuminate\Bus\Batch|null
-     */
-    public function findBatch()
     {
         if ($this->batchId) {
             return Container::getInstance()->make(BatchRepository::class)->find($this->batchId);
