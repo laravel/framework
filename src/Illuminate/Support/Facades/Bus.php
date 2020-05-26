8f39417d79e87ce7eacfcfdc89f31ad7ef590837
Mohamed Said <themohamedsaid@gmail.com>
2020-05-26 15:35:32 +0200

    add batch helper to facade

--- src/Illuminate/Support/Facades/Bus.php
+++ src/Illuminate/Support/Facades/Bus.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Testing\Fakes\BusFake;
  * @method static void assertDispatched(string $command, callable|int $callback = null)
  * @method static void assertDispatchedTimes(string $command, int $times = 1)
  * @method static void assertNotDispatched(string $command, callable|int $callback = null)
+ * @method static \Illuminate\Bus\PendingBatch batch(array $jobs)
  *
  * @see \Illuminate\Contracts\Bus\Dispatcher
  */
