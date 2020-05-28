01ec8f2066861e4bef931abc7fccd35c15002203
Derek MacDonald <derekmd@hotmail.com>
2020-05-28 10:06:10 -0400

    Attempt to gracefully exit queue:worker (#32992)
    App::terminating() callbacks don't get
    called in some cases when the queue:worker
    is shutdown:
    
    * memory limit exceeded
    * queue:restart has been called
    * database connection is lost
    * --stop-when-empty option is enabled
    * on SIGTERM interruption
    
    Instead of immediately calling exit(),
    bubble up the exit code and return
    it in WorkCommand@handle().
    
    Async SIGALRM interruptions still calls
    exit() and won't invoke App::terminating().
    
    Co-authored-by: Chris Fidao <fideloper@gmail.com>

--- src/Illuminate/Queue/Worker.php
+++ src/Illuminate/Queue/Worker.php
@@ -19,6 +19,10 @@ class Worker
 {
     use DetectsLostConnections;
 
+    const EXIT_SUCCESS = 0;
+    const EXIT_ERROR = 1;
+    const EXIT_MEMORY_LIMIT = 12;
+
     /**
      * The name of the worker.
      *
@@ -108,7 +112,7 @@ class Worker
      * @param  string  $connectionName
      * @param  string  $queue
      * @param  \Illuminate\Queue\WorkerOptions  $options
-     * @return void
+     * @return int
      */
     public function daemon($connectionName, $queue, WorkerOptions $options)
     {
@@ -123,7 +127,11 @@ class Worker
             // if it is we will just pause this worker for a given amount of time and
             // make sure we do not need to kill this worker process off completely.
             if (! $this->daemonShouldRun($options, $connectionName, $queue)) {
-                $this->pauseWorker($options, $lastRestart);
+                $status = $this->pauseWorker($options, $lastRestart);
+
+                if (! is_null($status)) {
+                    return $this->stop($status);
+                }
 
                 continue;
             }
@@ -155,7 +163,11 @@ class Worker
             // Finally, we will check to see if we have exceeded our memory limits or if
             // the queue should restart based on other indications. If so, we'll stop
             // this worker and let whatever is "monitoring" it restart the process.
-            $this->stopIfNecessary($options, $lastRestart, $job);
+            $status = $this->stopIfNecessary($options, $lastRestart, $job);
+
+            if (! is_null($status)) {
+                return $this->stop($status);
+            }
         }
     }
 
@@ -178,7 +190,7 @@ class Worker
                 );
             }
 
-            $this->kill(1);
+            $this->kill(static::EXIT_ERROR);
         });
 
         pcntl_alarm(
@@ -228,33 +240,33 @@ class Worker
      *
      * @param  \Illuminate\Queue\WorkerOptions  $options
      * @param  int  $lastRestart
-     * @return void
+     * @return int|null
      */
     protected function pauseWorker(WorkerOptions $options, $lastRestart)
     {
         $this->sleep($options->sleep > 0 ? $options->sleep : 1);
 
-        $this->stopIfNecessary($options, $lastRestart);
+        return $this->stopIfNecessary($options, $lastRestart);
     }
 
     /**
-     * Stop the process if necessary.
+     * Determine the exit code to stop the process if necessary.
      *
      * @param  \Illuminate\Queue\WorkerOptions  $options
      * @param  int  $lastRestart
      * @param  mixed  $job
-     * @return void
+     * @return int|null
      */
     protected function stopIfNecessary(WorkerOptions $options, $lastRestart, $job = null)
     {
         if ($this->shouldQuit) {
-            $this->stop();
+            return static::EXIT_SUCCESS;
         } elseif ($this->memoryExceeded($options->memory)) {
-            $this->stop(12);
+            return static::EXIT_MEMORY_LIMIT;
         } elseif ($this->queueShouldRestart($lastRestart)) {
-            $this->stop();
+            return static::EXIT_SUCCESS;
         } elseif ($options->stopWhenEmpty && is_null($job)) {
-            $this->stop();
+            return static::EXIT_SUCCESS;
         }
     }
 
@@ -647,13 +659,13 @@ class Worker
      * Stop listening and bail out of the script.
      *
      * @param  int  $status
-     * @return void
+     * @return int
      */
     public function stop($status = 0)
     {
         $this->events->dispatch(new WorkerStopping($status));
 
-        exit($status);
+        return $status;
     }
 
     /**
