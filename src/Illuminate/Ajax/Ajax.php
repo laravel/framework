063f262149dad3c1431b25443fcc5bee224e31c5
parsa mpsh <parsa84sh1384@gmail.com>
2020-05-31 13:37:39 +0430

    ajax: eval response code

--- src/Illuminate/Ajax/Ajax.php
+++ src/Illuminate/Ajax/Ajax.php
@@ -85,7 +85,7 @@ class Ajax{
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                alert(this.responseText);
+                eval(this.responseText);
                 }
             };
             xhttp.open("GET", url, true);
