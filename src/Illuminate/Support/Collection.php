f5de911832002671ade50d4165ca39732b1bf6bc
Victor Yoalli <victoryoalli@gmail.com>
2020-06-17 12:55:10 -0500

    Fixed missing argument type definition Enumerable::sort vs Collection::sort

--- src/Illuminate/Support/Collection.php
+++ src/Illuminate/Support/Collection.php
@@ -192,8 +192,7 @@ class Collection implements ArrayAccess, Enumerable
     public function crossJoin(...$lists)
     {
         return new static(Arr::crossJoin(
-            $this->items,
-            ...array_map([$this, 'getArrayableItems'], $lists)
+            $this->items, ...array_map([$this, 'getArrayableItems'], $lists)
         ));
     }
 
@@ -548,8 +547,7 @@ class Collection implements ArrayAccess, Enumerable
     public function intersectByKeys($items)
     {
         return new static(array_intersect_key(
-            $this->items,
-            $this->getArrayableItems($items)
+            $this->items, $this->getArrayableItems($items)
         ));
     }
 
