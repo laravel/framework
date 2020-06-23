d750abea34ea179e920294a462ec959decb29acc
Taylor Otwell <taylor@laravel.com>
2020-06-23 14:32:35 -0500

    Apply fixes from StyleCI (#33312)

--- tests/Integration/Database/DatabaseEloquentModelCustomCastingTest.php
+++ tests/Integration/Database/DatabaseEloquentModelCustomCastingTest.php
@@ -96,7 +96,7 @@ class DatabaseEloquentModelCustomCastingTest extends DatabaseTestCase
     public function testGetOriginalWithCastValueObjects()
     {
         $model = new TestEloquentModelWithCustomCast([
-            'address' => new Address('110 Kingsbrook St.', 'My Childhood House')
+            'address' => new Address('110 Kingsbrook St.', 'My Childhood House'),
         ]);
 
         $model->syncOriginal();
@@ -106,9 +106,8 @@ class DatabaseEloquentModelCustomCastingTest extends DatabaseTestCase
         $this->assertEquals('110 Kingsbrook St.', $model->getOriginal('address')->lineOne);
         $this->assertEquals('117 Spencer St.', $model->address->lineOne);
 
-
         $model = new TestEloquentModelWithCustomCast([
-            'address' => new Address('110 Kingsbrook St.', 'My Childhood House')
+            'address' => new Address('110 Kingsbrook St.', 'My Childhood House'),
         ]);
 
         $model->syncOriginal();
@@ -241,7 +240,7 @@ class AddressCaster implements CastsAttributes
     public function get($model, $key, $value, $attributes)
     {
         if (is_null($attributes['address_line_one'])) {
-            return null;
+            return;
         }
 
         return new Address($attributes['address_line_one'], $attributes['address_line_two']);
