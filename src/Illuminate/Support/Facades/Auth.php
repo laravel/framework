13981c3eda35b9b5b282dc1b453bf86afcc09e74
Anton Komarev <1849174+antonkomarev@users.noreply.github.com>
2020-06-23 18:40:58 +0300

    Check if UiServiceProvider loaded using special method (#33289)

--- src/Illuminate/Support/Facades/Auth.php
+++ src/Illuminate/Support/Facades/Auth.php
@@ -52,7 +52,7 @@ class Auth extends Facade
      */
     public static function routes(array $options = [])
     {
-        if (! array_key_exists(UiServiceProvider::class, static::$app->getLoadedProviders())) {
+        if (! static::$app->providerIsLoaded(UiServiceProvider::class)) {
             throw new RuntimeException('In order to use the Auth::routes() method, please install the laravel/ui package.');
         }
 
