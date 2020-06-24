ee8865cf8f000b4495182cb5b76135d324540c95
Dominic THOMAS <dominic.thomas76@gmail.com>
2020-06-24 23:38:14 +0200

    create Event class

---
+++ src/Illuminate/Http/Client/Event/PendingRequestSent.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Illuminate\Http\Client\Event;
+
+
+use Illuminate\Http\Client\Response;
+
+class PendingRequestSent
+{
+
+    /**
+     * The pending request method.
+     *
+     * @var string
+     */
+    public $method;
+
+    /**
+     * The Http client Response.
+     *
+     * @var Response
+     */
+    public $data;
+    /**
+     * @var Response|null
+     */
+    protected $response;
+    /**
+     * @var string
+     */
+    protected $url;
+    /**
+     * @var array
+     */
+    protected $options;
+
+
+    /**
+     * Create a new event instance.
+     *
+     * @param string $method
+     * @param string $url
+     * @param array $options
+     * @param \Illuminate\Http\Client\Response $response
+     */
+    public function __construct($method, $url, $options, $response = null)
+    {
+        $this->method = $method;
+        $this->response = $response;
+        $this->url = $url;
+        $this->options = $options;
+    }
+
+}
+
