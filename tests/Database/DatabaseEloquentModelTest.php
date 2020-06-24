06232457fc0a94257a9a8985f3e8bb918a310a79
Graham Campbell <graham@alt-three.com>
2020-06-24 14:48:00 +0100

    Correct implementation of float casting comparison

--- tests/Database/DatabaseEloquentModelTest.php
+++ tests/Database/DatabaseEloquentModelTest.php
@@ -148,6 +148,19 @@ class DatabaseEloquentModelTest extends TestCase
         $this->assertFalse($model->isClean(['foo', 'bar']));
     }
 
+    public function testCleanWhenFloatUpdateAttribute()
+    {
+        // test is equivalent
+        $model = new EloquentModelStub(['castedFloat' => 8 - 6.4]);
+        $model->syncOriginal();
+        $this->assertTrue($model->originalIsEquivalent('castedFloat', 1.6));
+
+        // test is not equivalent
+        $model = new EloquentModelStub(['castedFloat' => 5.6]);
+        $model->syncOriginal();
+        $this->assertFalse($model->originalIsEquivalent('castedFloat', 5.5));
+    }
+
     public function testCalculatedAttributes()
     {
         $model = new EloquentModelStub;
@@ -1992,6 +2005,7 @@ class EloquentModelStub extends Model
     protected $table = 'stub';
     protected $guarded = [];
     protected $morph_to_stub_type = EloquentModelSaveStub::class;
+    protected $casts = ['castedFloat' => 'float'];
 
     public function getListItemsAttribute($value)
     {
