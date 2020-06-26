f5160b8dc97fbffa38d2610a19bae1cbd3e7ffa2
ggolda <gleb.golda@streamlabs.com>
2020-06-26 11:28:01 -0700

    Guzzle-like request body summary added to HttpClient RequestException
    fix code style issues
    
    fix last code style issue

--- src/Illuminate/Http/Client/RequestException.php
+++ src/Illuminate/Http/Client/RequestException.php
@@ -21,8 +21,44 @@ class RequestException extends Exception
      */
     public function __construct(Response $response)
     {
-        parent::__construct("HTTP request returned status code {$response->status()}.", $response->status());
+        $message = "HTTP request returned status code {$response->status()}";
+        $summary = self::getResponseBodySummary($response);
 
+        if ($summary !== null) {
+            $message .= ":\n{$summary}\n";
+        }
+
+        parent::__construct($message, $response->status());
         $this->response = $response;
+    }
+
+    private static function getResponseBodySummary(Response $response, $truncateAt = 120)
+    {
+        $body = $response->toPsrResponse()->getBody();
+
+        if (! $body->isSeekable() || ! $body->isReadable()) {
+            return;
+        }
+
+        $size = $body->getSize();
+
+        if ($size === 0) {
+            return;
+        }
+
+        $summary = $body->read($truncateAt);
+        $body->rewind();
+
+        if ($size > $truncateAt) {
+            $summary .= ' (truncated...)';
+        }
+
+        // Matches any printable character, including unicode characters:
+        // letters, marks, numbers, punctuation, spacing, and separators.
+        if (preg_match('/[^\pL\pM\pN\pP\pS\pZ\n\r\t]/', $summary)) {
+            return;
+        }
+
+        return $summary;
     }
 }
