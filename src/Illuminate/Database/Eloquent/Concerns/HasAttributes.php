a22ad1faf558f226fae0f59fa347481ef85f95a5
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-07-03 04:37:47 +0100

    Added float comparison null checks (#33421)

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -1179,6 +1179,10 @@ trait HasAttributes
             return $this->castAttribute($key, $current) ==
                 $this->castAttribute($key, $original);
         } elseif ($this->hasCast($key, ['real', 'float', 'double'])) {
+            if (($current === null && $original !== null) || ($current !== null && $original === null)) {
+                return false;
+            }
+
             return abs($this->castAttribute($key, $current) - $this->castAttribute($key, $original)) < PHP_FLOAT_EPSILON * 4;
         } elseif ($this->hasCast($key)) {
             return $this->castAttribute($key, $current) ===
