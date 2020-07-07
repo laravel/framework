e5543c672e0beea6fd1237c2bea2efd83444b3d7
Taylor Otwell <taylorotwell@gmail.com>
2020-07-07 15:03:33 -0500

    add doc block

--- src/Illuminate/Events/functions.php
+++ src/Illuminate/Events/functions.php
@@ -4,6 +4,12 @@ namespace Illuminate\Events;
 
 use Closure;
 
+/**
+ * Create a new queued Closure event listener.
+ *
+ * @param  \Closure  $closure
+ * @return \Illuminate\Events\QueuedClosure
+ */
 function queueable(Closure $closure)
 {
     return new QueuedClosure($closure);
