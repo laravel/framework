8390fd777457ad9291533ded42e5d693fe21bacd
Taylor Otwell <taylorotwell@gmail.com>
2020-07-08 14:40:07 -0500

    check if function exists

--- src/Illuminate/Events/functions.php
+++ src/Illuminate/Events/functions.php
@@ -4,13 +4,15 @@ namespace Illuminate\Events;
 
 use Closure;
 
-/**
- * Create a new queued Closure event listener.
- *
- * @param  \Closure  $closure
- * @return \Illuminate\Events\QueuedClosure
- */
-function queueable(Closure $closure)
-{
-    return new QueuedClosure($closure);
+if (! function_exists('Illuminate\Events\queueable')) {
+    /**
+     * Create a new queued Closure event listener.
+     *
+     * @param  \Closure  $closure
+     * @return \Illuminate\Events\QueuedClosure
+     */
+    function queueable(Closure $closure)
+    {
+        return new QueuedClosure($closure);
+    }
 }
