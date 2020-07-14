971705c5eb08f21ef4619ae717bfb91af9dde641
Taylor Otwell <taylorotwell@gmail.com>
2020-07-14 08:09:48 -0500

    formatting

--- src/Illuminate/Container/BoundMethod.php
+++ src/Illuminate/Container/BoundMethod.php
@@ -24,7 +24,7 @@ class BoundMethod
      */
     public static function call($container, $callback, array $parameters = [], $defaultMethod = null)
     {
-        if (is_string($callback) && !$defaultMethod && method_exists($callback, '__invoke')) {
+        if (is_string($callback) && ! $defaultMethod && method_exists($callback, '__invoke')) {
             $defaultMethod = '__invoke';
         }
 
