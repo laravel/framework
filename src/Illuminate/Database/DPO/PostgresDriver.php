b14c6724ff3b15bbbf688d9ee24be7abe750a740
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-07-18 18:27:09 +0100

    Fix typo
    Co-authored-by: Dries Vints <dries@vints.io>

--- src/Illuminate/Database/DPO/PostgresDriver.php
+++ src/Illuminate/Database/DPO/PostgresDriver.php
@@ -10,7 +10,7 @@ class PostgresDriver extends AbstractPostgreSQLDriver
     public function connect(array $params)
     {
         if (! isset($params['pdo']) || ! $params['pdo'] instanceof PDO) {
-            throw new \InvalidArgumentException('Laravel requires the dpo property to be set and be a PDO instance.');
+            throw new \InvalidArgumentException('Laravel requires the pdo property to be set and be a PDO instance.');
         }
 
         return new Connection($params['pdo']);
