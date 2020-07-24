4f16b9fc6b707759d0be1de5f455652a38680b61
Alex Renoki <rennokki@gmail.com>
2020-07-24 13:55:46 +0300

    Fixed conflicts

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -863,7 +863,7 @@ class BelongsToMany extends Relation
 
         return $this->query->chunk($count, function ($results, $page) use ($callback) {
             return $callback($results, $page);
-        });
+        }, $columns);
     }
 
     /**
