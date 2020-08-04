e23edff1bf11d22eb49bd6ba30f66eb3fca27e17
Dries Vints <dries@vints.io>
2020-08-04 11:32:07 +0200

    Update Component.php

--- src/Illuminate/View/Component.php
+++ src/Illuminate/View/Component.php
@@ -49,14 +49,14 @@ abstract class Component
     /**
      * Get the view / view contents that represent the component.
      *
-     * @return \Illuminate\View\View|string
+     * @return \Illuminate\View\View|\Closure|string
      */
     abstract public function render();
 
     /**
      * Resolve the Blade view or view file that should be used when rendering the component.
      *
-     * @return \Illuminate\View\View|string
+     * @return \Illuminate\View\View|\Closure|string
      */
     public function resolveView()
     {
