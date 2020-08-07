b2ee14228d2114668f707b5c411614ead1330004
Grigory Tumakov <4266448+vokamut@users.noreply.github.com>
2020-08-07 16:50:12 +0300

    Delete space

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -139,7 +139,7 @@ if (! function_exists('data_get')) {
         if (is_null($key)) {
             return $target;
         }
-        
+
         if (Arr::accessible($target) && Arr::exists($target, $key)) {
             return $target[$key];
         }
