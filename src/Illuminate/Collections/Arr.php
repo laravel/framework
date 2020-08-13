06848158a8165dff5b8d445196d0c53db15b63d0
Marko Milivojevic <milivojemarko@gmail.com>
2020-08-13 14:29:03 +0200

    Arr::random add preserve keys option

--- src/Illuminate/Collections/Arr.php
+++ src/Illuminate/Collections/Arr.php
@@ -498,11 +498,12 @@ class Arr
      *
      * @param  array  $array
      * @param  int|null  $number
+     * @param  bool|false  $preserveKeys
      * @return mixed
      *
      * @throws \InvalidArgumentException
      */
-    public static function random($array, $number = null)
+    public static function random($array, $number = null, $preserveKeys = false)
     {
         $requested = is_null($number) ? 1 : $number;
 
@@ -526,8 +527,8 @@ class Arr
 
         $results = [];
 
-        foreach ((array) $keys as $key) {
-            $results[] = $array[$key];
+        foreach ((array) $keys as $index => $key) {
+            $results[$preserveKeys ? $key : $index] = $array[$key];
         }
 
         return $results;
