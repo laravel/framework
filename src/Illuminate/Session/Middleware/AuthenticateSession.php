2aa6a1155fe059d8648d297bfd321c3bffd97acd
Musa <40173603+voyula@users.noreply.github.com>
2020-08-14 18:56:31 +0300

    [8.x] Multi guard support

--- src/Illuminate/Session/Middleware/AuthenticateSession.php
+++ src/Illuminate/Session/Middleware/AuthenticateSession.php
@@ -47,11 +47,11 @@ class AuthenticateSession
             }
         }
 
-        if (! $request->session()->has('password_hash')) {
+        if (! $request->session()->has($this->auth->getDefaultDriver().'_password_hash')) {
             $this->storePasswordHashInSession($request);
         }
 
-        if ($request->session()->get('password_hash') !== $request->user()->getAuthPassword()) {
+        if ($request->session()->get($this->auth->getDefaultDriver().'_password_hash') !== $request->user()->getAuthPassword()) {
             $this->logout($request);
         }
 
@@ -73,7 +73,7 @@ class AuthenticateSession
         }
 
         $request->session()->put([
-            'password_hash' => $request->user()->getAuthPassword(),
+            $this->auth->getDefaultDriver().'_password_hash' => $request->user()->getAuthPassword(),
         ]);
     }
 
