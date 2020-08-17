ad3b448c1ae53ddab0abb82ed1f0467b4e18c5de
Lennart Carstens-Behrens <lennart.carbe@gmail.com>
2020-08-17 14:46:18 +0200

    composer v2 fix

--- src/Illuminate/Foundation/PackageManifest.php
+++ src/Illuminate/Foundation/PackageManifest.php
@@ -127,7 +127,7 @@ class PackageManifest
 
         $ignoreAll = in_array('*', $ignore = $this->packagesToIgnore());
 
-        $this->write(collect($packages)->mapWithKeys(function ($package) {
+        $this->write(collect($packages['packages'] ?? $packages)->mapWithKeys(function ($package) {
             return [$this->format($package['name']) => $package['extra']['laravel'] ?? []];
         })->each(function ($configuration) use (&$ignore) {
             $ignore = array_merge($ignore, $configuration['dont-discover'] ?? []);
