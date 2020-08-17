bec8a465fb9e27bfa9d45ace5884a3927e87e9cd
Leo <34514890+laristine@users.noreply.github.com>
2020-08-17 08:28:58 -0500

    Fix `@throws` in PHPDoc to use Throwable. (#33888)
    Co-authored-by: Leo <contact@leonguyen.com>

--- src/Illuminate/Http/Client/PendingRequest.php
+++ src/Illuminate/Http/Client/PendingRequest.php
@@ -525,7 +525,7 @@ class PendingRequest
      * @param  array  $options
      * @return \Illuminate\Http\Client\Response
      *
-     * @throws \Exception
+     * @throws \Throwable
      */
     public function send(string $method, string $url, array $options = [])
     {
