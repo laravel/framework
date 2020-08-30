c92c940a06f38f64e5a6ead1dc1aab06cce4f49d
Chris Morrell <inxilpro@users.noreply.github.com>
2020-08-30 10:38:56 -0400

    Move to instance methods

--- src/Illuminate/Routing/ImplicitRouteBinding.php
+++ src/Illuminate/Routing/ImplicitRouteBinding.php
@@ -10,6 +10,13 @@ use Illuminate\Support\Str;
 class ImplicitRouteBinding
 {
     /**
+     * The container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
+
+    /**
      * Resolve the implicit route bindings for the given route.
      *
      * @param  \Illuminate\Container\Container  $container
@@ -20,53 +27,7 @@ class ImplicitRouteBinding
      */
     public static function resolveForRoute($container, $route)
     {
-        foreach ($route->signatureParameters(UrlRoutable::class) as $parameter) {
-            if (! $parameterName = static::getParameterName($parameter->getName(), $route->parameters())) {
-                continue;
-            }
-
-            $model = static::resolveForRouteParameter(
-                $container, $route, $parameterName, Reflector::getParameterClassName($parameter)
-            );
-
-            $route->setParameter($parameterName, $model);
-        }
-    }
-
-    /**
-     * Resolve the implicit route bindings for the given route parameter and type.
-     *
-     * @param  \Illuminate\Container\Container  $container
-     * @param  \Illuminate\Routing\Route  $route
-     * @param  string  $parameterName
-     * @param  string  $parameterClass
-     * @return UrlRoutable
-     *
-     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
-     */
-    public static function resolveForRouteParameter($container, $route, $parameterName, $parameterClass)
-    {
-        $parameterValue = $route->parameter($parameterName);
-
-        if ($parameterValue instanceof UrlRoutable) {
-            return $parameterValue;
-        }
-
-        $instance = $container->make($parameterClass);
-
-        $parent = $route->parentOfParameter($parameterName);
-
-        if ($parent instanceof UrlRoutable && in_array($parameterName, array_keys($route->bindingFields()))) {
-            if (! $model = $parent->resolveChildRouteBinding(
-                $parameterName, $parameterValue, $route->bindingFieldFor($parameterName)
-            )) {
-                throw (new ModelNotFoundException)->setModel(get_class($instance), [$parameterValue]);
-            }
-        } elseif (! $model = $instance->resolveRouteBinding($parameterValue, $route->bindingFieldFor($parameterName))) {
-            throw (new ModelNotFoundException)->setModel(get_class($instance), [$parameterValue]);
-        }
-
-        return $model;
+        (new static($container))->resolveSignatureParameters($route);
     }
 
     /**
@@ -85,5 +46,74 @@ class ImplicitRouteBinding
         if (array_key_exists($snakedName = Str::snake($name), $parameters)) {
             return $snakedName;
         }
+    }
+
+    /**
+     * Create an implicit route binding instance.
+     *
+     * @param  \Illuminate\Container\Container  $container
+     */
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Resolve the implicit route bindings for the given route.
+     *
+     * @param  \Illuminate\Routing\Route  $route
+     * @return void
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function resolveSignatureParameters($route)
+    {
+        foreach ($route->signatureParameters(UrlRoutable::class) as $parameter) {
+            if (! $parameterName = static::getParameterName($parameter->getName(), $route->parameters())) {
+                continue;
+            }
+
+            $model = $this->resolveParameter(
+                $route, $parameterName, Reflector::getParameterClassName($parameter)
+            );
+
+            $route->setParameter($parameterName, $model);
+        }
+    }
+
+    /**
+     * Resolve the implicit route bindings for the given route parameter and type.
+     *
+     * @param  \Illuminate\Routing\Route $route
+     * @param  string                    $parameterName
+     * @param  string                    $parameterClassName
+     *
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Contracts\Routing\UrlRoutable
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    protected function resolveParameter($route, $parameterName, $parameterClassName)
+    {
+        $parameterValue = $route->parameter($parameterName);
+
+        if ($parameterValue instanceof UrlRoutable) {
+            return $parameterValue;
+        }
+
+        $instance = $this->container->make($parameterClassName);
+
+        $parent = $route->parentOfParameter($parameterName);
+
+        if ($parent instanceof UrlRoutable && in_array($parameterName, array_keys($route->bindingFields()))) {
+            if (! $model = $parent->resolveChildRouteBinding(
+                $parameterName, $parameterValue, $route->bindingFieldFor($parameterName)
+            )) {
+                throw (new ModelNotFoundException)->setModel(get_class($instance), [$parameterValue]);
+            }
+        } elseif (! $model = $instance->resolveRouteBinding($parameterValue, $route->bindingFieldFor($parameterName))) {
+            throw (new ModelNotFoundException)->setModel(get_class($instance), [$parameterValue]);
+        }
+
+        return $model;
     }
 }
