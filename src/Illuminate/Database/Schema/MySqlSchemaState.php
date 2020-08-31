1f53753e32dd10949ba723a2fbf05299c815ca46
Roberto B <roberto.butti@gmail.com>
2020-08-31 22:55:42 +0200

    schema:dump adding column-statistics=0 for mysqldump

--- src/Illuminate/Database/Schema/MySqlSchemaState.php
+++ src/Illuminate/Database/Schema/MySqlSchemaState.php
@@ -79,7 +79,7 @@ class MySqlSchemaState extends SchemaState
     {
         $gtidPurged = $this->connection->isMaria() ? '' : '--set-gtid-purged=OFF';
 
-        return 'mysqldump '.$gtidPurged.' --skip-add-drop-table --skip-add-locks --skip-comments --skip-set-charset --tz-utc --host=$LARAVEL_LOAD_HOST --port=$LARAVEL_LOAD_PORT --user=$LARAVEL_LOAD_USER --password=$LARAVEL_LOAD_PASSWORD $LARAVEL_LOAD_DATABASE';
+        return 'mysqldump '.$gtidPurged.' --column-statistics=0 --skip-add-drop-table --skip-add-locks --skip-comments --skip-set-charset --tz-utc --host=$LARAVEL_LOAD_HOST --port=$LARAVEL_LOAD_PORT --user=$LARAVEL_LOAD_USER --password=$LARAVEL_LOAD_PASSWORD $LARAVEL_LOAD_DATABASE';
     }
 
     /**
