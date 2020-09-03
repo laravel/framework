a3d8c5a34cb8502b5bb00c69c745489cfc3dc96c
Graham Campbell <graham@alt-three.com>
2020-09-03 17:07:48 +0100

    Bumped version to 9

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -33,7 +33,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '8.x-dev';
+    const VERSION = '9.x-dev';
 
     /**
      * The base path for the Laravel installation.
