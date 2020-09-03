eda8a0a1a6fbb0ac44c286d1244ce4d715c588dc
Taylor Otwell <taylor@laravel.com>
2020-09-03 09:16:41 -0500

    Update MissingAppKeyException.php

--- src/Illuminate/Encryption/MissingAppKeyException.php
+++ src/Illuminate/Encryption/MissingAppKeyException.php
@@ -6,6 +6,11 @@ use RuntimeException;
 
 class MissingAppKeyException extends RuntimeException
 {
+    /**
+     * Create a new exception instance.
+     *
+     * @return void
+     */
     public function __construct($message = 'No application encryption key has been specified.')
     {
         parent::__construct($message);
