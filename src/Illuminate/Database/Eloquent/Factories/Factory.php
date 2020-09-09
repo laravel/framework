aba3e943687c378cf7e0b7b258bebcfb5b7441f4
Zemmouri Rediane <z.rediane24@gmail.com>
2020-09-09 21:19:41 +0100

    Fix factory modelName resolver so that it takes into consideration the models that are not inside the new Models directory

--- src/Illuminate/Database/Eloquent/Factories/Factory.php
+++ src/Illuminate/Database/Eloquent/Factories/Factory.php
@@ -585,7 +585,7 @@ abstract class Factory
                         : 'App\\'.$factoryBasename;
         };
 
-        return $this->model ?: $resolver($this);
+        return $resolver($this);
     }
 
     /**
