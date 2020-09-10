0f0f883cc181c7ccdcf4b9eb740c796e0944e696
Sébastien Nikolaou <info@sebdesign.eu>
2020-09-10 15:46:30 +0300

    Improve signature for HasFactory::factory() (#34203)

--- src/Illuminate/Database/Eloquent/Factories/HasFactory.php
+++ src/Illuminate/Database/Eloquent/Factories/HasFactory.php
@@ -7,13 +7,14 @@ trait HasFactory
     /**
      * Get a new factory instance for the model.
      *
-     * @param  mixed  $parameters
+     * @param  callable|array|int|null  $count
+     * @param  callable|array  $state
      * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    public static function factory(...$parameters)
+    public static function factory($count = null, $state = [])
     {
         return Factory::factoryForModel(get_called_class())
-                    ->count(is_numeric($parameters[0] ?? null) ? $parameters[0] : null)
-                    ->state(is_array($parameters[0] ?? null) ? $parameters[0] : ($parameters[1] ?? []));
+                    ->count(is_numeric($count) ? $count : null)
+                    ->state(is_callable($count) || is_array($count) ? $count : $state);
     }
 }
