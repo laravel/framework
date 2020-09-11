75b796d9220233ac9c232ed5db32a9d5dbdc0d7d
gldrenthe89 <56250338+gldrenthe89@users.noreply.github.com>
2020-09-12 00:16:29 +0200

    Added light mode meta tags tot layout.blade.php (#34297)
    added both color-scheme and support-color-schemes meta tags with both only light as the default option.

--- src/Illuminate/Mail/resources/views/html/layout.blade.php
+++ src/Illuminate/Mail/resources/views/html/layout.blade.php
@@ -3,6 +3,8 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="color-scheme" content="light">
+<meta name="supported-color-schemes" content="light">
 </head>
 <body>
 <style>
