4bde31b24bf01b4d4a35ad31fafd8e4ca203b0f2
Taylor Otwell <taylorotwell@gmail.com>
2020-09-17 10:12:25 -0500

    add form request helper for email verification

---
+++ src/Illuminate/Foundation/Auth/EmailVerificationRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmailVerificationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (! hash_equals((string) $this->route('id'),
+                          (string) $this->user()->getKey())) {
+            return false;
+        }
+
+        if (! hash_equals((string) $this->route('hash'),
+                          sha1($this->user()->getEmailForVerification()))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Fulfill the email verification request.
+     *
+     * @return void
+     */
+    public function fulfill()
+    {
+        $this->user()->markEmailAsVerified();
+
+        event(new Verified($this->user()));
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        return $validator;
+    }
+}
