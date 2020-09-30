1e3ab42bb41900d87fb8aa0e4621a37705422371
Pascal Baljet <pascal@pascalbaljet.nl>
2020-09-30 15:09:00 +0200

    [8.x] Added 'pipeInto' method to EnumeratesValues trait (#34600)
    * Added 'pipeInto' method to EnumeratesValues trait
    
    * Update EnumeratesValues.php
    
    Co-authored-by: Pascal Baljet <pascal@protone.media>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Collections/Traits/EnumeratesValues.php
+++ src/Illuminate/Collections/Traits/EnumeratesValues.php
@@ -689,6 +689,17 @@ trait EnumeratesValues
     }
 
     /**
+     * Pass the collection into a new class.
+     *
+     * @param  string  $class
+     * @return mixed
+     */
+    public function pipeInto($class)
+    {
+        return new $class($this);
+    }
+
+    /**
      * Pass the collection to the given callback and then return it.
      *
      * @param  callable  $callback
