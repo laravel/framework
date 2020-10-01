d590d78dc286c88fcf38588f3e0f1d4fbcc45989
Taylor Otwell <taylorotwell@gmail.com>
2020-10-01 10:49:13 -0500

    add doc blocks

--- src/Illuminate/View/AppendableAttributeValue.php
+++ src/Illuminate/View/AppendableAttributeValue.php
@@ -4,8 +4,19 @@ namespace Illuminate\View;
 
 class AppendableAttributeValue
 {
+    /**
+     * The attribute value.
+     *
+     * @var mixed
+     */
     public $value;
 
+    /**
+     * Create a new appendable attribute value.
+     *
+     * @param  mixed  $value
+     * @return void
+     */
     public function __construct($value)
     {
         $this->value = $value;
