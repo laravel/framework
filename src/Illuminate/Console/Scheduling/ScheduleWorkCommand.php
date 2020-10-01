b044e767f42dd68fb8701f5baf000f215dc11116
Illia Sakovich <me@hivokas.com>
2020-10-01 13:17:36 +0300

    schedule worker

---
+++ src/Illuminate/Console/Scheduling/ScheduleWorkCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Illuminate\Console\Scheduling;
+
+use Illuminate\Console\Command;
+
+class ScheduleWorkCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'schedule:work';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start the schedule worker';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->info('Schedule worker started successfully.');
+
+        while (true) {
+            if (now()->second === 0) {
+                $this->call('schedule:run');
+            }
+
+            sleep(1);
+        }
+    }
+}
