e1257265e728d42049ce7bc6a07b554cd8fd8bc7
Bruno Corrêa <horphoz@gmail.com>
2020-10-02 23:01:39 -0300

    extendsFirst method the same way as the includesFirst

--- src/Illuminate/View/Compilers/Concerns/CompilesLayouts.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesLayouts.php
@@ -29,6 +29,23 @@ trait CompilesLayouts
 
         return '';
     }
+    
+    /**
+     * Compile the extends-first statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileExtendsFirst($expression)
+    {
+        $expression = $this->stripParentheses($expression);
+
+        $echo = "<?php echo \$__env->first({$expression}, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
+
+        $this->footer[] = $echo;
+
+        return '';
+    }
 
     /**
      * Compile the section statements into valid PHP.
