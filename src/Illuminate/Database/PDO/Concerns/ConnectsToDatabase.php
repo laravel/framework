093af30930f3352313e6323adf2b72e9bbdfbb39
Dries Vints <dries@vints.io>
2020-10-05 14:31:30 +0200

    Extract to trait

---
+++ src/Illuminate/Database/PDO/Concerns/ConnectsToDatabase.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Illuminate\Database\PDO\Concerns;
+
+use Illuminate\Database\PDO\Connection;
+
+trait ConnectsToDatabase
+{
+    public function connect(array $params)
+    {
+        if (! isset($params['pdo']) || ! $params['pdo'] instanceof PDO) {
+            throw new \InvalidArgumentException('Laravel requires the pdo property to be set and be a PDO instance.');
+        }
+
+        return new Connection($params['pdo']);
+    }
+}
