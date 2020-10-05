3203e95e4151bc2e67e1c71fb3051aa8ec069500
Dries Vints <dries@vints.io>
2020-10-05 14:31:30 +0200

    Extract to trait

--- src/Illuminate/Database/PDO/MySqlDriver.php
+++ src/Illuminate/Database/PDO/MySqlDriver.php
@@ -3,16 +3,9 @@
 namespace Illuminate\Database\PDO;
 
 use Doctrine\DBAL\Driver\AbstractMySQLDriver;
-use PDO;
+use Illuminate\Database\PDO\Concerns\ConnectsToDatabase;
 
 class MySqlDriver extends AbstractMySQLDriver
 {
-    public function connect(array $params)
-    {
-        if (! isset($params['pdo']) || ! $params['pdo'] instanceof PDO) {
-            throw new \InvalidArgumentException('Laravel requires the pdo property to be set and be a PDO instance.');
-        }
-
-        return new Connection($params['pdo']);
-    }
+    use ConnectsToDatabase;
 }
--- src/Illuminate/Database/PDO/SQLiteDriver.php
+++ src/Illuminate/Database/PDO/SQLiteDriver.php
@@ -3,16 +3,9 @@
 namespace Illuminate\Database\PDO;
 
 use Doctrine\DBAL\Driver\AbstractSQLiteDriver;
-use PDO;
+use Illuminate\Database\PDO\Concerns\ConnectsToDatabase;
 
 class SQLiteDriver extends AbstractSQLiteDriver
 {
-    public function connect(array $params)
-    {
-        if (! isset($params['pdo']) || ! $params['pdo'] instanceof PDO) {
-            throw new \InvalidArgumentException('Laravel requires the pdo property to be set and be a PDO instance.');
-        }
-
-        return new Connection($params['pdo']);
-    }
+    use ConnectsToDatabase;
 }
