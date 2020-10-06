1bb7ad664a3607f719af2d91c3f95cf71662dcd2
Taylor Otwell <taylorotwell@gmail.com>
2020-10-06 07:47:40 -0500

    allow attach method to accept many files

--- src/Illuminate/Http/Client/PendingRequest.php
+++ src/Illuminate/Http/Client/PendingRequest.php
@@ -181,14 +181,22 @@ class PendingRequest
     /**
      * Attach a file to the request.
      *
-     * @param  string  $name
+     * @param  string|array  $name
      * @param  string  $contents
      * @param  string|null  $filename
      * @param  array  $headers
      * @return $this
      */
-    public function attach($name, $contents, $filename = null, array $headers = [])
+    public function attach($name, $contents = '', $filename = null, array $headers = [])
     {
+        if (is_array($name)) {
+            foreach ($name as $file) {
+                $this->attach(...$file);
+            }
+
+            return $this;
+        }
+
         $this->asMultipart();
 
         $this->pendingFiles[] = array_filter([
@@ -197,21 +205,6 @@ class PendingRequest
             'headers' => $headers,
             'filename' => $filename,
         ]);
-
-        return $this;
-    }
-    
-    /**
-     * Attach multiple files to the request.
-     *
-     * @param  array  $files
-     * @return $this
-     */
-    public function attachMany(array $files)
-    {
-        foreach ($files as $file) {
-            $this->attach(...$file);
-        }
 
         return $this;
     }
