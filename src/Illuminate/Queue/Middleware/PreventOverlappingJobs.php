8f62a990dcec4ead90c09b784e341ef19104847e
Paras Malhotra <paras@insenseanalytics.com>
2020-10-11 20:44:54 +0530

    [8.x] Add job middleware to prevent overlapping jobs

---
+++ src/Illuminate/Queue/Middleware/PreventOverlappingJobs.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Illuminate\Queue\Middleware;
+
+use Illuminate\Contracts\Cache\Repository as Cache;
+
+class PreventOverlappingJobs
+{
+    /**
+     * The key of the job.
+     *
+     * @var string
+     */
+    public $key;
+
+    /**
+     * The prefix of the lock key.
+     *
+     * @var string
+     */
+    public $prefix;
+
+    /**
+     * Create a new overlapping jobs middleware instance.
+     *
+     * @param  string  $key
+     * @param  int  $expiresAt
+     * @param  string  $prefix
+     *
+     * @return void
+     */
+    public function __construct($key = '', $prefix = 'overlap:')
+    {
+        $this->key = $key;
+        $this->prefix = $prefix;
+    }
+
+    /**
+     * Process the job.
+     *
+     * @param  mixed  $job
+     * @param  callable  $next
+     * @return mixed
+     */
+    public function handle($job, $next)
+    {
+        $lock = app(Cache::class)->lock($this->getLockKey($job));
+
+        if ($lock->get()) {
+            $next($job);
+
+            $lock->release();
+        }
+    }
+
+    /**
+     * Set the prefix of the lock key.
+     *
+     * @param  string  $prefix
+     * @return $this
+     */
+    public function withPrefix($prefix)
+    {
+        $this->prefix = $prefix;
+
+        return $this;
+    }
+
+    /**
+     * Get the lock key.
+     *
+     * @param  mixed  $job
+     * @return string
+     */
+    public function getLockKey($job)
+    {
+        return $this->prefix.get_class($job).':'.$this->key;
+    }
+}
---
+++ tests/Integration/Queue/PreventOverlappingJobsTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Queue;
+
+use Illuminate\Bus\Dispatcher;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Contracts\Queue\Job;
+use Illuminate\Queue\CallQueuedHandler;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\PreventOverlappingJobs;
+use Mockery as m;
+use Orchestra\Testbench\TestCase;
+
+/**
+ * @group integration
+ */
+class PreventOverlappingJobsTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        m::close();
+    }
+
+    public function testNonOverlappingJobsAreExecuted()
+    {
+        OverlappingTestJob::$handled = false;
+        $instance = new CallQueuedHandler(new Dispatcher($this->app), $this->app);
+
+        $job = m::mock(Job::class);
+
+        $job->shouldReceive('hasFailed')->andReturn(false);
+        $job->shouldReceive('isReleased')->andReturn(false);
+        $job->shouldReceive('isDeletedOrReleased')->andReturn(false);
+        $job->shouldReceive('delete')->once();
+
+        $instance->call($job, [
+            'command' => serialize($command = new OverlappingTestJob),
+        ]);
+
+        $lockKey = (new PreventOverlappingJobs)->getLockKey($command);
+
+        $this->assertTrue(OverlappingTestJob::$handled);
+        $this->assertNull($this->app->get(Cache::class)->get($lockKey));
+    }
+
+    public function testOverlappingJobsAreNotExecuted()
+    {
+        OverlappingTestJob::$handled = false;
+        $instance = new CallQueuedHandler(new Dispatcher($this->app), $this->app);
+
+        $lockKey = (new PreventOverlappingJobs)->getLockKey($command = new OverlappingTestJob);
+        $this->app->get(Cache::class)->lock($lockKey, 10)->acquire();
+
+        $job = m::mock(Job::class);
+
+        $job->shouldReceive('hasFailed')->andReturn(false);
+        $job->shouldReceive('isReleased')->andReturn(false);
+        $job->shouldReceive('isDeletedOrReleased')->andReturn(false);
+        $job->shouldReceive('delete')->once();
+
+        $instance->call($job, [
+            'command' => serialize($command),
+        ]);
+
+        $this->assertFalse(OverlappingTestJob::$handled);
+    }
+}
+
+class OverlappingTestJob
+{
+    use InteractsWithQueue, Queueable;
+
+    public static $handled = false;
+
+    public function handle()
+    {
+        static::$handled = true;
+    }
+
+    public function middleware()
+    {
+        return [new PreventOverlappingJobs];
+    }
+}
