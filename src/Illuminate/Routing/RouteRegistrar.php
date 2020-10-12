6e5e36f1b06cf0ea9763bd47ad66b7aa983d6e32
Iman <imanghafoori1@gmail.com>
2020-10-12 17:50:10 +0330

    [8.x] Fix router inconsistency for namespaced route groups (#34793)
    * fix router for array actions within namespaced groups
    
    * add test for array action within namespaced groups
    
    * Update RouteRegistrar.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Routing/RouteRegistrar.php
+++ src/Illuminate/Routing/RouteRegistrar.php
@@ -183,6 +183,10 @@ class RouteRegistrar
         if (is_array($action) &&
             is_callable($action) &&
             ! Arr::isAssoc($action)) {
+            if (strncmp($action[0], '\\', 1)) {
+                $action[0] = '\\'.$action[0];
+            }
+
             $action = [
                 'uses' => $action[0].'@'.$action[1],
                 'controller' => $action[0].'@'.$action[1],
