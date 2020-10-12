92db0b7e7eaa8b29ec5d286daec03f5bf3b194b3
Felix Jahn <oruborus@hotmail.de>
2020-10-12 19:44:12 +0200

    Change return type hint for UploadedFile::get() (#34805)
    As file_get_contents returns false|string so should UploadedFile::get.
    This makes makes the check for readable content easier as one only needs to check that the return variable is !== _false_.
    
    Otherwise it would be neccessary to assert that the return variable is not _true_ as well.

--- src/Illuminate/Http/UploadedFile.php
+++ src/Illuminate/Http/UploadedFile.php
@@ -91,7 +91,7 @@ class UploadedFile extends SymfonyUploadedFile
     /**
      * Get the contents of the uploaded file.
      *
-     * @return bool|string
+     * @return false|string
      *
      * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
