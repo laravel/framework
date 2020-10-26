48b11cd7526707909dae22b2b4f28c55e126ba6e
Džuris <juris@glaive.pro>
2020-10-26 17:26:26 +0200

    Fix #34960

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1267,7 +1267,12 @@ trait ValidatesAttributes
             return false;
         }
 
-        return fmod($value, $parameters[0]) === 0.0;
+        // We know it's not a multiple of 0 without even attempting to divide by zero.
+        if ((float) $parameters[0] === 0.0) {
+            return false;
+        }
+
+        return bcmod($value, $parameters[0], 16) === '0.0000000000000000';
     }
 
     /**
