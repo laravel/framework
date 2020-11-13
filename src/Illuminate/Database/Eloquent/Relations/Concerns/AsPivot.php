7eb5594b33f3816bd36c709ffc20c65a4121b14b
Dries Vints <dries@vints.io>
2020-11-13 16:55:22 +0100

    Reset array keys for restoration

--- src/Illuminate/Database/Eloquent/Relations/Concerns/AsPivot.php
+++ src/Illuminate/Database/Eloquent/Relations/Concerns/AsPivot.php
@@ -284,6 +284,8 @@ trait AsPivot
      */
     protected function newQueryForCollectionRestoration(array $ids)
     {
+        $ids = array_values($ids);
+
         if (! Str::contains($ids[0], ':')) {
             return parent::newQueryForRestoration($ids);
         }
