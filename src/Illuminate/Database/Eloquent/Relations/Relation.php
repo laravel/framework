5d58b0fddbe81e6e6589b294c48a71ce095183e3
imanghafoori <imanghafoori1@gmail.com>
2020-11-17 23:52:52 +0330

    add table name as default morph type

--- src/Illuminate/Database/Eloquent/Relations/Relation.php
+++ src/Illuminate/Database/Eloquent/Relations/Relation.php
@@ -56,6 +56,13 @@ abstract class Relation
     public static $morphMap = [];
 
     /**
+     * Indicates if the morph relation type should default to table name.
+     *
+     * @var bool
+     */
+    public static $tableNameAsMorphType = false;
+
+    /**
      * Create a new relation instance.
      *
      * @param  \Illuminate\Database\Eloquent\Builder  $query
@@ -339,6 +346,16 @@ abstract class Relation
         }
 
         return static::$morphMap;
+    }
+
+    /**
+     * Changes the default morph type to table name.
+     *
+     * @return void
+     */
+    public static function tableNameAsMorphType()
+    {
+        self::$tableNameAsMorphType = true;
     }
 
     /**
