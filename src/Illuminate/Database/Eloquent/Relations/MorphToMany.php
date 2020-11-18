31e080c7d3f5614dab498467b2b9114f24a09c7a
Daniil Zobov <ddzobov@gmail.com>
2020-11-18 22:23:18 +0300

    Rename qualifyColumn to qualifyPivotColumn in BelongsToMany & MorphToMany (#35276)

--- src/Illuminate/Database/Eloquent/Relations/MorphToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/MorphToMany.php
@@ -68,7 +68,7 @@ class MorphToMany extends BelongsToMany
     {
         parent::addWhereConstraints();
 
-        $this->query->where($this->qualifyColumn($this->morphType), $this->morphClass);
+        $this->query->where($this->qualifyPivotColumn($this->morphType), $this->morphClass);
 
         return $this;
     }
@@ -83,7 +83,7 @@ class MorphToMany extends BelongsToMany
     {
         parent::addEagerConstraints($models);
 
-        $this->query->where($this->qualifyColumn($this->morphType), $this->morphClass);
+        $this->query->where($this->qualifyPivotColumn($this->morphType), $this->morphClass);
     }
 
     /**
@@ -111,7 +111,7 @@ class MorphToMany extends BelongsToMany
     public function getRelationExistenceQuery(Builder $query, Builder $parentQuery, $columns = ['*'])
     {
         return parent::getRelationExistenceQuery($query, $parentQuery, $columns)->where(
-            $this->qualifyColumn($this->morphType), $this->morphClass
+            $this->qualifyPivotColumn($this->morphType), $this->morphClass
         );
     }
 
@@ -173,7 +173,7 @@ class MorphToMany extends BelongsToMany
         $defaults = [$this->foreignPivotKey, $this->relatedPivotKey, $this->morphType];
 
         return collect(array_merge($defaults, $this->pivotColumns))->map(function ($column) {
-            return $this->qualifyColumn($column).' as pivot_'.$column;
+            return $this->qualifyPivotColumn($column).' as pivot_'.$column;
         })->unique()->all();
     }
 
