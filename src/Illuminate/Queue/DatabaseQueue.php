035e0432ed62ce97c72242de95e70bf967b964f9
Christian Frøystad <christian@securepractice.no>
2020-11-18 10:27:11 +0100

    Allow overriding the MySQL server version for database queuedriver

--- src/Illuminate/Queue/DatabaseQueue.php
+++ src/Illuminate/Queue/DatabaseQueue.php
@@ -233,7 +233,7 @@ class DatabaseQueue extends Queue implements QueueContract, ClearableQueue
     protected function getLockForPopping()
     {
         $databaseEngine = $this->database->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME);
-        $databaseVersion = $this->database->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
+        $databaseVersion = $this->database->getConfig('version') ?? $this->database->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
 
         if ($databaseEngine == 'mysql' && ! strpos($databaseVersion, 'MariaDB') && version_compare($databaseVersion, '8.0.1', '>=') ||
             $databaseEngine == 'pgsql' && version_compare($databaseVersion, '9.5', '>=')) {
