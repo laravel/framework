98a09871120c808311f48c10f4dc0767c6c396e9
Dimitar Nikov <dimitar@nikov.name>
2020-11-19 13:02:12 +0000

    Fix deprecated \Symfony\Component\HttpFoundation\Response::create() in convertExceptionToResponse()

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -465,7 +465,7 @@ class Handler implements ExceptionHandlerContract
      */
     protected function convertExceptionToResponse(Throwable $e)
     {
-        return SymfonyResponse::create(
+        return new SymfonyResponse(
             $this->renderExceptionContent($e),
             $this->isHttpException($e) ? $e->getStatusCode() : 500,
             $this->isHttpException($e) ? $e->getHeaders() : []
