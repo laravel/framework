e42111d3429466555e0516f8b2730769d8ce5bbc
Khalil Laleh <khalilst@gmail.com>
2020-11-27 23:59:53 +0330

    Rename the getRelationCountHash parameter to incrementJoinCount

--- src/Illuminate/Database/Eloquent/Relations/Relation.php
+++ src/Illuminate/Database/Eloquent/Relations/Relation.php
@@ -230,12 +230,12 @@ abstract class Relation
     /**
      * Get a relationship join table hash.
      *
-     * @param  bool $lockCount
+     * @param  bool $incrementJoinCount
      * @return string
      */
-    public function getRelationCountHash($lockCount = false)
+    public function getRelationCountHash($incrementJoinCount = true)
     {
-        return 'laravel_reserved_'.($lockCount ? static::$selfJoinCount : static::$selfJoinCount++);
+        return 'laravel_reserved_'.($incrementJoinCount ? static::$selfJoinCount++ : static::$selfJoinCount);
     }
 
     /**
