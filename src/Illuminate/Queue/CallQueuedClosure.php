5d60e25446a37989709e10e2668b3c95bfea13bb
Gihan S <gihanshp@gmail.com>
2020-11-28 11:40:26 +0530

    Removed Dispatchable trait laravel/lumen-framework#1136
    Dispatchable not used for lumen apps.

--- src/Illuminate/Queue/CallQueuedClosure.php
+++ src/Illuminate/Queue/CallQueuedClosure.php
@@ -8,12 +8,11 @@ use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
 use ReflectionFunction;
 
 class CallQueuedClosure implements ShouldQueue
 {
-    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Batchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * The serializable Closure instance.
