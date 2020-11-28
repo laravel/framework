8078bb40da0f4564949cccc1d3efd5fe693adefb
Nekrasov Ilya <nekrasov.ilya90@gmail.com>
2020-11-28 23:35:55 +0300

    Update Route Facade docblock to fix namespace def

--- src/Illuminate/Support/Facades/Route.php
+++ src/Illuminate/Support/Facades/Route.php
@@ -25,7 +25,7 @@ namespace Illuminate\Support\Facades;
  * @method static \Illuminate\Routing\RouteRegistrar domain(string $value)
  * @method static \Illuminate\Routing\RouteRegistrar middleware(array|string|null $middleware)
  * @method static \Illuminate\Routing\RouteRegistrar name(string $value)
- * @method static \Illuminate\Routing\RouteRegistrar namespace(string $value)
+ * @method static \Illuminate\Routing\RouteRegistrar namespace(string|null $value)
  * @method static \Illuminate\Routing\RouteRegistrar prefix(string  $prefix)
  * @method static \Illuminate\Routing\RouteRegistrar where(array  $where)
  * @method static \Illuminate\Routing\Router|\Illuminate\Routing\RouteRegistrar group(\Closure|string|array $attributes, \Closure|string $routes)
