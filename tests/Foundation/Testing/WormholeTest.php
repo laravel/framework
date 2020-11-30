7ade340cf27a00067f8cdd976391d8c4dabb9406
Kristoffer Alfheim <kristoffer.alfh@hey.com>
2020-11-30 17:18:27 +0100

    [8.x] Change Wormhole to use the Date Factory (#35421)
    * Change the Wormhole to use the Date Factory
    
    * Restore the default Date Factory

--- tests/Foundation/Testing/WormholeTest.php
+++ tests/Foundation/Testing/WormholeTest.php
@@ -2,7 +2,9 @@
 
 namespace Illuminate\Tests\Foundation\Testing;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Foundation\Testing\Wormhole;
+use Illuminate\Support\Facades\Date;
 use PHPUnit\Framework\TestCase;
 
 class WormholeTest extends TestCase
@@ -21,5 +23,27 @@ class WormholeTest extends TestCase
 
         // Assert we can go back to the present..
         $this->assertEquals($present->format('Y-m-d'), Wormhole::back()->format('Y-m-d'));
+    }
+
+    public function testCarbonImmutableCompatibility()
+    {
+        // Tell the Date Factory to use CarbonImmutable...
+        Date::use(CarbonImmutable::class);
+
+        // Record what time it is in 10 days...
+        $present = now();
+        $future = $present->addDays(10);
+
+        // Travel in time...
+        (new Wormhole(10))->days();
+
+        // Assert that the present time didn't get mutated...
+        $this->assertNotEquals($future->format('Y-m-d'), $present->format('Y-m-d'));
+
+        // Assert the time travel was successful...
+        $this->assertEquals($future->format('Y-m-d'), now()->format('Y-m-d'));
+
+        // Restore the default Date Factory...
+        Date::useDefault();
     }
 }
