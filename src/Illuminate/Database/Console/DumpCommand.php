67a2199afb13ba63505df43b55e9b4f816f4814f
mo7zayed <mohamedzayed709@yahoo.com>
2020-12-10 07:15:18 +0200

    Updated Illuminate/Database/Console/DumpCommand.php, Illuminate/Database/Events/SchemaDumped.php with

--- src/Illuminate/Database/Console/DumpCommand.php
+++ src/Illuminate/Database/Console/DumpCommand.php
@@ -20,6 +20,7 @@ class DumpCommand extends Command
     protected $signature = 'schema:dump
                 {--database= : The database connection to use}
                 {--path= : The path where the schema dump file should be stored}
+                {--extra-dump-flags= : Extra flags added to the dumper command}
                 {--prune : Delete all existing migration files}';
 
     /**
@@ -41,10 +42,12 @@ class DumpCommand extends Command
         $connection = $connections->connection($database = $this->input->getOption('database'));
 
         $this->schemaState($connection)->dump(
-            $connection, $path = $this->path($connection)
+            $connection,
+            $path = $this->path($connection),
+            $extraDumpFlags = $this->input->getOption('extra-dump-flags')
         );
 
-        $dispatcher->dispatch(new SchemaDumped($connection, $path));
+        $dispatcher->dispatch(new SchemaDumped($connection, $path, $extraDumpFlags));
 
         $this->info('Database schema dumped successfully.');
 
@@ -79,7 +82,7 @@ class DumpCommand extends Command
      */
     protected function path(Connection $connection)
     {
-        return tap($this->option('path') ?: database_path('schema/'.$connection->getName().'-schema.dump'), function ($path) {
+        return tap($this->option('path') ?: database_path('schema/' . $connection->getName() . '-schema.dump'), function ($path) {
             (new Filesystem)->ensureDirectoryExists(dirname($path));
         });
     }
--- src/Illuminate/Database/Events/SchemaDumped.php
+++ src/Illuminate/Database/Events/SchemaDumped.php
@@ -26,16 +26,25 @@ class SchemaDumped
     public $path;
 
     /**
+     * Extra flags added to the dumper command.
+     *
+     * @var string|null
+     */
+    public $extraDumpFlags;
+
+    /**
      * Create a new event instance.
      *
      * @param  \Illuminate\Database\Connection  $connection
      * @param  string  $path
+     * @param  string|null  $extraDumpFlags
      * @return void
      */
-    public function __construct($connection, $path)
+    public function __construct($connection, $path, $extraDumpFlags = null)
     {
         $this->connection = $connection;
         $this->connectionName = $connection->getName();
         $this->path = $path;
+        $this->extraDumpFlags = $extraDumpFlags;
     }
 }
