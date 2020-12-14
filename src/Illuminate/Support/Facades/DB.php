c4abf6e3a77b3e96dbb84bef940bfe7d52f3bae3
Markus Podar <markus@fischer.name>
2020-12-14 15:16:56 +0100

    add ::getPdo for ide autocomplete and static analyzers (#35596)

--- src/Illuminate/Support/Facades/DB.php
+++ src/Illuminate/Support/Facades/DB.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Support\Facades;
 
 /**
+ * @method static \Doctrine\DBAL\Driver\PDOConnection getPdo()
  * @method static \Illuminate\Database\ConnectionInterface connection(string $name = null)
  * @method static \Illuminate\Database\Query\Builder table(string $table, string $as = null)
  * @method static \Illuminate\Database\Query\Expression raw($value)
