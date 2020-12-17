f84ebc159f58b4afa68aa36f253ad70f6a3ba07a
Deleu <deleugyn@gmail.com>
2020-12-17 13:17:48 +0100

    PHP 8: method_exists(): Argument #1 () must be of type object|string, array given

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1160,6 +1160,10 @@ trait ValidatesAttributes
      */
     public function validateJson($attribute, $value)
     {
+        if (is_array($value)) {
+            return false;
+        }
+
         if (! is_scalar($value) && ! method_exists($value, '__toString')) {
             return false;
         }
