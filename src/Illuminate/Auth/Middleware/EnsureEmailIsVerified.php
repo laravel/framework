56355b8b791974f04e643583a503acd858713b74
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2020-12-26 21:43:36 -0300

    do not use foundation helper on auth component

--- src/Illuminate/Auth/Middleware/EnsureEmailIsVerified.php
+++ src/Illuminate/Auth/Middleware/EnsureEmailIsVerified.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\URL;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class EnsureEmailIsVerified
 {
@@ -16,15 +17,19 @@ class EnsureEmailIsVerified
      * @param  \Closure  $next
      * @param  string|null  $redirectToRoute
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|null
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
      */
     public function handle($request, Closure $next, $redirectToRoute = null)
     {
         if (! $request->user() ||
             ($request->user() instanceof MustVerifyEmail &&
             ! $request->user()->hasVerifiedEmail())) {
-            return $request->expectsJson()
-                    ? abort(403, 'Your email address is not verified.')
-                    : Redirect::guest(URL::route($redirectToRoute ?: 'verification.notice'));
+            if ($request->expectsJson()) {
+                throw new HttpException(403, 'Your email address is not verified.');
+            }
+
+            return Redirect::guest(URL::route($redirectToRoute ?: 'verification.notice'));
         }
 
         return $next($request);
