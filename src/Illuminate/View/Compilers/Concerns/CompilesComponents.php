f4110cce9381c072aa9e7ea6f2b933bc8ccc326e
Alejandro Fernández <afernandez@adhocexecutive.com>
2020-12-28 21:31:24 +0100

    Support for overriding component props using attributes spread

--- src/Illuminate/View/Compilers/Concerns/CompilesComponents.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesComponents.php
@@ -150,15 +150,17 @@ trait CompilesComponents
      */
     protected function compileProps($expression)
     {
-        return "<?php \$attributes = \$attributes->exceptProps{$expression}; ?>
+        return "<?php \$__props = \$attributes->only(array_keys{$expression}); ?>
+<?php \$attributes = \$attributes->exceptProps{$expression}; ?>
 <?php foreach (array_filter({$expression}, 'is_string', ARRAY_FILTER_USE_KEY) as \$__key => \$__value) {
-    \$\$__key = \$\$__key ?? \$__value;
+    \$\$__key = \$__props[\$__key] ?? \$\$__key ?? \$__value;
 } ?>
 <?php \$__defined_vars = get_defined_vars(); ?>
 <?php foreach (\$attributes as \$__key => \$__value) {
     if (array_key_exists(\$__key, \$__defined_vars)) unset(\$\$__key);
 } ?>
-<?php unset(\$__defined_vars); ?>";
+<?php unset(\$__defined_vars); ?>
+<?php unset(\$__props); ?>";
     }
 
     /**
