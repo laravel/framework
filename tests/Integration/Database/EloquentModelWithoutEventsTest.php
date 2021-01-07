68540cb4123317049f64d70ac2d775c83d636495
Gregori Piñeres <gregori.pineres02@gmail.com>
2021-01-07 14:19:34 -0400

    Adds functionality that allows specific eloquent events to be skipped.

--- tests/Integration/Database/EloquentModelWithoutEventsTest.php
+++ tests/Integration/Database/EloquentModelWithoutEventsTest.php
@@ -18,6 +18,7 @@ class EloquentModelWithoutEventsTest extends DatabaseTestCase
         Schema::create('auto_filled_models', function (Blueprint $table) {
             $table->increments('id');
             $table->text('project')->nullable();
+            $table->integer('stars')->default(0);
         });
     }
 
@@ -33,6 +34,30 @@ class EloquentModelWithoutEventsTest extends DatabaseTestCase
 
         $this->assertSame('Laravel', $model->project);
     }
+
+    public function testWithoutEventsRegistersBootedListenersForLaterWithName()
+    {
+        AutoFilledModel::flushEventListeners(['saving']);
+        $model = AutoFilledModel::create();
+        $this->assertNull($model->project);
+        $this->assertSame(1, $model->stars);
+    }
+
+    public function testWithoutEventsRegistersBootedListeners()
+    {
+        AutoFilledModel::flushEventListeners();
+        $model = AutoFilledModel::create();
+        $this->assertNull($model->project);
+        $this->assertNull($model->stars);
+    }
+
+    public function testWithoutEventsRegistersBootedListenersWithName()
+    {
+        AutoFilledModel::flushEventListeners(['created']);
+        $model = AutoFilledModel::create();
+        $this->assertSame('Laravel', $model->project);
+        $this->assertNull($model->stars);
+    }
 }
 
 class AutoFilledModel extends Model
@@ -47,6 +72,10 @@ class AutoFilledModel extends Model
 
         static::saving(function ($model) {
             $model->project = 'Laravel';
+        });
+
+        static::created(function ($model) {
+            $model->increment('stars');
         });
     }
 }
