81ff013df58fce8d8ecd723a290b2f10ea6256d6
redbastie <64050101+redbastie@users.noreply.github.com>
2021-01-20 05:12:38 -0500

    Create MigrateAutoCommand.php
    # How It Works
    
    The `migrate:auto` command will automatically diff the database and apply the necessary changes via Doctrine each time it is executed.
    
    ## Usage
    
    Specify a `migration` method in your Laravel models:
    
        class Vehicle extends Model
        {
            use HasFactory;
        
            public function migration(Blueprint $table)
            {
                $table->id();
                $table->string('name');
                $table->timestamps();
            }
        }
    
    Run the `migrate:auto` command:
    
        php artisan migrate:auto --fresh --seed --force
    
    Note that `--fresh`, `--seed`, and `--force` are optional.
    
    ## Traditional Migrations
    
    The `migrate:auto` command will run the traditional migration files before the automatic migration methods.

---
+++ src/Illuminate/Foundation/Console/MigrateAutoCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Illuminate\Foundation\Console;
+
+use Doctrine\DBAL\Schema\Comparator;
+use Illuminate\Console\Command;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class MigrateAutoCommand extends Command
+{
+    protected $signature = 'migrate:auto {--fresh} {--seed} {--force}';
+
+    public function handle()
+    {
+        Artisan::call('migrate' . ($this->option('fresh') ? ':fresh' : null) . ($this->option('force') ? ' --force' : null);
+
+        $filesystem = new Filesystem;
+        $dir = base_path('App/Models');
+
+        if ($filesystem->exists($dir)) {
+            foreach ($filesystem->allFiles($dir) as $file) {
+                $class = app('App\\Models\\' . str_replace(['/', '.php'], ['\\', ''], $file->getRelativePathname()));
+
+                if (method_exists($class, 'migration')) {
+                    if (Schema::hasTable($class->getTable())) {
+                        $tempTable = 'temp_' . $class->getTable();
+
+                        Schema::dropIfExists($tempTable);
+                        Schema::create($tempTable, function (Blueprint $table) use ($class) {
+                            $class->migration($table);
+                        });
+
+                        $schemaManager = $class->getConnection()->getDoctrineSchemaManager();
+                        $classTableDetails = $schemaManager->listTableDetails($class->getTable());
+                        $tempTableDetails = $schemaManager->listTableDetails($tempTable);
+                        $tableDiff = (new Comparator)->diffTable($classTableDetails, $tempTableDetails);
+
+                        if ($tableDiff) {
+                            $schemaManager->alterTable($tableDiff);
+                        }
+
+                        Schema::drop($tempTable);
+                    } else {
+                        Schema::create($class->getTable(), function (Blueprint $table) use ($class) {
+                            $class->migration($table);
+                        });
+                    }
+                }
+            }
+        }
+
+        $this->info('Migration complete!');
+
+        if ($this->option('seed')) {
+            Artisan::call('db:seed' . ($this->option('force') ? ' --force' : null));
+
+            $this->info('Seeding complete!');
+        }
+    }
+}
