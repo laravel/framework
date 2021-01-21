9dd38140dc2924daa1a020a3d7a45f9ceff03df3
Taylor Otwell <taylorotwell@gmail.com>
2021-01-21 08:10:48 -0600

    version

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -33,7 +33,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '7.30.3';
+    const VERSION = '7.30.4';
 
     /**
      * The base path for the Laravel installation.
