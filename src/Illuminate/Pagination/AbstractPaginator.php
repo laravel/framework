9d7f75e0b3c61a07987aad87af801688e5011b98
Dries Vints <dries@vints.io>
2021-01-26 18:15:42 +0100

    Typecast page number as integer (#36055)

--- src/Illuminate/Pagination/AbstractPaginator.php
+++ src/Illuminate/Pagination/AbstractPaginator.php
@@ -446,7 +446,7 @@ abstract class AbstractPaginator implements Htmlable
     public static function resolveCurrentPage($pageName = 'page', $default = 1)
     {
         if (isset(static::$currentPageResolver)) {
-            return call_user_func(static::$currentPageResolver, $pageName);
+            return (int) call_user_func(static::$currentPageResolver, $pageName);
         }
 
         return $default;
