306826fb60dfb5b5fb64d010e2ffa3dcb3f1c656
Ben Johnson <ben@indiehd.com>
2021-01-27 11:58:28 -0500

    [8.x] Fix issues with dumping PostgreSQL databases that contain multiple schemata (#36046)
    * Eliminate the need for search_path logic
    
    The PostgreSQL search_path logic that this commit removes was added with the intention of enabling support for a schema named anything other than "public". While the theory was sound, the implementation didn't take into account the behavior in databases in which *multiple* schemas exist. In multi-schema databases, the list of tables for which data should not be dumped was incorrect, leading to unexpected behavior.
    
    This revised approach takes advantage of PostgreSQL's support for pattern-based object references when specifying the list of tables for which data should not be dumped, and eliminates the need to perform complex search_path parsing altogether.
    
    The attendant Pull Request documentation explains how this technique works in detail.
    
    * Re-implement pg_restore fix that was reverted
    
    https://github.com/laravel/framework/issues/36054 was fixed in a previous commit, but appears to have been lost during subsequent edits in https://github.com/laravel/framework/commit/7be50a511955dea2bf4d6e30208b6fbf07eaa36e .
    
    This commit restores the changes made in https://github.com/laravel/framework/commit/502e75b2c8a4469363ed03adc974a5194e820ba7 .
    
    Fixes #36054
    
    * Fix PostgreSQL object reference pattern quoting
    
    While not required in a psql interactive terminal, this pattern requires outer double-quotes to function as intended when passed as a CLI argument.
    
    While simple in this specific instance, pattern quoting can grow complicated (depending on the pattern), but is well explained in the PostgreSQL manual:
    
    https://www.postgresql.org/docs/current/app-psql.html#APP-PSQL-PATTERNS

--- src/Illuminate/Database/Schema/PostgresSchemaState.php
+++ src/Illuminate/Database/Schema/PostgresSchemaState.php
@@ -16,16 +16,12 @@ class PostgresSchemaState extends SchemaState
      */
     public function dump(Connection $connection, $path)
     {
-        $schema = $connection->getConfig('schema', 'public');
-
-        $schema = $schema === 'public' ? '' : $schema.'.';
-
         $excludedTables = collect($connection->getSchemaBuilder()->getAllTables())
                         ->map->tablename
                         ->reject(function ($table) {
                             return $table === $this->migrationTable;
-                        })->map(function ($table) use ($schema) {
-                            return '--exclude-table-data='.$schema.$table;
+                        })->map(function ($table) {
+                            return '--exclude-table-data="*.'.$table.'"';
                         })->implode(' ');
 
         $this->makeProcess(
@@ -43,7 +39,7 @@ class PostgresSchemaState extends SchemaState
      */
     public function load($path)
     {
-        $command = 'PGPASSWORD=$LARAVEL_LOAD_PASSWORD pg_restore --no-owner --no-acl --host=$LARAVEL_LOAD_HOST --port=$LARAVEL_LOAD_PORT --username=$LARAVEL_LOAD_USER --dbname=$LARAVEL_LOAD_DATABASE $LARAVEL_LOAD_PATH';
+        $command = 'PGPASSWORD=$LARAVEL_LOAD_PASSWORD pg_restore --no-owner --no-acl --clean --if-exists --host=$LARAVEL_LOAD_HOST --port=$LARAVEL_LOAD_PORT --username=$LARAVEL_LOAD_USER --dbname=$LARAVEL_LOAD_DATABASE $LARAVEL_LOAD_PATH';
 
         if (Str::endsWith($path, '.sql')) {
             $command = 'PGPASSWORD=$LARAVEL_LOAD_PASSWORD psql --file=$LARAVEL_LOAD_PATH --host=$LARAVEL_LOAD_HOST --port=$LARAVEL_LOAD_PORT --username=$LARAVEL_LOAD_USER --dbname=$LARAVEL_LOAD_DATABASE';
