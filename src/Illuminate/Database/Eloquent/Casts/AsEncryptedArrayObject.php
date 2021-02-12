1be6d4ba450e994c4fb032f0463942221023519a
Taylor Otwell <taylorotwell@gmail.com>
2021-02-12 14:48:42 -0600

    add encrypted counterparts

---
+++ src/Illuminate/Database/Eloquent/Casts/AsEncryptedArrayObject.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Illuminate\Database\Eloquent\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\Castable;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Support\Facades\Crypt;
+
+class AsEncryptedArrayObject implements Castable
+{
+    /**
+     * Get the caster class to use when casting from / to this cast target.
+     *
+     * @param  array  $arguments
+     * @return object|string
+     */
+    public static function castUsing(array $arguments)
+    {
+        return new class implements CastsAttributes {
+            public function get($model, $key, $value, $attributes)
+            {
+                return new ArrayObject(json_decode(Crypt::decryptString($attributes[$key]), true));
+            }
+
+            public function set($model, $key, $value, $attributes)
+            {
+                return [$key => Crypt::encryptString(json_encode($value))];
+            }
+
+            public function serialize($model, string $key, $value, array $attributes)
+            {
+                return $value->getArrayCopy();
+            }
+        };
+    }
+}
