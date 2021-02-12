43d2c57d37b0e604ad6f52811e8901755a1aa3e0
Taylor Otwell <taylorotwell@gmail.com>
2021-02-12 10:23:44 -0600

    add array and collection object casts

---
+++ src/Illuminate/Database/Eloquent/Casts/AsCollection.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Illuminate\Database\Eloquent\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\Castable;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Support\Collection;
+
+class AsCollection implements Castable
+{
+    /**
+     * Get the caster class to use when casting from / to this cast target.
+     *
+     * @param  array  $arguments
+     * @return object|string
+     */
+    public static function castUsing(array $arguments)
+    {
+        return new class implements CastsAttributes {
+            public function get($model, $key, $value, $attributes)
+            {
+                return new Collection(json_decode($attributes[$key], true));
+            }
+
+            public function set($model, $key, $value, $attributes)
+            {
+                return [$key => json_encode($value)];
+            }
+        };
+    }
+}
