4aefa5408c403d999ae21266222a66368671fc55
Marco Aurélio Deleu <deleugyn@gmail.com>
2021-02-17 14:56:25 +0100

    [8.x] Clean up custom Queue payload between tests (#36295)
    * Clean up custom Queue payload between tests
    
    * StyleCI
    
    * Method accepts null
    
    * Use Illuminate\Foundation\Testing\TestCase instead of (#3)
    
    Orchestra\Testbench\TestCase
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    Co-authored-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Integration/Queue/CustomPayloadTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Queue;
+
+use Illuminate\Contracts\Bus\QueueingDispatcher;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Queue\Queue;
+use Illuminate\Support\ServiceProvider;
+use Orchestra\Testbench\Concerns\CreatesApplication;
+
+class CustomPayloadTest extends TestCase
+{
+    use CreatesApplication;
+
+    protected function getPackageProviders($app)
+    {
+        return [QueueServiceProvider::class];
+    }
+
+    public function websites()
+    {
+        yield ['laravel.com'];
+
+        yield ['blog.laravel.com'];
+    }
+
+    /**
+     * @dataProvider websites
+     */
+    public function test_custom_payload_gets_cleared_for_each_data_provider(string $websites)
+    {
+        $dispatcher = $this->app->make(QueueingDispatcher::class);
+
+        $dispatcher->dispatchToQueue(new MyJob);
+    }
+}
+
+class QueueServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind('one.time.password', function () {
+            return random_int(1, 10);
+        });
+
+        Queue::createPayloadUsing(function () {
+            $password = $this->app->make('one.time.password');
+
+            $this->app->offsetUnset('one.time.password');
+
+            return ['password' => $password];
+        });
+    }
+}
+
+class MyJob implements ShouldQueue
+{
+    public $connection = 'sync';
+
+    public function handle()
+    {
+    }
+}
