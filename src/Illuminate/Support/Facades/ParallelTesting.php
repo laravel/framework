a2a8d99790e03a96163c6d0c96b288cda9715065
Tom Irons <tom.irons@hotmail.com>
2021-02-23 14:34:08 -0500

    add missing method doc

--- src/Illuminate/Support/Facades/ParallelTesting.php
+++ src/Illuminate/Support/Facades/ParallelTesting.php
@@ -5,6 +5,7 @@ namespace Illuminate\Support\Facades;
 /**
  * @method static void setUpProcess(callable $callback)
  * @method static void setUpTestCase(callable $callback)
+ * @method static void setupTestDatabase(callable $callback)
  * @method static void tearDownProcess(callable $callback)
  * @method static void tearDownTestCase(callable $callback)
  * @method static int|false token()
