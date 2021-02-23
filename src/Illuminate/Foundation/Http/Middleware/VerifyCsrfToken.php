1fcf49c88c3ec252ef74c4586ec0ff5d3e683b4f
Abu Bakar Siddique <absiddique.live@gmail.com>
2021-02-23 16:36:40 +0600

    Fixed missing
    Added missing Single Quote

--- src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
+++ src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
@@ -190,7 +190,7 @@ class VerifyCsrfToken
 
         $response->headers->setCookie(
             new Cookie(
-                XSRF-TOKEN', $request->session()->token(), $this->availableAt(60 * $config['lifetime']),
+                'XSRF-TOKEN', $request->session()->token(), $this->availableAt(60 * $config['lifetime']),
                 $config['path'], $config['domain'], $config['secure'], $config['http_only'] ?? false, false,
                 $config['same_site'] ?? null
             )
