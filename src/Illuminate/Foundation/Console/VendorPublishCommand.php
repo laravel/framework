af76c93a7a7d0e6f8e9e9ca13005f54fbc6f2af7
ARCANEDEV <arcanedev.maroc@gmail.com>
2021-02-26 19:06:13 +0000

    [9.x] Fixing the Error : Class "League\Flysystem\Adapter\Local" not found
    This is related to the new Flysystem v2 (Check the PR #33612)

--- src/Illuminate/Foundation/Console/VendorPublishCommand.php
+++ src/Illuminate/Foundation/Console/VendorPublishCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
-use League\Flysystem\Adapter\Local as LocalAdapter;
+use League\Flysystem\Local\LocalFilesystemAdapter as LocalAdapter;
 use League\Flysystem\Filesystem as Flysystem;
 use League\Flysystem\MountManager;
 
@@ -254,8 +254,8 @@ class VendorPublishCommand extends Command
     protected function moveManagedFiles($manager)
     {
         foreach ($manager->listContents('from://', true) as $file) {
-            if ($file['type'] === 'file' && (! $manager->has('to://'.$file['path']) || $this->option('force'))) {
-                $manager->put('to://'.$file['path'], $manager->read('from://'.$file['path']));
+            if ($file['type'] === 'file' && (! $manager->fileExists('to://'.$file['path']) || $this->option('force'))) {
+                $manager->write('to://'.$file['path'], $manager->read('from://'.$file['path']));
             }
         }
     }
