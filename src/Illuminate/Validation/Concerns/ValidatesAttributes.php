d92a2796a07514b2085b0c50e57c7ad1b9f07dee
Jess Archer <jess@jessarcher.com>
2021-03-08 15:05:36 +1000

    Fix required_if treating true as null

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1422,7 +1422,7 @@ trait ValidatesAttributes
 
         [$values, $other] = $this->prepareValuesAndOther($parameters);
 
-        if (in_array($other, $values)) {
+        if (in_array($other, $values, is_bool($other))) {
             return $this->validateRequired($attribute, $value);
         }
 
