f8a009e4f92e2a47f46ab476a397688b16fd2a9b
Jess Archer <jess@jessarcher.com>
2021-03-08 15:11:02 +1000

    Fix exclude_unless treating true as null

--- tests/Validation/ValidationValidatorTest.php
+++ tests/Validation/ValidationValidatorTest.php
@@ -5466,6 +5466,14 @@ class ValidationValidatorTest extends TestCase
         );
         $this->assertTrue($validator->fails());
         $this->assertSame(['mouse' => ['validation.required']], $validator->messages()->toArray());
+
+        $validator = new Validator(
+            $this->getIlluminateArrayTranslator(),
+            ['foo' => true, 'bar' => 'baz'],
+            ['foo' => 'nullable', 'bar' => 'exclude_unless:foo,null']
+        );
+        $this->assertTrue($validator->passes());
+        $this->assertSame(['foo' => true], $validator->validated());
     }
 
     public function testExcludeValuesAreReallyRemoved()
