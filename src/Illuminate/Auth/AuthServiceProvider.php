819efa8f6030f163858524b0b5511a14ce6201fd
Taylor Otwell <taylorotwell@gmail.com>
2021-03-10 13:42:56 -0600

    remove old auth.loaded binding

--- src/Illuminate/Auth/AuthServiceProvider.php
+++ src/Illuminate/Auth/AuthServiceProvider.php
@@ -35,11 +35,6 @@ class AuthServiceProvider extends ServiceProvider
     protected function registerAuthenticator()
     {
         $this->app->singleton('auth', function ($app) {
-            // Once the authentication service has actually been requested by the developer
-            // we will set a variable in the application indicating such. This helps us
-            // know that we need to set any queued cookies in the after event later.
-            $app['auth.loaded'] = true;
-
             return new AuthManager($app);
         });
 
