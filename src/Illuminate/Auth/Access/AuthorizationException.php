d328291df06ac5d989e14c24254fdd073ad9f01a
Julien Roche <julien.roche@soyhuce.fr>
2021-03-12 09:50:10 +0100

    feat(Auth): translate authorization exception message

--- src/Illuminate/Auth/Access/AuthorizationException.php
+++ src/Illuminate/Auth/Access/AuthorizationException.php
@@ -24,7 +24,7 @@ class AuthorizationException extends Exception
      */
     public function __construct($message = null, $code = null, Throwable $previous = null)
     {
-        parent::__construct($message ?? 'This action is unauthorized.', 0, $previous);
+        parent::__construct($message ?? __('This action is unauthorized.'), 0, $previous);
 
         $this->code = $code ?: 0;
     }
