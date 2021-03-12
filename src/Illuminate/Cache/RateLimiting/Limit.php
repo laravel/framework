e1506ec8e517ef8c2e8004acd9015da1a5a90c4c
Craig Anderson <craig.anderson@intrepidws.com>
2021-03-11 21:07:58 -0500

    Change argument order of perMinues() rate limit method

--- src/Illuminate/Cache/RateLimiting/Limit.php
+++ src/Illuminate/Cache/RateLimiting/Limit.php
@@ -61,11 +61,11 @@ class Limit
     /**
      * Create a new rate limit using minutes as decay time.
      *
-     * @param  int  $decayMinutes
      * @param  int  $maxAttempts
+     * @param  int  $decayMinutes
      * @return static
      */
-    public static function perMinutes($decayMinutes, $maxAttempts)
+    public static function perMinutes($maxAttempts, $decayMinutes)
     {
         return new static('', $maxAttempts, $decayMinutes);
     }
