9e1274eea149dd6d165df2d55abfcb2de2cc6f19
Jacob Martin <jacob@trendcapital.com>
2021-03-19 15:31:47 -0700

    fix: exception when unable to create file

--- src/Illuminate/Filesystem/LockableFile.php
+++ src/Illuminate/Filesystem/LockableFile.php
@@ -2,6 +2,7 @@
 
 namespace Illuminate\Filesystem;
 
+use Exception;
 use Illuminate\Contracts\Filesystem\LockTimeoutException;
 
 class LockableFile
@@ -65,6 +66,10 @@ class LockableFile
     protected function createResource($path, $mode)
     {
         $this->handle = @fopen($path, $mode);
+
+        if (!$this->handle) {
+            throw new Exception('Could not create file: ' . $path . '; please check your permissions.');
+        }
     }
 
     /**
