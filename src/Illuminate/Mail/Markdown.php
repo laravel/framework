461c4488cad31479006711d40dcf848b3f956e1d
Taylor Otwell <taylorotwell@gmail.com>
2021-03-21 08:55:04 -0500

    formatting

--- src/Illuminate/Mail/Markdown.php
+++ src/Illuminate/Mail/Markdown.php
@@ -63,8 +63,8 @@ class Markdown
             'mail', $this->htmlComponentPaths()
         )->make($view, $data)->render();
 
-        if ($this->view->exists($custom = Str::start($this->theme, 'mail.'))) {
-            $theme = $custom;
+        if ($this->view->exists($customTheme = Str::start($this->theme, 'mail.'))) {
+            $theme = $customTheme;
         } else {
             $theme = Str::contains($this->theme, '::')
                 ? $this->theme
