6ebc2cef4442044605c9f893485bcc0b24220d88
Samson Endale <4sam21@gmail.com>
2021-03-26 04:05:16 +0300

    add a failing test with event subscribe

--- tests/Foundation/Console/EventListCommandTest.php
+++ tests/Foundation/Console/EventListCommandTest.php
@@ -87,8 +87,7 @@ class EventListCommandTest extends TestCase
             '| Some\Event | Some\Listener\FirstListener  |'.PHP_EOL.
             '|            | Some\Listener\SecondListener |'.PHP_EOL.
             '| Some\Other | Some\Listener\ThirdListener  |'.PHP_EOL.
-            '+------------+------------------------------+'.PHP_EOL
-            , $output->fetch()
+            '+------------+------------------------------+'.PHP_EOL, $output->fetch()
         );
     }
 
@@ -132,6 +131,76 @@ class EventListCommandTest extends TestCase
             '+------------+-----------------------------+'.PHP_EOL.
             '| Some\Other | Some\Listener\ThirdListener |'.PHP_EOL.
             '+------------+-----------------------------+'.PHP_EOL, $output->fetch()
+        );
+    }
+
+    public function testWithEventSubscribe()
+    {
+        $input = new ArrayInput([]);
+        $output = new BufferedOutput();
+
+        $container = m::mock(Application::class);
+        $container->shouldReceive('call');
+        $container->shouldReceive('getProviders')
+            ->with(EventServiceProvider::class)
+            ->andReturn([new TestSubscriberServiceProvider($container)]);
+
+        $container->shouldReceive('make')
+            ->with(OutputStyle::class, m::any())
+            ->andReturn(
+                new OutputStyle($input, $output)
+            );
+
+        $container->shouldReceive('eventsAreCached')->andReturn(false);
+        $container->shouldReceive('make')
+            ->with(TestSubscriber::class)
+            ->andReturn(new TestSubscriber());
+
+        $command = new EventListCommand();
+        $command->setLaravel($container);
+
+        $command->run($input, $output);
+        $command->handle();
+
+        $this->assertEquals(
+            '+-------------------------------+---------------------------------------------------------------------+'.PHP_EOL.
+            '| Event                         | Listeners                                                           |'.PHP_EOL.
+            '+-------------------------------+---------------------------------------------------------------------+'.PHP_EOL.
+            '| Illuminate\Auth\Events\Login  | Illuminate\Tests\Foundation\Console\TestSubscriber@handleUserLogin  |'.PHP_EOL.
+            '| Illuminate\Auth\Events\Logout | Illuminate\Tests\Foundation\Console\TestSubscriber@handleUserLogout |'.PHP_EOL.
+            '+-------------------------------+---------------------------------------------------------------------+'.PHP_EOL
+            , $output->fetch()
+        );
+    }
+}
+
+class TestSubscriberServiceProvider extends EventServiceProvider
+{
+    protected $subscribe = [
+        TestSubscriber::class,
+    ];
+}
+
+class TestSubscriber
+{
+    public function handleUserLogin($event)
+    {
+    }
+
+    public function handleUserLogout($event)
+    {
+    }
+
+    public function subscribe($events)
+    {
+        $events->listen(
+            'Illuminate\Auth\Events\Login',
+            static::class.'@handleUserLogin'
+        );
+
+        $events->listen(
+            'Illuminate\Auth\Events\Logout',
+            static::class.'@handleUserLogout'
         );
     }
 }
