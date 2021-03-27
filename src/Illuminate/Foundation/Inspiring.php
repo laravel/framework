5bd37142e15c64d444d3e20dad726917a35d9080
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2021-03-27 23:46:58 +0300

    A new word has been added to the collection.

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -50,6 +50,7 @@ class Inspiring
             'Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh',
             'Life is available only in the present moment. - Thich Nhat Hanh',
             'The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh',
+            'Just be yourself. life is too short to be someone else. - Selçuk Çukur'
         ])->random();
     }
 }
