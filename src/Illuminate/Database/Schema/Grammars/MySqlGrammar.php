8da2f5f175e22681a489a8e80d236543473fb807
Tim Martin <git@timmartin.me>
2021-03-30 12:51:40 -0400

    Use single quotes instead of double quotes when possible

--- src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
@@ -691,7 +691,7 @@ class MySqlGrammar extends Grammar
     {
         $columnType = $column->precision ? "datetime($column->precision)" : 'datetime';
 
-        $current = $column->precision ? "CURRENT_TIMESTAMP($column->precision)" : "CURRENT_TIMESTAMP";
+        $current = $column->precision ? "CURRENT_TIMESTAMP($column->precision)" : 'CURRENT_TIMESTAMP';
 
         $columnType = $column->useCurrent ? "$columnType default $current" : $columnType;
 
