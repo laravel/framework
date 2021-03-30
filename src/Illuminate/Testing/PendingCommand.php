7c4b97f0ed0eb805be89028679c048ff59f56df2
Derek MacDonald <derekmd@hotmail.com>
2021-03-30 09:48:41 -0400

    Fix doesntExpectOutput() always causing a failed test (#36806)
    On what should be a passing test when the doesntExpectOutput()
    arg hasn't been output, PHPUnit's tearDown() call to
    Mockery::close() will throw any exception.
    
    Mockery\Exception\InvalidCountException: Method doWrite($output, <Any>)
    from Mockery_0_Symfony_Component_Console_Output_BufferedOutput should be
    called exactly 1 times but called 0 times.
    
    During a successful test run, it _should_ be called 0 times.
    Remove once() to allow the `andReturnUsing()` callback to be
    invoked one or many times.
    
    And add integration test coverage to PendingCommand.

--- src/Illuminate/Testing/PendingCommand.php
+++ src/Illuminate/Testing/PendingCommand.php
@@ -329,7 +329,6 @@ class PendingCommand
 
         foreach ($this->test->unexpectedOutput as $output => $displayed) {
             $mock->shouldReceive('doWrite')
-                ->once()
                 ->ordered()
                 ->with($output, Mockery::any())
                 ->andReturnUsing(function () use ($output) {
