3235173b35abe526ee8ad84f068fc44bacf66936
Abdulrahman <a.saeed@zid.sa>
2021-03-30 18:39:48 +0300

    Add `lastPageUrl` method

--- src/Illuminate/Pagination/LengthAwarePaginator.php
+++ src/Illuminate/Pagination/LengthAwarePaginator.php
@@ -173,6 +173,16 @@ class LengthAwarePaginator extends AbstractPaginator implements Arrayable, Array
     }
 
     /**
+     * Get the URL for the last page.
+     *
+     * @return string
+     */
+    public function lastPageUrl()
+    {
+        return $this->url($this->lastPage());
+    }
+
+    /**
      * Get the instance as an array.
      *
      * @return array
@@ -185,7 +195,7 @@ class LengthAwarePaginator extends AbstractPaginator implements Arrayable, Array
             'first_page_url' => $this->url(1),
             'from' => $this->firstItem(),
             'last_page' => $this->lastPage(),
-            'last_page_url' => $this->url($this->lastPage()),
+            'last_page_url' => $this->lastPageUrl(),
             'links' => $this->linkCollection()->toArray(),
             'next_page_url' => $this->nextPageUrl(),
             'path' => $this->path(),
