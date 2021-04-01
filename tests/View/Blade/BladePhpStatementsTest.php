b53f6891ae82949c1e768a22b9297b5ee6e29221
Dries Vints <dries@vints.io>
2021-04-01 15:13:45 +0200

    Add failing test

--- tests/View/Blade/BladePhpStatementsTest.php
+++ tests/View/Blade/BladePhpStatementsTest.php
@@ -43,4 +43,13 @@ class BladePhpStatementsTest extends AbstractBladeTestCase
 
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
+
+    public function testStringWithParenthesisDataValue()
+    {
+        $string = "@php(\$data = ['test' => ')'])";
+
+        $expected = "<?php (\$data = ['test' => ')']); ?>";
+
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+    }
 }
