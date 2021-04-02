f05f390a2b05a8eb85b2d10168ae060fb89123d1
Taylor Otwell <taylor@laravel.com>
2021-04-02 11:08:49 -0500

    Allowing skipping TransformRequests middleware via Closure (#36856)
    * add skip when
    
    * break out to individual middleware

--- src/Illuminate/Foundation/Http/Middleware/ConvertEmptyStringsToNull.php
+++ src/Illuminate/Foundation/Http/Middleware/ConvertEmptyStringsToNull.php
@@ -2,8 +2,35 @@
 
 namespace Illuminate\Foundation\Http\Middleware;
 
+use Closure;
+
 class ConvertEmptyStringsToNull extends TransformsRequest
 {
+    /**
+     * All of the registered skip callbacks.
+     *
+     * @var array
+     */
+    protected static $skipCallbacks = [];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        foreach (static::$skipCallbacks as $callback) {
+            if ($callback($request)) {
+                return $next($request);
+            }
+        }
+
+        return parent::handle($request, $next);
+    }
+
     /**
      * Transform the given value.
      *
@@ -14,5 +41,16 @@ class ConvertEmptyStringsToNull extends TransformsRequest
     protected function transform($key, $value)
     {
         return is_string($value) && $value === '' ? null : $value;
+    }
+
+    /**
+     * Register a callback that instructs the middleware to be skipped.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public static function skipWhen(Closure $callback)
+    {
+        static::$skipCallbacks[] = $callback;
     }
 }
