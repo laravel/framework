c2e5b5a97590c1f060ec52f420257139322d6024
Daniil Zobov <ddzobov@gmail.com>
2021-04-07 00:54:51 +0300

    Remove code duplication

--- src/Illuminate/Foundation/Testing/RefreshDatabase.php
+++ src/Illuminate/Foundation/Testing/RefreshDatabase.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Console\Kernel;
 
 trait RefreshDatabase
 {
+    use DatabaseTransactions;
+
     /**
      * Define hooks to migrate the database before and after each test.
      *
@@ -88,48 +90,6 @@ trait RefreshDatabase
             ],
             $seeder ? ['--seeder' => $seeder] : ['--seed' => $this->shouldSeed()]
         );
-    }
-
-    /**
-     * Begin a database transaction on the testing database.
-     *
-     * @return void
-     */
-    public function beginDatabaseTransaction()
-    {
-        $database = $this->app->make('db');
-
-        foreach ($this->connectionsToTransact() as $name) {
-            $connection = $database->connection($name);
-            $dispatcher = $connection->getEventDispatcher();
-
-            $connection->unsetEventDispatcher();
-            $connection->beginTransaction();
-            $connection->setEventDispatcher($dispatcher);
-        }
-
-        $this->beforeApplicationDestroyed(function () use ($database) {
-            foreach ($this->connectionsToTransact() as $name) {
-                $connection = $database->connection($name);
-                $dispatcher = $connection->getEventDispatcher();
-
-                $connection->unsetEventDispatcher();
-                $connection->rollback();
-                $connection->setEventDispatcher($dispatcher);
-                $connection->disconnect();
-            }
-        });
-    }
-
-    /**
-     * The database connections that should have transactions.
-     *
-     * @return array
-     */
-    protected function connectionsToTransact()
-    {
-        return property_exists($this, 'connectionsToTransact')
-                            ? $this->connectionsToTransact : [null];
     }
 
     /**
