594930842e8e37119721e12b856784e23baa4044
Dries Vints <dries@vints.io>
2021-04-07 13:34:45 +0200

    Revert Blade changes (#36902)

--- tests/View/Blade/BladePhpStatementsTest.php
+++ tests/View/Blade/BladePhpStatementsTest.php
@@ -44,13 +44,15 @@ class BladePhpStatementsTest extends AbstractBladeTestCase
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 
-    public function testStringWithParenthesisDataValue()
+    public function testStringWithParenthesisCannotBeCompiled()
     {
         $string = "@php(\$data = ['test' => ')'])";
 
         $expected = "<?php (\$data = ['test' => ')']); ?>";
 
-        $this->assertEquals($expected, $this->compiler->compileString($string));
+        $actual = "<?php (\$data = ['test' => '); ?>'])";
+
+        $this->assertEquals($actual, $this->compiler->compileString($string));
     }
 
     public function testStringWithEmptyStringDataValue()
