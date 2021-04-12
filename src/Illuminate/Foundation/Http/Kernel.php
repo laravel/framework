3d8cbb41864e14026e7d5366f0a15dc171ba38c4
Mohamed Said <themohamedsaid@gmail.com>
2021-04-12 14:14:30 +0200

    add a method to remove a resolved view engine

--- src/Illuminate/Foundation/Http/Kernel.php
+++ src/Illuminate/Foundation/Http/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Http\Kernel as KernelContract;
 use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Pipeline;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Facade;
