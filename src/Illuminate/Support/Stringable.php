a69479a4f8d8bac9cc1086ee6b6a9d02ac6ff2b4
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-22 14:30:34 +0200

    [8.x] Add Stringable::whenNotEmpty() (#37080)

--- src/Illuminate/Support/Stringable.php
+++ src/Illuminate/Support/Stringable.php
@@ -745,6 +745,23 @@ class Stringable implements JsonSerializable
     }
 
     /**
+     * Execute the given callback if the string is not empty.
+     *
+     * @param  callable  $callback
+     * @return static
+     */
+    public function whenNotEmpty($callback)
+    {
+        if ($this->isNotEmpty()) {
+            $result = $callback($this);
+
+            return is_null($result) ? $this : $result;
+        }
+
+        return $this;
+    }
+
+    /**
      * Limit the number of words in a string.
      *
      * @param  int  $words
