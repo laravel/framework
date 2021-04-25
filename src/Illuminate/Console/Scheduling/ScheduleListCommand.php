47b2391e21e6e4b7e06942c947a3f9cda43ac3a0
bilogic <email@example.com>
2021-04-25 21:10:33 +0800

    include the schedule's timezone so that output is correct

--- src/Illuminate/Console/Scheduling/ScheduleListCommand.php
+++ src/Illuminate/Console/Scheduling/ScheduleListCommand.php
@@ -37,7 +37,7 @@ class ScheduleListCommand extends Command
                 $event->expression,
                 $event->description,
                 (new CronExpression($event->expression))
-                            ->getNextRunDate(Carbon::now())
+                            ->getNextRunDate(Carbon::now()->setTimezone($event->timezone))
                             ->setTimezone($this->option('timezone', config('app.timezone')))
                             ->format("Y-m-d H:i:s P"),
             ];
