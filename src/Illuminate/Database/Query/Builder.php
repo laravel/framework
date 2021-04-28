035b87718f61adb247fe78946c224d0a5cd2555e
Nuno Maduro <enunomaduro@gmail.com>
2021-04-28 19:22:54 +0100

    Fixes database offset value with non numbers (#37164)

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -2022,7 +2022,7 @@ class Builder
     {
         $property = $this->unions ? 'unionOffset' : 'offset';
 
-        $this->$property = max(0, $value);
+        $this->$property = max(0, (int) $value);
 
         return $this;
     }
