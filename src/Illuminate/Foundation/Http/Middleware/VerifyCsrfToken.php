cc4ece146e6671bfa42f2d3a680fe5349d8f643c
Lorenzo Losa <lorenzo.losa@otxgroup.com>
2021-05-05 11:01:14 +0200

    take expire_on_close in consideration for setting XSRF-TOKEN duration

--- src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
+++ src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
@@ -190,7 +190,8 @@ class VerifyCsrfToken
 
         $response->headers->setCookie(
             new Cookie(
-                'XSRF-TOKEN', $request->session()->token(), $this->availableAt(60 * $config['lifetime']),
+                'XSRF-TOKEN', $request->session()->token(),
+                $config['expire_on_close'] ? 0 : $this->availableAt(60 * $config['lifetime']),
                 $config['path'], $config['domain'], $config['secure'], false, false, $config['same_site'] ?? null
             )
         );
