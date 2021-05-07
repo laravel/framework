73bfd3663ca2d94ffcf567ee6e1653cde2f5910e
Bastien Philippe <bastien.philippe@soyhuce.fr>
2021-05-07 09:24:31 +0200

    Add withExists method to QueriesRelationships

--- src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
+++ src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
@@ -492,6 +492,84 @@ trait QueriesRelationships
     }
 
     /**
+     * Add subselect queries to include an existance value for a relationship.
+     *
+     * @param  string|array  $relations
+     * @return $this
+     */
+    public function withExists($relations)
+    {
+        if (empty($relations)) {
+            return $this;
+        }
+
+        if (is_null($this->query->columns)) {
+            $this->query->select([$this->query->from.'.*']);
+        }
+
+        $relations = is_array($relations) ? $relations : [$relations];
+
+        foreach ($this->parseWithRelations($relations) as $name => $constraints) {
+            // First we will determine if the name has been aliased using an "as" clause on the name
+            // and if it has we will extract the actual relationship name and the desired name of
+            // the resulting column. This allows multiple existences on the same relationships.
+            $segments = explode(' ', $name);
+
+            unset($alias);
+
+            if (count($segments) === 3 && Str::lower($segments[1]) === 'as') {
+                [$name, $alias] = [$segments[0], $segments[2]];
+            }
+
+            $relation = $this->getRelationWithoutConstraints($name);
+
+            $column = $relation->getModel()->getKeyName();
+
+            $hashedColumn = $this->getQuery()->from === $relation->getQuery()->getQuery()->from
+                ? "{$relation->getRelationCountHash(false)}.$column"
+                : $column;
+
+            $expression = $this->getQuery()->grammar->wrap($relation->getRelated()->qualifyColumn($hashedColumn));
+
+            // Here, we will grab the relationship sub-query and prepare to add it to the main query
+            // as a sub-select. First, we'll get the "has" query and use that to get the relation
+            // sub-query. We'll format this relationship name and append this column if needed.
+            $query = $relation->getRelationExistenceQuery(
+                $relation->getRelated()->newQuery(), $this, new Expression($expression)
+            )->setBindings([], 'select');
+
+            $query->callScope($constraints);
+
+            $query = $query->mergeConstraintsFrom($relation->getQuery())->toBase();
+
+            // If the query contains certain elements like orderings / more than one column selected
+            // then we will remove those elements from the query so that it will execute properly
+            // when given to the database. Otherwise, we may receive SQL errors or poor syntax.
+            $query->orders = null;
+            $query->setBindings([], 'order');
+
+            if (count($query->columns) > 1) {
+                $query->columns = [$query->columns[0]];
+                $query->bindings['select'] = [];
+            }
+
+            // Finally, we will make the proper column alias to the query and run this sub-select on
+            // the query builder. Then, we will return the builder instance back to the developer
+            // for further constraint chaining that needs to take place on the query as needed.
+            $alias = $alias ?? Str::snake(
+                    preg_replace('/[^[:alnum:][:space:]_]/u', '', "$name exists")
+                );
+
+            $this->selectRaw(
+                sprintf('exists(%s) as %s', $query->toSql(), $this->getQuery()->grammar->wrap($alias)),
+                $query->getBindings()
+            )->withCasts([$alias => 'bool']);
+        }
+
+        return $this;
+    }
+
+    /**
      * Add the "has" condition where clause to the query.
      *
      * @param  \Illuminate\Database\Eloquent\Builder  $hasQuery
