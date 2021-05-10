e4ce9c5c9f88eb59b069441ee556cc4dae0ec89a
Mohammad Emran Hasan <phpfour@gmail.com>
2021-05-10 20:02:31 +0600

    [8.x] Add exception to chunkById() when last id cannot be determined (#37294)
    * Add exception to chunkById() when last id cannot be determined
    
    * Fix styling issue
    
    * Update BuildsQueries.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Database/Concerns/BuildsQueries.php
+++ src/Illuminate/Database/Concerns/BuildsQueries.php
@@ -11,6 +11,7 @@ use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Collection;
 use Illuminate\Support\LazyCollection;
 use InvalidArgumentException;
+use RuntimeException;
 
 trait BuildsQueries
 {
@@ -133,6 +134,10 @@ trait BuildsQueries
             }
 
             $lastId = $results->last()->{$alias};
+
+            if ($lastId === null) {
+                throw new RuntimeException("The chunkById operation was aborted because the [{$alias}] column is not present in the query result.");
+            }
 
             unset($results);
 
