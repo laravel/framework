5b5a5c12e2a039569c97ae820e676fb2fa530436
cbl <lennart.carbe@gmail.com>
2021-05-15 01:05:40 +0200

    fixed pivot test

--- tests/Database/DatabaseEloquentPivotTest.php
+++ tests/Database/DatabaseEloquentPivotTest.php
@@ -2,11 +2,15 @@
 
 namespace Illuminate\Tests\Database;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\Pivot;
+use stdClass;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
-use stdClass;
+use Illuminate\Database\Connection;
+use Illuminate\Database\ConnectionResolverInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Query\Grammars\Grammar;
+use Illuminate\Database\Query\Processors\Processor;
 
 class DatabaseEloquentPivotTest extends TestCase
 {
@@ -19,6 +23,10 @@ class DatabaseEloquentPivotTest extends TestCase
     {
         $parent = m::mock(Model::class.'[getConnectionName]');
         $parent->shouldReceive('getConnectionName')->twice()->andReturn('connection');
+        $parent->setConnectionResolver($resolver = m::mock(ConnectionResolverInterface::class));
+        $resolver->shouldReceive('connection')->andReturn($connection = m::mock(Connection::class));
+        $connection->shouldReceive('getQueryGrammar')->andReturn($grammar = m::mock(Grammar::class));
+        $connection->shouldReceive('getPostProcessor')->andReturn($processor = m::mock(Processor::class));
         $parent->getConnection()->getQueryGrammar()->shouldReceive('getDateFormat')->andReturn('Y-m-d H:i:s');
         $parent->setDateFormat('Y-m-d H:i:s');
         $pivot = Pivot::fromAttributes($parent, ['foo' => 'bar', 'created_at' => '2015-09-12'], 'table', true);
