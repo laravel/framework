47cccf7289228f01eb47abd435869b5360d2c821
Taylor Otwell <taylorotwell@gmail.com>
2021-05-14 16:29:21 -0500

    add file

---
+++ src/Illuminate/Contracts/Database/Eloquent/SupportsPartialRelations.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Illuminate\Contracts\Database\Eloquent;
+
+use Closure;
+
+interface SupportsPartialRelations
+{
+    /**
+     * Indicate that the relation is a single result of a larger one-to-many relationship.
+     *
+     * @param  Closure|string|null $column
+     * @param  string|null $relation
+     * @param  string  $relation
+     * @return $this
+     */
+    public function ofMany($column = 'id', $aggregate = 'MAX', $relation = null);
+
+    /**
+     * Determine whether the relationship is a one-of-many relationship.
+     *
+     * @return bool
+     */
+    public function isOneOfMany();
+}
