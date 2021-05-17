30afb786be6413b5f86a8478933cafb843252344
Mohamed Said <themohamedsaid@gmail.com>
2021-05-17 10:28:25 +0200

    fix tests

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -427,19 +427,19 @@ trait HasAttributes
      */
     public function getRelationValue($key)
     {
-        if (! $this->isRelation($key)) {
-            return;
-        }
-
-        if ($this->withStrictLoading && ! $this->relationLoaded($key)) {
-            throw new StrictLoadingViolationException($this, $key);
-        }
-
         // If the key already exists in the relationships array, it just means the
         // relationship has already been loaded, so we'll just return it out of
         // here because there is no need to query within the relations twice.
         if ($this->relationLoaded($key)) {
             return $this->relations[$key];
+        }
+
+        if (! $this->isRelation($key)) {
+            return;
+        }
+
+        if ($this->withStrictLoading) {
+            throw new StrictLoadingViolationException($this, $key);
         }
 
         // If the "attribute" exists as a method on the model, we will just assume
