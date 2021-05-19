4590d55bff1e8b5888794860fb18591adde3713c
Alex Vanderbist <alex.vanderbist@gmail.com>
2021-05-19 14:32:05 +0200

    Replace Whoops interface with generic ExceptionRenderer interface

---
+++ src/Illuminate/Contracts/Foundation/ExceptionRenderer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Illuminate\Contracts\Foundation;
+
+interface ExceptionRenderer
+{
+    /**
+     * Renders the given exception as HTML.
+     *
+     * @param \Throwable $throwable
+     *
+     * @return string
+     */
+    public function render($throwable);
+}
