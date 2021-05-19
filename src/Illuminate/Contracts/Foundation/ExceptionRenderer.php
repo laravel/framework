b68826962fe28117f0efa9be001eee4d8fe7ad2e
Alex Vanderbist <alex.vanderbist@gmail.com>
2021-05-19 15:55:28 +0200

    Fix code style

--- src/Illuminate/Contracts/Foundation/ExceptionRenderer.php
+++ src/Illuminate/Contracts/Foundation/ExceptionRenderer.php
@@ -7,8 +7,7 @@ interface ExceptionRenderer
     /**
      * Renders the given exception as HTML.
      *
-     * @param \Throwable $throwable
-     *
+     * @param  \Throwable  $throwable
      * @return string
      */
     public function render($throwable);
