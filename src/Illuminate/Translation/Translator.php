e794e6ac01b43460bc562f8281d4429a22dbdeeb
Eugen <59449419+jenyok-dev@users.noreply.github.com>
2021-05-28 08:22:01 +0300

    Update Translator.php
    remove function is_null and changed condition for not equal null (optimization - overheads for function call)

--- src/Illuminate/Translation/Translator.php
+++ src/Illuminate/Translation/Translator.php
@@ -122,9 +122,9 @@ class Translator extends NamespacedItemResolver implements TranslatorContract
             $locales = $fallback ? $this->localeArray($locale) : [$locale];
 
             foreach ($locales as $locale) {
-                if (! is_null($line = $this->getLine(
+                if (($line = $this->getLine(
                     $namespace, $group, $locale, $item, $replace
-                ))) {
+                )) != null) {
                     return $line;
                 }
             }
