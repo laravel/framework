8c32afd91e229d6492acbafce7657fa3929652c5
ajcastro <ajcastro29@gmail.com>
2021-05-30 15:38:29 +0800

    Fix docblock

--- src/Illuminate/Foundation/Auth/Access/AuthorizesRequests.php
+++ src/Illuminate/Foundation/Auth/Access/AuthorizesRequests.php
@@ -128,6 +128,7 @@ trait AuthorizesRequests
      * Use only the given abilities.
      *
      * @param string|array $abilities
+     * @return this
      */
     public function onlyResourceAbilities($abilities)
     {
@@ -143,7 +144,7 @@ trait AuthorizesRequests
     /**
      * Exclude resource abilities.
      *
-     * @param  array $abilities
+     * @param string|array $abilities
      * @return this
      */
     public function excludeResourceAbilities($abilities)
