f9010bab858be9c2455b8d9112b77fae611f5149
Mohamed Said <themohamedsaid@gmail.com>
2021-05-31 17:51:04 +0200

    Allow connecting to read or write connections with the db command

--- src/Illuminate/Database/Console/DbCommand.php
+++ src/Illuminate/Database/Console/DbCommand.php
@@ -14,7 +14,9 @@ class DbCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'db {connection? : The database connection that should be used}';
+    protected $signature = 'db {connection? : The database connection that should be used}
+               {--read : Connect to the read connection}
+               {--write : Connect to the write connection}';
 
     /**
      * The console command description.
@@ -62,6 +64,12 @@ class DbCommand extends Command
 
         if (! empty($connection['url'])) {
             $connection = (new ConfigurationUrlParser)->parseConfiguration($connection);
+        }
+
+        if ($this->option('read')) {
+            $connection = array_merge($connection, $connection['read']);
+        } elseif ($this->option('write')) {
+            $connection = array_merge($connection, $connection['write']);
         }
 
         return $connection;
