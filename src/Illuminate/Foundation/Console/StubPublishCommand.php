98554934c8a448f9dffc308bb1dd564fad5ec6db
Bogdan Kharchenko <scriptor@gmail.com>
2021-06-03 14:19:53 -0400

    [8.x] Include Custom Notification Stubs

--- src/Illuminate/Foundation/Console/StubPublishCommand.php
+++ src/Illuminate/Foundation/Console/StubPublishCommand.php
@@ -38,6 +38,8 @@ class StubPublishCommand extends Command
             __DIR__.'/stubs/job.stub' => $stubsPath.'/job.stub',
             __DIR__.'/stubs/model.pivot.stub' => $stubsPath.'/model.pivot.stub',
             __DIR__.'/stubs/model.stub' => $stubsPath.'/model.stub',
+            __DIR__.'/stubs/notification.stub' => $stubsPath.'/notification.stub',
+            __DIR__.'/stubs/markdown-notification.stub' => $stubsPath.'/notification.stub',
             __DIR__.'/stubs/observer.stub' => $stubsPath.'/observer.stub',
             __DIR__.'/stubs/observer.plain.stub' => $stubsPath.'/observer.plain.stub',
             __DIR__.'/stubs/request.stub' => $stubsPath.'/request.stub',
