429639bf1354f3de6806dcc81eb702704ab12092
Jelle Spekken <jspekken@gmail.com>
2021-06-04 10:45:08 +0200

    Fixes correct return type
    This commit fixes the correct return type of the cursorPaginate method from `\Illuminate\Contracts\Pagination\Paginator` (which it doens't return) to `\Illuminate\Contracts\Pagination\CursorPaginator`

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -827,7 +827,7 @@ class Builder
      * @param  array  $columns
      * @param  string  $cursorName
      * @param  string|null  $cursor
-     * @return \Illuminate\Contracts\Pagination\Paginator
+     * @return \Illuminate\Contracts\Pagination\CursorPaginator
      * @throws \Illuminate\Pagination\CursorPaginationException
      */
     public function cursorPaginate($perPage = null, $columns = ['*'], $cursorName = 'cursor', $cursor = null)
