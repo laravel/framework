f7903ba532a972534bab4a9303b16c94399fbaf1
Arnaud Coolsaet <pecuchet@users.noreply.github.com>
2021-06-11 11:40:40 +0200

    Consistent column order across MySQL versions
    It looks like the ordinal position to sort columns from `information_schema.columns` in `MySqlGrammar::compileColumnListing()` is not always taken into account by MySQL/PDO. I have differing orders between mysql Ver 8.0.25 on Ubuntu 20.04.2 and the MySQL's official docker container for 5.7.32.

--- src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
@@ -74,7 +74,8 @@ class MySqlGrammar extends Grammar
      */
     public function compileColumnListing()
     {
-        return 'select column_name as `column_name` from information_schema.columns where table_schema = ? and table_name = ?';
+        return 'select column_name as `column_name` from information_schema.columns where table_schema = ? and table_name = ?'
+            . ' order by information_schema.columns.ordinal_position asc';
     }
 
     /**
