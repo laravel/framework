63c6421c461465e78d31e446a19e2d0cefc41d58
Joseph Silber <contact@josephsilber.com>
2021-06-11 10:55:12 -0400

    Add `Conditionable` support for callback conditions

--- tests/Support/SupportConditionableTest.php
+++ tests/Support/SupportConditionableTest.php
@@ -9,68 +9,148 @@ class SupportConditionableTest extends TestCase
 {
     public function testWhenConditionCallback()
     {
+        // With static condition
         $logger = (new ConditionableLogger())
             ->when(2, function ($logger, $condition) {
                 $logger->log('when', $condition);
-            }, function () {
+            }, function ($logger, $condition) {
                 $logger->log('default', $condition);
             });
 
         $this->assertSame(['when', 2], $logger->values);
+
+        // With callback condition
+        $logger = (new ConditionableLogger())->log('init')
+            ->when(function ($logger) {
+                return $logger->has('init');
+            }, function ($logger, $condition) {
+                $logger->log('when', $condition);
+            }, function ($logger, $condition) {
+                $logger->log('default', $condition);
+            });
+
+        $this->assertSame(['init', 'when', true], $logger->values);
     }
 
     public function testWhenDefaultCallback()
     {
+        // With static condition
         $logger = (new ConditionableLogger())
-            ->when(null, function () {
+            ->when(null, function ($logger, $condition) {
                 $logger->log('when', $condition);
             }, function ($logger, $condition) {
                 $logger->log('default', $condition);
             });
 
         $this->assertSame(['default', null], $logger->values);
+
+        // With callback condition
+        $logger = (new ConditionableLogger())
+            ->when(function ($logger) {
+                return $logger->has('missing');
+            }, function ($logger, $condition) {
+                $logger->log('when', $condition);
+            }, function ($logger, $condition) {
+                $logger->log('default', $condition);
+            });
+
+        $this->assertSame(['default', false], $logger->values);
     }
 
     public function testUnlessConditionCallback()
     {
+        // With static condition
         $logger = (new ConditionableLogger())
             ->unless(null, function ($logger, $condition) {
                 $logger->log('unless', $condition);
-            }, function () {
+            }, function ($logger, $condition) {
                 $logger->log('default', $condition);
             });
 
         $this->assertSame(['unless', null], $logger->values);
+
+        // With callback condition
+        $logger = (new ConditionableLogger())
+            ->unless(function ($logger) {
+                return $logger->has('missing');
+            }, function ($logger, $condition) {
+                $logger->log('unless', $condition);
+            }, function ($logger, $condition) {
+                $logger->log('default', $condition);
+            });
+
+        $this->assertSame(['unless', false], $logger->values);
     }
 
     public function testUnlessDefaultCallback()
     {
+        // With static condition
         $logger = (new ConditionableLogger())
-            ->unless(2, function () {
+            ->unless(2, function ($logger, $condition) {
                 $logger->log('unless', $condition);
             }, function ($logger, $condition) {
                 $logger->log('default', $condition);
             });
 
         $this->assertSame(['default', 2], $logger->values);
+
+        // With callback condition
+        $logger = (new ConditionableLogger())->log('init')
+            ->unless(function ($logger) {
+                return $logger->has('init');
+            }, function ($logger, $condition) {
+                $logger->log('unless', $condition);
+            }, function ($logger, $condition) {
+                $logger->log('default', $condition);
+            });
+
+        $this->assertSame(['init', 'default', true], $logger->values);
     }
 
     public function testWhenProxy()
     {
+        // With static condition
         $logger = (new ConditionableLogger())
             ->when(true)->log('one')
             ->when(false)->log('two');
 
         $this->assertSame(['one'], $logger->values);
+
+        // With callback condition
+        $logger = (new ConditionableLogger())->log('init')
+            ->when(function ($logger) {
+                return $logger->has('init');
+            })
+            ->log('one')
+            ->when(function ($logger) {
+                return $logger->has('missing');
+            })
+            ->log('two');
+
+        $this->assertSame(['init', 'one'], $logger->values);
     }
 
     public function testUnlessProxy()
     {
+        // With static condition
         $logger = (new ConditionableLogger())
             ->unless(true)->log('one')
             ->unless(false)->log('two');
 
         $this->assertSame(['two'], $logger->values);
+
+        // With callback condition
+        $logger = (new ConditionableLogger())->log('init')
+            ->unless(function ($logger) {
+                return $logger->has('init');
+            })
+            ->log('one')
+            ->unless(function ($logger) {
+                return $logger->has('missing');
+            })
+            ->log('two');
+
+        $this->assertSame(['init', 'two'], $logger->values);
     }
 }
 
@@ -85,5 +165,10 @@ class ConditionableLogger
         array_push($this->values, ...$values);
 
         return $this;
+    }
+
+    public function has($value)
+    {
+        return in_array($value, $this->values);
     }
 }
