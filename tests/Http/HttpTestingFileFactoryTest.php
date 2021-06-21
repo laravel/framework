ae25b7849036171c482462c929fafc8f6bb16cf8
Amir Reza Mehrbakhsh <amirrezax@hotmail.com>
2021-06-21 18:26:07 +0430

    Add GIF, WEBP, WBMP, BMP support to FileFactory::image() (#37743)

--- tests/Http/HttpTestingFileFactoryTest.php
+++ tests/Http/HttpTestingFileFactoryTest.php
@@ -28,13 +28,71 @@ class HttpTestingFileFactoryTest extends TestCase
             $this->markTestSkipped('The extension gd is missing from your system or was compiled without JPEG support.');
         }
 
-        $image = (new FileFactory)->image('test.jpeg', 15, 20);
+        $jpeg = (new FileFactory)->image('test.jpeg', 15, 20);
+        $jpg = (new FileFactory)->image('test.jpg');
 
-        $info = getimagesize($image->getRealPath());
+        $info = getimagesize($jpeg->getRealPath());
 
         $this->assertSame('image/jpeg', $info['mime']);
         $this->assertSame(15, $info[0]);
         $this->assertSame(20, $info[1]);
+        $this->assertSame(
+            'image/jpeg',
+            mime_content_type($jpg->getRealPath())
+        );
+    }
+
+    public function testImageGif()
+    {
+        if (! function_exists('imagegif'))
+            $this->markTestSkipped('The extension gd is missing from your system or was compiled without GIF support.');
+
+        $image = (new FileFactory)->image('test.gif');
+
+        $this->assertSame(
+            'image/gif',
+            mime_content_type($image->getRealPath())
+        );
+    }
+
+    public function testImageWebp()
+    {
+        if (! function_exists('imagewebp'))
+            $this->markTestSkipped('The extension gd is missing from your system or was compiled without WEBP support.');
+
+        $image = (new FileFactory)->image('test.webp');
+
+        $this->assertSame(
+            'image/webp',
+            mime_content_type($image->getRealPath())
+        );
+    }
+
+    public function testImageWbmp()
+    {
+        if (! function_exists('imagewbmp'))
+            $this->markTestSkipped('The extension gd is missing from your system or was compiled without WBMP support.');
+
+        $image = (new FileFactory)->image('test.wbmp');
+
+        $this->assertSame(
+            'image/vnd.wap.wbmp',
+            getimagesize($image->getRealPath())['mime']
+        );
+    }
+
+    public function testImageBmp()
+    {
+        if (! function_exists('imagebmp'))
+            $this->markTestSkipped('The extension gd is missing from your system or was compiled without BMP support.');
+
+        $image = (new FileFactory)->image('test.bmp');
+
+        $imagePath = $image->getRealPath();
+
+        $this->assertSame('image/x-ms-bmp', mime_content_type($imagePath));
+
+        $this->assertSame('image/bmp', getimagesize($imagePath)['mime']);
     }
 
     public function testCreateWithMimeType()
