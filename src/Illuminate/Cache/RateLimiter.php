77ef41fc897c5ab315539d9a2ba0b2fe7e4d6900
Ricardo Perez <ricpp87@gmail.com>
2021-06-24 13:21:46 -0500

    Make sure availableIn returns positive values

--- src/Illuminate/Cache/RateLimiter.php
+++ src/Illuminate/Cache/RateLimiter.php
@@ -161,6 +161,6 @@ class RateLimiter
      */
     public function availableIn($key)
     {
-        return $this->cache->get($key.':timer') - $this->currentTime();
+        return max(0, $this->cache->get($key.':timer') - $this->currentTime());
     }
 }
