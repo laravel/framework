d27132e879c5b9c494e8d648884c001961fbb0c2
Marcus Bointon <marcus@synchromedia.co.uk>
2021-06-25 17:14:04 +0200

    [8.x] Move primary after collate (#37815)
    * Move primary after collate, fixes #37811
    
    * Move primary after charset

--- src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
@@ -15,7 +15,7 @@ class MySqlGrammar extends Grammar
      * @var string[]
      */
     protected $modifiers = [
-        'Unsigned', 'Primary', 'Charset', 'Collate', 'VirtualAs', 'StoredAs', 'Nullable',
+        'Unsigned', 'Charset', 'Primary', 'Collate', 'VirtualAs', 'StoredAs', 'Nullable',
         'Srid', 'Default', 'Increment', 'Comment', 'After', 'First',
     ];
 
