9e12abef033c310aa6bc86cd785aa974f969176c
Mohamed Said <themohamedsaid@gmail.com>
2021-06-30 17:39:25 +0200

    reset the log context after each worker loop (#37865)

--- src/Illuminate/Queue/QueueServiceProvider.php
+++ src/Illuminate/Queue/QueueServiceProvider.php
@@ -167,6 +167,10 @@ class QueueServiceProvider extends ServiceProvider implements DeferrableProvider
             };
 
             $resetScope = function () use ($app) {
+                if (method_exists($app['log']->driver(), 'withoutContext')) {
+                    $app['log']->withoutContext();
+                }
+
                 return $app->forgetScopedInstances();
             };
 
