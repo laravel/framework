3ade2155a2af35964ee99f583f0a30d8ce70cb23
Lucas Michot <lucas@semalead.com>
2021-07-04 12:19:03 +0200

    Add missing params in docblocks

--- src/Illuminate/Foundation/Http/Kernel.php
+++ src/Illuminate/Foundation/Http/Kernel.php
@@ -459,7 +459,7 @@ class Kernel implements KernelContract
     /**
      * Set the Laravel application instance.
      *
-     * @param  \Illuminate\Contracts\Foundation\Application
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
      * @return $this
      */
     public function setApplication(Application $app)
