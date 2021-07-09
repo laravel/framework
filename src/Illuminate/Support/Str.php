7a82103f839d094ea007a233cfd8c00f678f271b
Colin O'Dell <colinodell@gmail.com>
2021-07-09 09:40:46 -0400

    Add support for both CommonMark 1.x and 2.x (#37954)

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -391,7 +391,7 @@ class Str
     {
         $converter = new GithubFlavoredMarkdownConverter($options);
 
-        return $converter->convertToHtml($string);
+        return (string) $converter->convertToHtml($string);
     }
 
     /**
