b7371f967e9b403dbad1a0b7c8ba617b0f121702
Rob Fonseca <robfonseca@gmail.com>
2021-07-15 20:56:52 -0400

    Apply another styling fix

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -2307,7 +2307,7 @@ class Builder
      */
     public function toRawSql()
     {
-        return array_reduce($this->getBindings(), function($sql, $binding) {
+        return array_reduce($this->getBindings(), function ($sql, $binding) {
             return preg_replace('/\?/', is_numeric($binding) ? $binding : "'".$binding."'", $sql, 1);
         }, $this->toSql());
     }
