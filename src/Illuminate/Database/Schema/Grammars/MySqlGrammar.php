b60d3472b4e9e8141c488a3ba63f7ac0c93dc6ba
郑蓬勃 <571821527@qq.com>
2021-07-22 09:45:37 +0800

    fix:Undefined property: stdClass::$column_name for run mysql 8.0.25

--- src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
@@ -39,7 +39,7 @@ class MySqlGrammar extends Grammar
      */
     public function compileColumnExists()
     {
-        return 'select column_name from information_schema.columns where table_schema = ? and table_name = ?';
+        return 'select column_name as `column_name` from information_schema.columns where table_schema = ? and table_name = ?';
     }
 
     /**
