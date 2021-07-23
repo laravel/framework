2dcff782b153727a8f026ac91b30ffb9e86873e3
Stephen Sweetland <steve.sweetland@gmail.com>
2021-07-23 12:50:34 +0100

    Update Mix.php
    Detect new ENV variable; Useful if you're serving a local app with ngrok, to change the url that gets output if in hot mode.

--- src/Illuminate/Foundation/Mix.php
+++ src/Illuminate/Foundation/Mix.php
@@ -31,6 +31,11 @@ class Mix
 
         if (is_file(public_path($manifestDirectory.'/hot'))) {
             $url = rtrim(file_get_contents(public_path($manifestDirectory.'/hot')));
+            
+            $hot_proxy_url = app('config')->get('app.mix_hot_proxy_url');
+            if (! empty($hot_proxy_url)) {
+                return new HtmlString("{$hot_proxy_url}{$path}");
+            }
 
             if (Str::startsWith($url, ['http://', 'https://'])) {
                 return new HtmlString(Str::after($url, ':').$path);
