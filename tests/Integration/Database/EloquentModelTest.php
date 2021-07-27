31a2469724f1211c63a129f57b6aad08000929da
ajcastro <ajcastro29@gmail.com>
2021-07-27 13:35:33 +0800

    Fix test, use normal functions

--- tests/Integration/Database/EloquentModelTest.php
+++ tests/Integration/Database/EloquentModelTest.php
@@ -120,8 +120,8 @@ class EloquentModelTest extends DatabaseTestCase
         $this->assertTrue($user->changedTo('score', 1, 0));
         $this->assertFalse($user->changedTo('score', 1, 2));
 
-        $this->assertTrue($user->changedTo('score', fn ($score) => $score > 0));
-        $this->assertFalse($user->changedTo('score', fn ($score) => $score < 0));
+        $this->assertTrue($user->changedTo('score', function($score) { return $score > 0;}));
+        $this->assertFalse($user->changedTo('score', function($score) { return $score < 0;}));
 
         $user->items = [1, 2];
 
@@ -175,8 +175,8 @@ class EloquentModelTest extends DatabaseTestCase
         $this->assertTrue($user->changingTo('score', 1, 0));
         $this->assertFalse($user->changingTo('score', 1, 2));
 
-        $this->assertTrue($user->changingTo('score', fn ($score) => $score > 0));
-        $this->assertFalse($user->changingTo('score', fn ($score) => $score < 0));
+        $this->assertTrue($user->changingTo('score', function($score) { return $score > 0;}));
+        $this->assertFalse($user->changingTo('score', function($score) { return $score < 0;}));
 
         $user->items = [1, 2];
 
