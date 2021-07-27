baa89a3ee32845a4f7f89cafc7546359dae61f58
n.marijanovic@hotmail.com <bittextures@gmail.com>
2021-07-27 19:35:58 +0200

    Added Illuminate\Support\Arr mergeByAppending method

--- src/Illuminate/Collections/Arr.php
+++ src/Illuminate/Collections/Arr.php
@@ -701,4 +701,24 @@ class Arr
 
         return is_array($value) ? $value : [$value];
     }
+
+    /**
+     * Merge two arrays by appending one array keys to other array values.
+     *
+     * @param  array  $toAppendOn
+     * @param  array  $toAppend
+     * @param  boolean  $returnDefault
+     * @param  mixed  $default
+     * @return array
+     */
+    public static function mergeByAppending(array $toAppendOn, array $toAppend, $returnDefault = false, $default = null)
+    {
+        return array_map(function ($value) use ($toAppend, $returnDefault, $default) {
+            return static::get(
+                $toAppend, $value, $returnDefault
+                    ? $default
+                    : $value
+            );
+        }, $toAppendOn);
+    }
 }
