1fd1bb164761ab6164ba6136110f88a27bdfb0c2
Dries Vints <dries@vints.io>
2021-08-03 16:03:47 +0200

    [8.x] Use actual countable interface on MessageBag (#38227)
    * Use actual countable interface on MessageBag
    
    * Remove Arrayable contract

--- src/Illuminate/Support/MessageBag.php
+++ src/Illuminate/Support/MessageBag.php
@@ -2,14 +2,12 @@
 
 namespace Illuminate\Support;
 
-use Countable;
-use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Contracts\Support\MessageBag as MessageBagContract;
 use Illuminate\Contracts\Support\MessageProvider;
 use JsonSerializable;
 
-class MessageBag implements Arrayable, Countable, Jsonable, JsonSerializable, MessageBagContract, MessageProvider
+class MessageBag implements Jsonable, JsonSerializable, MessageBagContract, MessageProvider
 {
     /**
      * All of the registered messages.
