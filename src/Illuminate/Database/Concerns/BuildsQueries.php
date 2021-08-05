95295d0fd8ef2c63cd0d9747e69ebbbab72982c4
Dries Vints <dries@vints.io>
2021-08-05 15:41:41 +0200

    Fix aliasing with cursor pagination (#38251)

--- src/Illuminate/Database/Concerns/BuildsQueries.php
+++ src/Illuminate/Database/Concerns/BuildsQueries.php
@@ -354,8 +354,10 @@ trait BuildsQueries
 
         if (! is_null($columns)) {
             foreach ($columns as $column) {
-                if (stripos($column, ' as ') !== false) {
-                    [$original, $alias] = explode(' as ', $column);
+                if (($position = stripos($column, ' as ')) !== false) {
+                    $as = substr($column, $position, 4);
+
+                    [$original, $alias] = explode($as, $column);
 
                     if ($parameter === $alias) {
                         return $original;
