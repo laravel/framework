76bbedbb384b2b0caeb2e248a811df6e964fed2d
DarkGhosthunter <DarkGhostHunter@gmail.com>
2021-08-08 19:54:32 -0400

    Adds UrlRouting capabilities to Carbon.

--- src/Illuminate/Support/Carbon.php
+++ src/Illuminate/Support/Carbon.php
@@ -4,8 +4,10 @@ namespace Illuminate\Support;
 
 use Carbon\Carbon as BaseCarbon;
 use Carbon\CarbonImmutable as BaseCarbonImmutable;
+use Carbon\Exceptions\InvalidFormatException;
+use Illuminate\Contracts\Routing\UrlRoutable;
 
-class Carbon extends BaseCarbon
+class Carbon extends BaseCarbon implements UrlRoutable
 {
     /**
      * {@inheritdoc}
@@ -14,5 +16,41 @@ class Carbon extends BaseCarbon
     {
         BaseCarbon::setTestNow($testNow);
         BaseCarbonImmutable::setTestNow($testNow);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRouteKey()
+    {
+        return 'date';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRouteKeyName()
+    {
+        return $this->getRouteKey();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveRouteBinding($value, $field = null)
+    {
+        try {
+            return static::createFromFormat($field ?? '!Y-m-d', $value)->setTime(0, 0);
+        } catch (InvalidFormatException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveChildRouteBinding($childType, $value, $field)
+    {
+        return null;
     }
 }
