9741181f6f591402a5ede6a244ecaa45f5493af1
Choraimy Kroonstuiver <3661474+axlon@users.noreply.github.com>
2021-08-09 16:12:47 +0200

    Widen Batch::add($jobs) parameter type

--- src/Illuminate/Bus/Batch.php
+++ src/Illuminate/Bus/Batch.php
@@ -156,7 +156,7 @@ class Batch implements Arrayable, JsonSerializable
     /**
      * Add additional jobs to the batch.
      *
-     * @param  \Illuminate\Support\Collection|array  $jobs
+     * @param  \Illuminate\Support\Enumerable|array  $jobs
      * @return self
      */
     public function add($jobs)
