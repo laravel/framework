bfe7655e3c1fc8350d7a3e2f5296050243aacd75
Dan Harrin <git@danharrin.com>
2021-08-13 08:50:36 +0100

    Create ComponentSlot class

---
+++ src/Illuminate/View/ComponentSlot.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Illuminate\View;
+
+use Illuminate\Contracts\Support\Htmlable;
+
+class ComponentSlot implements Htmlable
+{
+    /**
+     * The slot attribute bag.
+     *
+     * @var \Illuminate\View\ComponentAttributeBag
+     */
+    public $attributes;
+
+    /**
+     * The slot contents.
+     *
+     * @var string
+     */
+    protected $contents;
+
+    /**
+     * Create a new slot instance.
+     *
+     * @param  string  $contents
+     * @param  array  $attributes
+     * @return void
+     */
+    public function __construct($contents = '', $attributes = [])
+    {
+        $this->contents = $contents;
+        $this->withAttributes($attributes);
+    }
+
+    /**
+     * Get the HTML string.
+     *
+     * @return string
+     */
+    public function toHtml()
+    {
+        return $this->contents;
+    }
+
+    /**
+     * Determine if the given slot is empty.
+     *
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return $this->contents === '';
+    }
+
+    /**
+     * Determine if the given slot is not empty.
+     *
+     * @return bool
+     */
+    public function isNotEmpty()
+    {
+        return ! $this->isEmpty();
+    }
+
+    /**
+     * Set the extra attributes that the slot should make available.
+     *
+     * @param  array  $attributes
+     * @return $this
+     */
+    public function withAttributes(array $attributes)
+    {
+        $this->attributes = new ComponentAttributeBag($attributes);
+
+        return $this;
+    }
+
+    /**
+     * Get the HTML string.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->toHtml();
+    }
+}
