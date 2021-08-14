a668e6a2fef6421a9b171d2474561d101f9a2db8
Christoph Beyer <88290502+the-real-christoph@users.noreply.github.com>
2021-08-14 18:39:52 +0200

    [8.x] Check for incomplete class to prevent unexpected error when class cannot be loaded (#38379)
    * Check for incomplete class to prevent unexpected error when class cannot be loaded
    
    * Check for incomplete class to prevent unexpected error when class cannot be loaded (style)
    
    * Update RetryCommand.php
    
    Co-authored-by: Christoph Beyer <the-real-christoph>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Queue/Console/RetryCommand.php
+++ src/Illuminate/Queue/Console/RetryCommand.php
@@ -171,7 +171,7 @@ class RetryCommand extends Command
             throw new RuntimeException('Unable to extract job payload.');
         }
 
-        if (is_object($instance) && method_exists($instance, 'retryUntil')) {
+        if (is_object($instance) && ! $instance instanceof \__PHP_Incomplete_Class && method_exists($instance, 'retryUntil')) {
             $retryUntil = $instance->retryUntil();
 
             $payload['retryUntil'] = $retryUntil instanceof DateTimeInterface
