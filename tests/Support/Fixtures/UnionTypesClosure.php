ef39004cadd8284d37c321cbabae2b002fe7aa2e
Dries Vints <dries@vints.io>
2021-08-16 12:11:43 +0200

    Fix tests

---
+++ tests/Support/Fixtures/UnionTypesClosure.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Tests\Support\ExampleParameter;
+use Illuminate\Tests\Support\AnotherExampleParameter;
+
+return function (ExampleParameter | AnotherExampleParameter $a, $b) {
+    //
+};
