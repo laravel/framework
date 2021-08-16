108099b35263aaeaf32faed0e4fbb276f6566b8a
Taylor Otwell <taylorotwell@users.noreply.github.com>
2021-08-16 10:12:42 +0000

    Apply fixes from StyleCI

--- tests/Support/Fixtures/UnionTypesClosure.php
+++ tests/Support/Fixtures/UnionTypesClosure.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Tests\Support\ExampleParameter;
 use Illuminate\Tests\Support\AnotherExampleParameter;
+use Illuminate\Tests\Support\ExampleParameter;
 
 return function (ExampleParameter | AnotherExampleParameter $a, $b) {
     //
