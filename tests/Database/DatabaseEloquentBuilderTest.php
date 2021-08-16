7f2cd18b675010bdc1ebcdd2f7d62c54e88448f6
Gianluca <theVannu>
2021-08-16 19:16:30 +0200

    fix test 3

--- tests/Database/DatabaseEloquentBuilderTest.php
+++ tests/Database/DatabaseEloquentBuilderTest.php
@@ -203,7 +203,7 @@ class DatabaseEloquentBuilderTest extends TestCase
 
         $builder->setModel(new EloquentModelStub);
 
-        $this->assertEquals(['stub.column', 'stub.name'], $builder->qualifyColumns('column', 'name'));
+        $this->assertEquals(['stub.column', 'stub.name'], $builder->qualifyColumns(['column', 'name']));
     }
 
     public function testGetMethodLoadsModelsAndHydratesEagerRelations()
