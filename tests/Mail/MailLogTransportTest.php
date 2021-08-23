634998a3a4a65b08a2e932b719b6b38c6295da1a
Dries Vints <dries@vints.io>
2021-08-23 18:28:47 +0200

    Fix tests

--- tests/Mail/MailLogTransportTest.php
+++ tests/Mail/MailLogTransportTest.php
@@ -22,7 +22,7 @@ class MailLogTransportTest extends TestCase
             'path' => 'mail.log',
         ]);
 
-        $transport = app('mailer')->getSwiftMailer()->getTransport();
+        $transport = app('mailer')->getSymfonyTransport();
         $this->assertInstanceOf(LogTransport::class, $transport);
 
         $logger = $transport->logger();
@@ -38,7 +38,7 @@ class MailLogTransportTest extends TestCase
         $this->app['config']->set('mail.driver', 'log');
         $logger = $this->app->instance('log', new NullLogger);
 
-        $transportLogger = app('mailer')->getSwiftMailer()->getTransport()->logger();
+        $transportLogger = app('mailer')->getSymfonyTransport()->logger();
 
         $this->assertEquals($logger, $transportLogger);
     }
