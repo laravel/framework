14207fbb131f7cf7a9fe2cc9bf56998637800cdd
刘军 <995672751@qq.com>
2021-08-26 00:53:19 +0800

    Reduce duplicate calls
    Reduce duplicate calls

--- src/Illuminate/Queue/Worker.php
+++ src/Illuminate/Queue/Worker.php
@@ -126,7 +126,7 @@ class Worker
      */
     public function daemon($connectionName, $queue, WorkerOptions $options)
     {
-        if ($this->supportsAsyncSignals()) {
+        if ($supportsAsyncSignals = $this->supportsAsyncSignals()) {
             $this->listenForSignals();
         }
 
@@ -159,7 +159,7 @@ class Worker
                 $this->manager->connection($connectionName), $queue
             );
 
-            if ($this->supportsAsyncSignals()) {
+            if ($supportsAsyncSignals) {
                 $this->registerTimeoutHandler($job, $options);
             }
 
@@ -178,7 +178,7 @@ class Worker
                 $this->sleep($options->sleep);
             }
 
-            if ($this->supportsAsyncSignals()) {
+            if ($supportsAsyncSignals) {
                 $this->resetTimeoutHandler();
             }
 
