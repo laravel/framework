cb020a4ba569e3b1edb620af388181844ac9f0de
Ostap Bregin <osbre@protonmail.com>
2021-09-01 21:49:20 +0300

    Add PHPDoc method for `View` facade

--- src/Illuminate/Support/Facades/View.php
+++ src/Illuminate/Support/Facades/View.php
@@ -6,6 +6,7 @@ namespace Illuminate\Support\Facades;
  * @method static \Illuminate\Contracts\View\Factory addNamespace(string $namespace, string|array $hints)
  * @method static \Illuminate\Contracts\View\View first(array $views, \Illuminate\Contracts\Support\Arrayable|array $data = [], array $mergeData = [])
  * @method static \Illuminate\Contracts\View\Factory replaceNamespace(string $namespace, string|array $hints)
+ * @method static \Illuminate\Contracts\View\Factory addExtension(string $extension, string $engine, \Closure|null $resolver = null)
  * @method static \Illuminate\Contracts\View\View file(string $path, array $data = [], array $mergeData = [])
  * @method static \Illuminate\Contracts\View\View make(string $view, array $data = [], array $mergeData = [])
  * @method static array composer(array|string $views, \Closure|string $callback)
