bd7a519d50ff4c3350b0bd42debbe54c9fa05f56
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2021-09-06 19:41:08 +0100

    [9.x] Support the latest psr/container versions (#38692)
    * Support the latest psr/container versions
    
    * Added types

--- src/Illuminate/Container/Container.php
+++ src/Illuminate/Container/Container.php
@@ -189,7 +189,7 @@ class Container implements ArrayAccess, ContainerContract
     /**
      *  {@inheritdoc}
      */
-    public function has($id)
+    public function has(string $id): bool
     {
         return $this->bound($id);
     }
@@ -695,7 +695,7 @@ class Container implements ArrayAccess, ContainerContract
     /**
      *  {@inheritdoc}
      */
-    public function get($id)
+    public function get(string $id)
     {
         try {
             return $this->resolve($id);
