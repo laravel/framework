fcdd3a58bfa199bf5161614dd6dc07698c606500
Francisco Madeira <xico2k@gmail.com>
2021-09-20 15:41:28 +0100

    [9.x] Only throws BroadcastException on all drivers (#38862)
    * Normalize Broadcasting Exceptions to all return BroadcastException
    
    * Import BroadcastException and fix typo

--- src/Illuminate/Broadcasting/Broadcasters/AblyBroadcaster.php
+++ src/Illuminate/Broadcasting/Broadcasters/AblyBroadcaster.php
@@ -3,6 +3,8 @@
 namespace Illuminate\Broadcasting\Broadcasters;
 
 use Ably\AblyRest;
+use Ably\Exceptions\AblyException;
+use Illuminate\Broadcasting\BroadcastException;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
@@ -117,11 +119,19 @@ class AblyBroadcaster extends Broadcaster
      * @param  string  $event
      * @param  array  $payload
      * @return void
+     *
+     * @throws \Illuminate\Broadcasting\BroadcastException
      */
     public function broadcast(array $channels, $event, array $payload = [])
     {
-        foreach ($this->formatChannels($channels) as $channel) {
-            $this->ably->channels->get($channel)->publish($event, $payload);
+        try {
+            foreach ($this->formatChannels($channels) as $channel) {
+                $this->ably->channels->get($channel)->publish($event, $payload);
+            }
+        } catch (AblyException $e) {
+            throw new BroadcastException(
+                sprintf('Ably error: %s', $e->getMessage())
+            );
         }
     }
 
