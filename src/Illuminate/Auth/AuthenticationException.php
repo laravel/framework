b8750b5c2e7f52f7db27e30f752f5aa272977372
Jeffrey Angenent <jeffrey.angenent@23g.nl>
2021-09-20 21:17:11 +0200

    Use correct type hint for redirectTo

--- src/Illuminate/Auth/AuthenticationException.php
+++ src/Illuminate/Auth/AuthenticationException.php
@@ -16,7 +16,7 @@ class AuthenticationException extends Exception
     /**
      * The path the user should be redirected to.
      *
-     * @var string
+     * @var string|null
      */
     protected $redirectTo;
 
@@ -49,7 +49,7 @@ class AuthenticationException extends Exception
     /**
      * Get the path the user should be redirected to.
      *
-     * @return string
+     * @return string|null
      */
     public function redirectTo()
     {
