cb72e7b03ce793fa2fbdfc191eea4c680215fe5d
Francisco Madeira <xico2k@gmail.com>
2021-09-21 14:37:59 +0100

    [8.x] Add assertNotSoftDeleted Method (#38886)
    * Add assertNotSoftDelete Method
    
    * add test
    
    Co-authored-by: Taylor Otwell <taylorotwell@gmail.com>

--- tests/Foundation/FoundationInteractsWithDatabaseTest.php
+++ tests/Foundation/FoundationInteractsWithDatabaseTest.php
@@ -250,6 +250,72 @@ class FoundationInteractsWithDatabaseTest extends TestCase
         $this->assertSoftDeleted(new CustomProductStub($this->data));
     }
 
+    public function testAssertNotSoftDeletedInDatabaseFindsResults()
+    {
+        $builder = $this->mockCountBuilder(1);
+
+        $this->assertNotSoftDeleted($this->table, $this->data);
+    }
+
+    public function testAssertNotSoftDeletedSupportModelStrings()
+    {
+        $this->mockCountBuilder(1);
+
+        $this->assertNotSoftDeleted(ProductStub::class, $this->data);
+    }
+
+    public function testAssertNotSoftDeletedOnlyFindsMatchingModels()
+    {
+        $this->expectException(ExpectationFailedException::class);
+        $this->expectExceptionMessage('Failed asserting that any existing row');
+
+        $builder = $this->mockCountBuilder(0);
+
+        $builder->shouldReceive('get')->andReturn(collect(), collect(1));
+
+        $this->assertNotSoftDeleted(ProductStub::class, $this->data);
+    }
+
+    public function testAssertNotSoftDeletedInDatabaseDoesNotFindResults()
+    {
+        $this->expectException(ExpectationFailedException::class);
+        $this->expectExceptionMessage('The table is empty.');
+
+        $builder = $this->mockCountBuilder(0);
+
+        $builder->shouldReceive('get')->andReturn(collect());
+
+        $this->assertNotSoftDeleted($this->table, $this->data);
+    }
+
+    public function testAssertNotSoftDeletedInDatabaseDoesNotFindModelResults()
+    {
+        $this->expectException(ExpectationFailedException::class);
+        $this->expectExceptionMessage('The table is empty.');
+
+        $this->data = ['id' => 1];
+
+        $builder = $this->mockCountBuilder(0);
+
+        $builder->shouldReceive('get')->andReturn(collect());
+
+        $this->assertNotSoftDeleted(new ProductStub($this->data));
+    }
+
+    public function testAssertNotSoftDeletedInDatabaseDoesNotFindModelWithCustomColumnResults()
+    {
+        $this->expectException(ExpectationFailedException::class);
+        $this->expectExceptionMessage('The table is empty.');
+
+        $this->data = ['id' => 1];
+
+        $builder = $this->mockCountBuilder(0, 'trashed_at');
+
+        $builder->shouldReceive('get')->andReturn(collect());
+
+        $this->assertNotSoftDeleted(new CustomProductStub($this->data));
+    }
+
     public function testAssertExistsPassesWhenFindsResults()
     {
         $this->data = ['id' => 1];
@@ -282,6 +348,8 @@ class FoundationInteractsWithDatabaseTest extends TestCase
         $builder->shouldReceive('where')->with($this->data)->andReturnSelf();
 
         $builder->shouldReceive('whereNotNull')->with($deletedAtColumn)->andReturnSelf();
+
+        $builder->shouldReceive('whereNull')->with($deletedAtColumn)->andReturnSelf();
 
         $builder->shouldReceive('count')->andReturn($countResult)->byDefault();
 
