e055e1ef07a9f3a583d79764c756404569a74895
Taylor Otwell <taylorotwell@gmail.com>
2021-09-21 13:28:52 -0500

    fix doc block

--- src/Illuminate/Support/Facades/DB.php
+++ src/Illuminate/Support/Facades/DB.php
@@ -28,7 +28,7 @@ namespace Illuminate\Support\Facades;
  * @method static void enableQueryLog()
  * @method static void disableQueryLog()
  * @method static void flushQueryLog()
- * @method static void beforeExecuting(\Closure $callback)
+ * @method static \Illuminate\Database\Connection beforeExecuting(\Closure $callback)
  * @method static void listen(\Closure $callback)
  * @method static void rollBack(int $toLevel = null)
  * @method static void setDefaultConnection(string $name)
