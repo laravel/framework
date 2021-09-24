df9ea5dbddc73fadcd89398496f6c8cbdf58f286
Shalvah <shalvah@users.noreply.github.com>
2021-09-24 18:53:40 +0100

    [8.x] Add --pretend option for model:prune command (#38945)
    * Add --pretend option for model:prune command
    
    * Fix tests
    
    * Fix code style

--- src/Illuminate/Database/Console/PruneCommand.php
+++ src/Illuminate/Database/Console/PruneCommand.php
@@ -19,7 +19,8 @@ class PruneCommand extends Command
      */
     protected $signature = 'model:prune
                                 {--model=* : Class names of the models to be pruned}
-                                {--chunk=1000 : The number of models to retrieve per chunk of models to be deleted}';
+                                {--chunk=1000 : The number of models to retrieve per chunk of models to be deleted}
+                                {--pretend : Display the number of prunable records found instead of deleting them}';
 
     /**
      * The console command description.
@@ -40,6 +41,14 @@ class PruneCommand extends Command
 
         if ($models->isEmpty()) {
             $this->info('No prunable models found.');
+
+            return;
+        }
+
+        if ($this->option('pretend')) {
+            $models->each(function ($model) {
+                $this->pretendToPrune($model);
+            });
 
             return;
         }
@@ -103,5 +112,27 @@ class PruneCommand extends Command
         $uses = class_uses_recursive($model);
 
         return in_array(Prunable::class, $uses) || in_array(MassPrunable::class, $uses);
+    }
+
+    /**
+     * Display how many models will be pruned.
+     *
+     * @param  string  $model
+     * @return void
+     */
+    protected function pretendToPrune($model)
+    {
+        $instance = new $model;
+
+        $count = $instance->prunable()
+            ->when(in_array(SoftDeletes::class, class_uses_recursive(get_class($instance))), function ($query) {
+                $query->withTrashed();
+            })->count();
+
+        if ($count === 0) {
+            $this->info("No prunable [$model] records found.");
+        } else {
+            $this->info("{$count} [{$model}] records will be pruned.");
+        }
     }
 }
