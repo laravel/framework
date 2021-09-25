50cc14ffba1e432ed8334ee9ab7ba807eb1d0ea6
mateusjunges <mateus@junges.dev>
2021-09-25 20:11:16 -0300

    apply ci fixes

--- tests/Validation/ValidationPasswordRuleTest.php
+++ tests/Validation/ValidationPasswordRuleTest.php
@@ -235,7 +235,7 @@ class ValidationPasswordRuleTest extends TestCase
     public function testItPassesWithValidDataIfTheSameValidationRulesAreReused()
     {
         $rules = [
-            'password' => Password::default()
+            'password' => Password::default(),
         ];
 
         $v = new Validator(
