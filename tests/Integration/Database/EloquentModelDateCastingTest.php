0e403b0e715bb3354e45ae0e6e63bbec2317a80a
Andrew Minion <andrew@andrewrminion.com>
2021-09-27 13:01:28 -0500

    use underlying Carbon

--- tests/Integration/Database/EloquentModelDateCastingTest.php
+++ tests/Integration/Database/EloquentModelDateCastingTest.php
@@ -2,10 +2,10 @@
 
 namespace Illuminate\Tests\Integration\Database\EloquentModelDateCastingTest;
 
+use Carbon\Carbon;
 use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Tests\Integration\Database\DatabaseTestCase;
 
