7a092066f88982e5537397abd311588635aa8f3e
Marc van der Meulen <marc@level23.nl>
2021-09-29 11:55:33 +0200

    Add new lost connection message to DetectsLostConnections

--- src/Illuminate/Database/DetectsLostConnections.php
+++ src/Illuminate/Database/DetectsLostConnections.php
@@ -45,6 +45,7 @@ trait DetectsLostConnections
             'The connection is broken and recovery is not possible. The connection is marked by the client driver as unrecoverable. No attempt was made to restore the connection.',
             'SQLSTATE[HY000] [2002] php_network_getaddresses: getaddrinfo failed: Try again',
             'SQLSTATE[HY000]: General error: 7 SSL SYSCALL error: EOF detected',
+            'The client was disconnected by the server because of inactivity. See wait_timeout and interactive_timeout for configuring this behavior.',
         ]);
     }
 }
