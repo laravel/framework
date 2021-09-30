f0ac775c353f359ff717d75ed9353efa31a935de
Joseph Silber <contact@josephsilber.com>
2021-09-30 01:52:32 -0400

    Fix LazyCollection#unique() double enumeration
    Fixes #38817

--- src/Illuminate/Collections/Traits/EnumeratesValues.php
+++ src/Illuminate/Collections/Traits/EnumeratesValues.php
@@ -774,28 +774,6 @@ trait EnumeratesValues
     }
 
     /**
-     * Return only unique items from the collection array.
-     *
-     * @param  string|callable|null  $key
-     * @param  bool  $strict
-     * @return static
-     */
-    public function unique($key = null, $strict = false)
-    {
-        $callback = $this->valueRetriever($key);
-
-        $exists = [];
-
-        return $this->reject(function ($item, $key) use ($callback, $strict, &$exists) {
-            if (in_array($id = $callback($item, $key), $exists, $strict)) {
-                return true;
-            }
-
-            $exists[] = $id;
-        });
-    }
-
-    /**
      * Return only unique items from the collection array using strict comparison.
      *
      * @param  string|callable|null  $key
