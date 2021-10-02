cb6a8799f30bca4799b831816729b5018f9e1fa5
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-10-03 00:08:46 +0300

    perf: class_basename helper

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -70,7 +70,7 @@ if (! function_exists('class_basename')) {
     {
         $class = is_object($class) ? get_class($class) : $class;
 
-        return basename(str_replace('\\', '/', $class));
+        return (new \ReflectionClass($this))->getShortName();
     }
 }
 
