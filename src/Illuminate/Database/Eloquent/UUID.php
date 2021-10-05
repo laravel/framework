9ff2fdd8dbbbd52d1358a09816b02fbee452b76b
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-10-06 00:11:42 +0100

    Added UUID

---
+++ src/Illuminate/Database/Eloquent/UUID.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Illuminate\Database\Eloquent;
+
+use Illuminate\Support\Str;
+
+trait UUID
+{
+    /**
+     * Boot the soft deleting trait for a model.
+     *
+     * @return void
+     */
+    protected static function boot ()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
+            }
+        });
+    }
+
+    /**
+     * Disables auto-incrementing on model id.
+     *
+     * @return false
+     */
+    public function getIncrementing ()
+    {
+        return false;
+    }
+
+    /**
+     * Set key type to string, instead of integer.
+     *
+     * @return string
+     */
+    public function getKeyType ()
+    {
+        return 'string';
+    }
+}
