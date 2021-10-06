4ccd38bc067aba47aaab97b9cf87eca63976593e
Claas Augner <github@caugner.de>
2021-10-06 11:40:47 +0200

    App::abort() never returns

--- src/Illuminate/Support/Facades/App.php
+++ src/Illuminate/Support/Facades/App.php
@@ -36,7 +36,7 @@ namespace Illuminate\Support\Facades;
  * @method static string storagePath(string $path = '')
  * @method static string version()
  * @method static string|bool environment(string|array ...$environments)
- * @method static void abort(int $code, string $message = '', array $headers = [])
+ * @method static never abort(int $code, string $message = '', array $headers = [])
  * @method static void boot()
  * @method static void booted(callable $callback)
  * @method static void booting(callable $callback)
