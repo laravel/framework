f7c9f2c7c4d032af8c7c3fcec76c71313fb7ebb8
Nuno Maduro <enunomaduro@gmail.com>
2021-10-28 21:43:44 +0200

    [8.x] Improves internal `./bin/test` script (#39404)
    * Improves "test" script
    
    * Apply fixes from StyleCI
    
    * Reverts change regarding memcached
    
    Co-authored-by: Taylor Otwell <taylorotwell@users.noreply.github.com>

--- tests/Filesystem/FilesystemTest.php
+++ tests/Filesystem/FilesystemTest.php
@@ -21,7 +21,7 @@ class FilesystemTest extends TestCase
      */
     public static function setUpTempDir()
     {
-        self::$tempDir = __DIR__.'/tmp';
+        self::$tempDir = sys_get_temp_dir().'/tmp';
         mkdir(self::$tempDir);
     }
 
