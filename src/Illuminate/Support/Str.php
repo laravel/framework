92cfdfb5941c0b41f7d0e1d4e6a47750d73f679b
Italo <DarkGhostHunter@Gmail.com>
2021-10-29 10:34:03 -0300

    [8.x] Adds `mask()` for masking part of a string (#39393)
    * [8.x] Adds `mask()` for masking part of a string.
    
    * Removed multi-byte functions due to no string functions consistency.
    
    * Fixed method for PHP 7.3 and PHP 7.4.
    
    * Refactored helper for multi-byte strings and custom encoding.
    
    * Style fixing.
    
    * Update Str.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -399,6 +399,38 @@ class Str
     }
 
     /**
+     * Masks a portion of a string with a repeated character.
+     *
+     * @param  string  $string
+     * @param  string  $character
+     * @param  int  $index
+     * @param  int|null  $length
+     * @param  string  $encoding
+     * @return string
+     */
+    public static function mask($string, $character, $index, $length = null, $encoding = 'UTF-8')
+    {
+        if ($character === '') {
+            return $string;
+        }
+
+        if (is_null($length) && PHP_MAJOR_VERSION < 8) {
+            $length = mb_strlen($string, $encoding);
+        }
+
+        $segment = mb_substr($string, $index, $length, $encoding);
+
+        if ($segment === '') {
+            return $string;
+        }
+
+        $start = mb_substr($string, 0, mb_strpos($string, $segment, 0, $encoding), $encoding);
+        $end = mb_substr($string, mb_strpos($string, $segment, 0, $encoding) + mb_strlen($segment, $encoding));
+
+        return $start.str_repeat(mb_substr($character, 0, 1, $encoding), mb_strlen($segment, $encoding)).$end;
+    }
+
+    /**
      * Get the string matching the given pattern.
      *
      * @param  string  $pattern
