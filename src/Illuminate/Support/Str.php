30b5c0568858700b8c748c010933252a505afa00
divinity76 <divinity76@gmail.com>
2021-11-01 17:08:15 +0100

    micro-optimize Illuminate\Support\Str::endsWith()
    substr() creates a new string, substr_compare() doesn't
    
    notably, in PHP8+ str_ends_with might be an even better choice, but it looks like this code is trying to be PHP5-compatible, so for 5 and 7, substr_compare is the 2nd-best-option.
    
    if PHP<=7 compatibility isn't an issue, str_ends_with should be used instead though. substr_compare was introduced in 5.0.0, str_ends_with was introduced in PHP8.0.0

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -219,7 +219,7 @@ class Str
         foreach ((array) $needles as $needle) {
             if (
                 $needle !== '' && $needle !== null
-                && substr($haystack, -strlen($needle)) === (string) $needle
+                && 0 === substr_compare($haystack, $needle, - strlen($needle))
             ) {
                 return true;
             }
