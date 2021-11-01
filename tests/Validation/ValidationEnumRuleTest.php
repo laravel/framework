44f9749a9d99be0b4a6d05466d26f9a870bb3627
Mohamed Said <themohamedsaid@gmail.com>
2021-11-01 19:23:09 +0200

    [8.x] Add an Enum validation rule (#39437)
    * add an Enum validation rule
    
    * fix style
    
    * fix test requirements
    
    * fix style

---
+++ tests/Validation/ValidationEnumRuleTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Illuminate\Tests\Validation;
+
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
+use Illuminate\Validation\Rules\Enum;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationServiceProvider;
+use Illuminate\Validation\Validator;
+use PHPUnit\Framework\TestCase;
+
+if (PHP_VERSION_ID >= 80100) {
+    include 'Enums.php';
+}
+
+/**
+ * @requires PHP >= 8.1
+ */
+class ValidationEnumRuleTest extends TestCase
+{
+    public function testvalidationPassesWhenPassingCorrectEnum()
+    {
+        $v = new Validator(
+            resolve('translator'),
+            [
+                'status' => 'pending',
+                'int_status' => 1,
+            ],
+            [
+                'status' => new Enum(StringStatus::class),
+                'int_status' => new Enum(IntegerStatus::class),
+            ]
+        );
+
+        $this->assertFalse($v->fails());
+    }
+
+    public function testValidationFailsWhenProvidingNoExistingCases()
+    {
+        $v = new Validator(
+            resolve('translator'),
+            [
+                'status' => 'finished',
+            ],
+            [
+                'status' => new Enum(StringStatus::class),
+            ]
+        );
+
+        $this->assertTrue($v->fails());
+        $this->assertEquals(['The selected status is invalid.'], $v->messages()->get('status'));
+    }
+
+    public function testValidationFailsWhenProvidingDifferentType()
+    {
+        $v = new Validator(
+            resolve('translator'),
+            [
+                'status' => 10,
+            ],
+            [
+                'status' => new Enum(StringStatus::class),
+            ]
+        );
+
+        $this->assertTrue($v->fails());
+        $this->assertEquals(['The selected status is invalid.'], $v->messages()->get('status'));
+    }
+
+    public function testValidationPassesWhenProvidingDifferentTypeThatIsCastableToTheEnumType()
+    {
+        $v = new Validator(
+            resolve('translator'),
+            [
+                'status' => '1',
+            ],
+            [
+                'status' => new Enum(IntegerStatus::class),
+            ]
+        );
+
+        $this->assertFalse($v->fails());
+    }
+
+    public function testValidationFailsWhenProvidingNull()
+    {
+        $v = new Validator(
+            resolve('translator'),
+            [
+                'status' => null,
+            ],
+            [
+                'status' => new Enum(StringStatus::class),
+            ]
+        );
+
+        $this->assertTrue($v->fails());
+        $this->assertEquals(['The selected status is invalid.'], $v->messages()->get('status'));
+    }
+
+    public function testValidationPassesWhenProvidingNullButTheFieldIsNullable()
+    {
+        $v = new Validator(
+            resolve('translator'),
+            [
+                'status' => null,
+            ],
+            [
+                'status' => ['nullable', new Enum(StringStatus::class)],
+            ]
+        );
+
+        $this->assertFalse($v->fails());
+    }
+
+    protected function setUp(): void
+    {
+        $container = Container::getInstance();
+
+        $container->bind('translator', function () {
+            return new Translator(
+                new ArrayLoader, 'en'
+            );
+        });
+
+        Facade::setFacadeApplication($container);
+
+        (new ValidationServiceProvider($container))->register();
+    }
+
+    protected function tearDown(): void
+    {
+        Container::setInstance(null);
+
+        Facade::clearResolvedInstances();
+
+        Facade::setFacadeApplication(null);
+
+        Password::$defaultCallback = null;
+    }
+}
