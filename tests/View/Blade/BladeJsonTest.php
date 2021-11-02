8d5c9b81808200fea92d7ec8732688aad8bf306a
tanthammar <tinahammar@gmail.com>
2021-11-02 18:14:04 +0100

    update BladeJsonTest

--- tests/View/Blade/BladeJsonTest.php
+++ tests/View/Blade/BladeJsonTest.php
@@ -7,7 +7,7 @@ class BladeJsonTest extends AbstractBladeTestCase
     public function testStatementIsCompiledWithSafeDefaultEncodingOptions()
     {
         $string = 'var foo = @json($var);';
-        $expected = 'var foo = <?php echo json_encode($var, 15, 512) ?>;';
+        $expected = 'var foo = <?php echo Illuminate\Support\Json::encode($var, 15, 512) ?>;';
 
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
@@ -15,7 +15,7 @@ class BladeJsonTest extends AbstractBladeTestCase
     public function testEncodingOptionsCanBeOverwritten()
     {
         $string = 'var foo = @json($var, JSON_HEX_TAG);';
-        $expected = 'var foo = <?php echo json_encode($var, JSON_HEX_TAG, 512) ?>;';
+        $expected = 'var foo = <?php echo Illuminate\Support\Json::encode($var, JSON_HEX_TAG, 512) ?>;';
 
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
