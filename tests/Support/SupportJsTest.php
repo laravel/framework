bf2f68c76cacd375cd9366780582903c0c3204f9
tanthammar <tinahammar@gmail.com>
2021-11-03 11:04:47 +0100

    fixing tests backslash

--- tests/Support/SupportJsTest.php
+++ tests/Support/SupportJsTest.php
@@ -10,7 +10,7 @@ class SupportJsTest extends TestCase
     public function testJsFrom()
     {
         $this->assertEquals("'hey'", Js::from('hey'));
-        $this->assertEquals("'don\'t mess with my path\\path'", Js::from("don't mess with my path\path"));
+        $this->assertEquals("'don\'t mess with my path\\\path'", Js::from("don't mess with my path\path"));
         $this->assertEquals("JSON.parse(atob('eyJoZXkiOiJ0aGVyZSJ9'))", Js::from(['hey' => 'there']));
         $this->assertEquals("JSON.parse(atob('WyJoZXkiLCJ0aGVyZSJd'))", Js::from(['hey', 'there']));
     }
