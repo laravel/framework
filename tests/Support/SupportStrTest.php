3a4f0073d6efd81b80f9071648744682808bc0cd
Mwikala Kangwa <hello@mwikala.co.uk>
2021-11-06 02:20:09 +0000

    Apply style ci fixes

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -622,7 +622,7 @@ class SupportStrTest extends TestCase
         $this->assertSame([
             'name' => 'Ben Sherred',
             'foo' => 'bar',
-            'array' => ['ping', 'pong']
+            'array' => ['ping', 'pong'],
         ], Str::params('https://laravel.com/?name=Ben%20Sherred&foo=bar&array[]=ping&array[]=pong'));
     }
 }
