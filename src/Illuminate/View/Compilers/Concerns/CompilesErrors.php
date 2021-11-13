c8257ea723c83bdfbbab0cd90ac08f48d8b80cac
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-11-13 10:16:56 +0200

    fix code style

--- src/Illuminate/View/Compilers/Concerns/CompilesErrors.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesErrors.php
@@ -46,6 +46,7 @@ unset($__errorArgs, $__bag); ?>';
         $expression = $this->stripParentheses($expression);
         $expression = explode(', ', $expression);
         $bag = $expression[2] ?? '\'default\'';
+
         return '<?php $__errorArgs = ['.$expression[0].','.$bag.'];
 $__bag = $errors->getBag($__errorArgs[1]);
 if ($__bag->has($__errorArgs[0])){ echo '.$expression[1].';}
