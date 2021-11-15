54c49180e390bc6da0407c95f5a0eeb1387a2a1c
James Mooring <james@lioneagle.solutions>
2021-11-15 19:46:32 +1000

    add conditional policy responses

--- src/Illuminate/Auth/Access/HandlesAuthorization.php
+++ src/Illuminate/Auth/Access/HandlesAuthorization.php
@@ -27,4 +27,32 @@ trait HandlesAuthorization
     {
         return Response::deny($message, $code);
     }
+
+    /**
+     * Conditionally create an access response or throw
+     * an unauthorized exception.
+     *
+     * @param  mixed $condition
+     * @param  string|null  $message
+     * @param  mixed|null  $code
+     * @return \Illuminate\Auth\Access\Response
+     */
+    protected function allowIf($condition, $message = null, $code = null)
+    {
+        return value($condition) ? $this->allow() : $this->deny($message, $code);
+    }
+
+    /**
+     * Conditionally throw an unauthorized exception
+     * or create an access response.
+     *
+     * @param  mixed $condition
+     * @param  string|null  $message
+     * @param  mixed|null  $code
+     * @return \Illuminate\Auth\Access\Response
+     */
+    protected function denyIf($condition, $message = null, $code = null)
+    {
+        return value($condition) ? $this->deny($message, $code) : $this->allow();
+    }
 }
