46bcf50a6af571f904fe995a5499cd4257b987ec
Stephen Rees-Carter <stephen@rees-carter.net>
2021-11-18 09:07:44 +1000

    Add .phar to blocked PHP extensions
    By default Debian includes support for executing .phar files alongside
    .php and .phtml files, and should be included in the blocked list.

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1329,7 +1329,7 @@ trait ValidatesAttributes
         }
 
         $phpExtensions = [
-            'php', 'php3', 'php4', 'php5', 'phtml',
+            'php', 'php3', 'php4', 'php5', 'phtml', 'phar',
         ];
 
         return ($value instanceof UploadedFile)
