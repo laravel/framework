231a703be9b3e9b50855bde7bd3571934e8863f4
Mior Muhammad Zaki <crynobone@gmail.com>
2021-11-17 22:29:04 +0800

    [6.x] Fixes Query Builder to only cast integer when given other than `null` (#39644)
    * Add failing tests
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Only cast integer when given other than null
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Improve tests
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Database/DatabaseQueryBuilderTest.php
+++ tests/Database/DatabaseQueryBuilderTest.php
@@ -1250,6 +1250,14 @@ class DatabaseQueryBuilderTest extends TestCase
         $this->assertSame('select * from "users" limit 10 offset 5', $builder->toSql());
 
         $builder = $this->getBuilder();
+        $builder->select('*')->from('users')->limit(null);
+        $this->assertSame('select * from "users"', $builder->toSql());
+
+        $builder = $this->getBuilder();
+        $builder->select('*')->from('users')->limit(0);
+        $this->assertSame('select * from "users" limit 0', $builder->toSql());
+
+        $builder = $this->getBuilder();
         $builder->select('*')->from('users')->skip(5)->take(10);
         $this->assertSame('select * from "users" limit 10 offset 5', $builder->toSql());
 
@@ -1260,6 +1268,14 @@ class DatabaseQueryBuilderTest extends TestCase
         $builder = $this->getBuilder();
         $builder->select('*')->from('users')->skip(-5)->take(-10);
         $this->assertSame('select * from "users" offset 0', $builder->toSql());
+
+        $builder = $this->getBuilder();
+        $builder->select('*')->from('users')->skip(null)->take(null);
+        $this->assertSame('select * from "users" offset 0', $builder->toSql());
+
+        $builder = $this->getBuilder();
+        $builder->select('*')->from('users')->skip(5)->take(null);
+        $this->assertSame('select * from "users" offset 5', $builder->toSql());
     }
 
     public function testForPage()
