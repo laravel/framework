39cfcedf67c5a10d8692e0ce30e6c13713b0c666
Mior Muhammad Zaki <crynobone@gmail.com>
2021-11-18 23:05:20 +0800

    Add Forgot Password Integration tests
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Integration/Auth/ForgotPasswordTest.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Auth;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Password;
+use Orchestra\Testbench\Factories\UserFactory;
+use Orchestra\Testbench\TestCase;
+use Illuminate\Tests\Integration\Auth\Fixtures\AuthenticationTestUser;
+
+class ForgotPasswordTest extends TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
+    {
+        $app['config']->set('auth.providers.users.model', AuthenticationTestUser::class);
+    }
+
+    /**
+     * Define database migrations.
+     *
+     * @return void
+     */
+    protected function defineDatabaseMigrations()
+    {
+        $this->loadLaravelMigrations();
+    }
+
+    /**
+     * Define routes setup.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     *
+     * @return void
+     */
+    protected function defineRoutes($router)
+    {
+        $router->get('password/reset/{token}', function ($token) {
+            return 'Reset password!';
+        })->name('password.reset');
+
+        $router->get('custom/password/reset/{token}', function ($token) {
+            return 'Custom reset password!';
+        })->name('custom.password.reset');
+    }
+
+    /** @test */
+    public function it_can_send_forgot_password_email()
+    {
+        Notification::fake();
+
+        UserFactory::new()->create();
+
+        $user = AuthenticationTestUser::first();
+
+        Password::broker()->sendResetLink([
+            'email' => $user->email,
+        ]);
+
+        Notification::assertSentTo(
+            $user,
+            function (ResetPassword $notification, $channels) use ($user) {
+                $message = $notification->toMail($user);
+
+                return ! is_null($notification->token)
+                    && $message->actionUrl === route('password.reset', ['token' => $notification->token, 'email' => $user->email]);
+            }
+        );
+    }
+
+    /** @test */
+    public function it_can_send_forgot_password_email_via_create_url_using()
+    {
+        Notification::fake();
+
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+            return route('custom.password.reset', $token);
+        });
+
+        UserFactory::new()->create();
+
+        $user = AuthenticationTestUser::first();
+
+        Password::broker()->sendResetLink([
+            'email' => $user->email,
+        ]);
+
+        Notification::assertSentTo(
+            $user,
+            function (ResetPassword $notification, $channels) use ($user) {
+                $message = $notification->toMail($user);
+
+                return ! is_null($notification->token)
+                    && $message->actionUrl === route('custom.password.reset', ['token' => $notification->token]);
+            }
+        );
+    }
+
+    /** @test */
+    public function it_can_send_forgot_password_email_via_to_mail_using()
+    {
+        Notification::fake();
+
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            return (new MailMessage)
+                ->subject(__('Reset Password Notification'))
+                ->line(__('You are receiving this email because we received a password reset request for your account.'))
+                ->action(__('Reset Password'), route('custom.password.reset', $token))
+                ->line(__('If you did not request a password reset, no further action is required.'));
+        });
+
+        UserFactory::new()->create();
+
+        $user = AuthenticationTestUser::first();
+
+        Password::broker()->sendResetLink([
+            'email' => $user->email,
+        ]);
+
+        Notification::assertSentTo(
+            $user,
+            function (ResetPassword $notification, $channels) use ($user) {
+                $message = $notification->toMail($user);
+
+                return ! is_null($notification->token)
+                    && $message->actionUrl === route('custom.password.reset', ['token' => $notification->token]);
+            }
+        );
+    }
+}
