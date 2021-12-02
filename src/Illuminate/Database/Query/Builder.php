9ae74674e3f75028be62cf1d1ddf40448b5941f4
Wim Reckman <wimreckman@gmail.com>
2021-12-02 21:56:28 +0100

    Fix Illuminate\Database\Query\Builder::whereDate type hint for $column parameter (#39868)
    * Fix Illuminate\Database\Query\Builder::whereDate type hint for $column parameter
    
    * Update Builder.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -1242,7 +1242,7 @@ class Builder
     /**
      * Add a "where date" statement to the query.
      *
-     * @param  string  $column
+     * @param  \Illuminate\Database\Query\Expression|string  $column
      * @param  string  $operator
      * @param  \DateTimeInterface|string|null  $value
      * @param  string  $boolean
