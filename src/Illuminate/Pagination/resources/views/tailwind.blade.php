8ab98a1ddbd34e500fcc77072c07f0ab6c8d9093
Xander <xander@web-refinery.com>
2021-12-04 15:02:05 +0100

    Correct pagination when `firstItem()` is null
    When `firstItem()` is null, the template returns “Showing to of 28 results” 
    
    This PR does a quick check, and when needed will render “Showing 0 of 28 results” instead.

--- src/Illuminate/Pagination/resources/views/tailwind.blade.php
+++ src/Illuminate/Pagination/resources/views/tailwind.blade.php
@@ -26,9 +26,13 @@
             <div>
                 <p class="text-sm text-gray-700 leading-5">
                     {!! __('Showing') !!}
-                    <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                    {!! __('to') !!}
-                    <span class="font-medium">{{ $paginator->lastItem() }}</span>
+                    @if ($paginator->firstItem())
+                        <span class="font-medium">{{ $paginator->firstItem() }}</span>
+                        {!! __('to') !!}
+                        <span class="font-medium">{{ $paginator->lastItem() }}</span>
+                    @else
+                        {{ $paginator->count() }}
+                    @endif
                     {!! __('of') !!}
                     <span class="font-medium">{{ $paginator->total() }}</span>
                     {!! __('results') !!}
