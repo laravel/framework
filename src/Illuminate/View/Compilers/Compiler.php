3e61d54f27bb806ee8d83a585367ba3651b299a6
Taylor Otwell <taylor@laravel.com>
2021-12-06 09:07:30 -0600

    increment hash version on compiled views

--- src/Illuminate/View/Compilers/Compiler.php
+++ src/Illuminate/View/Compilers/Compiler.php
@@ -48,7 +48,7 @@ abstract class Compiler
      */
     public function getCompiledPath($path)
     {
-        return $this->cachePath.'/'.sha1($path).'.php';
+        return $this->cachePath.'/'.sha1('v2'.$path).'.php';
     }
 
     /**
