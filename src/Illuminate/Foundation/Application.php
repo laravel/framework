9776b3f00d8da5eb13b7d44dde1f10d4bdb4967d
Quynh Nguyen <seriquynh@gmail.com>
2021-12-11 08:52:45 +0700

    Fix Application afterLoadingEnvironment  method

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -230,7 +230,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      */
     public function afterLoadingEnvironment(Closure $callback)
     {
-        return $this->afterBootstrapping(
+        $this->afterBootstrapping(
             LoadEnvironmentVariables::class, $callback
         );
     }
