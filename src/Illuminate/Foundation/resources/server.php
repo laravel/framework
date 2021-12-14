94e846b4db46229a7939d9042d2557123fe45035
Nuno Maduro <enunomaduro@gmail.com>
2021-12-14 08:55:12 -0500

    [9.x] Allows to remove the `server.php` file (#40034)
    * Allows to remove the `server.php` file
    
    * Apply fixes from StyleCI
    
    Co-authored-by: Taylor Otwell <taylorotwell@users.noreply.github.com>

---
+++ src/Illuminate/Foundation/resources/server.php
@@ -0,0 +1,16 @@
+<?php
+
+$publicPath = getcwd();
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists($publicPath.$uri)) {
+    return false;
+}
+
+require_once $publicPath.'/index.php';
