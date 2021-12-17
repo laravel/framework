b4f9441b72e1d6da09e212adcbd6f708e35721c6
Nuno Maduro <enunomaduro@gmail.com>
2021-12-17 16:29:48 -0500

    Reverts changes on Register Facades

--- src/Illuminate/Foundation/Bootstrap/RegisterFacades.php
+++ src/Illuminate/Foundation/Bootstrap/RegisterFacades.php
@@ -3,13 +3,9 @@
 namespace Illuminate\Foundation\Bootstrap;
 
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Foundation\PackageManifest;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades;
-use Illuminate\Support\Js;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Facade;
 
 class RegisterFacades
 {
@@ -21,50 +17,11 @@ class RegisterFacades
      */
     public function bootstrap(Application $app)
     {
-        Facades\Facade::clearResolvedInstances();
+        Facade::clearResolvedInstances();
 
-        Facades\Facade::setFacadeApplication($app);
+        Facade::setFacadeApplication($app);
 
-        AliasLoader::getInstance(array_merge([
-            'App' => Facades\App::class,
-            'Arr' => Arr::class,
-            'Artisan' => Facades\Artisan::class,
-            'Auth' => Facades\Auth::class,
-            'Blade' => Facades\Blade::class,
-            'Broadcast' => Facades\Broadcast::class,
-            'Bus' => Facades\Bus::class,
-            'Cache' => Facades\Cache::class,
-            'Config' => Facades\Config::class,
-            'Cookie' => Facades\Cookie::class,
-            'Crypt' => Facades\Crypt::class,
-            'Date' => Facades\Date::class,
-            'DB' => Facades\DB::class,
-            'Eloquent' => Model::class,
-            'Event' => Facades\Event::class,
-            'File' => Facades\File::class,
-            'Gate' => Facades\Gate::class,
-            'Hash' => Facades\Hash::class,
-            'Http' => Facades\Http::class,
-            'Js' => Js::class,
-            'Lang' => Facades\Lang::class,
-            'Log' => Facades\Log::class,
-            'Mail' => Facades\Mail::class,
-            'Notification' => Facades\Notification::class,
-            'Password' => Facades\Password::class,
-            'Queue' => Facades\Queue::class,
-            'RateLimiter' => Facades\RateLimiter::class,
-            'Redirect' => Facades\Redirect::class,
-            'Request' => Facades\Request::class,
-            'Response' => Facades\Response::class,
-            'Route' => Facades\Route::class,
-            'Schema' => Facades\Schema::class,
-            'Session' => Facades\Session::class,
-            'Storage' => Facades\Storage::class,
-            'Str' => Str::class,
-            'URL' => Facades\URL::class,
-            'Validator' => Facades\Validator::class,
-            'View' => Facades\View::class,
-        ],
+        AliasLoader::getInstance(array_merge(
             $app->make('config')->get('app.aliases', []),
             $app->make(PackageManifest::class)->aliases()
         ))->register();
