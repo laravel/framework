d39d185c2e8b0a930c8a7bc3821899f7cda907ce
Anders Søgaard <andershagbard@gmail.com>
2021-12-23 14:36:14 +0100

    Fix doc

--- src/Illuminate/Notifications/Messages/SimpleMessage.php
+++ src/Illuminate/Notifications/Messages/SimpleMessage.php
@@ -160,7 +160,7 @@ class SimpleMessage
     /**
      * Add a line of text to the notification.
      *
-     * @param  mixed  $line
+     * @param  mixed  $lines
      * @return $this
      */
     public function lines($lines)
