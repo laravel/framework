28bc2631d1e46c0c55853f98f8f7f162f2f08f43
Ben James <in@benjam.es>
2022-01-01 17:47:25 +0000

    Make use of vendor:publish for stubs, so packages can hook into it
    Signed-off-by: Ben James <in@benjam.es>

--- src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
+++ src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
@@ -34,6 +34,13 @@ class FoundationServiceProvider extends AggregateServiceProvider
                 __DIR__.'/../Exceptions/views' => $this->app->resourcePath('views/errors/'),
             ], 'laravel-errors');
         }
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes(
+                $this->stubsToPublish(),
+                'laravel-stubs'
+            );
+        }
     }
 
     /**
@@ -112,5 +119,48 @@ class FoundationServiceProvider extends AggregateServiceProvider
                         ->push($event->context['exception']);
             }
         });
+    }
+
+    protected function stubsToPublish()
+    {
+        return [
+            __DIR__.'/../Console/stubs/cast.stub' => $this->app->basePath('stubs/cast.stub'),
+            __DIR__.'/../Console/stubs/console.stub' => $this->app->basePath('stubs/console.stub'),
+            __DIR__.'/../Console/stubs/event.stub' => $this->app->basePath('stubs/event.stub'),
+            __DIR__.'/../Console/stubs/job.queued.stub' => $this->app->basePath('stubs/job.queued.stub'),
+            __DIR__.'/../Console/stubs/job.stub' => $this->app->basePath('stubs/job.stub'),
+            __DIR__.'/../Console/stubs/mail.stub' => $this->app->basePath('stubs/mail.stub'),
+            __DIR__.'/../Console/stubs/markdown-mail.stub' => $this->app->basePath('stubs/markdown-mail.stub'),
+            __DIR__.'/../Console/stubs/markdown-notification.stub' => $this->app->basePath('stubs/markdown-notification.stub'),
+            __DIR__.'/../Console/stubs/model.pivot.stub' => $this->app->basePath('stubs/model.pivot.stub'),
+            __DIR__.'/../Console/stubs/model.stub' => $this->app->basePath('stubs/model.stub'),
+            __DIR__.'/../Console/stubs/notification.stub' => $this->app->basePath('stubs/notification.stub'),
+            __DIR__.'/../Console/stubs/observer.plain.stub' => $this->app->basePath('stubs/observer.plain.stub'),
+            __DIR__.'/../Console/stubs/observer.stub' => $this->app->basePath('stubs/observer.stub'),
+            __DIR__.'/../Console/stubs/policy.plain.stub' => $this->app->basePath('stubs/policy.plain.stub'),
+            __DIR__.'/../Console/stubs/policy.stub' => $this->app->basePath('stubs/policy.stub'),
+            __DIR__.'/../Console/stubs/provider.stub' => $this->app->basePath('stubs/provider.stub'),
+            __DIR__.'/../Console/stubs/request.stub' => $this->app->basePath('stubs/request.stub'),
+            __DIR__.'/../Console/stubs/resource-collection.stub' => $this->app->basePath('stubs/resource-collection.stub'),
+            __DIR__.'/../Console/stubs/resource.stub' => $this->app->basePath('stubs/resource.stub'),
+            __DIR__.'/../Console/stubs/rule.stub' => $this->app->basePath('stubs/rule.stub'),
+            __DIR__.'/../Console/stubs/test.stub' => $this->app->basePath('stubs/test.stub'),
+            __DIR__.'/../Console/stubs/test.unit.stub' => $this->app->basePath('stubs/test.unit.stub'),
+            __DIR__.'/../Console/stubs/view-component.stub' => $this->app->basePath('stubs/view-component.stub'),
+            __DIR__.'/../../Database/Console/Factories/stubs/factory.stub' => $this->app->basePath('stubs/factory.stub'),
+            __DIR__.'/../../Database/Console/Seeds/stubs/seeder.stub' => $this->app->basePath('stubs/seeder.stub'),
+            __DIR__.'/../../Database/Migrations/stubs/migration.create.stub' => $this->app->basePath('stubs/migration.create.stub'),
+            __DIR__.'/../../Database/Migrations/stubs/migration.stub' => $this->app->basePath('stubs/migration.stub'),
+            __DIR__.'/../../Database/Migrations/stubs/migration.update.stub' => $this->app->basePath('stubs/migration.update.stub'),
+            __DIR__.'/../../Routing/Console/stubs/controller.api.stub' => $this->app->basePath('stubs/controller.api.stub'),
+            __DIR__.'/../../Routing/Console/stubs/controller.invokable.stub' => $this->app->basePath('stubs/controller.invokable.stub'),
+            __DIR__.'/../../Routing/Console/stubs/controller.model.api.stub' => $this->app->basePath('stubs/controller.model.api.stub'),
+            __DIR__.'/../../Routing/Console/stubs/controller.model.stub' => $this->app->basePath('stubs/controller.model.stub'),
+            __DIR__.'/../../Routing/Console/stubs/controller.nested.api.stub' => $this->app->basePath('stubs/controller.nested.api.stub'),
+            __DIR__.'/../../Routing/Console/stubs/controller.nested.stub' => $this->app->basePath('stubs/controller.nested.stub'),
+            __DIR__.'/../../Routing/Console/stubs/controller.plain.stub' => $this->app->basePath('stubs/controller.plain.stub'),
+            __DIR__.'/../../Routing/Console/stubs/controller.stub' => $this->app->basePath('stubs/controller.stub'),
+            __DIR__.'/../../Routing/Console/stubs/middleware.stub' => $this->app->basePath('stubs/middleware.stub')
+        ];
     }
 }
