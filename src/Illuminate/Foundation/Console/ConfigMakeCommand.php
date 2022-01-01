cb31b0436c3d30b4da9ed8cbfd5ba8606ed34a83
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-01-01 20:51:12 +0100

    Use Laravel convention

---
+++ src/Illuminate/Foundation/Console/ConfigMakeCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Illuminate\Foundation\Console;
+
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Command\Command;
+
+class ConfigMakeCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:config {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a config file in the config directory';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $viewName = $this->argument('name') . '.php';
+        $configPath = config_path($viewName);
+
+        copy($this->getStub(), $configPath);
+
+        $this->info("Created config file $configPath");
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        $relativePath = '/stubs/config-make.stub';
+
+        return file_exists($customPath = $this->laravel->basePath(trim($relativePath, '/')))
+            ? $customPath
+            : __DIR__ . $relativePath;
+    }
+}
