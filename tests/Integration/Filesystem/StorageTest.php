005918b6068159c581f95393b9c15ab3e8012193
Mior Muhammad Zaki <crynobone@gmail.com>
2022-01-03 22:43:19 +0800

    Add failing tests
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Integration/Filesystem/StorageTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Filesystem;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Orchestra\Testbench\TestCase;
+use Symfony\Component\Process\Process;
+use function Orchestra\Testbench\artisan;
+
+class StorageTest extends TestCase
+{
+    protected $stubFile;
+
+    protected function setUp(): void
+    {
+        $this->afterApplicationCreated(function () {
+            File::put($file = storage_path('app/public/StarDewTaylor.png'), File::get(__DIR__.'/Fixtures/StarDewTaylor.png'));
+            $this->stubFile = $file;
+        });
+
+        $this->beforeApplicationDestroyed(function () {
+            if (File::exists($this->stubFile)) {
+                File::delete($this->stubFile);
+            }
+        });
+
+        parent::setUp();
+    }
+
+    /**
+     * @requires OS Linux|Darwin
+     */
+    public function testItCanDeleteViaStorage()
+    {
+        $this->assertTrue(File::exists($this->stubFile));
+        $this->assertTrue(File::isFile($this->stubFile));
+        Storage::disk('public')->assertExists('StarDewTaylor.png');
+        $this->assertTrue(Storage::disk('public')->exists('StarDewTaylor.png'));
+
+        Storage::disk('public')->delete('StarDewTaylor.png');
+
+        $this->assertFalse(File::exists($this->stubFile));
+        $this->assertFalse(File::isFile($this->stubFile));
+        Storage::disk('public')->assertMissing('StarDewTaylor.png');
+        $this->assertFalse(Storage::disk('public')->exists('StarDewTaylor.png'));
+    }
+
+    /**
+     * @requires OS Linux|Darwin
+     */
+    public function testItCanDeleteViaFilesystem()
+    {
+        $this->assertTrue(File::exists($this->stubFile));
+        $this->assertTrue(File::isFile($this->stubFile));
+        Storage::disk('public')->assertExists('StarDewTaylor.png');
+        $this->assertTrue(Storage::disk('public')->exists('StarDewTaylor.png'));
+
+        File::delete('StarDewTaylor.png');
+
+        $this->assertFalse(File::exists($this->stubFile));
+        $this->assertFalse(File::isFile($this->stubFile));
+        Storage::disk('public')->assertMissing('StarDewTaylor.png');
+        $this->assertFalse(Storage::disk('public')->exists('StarDewTaylor.png'));
+    }
+}
