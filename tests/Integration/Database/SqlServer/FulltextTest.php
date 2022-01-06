6bbbe3b5fb575aca34603d7ba60f573888cc8f54
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-01-06 16:42:53 +0100

    Add integration test

---
+++ tests/Integration/Database/SqlServer/FulltextTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Database\SqlServer;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class FulltextTest extends SqlServerTestCase
+{
+    protected function defineDatabaseMigrationsAfterDatabaseRefreshed()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+           $table->id('id');
+           $table->string('title', 200);
+           $table->text('body');
+           $table->fulltext(['title', 'body']);
+        });
+    }
+
+    protected function destroyDatabaseMigrations()
+    {
+        Schema::drop('articles');
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DB::table('articles')->insert([
+            ['title' => 'PostgreSQL Tutorial', 'body' => 'DBMS stands for DataBase ...'],
+            ['title' => 'How To Use PostgreSQL Well', 'body' => 'After you went through a ...'],
+            ['title' => 'Optimizing PostgreSQL', 'body' => 'In this tutorial, we show ...'],
+            ['title' => '1001 PostgreSQL Tricks', 'body' => '1. Never run mysqld as root. 2. ...'],
+            ['title' => 'PostgreSQL vs. YourSQL', 'body' => 'In the following database comparison ...'],
+            ['title' => 'PostgreSQL Security', 'body' => 'When configured properly, PostgreSQL ...'],
+        ]);
+    }
+
+    public function testWhereFulltext()
+    {
+        $articles = DB::table('articles')->whereFulltext(['title', 'body'], 'database')->orderBy('id')->get();
+
+        $this->assertCount(2, $articles);
+        $this->assertSame('PostgreSQL Tutorial', $articles[0]->title);
+        $this->assertSame('PostgreSQL vs. YourSQL', $articles[1]->title);
+    }
+
+    public function testWhereFulltextAsFreetext()
+    {
+        $articles = DB::table('articles')->whereFulltext(['title', 'body'], 'database'. ['mode' => 'freetext'])->orderBy('id')->get();
+
+        $this->assertCount(2, $articles);
+        $this->assertSame('PostgreSQL Tutorial', $articles[0]->title);
+        $this->assertSame('PostgreSQL vs. YourSQL', $articles[1]->title);
+    }
+}
