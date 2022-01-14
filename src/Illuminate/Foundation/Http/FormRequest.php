56b093c5e02b15f0f4259b6a83dc30c0368049c6
michaelnabil23 <46572405+michaelnabil230@users.noreply.github.com>
2022-01-14 18:01:58 +0200

    [8.x] Add `validated` function in `Request`

--- src/Illuminate/Foundation/Http/FormRequest.php
+++ src/Illuminate/Foundation/Http/FormRequest.php
@@ -5,7 +5,6 @@ namespace Illuminate\Foundation\Http;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Access\Response;
 use Illuminate\Contracts\Container\Container;
-use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 use Illuminate\Contracts\Validation\ValidatesWhenResolved;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Request;
@@ -53,78 +52,19 @@ class FormRequest extends Request implements ValidatesWhenResolved
     protected $redirectAction;
 
     /**
-     * The key to be used for the view error bag.
-     *
-     * @var string
-     */
-    protected $errorBag = 'default';
-
-    /**
-     * Indicates whether validation should stop after the first rule failure.
-     *
-     * @var bool
-     */
-    protected $stopOnFirstFailure = false;
-
-    /**
-     * The validator instance.
-     *
-     * @var \Illuminate\Contracts\Validation\Validator
-     */
-    protected $validator;
-
-    /**
-     * Get the validator instance for the request.
-     *
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function getValidatorInstance()
-    {
-        if ($this->validator) {
-            return $this->validator;
-        }
-
-        $factory = $this->container->make(ValidationFactory::class);
-
-        if (method_exists($this, 'validator')) {
-            $validator = $this->container->call([$this, 'validator'], compact('factory'));
-        } else {
-            $validator = $this->createDefaultValidator($factory);
-        }
-
-        if (method_exists($this, 'withValidator')) {
-            $this->withValidator($validator);
-        }
-
-        $this->setValidator($validator);
-
-        return $this->validator;
-    }
-
-    /**
      * Create the default validator instance.
      *
-     * @param  \Illuminate\Contracts\Validation\Factory  $factory
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function createDefaultValidator(ValidationFactory $factory)
+    public function validator()
     {
-        return $factory->make(
-            $this->validationData(), $this->container->call([$this, 'rules']),
-            $this->messages(), $this->attributes()
-        )->stopOnFirstFailure($this->stopOnFirstFailure);
+        return $this->validate(
+            $this->container->call([$this, 'rules']),
+            $this->messages(),
+            $this->attributes(),
+        );
     }
-
-    /**
-     * Get data to be validated from the request.
-     *
-     * @return array
-     */
-    public function validationData()
-    {
-        return $this->all();
-    }
-
+    
     /**
      * Handle a failed validation attempt.
      *
@@ -191,29 +131,6 @@ class FormRequest extends Request implements ValidatesWhenResolved
     }
 
     /**
-     * Get a validated input container for the validated input.
-     *
-     * @param  array|null  $keys
-     * @return \Illuminate\Support\ValidatedInput|array
-     */
-    public function safe(array $keys = null)
-    {
-        return is_array($keys)
-                    ? $this->validator->safe()->only($keys)
-                    : $this->validator->safe();
-    }
-
-    /**
-     * Get the validated data from the request.
-     *
-     * @return array
-     */
-    public function validated()
-    {
-        return $this->validator->validated();
-    }
-
-    /**
      * Get custom messages for validator errors.
      *
      * @return array
@@ -231,19 +148,6 @@ class FormRequest extends Request implements ValidatesWhenResolved
     public function attributes()
     {
         return [];
-    }
-
-    /**
-     * Set the Validator instance.
-     *
-     * @param  \Illuminate\Contracts\Validation\Validator  $validator
-     * @return $this
-     */
-    public function setValidator(Validator $validator)
-    {
-        $this->validator = $validator;
-
-        return $this;
     }
 
     /**
