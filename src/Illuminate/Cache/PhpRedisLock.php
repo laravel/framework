808e0d3b5cb72409c9d2c419ab1271b69b674020
Taylor Otwell <taylor@laravel.com>
2022-01-15 09:24:59 -0600

    remove protected methods that are not used anymore

--- src/Illuminate/Cache/PhpRedisLock.php
+++ src/Illuminate/Cache/PhpRedisLock.php
@@ -7,13 +7,6 @@ use Illuminate\Redis\Connections\PhpRedisConnection;
 class PhpRedisLock extends RedisLock
 {
     /**
-     * The phpredis factory implementation.
-     *
-     * @var \Illuminate\Redis\Connections\PhpredisConnection
-     */
-    protected $redis;
-
-    /**
      * Create a new phpredis lock instance.
      *
      * @param  \Illuminate\Redis\Connections\PhpRedisConnection  $redis
@@ -38,72 +31,5 @@ class PhpRedisLock extends RedisLock
             $this->name,
             ...$this->redis->pack([$this->owner])
         );
-    }
-
-    /**
-     * Get the owner key, serialized and compressed.
-     *
-     * @return string
-     *
-     * @throws \UnexpectedValueException
-     *
-     * @deprecated Will be removed in a later laravel version. Use PhpRedisConnection::pack.
-     * @see \Illuminate\Redis\Connections\PhpRedisConnection::pack
-     */
-    protected function serializedAndCompressedOwner(): string
-    {
-        return $this->redis->pack([$this->owner])[0];
-    }
-
-    /**
-     * Determine if compression is enabled.
-     *
-     * @return bool
-     *
-     * @deprecated Will be removed in a later laravel version. Use PhpRedisConnection::compressed.
-     * @see \Illuminate\Redis\Connections\PhpRedisConnection::compressed
-     */
-    protected function compressed(): bool
-    {
-        return $this->redis->compressed();
-    }
-
-    /**
-     * Determine if LZF compression is enabled.
-     *
-     * @return bool
-     *
-     * @deprecated Will be removed in a later laravel version. Use PhpRedisConnection::lzfCompressed.
-     * @see \Illuminate\Redis\Connections\PhpRedisConnection::lzfCompressed
-     */
-    protected function lzfCompressed(): bool
-    {
-        return $this->redis->lzfCompressed();
-    }
-
-    /**
-     * Determine if ZSTD compression is enabled.
-     *
-     * @return bool
-     *
-     * @deprecated Will be removed in a later laravel version. Use PhpRedisConnection::zstdCompressed.
-     * @see \Illuminate\Redis\Connections\PhpRedisConnection::zstdCompressed
-     */
-    protected function zstdCompressed(): bool
-    {
-        return $this->redis->zstdCompressed();
-    }
-
-    /**
-     * Determine if LZ4 compression is enabled.
-     *
-     * @return bool
-     *
-     * @deprecated Will be removed in a later laravel version. Use PhpRedisConnection::lz4Compressed.
-     * @see \Illuminate\Redis\Connections\PhpRedisConnection::lz4Compressed
-     */
-    protected function lz4Compressed(): bool
-    {
-        return $this->redis->lz4Compressed();
     }
 }
