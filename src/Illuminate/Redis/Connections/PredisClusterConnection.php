e07933f346f8195790729fc4d25cae2b1221f92b
Bernard Wiesner <ts-bernard.wiesner@rakuten.com>
2022-01-17 16:12:47 +0900

    move flushdb to PredisClusterConnection

--- src/Illuminate/Redis/Connections/PredisClusterConnection.php
+++ src/Illuminate/Redis/Connections/PredisClusterConnection.php
@@ -2,7 +2,19 @@
 
 namespace Illuminate\Redis\Connections;
 
+use Predis\Command\ServerFlushDatabase;
+
 class PredisClusterConnection extends PredisConnection
 {
-    //
+    /**
+     * Flush the selected Redis database.
+     *
+     * @return void
+     */
+    public function flushdb()
+    {
+        foreach ($this->getConnection()->getIterator() as $node) {
+            $node->executeCommand(new ServerFlushDatabase);
+        }
+    }
 }
