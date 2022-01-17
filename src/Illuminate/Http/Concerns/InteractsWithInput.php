0776ffdf2f01290a97f2e80b39c6e24b5067e23b
Alireza Saharkhiz <alireza.saharkhiz@outlook.com>
2022-01-17 18:49:12 +0330

    Fix 'strstr' function usage based on its signature (#40457)

--- src/Illuminate/Http/Concerns/InteractsWithInput.php
+++ src/Illuminate/Http/Concerns/InteractsWithInput.php
@@ -60,7 +60,7 @@ trait InteractsWithInput
         if ($position !== false) {
             $header = substr($header, $position + 7);
 
-            return strpos($header, ',') !== false ? strstr(',', $header, true) : $header;
+            return strpos($header, ',') !== false ? strstr($header, ',', true) : $header;
         }
     }
 
