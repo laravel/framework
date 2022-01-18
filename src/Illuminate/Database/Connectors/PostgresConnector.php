bda1d8f98bd6d35fbe5b307574ced490939d5bc7
Bram Ceulemans <bramceulemans@me.com>
2022-01-19 00:55:55 +0100

    Allow whitespace in dbname for PostgreSQL

--- src/Illuminate/Database/Connectors/PostgresConnector.php
+++ src/Illuminate/Database/Connectors/PostgresConnector.php
@@ -146,7 +146,7 @@ class PostgresConnector extends Connector implements ConnectorInterface
 
         $host = isset($host) ? "host={$host};" : '';
 
-        $dsn = "pgsql:{$host}dbname={$database}";
+        $dsn = "pgsql:{$host}dbname='{$database}'";
 
         // If a port was specified, we will add it to this Postgres DSN connections
         // format. Once we have done that we are ready to return this connection
