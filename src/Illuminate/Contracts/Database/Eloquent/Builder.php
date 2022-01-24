5cd1f60e8d990513ad8d7397d39c29b5d73cfc8f
Taylor Otwell <taylor@laravel.com>
2022-01-24 11:26:42 -0600

    Update Builder.php

--- src/Illuminate/Contracts/Database/Eloquent/Builder.php
+++ src/Illuminate/Contracts/Database/Eloquent/Builder.php
@@ -5,8 +5,7 @@ namespace Illuminate\Contracts\Database\Eloquent;
 use Illuminate\Contracts\Database\Query\Builder as BaseContract;
 
 /**
- * This interface is intentionally empty and exists mostly to improve IDE support
- * and provide a simple type-hinting option for those who prefer it.
+ * This interface is intentionally empty and exists to improve IDE support.
  *
  * @mixin \Illuminate\Database\Eloquent\Builder
  */
