5d02e1b44ac9a92776f20d228bf495ec377e7ee3
Bram <bram.agjm1@gmail.com>
2022-01-25 17:29:05 +0100

    Update Collection.php (#40621)

--- src/Illuminate/Collections/Collection.php
+++ src/Illuminate/Collections/Collection.php
@@ -363,7 +363,7 @@ class Collection implements ArrayAccess, CanBeEscapedWhenCastToString, Enumerabl
     /**
      * Run a filter over each of the items.
      *
-     * @param (callable(TValue): bool)|null  $callback
+     * @param (callable(TValue, TKey): bool)|null  $callback
      * @return static
      */
     public function filter(callable $callback = null)
