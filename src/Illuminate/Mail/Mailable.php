4e98d5f338961c38f5eb9d8a6f3b5670e9dc8a5b
Martijn Weghorst <m.weghorst@gmail.com>
2022-01-25 21:18:01 +0100

    Make `Illuminate\Mail\Mailable` macroable
    This PR makes the \Illuminate\Mail\Mailable class macroable. A simple use-case is abstracting the options array when having multiple Mailables that you want to attach raw pdf data to:
    
    ```php
    // Current situation:
    
    Mail::send((new MyMail())->attachData($pdfData, 'filename.pdf'[
        'mime' => 'application/pdf',
    ]));
    
    // Proposal:
    
    Mailable::macro('attachPdfData', function ($data, $name) {
        return $this->attachData($data, $name, [
            'mime' => 'application/pdf',
        ]);
    });
    
    Mail::send((new MyMail())->attachPdfData($pdfData, 'filename.pdf'));
    ```

--- src/Illuminate/Mail/Mailable.php
+++ src/Illuminate/Mail/Mailable.php
@@ -15,13 +15,16 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\ForwardsCalls;
 use Illuminate\Support\Traits\Localizable;
+use Illuminate\Support\Traits\Macroable;
 use PHPUnit\Framework\Assert as PHPUnit;
 use ReflectionClass;
 use ReflectionProperty;
 
 class Mailable implements MailableContract, Renderable
 {
-    use Conditionable, ForwardsCalls, Localizable;
+    use Conditionable, ForwardsCalls, Localizable, Macroable {
+        __call as macroCall;
+    }
 
     /**
      * The locale of the message.
@@ -1022,6 +1025,10 @@ class Mailable implements MailableContract, Renderable
      */
     public function __call($method, $parameters)
     {
+        if (static::hasMacro($method)) {
+            return $this->macroCall($method, $parameters);
+        }
+        
         if (Str::startsWith($method, 'with')) {
             return $this->with(Str::camel(substr($method, 4)), $parameters[0]);
         }
