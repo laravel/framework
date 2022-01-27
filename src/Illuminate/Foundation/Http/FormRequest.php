69c41598ce9b0476c10d99e5c39d17528d27b2f6
Thomas Omweri <thomasomweri@Thomass-MacBook-Air.local>
2022-01-27 09:02:38 +0300

    Prevents application Requests that extend FormRequest from repetitively including authorize method unless overriding with some logic

--- src/Illuminate/Foundation/Http/FormRequest.php
+++ src/Illuminate/Foundation/Http/FormRequest.php
@@ -271,4 +271,14 @@ class FormRequest extends Request implements ValidatesWhenResolved
 
         return $this;
     }
+
+    /**
+     *  Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
 }
