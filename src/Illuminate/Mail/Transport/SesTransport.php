8b44e299fab4bf6e0c2803ee7d189fff6ea80de4
Dries Vints <dries@vints.io>
2022-01-28 16:48:59 +0100

    Re-implement original SesTransport

---
+++ src/Illuminate/Mail/Transport/SesTransport.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Illuminate\Mail\Transport;
+
+use Aws\Exception\AwsException;
+use Aws\Ses\SesClient;
+use Exception;
+use Symfony\Component\Mailer\SentMessage;
+use Symfony\Component\Mailer\Transport\AbstractTransport;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\Email;
+
+class SesTransport extends AbstractTransport implements TransportInterface
+{
+    /**
+     * The Amazon SES instance.
+     *
+     * @var \Aws\Ses\SesClient
+     */
+    protected $ses;
+
+    /**
+     * The Amazon SES transmission options.
+     *
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * Create a new SES transport instance.
+     *
+     * @param  \Aws\Ses\SesClient  $ses
+     * @param  array  $options
+     * @return void
+     */
+    public function __construct(SesClient $ses, $options = [])
+    {
+        $this->ses = $ses;
+        $this->options = $options;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function doSend(SentMessage $message): void
+    {
+        try {
+            $this->ses->sendRawEmail(
+                array_merge(
+                    $this->options, [
+                        'Source' => $message->getEnvelope()->getSender()->toString(),
+                        'RawMessage' => [
+                            'Data' => $message->toString(),
+                        ],
+                    ]
+                )
+            );
+        } catch (AwsException $e) {
+            throw new Exception('Request to AWS SES API failed.', $e->getCode(), $e);
+        }
+    }
+
+    /**
+     * Get the string representation of the transport.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        $configuration = $this->ses->getConfig();
+
+        if (! $configuration->isDefault('endpoint')) {
+            $endpoint = parse_url($configuration->get('endpoint'));
+            $host = $endpoint['host'].($endpoint['port'] ?? null ? ':'.$endpoint['port'] : '');
+        } else {
+            $host = $configuration->get('region');
+        }
+
+        return sprintf('ses+api://%s@%s', $configuration->get('accessKeyId'), $host);
+    }
+
+    /**
+     * Get the Amazon SES client for the SesTransport instance.
+     *
+     * @return \Aws\Ses\SesClient
+     */
+    public function ses()
+    {
+        return $this->ses;
+    }
+
+    /**
+     * Get the transmission options being used by the transport.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Set the transmission options being used by the transport.
+     *
+     * @param  array  $options
+     * @return array
+     */
+    public function setOptions(array $options)
+    {
+        return $this->options = $options;
+    }
+}
