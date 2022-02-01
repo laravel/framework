9692bc8bdb376d2087856fe87a37112654a359da
Caleb Porzio <calebporzio@gmail.com>
2022-02-01 17:15:38 -0500

    Add --view flag to make:component command (#40741)

--- src/Illuminate/Foundation/Console/ComponentMakeCommand.php
+++ src/Illuminate/Foundation/Console/ComponentMakeCommand.php
@@ -46,6 +46,14 @@ class ComponentMakeCommand extends GeneratorCommand
      */
     public function handle()
     {
+        if ($this->option('view')) {
+            $this->writeView(function () {
+                $this->info($this->type.' created successfully.');
+            });
+
+            return;
+        }
+        
         if (parent::handle() === false && ! $this->option('force')) {
             return false;
         }
@@ -58,9 +66,10 @@ class ComponentMakeCommand extends GeneratorCommand
     /**
      * Write the view for the component.
      *
+     * @param  callable|null $onSuccess
      * @return void
      */
-    protected function writeView()
+    protected function writeView($onSuccess = null)
     {
         $path = $this->viewPath(
             str_replace('.', '/', 'components.'.$this->getView()).'.blade.php'
@@ -82,6 +91,10 @@ class ComponentMakeCommand extends GeneratorCommand
     <!-- '.Inspiring::quote().' -->
 </div>'
         );
+
+        if ($onSuccess) {
+            $onSuccess();
+        }
     }
 
     /**
@@ -167,6 +180,7 @@ class ComponentMakeCommand extends GeneratorCommand
         return [
             ['force', null, InputOption::VALUE_NONE, 'Create the class even if the component already exists'],
             ['inline', null, InputOption::VALUE_NONE, 'Create a component that renders an inline view'],
+            ['view', null, InputOption::VALUE_NONE, 'Create an anonymous component with only a view'],
         ];
     }
 }
