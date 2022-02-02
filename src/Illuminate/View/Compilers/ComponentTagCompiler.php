b8595b200b35ee1a0da53a5a4f9401fbc1785a61
Caleb Porzio <calebporzio@gmail.com>
2022-02-02 09:53:01 -0500

    [9.x] Add inline slot name syntax (<x-slot:name>) (#40747)
    * Add slot inline naming syntax
    
    * Fix tests
    
    * Fix tests

--- src/Illuminate/View/Compilers/ComponentTagCompiler.php
+++ src/Illuminate/View/Compilers/ComponentTagCompiler.php
@@ -406,8 +406,8 @@ class ComponentTagCompiler
             <
                 \s*
                 x[\-\:]slot
-                \s+
-                (:?)name=(?<name>(\"[^\"]+\"|\\\'[^\\\']+\\\'|[^\s>]+))
+                (?:\:(?<inlineName>\w+))?
+                (?:\s+(:?)name=(?<name>(\"[^\"]+\"|\\\'[^\\\']+\\\'|[^\s>]+)))?
                 (?<attributes>
                     (?:
                         \s+
@@ -438,9 +438,9 @@ class ComponentTagCompiler
         /x";
 
         $value = preg_replace_callback($pattern, function ($matches) {
-            $name = $this->stripQuotes($matches['name']);
+            $name = $this->stripQuotes($matches['inlineName'] ?: $matches['name']);
 
-            if ($matches[1] !== ':') {
+            if ($matches[2] !== ':') {
                 $name = "'{$name}'";
             }
 
