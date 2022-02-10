af3d5cb9e431f472e6fce0fe50ffe489de06c0d3
Taylor Otwell <taylor@laravel.com>
2022-02-10 08:38:50 -0600

    Update Handler.php

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -693,7 +693,7 @@ class Handler implements ExceptionHandlerContract
      * @param  \Throwable  $e
      * @return void
      *
-     * @internal This method is not meant to be used or overwritten outside the framework itself.
+     * @internal This method is not meant to be used or overwritten outside the framework.
      */
     public function renderForConsole($output, Throwable $e)
     {
