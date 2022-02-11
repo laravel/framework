21628fc435536caa7e4a25a4a247dbf42ee2257d
Maarten Buis <buismaarten@outlook.com>
2022-02-12 00:28:47 +0100

    Removed duplicate semicolon

--- src/Illuminate/Foundation/Exceptions/views/layout.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/layout.blade.php
@@ -11,7 +11,7 @@
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";;
+                font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
