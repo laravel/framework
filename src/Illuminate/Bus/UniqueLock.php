ad23fbd32d74e9cc791312090ef5b33253605588
Andrey Helldar <helldar@ai-rus.com>
2022-02-12 13:12:51 +0300

    Added the ability to use the uniqueFor method for Jobs

--- src/Illuminate/Bus/UniqueLock.php
+++ src/Illuminate/Bus/UniqueLock.php
@@ -36,13 +36,17 @@ class UniqueLock
                     ? $job->uniqueId()
                     : ($job->uniqueId ?? '');
 
+        $uniqueFor = method_exists($job, 'uniqueFor')
+                    ? $job->uniqueFor()
+                    : ($job->uniqueFor ?? 0);
+
         $cache = method_exists($job, 'uniqueVia')
                     ? $job->uniqueVia()
                     : $this->cache;
 
         return (bool) $cache->lock(
             $key = 'laravel_unique_job:'.get_class($job).$uniqueId,
-            $job->uniqueFor ?? 0
+            $uniqueFor
         )->get();
     }
 }
