ba4b11db3c7e1c4ed90f51048eae779952055c68
Luke Towers <github@luketowers.ca>
2022-02-15 08:48:41 -0600

    [9.x] Fix the error message when no routes are detected (#41017)
    * Fix the error message when no routes are detected
    
    This fixes the error message that's displayed when Laravel can't find any routes to list. Router->getRoutes() returns a RouteCollectionInterface, not an array.
    
    * Update src/Illuminate/Foundation/Console/RouteListCommand.php

--- src/Illuminate/Foundation/Console/RouteListCommand.php
+++ src/Illuminate/Foundation/Console/RouteListCommand.php
@@ -98,7 +98,7 @@ class RouteListCommand extends Command
     {
         $this->router->flushMiddlewareGroups();
 
-        if (empty($this->router->getRoutes())) {
+        if (! $this->router->getRoutes()->count()) {
             return $this->error("Your application doesn't have any routes.");
         }
 
