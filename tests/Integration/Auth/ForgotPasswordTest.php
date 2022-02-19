35e8ab6cfd843cd0b5e7daad43992706046508d6
Samuel Zeitler <zeitler@quintet.io>
2022-02-19 09:01:57 +0100

    added new url param to tests/Integration/Auth/ForgotPasswordTest.php

--- tests/Integration/Auth/ForgotPasswordTest.php
+++ tests/Integration/Auth/ForgotPasswordTest.php
@@ -90,12 +90,17 @@ class ForgotPasswordTest extends TestCase
     {
         Notification::fake();
 
-        ResetPassword::toMailUsing(function ($notifiable, $token) {
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+            return route('custom.password.reset', $token);
+        });
+
+        ResetPassword::toMailUsing(function ($notifiable, $token, $url) {
             return (new MailMessage)
                 ->subject(__('Reset Password Notification'))
                 ->line(__('You are receiving this email because we received a password reset request for your account.'))
-                ->action(__('Reset Password'), route('custom.password.reset', $token))
-                ->line(__('If you did not request a password reset, no further action is required.'));
+                ->action(__('Reset Password'), $url)
+                ->line(__('If you did not request a password reset, no further action is required.'))
+                ->line(__('Your reset token is: ' . $token));
         });
 
         UserFactory::new()->create();
