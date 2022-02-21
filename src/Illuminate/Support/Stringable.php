da08878870f309c99bb01b45dcf12481e08eb4ec
Yoeri Boven <yoeriboven@msn.com>
2022-02-21 22:06:10 +0100

    Added betweenFirst (#41144)

--- src/Illuminate/Support/Stringable.php
+++ src/Illuminate/Support/Stringable.php
@@ -131,6 +131,18 @@ class Stringable implements JsonSerializable
     }
 
     /**
+     * Get the smallest possible portion of a string between two given values.
+     *
+     * @param  string  $from
+     * @param  string  $to
+     * @return static
+     */
+    public function betweenFirst($from, $to)
+    {
+        return new static(Str::betweenFirst($this->value, $from, $to));
+    }
+
+    /**
      * Convert a value to camel case.
      *
      * @return static
