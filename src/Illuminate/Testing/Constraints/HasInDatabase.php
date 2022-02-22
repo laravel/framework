6103e79f8f8fa1c3790c352a50cb2fe03f0e6bea
gguney <gokhanguneygg@gmail.com>
2022-02-22 08:28:11 +0300

    Similar result and data comparison improvements

--- src/Illuminate/Testing/Constraints/HasInDatabase.php
+++ src/Illuminate/Testing/Constraints/HasInDatabase.php
@@ -84,7 +84,17 @@ class HasInDatabase extends Constraint
         )->limit($this->show)->get();
 
         if ($similarResults->isNotEmpty()) {
-            $description = 'Found similar results: '.json_encode($similarResults, JSON_PRETTY_PRINT);
+            $description = 'Found similar results: '.json_encode($similarResults, JSON_PRETTY_PRINT) . "\n";
+
+            foreach ($similarResults as $similarResult) {
+                foreach ($this->data as $fieldName => $value) {
+                    if ($similarResult->$fieldName != $value) {
+                        $description .= "- " . $fieldName . "\n" .
+                            "Given Data:           " . json_encode($value) . "\n" .
+                            "Found Similar Result: " . json_encode($similarResult->$fieldName) . "\n";
+                    }
+                }
+            }
         } else {
             $query = $this->database->table($table);
 
