065deef03ada1eb65d88327909233c5e2b137763
zsoltjanes <zsolt.janes@gmail.com>
2022-02-24 21:26:29 +0100

    Add dd() alias for the dump() function.

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -919,3 +919,17 @@ if (! function_exists('view')) {
         return $factory->make($view, $data, $mergeData);
     }
 }
+
+if (! function_exists('d')) {
+    /**
+     * It is an alias for the dump() function.
+     *
+     * @param  mixed  $var
+     * @param  mixed  ...$moreVars
+     * @return  array|mixed
+     */
+    function d($var, ...$moreVars)
+    {
+        return dump($var, ...$moreVars);
+    }
+}
