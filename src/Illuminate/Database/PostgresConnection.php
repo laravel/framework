7c2edfc8ee1125e7784d5bd1f20b72dd5518e383
Alin Eugen Deac <aedart@gmail.com>
2022-02-24 21:33:38 +0100

    [9.x] Add resource binding (#41233)
    * Add support for binding resources (#41180)
    
    * Remove duplicate code (#41180)
    
    Binding of resource values is now handled in `Connection`.
    
    * formatting
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Database/PostgresConnection.php
+++ src/Illuminate/Database/PostgresConnection.php
@@ -9,36 +9,9 @@ use Illuminate\Database\Schema\Grammars\PostgresGrammar as SchemaGrammar;
 use Illuminate\Database\Schema\PostgresBuilder;
 use Illuminate\Database\Schema\PostgresSchemaState;
 use Illuminate\Filesystem\Filesystem;
-use PDO;
 
 class PostgresConnection extends Connection
 {
-    /**
-     * Bind values to their parameters in the given statement.
-     *
-     * @param  \PDOStatement  $statement
-     * @param  array  $bindings
-     * @return void
-     */
-    public function bindValues($statement, $bindings)
-    {
-        foreach ($bindings as $key => $value) {
-            if (is_int($value)) {
-                $pdoParam = PDO::PARAM_INT;
-            } elseif (is_resource($value)) {
-                $pdoParam = PDO::PARAM_LOB;
-            } else {
-                $pdoParam = PDO::PARAM_STR;
-            }
-
-            $statement->bindValue(
-                is_string($key) ? $key : $key + 1,
-                $value,
-                $pdoParam
-            );
-        }
-    }
-
     /**
      * Get the default query grammar instance.
      *
