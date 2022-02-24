6c21120f9bf546ed0df22ed5e4c79f58fb94e643
danilopolani <danilo.polani@gmail.com>
2022-02-24 15:28:17 +0100

    add assertJsonValidationErrorRules testing helper

--- src/Illuminate/Validation/Validator.php
+++ src/Illuminate/Validation/Validator.php
@@ -1395,6 +1395,60 @@ class Validator implements ValidatorContract
     }
 
     /**
+     * Get the error messages for an attribute and a validation rule.
+     *
+     * @param  string  $attribute
+     * @param  string|\Illuminate\Contracts\Validation\Rule  $rule
+     * @return array
+     */
+    public function getErrorMessage(string $attribute, string|RuleContract $rule): array
+    {
+        [$rule, $parameters] = ValidationRuleParser::parse($rule);
+        $result = [];
+
+        if ($rule === '') {
+            return $result;
+        }
+
+        // First we will get the correct keys for the given attribute in case the field is nested in
+        // an array. Then we determine if the given rule accepts other field names as parameters.
+        // If so, we will replace any asterisks found in the parameters with the correct keys.
+        if ($this->dependsOnOtherFields($rule)) {
+            $parameters = $this->replaceDotInParameters($parameters);
+
+            if ($keys = $this->getExplicitKeys($attribute)) {
+                $parameters = $this->replaceAsterisksInParameters($parameters, $keys);
+            }
+        }
+
+        if ($rule instanceof RuleContract) {
+            $messages = $rule->message();
+
+            $messages = $messages ? (array) $messages : [get_class($rule)];
+
+            foreach ($messages as $message) {
+                $result[] = $this->makeReplacements(
+                    $message, $attribute, get_class($rule), []
+                );
+            }
+
+            return $result;
+        }
+
+        $attribute = str_replace(
+            [$this->dotPlaceholder, '__asterisk__'],
+            ['.', '*'],
+            $attribute
+        );
+
+        return [
+            $this->makeReplacements(
+                $this->getMessage($attribute, $rule), $attribute, $rule, $parameters
+            )
+        ];
+    }
+
+    /**
      * Get the Presence Verifier implementation.
      *
      * @param  string|null  $connection
