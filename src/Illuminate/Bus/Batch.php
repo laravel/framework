7e89927a5dd8e66e114bb121a9216a9455127510
Yoeri Boven <yoeriboven@msn.com>
2022-03-02 16:22:48 +0100

    [9.x] Fix docblock on Batch class (#41295)
    * Update Batch.php
    
    * Update Batch.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Bus/Batch.php
+++ src/Illuminate/Bus/Batch.php
@@ -155,7 +155,7 @@ class Batch implements Arrayable, JsonSerializable
     /**
      * Add additional jobs to the batch.
      *
-     * @param  \Illuminate\Support\Enumerable|array  $jobs
+     * @param  \Illuminate\Support\Enumerable|object|array  $jobs
      * @return self
      */
     public function add($jobs)
