6a7a5b4cc3be737441b5118ea64ea550a6bc9f4b
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2022-03-03 15:11:25 +0100

    [9.x] Fix MySqlSchemaState does not add --ssl-ca to mysql cli  (#41315)
    * fix: MySqlSchemaState does not add --ssl-ca to mysql cli it not configured
    
    * Update MySqlSchemaState.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Database/Schema/MySqlSchemaState.php
+++ src/Illuminate/Database/Schema/MySqlSchemaState.php
@@ -103,9 +103,15 @@ class MySqlSchemaState extends SchemaState
     {
         $value = ' --user="${:LARAVEL_LOAD_USER}" --password="${:LARAVEL_LOAD_PASSWORD}"';
 
-        $value .= $this->connection->getConfig()['unix_socket'] ?? false
+        $config = $this->connection->getConfig();
+
+        $value .= $config['unix_socket'] ?? false
                         ? ' --socket="${:LARAVEL_LOAD_SOCKET}"'
-                        : ' --host="${:LARAVEL_LOAD_HOST}" --port="${:LARAVEL_LOAD_PORT}" --ssl-ca="${:LARAVEL_LOAD_SSL_CA}"';
+                        : ' --host="${:LARAVEL_LOAD_HOST}" --port="${:LARAVEL_LOAD_PORT}"';
+
+        if (isset($config['options'][\PDO::MYSQL_ATTR_SSL_CA])) {
+            $value .= ' --ssl-ca="${:LARAVEL_LOAD_SSL_CA}"';
+        }
 
         return $value;
     }
