1dd01709ca432d774713fad84f237b90f1f25a14
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2022-03-12 01:09:00 +0300

    Update RouteListCommand.php

--- src/Illuminate/Foundation/Console/RouteListCommand.php
+++ src/Illuminate/Foundation/Console/RouteListCommand.php
@@ -452,7 +452,7 @@ class RouteListCommand extends Command
             ['path', null, InputOption::VALUE_OPTIONAL, 'Only show routes matching the given path pattern'],
             ['except-path', null, InputOption::VALUE_OPTIONAL, 'Do not display the routes matching the given path pattern'],
             ['reverse', 'r', InputOption::VALUE_NONE, 'Reverse the ordering of the routes'],
-            ['sort', null, InputOption::VALUE_OPTIONAL, 'The column (precedence, domain, method, uri, name, action, middleware) to sort by', 'uri'],
+            ['sort', null, InputOption::VALUE_OPTIONAL, 'The column (domain, method, uri, name, action, middleware) to sort by', 'uri'],
             ['except-vendor', null, InputOption::VALUE_NONE, 'Do not display routes defined by vendor packages'],
         ];
     }
