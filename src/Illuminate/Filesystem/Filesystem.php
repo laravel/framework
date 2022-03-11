aa15cbd2984fdd5c3b7e31b43db0d71e6282c3ac
mouyong <mouyong@users.noreply.github.com>
2022-03-11 19:16:22 +0800

    Update src/Illuminate/Filesystem/Filesystem.php
    Co-authored-by: Gökhan Güney <gokhanguneygg@gmail.com>

--- src/Illuminate/Filesystem/Filesystem.php
+++ src/Illuminate/Filesystem/Filesystem.php
@@ -702,6 +702,7 @@ class Filesystem
 
         // need unset $items before remove am empty dir.
         unset($items);
+        
         if (! $preserve) {
             @rmdir($directory);
         }
