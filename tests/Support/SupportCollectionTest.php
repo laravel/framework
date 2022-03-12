3d08ef7c80e14adf4e9df5189017574bac56cd58
DarkGhosthunter <DarkGhostHunter@gmail.com>
2022-03-11 22:35:57 -0300

    Adds key first/last.

--- tests/Support/SupportCollectionTest.php
+++ tests/Support/SupportCollectionTest.php
@@ -69,6 +69,17 @@ class SupportCollectionTest extends TestCase
         $this->assertSame('default', $result);
     }
 
+    public function testFirstKey()
+    {
+        $data = new Collection(['foo' => 'bar', 'baz' => 'quz', 'qux' => 'quux']);
+
+        $this->assertSame('foo', $data->firstKey());
+
+        $data = new Collection(['foo', 'bar', 'baz', 'quz']);
+
+        $this->assertSame(0, $data->firstKey());
+    }
+
     /**
      * @dataProvider collectionClassProvider
      */
@@ -328,6 +339,17 @@ class SupportCollectionTest extends TestCase
         $data = new $collection;
         $result = $data->last(null, 'default');
         $this->assertSame('default', $result);
+    }
+
+    public function testLastKey()
+    {
+        $data = new Collection(['foo' => 'bar', 'baz' => 'quz', 'qux' => 'quux']);
+
+        $this->assertSame('qux', $data->lastKey());
+
+        $data = new Collection(['foo', 'bar', 'baz', 'quz']);
+
+        $this->assertSame(3, $data->lastKey());
     }
 
     public function testPopReturnsAndRemovesLastItemInCollection()
