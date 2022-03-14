45d3e0a0c175ab85072e4d24f086386c38c6e342
Italo <DarkGhostHunter@Gmail.com>
2022-03-14 15:34:58 -0300

    [9.x] Adds "freezeTime" helper for tests. (#41460)
    * [9.x] Adds "freezeTime" helper for tests.
    
    * Removes start of second.
    Returns callback result, if any.
    
    * Adds freezeCurrentSecond for DB timestamps without subsecond precision.
    
    * formatting
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Foundation/Testing/Concerns/InteractsWithTime.php
+++ src/Illuminate/Foundation/Testing/Concerns/InteractsWithTime.php
@@ -8,6 +8,28 @@ use Illuminate\Support\Carbon;
 trait InteractsWithTime
 {
     /**
+     * Freeze time.
+     *
+     * @param  callable|null  $callback
+     * @return mixed
+     */
+    public function freezeTime($callback = null)
+    {
+        return $this->travelTo(Carbon::now(), $callback);
+    }
+
+    /**
+     * Freeze time at the beginning of the current second.
+     *
+     * @param  callable|null  $callback
+     * @return mixed
+     */
+    public function freezeSecond($callback = null)
+    {
+        return $this->travelTo(Carbon::now()->startOfSecond(), $callback);
+    }
+
+    /**
      * Begin travelling to another time.
      *
      * @param  int  $value
@@ -30,7 +52,7 @@ trait InteractsWithTime
         Carbon::setTestNow($date);
 
         if ($callback) {
-            return tap($callback(), function () {
+            return tap($callback(Carbon::now()), function () {
                 Carbon::setTestNow();
             });
         }
