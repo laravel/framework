ba674c6162ccb4f24c2e7a46ea51bce5b379e130
Jamie <ziadoz@gmail.com>
2022-03-17 00:05:30 +0000

    Match schedule command name exactly.

--- src/Illuminate/Console/Scheduling/ScheduleTestCommand.php
+++ src/Illuminate/Console/Scheduling/ScheduleTestCommand.php
@@ -2,6 +2,7 @@
 
 namespace Illuminate\Console\Scheduling;
 
+use Illuminate\Console\Application;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -51,7 +52,11 @@ class ScheduleTestCommand extends Command
         }
 
         if (! empty($name = $this->option('name'))) {
-            $matches = array_filter($commandNames, fn ($commandName) => Str::endsWith($commandName, $name));
+            $commandBinary = Application::phpBinary().' '.Application::artisanBinary();
+
+            $matches = array_filter($commandNames, function ($commandName) use ($commandBinary, $name) {
+                 return trim(Str::replace($commandBinary, '', $commandName)) === $name;
+            });
 
             if (count($matches) !== 1) {
                 return $this->error('No matching scheduled command found.');
