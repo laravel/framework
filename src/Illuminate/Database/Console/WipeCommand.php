a9cf8423b94d4ebffe8c2d75195f5b7b7a45350c
Dries Vints <dries@vints.io>
2022-03-18 11:29:44 +0100

    wip

--- src/Illuminate/Database/Console/WipeCommand.php
+++ src/Illuminate/Database/Console/WipeCommand.php
@@ -4,8 +4,10 @@ namespace Illuminate\Database\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Console\ConfirmableTrait;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputOption;
 
+#[AsCommand(name: 'db:wipe')]
 class WipeCommand extends Command
 {
     use ConfirmableTrait;
@@ -16,6 +18,17 @@ class WipeCommand extends Command
      * @var string
      */
     protected $name = 'db:wipe';
+
+    /**
+     * The name of the console command.
+     *
+     * This name is used to identify the command during lazy loading.
+     *
+     * @var string|null
+     *
+     * @deprecated
+     */
+    protected static $defaultName = 'db:wipe';
 
     /**
      * The console command description.
