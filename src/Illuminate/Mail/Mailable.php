bffc647a06d5d1d1b49fd65749df91fc99144c19
MaxGiting <MaxGiting@users.noreply.github.com>
2022-03-19 20:46:09 +0000

    [9.x] Add hasSubject method to Mailables (#41575)
    * Add hasSubject method
    
    * Add tests for hasSubject
    
    * Update Mailable.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Mail/Mailable.php
+++ src/Illuminate/Mail/Mailable.php
@@ -778,6 +778,17 @@ class Mailable implements MailableContract, Renderable
     }
 
     /**
+     * Determine if the mailable has the given subject.
+     *
+     * @param  string  $subject
+     * @return bool
+     */
+    public function hasSubject($subject)
+    {
+        return $this->subject === $subject;
+    }
+
+    /**
      * Set the Markdown template for the message.
      *
      * @param  string  $view
