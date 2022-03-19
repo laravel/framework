1291f29a95f19d6d3be3fbb8a044512eac27a37b
MaxGiting <MaxGiting@users.noreply.github.com>
2022-03-19 19:18:24 +0000

    Add hasSubject method

--- src/Illuminate/Mail/Mailable.php
+++ src/Illuminate/Mail/Mailable.php
@@ -778,6 +778,17 @@ class Mailable implements MailableContract, Renderable
     }
 
     /**
+     * Determine if the given subject is set on the mailable.
+     *
+     * @param  string  $subject
+     * @return bool
+     */
+    public function hasSubject($subject)
+    {
+        return $this->subject == $subject;
+    }
+
+    /**
      * Set the Markdown template for the message.
      *
      * @param  string  $view
