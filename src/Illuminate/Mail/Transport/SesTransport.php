1ad4a3713dd77a27dbe26fb60a01964cdaf8f714
Jon Baker <jon@servicefusion.com>
2022-03-22 08:14:10 -0500

    Style fix

--- src/Illuminate/Mail/Transport/SesTransport.php
+++ src/Illuminate/Mail/Transport/SesTransport.php
@@ -75,7 +75,7 @@ class SesTransport extends AbstractTransport
         } catch (AwsException $e) {
             throw new Exception('Request to AWS SES API failed.', $e->getCode(), $e);
         }
-        
+
         $messageId = $result->get('MessageId');
         $message->getOriginalMessage()->getHeaders()->addHeader('X-Message-ID', $messageId);
         $message->getOriginalMessage()->getHeaders()->addHeader('X-SES-Message-ID', $messageId);
