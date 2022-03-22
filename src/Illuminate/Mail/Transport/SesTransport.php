0564fb5482173ec08e064241a13c642475fb4310
J David Baker <me@jdavidbaker.com>
2022-03-22 11:34:18 -0500

    [9.x] Store SES message ID in X-SES-Message-ID header (#41615)
    * Store SES message ID in X-SES-Message-ID header
    
    * Using alias for Mockery
    
    * Accidentally removed a line in the test
    
    * Moving SES message ID retrieval outside of the try/catch block
    
    * Style fix
    
    * Last commit was an error
    
    * Update SesTransport.php
    
    Co-authored-by: Jon Baker <jon@servicefusion.com>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Mail/Transport/SesTransport.php
+++ src/Illuminate/Mail/Transport/SesTransport.php
@@ -57,7 +57,7 @@ class SesTransport extends AbstractTransport
         }
 
         try {
-            $this->ses->sendRawEmail(
+            $result = $this->ses->sendRawEmail(
                 array_merge(
                     $options, [
                         'Source' => $message->getEnvelope()->getSender()->toString(),
@@ -75,6 +75,11 @@ class SesTransport extends AbstractTransport
         } catch (AwsException $e) {
             throw new Exception('Request to AWS SES API failed.', $e->getCode(), $e);
         }
+
+        $messageId = $result->get('MessageId');
+
+        $message->getOriginalMessage()->getHeaders()->addHeader('X-Message-ID', $messageId);
+        $message->getOriginalMessage()->getHeaders()->addHeader('X-SES-Message-ID', $messageId);
     }
 
     /**
