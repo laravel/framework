6af731b43bc87c24bd273ca06f7b3b1279c6afe9
masoud <masouddarvishi56@yahoo.com>
2022-03-24 02:18:20 +0430

    add database test for unique validation

--- tests/Validation/ValidationUniqueRuleTest.php
+++ tests/Validation/ValidationUniqueRuleTest.php
@@ -2,12 +2,38 @@
 
 namespace Illuminate\Tests\Validation;
 
+use Illuminate\Database\Capsule\Manager as DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
+use Illuminate\Validation\DatabasePresenceVerifier;
 use Illuminate\Validation\Rules\Unique;
+use Illuminate\Validation\Validator;
 use PHPUnit\Framework\TestCase;
 
 class ValidationUniqueRuleTest extends TestCase
 {
+    /**
+     * Setup the database schema.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $db = new DB;
+
+        $db->addConnection([
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+        ]);
+
+        $db->bootEloquent();
+        $db->setAsGlobal();
+
+        $this->createSchema();
+    }
+
     public function testItCorrectlyFormatsAStringVersionOfTheRule()
     {
         $rule = new Unique('table');
@@ -92,6 +118,110 @@ class ValidationUniqueRuleTest extends TestCase
         $rule->withoutTrashed('softdeleted_at');
         $this->assertSame('unique:table,NULL,NULL,id,softdeleted_at,"NULL"', (string) $rule);
     }
+
+    public function testItCorrectlyValidateDataInDatabase()
+    {
+        $trans = $this->getIlluminateArrayTranslator();
+        $v = new Validator($trans, [], []);
+        $v->setPresenceVerifier(new DatabasePresenceVerifier(Model::getConnectionResolver()));
+
+        ValidationUniqueRuleTestUser::query()->insert(
+            [
+                ['id' => '1', 'username' => 'batman', 'deleted_at' => null],
+                ['id' => '2', 'username' => 'masoud', 'deleted_at' => '2021-10-12 00:00:00'],
+            ]
+        );
+
+        $rule = new Unique(ValidationUniqueRuleTestUser::class, 'username');
+        $v->setRules(['username' => $rule]);
+        $v->setData(['username' => 'batman']);
+        $this->assertFalse($v->passes());
+
+        $rule = new Unique(ValidationUniqueRuleTestUser::class, 'username');
+        $rule->ignore(1);
+        $v->setRules(['username' => $rule]);
+        $v->setData(['username' => 'batman']);
+        $this->assertTrue($v->passes());
+
+        $rule = new Unique(ValidationUniqueRuleTestUser::class, 'username');
+        $rule->ignore(ValidationUniqueRuleTestUser::find(1));
+        $v->setRules(['username' => $rule]);
+        $v->setData(['username' => 'batman']);
+        $this->assertTrue($v->passes());
+
+        $rule = new Unique(ValidationUniqueRuleTestUser::class, 'username');
+        $v->setRules(['username' => $rule]);
+        $v->setData(['username' => 'masoud']);
+        $this->assertFalse($v->passes());
+
+        $rule = new Unique(ValidationUniqueRuleTestUser::class, 'username');
+        $rule->withoutTrashed();
+        $v->setRules(['username' => $rule]);
+        $v->setData(['username' => 'masoud']);
+        $this->assertTrue($v->passes());
+    }
+
+    protected function createSchema()
+    {
+        $this->schema('default')->create('users', function ($table) {
+            $table->unsignedInteger('id');
+            $table->string('username');
+            $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Get a schema builder instance.
+     *
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    protected function schema($connection = 'default')
+    {
+        return $this->connection($connection)->getSchemaBuilder();
+    }
+
+    /**
+     * Get a database connection instance.
+     *
+     * @return \Illuminate\Database\Connection
+     */
+    protected function connection($connection = 'default')
+    {
+        return $this->getConnectionResolver()->connection($connection);
+    }
+
+    /**
+     * Get connection resolver.
+     *
+     * @return \Illuminate\Database\ConnectionResolverInterface
+     */
+    protected function getConnectionResolver()
+    {
+        return Model::getConnectionResolver();
+    }
+
+    /**
+     * Tear down the database schema.
+     *
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        $this->schema('default')->drop('users');
+    }
+
+    public function getIlluminateArrayTranslator()
+    {
+        return new Translator(
+            new ArrayLoader, 'en'
+        );
+    }
+}
+class ValidationUniqueRuleTestUser extends Model
+{
+    use SoftDeletes;
+    protected $table = 'users';
+    protected $guarded = [];
 }
 
 class EloquentModelStub extends Model
