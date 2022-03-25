80243c01685d2089430c4b9dec0bf5524c8eedbd
Barry Deen <barry.deen@gmail.com>
2022-03-24 21:17:23 -0400

    Result text brighter color on dark mode.

--- src/Illuminate/Pagination/resources/views/tailwind.blade.php
+++ src/Illuminate/Pagination/resources/views/tailwind.blade.php
@@ -24,7 +24,7 @@
 
         <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
             <div>
-                <p class="text-sm text-gray-700 leading-5">
+                <p class="text-sm text-gray-700 dark:text-gray-100 leading-5">
                     {!! __('Showing') !!}
                     @if ($paginator->firstItem())
                         <span class="font-medium">{{ $paginator->firstItem() }}</span>
