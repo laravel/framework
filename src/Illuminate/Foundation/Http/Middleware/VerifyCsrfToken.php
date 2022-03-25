2b1165c498a58ee5743260e3e7677a50f56e7b3e
PeterF <peterfliszar@users.noreply.github.com>
2022-03-25 12:17:20 +0100

    fix: VerifyCsrfToken - http_only config
    VerifyCsrfToken - newCookie() is did not used the session config "http_only" value.

--- src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
+++ src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
@@ -190,8 +190,15 @@ class VerifyCsrfToken
 
         $response->headers->setCookie(
             new Cookie(
-                'XSRF-TOKEN', $request->session()->token(), $this->availableAt(60 * $config['lifetime']),
-                $config['path'], $config['domain'], $config['secure'], false, false, $config['same_site'] ?? null
+                'XSRF-TOKEN', 
+                $request->session()->token(), 
+                $this->availableAt(60 * $config['lifetime']),
+                $config['path'], 
+                $config['domain'], 
+                $config['secure'], 
+                $config['http_only'], 
+                false, 
+                $config['same_site'] ?? null
             )
         );
 
