89a53635cb13d0a0af440856278fb40f338640c8
Derek <derekmd@hotmail.com>
2022-03-31 13:23:46 -0400

    Apply schema grammar fixes for JSON path array references
    Illuminate\Database\Query\Grammars\Grammar@wrapJsonFieldAndPath() and
    Illuminate\Database\Schema\Grammars\Grammar@wrapJsonFieldAndPath() were
    copy and pasted duplicates but the query version had JSON path fixes
    applied that the schema version doesn't have.
    
    Instead add a trait to make the two classes share these methods.

--- tests/Database/DatabaseSQLiteSchemaGrammarTest.php
+++ tests/Database/DatabaseSQLiteSchemaGrammarTest.php
@@ -923,6 +923,21 @@ class DatabaseSQLiteSchemaGrammarTest extends TestCase
         $this->assertSame('create table "users" ("my_json_column" varchar not null, "my_other_column" varchar as (json_extract("my_json_column", \'$."some_attribute"."nested"\')))', $statements[0]);
     }
 
+    public function testCreateTableWithVirtualAsColumnWhenJsonColumnHasArrayKey()
+    {
+        $blueprint = new Blueprint('users');
+        $blueprint->create();
+        $blueprint->string('my_json_column')->virtualAsJson('my_json_column->foo[0][1]');
+
+        $conn = $this->getConnection();
+        $conn->shouldReceive('getConfig')->andReturn(null);
+
+        $statements = $blueprint->toSql($conn, $this->getGrammar());
+
+        $this->assertCount(1, $statements);
+        $this->assertSame("create table \"users\" (\"my_json_column\" varchar as (json_extract(\"my_json_column\", '$.\"foo\"[0][1]')))", $statements[0]);
+    }
+
     public function testCreateTableWithStoredAsColumn()
     {
         $blueprint = new Blueprint('users');
