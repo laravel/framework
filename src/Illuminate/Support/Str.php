53c742531fe49f8a7c2cd2083e50164bf8d9c644
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2022-04-11 16:26:37 +0200

    [8.x] Add multibyte support to string padding helper functions (#41899)
    * Add multibyte support to string padding helper functions
    
    * Remove non-private `mbStrPad` method
    
    * Fix code style
    
    * Inline `str_pad` multibyte fix

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -489,7 +489,7 @@ class Str
      */
     public static function padBoth($value, $length, $pad = ' ')
     {
-        return str_pad($value, $length, $pad, STR_PAD_BOTH);
+        return str_pad($value, strlen($value) - mb_strlen($value) + $length, $pad, STR_PAD_BOTH);
     }
 
     /**
@@ -502,7 +502,7 @@ class Str
      */
     public static function padLeft($value, $length, $pad = ' ')
     {
-        return str_pad($value, $length, $pad, STR_PAD_LEFT);
+        return str_pad($value, strlen($value) - mb_strlen($value) + $length, $pad, STR_PAD_LEFT);
     }
 
     /**
@@ -515,7 +515,7 @@ class Str
      */
     public static function padRight($value, $length, $pad = ' ')
     {
-        return str_pad($value, $length, $pad, STR_PAD_RIGHT);
+        return str_pad($value, strlen($value) - mb_strlen($value) + $length, $pad, STR_PAD_RIGHT);
     }
 
     /**
