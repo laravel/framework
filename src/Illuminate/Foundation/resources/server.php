bd3ac842a4af4d86576234b08371ee54e1920772
Dries Vints <dries@vints.io>
2022-04-12 15:38:02 +0200

    Fix empty paths for server.php (#41933)

--- src/Illuminate/Foundation/resources/server.php
+++ src/Illuminate/Foundation/resources/server.php
@@ -3,7 +3,7 @@
 $publicPath = getcwd();
 
 $uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
