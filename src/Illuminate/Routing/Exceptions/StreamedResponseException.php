laravel/framework

commit 306e7e4225656e9f60b5c8d74827f9dc6b4c4a57

[9.x] Do not render exceptions in streamed download (#40701)
* Do not render exceptions in streamed download

* Update Handler.php

* Update StreamedResponseException.php

Co-authored-by: Taylor Otwell <taylor@laravel.com>

src/Illuminate/Routing/Exceptions/StreamedResponseException.php
@@ -1,0 +1,50 @@
+<?php
+
+namespace Illuminate\Routing\Exceptions;
+
+use Illuminate\Http\Response;
+use RuntimeException;
+use Throwable;
+
+class StreamedResponseException extends RuntimeException
+{
+    /**
+     * The actual exception thrown during the stream.
+     *
+     * @var \Throwable
+     */
+    public $originalException;
+
+    /**
+     * Create a new exception instance.
+     *
+     * @param  \Throwable  $originalException
+     * @return void
+     */
+    public function __construct(Throwable $originalException)
+    {
+        $this->originalException = $originalException;
+
+        parent::__construct($originalException->message);
+    }
+
+    /**
+     * Render the exception.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function render()
+    {
+        return new Response('');
+    }
+
+    /**
+     * Get the actual exception thrown during the stream.
+     *
+     * @return \Throwable
+     */
+    public function getInnerException()
+    {
+        return $this->originalException;
+    }
+}

src/Illuminate/Foundation/Exceptions/Handler.php
@@ -369,6 +369,11 @@ class Handler implements ExceptionHandlerContract
      */
     protected function mapException(Throwable $e)
     {
+        if (method_exists($e, 'getInnerException') &&
+            ($inner = $e->getInnerException()) instanceof Throwable) {
+            return $inner;
+        }
+
         foreach ($this->exceptionMap as $class => $mapper) {
             if (is_a($e, $class)) {
                 return $mapper($e);

src/Illuminate/Routing/ResponseFactory.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Routing\ResponseFactory as FactoryContract;
 use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Routing\Exceptions\StreamedResponseException;
 use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Macroable;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use Throwable;
 
 class ResponseFactory implements FactoryContract
 {
@@ -138,7 +140,15 @@ class ResponseFactory implements FactoryContract
      */
     public function streamDownload($callback, $name = null, array $headers = [], $disposition = 'attachment')
     {
-        $response = new StreamedResponse($callback, 200, $headers);
+        $withWrappedException = function () use ($callback) {
+            try {
+                $callback();
+            } catch (Throwable $e) {
+                throw new StreamedResponseException($e);
+            }
+        };
+
+        $response = new StreamedResponse($withWrappedException, 200, $headers);
 
         if (! is_null($name)) {
             $response->headers->set('Content-Disposition', $response->headers->makeDisposition(

commit 77a900587c708ce715089e751f5cf40b449a0ddc

[9.x] Fix error introduced in #40701
Hey, this PR just fixes a small bug in the change not render exception inside a streamed download.  
Currently it accesses the `->message` property of a `Throwable`, which is not defined in the interface and is (in my case) protected, which results in a new error.
This PR just changes that to the defined `->getMessage()` of a `Throwable`.

src/Illuminate/Routing/Exceptions/StreamedResponseException.php
@@ -25,7 +25,7 @@ class StreamedResponseException extends RuntimeException
     {
         $this->originalException = $originalException;
 
-        parent::__construct($originalException->message);
+        parent::__construct($originalException->getMessage());
     }
 
     /**