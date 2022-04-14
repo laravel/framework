bfb1774e78306fb4345c01a00f7a4fee5485d2e9
Brandon Surowiec <BrandonSurowiec@users.noreply.github.com>
2022-04-14 09:47:10 -0400

    [9.x] Fix PHP warnings when rendering long blade string (#41956) (#41970)
    * [9.x] Fix PHP warnings when rendering long blade string (#41745)
    
    * Apply fixes from StyleCI
    
    (cherry picked from commit bb0da4a58a49fdb81e17c7b5ee364a515b015f33)
    
    Co-authored-by: Andrew Bashtannik <bashtannik@gmail.com>

--- src/Illuminate/View/Component.php
+++ src/Illuminate/View/Component.php
@@ -75,7 +75,7 @@ abstract class Component
         $resolver = function ($view) {
             $factory = Container::getInstance()->make('view');
 
-            return $factory->exists($view)
+            return strlen($view) <= PHP_MAXPATHLEN && $factory->exists($view)
                         ? $view
                         : $this->createBladeViewFromString($factory, $view);
         };
