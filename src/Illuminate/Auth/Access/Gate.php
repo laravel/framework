6c807ba300da9d45736a12370b08abf1b517c350
ARI <99682351+bvtterfly@users.noreply.github.com>
2022-04-16 02:40:31 +0430

    [9.x] Use arrow function (#41995)
    * use arrow function
    
    * fix style
    
    * Update Gate.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Auth/Access/Gate.php
+++ src/Illuminate/Auth/Access/Gate.php
@@ -338,9 +338,9 @@ class Gate implements GateContract
      */
     public function check($abilities, $arguments = [])
     {
-        return collect($abilities)->every(function ($ability) use ($arguments) {
-            return $this->inspect($ability, $arguments)->allowed();
-        });
+        return collect($abilities)->every(
+            fn ($ability) => $this->inspect($ability, $arguments)->allowed()
+        );
     }
 
     /**
@@ -352,9 +352,7 @@ class Gate implements GateContract
      */
     public function any($abilities, $arguments = [])
     {
-        return collect($abilities)->contains(function ($ability) use ($arguments) {
-            return $this->check($ability, $arguments);
-        });
+        return collect($abilities)->contains(fn ($ability) => $this->check($ability, $arguments));
     }
 
     /**
@@ -819,9 +817,7 @@ class Gate implements GateContract
      */
     public function forUser($user)
     {
-        $callback = function () use ($user) {
-            return $user;
-        };
+        $callback = fn () => $user;
 
         return new static(
             $this->container, $callback, $this->abilities,
