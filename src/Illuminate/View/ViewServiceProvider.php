68e41fd3691b9aa5548e07c5caf38696c4082513
Taylor Otwell <taylor@laravel.com>
2022-04-22 14:03:25 -0500

    allow extension

--- src/Illuminate/View/ViewServiceProvider.php
+++ src/Illuminate/View/ViewServiceProvider.php
@@ -90,6 +90,7 @@ class ViewServiceProvider extends ServiceProvider
                 $app['config']['view.compiled'],
                 $app['config']->get('view.relative_hash', false) ? $app->basePath() : '',
                 $app['config']->get('view.cache', true),
+                $app['config']->get('view.compiled_extension', 'php'),
             ), function ($blade) {
                 $blade->component('dynamic-component', DynamicComponent::class);
             });
