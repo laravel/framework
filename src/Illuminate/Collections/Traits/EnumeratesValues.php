464837d19f20701eb07ac7bae8d65b587c621b82
Mike Wu <hello@mikewu.dev>
2022-04-27 11:00:38 +0900

    Updated Collection's jsonSerialize return type to be array

--- src/Illuminate/Collections/Traits/EnumeratesValues.php
+++ src/Illuminate/Collections/Traits/EnumeratesValues.php
@@ -877,9 +877,9 @@ trait EnumeratesValues
     /**
      * Convert the object into something JSON serializable.
      *
-     * @return array|string
+     * @return array
      */
-    public function jsonSerialize(): array|string
+    public function jsonSerialize(): array
     {
         return array_map(function ($value) {
             if ($value instanceof JsonSerializable) {
