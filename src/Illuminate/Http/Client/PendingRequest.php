6e5e599a32b35c666b6129cf9016f9ed3fc43d8d
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-05-04 01:18:29 +0700

    [8.x] Enable to modify HTTP Client request headers when using beforeSending() callback (#42244)
    * Fix return docblock definition
    
    * Adding reference on $request parameter and retrieve the latest value from the given callback
    
    * Create unit test to assert if http factory can add authorization header using before sending callback

--- src/Illuminate/Http/Client/PendingRequest.php
+++ src/Illuminate/Http/Client/PendingRequest.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\Macroable;
 use Psr\Http\Message\MessageInterface;
+use Psr\Http\Message\RequestInterface;
 use Symfony\Component\VarDumper\VarDumper;
 
 class PendingRequest
@@ -984,15 +985,21 @@ class PendingRequest
      *
      * @param  \GuzzleHttp\Psr7\RequestInterface  $request
      * @param  array  $options
-     * @return \Closure
+     * @return \GuzzleHttp\Psr7\RequestInterface
      */
     public function runBeforeSendingCallbacks($request, array $options)
     {
-        return tap($request, function ($request) use ($options) {
-            $this->beforeSendingCallbacks->each(function ($callback) use ($request, $options) {
-                call_user_func(
+        return tap($request, function (&$request) use ($options) {
+            $this->beforeSendingCallbacks->each(function ($callback) use (&$request, $options) {
+                $callbackResult = call_user_func(
                     $callback, (new Request($request))->withData($options['laravel_data']), $options, $this
                 );
+
+                if ($callbackResult instanceof RequestInterface) {
+                    $request = $callbackResult;
+                } elseif ($callbackResult instanceof Request) {
+                    $request = $callbackResult->toPsrRequest();
+                }
             });
         });
     }
