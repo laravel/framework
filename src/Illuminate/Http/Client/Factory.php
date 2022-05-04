347b9e144d3c7eae8af83c29cd5caf4dd352d80c
Dennis <3907144+denniseilander@users.noreply.github.com>
2022-05-04 22:44:22 +0200

    Added 'throwIf' method to PendingRequest (#42260)

--- src/Illuminate/Http/Client/Factory.php
+++ src/Illuminate/Http/Client/Factory.php
@@ -42,6 +42,7 @@ use PHPUnit\Framework\Assert as PHPUnit;
  * @method \Illuminate\Http\Client\PendingRequest withoutRedirecting()
  * @method \Illuminate\Http\Client\PendingRequest withoutVerifying()
  * @method \Illuminate\Http\Client\PendingRequest throw(callable $callback = null)
+ * @method \Illuminate\Http\Client\PendingRequest throwIf($condition)
  * @method array pool(callable $callback)
  * @method \Illuminate\Http\Client\Response delete(string $url, array $data = [])
  * @method \Illuminate\Http\Client\Response get(string $url, array|string|null $query = null)
