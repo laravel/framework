424d96ca9f429d189d6db413eff565dc6dc9c7cc
Taylor Otwell <taylor@laravel.com>
2022-05-05 13:57:48 -0500

    formatting

--- src/Illuminate/Notifications/NotificationSender.php
+++ src/Illuminate/Notifications/NotificationSender.php
@@ -213,10 +213,7 @@ class NotificationSender
                     (new SendQueuedNotifications($notifiable, $notification, [$channel]))
                             ->onConnection($notification->connection)
                             ->onQueue($queue)
-                            ->delay(is_array($delay) ?
-                                    ($delay[$channel] ?? null)
-                                    : $delay
-                            )
+                            ->delay(is_array($delay) ? ($delay[$channel] ?? null) : $delay)
                             ->through(
                                 array_merge(
                                     method_exists($notification, 'middleware') ? $notification->middleware() : [],
