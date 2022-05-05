c580f11a1e7316ac847fc6e0c37ec173848de7e2
Emílio B. Pedrollo <6577541+emiliopedrollo@users.noreply.github.com>
2022-05-05 15:54:11 -0300

    [9.x] Add method to extend localeArray generation (#42275)
    * Add method to extend localeArray generation
    
    * style
    
    * formatting
    
    * move method
    
    * formatting
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Translation/Translator.php
+++ src/Illuminate/Translation/Translator.php
@@ -50,6 +50,13 @@ class Translator extends NamespacedItemResolver implements TranslatorContract
     protected $selector;
 
     /**
+     * The callable that should be invoked to determine applicable locales.
+     *
+     * @var callable
+     */
+    protected $determineLocalesUsing;
+
+    /**
      * Create a new translator instance.
      *
      * @param  \Illuminate\Contracts\Translation\Loader  $loader
@@ -325,7 +332,20 @@ class Translator extends NamespacedItemResolver implements TranslatorContract
      */
     protected function localeArray($locale)
     {
-        return array_filter([$locale ?: $this->locale, $this->fallback]);
+        $locales = array_filter([$locale ?: $this->locale, $this->fallback]);
+
+        return call_user_func($this->determineLocalesUsing ?: fn () => $locales, $locales);
+    }
+
+    /**
+     * Specify a callback that should be invoked to determined the applicable locale array.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    public function determineLocalesUsing($callback)
+    {
+        $this->determineLocalesUsing = $callback;
     }
 
     /**
