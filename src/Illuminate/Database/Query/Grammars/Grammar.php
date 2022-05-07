f8aee4d0eea7bd6637485257113a6e06c060a9c5
Haytham Salama <haythamasalama@gmail.com>
2022-05-08 00:43:32 +0200

    add whereBetweenDate() to Grammar

--- src/Illuminate/Database/Query/Grammars/Grammar.php
+++ src/Illuminate/Database/Query/Grammars/Grammar.php
@@ -405,6 +405,24 @@ class Grammar extends BaseGrammar
     }
 
     /**
+     * Compile a "between date" clause.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  array  $where
+     * @return string
+     */
+    protected function whereBetweenDate(Builder $query, $where)
+    {
+        $between = $where['not'] ? 'not between' : 'between';
+
+        $min = $this->parameter(is_array($where['values']) ? reset($where['values']) : $where['values'][0]);
+
+        $max = $this->parameter(is_array($where['values']) ? end($where['values']) : $where['values'][1]);
+
+        return 'date('.$this->wrap($where['column']).') '.$between.' '.$min.' and '.$max;
+    }
+
+    /**
      * Compile a "where date" clause.
      *
      * @param  \Illuminate\Database\Query\Builder  $query
