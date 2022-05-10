c5e0a73c6c03c8d7d930dc65b795e937fe3f1811
Dries Vints <dries@vints.io>
2022-05-10 11:03:16 +0200

    wip

--- tests/Validation/ValidationValidatorTest.php
+++ tests/Validation/ValidationValidatorTest.php
@@ -2294,7 +2294,7 @@ class ValidationValidatorTest extends TestCase
         $v = new Validator($trans, ['foo' => '1..2'], ['foo' => 'digits_between:1,10']);
         $this->assertTrue($v->fails());
 
-        $v = new Validator($trans, ['foo' => '123.456.7'], ['foo' => 'digits_between:1,10']);
+        $v = new Validator($trans, ['foo' => '123.456.789'], ['foo' => 'digits_between:1,10']);
         $this->assertTrue($v->fails());
 
         $v = new Validator($trans, ['foo' => '...'], ['foo' => 'digits_between:1,10']);
