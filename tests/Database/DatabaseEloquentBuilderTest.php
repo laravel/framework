25127ba88d9a5419ea7a3e99829284e21f7194f2
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2022-05-10 17:52:58 -0300

    [9.x] Add `is_string` check to `QueriesRelationships@requalifyWhereTables` (#42341)
    * add is_string check
    
    * fix dynamic relation hash resolution

--- tests/Database/DatabaseEloquentBuilderTest.php
+++ tests/Database/DatabaseEloquentBuilderTest.php
@@ -1292,6 +1292,21 @@ class DatabaseEloquentBuilderTest extends TestCase
         $this->assertSame('select "eloquent_builder_test_model_parent_stubs".*, (select count(*) from "eloquent_builder_test_model_close_related_stubs" where "eloquent_builder_test_model_parent_stubs"."foo_id" = "eloquent_builder_test_model_close_related_stubs"."id") as "foo_bar", (select count(*) from "eloquent_builder_test_model_close_related_stubs" where "eloquent_builder_test_model_parent_stubs"."foo_id" = "eloquent_builder_test_model_close_related_stubs"."id") as "foo_count" from "eloquent_builder_test_model_parent_stubs"', $builder->toSql());
     }
 
+    public function testWithAggregateAndSelfRelationConstrain()
+    {
+        EloquentBuilderTestStub::resolveRelationUsing('children', function ($model) {
+            return $model->hasMany(EloquentBuilderTestStub::class, 'parent_id', 'id')->where('enum_value', new stdClass);
+        });
+
+        $model = new EloquentBuilderTestStub;
+        $this->mockConnectionForModel($model, '');
+        $relationHash = $model->children()->getRelationCountHash(false);
+
+        $builder = $model->withCount('children');
+
+        $this->assertSame(vsprintf('select "table".*, (select count(*) from "table" as "%s" where "table"."id" = "%s"."parent_id" and "enum_value" = ?) as "children_count" from "table"', [$relationHash, $relationHash]), $builder->toSql());
+    }
+
     public function testWithExists()
     {
         $model = new EloquentBuilderTestModelParentStub;
