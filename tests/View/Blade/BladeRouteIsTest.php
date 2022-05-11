c67e593e358a4c7976147b8533f7c9f22c44f953
Sheldon Knight <77359881+Sheldon-Knight@users.noreply.github.com>
2022-05-11 21:26:57 +0200

    Updare route Is Tests

--- tests/View/Blade/BladeRouteIsTest.php
+++ tests/View/Blade/BladeRouteIsTest.php
@@ -6,8 +6,8 @@ class BladeRouteIsTest extends AbstractBladeTestCase
 {
     public function testRouteIsCompiled()
     {
-        $string = '@routeIs(foo) bar @endRouteIs';
-        $expected = '<?php if(request()->routeIs(foo)): ?> bar <?php endif; ?>';
+        $string = '@routeIs(\'foo\', \'bar\') baz @endrouteIs';
+        $expected = '<?php if (request()->routeIs(\'foo\', \'bar\')): ?> baz <?php endif; ?>';
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 }
