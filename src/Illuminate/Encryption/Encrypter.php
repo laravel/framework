5a2008bc98c821001a4fa48212127d199f158f1c
Diadal <info@diadal.com.ng>
2022-05-12 11:43:05 -0700

    Update Encrypter.php
    enforce decrypt payload to string, fix for below error
    
    `base64_decode(): Passing null to parameter #1 ($string) of type string is deprecated in vendor/laravel/framework/src/Illuminate/Encryption/Encrypter.php on line 208`

--- src/Illuminate/Encryption/Encrypter.php
+++ src/Illuminate/Encryption/Encrypter.php
@@ -148,6 +148,7 @@ class Encrypter implements EncrypterContract, StringEncrypter
      */
     public function decrypt($payload, $unserialize = true)
     {
+        $payload = strval($payload);
         $payload = $this->getJsonPayload($payload);
 
         $iv = base64_decode($payload['iv']);
