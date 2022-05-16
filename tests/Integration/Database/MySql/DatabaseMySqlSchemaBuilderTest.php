0e32622638ef2f99a8fbc22f0c64f3f9d79b0f56
Andrew Broberg <andrew@broberg.id.au>
2022-05-17 09:00:34 +1000

    Fix test for MySQL 8

--- tests/Integration/Database/MySql/DatabaseMySqlSchemaBuilderTest.php
+++ tests/Integration/Database/MySql/DatabaseMySqlSchemaBuilderTest.php
@@ -22,7 +22,8 @@ class DatabaseMySqlSchemaBuilderTest extends MySqlTestCase
         $tableInfo = DB::table('information_schema.tables')
             ->where('table_schema', $this->app['config']->get('database.connections.mysql.database'))
             ->where('table_name', 'users')
-            ->select('table_comment')->first();
+            ->select('table_comment as table_comment')
+            ->first();
 
         $this->assertEquals('This is a comment', $tableInfo->table_comment);
 
