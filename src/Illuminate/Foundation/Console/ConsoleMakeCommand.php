647819bf3bc6e1ca81d5809740ca349bc5aa6032
Yes_Im <50667072+adideas@users.noreply.github.com>
2022-05-20 11:21:39 +0300

    Update ConsoleMakeCommand.php
    Unfortunately I forgot about "use class"

--- src/Illuminate/Foundation/Console/ConsoleMakeCommand.php
+++ src/Illuminate/Foundation/Console/ConsoleMakeCommand.php
@@ -4,6 +4,7 @@ namespace Illuminate\Foundation\Console;
 
 use Illuminate\Console\Concerns\CreatesMatchingTest;
 use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
