1769f727337a166f9d1e80d834c35e3a0dcc895c
Tim MacDonald <hello@timacdonald.me>
2022-05-25 16:26:07 +1000

    update docblock

--- src/Illuminate/View/Compilers/Concerns/CompilesHelpers.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesHelpers.php
@@ -52,7 +52,7 @@ trait CompilesHelpers
     /**
      * Compile the "vite" statements into valid PHP.
      *
-     * @param  string  $arguments
+     * @param  ?string  $arguments
      * @return string
      */
     protected function compileVite($arguments)
