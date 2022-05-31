0d7ac95d98986171ea2419cf8c35768e380e1595
Dries Vints <dries@vints.io>
2022-05-31 16:05:27 +0200

    Remove deprecated dates property (#42587)

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -106,15 +106,6 @@ trait HasAttributes
     ];
 
     /**
-     * The attributes that should be mutated to dates.
-     *
-     * @deprecated Use the "casts" property
-     *
-     * @var array
-     */
-    protected $dates = [];
-
-    /**
      * The storage format of the model's date columns.
      *
      * @var string
@@ -1366,16 +1357,10 @@ trait HasAttributes
      */
     public function getDates()
     {
-        if (! $this->usesTimestamps()) {
-            return $this->dates;
-        }
-
-        $defaults = [
+        return $this->usesTimestamps() ? [
             $this->getCreatedAtColumn(),
             $this->getUpdatedAtColumn(),
-        ];
-
-        return array_unique(array_merge($this->dates, $defaults));
+        ] : [];
     }
 
     /**
