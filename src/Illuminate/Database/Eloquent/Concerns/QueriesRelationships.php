702c119954f795554b78408a09b2c8c85f8b311e
Lito <lito@eordes.com>
2022-06-01 16:08:52 +0200

    [9.x] Added Eloquent withWhereHas method (#42597)
    * [9.x] Added Eloquent withWhereHas method
    
    * Fixed code style
    
    * Added withWhereHas tests. Related #42597
    
    * Fixed code style typo. Related #42597

--- src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
+++ src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
@@ -153,6 +153,23 @@ trait QueriesRelationships
     }
 
     /**
+     * Add a relationship count / exists condition to the query with where clauses.
+     *
+     * Also load the relationship with same condition.
+     *
+     * @param  string  $relation
+     * @param  \Closure|null  $callback
+     * @param  string  $operator
+     * @param  int  $count
+     * @return \Illuminate\Database\Eloquent\Builder|static
+     */
+    public function withWhereHas($relation, Closure $callback = null, $operator = '>=', $count = 1)
+    {
+        return $this->whereHas($relation, $callback, $operator, $count)
+            ->with($callback ? [$relation => fn ($query) => $callback($query)] : $relation);
+    }
+
+    /**
      * Add a relationship count / exists condition to the query with where clauses and an "or".
      *
      * @param  string  $relation
