c24bf578740726db090cae90a4f050d14c95a6bd
Mahmoud Mohamed Ramadan <48416569+mahmoudmohamedramadan@users.noreply.github.com>
2022-06-03 15:41:05 +0200

    Adds `withoutEagerLoad` function (#42641)
    * Adds `withoutEagerLoad` function
    
    * Update Builder.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -1573,6 +1573,19 @@ class Builder implements BuilderContract
 
         return $this;
     }
+    
+    /**
+     * Indicate that the given relationships should not be eagerly loaded.
+     *
+     * @param  array  $relations
+     * @return $this
+     */
+    public function withoutEagerLoad(array $relations)
+    {
+        $relations = array_diff(array_keys($this->model->getRelations()), $relations);
+
+        return $this->with($relations);
+    }
 
     /**
      * Flush the relationships being eagerly loaded.
