ecc18765dd40f9a5b8a5a0dd420b25f5d33ac6f9
Benedikt Franke <benedikt.franke@mll.com>
2022-06-03 10:49:41 +0200

    Increase reserved memory for error handling
    Follow up to https://github.com/laravel/framework/pull/42630
    
    The error handler should be able to report exceptions
    arising from exceeding the PHP memory limit. Because
    of changes made to error handling, the previously
    configured value is no longer sufficiently large.
    
    A similar change was also done in Symfony a while ago,
    see https://github.com/symfony/symfony/pull/44327.
    
    I used the following artisan command to determine the amount
    of memory required for error handling, using a reporter that
    simply writes to a file:
    
    ```php
    <?php declare(strict_types=1);
    
    namespace App\Console\Commands;
    
    use Illuminate\Console\Command;
    
    final class MeasureHandlerMemory extends Command
    {
        protected $signature = 'measure-handler-memory';
    
        private int $peak;
    
        public function handle(): void
        {
            $this->peak = memory_get_peak_usage();
    
            trigger_error('', E_USER_ERROR);
        }
    
        public function __destruct()
        {
            $used = memory_get_peak_usage() - $this->peak;
            echo "error handling used: " . $used . "\n";
    
            $before = memory_get_usage();
            $times = 10240;
            $reserve = str_repeat('x', $times);
            $after = memory_get_usage() - $before;
            echo 'repeat times ' . $times . ' reserves: ' . $after . "\n";
    
            $ratio = $after / $times;
            echo 'ratio between bytes and repeat: ' . $ratio . "\n";
    
            echo 'minimum times to repeat: ' . $used / $ratio . "\n";
        }
    }
    ```

--- src/Illuminate/Foundation/Bootstrap/HandleExceptions.php
+++ src/Illuminate/Foundation/Bootstrap/HandleExceptions.php
@@ -36,7 +36,12 @@ class HandleExceptions
      */
     public function bootstrap(Application $app)
     {
-        self::$reservedMemory = str_repeat('x', 10240);
+        // The amount of times to repeat needs to be determined experimentally:
+        // - save the result of memory_get_peak_usage()
+        // - trigger error handling through trigger_error()
+        // - subtract the previous from the current memory_get_peak_usage()
+        // - divide the result by 1.2 and add some leeway
+        self::$reservedMemory = str_repeat('x', 32768);
 
         $this->app = $app;
 
