b7a1b92096aec4c838e320ca55ef9f16839f0a04
SuperDJ <6484766+SuperDJ@users.noreply.github.com>
2022-06-06 17:07:47 +0200

    [9.x] Made Http::sequence()->push() consistent with Http::response() (#42659)
    * Made Http::sequence()->push() consistent with Http::response() by allowing null as the body. This will also fix an error in PHPStan.
    
    * changed type

--- src/Illuminate/Http/Client/ResponseSequence.php
+++ src/Illuminate/Http/Client/ResponseSequence.php
@@ -44,12 +44,12 @@ class ResponseSequence
     /**
      * Push a response to the sequence.
      *
-     * @param  string|array  $body
+     * @param  string|array|null  $body
      * @param  int  $status
      * @param  array  $headers
      * @return $this
      */
-    public function push($body = '', int $status = 200, array $headers = [])
+    public function push($body = null, int $status = 200, array $headers = [])
     {
         $body = is_array($body) ? json_encode($body) : $body;
 
