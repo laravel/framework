60d74e1fc67fb82b029f87f21d1cab4b6aa06016
Hendrik Mennen <77828605+hmennen90@users.noreply.github.com>
2022-06-08 14:31:32 +0200

    Moved DatabaseEloquentJsonTest.php in DatabaseEloquentWithCastsTest.php
    Added Test Routines for Mysql and Pgsql

--- tests/Database/DatabaseEloquentWithCastsTest.php
+++ tests/Database/DatabaseEloquentWithCastsTest.php
@@ -3,7 +3,9 @@
 namespace Illuminate\Tests\Database;
 
 use Illuminate\Database\Capsule\Manager as DB;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Schema\Blueprint;
 use PHPUnit\Framework\TestCase;
 
 class DatabaseEloquentWithCastsTest extends TestCase
@@ -19,6 +21,20 @@ class DatabaseEloquentWithCastsTest extends TestCase
             'database' => ':memory:',
         ]);
 
+        $db->addConnection([
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'forge',
+            'username' => 'forge'
+        ], 'mysql');
+
+        $db->addConnection([
+            'driver' => 'pgsql',
+            'host' => 'localhost',
+            'database' => 'forge',
+            'username' => 'forge'
+        ], 'pgsql');
+
         $db->bootEloquent();
         $db->setAsGlobal();
 
@@ -32,6 +48,48 @@ class DatabaseEloquentWithCastsTest extends TestCase
             $table->time('time');
             $table->timestamps();
         });
+
+        $this->schema()->create('json_arrays', function(Blueprint $table) {
+            $table->increments('id');
+            $table->json('sample_data');
+        });
+
+        $this->schema()->create('json_objects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->json('sample_data');
+        });
+
+        $this->schema('mysql')->create('json_arrays', function(Blueprint $table) {
+            $table->increments('id');
+            $table->json('sample_data');
+        });
+
+        $this->schema('mysql')->create('json_objects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->json('sample_data');
+        });
+
+        $this->schema('pgsql')->create('json_arrays', function(Blueprint $table) {
+            $table->increments('id');
+            $table->json('sample_data');
+        });
+
+        $this->schema('pgsql')->create('json_objects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->json('sample_data');
+        });
+    }
+
+    protected function tearDown(): void {
+        parent::tearDown();
+
+        $this->schema('mysql')->drop('json_arrays');
+
+        $this->schema('mysql')->drop('json_objects');
+
+        $this->schema('pgsql')->drop('json_arrays');
+
+        $this->schema('pgsql')->drop('json_objects');
     }
 
     public function testWithFirstOrNew()
@@ -59,14 +117,99 @@ class DatabaseEloquentWithCastsTest extends TestCase
         $this->assertSame($time1->id, $time2->id);
     }
 
+    public function testJsonArraysGetChangesIsEmptyOnAssignSameArray()
+    {
+        $sample_data = [
+            'aa' => 1,
+            'b' => 2
+        ];
+
+        $model = new JsonArray();
+        $model->sample_data = $sample_data;
+        $model->save();
+
+        $newModel = JsonArray::find(1);
+        $newModel->sample_data = $sample_data;
+        $newModel->save();
+
+        $mysqlModel = new JsonArray();
+        $mysqlModel->setConnection('mysql');
+        $mysqlModel->sample_data = $sample_data;
+        $model->save();
+
+        $newMysqlModel = new JsonArray();
+        $newMysqlModel->setConnection('mysql');
+        $newMysqlModel::find(1);
+        $newMysqlModel->sample_data = $sample_data;
+        $newMysqlModel->save();
+
+        $pgsqlModel = new JsonArray();
+        $pgsqlModel->setConnection('pgsql');
+        $pgsqlModel->sample_data = $sample_data;
+        $pgsqlModel->save();
+
+        $newPgsqlModel = new JsonArray();
+        $newPgsqlModel->setConnection('pgsql');
+        $newPgsqlModel->sample_data = $sample_data;
+        $newPgsqlModel->save();
+
+        $this->assertEmpty($newModel->getChanges());
+
+        $this->assertEmpty($newMysqlModel->getChanges());
+
+        $this->assertEmpty($newPgsqlModel->getChanges());
+    }
+
+    public function testJsonObjectsGetChangesIsEmptyOnAssignSameArray()
+    {
+        $sample_data = [
+            'aa' => 1,
+            'b' => 2
+        ];
+
+        $model = new JsonObject();
+        $model->sample_data = $sample_data;
+        $model->save();
+
+        $newModel = JsonObject::find(1);
+        $newModel->sample_data = $sample_data;
+        $newModel->save();
+
+        $mysqlModel = new JsonObject();
+        $mysqlModel->setConnection('mysql');
+        $mysqlModel->sample_data = $sample_data;
+        $mysqlModel->save();
+
+        $newMysqlModel = new JsonObject();
+        $newMysqlModel->setConnection('mysql');
+        $newMysqlModel->sample_data = $sample_data;
+        $newMysqlModel->save();
+
+        $pgsqlModel = new JsonObject();
+        $pgsqlModel->setConnection('pgsql');
+        $pgsqlModel->sample_data = $sample_data;
+        $pgsqlModel->save();
+
+        $newPgsqlModel = new JsonObject();
+        $newPgsqlModel->setConnection('pgsql');
+        $newPgsqlModel->sample_data = $sample_data;
+        $newPgsqlModel->save();
+
+        $this->assertEmpty($newModel->getChanges());
+
+        $this->assertEmpty($newMysqlModel->getChanges());
+
+        $this->assertEmpty($newPgsqlModel->getChanges());
+    }
+
     /**
      * Get a database connection instance.
      *
      * @return \Illuminate\Database\Connection
      */
-    protected function connection()
+    protected function connection($connection = null)
     {
-        return Eloquent::getConnectionResolver()->connection();
+        return Eloquent::getConnectionResolver()->connection($connection);
     }
 
     /**
@@ -74,9 +217,9 @@ class DatabaseEloquentWithCastsTest extends TestCase
      *
      * @return \Illuminate\Database\Schema\Builder
      */
-    protected function schema()
+    protected function schema($connection = null)
     {
-        return $this->connection()->getSchemaBuilder();
+        return $this->connection($connection)->getSchemaBuilder();
     }
 }
 
@@ -86,5 +229,23 @@ class Time extends Eloquent
 
     protected $casts = [
         'time' => 'datetime',
+    ];
+}
+
+class JsonObject extends Model
+{
+    public $timestamps = false;
+
+    protected $casts = [
+        'sample_data' => 'object'
+    ];
+}
+
+class JsonArray extends Model
+{
+    public $timestamps = false;
+
+    protected $casts = [
+        'sample_data' => 'array',
     ];
 }
