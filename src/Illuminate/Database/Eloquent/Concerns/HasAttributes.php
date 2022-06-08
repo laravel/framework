6c575574f590e152059f2e1f3121fb0f03afeab3
Hendrik Mennen <77828605+hmennen90@users.noreply.github.com>
2022-06-08 19:57:52 +0200

    Readded Array to Cast Check - maybe this is enough

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -1942,7 +1942,7 @@ trait HasAttributes
         } elseif ($this->isDateAttribute($key) || $this->isDateCastableWithCustomFormat($key)) {
             return $this->fromDateTime($attribute) ===
                 $this->fromDateTime($original);
-        } elseif ($this->hasCast($key, ['object', 'collection'])) {
+        } elseif ($this->hasCast($key, ['object', 'collection', 'array'])) {
             return $this->fromJson($attribute) ===
                 $this->fromJson($original);
         } elseif ($this->hasCast($key, ['real', 'float', 'double'])) {
