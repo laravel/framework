4612767fb012ed3729c29444e6960aa8b4a60aa7
zackrowe <4922356+zackrowe@users.noreply.github.com>
2022-06-09 05:49:24 -0600

    Add authorize function to Authorizable trait

--- src/Illuminate/Foundation/Auth/Access/Authorizable.php
+++ src/Illuminate/Foundation/Auth/Access/Authorizable.php
@@ -7,6 +7,20 @@ use Illuminate\Contracts\Auth\Access\Gate;
 trait Authorizable
 {
     /**
+     * Determine if the entity should be granted the given ability.
+     *
+     * @param  string  $ability
+     * @param  array|mixed  $arguments
+     * @return \Illuminate\Auth\Access\Response
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function authorize($ability, $arguments = [])
+    {
+        return app(Gate::class)->forUser($this)->authorize($ability, $arguments);
+    }
+
+    /**
      * Determine if the entity has the given abilities.
      *
      * @param  iterable|string  $abilities
