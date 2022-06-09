fe97114b151d198c1f2fdccde53c461a1485b8e4
zackrowe <4922356+zackrowe@users.noreply.github.com>
2022-06-09 06:12:48 -0600

    Revert "Remove test"
    This reverts commit e59d3a2968b97ba7abc18c80c81facb5afe5516b.

--- tests/Auth/AuthorizableTest.php
+++ tests/Auth/AuthorizableTest.php
@@ -10,50 +10,50 @@ use Illuminate\Foundation\Testing\TestCase;
 use Orchestra\Testbench\Concerns\CreatesApplication;
 use Throwable;
 
-//class AuthorizableTest extends TestCase
-//{
-//    use CreatesApplication;
-//
-//    public function testAuthorizeMethodThrowsAuthorizationExceptionWithPolicyDenial()
-//    {
-//        \Illuminate\Support\Facades\Gate::policy(AuthorizableTestDummy::class, AuthorizableTestPolicy::class);
-//
-//        $user = new User();
-//
-//        try {
-//            $user->authorize('willDeny', AuthorizableTestDummy::class);
-//        } catch (Throwable $e) {
-//            $this->assertInstanceOf(AuthorizationException::class, $e);
-//            $this->assertTrue($e->response()->denied());
-//        }
-//    }
-//
-//    public function testAuthorizeMethodReturnsAllowedResponseWithPolicySuccess()
-//    {
-//        \Illuminate\Support\Facades\Gate::policy(AuthorizableTestDummy::class, AuthorizableTestPolicy::class);
-//
-//        $user = new User();
-//
-//        $response = $user->authorize('willSucceed', AuthorizableTestDummy::class);
-//
-//        $this->assertTrue($response->allowed());
-//    }
-//}
-//
-//class AuthorizableTestDummy
-//{
-//
-//}
-//
-//class AuthorizableTestPolicy
-//{
-//    public function willDeny($user)
-//    {
-//        return false;
-//    }
-//
-//    public function willSucceed($user)
-//    {
-//        return true;
-//    }
-//}
+class AuthorizableTest extends TestCase
+{
+    use CreatesApplication;
+
+    public function testAuthorizeMethodThrowsAuthorizationExceptionWithPolicyDenial()
+    {
+        \Illuminate\Support\Facades\Gate::policy(AuthorizableTestDummy::class, AuthorizableTestPolicy::class);
+
+        $user = new User();
+
+        try {
+            $user->authorize('willDeny', AuthorizableTestDummy::class);
+        } catch (Throwable $e) {
+            $this->assertInstanceOf(AuthorizationException::class, $e);
+            $this->assertTrue($e->response()->denied());
+        }
+    }
+
+    public function testAuthorizeMethodReturnsAllowedResponseWithPolicySuccess()
+    {
+        \Illuminate\Support\Facades\Gate::policy(AuthorizableTestDummy::class, AuthorizableTestPolicy::class);
+
+        $user = new User();
+
+        $response = $user->authorize('willSucceed', AuthorizableTestDummy::class);
+
+        $this->assertTrue($response->allowed());
+    }
+}
+
+class AuthorizableTestDummy
+{
+
+}
+
+class AuthorizableTestPolicy
+{
+    public function willDeny($user)
+    {
+        return false;
+    }
+
+    public function willSucceed($user)
+    {
+        return true;
+    }
+}
