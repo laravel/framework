cd6e76fe864161af7307be7abb6d4d18cbe064ca
Daniel Esteve <daniel.esteve.rem@gmail.com>
2022-06-10 20:50:29 +0200

    [8.x] fix `Request` offsetExists without routeResolver (#42754)
    * fix Request offsetExists without routeResolver
    
    * simplify test
    
    * formatting
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Http/Request.php
+++ src/Illuminate/Http/Request.php
@@ -669,8 +669,10 @@ class Request extends SymfonyRequest implements Arrayable, ArrayAccess
     #[\ReturnTypeWillChange]
     public function offsetExists($offset)
     {
+        $route = $this->route();
+
         return Arr::has(
-            $this->all() + $this->route()->parameters(),
+            $this->all() + ($route ? $route->parameters() : []),
             $offset
         );
     }
