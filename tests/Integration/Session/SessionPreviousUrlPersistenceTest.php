ffc45737b404ed9cdaaa319992e8d9345486de7a
Tony Messias <tonysm@hey.com>
2022-06-15 19:59:01 -0300

    Remove unnecessary imports and bindings

--- tests/Integration/Session/SessionPreviousUrlPersistenceTest.php
+++ tests/Integration/Session/SessionPreviousUrlPersistenceTest.php
@@ -2,14 +2,8 @@
 
 namespace Illuminate\Tests\Integration\Session;
 
-use Illuminate\Contracts\Debug\ExceptionHandler;
-use Illuminate\Http\Response;
-use Illuminate\Session\NullSessionHandler;
-use Illuminate\Session\TokenMismatchException;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
-use Mockery as m;
 use Orchestra\Testbench\TestCase;
 
 class SessionPreviousUrlPersistenceTest extends TestCase
@@ -53,13 +47,6 @@ class SessionPreviousUrlPersistenceTest extends TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        $app->instance(
-            ExceptionHandler::class,
-            $handler = m::mock(ExceptionHandler::class)->shouldIgnoreMissing()
-        );
-
-        $handler->shouldReceive('render')->andReturn(new Response);
-
         $app['config']->set('app.key', Str::random(32));
         $app['config']->set('session.driver', 'array');
         $app['config']->set('session.expire_on_close', true);
