db3c0ad834b2e69e6f8a87d7120154c8c79f8fd1
Niels <niels@niels-online.net>
2022-06-15 21:41:36 +0200

    Update and add tests to allow for an array of keys

--- tests/View/Blade/BladeErrorTest.php
+++ tests/View/Blade/BladeErrorTest.php
@@ -13,14 +13,19 @@ class BladeErrorTest extends AbstractBladeTestCase
         $expected = '
 <?php $__errorArgs = [\'email\'];
 $__bag = $errors->getBag($__errorArgs[1] ?? \'default\');
-if ($__bag->has($__errorArgs[0])) :
+$__errorArgs[0] = is_array($__errorArgs[0]) ? $__errorArgs[0] : [$__errorArgs[0]];
+if ($__bag->hasAny($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
-$message = $__bag->first($__errorArgs[0]); ?>
+$message = false;
+$__i = 0;
+while (empty($message)) {
+     $message = $__bag->first($__errorArgs[0][$__i++]);
+} ?>
     <span><?php echo e($message); ?></span>
 <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
-unset($__errorArgs, $__bag); ?>';
+unset($__errorArgs, $__bag, $__i); ?>';
 
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
@@ -34,14 +39,71 @@ unset($__errorArgs, $__bag); ?>';
         $expected = '
 <?php $__errorArgs = [\'email\', \'customBag\'];
 $__bag = $errors->getBag($__errorArgs[1] ?? \'default\');
-if ($__bag->has($__errorArgs[0])) :
+$__errorArgs[0] = is_array($__errorArgs[0]) ? $__errorArgs[0] : [$__errorArgs[0]];
+if ($__bag->hasAny($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
-$message = $__bag->first($__errorArgs[0]); ?>
+$message = false;
+$__i = 0;
+while (empty($message)) {
+     $message = $__bag->first($__errorArgs[0][$__i++]);
+} ?>
     <span><?php echo e($message); ?></span>
 <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>';
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+    }
+
+    public function testErrorsWithMultipleKeysAreCompiled()
+    {
+        $string = '
+@error([\'email\', \'name\'])
+    <span>{{ $message }}</span>
+@enderror';
+        $expected = '
+<?php $__errorArgs = [[\'email\', \'name\']];
+$__bag = $errors->getBag($__errorArgs[1] ?? \'default\');
+$__errorArgs[0] = is_array($__errorArgs[0]) ? $__errorArgs[0] : [$__errorArgs[0]];
+if ($__bag->hasAny($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = false;
+$__i = 0;
+while (empty($message)) {
+     $message = $__bag->first($__errorArgs[0][$__i++]);
+} ?>
+    <span><?php echo e($message); ?></span>
+<?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag, $__i); ?>';
+
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+    }
+
+    public function testErrorsWithMultipleKeysWithBagsAreCompiled()
+    {
+        $string = '
+@error([\'email\', \'name\'], \'customBag\')
+    <span>{{ $message }}</span>
+@enderror';
+        $expected = '
+<?php $__errorArgs = [[\'email\', \'name\'], \'customBag\'];
+$__bag = $errors->getBag($__errorArgs[1] ?? \'default\');
+$__errorArgs[0] = is_array($__errorArgs[0]) ? $__errorArgs[0] : [$__errorArgs[0]];
+if ($__bag->hasAny($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = false;
+$__i = 0;
+while (empty($message)) {
+     $message = $__bag->first($__errorArgs[0][$__i++]);
+} ?>
+    <span><?php echo e($message); ?></span>
+<?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag, $__i); ?>';
+
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 }
