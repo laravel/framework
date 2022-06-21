55785d478770e7797ef52b373cc08d84d5b44e1d
Christopher Kelker <chris_j_kelker@hotmail.com>
2022-06-21 15:09:43 +0100

    contains stringable doesn't contain the ignoreCase parameter (#42893)
    Co-authored-by: Christopher Kelker <chris@wecreatedigital.co.uk>

--- src/Illuminate/Support/Stringable.php
+++ src/Illuminate/Support/Stringable.php
@@ -167,22 +167,24 @@ class Stringable implements JsonSerializable
      * Determine if a given string contains a given substring.
      *
      * @param  string|string[]  $needles
+     * @param  bool  $ignoreCase
      * @return bool
      */
-    public function contains($needles)
+    public function contains($needles, $ignoreCase = false)
     {
-        return Str::contains($this->value, $needles);
+        return Str::contains($this->value, $needles, $ignoreCase);
     }
 
     /**
      * Determine if a given string contains all array values.
      *
      * @param  array  $needles
+     * @param  bool  $ignoreCase
      * @return bool
      */
-    public function containsAll(array $needles)
+    public function containsAll(array $needles, $ignoreCase = false)
     {
-        return Str::containsAll($this->value, $needles);
+        return Str::containsAll($this->value, $needles, $ignoreCase);
     }
 
     /**
