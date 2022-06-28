587d7e0de4ab1e95a342c3b52c10806922bc99e3
mateusjunges <mateus@junges.dev>
2022-06-28 09:59:07 -0300

    append false response to responses array

--- src/Illuminate/Events/Dispatcher.php
+++ src/Illuminate/Events/Dispatcher.php
@@ -259,6 +259,7 @@ class Dispatcher implements DispatcherContract
             // the event to any further listeners down in the chain, else we keep on
             // looping through the listeners and firing every one in our sequence.
             if ($response === false) {
+                $responses[] = false;
                 break;
             }
 
