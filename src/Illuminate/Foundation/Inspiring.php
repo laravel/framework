9544b13b03871e560689aa920743426801aa57b0
Mücahit Bayraktar <immucahit@users.noreply.github.com>
2022-06-30 20:21:56 +0300

    quote from Mustafa Kemal Atatürk (#43022)

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -92,6 +92,7 @@ class Inspiring
             'Life is available only in the present moment. - Thich Nhat Hanh',
             'The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh',
             'Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie',
+            'The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk',
         ])->random();
     }
 }
