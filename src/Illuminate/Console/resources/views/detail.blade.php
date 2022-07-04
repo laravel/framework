3a1259d0df3fc0629f658a277fa872dbecdb3228
Nuno Maduro <enunomaduro@gmail.com>
2022-07-04 12:09:09 +0100

    Improves Detail output

--- src/Illuminate/Console/resources/views/detail.blade.php
+++ src/Illuminate/Console/resources/views/detail.blade.php
@@ -1,9 +1,11 @@
-<div class="flex space-x-1">
-    <span class="px-2 text-yellow uppercase font-bold">
+<div class="flex mx-2">
+    <span>
         {{ $left }}
     </span>
-    <span class="flex-1 content-repeat-[.] text-gray"></span>
-    <span>
-        {{ $right }}
-    </span>
+    <span class="flex-1 content-repeat-[.] text-gray ml-1"></span>
+    @if ($right !== '')
+        <span class="ml-1">
+            {{ $right }}
+        </span>
+    @endif
 </div>
