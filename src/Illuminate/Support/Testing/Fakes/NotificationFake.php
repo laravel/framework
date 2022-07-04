42107327608a58342ca05101e2074c56bd9e081b
Faisal Abuzaid <74982616+faisalabuzaid@users.noreply.github.com>
2022-07-04 19:36:45 +0300

    Fix pushing notification into notification when notifiable channels is empty

--- src/Illuminate/Support/Testing/Fakes/NotificationFake.php
+++ src/Illuminate/Support/Testing/Fakes/NotificationFake.php
@@ -320,12 +320,12 @@ class NotificationFake implements NotificationDispatcher, NotificationFactory
                     $notifiableChannels,
                     fn ($channel) => $notification->shouldSend($notifiable, $channel) !== false
                 );
-
-                if (empty($notifiableChannels)) {
-                    continue;
-                }
             }
 
+            if (empty($notifiableChannels)) {
+                    continue;
+            }
+            
             $this->notifications[get_class($notifiable)][$notifiable->getKey()][get_class($notification)][] = [
                 'notification' => $notification,
                 'channels' => $notifiableChannels,
