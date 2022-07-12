8ce9c30a5ec423ce7d0434cc3609fd0fd27cd77b
Nuno Maduro <enunomaduro@gmail.com>
2022-07-12 10:44:39 +0100

    Adds missing method on interface and tests

--- src/Illuminate/Console/Contracts/NewLineAware.php
+++ src/Illuminate/Console/Contracts/NewLineAware.php
@@ -4,5 +4,10 @@ namespace Illuminate\Console\Contracts;
 
 interface NewLineAware
 {
-    // ...
+    /**
+     * Whether a newline has already been written.
+     *
+     * @return bool
+     */
+    public function newLineWritten();
 }
