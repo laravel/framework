c3f5a803bb477066cd4b00675ff0207193ef660e
AbdlrahmanSaberAbdo <abdlrahmansaber652@gmail.com>
2022-07-15 09:09:12 +0200

    put data type of the return value to 'has' method, According to psr/container package

--- src/Illuminate/Container/Container.php
+++ src/Illuminate/Container/Container.php
@@ -191,7 +191,7 @@ class Container implements ArrayAccess, ContainerContract
      *
      * @return bool
      */
-    public function has($id)
+    public function has($id):bool
     {
         return $this->bound($id);
     }
