ee95ab83ea5f4c9ba7a7d711e64c4002e8f89c28
Hossein Hezami <hossein.hezami@gmail.com>
2022-07-17 15:59:50 +0430

    [9.x] Add inspiring quote by Henry Ford

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -95,6 +95,7 @@ class Inspiring
             'The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk',
             'Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead',
             'You must be the change you wish to see in the world. - Mahatma Gandhi',
+            'Failure is simply the opportunity to begin again, this time more intelligently. - Henry Ford',
         ])->map(fn ($quote) => static::formatForConsole($quote))->random();
     }
 
