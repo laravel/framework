ac091b55c817759eab31d45ae8a3baef8801af15
Lazizbek <lazerg2@gmail.com>
2022-07-18 11:14:54 +0500

    [9.x] Add famous quote by Benjamin Franklin

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -95,6 +95,7 @@ class Inspiring
             'The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk',
             'Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead',
             'You must be the change you wish to see in the world. - Mahatma Gandhi',
+            'Tell me and I forget. Teach me and I remember. Involve me and I learn. - Benjamin Franklin',
         ])->map(fn ($quote) => static::formatForConsole($quote))->random();
     }
 
