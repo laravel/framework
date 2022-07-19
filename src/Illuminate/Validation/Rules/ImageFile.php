ecaa880864107f973d19e5f4dd0c283cea4239bc
luke <lukeraymonddowning@gmail.com>
2022-07-19 09:32:49 +0100

    Adds `File` validation rule.

---
+++ src/Illuminate/Validation/Rules/ImageFile.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Illuminate\Validation\Rules;
+
+class ImageFile extends File
+{
+    public function __construct()
+    {
+        $this->rules('image');
+    }
+
+    /**
+     * The dimension constraints for the uploaded file.
+     *
+     * @param Dimensions $dimensions
+     */
+    public function dimensions($dimensions)
+    {
+        $this->rules($dimensions);
+
+        return $this;
+    }
+}
