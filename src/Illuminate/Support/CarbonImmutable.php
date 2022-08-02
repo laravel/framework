0960c11cac588ef3faf02840c225129694d2110b
Mateus Guimarães <mateus.jatene@gmail.com>
2022-08-01 22:49:01 -0300

    Add CarbonImmutable class

---
+++ src/Illuminate/Support/CarbonImmutable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Illuminate\Support;
+
+use Carbon\Carbon as BaseCarbon;
+use Carbon\CarbonImmutable as BaseCarbonImmutable;
+use Illuminate\Support\Traits\Conditionable;
+
+class CarbonImmutable extends BaseCarbonImmutable
+{
+    use Conditionable;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function setTestNow($testNow = null)
+    {
+        BaseCarbon::setTestNow($testNow);
+        BaseCarbonImmutable::setTestNow($testNow);
+    }
+}
