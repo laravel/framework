1f34ae2247fbc1db0390f647709c670c3417f141
Luke Towers <github@luketowers.ca>
2022-08-05 00:41:05 -0600

    Code styling fix

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -418,7 +418,7 @@ class Str
 
         return preg_match('/^[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/iD', $value) > 0;
     }
-    
+
     /**
      * Join items into a human readable list (e.g. "one, two, three, and four")
      * Uses different glue strings when there are only two elements and for
