b783602bdaeb1fc86d0240457dc612fb3f482e67
Luke Towers <github@luketowers.ca>
2022-08-05 00:46:55 -0600

    Add tests for Str::join()

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -425,6 +425,17 @@ class SupportStrTest extends TestCase
         $this->assertFalse(Str::isJson([]));
     }
 
+    public function testJoin()
+    {
+        $this->assertSame('bob', Str::join(['bob']));
+        $this->assertSame('bob and joe', Str::join(['bob', 'joe']));
+        $this->assertSame('bob, joe, and sally', Str::join(['bob', 'joe', 'sally']));
+        $this->assertSame('bob, joe and sally', Str::join(['bob', 'joe', 'sally'], ', ', ' and '));
+        $this->assertSame('bob, joe, and sally', Str::join(['bob', 'joe', 'sally']));
+        $this->assertSame('bob or joe', Str::join(['bob', 'joe'], ', ', ', or ', ' or '));
+        $this->assertSame('bob; joe; or sally', Str::join(['bob', 'joe', 'sally'], '; ', '; or '));
+    }
+
     public function testKebab()
     {
         $this->assertSame('laravel-php-framework', Str::kebab('LaravelPhpFramework'));
