5fcaec069f8a61c7bd6d023f49c2734f27f43c56
D. Nagy Gergő <hello@iamgergo.com>
2022-08-10 07:49:22 +0200

    Update facade

--- src/Illuminate/Support/Facades/Vite.php
+++ src/Illuminate/Support/Facades/Vite.php
@@ -2,13 +2,19 @@
 
 namespace Illuminate\Support\Facades;
 
+use Illuminate\Foundation\Vite as Factory;
+
 /**
  * @method static string useCspNonce(?string $nonce = null)
  * @method static string|null cspNonce()
  * @method static string asset(string $asset, string|null $buildDirectory)
+ * @method static string toHtml()
  * @method static \Illuminate\Foundation\Vite useIntegrityKey(string|false $key)
  * @method static \Illuminate\Foundation\Vite useScriptTagAttributes(callable|array $callback)
  * @method static \Illuminate\Foundation\Vite useStyleTagAttributes(callable|array $callback)
+ * @method static \Illuminte\Foundation\Vite withEntryPoints(array $entryPoints)
+ * @method static \Illuminte\Foundation\Vite useHotFile(string $path)
+ * @method static \Illuminte\Foundation\Vite useBuildDirectory(string $path)
  *
  * @see \Illuminate\Foundation\Vite
  */
@@ -21,6 +27,6 @@ class Vite extends Facade
      */
     protected static function getFacadeAccessor()
     {
-        return \Illuminate\Foundation\Vite::class;
+        return Factory::class;
     }
 }
