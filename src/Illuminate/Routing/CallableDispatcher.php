5b3b394de19069c41971b88fa2e404e41c3e018c
Tim MacDonald <hello@timacdonald.me>
2022-08-11 13:58:13 +1000

    bind the callable dispatcher to the container

--- src/Illuminate/Routing/CallableDispatcher.php
+++ src/Illuminate/Routing/CallableDispatcher.php
@@ -2,12 +2,31 @@
 
 namespace Illuminate\Routing;
 
+use Illuminate\Container\Container;
 use Illuminate\Routing\Contracts\CallableDispatcher as CallableDispatcherContract;
 use ReflectionFunction;
 
 class CallableDispatcher implements CallableDispatcherContract
 {
     use RouteDependencyResolverTrait;
+
+    /**
+     * The container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
+
+    /**
+     * Create a new controller dispatcher instance.
+     *
+     * @param  \Illuminate\Container\Container  $container
+     * @return void
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
 
     /**
      * Dispatch a request to a given callable.
@@ -18,8 +37,20 @@ class CallableDispatcher implements CallableDispatcherContract
      */
     public function dispatch(Route $route, $callable)
     {
-        return $callable(...array_values($this->resolveMethodDependencies(
+        return $callable(...array_values($this->resolveArguments($route, $callable)));
+    }
+
+    /**
+     * Resolve the arguments for the callable.
+     *
+     * @param  \Illuminate\Routing\Route  $route
+     * @param  callable  $callable
+     * @return array
+     */
+    protected function resolveArguments(Route $route, $callable)
+    {
+        return $this->resolveMethodDependencies(
             $route->parametersWithoutNulls(), new ReflectionFunction($callable)
-        )));
+        );
     }
 }
