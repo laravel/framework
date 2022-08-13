7d945e1fafe7e45f92cfbb45e5ba74b780f59bae
Seth Phat <phattranminh96@gmail.com>
2022-08-13 18:14:47 +0700

    Removed class exists check

--- src/Illuminate/Database/Migrations/MigrationCreator.php
+++ src/Illuminate/Database/Migrations/MigrationCreator.php
@@ -97,10 +97,6 @@ class MigrationCreator
                 $this->files->requireOnce($migrationFile);
             }
         }
-
-        if (class_exists($className = $this->getClassName($name))) {
-            throw new InvalidArgumentException("A {$className} class already exists.");
-        }
     }
 
     /**
