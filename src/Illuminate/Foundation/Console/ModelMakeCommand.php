e57fd4d5c298b775b5c629cc7b3c818e544dadb0
Nikolay Nikolaev <nikolaynikolaevvnn@gmail.com>
2022-08-20 23:35:21 +0200

    Mention form request classes being created by the make:model -a command (#43790)
    * Mention form request classes being created by the make:model -a command
    
    * Update ModelMakeCommand.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Foundation/Console/ModelMakeCommand.php
+++ src/Illuminate/Foundation/Console/ModelMakeCommand.php
@@ -218,7 +218,7 @@ class ModelMakeCommand extends GeneratorCommand
     protected function getOptions()
     {
         return [
-            ['all', 'a', InputOption::VALUE_NONE, 'Generate a migration, seeder, factory, policy, and resource controller for the model'],
+            ['all', 'a', InputOption::VALUE_NONE, 'Generate a migration, seeder, factory, policy, resource controller, and form request classes for the model'],
             ['controller', 'c', InputOption::VALUE_NONE, 'Create a new controller for the model'],
             ['factory', 'f', InputOption::VALUE_NONE, 'Create a new factory for the model'],
             ['force', null, InputOption::VALUE_NONE, 'Create the class even if the model already exists'],
