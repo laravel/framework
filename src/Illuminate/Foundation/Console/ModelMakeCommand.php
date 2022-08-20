067ed8db0c2f6b08bdd4faa145c2ee86f0ed4c92
Nikolay Nikolaev <nikolaynikolaevvnn@gmail.com>
2022-08-21 00:35:39 +0200

    Specify that make:model --api command creates a resource controller

--- src/Illuminate/Foundation/Console/ModelMakeCommand.php
+++ src/Illuminate/Foundation/Console/ModelMakeCommand.php
@@ -228,7 +228,7 @@ class ModelMakeCommand extends GeneratorCommand
             ['seed', 's', InputOption::VALUE_NONE, 'Create a new seeder for the model'],
             ['pivot', 'p', InputOption::VALUE_NONE, 'Indicates if the generated model should be a custom intermediate table model'],
             ['resource', 'r', InputOption::VALUE_NONE, 'Indicates if the generated controller should be a resource controller'],
-            ['api', null, InputOption::VALUE_NONE, 'Indicates if the generated controller should be an API controller'],
+            ['api', null, InputOption::VALUE_NONE, 'Indicates if the generated controller should be an API resource controller'],
             ['requests', 'R', InputOption::VALUE_NONE, 'Create new form request classes and use them in the resource controller'],
         ];
     }
