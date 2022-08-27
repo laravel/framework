a85515b6350714509d2c5f1a5a4ecb74ae616e7d
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-08-27 19:34:45 +0200

    Update Model.php

--- types/Database/Eloquent/Model.php
+++ types/Database/Eloquent/Model.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Database\Eloquent\Model;
-
 use function PHPStan\Testing\assertType;
 
 $factory = User::factory();
 assertType('Illuminate\Database\Eloquent\Factories\Factory<User>', $factory);
-
-Model::preventLazyLoading();
