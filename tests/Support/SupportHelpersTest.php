8a02e8fc5ad22e47cc0ebec075d6ff7edf0e7b8c
Amir Moharami Fard <a.moharami@gmail.com>
2022-08-29 18:27:59 +0430

    [9.x] Improve test for blank in Helpers (#43887)
    * [9.x] Improve test for blank in Helpers
    
    * [9.x] Improve test for blank in Helpers
    
    * [9.x] Improve test for blank in Helpers
    
    fix style code

--- tests/Support/SupportHelpersTest.php
+++ tests/Support/SupportHelpersTest.php
@@ -34,6 +34,21 @@ class SupportHelpersTest extends TestCase
         $this->assertEquals($str, e($html));
     }
 
+    public function testBlank()
+    {
+        $this->assertTrue(blank(null));
+        $this->assertTrue(blank(''));
+        $this->assertTrue(blank('  '));
+        $this->assertFalse(blank(10));
+        $this->assertFalse(blank(true));
+        $this->assertFalse(blank(false));
+        $this->assertFalse(blank(0));
+        $this->assertFalse(blank(0.0));
+
+        $object = new SupportTestCountable();
+        $this->assertTrue(blank($object));
+    }
+
     public function testClassBasename()
     {
         $this->assertSame('Baz', class_basename('Foo\Bar\Baz'));
