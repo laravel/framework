f5e6d8cfebe9e56d43e5cc217544f4cf488771a1
Matthias Nagel <matthias.h.nagel@posteo.de>
2022-09-03 11:51:06 +0200

    Add previous exception

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -536,7 +536,7 @@ class Handler implements ExceptionHandlerContract
         }
 
         if (! $this->isHttpException($e)) {
-            $e = new HttpException(500, $e->getMessage());
+            $e = new HttpException(500, $e->getMessage(), $e);
         }
 
         return $this->toIlluminateResponse(
