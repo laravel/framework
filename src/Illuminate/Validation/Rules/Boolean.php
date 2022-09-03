371cb8bd0990af36d8283c00b26ab9b3696007bb
Andrey Helldar <helldar@dragon-code.pro>
2022-09-03 15:44:33 +0300

    Fixed code-style

--- src/Illuminate/Validation/Rules/Boolean.php
+++ src/Illuminate/Validation/Rules/Boolean.php
@@ -21,9 +21,8 @@ class Boolean implements Rule
     /**
      * Determine if the validation rule passes.
      *
-     * @param string $attribute
-     * @param mixed $value
-     *
+     * @param  string  $attribute
+     * @param  mixed  $value
      * @return bool
      */
     public function passes($attribute, $value): bool
