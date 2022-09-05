791471be4f33d9aeba73d481f2efdee45323093e
JirakLu <jirak.lukas@gmail.com>
2022-09-05 14:50:05 +0200

    Fixed route prefix formatting in RouteGroup::formatPrefix(). Added tests for this issue.

--- src/Illuminate/Routing/RouteGroup.php
+++ src/Illuminate/Routing/RouteGroup.php
@@ -66,9 +66,9 @@ class RouteGroup
         $old = $old['prefix'] ?? '';
 
         if ($prependExistingPrefix) {
-            return isset($new['prefix']) ? trim($old, '/').'/'.trim($new['prefix'], '/') : $old;
+            return trim(isset($new['prefix']) ? trim($old, '/').'/'.trim($new['prefix'], '/') : $old, '/');
         } else {
-            return isset($new['prefix']) ? trim($new['prefix'], '/').'/'.trim($old, '/') : $old;
+            return trim(isset($new['prefix']) ? trim($new['prefix'], '/').'/'.trim($old, '/') : $old, '/');
         }
     }
 
