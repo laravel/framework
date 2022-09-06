aaa3a18d900a6f6b36c6fdd6aa6446a771356d83
Nuno Maduro <enunomaduro@gmail.com>
2022-09-06 08:51:07 +0100

    Adds `Facade::fake`

--- src/Illuminate/Support/Facades/Process.php
+++ src/Illuminate/Support/Facades/Process.php
@@ -19,6 +19,17 @@ use Illuminate\Console\Process\Factory;
 class Process extends Facade
 {
     /**
+     * Register a stub callable that will intercept processes and be able to return stub processes results.
+     *
+     * @param  (iterable<string, callable(\Symfony\Component\Process\Process): \Illuminate\Console\Contracts\ProcessResult>)|(callable(\Symfony\Component\Process\Process): \Illuminate\Console\Contracts\ProcessResult)|null  $callback
+     * @return \Illuminate\Console\Process\Factory
+     */
+    public static function fake($callback = null)
+    {
+        return tap(static::getFacadeRoot(), fn ($fake) => static::swap($fake->fake($callback)));
+    }
+
+    /**
      * Get the registered name of the component.
      *
      * @return string
