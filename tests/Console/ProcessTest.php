4b11e91424a29d69db12172c8082f33529d7d45c
Nuno Maduro <enunomaduro@gmail.com>
2022-09-06 09:22:55 +0100

    Fixes test on windows

--- tests/Console/ProcessTest.php
+++ tests/Console/ProcessTest.php
@@ -173,7 +173,7 @@ class ProcessTest extends TestCase
     {
         $exception = null;
 
-        $result = $this->factory->path(__DIR__)->timeout(0.1)->run($this->sleep(1));
+        $result = $this->factory->path(__DIR__)->timeout(1)->run($this->sleep(3));
 
         $this->assertSame(0.1, $result->process()->getTimeout());
 
