ff453e6028a6be8705935cc2ec913988f26fa8b4
Taylor Otwell <taylor@laravel.com>
2022-09-12 14:04:08 -0500

    formatting

--- src/Illuminate/Support/Testing/Fakes/BusFake.php
+++ src/Illuminate/Support/Testing/Fakes/BusFake.php
@@ -656,16 +656,6 @@ class BusFake implements QueueingDispatcher
     }
 
     /**
-     * Create a new, empty batch for testing.
-     *
-     * @return \Illuminate\Bus\Batch
-     */
-    public function fakeBatch()
-    {
-        return $this->batch([])->dispatch();
-    }
-
-    /**
      * Record the fake pending batch dispatch.
      *
      * @param  \Illuminate\Bus\PendingBatch  $pendingBatch
