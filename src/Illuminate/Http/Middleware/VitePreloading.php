2ef7dec921e9d45c14788e56b215310369078d99
Tim MacDonald <hello@timacdonald.me>
2022-09-12 14:01:21 +1000

    support (module)preloading with Vite

---
+++ src/Illuminate/Http/Middleware/VitePreloading.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Illuminate\Http\Middleware;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Vite;
+
+class VitePreloading
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     */
+    public function handle($request, $next)
+    {
+        return tap($next($request), function ($response) {
+            if (Vite::preloadedAssets() !== []) {
+                $response->header('Link', Collection::make(Vite::preloadedAssets())
+                    ->map(fn ($attributes, $url) => "<{$url}>; ".implode('; ', $attributes))
+                    ->sortByDesc(fn ($link) => str_contains($link, 'rel="preload"'))
+                    ->join(', '));
+            }
+        });
+    }
+}
---
+++ tests/Http/Middleware/VitePreloadingTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Illuminate\Tests\Http\Middleware;
+
+use Illuminate\Container\Container;
+use Illuminate\Foundation\Vite;
+use Illuminate\Http\Middleware\VitePreloading;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Facade;
+use PHPUnit\Framework\TestCase;
+
+class VitePreloadingTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        Facade::setFacadeApplication(null);
+        Facade::clearResolvedInstances();
+    }
+
+    public function testItDoesNotSetLinkTagWhenNoTagsHaveBeenPreloaded()
+    {
+        $app = new Container();
+        $app->instance(Vite::class, new class extends Vite
+        {
+            protected $preloadedAssets = [];
+        });
+        Facade::setFacadeApplication($app);
+
+        $response = (new VitePreloading)->handle(new Request, function () {
+            return new Response('Hello Laravel');
+        });
+
+        $this->assertNull($response->headers->get('Link'));
+    }
+
+    public function testItAddsPreloadLinkHeader()
+    {
+        $app = new Container();
+        $app->instance(Vite::class, new class extends Vite
+        {
+            protected $preloadedAssets = [
+                'https://laravel.com/app.js' => [
+                    'rel="modulepreload"',
+                    'foo="bar"',
+                ],
+            ];
+        });
+        Facade::setFacadeApplication($app);
+
+        $response = (new VitePreloading)->handle(new Request, function () {
+            return new Response('Hello Laravel');
+        });
+
+        $this->assertSame(
+            $response->headers->get('Link'),
+            '<https://laravel.com/app.js>; rel="modulepreload"; foo="bar"'
+        );
+    }
+
+    public function testItPrioritizesCss()
+    {
+        $app = new Container();
+        $app->instance(Vite::class, new class extends Vite
+        {
+            protected $preloadedAssets = [
+                'https://laravel.com/app.js' => [
+                    'rel="modulepreload"',
+                    'foo="bar"',
+                ],
+                'https://laravel.com/app.css' => [
+                    'rel="preload"',
+                    'bar="baz"',
+                ],
+            ];
+        });
+        Facade::setFacadeApplication($app);
+
+        $response = (new VitePreloading)->handle(new Request, function () {
+            return new Response('Hello Laravel');
+        });
+
+        $this->assertSame(
+            $response->headers->get('Link'),
+            '<https://laravel.com/app.css>; rel="preload"; bar="baz", <https://laravel.com/app.js>; rel="modulepreload"; foo="bar"'
+        );
+    }
+}
