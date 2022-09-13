93ff1bcda82c69ee2e3ce98e32ffb23186f858bc
Dries Vints <dries@vints.io>
2022-09-13 21:29:14 +0200

    [9.x] UUID and ULID support for Eloquent (#44074)
    * Eloquen UUID & ULID
    
    * wip
    
    * wip
    
    * Update tests/Integration/Database/EloquentPrimaryUlidTest.php
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * tweaking
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -14,6 +14,7 @@ use Ramsey\Uuid\Codec\TimestampFirstCombCodec;
 use Ramsey\Uuid\Generator\CombGenerator;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidFactory;
+use Symfony\Component\Uid\Ulid;
 use Traversable;
 use voku\helper\ASCII;
 
@@ -422,6 +423,21 @@ class Str
         }
 
         return preg_match('/^[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/iD', $value) > 0;
+    }
+
+    /**
+     * Determine if a given string is a valid ULID.
+     *
+     * @param  string  $value
+     * @return bool
+     */
+    public static function isUlid($value)
+    {
+        if (! is_string($value)) {
+            return false;
+        }
+
+        return Ulid::isValid($value);
     }
 
     /**
@@ -1304,6 +1320,16 @@ class Str
     public static function createUuidsNormally()
     {
         static::$uuidFactory = null;
+    }
+
+    /**
+     * Generate a ULID.
+     *
+     * @return \Symfony\Component\Uid\Ulid
+     */
+    public static function ulid()
+    {
+        return new Ulid();
     }
 
     /**
