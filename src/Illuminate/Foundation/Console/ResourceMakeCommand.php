55d29a818323b13aee60c60866995ff41dad2e0a
Taylor Otwell <taylor@laravel.com>
2022-09-13 08:45:27 -0500

    Update ResourceMakeCommand.php

--- src/Illuminate/Foundation/Console/ResourceMakeCommand.php
+++ src/Illuminate/Foundation/Console/ResourceMakeCommand.php
@@ -110,8 +110,8 @@ class ResourceMakeCommand extends GeneratorCommand
     protected function getOptions()
     {
         return [
-            ['collection', 'c', InputOption::VALUE_NONE, 'Create a resource collection'],
             ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the resource already exists'],
+            ['collection', 'c', InputOption::VALUE_NONE, 'Create a resource collection'],
         ];
     }
 }
