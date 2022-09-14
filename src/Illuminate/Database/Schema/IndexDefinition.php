797e660a72f6101c69dc06d7abd827a6b24d44da
Markus Koch <markuskooche@icloud.com>
2022-09-14 13:33:15 +0200

    Add support for unique deferrable initial deferred constants under PostgreSQL

--- src/Illuminate/Database/Schema/IndexDefinition.php
+++ src/Illuminate/Database/Schema/IndexDefinition.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Fluent;
 /**
  * @method $this algorithm(string $algorithm) Specify an algorithm for the index (MySQL/PostgreSQL)
  * @method $this language(string $language) Specify a language for the full text index (PostgreSQL)
+ * @method $this deferrable(bool $value = true) Set the unique index as deferrable (PostgreSQL)
+ * @method $this initiallyImmediate(bool $value = true) Set the default time to check the constraint (PostgreSQL)
  */
 class IndexDefinition extends Fluent
 {
