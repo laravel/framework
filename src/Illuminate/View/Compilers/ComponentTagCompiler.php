717618a0d32b5f8aa933dad551cf1a6300839ac9
Pascal Baljet <pascal@pascalbaljet.nl>
2022-09-21 17:39:13 +0200

    [9.x] Short attribute syntax for Blade Components (#44217)
    * Short attributes syntax
    
    * Simplify regex
    
    * Update ComponentTagCompiler.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/View/Compilers/ComponentTagCompiler.php
+++ src/Illuminate/View/Compilers/ComponentTagCompiler.php
@@ -120,6 +120,10 @@ class ComponentTagCompiler
                             )
                             |
                             (?:
+                                (\:\\\$)(\w+)
+                            )
+                            |
+                            (?:
                                 [\w\-:.@]+
                                 (
                                     =
@@ -498,6 +502,7 @@ class ComponentTagCompiler
      */
     protected function getAttributesFromAttributeString(string $attributeString)
     {
+        $attributeString = $this->parseShortAttributeSyntax($attributeString);
         $attributeString = $this->parseAttributeBag($attributeString);
         $attributeString = $this->parseComponentTagClassStatements($attributeString);
         $attributeString = $this->parseBindAttributes($attributeString);
@@ -548,6 +553,21 @@ class ComponentTagCompiler
 
             return [$attribute => $value];
         })->toArray();
+    }
+
+    /**
+     * Parses a short attribute syntax like :$foo into a fully-qualified syntax like :foo="$foo".
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function parseShortAttributeSyntax(string $value)
+    {
+        $pattern = "/\:\\\$(\w+)/x";
+
+        return preg_replace_callback($pattern, function (array $matches) {
+            return ":{$matches[1]}=\"\${$matches[1]}\"";
+        }, $value);
     }
 
     /**
