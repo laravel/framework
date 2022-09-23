b4293d7c18b08b363ac0af64ec04fb1d559b4698
Taylor Otwell <taylor@laravel.com>
2022-09-23 16:17:59 -0500

    add benchmark utility class

---
+++ src/Illuminate/Support/Benchmark.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Illuminate\Support;
+
+use Closure;
+use Illuminate\Support\Arr;
+
+class Benchmark
+{
+    /**
+     * Measure a callable or array of callables over the given number of iterations.
+     *
+     * @param  \Closure|array  $benchmarkables
+     * @param  int  $iterations
+     * @return array|float
+     */
+    public static function measure(Closure|array $benchmarkables, int $iterations = 1): array|float
+    {
+        return collect(Arr::wrap($benchmarkables))->map(function ($callback) use ($iterations) {
+            return collect(range(1, $iterations))->map(function () use ($callback) {
+                gc_collect_cycles();
+
+                $start = hrtime(true);
+
+                $callback();
+
+                return (hrtime(true) - $start) / 1000000;
+            })->average();
+        })->when(
+            $benchmarkables instanceof Closure,
+            fn ($c) => $c->first(),
+            fn ($c) => $c->all(),
+        );
+    }
+
+    /**
+     * Measure a callable or array of callables over the given number of iterations, then die and dump.
+     *
+     * @param  \Closure|array  $benchmarkables
+     * @param  int  $iterations
+     * @return void
+     */
+    public static function dd(Closure|array $benchmarkables, int $iterations = 1): void
+    {
+        dd(static::measure($benchmarkables, $iterations));
+    }
+}
