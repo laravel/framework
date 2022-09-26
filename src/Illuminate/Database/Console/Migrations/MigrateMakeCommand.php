daf7a2f5a0236130b5e4cf4917fd5045d867006f
nothehi <nodehi7813@gmail.com>
2022-09-26 15:31:53 +0330

    Improve option description

--- src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
+++ src/Illuminate/Database/Console/Migrations/MigrateMakeCommand.php
@@ -18,7 +18,7 @@ class MigrateMakeCommand extends BaseCommand
         {--create= : The table to be created}
         {--table= : The table to migrate}
         {--path= : The location where the migration file should be created}
-        {--before= : The name of table the migration file should be created before that}
+        {--before= : The name of the table that this migration should precede}
         {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
         {--fullpath : Output the full path of the migration}';
 
