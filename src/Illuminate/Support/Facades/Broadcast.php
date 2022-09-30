34c1e929b9182ab8b601cbdc2cd1204474e8866c
Andrew <browner12@gmail.com>
2022-09-29 22:50:25 -0500

    remove double spaces
    super minor formatting

--- src/Illuminate/Support/Facades/Broadcast.php
+++ src/Illuminate/Support/Facades/Broadcast.php
@@ -5,7 +5,7 @@ namespace Illuminate\Support\Facades;
 use Illuminate\Contracts\Broadcasting\Factory as BroadcastingFactoryContract;
 
 /**
- * @method static \Illuminate\Broadcasting\Broadcasters\Broadcaster channel(string $channel, callable|string  $callback, array $options = [])
+ * @method static \Illuminate\Broadcasting\Broadcasters\Broadcaster channel(string $channel, callable|string $callback, array $options = [])
  * @method static \Illuminate\Broadcasting\BroadcastManager socket($request = null)
  * @method static \Illuminate\Contracts\Broadcasting\Broadcaster connection($name = null);
  * @method static mixed auth(\Illuminate\Http\Request $request)
