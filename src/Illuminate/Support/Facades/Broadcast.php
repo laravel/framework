c9faf134fb096fa84644ef2487fced3ba5505ff2
Andrew <browner12@gmail.com>
2022-09-29 23:04:49 -0500

    correctly reference class
    classes in the root namespace should be prefixed with a slash

--- src/Illuminate/Support/Facades/Broadcast.php
+++ src/Illuminate/Support/Facades/Broadcast.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Broadcasting\Factory as BroadcastingFactoryContract;
  * @method static \Illuminate\Broadcasting\BroadcastManager socket($request = null)
  * @method static \Illuminate\Contracts\Broadcasting\Broadcaster connection($name = null);
  * @method static mixed auth(\Illuminate\Http\Request $request)
- * @method static void resolveAuthenticatedUserUsing(Closure $callback)
+ * @method static void resolveAuthenticatedUserUsing(\Closure $callback)
  * @method static void routes(array $attributes = null)
  *
  * @see \Illuminate\Contracts\Broadcasting\Factory
