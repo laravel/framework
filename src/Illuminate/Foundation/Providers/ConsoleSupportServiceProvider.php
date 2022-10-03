831c79dab6eb615d0e44afc0a56fa13878ac00d0
Jeroen Deviaene <jeroen@deviaene.eu>
2022-10-03 18:16:42 +0200

    Remove unused import statement

--- src/Illuminate/Foundation/Providers/ConsoleSupportServiceProvider.php
+++ src/Illuminate/Foundation/Providers/ConsoleSupportServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Illuminate\Foundation\Providers;
 
 use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Database\MigrationServiceProvider;
 use Illuminate\Support\AggregateServiceProvider;
 
 class ConsoleSupportServiceProvider extends AggregateServiceProvider implements DeferrableProvider
