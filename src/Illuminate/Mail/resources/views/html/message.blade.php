602c51130460cd52ee30b8ca90273b36400b2431
Tim MacDonald <hello@timacdonald.me>
2022-10-10 16:44:53 +1100

    fix attributes

--- src/Illuminate/Mail/resources/views/html/message.blade.php
+++ src/Illuminate/Mail/resources/views/html/message.blade.php
@@ -1,7 +1,7 @@
 <x-mail::layout>
 {{-- Header --}}
 <x-slot:header>
-<x-mail::header url="config('app.url')">
+<x-mail::header :url="config('app.url')">
 {{ config('app.name') }}
 </x-mail::header>
 </x-slot:header>
