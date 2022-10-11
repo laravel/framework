c8097a41063e9a0716bf6bf3d447045475d252cf
Nuno Maduro <enunomaduro@gmail.com>
2022-10-11 19:34:55 +0100

    Adds types casts `get` and `set` methods

--- src/Illuminate/Contracts/Database/Eloquent/CastsAttributes.php
+++ src/Illuminate/Contracts/Database/Eloquent/CastsAttributes.php
@@ -2,27 +2,19 @@
 
 namespace Illuminate\Contracts\Database\Eloquent;
 
+use Illuminate\Database\Eloquent\Model;
+
 interface CastsAttributes
 {
     /**
      * Transform the attribute from the underlying model values.
-     *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @param  string  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
-     * @return mixed
      */
-    public function get($model, string $key, $value, array $attributes);
+    public function get(Model $model, string $key, mixed $value, array $attributes);
 
     /**
      * Transform the attribute to its underlying model values.
      *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @param  string  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
      * @return mixed
      */
-    public function set($model, string $key, $value, array $attributes);
+    public function set(Model $model, string $key, mixed $value, array $attributes);
 }
