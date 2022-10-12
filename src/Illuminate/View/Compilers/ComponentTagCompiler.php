b1a5c6dcfb0910877b155957e5f32c39fbd3d6cd
Tim MacDonald <hello@timacdonald.me>
2022-10-12 11:25:36 +1100

    fix mail tags silently failing

--- src/Illuminate/View/Compilers/ComponentTagCompiler.php
+++ src/Illuminate/View/Compilers/ComponentTagCompiler.php
@@ -236,8 +236,12 @@ class ComponentTagCompiler
         // component and pass the component as a view parameter to the data so it
         // can be accessed within the component and we can render out the view.
         if (! class_exists($class)) {
+            $view = Str::startsWith($component, 'mail::')
+                ? "\$__env->getContainer()->make(Illuminate\\View\\Factory::class)->make('{$component}')"
+                : "'$class'";
+
             $parameters = [
-                'view' => "'$class'",
+                'view' => $view,
                 'data' => '['.$this->attributesToString($data->all(), $escapeBound = false).']',
             ];
 
