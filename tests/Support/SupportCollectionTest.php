770caed2e85c57879ae4f9cabda91a59e53ddbca
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2022-10-15 13:06:28 +0200

    Fix test result (4.)

--- tests/Support/SupportCollectionTest.php
+++ tests/Support/SupportCollectionTest.php
@@ -2830,7 +2830,7 @@ class SupportCollectionTest extends TestCase
         );
 
         $this->assertSame(
-            [5 => 5, 4, 3, 2, 1],
+            [5 => 5, 4 => 4, 3 => 3, 2 => 2, 1 => 1],
             $collection::rangeByKey(5, 1)->all()
         );
 
