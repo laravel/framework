7b833a98dcc0aa87225275c1ab033194096d4332
Nuno Maduro <enunomaduro@gmail.com>
2022-10-19 14:21:27 +0100

    [9.x] Forget component's cache and factory between tests (#44648)
    * Forget component's cache and factory between tests
    
    * Apply fixes from StyleCI
    
    * Adds `forgetComponentsResolver`
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Illuminate/Foundation/Testing/TestCase.php
+++ src/Illuminate/Foundation/Testing/TestCase.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Support\Str;
 use Illuminate\Testing\AssertableJsonString;
+use Illuminate\View\Component;
 use Mockery;
 use Mockery\Exception\InvalidCountException;
 use PHPUnit\Framework\ExpectationFailedException;
@@ -222,6 +223,9 @@ abstract class TestCase extends BaseTestCase
         $this->originalDeprecationHandler = null;
 
         Artisan::forgetBootstrappers();
+        Component::flushCache();
+        Component::forgetComponentsResolver();
+        Component::forgetFactory();
         Queue::createPayloadUsing(null);
         HandleExceptions::forgetApp();
 
