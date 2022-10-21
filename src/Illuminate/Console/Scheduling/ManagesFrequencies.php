eae7a705148a75b97e2837032c4e475c86a7bf42
Dylan Bridgman <light@ether.org.za>
2022-10-21 17:07:50 +0200

    Styling

--- src/Illuminate/Console/Scheduling/ManagesFrequencies.php
+++ src/Illuminate/Console/Scheduling/ManagesFrequencies.php
@@ -315,14 +315,14 @@ trait ManagesFrequencies
     /**
      * Schedule the event to run every day except for those specified.
      *
-     * @param array|int $days
+     * @param  array|int  $days
      * @return $this
      */
     public function everyDayExcept($days)
     {
         $days = is_array($days) ? $days : func_get_args();
 
-        return $this->days(join(',', array_diff(range(0,6), $days)));
+        return $this->days(join(',', array_diff(range(0, 6), $days)));
     }
 
     /**
