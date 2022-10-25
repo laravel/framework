2f01da88b441879ad3e77a0d7e8c1e2be2fad49d
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-25 15:02:12 +0200

    add more test

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -322,6 +322,7 @@ class SupportStrTest extends TestCase
         $this->assertSame('', Str::slug(''));
         $this->assertSame('bsm-allah', Str::slug('بسم الله', '-', 'en', ['allh' => 'allah']));
         $this->assertSame('500-dollar-bill', Str::slug('500$ bill', '-', 'en', ['$' => 'dollar']));
+        $this->assertSame('أحمد-في-المدرسة', Str::slug('أحمد@المدرسة', '-', null, ['@' =>'في']));
     }
 
     public function testStrStart()
