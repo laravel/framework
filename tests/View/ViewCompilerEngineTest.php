c06913f114fa5393a1331ed70a844d7efaf1ce59
Nuno Maduro <enunomaduro@gmail.com>
2022-10-26 14:53:22 +0100

    [9.x] Recompiles views when necessary (#44737)
    * Recompiles views when necessary
    
    * Fixes when filesystem cache file exists
    
    * Apply fixes from StyleCI
    
    * More tests
    
    * Apply fixes from StyleCI
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- tests/View/ViewCompilerEngineTest.php
+++ tests/View/ViewCompilerEngineTest.php
@@ -2,9 +2,13 @@
 
 namespace Illuminate\Tests\View;
 
+use ErrorException;
+use Exception;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\View\Compilers\CompilerInterface;
 use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\ViewException;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
@@ -55,8 +59,219 @@ class ViewCompilerEngineTest extends TestCase
         $engine->get(__DIR__.'/fixtures/foo.php');
     }
 
-    protected function getEngine()
+    public function testViewsAreRecompiledWhenCompiledViewIsMissingViaFileNotFoundException()
     {
-        return new CompilerEngine(m::mock(CompilerInterface::class), new Filesystem);
+        $compiled = __DIR__.'/fixtures/basic.php';
+        $path = __DIR__.'/fixtures/foo.php';
+
+        $files = m::mock(Filesystem::class);
+        $engine = $this->getEngine($files);
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andReturn('compiled-content');
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andThrow(new FileNotFoundException(
+                "File does not exist at path {$path}."
+            ));
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andReturn('compiled-content');
+
+        $engine->getCompiler()
+            ->shouldReceive('getCompiledPath')
+            ->times(3)
+            ->with($path)
+            ->andReturn($compiled);
+
+        $engine->getCompiler()
+            ->shouldReceive('isExpired')
+            ->once()
+            ->andReturn(true);
+
+        $engine->getCompiler()
+            ->shouldReceive('compile')
+            ->twice()
+            ->with($path);
+
+        $engine->get($path);
+        $engine->get($path);
+    }
+
+    public function testViewsAreRecompiledWhenCompiledViewIsMissingViaRequireException()
+    {
+        $compiled = __DIR__.'/fixtures/basic.php';
+        $path = __DIR__.'/fixtures/foo.php';
+
+        $files = m::mock(Filesystem::class);
+        $engine = $this->getEngine($files);
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andReturn('compiled-content');
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andThrow(new ErrorException(
+                "require({$path}): Failed to open stream: No such file or directory",
+            ));
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andReturn('compiled-content');
+
+        $engine->getCompiler()
+            ->shouldReceive('getCompiledPath')
+            ->times(3)
+            ->with($path)
+            ->andReturn($compiled);
+
+        $engine->getCompiler()
+            ->shouldReceive('isExpired')
+            ->once()
+            ->andReturn(true);
+
+        $engine->getCompiler()
+            ->shouldReceive('compile')
+            ->twice()
+            ->with($path);
+
+        $engine->get($path);
+        $engine->get($path);
+    }
+
+    public function testViewsAreRecompiledJustOnceWhenCompiledViewIsMissing()
+    {
+        $compiled = __DIR__.'/fixtures/basic.php';
+        $path = __DIR__.'/fixtures/foo.php';
+
+        $files = m::mock(Filesystem::class);
+        $engine = $this->getEngine($files);
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andReturn('compiled-content');
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andThrow(new FileNotFoundException(
+                "File does not exist at path {$path}."
+            ));
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andThrow(new FileNotFoundException(
+                "File does not exist at path {$path}."
+            ));
+
+        $engine->getCompiler()
+            ->shouldReceive('getCompiledPath')
+            ->times(3)
+            ->with($path)
+            ->andReturn($compiled);
+
+        $engine->getCompiler()
+            ->shouldReceive('isExpired')
+            ->once()
+            ->andReturn(true);
+
+        $engine->getCompiler()
+            ->shouldReceive('compile')
+            ->twice()
+            ->with($path);
+
+        $engine->get($path);
+
+        $this->expectException(ViewException::class);
+        $this->expectExceptionMessage("File does not exist at path {$path}.");
+        $engine->get($path);
+    }
+
+    public function testViewsAreNotRecompiledOnRegularViewException()
+    {
+        $compiled = __DIR__.'/fixtures/basic.php';
+        $path = __DIR__.'/fixtures/foo.php';
+
+        $files = m::mock(Filesystem::class);
+        $engine = $this->getEngine($files);
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andThrow(new Exception(
+                'Just an regular error...'
+            ));
+
+        $engine->getCompiler()
+            ->shouldReceive('isExpired')
+            ->once()
+            ->andReturn(false);
+
+        $engine->getCompiler()
+            ->shouldReceive('compile')
+            ->never();
+
+        $engine->getCompiler()
+            ->shouldReceive('getCompiledPath')
+            ->once()
+            ->with($path)
+            ->andReturn($compiled);
+
+        $this->expectException(ViewException::class);
+        $this->expectExceptionMessage('Just an regular error...');
+        $engine->get($path);
+    }
+
+    public function testViewsAreNotRecompiledIfTheyWereJustCompiled()
+    {
+        $compiled = __DIR__.'/fixtures/basic.php';
+        $path = __DIR__.'/fixtures/foo.php';
+
+        $files = m::mock(Filesystem::class);
+        $engine = $this->getEngine($files);
+
+        $files->shouldReceive('getRequire')
+            ->once()
+            ->with($compiled, [])
+            ->andThrow(new FileNotFoundException(
+                "File does not exist at path {$path}."
+            ));
+
+        $engine->getCompiler()
+            ->shouldReceive('isExpired')
+            ->once()
+            ->andReturn(true);
+
+        $engine->getCompiler()
+            ->shouldReceive('compile')
+            ->once()
+            ->with($path);
+
+        $engine->getCompiler()
+            ->shouldReceive('getCompiledPath')
+            ->once()
+            ->with($path)
+            ->andReturn($compiled);
+
+        $this->expectException(ViewException::class);
+        $this->expectExceptionMessage("File does not exist at path {$path}.");
+        $engine->get($path);
+    }
+
+    protected function getEngine($filesystem = null)
+    {
+        return new CompilerEngine(m::mock(CompilerInterface::class), $filesystem ?: new Filesystem);
     }
 }
