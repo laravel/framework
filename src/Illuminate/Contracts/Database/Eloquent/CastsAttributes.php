c66ab65a087f980bcbdc228598d4882c7f238f3e
Nuno Maduro <enunomaduro@gmail.com>
2022-10-28 13:19:01 +0100

    Reverts changes in docs

--- src/Illuminate/Contracts/Database/Eloquent/CastsAttributes.php
+++ src/Illuminate/Contracts/Database/Eloquent/CastsAttributes.php
@@ -8,12 +8,22 @@ interface CastsAttributes
 {
     /**
      * Transform the attribute from the underlying model values.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
      */
     public function get(Model $model, string $key, mixed $value, array $attributes);
 
     /**
      * Transform the attribute to its underlying model values.
      *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
      * @return mixed
      */
     public function set(Model $model, string $key, mixed $value, array $attributes);
