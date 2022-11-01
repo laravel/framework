a8a20fa6d3f01de1f6fc537694e6932061cb4f06
Karmendra Suthar <karmendra.js@gmail.com>
2022-11-01 16:41:14 +0530

    fixed count() issue to work with php 7.2
    fixed count(): Parameter must be an array or an object that implements Countable

--- src/Illuminate/Queue/SqsQueue.php
+++ src/Illuminate/Queue/SqsQueue.php
@@ -114,7 +114,7 @@ class SqsQueue extends Queue implements QueueContract
             ['QueueUrl' => $queue, 'AttributeNames' => ['ApproximateReceiveCount']]
         );
 
-        if (count($response['Messages']) > 0) {
+        if (! is_null($response['Messages']) && count($response['Messages']) > 0) {
             if ($this->jobCreator) {
                 return call_user_func($this->jobCreator, $this->container, $this->sqs, $queue, $response);
             } else {
