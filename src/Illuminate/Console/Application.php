0de8734170de48926e5a65c44e898b7078a0da0e
Bert <bertwijnhoven@hotmail.com>
2022-11-02 21:30:46 +0100

    [9.x] Improve input argument parsing for commands (#44662)
    * Update Application.php
    
    * Update Application.php
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Console/Application.php
+++ src/Illuminate/Console/Application.php
@@ -13,6 +13,7 @@ use Illuminate\Support\ProcessUtils;
 use Symfony\Component\Console\Application as SymfonyApplication;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Exception\CommandNotFoundException;
+use Symfony\Component\Console\Exception\ExceptionInterface;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputDefinition;
@@ -92,6 +93,12 @@ class Application extends SymfonyApplication implements ApplicationContract
         $commandName = $this->getCommandName(
             $input = $input ?: new ArgvInput
         );
+
+        try {
+            $input->bind($this->find($commandName)->getDefinition());
+        } catch (ExceptionInterface) {
+            // ...
+        }
 
         $this->events->dispatch(
             new CommandStarting(
