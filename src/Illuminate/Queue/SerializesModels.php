a5ce9e53b67867f798744ba38fd0ff43c598d2b3
Robert Kujawa <bobkujawa@gmail.com>
2022-11-04 23:35:16 -0600

    Remove __sleep & __wakeup from SerializesModels

--- src/Illuminate/Queue/SerializesModels.php
+++ src/Illuminate/Queue/SerializesModels.php
@@ -10,44 +10,6 @@ trait SerializesModels
     use SerializesAndRestoresModelIdentifiers;
 
     /**
-     * Prepare the instance for serialization.
-     *
-     * @return array
-     */
-    public function __sleep()
-    {
-        $properties = (new ReflectionClass($this))->getProperties();
-
-        foreach ($properties as $property) {
-            $property->setValue($this, $this->getSerializedPropertyValue(
-                $this->getPropertyValue($property)
-            ));
-        }
-
-        return array_values(array_filter(array_map(function ($p) {
-            return $p->isStatic() ? null : $p->getName();
-        }, $properties)));
-    }
-
-    /**
-     * Restore the model after serialization.
-     *
-     * @return void
-     */
-    public function __wakeup()
-    {
-        foreach ((new ReflectionClass($this))->getProperties() as $property) {
-            if ($property->isStatic()) {
-                continue;
-            }
-
-            $property->setValue($this, $this->getRestoredPropertyValue(
-                $this->getPropertyValue($property)
-            ));
-        }
-    }
-
-    /**
      * Prepare the instance values for serialization.
      *
      * @return array
