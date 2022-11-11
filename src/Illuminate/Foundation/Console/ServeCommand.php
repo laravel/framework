b1277596bb4d7ca9aec72976dbaf97f7d0e26585
Christoph Kluge <work@christoph-kluge.eu>
2022-11-11 16:44:24 +0100

    Restrict the allowed characters between the brackets. This is necessary because it might include nested brackets.
    "[13804] [Fri Nov 11 16:37:08 2022] [::1]" would resolve into "[Fri Nov 11 16:37:08 2022] [::1"

--- src/Illuminate/Foundation/Console/ServeCommand.php
+++ src/Illuminate/Foundation/Console/ServeCommand.php
@@ -289,7 +289,7 @@ class ServeCommand extends Command
     protected function getDateFromLine($line)
     {
         $regex = env('PHP_CLI_SERVER_WORKERS', 1) > 1
-            ? '/^\[\d+]\s\[(.*)]/'
+            ? '/^\[\d+]\s\[([a-zA-Z0-9: ]+)\]/'
             : '/^\[([^\]]+)\]/';
 
         preg_match($regex, $line, $matches);
