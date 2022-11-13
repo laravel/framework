d6fc01b244b7becb71b11926c789ee574f1a0052
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2022-11-13 13:36:31 +0100

    Show error when key:generate artisan command fails

--- src/Illuminate/Foundation/Console/KeyGenerateCommand.php
+++ src/Illuminate/Foundation/Console/KeyGenerateCommand.php
@@ -90,7 +90,9 @@ class KeyGenerateCommand extends Command
             return false;
         }
 
-        $this->writeNewEnvironmentFileWith($key);
+        if (! $this->writeNewEnvironmentFileWith($key)) {
+            return false;
+        }
 
         return true;
     }
@@ -99,15 +101,25 @@ class KeyGenerateCommand extends Command
      * Write a new environment file with the given key.
      *
      * @param  string  $key
-     * @return void
+     * @return bool
      */
     protected function writeNewEnvironmentFileWith($key)
     {
-        file_put_contents($this->laravel->environmentFilePath(), preg_replace(
+        $replaced = preg_replace(
             $this->keyReplacementPattern(),
             'APP_KEY='.$key,
-            file_get_contents($this->laravel->environmentFilePath())
-        ));
+            $input = file_get_contents($this->laravel->environmentFilePath())
+        );
+
+        if ($replaced === $input || $replaced === null) {
+            $this->error('No APP_KEY found in your .env file. Cannot set automatically.');
+
+            return false;
+        }
+
+        file_put_contents($this->laravel->environmentFilePath(), $replaced);
+
+        return true;
     }
 
     /**
