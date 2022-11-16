16a37fc9900fa9564d2802ff3b0c0fe2fc2a825c
Raja <mrajasays@gmail.com>
2022-11-17 01:06:16 +1100

    fix closing bracket :facepalm:

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -269,7 +269,7 @@ class Handler implements ExceptionHandlerContract
             $this->levels, fn ($level, $type) => $e instanceof $type, LogLevel::ERROR
         );
 
-        $context = array_merge($this->buildExceptionContext($e), $context];
+        $context = array_merge($this->buildExceptionContext($e), $context);
 
         method_exists($logger, $level)
             ? $logger->{$level}($e->getMessage(), $context)
