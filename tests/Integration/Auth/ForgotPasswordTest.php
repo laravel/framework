c65c2fad06f5cd69d11de7212957b931db61a920
Alexey Krupskiy <admin@inji.ru>
2022-11-18 15:37:20 +0700

    fix tests

--- tests/Integration/Auth/ForgotPasswordTest.php
+++ tests/Integration/Auth/ForgotPasswordTest.php
@@ -98,12 +98,16 @@ class ForgotPasswordTest extends TestCase
     {
         Notification::fake();
 
-        ResetPassword::toMailUsing(function ($notifiable, $token) {
+        ResetPassword::toMailUsing(function ($notifiable, $url) {
             return (new MailMessage)
                 ->subject(__('Reset Password Notification'))
                 ->line(__('You are receiving this email because we received a password reset request for your account.'))
-                ->action(__('Reset Password'), route('custom.password.reset', $token))
+                ->action(__('Reset Password'), $url)
                 ->line(__('If you did not request a password reset, no further action is required.'));
+        });
+
+        ResetPassword::createUrlUsing(function ($notifiable, $token){
+            return  route('custom.password.reset', $token);
         });
 
         UserFactory::new()->create();
