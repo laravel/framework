814b1bd8081ac9a6fe7cfba607e2fa46fde77dc6
Michael Nabil <46572405+michaelnabil230@users.noreply.github.com>
2022-11-25 02:24:07 +0200

    fix

--- src/Illuminate/Collections/helpers.php
+++ src/Illuminate/Collections/helpers.php
@@ -180,6 +180,7 @@ if (! function_exists('value')) {
      * Return the default value of the given value.
      *
      * @param  mixed  $value
+     * @param  mixed  $args
      * @return mixed
      */
     function value($value, ...$args)
