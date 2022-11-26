54f07048d188849db3505c7ae4190eadff12db14
Michael Nabil <46572405+michaelnabil230@users.noreply.github.com>
2022-11-26 17:39:21 +0200

    Update HandlePrecognitiveRequests.php

--- src/Illuminate/Foundation/Http/Middleware/HandlePrecognitiveRequests.php
+++ src/Illuminate/Foundation/Http/Middleware/HandlePrecognitiveRequests.php
@@ -5,6 +5,7 @@ namespace Illuminate\Foundation\Http\Middleware;
 use Illuminate\Container\Container;
 use Illuminate\Foundation\Routing\PrecognitionCallableDispatcher;
 use Illuminate\Foundation\Routing\PrecognitionControllerDispatcher;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Contracts\CallableDispatcher as CallableDispatcherContract;
 use Illuminate\Routing\Contracts\ControllerDispatcher as ControllerDispatcherContract;
 
