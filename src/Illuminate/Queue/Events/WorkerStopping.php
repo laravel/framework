e384ae1ab0c8fbad982843b6843605b7e9889284
Damilare Anjorin <damilareanjorin1@gmail.com>
2022-11-28 12:21:38 +0100

    [9.x] Add WorkOptions to WorkerStopping Event

--- src/Illuminate/Queue/Events/WorkerStopping.php
+++ src/Illuminate/Queue/Events/WorkerStopping.php
@@ -2,6 +2,8 @@
 
 namespace Illuminate\Queue\Events;
 
+use Illuminate\Queue\WorkerOptions;
+
 class WorkerStopping
 {
     /**
@@ -12,13 +14,22 @@ class WorkerStopping
     public $status;
 
     /**
+     * The Worker Options
+     *
+     * @var WorkerOptions|null
+     */
+    public $workerOptions;
+
+    /**
      * Create a new event instance.
      *
      * @param  int  $status
+     * @param WorkerOptions|null $workerOptions
      * @return void
      */
-    public function __construct($status = 0)
+    public function __construct($status = 0, $workerOptions = null)
     {
         $this->status = $status;
+        $this->workerOptions = $workerOptions;
     }
 }
