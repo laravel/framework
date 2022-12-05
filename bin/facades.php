e77a62b7ccab3559fc9d2b0f8311f83f45b27cd7
Tim MacDonald <hello@timacdonald.me>
2022-12-05 16:59:57 +1100

    add script

---
+++ bin/facades.php
@@ -0,0 +1,446 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Str;
+use Symfony\Component\Finder\Finder;
+
+$finder = (new Finder)
+    ->in(__DIR__.'/../src/Illuminate/Support/Facades')
+    ->notName('Facade.php');
+
+resolveFacades($finder)->each(function ($facade) {
+    $proxies = resolveDocSees($facade);
+
+    // Build a list of methods that are available on the Facade...
+
+    $resolvedMethods = $proxies->map(fn ($fqcn) => new ReflectionClass($fqcn))
+        ->flatMap(fn ($class) => [$class, ...resolveDocMixins($class)])
+        ->flatMap(resolveMethods(...))
+        ->reject(isMagic(...))
+        ->reject(isDeprecated(...))
+        ->reject(fulfillsBuiltinInterface(...))
+        ->reject(fn ($method) => conflictsWithFacade($facade, $method))
+        ->unique(resolveName(...))
+        ->map(normaliseDetails(...));
+
+    // Prepare the @template docblocks...
+
+    $templates = $resolvedMethods->flatMap(fn ($method) => is_string($method) ? [] : $method['templates'])
+        ->map(fn ($template) => " * @template {$template}")
+        ->unique()
+        ->whenNotEmpty(fn ($templates) => $templates->push(' *'));
+
+    // Prepare the @method docblocks...
+
+    $methods = $resolvedMethods->map(function ($method) {
+        if (is_string($method)) {
+            return " * @method static {$method}";
+        }
+
+        $parameters = $method['parameters']->map(function ($parameter) {
+            $rest = $parameter['variadic'] ? '...' : '';
+
+            $default = $parameter['optional'] ? ' = '.resolveDefaultValue($parameter) : '';
+
+            return "{$parameter['type']} {$rest}{$parameter['name']}{$default}";
+        });
+
+        return " * @method static {$method['returns']} {$method['name']}({$parameters->join(', ')})";
+    });
+
+    // Fix: ensure we keep the references to the Carbon library on the Date Facade...
+
+    if ($facade->getName() === Date::class) {
+        $methods->prepend(' *')
+                ->prepend(' * @see https://github.com/briannesbitt/Carbon/blob/master/src/Carbon/Factory.php')
+                ->prepend(' * @see https://carbon.nesbot.com/docs/');
+    }
+
+    // Generate the docblock...
+
+    $docblock = <<< PHP
+    /**
+    {$templates->merge($methods)->join(PHP_EOL)}
+     *
+    {$proxies->map(fn ($class) => " * @see {$class}")->join(PHP_EOL)}
+     */
+    PHP;
+
+    // Determine if the Facade docblock is up to date...
+
+    if (Str::contains(file_get_contents($facade->getFileName()) ?: '', $docblock)) {
+        return;
+    }
+
+    echo "❌ Did not find expected docblock for [{$facade->getName()}].";
+    echo PHP_EOL;
+    echo PHP_EOL;
+    echo $docblock;
+    exit(1);
+});
+
+echo '✅ Facade docblocks look good.';
+exit(0);
+
+/**
+ * Resolve the facades from the given directory.
+ *
+ * @param  \Symfony\Component\Finder\Finder  $finder
+ * @return \Illuminate\Support\Collection<\ReflectionClass>
+ */
+function resolveFacades($finder)
+{
+    return collect($finder)
+        ->map(fn ($file) => $file->getBaseName('.php'))
+        ->map(fn ($name) => "\\Illuminate\\Support\\Facades\\{$name}")
+        ->map(fn ($class) => new ReflectionClass($class));
+}
+
+/**
+ * Resolve the classes referenced in the @see docblocks.
+ *
+ * @param  \ReflectionClass  $class
+ * @return \Illuminate\Support\Collection<class-string>
+ */
+function resolveDocSees($class)
+{
+    return resolveDocTags($class->getDocComment() ?: '', '@see')
+        ->reject(fn ($tag) => Str::startsWith($tag, 'https://'));
+}
+
+/**
+ * Resolve the classes referenced methods in the @methods docblocks.
+ *
+ * @param  \ReflectionClass  $class
+ * @return \Illuminate\Support\Collection<string>
+ */
+function resolveDocMethods($class)
+{
+    return resolveDocTags($class->getDocComment() ?: '', '@method')
+        ->map(fn ($tag) => Str::squish($tag))
+        ->map(fn ($tag) => Str::before($tag, ')').')');
+}
+
+/**
+ * Resolve the parameters type from the @param docblocks.
+ *
+ * @param  \ReflectionMethodDecorator  $method
+ * @param  \ReflectionParameter  $parameter
+ * @return string|null
+ */
+function resolveDocParamType($method, $parameter)
+{
+    $tag = resolveDocTags($method->getDocComment() ?: '', '@param')
+        ->first(fn ($tag) => preg_match('/\$'.$parameter->getName().'\b/', $tag) === 1);
+
+    // As we didn't find a param type, we will now recursivly check if the prototype has a value specified...
+
+    if ($tag === null) {
+        try {
+            $prototype = new ReflectionMethodDecorator($method->getPrototype(), $method->sourceClass()->getName());
+
+            return resolveDocParamType($prototype, $parameter);
+        } catch (Throwable) {
+            return null;
+        }
+    }
+
+    // Strip the rest operator, variable name, and desription from the tag...
+
+    $types = Str::of($tag)
+        ->beforeLast("...\${$parameter->getName()}")
+        ->beforeLast("\${$parameter->getName()}")
+        ->trim()
+        ->toString();
+
+    // Replace references to '$this', 'static', or 'self' with the implementations FQCN...
+
+    $types = Str::of($types)
+        ->replace(['$this', 'static'], '\\'.$method->sourceClass()->getName())
+        ->replace('self', '\\'.$method->getDeclaringClass()->getName());
+
+    // Generics are being namespaced, so we will strip the namespace from them...
+
+    return resolveTemplateDocName($method)->reduce(function ($types, $template) use ($parameter) {
+        return Str::replace('\\'.$parameter->getDeclaringClass()->getNamespaceName().'\\'.$template, $template, $types);
+    }, $types);
+}
+
+/**
+ * Resolve the templates type from the @template docblocks.
+ *
+ * @param  \ReflectionMethod|ReflectionMethodDecorator  $method
+ * @return \Illuminate\Support\Collection<string>
+ */
+function resolveTemplateDocName($method)
+{
+    return resolveDocTags($method->getDocComment() ?: '', '@template')
+        ->map(fn ($tag) => Str::before($tag, ' '));
+}
+
+/**
+ * Resolve the return type from the @return docblock.
+ *
+ * @param  \ReflectionMethodDecorator  $method
+ * @return string|null
+ */
+function resolveReturnDocType($method)
+{
+    $types = resolveDocTags($method->getDocComment() ?: '', '@return')->first();
+
+    if ($types === null) {
+        return null;
+    }
+
+    // Strip the return description, but don't strip array generics...
+
+    if (Str::containsAll($types, ['<', '>'])) {
+        $types = Str::beforeLast($types, '>').'>';
+    } elseif (Str::contains($types, ' ')) {
+        $types = Str::before($types, ' ');
+    }
+
+    // Replace references to '$this', 'static', or 'self' with the implementations FQCN...
+
+    return Str::of($types)
+        ->replace(['$this', 'static'], '\\'.$method->sourceClass()->getName())
+        ->replace('self', '\\'.$method->getDeclaringClass()->getName())
+        ->toString();
+}
+
+/**
+ * Resolve the declared type.
+ *
+ * @param  \ReflectionType|null  $type
+ * @return string|null
+ */
+function resolveType($type)
+{
+    if ($type instanceof ReflectionIntersectionType) {
+        return collect($type->getTypes())
+            ->map(resolveType(...))
+            ->filter()
+            ->join('&');
+    }
+
+    if ($type instanceof ReflectionUnionType) {
+        return collect($type->getTypes())
+            ->map(resolveType(...))
+            ->filter()
+            ->join('|');
+    }
+
+    if ($type instanceof ReflectionNamedType && $type->getName() === 'null') {
+        return ($type->isBuiltin() ? '' : '\\').$type->getName();
+    }
+
+    if ($type instanceof ReflectionNamedType && $type->getName() !== 'null') {
+        return ($type->isBuiltin() ? '' : '\\').$type->getName().($type->allowsNull() ? '|null' : '');
+    }
+
+    return null;
+}
+
+/**
+ * Resolve the docblock tags.
+ *
+ * @param  string  $docblock
+ * @param  string  $tag
+ * @return \Illuminate\Support\Collection
+ */
+function resolveDocTags($docblock, $tag)
+{
+    return Str::of($docblock)
+        ->explode("\n")
+        ->skip(1)
+        ->reverse()
+        ->skip(1)
+        ->reverse()
+        ->map(fn ($line) => ltrim($line, ' \*'))
+        ->filter(fn ($line) => Str::startsWith($line, $tag))
+        ->map(fn ($line) => Str::of($line)->after($tag)->trim()->toString())
+        ->values();
+}
+
+/**
+ * Recursivly resolve docblock mixins.
+ *
+ * @param  \ReflectionClass  $class
+ * @return \Illuminate\Support\Collection<ReflectionClass>
+ */
+function resolveDocMixins($class)
+{
+    return resolveDocTags($class->getDocComment() ?: '', '@mixin')
+        ->map(fn ($mixin) => new ReflectionClass($mixin))
+        ->flatMap(fn ($mixin) => [$mixin, ...resolveDocMixins($mixin)]);
+}
+
+/**
+ * Determine if the method is magic.
+ *
+ * @param  string|\ReflectionMethod  $method
+ * @return bool
+ */
+function isMagic($method)
+{
+    return Str::startsWith(is_string($method) ? $method : $method->getName(), '__');
+}
+
+/**
+ * Determine if the method is deprecated.
+ *
+ * @param  string|\ReflectionMethod  $method
+ * @return bool
+ */
+function isDeprecated($method)
+{
+    return ! is_string($method) && $method->isDeprecated();
+}
+
+/**
+ * Determine if the method is for a builtin contract.
+ *
+ * @param  string|\ReflectionMethodDecorator  $method
+ * @return bool
+ */
+function fulfillsBuiltinInterface($method)
+{
+    if (is_string($method)) {
+        return false;
+    }
+
+    if ($method->sourceClass()->implementsInterface(ArrayAccess::class)) {
+        return in_array($method->getName(), ['offsetExists', 'offsetGet', 'offsetSet', 'offsetUnset']);
+    }
+
+    return false;
+}
+
+/**
+ * Resolve the methods name.
+ *
+ * @param  string|\ReflectionMethod  $method
+ * @return string
+ */
+function resolveName($method)
+{
+    return is_string($method)
+        ? Str::of($method)->after(' ')->before('(')->toString()
+        : $method->getName();
+}
+
+/**
+ * Resolve the classes methods.
+ *
+ * @param  \ReflectionClass  $class
+ * @return Illuminate\Support\Collection<ReflectionMethodDecorator|string>
+ */
+function resolveMethods($class)
+{
+    return collect($class->getMethods(ReflectionMethod::IS_PUBLIC))
+        ->map(fn ($method) => new ReflectionMethodDecorator($method, $class->getName()))
+        ->merge(resolveDocMethods($class));
+}
+
+/**
+ * Determine if the given method conflicts with a Facade method.
+ *
+ * @param  \ReflectionClass  $facade
+ * @param  \ReflectionMethod|string  $method
+ * @return bool
+ */
+function conflictsWithFacade($facade, $method)
+{
+    return collect($facade->getMethods(ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_STATIC))
+        ->map(fn ($method) => $method->getName())
+        ->contains(is_string($method) ? $method : $method->getName());
+}
+
+/**
+ * Normalise the method details into a easier format to work with.
+ *
+ * @param  string|\ReflectionMethodDecorator  $method
+ * @return string|array
+ */
+function normaliseDetails($method)
+{
+    return is_string($method) ? $method : [
+        'name' => $method->getName(),
+        'parameters' => collect($method->getParameters())
+            ->map(fn ($parameter) => [
+                'name' => '$'.$parameter->getName(),
+                'optional' => $parameter->isOptional() && ! $parameter->isVariadic(),
+                'default' => $parameter->isDefaultValueAvailable()
+                    ? $parameter->getDefaultValue()
+                    : "❌ Unknown default for [{$parameter->getName()}] in [{$parameter->getDeclaringClass()?->getName()}::{$parameter->getDeclaringFunction()->getName()}] ❌",
+                'variadic' => $parameter->isVariadic(),
+                'type' => resolveDocParamType($method, $parameter) ?? resolveType($parameter->getType()) ?? 'void',
+            ]),
+        'returns' => resolveReturnDocType($method) ?? resolveType($method->getReturnType()) ?? 'void',
+        'templates' => resolveDocTags($method->getDocComment(), '@template'),
+    ];
+}
+
+/**
+ * Resolve the default value for the parameter.
+ *
+ * @param  array  $parameter
+ * @return string
+ */
+function resolveDefaultValue($parameter)
+{
+    // Reflection limitation fix for:
+    // - Illuminate\Filesystem\Filesystem::ensureDirectoryExists()
+    // - Illuminate\Filesystem\Filesystem::makeDirectory()
+    if ($parameter['name'] === '$mode' && $parameter['default'] === 493) {
+        return '0755';
+    }
+
+    return Str::of(json_encode($parameter['default']) ?: 'unknown')
+        ->replace('"', "'")
+        ->replace('\\/', '/')
+        ->toString();
+}
+
+/**
+ * @mixin ReflectionMethod
+ */
+class ReflectionMethodDecorator
+{
+    /**
+     * @param  \ReflectionMethod  $method
+     * @param  class-string  $sourceClass
+     */
+    public function __construct(private $method, private $sourceClass)
+    {
+        //
+    }
+
+    /**
+     * @param  string  $name
+     * @param  array  $arguments
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        return $this->method->{$name}(...$arguments);
+    }
+
+    /**
+     * @return \ReflectionMethod
+     */
+    public function toBase()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @return \ReflectionClass
+     */
+    public function sourceClass()
+    {
+        return new ReflectionClass($this->sourceClass);
+    }
+}
