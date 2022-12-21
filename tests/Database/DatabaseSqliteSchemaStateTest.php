71046d74930ed3c7a82b648851bfea94bdd36240
recca0120 <recca0120@gmail.com>
2022-12-21 12:06:03 +0800

    load schema to in-memory database

--- tests/Database/DatabaseSqliteSchemaStateTest.php
+++ tests/Database/DatabaseSqliteSchemaStateTest.php
@@ -4,7 +4,9 @@ namespace Illuminate\Tests\Database;
 
 use Illuminate\Database\Schema\SqliteSchemaState;
 use Illuminate\Database\SQLiteConnection;
+use Illuminate\Filesystem\Filesystem;
 use Mockery as m;
+use PDO;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
 
@@ -21,6 +23,7 @@ class DatabaseSqliteSchemaStateTest extends TestCase
         $config = ['driver' => 'sqlite', 'database' => 'database/database.sqlite', 'prefix' => '', 'foreign_key_constraints' => true, 'name' => 'sqlite'];
         $connection = m::mock(SQLiteConnection::class);
         $connection->shouldReceive('getConfig')->andReturn($config);
+        $connection->shouldReceive('getDatabaseName')->andReturn($config['database']);
 
         $process = m::spy(Process::class);
         $processFactory = function () use ($process) {
@@ -36,4 +39,20 @@ class DatabaseSqliteSchemaStateTest extends TestCase
         ]);
     }
 
+    public function testLoadSchemaToInMemory(): void
+    {
+        $config = ['driver' => 'sqlite', 'database' => ':memory:', 'prefix' => '', 'foreign_key_constraints' => true, 'name' => 'sqlite'];
+        $connection = m::mock(SQLiteConnection::class);
+        $connection->shouldReceive('getConfig')->andReturn($config);
+        $connection->shouldReceive('getDatabaseName')->andReturn($config['database']);
+        $connection->shouldReceive('getPdo')->andReturn($pdo = m::spy(PDO::class));
+
+        $files = m::mock(Filesystem::class);
+        $files->shouldReceive('get')->andReturn('CREATE TABLE IF NOT EXISTS "migrations" ("id" integer not null primary key autoincrement, "migration" varchar not null, "batch" integer not null);');
+
+        $schemaState = new SqliteSchemaState($connection, $files);
+        $schemaState->load('database/schema/sqlite-schema.dump');
+
+        $pdo->shouldHaveReceived('exec')->with('CREATE TABLE IF NOT EXISTS "migrations" ("id" integer not null primary key autoincrement, "migration" varchar not null, "batch" integer not null);');
+    }
 }
