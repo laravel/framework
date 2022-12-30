55e33452a1ba54a5d79f05c83e773439f2ca77c2
Taylor Otwell <taylor@laravel.com>
2022-12-29 20:03:11 -0600

    rename test

--- tests/Integration/Http/ResourceTest.php
+++ tests/Integration/Http/ResourceTest.php
@@ -168,7 +168,7 @@ class ResourceTest extends TestCase
         ]);
     }
 
-    public function testResourcesMayHaveOptionalValues()
+    public function testResourcesMayHaveOptionalValuesUsingUnless()
     {
         Route::get('/', function () {
             return new PostResourceWithUnlessOptionalData(new Post([
