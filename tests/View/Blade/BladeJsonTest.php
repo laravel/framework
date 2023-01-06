be7d682bdaab4c690b9eb459bbbb27270dfdf4f8
Thomas Alrek <thomas@alrek.no>
2023-01-06 12:30:51 +0100

    Update BladeJsonTest.php

--- tests/View/Blade/BladeJsonTest.php
+++ tests/View/Blade/BladeJsonTest.php
@@ -7,7 +7,7 @@ class BladeJsonTest extends AbstractBladeTestCase
     public function testStatementIsCompiledWithSafeDefaultEncodingOptions()
     {
         $string = 'var foo = @json($var);';
-        $expected = 'var foo = <?php echo json_encode($var, 15, 512) ?>;';
+        $expected = 'var foo = <?php echo e(json_encode($var, 15, 512)) ?>;';
 
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
@@ -15,7 +15,16 @@ class BladeJsonTest extends AbstractBladeTestCase
     public function testEncodingOptionsCanBeOverwritten()
     {
         $string = 'var foo = @json($var, JSON_HEX_TAG);';
-        $expected = 'var foo = <?php echo json_encode($var, JSON_HEX_TAG, 512) ?>;';
+        $expected = 'var foo = <?php echo e(json_encode($var, JSON_HEX_TAG, 512)) ?>;';
+
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+    }
+
+    public function testStatementIsCompiledWithCorrectEchoFormat()
+    {
+        $this->compiler->withDoubleEncoding();
+        $string = 'var foo = @json($var, JSON_HEX_TAG);';
+        $expected = 'var foo = <?php echo e(json_encode($var, JSON_HEX_TAG, 512), true) ?>;';
 
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
