bbaaa63ad39592bff0bd19c994449656f79b6518
Aran Reeks <3313791+aran112000@users.noreply.github.com>
2023-01-13 16:01:41 +0000

    Bug fix: Prohibited validator should accept empty value
    Bug fix to match the behaviour shown in the Laravel documentation for the `prohibited` validation rule. Presently empty values weren't accepted which goes against the documented behaviour:
    
    https://laravel.com/docs/master/validation#rule-prohibited

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1681,7 +1681,7 @@ trait ValidatesAttributes
     }
 
     /**
-     * Validate that an attribute does not exist.
+     * Validate that an attribute does not exist, or must be empty
      *
      * @param  string  $attribute
      * @param  mixed  $value
@@ -1689,7 +1689,7 @@ trait ValidatesAttributes
      */
     public function validateProhibited($attribute, $value)
     {
-        return false;
+        return empty($value);
     }
 
     /**
