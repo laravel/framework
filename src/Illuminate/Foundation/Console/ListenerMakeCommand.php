a9b9c812224fdb797ba07d5f6a47d195277f38dd
Jess Archer <jess@jessarcher.com>
2023-01-17 17:43:00 +1000

    Formatting

--- src/Illuminate/Foundation/Console/ListenerMakeCommand.php
+++ src/Illuminate/Foundation/Console/ListenerMakeCommand.php
@@ -131,8 +131,8 @@ class ListenerMakeCommand extends GeneratorCommand
     /**
      * Interact further with the user if they were prompted for missing arguments.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
      * @return void
      */
     protected function afterPromptingForMissingArguments(InputInterface $input, OutputInterface $output)
--- src/Illuminate/Foundation/Console/PolicyMakeCommand.php
+++ src/Illuminate/Foundation/Console/PolicyMakeCommand.php
@@ -215,8 +215,8 @@ class PolicyMakeCommand extends GeneratorCommand
     /**
      * Interact further with the user if they were prompted for missing arguments.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
      * @return void
      */
     protected function afterPromptingForMissingArguments(InputInterface $input, OutputInterface $output)
