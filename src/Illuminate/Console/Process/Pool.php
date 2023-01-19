099ff00e4c372ee13dd3d577d2da4f5fa2096a01
Taylor Otwell <taylor@laravel.com>
2023-01-19 13:54:29 -0600

    [10.x] Process DX Layer (#45314)
    * initial process pass
    
    * working on faking
    
    * model fake invoked processes
    
    * continue modeling fake process invocations
    
    * order
    
    * working on recording
    
    * formatting
    
    * Fix variable name
    
    * initial work on pool
    
    * continue work on fakes and pools
    
    * improve testing and hints
    
    * clean up new lines in fakes
    
    * sequences
    
    * update wording
    
    * fix newlines
    
    * fix empty string
    
    * continued work on new lines and fakes
    
    * lint
    
    * clean up logic of replace output
    
    * tests
    
    * allow general start output function on pools
    
    * refactor pool object model
    
    * add output and errorOutput to invoked process
    
    * Apply fixes from StyleCI
    
    * add iterations method
    
    * more tests and collect method
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ src/Illuminate/Console/Process/Pool.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Illuminate\Console\Process;
+
+use InvalidArgumentException;
+
+class Pool
+{
+    /**
+     * The process factory instance.
+     *
+     * @var \Illuminate\Console\Process\Factory
+     */
+    protected $factory;
+
+    /**
+     * The callback that resolves the pending processes.
+     *
+     * @var callable
+     */
+    protected $callback;
+
+    /**
+     * The array of pending processes.
+     *
+     * @var array
+     */
+    protected $pendingProcesses = [];
+
+    /**
+     * Create a new process pool.
+     *
+     * @param  \Illuminate\Console\Process\Factory  $factory
+     * @param  callable  $callback
+     * @return void
+     */
+    public function __construct(Factory $factory, callable $callback)
+    {
+        $this->factory = $factory;
+        $this->callback = $callback;
+    }
+
+    /**
+     * Add a process to the pool with a key.
+     *
+     * @param  string  $key
+     * @return \Illuminate\Console\Process\PendingProcess
+     */
+    public function as(string $key)
+    {
+        return tap($this->factory->newPendingProcess(), function ($pendingProcess) use ($key) {
+            $this->pendingProcesses[$key] = $pendingProcess;
+        });
+    }
+
+    /**
+     * Start all of the processes in the pool.
+     *
+     * @param  callable|null  $output
+     * @return \Illuminate\Console\Process\InvokedProcessPool
+     */
+    public function start(?callable $output = null)
+    {
+        call_user_func($this->callback, $this);
+
+        return new InvokedProcessPool(
+            collect($this->pendingProcesses)
+                ->each(function ($pendingProcess) {
+                    if (! $pendingProcess instanceof PendingProcess) {
+                        throw new InvalidArgumentException('Process pool must only contain pending processes.');
+                    }
+                })->mapWithKeys(function ($pendingProcess, $key) use ($output) {
+                    return [$key => $pendingProcess->start(output: $output ? function ($type, $buffer) use ($key, $output) {
+                        $output($type, $buffer, $key);
+                    } : null)];
+                })
+            ->all()
+        );
+    }
+
+    /**
+     * Start and wait for the processes to finish.
+     *
+     * @return \Illuminate\Console\Process\ProcessPoolResults
+     */
+    public function run()
+    {
+        return $this->wait();
+    }
+
+    /**
+     * Start and wait for the processes to finish.
+     *
+     * @return \Illuminate\Console\Process\ProcessPoolResults
+     */
+    public function wait()
+    {
+        return $this->start()->wait();
+    }
+
+    /**
+     * Dynamically proxy methods calls to a new pending process.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return \Illuminate\Console\Process\PendingProcess
+     */
+    public function __call($method, $parameters)
+    {
+        return tap($this->factory->{$method}(...$parameters), function ($pendingProcess) {
+            $this->pendingProcesses[] = $pendingProcess;
+        });
+    }
+}
