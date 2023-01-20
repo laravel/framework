ae041a36e181f7d97927355c3f7c99fbb10c58ff
Perry van der Meer <11609290+PerryvanderMeer@users.noreply.github.com>
2023-01-20 10:41:17 +0100

    [9.x] fix typo (#45735)

--- src/Illuminate/Encryption/Encrypter.php
+++ src/Illuminate/Encryption/Encrypter.php
@@ -112,7 +112,7 @@ class Encrypter implements EncrypterContract, StringEncrypter
         $tag = base64_encode($tag ?? '');
 
         $mac = self::$supportedCiphers[strtolower($this->cipher)]['aead']
-            ? '' // For AEAD-algoritms, the tag / MAC is returned by openssl_encrypt...
+            ? '' // For AEAD-algorithms, the tag / MAC is returned by openssl_encrypt...
             : $this->hash($iv, $value);
 
         $json = json_encode(compact('iv', 'value', 'mac', 'tag'), JSON_UNESCAPED_SLASHES);
