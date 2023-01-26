5b806f33096ac9744613803dc34ed8792d8f9a66
miladev-ent <milwad.dev@gmail.com>
2023-01-26 11:38:28 +0330

    add compileSum in CompilesArray

--- src/Illuminate/View/Compilers/Concerns/CompilesArray.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesArray.php
@@ -4,5 +4,15 @@ namespace Illuminate\View\Compilers\Concerns;
 
 trait CompilesArray
 {
+    /**
+     * @param  string  $arguments
+     * @return string
+     */
+    protected function compileSum($arguments)
+    {
+        $arguments = str_replace(array('[', ']'), '', $arguments);
+        $arguments = explode(',', $arguments);
 
+        return "<?php echo array_sum($arguments) ?>";
+    }
 }
