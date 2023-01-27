78eb546e9c789ac84af87bb362535cc2883e4db4
Taylor Otwell <taylor@laravel.com>
2023-01-27 14:03:57 +0000

    rename property for better clarity

--- src/Illuminate/Foundation/Http/Kernel.php
+++ src/Illuminate/Foundation/Http/Kernel.php
@@ -66,8 +66,17 @@ class Kernel implements KernelContract
      * The application's route middleware.
      *
      * @var array<string, class-string|string>
+     *
+     * @deprecated
      */
     protected $routeMiddleware = [];
+
+    /**
+     * The application's middleware aliases.
+     *
+     * @var array<string, class-string|string>
+     */
+    protected $middlewareAliases = [];
 
     /**
      * All of the registered request duration handlers.
@@ -445,7 +454,7 @@ class Kernel implements KernelContract
             $this->router->middlewareGroup($key, $middleware);
         }
 
-        foreach ($this->routeMiddleware as $key => $middleware) {
+        foreach (array_merge($this->routeMiddleware, $this->middlewareAliases) as $key => $middleware) {
             $this->router->aliasMiddleware($key, $middleware);
         }
     }
@@ -504,13 +513,25 @@ class Kernel implements KernelContract
     }
 
     /**
-     * Get the application's route middleware.
+     * Get the application's route middleware aliases.
      *
      * @return array
+     *
+     * @deprecated
      */
     public function getRouteMiddleware()
     {
-        return $this->routeMiddleware;
+        return $this->getMiddlewareAliases();
+    }
+
+    /**
+     * Get the application's route middleware aliases.
+     *
+     * @return array
+     */
+    public function getMiddlewareAliases()
+    {
+        return array_merge($this->routeMiddleware, $this->middlewareAliases);
     }
 
     /**
