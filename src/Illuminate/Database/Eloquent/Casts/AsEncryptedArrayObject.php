cd8a333082d5641ce9cb93f63bb066a5406a14f3
Taylor Otwell <taylor@laravel.com>
2023-01-27 17:39:24 -0500

    Update AsEncryptedArrayObject.php

--- src/Illuminate/Database/Eloquent/Casts/AsEncryptedArrayObject.php
+++ src/Illuminate/Database/Eloquent/Casts/AsEncryptedArrayObject.php
@@ -11,6 +11,7 @@ class AsEncryptedArrayObject implements Castable
     /**
      * Get the caster class to use when casting from / to this cast target.
      *
+     * @param  array  $arguments
      * @return CastsAttributes<ArrayObject<array-key, mixed>, iterable>
      */
     public static function castUsing(array $arguments)
