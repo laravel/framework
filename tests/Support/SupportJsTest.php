b3f24016feac04ec97a0436943fa2d713aeb4981
Adrian Nürnberger <adrian@nuernberger.me>
2023-01-30 12:29:59 +0100

    ensure strings are tested

--- tests/Support/SupportJsTest.php
+++ tests/Support/SupportJsTest.php
@@ -16,6 +16,7 @@ class SupportJsTest extends TestCase
         $this->assertSame('true', (string) Js::from(true));
         $this->assertSame('1', (string) Js::from(1));
         $this->assertSame('1.1', (string) Js::from(1.1));
+        $this->assertSame("'Hello world'", (string) Js::from('Hello world'));
         $this->assertEquals(
             "'\\u003Cdiv class=\\u0022foo\\u0022\\u003E\\u0027quoted html\\u0027\\u003C\\/div\\u003E'",
             (string) Js::from('<div class="foo">\'quoted html\'</div>')
