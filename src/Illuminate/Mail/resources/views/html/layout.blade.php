f1da1922052853e6df005f6a9675f78ce366ca66
Ankur Kumar <ankurk91@users.noreply.github.com>
2023-01-30 10:25:28 +0530

    Add title tag in mail template

--- src/Illuminate/Mail/resources/views/html/layout.blade.php
+++ src/Illuminate/Mail/resources/views/html/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>{{ config('app.name') }}</title>
 <meta name="color-scheme" content="light">
 <meta name="supported-color-schemes" content="light">
 <style>
