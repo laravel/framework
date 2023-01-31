217e18a04e280f0a0f3a68478c02ea02cb0c33cc
Brandon Eichhorn <101891339+eduance@users.noreply.github.com>
2023-01-31 21:29:10 +0100

    [9.x] - 45782: Solve data to be dumped for separate schemes (#45805)
    * 45782: Allow all schemes to be dumped excluding the migrations table.
    
    * 45782: Allow all schemes to be dumped excluding the migrations table.
    
    * 45782: Allow all schemes to be dumped excluding the migrations table.

--- src/Illuminate/Database/Console/DumpCommand.php
+++ src/Illuminate/Database/Console/DumpCommand.php
@@ -94,7 +94,7 @@ class DumpCommand extends Command
      */
     protected function path(Connection $connection)
     {
-        return tap($this->option('path') ?: database_path('schema/'.$connection->getName().'-schema.dump'), function ($path) {
+        return tap($this->option('path') ?: database_path('schema/'.$connection->getName().'-schema.sql'), function ($path) {
             (new Filesystem)->ensureDirectoryExists(dirname($path));
         });
     }
