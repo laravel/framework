cddd0489949d9868f6aefb1ebf8e144b9810368e
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2023-01-31 22:06:09 -0300

    add support to handle exceptions on Timebox

--- src/Illuminate/Support/Timebox.php
+++ src/Illuminate/Support/Timebox.php
@@ -22,14 +22,23 @@ class Timebox
      */
     public function call(callable $callback, int $microseconds)
     {
+        $exception = null;
         $start = microtime(true);
 
-        $result = $callback($this);
+        try {
+            $result = $callback($this);
+        } catch (\Throwable $caugth) {
+            $exception = $caugth;
+        }
 
         $remainder = intval($microseconds - ((microtime(true) - $start) * 1000000));
 
         if (! $this->earlyReturn && $remainder > 0) {
             $this->usleep($remainder);
+        }
+
+        if ($exception) {
+            throw $exception;
         }
 
         return $result;
