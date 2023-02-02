232c141e1e1cbdfb400e42195c438d28b6da0bb3
Ringle <ringunger@gmail.com>
2023-02-02 16:50:07 +0300

    DocBlocks can be used by IDEs, like PhpStorm, to improve autocomplete suggestions. I normally add the @mixin to all of my models so that my autocompletes work perfectly. I Would like to have this set by default for all models. I Hope this is a good idea. Thanks.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -23,6 +23,9 @@ use Illuminate\Support\Traits\ForwardsCalls;
 use JsonSerializable;
 use LogicException;
 
+/**
+ * @mixin Builder
+ */
 abstract class Model implements Arrayable, ArrayAccess, CanBeEscapedWhenCastToString, HasBroadcastChannel, Jsonable, JsonSerializable, QueueableEntity, UrlRoutable
 {
     use Concerns\HasAttributes,
