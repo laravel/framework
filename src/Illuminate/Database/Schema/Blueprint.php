e8b299f6ac60e82fa65d66529ff87bbab94b52a2
James Hulena <99769087+Jim-Webfox@users.noreply.github.com>
2023-02-04 06:03:58 +1300

    Add creation and update datetime columns
    Add datetimes function to create "created_at" and "updated_at" columns using "datetime" columns instead off "timestamps" columns to add further future proofing.

--- src/Illuminate/Database/Schema/Blueprint.php
+++ src/Illuminate/Database/Schema/Blueprint.php
@@ -1202,6 +1202,19 @@ class Blueprint
 
         $this->timestampTz('updated_at', $precision)->nullable();
     }
+    
+     /**
+     * Add creation and update datetime columns to the table.
+     *
+     * @param  int|null  $precision
+     * @return void
+     */
+    public function datetimes($precision = 0)
+    {
+        $this->datetime('created_at', $precision)->nullable();
+
+        $this->datetime('updated_at', $precision)->nullable();
+    }
 
     /**
      * Add a "deleted at" timestamp for the table.
