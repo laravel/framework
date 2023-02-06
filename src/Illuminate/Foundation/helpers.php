ab437f8525bdff269e053b0e3ba186a8bb95c0a4
Jesper Noordsij <45041769+jnoordsij@users.noreply.github.com>
2023-02-06 16:26:56 +0100

    [10.x] Use common joinPaths function in Application for generating paths (#45968)
    * Use common joinPaths function in Application for generating paths
    
    * formatting
    
    * fix public_path helper
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Foundation/helpers.php
+++ src/Illuminate/Foundation/helpers.php
@@ -628,7 +628,7 @@ if (! function_exists('public_path')) {
      */
     function public_path($path = '')
     {
-        return app()->make('path.public').($path ? DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR) : $path);
+        return app()->joinPaths(app()->make('path.public'), $path);
     }
 }
 
