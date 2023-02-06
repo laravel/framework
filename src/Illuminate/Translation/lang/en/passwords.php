e8bcf11c903c57cc042902d2e2d5ce72c5f6abbd
Tim MacDonald <hello@timacdonald.me>
2023-02-07 02:04:21 +1100

    No yelling (#45973)

--- src/Illuminate/Translation/lang/en/passwords.php
+++ src/Illuminate/Translation/lang/en/passwords.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'reset' => 'Your password has been reset.',
+    'sent' => 'We have emailed your password reset link.',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
