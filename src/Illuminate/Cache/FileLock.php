fc8fe7492dcfd0442610eded5d68c4281a6297c9
Kieran Brahney <kieran@supportpal.com>
2023-02-08 11:42:23 +0000

    wip

--- src/Illuminate/Cache/FileLock.php
+++ src/Illuminate/Cache/FileLock.php
@@ -5,29 +5,6 @@ namespace Illuminate\Cache;
 class FileLock extends CacheLock
 {
     /**
-     * The cache store implementation.
-     *
-     * @var FileStore
-     */
-    protected $store;
-
-    /**
-     * Create a new lock instance.
-     *
-     * @param  FileStore  $store
-     * @param  string  $name
-     * @param  int  $seconds
-     * @param  string|null  $owner
-     * @return void
-     */
-    public function __construct(FileStore $store, $name, $seconds, $owner = null)
-    {
-        parent::__construct($name, $seconds, $owner);
-
-        $this->store = $store;
-    }
-
-    /**
      * Attempt to acquire the lock.
      *
      * @return bool
