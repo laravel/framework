f935d5c50f8953d4a6fef5ff5bb195fa917160bb
guizoxxv <guizoxxv@gmail.com>
2023-02-07 19:22:47 -0500

    Add MessageBag remove method

--- src/Illuminate/Support/MessageBag.php
+++ src/Illuminate/Support/MessageBag.php
@@ -79,6 +79,19 @@ class MessageBag implements Jsonable, JsonSerializable, MessageBagContract, Mess
     }
 
     /**
+     * Remove a message from the message bag.
+     *
+     * @param  string  $key
+     * @return $this
+     */
+    public function remove($key)
+    {
+        unset($this->messages[$key]);
+
+        return $this;
+    }
+
+    /**
      * Determine if a key and message combination already exists.
      *
      * @param  string  $key
