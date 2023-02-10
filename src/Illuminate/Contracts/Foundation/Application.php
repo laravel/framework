1bff2c132f9772b02b9f48454ca38b769d153ee2
Taka Oyama <toyama4649@hotmail.com>
2023-02-10 14:42:13 +0900

    fix: add hasDebugModeEnabled to Application Contract

--- src/Illuminate/Contracts/Foundation/Application.php
+++ src/Illuminate/Contracts/Foundation/Application.php
@@ -100,6 +100,13 @@ interface Application extends Container
     public function runningUnitTests();
 
     /**
+     * Determine if the application is running with debug mode enabled.
+     *
+     * @return bool
+     */
+    public function hasDebugModeEnabled();
+
+    /**
      * Get an instance of the maintenance mode manager implementation.
      *
      * @return \Illuminate\Contracts\Foundation\MaintenanceMode
