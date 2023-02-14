abfe0f1c4d7b1ef82b5cee38a4939776aee9d90c
foremtehan <53290883+foremtehan@users.noreply.github.com>
2023-02-14 21:12:41 +0330

    Update Str.php

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -735,11 +735,7 @@ class Str
     {
         return (new Collection)
                 ->when($letters, fn ($c) => $c->merge([
-                    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                    'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-                    'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                    array_merge(range('a', 'z'), range('A', 'Z')
                 ]))
                 ->when($numbers, fn ($c) => $c->merge([
                     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
