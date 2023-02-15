8bb4e57b99767bc98c9fc7e842cdcc07511041b2
s4muel <s4muel@users.noreply.github.com>
2023-02-15 15:49:30 +0100

    add AddQueuedCookiesToResponse to middlewarePriority so it is handled in the right place (#46130)
    * add AddQueuedCookiesToResponse to middlewarePriority so it is handled in the right place
    
    Hi there, i was trying to set `session storage` to `cookie`, but the cookies were not set at all. after a bit of search, this came up: https://github.com/laravel/framework/issues/43693
    and now i know what he meant. the queued cookies (https://github.com/laravel/framework/blob/10.x/src/Illuminate/Session/CookieSessionHandler.php#L101) never got sent back to the browser
    
    adding the middleware to the list so it is priority-sorted seems to resolve this issue (and fixes #43693)
    targets both 9.x and 10.x versions
    
    * update test

--- tests/Foundation/Http/KernelTest.php
+++ tests/Foundation/Http/KernelTest.php
@@ -31,6 +31,7 @@ class KernelTest extends TestCase
         $this->assertEquals([
             \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests::class,
