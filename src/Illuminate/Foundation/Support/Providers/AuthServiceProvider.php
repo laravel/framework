132446f87346ca16777c042e46401084427edcd1
Taylor Otwell <taylor@laravel.com>
2023-02-16 13:23:48 -0600

    formatting

--- src/Illuminate/Foundation/Support/Providers/AuthServiceProvider.php
+++ src/Illuminate/Foundation/Support/Providers/AuthServiceProvider.php
@@ -22,9 +22,29 @@ class AuthServiceProvider extends ServiceProvider
     public function register()
     {
         $this->booting(function () {
-            foreach ($this->policies as $model => $policy) {
-                Gate::policy($model, $policy);
-            }
+            $this->registerPolicies();
         });
     }
+
+    /**
+     * Register the application's policies.
+     *
+     * @return void
+     */
+    public function registerPolicies()
+    {
+        foreach ($this->policies() as $model => $policy) {
+             Gate::policy($model, $policy);
+         }
+    }
+
+    /**
+      * Get the policies defined on the provider.
+      *
+      * @return array<class-string, class-string>
+      */
+     public function policies()
+     {
+         return $this->policies;
+     }
 }
