f7ba894a7a758e3fc861bfa5fb39558550dbb997
Dries Vints <dries@vints.be>
2023-02-23 09:41:52 +0100

    Update MySqlGrammar.php

--- src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php
@@ -5,8 +5,6 @@ namespace Illuminate\Database\Schema\Grammars;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Fluent;
 use RuntimeException;
 
@@ -378,6 +376,7 @@ class MySqlGrammar extends Grammar
     public function compileDropColumn(Blueprint $blueprint, Fluent $command)
     {
         $columns = $this->prefixArray('drop column if exists', $this->wrapArray($command->columns));
+
         return 'alter table '.$this->wrapTable($blueprint).' '.implode(', ', $columns);
     }
 
