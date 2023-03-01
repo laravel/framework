6df5199fdb0ac2e1f8d384008872e88f593fa56c
Inani El Houssain <inanielhoussain@gmail.com>
2023-03-01 14:21:58 +0100

    Create SingletonController.php

---
+++ src/Illuminate/Routing/SingletonController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Illuminate\Routing;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+
+
+
+class SingletonController extends Controller
+{
+    use AuthorizesRequests, ValidatesRequests;
+  
+    
+    /**
+     * Show the form for creating the resource.
+     */
+    public function create(): never
+    {
+        abort(404);
+    }
+
+    /**
+     * Store the newly created resource in storage.
+     */
+    public function store(Request $request): never
+    {
+        abort(404);
+    }
+
+
+    /**
+     * Remove the resource from storage.
+     */
+    public function destroy(): never
+    {
+        abort(404);
+    }
+}
