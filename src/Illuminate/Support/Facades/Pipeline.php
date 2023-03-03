4ae98efe5caa2eb42b4ed56e85b18517bd492ad4
Jae Toole <61322528+jaetoole@users.noreply.github.com>
2023-03-03 15:55:44 +0000

    [10.x] Adding Pipeline Facade (#46271)
    * feat: added in pipeline facade, added singleton binding
    
    * Changed singleton to simple bind
    
    * chore: disabled caching of instance on pipeline facade, added in docblocks
    
    * formatting
    
    * Update facade docblocks
    
    ---------
    
    Co-authored-by: jaetooledev <jaetooledev@gmail.com>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>
    Co-authored-by: taylorotwell <taylorotwell@users.noreply.github.com>

---
+++ src/Illuminate/Support/Facades/Pipeline.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Illuminate\Support\Facades;
+
+class Pipeline extends Facade
+{
+    /**
+     * Indicates if the resolved instance should be cached.
+     *
+     * @var bool
+     */
+    protected static $cached = false;
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'pipeline';
+    }
+}
