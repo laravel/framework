8659cdffd18b03a24d68500c06961aa91d73f011
Tim MacDonald <hello@timacdonald.me>
2023-03-06 09:25:27 +1100

    adds method docblocks to Pipeline facade

--- src/Illuminate/Support/Facades/Pipeline.php
+++ src/Illuminate/Support/Facades/Pipeline.php
@@ -2,6 +2,17 @@
 
 namespace Illuminate\Support\Facades;
 
+/**
+ * @method static \Illuminate\Pipeline\Pipeline send(mixed $passable)
+ * @method static \Illuminate\Pipeline\Pipeline through(array|mixed $pipes)
+ * @method static \Illuminate\Pipeline\Pipeline pipe(array|mixed $pipes)
+ * @method static \Illuminate\Pipeline\Pipeline via(string $method)
+ * @method static mixed then(\Closure $destination)
+ * @method static mixed thenReturn()
+ * @method static \Illuminate\Pipeline\Pipeline setContainer(\Illuminate\Contracts\Container\Container $container)
+ *
+ * @see \Illuminate\Pipeline\Pipeline
+ */
 class Pipeline extends Facade
 {
     /**
