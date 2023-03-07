9080d894f31a43fbcd2456e20ec570ca487fa4cf
miladev-ent <milwad.dev@gmail.com>
2023-03-07 14:36:13 +0330

    fix ci

--- src/Illuminate/Database/Eloquent/Builder.php
+++ src/Illuminate/Database/Eloquent/Builder.php
@@ -306,7 +306,7 @@ class Builder implements BuilderContract
      * @param  mixed  $operator
      * @param  mixed  $value
      * @param  string  $boolean
-     * @param array|string  $columns
+     * @param  array|string  $columns
      * @return \Illuminate\Database\Eloquent\Model|static|null
      */
     public function firstWhere($column, $operator = null, $value = null, $boolean = 'and', $columns = ['*'])
