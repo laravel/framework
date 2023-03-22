aa7326949e1a85f13c13d5188a2d8332cf349d34
Taylor Otwell <taylor@laravel.com>
2023-03-22 17:51:14 -0400

    add json encoder and decoder used by casts

---
+++ src/Illuminate/Database/Eloquent/Casts/Json.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Illuminate\Database\Eloquent\Casts;
+
+class Json
+{
+    /**
+     * The custom JSON encoder.
+     *
+     * @var callable|null
+     */
+    protected $encoder;
+
+    /**
+     * The custom JSON decode.
+     *
+     * @var callable|null
+     */
+    protected $decoder;
+
+    /**
+     * Encode the given value.
+     */
+    public static function encode(mixed $value): mixed
+    {
+        return isset(static::$encoder) ? (static::$encoder)($value) : json_encode($value);
+    }
+
+    /**
+     * Decode the given value.
+     */
+    public static function decode(mixed $value): mixed
+    {
+        return isset(static::$decoder) ? (static::$decoder)($value) : json_decode($value, true);
+    }
+
+    /**
+     * Encode all values using the given callable.
+     */
+    public static function encodeUsing(?callable $encoder): void
+    {
+        static::$encoder = $encoder;
+    }
+
+    /**
+     * Decode all values using the given callable.
+     */
+    public static function decodeUsing(?callable $decoder): void
+    {
+        static::$decoder = $decoder;
+    }
+}
