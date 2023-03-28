e14d28c0f9403630d13f308bb43f3d3cb73d6d67
Taylor Otwell <taylor@laravel.com>
2023-03-28 14:03:54 -0400

    wip

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -38,7 +38,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '9.52.4';
+    const VERSION = '9.52.5';
 
     /**
      * The base path for the Laravel installation.
