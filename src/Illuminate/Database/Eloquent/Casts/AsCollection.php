99c1465fd433963dfdd0552b806b2e53ea5664f5
Ostap Brehin <osbre@protonmail.com>
2023-03-29 14:16:44 +0100

    Add check for a collection class

--- src/Illuminate/Database/Eloquent/Casts/AsCollection.php
+++ src/Illuminate/Database/Eloquent/Casts/AsCollection.php
@@ -5,6 +5,7 @@ namespace Illuminate\Database\Eloquent\Casts;
 use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Support\Collection;
+use InvalidArgumentException;
 
 class AsCollection implements Castable
 {
@@ -34,6 +35,10 @@ class AsCollection implements Castable
                 $data = Json::decode($attributes[$key]);
 
                 $collectionClass = $this->arguments[0] ?? Collection::class;
+
+                if (! is_subclass_of($collectionClass, Collection::class)) {
+                    throw new InvalidArgumentException('The provided class must be a Collection');
+                }
 
                 return is_array($data) ? new $collectionClass($data) : null;
             }
