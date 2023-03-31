f02a3e8420c6430b6154dff11b9008f86479f555
William Trevisan <william.trevisan@melhorenvio.com>
2023-03-31 17:10:00 -0300

    allows  receive closure in retry method from PendingRequest

--- src/Illuminate/Http/Client/PendingRequest.php
+++ src/Illuminate/Http/Client/PendingRequest.php
@@ -131,7 +131,7 @@ class PendingRequest
     /**
      * The number of milliseconds to wait between retries.
      *
-     * @var int
+     * @var Closure|int
      */
     protected $retryDelay = 100;
 
@@ -557,12 +557,12 @@ class PendingRequest
      * Specify the number of times the request should be attempted.
      *
      * @param  int  $times
-     * @param  int  $sleepMilliseconds
+     * @param  Closure|int  $sleepMilliseconds
      * @param  callable|null  $when
      * @param  bool  $throw
      * @return $this
      */
-    public function retry(int $times, int $sleepMilliseconds = 0, ?callable $when = null, bool $throw = true)
+    public function retry(int $times, Closure|int $sleepMilliseconds = 0, ?callable $when = null, bool $throw = true)
     {
         $this->tries = $times;
         $this->retryDelay = $sleepMilliseconds;
