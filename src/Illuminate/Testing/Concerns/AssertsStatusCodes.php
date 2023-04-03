f282c6d6478bb6662d81140fd324baee12e815a8
miladev-ent <milwad.dev@gmail.com>
2023-04-03 23:20:44 +0330

    add `assertGone` in AssertsStatusCodes

--- src/Illuminate/Testing/Concerns/AssertsStatusCodes.php
+++ src/Illuminate/Testing/Concerns/AssertsStatusCodes.php
@@ -144,6 +144,16 @@ trait AssertsStatusCodes
     }
 
     /**
+     * Assert that the response has a 410 "Gone" status code.
+     *
+     * @return $this
+     */
+    public function assertGone()
+    {
+        return $this->assertStatus(409);
+    }
+
+    /**
      * Assert that the response has a 415 "Unsupported Media Type" status code.
      *
      * @return $this
