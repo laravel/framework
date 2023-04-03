5c9656ed2ddd74ac84b711b231d100fea75446e5
Ostap Brehin <osbre@protonmail.com>
2023-04-03 20:57:27 +0100

    [10.x] Add ability to set a custom class for the `AsCollection` and `AsEncryptedCollection` casts (#46619)
    * Add ability to set a custom collection class for the `AsCollection` cast
    
    * Fix StyleCI
    
    * Pass all arguments instead of a collection class name
    
    * Ability to set a custom collection class in `AsEncryptedCollection` cast
    
    * Add check for a collection class
    
    * Revert "Add check for a collection class"
    
    This reverts commit 99c1465fd433963dfdd0552b806b2e53ea5664f5.
    
    * Switch to PHP 8 constructor property promotion syntax
    
    * Add check for a collection class
    
    * formatting
    
    * Update AsCollection.php
    
    * Update AsEncryptedCollection.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Database/Eloquent/Casts/AsCollection.php
+++ src/Illuminate/Database/Eloquent/Casts/AsCollection.php
@@ -5,6 +5,7 @@ namespace Illuminate\Database\Eloquent\Casts;
 use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Support\Collection;
+use InvalidArgumentException;
 
 class AsCollection implements Castable
 {
@@ -16,8 +17,12 @@ class AsCollection implements Castable
      */
     public static function castUsing(array $arguments)
     {
-        return new class implements CastsAttributes
+        return new class($arguments) implements CastsAttributes
         {
+            public function __construct(protected array $arguments)
+            {
+            }
+
             public function get($model, $key, $value, $attributes)
             {
                 if (! isset($attributes[$key])) {
@@ -26,7 +31,13 @@ class AsCollection implements Castable
 
                 $data = Json::decode($attributes[$key]);
 
-                return is_array($data) ? new Collection($data) : null;
+                $collectionClass = $this->arguments[0] ?? Collection::class;
+
+                if (! is_a($collectionClass, Collection::class, true)) {
+                    throw new InvalidArgumentException('The provided class must extend ['.Collection::class.'].');
+                }
+
+                return is_array($data) ? new $collectionClass($data) : null;
             }
 
             public function set($model, $key, $value, $attributes)
--- src/Illuminate/Database/Eloquent/Casts/AsEncryptedCollection.php
+++ src/Illuminate/Database/Eloquent/Casts/AsEncryptedCollection.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Crypt;
+use InvalidArgumentException;
 
 class AsEncryptedCollection implements Castable
 {
@@ -17,12 +18,22 @@ class AsEncryptedCollection implements Castable
      */
     public static function castUsing(array $arguments)
     {
-        return new class implements CastsAttributes
+        return new class($arguments) implements CastsAttributes
         {
+            public function __construct(protected array $arguments)
+            {
+            }
+
             public function get($model, $key, $value, $attributes)
             {
+                $collectionClass = $this->arguments[0] ?? Collection::class;
+
+                if (! is_a($collectionClass, Collection::class, true)) {
+                    throw new InvalidArgumentException('The provided class must extend ['.Collection::class.'].');
+                }
+
                 if (isset($attributes[$key])) {
-                    return new Collection(Json::decode(Crypt::decryptString($attributes[$key])));
+                    return new $collectionClass(Json::decode(Crypt::decryptString($attributes[$key])));
                 }
 
                 return null;
