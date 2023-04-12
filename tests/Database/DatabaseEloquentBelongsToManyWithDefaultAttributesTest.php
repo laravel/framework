91393de122513fdc83f3b494a10700cab7cccce5
bert-w <bertwijnhoven@hotmail.com>
2023-04-12 19:13:59 +0200

    styleci

--- tests/Database/DatabaseEloquentBelongsToManyWithDefaultAttributesTest.php
+++ tests/Database/DatabaseEloquentBelongsToManyWithDefaultAttributesTest.php
@@ -53,7 +53,7 @@ class DatabaseEloquentBelongsToManyWithDefaultAttributesTest extends TestCase
         $related->shouldReceive('qualifyColumn')->with('id')->andReturn('users.id');
 
         $builder->shouldReceive('join')->once()->with(
-            m::on( fn ($arg) => is_string($arg)), m::on( fn($arg) => is_callable($arg))
+            m::on(fn ($arg) => is_string($arg)), m::on(fn ($arg) => is_callable($arg))
         );
         $builder->shouldReceive('where')->once()->with('club_user.club_id', '=', 1);
         $builder->shouldReceive('where')->once()->with('club_user.is_admin', '=', 1, 'and');
