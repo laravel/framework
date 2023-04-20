9f1b321350ed144c451a9633b52f3554da52a170
Taylor Otwell <taylor@laravel.com>
2023-04-20 13:39:02 -0500

    add base middleware for guest, dont use redirect to on json requests by default

---
+++ src/Illuminate/Auth/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Illuminate\Auth\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$guards): Response
+    {
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                return redirect($this->redirectTo($request));
+            }
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Get the path the user should be redirected to when they are authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        return '/home';
+    }
+}
