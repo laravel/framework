6c7056ea10f80ac3774be2aba5f88859b52fbd26
Christian Rishøj <christian@rishoj.net>
2023-04-28 19:01:14 +0200

    follow default PHP behavior and replace invalid codepoints

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -120,7 +120,7 @@ if (! function_exists('e')) {
             $value = $value->value;
         }
 
-        return htmlspecialchars($value ?? '', ENT_QUOTES, 'UTF-8', $doubleEncode);
+        return htmlspecialchars($value ?? '', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', $doubleEncode);
     }
 }
 
