8de1aa2728785ce8bf94e7db92500e62df2cef5e
Kieran <kieran@supportpal.com>
2023-05-01 17:05:23 +0100

    [10.x] Mark commands as isolatable (#46925)
    * Ability to set default for --isolated option
    
    * set default exit code
    
    * Update Command.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Console/Command.php
+++ src/Illuminate/Console/Command.php
@@ -62,6 +62,20 @@ class Command extends SymfonyCommand
     protected $hidden = false;
 
     /**
+     * Indicates whether only one instance of the command can run at any given time.
+     *
+     * @var bool
+     */
+    protected $isolated = false;
+
+    /**
+     * The default exit code for isolated commands.
+     *
+     * @var int
+     */
+    protected $isolatedExitCode = self::SUCCESS;
+
+    /**
      * The console command name aliases.
      *
      * @var array
@@ -140,7 +154,7 @@ class Command extends SymfonyCommand
             null,
             InputOption::VALUE_OPTIONAL,
             'Do not run the command if another instance of the command is already running',
-            false
+            $this->isolated
         ));
     }
 
@@ -185,7 +199,7 @@ class Command extends SymfonyCommand
 
             return (int) (is_numeric($this->option('isolated'))
                         ? $this->option('isolated')
-                        : self::SUCCESS);
+                        : $this->isolatedExitCode);
         }
 
         $method = method_exists($this, 'handle') ? 'handle' : '__invoke';
