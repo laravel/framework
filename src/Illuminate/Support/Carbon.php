7746ce9ba74e41c868bd4f92812c77dac209115a
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2023-05-09 21:10:53 +0200

    Add dd and dump methods to Carbon

--- src/Illuminate/Support/Carbon.php
+++ src/Illuminate/Support/Carbon.php
@@ -5,6 +5,7 @@ namespace Illuminate\Support;
 use Carbon\Carbon as BaseCarbon;
 use Carbon\CarbonImmutable as BaseCarbonImmutable;
 use Illuminate\Support\Traits\Conditionable;
+use Symfony\Component\VarDumper\VarDumper;
 
 class Carbon extends BaseCarbon
 {
@@ -18,4 +19,27 @@ class Carbon extends BaseCarbon
         BaseCarbon::setTestNow($testNow);
         BaseCarbonImmutable::setTestNow($testNow);
     }
+	
+	/**
+	 * Dump the instance and end the script.
+	 *
+	 * @param  mixed  ...$args
+	 * @return never
+	 */
+	public function dd(...$args)
+	{
+		dd($this, ...$args);
+	}
+	
+	/**
+	 * Dump the instance.
+	 *
+	 * @return $this
+	 */
+	public function dump()
+	{
+		dump($this);
+		
+		return $this;
+	}
 }
