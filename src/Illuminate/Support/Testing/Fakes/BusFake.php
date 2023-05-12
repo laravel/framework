3050326d2eec94d4a3a58d27ef4c31ad78c0523b
miladev-ent <milwad.dev@gmail.com>
2023-05-12 21:00:45 +0330

    fix typo `except` method in BusFake

--- src/Illuminate/Support/Testing/Fakes/BusFake.php
+++ src/Illuminate/Support/Testing/Fakes/BusFake.php
@@ -90,7 +90,7 @@ class BusFake implements Fake, QueueingDispatcher
      * Specify the jobs that should be dispatched instead of faked.
      *
      * @param  array|string  $jobsToDispatch
-     * @return void
+     * @return $this
      */
     public function except($jobsToDispatch)
     {
