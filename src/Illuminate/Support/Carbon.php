91d8542c080dbfb61c353cdd16e34b791dc04de4
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2023-05-11 23:30:38 -0400

    [10.x] Adds ability to retrieve timestamp as Carbon instance from UUID/ULID.

--- src/Illuminate/Support/Carbon.php
+++ src/Illuminate/Support/Carbon.php
@@ -5,6 +5,10 @@ namespace Illuminate\Support;
 use Carbon\Carbon as BaseCarbon;
 use Carbon\CarbonImmutable as BaseCarbonImmutable;
 use Illuminate\Support\Traits\Conditionable;
+use Ramsey\Uuid\Exception\InvalidUuidStringException;
+use Ramsey\Uuid\Exception\UnsupportedOperationException;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Uid\Ulid;
 
 class Carbon extends BaseCarbon
 {
@@ -17,6 +21,31 @@ class Carbon extends BaseCarbon
     {
         BaseCarbon::setTestNow($testNow);
         BaseCarbonImmutable::setTestNow($testNow);
+    }
+
+    /**
+     * Creates a Carbon instance from the ULID or UUID.
+     *
+     * Returns "null" if there is no timestamp, or "false" if the UID is invalid.
+     *
+     * @param  \Ramsey\Uuid\Uuid|\Symfony\Component\Uid\Ulid|string  $uid
+     * @return \Illuminate\Support\Carbon|null|false
+     */
+    public static function createFromUid($uid)
+    {
+        if (Ulid::isValid($uid)) {
+            return static::createFromInterface(Ulid::fromString($uid)->getDateTime());
+        }
+
+        try {
+            $date = Uuid::fromString($uid)->getDateTime();
+        } catch (InvalidUuidStringException) {
+            return false;
+        } catch (UnsupportedOperationException) {
+            return null;
+        }
+
+        return static::createFromInterface($date);
     }
 
     /**
