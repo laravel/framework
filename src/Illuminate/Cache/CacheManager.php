ffd5b97c743f0b598008686ece6d63a717f09202
Milwad <98118400+milwad-dev@users.noreply.github.com>
2023-05-14 05:36:24 +0330

    fix style code of `resolve` method in CacheManager (#47069)

--- src/Illuminate/Cache/CacheManager.php
+++ src/Illuminate/Cache/CacheManager.php
@@ -90,15 +90,15 @@ class CacheManager implements FactoryContract
 
         if (isset($this->customCreators[$config['driver']])) {
             return $this->callCustomCreator($config);
-        } else {
-            $driverMethod = 'create'.ucfirst($config['driver']).'Driver';
-
-            if (method_exists($this, $driverMethod)) {
-                return $this->{$driverMethod}($config);
-            } else {
-                throw new InvalidArgumentException("Driver [{$config['driver']}] is not supported.");
-            }
         }
+
+        $driverMethod = 'create'.ucfirst($config['driver']).'Driver';
+
+        if (method_exists($this, $driverMethod)) {
+            return $this->{$driverMethod}($config);
+        }
+
+        throw new InvalidArgumentException("Driver [{$config['driver']}] is not supported.");
     }
 
     /**
