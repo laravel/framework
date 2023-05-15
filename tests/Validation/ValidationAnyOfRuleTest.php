f1340602d2c550a0b4378c36102c3cf94dbfba8a
Thomas Omweri <thomaso@ringier.co.za>
2023-05-15 09:22:44 +0300

    Resolve tests

--- tests/Validation/ValidationAnyOfRuleTest.php
+++ tests/Validation/ValidationAnyOfRuleTest.php
@@ -2,7 +2,12 @@
 
 namespace Illuminate\Tests\Validation;
 
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
 use Illuminate\Validation\Rules\AnyOf;
+use Illuminate\Validation\ValidationServiceProvider;
 use Illuminate\Validation\Validator;
 use PHPUnit\Framework\TestCase;
 
@@ -16,7 +21,7 @@ class ValidationAnyOfRuleTest extends TestCase
                 'value' => 'Laravel',
             ],
             [
-                'value' => new AnyOf(['string', 'decimal', 'integer']),
+                'value' => new AnyOf(['string', 'decimal:2', 'integer']),
             ]
         );
 
@@ -31,10 +36,34 @@ class ValidationAnyOfRuleTest extends TestCase
                 'value' => ['Laravel', 'Other'],
             ],
             [
-                'value' => new AnyOf(['string', 'decimal', 'integer']),
+                'value' => new AnyOf(['string', 'decimal:2', 'integer']),
             ]
         );
 
         $this->assertFalse($validator->passes());
+    }
+
+    protected function setUp(): void
+    {
+        $container = Container::getInstance();
+
+        $container->bind('translator', function () {
+            return new Translator(
+                new ArrayLoader, 'en'
+            );
+        });
+
+        Facade::setFacadeApplication($container);
+
+        (new ValidationServiceProvider($container))->register();
+    }
+
+    protected function tearDown(): void
+    {
+        Container::setInstance(null);
+
+        Facade::clearResolvedInstances();
+
+        Facade::setFacadeApplication(null);
     }
 }
--- tests/Validation/ValidationOrRuleTest.php
+++ tests/Validation/ValidationOrRuleTest.php
@@ -2,7 +2,12 @@
 
 namespace Illuminate\Tests\Validation;
 
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
 use Illuminate\Validation\Rules\OrRule;
+use Illuminate\Validation\ValidationServiceProvider;
 use Illuminate\Validation\Validator;
 use PHPUnit\Framework\TestCase;
 
@@ -36,5 +41,29 @@ class ValidationOrRuleTest extends TestCase
         );
 
         $this->assertFalse($validator->passes());
+    }
+
+    protected function setUp(): void
+    {
+        $container = Container::getInstance();
+
+        $container->bind('translator', function () {
+            return new Translator(
+                new ArrayLoader, 'en'
+            );
+        });
+
+        Facade::setFacadeApplication($container);
+
+        (new ValidationServiceProvider($container))->register();
+    }
+
+    protected function tearDown(): void
+    {
+        Container::setInstance(null);
+
+        Facade::clearResolvedInstances();
+
+        Facade::setFacadeApplication(null);
     }
 }
