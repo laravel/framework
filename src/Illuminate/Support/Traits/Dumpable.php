42df6164c72e365dc5f92263576b87101fcb6c8f
Nuno Maduro <enunomaduro@gmail.com>
2023-05-17 13:13:56 -0500

    Adds `Support\Traits\Dumpable`

---
+++ src/Illuminate/Support/Traits/Dumpable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Illuminate\Support\Traits;
+
+use Symfony\Component\VarDumper\VarDumper;
+
+trait Dumpable
+{
+    /**
+     * Dump the instance and end the script.
+     *
+     * @param  mixed  ...$args
+     * @return never
+     */
+    public function dd(...$args)
+    {
+        $this->dump(...$args);
+
+        dd();
+    }
+
+    /**
+     * Dump the instance.
+     *
+     * @param  mixed  ...$args
+     * @return $this
+     */
+    public function dump(...$args)
+    {
+        foreach ($args as $value) {
+            VarDumper::dump($value);
+        }
+
+        return $this;
+    }
+}
