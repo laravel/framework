21e14d614bb6c9ec6dc60b9754c53dc6841b539c
Taylor Otwell <taylor@laravel.com>
2023-05-21 16:40:27 -0500

    allow further middleware customization

--- src/Illuminate/Foundation/Configuration/Middleware.php
+++ src/Illuminate/Foundation/Configuration/Middleware.php
@@ -2,11 +2,150 @@
 
 namespace Illuminate\Foundation\Configuration;
 
+use Illuminate\Support\Arr;
+
 class Middleware
 {
+    /**
+     * The middleware that should be prepended to the global middleware stack.
+     *
+     * @var array
+     */
+    protected $prepends = [];
+
+    /**
+     * The middleware that should be appended to the global middleware stack.
+     *
+     * @var array
+     */
+    protected $appends = [];
+
+    /**
+     * The middleware that should be prepended to the specified groups.
+     *
+     * @var array
+     */
+    protected $groupPrepends = [];
+
+    /**
+     * The middleware that should be appended to the specified groups.
+     *
+     * @var array
+     */
+    protected $groupAppends = [];
+
+    /**
+     * The default middleware aliases.
+     *
+     * @var array
+     */
+    protected $aliases = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    ];
+
+    /**
+     * The custom middleware aliases.
+     *
+     * @var array
+     */
+    protected $customAliases = [];
+
+    /**
+     * Prepend middleware to the application's global middleware stack.
+     *
+     * @param  array|string  $middleware
+     * @return $this
+     */
+    public function prepend(array|string $middleware)
+    {
+        $this->prepends = array_merge(
+            Arr::wrap($middleware),
+            $this->prepends
+        );
+
+        return $this;
+    }
+
+    /**
+     * Append middleware to the application's global middleware stack.
+     *
+     * @param  array|string  $middleware
+     * @return $this
+     */
+    public function append(array|string $middleware)
+    {
+        $this->appends = array_merge(
+            $this->appends,
+            Arr::wrap($middleware)
+        );
+
+        return $this;
+    }
+
+    /**
+     * Prepend the given middleware to the specified group.
+     *
+     * @param  string  $group
+     * @param  array|string  $middleware
+     * @return $this
+     */
+    public function prependToGroup(string $group, array|string $middleware)
+    {
+        $this->groupPrepends[$group] = array_merge(
+            Arr::wrap($middleware),
+            $this->groupPrepends[$group] ?? []
+        );
+
+        return $this;
+    }
+
+    /**
+     * Append the given middleware to the specified group.
+     *
+     * @param  string  $group
+     * @param  array|string  $middleware
+     * @return $this
+     */
+    public function appendToGroup(string $group, array|string $middleware)
+    {
+        $this->groupAppends[$group] = array_merge(
+            Arr::wrap($middleware),
+            $this->groupAppends[$group] ?? []
+        );
+
+        return $this;
+    }
+
+    /**
+     * Register additional middleware aliases.
+     *
+     * @param  array  $aliases
+     * @return $this
+     */
+    public function withAliases(array $aliases)
+    {
+        $this->customAliases = $aliases;
+
+        return $this;
+    }
+
+    /**
+     * Get the global middleware.
+     *
+     * @return array
+     */
     public function getGlobalMiddleware()
     {
-        return [
+        $middleware = [
             // \Illuminate\Http\Middleware\TrustHosts::class,
             \Illuminate\Http\Middleware\TrustProxies::class,
             \Illuminate\Http\Middleware\HandleCors::class,
@@ -15,11 +154,20 @@ class Middleware
             \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
             \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         ];
+
+        return array_values(array_filter(
+            array_unique(array_merge($this->prepends, $middleware, $this->appends))
+        ));
     }
 
+    /**
+     * Get the middleware groups.
+     *
+     * @return array
+     */
     public function getMiddlewareGroups()
     {
-        return [
+        $middleware = [
             'web' => [
                 \Illuminate\Cookie\Middleware\EncryptCookies::class,
                 \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
@@ -35,21 +183,29 @@ class Middleware
                 \Illuminate\Routing\Middleware\SubstituteBindings::class,
             ],
         ];
+
+        foreach ($this->groupPrepends as $group => $prepends) {
+            $middleware[$group] = array_values(array_filter(
+                array_unique(array_merge($prepends, $middleware[$group] ?? []))
+            ));
+        }
+
+        foreach ($this->groupAppends as $group => $appends) {
+            $middleware[$group] = array_values(array_filter(
+                array_unique(array_merge($middleware[$group] ?? [], $appends))
+            ));
+        }
+
+        return $middleware;
     }
 
+    /**
+     * Get the middleware aliases.
+     *
+     * @return array
+     */
     public function getMiddlewareAliases()
     {
-        return [
-            'auth' => \App\Http\Middleware\Authenticate::class,
-            'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-            'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
-            'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-            'can' => \Illuminate\Auth\Middleware\Authorize::class,
-            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-            'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-            'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        ];
+        return array_merge($this->aliases, $this->customAliases);
     }
 }
