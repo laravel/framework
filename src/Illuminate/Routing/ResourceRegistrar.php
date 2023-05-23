5dd3717aa18cf5250906d7c7fafa36deb21aa546
Milwad <98118400+milwad-dev@users.noreply.github.com>
2023-05-23 21:47:17 +0330

    [10.x] Remove useless else statements (#47186)
    * remove useless else statement in `acquire` method in RedisLock
    
    * remove useless else statement in `verbs` method in ResourceRegistrar
    
    * remove useless else statement in `createDriver` method in Manager

--- src/Illuminate/Routing/ResourceRegistrar.php
+++ src/Illuminate/Routing/ResourceRegistrar.php
@@ -708,8 +708,8 @@ class ResourceRegistrar
     {
         if (empty($verbs)) {
             return static::$verbs;
-        } else {
-            static::$verbs = array_merge(static::$verbs, $verbs);
         }
+
+        static::$verbs = array_merge(static::$verbs, $verbs);
     }
 }
