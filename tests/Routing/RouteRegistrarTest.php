dc141b47b5ecd67e78d305dc586d5c52f04bb14b
Milwad <98118400+milwad-dev@users.noreply.github.com>
2023-05-30 17:45:36 +0330

    [10.x] Add test for `withTrashed` method in Routing (#47286)
    * add test for `withTrashed` method in Routing
    
    * add test for `resource route withTrashed` method in Routing

--- tests/Routing/RouteRegistrarTest.php
+++ tests/Routing/RouteRegistrarTest.php
@@ -136,6 +136,28 @@ class RouteRegistrarTest extends TestCase
         $this->assertEquals(['one'], $this->getRoute()->excludedMiddleware());
     }
 
+    public function testGetRouteWithTrashed()
+    {
+        $route = $this->router->get('users', [RouteRegistrarControllerStub::class, 'index'])->withTrashed();
+
+        $this->assertTrue($route->allowsTrashedBindings());
+    }
+
+    public function testResourceWithTrashed()
+    {
+        $this->router->resource('users', RouteRegistrarControllerStub::class)
+            ->only(['index', 'destroy'])
+            ->withTrashed([
+                'index',
+                'destroy',
+            ]);
+
+        /** @var \Illuminate\Routing\Route $route */
+        foreach ($this->router->getRoutes() as $route) {
+            $this->assertTrue($route->allowsTrashedBindings());
+        }
+    }
+
     public function testCanRegisterGetRouteWithClosureAction()
     {
         $this->router->middleware('get-middleware')->get('users', function () {
