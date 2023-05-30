182142b4be894ac1e0ca564f4806e0f32ae521d3
Volodya Khurshudyan <volodya.khurshudyan@softconstruct.com>
2023-05-30 15:02:45 +0400

    Add ResolveRouteBinding trait

--- src/Illuminate/Database/Eloquent/Concerns/HasUuids.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasUuids.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Str;
 
 trait HasUuids
 {
+    use ResolveRouteBinding;
+
     /**
      * Initialize the trait.
      *
@@ -35,29 +37,6 @@ trait HasUuids
     public function newUniqueId()
     {
         return (string) Str::orderedUuid();
-    }
-
-    /**
-     * Retrieve the model for a bound value.
-     *
-     * @param  \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Relations\Relation  $query
-     * @param  mixed  $value
-     * @param  string|null  $field
-     * @return \Illuminate\Database\Eloquent\Relations\Relation
-     *
-     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
-     */
-    public function resolveRouteBindingQuery($query, $value, $field = null)
-    {
-        if ($field && in_array($field, $this->uniqueIds()) && ! Str::isUuid($value)) {
-            throw (new ModelNotFoundException)->setModel(get_class($this), $value);
-        }
-
-        if (! $field && in_array($this->getRouteKeyName(), $this->uniqueIds()) && ! Str::isUuid($value)) {
-            throw (new ModelNotFoundException)->setModel(get_class($this), $value);
-        }
-
-        return parent::resolveRouteBindingQuery($query, $value, $field);
     }
 
     /**
---
+++ src/Illuminate/Database/Eloquent/Concerns/ResolveRouteBinding.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Illuminate\Database\Eloquent\Concerns;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Str;
+
+trait ResolveRouteBinding
+{
+    /**
+     * Retrieve the model for a bound value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Relations\Relation  $query
+     * @param  mixed  $value
+     * @param  string|null  $field
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function resolveRouteBindingQuery($query, $value, $field = null)
+    {
+        if ($field && in_array($field, $this->uniqueIds()) && ! Str::isUlid($value)) {
+            throw (new ModelNotFoundException)->setModel(get_class($this), $value);
+        }
+
+        if (! $field && in_array($this->getRouteKeyName(), $this->uniqueIds()) && ! Str::isUlid($value)) {
+            throw (new ModelNotFoundException)->setModel(get_class($this), $value);
+        }
+
+        return parent::resolveRouteBindingQuery($query, $value, $field);
+    }
+}
