2f534eed35d8d8450d986ada67dcd9fd8e1e0494
taylorotwell <taylorotwell@users.noreply.github.com>
2023-06-02 21:25:02 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/App.php
+++ src/Illuminate/Support/Facades/App.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Support\Facades;
 
 /**
+ * @method static void configure(string|null $baseDirectory = null)
  * @method static string version()
  * @method static void bootstrapWith(string[] $bootstrappers)
  * @method static void afterLoadingEnvironment(\Closure $callback)
@@ -55,6 +56,8 @@ namespace Illuminate\Support\Facades;
  * @method static void booting(callable $callback)
  * @method static void booted(callable $callback)
  * @method static \Symfony\Component\HttpFoundation\Response handle(\Symfony\Component\HttpFoundation\Request $request, int $type = 1, bool $catch = true)
+ * @method static void handleRequest(\Illuminate\Http\Request $request)
+ * @method static int handleCommand(\Symfony\Component\Console\Input\InputInterface $input)
  * @method static bool shouldSkipMiddleware()
  * @method static string getCachedServicesPath()
  * @method static string getCachedPackagesPath()
