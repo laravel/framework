946b8935d4e7a1ae0ca07e13b40dea2d5a8e1494
Taylor Otwell <taylor@laravel.com>
2023-06-06 16:51:09 -0500

    adjust how commands are registered

--- src/Illuminate/Foundation/Console/Kernel.php
+++ src/Illuminate/Foundation/Console/Kernel.php
@@ -561,9 +561,9 @@ class Kernel implements KernelContract
      * @param  array  $commands
      * @return $this
      */
-    public function setCommands(array $commands)
+    public function addCommands(array $commands)
     {
-        $this->commands = $commands;
+        $this->commands = array_values(array_unique(array_merge($this->commands, $commands)));
 
         return $this;
     }
@@ -574,9 +574,9 @@ class Kernel implements KernelContract
      * @param  array  $paths
      * @return $this
      */
-    public function setCommandPaths(array $paths)
+    public function addCommandPaths(array $paths)
     {
-        $this->commandPaths = $paths;
+        $this->commandPaths = array_values(array_unique(array_merge($this->commandPaths, $paths)));
 
         return $this;
     }
@@ -587,9 +587,9 @@ class Kernel implements KernelContract
      * @param  array  $paths
      * @return $this
      */
-    public function setCommandRoutePaths(array $paths)
+    public function addCommandRoutePaths(array $paths)
     {
-        $this->commandRoutePaths = $paths;
+        $this->commandRoutePaths = array_values(array_unique(array_merge($this->commandRoutePaths, $paths)));
 
         return $this;
     }
