1168c101237e137ee526116aca9c1ca5f19a017b
taylorotwell <taylorotwell@users.noreply.github.com>
2023-06-06 21:57:16 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/Artisan.php
+++ src/Illuminate/Support/Facades/Artisan.php
@@ -19,9 +19,9 @@ use Illuminate\Contracts\Console\Kernel as ConsoleKernelContract;
  * @method static void bootstrap()
  * @method static void bootstrapWithoutBootingProviders()
  * @method static void setArtisan(\Illuminate\Console\Application|null $artisan)
- * @method static \Illuminate\Foundation\Console\Kernel setCommands(array $commands)
- * @method static \Illuminate\Foundation\Console\Kernel setCommandPaths(array $paths)
- * @method static \Illuminate\Foundation\Console\Kernel setCommandRoutePaths(array $paths)
+ * @method static \Illuminate\Foundation\Console\Kernel addCommands(array $commands)
+ * @method static \Illuminate\Foundation\Console\Kernel addCommandPaths(array $paths)
+ * @method static \Illuminate\Foundation\Console\Kernel addCommandRoutePaths(array $paths)
  *
  * @see \Illuminate\Foundation\Console\Kernel
  */
