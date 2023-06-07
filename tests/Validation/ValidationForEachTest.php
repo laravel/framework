0307ae46864e2a315b643ee108d0626cf3e7a476
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2023-06-07 09:50:17 -0400

    [10.x] Support `ConditionalRules` within `NestedRules` (#47344)
    * failing test
    
    * filter conditional rules when compiling NestedRules
    
    * styleci
    
    * always filter conditional rules
    
    * modify test
    
    * split tests

--- tests/Validation/ValidationForEachTest.php
+++ tests/Validation/ValidationForEachTest.php
@@ -257,6 +257,70 @@ class ValidationForEachTest extends TestCase
         ], $v->getMessageBag()->toArray());
     }
 
+    public function testConditionalRulesCanBeAddedToForEachWithAssociativeArray()
+    {
+        $v = new Validator(
+            $this->getIlluminateArrayTranslator(),
+            [
+                'foo' => [
+                    ['bar' => true],
+                    ['bar' => false],
+                ],
+            ],
+            [
+                'foo.*' => Rule::forEach(fn(mixed $value, string $attribute) => [
+                    'bar' => Rule::when(true, ['accepted'], ['declined']),
+                ]),
+            ]
+        );
+
+        $this->assertEquals([
+            'foo.1.bar' => ['validation.accepted'],
+        ], $v->getMessageBag()->toArray());
+    }
+    public function testConditionalRulesCanBeAddedToForEachWithList()
+    {
+        $v = new Validator(
+            $this->getIlluminateArrayTranslator(),
+            [
+                'foo' => [
+                    ['bar' => true],
+                    ['bar' => false],
+                ],
+            ],
+            [
+                'foo.*.bar' => Rule::forEach(fn(mixed $value, string $attribute) => [
+                    Rule::when(true, ['accepted'], ['declined']),
+                ]),
+            ]);
+
+        $this->assertEquals([
+            'foo.1.bar' => ['validation.accepted'],
+        ], $v->getMessageBag()->toArray());
+
+    }
+
+    public function testConditionalRulesCanBeAddedToForEachWithObject()
+    {
+        $v = new Validator(
+            $this->getIlluminateArrayTranslator(),
+            [
+                'foo' => [
+                    ['bar' => true],
+                    ['bar' => false],
+                ],
+            ],
+            [
+            'foo.*.bar' => Rule::forEach(fn (mixed $value, string $attribute) =>
+                Rule::when(true, ['accepted'], ['declined']),
+            ),
+        ]);
+
+        $this->assertEquals([
+            'foo.1.bar' => ['validation.accepted'],
+        ], $v->getMessageBag()->toArray());
+    }
+
     protected function getTranslator()
     {
         return m::mock(TranslatorContract::class);
