484f0ddb3621639f1aefdb5d74d7fe3735c2d440
Taylor Otwell <taylor@laravel.com>
2023-06-07 08:21:21 -0500

    add precog

--- src/Illuminate/Foundation/Configuration/Middleware.php
+++ src/Illuminate/Foundation/Configuration/Middleware.php
@@ -96,6 +96,7 @@ class Middleware
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
