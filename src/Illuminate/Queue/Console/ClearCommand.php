844592cdf4e1f1bdd96407d047fc9a566e7c14cc
Taylor Otwell <taylor@laravel.com>
2023-06-08 08:37:59 -0500

    formatting

--- src/Illuminate/Queue/Console/ClearCommand.php
+++ src/Illuminate/Queue/Console/ClearCommand.php
@@ -54,7 +54,7 @@ class ClearCommand extends Command
         if ($queue instanceof ClearableQueue) {
             $count = $queue->clear($queueName);
 
-            $this->components->info('Cleared '.Str::plural('job', $count).' '.$jobLabel.' from the ['.$queueName.'] queue');
+            $this->components->info('Cleared '.$count.' '.Str::plural('job', $count).' from the ['.$queueName.'] queue');
         } else {
             $this->components->error('Clearing queues is not supported on ['.(new ReflectionClass($queue))->getShortName().']');
         }
