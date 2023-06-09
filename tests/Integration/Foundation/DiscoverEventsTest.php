cb5e629308eeae0f8407361fea46ea2774c4dee7
cwhite <cdwhite3@pm.me>
2023-06-09 07:49:33 -0500

    Add test for event autodiscovery in multiple domains

--- tests/Integration/Foundation/DiscoverEventsTest.php
+++ tests/Integration/Foundation/DiscoverEventsTest.php
@@ -8,8 +8,11 @@ use Illuminate\Tests\Integration\Foundation\Fixtures\EventDiscovery\Events\Event
 use Illuminate\Tests\Integration\Foundation\Fixtures\EventDiscovery\Listeners\AbstractListener;
 use Illuminate\Tests\Integration\Foundation\Fixtures\EventDiscovery\Listeners\Listener;
 use Illuminate\Tests\Integration\Foundation\Fixtures\EventDiscovery\Listeners\ListenerInterface;
+use Illuminate\Tests\Integration\Foundation\Fixtures\EventDiscovery\namespaces\app\Listener as AppListener;
+use Illuminate\Tests\Integration\Foundation\Fixtures\EventDiscovery\namespaces\domain\Listener as DomainListener;
 use Illuminate\Tests\Integration\Foundation\Fixtures\EventDiscovery\UnionListeners\UnionListener;
 use Orchestra\Testbench\TestCase;
+use ReflectionClass;
 
 class DiscoverEventsTest extends TestCase
 {
@@ -44,6 +47,34 @@ class DiscoverEventsTest extends TestCase
             ],
             EventTwo::class => [
                 UnionListener::class.'@handle',
+            ],
+        ], $events);
+    }
+
+    public function testEventsCanBeDiscoveredInDifferentNamespaces()
+    {
+        (new ReflectionClass(DiscoverEvents::class))
+            ->getProperty('namespaces')
+            ->setValue([
+                'App\\' => 'tests/Integration/Foundation/Fixtures/EventDiscovery/namespaces/app/',
+                'Domain\\' => 'tests/Integration/Foundation/Fixtures/EventDiscovery/namespaces/domain/',
+            ]);
+
+        class_alias(AppListener::class, 'App\Listener');
+        class_alias(DomainListener::class, 'Domain\Listener');
+
+        $events = DiscoverEvents::within(__DIR__.'/Fixtures/EventDiscovery/namespaces', getcwd());
+
+        $this->assertEqualsCanonicalizing([
+            EventOne::class => [
+                AppListener::class.'@handle',
+                AppListener::class.'@handleEventOne',
+                DomainListener::class.'@handle',
+                DomainListener::class.'@handleEventOne',
+            ],
+            EventTwo::class => [
+                AppListener::class.'@handleEventTwo',
+                DomainListener::class.'@handleEventTwo',
             ],
         ], $events);
     }
