9b009e5db99424cc9e207163008fc01d33bdeed9
Amir Ezazi <51764116+AEChabok@users.noreply.github.com>
2023-06-14 08:34:20 +0330

    Create CheckCommand.php
    Checking the queue in Larval  the limits of operating system jobs that, if the process is stopped by mistake, it has the ability to re-execute the queue so that the processes are not stopped.

---
+++ src/Illuminate/Queue/Console/CheckCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+
+class CheckCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'queue:check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check if the queue worker process is running and start it if not.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $workerName = 'queue:work';
+
+        $isRunning = $this->isProcessRunning($workerName);
+
+        if (!$isRunning) {
+            Log::info('Queue worker process is not running. Starting the worker...');
+
+            Artisan::call('queue:work', ['--daemon' => true]);
+        } else {
+            Log::info('Queue worker process is already running.');
+        }
+    }
+
+    /**
+     * @param string $processName
+     * @return bool
+     */
+    private function isProcessRunning(string $processName): bool
+    {
+        $processes = shell_exec('ps aux | grep "' . $processName . '" | grep -v grep');
+
+        return (bool) $processes;
+    }
+}
