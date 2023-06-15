380ffba3bc81870dfedb0c0ff9fe63330ef0831c
miladev-ent <milwad.dev@gmail.com>
2023-06-15 13:10:02 +0330

    fix return type of `prefixedResource` & `prefixedResource`

--- src/Illuminate/Routing/ResourceRegistrar.php
+++ src/Illuminate/Routing/ResourceRegistrar.php
@@ -179,7 +179,7 @@ class ResourceRegistrar
      * @param  string  $name
      * @param  string  $controller
      * @param  array  $options
-     * @return void
+     * @return Router
      */
     protected function prefixedResource($name, $controller, array $options)
     {
@@ -201,7 +201,7 @@ class ResourceRegistrar
      * @param  string  $name
      * @param  string  $controller
      * @param  array  $options
-     * @return void
+     * @return Router
      */
     protected function prefixedSingleton($name, $controller, array $options)
     {
