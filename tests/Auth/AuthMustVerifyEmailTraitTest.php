3720791405373260326409a63d3449a992c9f92c
Thai Nguyen Hung <hungthai1401.it@gmail.com>
2023-06-15 10:22:54 +0700

    `MustVerifyEmailTrait` test

---
+++ tests/Auth/AuthMustVerifyEmailTraitTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Illuminate\Tests\Auth;
+
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Support\Carbon;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class AuthMustVerifyEmailTraitTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    /**
+     * @return void
+     */
+    public function testWillConfirmEmailVerifiedAtColumn(): void
+    {
+        $user = Mockery::mock(User::class);
+        $user->makePartial();
+        $this->assertEquals('email_verified_at', $user->getEmailVerifiedAtColumn());
+    }
+
+    /**
+     * @return void
+     */
+    public function testWillConfirmCustomEmailVerifiedAtColumn(): void
+    {
+        $user = Mockery::mock(CustomUser::class);
+        $user->makePartial();
+        $this->assertEquals(CustomUser::EMAIL_VERIFIED_AT, $user->getEmailVerifiedAtColumn());
+    }
+
+    /**
+     * @return void
+     */
+    public function testWillHasVerifiedEmail(): void
+    {
+        $user = Mockery::mock(User::class);
+        $user->makePartial();
+        $user->{$user->getEmailVerifiedAtColumn()} = Carbon::now();
+        $this->assertTrue($user->hasVerifiedEmail());
+    }
+
+    /**
+     * @return void
+     */
+    public function testWillUpdateEmailVerifiedAtWhenMarkEmailAsVerified(): void
+    {
+        $user = Mockery::mock(User::class);
+        $user->makePartial();
+        $user->shouldReceive('forceFill')->withAnyArgs()->once()->andReturn(tap($user, function () use ($user) {
+            $user->{$user->getEmailVerifiedAtColumn()} = $user->freshTimestamp();
+
+            return $user;
+        }));
+        $user->shouldReceive('save')->once();
+        $user->markEmailAsVerified();
+        $this->assertInstanceOf(Carbon::class, $user->{$user->getEmailVerifiedAtColumn()});
+    }
+}
+
+class User
+{
+    use MustVerifyEmail;
+
+    public function save()
+    {
+        //
+    }
+
+    public function freshTimestamp(): Carbon
+    {
+        return Carbon::now();
+    }
+}
+
+class CustomUser
+{
+    use MustVerifyEmail;
+
+    public const EMAIL_VERIFIED_AT = 'custom_email_verified_at';
+}
