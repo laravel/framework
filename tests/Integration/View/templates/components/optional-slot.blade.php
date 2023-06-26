021179bce0b84351a481b68234ec37092f4c146e
indy koning <indy@justbetter.nl>
2023-06-26 16:01:04 +0200

    Fix tests

--- tests/Integration/View/templates/components/optional-slot.blade.php
+++ tests/Integration/View/templates/components/optional-slot.blade.php
@@ -1,7 +1,7 @@
-@slots(['optional_slot', 'another_slot' => ['contents' => 'dummy text', 'attributes' => ['class' => 'bg-red-500']]])
+@slots(['optionalSlot', 'anotherSlot' => ['contents' => 'dummy text', 'attributes' => ['class' => 'bg-red-500']]])
 
 <div {{ $attributes }}>
     {{ $slot }}
-    <div {{ $optional_slot->attributes }}>{{ $optional_slot }}</div>
-    <div {{ $another_slot->attributes }}>{{ $another_slot }}</div>
+    <div {{ $optionalSlot->attributes }}>{{ $optionalSlot }}</div>
+    <div {{ $anotherSlot->attributes }}>{{ $anotherSlot }}</div>
 </div>
--- tests/Integration/View/templates/uses-optional-slots.blade.php
+++ tests/Integration/View/templates/uses-optional-slots.blade.php
@@ -3,17 +3,17 @@
     Slot content
 </x-optional-slot>
 @elseif($optionalAsAttribute)
-<x-optional-slot optional_slot="Optional content" another_slot="Another slot content">
+<x-optional-slot optionalSlot="Optional content" anotherSlot="Another slot content">
     Slot content
 </x-optional-slot>
 @else
 <x-optional-slot>
     Slot content
-    <x-slot:optional_slot>
+    <x-slot:optionalSlot>
         Optional content
-    </x-slot:optional_slot>
-    <x-slot:another_slot>
+    </x-slot:optionalSlot>
+    <x-slot:anotherSlot>
         Another slot content
-    </x-slot:another_slot>
+    </x-slot:anotherSlot>
 </x-optional-slot>
 @endif
