c5d028b940f4be09a7b328466c1d0dc5af98dcae
Patryk Vizauer <vizauerp@gmail.com>
2023-07-04 15:22:09 +0200

    Add 'true', 'false' to validating boolean

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -470,7 +470,7 @@ trait ValidatesAttributes
      */
     public function validateBoolean($attribute, $value)
     {
-        $acceptable = [true, false, 0, 1, '0', '1'];
+        $acceptable = [true, false, 0, 1, '0', '1', 'true', 'false'];
 
         return in_array($value, $acceptable, true);
     }
