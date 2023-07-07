0719afe62a27dea128682f5bd42cff6915edc937
cwhite <cdwhite3@pm.me>
2023-07-07 12:14:35 -0500

    Update DatabaseRule to handle Enums for simple where clause

--- src/Illuminate/Validation/Rules/DatabaseRule.php
+++ src/Illuminate/Validation/Rules/DatabaseRule.php
@@ -2,6 +2,7 @@
 
 namespace Illuminate\Validation\Rules;
 
+use BackedEnum;
 use Closure;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Model;
@@ -81,7 +82,7 @@ trait DatabaseRule
      * Set a "where" constraint on the query.
      *
      * @param  \Closure|string  $column
-     * @param  \Illuminate\Contracts\Support\Arrayable|array|string|int|bool|null  $value
+     * @param  \Illuminate\Contracts\Support\Arrayable|\BackedEnum|\Closure|array|string|int|bool|null  $value
      * @return $this
      */
     public function where($column, $value = null)
@@ -98,6 +99,10 @@ trait DatabaseRule
             return $this->whereNull($column);
         }
 
+        if ($value instanceof BackedEnum) {
+            $value = $value->value;
+        }
+
         $this->wheres[] = compact('column', 'value');
 
         return $this;
@@ -107,13 +112,17 @@ trait DatabaseRule
      * Set a "where not" constraint on the query.
      *
      * @param  string  $column
-     * @param  \Illuminate\Contracts\Support\Arrayable|array|string  $value
+     * @param  \Illuminate\Contracts\Support\Arrayable|\BackedEnum|array|string  $value
      * @return $this
      */
     public function whereNot($column, $value)
     {
         if ($value instanceof Arrayable || is_array($value)) {
             return $this->whereNotIn($column, $value);
+        }
+
+        if ($value instanceof BackedEnum) {
+            $value = $value->value;
         }
 
         return $this->where($column, '!'.$value);
