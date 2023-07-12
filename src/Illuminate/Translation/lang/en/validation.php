55ea8691d12fd0aee44c13e973c6c25430bd1d14
Nafis Ahmed <102437433+ahmad-cit22@users.noreply.github.com>
2023-07-12 13:05:53 +0600

    Update "validation.php"
    Just corrected a grammatical mistake.

--- src/Illuminate/Translation/lang/en/validation.php
+++ src/Illuminate/Translation/lang/en/validation.php
@@ -144,7 +144,7 @@ return [
     ],
     'starts_with' => 'The :attribute field must start with one of the following: :values.',
     'string' => 'The :attribute field must be a string.',
-    'timezone' => 'The :attribute field must be a valid timezone.',
+    'timezone' => 'The :attribute field must be of a valid timezone.',
     'unique' => 'The :attribute has already been taken.',
     'uploaded' => 'The :attribute failed to upload.',
     'uppercase' => 'The :attribute field must be uppercase.',
