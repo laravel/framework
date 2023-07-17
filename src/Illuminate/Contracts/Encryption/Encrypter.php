e3b96807d1b999329b3dfa7316acac6c6766ba28
omaresmaeel <omaresmael590@gmail.com>
2023-07-17 18:51:29 +0300

    update encrypter interface to match the new implementation of encrypt/decrypt functions

--- src/Illuminate/Contracts/Encryption/Encrypter.php
+++ src/Illuminate/Contracts/Encryption/Encrypter.php
@@ -9,22 +9,24 @@ interface Encrypter
      *
      * @param  mixed  $value
      * @param  bool  $serialize
+     * @param  string|null  $key
      * @return string
      *
      * @throws \Illuminate\Contracts\Encryption\EncryptException
      */
-    public function encrypt($value, $serialize = true);
+    public function encrypt($value, $serialize = true, $key = null);
 
     /**
      * Decrypt the given value.
      *
      * @param  string  $payload
      * @param  bool  $unserialize
+     * @param  string|null  $key
      * @return mixed
      *
      * @throws \Illuminate\Contracts\Encryption\DecryptException
      */
-    public function decrypt($payload, $unserialize = true);
+    public function decrypt($payload, $unserialize = true, $key = null);
 
     /**
      * Get the encryption key that the encrypter is currently using.
