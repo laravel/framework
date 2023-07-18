8c07abc96583ad690e959be3f8cc959eeda03b5d
foremtehan <53290883+foremtehan@users.noreply.github.com>
2023-07-18 09:47:26 +0330

    Update functions.php

--- src/Illuminate/Events/functions.php
+++ src/Illuminate/Events/functions.php
@@ -4,15 +4,13 @@ namespace Illuminate\Events;
 
 use Closure;
 
-if (! function_exists('Illuminate\Events\queueable')) {
-    /**
-     * Create a new queued Closure event listener.
-     *
-     * @param  \Closure  $closure
-     * @return \Illuminate\Events\QueuedClosure
-     */
-    function queueable(Closure $closure)
-    {
-        return new QueuedClosure($closure);
-    }
+/**
+ * Create a new queued Closure event listener.
+ *
+ * @param  \Closure  $closure
+ * @return \Illuminate\Events\QueuedClosure
+ */
+function queueable(Closure $closure)
+{
+    return new QueuedClosure($closure);
 }
