0c015ecc9620ce965116fb64e2aeccc4625d0547
floodx92 <47539249+floodx92@users.noreply.github.com>
2023-07-21 16:39:01 +0200

    fix: Resolve multiple closure-based scheduled commands in schedule:test

--- src/Illuminate/Console/Scheduling/ScheduleTestCommand.php
+++ src/Illuminate/Console/Scheduling/ScheduleTestCommand.php
@@ -32,55 +32,77 @@ class ScheduleTestCommand extends Command
     public function handle(Schedule $schedule)
     {
         $phpBinary = Application::phpBinary();
-
         $commands = $schedule->events();
-
         $commandNames = [];
 
         foreach ($commands as $command) {
-            $commandNames[] = $command->command ?? $command->getSummaryForDisplay();
+            $commandName = $command->command ?? $command->getSummaryForDisplay();
+
+            $suffix = '';
+
+            if (in_array($commandName, $commandNames)) {
+                $suffix = ' (' . count(array_filter($commandNames, fn($command) => $command === $commandName)) . ')';
+            }
+
+            $commandNames[] = $commandName . $suffix;
         }
 
         if (empty($commandNames)) {
             return $this->components->info('No scheduled commands have been defined.');
         }
 
-        if (! empty($name = $this->option('name'))) {
+        if (!empty($name = $this->option('name'))) {
             $commandBinary = $phpBinary.' '.Application::artisanBinary();
-
-            $matches = array_filter($commandNames, function ($commandName) use ($commandBinary, $name) {
-                return trim(str_replace($commandBinary, '', $commandName)) === $name;
+            $matches = array_filter($commands, function ($command) use ($commandBinary, $name) {
+                return trim(str_replace($commandBinary, '', $command->getSummaryForDisplay())) === $name;
             });
 
-            if (count($matches) !== 1) {
-                $this->components->info('No matching scheduled command found.');
+            if (count($matches) > 0) {
+                foreach ($matches as $match) {
+                    $this->runSingleTask($match, $commandBinary);
+                }
 
                 return;
+            } else {
+                $this->components->info('No matching scheduled command found.');
+                return;
             }
-
-            $index = key($matches);
         } else {
             $index = array_search($this->components->choice('Which command would you like to run?', $commandNames), $commandNames);
+            $this->runSingleTask($commands[$index], $phpBinary);
         }
 
-        $event = $commands[$index];
+        $this->newLine();
+    }
 
-        $summary = $event->getSummaryForDisplay();
+    /**
+     * Runs a single task.
+     *
+     * This function is responsible for the execution of a single task.
+     * It prepares the description of the task, executes it and displays a summary.
+     *
+     * @param \Illuminate\Console\Scheduling\Event $task
+     * @param string $phpBinary
+     * @return void
+     */
+    protected function runSingleTask(\Illuminate\Console\Scheduling\Event $task, string $phpBinary)
+    {
+        $summary = $task->getSummaryForDisplay();
 
-        $command = $event instanceof CallbackEvent
+        $command = $task instanceof CallbackEvent
             ? $summary
-            : trim(str_replace($phpBinary, '', $event->command));
+            : trim(str_replace($phpBinary, '', $task->command));
 
         $description = sprintf(
             'Running [%s]%s',
             $command,
-            $event->runInBackground ? ' in background' : '',
+            $task->runInBackground ? ' in background' : '',
         );
 
-        $this->components->task($description, fn () => $event->run($this->laravel));
+        $this->components->task($description, fn () => $task->run($this->laravel));
 
-        if (! $event instanceof CallbackEvent) {
-            $this->components->bulletList([$event->getSummaryForDisplay()]);
+        if (!$task instanceof CallbackEvent) {
+            $this->components->bulletList([$task->getSummaryForDisplay()]);
         }
 
         $this->newLine();
