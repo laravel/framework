e39584c77d236ccd0d9ae6bf6669a58653736762
taylorotwell <taylorotwell@users.noreply.github.com>
2023-07-26 21:28:04 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/Storage.php
+++ src/Illuminate/Support/Facades/Storage.php
@@ -24,7 +24,7 @@ use Illuminate\Filesystem\Filesystem;
  * @method static bool exists(string $path)
  * @method static string|null get(string $path)
  * @method static resource|null readStream(string $path)
- * @method static bool put(string $path, string|resource $contents, mixed $options = [])
+ * @method static bool put(string $path, \Psr\Http\Message\StreamInterface|\Illuminate\Http\File|\Illuminate\Http\UploadedFile|string|resource $contents, mixed $options = [])
  * @method static bool writeStream(string $path, resource $resource, array $options = [])
  * @method static string getVisibility(string $path)
  * @method static bool setVisibility(string $path, string $visibility)
