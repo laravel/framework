d0f91ab09fb69b4dad62ed0448fe12a633158c85
khaledw62 <42186508+khaledw62@users.noreply.github.com>
2023-07-27 20:59:41 +0300

    Style ci

--- tests/Foundation/FoundationInteractsWithDatabaseTest.php
+++ tests/Foundation/FoundationInteractsWithDatabaseTest.php
@@ -442,7 +442,6 @@ class FoundationInteractsWithDatabaseTest extends TestCase
 
         $builder->shouldReceive('where')->with($key, $value)->andReturnSelf();
 
-
         $builder->shouldReceive('select')->with(array_keys($this->data))->andReturnSelf();
 
         $builder->shouldReceive('limit')->andReturnSelf();
@@ -481,7 +480,6 @@ class CustomProductStub extends ProductStub
 {
     const DELETED_AT = 'trashed_at';
 }
-
 
 class CustomProductStubWithCasts extends ProductStub
 {
