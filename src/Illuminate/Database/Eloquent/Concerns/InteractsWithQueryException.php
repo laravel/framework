3c12cc89e8db729ac8aa57a771ffd3068e959d4d
Tony Messias <tonysm@hey.com>
2023-08-05 19:17:59 -0300

    Adds more context to comments

--- src/Illuminate/Database/Eloquent/Concerns/InteractsWithQueryException.php
+++ src/Illuminate/Database/Eloquent/Concerns/InteractsWithQueryException.php
@@ -13,13 +13,17 @@ trait InteractsWithQueryException
      */
     protected function matchesUniqueConstraintException(QueryException $exception)
     {
-        // SQLite3
+        // SQLite 3.8.2 and above will return the newly formatted error message:
+        // "UNIQUE constraint failed: *table_name*.*column_name*", however in
+        // older versions, it returns "column *column_name* is not unique".
         if (preg_match('#(column(s)? .* (is|are) not unique|UNIQUE constraint failed: .*)#i', $exception->getMessage())) {
             return true;
         }
 
-        // MySQL
-        if (preg_match('#SQLSTATE\[23000\]: Integrity constraint violation: 1062#i', $exception->getMessage())) {
+        // We'll match against the message instead of the exception code because
+        // the exception code returns "23000" instead of "1062", which is the
+        // error code MySQL should return in UNIQUE constraints violations.
+        if (preg_match('#Integrity constraint violation: 1062#i', $exception->getMessage())) {
             return true;
         }
 
@@ -29,7 +33,7 @@ trait InteractsWithQueryException
         }
 
         // SQLServer
-        if (preg_match('#SQLSTATE\[23000\]:.*Cannot insert duplicate key row in object.*#i', $exception->getMessage())) {
+        if (preg_match('#Cannot insert duplicate key row in object#i', $exception->getMessage())) {
             return true;
         }
 
