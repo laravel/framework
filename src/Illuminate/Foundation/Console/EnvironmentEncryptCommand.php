617abe49759e66990764e966e5b15329773d43a0
Philipp <32090713+ouun@users.noreply.github.com>
2023-08-08 00:09:10 +0200

    Remove spaces

--- src/Illuminate/Foundation/Console/EnvironmentEncryptCommand.php
+++ src/Illuminate/Foundation/Console/EnvironmentEncryptCommand.php
@@ -64,7 +64,7 @@ class EnvironmentEncryptCommand extends Command
         $keyPassed = $key !== null;
 
         $environmentFile = $this->laravel->environmentFilePath();
-        
+
         if ($this->option('env')) {
             $environmentFile .= '.'.$this->option('env');
         }
