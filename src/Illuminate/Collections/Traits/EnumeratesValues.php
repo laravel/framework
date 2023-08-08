5c6f182058e8ca1437f9d337e52842354e656a88
Delowar Hossain <contact.delowar@gmail.com>
2023-08-08 22:32:11 +0600

    Fixing Code Convention

--- src/Illuminate/Collections/Traits/EnumeratesValues.php
+++ src/Illuminate/Collections/Traits/EnumeratesValues.php
@@ -565,10 +565,8 @@ trait EnumeratesValues
         if (is_iterable($key)) {
             $filteredCollection = $this;
             foreach ($key as $searchKey => $searchValue) {
-
                 $filteredCollection = $filteredCollection->filter($filteredCollection->operatorForWhere($searchKey, null , $searchValue));
             }
-            
             return $filteredCollection;     
         } else {
             return $this->filter($this->operatorForWhere(...func_get_args()));
