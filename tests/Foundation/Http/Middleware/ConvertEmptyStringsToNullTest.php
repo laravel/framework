c6a17bd9d049b21bc7bed1b6a3d5ae8094ca5ddc
Thai Nguyen Hung <hungthai1401.it@gmail.com>
2023-08-18 10:31:05 +0700

    test: skip convert empty string to null

--- tests/Foundation/Http/Middleware/ConvertEmptyStringsToNullTest.php
+++ tests/Foundation/Http/Middleware/ConvertEmptyStringsToNullTest.php
@@ -24,4 +24,21 @@ class ConvertEmptyStringsToNullTest extends TestCase
             $this->assertNull($request->get('baz'));
         });
     }
+
+    public function testSkipConvertsEmptyStringsToNull()
+    {
+        $middleware = new ConvertEmptyStringsToNull;
+        ConvertEmptyStringsToNull::skipWhen(fn ($request) => '' === $request->baz);
+        $symfonyRequest = new SymfonyRequest([
+            'foo' => 'bar',
+            'baz' => '',
+        ]);
+        $symfonyRequest->server->set('REQUEST_METHOD', 'GET');
+        $request = Request::createFromBase($symfonyRequest);
+
+        $middleware->handle($request, function (Request $request) {
+            $this->assertSame('bar', $request->get('foo'));
+            $this->assertSame('', $request->get('baz'));
+        });
+    }
 }
