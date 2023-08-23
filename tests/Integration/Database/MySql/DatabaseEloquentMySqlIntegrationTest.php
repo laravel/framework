cfad4d7bf4922a27028c82b22d6e81af53e403c8
Tony Messias <tonysm@hey.com>
2023-08-23 01:01:55 -0300

    Test createOrFirst within a transaction on MySQL

--- tests/Integration/Database/MySql/DatabaseEloquentMySqlIntegrationTest.php
+++ tests/Integration/Database/MySql/DatabaseEloquentMySqlIntegrationTest.php
@@ -4,6 +4,7 @@ namespace Illuminate\Tests\Integration\Database\MySql;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class DatabaseEloquentMySqlIntegrationTest extends MySqlTestCase
@@ -56,6 +57,22 @@ class DatabaseEloquentMySqlIntegrationTest extends MySqlTestCase
         );
 
         $this->assertSame('Nuno Maduro', $user4->name);
+    }
+
+    public function testCreateOrFirstWithinTransaction()
+    {
+        $user1 = DatabaseEloquentMySqlIntegrationUser::createOrFirst(['email' => 'taylor@laravel.com']);
+
+        DB::transaction(function () use ($user1) {
+            $user2 = DatabaseEloquentMySqlIntegrationUser::createOrFirst(
+                ['email' => 'taylor@laravel.com'],
+                ['name' => 'Taylor Otwell']
+            );
+
+            $this->assertEquals($user1->id, $user2->id);
+            $this->assertSame('taylor@laravel.com', $user2->email);
+            $this->assertNull($user2->name);
+        });
     }
 }
 
