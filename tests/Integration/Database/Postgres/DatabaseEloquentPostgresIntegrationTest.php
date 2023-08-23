6854a55dfb234295edd28b4afd20b7b72d3bb2b0
Tony Messias <tonysm@hey.com>
2023-08-23 02:52:06 -0300

    Revert "Moves the eloquent contract tests to a trait"
    This reverts commit a7e57c84ef8036e3dfb052d073943b1e9ad6ceb0.

--- tests/Integration/Database/Postgres/DatabaseEloquentPostgresIntegrationTest.php
+++ tests/Integration/Database/Postgres/DatabaseEloquentPostgresIntegrationTest.php
@@ -4,15 +4,11 @@ namespace Illuminate\Tests\Integration\Database\Postgres;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Tests\Integration\Database\WithEloquentIntegrationTests;
 
 class DatabaseEloquentPostgresIntegrationTest extends PostgresTestCase
 {
-    use WithEloquentIntegrationTests;
-
-    protected $eloquentModelClass = DatabaseEloquentPostgresIntegrationUser::class;
-
     protected function defineDatabaseMigrationsAfterDatabaseRefreshed()
     {
         if (! Schema::hasTable('database_eloquent_postgres_integration_users')) {
@@ -28,6 +24,55 @@ class DatabaseEloquentPostgresIntegrationTest extends PostgresTestCase
     protected function destroyDatabaseMigrations()
     {
         Schema::drop('database_eloquent_postgres_integration_users');
+    }
+
+    public function testCreateOrFirst()
+    {
+        $user1 = DatabaseEloquentPostgresIntegrationUser::createOrFirst(['email' => 'taylorotwell@gmail.com']);
+
+        $this->assertSame('taylorotwell@gmail.com', $user1->email);
+        $this->assertNull($user1->name);
+
+        $user2 = DatabaseEloquentPostgresIntegrationUser::createOrFirst(
+            ['email' => 'taylorotwell@gmail.com'],
+            ['name' => 'Taylor Otwell']
+        );
+
+        $this->assertEquals($user1->id, $user2->id);
+        $this->assertSame('taylorotwell@gmail.com', $user2->email);
+        $this->assertNull($user2->name);
+
+        $user3 = DatabaseEloquentPostgresIntegrationUser::createOrFirst(
+            ['email' => 'abigailotwell@gmail.com'],
+            ['name' => 'Abigail Otwell']
+        );
+
+        $this->assertNotEquals($user3->id, $user1->id);
+        $this->assertSame('abigailotwell@gmail.com', $user3->email);
+        $this->assertSame('Abigail Otwell', $user3->name);
+
+        $user4 = DatabaseEloquentPostgresIntegrationUser::createOrFirst(
+            ['name' => 'Dries Vints'],
+            ['name' => 'Nuno Maduro', 'email' => 'nuno@laravel.com']
+        );
+
+        $this->assertSame('Nuno Maduro', $user4->name);
+    }
+
+    public function testCreateOrFirstWithinTransaction()
+    {
+        $user1 = DatabaseEloquentPostgresIntegrationUser::create(['email' => 'taylor@laravel.com']);
+
+        DB::transaction(function () use ($user1) {
+            $user2 = DatabaseEloquentPostgresIntegrationUser::createOrFirst(
+                ['email' => 'taylor@laravel.com'],
+                ['name' => 'Taylor Otwell']
+            );
+
+            $this->assertEquals($user1->id, $user2->id);
+            $this->assertSame('taylor@laravel.com', $user2->email);
+            $this->assertNull($user2->name);
+        });
     }
 }
 
