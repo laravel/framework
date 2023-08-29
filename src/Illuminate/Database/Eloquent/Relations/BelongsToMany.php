e1ae50704e5bd902973270f2988a77564f3bbe8f
mpyw <ryosuke_i_628@yahoo.co.jp>
2023-08-29 22:54:19 +0900

    [10.x] Make the `updateOrCreate` methods in relations use `firstOrCreate` behind the scenes (#48213)
    * Make the `updateOrCreate` methods in relations use `firstOrCreate`
    
    Related to #48160, #48192
    
    * Fix tests
    
    * Fix: correctly apply unused `$joining` and `$touch`
    
    * Fix: `save()` should always accepts `['touch' => false]`

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -672,19 +672,13 @@ class BelongsToMany extends Relation
      */
     public function updateOrCreate(array $attributes, array $values = [], array $joining = [], $touch = true)
     {
-        if (is_null($instance = (clone $this)->where($attributes)->first())) {
-            if (is_null($instance = $this->related->where($attributes)->first())) {
-                return $this->create(array_merge($attributes, $values), $joining, $touch);
-            } else {
-                $this->attach($instance, $joining, $touch);
+        return tap($this->firstOrCreate($attributes, $values, $joining, $touch), function ($instance) use ($values) {
+            if (! $instance->wasRecentlyCreated) {
+                $instance->fill($values);
+
+                $instance->save(['touch' => false]);
             }
-        }
-
-        $instance->fill($values);
-
-        $instance->save(['touch' => false]);
-
-        return $instance;
+        });
     }
 
     /**
