6d0bd6739f9645c6d1eeac4942719c2b69262981
mobidev86 <mobidev86@gmail.com>
2023-09-04 15:31:49 +0530

    remove table prefix from DatabaseTruncation Trait to fix #48264

--- src/Illuminate/Foundation/Testing/DatabaseTruncation.php
+++ src/Illuminate/Foundation/Testing/DatabaseTruncation.php
@@ -85,10 +85,32 @@ trait DatabaseTruncation
                 fn ($tables) => $tables->intersect($this->tablesToTruncate),
                 fn ($tables) => $tables->diff($this->exceptTables($name))
             )
-            ->filter(fn ($table) => $connection->table($table)->exists())
-            ->each(fn ($table) => $connection->table($table)->truncate());
+            ->filter(fn ($table) => $connection->table($this->removeTablePrefix($connection, $table))->exists())
+            ->each(fn ($table) => $connection->table($this->removeTablePrefix($connection, $table))->truncate());
 
         $connection->setEventDispatcher($dispatcher);
+    }
+
+    /**
+     * Remove the table prefix from a table name, if it exists.
+     *
+     * @param  \Illuminate\Database\ConnectionInterface  $connection
+     * @param  string  $table
+     * @return string
+     */
+    protected function removeTablePrefix(ConnectionInterface $connection, string $table): string
+    {
+        // Retrieve the table prefix associated with the connection
+        $prefix = $connection->getTablePrefix();
+
+        // Check if the table name contains the prefix
+        if (strpos($table, $prefix) === 0) {
+            // If the prefix is found, remove it from the table name
+            return substr($table, strlen($prefix));
+        }
+
+        // If no prefix is found, return the original table name
+        return $table;
     }
 
     /**
