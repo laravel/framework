e7cea642c3b169968baa6ea71a4bc8b34743088c
Nuno Maduro <enunomaduro@gmail.com>
2023-09-07 13:57:12 +0100

    Adds `make:view` Artisan command

--- src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
+++ src/Illuminate/Foundation/Providers/ArtisanServiceProvider.php
@@ -76,6 +76,7 @@ use Illuminate\Foundation\Console\UpCommand;
 use Illuminate\Foundation\Console\VendorPublishCommand;
 use Illuminate\Foundation\Console\ViewCacheCommand;
 use Illuminate\Foundation\Console\ViewClearCommand;
+use Illuminate\Foundation\Console\ViewMakeCommand;
 use Illuminate\Notifications\Console\NotificationTableCommand;
 use Illuminate\Queue\Console\BatchesTableCommand;
 use Illuminate\Queue\Console\ClearCommand as QueueClearCommand;
@@ -203,6 +204,7 @@ class ArtisanServiceProvider extends ServiceProvider implements DeferrableProvid
         'Serve' => ServeCommand::class,
         'StubPublish' => StubPublishCommand::class,
         'TestMake' => TestMakeCommand::class,
+        'ViewMake' => ViewMakeCommand::class,
         'VendorPublish' => VendorPublishCommand::class,
     ];
 
