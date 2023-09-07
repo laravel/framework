1591fed925408fd03c9f2cd5230f333e34594c83
Chris Morrell <inxilpro@users.noreply.github.com>
2023-09-07 11:24:32 -0400

    Validate version and variant for UUID (#48321)

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -516,7 +516,11 @@ class Str
             return false;
         }
 
-        return preg_match('/^[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/iD', $value) > 0;
+        if ($value === '00000000-0000-0000-0000-000000000000') {
+            return true;
+        }
+
+        return preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iD', $value) > 0;
     }
 
     /**
