ec159a290a3404422b05490c73d30c97c70fefa4
sohelrana820 <me.sohelrana@gmail.com>
2023-09-12 00:24:59 +0600

    Fix some coding style!

--- tests/Translation/TranslationFileLoaderTest.php
+++ tests/Translation/TranslationFileLoaderTest.php
@@ -125,11 +125,11 @@ class TranslationFileLoaderTest extends TestCase
     public function testLoadMethodThrowExceptionWhenProvideInvalidJSON()
     {
         $loader = new FileLoader($files = m::mock(Filesystem::class), __DIR__);
-        $loader->addJsonPath(__DIR__ . '/invalid');
+        $loader->addJsonPath(__DIR__.'/invalid');
 
         $invalidJsonString = '.{"foo":"cricket", "baz": "football"}';
-        $files->shouldReceive('exists')->once()->with(__DIR__ . '/invalid/en.json')->andReturn(true);
-        $files->shouldReceive('get')->once()->with(__DIR__ . '/invalid/en.json')->andReturn($invalidJsonString);
+        $files->shouldReceive('exists')->once()->with(__DIR__.'/invalid/en.json')->andReturn(true);
+        $files->shouldReceive('get')->once()->with(__DIR__.'/invalid/en.json')->andReturn($invalidJsonString);
 
         $this->expectException(\RuntimeException::class);
         $loader->load('en', '*', '*');
@@ -146,8 +146,8 @@ class TranslationFileLoaderTest extends TestCase
     public function testAllAddedJsonPathsReturnProperly()
     {
         $loader = new FileLoader(m::mock(Filesystem::class), __DIR__);
-        $path1 = __DIR__ . '/another';
-        $path2 = __DIR__ . '/another2';
+        $path1 = __DIR__.'/another';
+        $path2 = __DIR__.'/another2';
         $loader->addJsonPath($path1);
         $loader->addJsonPath($path2);
         $this->assertEquals([$path1, $path2], $loader->jsonPaths());
