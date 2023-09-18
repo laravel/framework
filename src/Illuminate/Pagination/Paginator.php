5205429ec13dc32c736447ac6d308dc9917ab5c2
HiDemo <hiidemo@users.noreply.github.com>
2023-09-18 17:25:40 +0700

    Fix error: The current page items are only showing as first page

--- src/Illuminate/Pagination/Paginator.php
+++ src/Illuminate/Pagination/Paginator.php
@@ -69,7 +69,7 @@ class Paginator extends AbstractPaginator implements Arrayable, ArrayAccess, Cou
 
         $this->hasMore = $this->items->count() > $this->perPage;
 
-        $this->items = $this->items->slice(0, $this->perPage);
+        $this->items = $this->items->slice(($this->currentPage - 1) * $this->perPage, $this->perPage);
     }
 
     /**
