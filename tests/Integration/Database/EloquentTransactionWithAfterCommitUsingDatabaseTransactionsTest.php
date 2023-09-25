95dfe346f53afcea0d788ad3459a4676a7ca8ee2
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-25 20:52:24 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Database/EloquentTransactionWithAfterCommitUsingDatabaseTransactionsTest.php
+++ tests/Integration/Database/EloquentTransactionWithAfterCommitUsingDatabaseTransactionsTest.php
@@ -3,16 +3,33 @@
 namespace Illuminate\Tests\Integration\Database;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Orchestra\Testbench\TestCase;
 
-class EloquentTransactionWithAfterCommitUsingDatabaseTransactionsTest extends DatabaseTestCase
+class EloquentTransactionWithAfterCommitUsingDatabaseTransactionsTest extends TestCase
 {
     use EloquentTransactionWithAfterCommitTests;
     use DatabaseTransactions;
 
+    /**
+     * The current database driver.
+     *
+     * @return string
+     */
+    protected $driver;
+
+    protected function setUp(): void
+    {
+        $this->beforeApplicationDestroyed(function () {
+            foreach (array_keys($this->app['db']->getConnections()) as $name) {
+                $this->app['db']->purge($name);
+            }
+        });
+
+        parent::setUp();
+    }
+
     protected function getEnvironmentSetUp($app)
     {
-        parent::getEnvironmentSetUp($app);
-
         $connection = $app->make('config')->get('database.default');
 
         $db = $app['config']->get("database.connections.{$connection}");
@@ -20,5 +37,7 @@ class EloquentTransactionWithAfterCommitUsingDatabaseTransactionsTest extends Da
         if ($db['driver'] === 'sqlite' && $db['database'] == ':memory:') {
             $this->markTestSkipped('Test cannot be used with in-memory SQLite connection.');
         }
+
+        $this->driver = $db['driver'];
     }
 }
