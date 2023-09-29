5f43b061b57b66fc7321e38f74c83ec9d887200d
inxty <146063723+inxty@users.noreply.github.com>
2023-09-29 14:46:27 +0300

    Fix retrieving the date from the given PHP server output

--- src/Illuminate/Foundation/Console/ServeCommand.php
+++ src/Illuminate/Foundation/Console/ServeCommand.php
@@ -309,7 +309,11 @@ class ServeCommand extends Command
 
         preg_match($regex, $line, $matches);
 
-        return Carbon::createFromFormat('D M d H:i:s Y', $matches[1]);
+        if (isset($matches[1])) { 
+            return Carbon::createFromFormat('D M d H:i:s Y', $matches[1]);
+        } else {
+            Carbon::now();
+        }
     }
 
     /**
