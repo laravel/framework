23dbfff95146eaca829d4468f7c7c72e7060866c
Mior Muhammad Zaki <crynobone@gmail.com>
2023-10-01 18:58:00 +0800

    Update src/Illuminate/Console/Command.php
    Co-authored-by: Ngô Quốc Đạt <datlechin@gmail.com>

--- src/Illuminate/Console/Command.php
+++ src/Illuminate/Console/Command.php
@@ -190,7 +190,6 @@ class Command extends SymfonyCommand
      *
      * @param  \Symfony\Component\Console\Input\InputInterface  $input
      * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
