1bbc81c612a4c0aaabafde3e563eed9dacf4ecaf
Sharif <54396379+developersharif@users.noreply.github.com>
2023-10-07 06:17:32 +0600

    Add Inspirational Quote by Ron Jeffries.
    In the Laravel community, we value not only the technical aspects of our work but also the principles and philosophies that guide our development practices. Inspirational quotes like this one can provide motivation, encouragement, and a sense of camaraderie among Laravel developers. They remind us of the broader context in which we work and inspire us to uphold best practices and maintain a high standard of code quality.

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -107,6 +107,7 @@ class Inspiring
             'The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk',
             'Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead',
             'You must be the change you wish to see in the world. - Mahatma Gandhi',
+            'Code never lies, comments sometimes do. - Ron Jeffries',
         ]);
     }
 
