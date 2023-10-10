3b40b3696d37557d2125130fc49fed343b155f80
Mohammad ALTAWEEL <hello@mohammad.tech>
2023-10-10 15:56:32 +0300

    optimize `exists` validation for empty array input (#48684)

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -864,6 +864,10 @@ trait ValidatesAttributes
 
         $expected = is_array($value) ? count(array_unique($value)) : 1;
 
+        if ($expected === 0) {
+            return true;
+        }
+
         return $this->getExistCount(
             $connection, $table, $column, $value, $parameters
         ) >= $expected;
