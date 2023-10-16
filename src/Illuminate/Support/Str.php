1bae4cdd1efe8339ac548108234a1802eb5664ea
Hosmel Quintana <hosmelq@gmail.com>
2023-10-16 14:25:46 -0600

    [10.x] Add `replaceMatches` to Str class (#48727)
    * add replaceMatches to Str class
    
    * Update Str.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -1142,6 +1142,24 @@ class Str
     }
 
     /**
+     * Replace the patterns matching the given regular expression.
+     *
+     * @param  string  $pattern
+     * @param  \Closure|string  $replace
+     * @param  array|string  $subject
+     * @param  int  $limit
+     * @return string|string[]|null
+     */
+    public function replaceMatches($pattern, $replace, $subject, $limit = -1)
+    {
+        if ($replace instanceof Closure) {
+            return preg_replace_callback($pattern, $replace, $subject, $limit);
+        }
+
+        return preg_replace($pattern, $replace, $subject, $limit);
+    }
+
+    /**
      * Remove any occurrence of the given string in the subject.
      *
      * @param  string|iterable<string>  $search
