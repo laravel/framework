b009464522811d4e0db26f88efe3c839a909bbd9
Dries Vints <dries@vints.be>
2023-10-24 13:24:17 +0200

    Fix issue with ArrayCache

--- src/Illuminate/Cache/ArrayStore.php
+++ src/Illuminate/Cache/ArrayStore.php
@@ -57,7 +57,7 @@ class ArrayStore extends TaggableStore implements LockProvider
 
         $expiresAt = $item['expiresAt'] ?? 0;
 
-        if ($expiresAt !== 0 && (now()->getPreciseTimestamp(3) / 1000) >= $expiresAt) {
+        if ($expiresAt !== 0 && (now()->getPreciseTimestamp(3) / 1000) > $expiresAt) {
             $this->forget($key);
 
             return;
