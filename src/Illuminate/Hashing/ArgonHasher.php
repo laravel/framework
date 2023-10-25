98ffb759b5ac5918aa26dac4676d8fb713c810ea
Tim MacDonald <hello@timacdonald.me>
2023-10-26 06:32:34 +1100

    [10.x] Verify hash config (#48814)
    * Verify hash config
    
    * formatting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Hashing/ArgonHasher.php
+++ src/Illuminate/Hashing/ArgonHasher.php
@@ -95,7 +95,7 @@ class ArgonHasher extends AbstractHasher implements HasherContract
      */
     public function check($value, $hashedValue, array $options = [])
     {
-        if ($this->verifyAlgorithm && $this->info($hashedValue)['algoName'] !== 'argon2i') {
+        if ($this->verifyAlgorithm && ! $this->isUsingCorrectAlgorithm($hashedValue)) {
             throw new RuntimeException('This password does not use the Argon2i algorithm.');
         }
 
@@ -116,6 +116,56 @@ class ArgonHasher extends AbstractHasher implements HasherContract
             'time_cost' => $this->time($options),
             'threads' => $this->threads($options),
         ]);
+    }
+
+    /**
+     * Verifies that the configuration is less than or equal to what is configured.
+     *
+     * @internal
+     */
+    public function verifyConfiguration($value)
+    {
+        return $this->isUsingCorrectAlgorithm($value) && $this->isUsingValidOptions($value);
+    }
+
+    /**
+     * Verify the hashed value's algorithm.
+     *
+     * @param  string  $hashedValue
+     * @return bool
+     */
+    protected function isUsingCorrectAlgorithm($hashedValue)
+    {
+        return $this->info($hashedValue)['algoName'] === 'argon2i';
+    }
+
+    /**
+     * Verify the hashed value's options.
+     *
+     * @param  string  $hashedValue
+     * @return bool
+     */
+    protected function isUsingValidOptions($hashedValue)
+    {
+        ['options' => $options] = $this->info($hashedValue);
+
+        if (
+            ! is_int($options['memory_cost'] ?? null) ||
+            ! is_int($options['time_cost'] ?? null) ||
+            ! is_int($options['threads'] ?? null)
+        ) {
+            return false;
+        }
+
+        if (
+            $options['memory_cost'] > $this->memory ||
+            $options['time_cost'] > $this->time ||
+            $options['threads'] > $this->threads
+        ) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
