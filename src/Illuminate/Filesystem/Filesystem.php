7a74e37850287e1e63c74e8f7ca06b8b4dc254e0
Caen De Silva <caen@desilva.se>
2023-10-26 13:13:30 +0200

    Fix style

--- src/Illuminate/Filesystem/Filesystem.php
+++ src/Illuminate/Filesystem/Filesystem.php
@@ -799,7 +799,7 @@ class Filesystem
      */
     public function bytesToHuman($bytes, $precision = 2)
     {
-        $units = ['B','KB','MB','GB','TB','PB','EB','ZB','YB'];
+        $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
         for ($i = 0; ($bytes / 1024) > 0.9 && ($i < count($units) - 1); $i++) {
             $bytes /= 1024;
