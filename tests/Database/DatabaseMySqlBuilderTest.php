b517edca467716b5854a1d88b7899ff3fcd7d40d
Alexey Vesnin <avesnin@protonmail.ch>
2023-10-27 19:08:35 +0300

    Update DatabaseMySqlBuilderTest.php
    Fixing corresponding test

--- tests/Database/DatabaseMySqlBuilderTest.php
+++ tests/Database/DatabaseMySqlBuilderTest.php
@@ -24,7 +24,7 @@ class DatabaseMySqlBuilderTest extends TestCase
         $connection->shouldReceive('getConfig')->once()->with('collation')->andReturn('utf8mb4_unicode_ci');
         $connection->shouldReceive('getSchemaGrammar')->once()->andReturn($grammar);
         $connection->shouldReceive('statement')->once()->with(
-            'create database `my_temporary_database` default character set `utf8mb4` default collate `utf8mb4_unicode_ci`'
+            'create database if not exists `my_temporary_database` default character set `utf8mb4` default collate `utf8mb4_unicode_ci`'
         )->andReturn(true);
 
         $builder = new MySqlBuilder($connection);
