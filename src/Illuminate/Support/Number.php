b5fb4a901a7946da134d3985c427ebe6082a0203
Caen De Silva <caen@desilva.se>
2023-10-28 10:42:13 +0200

    Create a new Number utility class

---
+++ src/Illuminate/Support/Number.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Illuminate\Support;
+
+use Illuminate\Support\Traits\Macroable;
+
+class Number
+{
+    use Macroable;
+
+    //
+}
---
+++ tests/Support/SupportNumberTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Illuminate\Tests\Support;
+
+use Illuminate\Support\Number;
+use PHPUnit\Framework\TestCase;
+
+class SupportNumberTest extends TestCase
+{
+    //
+}
