fb0e6ce4cf13ee7f51c31c1b764cc7e639481b73
Caen De Silva <caen@desilva.se>
2023-10-28 10:44:46 +0200

    Add a `Number::bytesToHuman()` helper
    Ports https://github.com/laravel/framework/pull/48827 into the new `Number` utility class

--- src/Illuminate/Support/Number.php
+++ src/Illuminate/Support/Number.php
@@ -8,5 +8,21 @@ class Number
 {
     use Macroable;
 
-    //
+    /**
+     * Format the number of bytes to a human-readable string.
+     *
+     * @param  int  $bytes
+     * @param  int  $precision
+     * @return string
+     */
+    public static function bytesToHuman($bytes, $precision = 2)
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        for ($i = 0; ($bytes / 1024) > 0.9 && ($i < count($units) - 1); $i++) {
+            $bytes /= 1024;
+        }
+
+        return sprintf('%s %s', round($bytes, $precision), $units[$i]);
+    }
 }
--- tests/Support/SupportNumberTest.php
+++ tests/Support/SupportNumberTest.php
@@ -7,5 +7,19 @@ use PHPUnit\Framework\TestCase;
 
 class SupportNumberTest extends TestCase
 {
-    //
+    public function testBytesToHuman()
+    {
+        $this->assertSame('0 B', Number::bytesToHuman(0));
+        $this->assertSame('1 B', Number::bytesToHuman(1));
+        $this->assertSame('1 KB', Number::bytesToHuman(1024));
+        $this->assertSame('2 KB', Number::bytesToHuman(2048));
+        $this->assertSame('1.23 KB', Number::bytesToHuman(1264));
+        $this->assertSame('1.234 KB', Number::bytesToHuman(1264, 3));
+        $this->assertSame('5 GB', Number::bytesToHuman(1024 * 1024 * 1024 * 5));
+        $this->assertSame('10 TB', Number::bytesToHuman((1024 ** 4) * 10));
+        $this->assertSame('10 PB', Number::bytesToHuman((1024 ** 5) * 10));
+        $this->assertSame('1 ZB', Number::bytesToHuman(1024 ** 7));
+        $this->assertSame('1 YB', Number::bytesToHuman(1024 ** 8));
+        $this->assertSame('1024 YB', Number::bytesToHuman(1024 ** 9));
+    }
 }
