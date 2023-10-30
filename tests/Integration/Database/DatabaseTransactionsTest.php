8052b388c0425157a783a095084cfb26c6a81f97
Mateus Guimarães <mateus.jatene@gmail.com>
2023-10-29 21:18:04 -0300

    failing test

---
+++ tests/Integration/Database/DatabaseTransactionsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Database;
+
+use Illuminate\Support\Facades\DB;
+
+class DatabaseTransactionsTest extends DatabaseTestCase
+{
+    public function testTransactionCallbacksDoNotInterfereWithOneAnother()
+    {
+        [$firstObject, $secondObject, $thirdObject] = [
+            new TestObjectForTransactions(),
+            new TestObjectForTransactions(),
+            new TestObjectForTransactions(),
+        ];
+
+        DB::transaction(function () use ($thirdObject, $secondObject, $firstObject) { // Adds a transaction @ level 1
+            DB::transaction(function () use ($firstObject) { // Adds a transaction @ level 2
+                DB::afterCommit(fn () => $firstObject->handle()); // Adds a callback to be executed after transaction level 2 is committed
+            });
+
+            DB::afterCommit(fn () => $secondObject->handle()); // Adds a callback to be executed after transaction 1 @ lvl 1
+
+            try {
+                DB::transaction(function () use ($thirdObject) { // Adds a transaction 3 @ level 2
+                    DB::afterCommit(fn () => $thirdObject->handle());
+                    throw new \Exception();
+                });
+            } catch (\Exception) {}
+        });
+
+        $this->assertTrue($firstObject->ran);
+        $this->assertTrue($secondObject->ran);
+        $this->assertFalse($thirdObject->ran);
+    }
+}
+
+class TestObjectForTransactions
+{
+    public bool $ran = false;
+
+    public function handle()
+    {
+        $this->ran = true;
+    }
+}
