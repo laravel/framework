0aeececbde74888cfc24839961dd8c49452eb05f
sidneyprins <84329386+sidneyprins@users.noreply.github.com>
2023-10-30 15:29:38 +0100

    [10.x] Throw exception when trying to escape array for database connection (#48836)
    * Throw error when trying to escape array
    
    * Add tests to test escaping array throwing exception
    
    * Update Connection.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- tests/Integration/Database/SqlServer/EscapeTest.php
+++ tests/Integration/Database/SqlServer/EscapeTest.php
@@ -57,4 +57,11 @@ class EscapeTest extends SqlServerTestCase
 
         $this->app['db']->escape("I am hiding a \00 byte");
     }
+
+    public function testEscapeArray()
+    {
+        $this->expectException(RuntimeException::class);
+
+        $this->app['db']->escape(['a', 'b']);
+    }
 }
