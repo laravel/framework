7472b0b32e37ee81dff2f30e274378341101f052
Günther Debrauwer <gunther@nextapps.be>
2023-11-03 15:19:58 +0100

    Support throw and retry in async request

--- src/Illuminate/Http/Client/PendingRequest.php
+++ src/Illuminate/Http/Client/PendingRequest.php
@@ -997,9 +997,10 @@ class PendingRequest
      * @param  string  $method
      * @param  string  $url
      * @param  array  $options
+     * @param  integer  $attempt
      * @return \GuzzleHttp\Promise\PromiseInterface
      */
-    protected function makePromise(string $method, string $url, array $options = [])
+    protected function makePromise(string $method, string $url, array $options = [], int $attempt = 1)
     {
         return $this->promise = $this->sendRequest($method, $url, $options)
             ->then(function (MessageInterface $message) {
@@ -1011,9 +1012,50 @@ class PendingRequest
             ->otherwise(function (TransferException $e) {
                 if ($e instanceof ConnectException) {
                     $this->dispatchConnectionFailedEvent();
+
+                    return new ConnectionException($e->getMessage(), 0, $e);
                 }
 
                 return $e instanceof RequestException && $e->hasResponse() ? $this->populateResponse($this->newResponse($e->getResponse())) : $e;
+            })
+            ->then(function (Response|TransferException $response) use ($method, $url, $options, $attempt) {
+                if ($response instanceof Response && $response->successful()) {
+                    return $response;
+                }
+
+                if ($response instanceof RequestException) {
+                    $response = $this->populateResponse($this->newResponse($response->getResponse()));
+                }
+
+                try {
+                    $shouldRetry = $this->retryWhenCallback ? call_user_func($this->retryWhenCallback, $response->toException(), $this) : true;
+                } catch (Exception $exception) {
+                    $shouldRetry = false;
+
+                    return $exception;
+                }
+
+                if ($this->throwCallback && ($this->throwIfCallback === null || call_user_func($this->throwIfCallback, $response))) {
+                    try {
+                        $response->throw($this->throwCallback);
+                    } catch (Exception $exception) {
+                        return $exception;
+                    }
+                }
+
+                if ($attempt < $this->tries && $shouldRetry) {
+                    if ($this->retryDelay) {
+                        $options['delay'] = $this->retryDelay;
+                    }
+
+                    return $this->makePromise($method, $url, $options, $attempt + 1);
+                }
+
+                if ($this->tries > 1 && $this->retryThrow) {
+                    return $response->toException();
+                }
+
+                return $response;
             });
     }
 
