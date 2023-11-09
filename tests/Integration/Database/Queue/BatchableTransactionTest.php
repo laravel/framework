0a6d4ce0dd1061258f48933a3d2b6e83fd2819b0
Mior Muhammad Zaki <crynobone@gmail.com>
2023-11-10 07:57:01 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Database/Queue/BatchableTransactionTest.php
+++ tests/Integration/Database/Queue/BatchableTransactionTest.php
@@ -7,61 +7,55 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Tests\Integration\Database\DatabaseTestCase;
 use Orchestra\Testbench\Attributes\WithMigration;
-use Symfony\Component\Process\Exception\ProcessTimedOutException;
+use Symfony\Component\Process\Exception\ProcessSignaledException;
+use Symfony\Component\Process\Process;
 use Throwable;
-
 use function Orchestra\Testbench\remote;
 
-#[WithMigration]
-#[WithMigration('queue')]
+#[WithMigration('laravel', 'queue')]
 class BatchableTransactionTest extends DatabaseTestCase
 {
     use DatabaseMigrations;
 
     protected function defineEnvironment($app)
     {
+        if (Env::get('DB_CONNECTION') === 'testing') {
+            // $this->markTestSkipped('Test does not support using :memory: database connection');
+        }
+
         $app['config']->set([
+            'database.default' => 'sqlite',
             'queue.default' => 'database',
+            'queue.batching.database' => 'sqlite',
         ]);
     }
 
     public function testItCanHandleTimeoutJob()
     {
-        Bus::batch([new TimeOutJobWithTransaction()])
+        Bus::batch([new Fixtures\TimeOutJobWithTransaction()])
             ->allowFailures()
             ->dispatch();
 
-        sleep(2);
+        $this->assertSame(1, DB::table('jobs')->count());
+        $this->assertSame(0, DB::table('failed_jobs')->count());
+        $this->assertSame(1, DB::table('job_batches')->count());
 
         try {
-            $process = remote('queue:work')->setTimeout(2);
-            $process->run();
+            remote('queue:work', [
+                'DB_CONNECTION' => config('database.default'),
+                'QUEUE_CONNECTION' => config('queue.default'),
+            ])->run();
         } catch (Throwable $e) {
-            $this->assertInstanceOf(ProcessTimedOutException::class, $e);
+            $this->assertInstanceOf(ProcessSignaledException::class, $e);
+            $this->assertSame('The process has been signaled with signal "9".', $e->getMessage());
         }
 
-        dd(
-            $process->getOutput(),
-            DB::table('jobs')->get(),
-            DB::table('failed_jobs')->get(),
-            DB::table('job_batches')->get()
-        );
-    }
-}
-
-class TimeOutJobWithTransaction implements ShouldQueue
-{
-    use InteractsWithQueue, Queueable, Batchable;
-
-    public int $tries = 1;
-    public int $timeout = 2;
-
-    public function handle(): void
-    {
-        DB::transaction(fn () => sleep(20));
+        $this->assertSame(0, DB::table('jobs')->count());
+        $this->assertSame(1, DB::table('failed_jobs')->count());
     }
 }
