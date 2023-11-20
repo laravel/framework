5519ca085bc7c4a6fe4c9c5a759dcd8f8931678a
Taylor Otwell <taylor@laravel.com>
2023-11-20 15:24:59 -0600

    add slack config

--- config/services.php
+++ config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'slack' => [
+        'notifications' => [
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+        ],
+    ],
+
 ];
