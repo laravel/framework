742f83518e6c427c32b74996b3bc6b789c7128d6
Rob <30407321+robtesch@users.noreply.github.com>
2023-11-21 16:21:31 +0000

    [10.x] Prevent passing null to base64_decode in Encrypter (#49071)
    * Prevent passing null to base64_decode in Encrypter
    
    * Implement styleCI recommended changes
    
    * Update Encrypter.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Encryption/Encrypter.php
+++ src/Illuminate/Encryption/Encrypter.php
@@ -205,6 +205,10 @@ class Encrypter implements EncrypterContract, StringEncrypter
      */
     protected function getJsonPayload($payload)
     {
+        if (! is_string($payload)) {
+            throw new DecryptException('The payload is invalid.');
+        }
+
         $payload = json_decode(base64_decode($payload), true);
 
         // If the payload is not valid JSON or does not have the proper keys set we will
