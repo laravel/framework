e0249d3d588f03da4a231cfa2a996187f38c9785
Nuno Maduro <enunomaduro@gmail.com>
2023-11-24 14:49:24 +0000

    Adds `frontend_url` to `app.php` (#49118)

--- config/app.php
+++ config/app.php
@@ -57,6 +57,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
     'asset_url' => env('ASSET_URL'),
 
     /*
