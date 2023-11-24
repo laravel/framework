aa891a096a67fb69e34dfcc05c3d86d4f79ea235
Seb <sebastien@squareup.com>
2023-11-24 12:24:03 -0800

    hmm

--- tests/Bus/DynamoBatchTestWithTTL.php
+++ tests/Bus/DynamoBatchTestWithTTL.php
@@ -1,27 +1,27 @@
 <?php
 
-namespace Illuminate\Tests\Bus;
+// namespace Illuminate\Tests\Bus;
 
-class DynamoBatchTestWithTTL extends DynamoBatchTest
-{
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('queue.connections.sync1', [
-            'driver' => 'sync',
-        ]);
+// class DynamoBatchTestWithTTL extends DynamoBatchTest
+// {
+//     protected function getEnvironmentSetUp($app)
+//     {
+//         $app['config']->set('queue.connections.sync1', [
+//             'driver' => 'sync',
+//         ]);
 
-        $app['config']->set('queue.connections.sync2', [
-            'driver' => 'sync',
-        ]);
+//         $app['config']->set('queue.connections.sync2', [
+//             'driver' => 'sync',
+//         ]);
 
-        $app['config']->set('queue.batching', [
-            'driver' => 'dynamodb',
-            'region' => 'us-west-2',
-            'endpoint' => static::DYNAMO_ENDPOINT,
-            'key' => 'key',
-            'secret' => 'secret',
-            'ttl' => 60,
-            'ttlAttribute' => 'ttl_value',
-        ]);
-    }
-}
+//         $app['config']->set('queue.batching', [
+//             'driver' => 'dynamodb',
+//             'region' => 'us-west-2',
+//             'endpoint' => static::DYNAMO_ENDPOINT,
+//             'key' => 'key',
+//             'secret' => 'secret',
+//             'ttl' => 60,
+//             'ttlAttribute' => 'ttl_value',
+//         ]);
+//     }
+// }
