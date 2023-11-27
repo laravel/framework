2fd25f628111f97bac20b8aca6cac43ddf8b4d5f
taylorotwell <taylorotwell@users.noreply.github.com>
2023-11-27 15:31:31 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/Route.php
+++ src/Illuminate/Support/Facades/Route.php
@@ -90,6 +90,7 @@ namespace Illuminate\Support\Facades;
  * @method static \Illuminate\Routing\RouteRegistrar controller(string $controller)
  * @method static \Illuminate\Routing\RouteRegistrar domain(string $value)
  * @method static \Illuminate\Routing\RouteRegistrar middleware(array|string|null $middleware)
+ * @method static \Illuminate\Routing\RouteRegistrar missing(\Closure $missing)
  * @method static \Illuminate\Routing\RouteRegistrar name(string $value)
  * @method static \Illuminate\Routing\RouteRegistrar namespace(string|null $value)
  * @method static \Illuminate\Routing\RouteRegistrar prefix(string $prefix)
