e82b7e8ca849248e8986d7fb9bfe0258b232c63c
Taylor Otwell <taylor@laravel.com>
2023-11-30 16:36:36 -0600

    add partitioned entry

--- config/session.php
+++ config/session.php
@@ -199,4 +199,17 @@ return [
 
     'same_site' => env('SESSION_SAME_SITE', 'lax'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Partitioned Cookies
+    |--------------------------------------------------------------------------
+    |
+    | Setting this value to true will tie the cookie to the top-level site for
+    | a cross-site context. Partitioned cookies are accepted by the browser
+    | when flagged "secure" and the Same-Site attribute is set to "none".
+    |
+    */
+
+    'partitioned' => false,
+
 ];
