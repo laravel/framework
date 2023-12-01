e04ac7f3770e69127fcb92bf2d1c1b54e7ec0a67
junyang-chin <danny_cjy@hotmail.com>
2023-12-01 13:55:43 +0800

    add test

--- tests/Integration/Generators/ProviderMakeCommandTest.php
+++ tests/Integration/Generators/ProviderMakeCommandTest.php
@@ -21,4 +21,18 @@ class ProviderMakeCommandTest extends TestCase
             'public function boot()',
         ], 'app/Providers/FooServiceProvider.php');
     }
+
+    public function testItCanGenerateDeferredServiceProviderFile()
+    {
+        $this->artisan('make:provider', ['name' => 'FooServiceProvider'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Providers;',
+            'use Illuminate\Support\ServiceProvider;',
+            'class FooServiceProvider extends ServiceProvider implements DeferrableProvider',
+            'public function register()',
+            'public function provides()',
+        ], 'app/Providers/FooServiceProvider.php');
+    }
 }
