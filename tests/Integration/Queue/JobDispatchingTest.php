3d9e7798f2f43612981a319a4045e7e2de45075c
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-12 19:25:37 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Queue/JobDispatchingTest.php
+++ tests/Integration/Queue/JobDispatchingTest.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Orchestra\Testbench\Attributes\WithMigration;
 
 #[WithMigration('queue')]
 class JobDispatchingTest extends QueueTestCase
