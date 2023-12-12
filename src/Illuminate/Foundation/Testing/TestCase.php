8dc1054f3e907b019ef26b1ee288d1d40d94ae96
武田 憲太郎 <takeda@youmind.jp>
2023-12-12 14:59:48 +0900

    fix: Behavior when run without database

--- src/Illuminate/Foundation/Testing/TestCase.php
+++ src/Illuminate/Foundation/Testing/TestCase.php
@@ -197,8 +197,8 @@ abstract class TestCase extends BaseTestCase
 
             ParallelTesting::callTearDownTestCaseCallbacks($this);
 
-            $database = $this->app['db'];
-            foreach (array_keys($database->getConnections()) as $name) {
+            $database = $this->app['db'] ?? null;
+            foreach (array_keys($database?->getConnections() ?? []) as $name) {
                 $database->purge($name);
             }
 
