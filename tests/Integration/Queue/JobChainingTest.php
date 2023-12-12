7b47bbcafd4948770d95d0df07c811b293b4497c
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-12 19:08:11 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Queue/JobChainingTest.php
+++ tests/Integration/Queue/JobChainingTest.php
@@ -395,7 +395,7 @@ class JobChainingTest extends QueueTestCase
 
         $this->assertEquals(
             match ($this->getQueueDriver()) {
-                'sync' => ['c1', 'c2', 'bc1', 'bc2', 'bb1', 'bb2', 'b1', 'b2-0', 'b2-1', 'b2-2', 'b2-3', 'b2', 'b3', 'b4', 'c3']
+                'sync' => ['c1', 'c2', 'bc1', 'bc2', 'bb1', 'bb2', 'b1', 'b2-0', 'b2-1', 'b2-2', 'b2-3', 'b2', 'b3', 'b4', 'c3'],
                 default => ['c1', 'c2', 'bc1', 'b1', 'b2', 'b3', 'b4', 'bc2', 'b2-0', 'b2-1', 'b2-2', 'b2-3', 'bb1', 'bb2', 'c3'],
             },
             JobRunRecorder::$results
