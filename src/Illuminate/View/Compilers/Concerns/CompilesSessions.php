6f620c991cd0370cc32458a03ac4f6617d403728
Jared Lewis <17649602+jrd-lewis@users.noreply.github.com>
2023-12-13 15:15:38 -0500

    Refactor to `$__sessionPrevious` array for nesting

--- src/Illuminate/View/Compilers/Concerns/CompilesSessions.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesSessions.php
@@ -16,7 +16,7 @@ trait CompilesSessions
 
         return '<?php $__sessionArgs = ['.$expression.'];
 if (session()->has($__sessionArgs[0])) :
-if (isset($session)) { $__sessionOriginal = $session; }
+if (isset($session)) { $__sessionPrevious[] = $session; }
 $session = session()->get($__sessionArgs[0]); ?>';
     }
 
@@ -29,7 +29,7 @@ $session = session()->get($__sessionArgs[0]); ?>';
     protected function compileEndsession($expression)
     {
         return '<?php unset($session);
-if (isset($__sessionOriginal)) { $session = $__sessionOriginal; }
+if (isset($__sessionPrevious) && !empty($__sessionPrevious)) { $session = array_pop($__sessionPrevious); }
 endif;
 unset($__sessionArgs); ?>';
     }
--- tests/View/Blade/BladeSessionTest.php
+++ tests/View/Blade/BladeSessionTest.php
@@ -13,11 +13,11 @@ class BladeSessionTest extends AbstractBladeTestCase
         $expected = '
 <?php $__sessionArgs = [\'status\'];
 if (session()->has($__sessionArgs[0])) :
-if (isset($session)) { $__sessionOriginal = $session; }
+if (isset($session)) { $__sessionPrevious[] = $session; }
 $session = session()->get($__sessionArgs[0]); ?>
     <span><?php echo e($session); ?></span>
 <?php unset($session);
-if (isset($__sessionOriginal)) { $session = $__sessionOriginal; }
+if (isset($__sessionPrevious) && !empty($__sessionPrevious)) { $session = array_pop($__sessionPrevious); }
 endif;
 unset($__sessionArgs); ?>';
 
