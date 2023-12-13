4e0b1256077fa997d2a77a081e4ca790fe8e01ca
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-13 21:22:29 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Queue/DynamoBatchTestWithTTL.php
+++ tests/Integration/Queue/DynamoBatchTestWithTTL.php
@@ -3,19 +3,17 @@
 namespace Illuminate\Tests\Integration\Queue;
 
 use Illuminate\Support\Env;
+use Orchestra\Testbench\Attributes\RequiresEnv;
 
+#[RequiresEnv('DYNAMODB_ENDPOINT')]
 class DynamoBatchTestWithTTL extends DynamoBatchTest
 {
     protected function defineEnvironment($app)
     {
-        if (is_null($endpoint = Env::get('DYNAMODB_ENDPOINT'))) {
-            $this->markTestSkipped('Require `dynamodb` to be configured');
-        }
-
         $app['config']->set('queue.batching', [
             'driver' => 'dynamodb',
             'region' => 'us-west-2',
-            'endpoint' => $endpoint,
+            'endpoint' => Env::get('DYNAMODB_ENDPOINT'),
             'key' => 'key',
             'secret' => 'secret',
             'ttl' => 1,
