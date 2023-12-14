508e7098a40d780e70d7bb24928b4ac61fedeb3e
rmunate <raulunatecastro@MacBook-Pro-de-Raul.local>
2023-12-13 20:54:56 -0500

    Fixed StyleCI

--- src/Illuminate/Support/Traits/ConstantAccessor.php
+++ src/Illuminate/Support/Traits/ConstantAccessor.php
@@ -6,8 +6,7 @@ trait ConstantAccessor
 {
     /**
      * Access a constant of the class using the trait.
-     *
-     * @param string $name The name of the constant to access.
+     * @param  string  $name  The name of the constant to access.
      *
      * @return mixed The value of the constant.
      * @throws \ReflectionException If the constant does not exist or is not accessible.
@@ -15,8 +14,8 @@ trait ConstantAccessor
     public static function constant(string $name)
     {
         $class = get_called_class();
-        
-        if (!defined("$class::$name")) {
+
+        if (! defined("$class::$name")) {
             throw new \ReflectionException("Constant $class::$name does not exist.");
         }
 
