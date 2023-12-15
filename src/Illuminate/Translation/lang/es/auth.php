bf0ecb2d265f5b6154cbd7fc4b3ffcd9bd93e973
Efrain Hernandez <ehernandez@buk.co>
2023-12-14 19:26:18 -0500

    feat: Add spanish to default translations

---
+++ src/Illuminate/Translation/lang/es/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Las credenciales no concuerdan con ningun registro.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de iniciar sesión. Por favor reintente en :seconds segundos.',
+
+];
