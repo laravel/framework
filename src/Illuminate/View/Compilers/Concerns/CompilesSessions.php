ef9baf7fbf33da30e42893585c77484d7e8293e2
Taylor Otwell <taylor@laravel.com>
2023-12-15 09:07:14 -0600

    use value

--- src/Illuminate/View/Compilers/Concerns/CompilesSessions.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesSessions.php
@@ -16,8 +16,8 @@ trait CompilesSessions
 
         return '<?php $__sessionArgs = ['.$expression.'];
 if (session()->has($__sessionArgs[0])) :
-if (isset($session)) { $__sessionPrevious[] = $session; }
-$session = session()->get($__sessionArgs[0]); ?>';
+if (isset($value)) { $__sessionPrevious[] = $value; }
+$value = session()->get($__sessionArgs[0]); ?>';
     }
 
     /**
@@ -28,8 +28,8 @@ $session = session()->get($__sessionArgs[0]); ?>';
      */
     protected function compileEndsession($expression)
     {
-        return '<?php unset($session);
-if (isset($__sessionPrevious) && !empty($__sessionPrevious)) { $session = array_pop($__sessionPrevious); }
+        return '<?php unset($value);
+if (isset($__sessionPrevious) && !empty($__sessionPrevious)) { $value = array_pop($__sessionPrevious); }
 if (isset($__sessionPrevious) && empty($__sessionPrevious)) { unset($__sessionPrevious); }
 endif;
 unset($__sessionArgs); ?>';
--- tests/View/Blade/BladeSessionTest.php
+++ tests/View/Blade/BladeSessionTest.php
@@ -8,16 +8,16 @@ class BladeSessionTest extends AbstractBladeTestCase
     {
         $string = '
 @session(\'status\')
-    <span>{{ $session }}</span>
+    <span>{{ $value }}</span>
 @endsession';
         $expected = '
 <?php $__sessionArgs = [\'status\'];
 if (session()->has($__sessionArgs[0])) :
-if (isset($session)) { $__sessionPrevious[] = $session; }
-$session = session()->get($__sessionArgs[0]); ?>
-    <span><?php echo e($session); ?></span>
-<?php unset($session);
-if (isset($__sessionPrevious) && !empty($__sessionPrevious)) { $session = array_pop($__sessionPrevious); }
+if (isset($value)) { $__sessionPrevious[] = $value; }
+$value = session()->get($__sessionArgs[0]); ?>
+    <span><?php echo e($value); ?></span>
+<?php unset($value);
+if (isset($__sessionPrevious) && !empty($__sessionPrevious)) { $value = array_pop($__sessionPrevious); }
 if (isset($__sessionPrevious) && empty($__sessionPrevious)) { unset($__sessionPrevious); }
 endif;
 unset($__sessionArgs); ?>';
