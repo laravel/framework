fbb5e4fe64a7a6b14eb2e3179d39b7b21f90b010
Mior Muhammad Zaki <crynobone@gmail.com>
2023-12-20 06:35:17 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Filesystem/functions.php
+++ src/Illuminate/Filesystem/functions.php
@@ -6,11 +6,11 @@ if (! function_exists('Illuminate\Filesystem\join_paths')) {
     /**
      * Join the given paths together.
      *
-     * @param  string  $basePath
+     * @param  string|null  $basePath
      * @param  string  ...$paths
      * @return string
      */
-    function join_paths(string $basePath, string ...$paths)
+    function join_paths($basePath, string ...$paths)
     {
         return $basePath.collect($paths)->reject(fn ($path) => empty($path))
                 ->transform(fn ($path) => DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR))
