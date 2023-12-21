5178c97f8f7254058cb72fdd9cb952ecc1299459
Tyler <tylernathanreed@gmail.com>
2023-12-21 09:16:49 -0600

    ~ Don't explicitly test for null

--- tests/Filesystem/JoinPathsHelperTest.php
+++ tests/Filesystem/JoinPathsHelperTest.php
@@ -21,7 +21,6 @@ class JoinPathsHelperTest extends TestCase
     {
         yield ['app/Http/Kernel.php', join_paths('app', 'Http', 'Kernel.php')];
         yield ['app/Http/Kernel.php', join_paths('app', '', 'Http', 'Kernel.php')];
-        yield ['app/Http/Kernel.php', join_paths('app', null, 'Http', 'Kernel.php')];
     }
 
     #[RequiresOperatingSystem('Windows')]
@@ -35,6 +34,5 @@ class JoinPathsHelperTest extends TestCase
     {
         yield ['app\Http\Kernel.php', join_paths('app', 'Http', 'Kernel.php')];
         yield ['app\Http\Kernel.php', join_paths('app', '', 'Http', 'Kernel.php')];
-        yield ['app\Http\Kernel.php', join_paths('app', null, 'Http', 'Kernel.php')];
     }
 }
