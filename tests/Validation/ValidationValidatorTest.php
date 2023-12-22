laravel/framework

commit ddade2c045f983a823420dbef462c98e8ac475c8

Use native json_validate (#49413)

src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1438,6 +1438,10 @@ trait ValidatesAttributes
             return false;
         }
 
+        if (function_exists('json_validate')) {
+            return json_validate($value);
+        }
+
         json_decode($value);
 
         return json_last_error() === JSON_ERROR_NONE;

commit cb19e133d41a844222e696de10fcf94ff036de8e

test: validateJson should return false when value is null
Fails with Laravel Framework 10.38.2 in PHP < 8.3, introduced in #49413

tests/Validation/ValidationValidatorTest.php
@@ -19,6 +19,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Exceptions\MathException;
+use Illuminate\Support\Stringable;
 use Illuminate\Translation\ArrayLoader;
 use Illuminate\Translation\Translator;
 use Illuminate\Validation\DatabasePresenceVerifierInterface;
@@ -2817,6 +2818,14 @@ class ValidationValidatorTest extends TestCase
         $trans = $this->getIlluminateArrayTranslator();
         $v = new Validator($trans, ['foo' => ['array']], ['foo' => 'json']);
         $this->assertFalse($v->passes());
+
+        $trans = $this->getIlluminateArrayTranslator();
+        $v = new Validator($trans, ['foo' => null], ['foo' => 'json']);
+        $this->assertFalse($v->passes());
+
+        $trans = $this->getIlluminateArrayTranslator();
+        $v = new Validator($trans, ['foo' => new Stringable('[]')], ['foo' => 'json']);
+        $this->assertTrue($v->passes());
     }
 
     public function testValidateBoolean()