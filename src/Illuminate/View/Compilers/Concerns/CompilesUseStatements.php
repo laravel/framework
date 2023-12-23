176483a94a809e2bf759d4a8ed7024f728bfce74
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-23 21:36:49 +0530

    Add support for array-based use statements in Blade templates

--- src/Illuminate/View/Compilers/Concerns/CompilesUseStatements.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesUseStatements.php
@@ -12,11 +12,25 @@ trait CompilesUseStatements
      */
     protected function compileUse($expression)
     {
-        $segments = explode(',', preg_replace("/[\(\)]/", '', $expression));
+        $expression = preg_replace("/[\(\)]/", '', $expression);
 
-        $use = ltrim(trim($segments[0], " '\""), '\\');
-        $as = isset($segments[1]) ? ' as '.trim($segments[1], " '\"") : '';
+        // if it is not start with '[' therefore it is single namespace
+        // so we need to convert it to $namespace => $alias expression
+        if (! str_starts_with($expression, '[')) {
+            $expression = str_replace(',', '=>', $expression);
+        }
 
-        return "<?php use \\{$use}{$as}; ?>";
+        // it is start with '[' therefore it is array and it may have multiple namespaces
+        // as it won't be valid json we need to parse it manually and get namespaces and aliases
+        // below code is to get namespaces and aliases from [$namespace => $alias, ...] expression
+        $namespaces = explode(',', trim(preg_replace('/\\\\/', '\\', $expression), '[]'));
+
+        $useStatements = '<?php';
+        foreach ($namespaces as $namespace) {
+            [$use, $as] = array_pad(explode('=>', $namespace, 2), 2, '');
+            $useStatements .= ' use \\'.ltrim(trim($use, " '\""), '\\').($as ? ' as '.ltrim(trim($as, " '\""), '\\') : '').';';
+        }
+
+        return $useStatements.' ?>';
     }
 }
