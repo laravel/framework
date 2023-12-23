0229c3642b0aa92969629282a95068580d84d666
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-23 17:34:07 +0530

    Fix use statement compilation in Blade templates

--- src/Illuminate/View/Compilers/Concerns/CompilesUseStatements.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesUseStatements.php
@@ -15,7 +15,7 @@ trait CompilesUseStatements
         $segments = explode(',', preg_replace("/[\(\)]/", '', $expression));
 
         $use = trim($segments[0], " '\"");
-        $as = isset($segments[1]) ? ' as '.trim($segments[1], " '\"") : '';
+        $as = isset($segments[1]) ? ' as '.ltrim(trim($segments[0], " '\""), '\\') : '';
 
         return "<?php use \\{$use}{$as}; ?>";
     }
--- tests/View/Blade/BladeUseTest.php
+++ tests/View/Blade/BladeUseTest.php
@@ -17,4 +17,11 @@ class BladeUseTest extends AbstractBladeTestCase
         $expected = "Foo <?php use \SomeNamespace\SomeClass; ?> bar";
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
+
+    public function testUseStatementsWithBackslashAtBeginningAreCompiled()
+    {
+        $string = "Foo @use('\SomeNamespace\SomeClass', 'Foo') bar";
+        $expected = "Foo <?php use \SomeNamespace\SomeClass as Foo; ?> bar";
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+    }
 }
