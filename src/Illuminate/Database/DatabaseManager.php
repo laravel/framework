4ede364bbe93ceef70d309390062e6cfa188b78b
Marco Aurélio Deleu <deleugyn@gmail.com>
2023-12-30 15:39:27 -0300

    StyleCI

--- src/Illuminate/Database/DatabaseManager.php
+++ src/Illuminate/Database/DatabaseManager.php
@@ -114,7 +114,6 @@ class DatabaseManager implements ConnectionResolverInterface
      * @param  array  $config
      * @return \Illuminate\Database\ConnectionInterface
      */
-
     public function connectUsing(string $name, array $config): ConnectionInterface
     {
         if (isset($this->connections[$name])) {
@@ -247,7 +246,7 @@ class DatabaseManager implements ConnectionResolverInterface
     /**
      * If the event dispatcher is available dispatches the ConnectionEstablished event.
      *
-     * @param Connection $connection
+     * @param  Connection  $connection
      * @return void
      */
     protected function dispatchConnectionEstablishedEvent(Connection $connection): void
--- tests/Integration/Database/DatabaseConnectionsTest.php
+++ tests/Integration/Database/DatabaseConnectionsTest.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Illuminate\Tests\Integration\Database;
 
