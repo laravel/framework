bd3161db1c7d52a3f5521240f0609703ca0ffc1e
Dwight Watson <dwightwatson@users.noreply.github.com>
2024-01-04 09:42:32 +1100

    [10.x] Add assertViewEmpty to TestView (#49558)
    * Add assertEmpty to TestView
    
    * Update TestView.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Testing/TestView.php
+++ src/Illuminate/Testing/TestView.php
@@ -107,6 +107,16 @@ class TestView
     }
 
     /**
+     * Assert that the view's rendered content is empty.
+     */
+    public function assertViewEmpty()
+    {
+        PHPUnit::assertEmpty($this->rendered);
+
+        return true;
+    }
+
+    /**
      * Assert that the given string is contained within the view.
      *
      * @param  string  $value
