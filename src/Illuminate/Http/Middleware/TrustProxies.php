3193509ea30517889375d7474dcd3d9151883248
basteln3rk <gnutzmann@flexponsive.net>
2024-01-05 23:35:14 +0000

    change: determine default proxy header mappings only through getTrustedHeaderNames() method. fix that "default" defined through class property did not include HEADER_X_FORWARDED_PREFIX while default defined through getTrustedHeaderNames() did

--- src/Illuminate/Http/Middleware/TrustProxies.php
+++ src/Illuminate/Http/Middleware/TrustProxies.php
@@ -19,11 +19,7 @@ class TrustProxies
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_FOR |
-                         Request::HEADER_X_FORWARDED_HOST |
-                         Request::HEADER_X_FORWARDED_PORT |
-                         Request::HEADER_X_FORWARDED_PROTO |
-                         Request::HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers;
 
     /**
      * Handle an incoming request.
