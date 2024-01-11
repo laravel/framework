659cf83e900cc033d7f24cd5bbfcf195dac0c667
fuwasegu <admin@fuwasegu.com>
2024-01-11 19:17:06 +0900

    test: 💍 add test

---
+++ tests/Integration/Database/DatabaseEloquentBuilderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Illuminate\Tests\Integration\Database;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DatabaseEloquentBuilderTest extends DatabaseTestCase
+{
+    public function testCreateAndRefresh()
+    {
+        Schema::create('sample', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+
+        $model = new class extends Model {
+            protected $table = 'sample';
+        };
+
+        $record = $model->newModelQuery()->createAndRefresh([
+            'name' => 'Taylor Otwell',
+        ]);
+
+        $this->assertDatabaseHas('sample', [
+            'name' => 'Taylor Otwell',
+            'status' => '1',
+        ]);
+
+        $this->assertIsInt($record->id);
+        $this->assertSame('Taylor Otwell', $record->name);
+        $this->assertSame(1, $record->status);
+    }
+}
