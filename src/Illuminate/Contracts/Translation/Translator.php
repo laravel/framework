9fec940b5c5382136beb25954ecca060337bb535
Phil Bates <philbates35@gmail.com>
2024-01-15 18:52:32 +0000

    [10.x] Officially support floats in trans_choice (#49693)
    This already works fine with no further changes needed, so I'm not
    sure if there's a reason that floats were never officially supported.
    This is how it is currently, which you can see works perfectly
    fine:
    
    Given:
    
        // lang/en/foo.php
        return [
            'hours' => 'A total of :hours hour|A total of :hours hours',
        ];
    
    Then:
    
        trans_choice('foo.hours', 1, ['hours' => 1]) === 'A total of 1 hour'
        trans_choice('foo.hours', 1.0, ['hours' => 1.0]) === 'A total of 1 hour'
        trans_choice('foo.hours', 1.1, ['hours' => 1.1]) === 'A total of 1.1 hours'
        trans_choice('foo.hours', 0.9, ['hours' => 0.9]) === 'A total of 0.9 hours'
    
    However, when running phpstan & larastan on a Laravel project that
    passes a float to trans_choice when wanting to display text similar
    to those examples ("A total of X hour[s]") it results in an error
    because the only documented allowed types are \Countable|int|array.
    
    Co-authored-by: Phil Bates <phil.bates@theshopworks.com>

--- src/Illuminate/Contracts/Translation/Translator.php
+++ src/Illuminate/Contracts/Translation/Translator.php
@@ -18,7 +18,7 @@ interface Translator
      * Get a translation according to an integer value.
      *
      * @param  string  $key
-     * @param  \Countable|int|array  $number
+     * @param  \Countable|int|float|array  $number
      * @param  array  $replace
      * @param  string|null  $locale
      * @return string
