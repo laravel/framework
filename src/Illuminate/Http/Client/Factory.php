b754ca6c258a9622aa2a9fb92f1daa1dbe22549b
Tim MacDonald <hello@timacdonald.me>
2024-01-22 09:58:11 +1100

    [10.x] Global default options for the http factory (#49767)
    * Global default options for the http factory
    
    * formatting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Http/Client/Factory.php
+++ src/Illuminate/Http/Client/Factory.php
@@ -37,6 +37,13 @@ class Factory
     protected $globalMiddleware = [];
 
     /**
+     * The options to apply to every request.
+     *
+     * @var array
+     */
+    protected $globalOptions = [];
+
+    /**
      * The stub callables that will handle requests.
      *
      * @var \Illuminate\Support\Collection
@@ -119,6 +126,19 @@ class Factory
     public function globalResponseMiddleware($middleware)
     {
         $this->globalMiddleware[] = Middleware::mapResponse($middleware);
+
+        return $this;
+    }
+
+    /**
+     * Set the options to apply to every request.
+     *
+     * @param  array  $options
+     * @return $this
+     */
+    public function globalOptions($options)
+    {
+        $this->globalOptions = $options;
 
         return $this;
     }
@@ -400,7 +420,7 @@ class Factory
      */
     protected function newPendingRequest()
     {
-        return new PendingRequest($this, $this->globalMiddleware);
+        return (new PendingRequest($this, $this->globalMiddleware))->withOptions($this->globalOptions);
     }
 
     /**
