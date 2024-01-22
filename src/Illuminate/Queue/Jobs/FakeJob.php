dfc24d128b45cc4682dac8ad91e8722608b27447
Taylor Otwell <taylor@laravel.com>
2024-01-22 09:04:29 -0600

    [11.x] Fake Queue Interactions (#49766)
    * prototype fake queue interactions
    
    * check delay
    
    * Apply fixes from StyleCI
    
    * fix job id
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ src/Illuminate/Queue/Jobs/FakeJob.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Illuminate\Queue\Jobs;
+
+use Illuminate\Support\Str;
+
+class FakeJob extends Job
+{
+    /**
+     * The number of seconds the released job was delayed.
+     *
+     * @var int
+     */
+    public $releaseDelay;
+
+    /**
+     * The exception the job failed with.
+     *
+     * @var \Throwable
+     */
+    public $failedWith;
+
+    /**
+     * Get the job identifier.
+     *
+     * @return string
+     */
+    public function getJobId()
+    {
+        return once(fn () => (string) Str::uuid());
+    }
+
+    /**
+     * Get the raw body of the job.
+     *
+     * @return string
+     */
+    public function getRawBody()
+    {
+        return '';
+    }
+
+    /**
+     * Release the job back into the queue after (n) seconds.
+     *
+     * @param  \DateTimeInterface|\DateInterval|int  $delay
+     * @return void
+     */
+    public function release($delay = 0)
+    {
+        $this->released = true;
+        $this->releaseDelay = $delay;
+    }
+
+    /**
+     * Delete the job from the queue.
+     *
+     * @return void
+     */
+    public function delete()
+    {
+        $this->deleted = true;
+    }
+
+    /**
+     * Delete the job, call the "failed" method, and raise the failed job event.
+     *
+     * @param  \Throwable|null  $e
+     * @return void
+     */
+    public function fail($exception = null)
+    {
+        $this->failed = true;
+        $this->failedWith = $exception;
+    }
+}
