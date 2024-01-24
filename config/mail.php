43f31a109f904ce06c9643cc6097c9bbafd470fe
Nuno Maduro <enunomaduro@gmail.com>
2024-01-24 15:01:22 +0000

    [11.x] Adjusts default mail environment variables (#49809)
    * Adjust default email values
    
    * Adjusts MAIL_PORT

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'log'),
 
     /*
     |--------------------------------------------------------------------------
@@ -37,8 +37,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', '127.0.0.1'),
+            'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
