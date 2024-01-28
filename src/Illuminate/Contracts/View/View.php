58e82b9bcd1ea89f7b70542c72d3360cd605eba5
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-01-28 00:49:22 -0300

    extended View contract with fragments methods

--- src/Illuminate/Contracts/View/View.php
+++ src/Illuminate/Contracts/View/View.php
@@ -28,4 +28,42 @@ interface View extends Renderable
      * @return array
      */
     public function getData();
+
+
+    /**
+     * Get the evaluated contents of a given fragment.
+     *
+     * @param  string  $fragment
+     * @return string
+     */
+    public function fragment($fragment);
+
+
+    /**
+     * Get the evaluated contents for a given array of fragments or return all fragments.
+     *
+     * @param  array|null  $fragments
+     * @return string
+     */
+    public function fragments(?array $fragments = null);
+
+
+    /**
+     * Get the evaluated contents of a given fragment if the given condition is true.
+     *
+     * @param  bool  $boolean
+     * @param  string  $fragment
+     * @return string
+     */
+    public function fragmentIf($boolean, $fragment);
+
+
+    /**
+     * Get the evaluated contents for a given array of fragments if the given condition is true.
+     *
+     * @param  bool  $boolean
+     * @param  array|null  $fragments
+     * @return string
+     */
+    public function fragmentsIf($boolean, ?array $fragments = null);
 }
