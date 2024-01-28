69ee952de89105e96fd7d337351d54b33e5dea5c
Mateus Junges <mateus@junges.dev>
2024-01-28 03:04:06 -0300

    Use correct validation message

--- src/Illuminate/Validation/Concerns/FormatsMessages.php
+++ src/Illuminate/Validation/Concerns/FormatsMessages.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Validation\Concerns;
 
 use Closure;
+use Illuminate\Http\File;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -218,15 +219,13 @@ trait FormatsMessages
         // We assume that the attributes present in the file array are files so that
         // means that if the attribute does not have a numeric rule and the files
         // list doesn't have it we'll just consider it a string by elimination.
-        if ($this->hasRule($attribute, $this->numericRules)) {
-            return 'numeric';
-        } elseif ($this->hasRule($attribute, ['Array'])) {
-            return 'array';
-        } elseif ($this->getValue($attribute) instanceof UploadedFile) {
-            return 'file';
-        }
-
-        return 'string';
+        return match(true) {
+            $this->hasRule($attribute, $this->numericRules) => 'numeric',
+            $this->hasRule($attribute, ['Array']) => 'array',
+            $this->getValue($attribute) instanceof UploadedFile => 'file',
+            $this->getValue($attribute) instanceof File => 'file',
+            default => 'string',
+        };
     }
 
     /**
