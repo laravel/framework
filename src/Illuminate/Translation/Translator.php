7bb9885fe5bda3f3103ce78bf0819c0f3843bef8
Vic GUTT <guttvictor@yahoo.fr>
2024-01-29 19:36:08 +0100

    fix: "Translator" returns correct locale for missing keys

--- src/Illuminate/Translation/Translator.php
+++ src/Illuminate/Translation/Translator.php
@@ -160,9 +160,9 @@ class Translator extends NamespacedItemResolver implements TranslatorContract
             // the translator was instantiated. Then, we can load the lines and return.
             $locales = $fallback ? $this->localeArray($locale) : [$locale];
 
-            foreach ($locales as $locale) {
+            foreach ($locales as $_locale) {
                 if (! is_null($line = $this->getLine(
-                    $namespace, $group, $locale, $item, $replace
+                    $namespace, $group, $_locale, $item, $replace
                 ))) {
                     return $line;
                 }
