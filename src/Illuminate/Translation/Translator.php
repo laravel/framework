549de01ea9f7fa17b272d9baa04be0fe186b9c26
Vic GUTT <guttvictor@yahoo.fr>
2024-01-30 09:19:03 +0100

    chore: variable rename

--- src/Illuminate/Translation/Translator.php
+++ src/Illuminate/Translation/Translator.php
@@ -160,9 +160,9 @@ class Translator extends NamespacedItemResolver implements TranslatorContract
             // the translator was instantiated. Then, we can load the lines and return.
             $locales = $fallback ? $this->localeArray($locale) : [$locale];
 
-            foreach ($locales as $_locale) {
+            foreach ($locales as $languageLineLocale) {
                 if (! is_null($line = $this->getLine(
-                    $namespace, $group, $_locale, $item, $replace
+                    $namespace, $group, $languageLineLocale, $item, $replace
                 ))) {
                     return $line;
                 }
