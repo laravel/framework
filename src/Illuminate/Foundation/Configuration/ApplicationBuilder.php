1e47bb92d0b539711cf5d84083c94789ae07677b
Taylor Otwell <taylor@laravel.com>
2024-02-02 12:31:10 -0600

    fix method

--- src/Illuminate/Foundation/Configuration/ApplicationBuilder.php
+++ src/Illuminate/Foundation/Configuration/ApplicationBuilder.php
@@ -205,7 +205,7 @@ class ApplicationBuilder
     {
         $this->app->afterResolving(HttpKernel::class, function ($kernel) use ($callback) {
             $middleware = (new Middleware)
-                ->auth(redirectTo: fn () => route('login'));
+                ->redirectUsersUsing(redirectTo: fn () => route('login'));
 
             $callback($middleware);
 
