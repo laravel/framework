d8f1d30874e15e87215e03f265df58cbf04093dd
Mior Muhammad Zaki <crynobone@gmail.com>
2024-02-07 22:05:15 +0800

    [11.x] Check `0001_01_01_000002_create_jobs_table.php` before publishing `job_batches` migration
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Queue/Console/BatchesTableCommand.php
+++ src/Illuminate/Queue/Console/BatchesTableCommand.php
@@ -48,4 +48,23 @@ class BatchesTableCommand extends MigrationGeneratorCommand
     {
         return __DIR__.'/stubs/batches.stub';
     }
+
+    /**
+     * Determine whether a migration for the table already exists.
+     *
+     * @param  string  $table
+     * @return bool
+     */
+    protected function migrationExists($table)
+    {
+        if ($table !== 'job_batches') {
+            return parent::migrationExists($table);
+        }
+
+        return count($this->files->glob(sprintf(
+            '{%s,%s}',
+            join_paths($this->laravel->databasePath('migrations'), '*_*_*_*_create_'.$table.'_table.php'),
+            join_paths($this->laravel->databasePath('migrations'), '0001_01_01_000002_create_jobs_table.php'),
+        ))) !== 0;
+    }
 }
