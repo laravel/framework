68e5323800fce1f55d92fedcd37946891641a74c
Saeed Hosseini <39903221+saeedhosseiinii@users.noreply.github.com>
2024-02-09 20:29:57 +0330

    [11.x] Remove unused property

--- src/Illuminate/Foundation/Configuration/Middleware.php
+++ src/Illuminate/Foundation/Configuration/Middleware.php
@@ -126,26 +126,6 @@ class Middleware
     protected $authenticatedSessions = false;
 
     /**
-     * The default middleware aliases.
-     *
-     * @var array
-     */
-    protected $aliases = [
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
-        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \Illuminate\Auth\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
-        'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
-        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'subscribed' => \Spark\Http\Middleware\VerifyBillableIsSubscribed::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
-
-    /**
      * The custom middleware aliases.
      *
      * @var array
