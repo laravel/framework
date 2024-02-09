91943748f0523754a7d75a41fd314f0998a8caf9
Hafez Divandari <hafezdivandari@gmail.com>
2024-02-09 22:52:37 +0330

    use blueprint prefix on sqlite

--- src/Illuminate/Database/Schema/Grammars/SQLiteGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/SQLiteGrammar.php
@@ -295,8 +295,8 @@ class SQLiteGrammar extends Grammar
             fn ($index) => $this->{'compile'.ucfirst($index->name)}($blueprint, $index)
         )->all();
 
-        $tempTable = $this->wrap('__temp__'.$this->getTablePrefix().$table);
-        $table = $this->wrap($this->getTablePrefix().$table);
+        $tempTable = $this->wrap('__temp__'.$blueprint->getPrefix().$table);
+        $table = $this->wrapTable($blueprint);
         $columnNames = implode(', ', $columnNames);
 
         $foreignKeyConstraintsEnabled = $connection->scalar('pragma foreign_keys');
