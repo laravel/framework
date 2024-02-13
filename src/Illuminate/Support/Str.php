6f8323a4e2fbccc5aae973aafd8ecbbfad174e10
MaximAL <almaximal@ya.ru>
2024-02-13 16:01:44 +0300

    Remove regex case insensitivity modifier in UUID detection to speed it up

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -542,7 +542,7 @@ class Str
             return false;
         }
 
-        return preg_match('/^[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}$/iD', $value) > 0;
+        return preg_match('/^[\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12}$/D', $value) > 0;
     }
 
     /**
