7e7e035daa528fbd3ea16befe1b4eab8838ed93f
Nuno Maduro <enunomaduro@gmail.com>
2024-02-15 15:20:01 +0000

    Move types file

--- types/Foundation/Configuration/Middleware.php
+++ types/Foundation/Configuration/Middleware.php
@@ -4,6 +4,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 $middleware = new Middleware();
 
+$middleware->convertEmptyStringsToNull(except: [
+    fn ($request) => $request->has('skip-all-1'),
+    fn ($request) => $request->has('skip-all-2'),
+]);
+
 $middleware->trimStrings(except: [
     'aaa',
     fn ($request) => $request->has('skip-all'),
