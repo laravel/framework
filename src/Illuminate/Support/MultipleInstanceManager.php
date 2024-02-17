5b0581eed0fcdf07b9ba032e9e05fe3a03fd4eef
Mahmoudzadeh <s.a.mahmoudzadeh@gmail.com>
2024-02-18 01:31:12 +0330

    feat(MultipleInstanceManager): improvement code style using early return

--- src/Illuminate/Support/MultipleInstanceManager.php
+++ src/Illuminate/Support/MultipleInstanceManager.php
@@ -109,15 +109,15 @@ abstract class MultipleInstanceManager
 
         if (isset($this->customCreators[$config['driver']])) {
             return $this->callCustomCreator($config);
-        } else {
-            $driverMethod = 'create'.ucfirst($config['driver']).'Driver';
-
-            if (method_exists($this, $driverMethod)) {
-                return $this->{$driverMethod}($config);
-            } else {
-                throw new InvalidArgumentException("Instance driver [{$config['driver']}] is not supported.");
-            }
         }
+
+        $driverMethod = 'create'.ucfirst($config['driver']).'Driver';
+
+        if (method_exists($this, $driverMethod)) {
+            return $this->{$driverMethod}($config);
+        }
+
+        throw new InvalidArgumentException("Instance driver [{$config['driver']}] is not supported.");
     }
 
     /**
