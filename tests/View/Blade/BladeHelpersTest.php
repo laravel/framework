4f1b34e1c3aa74eec4d59a873d0c42c1a51307ad
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2024-02-17 22:51:46 +0330

    fix styleci

--- tests/View/Blade/BladeHelpersTest.php
+++ tests/View/Blade/BladeHelpersTest.php
@@ -8,7 +8,7 @@ class BladeHelpersTest extends AbstractBladeTestCase
     {
         $this->assertSame('<?php echo csrf_field(); ?>', $this->compiler->compileString('@csrf'));
         $this->assertSame('<?php echo method_field(\'patch\'); ?>', $this->compiler->compileString("@method('patch')"));
-        $this->assertSame('<?php echo method_field(\'delete\'); ?>', $this->compiler->compileString("@delete"));
+        $this->assertSame('<?php echo method_field(\'delete\'); ?>', $this->compiler->compileString('@delete'));
         $this->assertSame('<?php dd($var1); ?>', $this->compiler->compileString('@dd($var1)'));
         $this->assertSame('<?php dd($var1, $var2); ?>', $this->compiler->compileString('@dd($var1, $var2)'));
         $this->assertSame('<?php dump($var1, $var2); ?>', $this->compiler->compileString('@dump($var1, $var2)'));
