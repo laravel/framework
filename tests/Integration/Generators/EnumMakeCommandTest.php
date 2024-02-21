083471667c57721d9fa1d4e2015bc2ad805883cf
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2024-02-21 15:29:15 +0330

    add testItCanGenerateEnumFile

--- tests/Integration/Generators/EnumMakeCommandTest.php
+++ tests/Integration/Generators/EnumMakeCommandTest.php
@@ -6,5 +6,14 @@ use Illuminate\Tests\Integration\Generators\TestCase;
 
 class EnumMakeCommandTest extends TestCase
 {
-    
+    public function testItCanGenerateEnumFile()
+    {
+        $this->artisan('make:enum', ['name' => 'StatusEnum'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App;',
+            'enum StatusEnum',
+        ], 'app/StatusEnum.php');
+    }
 }
