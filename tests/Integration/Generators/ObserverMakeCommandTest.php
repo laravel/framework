075e1a4e3f71425bd770e1acf33a8a0badcb6e6b
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2024-02-22 12:02:56 +0330

    add tests

--- tests/Integration/Generators/ObserverMakeCommandTest.php
+++ tests/Integration/Generators/ObserverMakeCommandTest.php
@@ -33,4 +33,22 @@ class ObserverMakeCommandTest extends TestCase
             'public function deleted(Foo $foo)',
         ], 'app/Observers/FooObserver.php');
     }
+
+    public function testItCanGenerateObserverFileWithModelWithSoftDelete()
+    {
+        $this->artisan('make:observer', ['name' => 'FooObserver', '--model' => 'Foo', '--softDelete' => true])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Observers;',
+            'use App\Models\Foo;',
+            'class FooObserver',
+            'public function created(Foo $foo)',
+            'public function updated(Foo $foo)',
+            'public function deleted(Foo $foo)',
+            'public function softDeleted(Foo $foo)',
+            'public function restored(Foo $foo)',
+            'public function forceDeleted(Foo $foo)',
+        ], 'app/Observers/FooObserver.php');
+    }
 }
