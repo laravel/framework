e0be50a7b770c46b522377cb46318e06e312014e
taylorotwell <taylorotwell@users.noreply.github.com>
2024-02-22 14:52:40 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/Process.php
+++ src/Illuminate/Support/Facades/Process.php
@@ -17,7 +17,7 @@ use Illuminate\Process\Factory;
  * @method static \Illuminate\Process\PendingProcess tty(bool $tty = true)
  * @method static \Illuminate\Process\PendingProcess options(array $options)
  * @method static \Illuminate\Contracts\Process\ProcessResult run(array|string|null $command = null, callable|null $output = null)
- * @method static \Illuminate\Process\InvokedProcess start(array|string|null $command = null, callable $output = null)
+ * @method static \Illuminate\Process\InvokedProcess start(array|string|null $command = null, callable|null $output = null)
  * @method static \Illuminate\Process\PendingProcess withFakeHandlers(array $fakeHandlers)
  * @method static \Illuminate\Process\PendingProcess|mixed when(\Closure|mixed|null $value = null, callable|null $callback = null, callable|null $default = null)
  * @method static \Illuminate\Process\PendingProcess|mixed unless(\Closure|mixed|null $value = null, callable|null $callback = null, callable|null $default = null)
