4eeb3f32c48a43203d8a9d6a1689ace58aa488aa
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2024-02-26 16:07:15 +0200

    Fix the issue of `$wrap` collection resource attribute 💼

--- src/Illuminate/Http/Resources/Json/JsonResource.php
+++ src/Illuminate/Http/Resources/Json/JsonResource.php
@@ -82,6 +82,10 @@ class JsonResource implements ArrayAccess, JsonSerializable, Responsable, UrlRou
             if (property_exists(static::class, 'preserveKeys')) {
                 $collection->preserveKeys = (new static([]))->preserveKeys === true;
             }
+
+            if (property_exists(static::class, 'wrap')) {
+                $collection::$wrap = static::$wrap;
+            }
         });
     }
 
