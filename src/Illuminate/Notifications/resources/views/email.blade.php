fef094a388ed60a8215d8c2dcd94811e937d80d4
Punyapal Shah <mrpunyapal@gmail.com>
2024-03-09 18:19:07 +0530

    Fix action button color in email template

--- src/Illuminate/Notifications/resources/views/email.blade.php
+++ src/Illuminate/Notifications/resources/views/email.blade.php
@@ -19,7 +19,7 @@
 {{-- Action Button --}}
 @isset($actionText)
 <?php
-    $color = match ($level) {
+    $color = $actionColor ?? match ($level) {
         'success', 'error' => $level,
         default => 'primary',
     };
