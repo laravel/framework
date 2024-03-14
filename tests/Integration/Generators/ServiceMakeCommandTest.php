4489f1fa2663f60de7d0de3e944de65ad179a731
S.a Mahmoudzadeh <s.a.mahmoudzadeh@gmail.com>
2024-03-15 02:39:49 +0330

    test(test\integration): create ServiceMakeCommandTest

---
+++ tests/Integration/Generators/ServiceMakeCommandTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Generators;
+
+class ServiceMakeCommandTest  extends TestCase
+{
+    protected $files = [
+        'app/FooService.php',
+    ];
+
+    public function testItCanGenerateServiceFile()
+    {
+        $this->artisan('make:service', ['name' => 'FooService'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App;',
+            'class FooService',
+        ], 'app/FooService.php');
+    }
+}
