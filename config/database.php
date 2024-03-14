bd4aeef5d69bb688957cf833504009b57661260b
Thijs van den Anker <thijs@bitsnbolts.nl>
2024-03-14 12:42:36 +0100

    Use SQL Server for the test

--- config/database.php
+++ config/database.php
@@ -106,7 +106,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
