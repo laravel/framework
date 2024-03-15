398f49485e305756409b52af64837c784fd30de9
Taylor Otwell <taylor@laravel.com>
2024-03-15 16:42:51 -0500

    accept attributes for channels

--- src/Illuminate/Foundation/Configuration/ApplicationBuilder.php
+++ src/Illuminate/Foundation/Configuration/ApplicationBuilder.php
@@ -105,12 +105,13 @@ class ApplicationBuilder
      * Register the braodcasting services for the application.
      *
      * @param  string  $channels
+     * @param  array  $attributes
      * @return $this
      */
-    public function withBroadcasting(string $channels)
+    public function withBroadcasting(string $channels, array $attributes = [])
     {
-        $this->app->booted(function () use ($channels) {
-            Broadcast::routes();
+        $this->app->booted(function () use ($channels, $attributes) {
+            Broadcast::routes(! empty($attributes) ? $attributes : null);
 
             if (file_exists($channels)) {
                 require $channels;
