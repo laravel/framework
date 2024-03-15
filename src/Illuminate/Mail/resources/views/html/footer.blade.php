b2a8d03f3dd87415b10a832c0d0b7806c79a2c9f
Kani Robinson <me@kanirobinson.co.uk>
2024-03-15 16:15:32 +0000

    Update footer.blade.php to use the markdown helper

--- src/Illuminate/Mail/resources/views/html/footer.blade.php
+++ src/Illuminate/Mail/resources/views/html/footer.blade.php
@@ -3,7 +3,7 @@
 <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
 <td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
+{{ markdown()->parse($slot) }}
 </td>
 </tr>
 </table>
