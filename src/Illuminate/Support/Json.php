80caa57072b155d98ab19a0a237f312033c9c4a3
Italo <DarkGhostHunter@Gmail.com>
2024-03-19 16:01:59 -0300

    [11.x] Adds JSON (transport) helper class.

---
+++ src/Illuminate/Support/Json.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace Illuminate\Support;
+
+use ArrayAccess;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Dumpable;
+use Illuminate\Support\Traits\Tappable;
+use JsonSerializable;
+use stdClass;
+use Stringable;
+
+class Json implements Arrayable, Jsonable, JsonSerializable, Stringable, Responsable, ArrayAccess
+{
+    use Conditionable, Dumpable, Tappable;
+
+    /**
+     * The JSON data array.
+     *
+     * @var array
+     */
+    protected $items;
+
+    /**
+     * Create a new JSON transport.
+     *
+     * @param  \Illuminate\Contracts\Support\Arrayable|array  $items
+     */
+    public function __construct($items = [])
+    {
+        $this->items = $items instanceof Arrayable ? $items->toArray() : $items;
+    }
+
+    /**
+     * Adds a value to the underlying JSON data.
+     *
+     * @param  array-key  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function set($key, $value): void
+    {
+        Arr::set($this->items, $key, $value);
+    }
+
+    /**
+     * Fill in data where it's missing in the JSON data.
+     *
+     * @param  array-key  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function fill($key, $value)
+    {
+        if ($this->missing($key)) {
+            $this->set($key, $value);
+        }
+    }
+
+    /**
+     * Retrieves a value from the underlying JSON data.
+     *
+     * @template TDefault
+     * @param  array-key  $key
+     * @param  TDefault  $default
+     * @return TDefault|mixed|null
+     */
+    public function get($key, $default = null)
+    {
+        return Arr::get($this->items, $key, $default);
+    }
+
+    /**
+     * Determine if the given key in the JSON data exists and is not "null".
+     *
+     * @param  array-key  $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        return $this->get($key) !== null;
+    }
+
+    /**
+     * Determine if the given key in the JSON data is missing or is "null".
+     *
+     * @param  array-key  $key
+     * @return bool
+     */
+    public function missing($key)
+    {
+        return !$this->has($key);
+    }
+
+    /**
+     * Determine if the given key in the JSON data is set.
+     *
+     * @param  array-key  $key
+     * @return bool
+     */
+    public function hasKey($key)
+    {
+        return $this->get($key, $placeholder = new stdClass) !== $placeholder;
+    }
+
+    /**
+     * Determine if the given key in the JSON data is not set.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function missingKey($key)
+    {
+        return !$this->hasKey($key);
+    }
+
+    /**
+     * Forgets a given key from the JSON data.
+     *
+     * @param  array-key  $key
+     * @return void
+     */
+    public function forget($key)
+    {
+        Arr::forget($this->items, $key);
+    }
+
+    /**
+     * Returns the underlying JSON items.
+     *
+     * @return array
+     */
+    public function items()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param  int  $options
+     * @return string
+     */
+    public function toJson($options = 0): string
+    {
+        return json_encode($this->jsonSerialize(), $options);
+    }
+
+    /**
+     * Convert the object to its JSON representation, or throw an exception.
+     *
+     * @param  int  $options
+     * @return string
+     */
+    public function toJsonOrFail($options = 0): string
+    {
+        return $this->toJson($options | JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return array_map(fn($value) => $value instanceof Arrayable ? $value->toArray() : $value, $this->items);
+    }
+
+    /**
+     * Specify data which should be serialized to JSON.
+     *
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @throws \JsonException
+     */
+    public function __toString(): string
+    {
+        return $this->toJson();
+    }
+
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function toResponse($request)
+    {
+        return new \Symfony\Component\HttpFoundation\JsonResponse($this);
+    }
+
+    /**
+     * Dynamically retrieve a value from the JSON items.
+     *
+     * @param  string  $key
+     * @return mixed|null
+     */
+    public function __get(string $key)
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * Dynamically set a value in the JSON items.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function __set(string $key, $value): void
+    {
+        $this->set($key, $value);
+    }
+
+    /**
+     * Dynamically check if a key in the JSON items exists.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function __isset(string $key): bool
+    {
+        return $this->has($key);
+    }
+
+    /**
+     * Dynamically forget a key from the JSON items.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function __unset(string $key): void
+    {
+        $this->forget($key);
+    }
+
+    /**
+     * Whether an offset exists.
+     *
+     * @param  mixed  $offset
+     * @return bool
+     */
+    public function offsetExists(mixed $offset): bool
+    {
+        return $this->has($offset);
+    }
+
+    /**
+     * Offset to retrieve.
+     *
+     * @param  mixed  $offset
+     * @return mixed|null
+     */
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * Offset to set.
+     *
+     * @param  array-key  $offset
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->set($offset, $value);
+    }
+
+    /**
+     * Offset to unset.
+     *
+     * @param  array-key  $offset
+     * @return void
+     */
+    public function offsetUnset(mixed $offset): void
+    {
+        $this->forget($offset);
+    }
+
+    /**
+     * Create a new instance from a JSON string.
+     *
+     * @param  string  $json
+     * @return static
+     */
+    public static function fromString(string $json): static
+    {
+        return new static(json_decode($json, true));
+    }
+
+    /**
+     * Create a new instance.
+     *
+     * @param  array  $json
+     * @return $this
+     */
+    public static function make($json = []): static
+    {
+        return new static($json);
+    }
+}
---
+++ tests/Support/JsonTest.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Illuminate\Tests\Support;
+
+use Illuminate\Support\Collection;
+use JsonException;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Json;
+use function json_encode;
+
+class JsonTest extends TestCase
+{
+    public function testInstanceFromArrayable()
+    {
+        $json = new Json(new Json(['foo' => ['bar' => 'baz']]));
+
+        $this->assertSame(['foo' => ['bar' => 'baz']], $json->items());
+    }
+
+    public function testGetsAndSetsValues()
+    {
+        $json = new Json(['foo' => ['bar' => 'baz']]);
+
+        $this->assertSame('baz', $json->get('foo.bar'));
+
+        $json->set('foo.bar', 'quz');
+
+        $this->assertSame('quz', $json->get('foo.bar'));
+    }
+
+    public function testGetsDefaultValue()
+    {
+        $json = new Json(['foo' => ['bar' => 'baz']]);
+
+        $this->assertNull($json->get('foo.invalid'));
+        $this->assertSame('default', $json->get('foo.invalid', 'default'));
+        $this->assertSame('default', $json->get('foo.invalid', fn () => 'default'));
+    }
+
+    public function testHasValue()
+    {
+        $json = new Json(['foo' => ['bar' => 'baz']]);
+
+        $this->assertTrue($json->has('foo'));
+        $this->assertFalse($json->missing('foo'));
+
+        $this->assertTrue($json->has('foo.bar'));
+        $this->assertFalse($json->missing('foo.bar'));
+
+        $this->assertFalse($json->has('invalid'));
+        $this->assertTrue($json->missing('invalid'));
+    }
+
+    public function testHasKey()
+    {
+        $json = new Json(['foo' => ['bar' => 'baz', 'null' => null]]);
+
+        $this->assertTrue($json->hasKey('foo'));
+        $this->assertFalse($json->missingKey('foo'));
+
+        $this->assertTrue($json->hasKey('foo.null'));
+        $this->assertFalse($json->missingKey('foo.null'));
+
+        $this->assertFalse($json->hasKey('invalid'));
+        $this->assertTrue($json->missingKey('invalid'));
+    }
+
+    public function testForgetsValue()
+    {
+        $json = new Json(['foo' => ['bar' => 'baz']]);
+
+        $json->forget('foo.bar');
+
+        $this->assertNull($json->get('foo.bar'));
+    }
+
+    public function testToJson()
+    {
+        $json = new Json(['foo' => ['bar' => 'baz']]);
+
+        $this->assertSame('{"foo":{"bar":"baz"}}', $json->toJson());
+        $this->assertSame(<<<'JSON'
+{
+    "foo": {
+        "bar": "baz"
+    }
+}
+JSON, $json->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testToJsonOrFail()
+    {
+        $a = new class {
+            public $otherObject;
+        };
+
+        $b = new class {
+            public $otherObject;
+        };
+
+        $a->otherObject = $b;
+        $b->otherObject = $a;
+
+        $json = new Json(["😓" => $a]);
+
+        $this->expectException(JsonException::class);
+
+        $json->toJsonOrFail();
+    }
+
+    public function testToArray()
+    {
+        $this->assertSame(['foo' => ['bar' => 'baz']], (new Json(['foo' => ['bar' => 'baz']]))->toArray());
+
+        $arrayable = new Json(['foo' => new Collection(['bar' => 'baz'])]);
+
+        $this->assertSame(['foo' => ['bar' => 'baz']], $arrayable->toArray());
+    }
+
+    public function testJsonSerialization()
+    {
+        $json = new Json(['foo' => ['bar' => new Collection('baz')]]);
+
+        $this->assertSame('{"foo":{"bar":["baz"]}}', json_encode($json));
+    }
+
+    public function testToString()
+    {
+        $json = new Json(['foo' => ['bar' => new Collection('baz')]]);
+
+        $this->assertSame('{"foo":{"bar":["baz"]}}', (string) $json);
+    }
+
+    public function testObjectAccess()
+    {
+        $json = new Json(['foo' => ['bar' => 'baz']]);
+
+        $this->assertSame('baz', $json->{'foo.bar'});
+        $this->assertTrue(isset($json->{'foo.bar'}));
+
+        $json->{'foo.bar'} = 'quz';
+
+        $this->assertSame('quz', $json->{'foo.bar'});
+
+        unset($json->{'foo.bar'});
+
+        $this->assertNull($json->{'foo.bar'});
+    }
+
+    public function testArrayAccess()
+    {
+        $json = new Json(['foo' => ['bar' => 'baz']]);
+
+        $this->assertSame('baz', $json['foo.bar']);
+        $this->assertTrue(isset($json['foo.bar']));
+
+        $json['foo.bar'] = 'quz';
+
+        $this->assertSame('quz', $json['foo.bar']);
+
+        unset($json['foo.bar']);
+
+        $this->assertNull($json['foo.bar']);
+    }
+
+    public function testFromString()
+    {
+        $this->assertSame(['foo' => ['bar' => 'baz']], Json::fromString('{"foo":{"bar":"baz"}}')->items());
+    }
+
+    public function testMake()
+    {
+        $this->assertSame([], Json::make()->items());
+        $this->assertSame(['foo' => ['bar' => 'baz']], Json::make(['foo' => ['bar' => 'baz']])->items());
+    }
+}
