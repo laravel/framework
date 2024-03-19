0fb25edda0705307b83a4a1a7a63ef1f9c00d817
Jan Nedbal <janedbal@gmail.com>
2024-03-19 17:26:47 +0100

    Fix code style

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
