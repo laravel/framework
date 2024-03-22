612755a4d99dd575033b57e0539e4dcdb137334b
= <inman.turbo@gmail.com>
2024-03-21 21:35:24 -0400

    add trait to support stub option

---
+++ src/Illuminate/Console/Concerns/HasStubOption.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Illuminate\Console\Concerns;
+
+trait HasStubOption
+{
+    /**
+     * Get the console command arguments.
+     */
+    abstract protected function getOptions(): array;
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    #[\Override]
+    protected function buildClass($name)
+    {
+        $stub = $this->files->get($this->getStubOption() ?? $this->getStub());
+
+        return $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
+    }
+
+    /**
+     * Get a stub file for the generator from a stub option.
+     *
+     * @return string|null
+     */
+    protected function getStubOption()
+    {
+        if (! $this->hasOption('stub') || ! $this->option('stub')) {
+            return null;
+        }
+
+        $stub = str_replace(['\\', '/'], DIRECTORY_SEPARATOR, trim($this->option('stub')));
+
+        return match (true) {
+            file_exists($namedStub = $this->laravel->basePath('stubs'.DIRECTORY_SEPARATOR.$stub.'.stub')) => $namedStub,
+            file_exists($stubPath = $stub) => $stubPath,
+            default => null,
+        };
+    }
+}
