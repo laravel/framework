446a919e81916522ab08d479e6dc00700b7aa37d
Vincenzo Raco <vincenzoraco@users.noreply.github.com>
2024-03-29 14:20:41 +0800

    Add withoutEventDiscovery test

---
+++ tests/Integration/Foundation/Configuration/WithoutEventDiscoveryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Foundation\Configuration;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class WithoutEventDiscoveryTest extends TestCase
+{
+    protected function resolveApplication()
+    {
+        return Application::configure(static::applicationBasePath())
+            ->withoutEventDiscovery()
+            ->create();
+    }
+
+
+    public function testDisablesEventDiscovery()
+    {
+        $this->assertFalse(
+            collect($this->app->getProviders(EventServiceProvider::class))
+                ->firstOrFail()
+                ->shouldDiscoverEvents()
+        );
+    }
+}
