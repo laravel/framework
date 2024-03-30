98f86ad4e18f00e7303670f97d7499c1f7698128
Hafez Divandari <hafezdivandari@gmail.com>
2024-03-30 19:35:47 +0330

    wip

--- tests/Integration/Database/SchemaBuilderSchemaNameTest.php
+++ tests/Integration/Database/SchemaBuilderSchemaNameTest.php
@@ -433,27 +433,8 @@ class SchemaBuilderSchemaNameTest extends DatabaseTestCase
         }
 
         $connection = DB::connection($connection);
-        $schema = $connection->getSchemaBuilder();
-
-        $this->assertEquals('dbo', $connection->scalar('select schema_name()'));
 
         var_dump($connection->scalar('select current_user'));
-
-        $connection->statement('alter user SA with default_schema=my_schema');
-
-        $this->assertEquals('my_schema', $connection->scalar('select schema_name()'));
-
-        $schema->create('table', function (Blueprint $table) {
-            $table->id();
-        });
-
-        $this->assertTrue($schema->hasTable('table'));
-        $this->assertTrue($schema->hasTable('my_schema.table'));
-        $this->assertFalse($schema->hasTable('dbo.table'));
-
-        $connection->statement('alter user SA with default_schema=dbo');
-
-        $this->assertEquals('dbo', $connection->scalar('select schema_name()'));
     }
 
     public static function connectionProvider(): array
