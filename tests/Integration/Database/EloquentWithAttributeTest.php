d202ae01ea60e4e9972eb111f22040abbb659ac4
Punyapal Shah <mrpunyapal@gmail.com>
2024-03-31 17:01:32 +0530

    Add EloquentWithAttributeTest for testing Eloquent model relationships

---
+++ tests/Integration/Database/EloquentWithAttributeTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Database\EloquentwithAttributeTest;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Tests\Integration\Database\DatabaseTestCase;
+
+class EloquentWithAttributeTest extends DatabaseTestCase
+{
+    protected function afterRefreshingDatabase()
+    {
+        Schema::create('one', function (Blueprint $table) {
+            $table->increments('id');
+        });
+
+        Schema::create('two', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('one_id');
+            $table->string('attribute');
+        });
+    }
+
+    public function testItBasic()
+    {
+        $one = Model1::create();
+        $one->twos()->Create(['attribute' => 'value']);
+
+        $results = Model1::withAttribute('twos', 'attribute');
+
+        $this->assertEquals([
+            ['id' => 1, 'twos_attribute' => 'value'],
+        ], $results->get()->toArray());
+    }
+}
+
+class Model1 extends Model
+{
+    public $table = 'one';
+    public $timestamps = false;
+    protected $guarded = [];
+
+    public function twos()
+    {
+        return $this->hasMany(Model2::class, 'one_id');
+    }
+}
+
+class Model2 extends Model
+{
+    public $table = 'two';
+    public $timestamps = false;
+    protected $guarded = [];
+}
