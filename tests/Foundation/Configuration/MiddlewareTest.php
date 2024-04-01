9484308dd1e5c7dc4433a19ee466da52fbcfed80
Choraimy Kroonstuiver <3661474+axlon@users.noreply.github.com>
2024-04-01 14:57:06 +0200

    Remove unused import

--- tests/Foundation/Configuration/MiddlewareTest.php
+++ tests/Foundation/Configuration/MiddlewareTest.php
@@ -2,7 +2,6 @@
 
 namespace Illuminate\Tests\Foundation\Configuration;
 
-use Illuminate\Config\Repository;
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Encryption\Encrypter;
 use Illuminate\Contracts\Foundation\Application;
