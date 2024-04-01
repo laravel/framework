b0d60523c4f735c7a269f5c4363d021f3e0a26f0
Punyapal Shah <53343069+MrPunyapal@users.noreply.github.com>
2024-04-01 08:18:30 +0530

    formatting

--- src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
+++ src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
@@ -659,7 +659,7 @@ trait QueriesRelationships
             // If the query contains certain elements like orderings / more than one column selected
             // then we will remove those elements from the query so that it will execute properly
             // when given to the database. Otherwise, we may receive SQL errors or poor syntax.
-            if ($function){
+            if ($function) {
                 $query->orders = null;
                 $query->setBindings([], 'order');
             }
