8d4da418fe97b116213f1d267d1dd005a7093cdb
Mior Muhammad Zaki <crynobone@gmail.com>
2024-04-04 06:10:52 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Generators/EnumMakeCommandTest.php
+++ tests/Integration/Generators/EnumMakeCommandTest.php
@@ -6,6 +6,13 @@ use Illuminate\Tests\Integration\Generators\TestCase;
 
 class EnumMakeCommandTest extends TestCase
 {
+    protected $files = [
+        'app/IntEnum.php',
+        'app/StatusEnum.php',
+        'app/StringEnum.php',
+        'app/*/OrderStatusEnum.php',
+    ];
+
     public function testItCanGenerateEnumFile()
     {
         $this->artisan('make:enum', ['name' => 'StatusEnum'])
@@ -43,11 +50,10 @@ class EnumMakeCommandTest extends TestCase
     {
         $enumsFolderPath = app_path('Enums');
 
-        $isFolderCreated = false;
-        if (! is_dir($enumsFolderPath)) {
-            mkdir($enumsFolderPath, 0777, true);
-            $isFolderCreated = true;
-        }
+        /** @var \Illuminate\Filesystem\Filesystem $files */
+        $files = $this->app['files'];
+
+        $files->ensureDirectoryExists($enumsFolderPath);
 
         $this->artisan('make:enum', ['name' => 'OrderStatusEnum'])
             ->assertExitCode(0);
@@ -57,22 +63,17 @@ class EnumMakeCommandTest extends TestCase
             'enum OrderStatusEnum',
         ], 'app/Enums/OrderStatusEnum.php');
 
-        unlink($enumsFolderPath.'/OrderStatusEnum.php');
-
-        if ($isFolderCreated) {
-            rmdir($enumsFolderPath);
-        }
+        $files->deleteDirectory($enumsFolderPath);
     }
 
     public function testItCanGenerateEnumFileInEnumerationsFolder()
     {
         $enumerationsFolderPath = app_path('Enumerations');
 
-        $isFolderCreated = false;
-        if (! is_dir($enumerationsFolderPath)) {
-            mkdir($enumerationsFolderPath, 0777, true);
-            $isFolderCreated = true;
-        }
+        /** @var \Illuminate\Filesystem\Filesystem $files */
+        $files = $this->app['files'];
+
+        $files->ensureDirectoryExists($enumerationsFolderPath);
 
         $this->artisan('make:enum', ['name' => 'OrderStatusEnum'])
             ->assertExitCode(0);
@@ -82,10 +83,6 @@ class EnumMakeCommandTest extends TestCase
             'enum OrderStatusEnum',
         ], 'app/Enumerations/OrderStatusEnum.php');
 
-        unlink($enumerationsFolderPath.'/OrderStatusEnum.php');
-
-        if ($isFolderCreated) {
-            rmdir($enumerationsFolderPath);
-        }
+        $files->deleteDirectory($enumerationsFolderPath);
     }
 }
