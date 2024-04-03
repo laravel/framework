1f57e491814525a7eeba2d7626fec11a547bff12
Md. Mottasin Lemon <68915904+lmottasin@users.noreply.github.com>
2024-04-03 18:34:17 +0000

    feat: add test for enum make when Enums or Enumerations folder exists

--- tests/Integration/Generators/EnumMakeCommandTest.php
+++ tests/Integration/Generators/EnumMakeCommandTest.php
@@ -38,4 +38,54 @@ class EnumMakeCommandTest extends TestCase
             'enum IntEnum: int',
         ], 'app/IntEnum.php');
     }
+
+    public function testItCanGenerateEnumFileInEnumsFolder()
+    {
+        $enumsFolderPath = app_path('Enums');
+
+        $isFolderCreated = false;
+        if (!is_dir($enumsFolderPath)) {
+            mkdir($enumsFolderPath, 0777, true);
+            $isFolderCreated = true;
+        }
+
+        $this->artisan('make:enum', ['name' => 'OrderStatusEnum'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Enums;',
+            'enum OrderStatusEnum',
+        ], 'app/Enums/OrderStatusEnum.php');
+
+        unlink($enumsFolderPath.'/OrderStatusEnum.php');
+
+        if ($isFolderCreated) {
+            rmdir($enumsFolderPath);
+        }
+    }
+
+    public function testItCanGenerateEnumFileInEnumerationsFolder()
+    {
+        $enumerationsFolderPath = app_path('Enumerations');
+
+        $isFolderCreated = false;
+        if (!is_dir($enumerationsFolderPath)) {
+            mkdir($enumerationsFolderPath, 0777, true);
+            $isFolderCreated = true;
+        }
+
+        $this->artisan('make:enum', ['name' => 'OrderStatusEnum'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Enumerations;',
+            'enum OrderStatusEnum',
+        ], 'app/Enumerations/OrderStatusEnum.php');
+
+        unlink($enumerationsFolderPath.'/OrderStatusEnum.php');
+
+        if ($isFolderCreated) {
+            rmdir($enumerationsFolderPath);
+        }
+    }
 }
