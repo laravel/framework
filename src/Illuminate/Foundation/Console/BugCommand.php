7dd51a3f2f5a2dda884b5b0253d8ee6773138e2f
Milwad <milwad.dev@gmail.com>
2024-04-07 17:03:35 +0330

    Create BugCommand.php

---
+++ src/Illuminate/Foundation/Console/BugCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Illuminate\Foundation\Console;
+
+use Closure;
+use Illuminate\Console\Command;
+use Illuminate\Support\Composer;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'bug')]
+class BugCommand extends Command
+{
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'bug {--title= : The title of bug}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = ''; // TODO
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        return 0;
+    }
+}
