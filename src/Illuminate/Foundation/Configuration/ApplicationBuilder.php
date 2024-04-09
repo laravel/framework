02784ff8ddaf41073ad474947913df75aea1c8ad
Michael Nabil <46572405+michaelnabil230@users.noreply.github.com>
2024-04-09 14:49:24 +0200

    Fix

--- src/Illuminate/Foundation/Configuration/ApplicationBuilder.php
+++ src/Illuminate/Foundation/Configuration/ApplicationBuilder.php
@@ -289,7 +289,7 @@ class ApplicationBuilder
     /**
      * Register the scheduled tasks for the application.
      *
-     * @param  callable(Schedule $schedule): void  $callback
+     * @param  callable(\Illuminate\Console\Scheduling\Schedule $schedule): void  $callback
      * @return $this
      */
     public function withSchedule(callable $callback)
