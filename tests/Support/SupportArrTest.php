0b67185066ca476fa7ca7e3721b316842861daa7
S.a Mahmoudzadeh <36761585+saMahmoudzadeh@users.noreply.github.com>
2024-04-09 17:54:04 +0330

    [11.x] improvement test for prepend method (#50968)
    * test: improvement test for prepend method
    
    * refactor: add use TypeError
    
    * wip testPrependWhenKeyIsArray method
    
    * wip

--- tests/Support/SupportArrTest.php
+++ tests/Support/SupportArrTest.php
@@ -775,6 +775,21 @@ class SupportArrTest extends TestCase
 
         $array = Arr::prepend(['one' => 1, 'two' => 2], 0, null);
         $this->assertEquals([null => 0, 'one' => 1, 'two' => 2], $array);
+
+        $array = Arr::prepend(['one', 'two'], null, '');
+        $this->assertEquals(['' => null, 'one', 'two'], $array);
+
+        $array = Arr::prepend([], 'zero');
+        $this->assertEquals(['zero'], $array);
+
+        $array = Arr::prepend([''], 'zero');
+        $this->assertEquals(['zero', ''], $array);
+
+        $array = Arr::prepend(['one', 'two'], ['zero']);
+        $this->assertEquals([['zero'], 'one', 'two'], $array);
+
+        $array = Arr::prepend(['one', 'two'], ['zero'], 'key');
+        $this->assertEquals(['key' => ['zero'], 'one', 'two'], $array);
     }
 
     public function testPull()
