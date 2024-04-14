a54260a765550a3fbb02a5a472f5448bfeeb1523
James <james@lioneagle.solutions>
2024-04-14 14:36:41 +1000

    add textarea fallback

--- src/Illuminate/Console/Concerns/ConfiguresPrompts.php
+++ src/Illuminate/Console/Concerns/ConfiguresPrompts.php
@@ -12,6 +12,7 @@ use Laravel\Prompts\Prompt;
 use Laravel\Prompts\SearchPrompt;
 use Laravel\Prompts\SelectPrompt;
 use Laravel\Prompts\SuggestPrompt;
+use Laravel\Prompts\TextareaPrompt;
 use Laravel\Prompts\TextPrompt;
 use stdClass;
 use Symfony\Component\Console\Input\InputInterface;
@@ -35,6 +36,12 @@ trait ConfiguresPrompts
         Prompt::fallbackWhen(windows_os() || $this->laravel->runningUnitTests());
 
         TextPrompt::fallbackUsing(fn (TextPrompt $prompt) => $this->promptUntilValid(
+            fn () => $this->components->ask($prompt->label, $prompt->default ?: null) ?? '',
+            $prompt->required,
+            $prompt->validate
+        ));
+
+        TextareaPrompt::fallbackUsing(fn (TextareaPrompt $prompt) => $this->promptUntilValid(
             fn () => $this->components->ask($prompt->label, $prompt->default ?: null) ?? '',
             $prompt->required,
             $prompt->validate
