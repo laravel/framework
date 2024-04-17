82c318b11d54a46d0a7ca710e6be1b3b45d85147
Milwad <milwad.dev@gmail.com>
2024-04-17 10:09:43 +0330

    Update InterfaceMakeCommandTest.php

--- tests/Integration/Generators/InterfaceMakeCommandTest.php
+++ tests/Integration/Generators/InterfaceMakeCommandTest.php
@@ -6,6 +6,12 @@ use Illuminate\Tests\Integration\Generators\TestCase;
 
 class InterfaceMakeCommandTest extends TestCase
 {
+    protected $files = [
+        'app/Gateway.php',
+        'app/Contracts/Gateway.php',
+        'app/Interfaces/Gateway.php',
+    ];
+
     public function testItCanGenerateInterfaceFile()
     {
         $this->artisan('make:interface', ['name' => 'Gateway'])
@@ -33,6 +39,27 @@ class InterfaceMakeCommandTest extends TestCase
             'namespace App\Contracts;',
             'interface Gateway',
         ], 'app/Contracts/Gateway.php');
+
+        $files->deleteDirectory($interfacesFolderPath);
+    }
+
+
+    public function testItCanGenerateInterfaceFileWhenInterfacesFolderExists()
+    {
+        $interfacesFolderPath = app_path('Interfaces');
+
+        /** @var \Illuminate\Filesystem\Filesystem $files */
+        $files = $this->app['files'];
+
+        $files->ensureDirectoryExists($interfacesFolderPath);
+
+        $this->artisan('make:interface', ['name' => 'Gateway'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Interfaces;',
+            'interface Gateway',
+        ], 'app/Interfaces/Gateway.php');
 
         $files->deleteDirectory($interfacesFolderPath);
     }
