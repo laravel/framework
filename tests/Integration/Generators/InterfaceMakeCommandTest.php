724af37a65e3d58bc36ca05563f289a762ba29ca
Milwad <98118400+milwad-dev@users.noreply.github.com>
2024-04-17 17:47:38 +0330

    [11.x] Add tests for `make:interface` & `make:trait` when the folders exists (#51094)
    * Update TraitMakeCommandTest.php
    
    * Update TraitMakeCommandTest.php
    
    * Update InterfaceMakeCommandTest.php
    
    * Update InterfaceMakeCommandTest.php
    
    * Update InterfaceMakeCommandTest.php

--- tests/Integration/Generators/InterfaceMakeCommandTest.php
+++ tests/Integration/Generators/InterfaceMakeCommandTest.php
@@ -6,6 +6,12 @@ use Illuminate\Tests\Integration\Generators\TestCase;
 
 class InterfaceMakeCommandTest extends TestCase
 {
+    protected $files = [
+        'app/Gateway.php',
+        'app/Contracts/Gateway.php',
+        'app/Interfaces/Gateway.php',
+    ];
+
     public function testItCanGenerateInterfaceFile()
     {
         $this->artisan('make:interface', ['name' => 'Gateway'])
@@ -15,5 +21,45 @@ class InterfaceMakeCommandTest extends TestCase
             'namespace App;',
             'interface Gateway',
         ], 'app/Gateway.php');
+    }
+
+    public function testItCanGenerateInterfaceFileWhenContractsFolderExists()
+    {
+        $interfacesFolderPath = app_path('Contracts');
+
+        /** @var \Illuminate\Filesystem\Filesystem $files */
+        $files = $this->app['files'];
+
+        $files->ensureDirectoryExists($interfacesFolderPath);
+
+        $this->artisan('make:interface', ['name' => 'Gateway'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Contracts;',
+            'interface Gateway',
+        ], 'app/Contracts/Gateway.php');
+
+        $files->deleteDirectory($interfacesFolderPath);
+    }
+
+    public function testItCanGenerateInterfaceFileWhenInterfacesFolderExists()
+    {
+        $interfacesFolderPath = app_path('Interfaces');
+
+        /** @var \Illuminate\Filesystem\Filesystem $files */
+        $files = $this->app['files'];
+
+        $files->ensureDirectoryExists($interfacesFolderPath);
+
+        $this->artisan('make:interface', ['name' => 'Gateway'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Interfaces;',
+            'interface Gateway',
+        ], 'app/Interfaces/Gateway.php');
+
+        $files->deleteDirectory($interfacesFolderPath);
     }
 }
--- tests/Integration/Generators/TraitMakeCommandTest.php
+++ tests/Integration/Generators/TraitMakeCommandTest.php
@@ -16,4 +16,44 @@ class TraitMakeCommandTest extends TestCase
             'trait FooTrait',
         ], 'app/FooTrait.php');
     }
+
+    public function testItCanGenerateTraitFileWhenTraitsFolderExists()
+    {
+        $traitsFolderPath = app_path('Traits');
+
+        /** @var \Illuminate\Filesystem\Filesystem $files */
+        $files = $this->app['files'];
+
+        $files->ensureDirectoryExists($traitsFolderPath);
+
+        $this->artisan('make:trait', ['name' => 'FooTrait'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Traits;',
+            'trait FooTrait',
+        ], 'app/Traits/FooTrait.php');
+
+        $files->deleteDirectory($traitsFolderPath);
+    }
+
+    public function testItCanGenerateTraitFileWhenConcernsFolderExists()
+    {
+        $traitsFolderPath = app_path('Concerns');
+
+        /** @var \Illuminate\Filesystem\Filesystem $files */
+        $files = $this->app['files'];
+
+        $files->ensureDirectoryExists($traitsFolderPath);
+
+        $this->artisan('make:trait', ['name' => 'FooTrait'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Concerns;',
+            'trait FooTrait',
+        ], 'app/Concerns/FooTrait.php');
+
+        $files->deleteDirectory($traitsFolderPath);
+    }
 }
