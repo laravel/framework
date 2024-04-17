eb31c95e1b2defdb4928605de4c02e74db02efa0
Milwad <milwad.dev@gmail.com>
2024-04-17 09:59:35 +0330

    Update TraitMakeCommandTest.php

--- tests/Integration/Generators/TraitMakeCommandTest.php
+++ tests/Integration/Generators/TraitMakeCommandTest.php
@@ -16,4 +16,24 @@ class TraitMakeCommandTest extends TestCase
             'trait FooTrait',
         ], 'app/FooTrait.php');
     }
+
+    public function testItCanGenerateTraitFileWhenTraitsFolderExists()
+    {
+        $traitsFolderPath = app_path('Traits');
+
+        /** @var \Illuminate\Filesystem\Filesystem $files */
+        $files = $this->app['files'];
+
+        $files->ensureDirectoryExists($traitsFolderPath);
+
+        $this->artisan('make:trait', ['name' => 'FooTrait'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Traits;',
+            'trait FooTrait',
+        ], 'app/Traits/FooTrait.php');
+
+        $files->deleteDirectory($traitsFolderPath);
+    }
 }
