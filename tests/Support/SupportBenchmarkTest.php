2111fb926d7c3cc24a09599f7dc010f7e81bb20a
S.a Mahmoudzadeh <s.a.mahmoudzadeh@gmail.com>
2024-04-19 00:45:41 +0330

    add some tests for Benchmark

---
+++ tests/Support/SupportBenchmarkTest.php
@@ -0,0 +1,101 @@
+<?php declare(strict_types=1);
+
+namespace Illuminate\Tests\Support;
+
+use Illuminate\Support\Benchmark;
+use PHPUnit\Framework\TestCase;
+
+class SupportBenchmarkTest extends TestCase
+{
+    public function testMeasureWithClosure(): void
+    {
+        $closure = static function () {
+            // Some operation to benchmark
+            $arr = range(1, 1000);
+            array_sum($arr);
+        };
+
+        $result = Benchmark::measure($closure, 10);
+
+        $this->assertIsFloat($result);
+    }
+
+    public function testMeasureWithArray(): void
+    {
+        $callbacks = [
+            function () {
+                // Some operation to benchmark
+                $arr = range(1, 1000);
+                array_sum($arr);
+            },
+            function () {
+                // Another operation to benchmark
+                $arr = range(1, 1000);
+                array_product($arr);
+            }
+        ];
+
+        $result = Benchmark::measure($callbacks, 10);
+
+        $this->assertIsArray($result);
+        $this->assertCount(2, $result);
+    }
+
+    public function testMeasureWithException(): void
+    {
+        $this->expectException(\Exception::class);
+
+        /**
+         * @throws \Exception
+         */
+        $callback = static function () {
+            throw new \Exception("Test exception");
+        };
+
+        Benchmark::measure($callback,1);
+    }
+
+    public function testValueWithClosure(): void
+    {
+        $closure = static function () {
+            $arr = range(1, 1000);
+            array_sum($arr);
+        };
+
+        $result = Benchmark::value($closure);
+
+        $this->assertIsArray($result);
+        $this->assertCount(2, $result);
+        $this->assertIsFloat($result[1]);
+    }
+
+    public function testValueWithException(): void
+    {
+        $this->expectException(\Exception::class);
+
+        /**
+         * @throws \Exception
+         */
+        $callback = static function () {
+            throw new \Exception("Test exception");
+        };
+
+        Benchmark::value($callback);
+    }
+
+    public function testDDWithCallable(): void
+    {
+        $callable = 'strlen';
+
+        $this->expectException(\Error::class);
+        Benchmark::dd($callable, 10);
+    }
+
+    public function testDDWithException(): void
+    {
+        $this->expectException(\Exception::class);
+
+        $callback = fn() => throw new \Exception('Test exception');
+        Benchmark::dd($callback);
+    }
+}
