f9877a9031f0aeb140abd1152b833e419d7701c9
Nuno Maduro <enunomaduro@gmail.com>
2024-04-30 18:19:18 +0100

    Reduces css size

--- src/Illuminate/Foundation/resources/exceptions/renderer/tailwind.config.js
+++ src/Illuminate/Foundation/resources/exceptions/renderer/tailwind.config.js
@@ -3,29 +3,12 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./**/*.blade.php'],
-    safelist: [
-        {
-            pattern: /grid-cols-(\d+)/,
-            variants: ['sm', 'md', 'lg', 'xl', '2xl', 'default', 'default:lg'],
-        },
-        {
-            pattern: /(row|col)-span-(\d+|full)/,
-            variants: ['sm', 'md', 'lg', 'xl', '2xl', 'default', 'default:lg'],
-        },
-        {
-            pattern: /h-\d+/,
-            variants: ['sm', 'md', 'lg', 'xl', '2xl'],
-        },
-    ],
     darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-            height: {
-                128: '32rem',
-            },
+            }
         },
     },
     plugins: [],
