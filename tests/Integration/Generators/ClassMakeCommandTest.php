b3af9a896436f59838b08c621c40cac48e510869
S.a Mahmoudzadeh <s.a.mahmoudzadeh@gmail.com>
2024-04-30 09:26:57 +0330

    add some tests for strict mode

--- tests/Integration/Generators/ClassMakeCommandTest.php
+++ tests/Integration/Generators/ClassMakeCommandTest.php
@@ -4,7 +4,12 @@ namespace Illuminate\Tests\Integration\Generators;
 
 class ClassMakeCommandTest extends TestCase
 {
-    public function testItCanGenerateClassFile()
+    protected array $files = [
+        'app/Reverb.php',
+        'app/Notification.php'
+    ];
+
+    public function testItCanGenerateClassFile(): void
     {
         $this->artisan('make:class', ['name' => 'Reverb'])
             ->assertExitCode(0);
@@ -16,12 +21,39 @@ class ClassMakeCommandTest extends TestCase
         ], 'app/Reverb.php');
     }
 
-    public function testItCanGenerateInvokableClassFile()
+    public function testItCanGenerateStrictClassFile(): void
+    {
+        $this->artisan('make:class', ['name' => 'Reverb', '--strict' => true])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'declare(strict_types=1)',
+            'namespace App;',
+            'class Reverb',
+            'public function __construct()',
+        ], 'app/Reverb.php');
+    }
+
+    public function testItCanGenerateInvokableClassFile(): void
     {
         $this->artisan('make:class', ['name' => 'Notification', '--invokable' => true])
             ->assertExitCode(0);
 
         $this->assertFileContains([
+            'namespace App;',
+            'class Notification',
+            'public function __construct()',
+            'public function __invoke()',
+        ], 'app/Notification.php');
+    }
+
+    public function testItCanGenerateStrictInvokableClassFile(): void
+    {
+        $this->artisan('make:class', ['name' => 'Notification', '--invokable' => true, '--strict' => true])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'declare(strict_types=1)',
             'namespace App;',
             'class Notification',
             'public function __construct()',
