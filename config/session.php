782981fef470639a9caafd7d1888340a0f28db40
Jonathan Goode <u01jmg3@gmail.com>
2024-05-13 21:18:13 +0100

    Sync configs with laravel/laravel

--- config/session.php
+++ config/session.php
@@ -191,7 +191,7 @@ return [
     |
     | This option determines how your cookies behave when cross-site requests
     | take place, and can be used to mitigate CSRF attacks. By default, we
-    | will set this value to "lax" since this is a secure default value.
+    | will set this value to "lax" to permit secure cross-site requests.
     |
     | See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#samesitesamesite-value
     |
