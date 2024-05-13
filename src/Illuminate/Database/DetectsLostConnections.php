8059bea88f78e431659f6adc982e31e0475d17bc
Khin Nyunt <33210614+cragonnyunt@users.noreply.github.com>
2024-05-13 23:37:45 +0630

    Change ssl: broken pipe to broken pipe (#51388)
    Without using SSL connection to database does not include string `SSL` in error message

--- src/Illuminate/Database/DetectsLostConnections.php
+++ src/Illuminate/Database/DetectsLostConnections.php
@@ -52,7 +52,7 @@ trait DetectsLostConnections
             'SSL: Connection timed out',
             'SQLSTATE[HY000]: General error: 1105 The last transaction was aborted due to Seamless Scaling. Please retry.',
             'Temporary failure in name resolution',
-            'SSL: Broken pipe',
+            'Broken pipe',
             'SQLSTATE[08S01]: Communication link failure',
             'SQLSTATE[08006] [7] could not connect to server: Connection refused Is the server running on host',
             'SQLSTATE[HY000]: General error: 7 SSL SYSCALL error: No route to host',
