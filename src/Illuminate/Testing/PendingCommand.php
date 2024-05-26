228c65096a140ef260bdc7acdcc02ff08c53171e
Perry van der Meer <11609290+PerryvanderMeer@users.noreply.github.com>
2024-05-26 13:44:34 +0200

    use Macroable

--- src/Illuminate/Testing/PendingCommand.php
+++ src/Illuminate/Testing/PendingCommand.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Macroable;
 use Mockery;
 use Mockery\Exception\NoMatchingExpectationException;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
@@ -20,6 +21,7 @@ use Symfony\Component\Console\Output\BufferedOutput;
 class PendingCommand
 {
     use Conditionable;
+    use Macroable;
 
     /**
      * The test being run.
