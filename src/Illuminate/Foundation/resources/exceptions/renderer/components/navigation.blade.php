b2e9ffcc55d2ae28f5bfbbe86d4e4af34ac61a6b
Henri De Veene <467201+Henridv@users.noreply.github.com>
2024-05-30 16:11:05 +0200

    Fix typo in filename (#51643)

--- src/Illuminate/Foundation/resources/exceptions/renderer/components/navigation.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer/components/navigation.blade.php
@@ -21,7 +21,7 @@
             </div>
 
             <div class="flex items-center gap-3 sm:gap-6">
-                <x-laravel-exceptions-renderer::theme-swicher />
+                <x-laravel-exceptions-renderer::theme-switcher />
             </div>
         </div>
     </div>
