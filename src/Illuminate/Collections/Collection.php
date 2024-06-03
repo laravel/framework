8154eb6e4b9673f332e2c26daf7730e409d443cc
Faissal Wahabali <fwahabali@gmail.com>
2024-06-03 14:02:27 +0100

    [10.x] Fix collection shift less than one item (#51686)
    * fix collection shift less than 1
    
    * throw exception earlier
    
    * Update Collection.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Collections/Collection.php
+++ src/Illuminate/Collections/Collection.php
@@ -7,6 +7,7 @@ use ArrayIterator;
 use Illuminate\Contracts\Support\CanBeEscapedWhenCastToString;
 use Illuminate\Support\Traits\EnumeratesValues;
 use Illuminate\Support\Traits\Macroable;
+use InvalidArgumentException;
 use stdClass;
 use Traversable;
 
@@ -1124,15 +1125,21 @@ class Collection implements ArrayAccess, CanBeEscapedWhenCastToString, Enumerabl
      *
      * @param  int  $count
      * @return static<int, TValue>|TValue|null
+     *
+     * @throws \InvalidArgumentException
      */
     public function shift($count = 1)
     {
-        if ($count === 1) {
-            return array_shift($this->items);
+        if ($count < 0) {
+            throw new InvalidArgumentException('Number of shifted items may not be less than zero.');
         }
 
-        if ($this->isEmpty()) {
+        if ($count === 0 || $this->isEmpty()) {
             return new static;
+        }
+
+        if ($count === 1) {
+            return array_shift($this->items);
         }
 
         $results = [];
