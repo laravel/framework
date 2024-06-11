cd1c4bc9a6411a679ced4a5251c9c6cf4ca4d9a9
Mior Muhammad Zaki <crynobone@gmail.com>
2024-06-11 16:39:14 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Foundation/Configuration/WithScheduleTest.php
+++ tests/Integration/Foundation/Configuration/WithScheduleTest.php
@@ -5,7 +5,9 @@ namespace Illuminate\Tests\Integration\Foundation\Configuration;
 use Illuminate\Console\Scheduling\ScheduleListCommand;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Artisan;
 use Orchestra\Testbench\TestCase;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
 
 class WithScheduleTest extends TestCase
 {
@@ -22,6 +24,10 @@ class WithScheduleTest extends TestCase
         return Application::configure(static::applicationBasePath())
             ->withSchedule(function ($schedule) {
                 $schedule->command('schedule:clear-cache')->everyMinute();
+
+                Artisan::command('test:example', function () {
+                    $this->comment('Example');
+                })->everyMinute();
             })
             ->withCommands([__DIR__.'/stubs/console.php'])
             ->create();
@@ -32,6 +38,15 @@ class WithScheduleTest extends TestCase
         $this->artisan(ScheduleListCommand::class)
             ->assertSuccessful()
             ->expectsOutputToContain('  0 * * * *  php artisan test:inspire .............. Next Due: 1 hour from now')
+            ->expectsOutputToContain('  * * * * *  php artisan test:example ............ Next Due: 1 minute from now')
             ->expectsOutputToContain('  * * * * *  php artisan schedule:clear-cache .... Next Due: 1 minute from now');
+    }
+
+    public function testCommandDeclaredWithinWithScheduleCannotBeExecutedDirectly()
+    {
+        $this->expectException(CommandNotFoundException::class);
+        $this->expectExceptionMessage('The command "test:example" does not exist.');
+
+        $this->artisan('test:example');
     }
 }
