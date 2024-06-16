1170df3c0fc4ebcf0079566e6662917de5804460
tamiroh <tamiroh71@gmail.com>
2024-06-16 16:28:41 +0900

    Fix typo

--- src/Illuminate/Foundation/Console/VendorPublishCommand.php
+++ src/Illuminate/Foundation/Console/VendorPublishCommand.php
@@ -394,7 +394,7 @@ class VendorPublishCommand extends Command
     }
 
     /**
-     * Intruct the command to not update the dates on migrations when publishing.
+     * Instruct the command to not update the dates on migrations when publishing.
      *
      * @return void
      */
