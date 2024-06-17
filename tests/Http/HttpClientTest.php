59840fc22e2df94db07c14b6e8e83dbea97dadf2
Dwight Watson <dwightwatson@users.noreply.github.com>
2024-06-17 19:49:03 +1000

    Add unprocessableContent and update unprocessableEntity

--- tests/Http/HttpClientTest.php
+++ tests/Http/HttpClientTest.php
@@ -206,6 +206,20 @@ class HttpClientTest extends TestCase
         $this->assertFalse($response->conflict());
     }
 
+    public function testUnprocessableContentRequest()
+    {
+        $this->factory->fake([
+            'vapor.laravel.com' => $this->factory::response('', HttpResponse::HTTP_UNPROCESSABLE_ENTITY),
+            'forge.laravel.com' => $this->factory::response('', HttpResponse::HTTP_OK),
+        ]);
+
+        $response = $this->factory->post('http://vapor.laravel.com');
+        $this->assertTrue($response->unprocessableContent());
+
+        $response = $this->factory->post('http://forge.laravel.com');
+        $this->assertFalse($response->unprocessableContent());
+    }
+
     public function testUnprocessableEntityRequest()
     {
         $this->factory->fake([
