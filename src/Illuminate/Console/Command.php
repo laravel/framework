9e95d7ba8f8c989087e9448810bcb0d1bf4ad4a5
Mior Muhammad Zaki <crynobone@gmail.com>
2024-06-18 14:32:00 +0800

    [10.x] Fixes unable to call another command as a initialized instance of `Command` class.
    fixes #51822
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Console/Command.php
+++ src/Illuminate/Console/Command.php
@@ -236,11 +236,13 @@ class Command extends SymfonyCommand
      */
     protected function resolveCommand($command)
     {
-        if (! class_exists($command)) {
-            return $this->getApplication()->find($command);
-        }
+        if (is_string($command)) {
+            if (! class_exists($command)) {
+                return $this->getApplication()->find($command);
+            }
 
-        $command = $this->laravel->make($command);
+            $command = $this->laravel->make($command);
+        }
 
         if ($command instanceof SymfonyCommand) {
             $command->setApplication($this->getApplication());
