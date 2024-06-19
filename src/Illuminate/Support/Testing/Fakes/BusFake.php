2b3fe96dc41c2f5188c8f67405b1f48f02306ca2
Andrew Minion <andrew@andrewrminion.com>
2024-06-19 16:15:44 -0500

    improve batched jobs

--- src/Illuminate/Support/Testing/Fakes/BusFake.php
+++ src/Illuminate/Support/Testing/Fakes/BusFake.php
@@ -496,7 +496,12 @@ class BusFake implements Fake, QueueingDispatcher
      */
     public function assertNothingBatched()
     {
-        PHPUnit::assertEmpty($this->batches, 'Batched jobs were dispatched unexpectedly.');
+        $jobNames = collect($this->batches)
+            ->map(fn ($batch) => $batch->jobs->map(fn ($job) => get_class($job)))
+            ->flatten()
+            ->join("\n- ");
+
+        PHPUnit::assertEmpty($this->batches, "The following batched jobs were dispatched unexpectedly:\n\n- $jobNames\n");
     }
 
     /**
