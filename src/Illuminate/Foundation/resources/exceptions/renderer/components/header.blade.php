b25b4a9f0dd8d8dabe08329f8750c3142222ec31
Sheng Slogar <sslogar@intelliprocessing.com>
2024-06-21 11:08:18 -0500

    Truncate long HTTP hosts

--- src/Illuminate/Foundation/resources/exceptions/renderer/components/header.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer/components/header.blade.php
@@ -14,13 +14,13 @@
             </div>
         </div>
 
-        <div class="hidden text-right shrink-0 md:block md:min-w-64">
+        <div class="hidden text-right shrink-0 md:block md:min-w-64 md:max-w-80">
             <div>
-                <span class="rounded-full bg-gray-200 px-3 py-2 text-sm leading-5 text-gray-900 dark:bg-gray-800 dark:text-white">
+                <span class="inline-block rounded-full bg-gray-200 px-3 py-2 text-sm leading-5 text-gray-900 max-w-full truncate dark:bg-gray-800 dark:text-white">
                     {{ $exception->request()->method() }} {{ $exception->request()->httpHost() }}
                 </span>
             </div>
-            <div class="mt-4 px-4">
+            <div class="px-4">
                 <span class="text-sm text-gray-500 dark:text-gray-400">PHP {{ PHP_VERSION }} — Laravel {{ app()->version() }}</span>
             </div>
         </div>
