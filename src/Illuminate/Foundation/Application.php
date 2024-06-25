9a6d9cea83cfa6b9e8eda05c89741d0411d8ebe8
driesvints <driesvints@users.noreply.github.com>
2024-06-25 19:33:56 +0000

    Update version to v11.12.0

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -45,7 +45,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '11.11.1';
+    const VERSION = '11.12.0';
 
     /**
      * The base path for the Laravel installation.
