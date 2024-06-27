b28b2eff7c93a4e0374d3d1ad1d6382139e64edd
Ostap Brehin <osbre@protonmail.com>
2024-06-27 02:05:49 +0100

    Update GeneratorCommand.php

--- src/Illuminate/Console/GeneratorCommand.php
+++ src/Illuminate/Console/GeneratorCommand.php
@@ -417,7 +417,7 @@ abstract class GeneratorCommand extends Command implements PromptsForMissingInpu
      */
     protected function getNameInput()
     {
-        return (string) Str::of($this->argument('name'))->trim()->rtrim('.php');
+        return (string) Str::of($this->argument('name'))->trim()->chopEnd('.php');
     }
 
     /**
