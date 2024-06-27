52245da52765fb4ef1c45dee9aab55408e07b110
Sheng-Liang Slogar <sheng@diglactic.com>
2024-06-27 03:48:09 -0500

    [11.x] Account for long strings on new Laravel error page (#51880)
    * Force-wrap long exception message strings
    
    * Truncate long exception class names
    
    * Prevent long domain names from overflowing outside card
    
    * Truncate long HTTP hosts
    
    * Rebuild styles

--- src/Illuminate/Foundation/resources/exceptions/renderer/components/header.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer/components/header.blade.php
@@ -1,24 +1,26 @@
 <x-laravel-exceptions-renderer::card>
     <div class="md:flex md:items-center md:justify-between md:gap-2">
-        <div>
-            <div class="inline-block rounded-full bg-red-500/20 px-3 py-2 dark:bg-red-500/20">
-                <span class="hidden text-sm font-bold leading-5 text-red-500 md:inline-block lg:text-base">
+        <div class="min-w-0">
+            <div class="inline-block rounded-full bg-red-500/20 px-3 py-2 max-w-full text-sm font-bold leading-5 text-red-500 truncate lg:text-base dark:bg-red-500/20">
+                <span class="hidden md:inline">
                     {{ $exception->class() }}
                 </span>
-                <span class="text-sm font-bold leading-5 text-red-500 md:hidden lg:text-base">
+                <span class="md:hidden">
                     {{ implode(' ', array_slice(explode('\\', $exception->class()), -1)) }}
                 </span>
             </div>
-            <div class="mt-4 text-lg font-semibold text-gray-900 dark:text-white lg:text-2xl">{{ $exception->message() }}</div>
+            <div class="mt-4 text-lg font-semibold text-gray-900 break-words dark:text-white lg:text-2xl">
+                {{ $exception->message() }}
+            </div>
         </div>
 
-        <div class="hidden text-right md:block md:min-w-64">
+        <div class="hidden text-right shrink-0 md:block md:min-w-64 md:max-w-80">
             <div>
-                <span class="rounded-full bg-gray-200 px-3 py-2 text-sm leading-5 text-gray-900 dark:bg-gray-800 dark:text-white">
+                <span class="inline-block rounded-full bg-gray-200 px-3 py-2 text-sm leading-5 text-gray-900 max-w-full truncate dark:bg-gray-800 dark:text-white">
                     {{ $exception->request()->method() }} {{ $exception->request()->httpHost() }}
                 </span>
             </div>
-            <div class="mt-4 px-4">
+            <div class="px-4">
                 <span class="text-sm text-gray-500 dark:text-gray-400">PHP {{ PHP_VERSION }} — Laravel {{ app()->version() }}</span>
             </div>
         </div>
