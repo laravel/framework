5d006a562c6a2bb6f73c77491ee59117a103217d
Caleb White <cdwhite3@pm.me>
2024-06-28 15:14:10 -0500

    [11.x] feat: add more specific types and tests for helpers (#51938)
    * feat: add more specific types and tests for helpers
    
    * feat: narrow types for `filled` and `blank` helpers

--- types/Support/Helpers.php
+++ types/Support/Helpers.php
@@ -2,73 +2,59 @@
 
 use function PHPStan\Testing\assertType;
 
+/** @var bool|float|int|string|null $value */
+if (filled($value)) {
+    assertType('bool|float|int|non-empty-string', $value);
+} else {
+    assertType('string|null', $value);
+}
+
+if (blank($value)) {
+    assertType('string|null', $value);
+} else {
+    assertType('bool|float|int|non-empty-string', $value);
+}
+
+assertType('User', object_get(new User(), null));
+assertType('User', object_get(new User(), ''));
+assertType('mixed', object_get(new User(), 'name'));
+
 assertType('int', once(fn () => 1));
-
-assertType('User', once(fn () => new User()));
-
-$value = once(function () { // @phpstan-ignore-line
-    //
-});
-
-assertType('null', $value);
-
-assertType('User', with(new User()));
-assertType('bool', with(new User())->save());
-
-assertType('User', with(new User(), function (User $user) {
-    return $user;
-}));
-assertType('User', with(new User(), function (User $user): User {
-    return $user;
+assertType('null', once(function () { /** @phpstan-ignore function.void (testing void) */
 }));
 
-assertType('User', with(new User(), function ($user) {
-    /** @var User $user */
-    return $user;
-}));
-assertType('User', with(new User(), function ($user): User {
-    /** @var User $user */
-    return $user;
-}));
+assertType('Illuminate\Support\Optional', optional());
+assertType('null', optional(null, fn () => 1));
+assertType('int', optional('foo', function ($value) {
+    assertType('string', $value);
 
-assertType('int', with(new User(), function ($user) {
-    assertType('User', $user);
-
-    return 10;
-}));
-assertType('int', with(new User(), function ($user): int {
-    assertType('User', $user);
-
-    return 10;
+    return 1;
 }));
 
-assertType('User', with(new User(), function ($user) {
-    return $user;
-}));
-assertType('User', with(new User(), function ($user): User {
-    return $user;
-}));
+assertType('int', retry(5, fn () => 1));
 
-// falls back to default if provided
-assertType('int|null', transform(optional(), fn () => 1));
-// default as callable
-assertType('int|string', transform(optional(), fn () => 1, fn () => 'string'));
-
-// non empty values
-assertType('int', transform('filled', fn () => 1));
-assertType('int', transform(['filled'], fn () => 1));
-assertType('int', transform(new User(), fn () => 1));
-
-// "empty" values
-assertType('null', transform(null, fn () => 1));
-assertType('null', transform('', fn () => 1));
-assertType('null', transform([], fn () => 1));
-
-assertType('int|null', rescue(fn () => 123));
-assertType('int', rescue(fn () => 123, 345));
-assertType('int', rescue(fn () => 123, fn () => 345));
+assertType('object', str());
+assertType('Illuminate\Support\Stringable', str('foo'));
 
 assertType('User', tap(new User(), function ($user) {
     assertType('User', $user);
 }));
 assertType('Illuminate\Support\HigherOrderTapProxy', tap(new User()));
+
+assertType('bool', throw_if(true, Exception::class));
+
+assertType('bool', throw_unless(true, Exception::class));
+
+assertType('int', transform('filled', fn () => 1, true));
+assertType('int', transform(['filled'], fn () => 1));
+assertType('null', transform('', fn () => 1));
+assertType('bool', transform('', fn () => 1, true));
+assertType('bool', transform('', fn () => 1, fn () => true));
+
+assertType('User', with(new User()));
+assertType('bool', with(new User())->save());
+assertType('int', with(new User(), function ($user) {
+    assertType('User', $user);
+
+    return 10;
+}));
