adbbeb34077924a873a409edc056570b83446ab9
Dimitrios Kontoulis <kontoulisdhm@gmail.com>
2024-06-28 11:26:21 +0300

    added doc block

--- src/Illuminate/Database/Eloquent/Attributes/Cast.php
+++ src/Illuminate/Database/Eloquent/Attributes/Cast.php
@@ -6,6 +6,9 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
 class Cast
 {
+    /**
+     * Create a new attribute instance.
+     */
     public function __construct(private string $property, private string|array $type)
     {
     }
