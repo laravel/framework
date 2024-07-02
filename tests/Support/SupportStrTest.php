fc10576f04c06cdb1a16007aba9c426eb6f0e203
Matt Stenson <matt.stenson@gmail.com>
2024-07-02 12:10:35 -0600

    Fix white space

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -1513,7 +1513,7 @@ class SupportStrTest extends TestCase
         $this->assertTrue(
             Str::of(Str::password(letters: false, upperLetters: true))->contains(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
         );
-        
+
         $this->assertTrue(
             Str::of(Str::password(letters: false, lowerLetters: true))->contains(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
         );
