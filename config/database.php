e6360d440bc19ef68b948292e3631ea865825f7b
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2024-07-07 16:02:21 -0400

    Wip

--- config/database.php
+++ config/database.php
@@ -37,7 +37,7 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => env('DB_BUSY_TIMEOUT'),
+            'busy_timeout' => null,
         ],
 
         'mysql' => [
