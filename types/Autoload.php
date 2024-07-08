2f1f3301bdb4d6ac45f980702e2d2bc544e2d104
Caleb White <cdwhite3@pm.me>
2024-07-08 10:08:27 -0500

    feat: improve Factory generics (#52005)

--- types/Autoload.php
+++ types/Autoload.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\MassPrunable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\HasDatabaseNotifications;
@@ -9,9 +11,27 @@ use Illuminate\Notifications\HasDatabaseNotifications;
 class User extends Authenticatable
 {
     use HasDatabaseNotifications;
+    /** @use HasFactory<UserFactory> */
     use HasFactory;
     use MassPrunable;
     use SoftDeletes;
+
+    protected static string $factory = UserFactory::class;
+}
+
+/** @extends Factory<User> */
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition(): array
+    {
+        return [];
+    }
+}
+
+class Post extends Model
+{
 }
 
 enum UserType
