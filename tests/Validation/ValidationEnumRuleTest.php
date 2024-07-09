93748010c633ccb5763b27278dd501e43c10ef0d
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2024-07-09 16:03:09 -0400

    Remove duplicate line

--- tests/Validation/ValidationEnumRuleTest.php
+++ tests/Validation/ValidationEnumRuleTest.php
@@ -256,7 +256,6 @@ class ValidationEnumRuleTest extends TestCase
             [IntegerStatus::done, [IntegerStatus::done, IntegerStatus::pending], true],
             [IntegerStatus::done, new ArrayObject([IntegerStatus::done, IntegerStatus::pending]), true],
             [IntegerStatus::done, new Collection([IntegerStatus::done, IntegerStatus::pending]), true],
-            [IntegerStatus::done, [IntegerStatus::done, IntegerStatus::pending], true],
             [IntegerStatus::pending->value, [IntegerStatus::done, IntegerStatus::pending], true],
             [IntegerStatus::done->value, IntegerStatus::pending, false],
         ];
