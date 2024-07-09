c6855bd45d3218c2886e43b44eb1fbca6151e777
LaMbo <alambicchi@overmob.com>
2024-07-09 09:22:23 +0200

    added trusted proxies configuration

---
+++ config/trustedproxy.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Proxies
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to customize the load balancers or proxies
+    | that should be trusted by your application.
+    |
+    */
+
+    'proxies' => env('TRUSTEDPROXY_PROXIES'),
+
+];
