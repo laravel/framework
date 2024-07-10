3343f2b1f78ac0914d2fe236ece58dee65f6ad24
Quỳnh Nguyễn <seriquynh@gmail.com>
2024-07-11 02:04:49 +0700

    Fix docblock for \Illuminate\Validation\ClosureValidationRule::message() (#52077)

--- src/Illuminate/Validation/ClosureValidationRule.php
+++ src/Illuminate/Validation/ClosureValidationRule.php
@@ -72,7 +72,7 @@ class ClosureValidationRule implements RuleContract, ValidatorAwareRule
     /**
      * Get the validation error messages.
      *
-     * @return string
+     * @return array
      */
     public function message()
     {
