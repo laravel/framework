a6d31a1036eb1e18f60d09bc63f216f1ae4b7a57
omaresmaeel <omaresmael590@gmail.com>
2024-07-10 20:17:45 +0300

    [11.x] add trustedproxy config file to allow dynamic assignment for IPs

---
+++ config/trustedproxy.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Proxies
+    |--------------------------------------------------------------------------
+    |
+    | This option control the trusted proxies of the application. You may
+    | add your proxies' IPs as required. The default behaviour is to trust
+    | no IPs
+    |
+    */
+    'proxies' => null,
+];
