9f1bb1550e5d49088076bc5e1c9590ad97475e5f
Italo Israel Baeza Cabrera <darghosthunter@gmail.com>
2024-07-14 20:11:51 -0400

    [11.x] Recreates framework storage directory

---
+++ src/Illuminate/Foundation/Console/StorageRecreateCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Illuminate\Foundation\Console;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'storage:framework')]
+class StorageRecreateCommand extends Command
+{
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'storage:framework';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Recreates storage directories configured for the application';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        foreach ($this->directories() as $directory) {
+            if (file_exists($directory)) {
+                $this->components->warn("The storage path [$directory] already exists.");
+                continue;
+            }
+
+            $this->laravel->make('files')->ensureDirectoryExists($directory);
+
+            $this->components->info("The storage path [$directory] has been created.");
+        }
+    }
+
+    /**
+     * Get the default storage directories that are configured for the application.
+     *
+     * @return array
+     */
+    protected function directories()
+    {
+        return [
+            $this->laravel->storagePath('framework/cache/data'),
+            $this->laravel->storagePath('framework/sessions'),
+            $this->laravel->storagePath('framework/testings'),
+            $this->laravel->storagePath('framework/views'),
+        ];
+    }
+}
