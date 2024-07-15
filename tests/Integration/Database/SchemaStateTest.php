2351a8fb15ef236d19d41e0103995277c7a90922
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2024-07-15 14:08:47 -0400

    Wip

--- tests/Integration/Database/SchemaStateTest.php
+++ tests/Integration/Database/SchemaStateTest.php
@@ -13,13 +13,6 @@ class SchemaStateTest extends DatabaseTestCase
         'database/schema',
     ];
 
-    protected function createSqliteDatabase(): void
-    {
-        DB::connection('sqlite')->getSchemaBuilder()->createDatabase(
-            DB::connection('sqlite')->getConfig('database')
-        );
-    }
-
     public function testSchemaDumpOnSqlite()
     {
         if ($this->driver !== 'sqlite') {
@@ -27,15 +20,14 @@ class SchemaStateTest extends DatabaseTestCase
         }
 
         $connection = DB::connection('sqlite');
-
-        $this->createSqliteDatabase();
-
-        $this->app['files']->ensureDirectoryExists(database_path('schema'));
+        $connection->getSchemaBuilder()->createDatabase($connection->getConfig('database'));
 
         $connection->statement('CREATE TABLE users(id integer primary key autoincrement not null, email varchar not null, name varchar not null);');
         $connection->statement('CREATE UNIQUE INDEX users_email_unique on users (email);');
         $connection->statement('INSERT INTO users (email, name) VALUES ("taylor@laravel.com", "Taylor Otwell");');
         $connection->statement('PRAGMA optimize;');
+
+        $this->app['files']->ensureDirectoryExists(database_path('schema'));
 
         $connection->getSchemaState()->dump($connection, database_path('schema/sqlite-schema.sql'));
 
