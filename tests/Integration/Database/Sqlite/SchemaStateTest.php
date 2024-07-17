ff9667621494b875cfe6a48dc9d934c3ce6e8977
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2024-07-17 13:45:02 -0400

    Simplify test

--- tests/Integration/Database/Sqlite/SchemaStateTest.php
+++ tests/Integration/Database/Sqlite/SchemaStateTest.php
@@ -26,12 +26,9 @@ class SchemaStateTest extends DatabaseTestCase
         $connection->getSchemaBuilder()->createDatabase($connection->getConfig('database'));
 
         $connection->statement('CREATE TABLE users(id integer primary key autoincrement not null, email varchar not null, name varchar not null);');
-        $connection->statement('CREATE UNIQUE INDEX users_email_unique on users (email);');
         $connection->statement('INSERT INTO users (email, name) VALUES ("taylor@laravel.com", "Taylor Otwell");');
 
-        $connection->statement('PRAGMA optimize;');
-
-        $this->assertTrue($connection->table('sqlite_stat1')->exists());
+        $this->assertTrue($connection->table('sqlite_sequence')->exists());
 
         $this->app['files']->ensureDirectoryExists(database_path('schema'));
 
@@ -40,10 +37,8 @@ class SchemaStateTest extends DatabaseTestCase
         $this->assertFileContains([
             'CREATE TABLE users',
         ], 'database/schema/sqlite-schema.sql');
-        $this->assertFileDoesNotContains([
+        $this->assertFileNotContains([
             'sqlite_sequence',
-            'sqlite_stat1',
-            'sqlite_stat4',
         ], 'database/schema/sqlite-schema.sql');
     }
 }
