64129a0ded750c8e74caeec88519da32f54ee350
Enzo Innocenzi <enzo@innocenzi.dev>
2024-07-17 14:30:29 +0200

    feat: allow non-contextual attributes to have an `after` callback

--- src/Illuminate/Container/Container.php
+++ src/Illuminate/Container/Container.php
@@ -1366,12 +1366,8 @@ class Container implements ArrayAccess, ContainerContract
     protected function fireAfterResolvingAttributeCallbacks(array $attributes, $object)
     {
         foreach ($attributes as $attribute) {
-            if (is_a($attribute->getName(), ContextualAttribute::class, true)) {
-                $instance = $attribute->newInstance();
-
-                if (method_exists($instance, 'after')) {
-                    $instance->after($instance, $object, $this);
-                }
+            if (method_exists($instance = $attribute->newInstance(), 'after')) {
+                $instance->after($instance, $object, $this);
             }
 
             $callbacks = $this->getCallbacksForType(
