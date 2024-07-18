af716bde80cdda9b74c105e97ef4e97c3e5550b4
Caleb White <cdwhite3@pm.me>
2024-07-18 16:15:45 -0500

    fix: Model/JsonResource::toJson should not fail with prior json errors

--- tests/Http/JsonResourceTest.php
+++ tests/Http/JsonResourceTest.php
@@ -5,6 +5,7 @@ namespace Illuminate\Tests\Http;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\MissingValue;
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class JsonResourceTest extends TestCase
@@ -23,5 +24,21 @@ class JsonResourceTest extends TestCase
 
         $this->assertNull($resource->whenAggregated('relation', 'column', 'sum'));
         $this->assertNull($resource->whenCounted('relation'));
+    }
+
+    public function testJsonResourceToJsonSucceedsWithPriorErrors(): void
+    {
+        $model = new class extends Model {};
+
+        $resource = m::mock(JsonResource::class, ['resource' => $model])
+            ->makePartial()
+            ->shouldReceive('jsonSerialize')->once()->andReturn(['foo' => 'bar'])
+            ->getMock();
+
+        // Simulate a JSON error
+        json_decode('{');
+        $this->assertTrue(json_last_error() !== JSON_ERROR_NONE);
+
+        $this->assertSame('{"foo":"bar"}', $resource->toJson(JSON_THROW_ON_ERROR));
     }
 }
