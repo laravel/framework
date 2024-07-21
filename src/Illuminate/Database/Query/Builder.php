f8cc9a4a3508398f1691a788ec2fd069bff31e2e
Xakki <xakki@ya.ru>
2024-07-21 23:13:28 +0300

    Fix Builder - support camelCase attributes
    Use Model::snakeAttributes for where conditions

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Concerns\BuildsQueries;
 use Illuminate\Database\Concerns\ExplainsQueries;
 use Illuminate\Database\ConnectionInterface;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Query\Grammars\Grammar;
 use Illuminate\Database\Query\Processors\Processor;
@@ -2000,7 +2001,7 @@ class Builder implements BuilderContract
         // clause on the query. Then we'll increment the parameter index values.
         $bool = strtolower($connector);
 
-        $this->where(Str::snake($segment), '=', $parameters[$index], $bool);
+        $this->where(lcfirst(Model::$snakeAttributes ? Str::snake($segment) : $segment), '=', $parameters[$index], $bool);
     }
 
     /**
