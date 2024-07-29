ff5cdef8bf683f0831a1cb094f7c5eefae9e1ee2
Anton5360 <72033639+Anton5360@users.noreply.github.com>
2024-07-29 00:52:43 -0600

    [11.x] Add php doc for ServiceProvider bindings and singletons properties (#52298)
    * Add php doc for bindings and singletons magick properties
    
    * Add description

--- src/Illuminate/Support/ServiceProvider.php
+++ src/Illuminate/Support/ServiceProvider.php
@@ -10,6 +10,10 @@ use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Database\Eloquent\Factory as ModelFactory;
 use Illuminate\View\Compilers\BladeCompiler;
 
+/**
+ * @property array<class-string, class-string> $bindings All of the container bindings that should be registered.
+ * @property array<class-string, class-string> $singletons All of the singletons that should be registered.
+ */
 abstract class ServiceProvider
 {
     /**
