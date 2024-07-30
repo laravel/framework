c8a03130a6b15b2cf22c20d12c42a8aa1a49e1dd
Christoph Rumpel <christoph@christoph-rumpel.com>
2024-07-30 16:47:21 +0200

    [11.x] Fix tests in wrong file

--- tests/Integration/Generators/ViewMakeCommandTest.php
+++ tests/Integration/Generators/ViewMakeCommandTest.php
@@ -26,38 +26,4 @@ class ViewMakeCommandTest extends TestCase
         $this->assertFilenameExists('resources/views/foo.blade.php');
         $this->assertFilenameExists('tests/Feature/View/FooTest.php');
     }
-
-    public function testItCanGenerateMailWithNoInitialInput()
-    {
-        $this->artisan('make:mail')
-            ->expectsQuestion('What should the mailable be named?', 'FooMail')
-            ->expectsQuestion('Would you like to create a view?', 'none')
-            ->assertExitCode(0);
-
-        $this->assertFilenameExists('app/Mail/FooMail.php');
-        $this->assertFilenameDoesNotExists('resources/views/mail/foo-mail.blade.php');
-    }
-
-    public function testItCanGenerateMailWithViewWithNoInitialInput()
-    {
-        $this->artisan('make:mail')
-            ->expectsQuestion('What should the mailable be named?', 'MyFooMail')
-            ->expectsQuestion('Would you like to create a view?', 'view')
-            ->assertExitCode(0);
-
-        $this->assertFilenameExists('app/Mail/MyFooMail.php');
-        $this->assertFilenameExists('resources/views/mail/my-foo-mail.blade.php');
-    }
-
-    public function testItCanGenerateMailWithMarkdownViewWithNoInitialInput()
-    {
-        $this->artisan('make:mail')
-
-            ->expectsQuestion('What should the mailable be named?', 'FooMail')
-            ->expectsQuestion('Would you like to create a view?', 'markdown')
-            ->assertExitCode(0);
-
-        $this->assertFilenameExists('app/Mail/MyFooMail.php');
-        $this->assertFilenameExists('resources/views/mail/my-foo-mail.blade.php');
-    }
 }
