2a87f9133118975de3c3b4ec60e2a290dfe7f716
Chris Page <sirhc.page@gmail.com>
2024-08-01 20:08:33 +0100

    Declare exceptions unreportable using the ShouldntReport interface (#52337)
    * Added ability to define exceptions as unreportable using ShouldntReport interface
    
    * Update ShouldntReport.php
    
    * formatting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Foundation/Exceptions/Handler.php
+++ src/Illuminate/Foundation/Exceptions/Handler.php
@@ -13,6 +13,7 @@ use Illuminate\Console\View\Components\BulletList;
 use Illuminate\Console\View\Components\Error;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
+use Illuminate\Contracts\Debug\ShouldntReport;
 use Illuminate\Contracts\Foundation\ExceptionRenderer;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -398,6 +399,10 @@ class Handler implements ExceptionHandlerContract
     protected function shouldntReport(Throwable $e)
     {
         if ($this->withoutDuplicates && ($this->reportedExceptionMap[$e] ?? false)) {
+            return true;
+        }
+
+        if ($e instanceof ShouldntReport) {
             return true;
         }
 
