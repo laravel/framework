630df64539f438c16e912712c8a835a3fccb0187
taylorotwell <taylorotwell@users.noreply.github.com>
2024-08-01 20:30:35 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/Crypt.php
+++ src/Illuminate/Support/Facades/Crypt.php
@@ -3,6 +3,17 @@
 namespace Illuminate\Support\Facades;
 
 /**
+ * @method static bool supported(string $key, string $cipher)
+ * @method static string generateKey(string $cipher)
+ * @method static string encrypt(mixed $value, bool $serialize = true)
+ * @method static string encryptString(string $value)
+ * @method static mixed decrypt(string $payload, bool $unserialize = true)
+ * @method static string decryptString(string $payload)
+ * @method static string getKey()
+ * @method static array getAllKeys()
+ * @method static array getPreviousKeys()
+ * @method static \Illuminate\Encryption\Encrypter previousKeys(array $keys)
+ *
  * @see \Illuminate\Encryption\Encrypter
  */
 class Crypt extends Facade
--- src/Illuminate/Support/Facades/Pipeline.php
+++ src/Illuminate/Support/Facades/Pipeline.php
@@ -3,6 +3,16 @@
 namespace Illuminate\Support\Facades;
 
 /**
+ * @method static \Illuminate\Pipeline\Pipeline send(mixed $passable)
+ * @method static \Illuminate\Pipeline\Pipeline through(array|mixed $pipes)
+ * @method static \Illuminate\Pipeline\Pipeline pipe(array|mixed $pipes)
+ * @method static \Illuminate\Pipeline\Pipeline via(string $method)
+ * @method static mixed then(\Closure $destination)
+ * @method static mixed thenReturn()
+ * @method static \Illuminate\Pipeline\Pipeline setContainer(\Illuminate\Contracts\Container\Container $container)
+ * @method static \Illuminate\Pipeline\Pipeline|mixed when(\Closure|mixed|null $value = null, callable|null $callback = null, callable|null $default = null)
+ * @method static \Illuminate\Pipeline\Pipeline|mixed unless(\Closure|mixed|null $value = null, callable|null $callback = null, callable|null $default = null)
+ *
  * @see \Illuminate\Pipeline\Pipeline
  */
 class Pipeline extends Facade
