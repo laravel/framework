ce66665a0763ce4f9fd672cfcc6f639d15c3e44b
Thomas Liu <thomasliu1996@gmail.com>
2024-08-06 00:38:48 -0400

    add UniqueJobDispatchingTest

---
+++ tests/Integration/Queue/UniqueJobDispatchingTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Queue;
+
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Exceptions\JobDispatchedException;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Queue;
+use Orchestra\Testbench\Attributes\WithMigration;
+
+#[WithMigration]
+#[WithMigration('cache')]
+#[WithMigration('queue')]
+class UniqueJobDispatchingTest extends QueueTestCase
+{
+    protected function defineEnvironment($app)
+    {
+        parent::defineEnvironment($app);
+
+        $app['config']->set('cache.default', 'database');
+    }
+    public function testSubsequentUniqueJobDispatchAreIgnored()
+    {
+        $this->markTestSkippedWhenUsingSyncQueueDriver();
+
+        Queue::fake();
+
+        ThrowableUniqueJob::dispatch();
+        ThrowableUniqueJob::dispatch();
+
+        Queue::assertPushed(ThrowableUniqueJob::class);
+    }
+
+    public function testSubsequentUniqueJobDispatchCanThrow()
+    {
+        $this->markTestSkippedWhenUsingSyncQueueDriver();
+
+        Queue::fake();
+        $this->expectException(JobDispatchedException::class);
+
+        ThrowableUniqueJob::dispatch()->throw();
+        ThrowableUniqueJob::dispatch()->throw();
+
+        Queue::assertPushed(ThrowableUniqueJob::class);
+    }
+
+    public function testSubsequentUniqueJobDispatchCanThrowWithCallback()
+    {
+        $this->markTestSkippedWhenUsingSyncQueueDriver();
+
+        Queue::fake();
+        $this->expectException(JobDispatchedException::class);
+        $value = 0;
+
+        ThrowableUniqueJob::dispatch()->throw(fn () => $value = 1);
+        ThrowableUniqueJob::dispatch()->throw(fn () => $value = 2);
+
+        Queue::assertPushed(ThrowableUniqueJob::class);
+        $this->assertEquals(2, $value);
+    }
+}
+
+class ThrowableUniqueJob implements ShouldQueue, ShouldBeUnique
+{
+    use Queueable;
+}
