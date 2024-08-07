0138d780cb6ba829ab6e312b593a31d1d091736c
Caleb White <cdwhite3@pm.me>
2024-08-07 16:07:46 -0500

    fix: prevent casting empty string to array from triggering json error (#52415)

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -1271,7 +1271,11 @@ trait HasAttributes
      */
     public function fromJson($value, $asObject = false)
     {
-        return Json::decode($value ?? '', ! $asObject);
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        return Json::decode($value, ! $asObject);
     }
 
     /**
