860157cebe821b8e899331705a210fb26d8ab719
hadees-ahmed <hadeesahmed@yahoo.com>
2024-08-08 15:08:38 +0100

    test hash generation is same both way

---
+++ tests/Hashing/HashTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Illuminate\Tests\Hashing;
+
+use Carbon\Carbon;
+use Orchestra\Testbench\TestCase;
+
+class HashTest extends TestCase
+{
+    public function testSha1HashIsConsistentForSameInput()
+    {
+        $email = 'taylor@laravel.com';
+        $timeStamp = Carbon::now();
+
+        $firstHash = sha1($email . $timeStamp);
+        $secondHash= sha1($email . $timeStamp);
+
+        $this->assertTrue(hash_equals($firstHash, $secondHash));
+    }
+}
