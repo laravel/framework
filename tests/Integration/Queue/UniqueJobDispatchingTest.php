c970ed4608caf2a53a725c776bcf20258bafba7a
Thomas Liu <164444850+tiramisuliu@users.noreply.github.com>
2024-08-10 17:01:22 -0400

    use default test settings from the parent class

--- tests/Integration/Queue/UniqueJobDispatchingTest.php
+++ tests/Integration/Queue/UniqueJobDispatchingTest.php
@@ -7,20 +7,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Exceptions\JobDispatchedException;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Queue;
-use Orchestra\Testbench\Attributes\WithMigration;
 
-#[WithMigration]
-#[WithMigration('cache')]
-#[WithMigration('queue')]
 class UniqueJobDispatchingTest extends QueueTestCase
 {
-    protected function defineEnvironment($app)
-    {
-        parent::defineEnvironment($app);
-
-        $app['config']->set('cache.default', 'database');
-    }
-
     public function testSubsequentUniqueJobDispatchAreIgnored()
     {
         $this->markTestSkippedWhenUsingSyncQueueDriver();
