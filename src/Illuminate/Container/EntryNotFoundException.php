ff2000ca292bd0c8f8b82f8a9690e84028993a23
AJ Meireles <alvaro.meireles@live.com>
2024-08-19 18:46:23 -0300

    adding exception description message for EntryNotFoundException

--- src/Illuminate/Container/EntryNotFoundException.php
+++ src/Illuminate/Container/EntryNotFoundException.php
@@ -7,5 +7,15 @@ use Psr\Container\NotFoundExceptionInterface;
 
 class EntryNotFoundException extends Exception implements NotFoundExceptionInterface
 {
-    //
+    /**
+     * Create a new exception instance.
+     *
+     * @param  string  $id
+     * @param  int  $code
+     * @return void
+     */
+    public function __construct($id, $code, $previous)
+    {
+        parent::__construct("No container bind defined for: {$id}.", $code, $previous);
+    }
 }
