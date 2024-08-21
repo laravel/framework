c6f0ca6500b3d9db8554cc10708bbd2c756ad7d7
ju5t <ju5t@users.noreply.github.com>
2024-08-21 11:01:06 +0200

    chore: remove duplicate parentheses
    Co-authored-by: Julius Kiekbusch <contact@julius-kiekbusch.de>

--- src/Illuminate/Database/Connectors/Connector.php
+++ src/Illuminate/Database/Connectors/Connector.php
@@ -62,7 +62,7 @@ class Connector
      */
     protected function createPdoConnection($dsn, $username, $password, $options)
     {
-        return (version_compare(phpversion(), '8.4.0', '<'))
+        return version_compare(phpversion(), '8.4.0', '<')
             ? new PDO($dsn, $username, $password, $options)
             : PDO::connect($dsn, $username, $password, $options);
     }
