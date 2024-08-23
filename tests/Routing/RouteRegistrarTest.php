86b4dd9cd935667c0be5995e1334920f86cc4fae
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2024-08-23 14:13:03 +0800

    style

--- tests/Routing/RouteRegistrarTest.php
+++ tests/Routing/RouteRegistrarTest.php
@@ -1115,7 +1115,7 @@ class RouteRegistrarTest extends TestCase
 
     public function testCanSetRouteNameUsingStringBackedEnum()
     {
-        $this->router->name(RouteNameEnum::UserIndex)->get('users', fn() => 'all-users');
+        $this->router->name(RouteNameEnum::UserIndex)->get('users', fn () => 'all-users');
 
         $this->assertSame('users.index', $this->getRoute()->getName());
     }
@@ -1124,12 +1124,12 @@ class RouteRegistrarTest extends TestCase
     {
         $this->expectExceptionObject(new \InvalidArgumentException('Attribute [name] expects a string backed enum.'));
 
-        $this->router->name(IntegerEnum::One)->get('users', fn() => 'all-users');
+        $this->router->name(IntegerEnum::One)->get('users', fn () => 'all-users');
     }
 
     public function testCanSetRouteDomainUsingStringBackedEnum()
     {
-        $this->router->domain(RouteDomainEnum::DashboardDomain)->get('users', fn() => 'all-users');
+        $this->router->domain(RouteDomainEnum::DashboardDomain)->get('users', fn () => 'all-users');
 
         $this->assertSame('dashboard.myapp.com', $this->getRoute()->getDomain());
     }
@@ -1138,7 +1138,7 @@ class RouteRegistrarTest extends TestCase
     {
         $this->expectExceptionObject(new \InvalidArgumentException('Attribute [domain] expects a string backed enum.'));
 
-        $this->router->domain(IntegerEnum::One)->get('users', fn() => 'all-users');
+        $this->router->domain(IntegerEnum::One)->get('users', fn () => 'all-users');
     }
 
     public function testPushMiddlewareToGroup()
