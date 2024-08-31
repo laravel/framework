7d7071cd13a886fe9ba2f7b01a153f5db8256a56
Ashot Gharakeshishyan <a.gharakeshishyan@aico.swiss>
2024-08-31 13:01:33 +0400

    add @log directive

--- tests/Testing/BladeLogTest.php
+++ tests/Testing/BladeLogTest.php
@@ -9,7 +9,6 @@ class BladeLogTest extends AbstractBladeTestCase
     public function testLogAreCompiled()
     {
         $this->assertSame("<?php Log::info('User authenticated.'); ?>", $this->compiler->compileString('@log(\'User authenticated.\')'));
-        $this->assertSame("<?php Log::error('Error occurred.'); ?>", $this->compiler->compileString('@log(\'Error occurred.\', \'error\')'));
         $this->assertSame("<?php Log::debug('Debug occurred.'); ?>", $this->compiler->compileString('@log(\'Debug occurred.\', \'debug\')'));
         $this->assertSame("<?php Log::alert('Alert occurred.'); ?>", $this->compiler->compileString('@log(\'Alert occurred.\', \'alert\')'));
         $this->assertSame("<?php Log::critical('Critical occurred.'); ?>", $this->compiler->compileString('@log(\'Critical occurred.\', \'critical\')'));
