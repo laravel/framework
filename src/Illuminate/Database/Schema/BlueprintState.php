85b7ca8414f9486c37b0a5f87d81fb99aad17ddc
Talgat Hairullov <incrize@gmail.com>
2024-09-03 17:32:15 +0400

    [11.x] SQLite Error: "General error: 1 no such table" after adding a foreign key when using a table prefix. (#52578)
    * Added removal of the table prefix when defining a foreign key.
    
    * Code style fixed.

--- src/Illuminate/Database/Schema/BlueprintState.php
+++ src/Illuminate/Database/Schema/BlueprintState.php
@@ -105,7 +105,7 @@ class BlueprintState
 
         $this->foreignKeys = collect($schema->getForeignKeys($table))->map(fn ($foreignKey) => new ForeignKeyDefinition([
             'columns' => $foreignKey['columns'],
-            'on' => $foreignKey['foreign_table'],
+            'on' => $this->withoutTablePrefix($foreignKey['foreign_table']),
             'references' => $foreignKey['foreign_columns'],
             'onUpdate' => $foreignKey['on_update'],
             'onDelete' => $foreignKey['on_delete'],
@@ -250,5 +250,20 @@ class BlueprintState
 
                 break;
         }
+    }
+
+    /**
+     * Remove the table prefix from a table name, if it exists.
+     *
+     * @param  string  $table
+     * @return string
+     */
+    protected function withoutTablePrefix(string $table)
+    {
+        $prefix = $this->connection->getTablePrefix();
+
+        return str_starts_with($table, $prefix)
+            ? substr($table, strlen($prefix))
+            : $table;
     }
 }
