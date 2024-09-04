084178895fa42cddc90c01f2b25a0e1bd0315829
Julius Kiekbusch <contact@julius-kiekbusch.de>
2024-09-04 16:06:16 +0200

    [DB-Test] Assert different number of types in pgsql 10 (#52638)
    * Find out which types are not counted
    
    * dump types
    
    * Bypass count check for pgsql 10

--- tests/Integration/Database/SchemaBuilderTest.php
+++ tests/Integration/Database/SchemaBuilderTest.php
@@ -355,7 +355,12 @@ class SchemaBuilderTest extends DatabaseTestCase
 
         $types = Schema::getTypes();
 
-        $this->assertCount(13, $types);
+        if (version_compare($this->getConnection()->getServerVersion(), '14.0', '<')) {
+            $this->assertCount(10, $types);
+        } else {
+            $this->assertCount(13, $types);
+        }
+
         $this->assertTrue(collect($types)->contains(fn ($type) => $type['name'] === 'pseudo_foo' && $type['type'] === 'pseudo' && ! $type['implicit']));
         $this->assertTrue(collect($types)->contains(fn ($type) => $type['name'] === 'comp_foo' && $type['type'] === 'composite' && ! $type['implicit']));
         $this->assertTrue(collect($types)->contains(fn ($type) => $type['name'] === 'enum_foo' && $type['type'] === 'enum' && ! $type['implicit']));
