e2ad1df44cbc9bf0b22e016d550832a9e1c93b78
Hafez Divandari <hafezdivandari@gmail.com>
2024-09-06 16:37:50 +0330

    formatting

--- tests/Integration/Database/DatabaseSchemaBuilderTest.php
+++ tests/Integration/Database/DatabaseSchemaBuilderTest.php
@@ -98,16 +98,16 @@ class DatabaseSchemaBuilderTest extends TestCase
 
         $foreignKeys = collect($schema->getForeignKeys('table2'));
 
-        $this->assertTrue($foreignKeys->contains(fn ($fk) =>
-            $fk['foreign_table'] === 'example_table1' &&
-            $fk['foreign_columns'] === ['id'] &&
-            $fk['columns'] === ['author_id'])
+        $this->assertTrue($foreignKeys->contains(
+            fn ($fk) => $fk['foreign_table'] === 'example_table1' &&
+                $fk['foreign_columns'] === ['id'] &&
+                $fk['columns'] === ['author_id'])
         );
 
-        $this->assertTrue($foreignKeys->contains(fn ($fk) =>
-            $fk['foreign_table'] === 'example_table1' &&
-            $fk['foreign_columns'] === ['id'] &&
-            $fk['columns'] === ['moderator_id'])
+        $this->assertTrue($foreignKeys->contains(
+            fn ($fk) => $fk['foreign_table'] === 'example_table1' &&
+                $fk['foreign_columns'] === ['id'] &&
+                $fk['columns'] === ['moderator_id'])
         );
     }
 
@@ -122,17 +122,16 @@ class DatabaseSchemaBuilderTest extends TestCase
             $table->foreignId('item_id')->nullable()->constrained('items');
         });
 
-        $this->assertTrue(collect(Schema::getForeignKeys('items'))->contains(fn ($fk) =>
-            $fk['foreign_table'] === 'items' &&
-            $fk['foreign_columns'] === ['id'] &&
-            $fk['columns'] === ['item_id'])
-        );
+        $this->assertTrue(collect(Schema::getForeignKeys('items'))->contains(
+            fn ($fk) => $fk['foreign_table'] === 'items' &&
+                $fk['foreign_columns'] === ['id'] &&
+                $fk['columns'] === ['item_id']
+        ));
 
         $columns = Schema::getColumns('items');
 
-        $this->assertTrue(collect($columns)->contains(fn ($column) =>
-            $column['name'] === 'flags' &&
-            $column['default'] === 'JSON_ARRAY()'
+        $this->assertTrue(collect($columns)->contains(
+            fn ($column) => $column['name'] === 'flags' && $column['default'] === 'JSON_ARRAY()'
         ));
 
         $this->assertTrue(collect($columns)->contains(fn ($column) => $column['name'] === 'item_id' && $column['nullable']));
