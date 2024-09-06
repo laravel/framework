04aaa295d5aad21749a660e4f78a90326f48b903
Cam Kemshal-Bell <cam@iterated.tech>
2024-09-06 23:11:11 +1000

    fix(dimensions): build the validation rules and methods for data & validator aware contracts

--- src/Illuminate/Validation/Rules/Dimensions.php
+++ src/Illuminate/Validation/Rules/Dimensions.php
@@ -5,8 +5,12 @@ namespace Illuminate\Validation\Rules;
 use Illuminate\Contracts\Validation\DataAwareRule;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Contracts\Validation\ValidatorAwareRule;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\Macroable;
+use Illuminate\Testing\Exceptions\InvalidArgumentException;
 
 class Dimensions implements Rule, DataAwareRule, ValidatorAwareRule
 {
@@ -286,16 +290,164 @@ class Dimensions implements Rule, DataAwareRule, ValidatorAwareRule
     /**
      * Build the array of underlying validation rules based on the current state.
      *
-     * @return string
+     * @return array
      */
-    public function __toString()
+    protected function buildValidationRules()
     {
-        $result = '';
+        $rules = [];
 
-        foreach ($this->constraints as $key => $value) {
-            $result .= "$key=$value,";
+        if ($this->width) {
+            $rules[] = "width:{$this->width}";
         }
 
-        return 'dimensions:'.substr($result, 0, -1);
+        if ($this->minWidth || $this->maxWidth) {
+            $rules[] = match (true) {
+                is_null($this->maxWidth) => "min_width:{$this->minWidth}",
+                is_null($this->minWidth) => "max_width:{$this->maxWidth}",
+                $this->minWidth !== $this->maxWidth => "width_between:{$this->minWidth},{$this->maxWidth}",
+                default => null,
+            };
+        }
+
+        if ($this->height) {
+            $rules[] = "height:{$this->height}";
+        }
+
+        if ($this->minHeight || $this->maxHeight) {
+            $rules[] = match (true) {
+                is_null($this->maxHeight) => "min_height:{$this->minHeight}",
+                is_null($this->minHeight) => "max_height:{$this->maxHeight}",
+                $this->minHeight !== $this->maxHeight => "height_between:{$this->minHeight},{$this->maxHeight}",
+                default => null,
+            };
+        }
+
+        if ($this->ratio) {
+            $rules[] = "ratio:{$this->ratio}";
+        }
+
+        if ($this->minRatio || $this->maxRatio) {
+            $rules[] = match (true) {
+                is_null($this->maxRatio) => "min_ratio:{$this->minRatio}",
+                is_null($this->minRatio) => "max_ratio:{$this->maxRatio}",
+                $this->minRatio !== $this->maxRatio => "ratio_between:{$this->minRatio},{$this->maxRatio}",
+                default => null,
+            };
+        }
+
+        return array_filter($rules);
+    }
+
+    /**
+     * Set the default callback to be used for determining the dimensions rule.
+     *
+     * If no arguments are passed, the default dimension rule configuration will be returned.
+     *
+     * @param  static|callable|null  $callback
+     * @return static|null
+     */
+    public static function defaults($callback = null)
+    {
+        if ($callback === null) {
+            return static::default();
+        }
+
+        if (! is_callable($callback) && ! $callback instanceof static) {
+            throw new InvalidArgumentException('The given callback should be callable or an instance of '.static::class);
+        }
+
+        static::$defaultCallback = $callback;
+    }
+
+    /**
+     * Get the default configuration of the dimensions rule.
+     *
+     * @return static
+     */
+    public static function default()
+    {
+        $file = is_callable(static::$defaultCallback)
+            ? call_user_func(static::$defaultCallback)
+            : static::$defaultCallback;
+
+        return $file instanceof Rule ? $file : new self();
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $this->messages = [];
+
+        $validator = Validator::make(
+            $this->data,
+            [$attribute => $this->buildValidationRules()],
+            $this->validator->customMessages,
+            $this->validator->customAttributes
+        );
+
+        if ($validator->fails()) {
+            return $this->fail($validator->messages()->all());
+        }
+
+        return true;
+    }
+
+    /**
+     * Adds the given failures, and return false.
+     *
+     * @param  array|string  $messages
+     * @return bool
+     */
+    protected function fail($messages)
+    {
+        $messages = collect(Arr::wrap($messages))->map(
+            fn($message) => $this->validator->getTranslator()->get($message)
+        )->all();
+
+        $this->messages = array_merge($this->messages, $messages);
+
+        return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return array
+     */
+    public function message()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * Set the current validator.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return $this
+     */
+    public function setValidator($validator)
+    {
+        $this->validator = $validator;
+
+        return $this;
+    }
+
+    /**
+     * Set the current data under validation.
+     *
+     * @param  array  $data
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
     }
 }
