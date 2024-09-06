2714be744062fba49b2ea39a19e835166216ba86
Hafez Divandari <hafezdivandari@gmail.com>
2024-09-06 17:08:22 +0330

    [11.x] Fix alter table expressions on SQLite (#52678)
    * Revert "[11.x] SQLite Error: "General error: 1 no such table" after adding a foreign key when using a table prefix. (#52578)"
    
    This reverts commit 85b7ca8414f9486c37b0a5f87d81fb99aad17ddc.
    
    * fixes #52578
    
    * fixes #52655
    
    * formatting

--- src/Illuminate/Database/Schema/BlueprintState.php
+++ src/Illuminate/Database/Schema/BlueprintState.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Connection;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Grammars\Grammar;
 use Illuminate\Support\Fluent;
+use Illuminate\Support\Str;
 
 class BlueprintState
 {
@@ -80,7 +81,7 @@ class BlueprintState
             'type' => $column['type_name'],
             'full_type_definition' => $column['type'],
             'nullable' => $column['nullable'],
-            'default' => is_null($column['default']) ? null : new Expression($column['default']),
+            'default' => is_null($column['default']) ? null : new Expression(Str::wrap($column['default'], '(', ')')),
             'autoIncrement' => $column['auto_increment'],
             'collation' => $column['collation'],
             'comment' => $column['comment'],
@@ -105,7 +106,7 @@ class BlueprintState
 
         $this->foreignKeys = collect($schema->getForeignKeys($table))->map(fn ($foreignKey) => new ForeignKeyDefinition([
             'columns' => $foreignKey['columns'],
-            'on' => $this->withoutTablePrefix($foreignKey['foreign_table']),
+            'on' => new Expression($foreignKey['foreign_table']),
             'references' => $foreignKey['foreign_columns'],
             'onUpdate' => $foreignKey['on_update'],
             'onDelete' => $foreignKey['on_delete'],
@@ -250,20 +251,5 @@ class BlueprintState
 
                 break;
         }
-    }
-
-    /**
-     * Remove the table prefix from a table name, if it exists.
-     *
-     * @param  string  $table
-     * @return string
-     */
-    protected function withoutTablePrefix(string $table)
-    {
-        $prefix = $this->connection->getTablePrefix();
-
-        return str_starts_with($table, $prefix)
-            ? substr($table, strlen($prefix))
-            : $table;
     }
 }
