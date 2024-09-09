9587bb22db4fb235eb154a156688b6a421267606
Volodya Kurshudyan <70023120+xurshudyan@users.noreply.github.com>
2024-09-09 17:56:48 +0400

    Refactor getInstance method to use null coalescing assignment (#52693)
    Co-authored-by: Xurshudyan <v.khurshudyan@aico.swiss>

--- src/Illuminate/Container/Container.php
+++ src/Illuminate/Container/Container.php
@@ -1519,11 +1519,7 @@ class Container implements ArrayAccess, ContainerContract
      */
     public static function getInstance()
     {
-        if (is_null(static::$instance)) {
-            static::$instance = new static;
-        }
-
-        return static::$instance;
+        return static::$instance ??= new static;
     }
 
     /**
