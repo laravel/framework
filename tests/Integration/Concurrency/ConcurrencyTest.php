4426a898dcb1915e96376fc1b9d72db96480367f
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-09 23:31:51 +0800

    Concurrency Improvements
    1. Add a working tests for Concurrency
    2. Update composer.json to include replace `illuminate/concurrency`
    3. Update PHP versions
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Concurrency/ConcurrencyTest.php
+++ tests/Integration/Concurrency/ConcurrencyTest.php
@@ -4,17 +4,35 @@ namespace Illuminate\Tests\Integration\Console;
 
 use Illuminate\Support\Facades\Concurrency;
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\RequiresOperatingSystem;
 
+#[RequiresOperatingSystem('Linux|DAR')]
 class ConcurrencyTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        $this->defineStashRoutes(<<<PHP
+<?php
+use Illuminate\Support\Facades\Concurrency;
+use Illuminate\Support\Facades\Route;
+
+Route::any('/concurrency', function () {
+    return Concurrency::run([
+        fn () => 1 + 1,
+        fn () => 2 + 2,
+    ]);
+});
+PHP);
+
+        parent::setUp();
+    }
+
     public function testWorkCanBeDistributed()
     {
-        $this->markTestSkipped('Todo...');
+        $response = $this->get('concurrency')
+            ->assertOk();
 
-        [$first, $second] = Concurrency::run([
-            fn () => 1 + 1,
-            fn () => 2 + 2,
-        ]);
+        [$first, $second] = $response->original;
 
         $this->assertEquals(2, $first);
         $this->assertEquals(4, $second);
