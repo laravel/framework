d71f4ee66a9bce66ca36a8e74671f8f0366e50c2
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-10 00:21:16 +0800

    Concurrency Improvements (#52713)
    * Concurrency Improvements
    
    1. Add a working tests for Concurrency
    2. Update composer.json to include replace `illuminate/concurrency`
    3. Update PHP versions
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Apply fixes from StyleCI
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Illuminate/Concurrency/ForkDriver.php
+++ src/Illuminate/Concurrency/ForkDriver.php
@@ -14,6 +14,7 @@ class ForkDriver
      */
     public function run(Closure|array $tasks): array
     {
+        /** @phpstan-ignore class.notFound */
         return Fork::new()->run(...Arr::wrap($tasks));
     }
 
