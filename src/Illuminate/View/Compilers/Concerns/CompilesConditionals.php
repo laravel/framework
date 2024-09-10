a214523cb79566daf1621318b298e8c8890b9ca6
Milwad <milwad.dev@gmail.com>
2024-09-10 19:12:49 +0330

    add `match` directive

--- src/Illuminate/View/Compilers/Concerns/CompilesConditionals.php
+++ src/Illuminate/View/Compilers/Concerns/CompilesConditionals.php
@@ -283,6 +283,21 @@ trait CompilesConditionals
     }
 
     /**
+     * Compile the match statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileMatch($expression)
+    {
+        $parts = explode(',', $this->stripParentheses($expression), 2);
+        $parts[1] = substr($parts[1], 2);
+        $parts[1] = substr_replace($parts[1], "", -1);
+
+        return "<?php echo match({$parts[0]}) { {$parts[1]} }; ?>";
+    }
+
+    /**
      * Compile a once block into valid PHP.
      *
      * @param  string|null  $id
