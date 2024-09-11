2d55f4b016e84bb4edca3e28e7bcb717e9400099
AJ Meireles <alvaro.meireles@live.com>
2024-09-11 19:11:44 -0300

    [Feat]: Add Doc Block for Concurrency Facade

--- src/Illuminate/Support/Facades/Concurrency.php
+++ src/Illuminate/Support/Facades/Concurrency.php
@@ -5,6 +5,13 @@ namespace Illuminate\Support\Facades;
 use Illuminate\Concurrency\ConcurrencyManager;
 
 /**
+ * @method static \Illuminate\Foundation\Defer\DeferredCallback defer(\Closure|array $tasks)
+ * @method static \Illuminate\Contracts\Concurrency\Driver driver(string|null $name = null)
+ * @method static string getDefaultInstance()
+ * @method static void getInstanceConfig(string $name)
+ * @method static void setDefaultInstance(string $name)
+ * @method static array run(\Closure|array $tasks)
+ *
  * @see \Illuminate\Concurrency\ConcurrencyManager
  */
 class Concurrency extends Facade
