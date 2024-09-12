d00642feed0a82f331124a361116a55160c9d268
JaguarJack <njphper@gmail.com>
2024-09-12 08:20:13 +0800

    Add @method annotations to Concurrency facade for IDE support

--- src/Illuminate/Support/Facades/Concurrency.php
+++ src/Illuminate/Support/Facades/Concurrency.php
@@ -3,8 +3,13 @@
 namespace Illuminate\Support\Facades;
 
 use Illuminate\Concurrency\ConcurrencyManager;
+use Illuminate\Foundation\Defer\DeferredCallback;
 
 /**
+ *
+ * @method static array run(\Closure|array $tasks)
+ * @method static DeferredCallback defer(\Closure|array $tasks)
+ *
  * @see \Illuminate\Concurrency\ConcurrencyManager
  */
 class Concurrency extends Facade
