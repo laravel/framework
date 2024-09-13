5ab08ed88312b90dd23c62622b3872a82af9020a
Dan Matthews <dan@danmatthews.me>
2024-09-13 16:53:08 +0100

    Syntax fix.

--- tests/View/Blade/BladeAttributesTest.php
+++ tests/View/Blade/BladeAttributesTest.php
@@ -8,7 +8,6 @@ class BladeAttributesTest extends AbstractBladeTestCase
     {
         $string = "<span @attributes(['class=\"mt-1\"' => true, 'disabled=\"disabled\"' => false, 'role=\"button\"' ])></span>";
         $expected = "<span class=\"<?php echo \Illuminate\Support\Arr::toCssClasses(['font-bold', 'mt-4', 'ml-2' => true, 'mr-2' => false]); ?>\"></span>";
-
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 }
