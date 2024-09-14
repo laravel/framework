d82256e39251b88cede3a5fd2da3d3f676733ff2
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-14 14:50:52 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Process/ExecutableFinderDecorator.php
+++ src/Illuminate/Process/ExecutableFinderDecorator.php
@@ -2,10 +2,65 @@
 
 namespace Illuminate\Process;
 
+use Symfony\Component\Process\ExecutableFinder;
+
+use function Illuminate\Filesystem\join_paths;
+
 class ExecutableFinderDecorator
 {
-    public function __construct(protected ExecutableFinder $decorator)
+    /**
+     * The Symfony's Executable Finder implementation.
+     *
+     * @var Symfony\Component\Process\PhpExecutableFinder
+     */
+    private ExecutableFinder $decorator;
+
+    /**
+     * Create a new Executable Finder Decorator.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        //
+        $this->decorator = new ExecutableFinder();
+    }
+
+    /**
+     * Replaces default suffixes of executable.
+     *
+     * @param  array  $suffixes
+     * @return void
+     */
+    public function setSuffixes(array $suffixes): void
+    {
+        $this->decorator->setSuffixes($suffixes);
+    }
+
+    /**
+     * Adds new possible suffix to check for executable.
+     *
+     * @param  string  $suffix
+     * @return void
+     */
+    public function addSuffix(string $suffix): void
+    {
+        $this->decorator->addSuffix($suffix);
+    }
+
+    /**
+     * Finds an executable by name.
+     *
+     * @param  string  $name
+     * @param  string|null  $default
+     * @param  array  $extraDirs
+     * @return string|null
+     */
+    public function find(string $name, ?string $default = null, array $extraDirs = []): ?string
+    {
+        if ($herdPath = getenv('HERD_HOME')) {
+            $extraDirs[] = join_paths($herdPath, 'bin');
+        }
+
+        return $this->decorator->find($name, $default, $extraDirs);
     }
 }
--- src/Illuminate/Process/PhpExecutableFinder.php
+++ src/Illuminate/Process/PhpExecutableFinder.php
@@ -6,8 +6,16 @@ use Symfony\Component\Process\PhpExecutableFinder as SymfonyPhpExecutableFinder;
 
 class PhpExecutableFinder extends SymfonyPhpExecutableFinder
 {
+    /**
+     * The Symfony's Executable Finder Decorator implementation.
+     *
+     * @var Symfony\Component\Process\PhpExecutableFinder
+     */
     private ExecutableFinderDecorator $executableFinder;
 
+    /**
+     * Construct a new PHP Executable Finder.
+     */
     public function __construct()
     {
         $this->executableFinder = new ExecutableFinderDecorator();
