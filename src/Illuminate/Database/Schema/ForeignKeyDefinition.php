994b9bf390d115ade272415b2a9357b173bf8fb8
gisoo <nasrgisu@gmail.com>
2024-09-14 23:38:46 +0330

    add nullOnUpdate() method to ForeignKeyDefinition

--- src/Illuminate/Database/Schema/ForeignKeyDefinition.php
+++ src/Illuminate/Database/Schema/ForeignKeyDefinition.php
@@ -45,6 +45,16 @@ class ForeignKeyDefinition extends Fluent
     }
 
     /**
+     * Indicate that updates should set the foreign key value to null.
+     *
+     * @return $this
+     */
+    public function nullOnUpdate()
+    {
+        return $this->onUpdate('set null');
+    }
+
+    /**
      * Indicate that deletes should cascade.
      *
      * @return $this
