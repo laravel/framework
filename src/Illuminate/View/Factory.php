6143186f3e3b7dbdece6ccb0823c09495aec4492
Noboru Shiroiwa <14008307+nshiro@users.noreply.github.com>
2024-09-16 23:20:17 +0900

    [11.x] Add prependLocation method to View Factory (#52806)

--- src/Illuminate/View/Factory.php
+++ src/Illuminate/View/Factory.php
@@ -425,6 +425,17 @@ class Factory implements FactoryContract
     }
 
     /**
+     * Prepend a location to the array of view locations.
+     *
+     * @param  string  $location
+     * @return void
+     */
+    public function prependLocation($location)
+    {
+        $this->finder->prependLocation($location);
+    }
+
+    /**
      * Add a new namespace to the loader.
      *
      * @param  string  $namespace
