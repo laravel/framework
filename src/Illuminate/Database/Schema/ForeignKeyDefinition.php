00bad59faff615743134a5583e2c03e3dbdf3a6f
Taylor Otwell <taylor@laravel.com>
2024-09-16 09:25:20 -0500

    Update ForeignKeyDefinition.php

--- src/Illuminate/Database/Schema/ForeignKeyDefinition.php
+++ src/Illuminate/Database/Schema/ForeignKeyDefinition.php
@@ -35,16 +35,6 @@ class ForeignKeyDefinition extends Fluent
     }
 
     /**
-     * Indicate that updates should have "no action".
-     *
-     * @return $this
-     */
-    public function noActionOnUpdate()
-    {
-        return $this->onUpdate('no action');
-    }
-
-    /**
      * Indicate that updates should set the foreign key value to null.
      *
      * @return $this
@@ -52,6 +42,16 @@ class ForeignKeyDefinition extends Fluent
     public function nullOnUpdate()
     {
         return $this->onUpdate('set null');
+    }
+
+    /**
+     * Indicate that updates should have "no action".
+     *
+     * @return $this
+     */
+    public function noActionOnUpdate()
+    {
+        return $this->onUpdate('no action');
     }
 
     /**
