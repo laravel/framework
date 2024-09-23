4f6cca39e0bbe36b448f809f1761af11f3dceab5
Mior Muhammad Zaki <crynobone@gmail.com>
2024-09-23 12:24:09 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Database/SqlServer/SqlServerTestCase.php
+++ tests/Integration/Database/SqlServer/SqlServerTestCase.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Tests\Integration\Database\SqlServer;
 
 use Illuminate\Tests\Integration\Database\DatabaseTestCase;
+use Orchestra\Testbench\Attributes\RequiresDatabase;
 
 #[RequiresDatabase('sqlsrv', default: true)]
 abstract class SqlServerTestCase extends DatabaseTestCase
