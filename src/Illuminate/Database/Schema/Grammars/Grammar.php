257bf76b7131d189940c4bb571d93a1c08be8e52
Flavio Schoute <flavio.schoute@gmail.com>
2024-09-25 18:06:56 +0200

    Fixed dock block on typeVector method in Grammer

--- src/Illuminate/Database/Schema/Grammars/Grammar.php
+++ src/Illuminate/Database/Schema/Grammars/Grammar.php
@@ -74,7 +74,8 @@ abstract class Grammar extends BaseGrammar
      */
     public function compileRenameColumn(Blueprint $blueprint, Fluent $command, Connection $connection)
     {
-        return sprintf('alter table %s rename column %s to %s',
+        return sprintf(
+            'alter table %s rename column %s to %s',
             $this->wrapTable($blueprint),
             $this->wrap($command->from),
             $this->wrap($command->to)
@@ -136,7 +137,8 @@ abstract class Grammar extends BaseGrammar
         // We need to prepare several of the elements of the foreign key definition
         // before we can create the SQL, such as wrapping the tables and convert
         // an array of columns to comma-delimited strings for the SQL queries.
-        $sql = sprintf('alter table %s add constraint %s ',
+        $sql = sprintf(
+            'alter table %s add constraint %s ',
             $this->wrapTable($blueprint),
             $this->wrap($command->index)
         );
@@ -144,7 +146,8 @@ abstract class Grammar extends BaseGrammar
         // Once we have the initial portion of the SQL statement we will add on the
         // key name, table name, and referenced columns. These will complete the
         // main portion of the SQL statement and this SQL will almost be done.
-        $sql .= sprintf('foreign key (%s) references %s (%s)',
+        $sql .= sprintf(
+            'foreign key (%s) references %s (%s)',
             $this->columnize($command->columns),
             $this->wrapTable($command->on),
             $this->columnize((array) $command->references)
@@ -239,6 +242,8 @@ abstract class Grammar extends BaseGrammar
      *
      * @param  \Illuminate\Support\Fluent  $column
      * @return string
+     *
+     * @throws \RuntimeException
      */
     protected function typeVector(Fluent $column)
     {
