dbcfffbe7b6b65594ebfd3f53112e754be57a655
Kay Wei <weikaiii@sina.cn>
2024-09-25 10:33:39 +0800

    Fix incorrect PHPDoc for Response in TestResponse

---
+++ types/Testing/TestResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Testing\TestResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+use function PHPStan\Testing\assertType;
+
+$response = TestResponse::fromBaseResponse(response()->redirectTo(''));
+assertType(RedirectResponse::class, $response->baseResponse);
+
+$response = TestResponse::fromBaseResponse(response()->download(''));
+assertType(BinaryFileResponse::class, $response->baseResponse);
+
+$response = TestResponse::fromBaseResponse(response()->json());
+assertType(JsonResponse::class, $response->baseResponse);
+
+$response = TestResponse::fromBaseResponse(response()->streamDownload(fn () => 1));
+assertType(StreamedResponse::class, $response->baseResponse);
