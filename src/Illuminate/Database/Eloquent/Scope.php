b32095aed148a94277b2c9b7bc93326ddc5bcd88
Kay Wei <weikaiii@sina.cn>
2024-09-26 17:12:14 +0800

    Fix not specify generic type for Eloquent Builder

--- src/Illuminate/Database/Eloquent/Scope.php
+++ src/Illuminate/Database/Eloquent/Scope.php
@@ -7,7 +7,7 @@ interface Scope
     /**
      * Apply the scope to a given Eloquent query builder.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Builder<*>  $builder
      * @param  \Illuminate\Database\Eloquent\Model  $model
      * @return void
      */
