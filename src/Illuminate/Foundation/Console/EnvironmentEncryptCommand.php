80b0d321cce50be44b02159884a80845c811ef3f
Harm Hermsen <harm@time-team.nl>
2024-10-09 09:44:39 +0200

    style: remove whitespace changes

--- src/Illuminate/Foundation/Console/EnvironmentEncryptCommand.php
+++ src/Illuminate/Foundation/Console/EnvironmentEncryptCommand.php
@@ -70,10 +70,10 @@ class EnvironmentEncryptCommand extends Command
         $keyPassed = $key !== null;
 
         $environmentFile = $this->option('env')
-            ? Str::finish(dirname($this->laravel->environmentFilePath()), DIRECTORY_SEPARATOR) . '.env.' . $this->option('env')
-            : $this->laravel->environmentFilePath();
+                            ? Str::finish(dirname($this->laravel->environmentFilePath()), DIRECTORY_SEPARATOR).'.env.'.$this->option('env')
+                            : $this->laravel->environmentFilePath();
 
-        $encryptedFile = $environmentFile . '.encrypted';
+        $encryptedFile = $environmentFile.'.encrypted';
 
         if (! $keyPassed) {
             $key = Encrypter::generateKey($cipher);
@@ -104,7 +104,7 @@ class EnvironmentEncryptCommand extends Command
 
         $this->components->info('Environment successfully encrypted.');
 
-        $this->components->twoColumnDetail('Key', $keyPassed ? $key : 'base64:' . base64_encode($key));
+        $this->components->twoColumnDetail('Key', $keyPassed ? $key : 'base64:'.base64_encode($key));
         $this->components->twoColumnDetail('Cipher', $cipher);
         $this->components->twoColumnDetail('Encrypted file', $encryptedFile);
 
