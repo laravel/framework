7fe1d54d7ec0ff2ef50af911e3a5cd4c41c61b88
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-10 11:57:42 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Testing/Fluent/Concerns/Matching.php
+++ src/Illuminate/Testing/Fluent/Concerns/Matching.php
@@ -32,7 +32,9 @@ trait Matching
             return $this;
         }
 
-        $this->ensureSorted(mutate($expected));
+        $expected = mutate($expected);
+
+        $this->ensureSorted($expected);
         $this->ensureSorted($actual);
 
         PHPUnit::assertSame(
