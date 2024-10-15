e83618db2b35ca04b4b465b4bd0070d4fa371995
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-16 07:19:49 +0800

    [11.x] Disable `PHP_CLI_SERVER_WORKERS` on Windows environment
    fixes laravel/framework#53175
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Foundation/Console/ServeCommand.php
+++ src/Illuminate/Foundation/Console/ServeCommand.php
@@ -357,7 +357,7 @@ class ServeCommand extends Command
      */
     protected function getDateFromLine($line)
     {
-        $regex = env('PHP_CLI_SERVER_WORKERS', 1) > 1
+        $regex = ! windows_os() && env('PHP_CLI_SERVER_WORKERS', 1) > 1
             ? '/^\[\d+]\s\[([a-zA-Z0-9: ]+)\]/'
             : '/^\[([^\]]+)\]/';
 
