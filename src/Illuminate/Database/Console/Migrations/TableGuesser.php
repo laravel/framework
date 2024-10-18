5e13acf72e53fb104f22ad4a118008a3eca7094d
Sebastian Hädrich <sebastian.haedrich@mailbox.org>
2024-10-18 05:57:14 +0200

    style: :rotating_light: Remove white space

--- src/Illuminate/Database/Console/Migrations/TableGuesser.php
+++ src/Illuminate/Database/Console/Migrations/TableGuesser.php
@@ -4,7 +4,7 @@ namespace Illuminate\Database\Console\Migrations;
 
 class TableGuesser
 {
-    const PATTERN = '/^(?<method>create)_(?<table>\w+?)(?:_table)?$|(?J)(?<method>add|drop|change).+_(?:to|from|in)_(?J)(?<table>\w+?)(?:_table)?$/'; 
+    const PATTERN = '/^(?<method>create)_(?<table>\w+?)(?:_table)?$|(?J)(?<method>add|drop|change).+_(?:to|from|in)_(?J)(?<table>\w+?)(?:_table)?$/';
 
     /**
      * Attempt to guess the table name and "creation" status of the given migration.
