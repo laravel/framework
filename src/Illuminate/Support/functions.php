f7e2fabf6f42710fbf50dd299398c13a00355dab
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-18 09:51:36 +0800

    [11.x] Fixes handling `Js::from(collect());` (#53206)
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Support/functions.php
+++ src/Illuminate/Support/functions.php
@@ -43,16 +43,12 @@ if (! function_exists('Illuminate\Support\enum_value')) {
      */
     function enum_value($value, $default = null)
     {
-        if (empty($value)) {
-            return $value;
-        }
-
         return transform($value, fn ($value) => match (true) {
             $value instanceof \BackedEnum => $value->value,
             $value instanceof \UnitEnum => $value->name,
 
             default => $value,
-        }, $default);
+        }, $default ?? $value);
     }
 }
 
