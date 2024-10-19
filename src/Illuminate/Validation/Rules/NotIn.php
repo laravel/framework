b42286f0d878faae0590d35f0153bfc13bd376cd
Xurshudyan <v.khurshudyan@aico.swiss>
2024-10-19 21:14:50 +0400

    Refactor NotIn rule to extend In rule

--- src/Illuminate/Validation/Rules/NotIn.php
+++ src/Illuminate/Validation/Rules/NotIn.php
@@ -2,12 +2,7 @@
 
 namespace Illuminate\Validation\Rules;
 
-use Illuminate\Contracts\Support\Arrayable;
-use Stringable;
-
-use function Illuminate\Support\enum_value;
-
-class NotIn implements Stringable
+class NotIn extends In
 {
     /**
      * The name of the rule.
@@ -15,42 +10,4 @@ class NotIn implements Stringable
      * @var string
      */
     protected $rule = 'not_in';
-
-    /**
-     * The accepted values.
-     *
-     * @var array
-     */
-    protected $values;
-
-    /**
-     * Create a new "not in" rule instance.
-     *
-     * @param  \Illuminate\Contracts\Support\Arrayable|\BackedEnum|\UnitEnum|array|string  $values
-     * @return void
-     */
-    public function __construct($values)
-    {
-        if ($values instanceof Arrayable) {
-            $values = $values->toArray();
-        }
-
-        $this->values = is_array($values) ? $values : func_get_args();
-    }
-
-    /**
-     * Convert the rule to a validation string.
-     *
-     * @return string
-     */
-    public function __toString()
-    {
-        $values = array_map(function ($value) {
-            $value = enum_value($value);
-
-            return '"'.str_replace('"', '""', $value).'"';
-        }, $this->values);
-
-        return $this->rule.':'.implode(',', $values);
-    }
 }
