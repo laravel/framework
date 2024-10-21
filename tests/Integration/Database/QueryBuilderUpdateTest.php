c0d1f5b58f7fd3680dada13ea890e31cd3fdbf7a
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-21 11:44:32 +0800

    Test Improvements
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Integration/Database/QueryBuilderUpdateTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Database;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+class QueryBuilderUpdateTest extends DatabaseTestCase
+{
+    protected function afterRefreshingDatabase()
+    {
+        Schema::create('example', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('title')->nullable();
+            $table->json('payload')->nullable();
+        });
+    }
+
+    #[DataProvider('jsonValuesDataProvider')]
+    public function testBasicUpdateForJson($given, $expected)
+    {
+        $db = DB::table('example')->insert([
+            'name' => 'Taylor Otwell',
+            'title' => 'Mr.',
+        ]);
+
+        DB::table('example')->update(['payload' => $given]);
+
+        $this->assertDatabaseHas('example', [
+            'name' => 'Taylor Otwell',
+            'title' => 'Mr.',
+            'payload' => $expected,
+        ]);
+    }
+
+    public static function jsonValuesDataProvider()
+    {
+        yield [['Laravel', 'Founder'], json_encode(['Laravel', 'Founder'])];
+        yield [collect(['Laravel', 'Founder']), json_encode(['Laravel', 'Founder'])];
+    }
+}
