f51e442f6320bf32419b3141f726a559b937b9da
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-10-30 18:10:39 +0100

    Fix cs

--- src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
+++ src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
@@ -240,7 +240,7 @@ class FoundationServiceProvider extends AggregateServiceProvider
     protected function registerExceptionRenderer()
     {
         $this->loadViewsFrom(__DIR__.'/../Exceptions/views', 'laravel-exceptions');
-        
+
         if (! $this->app->hasDebugModeEnabled()) {
             return;
         }
