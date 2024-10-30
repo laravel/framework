7fc525e023535993d7b8f957bae1d1f30f1a31be
Samuel Weirich <4281791+SamuelWei@users.noreply.github.com>
2024-10-30 18:00:10 +0100

    Create cache for laravel default exception blade files

--- src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
+++ src/Illuminate/Foundation/Providers/FoundationServiceProvider.php
@@ -244,6 +244,7 @@ class FoundationServiceProvider extends AggregateServiceProvider
         }
 
         $this->loadViewsFrom(__DIR__.'/../resources/exceptions/renderer', 'laravel-exceptions-renderer');
+        $this->loadViewsFrom(__DIR__.'/../Exceptions/views', 'laravel-exceptions');
 
         $this->app->singleton(Renderer::class, function (Application $app) {
             $errorRenderer = new HtmlErrorRenderer(
