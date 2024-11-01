2e62a2dedb41a88896c7ae4b64d5849512d7baa4
Steve Bauman <steven_bauman@outlook.com>
2024-11-01 16:12:42 +0530

    Add Url support classes

---
+++ src/Illuminate/Support/Url.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Illuminate\Support;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class Url implements Arrayable
+{
+    /**
+     * Constructor.
+     */
+    public function __construct(
+        public $scheme = null,
+        public $host = null,
+        public $port = null,
+        public $user = null,
+        public $pass = null,
+        public $path = null,
+        public $query = null,
+        public $fragment = null,
+    ) {}
+
+    /**
+     * Parse a URL string into a URL object.
+     *
+     * @param  string  $url
+     *
+     * @return static
+     */
+    public static function parse($url)
+    {
+        $components = parse_url($url) ?? [];
+
+        return new static(
+            $components['scheme'] ?? null,
+            $components['host'] ?? null,
+            $components['port'] ?? null,
+            $components['user'] ?? null,
+            $components['pass'] ?? null,
+            $components['path'] ?? null,
+            $components['query'] ?? null,
+            $components['fragment'] ?? null,
+        );
+    }
+
+    /**
+     * Get the URL query parameters.
+     *
+     * @return \Illuminate\Support\UrlQueryParameters
+     */
+    public function query()
+    {
+        return UrlQueryParameters::parse($this->query);
+    }
+
+    /**
+     * Convert the URL object to an array.
+     *
+     * @return array<string, string|null>
+     */
+    public function toArray()
+    {
+        return [
+            'scheme' => $this->scheme,
+            'host' => $this->host,
+            'port' => $this->port,
+            'user' => $this->user,
+            'pass' => $this->pass,
+            'path' => $this->path,
+            'query' => $this->query,
+            'fragment' => $this->fragment,
+        ];
+    }
+}
---
+++ src/Illuminate/Support/UrlQueryParameters.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Illuminate\Support;
+use Illuminate\Contracts\Support\Arrayable;
+
+class UrlQueryParameters implements Arrayable
+{
+    /**
+     * Constructor.
+     *
+     * @param  array  $parameters
+     * @return void
+     */
+    public function __construct(
+        protected array $parameters = []
+    ) {
+    }
+
+    /**
+     * Parse a query string.
+     *
+     * @param  string|null  $query
+     *
+     * @return static
+     */
+    public static function parse($query)
+    {
+        if (is_null($query)) {
+            return new static;
+        }
+
+        parse_str(
+            Str::of(urldecode($query))
+                ->replaceStart('?', '')
+                ->toString(),
+            $params
+        );
+
+        return new static($params);
+    }
+
+    /**
+     * Get a parameter value.
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        return $this->parameters[$key] ?? $default;
+    }
+
+    /**
+     * Check if a parameter exists.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        return isset($this->parameters[$key]);
+    }
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray()
+    {
+        return $this->parameters;
+    }
+}
+
