d4263683c4e07d66f18b959f0aa1d48894abef11
Kevin Bui <hoanglongbui89@yahoo.com.vn>
2024-11-05 15:35:21 +1000

    create a database and a provider in separate methods.

--- tests/Queue/DatabaseFailedJobProviderTest.php
+++ tests/Queue/DatabaseFailedJobProviderTest.php
@@ -13,20 +13,22 @@ use RuntimeException;
 
 class DatabaseFailedJobProviderTest extends TestCase
 {
+    protected $db;
+
+    protected $provider;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->createDatabaseWithFailedJobTable()
+            ->createProvider();
+    }
+
     public function testCanFlushFailedJobs()
     {
         Date::setTestNow(Date::now());
 
-        $db = new DB;
-        $db->addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ]);
-
-        $db->getConnection()->getSchemaBuilder()->create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->timestamp('failed_at')->useCurrent();
-        });
+        $db = $this->createSimpleDatabaseWithFailedJobTable();
 
         $provider = new DatabaseFailedJobProvider($db->getDatabaseManager(), 'default', 'failed_jobs');
 
@@ -45,123 +47,95 @@ class DatabaseFailedJobProviderTest extends TestCase
 
     public function testCanProperlyLogFailedJob()
     {
-        $db = new DB;
-        $db->addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ]);
-
-        $db->getConnection()->getSchemaBuilder()->create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
-        });
-
         $uuid = Str::uuid();
 
         $exception = new Exception(mb_convert_encoding('ÐÑÙ0E\xE2\x�98\xA0World��7B¹!þÿ', 'ISO-8859-1', 'UTF-8'));
-        $provider = new DatabaseFailedJobProvider($db->getDatabaseManager(), 'default', 'failed_jobs');
+        $provider = new DatabaseFailedJobProvider($this->db->getDatabaseManager(), 'default', 'failed_jobs');
 
         $provider->log('database', 'default', json_encode(['uuid' => (string) $uuid]), $exception);
 
         $exception = (string) mb_convert_encoding($exception, 'UTF-8');
 
-        $this->assertSame(1, $db->getConnection()->table('failed_jobs')->count());
-        $this->assertSame($exception, $db->getConnection()->table('failed_jobs')->first()->exception);
+        $this->assertSame(1, $this->failedJobsTable()->count());
+        $this->assertSame($exception, $this->failedJobsTable()->first()->exception);
     }
 
     public function testJobsCanBeCounted()
     {
-        $db = new DB;
-        $db->addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ]);
-        $db->getConnection()->getSchemaBuilder()->create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
-        });
-        $provider = new DatabaseFailedJobProvider($db->getDatabaseManager(), 'default', 'failed_jobs');
+        $this->assertSame(0, $this->provider->count());
 
-        $this->assertSame(0, $provider->count());
+        $this->provider->log('database', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->assertSame(1, $this->provider->count());
 
-        $provider->log('database', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $this->assertSame(1, $provider->count());
-
-        $provider->log('database', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $provider->log('another-connection', 'another-queue', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $this->assertSame(3, $provider->count());
+        $this->provider->log('database', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->provider->log('another-connection', 'another-queue', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->assertSame(3, $this->provider->count());
     }
 
     public function testJobsCanBeCountedByConnection()
     {
-        $db = new DB;
-        $db->addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ]);
-        $db->getConnection()->getSchemaBuilder()->create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
-        });
-        $provider = new DatabaseFailedJobProvider($db->getDatabaseManager(), 'default', 'failed_jobs');
+        $this->provider->log('connection-1', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->provider->log('connection-2', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->assertSame(1, $this->provider->count('connection-1'));
+        $this->assertSame(1, $this->provider->count('connection-2'));
 
-        $provider->log('connection-1', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $provider->log('connection-2', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $this->assertSame(1, $provider->count('connection-1'));
-        $this->assertSame(1, $provider->count('connection-2'));
-
-        $provider->log('connection-1', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $this->assertSame(2, $provider->count('connection-1'));
-        $this->assertSame(1, $provider->count('connection-2'));
+        $this->provider->log('connection-1', 'default', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->assertSame(2, $this->provider->count('connection-1'));
+        $this->assertSame(1, $this->provider->count('connection-2'));
     }
 
     public function testJobsCanBeCountedByQueue()
     {
-        $db = new DB;
-        $db->addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ]);
-        $db->getConnection()->getSchemaBuilder()->create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
-        });
-        $provider = new DatabaseFailedJobProvider($db->getDatabaseManager(), 'default', 'failed_jobs');
+        $this->provider->log('database', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->provider->log('database', 'queue-2', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->assertSame(1, $this->provider->count(queue: 'queue-1'));
+        $this->assertSame(1, $this->provider->count(queue: 'queue-2'));
 
-        $provider->log('database', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $provider->log('database', 'queue-2', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $this->assertSame(1, $provider->count(queue: 'queue-1'));
-        $this->assertSame(1, $provider->count(queue: 'queue-2'));
-
-        $provider->log('database', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $this->assertSame(2, $provider->count(queue: 'queue-1'));
-        $this->assertSame(1, $provider->count(queue: 'queue-2'));
+        $this->provider->log('database', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->assertSame(2, $this->provider->count(queue: 'queue-1'));
+        $this->assertSame(1, $this->provider->count(queue: 'queue-2'));
     }
 
     public function testJobsCanBeCountedByQueueAndConnection()
+    {
+        $this->provider->log('connection-1', 'queue-99', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->provider->log('connection-1', 'queue-99', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->provider->log('connection-2', 'queue-99', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->provider->log('connection-1', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->provider->log('connection-2', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+        $this->provider->log('connection-2', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
+
+        $this->assertSame(2, $this->provider->count('connection-1', 'queue-99'));
+        $this->assertSame(1, $this->provider->count('connection-2', 'queue-99'));
+        $this->assertSame(1, $this->provider->count('connection-1', 'queue-1'));
+        $this->assertSame(2, $this->provider->count('connection-2', 'queue-1'));
+    }
+
+    protected function createSimpleDatabaseWithFailedJobTable()
     {
         $db = new DB;
         $db->addConnection([
             'driver' => 'sqlite',
             'database' => ':memory:',
         ]);
+
         $db->getConnection()->getSchemaBuilder()->create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('failed_at')->useCurrent();
+        });
+
+        return $db;
+    }
+
+    protected function createDatabaseWithFailedJobTable()
+    {
+        $this->db = new DB;
+        $this->db->addConnection([
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+        ]);
+
+        $this->db->getConnection()->getSchemaBuilder()->create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->text('connection');
             $table->text('queue');
@@ -169,17 +143,19 @@ class DatabaseFailedJobProviderTest extends TestCase
             $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
         });
-        $provider = new DatabaseFailedJobProvider($db->getDatabaseManager(), 'default', 'failed_jobs');
 
-        $provider->log('connection-1', 'queue-99', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $provider->log('connection-1', 'queue-99', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $provider->log('connection-2', 'queue-99', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $provider->log('connection-1', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $provider->log('connection-2', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $provider->log('connection-2', 'queue-1', json_encode(['uuid' => (string) Str::uuid()]), new RuntimeException());
-        $this->assertSame(2, $provider->count('connection-1', 'queue-99'));
-        $this->assertSame(1, $provider->count('connection-2', 'queue-99'));
-        $this->assertSame(1, $provider->count('connection-1', 'queue-1'));
-        $this->assertSame(2, $provider->count('connection-2', 'queue-1'));
+        return $this;
+    }
+
+    protected function createProvider(string $database = 'default', string $table = 'failed_jobs')
+    {
+        $this->provider = new DatabaseFailedJobProvider($this->db->getDatabaseManager(), $database, $table);
+
+        return $this;
+    }
+
+    protected function failedJobsTable()
+    {
+        return $this->db->getConnection()->table('failed_jobs');
     }
 }
