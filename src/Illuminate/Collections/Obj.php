83e70a18b801184faaec3938745daaccf1510d35
zakariaarrid <zakariaarrid0@gmail.com>
2024-11-11 16:16:43 +0100

    [11.x] Add deepArrayify Method to Easily Convert Nested Objects into Arrays in Obj.php

---
+++ src/Illuminate/Collections/Obj.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Illuminate\Collections;
+
+use Illuminate\Support\Traits\Macroable;
+
+class Obj
+{
+    use Macroable;
+
+    /**
+     * Recursively convert all objects to arrays.
+     *
+     * @param  mixed  $object
+     * @return array
+     */
+    public static function deepArrayify($object)
+    {
+        if (is_object($object)) {
+            $object = get_object_vars($object);
+        }
+
+        return array_map(function ($value) {
+            if (is_object($value) || is_array($value)) {
+                return self::deepArrayify($value);
+            }
+            return $value;
+        }, $object);
+    }
+}
---
+++ tests/Support/SupportObjTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Illuminate\Tests\Support;
+
+use Illuminate\Collections\Obj;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class SupportObjTest extends TestCase
+{
+    public function testDeepArrayify(): void
+    {
+        // Main object representing a company or organization
+        $organization = new stdClass();
+        $organization->name = 'Acme Corp';
+
+        // First level nested object representing a department
+        $department = new stdClass();
+        $department->budget = 12000;
+
+        // Second level nested object within $department, representing a team or project
+        $team = new stdClass();
+        $team->description = "Research and Development";
+
+        // Assign nested structure
+        $department->team = $team;  // $department contains $team as a nested object
+        $organization->departments = [$department]; // $organization contains $department as a nested object
+
+        $array = Obj::deepArrayify($organization);
+
+        $this->assertEquals(
+            [
+              "name" => "Acme Corp",
+              "departments" =>  [
+                    0 =>  [
+                      "budget" => 12000,
+                      "team" =>  [
+                        "description" => "Research and Development"
+                      ]
+                    ]
+              ]
+            ], $array
+        );
+    }
+}
