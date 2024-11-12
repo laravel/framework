61adf1a1bd3c6fe7661640b95347cdd25c7384b7
AG <ashotgharakeshishyan@gmail.com>
2024-11-12 18:55:38 +0400

    [12.x] Add step parameter to LazyCollection range method (#53473)
    * add step in collection range
    
    * add step in collection range
    
    * add step in collection range
    
    ---------
    
    Co-authored-by: Ashot Gharakeshishyan <a.gharakeshishyan@aico.swiss>

--- src/Illuminate/Collections/Enumerable.php
+++ src/Illuminate/Collections/Enumerable.php
@@ -45,9 +45,10 @@ interface Enumerable extends Arrayable, Countable, IteratorAggregate, Jsonable, 
      *
      * @param  int  $from
      * @param  int  $to
+     * @param  int  $step
      * @return static
      */
-    public static function range($from, $to);
+    public static function range($from, $to, $step = 1);
 
     /**
      * Wrap the given value in a collection if applicable.
--- src/Illuminate/Collections/LazyCollection.php
+++ src/Illuminate/Collections/LazyCollection.php
@@ -75,17 +75,22 @@ class LazyCollection implements CanBeEscapedWhenCastToString, Enumerable
      *
      * @param  int  $from
      * @param  int  $to
+     * @param  int  $step
      * @return static<int, int>
      */
-    public static function range($from, $to)
+    public static function range($from, $to, $step = 1)
     {
-        return new static(function () use ($from, $to) {
+        if ($step == 0) {
+            throw new InvalidArgumentException('Step value cannot be zero.');
+        }
+
+        return new static(function () use ($from, $to, $step) {
             if ($from <= $to) {
-                for (; $from <= $to; $from++) {
+                for (; $from <= $to; $from += abs($step)) {
                     yield $from;
                 }
             } else {
-                for (; $from >= $to; $from--) {
+                for (; $from >= $to; $from -= abs($step)) {
                     yield $from;
                 }
             }
