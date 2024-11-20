2c9dbf6a0d6ffc4c8d89912f258c447a995c2ae4
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-20 14:18:42 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Mail/MailManagerTest.php
+++ tests/Mail/MailManagerTest.php
@@ -34,7 +34,7 @@ class MailManagerTest extends TestCase
     {
         $this->app['config']->set('mail.mailers.smtp_url', [
             'scheme' => $scheme,
-            'url' => "{$scheme}://usr:pwd@127.0.0.2:5876",
+            'url' => 'smtp://usr:pwd@127.0.0.2:5876',
         ]);
 
         $mailer = $this->app['mail.manager']->mailer('smtp_url');
