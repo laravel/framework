19b8ed48a01a97bd7afc9efe953e6b774e04ce82
Taylor Otwell <taylor@laravel.com>
2024-11-25 16:37:58 -0600

    formatting

--- src/Illuminate/Events/Dispatcher.php
+++ src/Illuminate/Events/Dispatcher.php
@@ -80,8 +80,8 @@ class Dispatcher implements DispatcherContract
     /**
      * Register an event listener with the dispatcher.
      *
-     * @param  \Illuminate\Events\QueuedClosure|string|class-string|array|callable  $events
-     * @param  \Illuminate\Events\QueuedClosure|class-string|array|callable|null  $listener
+     * @param  \Illuminate\Events\QueuedClosure|callable|array|class-string|string  $events
+     * @param  \Illuminate\Events\QueuedClosure|callable|array|class-string|null  $listener
      * @return void
      */
     public function listen($events, $listener = null)
