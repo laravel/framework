6edc4cbc3b1fc601e15a06f4398b26b6bbe1b745
Aditya Jodhani <113779419+amjpdevp@users.noreply.github.com>
2024-11-25 09:25:48 +0530

    Updated Docblock Comments

--- src/Illuminate/Http/Concerns/InteractsWithInput.php
+++ src/Illuminate/Http/Concerns/InteractsWithInput.php
@@ -676,12 +676,11 @@ trait InteractsWithInput
     }
 
     /**
-     * Dump the provided keys with values from the input data
+     * Dump the input except provided keys with values from the request data
      * 
      * @param  mixed  ...$keys
      * @return never
      */
-
     public function ddExcept(...$keys)
     {
         $this->dump($this->except($keys));
