f132b23b13909cc22c615c01b0c5640541c3da0c
taylorotwell <taylorotwell@users.noreply.github.com>
2024-11-26 15:32:57 +0000

    Update version to v10.48.25

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -40,7 +40,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '10.48.24';
+    const VERSION = '10.48.25';
 
     /**
      * The base path for the Laravel installation.
