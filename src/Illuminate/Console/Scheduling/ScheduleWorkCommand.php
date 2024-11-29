a4e21d33a40cedb5f8742f820d1ff13eb3485314
Abdel Elrafa <AbdelElrafa@users.noreply.github.com>
2024-11-29 10:52:44 -0500

    Update the message for the schedule:work command to reflect that it will run sub minute tasks and not only tasks every minute.

--- src/Illuminate/Console/Scheduling/ScheduleWorkCommand.php
+++ src/Illuminate/Console/Scheduling/ScheduleWorkCommand.php
@@ -35,7 +35,7 @@ class ScheduleWorkCommand extends Command
     public function handle()
     {
         $this->components->info(
-            'Running scheduled tasks every minute.',
+            'Watching for scheduled tasks every second.',
             $this->getLaravel()->environment('local') ? OutputInterface::VERBOSITY_NORMAL : OutputInterface::VERBOSITY_VERBOSE
         );
 
