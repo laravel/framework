cead0254d3df3ec5030677362106f15f8bddc454
TWithers <timothy.withers@gmail.com>
2024-12-04 23:47:51 -0700

    Added comments to methods

--- src/Illuminate/Queue/Middleware/HasMiddlewareHooks.php
+++ src/Illuminate/Queue/Middleware/HasMiddlewareHooks.php
@@ -7,13 +7,25 @@ use Illuminate\Support\Traits\ForwardsCalls;
 trait HasMiddlewareHooks
 {
     use ForwardsCalls;
-    protected mixed $hookWrapper = null;
 
+    /**
+     * The wrapper we are creating for this middleware.
+     */
+    protected ?WrappedMiddleware $hookWrapper = null;
+
+    /**
+     * Adds a hook to the middleware and returns a wrapped middleware instance.
+     * The hook should accept the job instance as its only argument.
+     * Supported hooks are `before`, `after`, and `onFail`.
+     */
     public function addHook(string $hookType, callable $hook): WrappedMiddleware
     {
         return $this->forwardCallTo($this->getHookWrapper(), $hookType, [$hook]);
     }
 
+    /**
+     * Initializes and returns the middleware wrapper.
+     */
     protected function getHookWrapper(): WrappedMiddleware
     {
         if (! $this->hookWrapper) {
@@ -23,21 +35,37 @@ trait HasMiddlewareHooks
         return $this->hookWrapper;
     }
 
+    /**
+     * Registers a callback to be executed before the middleware is handled.
+     * If the callback returns false or releases or fails the job, the middleware will be aborted.
+     * The callback should accept the job instance as its only argument.
+     */
     public function before(callable $callback): WrappedMiddleware
     {
         return $this->addHook('before', $callback);
     }
 
+    /**
+     * Registers a callback to be executed after the middleware is handled, but before `$next` is called.
+     * The callback should accept the job instance as its only argument.
+     */
     public function after(callable $callback): WrappedMiddleware
     {
         return $this->addHook('after', $callback);
     }
 
+    /**
+     * Registers a callback to be executed if the middleware does not call `$next` and fails.
+     * The callback should accept the job instance as its only argument.
+     */
     public function onFail(callable $callback): WrappedMiddleware
     {
         return $this->addHook('onFail', $callback);
     }
 
+    /**
+     * Same as `after`, just a more semantic name.
+     */
     public function onPass(callable $callback): WrappedMiddleware
     {
         return $this->addHook('after', $callback);
--- src/Illuminate/Queue/Middleware/WrappedMiddleware.php
+++ src/Illuminate/Queue/Middleware/WrappedMiddleware.php
@@ -4,20 +4,48 @@ namespace Illuminate\Queue\Middleware;
 
 class WrappedMiddleware
 {
+    /**
+     * The callback that is executed before the middleware is handled.
+     *
+     * @var callable|null
+     */
     protected mixed $beforeMiddleware = null;
 
+    /**
+     * The callback that is executed after the middleware is handled successfully (before `$next` is called).
+     *
+     * @var callable|null
+     */
     protected mixed $afterMiddleware = null;
 
+    /**
+     * The callback that is executed if the middleware does not call `$next` and fails.
+     *
+     * @var callable|null
+     */
     protected mixed $onFail = null;
 
+    /**
+     * Indicates if the middleware has passed.
+     */
     protected bool $middlewarePassed = false;
 
+    /**
+     * @var object $middleware The initialized middleware instance.
+     */
     public function __construct(
         protected object $middleware,
     ) {
     }
 
-    public function handle($job, $next): void
+    /**
+     * Handles the job by wrapping the initial middleware and executing the before and after callbacks
+     *
+     * @param  mixed  $job
+     * @param  callable  $next
+     * @return mixed
+     */
+    public function handle($job, $next)
     {
         if (
             $this->beforeMiddleware &&
@@ -47,6 +75,11 @@ class WrappedMiddleware
         }
     }
 
+    /**
+     * Registers a callback to be executed before the middleware is handled.
+     * If the callback returns false or releases or fails the job, the middleware will be aborted.
+     * The callback should accept the job instance as its only argument.
+     */
     public function before(callable $before): object
     {
         $this->beforeMiddleware = $before;
@@ -54,6 +87,10 @@ class WrappedMiddleware
         return $this;
     }
 
+    /**
+     * Registers a callback to be executed after the middleware is handled, but before `$next` is called.
+     * The callback should accept the job instance as its only argument.
+     */
     public function after(callable $after): object
     {
         $this->afterMiddleware = $after;
@@ -61,6 +98,10 @@ class WrappedMiddleware
         return $this;
     }
 
+    /**
+     * Registers a callback to be executed if the middleware does not call `$next` and fails.
+     * The callback should accept the job instance as its only argument.
+     */
     public function onFail(callable $callback): object
     {
         $this->onFail = $callback;
@@ -68,6 +109,9 @@ class WrappedMiddleware
         return $this;
     }
 
+    /**
+     * Same as `after`, just a more semantic name.
+     */
     public function onPass(callable $callback): object
     {
         $this->after($callback);
@@ -75,6 +119,11 @@ class WrappedMiddleware
         return $this;
     }
 
+    /**
+     * Adds a hook to the middleware and returns a wrapped middleware instance.
+     * The hook should accept the job instance as its only argument.
+     * Supported hooks are `before`, `after`, and `onFail`.
+     */
     public function addHook(string $hookType, callable $hook): object
     {
         return $this->$hookType($hook);
