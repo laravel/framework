5989aa443065e9d99dc6f418fe2190189b0b859d
Bentley O'Kane-Chase <bentleyo@gmail.com>
2024-12-11 19:35:59 +1000

    Specify node when sending scan to RedisCluster. Default to using the first master node (#53826)

--- src/Illuminate/Redis/Connections/PhpRedisClusterConnection.php
+++ src/Illuminate/Redis/Connections/PhpRedisClusterConnection.php
@@ -2,12 +2,28 @@
 
 namespace Illuminate\Redis\Connections;
 
+use InvalidArgumentException;
+
 class PhpRedisClusterConnection extends PhpRedisConnection
 {
     /**
+     * The RedisCluster client.
+     *
+     * @var \RedisCluster
+     */
+    protected $client;
+
+    /**
+     * The default node to use from the cluster.
+     *
+     * @var string|array
+     */
+    protected $defaultNode;
+
+    /**
      * Flush the selected Redis database on all master nodes.
      *
-     * @return mixed
+     * @return void
      */
     public function flushdb()
     {
@@ -20,5 +36,45 @@ class PhpRedisClusterConnection extends PhpRedisConnection
                 ? $this->command('rawCommand', [$master, 'flushdb', 'async'])
                 : $this->command('flushdb', [$master]);
         }
+    }
+
+    /**
+     * Scans all keys based on options.
+     *
+     * @param  mixed  $cursor
+     * @param  array  $options
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function scan($cursor, $options = [])
+    {
+        $result = $this->client->scan($cursor,
+            $options['node'] ?? $this->defaultNode(),
+            $options['match'] ?? '*',
+            $options['count'] ?? 10
+        );
+
+        if ($result === false) {
+            $result = [];
+        }
+
+        return $cursor === 0 && empty($result) ? false : [$cursor, $result];
+    }
+
+    /**
+     * Return default node to use for cluster.
+     *
+     * @return string|array
+     *
+     * @throws \InvalidArgumentException
+     */
+    private function defaultNode()
+    {
+        if (! isset($this->defaultNode)) {
+            $this->defaultNode = $this->client->_masters()[0] ?? throw new InvalidArgumentException('No master nodes found in the cluster.');
+        }
+
+        return $this->defaultNode;
     }
 }
