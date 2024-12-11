4d4174ea4e113e847bcb1fb89a6e02432c4cbbcb
Andrew Brown <browner12@gmail.com>
2024-12-11 16:50:53 -0600

    use promoted properties for Notification events (#53853)
    - move comments from properties to constructor docblock

--- src/Illuminate/Notifications/Events/BroadcastNotificationCreated.php
+++ src/Illuminate/Notifications/Events/BroadcastNotificationCreated.php
@@ -15,39 +15,18 @@ class BroadcastNotificationCreated implements ShouldBroadcast
     use Queueable, SerializesModels;
 
     /**
-     * The notifiable entity who received the notification.
-     *
-     * @var mixed
-     */
-    public $notifiable;
-
-    /**
-     * The notification instance.
-     *
-     * @var \Illuminate\Notifications\Notification
-     */
-    public $notification;
-
-    /**
-     * The notification data.
-     *
-     * @var array
-     */
-    public $data = [];
-
-    /**
      * Create a new event instance.
      *
-     * @param  mixed  $notifiable
-     * @param  \Illuminate\Notifications\Notification  $notification
-     * @param  array  $data
+     * @param  mixed  $notifiable  The notifiable entity who received the notification.
+     * @param  \Illuminate\Notifications\Notification  $notification  The notification instance.
+     * @param  array  $data  The notification data.
      * @return void
      */
-    public function __construct($notifiable, $notification, $data)
-    {
-        $this->data = $data;
-        $this->notifiable = $notifiable;
-        $this->notification = $notification;
+    public function __construct(
+        public $notifiable,
+        public $notification,
+        public $data = [],
+    ) {
     }
 
     /**
