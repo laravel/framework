c58943900f7f7ede2e91d0bd0bfd32583137b385
taylorotwell <taylorotwell@users.noreply.github.com>
2024-12-11 16:19:00 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/DB.php
+++ src/Illuminate/Support/Facades/DB.php
@@ -103,6 +103,7 @@ use Illuminate\Database\Console\WipeCommand;
  * @method static string getTablePrefix()
  * @method static \Illuminate\Database\Connection setTablePrefix(string $prefix)
  * @method static \Illuminate\Database\Grammar withTablePrefix(\Illuminate\Database\Grammar $grammar)
+ * @method static void withoutTablePrefix(\Closure $callback)
  * @method static string getServerVersion()
  * @method static void resolverFor(string $driver, \Closure $callback)
  * @method static \Closure|null getResolver(string $driver)
