754c6cc8186cdb136e0ad3404cbe1d411f91f5f5
Andrew Brown <browner12@gmail.com>
2024-12-11 15:56:07 -0600

    use promoted properties for Queue events
    - move comments from properites to constructor docblock

--- src/Illuminate/Queue/Events/JobPopped.php
+++ src/Illuminate/Queue/Events/JobPopped.php
@@ -5,29 +5,15 @@ namespace Illuminate\Queue\Events;
 class JobPopped
 {
     /**
-     * The connection name.
-     *
-     * @var string
-     */
-    public $connectionName;
-
-    /**
-     * The job instance.
-     *
-     * @var \Illuminate\Contracts\Queue\Job|null
-     */
-    public $job;
-
-    /**
      * Create a new event instance.
      *
-     * @param  string  $connectionName
-     * @param  \Illuminate\Contracts\Queue\Job|null  $job
+     * @param  string  $connectionName  The connection name.
+     * @param  \Illuminate\Contracts\Queue\Job|null  $job  The job instance.
      * @return void
      */
-    public function __construct($connectionName, $job)
-    {
-        $this->connectionName = $connectionName;
-        $this->job = $job;
+    public function __construct(
+        public $connectionName,
+        public $job,
+    ) {
     }
 }
--- src/Illuminate/Queue/Events/Looping.php
+++ src/Illuminate/Queue/Events/Looping.php
@@ -5,29 +5,15 @@ namespace Illuminate\Queue\Events;
 class Looping
 {
     /**
-     * The connection name.
-     *
-     * @var string
-     */
-    public $connectionName;
-
-    /**
-     * The queue name.
-     *
-     * @var string
-     */
-    public $queue;
-
-    /**
      * Create a new event instance.
      *
-     * @param  string  $connectionName
-     * @param  string  $queue
+     * @param  string  $connectionName  The connection name.
+     * @param  string  $queue  The queue name.
      * @return void
      */
-    public function __construct($connectionName, $queue)
-    {
-        $this->queue = $queue;
-        $this->connectionName = $connectionName;
+    public function __construct(
+        public $connectionName,
+        public $queue,
+    ) {
     }
 }
