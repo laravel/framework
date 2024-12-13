49647681b206eb62606e5722bd586d8a24414c8c
Lucas Michot <lucas@semalead.com>
2024-12-13 16:40:35 +0100

    [11.x] Add PHP 8.4 with herd to passthrough variables (#53885)

--- src/Illuminate/Foundation/Console/ServeCommand.php
+++ src/Illuminate/Foundation/Console/ServeCommand.php
@@ -72,6 +72,7 @@ class ServeCommand extends Command
         'HERD_PHP_81_INI_SCAN_DIR',
         'HERD_PHP_82_INI_SCAN_DIR',
         'HERD_PHP_83_INI_SCAN_DIR',
+        'HERD_PHP_84_INI_SCAN_DIR',
         'IGNITION_LOCAL_SITES_PATH',
         'LARAVEL_SAIL',
         'PATH',
