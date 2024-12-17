d8a183c0b735114261b1fadae9e7b1315b7ec848
Taylor Otwell <taylor@laravel.com>
2024-12-17 09:08:12 -0600

    Update HasUniqueStringIds.php

--- src/Illuminate/Database/Eloquent/Concerns/HasUniqueStringIds.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasUniqueStringIds.php
@@ -93,7 +93,7 @@ trait HasUniqueStringIds
     }
 
     /**
-     * Throw an exception when there the key isn't valid.
+     * Throw an exception for the given invalid unique ID.
      *
      * @param  mixed  $value
      * @param  string|null  $field
