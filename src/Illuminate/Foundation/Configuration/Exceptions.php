ca3f79b2939d30e60fad09c59c44e0630ed85db2
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2024-12-16 20:55:37 -0500

    Update Exceptions.php

--- src/Illuminate/Foundation/Configuration/Exceptions.php
+++ src/Illuminate/Foundation/Configuration/Exceptions.php
@@ -27,7 +27,7 @@ class Exceptions
      */
     public function report(callable $using)
     {
-        return $this->handler->reportable($using);
+        return $this->reportable($using);
     }
 
     /**
