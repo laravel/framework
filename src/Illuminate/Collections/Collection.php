f3ed42a2987f5b2e8c54534038c1ed2d3791e0db
Jefta van der Horst <jefta@siip.group>
2024-12-18 15:14:45 +0100

    Update Collection::hasAny (#53963)

--- src/Illuminate/Collections/Collection.php
+++ src/Illuminate/Collections/Collection.php
@@ -589,7 +589,7 @@ class Collection implements ArrayAccess, CanBeEscapedWhenCastToString, Enumerabl
     /**
      * Determine if any of the keys exist in the collection.
      *
-     * @param  mixed  $key
+     * @param  TKey|array<array-key, TKey>  $key
      * @return bool
      */
     public function hasAny($key)
@@ -601,7 +601,7 @@ class Collection implements ArrayAccess, CanBeEscapedWhenCastToString, Enumerabl
         $keys = is_array($key) ? $key : func_get_args();
 
         foreach ($keys as $value) {
-            if ($this->has($value)) {
+            if (array_key_exists($value, $this->items)) {
                 return true;
             }
         }
