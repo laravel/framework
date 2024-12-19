ece494272f8442ef3b3d5a1935e53cb67b841ed6
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-12-19 22:07:05 +0100

    Fix spacing

--- src/Illuminate/Support/Date/constants.php
+++ src/Illuminate/Support/Date/constants.php
@@ -2,22 +2,22 @@
 
 namespace Illuminate\Support\Date;
 
-if (! defined(__NAMESPACE__.'\\'.($constant  = 'SECONDS_PER_MINUTE'))) {
+if (! defined(__NAMESPACE__.'\\'.($constant = 'SECONDS_PER_MINUTE'))) {
     define(__NAMESPACE__.'\\'.$constant, 60);
 }
 
-if (! defined(__NAMESPACE__.'\\'.($constant  = 'MINUTES_PER_HOUR'))) {
+if (! defined(__NAMESPACE__.'\\'.($constant = 'MINUTES_PER_HOUR'))) {
     define(__NAMESPACE__.'\\'.$constant, 60);
 }
 
-if (! defined(__NAMESPACE__.'\\'.($constant  = 'HOURS_PER_DAY'))) {
+if (! defined(__NAMESPACE__.'\\'.($constant = 'HOURS_PER_DAY'))) {
     define(__NAMESPACE__.'\\'.$constant, 24);
 }
 
-if (! defined(__NAMESPACE__.'\\'.($constant  = 'DAYS_PER_WEEK'))) {
+if (! defined(__NAMESPACE__.'\\'.($constant = 'DAYS_PER_WEEK'))) {
     define(__NAMESPACE__.'\\'.$constant, 7);
 }
 
-if (! defined(__NAMESPACE__.'\\'.($constant  = 'MONTHS_PER_YEAR'))) {
+if (! defined(__NAMESPACE__.'\\'.($constant = 'MONTHS_PER_YEAR'))) {
     define(__NAMESPACE__.'\\'.$constant, 12);
 }
