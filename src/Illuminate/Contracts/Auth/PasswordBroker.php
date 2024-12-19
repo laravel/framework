41a889a6acfc5eb255df6a20f066bf0732a917fd
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-12-19 08:34:58 +0100

    Deprecation messages and correct PHPDoc types
    Follow-up to 3a251cf9546bf3d050904aba200ee54e1aa9ee3c

--- src/Illuminate/Contracts/Auth/PasswordBroker.php
+++ src/Illuminate/Contracts/Auth/PasswordBroker.php
@@ -10,35 +10,40 @@ interface PasswordBroker
     /**
      * Constant representing a successfully sent reminder.
      *
-     * @var string
+     * @var \Illuminate\Auth\Enums\PasswordStatus
+     * @deprecated Use \Illuminate\Auth\Enums\PasswordStatus::ResetLinkSent instead
      */
     const RESET_LINK_SENT = PasswordStatus::ResetLinkSent;
 
     /**
      * Constant representing a successfully reset password.
      *
-     * @var string
+     * @var \Illuminate\Auth\Enums\PasswordStatus
+     * @deprecated Use \Illuminate\Auth\Enums\PasswordStatus::PasswordReset instead
      */
     const PASSWORD_RESET = PasswordStatus::PasswordReset;
 
     /**
      * Constant representing the user not found response.
      *
-     * @var string
+     * @var \Illuminate\Auth\Enums\PasswordStatus
+     * @deprecated Use \Illuminate\Auth\Enums\PasswordStatus::InvalidUser instead
      */
     const INVALID_USER = PasswordStatus::InvalidUser;
 
     /**
      * Constant representing an invalid token.
      *
-     * @var string
+     * @var \Illuminate\Auth\Enums\PasswordStatus
+     * @deprecated Use \Illuminate\Auth\Enums\PasswordStatus::InvalidToken instead
      */
     const INVALID_TOKEN = PasswordStatus::InvalidToken;
 
     /**
      * Constant representing a throttled reset attempt.
      *
-     * @var string
+     * @var \Illuminate\Auth\Enums\PasswordStatus
+     * @deprecated Use \Illuminate\Auth\Enums\PasswordStatus::ResetThrottled instead
      */
     const RESET_THROTTLED = PasswordStatus::ResetThrottled;
 
