b805f203801226f6973419ebd69981bebab03152
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-01-03 15:53:39 -0500

    [11.x] Feauture - Convert number to a stringable format

--- tests/Support/SupportNumberTest.php
+++ tests/Support/SupportNumberTest.php
@@ -87,16 +87,16 @@ class SupportNumberTest extends TestCase
     #[RequiresPhpExtension('intl')]
     public function testSpelloutWithThreshold()
     {
-        $this->assertSame('9', Number::spell(9, after: 10));
-        $this->assertSame('10', Number::spell(10, after: 10));
-        $this->assertSame('eleven', Number::spell(11, after: 10));
+        $this->assertSame('9', (string) Number::spell(9, after: 10));
+        $this->assertSame('10', (string) Number::spell(10, after: 10));
+        $this->assertSame('eleven', (string) Number::spell(11, after: 10));
 
-        $this->assertSame('nine', Number::spell(9, until: 10));
-        $this->assertSame('10', Number::spell(10, until: 10));
-        $this->assertSame('11', Number::spell(11, until: 10));
+        $this->assertSame('nine', (string) Number::spell(9, until: 10));
+        $this->assertSame('10', (string) Number::spell(10, until: 10));
+        $this->assertSame('11', (string) Number::spell(11, until: 10));
 
-        $this->assertSame('ten thousand', Number::spell(10000, until: 50000));
-        $this->assertSame('100,000', Number::spell(100000, until: 50000));
+        $this->assertSame('ten thousand', (string) Number::spell(10000, until: 50000));
+        $this->assertSame('100,000', (string) Number::spell(100000, until: 50000));
     }
 
     public function testOrdinal()
