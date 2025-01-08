f32fa70d88a4541370616995fca05e185de26154
Andrew Brown <browner12@gmail.com>
2025-01-08 14:02:53 -0600

    simplify `HasFactory`
    - passing arguments to the the `factory()` method is completely undocumented
    - possibly/probably results in unnecessary calls to `count()` or `state()`
    - is less intuitive/readable than its chained counterpart

--- src/Illuminate/Database/Eloquent/Factories/HasFactory.php
+++ src/Illuminate/Database/Eloquent/Factories/HasFactory.php
@@ -10,17 +10,11 @@ trait HasFactory
     /**
      * Get a new factory instance for the model.
      *
-     * @param  (callable(array<string, mixed>, static|null): array<string, mixed>)|array<string, mixed>|int|null  $count
-     * @param  (callable(array<string, mixed>, static|null): array<string, mixed>)|array<string, mixed>  $state
      * @return TFactory
      */
-    public static function factory($count = null, $state = [])
+    public static function factory()
     {
-        $factory = static::newFactory() ?? Factory::factoryForModel(static::class);
-
-        return $factory
-                    ->count(is_numeric($count) ? $count : null)
-                    ->state(is_callable($count) || is_array($count) ? $count : $state);
+        return static::newFactory() ?? Factory::factoryForModel(static::class);
     }
 
     /**
@@ -30,10 +24,8 @@ trait HasFactory
      */
     protected static function newFactory()
     {
-        if (isset(static::$factory)) {
-            return static::$factory::new();
-        }
-
-        return null;
+        return isset(static::$factory)
+            ? static::$factory::new()
+            : null;
     }
 }
