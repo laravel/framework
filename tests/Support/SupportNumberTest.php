1ad958a27aa14086a6b1ef184d2a6ad0791ba026
Richard Trudel <rtrudel@quebec.studio>
2025-01-11 14:15:48 -0500

    Adding test in Support/SupportNumberTest.php

--- tests/Support/SupportNumberTest.php
+++ tests/Support/SupportNumberTest.php
@@ -165,6 +165,11 @@ class SupportNumberTest extends TestCase
         $this->assertSame('1 234,56 $US', Number::currency(1234.56, 'USD', 'fr'));
     }
 
+    public function testToCurrencyInCents()
+    {
+        $this->assertSame('$15.00', Number::currency(1500, 'USD', 'en', true));
+    }
+
     public function testBytesToHuman()
     {
         $this->assertSame('0 B', Number::fileSize(0));
