ffa1a12b7ff81690a35487ac583eb8ec0c2660f5
Chris Arter <chris@arter.dev>
2025-01-15 17:27:55 -0500

    Introduces UseFactory attribute (#54065)
    * introduce UseFactory attribute
    
    * review updates
    
    * Update HasFactory.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

---
+++ src/Illuminate/Database/Eloquent/Attributes/UseFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Illuminate\Database\Eloquent\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class UseFactory
+{
+    /**
+     * Create a new attribute instance.
+     *
+     * @param  class-string $factoryClass
+     * @return void
+     */
+    public function __construct(public string $factoryClass) {}
+}
