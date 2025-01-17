08c2101b41e1c74826741b9a121428bd958b7c6e
decaylala <decaylala@users.noreply.github.com>
2025-01-18 03:43:24 +0800

    [11.x]  Fix job not logged in failed_jobs table if timeout occurs within database transaction (#54173)
    * Rollback timeout job transaction to ensure failed jobs are logged in the failed_jobs table
    
    * Rollback timeout job transaction to root level
    
    * format
    
    * refactor
    
    * add new stubs instead of changing the old stub
    
    * formatting
    
    * foramtting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

---
+++ tests/Integration/Database/Queue/Fixtures/TimeOutNonBatchableJobWithTransaction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Database\Queue\Fixtures;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\DB;
+
+class TimeOutNonBatchableJobWithTransaction implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable;
+
+    public int $tries = 1;
+    public int $timeout = 2;
+
+    public function handle(): void
+    {
+        DB::transaction(fn () => sleep(20));
+    }
+}
