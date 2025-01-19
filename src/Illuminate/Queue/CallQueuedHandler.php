cc5884f3a7860b5ac77e0b5f558d5143f4b226a9
Mathias Grimm <mathiasgrimm@gmail.com>
2025-01-19 15:55:58 -0300

    fix: Don't release lock for ShouldBeUniqueUntilProcessing Job that gets released

--- src/Illuminate/Queue/CallQueuedHandler.php
+++ src/Illuminate/Queue/CallQueuedHandler.php
@@ -64,10 +64,6 @@ class CallQueuedHandler
             return $this->handleModelNotFound($job, $e);
         }
 
-        if ($command instanceof ShouldBeUniqueUntilProcessing) {
-            $this->ensureUniqueJobLockIsReleased($command);
-        }
-
         $this->dispatchThroughMiddleware($job, $command);
 
         if (! $job->isReleased() && ! $command instanceof ShouldBeUniqueUntilProcessing) {
@@ -121,6 +117,10 @@ class CallQueuedHandler
         return (new Pipeline($this->container))->send($command)
                 ->through(array_merge(method_exists($command, 'middleware') ? $command->middleware() : [], $command->middleware ?? []))
                 ->then(function ($command) use ($job) {
+                    if ($command instanceof ShouldBeUniqueUntilProcessing) {
+                        $this->ensureUniqueJobLockIsReleased($command);
+                    }
+
                     return $this->dispatcher->dispatchNow(
                         $command, $this->resolveHandler($job, $command)
                     );
---
+++ tests/Integration/Queue/UniqueUntilProcessingJobTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Queue;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUniqueUntilProcessing;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\DB;
+use Orchestra\Testbench\Attributes\WithMigration;
+
+#[WithMigration]
+#[WithMigration('cache')]
+#[WithMigration('queue')]
+class UniqueUntilProcessingJobTest extends QueueTestCase
+{
+    protected function defineEnvironment($app)
+    {
+        parent::defineEnvironment($app);
+        $app['config']->set('queue.default', 'database');
+        $app['config']->set('cache.default', 'database');
+        $this->driver = 'database';
+    }
+
+    public function testShouldBeUniqueUntilProcessingReleasesLockWhenJobIsReleasedByAMiddleware()
+    {
+        // Job that does not release and gets processed
+        UniqueTestJob::dispatch();
+        $lockKey = DB::table('cache_locks')->orderBy('id')->first()->key;
+        $this->assertNotNull($lockKey);
+        $this->runQueueWorkerCommand(['--once' => true]);
+        $this->assertFalse(UniqueTestJob::$released);
+        $lockKey = DB::table('cache_locks')->orderBy('id')->first()->key ?? null;
+        $this->assertNull($lockKey);
+
+        // Job that releases and does not get processed
+        UniqueUntilProcessingJob::dispatch();
+        $lockKey = DB::table('cache_locks')->orderBy('id')->first()->key;
+        $this->assertNotNull($lockKey);
+        $this->runQueueWorkerCommand(['--once' => true]);
+        $this->assertTrue(UniqueUntilProcessingJob::$released);
+        $lockKey = DB::table('cache_locks')->orderBy('id')->first()->key ?? null;
+        $this->assertNotNull($lockKey);
+    }
+}
+
+class UniqueTestJob implements ShouldQueue, ShouldBeUniqueUntilProcessing
+{
+    use InteractsWithQueue, Queueable, Dispatchable;
+
+    public static $handled = false;
+    public static $released = false;
+
+
+    public function handle()
+    {
+        static::$handled = true;
+    }
+}
+
+
+class UniqueUntilProcessingJob extends UniqueTestJob
+{
+
+    public function middleware()
+    {
+        return [
+            function ($job) {
+                static::$released = true;
+                $job->release(30);
+            }
+        ];
+    }
+
+    public function uniqueId()
+    {
+        return 100;
+    }
+}
