6e0e2cbd584a2988348409fe15bb2ead87aeafec
benatoff <146389211+benatoff@users.noreply.github.com>
2025-01-21 17:13:55 +0100

    fix: collapseWithKeys on empty collection (#54290)
    * fix: collapseWithKeys on empty collection
    
    * Update Collection.php
    
    * Update Collection.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Collections/Collection.php
+++ src/Illuminate/Collections/Collection.php
@@ -148,6 +148,10 @@ class Collection implements ArrayAccess, CanBeEscapedWhenCastToString, Enumerabl
      */
     public function collapseWithKeys()
     {
+        if (! $this->items) {
+            return new static;
+        }
+
         $results = [];
 
         foreach ($this->items as $key => $values) {
