60bc9a0c6246e38047aef6715794a7d62eb8e7ed
Craig Morris <craig.michael.morris@gmail.com>
2025-01-23 21:53:52 +1100

    Add dump and dd methods to Response class for debugging

--- src/Illuminate/Http/Client/Response.php
+++ src/Illuminate/Http/Client/Response.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Fluent;
 use Illuminate\Support\Traits\Macroable;
 use LogicException;
 use Stringable;
+use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * @mixin \Psr\Http\Message\ResponseInterface
@@ -300,6 +301,68 @@ class Response implements ArrayAccess, Stringable
         if ($this->failed()) {
             return new RequestException($this);
         }
+    }
+
+    /**
+     * Dump the content from the response.
+     *
+     * @param  string|null  $key
+     * @return $this
+     */
+    public function dump($key = null)
+    {
+        $content = $this->body();
+
+        $json = json_decode($content);
+
+        if (json_last_error() === JSON_ERROR_NONE) {
+            $content = $json;
+        }
+
+        if (! is_null($key)) {
+            dump(data_get($content, $key));
+        } else {
+            dump($content);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Dump the content from the response and end the script.
+     *
+     * @param  string|null  $key
+     * @return never
+     */
+    public function dd($key = null)
+    {
+        $this->dump($key);
+
+        exit(1);
+    }
+
+    /**
+     * Dump the headers from the response.
+     *
+     * @return $this
+     */
+    public function dumpHeaders()
+    {
+        dump($this->headers());
+
+        return $this;
+    }
+
+    /**
+     * Dump the headers from the response and end the script.
+     *
+     * @return never
+     */
+    public function ddHeaders()
+    {
+        $this->dumpHeaders();
+
+        exit(1);
     }
 
     /**
