ae05e49e4763f099838e118c5742131b2661f225
Taylor Otwell <taylor@laravel.com>
2025-01-24 16:24:02 -0600

    wip

--- src/Illuminate/Database/Connectors/PostgresConnector.php
+++ src/Illuminate/Database/Connectors/PostgresConnector.php
@@ -51,65 +51,6 @@ class PostgresConnector extends Connector implements ConnectorInterface
     }
 
     /**
-     * Set the connection transaction isolation level.
-     *
-     * @param  \PDO  $connection
-     * @param  array  $config
-     * @return void
-     */
-    protected function configureIsolationLevel($connection, array $config)
-    {
-        if (isset($config['isolation_level'])) {
-            $connection->prepare("set session characteristics as transaction isolation level {$config['isolation_level']}")->execute();
-        }
-    }
-
-    /**
-     * Set the timezone on the connection.
-     *
-     * @param  \PDO  $connection
-     * @param  array  $config
-     * @return void
-     */
-    protected function configureTimezone($connection, array $config)
-    {
-        if (isset($config['timezone'])) {
-            $timezone = $config['timezone'];
-
-            $connection->prepare("set time zone '{$timezone}'")->execute();
-        }
-    }
-
-    /**
-     * Set the "search_path" on the database connection.
-     *
-     * @param  \PDO  $connection
-     * @param  array  $config
-     * @return void
-     */
-    protected function configureSearchPath($connection, $config)
-    {
-        if (isset($config['search_path']) || isset($config['schema'])) {
-            $searchPath = $this->quoteSearchPath(
-                $this->parseSearchPath($config['search_path'] ?? $config['schema'])
-            );
-
-            $connection->prepare("set search_path to {$searchPath}")->execute();
-        }
-    }
-
-    /**
-     * Format the search path for the DSN.
-     *
-     * @param  array  $searchPath
-     * @return string
-     */
-    protected function quoteSearchPath($searchPath)
-    {
-        return count($searchPath) === 1 ? '"'.$searchPath[0].'"' : '"'.implode('", "', $searchPath).'"';
-    }
-
-    /**
      * Create a DSN string from a configuration.
      *
      * @param  array  $config
@@ -169,6 +110,65 @@ class PostgresConnector extends Connector implements ConnectorInterface
         }
 
         return $dsn;
+    }
+
+    /**
+     * Set the connection transaction isolation level.
+     *
+     * @param  \PDO  $connection
+     * @param  array  $config
+     * @return void
+     */
+    protected function configureIsolationLevel($connection, array $config)
+    {
+        if (isset($config['isolation_level'])) {
+            $connection->prepare("set session characteristics as transaction isolation level {$config['isolation_level']}")->execute();
+        }
+    }
+
+    /**
+     * Set the timezone on the connection.
+     *
+     * @param  \PDO  $connection
+     * @param  array  $config
+     * @return void
+     */
+    protected function configureTimezone($connection, array $config)
+    {
+        if (isset($config['timezone'])) {
+            $timezone = $config['timezone'];
+
+            $connection->prepare("set time zone '{$timezone}'")->execute();
+        }
+    }
+
+    /**
+     * Set the "search_path" on the database connection.
+     *
+     * @param  \PDO  $connection
+     * @param  array  $config
+     * @return void
+     */
+    protected function configureSearchPath($connection, $config)
+    {
+        if (isset($config['search_path']) || isset($config['schema'])) {
+            $searchPath = $this->quoteSearchPath(
+                $this->parseSearchPath($config['search_path'] ?? $config['schema'])
+            );
+
+            $connection->prepare("set search_path to {$searchPath}")->execute();
+        }
+    }
+
+    /**
+     * Format the search path for the DSN.
+     *
+     * @param  array  $searchPath
+     * @return string
+     */
+    protected function quoteSearchPath($searchPath)
+    {
+        return count($searchPath) === 1 ? '"'.$searchPath[0].'"' : '"'.implode('", "', $searchPath).'"';
     }
 
     /**
