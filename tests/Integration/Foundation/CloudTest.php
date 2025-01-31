bef330c11c03c28dfde371f4e9a47c2f295a1542
Mior Muhammad Zaki <crynobone@gmail.com>
2025-01-31 08:54:17 +0800

    Test Improvements
    Using the `WithConfig` attribute to load configuration during the Laravel bootstrapping step for a specific test (between `RegisterProviders` and `BootProviders`) instead of loading the configuration when the application is booted.

--- tests/Integration/Foundation/CloudTest.php
+++ tests/Integration/Foundation/CloudTest.php
@@ -3,18 +3,14 @@
 namespace Illuminate\Tests\Integration\Foundation;
 
 use Illuminate\Foundation\Cloud;
+use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\TestCase;
 
 class CloudTest extends TestCase
 {
+    #[WithConfig('database.connections.pgsql', ['host' => 'test-pooler.pg.laravel.cloud', 'username' => 'test-username', 'password' => 'test-password'])
     public function test_it_can_resolve_core_container_aliases()
     {
-        $this->app['config']->set('database.connections.pgsql', [
-            'host' => 'test-pooler.pg.laravel.cloud',
-            'username' => 'test-username',
-            'password' => 'test-password',
-        ]);
-
         Cloud::configureUnpooledPostgresConnection($this->app);
 
         $this->assertEquals([
