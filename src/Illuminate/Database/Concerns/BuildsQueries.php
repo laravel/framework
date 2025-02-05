74cea9ce89eb481cd192f11ad0ada18bce4504ab
Choraimy Kroonstuiver <3661474+axlon@users.noreply.github.com>
2025-02-05 11:36:29 +0100

    Add generics to lazy queries (#54453)

--- src/Illuminate/Database/Concerns/BuildsQueries.php
+++ src/Illuminate/Database/Concerns/BuildsQueries.php
@@ -224,7 +224,7 @@ trait BuildsQueries
      * Query lazily, by chunks of the given size.
      *
      * @param  int  $chunkSize
-     * @return \Illuminate\Support\LazyCollection
+     * @return \Illuminate\Support\LazyCollection<int, TValue>
      *
      * @throws \InvalidArgumentException
      */
@@ -259,7 +259,7 @@ trait BuildsQueries
      * @param  int  $chunkSize
      * @param  string|null  $column
      * @param  string|null  $alias
-     * @return \Illuminate\Support\LazyCollection
+     * @return \Illuminate\Support\LazyCollection<int, TValue>
      *
      * @throws \InvalidArgumentException
      */
@@ -274,7 +274,7 @@ trait BuildsQueries
      * @param  int  $chunkSize
      * @param  string|null  $column
      * @param  string|null  $alias
-     * @return \Illuminate\Support\LazyCollection
+     * @return \Illuminate\Support\LazyCollection<int, TValue>
      *
      * @throws \InvalidArgumentException
      */
