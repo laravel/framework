917a3febc3889de8c1957382f2d39f730e231432
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-02-06 17:52:28 -0500

    [11.x] Optimize `PendingBatch@ensureJobIsBatchable` (#54485)
    * simplify PendingBatch@ensureJobIsBatchable; memoize the classes which are batchable
    
    * no need to recheck the batch jobs
    
    * Update PendingBatch.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Bus/PendingBatch.php
+++ src/Illuminate/Bus/PendingBatch.php
@@ -48,6 +48,13 @@ class PendingBatch
     public $options = [];
 
     /**
+     * Jobs that have been verified to contain the Batchable trait.
+     *
+     * @var array<class-string, bool>
+     */
+    protected static $batchableClasses = [];
+
+    /**
      * Create a new pending batch instance.
      *
      * @param  \Illuminate\Contracts\Container\Container  $container
@@ -92,14 +99,16 @@ class PendingBatch
     {
         foreach (Arr::wrap($job) as $job) {
             if ($job instanceof PendingBatch) {
-                $this->ensureJobIsBatchable($job->jobs->all());
-
                 return;
             }
 
-            if (! in_array(Batchable::class, class_uses_recursive($job))) {
+            if (! (static::$batchableClasses[$job::class] ?? false) && ! in_array(Batchable::class, class_uses_recursive($job))) {
+                static::$batchableClasses[$job::class] = false;
+
                 throw new RuntimeException(sprintf('Attempted to batch job [%s], but it does not use the Batchable trait.', $job::class));
             }
+
+            static::$batchableClasses[$job::class] = true;
         }
     }
 
