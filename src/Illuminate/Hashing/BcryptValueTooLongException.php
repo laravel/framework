41b4d15295acb368d949f97c2897a846fd478169
Alan Cole <waxim@users.noreply.github.com>
2025-02-06 20:51:56 +0000

    Update src/Illuminate/Hashing/BcryptValueTooLongException.php
    Co-authored-by: Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>

--- src/Illuminate/Hashing/BcryptValueTooLongException.php
+++ src/Illuminate/Hashing/BcryptValueTooLongException.php
@@ -8,6 +8,6 @@ class BcryptValueTooLongException extends RuntimeException
 {
     public function __construct(int $maxLength = BcryptHasher::BCRYPT_STR_LIMIT)
     {
-        parent::__construct("Value is too long. Maximum length is {$maxLength} characters.");
+        parent::__construct("Value to hash is too long. Bcrypt only allows for a maximum length of {$maxLength} characters. Shorten your value or use a different hashing algorithm.");
     }
 }
