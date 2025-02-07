05532f4d2d60d33651aed1586e8c105aded1b5d0
yuriizee <yuriyzee@gmail.com>
2025-02-07 16:10:45 +0200

    [11.x] Added the ability to disable secure SSL connection for DB 8.0 versions and refactoring options
    fixes (#54269)
    
    --ssl
    --ssl-ca

--- config/database.php
+++ config/database.php
@@ -59,6 +59,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT'),
             ]) : [],
         ],
 
