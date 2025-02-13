b902504a8d7ec8cdcd50c3d1059cb4e57dde670d
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2025-02-13 16:08:05 +0600

    email validator enum

---
+++ src/Illuminate/Validation/Rules/EmailValidation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Illuminate\Validation\Rules;
+
+use Egulias\EmailValidator\Validation\RFCValidation;
+use Egulias\EmailValidator\Validation\DNSCheckValidation;
+use Illuminate\Validation\Concerns\FilterEmailValidation;
+use Egulias\EmailValidator\Validation\NoRFCWarningsValidation;
+use Egulias\EmailValidator\Validation\Extra\SpoofCheckValidation;
+use Egulias\EmailValidator\Validation\EmailValidation as EguliasEmailValidation;
+
+enum EmailValidation
+{
+    case Strict;
+    case Dns;
+    case Spoof;
+    case Filter;
+    case Rfc;
+
+    public function validation() : EguliasEmailValidation
+    {
+        return match ($this) {
+            self::Strict => new NoRFCWarningsValidation(),
+            self::Dns => new DNSCheckValidation(),
+            self::Spoof => new SpoofCheckValidation(),
+            self::Filter => new FilterEmailValidation(),
+            self::Rfc => new RFCValidation(),
+            default => new RFCValidation(),
+        };
+    }
+}
