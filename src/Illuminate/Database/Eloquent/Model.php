937ed1e96e9c49fe193ae717ec3dd9ffdd939fec
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2025-02-13 17:50:55 -0300

    Removes unused import.

--- src/Illuminate/Database/Eloquent/Model.php
+++ src/Illuminate/Database/Eloquent/Model.php
@@ -3,7 +3,6 @@
 namespace Illuminate\Database\Eloquent;
 
 use ArrayAccess;
-use Closure;
 use Illuminate\Contracts\Broadcasting\HasBroadcastChannel;
 use Illuminate\Contracts\Queue\QueueableCollection;
 use Illuminate\Contracts\Queue\QueueableEntity;
