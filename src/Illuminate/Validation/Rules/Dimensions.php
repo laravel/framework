b7f332ded6b16058918889dec953ba886b3868f0
Cam Kemshal-Bell <cam@iterated.tech>
2025-02-17 16:10:28 +1100

    feat: update rule to add ValidatorAware & DataAware for better fluent implementation

--- src/Illuminate/Validation/Rules/Dimensions.php
+++ src/Illuminate/Validation/Rules/Dimensions.php
@@ -2,29 +2,152 @@
 
 namespace Illuminate\Validation\Rules;
 
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Contracts\Validation\ValidatorAwareRule;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Conditionable;
-use Stringable;
+use Illuminate\Support\Traits\Macroable;
+use InvalidArgumentException;
 
-class Dimensions implements Stringable
+class Dimensions implements DataAwareRule, Rule, ValidatorAwareRule
 {
-    use Conditionable;
+    use Conditionable, Macroable;
 
     /**
-     * The constraints for the dimensions rule.
+     * The width constraint in pixels.
+     *
+     * @var null|int
+     */
+    protected $width = null;
+
+    /**
+     * The minimum size in pixels that the image can be.
+     *
+     * @var null|int
+     */
+    protected $minWidth = null;
+
+    /**
+     * The maximum size in pixels that the image can be.
+     *
+     * @var null|int
+     */
+    protected $maxWidth = null;
+
+    /**
+     * The width between constraint.
+     *
+     * @var null|array
+     */
+    protected $widthBetween = null;
+
+    /**
+     * The height constraint in pixels.
+     *
+     * @var null|int
+     */
+    protected $height = null;
+
+    /**
+     * The minimum size in pixels that the image can be.
+     *
+     * @var null|int
+     */
+    protected $minHeight = null;
+
+    /**
+     * The maximum size in pixels that the image can be.
+     *
+     * @var null|int
+     */
+    protected $maxHeight = null;
+
+    /**
+     * The height between constraint.
+     *
+     * @var null|array
+     */
+    protected $heightBetween = null;
+
+    /**
+     * The ratio constraint.
+     *
+     * @var null|float
+     */
+    protected $ratio = null;
+
+    /**
+     * The minimum aspect ratio constraint.
+     *
+     * @var null|float
+     */
+    protected $minRatio = null;
+
+    /**
+     * The maximum aspect ratio constraint.
+     *
+     * @var null|float
+     */
+    protected $maxRatio = null;
+
+    /**
+     * The aspect ratio range constraint.
+     *
+     * @var null|array
+     */
+    protected $ratioBetween = null;
+
+    /**
+     * The error message after validation, if any.
      *
      * @var array
      */
-    protected $constraints = [];
+    protected $messages = [];
+
+    /**
+     * The data under validation.
+     *
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * The validator performing the validation.
+     *
+     * @var \Illuminate\Validation\Validator
+     */
+    protected $validator;
+
+    /**
+     * The callback that will generate the "default" version of the dimensions rule.
+     *
+     * @var string|array|callable|null
+     */
+    public static $defaultCallback;
+
+    /**
+     * Additional validation rules that should be merged into the default rules during validation.
+     *
+     * @var array
+     */
+    protected $customRules = [];
 
     /**
      * Create a new dimensions rule instance.
      *
-     * @param  array  $constraints
      * @return void
      */
     public function __construct(array $constraints = [])
     {
-        $this->constraints = $constraints;
+        foreach ($constraints as $key => $value) {
+            $key = Str::camel($key);
+            if (method_exists($this, $key)) {
+                $this->{$key}($value);
+            }
+        }
     }
 
     /**
@@ -35,7 +158,7 @@ class Dimensions implements Stringable
      */
     public function width($value)
     {
-        $this->constraints['width'] = $value;
+        $this->width = $value;
 
         return $this;
     }
@@ -48,7 +171,7 @@ class Dimensions implements Stringable
      */
     public function height($value)
     {
-        $this->constraints['height'] = $value;
+        $this->height = $value;
 
         return $this;
     }
@@ -61,7 +184,7 @@ class Dimensions implements Stringable
      */
     public function minWidth($value)
     {
-        $this->constraints['min_width'] = $value;
+        $this->minWidth = $value;
 
         return $this;
     }
@@ -74,7 +197,7 @@ class Dimensions implements Stringable
      */
     public function minHeight($value)
     {
-        $this->constraints['min_height'] = $value;
+        $this->minHeight = $value;
 
         return $this;
     }
@@ -87,7 +210,7 @@ class Dimensions implements Stringable
      */
     public function maxWidth($value)
     {
-        $this->constraints['max_width'] = $value;
+        $this->maxWidth = $value;
 
         return $this;
     }
@@ -100,7 +223,35 @@ class Dimensions implements Stringable
      */
     public function maxHeight($value)
     {
-        $this->constraints['max_height'] = $value;
+        $this->maxHeight = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set the width between constraint.
+     *
+     * @param  int  $min
+     * @param  int  $max
+     * @return $this
+     */
+    public function widthBetween($min, $max)
+    {
+        $this->widthBetween = [$min, $max];
+
+        return $this;
+    }
+
+    /**
+     * Set the height between constraint.
+     *
+     * @param  int  $min
+     * @param  int  $max
+     * @return $this
+     */
+    public function heightBetween($min, $max)
+    {
+        $this->heightBetween = [$min, $max];
 
         return $this;
     }
@@ -113,39 +264,39 @@ class Dimensions implements Stringable
      */
     public function ratio($value)
     {
-        $this->constraints['ratio'] = $value;
+        $this->ratio = $value;
 
         return $this;
     }
 
     /**
-     * Set the minimum aspect ratio.
+     * Set the minimum aspect ratio constraint.
      *
      * @param  float  $value
      * @return $this
      */
     public function minRatio($value)
     {
-        $this->constraints['min_ratio'] = $value;
+        $this->minRatio = $value;
 
         return $this;
     }
 
     /**
-     * Set the maximum aspect ratio.
+     * Set the maximum aspect ratio constraint.
      *
      * @param  float  $value
      * @return $this
      */
     public function maxRatio($value)
     {
-        $this->constraints['max_ratio'] = $value;
+        $this->maxRatio = $value;
 
         return $this;
     }
 
     /**
-     * Set the aspect ratio range.
+     * Set the aspect ratio between constraint.
      *
      * @param  float  $min
      * @param  float  $max
@@ -153,25 +304,165 @@ class Dimensions implements Stringable
      */
     public function ratioBetween($min, $max)
     {
-        $this->constraints['min_ratio'] = $min;
-        $this->constraints['max_ratio'] = $max;
+        $this->ratioBetween = [$min, $max];
 
         return $this;
     }
 
     /**
-     * Convert the rule to a validation string.
+     * Build the array of underlying validation rules based on the current state.
      *
-     * @return string
+     * @return array
      */
-    public function __toString()
+    protected function buildValidationRules()
     {
-        $result = '';
+        $rules = [];
 
-        foreach ($this->constraints as $key => $value) {
-            $result .= "$key=$value,";
+        $validationRules = [
+            'width', 'minWidth', 'maxWidth', 'widthBetween',
+            'height', 'minHeight', 'maxHeight', 'heightBetween',
+            'ratio', 'minRatio', 'maxRatio', 'ratioBetween',
+        ];
+
+        foreach ($validationRules as $property) {
+            if ($this->{$property} !== null) {
+                $value = $this->{$property};
+
+                if (is_array($value)) {
+                    $value = implode(',', $value);
+                }
+
+                $ruleName = Str::snake($property);
+                $rules[] = "{$ruleName}:{$value}";
+            }
         }
 
-        return 'dimensions:'.substr($result, 0, -1);
+        return array_merge($rules, $this->customRules);
+    }
+
+    /**
+     * Set the default callback to be used for determining default rules.
+     *
+     * If no arguments are passed, the default dimensions rule configuration will be returned.
+     *
+     * @param  static|callable|null  $callback
+     * @return static|void
+     */
+    public static function defaults($callback = null)
+    {
+        if (is_null($callback)) {
+            return static::default();
+        }
+
+        if (! is_callable($callback) && ! $callback instanceof static) {
+            throw new InvalidArgumentException('The given callback should be callable or an instance of '.static::class);
+        }
+
+        static::$defaultCallback = $callback;
+    }
+
+    /**
+     * Get the default configuration of the dimensions rule.
+     *
+     * @return static
+     */
+    public static function default()
+    {
+        $dimensions = is_callable(static::$defaultCallback)
+            ? call_user_func(static::$defaultCallback)
+            : static::$defaultCallback;
+
+        return $dimensions instanceof Rule ? $dimensions : new self;
+    }
+
+    /**
+     * Specify additional validation rules that should be merged with the default rules during validation.
+     *
+     * @param  string|array  $rules
+     * @return $this
+     */
+    public function rules($rules)
+    {
+        $this->customRules = array_merge($this->customRules, Arr::wrap($rules));
+
+        return $this;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     * */
+    public function passes($attribute, $value)
+    {
+        $this->messages = [];
+
+        $validator = Validator::make(
+            $this->data,
+            [$attribute => $this->buildValidationRules()],
+            $this->validator->customMessages,
+            $this->validator->customAttributes
+        );
+
+        if ($validator->fails()) {
+            return $this->fail($validator->messages()->all());
+        }
+
+        return true;
+    }
+
+    /**
+     * Adds the given failures, and return false.
+     *
+     * @param  array|string  $messages
+     * @return bool
+     */
+    protected function fail($messages)
+    {
+        $messages = collect(Arr::wrap($messages))->map(
+            fn ($message) => $this->validator->getTranslator()->get($message)
+        )->all();
+
+        $this->messages = array_merge($this->messages, $messages);
+
+        return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return array
+     */
+    public function message()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * Set the current validator.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return $this
+     */
+    public function setValidator($validator)
+    {
+        $this->validator = $validator;
+
+        return $this;
+    }
+
+    /**
+     * Set the current data under validation.
+     *
+     * @param  array  $data
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
     }
 }
