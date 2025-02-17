bc1868c471244eefc0b0e00daf028e2959a4e93b
Nick Hamnett <nick@sameoldnick.com>
2025-02-16 18:02:14 -0700

    Tests factory model names are correct

--- tests/Database/DatabaseEloquentFactoryTest.php
+++ tests/Database/DatabaseEloquentFactoryTest.php
@@ -829,6 +829,12 @@ class DatabaseEloquentFactoryTest extends TestCase
         $this->assertNull($post->user_id);
     }
 
+    public function test_factory_model_names_correct()
+    {
+        $this->assertEquals(FactoryTestUseFactoryAttribute::factory()->modelName(), FactoryTestUseFactoryAttribute::class);
+        $this->assertEquals(FactoryTestGuessModel::factory()->modelName(), FactoryTestGuessModel::class);
+    }
+
     /**
      * Get a database connection instance.
      *
@@ -981,4 +987,42 @@ class FactoryTestRole extends Eloquent
     {
         return $this->belongsToMany(FactoryTestUser::class, 'role_user', 'role_id', 'user_id')->withPivot('admin');
     }
+}
+
+class FactoryTestGuessModelFactory extends Factory
+{
+    protected static function appNamespace()
+    {
+        return __NAMESPACE__ . '\\';
+    }
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+        ];
+    }
+}
+
+class FactoryTestGuessModel extends Eloquent
+{
+    use HasFactory;
+
+    protected static $factory = FactoryTestGuessModelFactory::class;
+}
+
+class FactoryTestUseFactoryAttributeFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+        ];
+    }
+}
+
+#[UseFactory(FactoryTestUseFactoryAttributeFactory::class)]
+class FactoryTestUseFactoryAttribute extends Eloquent
+{
+    use HasFactory;
 }
