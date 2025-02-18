laravel/framework

commit 2155a6330bca67e3e42dcc75c97cb359caf98b18

use value helper for $perPage as used for $total (#54650)

src/Illuminate/Database/Eloquent/Builder.php
@@ -1013,10 +1013,7 @@ class Builder implements BuilderContract
 
         $total = value($total) ?? $this->toBase()->getCountForPagination();
 
-        $perPage = ($perPage instanceof Closure
-            ? $perPage($total)
-            : $perPage
-        ) ?: $this->model->getPerPage();
+        $perPage = value($perPage, $total) ?? $this->model->getPerPage();
 
         $results = $total
             ? $this->forPage($page, $perPage)->get($columns)

src/Illuminate/Database/Query/Builder.php
@@ -3150,7 +3150,7 @@ class Builder implements BuilderContract
 
         $total = value($total) ?? $this->getCountForPagination();
 
-        $perPage = $perPage instanceof Closure ? $perPage($total) : $perPage;
+        $perPage = value($perPage, $total);
 
         $results = $total ? $this->forPage($page, $perPage)->get($columns) : new Collection;
 

commit 39d9770f048e719a5fec280e75ab06f984695a7c

[11.x] Fix "Divide by Zero" regression bug introduced in #54650
fixes #54679

Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

src/Illuminate/Database/Eloquent/Builder.php
@@ -1013,7 +1013,7 @@ class Builder implements BuilderContract
 
         $total = value($total) ?? $this->toBase()->getCountForPagination();
 
-        $perPage = value($perPage, $total) ?? $this->model->getPerPage();
+        $perPage = value($perPage, $total) ?: $this->model->getPerPage();
 
         $results = $total
             ? $this->forPage($page, $perPage)->get($columns)