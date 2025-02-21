7cb72e3fe21cc2b760cc10c88c7eeac43b03803c
Androshchuk Andrii <androschuk.andriy@gmail.com>
2025-02-22 00:12:03 +0200

    Fix $extraAdapters initialization by setting it to an empty array.

--- src/Illuminate/Support/Env.php
+++ src/Illuminate/Support/Env.php
@@ -29,7 +29,7 @@ class Env
      *
      * @var array<\Dotenv\Repository\Adapter\AdapterInterface>
      */
-    protected static $extraAdapters;
+    protected static $extraAdapters = [];
 
     /**
      * Enable the putenv adapter.
