2cbb3ad00a021444d66de5e1612625fce6cb3cd4
Andreas Svartsjö <andreas@svartsjo.nu>
2025-02-26 14:19:22 +0100

    Add Redis scheme configuration to database settings

--- config/database.php
+++ config/database.php
@@ -153,6 +153,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
@@ -162,6 +163,7 @@ return [
         ],
 
         'cache' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
