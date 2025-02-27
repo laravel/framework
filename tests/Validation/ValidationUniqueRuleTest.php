a71794b46f52e37bae3b5b5c1207acdaa6bcbab4
Ali Khosravi <alikhosravi175@gmail.com>
2025-02-27 21:58:27 +0330

    Add Tests for Handling Null Primary Keys and Special Values in Unique Validation Rule (#54823)
    * Add test for handling ignoreModel with NULL primary key values
    
    * Add test case for handling special values in 'where' conditions (NULL, 0, etc.)

--- tests/Validation/ValidationUniqueRuleTest.php
+++ tests/Validation/ValidationUniqueRuleTest.php
@@ -103,6 +103,40 @@ class ValidationUniqueRuleTest extends TestCase
         $rule->onlyTrashed('softdeleted_at');
         $this->assertSame('unique:table,NULL,NULL,id,softdeleted_at,"NOT_NULL"', (string) $rule);
     }
+
+    public function testItHandlesNullPrimaryKeyInIgnoreModel()
+    {
+        $model = new EloquentModelStub(['id_column' => null]);
+
+        $rule = new Unique('table', 'column');
+        $rule->ignore($model);
+        $rule->where('foo', 'bar');
+        $this->assertSame('unique:table,column,NULL,id_column,foo,"bar"', (string) $rule);
+
+        $rule = new Unique('table', 'column');
+        $rule->ignore($model, 'id_column');
+        $rule->where('foo', 'bar');
+        $this->assertSame('unique:table,column,NULL,id_column,foo,"bar"', (string) $rule);
+    }
+
+    public function testItHandlesWhereWithSpecialValues()
+    {
+        $rule = new Unique('table', 'column');
+        $rule->where('foo', null);
+        $this->assertSame('unique:table,column,NULL,id,foo,"NULL"', (string) $rule);
+
+        $rule = new Unique('table', 'column');
+        $rule->whereNull('foo');
+        $this->assertSame('unique:table,column,NULL,id,foo,"NULL"', (string) $rule);
+
+        $rule = new Unique('table', 'column');
+        $rule->whereNotNull('foo');
+        $this->assertSame('unique:table,column,NULL,id,foo,"NOT_NULL"', (string) $rule);
+
+        $rule = new Unique('table', 'column');
+        $rule->where('foo', 0);
+        $this->assertSame('unique:table,column,NULL,id,foo,"0"', (string) $rule);
+    }
 }
 
 class EloquentModelStub extends Model
