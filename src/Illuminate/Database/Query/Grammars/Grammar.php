750ae66fbf46982aabf9c7aff786c9d03e52e9c0
Dan Hardman <danhardman@users.noreply.github.com>
2025-02-27 16:24:23 +0000

    Linting for StyleCI

--- src/Illuminate/Database/Query/Grammars/Grammar.php
+++ src/Illuminate/Database/Query/Grammars/Grammar.php
@@ -994,7 +994,7 @@ class Grammar extends BaseGrammar
      */
     protected function compileLimit(Builder $query, $limit)
     {
-        return 'limit '. $limit;
+        return 'limit '.$limit;
     }
 
     /**
