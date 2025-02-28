7b8e12d8f4218615370d329a258f0b7c4afc6237
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-02-28 11:59:47 -0500

    [12.x] Do not loop through middleware when excluded is empty (#54837)
    * simplify resolving middleware
    
    * style
    
    * avoid loop above

--- src/Illuminate/Routing/Router.php
+++ src/Illuminate/Routing/Router.php
@@ -829,35 +829,35 @@ class Router implements BindingRegistrar, RegistrarContract
      */
     public function resolveMiddleware(array $middleware, array $excluded = [])
     {
-        $excluded = (new Collection($excluded))->map(function ($name) {
+        $excluded = $excluded === [] ? $excluded : (new Collection($excluded))->map(function ($name) {
             return (array) MiddlewareNameResolver::resolve($name, $this->middleware, $this->middlewareGroups);
         })->flatten()->values()->all();
 
         $middleware = (new Collection($middleware))->map(function ($name) {
             return (array) MiddlewareNameResolver::resolve($name, $this->middleware, $this->middlewareGroups);
-        })->flatten()->reject(function ($name) use ($excluded) {
-            if (empty($excluded)) {
-                return false;
-            }
+        })->flatten()
+            ->when(
+                ! empty($excluded),
+                fn ($collection) => $collection->reject(function ($name) use ($excluded) {
+                    if ($name instanceof Closure) {
+                        return false;
+                    }
 
-            if ($name instanceof Closure) {
-                return false;
-            }
+                    if (in_array($name, $excluded, true)) {
+                        return true;
+                    }
 
-            if (in_array($name, $excluded, true)) {
-                return true;
-            }
+                    if (! class_exists($name)) {
+                        return false;
+                    }
 
-            if (! class_exists($name)) {
-                return false;
-            }
+                    $reflection = new ReflectionClass($name);
 
-            $reflection = new ReflectionClass($name);
-
-            return (new Collection($excluded))->contains(
-                fn ($exclude) => class_exists($exclude) && $reflection->isSubclassOf($exclude)
-            );
-        })->values();
+                    return (new Collection($excluded))->contains(
+                        fn ($exclude) => class_exists($exclude) && $reflection->isSubclassOf($exclude)
+                    );
+                })
+            )->values();
 
         return $this->sortMiddleware($middleware);
     }
