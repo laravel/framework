31cd3ef8cd436b883c330d5f26543e447a896616
co63oc <co63oc@users.noreply.github.com>
2025-02-28 09:16:12 +0800

    Fix

--- src/Illuminate/Filesystem/LocalFilesystemAdapter.php
+++ src/Illuminate/Filesystem/LocalFilesystemAdapter.php
@@ -87,7 +87,7 @@ class LocalFilesystemAdapter extends FilesystemAdapter
     }
 
     /**
-     * Indiate that signed URLs should serve the corresponding files.
+     * Indicate that signed URLs should serve the corresponding files.
      *
      * @param  bool  $serve
      * @param  \Closure|null  $urlGeneratorResolver
