0796783c9f0d2ddcf982a223a2abd68283eec7f6
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-04 07:37:42 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Concurrency/ConcurrencyTest.php
+++ tests/Integration/Concurrency/ConcurrencyTest.php
@@ -7,12 +7,10 @@ use Illuminate\Concurrency\ProcessDriver;
 use Illuminate\Foundation\Application;
 use Illuminate\Process\Factory as ProcessFactory;
 use Illuminate\Support\Facades\Concurrency;
-use Orchestra\Testbench\Attributes\UsesVendor;
 use Orchestra\Testbench\TestCase;
 use PHPUnit\Framework\Attributes\RequiresOperatingSystem;
 
 #[RequiresOperatingSystem('Linux|DAR')]
-#[UsesVendor]
 class ConcurrencyTest extends TestCase
 {
     protected function setUp(): void
