b3e5ab544dc1cb51517e9af5622c5715117b66a7
mihaileu <54336101+mihaileu@users.noreply.github.com>
2025-03-13 11:15:02 +0000

    use laravel code style

--- src/Illuminate/Log/Context/ContextLogProcessor.php
+++ src/Illuminate/Log/Context/ContextLogProcessor.php
@@ -18,7 +18,7 @@ class ContextLogProcessor implements ContextLogProcessorContract
     public function __invoke(LogRecord $record): LogRecord
     {
         $app = Container::getInstance();
-        if (!$app->bound(ContextRepository::class)) {
+        if (! $app->bound(ContextRepository::class)) {
             return $record;
         }
 
