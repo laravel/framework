490ac4f43cbd0b0483971f16a265393f2419a83c
Liam Duckett <liamduckett2@gmail.com>
2025-03-15 18:23:06 +0000

    add failing type test

--- types/Support/Collection.php
+++ types/Support/Collection.php
@@ -873,7 +873,7 @@ assertType('Illuminate\Support\Collection<int, int>', $collection->make([1])->so
 assertType('Illuminate\Support\Collection<string, string>', $collection->make(['string' => 'string'])->sortKeysDesc(1));
 
 assertType('mixed', $collection->make([1])->sum('string'));
-assertType('mixed', $collection->make(['string'])->sum(function ($string) {
+assertType('int', $collection->make(['string'])->sum(function ($string) {
     assertType('string', $string);
 
     return 1;
