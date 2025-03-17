172cbec653a93abc23d1460f0e46685c9988c596
Andrew Brown <browner12@gmail.com>
2025-03-17 18:50:07 -0500

    minor formatting

--- src/Illuminate/Validation/Validator.php
+++ src/Illuminate/Validation/Validator.php
@@ -1221,7 +1221,7 @@ class Validator implements ValidatorContract
     {
         $rules = (new Collection($rules))
             ->mapWithKeys(function ($value, $key) {
-                return [str_replace('\.', '__dot__' . static::$placeholderHash, $key) => $value];
+                return [str_replace('\.', '__dot__'.static::$placeholderHash, $key) => $value];
             })
             ->toArray();
 
