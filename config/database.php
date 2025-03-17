b44eeb9fd10b82cd961e26d28584ef5ae6d10d4f
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2025-03-18 02:43:53 +0600

    Adding SSL encryption and support for MySQL connection (#55048)
    Added `MYSQL_ATTR_SSL_CERT`, `MYSQL_ATTR_SSL_KEY`, and `MYSQL_ATTR_SSL_VERIFY_SERVER_CERT

--- config/database.php
+++ config/database.php
@@ -60,6 +60,9 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CERT => env('MYSQL_ATTR_SSL_CERT'),
+                PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
             ]) : [],
         ],
 
