758dfc54965a668417c204e867b205aff37012e9
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-24 00:52:12 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Mail/resources/views/html/footer.blade.php
+++ src/Illuminate/Mail/resources/views/html/footer.blade.php
@@ -3,7 +3,7 @@
 <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
 <td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
+{!! Illuminate\Mail\Markdown::parse($slot) !!}
 </td>
 </tr>
 </table>
