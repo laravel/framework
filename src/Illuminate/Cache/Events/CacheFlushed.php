9447cb235020a5b5297f66411797499ef864d3aa
Tech Wolf TW <pandiselvamm004@gmail.com>
2025-03-23 14:52:20 +0530

    [12.x] Deleted New CacheFlush Event

--- src/Illuminate/Cache/Events/CacheFlushed.php
+++ src/Illuminate/Cache/Events/CacheFlushed.php
@@ -2,7 +2,23 @@
 
 namespace Illuminate\Cache\Events;
 
-class CacheFlushed extends CacheFlushEvent
+class CacheFlushed
 {
-    //
+    /**
+     * The name of the cache store.
+     *
+     * @var string|null
+     */
+    public $storeName;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  string|null  $storeName
+     * @return void
+     */
+    public function __construct($storeName)
+    {
+        $this->storeName = $storeName;
+    }
 }
--- src/Illuminate/Cache/Events/CacheFlushing.php
+++ src/Illuminate/Cache/Events/CacheFlushing.php
@@ -2,7 +2,23 @@
 
 namespace Illuminate\Cache\Events;
 
-class CacheFlushing extends CacheFlushEvent
+class CacheFlushing
 {
-    //
+    /**
+     * The name of the cache store.
+     *
+     * @var string|null
+     */
+    public $storeName;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  string|null  $storeName
+     * @return void
+     */
+    public function __construct($storeName)
+    {
+        $this->storeName = $storeName;
+    }
 }
