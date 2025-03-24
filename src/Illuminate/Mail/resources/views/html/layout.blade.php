b273d790d01483a6929c7460d1915e51d3a3356c
Mior Muhammad Zaki <crynobone@gmail.com>
2025-03-24 09:57:26 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Mail/resources/views/html/layout.blade.php
+++ src/Illuminate/Mail/resources/views/html/layout.blade.php
@@ -40,7 +40,7 @@ width: 100% !important;
 <!-- Body content -->
 <tr>
 <td class="content-cell">
-{!! Illuminate\Mail\Markdown::parse($slot) !!}
+{{ Illuminate\Mail\Markdown::parse($slot) }}
 
 {!! $subcopy ?? '' !!}
 </td>
