b202d2502432be295b5b96c8e544cc7c27742768
Tim MacDonald <hello@timacdonald.me>
2025-03-26 10:51:31 +1000

    Fix builder return type in type tests

--- types/Database/Query/Builder.php
+++ types/Database/Query/Builder.php
@@ -36,12 +36,12 @@ function test(Builder $query, EloquentBuilder $userQuery): void
     assertType('Illuminate\Support\LazyCollection<int, object>', $query->lazy());
     assertType('Illuminate\Support\LazyCollection<int, object>', $query->lazyById());
     assertType('Illuminate\Support\LazyCollection<int, object>', $query->lazyByIdDesc());
-    assertType('Illuminate\Database\Query\Builder', $query->tap(function () {
+    assertType('Illuminate\Database\Query\Builder', $query->pipe(function () {
         //
     }));
-    assertType('Illuminate\Database\Query\Builder', $query->tap(fn () => null));
-    assertType('Illuminate\Database\Query\Builder', $query->tap(fn ($query) => $query));
-    assertType('int', $query->tap(fn ($query) => 5));
+    assertType('Illuminate\Database\Query\Builder', $query->pipe(fn () => null));
+    assertType('Illuminate\Database\Query\Builder', $query->pipe(fn ($query) => $query));
+    assertType('int', $query->pipe(fn ($query) => 5));
 
     $query->chunk(1, function ($users, $page) {
         assertType('Illuminate\Support\Collection<int, object>', $users);
