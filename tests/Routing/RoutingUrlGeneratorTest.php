laravel/framework

commit 27acc099f252f92ade3c1db0769e4385e5a3b8c5

[12.x] Various URL generation bugfixes (#54811)
* Add regression test: passed parameters have precedence over defaults

* Add RouteUrlGenerator::formatParameters()
This method turns a list of passed parameters into a list of *named*
parameters (where possible) reducing ambiguity and making other code
work more accurately with positional route parameters, especially
when URL::defaults() is involved.

This commit also resolves a known bug -- removing a 'mark skipped'
mark for a test.

* Add test: complex route generation with defaults and binding fields

* Add support for URL::defaults(['model:slug' => 'foo'])

* Code style: Apply StyleCI diff

* Cleanup, additional test cases

* formatting

---------

Co-authored-by: Taylor Otwell <taylor@laravel.com>

src/Illuminate/Routing/RouteUrlGenerator.php
@@ -2,8 +2,11 @@
 
 namespace Illuminate\Routing;
 
+use BackedEnum;
+use Illuminate\Contracts\Routing\UrlRoutable;
 use Illuminate\Routing\Exceptions\UrlGenerationException;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 
 class RouteUrlGenerator
 {
@@ -74,6 +77,8 @@ class RouteUrlGenerator
      */
     public function to($route, $parameters = [], $absolute = false)
     {
+        $parameters = $this->formatParameters($route, $parameters);
+
         $domain = $this->getRouteDomain($route, $parameters);
 
         // First we will construct the entire URI including the root and query string. Once it
@@ -165,6 +170,95 @@ class RouteUrlGenerator
         return ($secure && $port === 443) || (! $secure && $port === 80)
             ? $domain
             : $domain.':'.$port;
+    }
+
+    /**
+     * Format the array of route parameters.
+     *
+     * @param  \Illuminate\Routing\Route  $route
+     * @param  mixed  $parameters
+     * @return array
+     */
+    protected function formatParameters(Route $route, $parameters)
+    {
+        $parameters = Arr::wrap($parameters);
+
+        $passedParameterCount = count($parameters);
+
+        $namedParameters = [];
+        $namedQueryParameters = [];
+        $routeParametersWithoutDefaultsOrNamedParameters = [];
+
+        $routeParameters = $route->parameterNames();
+
+        foreach ($routeParameters as $name) {
+            if (isset($parameters[$name])) {
+                // Named parameters don't need any special handling...
+                $namedParameters[$name] = $parameters[$name];
+                unset($parameters[$name]);
+
+                continue;
+            } elseif (! isset($this->defaultParameters[$name])) {
+                // No named parameter or default value, try to match to positional parameter below...
+                array_push($routeParametersWithoutDefaultsOrNamedParameters, $name);
+            }
+
+            $namedParameters[$name] = '';
+        }
+
+        // Named parameters that don't have route parameters will be used for query string...
+        foreach ($parameters as $key => $value) {
+            if (is_string($key)) {
+                $namedQueryParameters[$key] = $value;
+
+                unset($parameters[$key]);
+            }
+        }
+
+        // Match positional parameters to the route parameters that didn't have a value in order...
+        if (count($parameters) == count($routeParametersWithoutDefaultsOrNamedParameters)) {
+            foreach (array_reverse($routeParametersWithoutDefaultsOrNamedParameters) as $name) {
+                if (count($parameters) === 0) {
+                    break;
+                }
+
+                $namedParameters[$name] = array_pop($parameters);
+            }
+        }
+
+        // If there are extra parameters, just fill left to right... if not, fill right to left and try to use defaults...
+        $extraParameters = $passedParameterCount > count($routeParameters);
+
+        foreach ($extraParameters ? $namedParameters : array_reverse($namedParameters) as $key => $value) {
+            $bindingField = $route->bindingFieldFor($key);
+
+            $defaultParameterKey = $bindingField ? "$key:$bindingField" : $key;
+
+            if ($value !== '') {
+                continue;
+            } elseif (! empty($parameters)) {
+                $namedParameters[$key] = $extraParameters ? array_shift($parameters) : array_pop($parameters);
+            } elseif (isset($this->defaultParameters[$defaultParameterKey])) {
+                $namedParameters[$key] = $this->defaultParameters[$defaultParameterKey];
+            }
+        }
+
+        // Any remaining values in $parameters are unnamed query string parameters...
+        $parameters = array_merge($namedParameters, $namedQueryParameters, $parameters);
+
+        $parameters = Collection::wrap($parameters)->map(function ($value, $key) use ($route) {
+            return $value instanceof UrlRoutable && $route->bindingFieldFor($key)
+                    ? $value->{$route->bindingFieldFor($key)}
+                    : $value;
+        })->all();
+
+        array_walk_recursive($parameters, function (&$item) {
+            if ($item instanceof BackedEnum) {
+                $item = $item->value;
+            }
+        });
+
+        return $this->url->formatParameters($parameters);
     }
 
     /**

src/Illuminate/Routing/UrlGenerator.php
@@ -538,20 +538,8 @@ class UrlGenerator implements UrlGeneratorContract
      */
     public function toRoute($route, $parameters, $absolute)
     {
-        $parameters = Collection::wrap($parameters)->map(function ($value, $key) use ($route) {
-            return $value instanceof UrlRoutable && $route->bindingFieldFor($key)
-                ? $value->{$route->bindingFieldFor($key)}
-                : $value;
-        })->all();
-
-        array_walk_recursive($parameters, function (&$item) {
-            if ($item instanceof BackedEnum) {
-                $item = $item->value;
-            }
-        });
-
         return $this->routeUrl()->to(
-            $route, $this->formatParameters($parameters), $absolute
+            $route, $parameters, $absolute
         );
     }
 

commit 27301406c7457e65476c04a61ef321aae5a81a0f

[12.x] Fix URL generation with optional parameters (regression in #54811) (#55213)
* Fix URL generation with optional parameters (regression in #54811)
This reworks the logic from conditionally reversing passed parameters to
instead computing an offset and filling parameters left-to-right from there.

* Apply StyleCI diff

* formatting

---------

Co-authored-by: Taylor Otwell <taylor@laravel.com>

src/Illuminate/Routing/Route.php
@@ -1321,9 +1321,9 @@ class Route
     /**
      * Get the optional parameter names for the route.
      *
-     * @return array
+     * @return array<string, null>
      */
-    protected function getOptionalParameterNames()
+    public function getOptionalParameterNames()
     {
         preg_match_all('/\{(\w+?)\?\}/', $this->uri(), $matches);
 

src/Illuminate/Routing/RouteUrlGenerator.php
@@ -183,13 +183,12 @@ class RouteUrlGenerator
     {
         $parameters = Arr::wrap($parameters);
 
-        $passedParameterCount = count($parameters);
-
         $namedParameters = [];
         $namedQueryParameters = [];
-        $routeParametersWithoutDefaultsOrNamedParameters = [];
+        $requiredRouteParametersWithoutDefaultsOrNamedParameters = [];
 
         $routeParameters = $route->parameterNames();
+        $optionalParameters = $route->getOptionalParameterNames();
 
         foreach ($routeParameters as $name) {
             if (isset($parameters[$name])) {
@@ -198,9 +197,9 @@ class RouteUrlGenerator
                 unset($parameters[$name]);
 
                 continue;
-            } elseif (! isset($this->defaultParameters[$name])) {
-                // No named parameter or default value, try to match to positional parameter below...
-                array_push($routeParametersWithoutDefaultsOrNamedParameters, $name);
+            } elseif (! isset($this->defaultParameters[$name]) && ! isset($optionalParameters[$name])) {
+                // No named parameter or default value for a required parameter, try to match to positional parameter below...
+                array_push($requiredRouteParametersWithoutDefaultsOrNamedParameters, $name);
             }
 
             $namedParameters[$name] = '';
@@ -216,8 +215,8 @@ class RouteUrlGenerator
         }
 
         // Match positional parameters to the route parameters that didn't have a value in order...
-        if (count($parameters) == count($routeParametersWithoutDefaultsOrNamedParameters)) {
-            foreach (array_reverse($routeParametersWithoutDefaultsOrNamedParameters) as $name) {
+        if (count($parameters) == count($requiredRouteParametersWithoutDefaultsOrNamedParameters)) {
+            foreach (array_reverse($requiredRouteParametersWithoutDefaultsOrNamedParameters) as $name) {
                 if (count($parameters) === 0) {
                     break;
                 }
@@ -226,19 +225,61 @@ class RouteUrlGenerator
             }
         }
 
-        // If there are extra parameters, just fill left to right... if not, fill right to left and try to use defaults...
-        $extraParameters = $passedParameterCount > count($routeParameters);
+        $offset = 0;
+        $emptyParameters = array_filter($namedParameters, static fn ($val) => $val === '');
 
-        foreach ($extraParameters ? $namedParameters : array_reverse($namedParameters) as $key => $value) {
-            $bindingField = $route->bindingFieldFor($key);
+        if (count($requiredRouteParametersWithoutDefaultsOrNamedParameters) !== 0 &&
+            count($parameters) !== count($emptyParameters)) {
+            // Find the index of the first required parameter...
+            $offset = array_search($requiredRouteParametersWithoutDefaultsOrNamedParameters[0], array_keys($namedParameters));
 
-            $defaultParameterKey = $bindingField ? "$key:$bindingField" : $key;
+            // If more empty parameters remain, adjust the offset...
+            $remaining = count($emptyParameters) - $offset - count($parameters);
 
-            if ($value !== '') {
+            if ($remaining < 0) {
+                // Effectively subtract the remaining count since it's negative...
+                $offset += $remaining;
+            }
+
+            // Correct offset if it goes below zero...
+            if ($offset < 0) {
+                $offset = 0;
+            }
+        } elseif (count($requiredRouteParametersWithoutDefaultsOrNamedParameters) === 0 && count($parameters) !== 0) {
+            // Handle the case where all passed parameters are for parameters that have default values...
+            $remainingCount = count($parameters);
+
+            // Loop over empty parameters backwards and stop when we run out of passed parameters...
+            for ($i = count($namedParameters) - 1; $i >= 0; $i--) {
+                if ($namedParameters[array_keys($namedParameters)[$i]] === '') {
+                    $offset = $i;
+                    $remainingCount--;
+
+                    if ($remainingCount === 0) {
+                        // If there are no more passed parameters, we stop here...
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Starting from the offset, match any passed parameters from left to right...
+        for ($i = $offset; $i < count($namedParameters); $i++) {
+            $key = array_keys($namedParameters)[$i];
+
+            if ($namedParameters[$key] !== '') {
                 continue;
             } elseif (! empty($parameters)) {
-                $namedParameters[$key] = $extraParameters ? array_shift($parameters) : array_pop($parameters);
-            } elseif (isset($this->defaultParameters[$defaultParameterKey])) {
+                $namedParameters[$key] = array_shift($parameters);
+            }
+        }
+
+        // Fill leftmost parameters with defaults if the loop above was offset...
+        foreach ($namedParameters as $key => $value) {
+            $bindingField = $route->bindingFieldFor($key);
+            $defaultParameterKey = $bindingField ? "$key:$bindingField" : $key;
+
+            if ($value === '' && isset($this->defaultParameters[$defaultParameterKey])) {
                 $namedParameters[$key] = $this->defaultParameters[$defaultParameterKey];
             }
         }

tests/Routing/RoutingUrlGeneratorTest.php
@@ -1821,6 +1821,167 @@ class RoutingUrlGeneratorTest extends TestCase
             $url->route('tenantPostUser', ['concretePost', 'extra' => 'query']),
         );
     }
+
+    public function testUrlGenerationWithOptionalParameters(): void
+    {
+        $url = new UrlGenerator(
+            $routes = new RouteCollection,
+            Request::create('https://www.foo.com/')
+        );
+
+        $url->defaults([
+            'tenant' => 'defaultTenant',
+            'user' => 'defaultUser',
+        ]);
+
+        /**
+         * Route with one required parameter and one optional parameter.
+         */
+        $route = new Route(['GET'], 'postOptionalMethod/{post}/{method?}', ['as' => 'postOptionalMethod', fn () => '']);
+        $routes->add($route);
+
+        $this->assertSame(
+            'https://www.foo.com/postOptionalMethod/1',
+            $url->route('postOptionalMethod', 1),
+        );
+
+        $this->assertSame(
+            'https://www.foo.com/postOptionalMethod/1/2',
+            $url->route('postOptionalMethod', [1, 2]),
+        );
+
+        /**
+         * Route with two optional parameters.
+         */
+        $route = new Route(['GET'], 'optionalPostOptionalMethod/{post}/{method?}', ['as' => 'optionalPostOptionalMethod', fn () => '']);
+        $routes->add($route);
+
+        $this->assertSame(
+            'https://www.foo.com/optionalPostOptionalMethod/1',
+            $url->route('optionalPostOptionalMethod', 1),
+        );
+
+        $this->assertSame(
+            'https://www.foo.com/optionalPostOptionalMethod/1/2',
+            $url->route('optionalPostOptionalMethod', [1, 2]),
+        );
+
+        /**
+         * Route with one default parameter, one required parameter, and one optional parameter.
+         */
+        $route = new Route(['GET'], 'tenantPostOptionalMethod/{tenant}/{post}/{method?}', ['as' => 'tenantPostOptionalMethod', fn () => '']);
+        $routes->add($route);
+
+        // Passing one parameter
+        $this->assertSame(
+            'https://www.foo.com/tenantPostOptionalMethod/defaultTenant/concretePost',
+            $url->route('tenantPostOptionalMethod', ['concretePost']),
+        );
+
+        // Passing two parameters: optional parameter is prioritized over parameter with a default value
+        $this->assertSame(
+            'https://www.foo.com/tenantPostOptionalMethod/defaultTenant/concretePost/concreteMethod',
+            $url->route('tenantPostOptionalMethod', ['concretePost', 'concreteMethod']),
+        );
+
+        // Passing all three parameters
+        $this->assertSame(
+            'https://www.foo.com/tenantPostOptionalMethod/concreteTenant/concretePost/concreteMethod',
+            $url->route('tenantPostOptionalMethod', ['concreteTenant', 'concretePost', 'concreteMethod']),
+        );
+
+        /**
+         * Route with two default parameters, one required parameter, and one optional parameter.
+         */
+        $route = new Route(['GET'], 'tenantUserPostOptionalMethod/{tenant}/{user}/{post}/{method?}', ['as' => 'tenantUserPostOptionalMethod', fn () => '']);
+        $routes->add($route);
+
+        // Passing one parameter
+        $this->assertSame(
+            'https://www.foo.com/tenantUserPostOptionalMethod/defaultTenant/defaultUser/concretePost',
+            $url->route('tenantUserPostOptionalMethod', ['concretePost']),
+        );
+
+        // Passing two parameters: optional parameter is prioritized over parameters with default values
+        $this->assertSame(
+            'https://www.foo.com/tenantUserPostOptionalMethod/defaultTenant/defaultUser/concretePost/concreteMethod',
+            $url->route('tenantUserPostOptionalMethod', ['concretePost', 'concreteMethod']),
+        );
+
+        // Passing three parameters: only the leftmost parameter with a default value uses its default value
+        $this->assertSame(
+            'https://www.foo.com/tenantUserPostOptionalMethod/defaultTenant/concreteUser/concretePost/concreteMethod',
+            $url->route('tenantUserPostOptionalMethod', ['concreteUser', 'concretePost', 'concreteMethod']),
+        );
+
+        // Same as the assertion above, but using some named parameters
+        $this->assertSame(
+            'https://www.foo.com/tenantUserPostOptionalMethod/defaultTenant/concreteUser/concretePost/concreteMethod',
+            $url->route('tenantUserPostOptionalMethod', ['user' => 'concreteUser', 'concretePost', 'concreteMethod']),
+        );
+
+        // Also using a named parameter, but this time for the post parameter
+        $this->assertSame(
+            'https://www.foo.com/tenantUserPostOptionalMethod/defaultTenant/concreteUser/concretePost/concreteMethod',
+            $url->route('tenantUserPostOptionalMethod', ['concreteUser', 'post' => 'concretePost', 'concreteMethod']),
+        );
+
+        // Also using a named parameter, but this time for the optional method parameter
+        $this->assertSame(
+            'https://www.foo.com/tenantUserPostOptionalMethod/defaultTenant/concreteUser/concretePost/concreteMethod',
+            $url->route('tenantUserPostOptionalMethod', ['concreteUser', 'concretePost', 'method' => 'concreteMethod']),
+        );
+
+        // Passing all four parameters
+        $this->assertSame(
+            'https://www.foo.com/tenantUserPostOptionalMethod/concreteTenant/concreteUser/concretePost/concreteMethod',
+            $url->route('tenantUserPostOptionalMethod', ['concreteTenant', 'concreteUser', 'concretePost', 'concreteMethod']),
+        );
+
+        /**
+         * Route with a default parameter, a required parameter, another default parameter, and finally an optional parameter.
+         *
+         * This tests interleaved default parameters when combined with optional parameters.
+         */
+        $route = new Route(['GET'], 'tenantPostUserOptionalMethod/{tenant}/{post}/{user}/{method?}', ['as' => 'tenantPostUserOptionalMethod', fn () => '']);
+        $routes->add($route);
+
+        // Passing one parameter
+        $this->assertSame(
+            'https://www.foo.com/tenantPostUserOptionalMethod/defaultTenant/concretePost/defaultUser',
+            $url->route('tenantPostUserOptionalMethod', ['concretePost']),
+        );
+
+        // Passing two parameters: optional parameter is prioritized over parameters with default values
+        $this->assertSame(
+            'https://www.foo.com/tenantPostUserOptionalMethod/defaultTenant/concretePost/defaultUser/concreteMethod',
+            $url->route('tenantPostUserOptionalMethod', ['concretePost', 'concreteMethod']),
+        );
+
+        // Same as the assertion above, but using some named parameters
+        $this->assertSame(
+            'https://www.foo.com/tenantPostUserOptionalMethod/defaultTenant/concretePost/defaultUser/concreteMethod',
+            $url->route('tenantPostUserOptionalMethod', ['post' => 'concretePost', 'concreteMethod']),
+        );
+
+        // Also using a named parameter, but this time for the optional parameter
+        $this->assertSame(
+            'https://www.foo.com/tenantPostUserOptionalMethod/defaultTenant/concretePost/defaultUser/concreteMethod',
+            $url->route('tenantPostUserOptionalMethod', ['concretePost', 'method' => 'concreteMethod']),
+        );
+
+        // Passing three parameters: only the leftmost parameter with a default value uses its default value
+        $this->assertSame(
+            'https://www.foo.com/tenantPostUserOptionalMethod/defaultTenant/concretePost/concreteUser/concreteMethod',
+            $url->route('tenantPostUserOptionalMethod', ['concretePost', 'concreteUser', 'concreteMethod']),
+        );
+
+        // Passing all four parameters
+        $this->assertSame(
+            'https://www.foo.com/tenantPostUserOptionalMethod/concreteTenant/concretePost/concreteUser/concreteMethod',
+            $url->route('tenantPostUserOptionalMethod', ['concreteTenant', 'concretePost', 'concreteUser', 'concreteMethod']),
+        );
+    }
 }
 
 class RoutableInterfaceStub implements UrlRoutable