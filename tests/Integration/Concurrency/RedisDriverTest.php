7e14ddcbedb89a982a412e106d8a932304adce19
AmirHossein Fallah <amirhossein.fallah@arvancloud.ir>
2025-03-31 05:17:42 +0330

    Apply Pint style fixes to RedisDriverTest

--- tests/Integration/Concurrency/RedisDriverTest.php
+++ tests/Integration/Concurrency/RedisDriverTest.php
@@ -6,7 +6,6 @@ use Exception;
 use Illuminate\Concurrency\RedisDriver;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithRedis;
 use Illuminate\Redis\RedisManager;
-use Laravel\SerializableClosure\SerializableClosure;
 use Mockery as m;
 use Orchestra\Testbench\TestCase;
 
@@ -38,7 +37,7 @@ class RedisDriverTest extends TestCase
         // Setup mock Redis connection to avoid actual Redis calls
         $this->redisManager = m::mock(RedisManager::class);
         $this->mockRedisConnection = m::mock(\Illuminate\Redis\Connections\Connection::class);
-        
+
         // Set up common expectations for the Redis connection
         $this->redisManager->shouldReceive('connection')
             ->with('default')
@@ -48,7 +47,7 @@ class RedisDriverTest extends TestCase
     protected function tearDown(): void
     {
         parent::tearDown();
-        
+
         $this->tearDownRedis();
         m::close();
     }
@@ -59,20 +58,21 @@ class RedisDriverTest extends TestCase
         $task = function () {
             return 'Hello, World!';
         };
-        
+
         // Mock Redis calls
         $taskId = null;
         $this->mockRedisConnection->shouldReceive('set')
             ->with(m::pattern('/^testing:concurrency:.*:task$/'), m::type('string'), 'EX', 3600)
             ->andReturnUsing(function ($key, $value) use (&$taskId) {
                 $taskId = str_replace(':task', '', $key);
+
                 return true;
             });
-            
+
         $this->mockRedisConnection->shouldReceive('rpush')
             ->with($this->queuePrefix.'queue', m::pattern('/^testing:concurrency:.*$/'))
             ->andReturn(1);
-        
+
         $this->mockRedisConnection->shouldReceive('get')
             ->with(m::pattern('/^testing:concurrency:.*:result$/'))
             ->andReturnUsing(function ($key) use (&$taskId) {
@@ -80,9 +80,10 @@ class RedisDriverTest extends TestCase
                 if (str_replace(':result', '', $key) === $taskId) {
                     return serialize(['result' => 'Hello, World!']);
                 }
+
                 return null;
             });
-            
+
         $this->mockRedisConnection->shouldReceive('del')
             ->with(m::pattern('/^testing:concurrency:.*:task$/'), m::pattern('/^testing:concurrency:.*:result$/'))
             ->andReturn(2);
@@ -109,45 +110,46 @@ class RedisDriverTest extends TestCase
                 return 'Task 3';
             },
         ];
-        
+
         // Store task IDs
         $taskIds = [];
-        
+
         // Mock Redis calls
         $this->mockRedisConnection->shouldReceive('set')
             ->with(m::pattern('/^testing:concurrency:.*:task$/'), m::type('string'), 'EX', 3600)
             ->andReturnUsing(function ($key, $value) use (&$taskIds) {
                 $taskId = str_replace(':task', '', $key);
                 $taskIds[] = $taskId;
+
                 return true;
             })
             ->times(3);
-            
+
         $this->mockRedisConnection->shouldReceive('rpush')
             ->with($this->queuePrefix.'queue', m::pattern('/^testing:concurrency:.*$/'))
             ->andReturn(1)
             ->times(3);
-        
+
         $resultMap = [
             'Task 1',
             'Task 2',
             'Task 3',
         ];
-        
+
         $this->mockRedisConnection->shouldReceive('get')
             ->with(m::pattern('/^testing:concurrency:.*:result$/'))
             ->andReturnUsing(function ($key) use (&$taskIds, $resultMap) {
                 // Find which task this is
                 $taskKey = str_replace(':result', '', $key);
                 $index = array_search($taskKey, $taskIds);
-                
+
                 if ($index !== false) {
                     return serialize(['result' => $resultMap[$index]]);
                 }
-                
+
                 return null;
             });
-            
+
         $this->mockRedisConnection->shouldReceive('del')
             ->with(m::pattern('/^testing:concurrency:.*:task$/'), m::pattern('/^testing:concurrency:.*:result$/'))
             ->andReturn(2)
@@ -167,7 +169,7 @@ class RedisDriverTest extends TestCase
         $this->mockRedisConnection->shouldReceive('set')
             ->with(m::pattern('/^testing:concurrency:.*:task$/'), m::type('string'), 'EX', 3600)
             ->andReturn(true);
-            
+
         $this->mockRedisConnection->shouldReceive('rpush')
             ->with($this->queuePrefix.'deferred', m::pattern('/^testing:concurrency:.*$/'))
             ->andReturn(1);
@@ -188,20 +190,21 @@ class RedisDriverTest extends TestCase
         $task = function () {
             throw new Exception('Test exception');
         };
-        
+
         // Mock Redis calls
         $taskId = null;
         $this->mockRedisConnection->shouldReceive('set')
             ->with(m::pattern('/^testing:concurrency:.*:task$/'), m::type('string'), 'EX', 3600)
             ->andReturnUsing(function ($key, $value) use (&$taskId) {
                 $taskId = str_replace(':task', '', $key);
+
                 return true;
             });
-            
+
         $this->mockRedisConnection->shouldReceive('rpush')
             ->with($this->queuePrefix.'queue', m::pattern('/^testing:concurrency:.*$/'))
             ->andReturn(1);
-        
+
         $this->mockRedisConnection->shouldReceive('get')
             ->with(m::pattern('/^testing:concurrency:.*:result$/'))
             ->andReturnUsing(function ($key) use (&$taskId) {
@@ -209,16 +212,17 @@ class RedisDriverTest extends TestCase
                 if (str_replace(':result', '', $key) === $taskId) {
                     return serialize(['error' => 'Test exception']);
                 }
+
                 return null;
             });
 
         // Create and test the driver
         $driver = new RedisDriver($this->redisManager, 'default', $this->queuePrefix);
-        
+
         // Test for exception
         $this->expectException(Exception::class);
         $this->expectExceptionMessage('Test exception');
-        
+
         $driver->run($task);
     }
 }
