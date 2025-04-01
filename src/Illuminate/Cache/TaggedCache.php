ba7aa734217d60d4c25e922f4cfc31ec95ac190a
erikn69 <erikn_69@hotmail.com>
2025-04-01 09:27:12 -0500

    Support taggeable store flushed cache events (#55223)

--- src/Illuminate/Cache/TaggedCache.php
+++ src/Illuminate/Cache/TaggedCache.php
@@ -2,6 +2,8 @@
 
 namespace Illuminate\Cache;
 
+use Illuminate\Cache\Events\CacheFlushed;
+use Illuminate\Cache\Events\CacheFlushing;
 use Illuminate\Contracts\Cache\Store;
 
 class TaggedCache extends Repository
@@ -77,7 +79,11 @@ class TaggedCache extends Repository
      */
     public function flush()
     {
+        parent::event(new CacheFlushing($this->getName()));
+
         $this->tags->reset();
+
+        parent::event(new CacheFlushed($this->getName()));
 
         return true;
     }
