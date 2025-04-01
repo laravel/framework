laravel/framework

commit 14598f62bf8305bbe2a94ee4e2848d6ec2e05587

feat: attribute mapping in favor of potentially indexed mapping

src/Illuminate/Validation/Rules/AnyOf.php
@@ -60,8 +60,8 @@ class AnyOf implements Rule, ValidatorAwareRule
 
         foreach ($this->rules as $rule) {
             $validator = Validator::make(
-                Arr::wrap($value),
-                Arr::isAssoc(Arr::wrap($rule)) ? $rule : [$rule],
+                Arr::isAssoc(Arr::wrap($value)) ? $value : [$attribute => $value],
+                Arr::isAssoc(Arr::wrap($rule)) ? $rule : [$attribute => $rule],
                 $this->validator->customMessages,
                 $this->validator->customAttributes
             );

commit 9366e99da9b06430825cec7645a1bcc113e275db

fix(wip): regression introduced in 14598f62bf8305bbe2a94ee4e2848d6ec2e05587
https://github.com/laravel/framework/pull/55191#issuecomment-2770383023

src/Illuminate/Validation/Rules/AnyOf.php
@@ -51,8 +51,8 @@ class AnyOf implements Rule, ValidatorAwareRule
     {
         foreach ($this->rules as $rule) {
             $validator = Validator::make(
-                Arr::isAssoc(Arr::wrap($value)) ? $value : [$attribute => $value],
-                Arr::isAssoc(Arr::wrap($rule)) ? $rule : [$attribute => $rule],
+                Arr::isAssoc(Arr::wrap($value)) ? $value : [$value],
+                Arr::isAssoc(Arr::wrap($rule)) ? $rule : [$rule],
                 $this->validator->customMessages,
                 $this->validator->customAttributes
             );

tests/Validation/ValidationAnyOfRuleTest.php
@@ -221,6 +221,34 @@ class ValidationAnyOfRuleTest extends TestCase
         $this->assertFalse($validator->passes());
     }
 
+    public function testStarRuleSimple()
+    {
+        $rule = [
+            'persons.*.age' => ['required', Rule::anyOf([
+                ['min:10'],
+                ['integer'],
+            ])],
+        ];
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['age' => 12],
+                ['age' => 'foobar'],
+            ],
+        ], $rule);
+
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['age' => 12],
+                ['age' => 'foobarbazqux'],
+            ],
+        ], $rule);
+
+        $this->assertTrue($validator->passes());
+    }
+
     protected function setUpRuleSets()
     {
         $this->taggedUnionRules = [