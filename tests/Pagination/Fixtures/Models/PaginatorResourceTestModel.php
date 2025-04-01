c738f7f26f38a68c0423f72ff208de22f7c43d31
Taylor Otwell <taylor@laravel.com>
2025-04-01 15:03:06 -0500

    formatting

---
+++ tests/Pagination/Fixtures/Models/PaginatorResourceTestModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Illuminate\Tests\Pagination\Fixtures\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+
+class PaginatorResourceTestModel extends Model
+{
+    //
+}
--- tests/Pagination/PaginatorResourceTest.php
+++ tests/Pagination/PaginatorResourceTest.php
@@ -5,6 +5,7 @@ namespace Illuminate\Tests\Pagination;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Tests\Pagination\Fixtures\Models\PaginatorResourceTestModel;
 use PHPUnit\Framework\TestCase;
 
 class PaginatorResourceTest extends TestCase
@@ -23,7 +24,7 @@ class PaginatorResourceTest extends TestCase
     public function testItThrowsExceptionWhenResourceCannotBeFound()
     {
         $this->expectException(\LogicException::class);
-        $this->expectExceptionMessage('Failed to find resource class for model [Illuminate\Tests\Pagination\PaginatorResourceTestModel].');
+        $this->expectExceptionMessage('Failed to find resource class for model [Illuminate\Tests\Pagination\Fixtures\Models\PaginatorResourceTestModel].');
 
         $paginator = new PaginatorResourceTestPaginator([
             new PaginatorResourceTestModel(),
@@ -38,17 +39,12 @@ class PaginatorResourceTest extends TestCase
             new PaginatorResourceTestModel(),
         ], 1, 1, 1);
 
-        class_alias(PaginatorResourceTestResource::class, 'App\Http\Resources\PaginatorResourceTestModelResource');
+        class_alias(PaginatorResourceTestResource::class, 'Illuminate\Tests\Pagination\Fixtures\Http\Resources\PaginatorResourceTestModelResource');
 
         $resource = $paginator->toResourceCollection();
 
         $this->assertInstanceOf(JsonResource::class, $resource);
     }
-}
-
-class PaginatorResourceTestModel extends Model
-{
-    //
 }
 
 class PaginatorResourceTestResource extends JsonResource
