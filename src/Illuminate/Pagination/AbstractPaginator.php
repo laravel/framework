83d07e96740af19d2f0c9ce6f1db9b50de3c064b
Günther Debrauwer <debrauwer.gunther@gmail.com>
2025-04-02 19:00:22 +0200

    [12.x] `AbstractPaginator` should implement `CanBeEscapedWhenCastToString` (#55256)
    * Add failing test
    
    * Update AbstractPaginator
    
    * Update AbstractPaginator.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Pagination/AbstractPaginator.php
+++ src/Illuminate/Pagination/AbstractPaginator.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Pagination;
 
 use Closure;
+use Illuminate\Contracts\Support\CanBeEscapedWhenCastToString;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
@@ -18,7 +19,7 @@ use Traversable;
  *
  * @mixin \Illuminate\Support\Collection<TKey, TValue>
  */
-abstract class AbstractPaginator implements Htmlable, Stringable
+abstract class AbstractPaginator implements CanBeEscapedWhenCastToString, Htmlable, Stringable
 {
     use ForwardsCalls, Tappable;
 
@@ -70,6 +71,13 @@ abstract class AbstractPaginator implements Htmlable, Stringable
      * @var string
      */
     protected $pageName = 'page';
+
+    /**
+     * Indicates that the paginator's string representation should be escaped when __toString is invoked.
+     *
+     * @var bool
+     */
+    protected $escapeWhenCastingToString = false;
 
     /**
      * The number of links to display on each side of current page link.
@@ -797,6 +805,21 @@ abstract class AbstractPaginator implements Htmlable, Stringable
      */
     public function __toString()
     {
-        return (string) $this->render();
+        return $this->escapeWhenCastingToString
+            ? e((string) $this->render())
+            : (string) $this->render();
+    }
+
+    /**
+     * Indicate that the paginator's string representation should be escaped when __toString is invoked.
+     *
+     * @param  bool  $escape
+     * @return $this
+     */
+    public function escapeWhenCastingToString($escape = true)
+    {
+        $this->escapeWhenCastingToString = $escape;
+
+        return $this;
     }
 }
