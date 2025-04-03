34e05cb8a708f847367810cf9f4f52407692c445
taylorotwell <463230+taylorotwell@users.noreply.github.com>
2025-04-03 15:01:23 +0000

    Update version to v12.7.0

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -45,7 +45,7 @@ class Application extends Container implements ApplicationContract, CachesConfig
      *
      * @var string
      */
-    const VERSION = '12.6.0';
+    const VERSION = '12.7.0';
 
     /**
      * The base path for the Laravel installation.
