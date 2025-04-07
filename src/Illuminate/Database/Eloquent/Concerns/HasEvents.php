f404792bb8305406681a31cd811e6015b16e6f39
Will Rowe <wrowe@will-rowe.com>
2025-04-07 12:09:03 -0400

    Use `whenBooted` instead of registering an event listener
    - This removes any reliance on the event dispatcher, which complicated testing.

--- src/Illuminate/Database/Eloquent/Concerns/HasEvents.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasEvents.php
@@ -38,9 +38,7 @@ trait HasEvents
      */
     public static function bootHasEvents()
     {
-        static::registerModelEvent('booted', function () {
-            static::observe(static::resolveObserveAttributes());
-        });
+        static::whenBooted(fn () => static::observe(static::resolveObserveAttributes()));
     }
 
     /**
