497b57b8f0514d49ad6495448cd47fdb57b6ca22
Takayasu Oyama <t-oyama@colopl.co.jp>
2025-04-07 16:03:01 +0900

    [12.x] add generics to array types for Schema Grammars

--- src/Illuminate/Database/Schema/Grammars/SQLiteGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/SQLiteGrammar.php
@@ -319,7 +319,7 @@ class SQLiteGrammar extends Grammar
      *
      * @param  \Illuminate\Database\Schema\Blueprint  $blueprint
      * @param  \Illuminate\Support\Fluent  $command
-     * @return array|string
+     * @return list<string>|string
      */
     public function compileAlter(Blueprint $blueprint, Fluent $command)
     {
@@ -370,13 +370,7 @@ class SQLiteGrammar extends Grammar
         ], $indexes, [$foreignKeyConstraintsEnabled ? $this->compileEnableForeignKeyConstraints() : null]));
     }
 
-    /**
-     * Compile a change column command into a series of SQL statements.
-     *
-     * @param  \Illuminate\Database\Schema\Blueprint  $blueprint
-     * @param  \Illuminate\Support\Fluent  $command
-     * @return array|string
-     */
+    /** @inheritDoc */
     public function compileChange(Blueprint $blueprint, Fluent $command)
     {
         // Handled on table alteration...
@@ -526,7 +520,7 @@ class SQLiteGrammar extends Grammar
      *
      * @param  \Illuminate\Database\Schema\Blueprint  $blueprint
      * @param  \Illuminate\Support\Fluent  $command
-     * @return array|null
+     * @return list<string>|null
      */
     public function compileDropColumn(Blueprint $blueprint, Fluent $command)
     {
