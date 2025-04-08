laravel/framework

commit 14598f62bf8305bbe2a94ee4e2848d6ec2e05587

feat: attribute mapping in favor of potentially indexed mapping

src/Illuminate/Validation/Rules/AnyOf.php
@@ -60,8 +60,8 @@ class AnyOf implements Rule, ValidatorAwareRule
 
         foreach ($this->rules as $rule) {
             $validator = Validator::make(
-                Arr::wrap($value),
-                Arr::isAssoc(Arr::wrap($rule)) ? $rule : [$rule],
+                Arr::isAssoc(Arr::wrap($value)) ? $value : [$attribute => $value],
+                Arr::isAssoc(Arr::wrap($rule)) ? $rule : [$attribute => $rule],
                 $this->validator->customMessages,
                 $this->validator->customAttributes
             );

commit 08960dbd8c2843402455151c466497520488650d

[12.x] Introduce Rule::anyOf() for Validating Against Multiple Rule Sets (#55191)
* [12.x] introduce `Rule::oneOf()` (#https://github.com/laravel/framework/discussions/54880)

* chore: apply styleCI

* feat: add nested oneOf validation test

* chore: apply styleCI

* refactor: rename `oneof` into `anyof` to fit implementation

* fix: wrong failure message

* feat: update base tests

* feat: add test case

* chore: apply styleCI

* formatting

* feat: allow string fields

* feat: add test and clean nested rules

* chore: apply styleCI

* failing test

* Validation tests (#1)

* feat: add more validation tests

* wip: add failing test

* wip: add basic string rule validations

* chore: rename object fields for better debugging

* refactor: rename ruleSets to rules

* fix: respect array rule validation

---------

Co-authored-by: Christian Ascone <ascone.christian@gmail.com>

* fix: this should be passing because AnyOf has no type relevance and 'required' only checks to see if the field has something in it

---------

Co-authored-by: Christian Ascone <ascone.christian@gmail.com>

---------

Co-authored-by: Christian Ascone <ascone.christian@gmail.com>

* chore: correspond with recent changes https://github.com/brianferri/framework/pull/1/commits/de3b902a950b8f5ba8edaafa273f91d7c6ade295

* chore: remove unused private property

* feat: attribute mapping in favor of potentially indexed mapping

* feat: add more tests

* refactor(tests): remove unnecessary amount of tests, rename parameter properties to be more descriptive/analogous to use cases

* chore: apply styleCI

* feat: add tests to verify compliance with dot notation nesting validator

* formatting

* fix: remove messages

* fix(wip): regression introduced in 14598f62bf8305bbe2a94ee4e2848d6ec2e05587

https://github.com/laravel/framework/pull/55191#issuecomment-2770383023

* feat: implement star rule counter tests for simple and nested rules

Co-authored-by: Christian Ascone <ascone.christian@gmail.com>

src/Illuminate/Translation/lang/en/validation.php
@@ -21,6 +21,7 @@ return [
     'alpha' => 'The :attribute field must only contain letters.',
     'alpha_dash' => 'The :attribute field must only contain letters, numbers, dashes, and underscores.',
     'alpha_num' => 'The :attribute field must only contain letters and numbers.',
+    'any_of' => 'The :attribute field is invalid.',
     'array' => 'The :attribute field must be an array.',
     'ascii' => 'The :attribute field must only contain single-byte alphanumeric characters and symbols.',
     'before' => 'The :attribute field must be a date before :date.',

src/Illuminate/Validation/Rule.php
@@ -5,6 +5,7 @@ namespace Illuminate\Validation;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Traits\Macroable;
+use Illuminate\Validation\Rules\AnyOf;
 use Illuminate\Validation\Rules\ArrayRule;
 use Illuminate\Validation\Rules\Can;
 use Illuminate\Validation\Rules\Date;
@@ -244,6 +245,19 @@ class Rule
     public static function numeric()
     {
         return new Numeric;
+    }
+
+    /**
+     * Get an "any of" rule builder instance.
+     *
+     * @param  array
+     * @return \Illuminate\Validation\Rules\AnyOf
+     *
+     * @throws \InvalidArgumentException
+     */
+    public static function anyOf($rules)
+    {
+        return new AnyOf($rules);
     }
 
     /**

src/Illuminate/Validation/Rules/AnyOf.php
@@ -1,0 +1,94 @@
+<?php
+
+namespace Illuminate\Validation\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Contracts\Validation\ValidatorAwareRule;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
+
+class AnyOf implements Rule, ValidatorAwareRule
+{
+    /**
+     * The rules to match against.
+     *
+     * @var array
+     */
+    protected array $rules = [];
+
+    /**
+     * The validator performing the validation.
+     *
+     * @var \Illuminate\Validation\Validator
+     */
+    protected $validator;
+
+    /**
+     * Sets the validation rules to match against.
+     *
+     * @param  Illuminate\Contracts\Validation\ValidationRule[][]  $rules
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($rules)
+    {
+        if (! is_array($rules)) {
+            throw new InvalidArgumentException('The provided value must be an array of validation rules.');
+        }
+
+        $this->rules = $rules;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        foreach ($this->rules as $rule) {
+            $validator = Validator::make(
+                Arr::isAssoc(Arr::wrap($value)) ? $value : [$value],
+                Arr::isAssoc(Arr::wrap($rule)) ? $rule : [$rule],
+                $this->validator->customMessages,
+                $this->validator->customAttributes
+            );
+
+            if ($validator->passes()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation error messages.
+     *
+     * @return array
+     */
+    public function message()
+    {
+        $message = $this->validator->getTranslator()->get('validation.any_of');
+
+        return $message === 'validation.any_of'
+            ? ['The :attribute field is invalid.']
+            : $message;
+    }
+
+    /**
+     * Set the current validator.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return $this
+     */
+    public function setValidator($validator)
+    {
+        $this->validator = $validator;
+
+        return $this;
+    }
+}

tests/Validation/ValidationAnyOfRuleTest.php
@@ -1,0 +1,376 @@
+<?php
+
+namespace Illuminate\Tests\Validation;
+
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Translation\ArrayLoader;
+use Illuminate\Translation\Translator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationServiceProvider;
+use Illuminate\Validation\Validator;
+use PHPUnit\Framework\TestCase;
+
+enum TaggedUnionDiscriminatorType: string
+{
+    case EMAIL = 'email';
+    case URL = 'url';
+}
+
+class ValidationAnyOfRuleTest extends TestCase
+{
+    private array $taggedUnionRules;
+    private array $dotNotationNestedRules;
+    private array $nestedRules;
+
+    public function testBasicValidation()
+    {
+        $rule = Rule::anyOf([
+            ['required', 'uuid:4'],
+            ['required', 'email'],
+        ]);
+        $idRule = ['id' => $rule];
+        $requiredIdRule = ['id' => ['required', $rule]];
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => 'taylor@laravel.com',
+        ], $idRule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [], $idRule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [], $requiredIdRule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => '3c8ff5cb-4bc1-457b-a477-1833c477b254',
+        ], $idRule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => null,
+        ], $idRule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => '',
+        ], $idRule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => '',
+        ], $requiredIdRule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => 'abc',
+        ], $idRule);
+        $this->assertFalse($validator->passes());
+    }
+
+    public function testBasicStringValidation()
+    {
+        $rule = Rule::anyOf([
+            'required|uuid:4',
+            'required|email',
+        ]);
+        $idRule = ['id' => $rule];
+        $requiredIdRule = ['id' => ['required', $rule]];
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => 'test@example.com',
+        ], $idRule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [], $idRule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [], $requiredIdRule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => '3c8ff5cb-4bc1-457b-a477-1833c477b254',
+        ], $idRule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => null,
+        ], $idRule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => '',
+        ], $idRule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => '',
+        ], $requiredIdRule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'id' => 'abc',
+        ], $idRule);
+        $this->assertFalse($validator->passes());
+    }
+
+    public function testTaggedUnionObjects()
+    {
+        $validator = new Validator(resolve('translator'), [
+            'data' => [
+                'type' => TaggedUnionDiscriminatorType::EMAIL->value,
+                'email' => 'taylor@laravel.com',
+            ],
+        ], ['data' => Rule::anyOf($this->taggedUnionRules)]);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'data' => [
+                'type' => TaggedUnionDiscriminatorType::EMAIL->value,
+                'email' => 'invalid-email',
+            ],
+        ], ['data' => Rule::anyOf($this->taggedUnionRules)]);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'data' => [
+                'type' => TaggedUnionDiscriminatorType::URL->value,
+                'url' => 'http://laravel.com',
+            ],
+        ], ['data' => Rule::anyOf($this->taggedUnionRules)]);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'data' => [
+                'type' => TaggedUnionDiscriminatorType::URL->value,
+                'url' => 'not-a-url',
+            ],
+        ], ['data' => Rule::anyOf($this->taggedUnionRules)]);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'data' => [
+                'type' => TaggedUnionDiscriminatorType::EMAIL->value,
+                'url' => 'url-should-not-be-present-with-email-discriminator',
+            ],
+        ], ['data' => Rule::anyOf($this->taggedUnionRules)]);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'data' => [
+                'type' => 'doesnt-exist',
+                'email' => 'taylor@laravel.com',
+            ],
+        ], ['data' => Rule::anyOf($this->taggedUnionRules)]);
+        $this->assertFalse($validator->passes());
+    }
+
+    public function testNestedValidation()
+    {
+        $validator = new Validator(resolve('translator'), [
+            'user' => [
+                'identifier' => 1,
+                'properties' => [
+                    'name' => 'Taylor',
+                    'surname' => 'Otwell',
+                ],
+            ],
+        ], $this->nestedRules);
+        $this->assertTrue($validator->passes());
+        $validator->setRules($this->dotNotationNestedRules);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'user' => [
+                'identifier' => 'taylor@laravel.com',
+                'properties' => [
+                    'bio' => 'biography',
+                    'name' => 'Taylor',
+                    'surname' => 'Otwell',
+                ],
+            ],
+        ], $this->nestedRules);
+        $this->assertTrue($validator->passes());
+        $validator->setRules($this->dotNotationNestedRules);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'user' => [
+                'identifier' => 'taylor@laravel.com',
+                'properties' => [
+                    'name' => null,
+                    'surname' => 'Otwell',
+                ],
+            ],
+        ], $this->nestedRules);
+        $this->assertFalse($validator->passes());
+        $validator->setRules($this->dotNotationNestedRules);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'user' => [
+                'properties' => [
+                    'name' => 'Taylor',
+                    'surname' => 'Otwell',
+                ],
+            ],
+        ], $this->nestedRules);
+        $this->assertFalse($validator->passes());
+        $validator->setRules($this->dotNotationNestedRules);
+        $this->assertFalse($validator->passes());
+    }
+
+    public function testStarRuleSimple()
+    {
+        $rule = [
+            'persons.*.age' => ['required', Rule::anyOf([
+                ['min:10'],
+                ['integer'],
+            ])],
+        ];
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['age' => 12],
+                ['age' => 'foobar'],
+            ],
+        ], $rule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['age' => 'foobarbazqux'],
+                ['month' => 12],
+            ],
+        ], $rule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['age' => 12],
+                ['age' => 'foobarbazqux'],
+            ],
+        ], $rule);
+        $this->assertTrue($validator->passes());
+    }
+
+    public function testStarRuleNested()
+    {
+        $rule = [
+            'persons.*.birth' => ['required', Rule::anyOf([
+                ['year' => 'required|integer'],
+                'required|min:10',
+            ])],
+        ];
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['age' => ['year' => 12]],
+            ],
+        ], $rule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['birth' => ['month' => 12]],
+            ],
+        ], $rule);
+        $this->assertFalse($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['birth' => ['year' => 12]],
+            ],
+        ], $rule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['birth' => 'foobarbazqux'],
+                ['birth' => [
+                    'year' => 12,
+                ]],
+            ],
+        ], $rule);
+        $this->assertTrue($validator->passes());
+
+        $validator = new Validator(resolve('translator'), [
+            'persons' => [
+                ['birth' => 'foobar'],
+                ['birth' => [
+                    'year' => 12,
+                ]],
+            ],
+        ], $rule);
+        $this->assertFalse($validator->passes());
+    }
+
+    protected function setUpRuleSets()
+    {
+        $this->taggedUnionRules = [
+            [
+                'type' => ['required', Rule::in([TaggedUnionDiscriminatorType::EMAIL])],
+                'email' => ['required', 'email:rfc'],
+            ],
+            [
+                'type' => ['required', Rule::in([TaggedUnionDiscriminatorType::URL])],
+                'url' => ['required', 'url:http,https'],
+            ],
+        ];
+
+        // Using AnyOf as nesting feature
+        $this->nestedRules = [
+            'user' => Rule::anyOf([
+                [
+                    'identifier' => ['required', Rule::anyOf([
+                        'email:rfc',
+                        'integer',
+                    ])],
+                    'properties' => ['required', Rule::anyOf([
+                        [
+                            'bio' => 'nullable',
+                            'name' => 'required',
+                            'surname' => 'required',
+                        ],
+                    ])],
+                ],
+            ]),
+        ];
+
+        $this->dotNotationNestedRules = [
+            'user.identifier' => ['required', Rule::anyOf([
+                'email:rfc',
+                'integer',
+            ])],
+            'user.properties.bio' => 'nullable',
+            'user.properties.name' => 'required',
+            'user.properties.surname' => 'required',
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $container = Container::getInstance();
+        $container->bind('translator', function () {
+            return new Translator(
+                new ArrayLoader,
+                'en'
+            );
+        });
+
+        Facade::setFacadeApplication($container);
+        (new ValidationServiceProvider($container))->register();
+
+        $this->setUpRuleSets();
+    }
+
+    protected function tearDown(): void
+    {
+        Container::setInstance(null);
+        Facade::clearResolvedInstances();
+        Facade::setFacadeApplication(null);
+    }
+}