6fc9c926ba345a7abcefd1ef45adf94248ae34a0
Alexandr Chernyaev <bliz48rus@gmail.com>
2025-04-10 01:05:58 +0300

    Add test for handling files with GET parameters in Vite

--- tests/Foundation/FoundationViteTest.php
+++ tests/Foundation/FoundationViteTest.php
@@ -1693,6 +1693,39 @@ class FoundationViteTest extends TestCase
         $this->cleanViteManifest($buildDir);
     }
 
+    public function testItCorrectlyHandlesFilesWithGetParameters(): void
+    {
+        $buildDir = Str::random();
+        $this->makeViteManifest([
+            "resources/js/app.js"     => [
+                "file"    => "assets/app.js?v=a42ae7b4",
+                "isEntry" => true,
+                "src"     => "resources/js/app.js",
+            ],
+            "resources/sass/app.scss" => [
+                "file"    => "assets/app.css?v=2b2b9de9",
+                "isEntry" => true,
+                "src"     => "resources/sass/app.scss",
+            ],
+        ], $buildDir);
+
+        $result = app(Vite::class)
+            ->useBuildDirectory($buildDir)
+            ->withEntryPoints(['resources/js/app.js', 'resources/sass/app.scss']);
+
+        $this->assertStringContainsString(
+            '<link rel="stylesheet" href="https://example.com/' . $buildDir . '/assets/app.css?v=2b2b9de9" />',
+            $result->toHtml()
+        );
+
+        $this->assertStringContainsString(
+            '<script type="module" src="https://example.com/' . $buildDir . '/assets/app.js?v=a42ae7b4"></script>',
+            $result->toHtml()
+        );
+
+        $this->cleanViteManifest($buildDir);
+    }
+
     protected function cleanViteManifest($path = 'build')
     {
         if (file_exists(public_path("{$path}/manifest.json"))) {
