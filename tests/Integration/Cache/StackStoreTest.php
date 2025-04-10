f8f8a115b3694e28d048e1603733669fe416b1b6
Tim MacDonald <hello@timacdonald.me>
2025-04-11 08:14:54 +1000

    wip

---
+++ tests/Integration/Cache/StackStoreTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Cache;
+
+use DateTime;
+use Illuminate\Cache\RedisStore;
+use Illuminate\Cache\StackStore;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithRedis;
+use Illuminate\Redis\Connections\PhpRedisClusterConnection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Sleep;
+use Mockery as m;
+use Orchestra\Testbench\TestCase;
+
+class StackStoreTest extends TestCase
+{
+    use InteractsWithRedis;
+
+    /** {@inheritdoc} */
+    #[\Override]
+    protected function setUp(): void
+    {
+        $this->afterApplicationCreated(function () {
+            $this->setUpRedis();
+        });
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->tearDownRedis();
+        });
+
+        parent::setUp();
+    }
+
+    public function test_get()
+    {
+        Cache::extend('stack:redis,database', function () {
+            return new StackStore;
+        });
+        $cache = Cache::store('stack');
+
+            dd($cache);
+    }
+}
