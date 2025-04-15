634ec451d58e0953d796405b4400bd582414f04a
Iman <imanghafoori1@gmail.com>
2025-04-15 18:56:22 +0330

    Add tests for Cloud.php

--- tests/Integration/Foundation/CloudTest.php
+++ tests/Integration/Foundation/CloudTest.php
@@ -2,7 +2,11 @@
 
 namespace Illuminate\Tests\Integration\Foundation;
 
+use Illuminate\Database\Migrations\Migrator;
+use Illuminate\Foundation\Bootstrap\HandleExceptions;
 use Illuminate\Foundation\Cloud;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Handler\SocketHandler;
 use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\TestCase;
 
@@ -51,5 +55,93 @@ class CloudTest extends TestCase
         $this->assertEquals('test-access-key-id', $this->app['config']->get('filesystems.disks.test-disk.key'));
 
         unset($_SERVER['LARAVEL_CLOUD_DISK_CONFIG']);
+    }
+
+    public function test_it_can_bootstrap_bootstraped()
+    {
+        Cloud::bootstrapperBootstrapped($this->app, 'random_string');
+        $this->assertEquals(null, $this->app['config']->get('logging.channels.laravel-cloud-socket'));
+
+        Cloud::bootstrapperBootstrapped($this->app, HandleExceptions::class);
+        unset($_ENV['LARAVEL_CLOUD_LOG_SOCKET']);
+        $this->assertEquals(['includeStacktraces' => true], $this->app['config']->get('logging.channels.stderr.formatter_with'));
+        $this->assertEquals([
+            'driver' => 'monolog',
+            'handler' => SocketHandler::class,
+            'formatter' => JsonFormatter::class,
+            'formatter_with' => [
+                'includeStacktraces' => true,
+            ],
+            'with' => [
+                'connectionString' => 'unix:///tmp/cloud-init.sock',
+                'persistent' => true,
+            ],
+        ], $this->app['config']->get('logging.channels.laravel-cloud-socket'));
+
+        $_ENV['LARAVEL_CLOUD_LOG_SOCKET'] = 'my_random_socket 1';
+        $_SERVER['LARAVEL_CLOUD_LOG_SOCKET'] = 'my_random_socket 2';
+
+        Cloud::bootstrapperBootstrapped($this->app, HandleExceptions::class);
+
+        $this->assertEquals([
+            'driver' => 'monolog',
+            'handler' => SocketHandler::class,
+            'formatter' => JsonFormatter::class,
+            'formatter_with' => [
+                'includeStacktraces' => true,
+            ],
+            'with' => [
+                'connectionString' => 'my_random_socket 1',
+                'persistent' => true,
+            ],
+        ], $this->app['config']->get('logging.channels.laravel-cloud-socket'));
+
+        unset($_ENV['LARAVEL_CLOUD_LOG_SOCKET']);
+        unset($_SERVER['LARAVEL_CLOUD_LOG_SOCKET']);
+    }
+
+    public function test_it_configures_migrator_to_use_unpooled_pgsql()
+    {
+        $this->app['config']->set('database.connections.pgsql-unpooled', null);
+
+        Cloud::ensureMigrationsUseUnpooledConnection($this->app);
+        $callback = MyMigrator::getResolver();
+        $this->assertNull($callback);
+
+        $this->app['config']->set('database.connections.pgsql-unpooled', []);
+
+        Cloud::ensureMigrationsUseUnpooledConnection($this->app);
+        $callback = MyMigrator::getResolver();
+
+        $spyResolver = new class
+        {
+            public $connection;
+
+            public function connection($connectionName)
+            {
+                $this->connection = $connectionName;
+            }
+        };
+
+        $callback($spyResolver, 'pgsql');
+        $this->assertEquals('pgsql-unpooled', $spyResolver->connection);
+
+        $callback($spyResolver, 'mysql');
+        $this->assertEquals('mysql', $spyResolver->connection);
+
+        MyMigrator::unsetResolver();
+    }
+}
+
+class MyMigrator extends Migrator
+{
+    public static function getResolver()
+    {
+        return self::$connectionResolverCallback;
+    }
+
+    public static function unsetResolver()
+    {
+        self::$connectionResolverCallback = null;
     }
 }
