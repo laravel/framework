2b332acecc18d1ba04074876f8dc20c1a6cbb5d1
Ostap Brehin <osbre@protonmail.com>
2025-04-17 22:42:03 +0100

    Test `@use` directive without quotes

--- tests/View/Blade/BladeUseTest.php
+++ tests/View/Blade/BladeUseTest.php
@@ -6,29 +6,45 @@ class BladeUseTest extends AbstractBladeTestCase
 {
     public function testUseStatementsAreCompiled()
     {
-        $string = "Foo @use('SomeNamespace\SomeClass', 'Foo') bar";
         $expected = "Foo <?php use \SomeNamespace\SomeClass as Foo; ?> bar";
+
+        $string = "Foo @use('SomeNamespace\SomeClass', 'Foo') bar";
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+
+        $string = "Foo @use(SomeNamespace\SomeClass, Foo) bar";
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 
     public function testUseStatementsWithoutAsAreCompiled()
     {
-        $string = "Foo @use('SomeNamespace\SomeClass') bar";
         $expected = "Foo <?php use \SomeNamespace\SomeClass; ?> bar";
+
+        $string = "Foo @use('SomeNamespace\SomeClass') bar";
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+
+        $string = "Foo @use(SomeNamespace\SomeClass) bar";
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 
     public function testUseStatementsWithBackslashAtBeginningAreCompiled()
     {
-        $string = "Foo @use('\SomeNamespace\SomeClass') bar";
         $expected = "Foo <?php use \SomeNamespace\SomeClass; ?> bar";
+
+        $string = "Foo @use('\SomeNamespace\SomeClass') bar";
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+
+        $string = "Foo @use(\SomeNamespace\SomeClass) bar";
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 
     public function testUseStatementsWithBackslashAtBeginningAndAliasedAreCompiled()
     {
-        $string = "Foo @use('\SomeNamespace\SomeClass', 'Foo') bar";
         $expected = "Foo <?php use \SomeNamespace\SomeClass as Foo; ?> bar";
+
+        $string = "Foo @use('\SomeNamespace\SomeClass', 'Foo') bar";
+        $this->assertEquals($expected, $this->compiler->compileString($string));
+
+        $string = "Foo @use(\SomeNamespace\SomeClass, Foo) bar";
         $this->assertEquals($expected, $this->compiler->compileString($string));
     }
 }
