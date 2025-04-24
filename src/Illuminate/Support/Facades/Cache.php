53f41419db21cb76fceddeb0ae52c655f10b2cc9
timacdonald <24803032+timacdonald@users.noreply.github.com>
2025-04-24 23:41:56 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/Cache.php
+++ src/Illuminate/Support/Facades/Cache.php
@@ -5,6 +5,7 @@ namespace Illuminate\Support\Facades;
 /**
  * @method static \Illuminate\Contracts\Cache\Repository store(string|null $name = null)
  * @method static \Illuminate\Contracts\Cache\Repository driver(string|null $driver = null)
+ * @method static \Illuminate\Contracts\Cache\Repository memo(string|null $driver = null)
  * @method static \Illuminate\Contracts\Cache\Repository resolve(string $name)
  * @method static \Illuminate\Cache\Repository build(array $config)
  * @method static \Illuminate\Cache\Repository repository(\Illuminate\Contracts\Cache\Store $store, array $config = [])
