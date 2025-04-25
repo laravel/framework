53c62602d55d1b36056a20445857ddf0e72fc385
Günther Debrauwer <debrauwer.gunther@gmail.com>
2025-04-25 16:20:16 +0200

    Respect pendingAttributes in factories

--- src/Illuminate/Database/Eloquent/Factories/BelongsToManyRelationship.php
+++ src/Illuminate/Database/Eloquent/Factories/BelongsToManyRelationship.php
@@ -50,7 +50,9 @@ class BelongsToManyRelationship
      */
     public function createFor(Model $model)
     {
-        Collection::wrap($this->factory instanceof Factory ? $this->factory->create([], $model) : $this->factory)->each(function ($attachable) use ($model) {
+        $relationship = $model->{$this->relationship}();
+
+        Collection::wrap($this->factory instanceof Factory ? $this->factory->prependState($relationship->getQuery()->pendingAttributes)->create([], $model) : $this->factory)->each(function ($attachable) use ($model) {
             $model->{$this->relationship}()->attach(
                 $attachable,
                 is_callable($this->pivot) ? call_user_func($this->pivot, $model) : $this->pivot
