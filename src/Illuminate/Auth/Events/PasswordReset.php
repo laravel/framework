711e9acfb1322126569268eacf112b8fccac22fa
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2025-04-30 00:11:37 +0600

    fix: styleci issue

--- src/Illuminate/Auth/Events/PasswordReset.php
+++ src/Illuminate/Auth/Events/PasswordReset.php
@@ -2,8 +2,8 @@
 
 namespace Illuminate\Auth\Events;
 
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class PasswordReset
 {
