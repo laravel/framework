b9342ff755dfea947e9d22687d0a0169d568b09e
taylorotwell <463230+taylorotwell@users.noreply.github.com>
2025-04-30 14:33:58 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/Schema.php
+++ src/Illuminate/Support/Facades/Schema.php
@@ -44,7 +44,7 @@ namespace Illuminate\Support\Facades;
  * @method static string|null getCurrentSchemaName()
  * @method static array parseSchemaAndTable(string $reference, string|bool|null $withDefaultSchema = null)
  * @method static \Illuminate\Database\Connection getConnection()
- * @method static void blueprintResolver(\Closure $resolver)
+ * @method static void blueprintResolver(\Closure|null $resolver)
  * @method static void macro(string $name, object|callable $macro)
  * @method static void mixin(object $mixin, bool $replace = true)
  * @method static bool hasMacro(string $name)
