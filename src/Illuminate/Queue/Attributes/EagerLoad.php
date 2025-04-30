6550c65078539f96f2da37d58cdecd95dfe69f9c
Tim MacDonald <hello@timacdonald.me>
2025-05-01 08:36:34 +1000

    Improve typing

--- src/Illuminate/Queue/Attributes/EagerLoad.php
+++ src/Illuminate/Queue/Attributes/EagerLoad.php
@@ -7,6 +7,9 @@ use Attribute;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class EagerLoad
 {
+    /**
+     * @param  list<string>  $relations
+     */
     public function __construct(public array $relations)
     {
         //
