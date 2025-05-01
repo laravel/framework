eb7be29c680bc556f05dc151f87dfa235d639a36
Achraf AAMRI <36072352+achrafAa@users.noreply.github.com>
2025-05-01 23:52:45 +0100

    Add tests for background command execution with and without explicit return

--- tests/Integration/Console/Scheduling/ScheduleRunCommandTest.php
+++ tests/Integration/Console/Scheduling/ScheduleRunCommandTest.php
@@ -151,4 +151,66 @@ class ScheduleRunCommandTest extends TestCase
         Event::assertDispatched(ScheduledTaskFinished::class);
         Event::assertNotDispatched(ScheduledTaskFailed::class);
     }
+
+    /**
+     * @throws BindingResolutionException
+     */
+    public function test_successful_command_in_background_does_not_trigger_event()
+    {
+        Event::fake([
+            ScheduledTaskStarting::class,
+            ScheduledTaskFinished::class,
+            ScheduledTaskFailed::class,
+        ]);
+
+        // Create a schedule and add the command
+        $schedule = $this->app->make(Schedule::class);
+        $task = $schedule->exec('exit 0')
+            ->everyMinute()
+            ->runInBackground();
+
+        // Make sure it will run regardless of schedule
+        $task->when(function () {
+            return true;
+        });
+
+        // Execute the scheduler
+        $this->artisan('schedule:run');
+
+        // Verify the event sequence
+        Event::assertDispatched(ScheduledTaskStarting::class);
+        Event::assertDispatched(ScheduledTaskFinished::class);
+        Event::assertNotDispatched(ScheduledTaskFailed::class);
+    }
+
+    /**
+     * @throws BindingResolutionException
+     */
+    public function test_command_with_no_explicit_return_in_background_does_not_trigger_event()
+    {
+        Event::fake([
+            ScheduledTaskStarting::class,
+            ScheduledTaskFinished::class,
+            ScheduledTaskFailed::class,
+        ]);
+
+        // Create a schedule and add the command that just performs an action without explicit exit
+        $schedule = $this->app->make(Schedule::class);
+        $task = $schedule->exec('true')
+            ->everyMinute()
+            ->runInBackground();
+
+        // Make sure it will run regardless of schedule
+        $task->when(function () {
+            return true;
+        });
+
+        // Execute the scheduler
+        $this->artisan('schedule:run');
+
+        // Verify the event sequence
+        Event::assertDispatched(ScheduledTaskStarting::class);
+        Event::assertDispatched(ScheduledTaskFinished::class);
+        Event::assertNotDispatched(ScheduledTaskFailed::class);
+    }
 }
