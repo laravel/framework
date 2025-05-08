a81d05db52dc138856c7ecc187287775cbad21f7
Taylor Otwell <taylor@laravel.com>
2025-05-08 07:28:23 -0700

    formatting

--- src/Illuminate/Support/Number.php
+++ src/Illuminate/Support/Number.php
@@ -166,7 +166,10 @@ class Number
     public static function fileSize(int|float $bytes, int $precision = 0, ?int $maxPrecision = null, bool $useBinaryPrefix = false)
     {
         $base = $useBinaryPrefix ? 1024 : 1000;
-        $units = $useBinaryPrefix ? ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB', 'RiB', 'QiB'] : ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'RB', 'QB'];
+
+        $units = $useBinaryPrefix
+            ? ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB', 'RiB', 'QiB']
+            : ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'RB', 'QB'];
 
         for ($i = 0; ($bytes / $base) > 0.9 && ($i < count($units) - 1); $i++) {
             $bytes /= $base;
