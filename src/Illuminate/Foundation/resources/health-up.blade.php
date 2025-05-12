ed877a09814c280d92e69e9958ac74e240333fcb
Gabriel R. Barbosa <gabrieltads2016@gmail.com>
2025-05-11 22:49:33 -0300

    Update font import in health-up.blade.php

--- src/Illuminate/Foundation/resources/health-up.blade.php
+++ src/Illuminate/Foundation/resources/health-up.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css2?family=Figtree:wght@400;600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
     <script src="https://cdn.tailwindcss.com"></script>
