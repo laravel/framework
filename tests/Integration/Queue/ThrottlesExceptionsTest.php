e72d741a49aea708d70447838db9e48a837ac1ca
Moshe Brodsky <44633930+moshe-autoleadstar@users.noreply.github.com>
2025-05-13 16:49:36 +0300

    Add deleteWhen for throttle exceptions job middleware (#55718)
    * Add deleteWhen for throttle exceptions job middleware
    
    * adding for redis and adding a test
    
    * attempt to fix test
    
    * add expectation for isReleased check
    
    * ok so its called twice
    
    * change terminology from delete to skip
    
    * formatting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- tests/Integration/Queue/ThrottlesExceptionsTest.php
+++ tests/Integration/Queue/ThrottlesExceptionsTest.php
@@ -40,6 +40,11 @@ class ThrottlesExceptionsTest extends TestCase
         $this->assertJobWasReleasedWithDelay(CircuitBreakerTestJob::class);
     }
 
+    public function testCircuitCanSkipJob()
+    {
+        $this->assertJobWasDeleted(CircuitBreakerSkipJob::class);
+    }
+
     protected function assertJobWasReleasedImmediately($class)
     {
         $class::$handled = false;
@@ -80,6 +85,27 @@ class ThrottlesExceptionsTest extends TestCase
         ]);
 
         $this->assertFalse($class::$handled);
+    }
+
+    protected function assertJobWasDeleted($class)
+    {
+        $class::$handled = false;
+        $instance = new CallQueuedHandler(new Dispatcher($this->app), $this->app);
+
+        $job = m::mock(Job::class);
+
+        $job->shouldReceive('hasFailed')->once()->andReturn(false);
+        $job->shouldReceive('delete')->once();
+        $job->shouldReceive('isDeleted')->andReturn(true);
+        $job->shouldReceive('isReleased')->twice()->andReturn(false);
+        $job->shouldReceive('isDeletedOrReleased')->once()->andReturn(true);
+        $job->shouldReceive('uuid')->andReturn('simple-test-uuid');
+
+        $instance->call($job, [
+            'command' => serialize($command = new $class),
+        ]);
+
+        $this->assertTrue($class::$handled);
     }
 
     protected function assertJobRanSuccessfully($class)
@@ -311,6 +337,25 @@ class CircuitBreakerTestJob
     public function middleware()
     {
         return [(new ThrottlesExceptions(2, 10 * 60))->by('test')];
+    }
+}
+
+class CircuitBreakerSkipJob
+{
+    use InteractsWithQueue, Queueable;
+
+    public static $handled = false;
+
+    public function handle()
+    {
+        static::$handled = true;
+
+        throw new Exception;
+    }
+
+    public function middleware()
+    {
+        return [(new ThrottlesExceptions(2, 10 * 60))->deleteWhen(Exception::class)];
     }
 }
 
