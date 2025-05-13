e134fe93db9ac415cbd342f897366086dab53635
Tony Lea <tony@devdojo.com>
2025-05-13 13:49:48 -0400

    Easily implement broadcasting in a React/Vue Typescript app (Starter Kits) (#55170)
    * Adding initial trial for the framework specific echo lib
    
    * few more updates
    
    * Adding functionality for vue composable
    
    * updating the react hook with updated config options
    
    * Adding the configure code injection step
    
    * Getting styleCI to pass
    
    * removing the useEcho stubs, instead will be added to laravel-echo npm package
    
    * fix spacing
    
    * fix spacing
    
    * fix spacing
    
    * making methods more efficient
    
    * making methods more efficient
    
    * updates to utilize the new packages
    
    * Update BroadcastingInstallCommand.php
    
    * better value detection for .env
    
    * Update BroadcastingInstallCommand.php
    
    * Update BroadcastingInstallCommand.php
    
    * Update BroadcastingInstallCommand.php
    
    * Update BroadcastingInstallCommand.php
    
    * Update BroadcastingInstallCommand.php
    
    * formatting
    
    * Update BroadcastingInstallCommand.php
    
    * formatting
    
    * writeVariable(s) env helpers
    
    * handle blank values cleanly
    
    * use the env variable writer
    
    * unhandle match case
    
    * no need to ask for public key
    
    * warn about pusher protocol support
    
    * move the ably warning up so that it's visible longer
    
    * enable
    
    * driver specific stubs
    
    * hopefully fix line endings
    
    ---------
    
    Co-authored-by: Joe Tannenbaum <joe.tannenbaum@laravel.com>
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Support/Env.php
+++ src/Illuminate/Support/Env.php
@@ -5,6 +5,8 @@ namespace Illuminate\Support;
 use Closure;
 use Dotenv\Repository\Adapter\PutenvAdapter;
 use Dotenv\Repository\RepositoryBuilder;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Filesystem\Filesystem;
 use PhpOption\Option;
 use RuntimeException;
 
@@ -112,6 +114,133 @@ class Env
     public static function getOrFail($key)
     {
         return self::getOption($key)->getOrThrow(new RuntimeException("Environment variable [$key] has no value."));
+    }
+
+    /**
+     * Write an array of key-value pairs to the environment file.
+     *
+     * @param  array  $variables
+     * @param  string  $pathToFile
+     * @param  bool  $overwrite
+     * @return void
+     *
+     * @throws RuntimeException
+     * @throws FileNotFoundException
+     */
+    public static function writeVariables(array $variables, string $pathToFile, bool $overwrite = false): void
+    {
+        $filesystem = new Filesystem;
+
+        if ($filesystem->missing($pathToFile)) {
+            throw new RuntimeException("The file [{$pathToFile}] does not exist.");
+        }
+
+        $lines = explode(PHP_EOL, $filesystem->get($pathToFile));
+
+        foreach ($variables as $key => $value) {
+            $lines = self::addVariableToEnvContents($key, $value, $lines, $overwrite);
+        }
+
+        $filesystem->put($pathToFile, implode(PHP_EOL, $lines));
+    }
+
+    /**
+     * Write a single key-value pair to the environment file.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  string  $pathToFile
+     * @param  bool  $overwrite
+     * @return void
+     *
+     * @throws RuntimeException
+     * @throws FileNotFoundException
+     */
+    public static function writeVariable(string $key, mixed $value, string $pathToFile, bool $overwrite = false): void
+    {
+        $filesystem = new Filesystem;
+
+        if ($filesystem->missing($pathToFile)) {
+            throw new RuntimeException("The file [{$pathToFile}] does not exist.");
+        }
+
+        $envContent = $filesystem->get($pathToFile);
+
+        $lines = explode(PHP_EOL, $envContent);
+        $lines = self::addVariableToEnvContents($key, $value, $lines, $overwrite);
+
+        $filesystem->put($pathToFile, implode(PHP_EOL, $lines));
+    }
+
+    /**
+     * Add a variable to the environment file contents.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $envLines
+     * @param  bool  $overwrite
+     * @return array
+     */
+    protected static function addVariableToEnvContents(string $key, mixed $value, array $envLines, bool $overwrite): array
+    {
+        $prefix = explode('_', $key)[0].'_';
+        $lastPrefixIndex = -1;
+
+        $shouldQuote = preg_match('/^[a-zA-z0-9]+$/', $value) === 0;
+
+        $lineToAddVariations = [
+            $key.'='.(is_string($value) ? '"'.addslashes($value).'"' : $value),
+            $key.'='.(is_string($value) ? "'".addslashes($value)."'" : $value),
+            $key.'='.$value,
+        ];
+
+        $lineToAdd = $shouldQuote ? $lineToAddVariations[0] : $lineToAddVariations[2];
+
+        if ($value === '') {
+            $lineToAdd = $key.'=';
+        }
+
+        foreach ($envLines as $index => $line) {
+            if (str_starts_with($line, $prefix)) {
+                $lastPrefixIndex = $index;
+            }
+
+            if (in_array($line, $lineToAddVariations)) {
+                // This exact line already exists, so we don't need to add it again.
+                return $envLines;
+            }
+
+            if ($line === $key.'=') {
+                // If the value is empty, we can replace it with the new value.
+                $envLines[$index] = $lineToAdd;
+
+                return $envLines;
+            }
+
+            if (str_starts_with($line, $key.'=')) {
+                if (! $overwrite) {
+                    return $envLines;
+                }
+
+                $envLines[$index] = $lineToAdd;
+
+                return $envLines;
+            }
+        }
+
+        if ($lastPrefixIndex === -1) {
+            if (count($envLines) && $envLines[count($envLines) - 1] !== '') {
+                $envLines[] = '';
+            }
+
+            return array_merge($envLines, [$lineToAdd]);
+        }
+
+        return array_merge(
+            array_slice($envLines, 0, $lastPrefixIndex + 1),
+            [$lineToAdd],
+            array_slice($envLines, $lastPrefixIndex + 1)
+        );
     }
 
     /**
