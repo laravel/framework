f8d768b461e71c70c1d6f144bc2bf450573ab4e1
Norman Huth <norman@huth.it>
2025-05-19 21:33:05 +0200

    Correct return in PhpDoc for command fail method

--- src/Illuminate/Console/Command.php
+++ src/Illuminate/Console/Command.php
@@ -263,7 +263,7 @@ class Command extends SymfonyCommand
      * Fail the command manually.
      *
      * @param  \Throwable|string|null  $exception
-     * @return void
+     * @return never
      *
      * @throws \Illuminate\Console\ManuallyFailedException|\Throwable
      */
