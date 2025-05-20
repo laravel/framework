a9bfc0d37d1580bdafe39735b12fa8656fb3f24b
Taylor Otwell <taylor@laravel.com>
2025-05-20 08:53:37 -0500

    simplify response streams when using generators

--- src/Illuminate/Routing/ResponseFactory.php
+++ src/Illuminate/Routing/ResponseFactory.php
@@ -12,6 +12,7 @@ use Illuminate\Routing\Exceptions\StreamedResponseException;
 use Illuminate\Support\Js;
 use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Macroable;
+use ReflectionFunction;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\StreamedJsonResponse;
 use Symfony\Component\HttpFoundation\StreamedResponse;
@@ -192,6 +193,16 @@ class ResponseFactory implements FactoryContract
      */
     public function stream($callback, $status = 200, array $headers = [])
     {
+        if ((new ReflectionFunction($callback))->isGenerator()) {
+            return new StreamedResponse(function () use ($callback) {
+                foreach ($callback() as $chunk) {
+                    echo $chunk;
+                    ob_flush();
+                    flush();
+                }
+            }, $status, array_merge($headers, ['X-Accel-Buffering' => 'no']));
+        }
+
         return new StreamedResponse($callback, $status, $headers);
     }
 
