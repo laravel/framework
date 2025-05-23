74a8e64ab2b98bb54efa6878c6b44307362bb0d9
jkpeyi <jkpeyi@gmail.com>
2025-05-23 15:25:23 +0100

    feat: add json helper function for standardized JSON responses

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -526,3 +526,16 @@ if (! function_exists('with')) {
         return is_null($callback) ? $value : $callback($value);
     }
 }
+
+
+if(!function_exists('json')){
+    function json(
+        $data = [],
+        $status = 200,
+        array $headers = [],
+        $options = 0
+    ) {
+        return response()->json($data, $status, $headers, $options);
+    }
+}
+
