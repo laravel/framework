894bb1c4d2ce27a0196d52a3eb9d89e3c3d9876a
BurtDS <bert.deswaef@gmail.com>
2025-05-27 17:00:43 +0200

    🔨 If Enums as default directory for new Enums

--- src/Illuminate/Foundation/Console/EnumMakeCommand.php
+++ src/Illuminate/Foundation/Console/EnumMakeCommand.php
@@ -72,7 +72,7 @@ class EnumMakeCommand extends GeneratorCommand
         return match (true) {
             is_dir(app_path('Enums')) => $rootNamespace.'\\Enums',
             is_dir(app_path('Enumerations')) => $rootNamespace.'\\Enumerations',
-            default => $rootNamespace,
+            default => $rootNamespace.'\\Enums',
         };
     }
 
