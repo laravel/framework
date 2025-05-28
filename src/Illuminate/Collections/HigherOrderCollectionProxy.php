3d084e4e9d10494c335e3e5515e87a14a3a0d860
Sergey Danilchenko <daniser@mail.ru>
2025-05-28 15:08:33 +0200

    [12.x] Ability to perform higher order static calls on collection items (#55880)
    * [12.x] Ability to perform higher order static calls on collection items
    
    * update test
    
    ---------
    
    Co-authored-by: Sergey Danilchenko <s.danilchenko@ttbooking.ru>

--- src/Illuminate/Collections/HigherOrderCollectionProxy.php
+++ src/Illuminate/Collections/HigherOrderCollectionProxy.php
@@ -61,7 +61,9 @@ class HigherOrderCollectionProxy
     public function __call($method, $parameters)
     {
         return $this->collection->{$this->method}(function ($value) use ($method, $parameters) {
-            return $value->{$method}(...$parameters);
+            return is_string($value)
+                ? $value::{$method}(...$parameters)
+                : $value->{$method}(...$parameters);
         });
     }
 }
