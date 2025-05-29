65e9a084b347d597a54faca8e3b239fc17c3e1d3
Yitz Willroth <ewillroth@insomniacookies.com>
2025-05-29 11:13:41 -0400

    test: add tests for handler resolution

---
+++ tests/Bus/BusHandlerResolutionTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Bus;
+
+use Illuminate\Bus\Attributes\HandledBy;
+use Illuminate\Bus\Dispatcher;
+use Illuminate\Container\Container;
+use PHPUnit\Framework\TestCase;
+
+class BusHandlerResolutionTest extends TestCase
+{
+    public function test_command_handler_correctly_resolved_via_attribute()
+    {
+        $resolution = (new Dispatcher(new Container()))
+            ->getCommandHandler(new CommandForAttributeHandler());
+        
+        $this->assertInstanceOf(AttributeHandler::class, $resolution);
+    }
+
+    public function test_command_handler_correctly_resolved_via_map()
+    {
+        $resolution = tap(
+            new Dispatcher(new Container()), 
+            function($dispatcher) {
+                $dispatcher->map([CommandForMapHandler::class => MapHandler::class]);
+        })->getCommandHandler(new CommandForMapHandler());
+        
+        $this->assertInstanceOf(MapHandler::class, $resolution);
+    }
+
+    public function test_handler_resolution_returns_false_when_no_attribute_or_mapping()
+    {
+        $resolution = (new Dispatcher(new Container()))
+            ->getCommandHandler(new CommandForMapHandler());
+        
+        $this->assertFalse($resolution);
+    }
+    
+    public function test_mapped_handler_takes_precedence_over_attribute_handler()
+    {
+        $resolution = tap(
+            new Dispatcher(new Container()), 
+            function($dispatcher) {
+                $dispatcher->map([CommandForAttributeHandler::class => MapHandler::class]);
+        })->getCommandHandler(new CommandForAttributeHandler());
+                        
+        $this->assertInstanceOf(MapHandler::class, $resolution);
+        $this->assertNotInstanceOf(AttributeHandler::class, $resolution);
+    }
+
+    public function test_attribute_resolution_handles_reflection_exceptions_gracefully()
+    {
+        $resolution = (new Dispatcher(new Container()))
+            ->getCommandHandler(new \stdClass());
+    
+        $this->assertFalse($resolution);
+    }
+}
+
+#[HandledBy(AttributeHandler::class)]
+class CommandForAttributeHandler {}
+
+class CommandForMapHandler {}
+
+class AttributeHandler
+{
+    public function handle($command) {}
+}
+
+class MapHandler
+{
+    public function handle($command){}
+}
