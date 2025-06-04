54c901fcf6c1261b5244d8718182428fe82eaf94
yelbasheer <jodeveloper8@gmail.com>
2025-06-04 10:09:23 +0300

    follow styleci

--- tests/Support/SupportArrTest.php
+++ tests/Support/SupportArrTest.php
@@ -1549,7 +1549,9 @@ class SupportArrTest extends TestCase
         $this->assertSame($subject, Arr::from($items));
 
         $items = new WeakMap;
-        $items[$temp = new class {}] = 'bar';
+        $items[$temp = new class
+        {
+        }] = 'bar';
         $this->assertSame(['bar'], Arr::from($items));
 
         $this->expectException(InvalidArgumentException::class);
