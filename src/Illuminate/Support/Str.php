12355a41b48cbf1059b035fef2e1c61b799fdf79
SEMEREDIN NURGEBA <semernur11@gmail.com>
2025-06-12 10:10:29 +0300

    fix:code style.

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -2096,13 +2096,17 @@ class Str
      */
     public static function ordinal(int $number)
     {
-        if (!in_array(($number % 100), [11, 12, 13])) {
+        if (! in_array($number % 100, [11, 12, 13])) {
             switch ($number % 10) {
-                case 1: return $number . "st";
-                case 2: return $number . "nd";
-                case 3: return $number . "rd";
+                case 1:
+                    return $number . 'st';
+                case 2:
+                    return $number . 'nd';
+                case 3:
+                    return $number . 'rd';
             }
         }
-        return $number . "th";
+
+        return $number . 'th';
     }
 }
