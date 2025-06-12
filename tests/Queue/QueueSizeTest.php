be97926b9b42c240017648240c305d4b908c1954
Taylor Otwell <taylor@laravel.com>
2025-06-12 10:03:09 -0500

    remove test

--- tests/Queue/QueueSizeTest.php
+++ tests/Queue/QueueSizeTest.php
@@ -25,41 +25,6 @@ class QueueSizeTest extends TestCase
         $this->assertEquals(2, Queue::size());
         $this->assertEquals(1, Queue::size('Q2'));
     }
-
-    public function test_driver_methods_exist_and_return_expected_defaults()
-    {
-        Queue::fake();
-
-        $queue = Queue::connection();
-
-        $job = new TestJob1;
-
-        dispatch($job);
-        dispatch($job);
-        dispatch(new TestJob2)->delay(5);
-        dispatch($job)->onQueue('Q2');
-
-        $this->assertEquals(2, $queue->pendingSize());
-        $this->assertEquals(1, $queue->delayedSize());
-        $this->assertEquals(0, $queue->reservedSize());
-        $this->assertIsInt($queue->creationTimeOfOldestPendingJob());
-
-        $this->assertEquals(1, $queue->pendingSize('Q2'));
-        $this->assertEquals(0, $queue->delayedSize('Q2'));
-        $this->assertEquals(0, $queue->reservedSize('Q2'));
-        $this->assertIsInt($queue->creationTimeOfOldestPendingJob('Q2'));
-
-        $queue->process();
-        $queue->process('Q2');
-
-        $this->assertEquals(1, $queue->pendingSize());
-        $this->assertEquals(1, $queue->delayedSize());
-        $this->assertEquals(1, $queue->reservedSize());
-
-        $this->assertEquals(0, $queue->pendingSize('Q2'));
-        $this->assertEquals(0, $queue->delayedSize('Q2'));
-        $this->assertEquals(1, $queue->reservedSize('Q2'));
-    }
 }
 
 class TestJob1 implements ShouldQueue
