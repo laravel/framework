bc20a697098d54dc0ad216f9b31d04d8c2a11cc5
JordanchoEftimov <ejordancho@gmail.com>
2025-06-15 00:53:06 +0200

    Optimise join and set functions in Arr class

--- src/Illuminate/Collections/Arr.php
+++ src/Illuminate/Collections/Arr.php
@@ -607,11 +607,13 @@ class Arr
             return implode($glue, $array);
         }
 
-        if (count($array) === 0) {
+        $countArray = count($array);
+
+        if ($countArray === 0) {
             return '';
         }
 
-        if (count($array) === 1) {
+        if ($countArray === 1) {
             return end($array);
         }
 
@@ -914,11 +916,13 @@ class Arr
 
         $keys = explode('.', $key);
 
-        foreach ($keys as $i => $key) {
-            if (count($keys) === 1) {
-                break;
-            }
+        if (count($keys) === 1) {
+            $array[$keys[0]] = $value;
 
+            return $array;
+        }
+
+        foreach ($keys as $i => $key) {
             unset($keys[$i]);
 
             // If the key doesn't exist at this depth, we will just create an empty array
