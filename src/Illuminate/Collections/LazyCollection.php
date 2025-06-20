ab3cd3e88c4cfcb9d4a04748232c382d89f468ff
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2025-06-20 20:17:24 +0700

    add contains any to lazy collection

--- src/Illuminate/Collections/LazyCollection.php
+++ src/Illuminate/Collections/LazyCollection.php
@@ -249,6 +249,17 @@ class LazyCollection implements CanBeEscapedWhenCastToString, Enumerable
     }
 
     /**
+     * Determine if one or multiple items exists in the collection.
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public function containsAny($value = null)
+    {
+        return $this->contains(fn ($item) => in_array($item, Arr::wrap($value)));
+    }
+
+    /**
      * Determine if an item exists, using strict comparison.
      *
      * @param  (callable(TValue): bool)|TValue|array-key  $key
