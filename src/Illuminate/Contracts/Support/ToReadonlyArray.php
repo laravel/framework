4e6bac610f32964e401beff4f9f8506601c6f65a
KalimeroMK <zbogoevski@gmail.com>
2025-06-26 08:48:44 +0200

    Add ToReadonlyArray interface and CastsToReadonlyArray trait

---
+++ src/Illuminate/Contracts/Support/ToReadonlyArray.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Illuminate\Contracts\Support;
+
+interface ToReadonlyArray
+{
+    public function toReadonlyArray(): array;
+}
---
+++ tests/Support/ReadonlyArrayTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Illuminate\Tests\Support;
+
+use Illuminate\Contracts\Support\ToReadonlyArray;
+use Illuminate\Support\Traits\CastsToReadonlyArray;
+use PHPUnit\Framework\TestCase;
+
+class ReadonlyArrayTest extends TestCase
+{
+    public function testCanConvertToReadonlyArray(): void
+    {
+        $dto = new class implements ToReadonlyArray {
+            use CastsToReadonlyArray;
+
+            public function __construct(
+                public readonly string $name = 'Zoran',
+                public readonly int $age = 33,
+            ) {}
+        };
+
+        $this->assertEquals([
+            'name' => 'Zoran',
+            'age' => 33,
+        ], $dto->toReadonlyArray());
+    }
+}
