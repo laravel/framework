0c03c7334fbeb273724414babb856c1713b532ec
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2025-07-01 09:46:59 +0100

    Revert "Pass the limiter to the when & report callbacks (#56129)"
    This reverts commit 372a22a2d086e045da3075638834a61ffc000da2.

--- src/Illuminate/Queue/Middleware/ThrottlesExceptions.php
+++ src/Illuminate/Queue/Middleware/ThrottlesExceptions.php
@@ -104,11 +104,11 @@ class ThrottlesExceptions
 
             $this->limiter->clear($jobKey);
         } catch (Throwable $throwable) {
-            if ($this->whenCallback && ! call_user_func($this->whenCallback, $throwable, $this->limiter)) {
+            if ($this->whenCallback && ! call_user_func($this->whenCallback, $throwable)) {
                 throw $throwable;
             }
 
-            if ($this->reportCallback && call_user_func($this->reportCallback, $throwable, $this->limiter)) {
+            if ($this->reportCallback && call_user_func($this->reportCallback, $throwable)) {
                 report($throwable);
             }
 
