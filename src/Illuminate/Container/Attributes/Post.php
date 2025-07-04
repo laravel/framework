21c1a310907e6f7bb277216c7dd29fb398127a8f
Jamie York <ziadoz@gmail.com>
2025-07-04 22:29:57 +0100

    get and post contextual attributes

---
+++ src/Illuminate/Container/Attributes/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Illuminate\Container\Attributes;
+
+use Attribute;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Container\ContextualAttribute;
+
+#[Attribute(Attribute::TARGET_PARAMETER)]
+class Post implements ContextualAttribute
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct(public string $key, public mixed $default = null)
+    {
+    }
+
+    /**
+     * Resolve the POST data from the request.
+     */
+    public static function resolve(self $attribute, Container $container): mixed
+    {
+        return $container->make('request')->post($attribute->key, $attribute->default);
+    }
+}
