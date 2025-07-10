6dcd22347cd4b2b8ab7e2d9ce302f42c8ad9784f
Pinchos Sudak <67613598+pintend@users.noreply.github.com>
2025-07-10 13:02:24 -0400

    fixed test

--- tests/Support/SupportCollectionTest.php
+++ tests/Support/SupportCollectionTest.php
@@ -5651,10 +5651,10 @@ class SupportCollectionTest extends TestCase
     #[DataProvider('collectionClassProvider')]
     public function testEnsureForObjectsClassStrings($collection)
     {
-        $data = $collection::make([new stdClass, stdClass::class, stdClass::class]);
+        $data = $collection::make([new stdClass, new stdClass, new stdClass, stdClass::class]);
         $data->ensure(stdClass::class, true);
 
-        $data = $collection::make([new stdClass, stdClass::class, stdClass::class]);
+        $data = $collection::make([new stdClass, new stdClass, new stdClass, stdClass::class]);
         $this->expectException(UnexpectedValueException::class);
         $this->expectExceptionMessage(sprintf('Collection should only include [%s] items, but \'%s\' found at position %d.', class_basename(new stdClass()), gettype(stdClass::class), 3));
         $data->ensure(stdClass::class);
