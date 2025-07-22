7a18119160458af5c5ff613b1d1b17e5cc24423c
Mior Muhammad Zaki <crynobone@gmail.com>
2025-07-22 14:24:33 +0800

    [12.x] Fix `migrate:fresh` command on SQLite with WAL journal_mode
    fix https://github.com/laravel/framework/issues/56357
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Integration/Database/Sqlite/Console/MigrateFreshCommandWithJournalModeWalTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Database\Sqlite\Console;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Schema;
+use Orchestra\Testbench\Attributes\WithConfig;
+use Orchestra\Testbench\TestCase;
+
+use function Illuminate\Filesystem\join_paths;
+use function Orchestra\Testbench\default_migration_path;
+
+#[WithConfig('database.default', 'sqlite')]
+#[WithConfig('database.connections.sqlite.journal_mode', 'wal')]
+class MigrateFreshCommandWithJournalModeWalTest extends TestCase
+{
+    /** {@inheritDoc} */
+    #[\Override]
+    protected function setUp(): void
+    {
+        $files = new Filesystem;
+
+        $this->afterApplicationCreated(function () use ($files) {
+            $files->copy(join_paths(__DIR__, 'stubs', 'database-journal-mode-wal.sqlite'), database_path('database.sqlite'));
+        });
+
+        $this->beforeApplicationDestroyed(function () use ($files) {
+            $files->delete(database_path('database.sqlite'));
+        });
+
+        parent::setUp();
+    }
+
+    public function testRunningMigrateFreshCommandWithWalJournalMode()
+    {
+        $this->assertTrue(Schema::hasTable('users'));
+
+        $this->artisan('migrate:fresh', [
+            '--realpath' => true,
+            '--path' => default_migration_path(),
+        ])->run();
+
+        $this->assertTrue(Schema::hasTable('users'));
+    }
+}
