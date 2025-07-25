27290a567cca3c0410e6546656053bda8e841741
Michael Nabil <michael.nabil230@gmail.com>
2025-07-25 21:52:57 +0300

    refine type hinting for callback parameters in validation rules

--- src/Illuminate/Validation/Rules/RequiredIf.php
+++ src/Illuminate/Validation/Rules/RequiredIf.php
@@ -11,14 +11,14 @@ class RequiredIf implements Stringable
     /**
      * The condition that validates the attribute.
      *
-     * @var \Closure|bool
+     * @var (\Closure(): bool)|bool
      */
     public $condition;
 
     /**
      * Create a new required validation rule based on a condition.
      *
-     * @param  \Closure|bool  $condition
+     * @param  (\Closure(): bool)|bool $condition
      */
     public function __construct($condition)
     {
