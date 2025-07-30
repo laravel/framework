18c5dce225364d21c4e76ec98ccd6236b8c2b0e5
yousef kadah <yousefkadah@gmail.com>
2025-07-30 17:38:59 +0300

    Add docblock spacing for validateRelationExistence method

--- src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
+++ src/Illuminate/Database/Eloquent/Concerns/QueriesRelationships.php
@@ -85,6 +85,7 @@ trait QueriesRelationships
      * Validate that the given relation exists on the model.
      *
      * @param  string  $relation
+     *
      * @throws \Illuminate\Database\Eloquent\RelationNotFoundException
      */
     protected function validateRelationExistence($relation)
