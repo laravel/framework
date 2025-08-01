53504c800b51d15415fff83f03a1f0f5917844cc
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2025-08-02 01:58:36 +0200

    StyleCI

--- src/Illuminate/Support/functions.php
+++ src/Illuminate/Support/functions.php
@@ -56,15 +56,16 @@ if (! function_exists('Illuminate\Support\partial_application')) {
     /**
      * @template TReturn
      * @template TPartial of (callable(): TReturn)
+     *
      * @param  TPartial  $fn
      * @return TPartial|TReturn
      */
     function partial_application(callable $fn, array $params = [])
     {
         $paramCount = (new ReflectionFunction($fn))->getNumberOfParameters();
-    
+
         return fn (...$args) => (count($params) + count($args)) === $paramCount
             ? $fn(...[...$params, ...$args])
-            : partial_application($fn, [ ...$params, ...$args ]);
+            : partial_application($fn, [...$params, ...$args]);
     }
 }
