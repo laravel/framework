30c2137bc88dc51c958ebc68885f3f52932af24e
ericmp <ericmorenopou@gmail.com>
2025-08-05 18:46:38 +0200

    refactor: add docblocks for protocols property and method in Url rule

--- src/Illuminate/Validation/Rules/Url.php
+++ src/Illuminate/Validation/Rules/Url.php
@@ -6,8 +6,14 @@ use Stringable;
 
 class Url implements Stringable
 {
+    /**
+     * @var string[]|null $protocols
+     */
     protected ?array $protocols = null;
 
+    /**
+     * @param string[]|null $protocols
+     */
     public function protocols(array $protocols): static
     {
         $this->protocols = $protocols;
