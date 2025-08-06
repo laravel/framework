d63a1b8a516fc201f1aa79211b59ab79b564aea8
Wendell Adriel <wendelladriel.ti@gmail.com>
2025-08-06 12:39:31 +0100

    Style fixes

--- src/Illuminate/Console/Attributes/ValueOption.php
+++ src/Illuminate/Console/Attributes/ValueOption.php
@@ -14,16 +14,16 @@ class ValueOption extends Option
      * @param  bool  $array
      * @param  array|string|null  $shortcut
      * @param  string  $description
-     * @param  array|bool|float|int|string|null $default
-     * @param  array|Closure $suggestedValues
+     * @param  array|bool|float|int|string|null  $default
+     * @param  array|Closure  $suggestedValues
      */
     public function __construct(
         public string $name,
         public bool $array = false,
         public array|string|null $shortcut = null,
         public string $description = '',
-        public array|bool|float|int|string|null  $default = null,
-        public array|Closure  $suggestedValues = [],
+        public array|bool|float|int|string|null $default = null,
+        public array|Closure $suggestedValues = [],
     ) {
         parent::__construct(
             name: $name,
