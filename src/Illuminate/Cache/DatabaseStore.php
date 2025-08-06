b1f03d3e0ed7330881ae1ecdca22a191a2317d84
Taylor Otwell <taylor@laravel.com>
2025-08-06 10:33:06 -0500

    Update DatabaseStore.php

--- src/Illuminate/Cache/DatabaseStore.php
+++ src/Illuminate/Cache/DatabaseStore.php
@@ -446,16 +446,6 @@ class DatabaseStore implements LockProvider, Store
     }
 
     /**
-     * Get the connection used to manage locks.
-     *
-     * @return \Illuminate\Database\ConnectionInterface
-     */
-    public function getLockConnection()
-    {
-        return $this->lockConnection;
-    }
-
-    /**
      * Set the underlying database connection.
      *
      * @param  \Illuminate\Database\ConnectionInterface  $connection
@@ -466,6 +456,16 @@ class DatabaseStore implements LockProvider, Store
         $this->connection = $connection;
 
         return $this;
+    }
+
+    /**
+     * Get the connection used to manage locks.
+     *
+     * @return \Illuminate\Database\ConnectionInterface
+     */
+    public function getLockConnection()
+    {
+        return $this->lockConnection;
     }
 
     /**
