b83089f5de44128a6fead7e95c85ee16483dc1b4
Jesper Noordsij <45041769+jnoordsij@users.noreply.github.com>
2025-08-11 16:49:28 +0200

    [12.x] Prefer Symfony PHP polyfills over `function_exists` calls (#56621)
    * Require Symfony PHP 8.3 polyfill in Support in favor of function existence check
    
    * Require Symfony PHP 8.3 polyfill in Testing and Validation in favor of function existence check

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1631,13 +1631,7 @@ trait ValidatesAttributes
             return false;
         }
 
-        if (function_exists('json_validate')) {
-            return json_validate($value);
-        }
-
-        json_decode($value);
-
-        return json_last_error() === JSON_ERROR_NONE;
+        return json_validate($value);
     }
 
     /**
