c457c3330f21951b56336f26c18d277b314d0132
ericmp <ericmorenopou@gmail.com>
2025-08-11 20:01:17 +0200

    feat: Between rule - remove min max InvalidArgumentException validation

--- src/Illuminate/Validation/Rules/Between.php
+++ src/Illuminate/Validation/Rules/Between.php
@@ -2,30 +2,14 @@
 
 namespace Illuminate\Validation\Rules;
 
-use InvalidArgumentException;
 use Stringable;
 
 class Between implements Stringable
 {
-    protected int|float $min;
-    protected int|float $max;
-
-    /**
-     * Create a new rule instance.
-     *
-     * @param  int|float  $min
-     * @param  int|float  $max
-     *
-     * @throws InvalidArgumentException
-     */
-    public function __construct(int|float $min, int|float $max)
-    {
-        if ($min > $max) {
-            throw new InvalidArgumentException('The min value cannot be greater than the max value.');
-        }
-
-        $this->min = $min;
-        $this->max = $max;
+    public function __construct(
+        protected int|float $min,
+        protected int|float $max
+    ) {
     }
 
     public function __toString(): string
