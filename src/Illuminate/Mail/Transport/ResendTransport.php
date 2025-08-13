5521c5efa31026244322e937fce03ae24280a508
cpenned <66193019+cpenned@users.noreply.github.com>
2025-08-12 19:57:47 -0600

    refactor: update cid param name (#56634)
    * refactor: update cid param name
    
    * Update src/Illuminate/Mail/Transport/ResendTransport.php
    
    Co-authored-by: Jayan Ratna <jayanratna@gmail.com>
    
    * Update src/Illuminate/Mail/Transport/ResendTransport.php
    
    Co-authored-by: Jayan Ratna <jayanratna@gmail.com>
    
    ---------
    
    Co-authored-by: Jayan Ratna <jayanratna@gmail.com>

--- src/Illuminate/Mail/Transport/ResendTransport.php
+++ src/Illuminate/Mail/Transport/ResendTransport.php
@@ -89,7 +89,7 @@ class ResendTransport extends AbstractTransport
                 ];
 
                 if ($disposition === 'inline') {
-                    $item['inline_content_id'] = $attachment->hasContentId() ? $attachment->getContentId() : $filename;
+                    $item['content_id'] = $attachment->hasContentId() ? $attachment->getContentId() : $filename;
                 }
 
                 $attachments[] = $item;
