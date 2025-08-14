de99773a96d3046093fda952f564fa668c9704da
Steve Bauman <steven_bauman@outlook.com>
2025-08-14 10:13:19 -0400

    Add tests

--- tests/Integration/Database/EloquentModelTest.php
+++ tests/Integration/Database/EloquentModelTest.php
@@ -22,6 +22,12 @@ class EloquentModelTest extends DatabaseTestCase
             $table->string('name');
             $table->string('title');
         });
+
+        Schema::create('test_model3', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->json('data');
+        });
     }
 
     public function testUserCanUpdateNullableDate()
@@ -104,6 +110,76 @@ class EloquentModelTest extends DatabaseTestCase
         $this->assertEmpty($user->getPrevious());
     }
 
+    public function testGetChangesWithKey()
+    {
+        $user = TestModel2::create([
+            'name' => $originalName = Str::random(),
+            'title' => $originalTitle = Str::random(),
+        ]);
+
+        $user->name = $newName = Str::random();
+        $user->title = $newTitle = Str::random();
+        $user->save();
+
+        $this->assertNull($user->getChanges('nonexistent'));
+        $this->assertSame($newName, $user->getChanges('name'));
+        $this->assertSame($newTitle, $user->getChanges('title'));
+        $this->assertSame('default', $user->getChanges('nonexistent', 'default'));
+        $this->assertEquals(['name' => $newName, 'title' => $newTitle], $user->getChanges());
+    }
+
+    public function testGetPreviousWithKey()
+    {
+        $user = TestModel2::create([
+            'name' => $originalName = Str::random(),
+            'title' => $originalTitle = Str::random(),
+        ]);
+
+        $user->name = $newName = Str::random();
+        $user->title = $newTitle = Str::random();
+        $user->save();
+
+        $this->assertNull($user->getPrevious('nonexistent'));
+        $this->assertSame($originalName, $user->getPrevious('name'));
+        $this->assertSame($originalTitle, $user->getPrevious('title'));
+        $this->assertSame('default', $user->getPrevious('nonexistent', 'default'));
+        $this->assertEquals(['name' => $originalName, 'title' => $originalTitle], $user->getPrevious());
+    }
+
+    public function testGetChangesAndPreviousWithCasts()
+    {
+        $user = TestModel3::create([
+            'name' => $originalName = Str::random(),
+            'data' => $originalData = ['key' => 'value'],
+        ]);
+
+        $user->name = $newName = Str::random();
+        $user->data = $newData = ['key' => 'new_value'];
+        $user->save();
+
+        $this->assertSame($newName, $user->getChanges('name'));
+        $this->assertEquals($newData, $user->getChanges('data'));
+        $this->assertSame($originalName, $user->getPrevious('name'));
+        $this->assertEquals($originalData, $user->getPrevious('data'));
+        $this->assertEquals(['name' => $newName, 'data' => $newData], $user->getChanges());
+        $this->assertEquals(['name' => $originalName, 'data' => $originalData], $user->getPrevious());
+    }
+
+    public function testGetChangesAndPreviousWithNoChanges()
+    {
+        $user = TestModel2::create([
+            'name' => $name = Str::random(),
+            'title' => $title = Str::random(),
+        ]);
+
+        $this->assertEmpty($user->getChanges());
+        $this->assertEmpty($user->getPrevious());
+        $this->assertNull($user->getChanges('name'));
+        $this->assertNull($user->getPrevious('name'));
+        $this->assertSame('default', $user->getChanges('name', 'default'));
+        $this->assertSame('default', $user->getPrevious('name', 'default'));
+    }
+
     public function testInsertRecordWithReservedWordFieldName()
     {
         Schema::create('actions', function (Blueprint $table) {
@@ -150,4 +226,12 @@ class TestModel2 extends Model
     public $table = 'test_model2';
     public $timestamps = false;
     protected $guarded = [];
+}
+
+class TestModel3 extends Model
+{
+    public $table = 'test_model3';
+    public $timestamps = false;
+    protected $guarded = [];
+    protected $casts = ['data' => 'array'];
 }
