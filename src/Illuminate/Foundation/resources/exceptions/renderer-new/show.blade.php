b0ddee45e8bcf63d7a64d73e86c084f2b6f31038
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-20 14:31:48 +0900

    Add routing-parameter component

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/show.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/show.blade.php
@@ -414,19 +414,7 @@
 
             <x-laravel-exceptions-renderer-new::routing :routing="$exception->applicationRouteContext()" />
 
-            <!-- Routing Parameters -->
-            <div class="flex flex-col gap-2">
-                <div class="flex gap-6 items-start justify-start pb-3 w-full">
-                    <h2 class="text-lg font-semibold">Routing parameters</h2>
-                </div>
-                @if ($routeParametersContext = $exception->applicationRouteParametersContext())
-                <div class="bg-white/[0.02] border border-white/5 rounded-md shadow-[0px_16px_32px_-8px_rgba(12,12,13,0.4)] w-full overflow-hidden p-5">
-                    <pre class="text-sm font-mono max-w-[780px] w-full"><code>{{ $routeParametersContext }}</code></pre>
-                </div>
-                @else
-                <x-laravel-exceptions-renderer-new::empty-state message="No routing parameters" />
-                @endif
-            </div>
+            <x-laravel-exceptions-renderer-new::routing-parameter :routeParameters="$exception->applicationRouteParametersContext()" />
         </x-laravel-exceptions-renderer-new::section-container>
 
         <x-laravel-exceptions-renderer-new::separator />
