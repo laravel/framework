1e93e682b858ea47670a582310a847d8ecbe1527
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-20 14:57:42 +0900

    Add topbar component

---
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/topbar.blade.php
@@ -0,0 +1,12 @@
+@props(['title'])
+
+<div class="flex items-center gap-2">
+    <div class="w-[18px] h-[18px]">
+        <svg class="w-full h-full" fill="currentColor" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.178 2.625-1.516 2.625H3.72c-1.337 0-2.19-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+        </svg>
+    </div>
+    <div class="font-medium text-sm">
+        {{ $title }}
+    </div>
+</div>
