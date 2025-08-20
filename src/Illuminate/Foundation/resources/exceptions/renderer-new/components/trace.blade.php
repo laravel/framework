8c27ef0c9b26ecfc2dfef281e64c8371c35433d8
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-20 15:40:08 +0900

    Add trace component skeleton

---
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/trace.blade.php
@@ -0,0 +1,16 @@
+@props(['exception'])
+
+<div class="flex flex-col gap-1 bg-white/[0.01] border border-neutral-800 rounded-xl p-[10px]">
+    <div class="flex items-center gap-2.5 p-2">
+        <div class="bg-neutral-800 rounded-md w-6 h-6 flex items-center justify-center p-1">
+            <svg class="w-2.5 h-2.5" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.178 2.625-1.516 2.625H3.72c-1.337 0-2.19-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <h3 class="text-base font-semibold">Exception trace</h3>
+    </div>
+
+    <div>
+
+    </div>
+</div>
