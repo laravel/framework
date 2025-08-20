038a6dd42855df8bb3cf45ce5485adf49a001ed8
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-20 14:31:48 +0900

    Add routing-parameter component

---
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/routing-parameter.blade.php
@@ -0,0 +1,12 @@
+@props(['routeParameters'])
+
+<div class="flex flex-col gap-3">
+    <h2 class="text-lg font-semibold">Routing parameters</h2>
+    @if ($routeParameters)
+    <div class="bg-white/[0.02] border border-white/5 rounded-md shadow-[0px_16px_32px_-8px_rgba(12,12,13,0.4)] overflow-hidden p-5">
+        <pre class="text-sm font-mono"><code>{{ $routeParameters }}</code></pre>
+    </div>
+    @else
+    <x-laravel-exceptions-renderer-new::empty-state message="No routing parameters" />
+    @endif
+</div>
