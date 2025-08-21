7a28dd359c0cc6c72b2fe8ed10d781fc18dd2125
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-21 12:26:54 +0900

    Add formatted-source and file-with-line components

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/frame.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/frame.blade.php
@@ -1,10 +1,6 @@
 @props(['frame', 'previousFrame'])
 
 <div class="flex items-center justify-between gap-2.5 p-2">
-    <div class="text-sm font-mono text-neutral-400">
-        {{ $frame->source() }}@if($previousFrame){{ '->' . $previousFrame->callable() }}@endif
-    </div>
-    <div class="text-xs font-mono text-neutral-400">
-        {{ $frame->file() }}:{{ $frame->line() }}
-    </div>
+    <x-laravel-exceptions-renderer-new::formatted-source :$frame :$previousFrame />
+    <x-laravel-exceptions-renderer-new::file-with-line :file="$frame->file()" :line="$frame->line()" />
 </div>
