9ba4e9dec85cc5b55ae3794d835544c2edc125a3
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-22 15:51:47 +0900

    Syntax highlight code snippet

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/frame-code.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/frame-code.blade.php
@@ -1,5 +1,15 @@
 @props(['code', 'highlightedLine'])
 
-<div class="px-5 py-3 bg-neutral-50 border-t border-neutral-100 dark:bg-neutral-900 dark:border-white/10">
-    <pre class="text-xs lg:text-sm"><code class="max-h-32 overflow-y-hidden overflow-x-scroll scrollbar-hidden">{{ $code }}</code></pre>
+@use('Phiki\Phiki')
+@use('Phiki\Grammar\Grammar')
+@use('Phiki\Theme\Theme')
+
+@php
+    $code = (new Phiki)->codeToHtml($code, Grammar::Php, Theme::OneDarkPro, withGutter: true, startingLineNumber: $highlightedLine - 5);
+@endphp
+
+<div
+    class="bg-neutral-50 border-t border-neutral-100 [&_pre]:bg-transparent! [&_.line]:block [&_.line]:px-4 [&_.line]:py-1 [&_.line-number]:mr-6 [&_.line]:even:dark:bg-white/2 [&_.line]:odd:dark:bg-white/4 [&_.line:nth-child(6)]:dark:bg-rose-700/30 dark:bg-neutral-900 dark:border-white/10"
+>
+    {!! $code !!}
 </div>
