55f1b9bdbe286c78c5e64ed31a2ea5d6a88ce596
MohammadQasemi <m.qasemi1372@gmail.com>
2025-08-24 11:25:28 +0330

    Add FileGenerated event class
    - Create FileGenerated event in Illuminate\Console\Events
    - Stores path and type of generated file

---
+++ src/Illuminate/Console/Events/FileGenerated.php
@@ -0,0 +1,19 @@
+<?php
+namespace Illuminate\Console\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class FileGenerated
+{
+    use Dispatchable, SerializesModels;
+
+    public string $path;
+    public string $type;
+
+    public function __construct(string $path, string $type)
+    {
+        $this->path = $path;
+        $this->type = $type;
+    }
+}
