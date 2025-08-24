824f52d44be0f509379ff91da9e2f893b40eac4d
MohammadQasemi <m.qasemi1372@gmail.com>
2025-08-24 14:46:00 +0330

    Refactor: Use Str::of with Stringable in CompilerEngine

--- src/Illuminate/View/Engines/CompilerEngine.php
+++ src/Illuminate/View/Engines/CompilerEngine.php
@@ -6,7 +6,7 @@ use Illuminate\Database\RecordNotFoundException;
 use Illuminate\Database\RecordsNotFoundException;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
 use Illuminate\View\Compilers\CompilerInterface;
 use Illuminate\View\ViewException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -74,7 +74,8 @@ class CompilerEngine extends PhpEngine
         try {
             $results = $this->evaluatePath($this->compiler->getCompiledPath($path), $data);
         } catch (ViewException $e) {
-            if (! Str::of($e->getMessage())->contains(['No such file or directory', 'File does not exist at path'])) {
+            $string = new Stringable($e->getMessage());
+            if (! $string->contains(['No such file or directory', 'File does not exist at path'])) {
                 throw $e;
             }
 
