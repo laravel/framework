9a7653ce0bff5d72c97aea85214dd40c8dfb63d0
MohammadQasemi <m.qasemi1372@gmail.com>
2025-08-25 07:25:14 +0330

    refactor: use Headers instead of Str::of in InteractsWithData

--- src/Illuminate/Support/Traits/InteractsWithData.php
+++ src/Illuminate/Support/Traits/InteractsWithData.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
 use stdClass;
 
 use function Illuminate\Support\enum_value;
@@ -243,7 +244,7 @@ trait InteractsWithData
      */
     public function string($key, $default = null)
     {
-        return Str::of($this->data($key, $default));
+        return new Stringable($this->data($key, $default));
     }
 
     /**
