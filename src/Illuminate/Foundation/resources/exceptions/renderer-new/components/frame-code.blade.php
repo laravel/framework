adfc38dee96bc80be4453802fa4446748cb031b9
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-25 14:06:42 +0900

    Use LineDecoration to highlight code snippet

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/frame-code.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/frame-code.blade.php
@@ -3,15 +3,19 @@
 @use('Phiki\Adapters\Laravel\Facades\Phiki')
 @use('Phiki\Grammar\Grammar')
 @use('Phiki\Theme\Theme')
+@use('Phiki\Transformers\Decorations\LineDecoration')
 
 @php
     $code = Phiki::codeToHtml($code, Grammar::Php, Theme::OneDarkPro)
         ->withGutter()
-        ->startingLine($highlightedLine - 5);
+        ->startingLine($highlightedLine - 5)
+        ->decoration(
+            LineDecoration::forLine(5)->class('dark:bg-rose-700/30!'),
+        );
 @endphp
 
 <div
-    class="bg-neutral-50 border-t border-neutral-100 [&_pre]:bg-transparent! [&_.line]:block [&_.line]:px-4 [&_.line]:py-1 [&_.line-number]:mr-6 [&_.line]:even:dark:bg-white/2 [&_.line]:odd:dark:bg-white/4 [&_.line:nth-child(6)]:dark:bg-rose-700/30 dark:bg-neutral-900 dark:border-white/10"
+    class="bg-neutral-50 border-t border-neutral-100 [&_pre]:bg-transparent! [&_.line]:block [&_.line]:px-4 [&_.line]:py-1 [&_.line-number]:mr-6 [&_.line]:even:dark:bg-white/2 [&_.line]:odd:dark:bg-white/4 dark:bg-neutral-900 dark:border-white/10"
 >
     {!! $code !!}
 </div>
