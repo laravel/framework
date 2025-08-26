a8bf0979a6774e554b1e5616c0f5e0b74dc937f9
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-26 10:11:22 +0900

    Update method badge

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/overview.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/overview.blade.php
@@ -26,11 +26,11 @@
         <div class="flex items-center gap-2 h-10">
             <div class="text-sm font-mono text-neutral-400 uppercase">METHOD</div>
             <div class="flex-1 h-3 border-b-2 border-dotted border-white/20"></div>
-            <div class="bg-rose-500 border border-emerald-800 rounded-md h-6 flex items-center gap-1.5 px-[6px]">
+            <div class="text-emerald-400 bg-emerald-950 border border-emerald-800 rounded-md h-6 flex items-center gap-1.5 px-[6px]">
                 <svg class="w-2.5 h-2.5" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clip-rule="evenodd" />
                 </svg>
-                <span class="text-[13px] font-mono text-neutral-300">{{ $request->method() }}</span>
+                <span class="text-[13px] font-mono">{{ $request->method() }}</span>
             </div>
         </div>
     </div>
