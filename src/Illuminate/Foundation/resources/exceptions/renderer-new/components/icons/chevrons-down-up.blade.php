b1d8d3dab6e125116574eeb09eb85d9008620513
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-27 14:44:10 +0900

    Use current color in SVGs

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/icons/chevrons-down-up.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/icons/chevrons-down-up.blade.php
@@ -1,7 +1,7 @@
 <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewBox="0 0 8 12" fill="none" {{ $attributes }}>
   <g clip-path="url(#clip0_14550_6168)">
-    <path d="M6.75 11.0001L4 8.25012L1.25 11.0001" stroke="#10B981" style="stroke:#10B981;stroke:color(display-p3 0.0627 0.7255 0.5059);stroke-opacity:1;" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M6.75 1.50012L4 4.25012L1.25 1.50012" stroke="#10B981" style="stroke:#10B981;stroke:color(display-p3 0.0627 0.7255 0.5059);stroke-opacity:1;" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6.75 11.0001L4 8.25012L1.25 11.0001" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6.75 1.50012L4 4.25012L1.25 1.50012" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
   </g>
   <defs>
     <clipPath id="clip0_14550_6168">
