ef05d2159e199579bc5496763ab07017bce26a45
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-27 15:45:08 +0900

    Don't show callable when the frame doesn't have a class

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/formatted-source.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/formatted-source.blade.php
@@ -3,6 +3,16 @@
 <div
     {{ $attributes->merge(['class' => 'text-sm font-mono text-neutral-400']) }}
 >
-    <span class="text-violet-500 dark:text-violet-400">{{ $frame->source() }}@if($frame->previous()){{ '->' . $frame->previous()->callable() }}@endif</span><!--
-    --><span class="text-orange-400 dark:text-orange-300 opacity-50">()</span>
+    <span class="text-violet-500 dark:text-violet-400">
+        <span class="text-violet-500 dark:text-violet-400">
+        @if($class = $frame->class())
+            {{ $class }}{{--
+            --}}@if($frame->previous()){{--
+                --}}{{ '->' . $frame->previous()->callable() }}{{--
+            --}}@endif{{--
+            --}}<span class="text-orange-400 dark:text-orange-300 opacity-50">()</span>
+        @else
+            {{ $frame->source() }}
+        @endif
+    </span>
 </div>
