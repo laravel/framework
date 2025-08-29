a9351ec5383575e5a80eca80d5aa24768779acb3
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-29 16:20:37 +0900

    Copy request URL to clipboard

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/expand-button.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/expand-button.blade.php
@@ -3,7 +3,7 @@
 <button
     x-data="{ expanded: {{ $expanded ? 'true' : 'false' }} }"
     type="button"
-    class="flex h-6 w-6 items-center justify-center rounded-md border dark:border-white/8"
+    class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-md border dark:border-white/8"
     :class="{
         'text-emerald-500 dark:bg-white/5': expanded,
         'text-neutral-500 dark:bg-white/3': !expanded,
