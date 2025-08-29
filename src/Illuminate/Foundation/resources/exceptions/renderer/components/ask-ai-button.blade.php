f38f9e604e1bf93c0dfdffb144fc91d1f36c3b8b
theHocineSaad <saadhocine0@gmail.com>
2025-08-29 20:14:08 +0200

    Fix target attribute typo in Ask AI button link

--- src/Illuminate/Foundation/resources/exceptions/renderer/components/ask-ai-button.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer/components/ask-ai-button.blade.php
@@ -10,7 +10,7 @@
     <div x-show="menu"
         class="absolute mt-1 right-0 z-10 flex origin-top-right flex-col rounded-md bg-white shadow-xl ring-1 ring-gray-900/5 dark:bg-gray-800"
         style="display: none" @click="menu = false">
-        <a href="https://chat.openai.com/?q={{ $uriEncodedExceptionMarkdown }}" target="_blanc"
+        <a href="https://chat.openai.com/?q={{ $uriEncodedExceptionMarkdown }}" target="_blank"
             rel="noopener noreferrer"
             class="flex items-center gap-3 px-4 py-2 whitespace-nowrap hover:rounded-t-md hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-900 dark:text-gray-100">
             <x-laravel-exceptions-renderer::icons.chatgpt-logo class="h-5 w-5" />
