15430f8e909083abe27a849744e2a6980847b7bd
Ryuta Hamasaki <avosalmon@gmail.com>
2025-08-29 16:44:53 +0900

    Truncate long source

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/formatted-source.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/formatted-source.blade.php
@@ -1,18 +1,15 @@
 @props(['frame'])
 
-<div
-    {{ $attributes->merge(['class' => 'text-sm font-mono text-neutral-400']) }}
+<span
+    {{ $attributes->merge(['class' => 'text-xs font-mono truncate text-violet-500 dark:text-violet-400']) }}
 >
-    <span class="text-violet-500 dark:text-violet-400">
-        <span class="text-violet-500 dark:text-violet-400">
-        @if($class = $frame->class())
-            {{ $class }}{{--
-            --}}@if($frame->previous()){{--
-                --}}{{ '->' . $frame->previous()->callable() }}{{--
-            --}}@endif{{--
-            --}}<span class="text-orange-400 dark:text-orange-300 opacity-50">()</span>
-        @else
-            {{ $frame->source() }}
-        @endif
-    </span>
-</div>
+    @if($class = $frame->class())
+        {{ $class }}{{--
+        --}}@if($frame->previous()){{--
+            --}}{{ '->' . $frame->previous()->callable() }}{{--
+        --}}@endif{{--
+        --}}<span class="text-orange-400 dark:text-orange-300 opacity-50">()</span>
+    @else
+        {{ $frame->source() }}
+    @endif
+</span>
