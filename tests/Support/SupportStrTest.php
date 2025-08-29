493df2d5d45057383a1aea5821239ceffd3669ce
Moamen Eltouny <raggigroup@gmail.com>
2025-08-30 00:10:09 +0300

    Add additional test cases for Str::slug to cover camel case and abbreviations

--- tests/Support/SupportStrTest.php
+++ tests/Support/SupportStrTest.php
@@ -516,6 +516,9 @@ class SupportStrTest extends TestCase
         $this->assertSame('500-dollar-bill', Str::slug('500$--bill', '-', 'en', ['$' => 'dollar']));
         $this->assertSame('500-dollar-bill', Str::slug('500-$--bill', '-', 'en', ['$' => 'dollar']));
         $this->assertSame('أحمد-في-المدرسة', Str::slug('أحمد@المدرسة', '-', null, ['@' => 'في']));
+        $this->assertSame('method-name-in-camel-case', Str::slug('methodNameInCamelCase'));
+        $this->assertSame('class-name-is-long', Str::slug('ClassNameIsLong'));
+        $this->assertSame('there-is-faq-module-here', Str::slug('There is FAQ module here'));
     }
 
     public function testStrStart()
