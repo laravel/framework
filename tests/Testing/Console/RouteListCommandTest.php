a7917a7f6447261873150f90201bccfa766352ac
Mior Muhammad Zaki <crynobone@gmail.com>
2025-08-31 04:10:56 +0800

    [12.x] Test Improvements (#56838)
    * [12.x] Test Improvements
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Apply fixes from StyleCI
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- tests/Testing/Console/RouteListCommandTest.php
+++ tests/Testing/Console/RouteListCommandTest.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Console\RouteListCommand;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithDeprecationHandling;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Facade;
 use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\TestCase;
 
@@ -101,9 +100,10 @@ class RouteListCommandTest extends TestCase
             ->expectsOutput('');
     }
 
+    #[IgnorePhpunitDeprecations]
     public function testRouteCanBeFilteredByName()
     {
-        $this->withoutDeprecationHandling();
+        // $this->withoutDeprecationHandling();
 
         $this->router->get('/', function () {
             //
@@ -125,6 +125,10 @@ class RouteListCommandTest extends TestCase
     {
         $this->withoutDeprecationHandling();
 
+        RouteListCommand::resolveTerminalWidthUsing(function () {
+            return 82;
+        });
+
         $this->router->get('/', function () {
             //
         });
@@ -137,7 +141,7 @@ class RouteListCommandTest extends TestCase
                 '  GET|HEAD       foo/{user} Illuminate\Tests\Testing\Console\FooController@show'
             )->expectsOutput('')
             ->expectsOutput(
-                '                                                  Showing [1] routes'
+                '                                                              Showing [1] routes'
             )
             ->expectsOutput('');
     }
@@ -157,15 +161,6 @@ class RouteListCommandTest extends TestCase
             ->expectsOutput('')
             ->expectsOutput('                                                  Showing [3] routes')
             ->expectsOutput('');
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        Facade::setFacadeApplication(null);
-
-        RouteListCommand::resolveTerminalWidthUsing(null);
     }
 }
 
