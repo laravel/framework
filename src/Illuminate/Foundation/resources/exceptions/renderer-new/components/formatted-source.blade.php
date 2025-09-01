15970c7c1887f06329ab00263242cc47c6f7dc8a
Ryuta Hamasaki <avosalmon@gmail.com>
2025-09-01 11:59:45 +0900

    Add tooltip

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/formatted-source.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/formatted-source.blade.php
@@ -1,15 +1,33 @@
 @props(['frame'])
 
-<span
-    {{ $attributes->merge(['class' => 'text-xs font-mono truncate text-violet-500 dark:text-violet-400']) }}
->
-    @if($class = $frame->class())
-        {{ $class }}{{--
-        --}}@if($frame->previous()){{--
-            --}}{{ '->' . $frame->previous()->callable() }}{{--
-        --}}@endif{{--
-        --}}<span class="text-orange-400 dark:text-orange-300 opacity-50">()</span>
-    @else
-        {{ $frame->source() }}
-    @endif
-</span>
+@php
+    if ($class = $frame->class()) {
+        $source = $class;
+
+        if ($previous = $frame->previous()) {
+            $source .= "->{$previous->callable()}()";
+        }
+    } else {
+        $source = $frame->source();
+    }
+@endphp
+
+<x-laravel-exceptions-renderer-new::tooltip>
+    <x-slot:trigger>
+        <span
+            {{ $attributes->merge(['class' => 'text-xs font-mono truncate text-violet-500 dark:text-violet-400']) }}
+        >
+            @if($class = $frame->class())
+                {{ $class }}{{--
+                --}}@if($frame->previous()){{--
+                    --}}{{ '->' . $frame->previous()->callable() }}{{--
+                --}}@endif{{--
+                --}}<span class="text-orange-400 dark:text-orange-300 opacity-50">()</span>
+            @else
+                {{ $frame->source() }}
+            @endif
+        </span>
+    </x-slot>
+
+    <span>{{ $source }}</span>
+</x-laravel-exceptions-renderer-new::tooltip>
---
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/tooltip.blade.php
@@ -0,0 +1,32 @@
+<div
+    class="relative"
+    x-data="{
+        open: false,
+        timeout: null,
+        show() {
+            this.timeout = setTimeout(() => { this.open = true }, 500)
+        },
+        hide() {
+            clearTimeout(this.timeout)
+            this.open = false
+        }
+    }"
+    @mouseenter="show()"
+    @mouseleave="hide()"
+>
+    <div>
+        {{ $trigger }}
+    </div>
+    <div
+        x-show="open"
+        @class([
+            'absolute bottom-full left-1/2 transform -translate-x-1/2 w-max px-2 py-1',
+            'animate-in fade-in-0 zoom-in-95 z-50 overflow-hidden rounded-md border text-xs shadow-md backdrop-blur-md',
+            'border-neutral-800 bg-neutral-900 text-white',
+            'dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100',
+            'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2'
+        ])
+    >
+        {{ $slot }}
+    </div>
+</div>
