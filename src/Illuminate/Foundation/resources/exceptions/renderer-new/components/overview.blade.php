7e63c298492d63cde55f0c888fe143a3c24d4963
Ryuta Hamasaki <avosalmon@gmail.com>
2025-09-04 16:24:18 +0900

    Add alert icon

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/overview.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/overview.blade.php
@@ -16,9 +16,7 @@
             <div class="text-sm font-mono text-neutral-400 uppercase">STATUS CODE</div>
             <div class="flex-1 h-3 border-b-2 border-dotted border-white/20"></div>
             <div class="bg-rose-500 border border-rose-500 rounded-md h-6 flex items-center gap-1.5 px-1.5">
-                <svg class="w-2.5 h-2.5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.178 2.625-1.516 2.625H3.72c-1.337 0-2.19-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
-                </svg>
+                <x-laravel-exceptions-renderer-new::icons.alert class="w-2.5 h-2.5" />
                 <span class="text-[13px] font-mono text-neutral-300">500</span>
             </div>
         </div>
