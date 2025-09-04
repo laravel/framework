e98383ebbceb832ab71458deba8fe2c223007792
Ryuta Hamasaki <avosalmon@gmail.com>
2025-09-04 15:31:36 +0900

    Add copy icon

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/request-url.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/request-url.blade.php
@@ -40,12 +40,8 @@
             @click="copyToClipboard()"
             class="bg-white/[0.05] rounded-md w-6 h-6 flex flex-shrink-0 items-center justify-center cursor-pointer transition-colors hover:bg-white/10"
         >
-            <svg x-show="!copied" class="w-3 h-3 text-neutral-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
-            </svg>
-            <svg x-show="copied" class="w-3 h-3 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-            </svg>
+            <x-laravel-exceptions-renderer-new::icons.copy class="w-3 h-3 text-neutral-400" x-show="!copied" />
+            <x-laravel-exceptions-renderer-new::icons.check class="w-3 h-3 text-emerald-500" x-show="copied" />
         </button>
     </div>
 </div>
