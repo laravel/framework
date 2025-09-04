1bbd59885523ce3068d05edddca291da2ba312fe
Tim MacDonald <hello@timacdonald.me>
2025-09-04 13:13:46 +1000

    formatting

--- tests/Integration/Routing/Fixtures/app.php
+++ tests/Integration/Routing/Fixtures/app.php
@@ -3,8 +3,6 @@
 namespace Illuminate\Tests\Integration\Routing\Fixtures;
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
 if (! class_exists(AppCache::class)) {
     class AppCache
