062f3bdac8d6f8518de345569587fec772d6f6f1
Ahmed Alaa <92916738+AhmedAlaa4611@users.noreply.github.com>
2025-09-05 21:56:05 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -149,7 +149,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
