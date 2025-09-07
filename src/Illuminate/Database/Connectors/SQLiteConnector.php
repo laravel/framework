f0f4b8931bbe56dea805ef308ff085b804ce5420
Samuel Štancl <samuel@archte.ch>
2025-09-07 16:12:14 +0200

    SQLite: Allow setting any pragmas

--- src/Illuminate/Database/Connectors/SQLiteConnector.php
+++ src/Illuminate/Database/Connectors/SQLiteConnector.php
@@ -20,6 +20,8 @@ class SQLiteConnector extends Connector implements ConnectorInterface
 
         $connection = $this->createConnection("sqlite:{$path}", $config, $options);
 
+        $this->configurePragmas($connection, $config);
+
         $this->configureForeignKeyConstraints($connection, $config);
         $this->configureBusyTimeout($connection, $config);
         $this->configureJournalMode($connection, $config);
@@ -126,5 +128,23 @@ class SQLiteConnector extends Connector implements ConnectorInterface
         }
 
         $connection->prepare("pragma synchronous = {$config['synchronous']}")->execute();
+    }
+
+    /**
+     * Set other user-configured pragmas.
+     *
+     * @param  \PDO  $connection
+     * @param  array  $config
+     * @return void
+     */
+    protected function configurePragmas($connection, array $config): void
+    {
+        if (! isset($config['pragmas'])) {
+            return;
+        }
+
+        foreach ($config['pragmas'] as $pragma => $value) {
+            $connection->prepare("pragma {$pragma} = {$value}")->execute();
+        }
     }
 }
