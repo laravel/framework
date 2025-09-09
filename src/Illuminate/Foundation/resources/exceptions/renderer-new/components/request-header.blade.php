bad2ba18647c9089c5a9571ccc79ed54c9fa7378
Ryuta Hamasaki <avosalmon@gmail.com>
2025-09-09 17:21:01 +0900

    Uppercase keys

--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/request-header.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/request-header.blade.php
@@ -5,7 +5,7 @@
     <div class="flex flex-col">
         @foreach ($headers as $key => $value)
         <div class="flex max-w-full items-baseline gap-2 h-10 text-sm font-mono">
-            <div class="text-neutral-500 dark:text-neutral-400 shrink-0">{{ $key }}</div>
+            <div class="uppercase text-neutral-500 dark:text-neutral-400 shrink-0">{{ $key }}</div>
             <div class="min-w-6 grow h-3 border-b-2 border-dotted border-neutral-300 dark:border-white/20"></div>
             <div class="truncate text-neutral-900 dark:text-white">
                 <span data-tippy-content="{{ $value }}">
--- src/Illuminate/Foundation/resources/exceptions/renderer-new/components/routing.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer-new/components/routing.blade.php
@@ -4,11 +4,13 @@
     <h2 class="text-lg font-semibold">Routing</h2>
     <div class="flex flex-col">
         @foreach ($routing as $key => $value)
-        <div class="flex items-center gap-2 h-10">
-            <div class="text-sm font-mono text-neutral-400 capitalize">{{ $key }}</div>
-            <div class="flex-1 h-3 border-b-2 border-dotted border-white/20"></div>
-            <div class="text-sm font-mono overflow-hidden text-ellipsis">
-                {{ $value }}
+        <div class="flex max-w-full items-baseline gap-2 h-10 text-sm font-mono">
+            <div class="uppercase text-neutral-500 dark:text-neutral-400 shrink-0">{{ $key }}</div>
+            <div class="min-w-6 grow h-3 border-b-2 border-dotted border-neutral-300 dark:border-white/20"></div>
+            <div class="truncate text-neutral-900 dark:text-white">
+                <span data-tippy-content="{{ $value }}">
+                    {{ $value }}
+                </span>
             </div>
         </div>
         @endforeach
