fe4995752f6954b284ec7d9f62e5dea47d352a12
Amir Hossein Shokri <shokridev@gmail.com>
2025-09-10 22:45:00 +0330

    Improve docblocks for nullable parameters (#56997)

--- src/Illuminate/Support/Stringable.php
+++ src/Illuminate/Support/Stringable.php
@@ -1045,7 +1045,7 @@ class Stringable implements JsonSerializable, ArrayAccess, BaseStringable
     /**
      * Trim the string of the given characters.
      *
-     * @param  string  $characters
+     * @param  string|null  $characters
      * @return static
      */
     public function trim($characters = null)
@@ -1056,7 +1056,7 @@ class Stringable implements JsonSerializable, ArrayAccess, BaseStringable
     /**
      * Left trim the string of the given characters.
      *
-     * @param  string  $characters
+     * @param  string|null  $characters
      * @return static
      */
     public function ltrim($characters = null)
@@ -1067,7 +1067,7 @@ class Stringable implements JsonSerializable, ArrayAccess, BaseStringable
     /**
      * Right trim the string of the given characters.
      *
-     * @param  string  $characters
+     * @param  string|null  $characters
      * @return static
      */
     public function rtrim($characters = null)
