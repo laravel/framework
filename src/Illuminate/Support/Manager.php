6e6776c0daa78724535812be9d70ed03139329e9
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-09-11 21:00:10 +0200

    [12.x] Simplify driver instance decoration in manager

--- src/Illuminate/Support/Manager.php
+++ src/Illuminate/Support/Manager.php
@@ -75,7 +75,21 @@ abstract class Manager
         // If the given driver has not been created before, we will create the instances
         // here and cache it so we can return it next time very quickly. If there is
         // already a driver created by this name, we'll just return that instance.
-        return $this->drivers[$driver] ??= $this->createDriver($driver);
+        return $this->drivers[$driver] ??= $this->decorateInstance(
+            $this->createDriver($driver), $driver
+        );
+    }
+
+    /**
+     * Optionally decorate a driver instance.
+     *
+     * @param  mixed  $instance
+     * @param  string  $driver
+     * @return mixed
+     */
+    protected function decorateInstance($instance, $driver)
+    {
+        return $instance;
     }
 
     /**
