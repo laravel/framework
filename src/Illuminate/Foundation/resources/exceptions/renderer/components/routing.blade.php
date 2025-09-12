abee6206dfe430d8269d689337f4163de6e0b0fd
Ryuta Hamasaki <avosalmon@gmail.com>
2025-09-12 23:33:44 +0900

    Add empty state for routing context

--- src/Illuminate/Foundation/resources/exceptions/renderer/components/routing.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer/components/routing.blade.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col gap-3">
     <h2 class="text-lg font-semibold">Routing</h2>
     <div class="flex flex-col">
-        @foreach ($routing as $key => $value)
+        @forelse ($routing as $key => $value)
         <div class="flex max-w-full items-baseline gap-2 h-10 text-sm font-mono">
             <div class="uppercase text-neutral-500 dark:text-neutral-400 shrink-0">{{ $key }}</div>
             <div class="min-w-6 grow h-3 border-b-2 border-dotted border-neutral-300 dark:border-white/20"></div>
@@ -13,6 +13,8 @@
                 </span>
             </div>
         </div>
-        @endforeach
+        @empty
+        <x-laravel-exceptions-renderer::empty-state message="No routing context" />
+        @endforelse
     </div>
 </div>
