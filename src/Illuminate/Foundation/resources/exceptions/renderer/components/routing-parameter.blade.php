c96747518a5e17b4a319e4e04340993569d42461
Ryuta Hamasaki <avosalmon@gmail.com>
2025-09-15 15:19:37 +0900

    Apply bg-white without opacity in light mode

--- src/Illuminate/Foundation/resources/exceptions/renderer/components/routing-parameter.blade.php
+++ src/Illuminate/Foundation/resources/exceptions/renderer/components/routing-parameter.blade.php
@@ -3,7 +3,7 @@
 <div class="flex flex-col gap-3">
     <h2 class="text-lg font-semibold">Routing parameters</h2>
     @if ($routeParameters)
-    <div class="bg-white/[2%] border border-neutral-200 dark:border-neutral-800 rounded-md overflow-x-auto p-5 text-sm font-mono shadow-xs">
+    <div class="bg-white dark:bg-white/[2%] border border-neutral-200 dark:border-neutral-800 rounded-md overflow-x-auto p-5 text-sm font-mono shadow-xs">
         <x-laravel-exceptions-renderer::syntax-highlight :code="$routeParameters" grammar="json" />
     </div>
     @else
