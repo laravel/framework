8c4226a7ddec9716f3a0b64b50e611636774625c
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2025-09-18 15:41:18 +0100

    Remove commented code from new test - its not been used from the original since v9 no point adding it here

--- tests/Validation/ValidationValidatorTest.php
+++ tests/Validation/ValidationValidatorTest.php
@@ -853,10 +853,6 @@ class ValidationValidatorTest extends TestCase
     {
         $trans = $this->getIlluminateArrayTranslator();
 
-        // $v = new Validator($trans, ['name' => ''], ['name' => 'required'], ['name.required' => ':ordinal-position Name is required.']);
-        // $this->assertFalse($v->passes());
-        // $this->assertSame('1st Name is required.', $v->messages()->first('name'));
-
         $v = new Validator($trans, ['input' => [['name' => '']]], ['input.*.name' => 'required'], ['input.*.name.required' => 'The :ordinal-position Name is required.']);
         $this->assertFalse($v->passes());
         $this->assertSame('The 1st Name is required.', $v->messages()->first('input.*.name'));
