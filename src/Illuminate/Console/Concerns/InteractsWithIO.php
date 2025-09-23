2422abdd91a62d425918c9dd4af17a1edf90925d
Taylor Otwell <taylor@laravel.com>
2025-09-23 08:47:38 -0500

    Update InteractsWithIO.php

--- src/Illuminate/Console/Concerns/InteractsWithIO.php
+++ src/Illuminate/Console/Concerns/InteractsWithIO.php
@@ -431,6 +431,7 @@ trait InteractsWithIO
     protected function parseVerbosity($level = null)
     {
         $level ??= '';
+
         if (isset($this->verbosityMap[$level])) {
             $level = $this->verbosityMap[$level];
         } elseif (! is_int($level)) {
