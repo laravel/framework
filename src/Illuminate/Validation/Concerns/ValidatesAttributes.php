ec495569cfe7b299ae97632b0718f9f979ae74cd
ntm-dev <hoangtupro.a4@gmail.com>
2025-09-25 11:27:14 +0700

    Fix: Make validateInteger parameters optional for backward compatibility

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -1556,7 +1556,7 @@ trait ValidatesAttributes
      * @param  array{0: 'strict'}  $parameters
      * @return bool
      */
-    public function validateInteger($attribute, $value, array $parameters)
+    public function validateInteger($attribute, $value, array $parameters = [])
     {
         if (($parameters[0] ?? null) === 'strict') {
             return is_int($value);
