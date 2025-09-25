309c08791197af9c8b69611481908952522b9cd3
Roy <bonroyage@users.noreply.github.com>
2025-09-25 15:52:43 +0200

    [12.x] Fix nested `can` and inherit models on route groups (#57172)
    * Add failing tests
    
    * Support multiple can() methods and models

--- tests/Routing/RouteRegistrarTest.php
+++ tests/Routing/RouteRegistrarTest.php
@@ -909,6 +909,29 @@ class RouteRegistrarTest extends TestCase
         $this->seeMiddleware('can:test');
     }
 
+    public function testCanSetMiddlewareCanWithModelsOnGroups()
+    {
+        $this->router->can('view', 'post')->group(function ($router) {
+            $router->get('/post/{post}');
+        });
+
+        $this->seeMiddleware('can:view,post');
+    }
+
+    public function testCanSetMiddlewareCanNestedOnGroups()
+    {
+        $this->router->can('access-admin')->group(function ($router) {
+            $router->can('edit', 'post')->group(function ($router) {
+                $router->get('/post/{post}/edit');
+            });
+        });
+
+        $this->assertEquals([
+            'can:access-admin',
+            'can:edit,post',
+        ], $this->getRoute()->middleware());
+    }
+
     public function testCanSetMiddlewareForSpecifiedMethodsOnRegisteredResource()
     {
         $this->router->resource('users', RouteRegistrarControllerStub::class)
