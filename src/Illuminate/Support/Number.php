8eed2f1c7359a7e46554c7b970ffa918d8c5b378
Van Duong <vandt147@outlook.com>
2025-09-26 19:48:43 +0700

    Use single quotes instead of double quotes

--- src/Illuminate/Support/Number.php
+++ src/Illuminate/Support/Number.php
@@ -350,7 +350,7 @@ class Number
         }
 
         $values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-        $symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+        $symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
         $roman = '';
         for ($i = 0; $i < count($values); $i++) {
