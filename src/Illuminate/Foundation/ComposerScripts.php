c360326c5963dbbf10e728751023ee7edace856e
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-09-30 11:07:10 -0400

    Define LARAVEL_START if not already defined

--- src/Illuminate/Foundation/ComposerScripts.php
+++ src/Illuminate/Foundation/ComposerScripts.php
@@ -63,7 +63,9 @@ class ComposerScripts
 
         require_once $vendorDir.'/autoload.php';
 
+        if (! defined('LARAVEL_START')) {
         define('LARAVEL_START', microtime(true));
+        }
 
         /** @var Application $app */
         $app = require_once $bootstrapFile;
