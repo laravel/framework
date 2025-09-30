37cf004e62b017f0bd2576d194883480e8e0396f
Matt Jones <matt@eighteen73.co.uk>
2025-09-30 12:08:16 +0100

    Works for both traits and direct inheritance

--- src/Illuminate/Database/Eloquent/Concerns/HasGlobalScopes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasGlobalScopes.php
@@ -32,8 +32,13 @@ trait HasGlobalScopes
     {
         $reflectionClass = new ReflectionClass(static::class);
 
-        return (new Collection($reflectionClass->getAttributes(ScopedBy::class, ReflectionAttribute::IS_INSTANCEOF)))
-            ->map(fn ($attribute) => $attribute->getArguments())
+        $attributes = (new Collection($reflectionClass->getAttributes(ScopedBy::class, ReflectionAttribute::IS_INSTANCEOF)));
+
+        foreach ($reflectionClass->getTraits() as $trait) {
+            $attributes->push(...$trait->getAttributes(ScopedBy::class, ReflectionAttribute::IS_INSTANCEOF));
+        }
+
+        return $attributes->map(fn ($attribute) => $attribute->getArguments())
             ->flatten()
             ->all();
     }
