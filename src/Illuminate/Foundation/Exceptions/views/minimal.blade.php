d1e2ee76c9a74a0335b9e55f1156759c2a320679
Jimmi Hansen <37844753+FoksVHox@users.noreply.github.com>
2025-10-09 09:12:16 +0200

    Update text color in minimal error view to ensure better accessibility

--- src/Illuminate/Foundation/Exceptions/views/minimal.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/minimal.blade.php
@@ -20,11 +20,11 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                    <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                    <div class="px-4 text-lg text-gray-600 dark:text-gray-300 border-r border-gray-400 tracking-wider">
                         @yield('code')
                     </div>
 
-                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+                    <div class="ml-4 text-lg text-gray-600 dark:text-gray-300 uppercase tracking-wider">
                         @yield('message')
                     </div>
                 </div>
