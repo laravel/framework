602e5c71cc39588434f967679bab11fb494ac211
Mior Muhammad Zaki <crynobone@gmail.com>
2025-10-15 01:26:02 +0800

    [12.x] Fix `DB::update()` with subqueries is not supported for all databases (#57381)
    * Test Improvements
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Apply fixes from StyleCI
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Illuminate/Database/Query/Grammars/SQLiteGrammar.php
+++ src/Illuminate/Database/Query/Grammars/SQLiteGrammar.php
@@ -384,6 +384,7 @@ class SQLiteGrammar extends Grammar
      * @param  array  $values
      * @return array
      */
+    #[\Override]
     public function prepareBindingsForUpdate(array $bindings, array $values)
     {
         $groups = $this->groupJsonColumnsForUpdate($values);
@@ -395,6 +396,8 @@ class SQLiteGrammar extends Grammar
             ->all();
 
         $cleanBindings = Arr::except($bindings, 'select');
+
+        $values = Arr::flatten(array_map(fn ($value) => value($value), $values));
 
         return array_values(
             array_merge($values, Arr::flatten($cleanBindings))
