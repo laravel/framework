4b7f7d47dfc5f6f500ce1ccee4589ff50d4f2b49
Arif Dewan <cseknowledge@gmail.com>
2025-10-14 23:49:36 +0600

    Added Brackets editor support in ResolvesDumpSource

--- src/Illuminate/Foundation/Concerns/ResolvesDumpSource.php
+++ src/Illuminate/Foundation/Concerns/ResolvesDumpSource.php
@@ -13,6 +13,7 @@ trait ResolvesDumpSource
      */
     protected $editorHrefs = [
         'atom' => 'atom://core/open/file?filename={file}&line={line}',
+        'brackets' => 'brackets://open?url=file://{file}&line={line}',
         'cursor' => 'cursor://file/{file}:{line}',
         'emacs' => 'emacs://open?url=file://{file}&line={line}',
         'fleet' => 'fleet://open?file={file}&line={line}',
