231bfb839abc56b5a844ec181aa4fb469ae8bfb1
Neil Carlo Sucuangco <necafasu@gmail.com>
2025-10-16 06:51:29 +0800

    Update SqsConnectorTest.php

--- tests/Queue/SqsConnectorTest.php
+++ tests/Queue/SqsConnectorTest.php
@@ -4,27 +4,14 @@ namespace Illuminate\Tests\Queue;
 
 use Illuminate\Queue\Connectors\SqsConnector;
 use Illuminate\Queue\SqsQueue;
-use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class SqsConnectorTest extends TestCase
 {
-    protected function tearDown(): void
-    {
-        m::close();
-    }
-
     public function testConnectDerivesEndpointFromPrefixUrl()
     {
         $prefix = 'http://elasticmq.local:9324/000000000000';
         $expectedEndpoint = 'http://elasticmq.local:9324';
-
-        $overloaded = m::mock('overload:Aws\\Sqs\\SqsClient');
-        $overloaded->shouldReceive('__construct')->once()->with(m::on(function ($cfg) use ($expectedEndpoint) {
-            return isset($cfg['endpoint']) && $cfg['endpoint'] === $expectedEndpoint
-                && isset($cfg['region']) && is_string($cfg['region'])
-                && isset($cfg['version']) && $cfg['version'] === 'latest';
-        }));
 
         $connector = new SqsConnector();
 
@@ -38,5 +25,9 @@ class SqsConnectorTest extends TestCase
         ]);
 
         $this->assertInstanceOf(SqsQueue::class, $queue);
+
+        /** @var SqsQueue $queue */
+        $endpointUri = $queue->getSqs()->getEndpoint();
+        $this->assertSame($expectedEndpoint, (string) $endpointUri);
     }
 }
