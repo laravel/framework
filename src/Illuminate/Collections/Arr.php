a2c65a55275670f98b1bbe41408f5bfffb4f72bc
Akash sarkar <44928766+theakashsarkar@users.noreply.github.com>
2025-10-21 12:10:29 +0600

    refactor: enforce strict boolean validation and improve docblocks in boolean() method

--- src/Illuminate/Collections/Arr.php
+++ src/Illuminate/Collections/Arr.php
@@ -84,18 +84,21 @@ class Arr
     /**
      * Get a boolean item from an array using "dot" notation.
      *
+     * @param  \ArrayAccess|array  $array
+     * @param  string|int|null  $key
+     * @param  bool|null  $default
+     * @return bool
+     *
      * @throws \InvalidArgumentException
      */
     public static function boolean(ArrayAccess|array $array, string|int|null $key, ?bool $default = null): bool
     {
         $value = Arr::get($array, $key, $default);
-
         if (! is_bool($value)) {
             throw new InvalidArgumentException(
                 sprintf('Array value for key [%s] must be a boolean, %s found.', $key, gettype($value))
             );
         }
-
         return $value;
     }
 
