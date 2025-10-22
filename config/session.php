237ee4992bb40e4f4bfc3312db51dc3c6d7648e9
Kjell Kind <kjellkind@gmail.com>
2025-10-22 20:22:40 +0200

    Update session.php

--- config/session.php
+++ config/session.php
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
