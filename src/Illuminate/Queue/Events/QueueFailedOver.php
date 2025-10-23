4177b58c2979ff49282b6a81ec25288dfe086a62
Ahmed Alaa <92916738+AhmedAlaa4611@users.noreply.github.com>
2025-10-23 17:57:50 +0300

    Docblock mismatch in QueueFailedOver constructor

--- src/Illuminate/Queue/Events/QueueFailedOver.php
+++ src/Illuminate/Queue/Events/QueueFailedOver.php
@@ -10,7 +10,8 @@ class QueueFailedOver
      * Create a new event instance.
      *
      * @param  string  $connectionName  The queue connection that failed.
-     * @param  \Closure|string|object  $job  The job instance.
+     * @param This needs to be updated...
+     * @param We need another line for $exception...
      */
     public function __construct(
         public ?string $connectionName,
