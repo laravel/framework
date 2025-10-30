cbd49c095c7fd79708a715625f271fcb491f20e5
Donatas Šimkevičius <donatas.smk@gmail.com>
2025-10-30 14:28:25 +0200

    add tests for type, included merge and id in attributes

--- tests/Http/Resources/JsonApi/JsonApiResourceTest.php
+++ tests/Http/Resources/JsonApi/JsonApiResourceTest.php
@@ -3,8 +3,17 @@
 namespace Illuminate\Tests\Http\Resources\JsonApi;
 
 use BadMethodCallException;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Routing\ResponseFactory as ResponseFactoryContract;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\JsonApi\JsonApiResource;
+use Illuminate\Routing\Redirector;
+use Illuminate\Routing\ResponseFactory;
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class JsonApiResourceTest extends TestCase
@@ -13,6 +22,8 @@ class JsonApiResourceTest extends TestCase
     {
         JsonResource::flushState();
         JsonApiResource::flushState();
+        Container::getInstance()->flush();
+        Relation::morphMap([], false);
     }
 
     public function testResponseWrapperIsHardCodedToData()
@@ -37,4 +48,62 @@ class JsonApiResourceTest extends TestCase
 
         JsonApiResource::withoutWrapping();
     }
+
+    public function testResourceTypeIsPickedFromMorph()
+    {
+        Relation::morphMap([JsonApiModel::class => 'json-api-model']);
+        $model = new JsonApiModel(['id' => 1, 'name' => 'User']);
+
+        $responseData = $this->modelToJsonApiData($model)['data'];
+
+        $this->assertArrayHasKey('type', $responseData);
+        $this->assertSame('json-api-model', $responseData['type']);
+    }
+
+    public function testIncludedResourceDoesNotContainPrimaryKey()
+    {
+        Relation::morphMap([JsonApiModel::class => 'json-api-model']);
+        $model = new JsonApiModel(['id' => 1, 'name' => 'User']);
+        $model->setRelation('manager', new JsonApiModel(['id' => 2, 'name' => 'Manager']));
+        $model->setRelation('deputy', new JsonApiModel(['id' => 2, 'email' => 'deputy@example.com']));
+
+        $responseData = $this->modelToJsonApiData($model);
+        $this->assertArrayNotHasKey('id', $responseData['included'][0]['attributes']);
+    }
+
+    public function testIncludedMatchingResourceAttributesAreMerged()
+    {
+        Relation::morphMap([JsonApiModel::class => 'json-api-model']);
+        $model = new JsonApiModel(['id' => 1, 'name' => 'User']);
+        $model->setRelation('manager', new JsonApiModel(['id' => 2, 'name' => 'Manager']));
+        $model->setRelation('deputy', new JsonApiModel(['id' => 2, 'email' => 'deputy@example.com']));
+
+        $responseData = $this->modelToJsonApiData($model);
+
+        $this->assertEquals([
+            'id' => '2',
+            'type' => 'json-api-model',
+            'attributes' => [
+                'name' => 'Bob',
+                'email' => 'bob@example.com',
+            ],
+        ], $responseData['included'][0]);
+    }
+
+    protected function modelToJsonApiData(Model $model): array
+    {
+        Container::getInstance()->instance(ResponseFactoryContract::class, new ResponseFactory(
+            m::mock(ViewFactory::class),
+            m::mock(Redirector::class)
+        ));
+
+        return (new JsonApiResource($model))
+            ->toResponse(new Request)
+            ->getData(true);
+    }
+}
+
+class JsonApiModel extends Model
+{
+    protected $guarded = [];
 }
