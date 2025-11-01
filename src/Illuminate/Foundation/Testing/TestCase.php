3c84de0f6bdb95d1d4e6368573d51155ae4e32cc
Luke Kuzmish <lakuzmish@gmail.com>
2025-11-01 11:48:44 -0400

    lighter check first

--- src/Illuminate/Foundation/Testing/TestCase.php
+++ src/Illuminate/Foundation/Testing/TestCase.php
@@ -29,7 +29,7 @@ abstract class TestCase extends BaseTestCase
     {
         $app = require Application::inferBasePath().'/bootstrap/app.php';
 
-        if (in_array(WithCachedRoutes::class, class_uses_recursive(self::class)) && isset(CachedState::$cachedRoutes)) {
+        if (isset(CachedState::$cachedRoutes) && in_array(WithCachedRoutes::class, class_uses_recursive(self::class))) {
             $app->booting(fn () => $this->markRoutesCached($app));
         }
 
