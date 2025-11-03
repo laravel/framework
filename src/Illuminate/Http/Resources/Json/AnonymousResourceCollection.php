e3d53722fc6e4acce85bb027fffba7a4812f7bc0
Trevor Varwig <8144988+tvarwig@users.noreply.github.com>
2025-11-03 06:32:11 -0600

    added collection functions to AnonymousResourceCollection

--- src/Illuminate/Http/Resources/Json/AnonymousResourceCollection.php
+++ src/Illuminate/Http/Resources/Json/AnonymousResourceCollection.php
@@ -2,6 +2,8 @@
 
 namespace Illuminate\Http\Resources\Json;
 
+use Illuminate\Support\Arr;
+
 class AnonymousResourceCollection extends ResourceCollection
 {
     /**
@@ -29,5 +31,79 @@ class AnonymousResourceCollection extends ResourceCollection
         $this->collects = $collects;
 
         parent::__construct($resource);
+    }
+
+    /**
+     * Resolve all resources and return as a Collection.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function toCollection()
+    {
+        return $this->collection->map(function ($resource) {
+            return $resource->resolve(request());
+        });
+    }
+
+    /**
+     * Apply only() to each item in the collection.
+     *
+     * @param  array|string  $keys
+     * @return array
+     */
+    public function only($keys)
+    {
+        $keys = is_array($keys) ? $keys : func_get_args();
+        return $this->toCollection()->map(function ($item) use ($keys) {
+            return Arr::only($item, $keys);
+        })->values()->all();
+    }
+
+    /**
+     * Apply except() to each item in the collection.
+     *
+     * @param  array|string  $keys
+     * @return array
+     */
+    public function except($keys)
+    {
+        $keys = is_array($keys) ? $keys : func_get_args();
+        return $this->toCollection()->map(function ($item) use ($keys) {
+            return Arr::except($item, $keys);
+        })->values()->all();
+    }
+
+    /**
+     * Filter the collection.
+     *
+     * @param  callable|null  $callback
+     * @return array
+     */
+    public function filter($callback = null)
+    {
+        return $this->toCollection()->filter($callback)->values()->all();
+    }
+
+    /**
+     * Map over the collection.
+     *
+     * @param  callable  $callback
+     * @return array
+     */
+    public function map($callback)
+    {
+        return $this->toCollection()->map($callback)->values()->all();
+    }
+
+    /**
+     * Pluck values from the collection.
+     *
+     * @param  string|int  $value
+     * @param  string|int|null  $key
+     * @return array
+     */
+    public function pluck($value, $key = null)
+    {
+        return $this->toCollection()->pluck($value, $key)->all();
     }
 }
