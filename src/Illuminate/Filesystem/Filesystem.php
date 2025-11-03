cfaf91b7db2c9a6b991fcb889403e18017cf113f
Andrew Brown <browner12@gmail.com>
2025-11-02 19:22:30 -0600

    add types to method signatures
    all of these methods are essentially aliases/helpers for underlying PHP strict typed methods. this mean we can add our own typing with little risk, as passing an invalid type would have caused an error when it reached the underlying method. this change moves the error up a level to the Laravel code.
    
    this will also more strictly document the actual types that can be returned from some of the methods. for example, when we were hinting a return value of `array`, but it could actually return `false|array`.
    
    the one required change here would be if anyone was subclassing this class, as they would need to update their return types to be compatible.

--- src/Illuminate/Filesystem/Filesystem.php
+++ src/Illuminate/Filesystem/Filesystem.php
@@ -20,22 +20,16 @@ class Filesystem
 
     /**
      * Determine if a file or directory exists.
-     *
-     * @param  string  $path
-     * @return bool
      */
-    public function exists($path)
+    public function exists(string $path): bool
     {
         return file_exists($path);
     }
 
     /**
      * Determine if a file or directory is missing.
-     *
-     * @param  string  $path
-     * @return bool
      */
-    public function missing($path)
+    public function missing(string $path): bool
     {
         return ! $this->exists($path);
     }
@@ -181,12 +175,8 @@ class Filesystem
 
     /**
      * Get the hash of the file at the given path.
-     *
-     * @param  string  $path
-     * @param  string  $algorithm
-     * @return string|false
      */
-    public function hash($path, $algorithm = 'md5')
+    public function hash(string $path, string $algorithm = 'md5'): string|false
     {
         return hash_file($algorithm, $path);
     }
@@ -320,24 +310,16 @@ class Filesystem
 
     /**
      * Move a file to a new location.
-     *
-     * @param  string  $path
-     * @param  string  $target
-     * @return bool
      */
-    public function move($path, $target)
+    public function move(string $path, string $target): bool
     {
         return rename($path, $target);
     }
 
     /**
      * Copy a file to a new location.
-     *
-     * @param  string  $path
-     * @param  string  $target
-     * @return bool
      */
-    public function copy($path, $target)
+    public function copy(string $path, string $target): bool
     {
         return copy($path, $target);
     }
@@ -388,44 +370,32 @@ class Filesystem
 
     /**
      * Extract the file name from a file path.
-     *
-     * @param  string  $path
-     * @return string
      */
-    public function name($path)
+    public function name(string $path): array|string
     {
         return pathinfo($path, PATHINFO_FILENAME);
     }
 
     /**
      * Extract the trailing name component from a file path.
-     *
-     * @param  string  $path
-     * @return string
      */
-    public function basename($path)
+    public function basename(string $path): array|string
     {
         return pathinfo($path, PATHINFO_BASENAME);
     }
 
     /**
      * Extract the parent directory from a file path.
-     *
-     * @param  string  $path
-     * @return string
      */
-    public function dirname($path)
+    public function dirname(string $path): array|string
     {
         return pathinfo($path, PATHINFO_DIRNAME);
     }
 
     /**
      * Extract the file extension from a file path.
-     *
-     * @param  string  $path
-     * @return string
      */
-    public function extension($path)
+    public function extension(string $path): array|string
     {
         return pathinfo($path, PATHINFO_EXTENSION);
     }
@@ -451,55 +421,40 @@ class Filesystem
 
     /**
      * Get the file type of a given file.
-     *
-     * @param  string  $path
-     * @return string
      */
-    public function type($path)
+    public function type(string $path): false|string
     {
         return filetype($path);
     }
 
     /**
      * Get the mime-type of a given file.
-     *
-     * @param  string  $path
-     * @return string|false
      */
-    public function mimeType($path)
+    public function mimeType(string $path): false|string
     {
         return finfo_file(finfo_open(FILEINFO_MIME_TYPE), $path);
     }
 
     /**
      * Get the file size of a given file.
-     *
-     * @param  string  $path
-     * @return int
      */
-    public function size($path)
+    public function size(string $path): false|int
     {
         return filesize($path);
     }
 
     /**
      * Get the file's last modification time.
-     *
-     * @param  string  $path
-     * @return int
      */
-    public function lastModified($path)
+    public function lastModified(string $path): false|int
     {
         return filemtime($path);
     }
 
     /**
      * Determine if the given path is a directory.
-     *
-     * @param  string  $directory
-     * @return bool
      */
-    public function isDirectory($directory)
+    public function isDirectory(string $directory): bool
     {
         return is_dir($directory);
     }
@@ -518,22 +473,16 @@ class Filesystem
 
     /**
      * Determine if the given path is readable.
-     *
-     * @param  string  $path
-     * @return bool
      */
-    public function isReadable($path)
+    public function isReadable(string $path): bool
     {
         return is_readable($path);
     }
 
     /**
      * Determine if the given path is writable.
-     *
-     * @param  string  $path
-     * @return bool
      */
-    public function isWritable($path)
+    public function isWritable(string $path): bool
     {
         return is_writable($path);
     }
@@ -554,23 +503,16 @@ class Filesystem
 
     /**
      * Determine if the given path is a file.
-     *
-     * @param  string  $file
-     * @return bool
      */
-    public function isFile($file)
+    public function isFile(string $file): bool
     {
         return is_file($file);
     }
 
     /**
      * Find path names matching a given pattern.
-     *
-     * @param  string  $pattern
-     * @param  int  $flags
-     * @return array
      */
-    public function glob($pattern, $flags = 0)
+    public function glob(string $pattern, int $flags = 0): false|array
     {
         return glob($pattern, $flags);
     }
