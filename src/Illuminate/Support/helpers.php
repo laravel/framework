0000a3caca7eb9907b6ea54c5359176b9b2ddb06
Farhad Karami <79833598+farad-tech@users.noreply.github.com>
2025-11-05 01:43:29 +0330

    Update src/Illuminate/Support/helpers.php
    Co-authored-by: Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -532,12 +532,7 @@ if (! function_exists('to_english_numbers')) {
      */
     function to_english_numbers($value): string
     {
-        return strtr($value, [
-            '۰' => '0', '۱' => '1', '۲' => '2', '۳' => '3', '۴' => '4',
-            '۵' => '5', '۶' => '6', '۷' => '7', '۸' => '8', '۹' => '9',
-            '٠' => '0', '١' => '1', '٢' => '2', '٣' => '3', '٤' => '4',
-            '٥' => '5', '٦' => '6', '٧' => '7', '٨' => '8', '٩' => '9',
-        ]);
+        return Number::toEnglishFormat($value);
     }
 }
 
