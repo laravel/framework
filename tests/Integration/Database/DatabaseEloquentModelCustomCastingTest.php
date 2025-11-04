f9fed571a68269c8fc0fce126ae299d892d902df
ug-christoph <chrisoph@usergems.com>
2025-11-04 18:29:28 +0100

    add test for mutator - cast dependency

--- tests/Integration/Database/DatabaseEloquentModelCustomCastingTest.php
+++ tests/Integration/Database/DatabaseEloquentModelCustomCastingTest.php
@@ -287,6 +287,16 @@ class DatabaseEloquentModelCustomCastingTest extends DatabaseTestCase
 
         $model->undefined_cast_column = 'Glāžšķūņu rūķīši';
     }
+
+    public function testMutatorCanDependOnAnotherCastedAttribute()
+    {
+        $model = new TestEloquentModelWithCustomCast([
+            'address_line_one' => '110 Kingsbrook St.',
+            'address_line_two' => 'My Childhood House',
+        ]);
+        $model->address->lineOne = 'Changed St.';
+        $this->assertSame('Changed St. (My Childhood House)', $model->address_string);
+    }
 }
 
 class TestEloquentModelWithCustomCast extends Model
@@ -319,6 +329,27 @@ class TestEloquentModelWithCustomCast extends Model
         'anniversary_on_with_object_caching' => DateTimezoneCasterWithObjectCaching::class.':America/New_York',
         'anniversary_on_without_object_caching' => DateTimezoneCasterWithoutObjectCaching::class.':America/New_York',
     ];
+
+
+    /**
+     * A computed attribute that depends on another casted attribute.
+     *
+     * This simulates a mutator that uses the value of a casted property.
+     */
+    protected function addressString(): \Illuminate\Database\Eloquent\Casts\Attribute
+    {
+        return \Illuminate\Database\Eloquent\Casts\Attribute::get(function () {
+            $address = $this->address;
+
+            // If mergeAttributesFromClassCasts() hasn't prepared casts properly,
+            // this could be an array instead of an Address instance.
+            if (! $address instanceof Address) {
+                throw new \RuntimeException('Address was not cast before mutator access.');
+            }
+
+            return "{$address->lineOne} ({$address->lineTwo})";
+        });
+    }
 }
 
 class HashCaster implements CastsInboundAttributes
