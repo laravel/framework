64cacc96efa149b3ea27599ec78520047bc32392
Mior Muhammad Zaki <crynobone@gmail.com>
2025-11-07 18:14:03 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Http/Resources/JsonApi/JsonApiRequestTest.php
+++ tests/Integration/Http/Resources/JsonApi/JsonApiRequestTest.php
@@ -2,9 +2,46 @@
 
 namespace Illuminate\Tests\Integration\Http\Resources\JsonApi;
 
+use Illuminate\Http\Resources\JsonApi\JsonApiRequest;
+
 class JsonApiRequestTest extends TestCase
 {
-    public function testItCanResolveSparseFieldset(string $queryString, array $expected)
+    public function testItCanResolveSparseFields()
     {
+        $request = JsonApiRequest::create(uri: '/?'.http_build_query([
+            'fields' => [
+                'users' => 'name,email',
+                'teams' => 'name',
+            ],
+        ]));
+
+        $this->assertSame(['name', 'email'], $request->sparseFields('users'));
+        $this->assertSame(['name'], $request->sparseFields('teams'));
+        $this->assertSame([], $request->sparseFields('posts'));
+    }
+
+    public function testItCanResolveEmptySparseFields()
+    {
+        $request = JsonApiRequest::create(uri: '/');
+
+        $this->assertSame([], $request->sparseFields('users'));
+        $this->assertSame([], $request->sparseFields('teams'));
+        $this->assertSame([], $request->sparseFields('posts'));
+    }
+
+    public function testItCanResolveSparseIncluded()
+    {
+        $request = JsonApiRequest::create(uri: '/?'.http_build_query([
+            'include' => 'teams,users',
+        ]));
+
+        $this->assertSame(['teams', 'users'], $request->sparseIncluded());
+    }
+
+    public function testItCanResolveEmptySparseIncluded()
+    {
+        $request = JsonApiRequest::create(uri: '/');
+
+        $this->assertSame([], $request->sparseIncluded());
     }
 }
