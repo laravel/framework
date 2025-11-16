14cb6bfc2df49ddb76fc6930de85bafd68d5fd96
yousef kadah <yousefkadah@gmail.com>
2025-11-17 00:19:37 +0200

    Remove pause/resume methods from Queue Factory interface and add facade methods for queue management

--- src/Illuminate/Contracts/Queue/Factory.php
+++ src/Illuminate/Contracts/Queue/Factory.php
@@ -11,36 +11,4 @@ interface Factory
      * @return \Illuminate\Contracts\Queue\Queue
      */
     public function connection($name = null);
-
-    /**
-     * Pause a queue by name.
-     *
-     * @param  string  $queue
-     * @param  int  $ttl
-     * @return void
-     */
-    public function pause($queue, $ttl = 86400);
-
-    /**
-     * Resume a paused queue by name.
-     *
-     * @param  string  $queue
-     * @return void
-     */
-    public function resume($queue);
-
-    /**
-     * Determine if a queue is paused.
-     *
-     * @param  string  $queue
-     * @return bool
-     */
-    public function isPaused($queue);
-
-    /**
-     * Get all paused queues.
-     *
-     * @return array
-     */
-    public function getPausedQueues();
 }
