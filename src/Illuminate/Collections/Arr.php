d68663dbfc5588d2d6de158419f60a94ed0e1023
Mior Muhammad Zaki <crynobone@gmail.com>
2025-11-20 10:32:01 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Collections/Arr.php
+++ src/Illuminate/Collections/Arr.php
@@ -852,7 +852,7 @@ class Arr
         if (func_num_args() == 2) {
             array_unshift($array, $value);
         } else {
-            $array = [$key => $value] + $array;
+            $array = [($key ?? '') => $value] + $array;
         }
 
         return $array;
--- tests/Support/SupportCollectionTest.php
+++ tests/Support/SupportCollectionTest.php
@@ -4231,7 +4231,7 @@ class SupportCollectionTest extends TestCase
 
         $c = new Collection(['one' => 1, 'two' => 2]);
         $this->assertEquals(
-            [null => 0, 'one' => 1, 'two' => 2],
+            [PHP_VERSION_ID >= 80500 ? '' : null => 0, 'one' => 1, 'two' => 2],
             $c->prepend(0, null)->all()
         );
     }
