999d415ae012004492f8045c19a0c0a06011ea61
Mior Muhammad Zaki <crynobone@gmail.com>
2025-11-20 15:37:28 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Support/SupportCollectionTest.php
+++ tests/Support/SupportCollectionTest.php
@@ -4231,7 +4231,7 @@ class SupportCollectionTest extends TestCase
 
         $c = new Collection(['one' => 1, 'two' => 2]);
         $this->assertEquals(
-            ['' => 0, 'one' => 1, 'two' => 2],
+            [null => 0, 'one' => 1, 'two' => 2],
             $c->prepend(0, null)->all()
         );
     }
