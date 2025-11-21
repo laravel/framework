489261234eb7da1e0fcf573f9aab91c8f978a493
Marian Pop <mvpopuk@gmail.com>
2025-11-22 01:08:17 +0200

    style: fix code style

--- src/Illuminate/Support/Stringable.php
+++ src/Illuminate/Support/Stringable.php
@@ -1318,25 +1318,25 @@ class Stringable implements JsonSerializable, ArrayAccess, BaseStringable
         return $this->when($this->test($pattern), $callback, $default);
     }
 
-    /**
-    * Execute the given callback unless the string is empty.
-    *
-    * @param  callable  $callback
-    * @param  callable|null  $default
-    * @return static
-    */
+     /**
+     * Execute the given callback unless the string is empty.
+     *
+     * @param  callable  $callback
+     * @param  callable|null  $default
+     * @return static
+     */
     public function unlessEmpty(callable $callback, ?callable $default = null)
     {
         return $this->unless($this->isEmpty(), $callback, $default);
     }
 
-    /**
-    * Execute the given callback unless the string is not empty.
-    *
-    * @param  callable  $callback
-    * @param  callable|null  $default
-    * @return static
-    */
+     /**
+     * Execute the given callback unless the string is not empty.
+     *
+     * @param  callable  $callback
+     * @param  callable|null  $default
+     * @return static
+     */
     public function unlessNotEmpty(callable $callback, ?callable $default = null)
     {
         return $this->unless(! $this->isEmpty(), $callback, $default);
