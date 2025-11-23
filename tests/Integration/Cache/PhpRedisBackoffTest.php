976d0a82ad5b34050123dcba32938099ebc919d1
Vadim Dvorovenko <vadimon@mail.ru>
2025-11-23 20:51:10 +0700

    [12.x] Moving redis integration tests (#57860)
    * [12.x] moving RedisCacheIntegrationTest to Integration tests
    
    * [12.x] CACHE_STORE env for Redis Cache tests
    
    * [12.x] extract Backoff test to separate test

--- tests/Integration/Cache/PhpRedisBackoffTest.php
+++ tests/Integration/Cache/PhpRedisBackoffTest.php
@@ -1,10 +1,7 @@
 <?php
 
-namespace Illuminate\Tests\Cache;
+namespace Illuminate\Tests\Integration\Cache;
 
-use Illuminate\Cache\RateLimiter;
-use Illuminate\Cache\RedisStore;
-use Illuminate\Cache\Repository;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithRedis;
 use Illuminate\Redis\RedisManager;
@@ -16,7 +13,7 @@ use PHPUnit\Framework\TestCase;
 use Redis;
 
 #[RequiresPhpExtension('redis')]
-class RedisCacheIntegrationTest extends TestCase
+class PhpRedisBackoffTest extends TestCase
 {
     use InteractsWithRedis;
 
@@ -24,70 +21,17 @@ class RedisCacheIntegrationTest extends TestCase
     {
         parent::setUp();
         $this->setUpRedis();
+
+        $client = $this->redis['phpredis']->connection()->client();
+        if (! $client instanceof Redis) {
+            $this->markTestSkipped('Backoff option is only supported with phpredis in non-cluster mode');
+        }
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
         $this->tearDownRedis();
-    }
-
-    /**
-     * @param  string  $driver
-     */
-    #[DataProvider('redisDriverProvider')]
-    public function testRedisCacheAddTwice($driver)
-    {
-        $store = new RedisStore($this->redis[$driver]);
-        $repository = new Repository($store);
-        $this->assertTrue($repository->add('k', 'v', 3600));
-        $this->assertFalse($repository->add('k', 'v', 3600));
-        $this->assertGreaterThan(3500, $this->redis[$driver]->connection()->ttl('k'));
-    }
-
-    /**
-     * @param  string  $driver
-     */
-    #[DataProvider('redisDriverProvider')]
-    public function testRedisCacheRateLimiter($driver)
-    {
-        $store = new RedisStore($this->redis[$driver]);
-        $repository = new Repository($store);
-        $rateLimiter = new RateLimiter($repository);
-
-        $this->assertFalse($rateLimiter->tooManyAttempts('key', 1));
-        $this->assertEquals(1, $rateLimiter->hit('key', 60));
-        $this->assertTrue($rateLimiter->tooManyAttempts('key', 1));
-        $this->assertFalse($rateLimiter->tooManyAttempts('key', 2));
-    }
-
-    /**
-     * Breaking change.
-     *
-     * @param  string  $driver
-     */
-    #[DataProvider('redisDriverProvider')]
-    public function testRedisCacheAddFalse($driver)
-    {
-        $store = new RedisStore($this->redis[$driver]);
-        $repository = new Repository($store);
-        $repository->forever('k', false);
-        $this->assertFalse($repository->add('k', 'v', 60));
-        $this->assertEquals(-1, $this->redis[$driver]->connection()->ttl('k'));
-    }
-
-    /**
-     * Breaking change.
-     *
-     * @param  string  $driver
-     */
-    #[DataProvider('redisDriverProvider')]
-    public function testRedisCacheAddNull($driver)
-    {
-        $store = new RedisStore($this->redis[$driver]);
-        $repository = new Repository($store);
-        $repository->forever('k', null);
-        $this->assertFalse($repository->add('k', 'v', 60));
     }
 
     #[DataProvider('phpRedisBackoffAlgorithmsProvider')]
