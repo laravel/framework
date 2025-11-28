c801ab25ac0a06529d657618dce8de24ee4a474a
Taylor Otwell <taylor@laravel.com>
2025-11-28 11:57:31 -0600

    formatting

--- src/Illuminate/Http/Client/FluentPromise.php
+++ src/Illuminate/Http/Client/FluentPromise.php
@@ -21,6 +21,48 @@ class FluentPromise implements PromiseInterface
     {
     }
 
+    #[\Override]
+    public function then(?callable $onFulfilled = null, ?callable $onRejected = null): PromiseInterface
+    {
+        return $this->__call('then', [$onFulfilled, $onRejected]);
+    }
+
+    #[\Override]
+    public function otherwise(callable $onRejected): PromiseInterface
+    {
+        return $this->__call('otherwise', [$onRejected]);
+    }
+
+    #[\Override]
+    public function resolve($value): void
+    {
+        $this->guzzlePromise->resolve($value);
+    }
+
+    #[\Override]
+    public function reject($reason): void
+    {
+        $this->guzzlePromise->reject($reason);
+    }
+
+    #[\Override]
+    public function cancel(): void
+    {
+        $this->guzzlePromise->cancel();
+    }
+
+    #[\Override]
+    public function wait(bool $unwrap = true)
+    {
+        return $this->__call('wait', [$unwrap]);
+    }
+
+    #[\Override]
+    public function getState(): string
+    {
+        return $this->guzzlePromise->getState();
+    }
+
     /**
      * Get the underlying Guzzle promise.
      *
@@ -49,47 +91,5 @@ class FluentPromise implements PromiseInterface
         $this->guzzlePromise = $result;
 
         return $this;
-    }
-
-    #[\Override]
-    public function then(?callable $onFulfilled = null, ?callable $onRejected = null): PromiseInterface
-    {
-        return $this->__call('then', [$onFulfilled, $onRejected]);
-    }
-
-    #[\Override]
-    public function otherwise(callable $onRejected): PromiseInterface
-    {
-        return $this->__call('otherwise', [$onRejected]);
-    }
-
-    #[\Override]
-    public function getState(): string
-    {
-        return $this->guzzlePromise->getState();
-    }
-
-    #[\Override]
-    public function resolve($value): void
-    {
-        $this->guzzlePromise->resolve($value);
-    }
-
-    #[\Override]
-    public function reject($reason): void
-    {
-        $this->guzzlePromise->reject($reason);
-    }
-
-    #[\Override]
-    public function cancel(): void
-    {
-        $this->guzzlePromise->cancel();
-    }
-
-    #[\Override]
-    public function wait(bool $unwrap = true)
-    {
-        return $this->__call('wait', [$unwrap]);
     }
 }
