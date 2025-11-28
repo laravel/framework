b0c1fbf818cb44f3682433f954f8da0e1a2c1b2e
Luke Kuzmish <lakuzmish@gmail.com>
2025-11-28 09:53:11 -0500

    tests

--- tests/Integration/Http/HttpClientTest.php
+++ tests/Integration/Http/HttpClientTest.php
@@ -3,6 +3,9 @@
 namespace Illuminate\Tests\Integration\Http;
 
 use Illuminate\Http\Client\Events\RequestSending;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Pool;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Facade;
@@ -36,5 +39,32 @@ class HttpClientTest extends TestCase
         Facade::clearResolvedInstances();
 
         $this->assertCount(2, Http::getGlobalMiddleware());
+    }
+
+    public function testPoolCanReceiveCallback(): void
+    {
+        Http::fake([
+            'https://laravel.com*' => Http::response('Laravel'),
+            'https://forge.laravel.com*' => Http::response('Forge'),
+            'https://nightwatch.laravel.com*' => Http::response('Tim n Jess'),
+        ]);
+
+        $responses = Http::pool(function (Pool $pool) {
+            $pool->as('laravel', function (PendingRequest $request) {
+                $request->get('https://laravel.com');
+            });
+            $pool->as(
+                'forge',
+                fn (PendingRequest $request) => $request->get('https://forge.laravel.com')
+                    ->then(fn (Response $response): int => strlen($response->getBody()))
+            );
+            $pool->as('nightwatch')->get('https://nightwatch.laravel.com');
+        });
+
+        $this->assertInstanceOf(Response::class, $responses['laravel']);
+        $this->assertEquals(5, $responses['forge']);
+        $this->assertEquals('Tim n Jess', $responses['nightwatch']->getBody());
+
+        $this->assertCount(3, Http::recorded());
     }
 }
