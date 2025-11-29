c5e072fc35c3f1f436ce45ffff24d6f4ba0db3b5
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-11-29 11:18:11 -0500

    Update PendingRequest.php

--- src/Illuminate/Http/Client/PendingRequest.php
+++ src/Illuminate/Http/Client/PendingRequest.php
@@ -885,10 +885,10 @@ class PendingRequest
      * Send a pool of asynchronous requests concurrently.
      *
      * @param  (callable(\Illuminate\Http\Client\Pool): mixed)  $callback
-     * @param  int|null|(callable(non-negative-int): int)  $concurrency
+     * @param  int|null  $concurrency
      * @return array<array-key, \Illuminate\Http\Client\Response|\Illuminate\Http\Client\ConnectionException|\Illuminate\Http\Client\RequestException>
      */
-    public function pool(callable $callback, int|callable|null $concurrency = 2)
+    public function pool(callable $callback, ?int $concurrency = 2)
     {
         $results = [];
 
