53905ecbfa497890c12e1c70f45212a1b8d46fcf
Luke Kuzmish <lakuzmish@gmail.com>
2025-11-30 07:33:31 -0500

    proper target

--- src/Illuminate/Container/Attributes/Lazy.php
+++ src/Illuminate/Container/Attributes/Lazy.php
@@ -6,13 +6,12 @@ use Attribute;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Container\ContextualAttribute;
 use ReflectionNamedType;
-use ReflectionParameter;
 
-#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_CLASS | Attribute::TARGET_PARAMETER)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_PARAMETER)]
 class Lazy implements ContextualAttribute
 {
     public static function resolve(self $attribute, Container $container, ReflectionNamedType $type)
     {
-        return proxy($type->getName(), fn () => $container->make($type->getName()));
+        return proxy($type->getName(), static fn () => $container->make($type->getName()));
     }
 }
