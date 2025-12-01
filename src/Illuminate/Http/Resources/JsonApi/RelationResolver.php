a524678389572676b63b926e072656c94ee2f0bd
Mior Muhammad Zaki <crynobone@gmail.com>
2025-12-01 10:59:22 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Illuminate/Http/Resources/JsonApi/RelationResolver.php
+++ src/Illuminate/Http/Resources/JsonApi/RelationResolver.php
@@ -2,8 +2,9 @@
 
 namespace Illuminate\Http\Resources\JsonApi;
 
+use Closure;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Collection;
 
 class RelationResolver
 {
