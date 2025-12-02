dd055b0994df604ddab4ce1342fc52e6eff4dc45
Punyapal Shah <mrpunyapal@gmail.com>
2025-12-02 12:43:16 +0530

    Add dark mode support for email templates and extract media queries

--- src/Illuminate/Mail/resources/views/html/layout.blade.php
+++ src/Illuminate/Mail/resources/views/html/layout.blade.php
@@ -4,8 +4,8 @@
 <title>{{ config('app.name') }}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="color-scheme" content="light">
-<meta name="supported-color-schemes" content="light">
+<meta name="color-scheme" content="light dark">
+<meta name="supported-color-schemes" content="light dark">
 <style>
 @media only screen and (max-width: 600px) {
 .inner-body {
@@ -22,6 +22,8 @@ width: 100% !important;
 width: 100% !important;
 }
 }
+
+{!! \Illuminate\Mail\Markdown::getHeadStyles() !!}
 </style>
 {!! $head ?? '' !!}
 </head>
