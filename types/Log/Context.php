cbadb269cca2ebb1be47a32619e19aa523191706
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-12-04 05:02:49 -0500

    explicit

--- types/Log/Context.php
+++ types/Log/Context.php
@@ -8,7 +8,7 @@ use function PHPStan\Testing\assertType;
 $repository = new Repository(new Dispatcher());
 
 $value = $repository->scope(fn (): int => random_int(-100, 100));
-assertType('int', $value);
+assertType('int<-100, 100>', $value);
 
 $void = $repository->scope(function () { // @phpstan-ignore method.void
 });
