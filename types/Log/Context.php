f0c99866cd53ceacb713d72f08c5b9caaeb5ffab
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-12-04 04:58:25 -0500

    is that random enough for you, stan?

--- types/Log/Context.php
+++ types/Log/Context.php
@@ -7,7 +7,7 @@ use function PHPStan\Testing\assertType;
 
 $repository = new Repository(new Dispatcher());
 
-$value = $repository->scope(fn (): string => str_repeat('a', 5));
+$value = $repository->scope(fn (): string => str_repeat('a', random_int(1, 4)));
 assertType('string', $value);
 
 $void = $repository->scope(function () { }); // @phpstan-ignore method.void
