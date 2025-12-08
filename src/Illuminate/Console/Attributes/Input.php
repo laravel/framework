faf8ce43ada593fbd671fae7a8923a772155afdc
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2025-12-08 16:42:46 +0100

    Add Input abstract attribute class

---
+++ src/Illuminate/Console/Attributes/Input.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Illuminate\Console\Attributes;
+
+use Attribute;
+use ReflectionParameter;
+use ReflectionIntersectionType;
+use ReflectionNamedType;
+use ReflectionType;
+use ReflectionUnionType;
+
+#[Attribute(Attribute::TARGET_PARAMETER)]
+abstract class Input
+{
+    public function __construct(public string $parameter)
+    {
+    }
+
+    /**
+     * Resolve the input.
+     *
+     * @param  self  $attribute
+     * @return mixed
+     *
+     * @throws \TypeError on type mismatch
+     */
+    public static function resolve(self $attribute, ReflectionParameter $parameter)
+    {
+        $input = $attribute->getInput();
+        $type = $parameter->getType();
+
+        if (! $this->checkType($type, $input)) {
+            throw new TypeError(sprintf('"%s" is not of type string. %s given', $attribute->parameter, get_debug_type($input)));
+        }
+      
+        return $input;
+    }
+
+    private function checkType(ReflectionType $expectedType, $actualValue): bool
+    {
+        return match (true) {
+            $expectedType instanceof ReflectionIntersectionType => array_all($expectedType->getTypes(), $this->checkType(...)),
+            $expectedType instanceof ReflectionUnionType => array_any($expectedType->getTypes(), $this->checkType(...)),
+            $expectedType instanceof ReflectionNamedType => class_exists($expectedType) ? $actualValue instanceof $expectedType : get_debug_type($actualValue) === $expectedType->getName(),
+        };
+    }
+
+    /**
+     * @throws \InvalidArgumentException when neither an option nor an argument
+     *                                   with give key exists and no default value was given 
+     */
+    abstract private function getInput();
+}
