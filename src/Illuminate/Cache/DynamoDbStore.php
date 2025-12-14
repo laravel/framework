e509cdfa223f5975f6e14ade56a7ec949f21d03e
Luke Kuzmish <lakuzmish@gmail.com>
2025-12-14 14:34:08 -0500

    oooh that's why there was a map there

--- src/Illuminate/Cache/DynamoDbStore.php
+++ src/Illuminate/Cache/DynamoDbStore.php
@@ -113,7 +113,7 @@ class DynamoDbStore implements LockProvider, Store
         $now = Carbon::now();
 
         return array_merge(
-            (new Collection(array_flip($keys)))->all(),
+            (new Collection($keys))->mapWithKeys(fn ($key) => [$key => null])->all(),
             (new Collection($response['Responses'][$this->table]))->mapWithKeys(function ($response) use ($now) {
                 if ($this->isExpired($response, $now)) {
                     $value = null;
