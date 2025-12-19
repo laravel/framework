deeeb6c77f2c8f5c0f249934fdef4879bcbbe060
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2025-12-19 23:45:57 +0100

    [12.x] add default error message

--- src/Illuminate/Translation/lang/en/validation.php
+++ src/Illuminate/Translation/lang/en/validation.php
@@ -146,6 +146,7 @@ return [
     'required_if_accepted' => 'The :attribute field is required when :other is accepted.',
     'required_if_declined' => 'The :attribute field is required when :other is declined.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
+    'required_unless_accepted' => 'The :attribute field is required unless :other is accepted.',
     'required_with' => 'The :attribute field is required when :values is present.',
     'required_with_all' => 'The :attribute field is required when :values are present.',
     'required_without' => 'The :attribute field is required when :values is not present.',
