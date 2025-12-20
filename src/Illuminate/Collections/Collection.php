51390b68f4b8f12171385bb2d3ad1d6b5d878b25
Jesper Noordsij <jesper.noordsij@gmail.com>
2025-12-20 11:18:05 +0100

    Fixup missing parentheses in Collection@countBy param docblocks

--- src/Illuminate/Collections/Collection.php
+++ src/Illuminate/Collections/Collection.php
@@ -1863,7 +1863,7 @@ class Collection implements ArrayAccess, CanBeEscapedWhenCastToString, Enumerabl
     /**
      * Count the number of items in the collection by a field or using a callback.
      *
-     * @param  (callable(TValue, TKey): array-key|\UnitEnum)|string|null  $countBy
+     * @param  (callable(TValue, TKey): (array-key|\UnitEnum))|string|null  $countBy
      * @return static<array-key, int>
      */
     public function countBy($countBy = null)
