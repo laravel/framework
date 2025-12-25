27de0d9eaf1889da0421d9c21402c84de4cacfc3
Jack Bayliss <jjbayliss@icloud.com>
2025-12-25 16:28:01 +0000

    Adjust AuthDatabaseTokenRepositoryTest (#58206)

--- tests/Auth/AuthDatabaseTokenRepositoryTest.php
+++ tests/Auth/AuthDatabaseTokenRepositoryTest.php
@@ -104,6 +104,8 @@ class AuthDatabaseTokenRepositoryTest extends TestCase
 
     public function testRecentlyCreatedReturnsTrueIfRecordIsRecentlyCreated()
     {
+        Carbon::setTestNow(Carbon::now());
+
         $repo = $this->getRepo();
         $repo->getConnection()->shouldReceive('table')->once()->with('table')->andReturn($query = m::mock(stdClass::class));
         $query->shouldReceive('where')->once()->with('email', 'email')->andReturn($query);
@@ -113,10 +115,14 @@ class AuthDatabaseTokenRepositoryTest extends TestCase
         $user->shouldReceive('getEmailForPasswordReset')->once()->andReturn('email');
 
         $this->assertTrue($repo->recentlyCreatedToken($user));
+
+        Carbon::setTestNow();
     }
 
     public function testRecentlyCreatedReturnsFalseIfValidRecordExists()
     {
+        Carbon::setTestNow(Carbon::now());
+
         $repo = $this->getRepo();
         $repo->getConnection()->shouldReceive('table')->once()->with('table')->andReturn($query = m::mock(stdClass::class));
         $query->shouldReceive('where')->once()->with('email', 'email')->andReturn($query);
@@ -126,6 +132,8 @@ class AuthDatabaseTokenRepositoryTest extends TestCase
         $user->shouldReceive('getEmailForPasswordReset')->once()->andReturn('email');
 
         $this->assertFalse($repo->recentlyCreatedToken($user));
+
+        Carbon::setTestNow();
     }
 
     public function testDeleteMethodDeletesByToken()
