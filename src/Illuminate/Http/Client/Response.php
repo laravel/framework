91049f769dffb160518c4aeed25ed40a969a25ae
foremtehan <53290883+foremtehan@users.noreply.github.com>
2025-12-29 15:02:28 -0500

    [12.x] Simplify json decoding with null coalescing assignment

--- src/Illuminate/Http/Client/Response.php
+++ src/Illuminate/Http/Client/Response.php
@@ -84,9 +84,7 @@ class Response implements ArrayAccess, Stringable
      */
     public function json($key = null, $default = null)
     {
-        if (! $this->decoded) {
-            $this->decoded = json_decode($this->body(), true);
-        }
+        $this->decoded ??= json_decode($this->body(), true);
 
         if (is_null($key)) {
             return $this->decoded;
