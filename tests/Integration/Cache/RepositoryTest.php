5d5f548fb9e76ab71a9531961a3c5f261b429624
Jack <jjbayliss@icloud.com>
2026-01-01 17:28:51 +0000

    Add enum support to repository

--- tests/Integration/Cache/RepositoryTest.php
+++ tests/Integration/Cache/RepositoryTest.php
@@ -279,4 +279,53 @@ class RepositoryTest extends TestCase
         $this->assertSame('foo', $events[0]->key);
         $this->assertSame(1, $events[0]->seconds);
     }
+
+    public function testWorksWithEnumKey()
+    {
+        $cache = Cache::driver('array');
+
+        // put / get / has / missing
+        $cache->put(TestCacheKey::FOO, 'value');
+        $this->assertSame('value', $cache->get(TestCacheKey::FOO));
+        $this->assertTrue($cache->has(TestCacheKey::FOO));
+        $this->assertFalse($cache->missing(TestCacheKey::FOO));
+
+        // pull
+        $this->assertSame('value', $cache->pull(TestCacheKey::FOO));
+        $this->assertNull($cache->get(TestCacheKey::FOO));
+
+        // add
+        $this->assertTrue($cache->add(TestCacheKey::FOO, 'added', 3600));
+        $this->assertFalse($cache->add(TestCacheKey::FOO, 'duplicate', 3600));
+        $this->assertSame('added', $cache->get(TestCacheKey::FOO));
+
+        // forever
+        $cache->forever(TestCacheKey::BAR, 'forever');
+        $this->assertSame('forever', $cache->get(TestCacheKey::BAR));
+
+        // remember / rememberForever / sear
+        $this->assertSame('remember', $cache->remember(TestCacheKey::BAZ, 3600, fn () => 'remember'));
+        $this->assertSame('forever', $cache->rememberForever(TestCacheKey::QUX, fn () => 'forever'));
+        $this->assertSame('forever', $cache->sear(TestCacheKey::QUX, fn () => 'ignored'));
+
+        // increment / decrement
+        $cache->put(TestCacheKey::FOO, 5);
+        $this->assertSame(6, $cache->increment(TestCacheKey::FOO));
+        $this->assertSame(5, $cache->decrement(TestCacheKey::FOO));
+
+        // forget
+        $cache->put(TestCacheKey::FOO, 'x');
+        $this->assertTrue($cache->forget(TestCacheKey::FOO));
+        $this->assertNull($cache->get(TestCacheKey::FOO));
+    }
+
+}
+
+enum TestCacheKey: string
+{
+    case FOO = 'foo';
+    case BAR = 'bar';
+    case BAZ = 'baz';
+    case QUX = 'qux';
+
 }
