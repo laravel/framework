e7d021fa2b36438a0eaa1276aa8658a406e19daf
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2025-12-31 21:48:20 -0500

    [13.x] Return data object from `ModelInspector` to make `show:model` more flexible (#58230)
    * Update facade docblocks
    
    * change to an array
    
    * model info
    
    * switch to DTO
    
    * switch to property access
    
    * Enhance docblocks and improve offsetExists method
    
    * Refactor offsetExists and offsetGet methods
    
    * Apply suggestion from @shaedrich
    
    Co-authored-by: Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>

--- tests/Integration/Database/ModelInspectorTest.php
+++ tests/Integration/Database/ModelInspectorTest.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelInfo;
 use Illuminate\Database\Eloquent\ModelInspector;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Schema\Blueprint;
@@ -50,7 +51,7 @@ class ModelInspectorTest extends DatabaseTestCase
         $this->assertModelInfo($modelInfo);
     }
 
-    private function assertModelInfo(array $modelInfo)
+    private function assertModelInfo(ModelInfo|array $modelInfo)
     {
         $this->assertEquals(ModelInspectorTestModel::class, $modelInfo['class']);
         $this->assertEquals(Schema::getConnection()->getConfig()['name'], $modelInfo['database']);
@@ -166,6 +167,8 @@ class ModelInspectorTest extends DatabaseTestCase
         $this->assertEquals('created', $modelInfo['observers'][0]['event']);
         $this->assertCount(1, $modelInfo['observers'][0]['observer']);
         $this->assertEquals("Illuminate\Tests\Integration\Database\ModelInspectorTestModelObserver@created", $modelInfo['observers'][0]['observer'][0]);
+        $this->assertEquals(ModelInspectorTestModelEloquentCollection::class, $modelInfo['collection']);
+        $this->assertEquals(ModelInspectorTestModelBuilder::class, $modelInfo['builder']);
     }
 
     private function assertAttributes($expectedAttributes, $actualAttributes)
