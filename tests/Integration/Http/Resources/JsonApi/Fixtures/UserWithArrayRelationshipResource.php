cd1633464ebb5a78c61902132ce4ca3154fed172
Mateus Guimarães <mateus.jatene@gmail.com>
2026-01-08 20:27:51 -0300

    Deduplicate circular refs

--- tests/Integration/Http/Resources/JsonApi/Fixtures/UserWithArrayRelationshipResource.php
+++ tests/Integration/Http/Resources/JsonApi/Fixtures/UserWithArrayRelationshipResource.php
@@ -5,15 +5,13 @@ namespace Illuminate\Tests\Integration\Http\Resources\JsonApi\Fixtures;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\JsonApi\JsonApiResource;
 
-class AuthorResource extends JsonApiResource
+class UserWithArrayRelationshipResource extends JsonApiResource
 {
-    protected array $relationships = [
-        'comments',
-        'profile',
-        'chaperonePosts' => PostResource::class,
-    ];
+    public function toType(Request $request)
+    {
+        return 'users';
+    }
 
-    #[\Override]
     public function toAttributes(Request $request)
     {
         return [
