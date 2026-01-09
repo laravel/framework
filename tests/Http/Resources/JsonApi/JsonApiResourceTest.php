9db2ff3d90c95ed79535e0dd2cc13c7caacc5bf3
Mateus Guimarães <mateus.jatene@gmail.com>
2026-01-09 01:00:30 -0300

    Revert "more failing tests"
    This reverts commit c32d36a33b4d525e218f0133bba287d49c8c7c43.

--- tests/Http/Resources/JsonApi/JsonApiResourceTest.php
+++ tests/Http/Resources/JsonApi/JsonApiResourceTest.php
@@ -3,11 +3,7 @@
 namespace Illuminate\Tests\Http\Resources\JsonApi;
 
 use BadMethodCallException;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Http\Resources\JsonApi\Exceptions\ResourceIdentificationException;
-use Illuminate\Http\Resources\JsonApi\JsonApiRequest;
 use Illuminate\Http\Resources\JsonApi\JsonApiResource;
 use PHPUnit\Framework\TestCase;
 
@@ -42,50 +38,5 @@ class JsonApiResourceTest extends TestCase
         $this->expectExceptionMessage('Using Illuminate\Http\Resources\JsonApi\JsonApiResource::withoutWrapping() method is not allowed.');
 
         JsonApiResource::withoutWrapping();
-    }
-
-    public function testPlainJsonResourceWithArrayCannotBeIncluded()
-    {
-        $this->expectException(ResourceIdentificationException::class);
-
-        $model = new class extends Model {};
-        $model->id = 1;
-
-        $resource = new JsonApiResource($model);
-        $resource->loadedRelationshipsMap = [
-            [new JsonResource(['id' => 1, 'name' => 'test']), 'things', '1', true],
-        ];
-
-        $resource->resolveIncludedResourceObjects(new JsonApiRequest);
-    }
-
-    public function testIncludedResourcesCanBeArrayBackedCustomResources()
-    {
-        $model = new class extends Model {};
-        $model->id = 1;
-
-        $resource = new JsonApiResource($model);
-        $resource->loadedRelationshipsMap = [
-            [new ArrayBackedJsonApiResource(['id' => 99, 'name' => 'test']), 'things', '99', true],
-        ];
-
-        $included = $resource->resolveIncludedResourceObjects(new JsonApiRequest);
-
-        $this->assertCount(1, $included);
-        $this->assertSame('99', $included[0]['id']);
-        $this->assertSame('things', $included[0]['type']);
-    }
-}
-
-class ArrayBackedJsonApiResource extends JsonApiResource
-{
-    public function toId(Request $request)
-    {
-        return (string) $this->resource['id'];
-    }
-
-    public function toAttributes(Request $request)
-    {
-        return ['name' => $this->resource['name']];
     }
 }
