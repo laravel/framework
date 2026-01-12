eb2d2281e92b045d4db359f821e134466c584725
Taylor Otwell <taylor@laravel.com>
2026-01-12 09:30:55 -0600

    fix typo

--- src/Illuminate/Queue/WorkerStopReason.php
+++ src/Illuminate/Queue/WorkerStopReason.php
@@ -6,7 +6,7 @@ enum WorkerStopReason: string
 {
     case Interrupted = 'interrupted';
     case MaxJobsExceeded = 'max_jobs';
-    case MaxMemoryExceededMemory = 'memory';
+    case MaxMemoryExceeded = 'memory';
     case MaxTimeExceeded = 'max_time';
     case QueueEmpty = 'empty';
     case ReceivedRestartSignal = 'restart_signal';
