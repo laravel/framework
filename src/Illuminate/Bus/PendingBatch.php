8eb97944a74d52031c47c81df9b93d790f996856
Taylor Otwell <taylor@laravel.com>
2026-01-13 08:50:59 -0600

    Ensure jobs are re-indexed in PendingBatch

--- src/Illuminate/Bus/PendingBatch.php
+++ src/Illuminate/Bus/PendingBatch.php
@@ -64,7 +64,7 @@ class PendingBatch
     {
         $this->container = $container;
 
-        $this->jobs = $jobs->filter()->each(function (object|array $job) {
+        $this->jobs = $jobs->filter()->values()->each(function (object|array $job) {
             $this->ensureJobIsBatchable($job);
         });
     }
