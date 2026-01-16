b655bf433b83ba7a1c3a6f39df789af4fa62f6c3
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2026-01-16 08:41:56 -0800

    add VerifyCsrfToken alias

---
+++ src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Illuminate\Foundation\Http\Middleware;
+
+/**
+ * @deprecated Use PreventRequestForgery instead.
+ */
+class VerifyCsrfToken extends PreventRequestForgery
+{
+    //
+}
