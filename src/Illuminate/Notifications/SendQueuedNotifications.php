6cf145704b0b3aba5d8f9772488f4cfdf9ff3896
Taylor Otwell <taylor@laravel.com>
2026-01-16 09:46:11 -0800

    Update SendQueuedNotifications.php

--- src/Illuminate/Notifications/SendQueuedNotifications.php
+++ src/Illuminate/Notifications/SendQueuedNotifications.php
@@ -87,6 +87,7 @@ class SendQueuedNotifications implements ShouldQueue
         $this->tries = property_exists($notification, 'tries') ? $notification->tries : null;
         $this->timeout = property_exists($notification, 'timeout') ? $notification->timeout : null;
         $this->maxExceptions = property_exists($notification, 'maxExceptions') ? $notification->maxExceptions : null;
+
         $this->deleteWhenMissingModels = property_exists($notification, 'deleteWhenMissingModels')
             ? $notification->deleteWhenMissingModels
             : false;
