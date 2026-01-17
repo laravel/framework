be7b10d26545a0141da4fccb06b0aafbd3043ed7
Jack <jjbayliss@icloud.com>
2026-01-17 23:20:47 +0000

    Add queue name to JobPopping

--- src/Illuminate/Queue/Events/JobPopping.php
+++ src/Illuminate/Queue/Events/JobPopping.php
@@ -8,9 +8,11 @@ class JobPopping
      * Create a new event instance.
      *
      * @param  string  $connectionName  The connection name.
+     * @param  string  $queueName  The queue name.
      */
     public function __construct(
         public $connectionName,
+        public $queueName
     ) {
     }
 }
