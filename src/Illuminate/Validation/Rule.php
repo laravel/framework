12dbb2a4dbd27677dc090f0efb3b4ae802e840f4
Shaung Bhone <phonelaayy@gmail.com>
2026-01-18 01:56:13 +0700

    Remove wrong commit

--- src/Illuminate/Validation/Rule.php
+++ src/Illuminate/Validation/Rule.php
@@ -196,14 +196,6 @@ class Rule
     }
 
     /**
-     * Get a datetime rule builder instance.
-     */
-    public static function dateTime(): Date
-    {
-        return (new Date)->format('Y-m-d H:i:s');
-    }
-
-    /**
      * Get an email rule builder instance.
      *
      * @return \Illuminate\Validation\Rules\Email
