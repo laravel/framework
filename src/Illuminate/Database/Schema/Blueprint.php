f6be334d1471f5a613571d1e4916d2bb21fc6001
Bram Ceulemans <bram@ceulemans.dev>
2026-01-21 17:49:25 +0000

    Use `isset()` instead of `in_array()` for trait

--- src/Illuminate/Database/Schema/Blueprint.php
+++ src/Illuminate/Database/Schema/Blueprint.php
@@ -1062,7 +1062,7 @@ class Blueprint
 
         $modelTraits = class_uses_recursive($model);
 
-        if (in_array(HasUlids::class, $modelTraits, true)) {
+        if (isset($modelTraits[HasUlids::class])) {
             return $this->foreignUlid($column, 26)
                 ->table($model->getTable())
                 ->referencesModelColumn($model->getKeyName());
