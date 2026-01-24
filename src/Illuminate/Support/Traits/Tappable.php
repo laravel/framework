77e430a1da0a6b0568f161d81d0a16650be0af73
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2026-01-24 23:27:24 +0100

    Add until logic to Tappable trait

--- src/Illuminate/Support/Traits/Tappable.php
+++ src/Illuminate/Support/Traits/Tappable.php
@@ -7,11 +7,24 @@ trait Tappable
     /**
      * Call the given Closure with this instance then return the instance.
      *
-     * @param  (callable($this): mixed)|null  $callback
+     * @param  (callable($this): mixed)|int|null  $callback
      * @return ($callback is null ? \Illuminate\Support\HigherOrderTapProxy : $this)
      */
     public function tap($callback = null)
     {
         return tap($this, $callback);
+    }
+
+    /**
+     * Call the given Closure with this instance until the given condition returns false
+     * then return the instance.
+     *
+     * @param  (callable(): bool)  $until
+     * @param  (callable($this): mixed)|null  $callback
+     * @return ($callback is null ? \Illuminate\Support\HigherOrderTapProxy : $this)
+     */
+    public function tapUntil($until, $callback = null)
+    {
+        return tap_until($this, $until, $callback);
     }
 }
