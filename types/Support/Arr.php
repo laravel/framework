1c77d29e01f408e4d8fcb0493c5a8a07632de5a4
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2026-01-27 23:25:20 +0100

    oO

--- types/Support/Arr.php
+++ types/Support/Arr.php
@@ -142,16 +142,16 @@ assertType('false', Arr::arrayable(1));
 
 assertType('array<int, array<1|2|3>>', Arr::crossJoin([1], [2], ['a' => 3]));
 
-/** @phpstan-ignore staticMethod.alreadyNarrowedType */
+/** @phpstan-ignore staticMethod.impossibleType , staticMethod.alreadyNarrowedType */
 assertType('false', Arr::isAssoc([1]));
 
-/** @phpstan-ignore staticMethod.alreadyNarrowedType */
+/** @phpstan-ignore staticMethod.impossibleType , staticMethod.alreadyNarrowedType */
 assertType('true', Arr::isAssoc(['a' => 1]));
 
-/** @phpstan-ignore staticMethod.alreadyNarrowedType */
+/** @phpstan-ignore staticMethod.impossibleType , staticMethod.alreadyNarrowedType */
 assertType('true', Arr::isList([1]));
 
-/** @phpstan-ignore staticMethod.alreadyNarrowedType */
+/** @phpstan-ignore staticMethod.impossibleType , staticMethod.alreadyNarrowedType */
 assertType('false', Arr::isList(['a' => 1]));
 
 assertType('array<1|2|3>', Arr::sort([1, 3, 2]));
