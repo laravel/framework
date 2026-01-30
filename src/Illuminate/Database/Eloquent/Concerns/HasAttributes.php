43633276340cff8434a1bd9e08d266bb34d61062
Jack Bayliss <jjbayliss@icloud.com>
2026-01-30 22:35:12 +0000

    [12.x] brick/math `of` float deprecation (#58563)
    * Sort math deprecation bits
    
    * adjust this
    
    * Update ValidatesAttributes.php

--- src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasAttributes.php
@@ -1502,7 +1502,7 @@ trait HasAttributes
     protected function asDecimal($value, $decimals)
     {
         try {
-            return (string) BigDecimal::of($value)->toScale($decimals, RoundingMode::HALF_UP);
+            return (string) BigDecimal::of((string) $value)->toScale($decimals, RoundingMode::HALF_UP);
         } catch (BrickMathException $e) {
             throw new MathException('Unable to cast value to a decimal.', previous: $e);
         }
