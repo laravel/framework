b29e325fe63c109d3498dbc3b21e7a334701a564
Günther Debrauwer <gunther.debrauwer@whoownsthezebra.be>
2026-02-03 18:28:55 +0100

    Fix

--- tests/Support/SupportTestingBusFakeTest.php
+++ tests/Support/SupportTestingBusFakeTest.php
@@ -865,12 +865,6 @@ class SupportTestingBusFakeTest extends TestCase
             ]);
         });
 
-        $this->fake->assertBatched([
-            new BusFakeJobWithSerialization('foo'),
-            new BusFakeJobWithSerialization('bar'),
-            new BusFakeJobWithSerialization('baz'),
-        ]);
-
         try {
             $this->fake->assertBatched(function (PendingBatchFake $batchedCollection) {
                 return $batchedCollection->assertJobs([
