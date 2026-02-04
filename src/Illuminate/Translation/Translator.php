b8d9b9f1c73a399672fece340efe8cabbc5bd5bc
Jack <jjbayliss@icloud.com>
2026-02-04 22:50:03 +0000

    Revert "Update Translator.php"
    This reverts commit 0680c99c0d33542b5d7723038fcb624a1bc15c51.

--- src/Illuminate/Translation/Translator.php
+++ src/Illuminate/Translation/Translator.php
@@ -456,9 +456,7 @@ class Translator extends NamespacedItemResolver implements TranslatorContract
     {
         $locales = array_filter([$locale ?: $this->locale, $this->fallback]);
 
-        $determined  = call_user_func($this->determineLocalesUsing ?: fn () => $locales, $locales);
-
-        dd($determined);
+        $determined = call_user_func($this->determineLocalesUsing ?: fn () => $locales, $locales);
 
         return array_values(array_unique($determined));
     }
