5ef4fcdefd161acbf0ef0fc7f3481ed323403164
Luke Kuzmish <42181698+cosmastech@users.noreply.github.com>
2026-02-04 10:14:59 -0500

    use is_a (#58607)

--- src/Illuminate/Support/Traits/InteractsWithData.php
+++ src/Illuminate/Support/Traits/InteractsWithData.php
@@ -373,7 +373,7 @@ trait InteractsWithData
      */
     protected function isBackedEnum($enumClass)
     {
-        return enum_exists($enumClass) && method_exists($enumClass, 'tryFrom');
+        return is_a($enumClass, \BackedEnum::class, true);
     }
 
     /**
