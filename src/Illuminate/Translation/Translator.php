0680c99c0d33542b5d7723038fcb624a1bc15c51
Jack <jjbayliss@icloud.com>
2026-02-04 22:49:58 +0000

    Update Translator.php

--- src/Illuminate/Translation/Translator.php
+++ src/Illuminate/Translation/Translator.php
@@ -456,7 +456,9 @@ class Translator extends NamespacedItemResolver implements TranslatorContract
     {
         $locales = array_filter([$locale ?: $this->locale, $this->fallback]);
 
-        $determined = call_user_func($this->determineLocalesUsing ?: fn () => $locales, $locales);
+        $determined  = call_user_func($this->determineLocalesUsing ?: fn () => $locales, $locales);
+
+        dd($determined);
 
         return array_values(array_unique($determined));
     }
