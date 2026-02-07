74942ea9be11db82448402d4bf82fefb1a7c2449
Ali Khosrojerdi <ali7.khosrojerdi@gmail.com>
2026-02-07 22:17:10 +0330

    refactor: remove hard code function name and simplify return with ??

--- src/Illuminate/Database/Eloquent/Concerns/HasRelationships.php
+++ src/Illuminate/Database/Eloquent/Concerns/HasRelationships.php
@@ -908,11 +908,11 @@ trait HasRelationships
         $caller = Arr::first(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), function ($trace) {
             return ! in_array(
                 $trace['function'],
-                array_merge(static::$manyMethods, ['guessBelongsToManyRelation'])
+                array_merge(static::$manyMethods, [__FUNCTION__])
             );
         });
 
-        return ! is_null($caller) ? $caller['function'] : null;
+        return $caller['function'] ?? null;
     }
 
     /**
