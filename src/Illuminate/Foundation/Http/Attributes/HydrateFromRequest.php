f52bc9b877031e7100a680061c59cd30083d94a2
Luke Kuzmish <lakuzmish@gmail.com>
2026-02-09 18:10:50 -0500

    remove unused attribute classification

--- src/Illuminate/Foundation/Http/Attributes/HydrateFromRequest.php
+++ src/Illuminate/Foundation/Http/Attributes/HydrateFromRequest.php
@@ -4,7 +4,7 @@ namespace Illuminate\Foundation\Http\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_PARAMETER)]
 class HydrateFromRequest
 {
     //
