1ffe3841f758f80e54822faf5dfc40eddef05550
Ali Khosrojerdi <ali7.khosrojerdi@gmail.com>
2026-02-09 18:24:08 +0330

    feat: add allowedAbilities and deniedAbilities

--- src/Illuminate/Foundation/Auth/Access/Authorizable.php
+++ src/Illuminate/Foundation/Auth/Access/Authorizable.php
@@ -53,4 +53,38 @@ trait Authorizable
     {
         return $this->cant($abilities, $arguments);
     }
+
+    /**
+     * Get abilities that the user has from the given list.
+     *
+     * @param  iterable|\UnitEnum|string  $abilities
+     * @param  mixed  $arguments
+     * @return array
+     */
+    public function allowedAbilities($abilities, $arguments = [])
+    {
+        $abilities = is_array($abilities) ? $abilities : [$abilities];
+        $gate = app(Gate::class)->forUser($this);
+
+        return collect($abilities)->filter(function ($ability) use ($gate, $arguments) {
+            return $gate->check($ability, $arguments);
+        })->values()->all();
+    }
+
+    /**
+     * Get abilities that the user does not have from the given list.
+     *
+     * @param  iterable|\UnitEnum|string  $abilities
+     * @param  mixed  $arguments
+     * @return array
+     */
+    public function deniedAbilities($abilities, $arguments = [])
+    {
+        $abilities = is_array($abilities) ? $abilities : [$abilities];
+        $gate = app(Gate::class)->forUser($this);
+
+        return collect($abilities)->filter(function ($ability) use ($gate, $arguments) {
+            return ! $gate->check($ability, $arguments);
+        })->values()->all();
+    }
 }
