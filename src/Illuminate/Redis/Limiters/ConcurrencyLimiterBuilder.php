e677e02429caef93660815506c42b452dfc28752
Mathias Grimm <mathiasgrimm@gmail.com>
2026-02-12 09:37:36 -0300

    wip

--- src/Illuminate/Redis/Limiters/ConcurrencyLimiterBuilder.php
+++ src/Illuminate/Redis/Limiters/ConcurrencyLimiterBuilder.php
@@ -3,32 +3,9 @@
 namespace Illuminate\Redis\Limiters;
 
 use Illuminate\Cache\Limiters\ConcurrencyLimiterBuilder as BaseConcurrencyLimiterBuilder;
-use Illuminate\Contracts\Redis\LimiterTimeoutException;
 
 class ConcurrencyLimiterBuilder extends BaseConcurrencyLimiterBuilder
 {
-    /**
-     * Execute the given callback if a lock is obtained, otherwise call the failure callback.
-     *
-     * @param  callable  $callback
-     * @param  callable|null  $failure
-     * @return mixed
-     *
-     * @throws \Illuminate\Contracts\Redis\LimiterTimeoutException
-     */
-    public function then(callable $callback, ?callable $failure = null)
-    {
-        try {
-            return $this->createLimiter()->block($this->timeout, $callback, $this->sleep);
-        } catch (LimiterTimeoutException $e) {
-            if ($failure) {
-                return $failure($e);
-            }
-
-            throw $e;
-        }
-    }
-
     /**
      * Create the concurrency limiter instance.
      *
