af49410535000e82b450f7b5ac5ccd11697b8ff3
AmirHossein Fallah <amirhacking85@gmail.com>
2026-02-19 17:03:01 +0330

    [12.x] Simplify TokenGuard methods

--- src/Illuminate/Auth/TokenGuard.php
+++ src/Illuminate/Auth/TokenGuard.php
@@ -96,21 +96,10 @@ class TokenGuard implements Guard
      */
     public function getTokenForRequest()
     {
-        $token = $this->request->query($this->inputKey);
-
-        if (empty($token)) {
-            $token = $this->request->input($this->inputKey);
-        }
-
-        if (empty($token)) {
-            $token = $this->request->bearerToken();
-        }
-
-        if (empty($token)) {
-            $token = $this->request->getPassword();
-        }
-
-        return $token;
+        return $this->request->query($this->inputKey)
+            ?: $this->request->input($this->inputKey)
+            ?: $this->request->bearerToken()
+            ?: $this->request->getPassword();
     }
 
     /**
@@ -127,11 +116,7 @@ class TokenGuard implements Guard
 
         $credentials = [$this->storageKey => $credentials[$this->inputKey]];
 
-        if ($this->provider->retrieveByCredentials($credentials)) {
-            return true;
-        }
-
-        return false;
+        return (bool) $this->provider->retrieveByCredentials($credentials);
     }
 
     /**
