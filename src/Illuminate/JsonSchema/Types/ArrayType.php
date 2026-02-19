1eb3d494d63ac264a1d7d39ed728c273009afabb
AmirHossein Fallah <amirhacking85@gmail.com>
2026-02-19 16:52:06 +0330

    [12.x] Add uniqueItems support to ArrayType

--- src/Illuminate/JsonSchema/Types/ArrayType.php
+++ src/Illuminate/JsonSchema/Types/ArrayType.php
@@ -20,6 +20,11 @@ class ArrayType extends Type
     protected ?Type $items = null;
 
     /**
+     * Whether the array items must be unique.
+     */
+    protected ?bool $uniqueItems = null;
+
+    /**
      * Set the minimum number of items (inclusive).
      */
     public function min(int $value): static
@@ -45,6 +50,18 @@ class ArrayType extends Type
     public function items(Type $type): static
     {
         $this->items = $type;
+
+        return $this;
+    }
+
+    /**
+     * Indicate that array items must be unique.
+     */
+    public function uniqueItems(bool $uniqueItems = true): static
+    {
+        if ($uniqueItems) {
+            $this->uniqueItems = true;
+        }
 
         return $this;
     }
