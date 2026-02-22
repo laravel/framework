1f5919e8bcae4adb4f2f269765a28efef3962224
Jack <jjbayliss@icloud.com>
2026-02-22 20:52:47 +0000

    add a successfull test

--- tests/Integration/Filesystem/FilesystemServiceProviderTest.php
+++ tests/Integration/Filesystem/FilesystemServiceProviderTest.php
@@ -9,7 +9,7 @@ use Orchestra\Testbench\TestCase;
 
 class FilesystemServiceProviderTest extends TestCase
 {
-    public function test_it_throws_when_served_disks_have_conflicting_uris()
+    public function test_it_throws_when_served_disks_have_conflicting_uris(): void
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('The [other] disk conflicts with the [local] disk at [/storage]. Each served disk must have a unique [url].');
@@ -28,5 +28,27 @@ class FilesystemServiceProviderTest extends TestCase
         ]]);
 
         (new FilesystemServiceProvider($this->app))->boot();
+    }
+
+    public function test_served_disks_with_unique_urls_do_not_conflict(): void
+    {
+        config(['filesystems.disks' => [
+            'local' => [
+                'driver' => 'local',
+                'root' => storage_path('app'),
+                'serve' => true,
+                'url' => '/storage',
+            ],
+            'other' => [
+                'driver' => 'local',
+                'root' => storage_path('other'),
+                'serve' => true,
+                'url' => '/other',
+            ],
+        ]]);
+
+        (new FilesystemServiceProvider($this->app))->boot();
+
+        $this->assertCount(2, $this->app->make('config')->get('filesystems.disks'));
     }
 }
