8115c32a38f1dbc426c8b16168f7e14bb5a845ef
ryanschaefer <contact.devsquid@gmail.com>
2026-02-22 20:49:33 -0500

    Fix Invalid Types
    Fix Invalid Types
    
    Fixes invalid types.
    
    ##Summary of changes
    
    ### PendingBatch.php
    235 - Closures cannot act as bounds. Seems like this was just a result of the type being long. Used a type alias instead.
    
    ### Dispatcher.php
    239 - parens are required for parsing
    560 - input is a class string, we cannot assert that the class string is an instance of class, instead we can assert its a class string of the class
    819 - Type was resolved to callable|null, it seems the callable should return type|null instead.
    
    ### BladeCompiler.php
    977+983 - not all callables support bindTo, only Closures do

--- src/Illuminate/View/Compilers/BladeCompiler.php
+++ src/Illuminate/View/Compilers/BladeCompiler.php
@@ -974,13 +974,13 @@ class BladeCompiler extends Compiler implements CompilerInterface
      * Register a handler for custom directives.
      *
      * @param  string  $name
-     * @param  callable  $handler
+     * @param  \Closure  $handler
      * @param  bool  $bind
      * @return void
      *
      * @throws \InvalidArgumentException
      */
-    public function directive($name, callable $handler, bool $bind = false)
+    public function directive($name, \Closure $handler, bool $bind = false)
     {
         if (! preg_match('/^\w+(?:::\w+)?$/x', $name)) {
             throw new InvalidArgumentException("The directive name [{$name}] is not valid. Directive names must only contain alphanumeric characters and underscores.");
