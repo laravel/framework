a17d331ae6d280e6997ea514336a7fdf1d9f0c13
Taylor Otwell <taylor@laravel.com>
2026-02-22 18:17:42 -0600

    Update SQLiteGrammar.php

--- src/Illuminate/Database/Query/Grammars/SQLiteGrammar.php
+++ src/Illuminate/Database/Query/Grammars/SQLiteGrammar.php
@@ -45,18 +45,6 @@ class SQLiteGrammar extends Grammar
     }
 
     /**
-     * Compile a "where null safe equals" clause.
-     *
-     * @param  \Illuminate\Database\Query\Builder  $query
-     * @param  array  $where
-     * @return string
-     */
-    protected function whereNullSafeEquals(Builder $query, $where)
-    {
-        return $this->wrap($where['column']).' is '.$this->parameter($where['value']);
-    }
-
-    /**
      * Compile a "where like" clause.
      *
      * @param  \Illuminate\Database\Query\Builder  $query
@@ -87,6 +75,18 @@ class SQLiteGrammar extends Grammar
             ['[*]', '[?]', '*', '?'],
             $value
         );
+    }
+
+    /**
+     * Compile a "where null safe equals" clause.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  array  $where
+     * @return string
+     */
+    protected function whereNullSafeEquals(Builder $query, $where)
+    {
+        return $this->wrap($where['column']).' is '.$this->parameter($where['value']);
     }
 
     /**
