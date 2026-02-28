df25efa3cea19ab1489182866d13c47da538202f
webard <b.gajda@webard.me>
2026-02-28 15:26:50 +0100

    docblocks

--- src/Illuminate/Macroable/MacroOverrides.php
+++ src/Illuminate/Macroable/MacroOverrides.php
@@ -6,13 +6,29 @@ namespace Illuminate\Support;
 
 class MacroOverrides
 {
+    /**
+     * Indicates if a macros should not be overridden when it already exists.
+     *
+     * @var bool
+     */
     protected static $preventMacroOverrides = false;
 
-    public static function prevent($prevent = true)
+    /**
+     * Prevents macro overrides.
+     *
+     * @param  bool  $prevent
+     * @return void
+     */
+    public static function prevent($prevent = true): void
     {
         static::$preventMacroOverrides = $prevent;
     }
 
+    /**
+     * Determines if macro overrides should be prevented.
+     *
+     * @return bool
+     */
     public static function shouldPrevent(): bool
     {
         return static::$preventMacroOverrides;
