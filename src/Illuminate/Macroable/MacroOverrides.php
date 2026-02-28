e87c8fb4385f2a78b1acdc9132ed94a9db6d0d9a
webard <b.gajda@webard.me>
2026-02-28 15:21:41 +0100

    prevent macro overrides mechanism

---
+++ src/Illuminate/Macroable/MacroOverrides.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Illuminate\Support;
+
+class MacroOverrides
+{
+    protected static $preventMacroOverrides = false;
+
+    public static function preventGlobally($prevent = true)
+    {
+        static::$preventMacroOverrides = $prevent;
+    }
+
+    public static function shouldPrevent(): bool
+    {
+        return static::$preventMacroOverrides;
+    }
+}
