33d3b859c860197cac4d8cc8ca49c3ca33139cd2
JurianArie <28654085+JurianArie@users.noreply.github.com>
2026-03-01 15:14:41 +0100

    [13.x] Add controller middleware attribute (#59030)
    * Add controller middleware attribute
    
    * Account for missing controller methods
    
    Fixes test:
    
    1) Illuminate\Tests\Routing\RouteRegistrarTest::testCanSetMiddlewareForSpecifiedMethodsOnRegisteredResource
    ReflectionException: Method Illuminate\Tests\Routing\RouteRegistrarControllerStub::create() does not exist
    
    * expected should be the first arg
    
    * formatting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

---
+++ tests/Integration/Routing/MiddlewareAttributeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Routing;
+
+use Illuminate\Routing\Attributes\Controllers\Middleware;
+use Illuminate\Support\Facades\Route;
+use Orchestra\Testbench\TestCase;
+
+class MiddlewareAttributeTest extends TestCase
+{
+    public function test_attribute_middleware_is_respected(): void
+    {
+        $route = Route::get('/', [MiddlewareAttributeController::class, 'index']);
+        $this->assertEquals([
+            'all',
+            'only-index',
+            'also-index',
+        ], $route->controllerMiddleware());
+
+        $route = Route::get('/', [MiddlewareAttributeController::class, 'show']);
+        $this->assertEquals([
+            'all',
+            'except-index',
+        ], $route->controllerMiddleware());
+    }
+}
+
+#[Middleware('all')]
+#[Middleware('only-index', only: ['index'])]
+#[Middleware('except-index', except: ['index'])]
+class MiddlewareAttributeController
+{
+    #[Middleware('also-index')]
+    public function index(): void
+    {
+        // ...
+    }
+
+    public function show(): void
+    {
+        // ...
+    }
+}
