0d88bc2f2b4a2e8558d9560d3e3c76c2778cb100
sumaiazaman <saktar50.cse@gmail.com>
2026-03-01 16:01:01 +0600

    [12.x] Refactor: convert switch to match in operatorForWhere()

--- src/Illuminate/Collections/Traits/EnumeratesValues.php
+++ src/Illuminate/Collections/Traits/EnumeratesValues.php
@@ -1136,20 +1136,18 @@ trait EnumeratesValues
                 return in_array($operator, ['!=', '<>', '!==']);
             }
 
-            switch ($operator) {
-                default:
-                case '=':
-                case '==':  return $retrieved == $value;
-                case '!=':
-                case '<>':  return $retrieved != $value;
-                case '<':   return $retrieved < $value;
-                case '>':   return $retrieved > $value;
-                case '<=':  return $retrieved <= $value;
-                case '>=':  return $retrieved >= $value;
-                case '===': return $retrieved === $value;
-                case '!==': return $retrieved !== $value;
-                case '<=>': return $retrieved <=> $value;
-            }
+            return match ($operator) {
+                '=', '=='   => $retrieved == $value,
+                '!=', '<>'  => $retrieved != $value,
+                '<'         => $retrieved < $value,
+                '>'         => $retrieved > $value,
+                '<='        => $retrieved <= $value,
+                '>='        => $retrieved >= $value,
+                '==='       => $retrieved === $value,
+                '!=='       => $retrieved !== $value,
+                '<=>'       => $retrieved <=> $value,
+                default     => $retrieved == $value,
+            };
         };
     }
 
