14fadeb744d0d1966dec2a68da53fa174824258b
Taylor Otwell <taylor@laravel.com>
2026-03-02 09:44:43 +0100

    allow closure

--- src/Illuminate/Routing/Attributes/Controllers/Middleware.php
+++ src/Illuminate/Routing/Attributes/Controllers/Middleware.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Routing\Attributes\Controllers;
 
 use Attribute;
+use Closure;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Middleware
@@ -12,7 +13,7 @@ class Middleware
      * @param  array<string>|null  $except
      */
     public function __construct(
-        public string $value,
+        public Closure|string $value,
         public ?array $only = null,
         public ?array $except = null,
     ) {
