c6a3c236d716af5615392ea87bc89a7dc1878da0
Taylor Otwell <taylor@laravel.com>
2026-03-02 09:33:10 +0100

    formatting

--- src/Illuminate/Routing/Attributes/Controllers/Authorize.php
+++ src/Illuminate/Routing/Attributes/Controllers/Authorize.php
@@ -5,17 +5,17 @@ namespace Illuminate\Routing\Attributes\Controllers;
 use Attribute;
 use Illuminate\Auth\Middleware\Authorize as AuthorizeMiddleware;
 use Illuminate\Support\Arr;
+use UnitEnum;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Authorize extends Middleware
 {
     /**
-     * @param  \UnitEnum|string  $ability
-     * @param  string|array<string>|null  $models
+     * @param  array<string>|string|null  $models
      */
     public function __construct(
-        $ability,
-        $models = null,
+        UnitEnum|string $ability,
+        array|string|null $models = null,
         ?array $only = null,
         ?array $except = null,
     ) {
