c4845a84fe28c92afbe689f3c60287e08aecb539
David Bomba <turbo124@gmail.com>
2026-03-02 19:26:07 +1100

    Memory Limit passed as string when run from supervisor (#59049)
    * Cast  to int
    
    * Update Worker.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Queue/Worker.php
+++ src/Illuminate/Queue/Worker.php
@@ -816,7 +816,7 @@ class Worker
      */
     public function memoryExceeded($memoryLimit)
     {
-        return $memoryLimit > 0 && (memory_get_usage(true) / 1024 / 1024) >= $memoryLimit;
+        return ((int) $memoryLimit) > 0 && (memory_get_usage(true) / 1024 / 1024) >= ((int) $memoryLimit);
     }
 
     /**
