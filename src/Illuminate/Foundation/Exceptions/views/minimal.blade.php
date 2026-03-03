c4697ff4658630a15fc0ca2c4eb8c96af21a64e9
Taylor Otwell <taylor@laravel.com>
2026-03-03 10:41:21 +0100

    update error template

--- src/Illuminate/Foundation/Exceptions/views/minimal.blade.php
+++ src/Illuminate/Foundation/Exceptions/views/minimal.blade.php
@@ -16,15 +16,15 @@
             }
         </style>
     </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0" role="main">
+    <body class="antialiased bg-white text-black">
+        <div class="relative flex items-top justify-center min-h-screen bg-white text-black sm:items-center sm:pt-0" role="main">
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                    <h1 class="px-4 text-lg dark:text-gray-300 text-gray-700 border-r border-gray-400 tracking-wider">
+                    <h1 class="px-4 text-lg text-black border-r border-gray-400">
                         @yield('code')
                     </h1>
 
-                    <div class="ml-4 text-lg dark:text-gray-300 text-gray-700 uppercase tracking-wider">
+                    <div class="ml-4 text-lg text-black">
                         @yield('message')
                     </div>
                 </div>
