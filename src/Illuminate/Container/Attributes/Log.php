d6f6184770731cdd41c41666be4cd01594695d10
Luke Kuzmish <lakuzmish@gmail.com>
2026-03-05 06:35:39 -0500

    Seb suggests

--- src/Illuminate/Container/Attributes/Log.php
+++ src/Illuminate/Container/Attributes/Log.php
@@ -5,18 +5,20 @@ namespace Illuminate\Container\Attributes;
 use Attribute;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Container\ContextualAttribute;
+use UnitEnum;
+use function Illuminate\Support\enum_value;
 
 #[Attribute(Attribute::TARGET_PARAMETER)]
 class Log implements ContextualAttribute
 {
     /**
      * Create a new class instance.
-     * @param  string|null  $channel  The log configuration's channel name.
-     * @param  string|null  $name  The name to prefix all logs with. Only to be used with Monolog drivers.
+     * @param  string|UnitEnum|null  $channel  The log configuration's channel name.
+     * @param  string|UnitEnum|null  $name  The name to prefix all logs with. Only to be used with Monolog drivers.
      */
     public function __construct(
-        public ?string $channel = null,
-        public ?string $name = null,
+        public string|UnitEnum|null $channel = null,
+        public string|UnitEnum|null $name = null,
     ) {
     }
 
@@ -29,9 +31,9 @@ class Log implements ContextualAttribute
      */
     public static function resolve(self $attribute, Container $container)
     {
-        $logger = $container->make('log')->channel($attribute->channel);
+        $logger = $container->make('log')->channel(enum_value($attribute->channel));
         if ($attribute->name !== null) {
-            $logger = $logger->withName($attribute->name);
+            $logger = $logger->withName(enum_value($attribute->name));
         }
 
         return $logger;
