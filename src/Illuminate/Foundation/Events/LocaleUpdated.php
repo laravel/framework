7d9330ac6846bf4a75f4279e2d6f33edfe82be79
sumaiazaman <saktar50.cse@gmail.com>
2026-03-13 15:38:39 +0600

    [14.x] Use constructor promotion and typed properties in Foundation events

--- src/Illuminate/Foundation/Events/LocaleUpdated.php
+++ src/Illuminate/Foundation/Events/LocaleUpdated.php
@@ -5,29 +5,11 @@ namespace Illuminate\Foundation\Events;
 class LocaleUpdated
 {
     /**
-     * The new locale.
-     *
-     * @var string
-     */
-    public $locale;
-
-    /**
-     * The previous locale.
-     *
-     * @var ?string
-     */
-    public $previousLocale;
-
-    /**
      * Create a new event instance.
-     *
-     * @param  string  $locale
-     * @param  ?string  $previousLocale
      */
-    public function __construct($locale, $previousLocale = null)
-    {
-        $this->locale = $locale;
-
-        $this->previousLocale = $previousLocale;
+    public function __construct(
+        public string $locale,
+        public ?string $previousLocale = null,
+    ) {
     }
 }
