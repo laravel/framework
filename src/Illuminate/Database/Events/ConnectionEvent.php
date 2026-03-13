dd75f2ae5b2b08a477702e40a1aa505026d3862d
sumaiazaman <saktar50.cse@gmail.com>
2026-03-13 15:34:40 +0600

    [14.x] Use constructor promotion and typed properties in database events

--- src/Illuminate/Database/Events/ConnectionEvent.php
+++ src/Illuminate/Database/Events/ConnectionEvent.php
@@ -2,30 +2,21 @@
 
 namespace Illuminate\Database\Events;
 
+use Illuminate\Database\Connection;
+
 abstract class ConnectionEvent
 {
     /**
      * The name of the connection.
-     *
-     * @var string
      */
-    public $connectionName;
-
-    /**
-     * The database connection instance.
-     *
-     * @var \Illuminate\Database\Connection
-     */
-    public $connection;
+    public string $connectionName;
 
     /**
      * Create a new event instance.
-     *
-     * @param  \Illuminate\Database\Connection  $connection
      */
-    public function __construct($connection)
-    {
-        $this->connection = $connection;
+    public function __construct(
+        public Connection $connection,
+    ) {
         $this->connectionName = $connection->getName();
     }
 }
