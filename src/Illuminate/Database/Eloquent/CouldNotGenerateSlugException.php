3b52577cf663cc14f38f6d85660004f8c43bdb47
nuno maduro <enunomaduro@gmail.com>
2026-03-16 15:58:07 +0000

    fix: default message

--- src/Illuminate/Database/Eloquent/CouldNotGenerateSlugException.php
+++ src/Illuminate/Database/Eloquent/CouldNotGenerateSlugException.php
@@ -9,7 +9,7 @@ class CouldNotGenerateSlugException extends RuntimeException
     /**
      * Create a new exception instance.
      */
-    public function __construct(string $message = 'Could not generate a slug.')
+    public function __construct(string $message)
     {
         parent::__construct($message);
     }
