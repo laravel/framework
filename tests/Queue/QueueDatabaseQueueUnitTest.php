ddfddc420da4bc7fbd1243daae46cf33d6b87a5c
galahadxvi <mike@galahadcreative.com>
2026-03-27 13:09:05 +0000

    wip

--- tests/Queue/QueueDatabaseQueueUnitTest.php
+++ tests/Queue/QueueDatabaseQueueUnitTest.php
@@ -148,20 +148,6 @@ class QueueDatabaseQueueUnitTest extends TestCase
         ]);
     }
 
-    public function testCreatePayloadIncludesDeleteWhenMissingModels()
-    {
-        $queue = m::mock(Queue::class)->makePartial();
-        $class = new ReflectionClass(Queue::class);
-
-        $createPayload = $class->getMethod('createPayload');
-        $payload = json_decode($createPayload->invokeArgs($queue, [
-            new DeleteWhenMissingModelsTestJob,
-            'queue-name',
-        ]), true);
-
-        $this->assertTrue($payload['deleteWhenMissingModels']);
-    }
-
     public function testBulkBatchPushesOntoDatabase()
     {
         $uuid = Str::uuid();
