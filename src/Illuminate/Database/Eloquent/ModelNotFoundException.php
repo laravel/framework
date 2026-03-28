a5428442d5c36ee5fdfdfcc1f2a421828110a11a
Joe Theuerkauf <joe@theuerkauf.dev>
2026-03-28 01:05:07 -0400

    Fix #59147 - enum_value() should be a Closure
    * Adjusted previous fix, passing enum_value(...)
      instead of 'enum_value' to array_map() to fix test results
    
    See: https://github.com/laravel/framework/pull/59147

--- src/Illuminate/Database/Eloquent/ModelNotFoundException.php
+++ src/Illuminate/Database/Eloquent/ModelNotFoundException.php
@@ -5,6 +5,8 @@ namespace Illuminate\Database\Eloquent;
 use Illuminate\Database\RecordsNotFoundException;
 use Illuminate\Support\Arr;
 
+use function Illuminate\Support\enum_value;
+
 /**
  * @template TModel of \Illuminate\Database\Eloquent\Model
  */
@@ -35,7 +37,7 @@ class ModelNotFoundException extends RecordsNotFoundException
     {
         $this->model = $model;
 
-        $this->ids = array_map('enum_value', Arr::wrap($ids));
+        $this->ids = array_map(enum_value(...), Arr::wrap($ids));
 
         $this->message = "No query results for model [{$model}]";
 
