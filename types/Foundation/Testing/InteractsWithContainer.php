494bdc0dc142ac81b9779491204f66bfa183ea84
Kayw <weikaiii@sina.cn>
2026-04-01 16:43:55 +0800

    Fix StyleCI formatting

--- types/Foundation/Testing/InteractsWithContainer.php
+++ types/Foundation/Testing/InteractsWithContainer.php
@@ -4,7 +4,6 @@ namespace Illuminate\Types\Foundation\Testing;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithContainer;
-use Mockery\MockInterface;
 use User;
 
 use function PHPStan\Testing\assertType;
@@ -18,13 +17,16 @@ class InteractsWithContainerTestCase
     public function test(): void
     {
         assertType('Mockery\MockInterface&User', $this->mock(User::class));
-        assertType('Mockery\MockInterface&User', $this->mock(User::class, function ($mock) {}));
+        assertType('Mockery\MockInterface&User', $this->mock(User::class, function ($mock) {
+        }));
 
         assertType('Mockery\MockInterface&User', $this->partialMock(User::class));
-        assertType('Mockery\MockInterface&User', $this->partialMock(User::class, function ($mock) {}));
+        assertType('Mockery\MockInterface&User', $this->partialMock(User::class, function ($mock) {
+        }));
 
         assertType('Mockery\MockInterface&User', $this->spy(User::class));
-        assertType('Mockery\MockInterface&User', $this->spy(User::class, function ($mock) {}));
+        assertType('Mockery\MockInterface&User', $this->spy(User::class, function ($mock) {
+        }));
 
         assertType('Mockery\MockInterface', $this->mock('my.service'));
         assertType('Mockery\MockInterface', $this->partialMock('my.service'));
