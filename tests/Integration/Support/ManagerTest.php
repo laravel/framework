9152f353b78969dcdc9ffd068d6bd544136db60b
Sebastian Cabarcas Berrio <42840369+scabarcas17@users.noreply.github.com>
2026-04-13 08:02:05 -0500

    [13.x] Add enum support to Manager driver method (#59659)
    * [13.x] Add enum support to Manager driver method
    
    * Remove unnecessary enum_value import
    
    ---------
    
    Co-authored-by: sebastian cabarcas <sebastian@grupoav.cl>

--- tests/Integration/Support/ManagerTest.php
+++ tests/Integration/Support/ManagerTest.php
@@ -31,4 +31,56 @@ class ManagerTest extends TestCase
         $manager->extend(__CLASS__, static fn () => $driver);
         $this->assertSame($driver, $manager->driver(__CLASS__));
     }
+
+    public function testEnumDriverCanBeResolved()
+    {
+        $manager = new NullableManager($this->app);
+        $driver = new stdClass;
+
+        $manager->extend('my_driver', static fn () => $driver);
+        $this->assertSame($driver, $manager->driver(ManagerDriverName::MyDriver));
+    }
+
+    public function testEnumDriverIsCached()
+    {
+        $manager = new NullableManager($this->app);
+
+        $manager->extend('my_driver', static fn () => new stdClass);
+
+        $driver1 = $manager->driver(ManagerDriverName::MyDriver);
+        $driver2 = $manager->driver(ManagerDriverName::MyDriver);
+
+        $this->assertSame($driver1, $driver2);
+    }
+
+    public function testEnumDriverMatchesStringDriver()
+    {
+        $manager = new NullableManager($this->app);
+
+        $manager->extend('my_driver', static fn () => new stdClass);
+
+        $fromEnum = $manager->driver(ManagerDriverName::MyDriver);
+        $fromString = $manager->driver('my_driver');
+
+        $this->assertSame($fromEnum, $fromString);
+    }
+
+    public function testUnitEnumDriverCanBeResolved()
+    {
+        $manager = new NullableManager($this->app);
+        $driver = new stdClass;
+
+        $manager->extend('MyDriver', static fn () => $driver);
+        $this->assertSame($driver, $manager->driver(ManagerUnitDriverName::MyDriver));
+    }
+}
+
+enum ManagerDriverName: string
+{
+    case MyDriver = 'my_driver';
+}
+
+enum ManagerUnitDriverName
+{
+    case MyDriver;
 }
