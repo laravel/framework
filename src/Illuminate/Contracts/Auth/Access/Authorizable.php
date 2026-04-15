e8e4f085fe2936d1d46ec130c313234d6080c58a
Will Rowe <wrowe@will-rowe.com>
2026-04-15 10:43:28 -0400

    Add 'authorize' method to the contract

--- src/Illuminate/Contracts/Auth/Access/Authorizable.php
+++ src/Illuminate/Contracts/Auth/Access/Authorizable.php
@@ -5,6 +5,17 @@ namespace Illuminate\Contracts\Auth\Access;
 interface Authorizable
 {
     /**
+     * Determine if the given ability should be granted for the entity.
+     *
+     * @param  \UnitEnum|string  $ability
+     * @param  mixed  $arguments
+     * @return \Illuminate\Auth\Access\Response
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function authorize($ability, $arguments = []);
+
+    /**
      * Determine if the entity has a given ability.
      *
      * @param  iterable|string  $abilities
