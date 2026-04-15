70ac20e2448410e5862ef9448a903d677ba24924
Lucas Michot <lucas@zaiple.com>
2026-04-15 12:51:43 +0200

    Add rules for testsuite

--- rector.php
+++ rector.php
@@ -34,7 +34,54 @@ use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
 use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\Php83\Rector\FuncCall\DynamicClassConstFetchRector;
+use Rector\PHPUnit\CodeQuality\Rector\CallLike\DirectInstanceOverMockArgRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\ConstructClassMethodToSetUpTestCaseRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\InlineStubPropertyToCreateStubMethodCallRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\NarrowUnusedSetUpDefinedPropertyRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\RemoveNeverUsedMockPropertyRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\EntityDocumentCreateMockToDirectNewRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\RemoveEmptyTestMethodRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\RemoveStandaloneCreateMockRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
+use Rector\PHPUnit\CodeQuality\Rector\Foreach_\SimplifyForeachInstanceOfRector;
+use Rector\PHPUnit\CodeQuality\Rector\FuncCall\AssertFuncCallToPHPUnitAssertRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\MergeWithCallableAndWillReturnRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\NarrowIdenticalWithConsecutiveRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\NarrowSingleWillReturnCallbackRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\RemoveExpectAnyFromMockRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\SimplerWithIsInstanceOfRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\SingleWithConsecutiveToWithRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\UseSpecificWillMethodRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\UseSpecificWithMethodRector;
+use Rector\PHPUnit\PHPUnit60\Rector\MethodCall\GetMockBuilderGetMockToCreateMockRector;
+use Rector\PHPUnit\PHPUnit90\Rector\MethodCall\ReplaceAtMethodWithDesiredMatcherRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
+
+$testsuiteRules = [
+    AssertFuncCallToPHPUnitAssertRector::class,
+    ConstructClassMethodToSetUpTestCaseRector::class,
+    DirectInstanceOverMockArgRector::class,
+    EntityDocumentCreateMockToDirectNewRector::class,
+    GetMockBuilderGetMockToCreateMockRector::class,
+    InlineStubPropertyToCreateStubMethodCallRector::class,
+    MergeWithCallableAndWillReturnRector::class,
+    NarrowIdenticalWithConsecutiveRector::class,
+    NarrowSingleWillReturnCallbackRector::class,
+    NarrowUnusedSetUpDefinedPropertyRector::class,
+    PreferPHPUnitThisCallRector::class,
+    RemoveEmptyTestMethodRector::class,
+    RemoveExpectAnyFromMockRector::class,
+    RemoveNeverUsedMockPropertyRector::class,
+    RemoveStandaloneCreateMockRector::class,
+    ReplaceAtMethodWithDesiredMatcherRector::class,
+    ReplaceTestAnnotationWithPrefixedFunctionRector::class,
+    SimplerWithIsInstanceOfRector::class,
+    SimplifyForeachInstanceOfRector::class,
+    SingleWithConsecutiveToWithRector::class,
+    UseSpecificWillMethodRector::class,
+    UseSpecificWithMethodRector::class,
+];
 
 return RectorConfig::configure()
     ->withRootFiles()
@@ -78,6 +125,7 @@ return RectorConfig::configure()
         'tests/Foundation/fixtures/bad-syntax-strategy.php',
     ])
     ->withRules([
+        ...$testsuiteRules,
         CountArrayToEmptyArrayComparisonRector::class,
         StrlenZeroToIdenticalEmptyStringRector::class,
     ])
