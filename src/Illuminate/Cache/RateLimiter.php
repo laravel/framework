68b2b02ee5027f1626f28ee95f2c6bac70756fb0
Paul A. <impaul@hey.com>
2026-04-15 16:05:40 +0300

    [12.x] Fix infinite rate limiter TTL on custom increments (#59693)
    * wip
    
    * add test
    
    * fix test

--- src/Illuminate/Cache/RateLimiter.php
+++ src/Illuminate/Cache/RateLimiter.php
@@ -171,9 +171,9 @@ class RateLimiter
 
         $hits = (int) $this->cache->increment($key, $amount);
 
-        if (! $added && $hits == 1) {
+        if (! $added && $hits == $amount) {
             $this->withoutSerializationOrCompression(
-                fn () => $this->cache->put($key, 1, $decaySeconds)
+                fn () => $this->cache->put($key, $amount, $decaySeconds)
             );
         }
 
