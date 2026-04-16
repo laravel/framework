44c74ebb597825d56ae3a1a249697e913e2b15c2
Jack Bayliss <jjbayliss@icloud.com>
2026-04-16 14:43:54 +0100

    [13.x]  Allow array of pivot arrays to be passed to hasAttached (#59723)
    * 13.x-allow-has-attached-to-pass-arrays
    
    improve
    
    * Update Factory.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Database/Eloquent/Factories/Factory.php
+++ src/Illuminate/Database/Eloquent/Factories/Factory.php
@@ -724,6 +724,14 @@ abstract class Factory
      */
     public function hasAttached($factory, $pivot = [], $relationship = null)
     {
+        if (is_array($pivot) && count($pivot) > 0 && array_all($pivot, fn ($p) => is_array($p))) {
+            $factory = $factory instanceof Factory && $factory->count === null 
+                ? $factory->count(count($pivot)) 
+                : $factory;
+
+            $pivot = new Sequence(...$pivot);
+        }
+
         return $this->newInstance([
             'has' => $this->has->concat([new BelongsToManyRelationship(
                 $factory,
