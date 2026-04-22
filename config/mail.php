04b8dd97c9fcb2480e1d7869e4350506f4b96bdf
Bjarn Bronsveld <bjarn@bronsveld.me>
2026-04-22 17:31:55 +0200

    Add Lettermint mail transport config to stubs

--- config/mail.php
+++ config/mail.php
@@ -65,6 +65,10 @@ return [
             'transport' => 'resend',
         ],
 
+        'lettermint' => [
+            'transport' => 'lettermint',
+        ],
+
         'sendmail' => [
             'transport' => 'sendmail',
             'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
--- config/services.php
+++ config/services.php
@@ -22,6 +22,11 @@ return [
         'key' => env('RESEND_KEY'),
     ],
 
+    'lettermint' => [
+        'key' => env('LETTERMINT_TOKEN'),
+        'route' => env('LETTERMINT_ROUTE'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
