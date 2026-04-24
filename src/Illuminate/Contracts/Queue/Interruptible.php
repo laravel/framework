87439a2363bcdd600f5f57b56807eebad0499c98
Taylor Otwell <taylor@laravel.com>
2026-04-24 09:49:35 -0500

    add doc block

--- src/Illuminate/Contracts/Queue/Interruptible.php
+++ src/Illuminate/Contracts/Queue/Interruptible.php
@@ -4,5 +4,11 @@ namespace Illuminate\Contracts\Queue;
 
 interface Interruptible
 {
+    /**
+     * Handle a signal received by the queue worker.
+     *
+     * @param  int  $signal
+     * @return void
+     */
     public function interrupted(int $signal): void;
 }
