928b0e5851bac30b8b638daf0f22923c8c0beadb
Rodolphe MOUTTE <rmd974@users.noreply.github.com>
2026-04-27 15:47:57 +0200

    [13.x] Fix factory hasAttached method pivot JSON attribute handling (#59856)
    * Fix Undefined array key 0 exception when setting pivot json columns with hasAttached factory method
    
    * Replace unnecessary import with native function call
    
    ---------
    
    Co-authored-by: Rodolphe Moutte <r.moutte@pm.me>

--- src/Illuminate/Database/Eloquent/Factories/Factory.php
+++ src/Illuminate/Database/Eloquent/Factories/Factory.php
@@ -724,7 +724,7 @@ abstract class Factory
      */
     public function hasAttached($factory, $pivot = [], $relationship = null)
     {
-        if (is_array($pivot) && $pivot !== [] && array_all($pivot, fn ($p) => is_array($p))) {
+        if (is_array($pivot) && $pivot !== [] && array_is_list($pivot) && array_all($pivot, fn ($p) => is_array($p))) {
             $factory = $factory instanceof Factory && $factory->count === null
                 ? $factory->count(count($pivot))
                 : $factory;
