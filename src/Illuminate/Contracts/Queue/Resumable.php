c982549c41e1bb2250437affccc7709c2bab6406
Luke Kuzmish <lakuzmish@gmail.com>
2026-04-29 08:51:25 -0400

    wip-in

--- src/Illuminate/Contracts/Queue/Resumable.php
+++ src/Illuminate/Contracts/Queue/Resumable.php
@@ -2,7 +2,21 @@
 
 namespace Illuminate\Contracts\Queue;
 
+use Illuminate\Bus\Workflow\ResumeState;
+use Illuminate\Bus\Workflow\Workflow;
+
 interface Resumable
 {
-    public function setCheckpointData(array $checkpointData): static;
+    public function setResumeState(?ResumeState $resumeState): static;
+
+    public function resumeStateKey(): string;
+
+    public function setWorkflow(Workflow $workflow): static;
+
+    /**
+     * @return \DateTimeInterface|\DateInterval|int|null
+     */
+    public function getResumeStateTtl();
+
+    public function getWorkflow(): Workflow;
 }
