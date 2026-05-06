2f991856f500669a44582f597896084135e0b623
Jack Bayliss <jjbayliss@icloud.com>
2026-05-06 19:50:26 +0100

    [13.x] Add Preparable interface for Jobs (#59879)
    * 13.x add InteractsWithDispatch and prepare method
    
    use snake, I suppose and implements order
    
    mixed up in this
    
    clean up a bit
    
    think harder
    
    wording
    
    Revert "tests"
    
    This reverts commit 360d8011eee801dea5227a25057c3aa2f0646773.
    
    tests
    
    Update PendingDispatch.php
    
    rename methods
    
    rename
    
    adjust type
    
    Update BeforeDispatch.php
    
    damn you mr cs
    
    doc block
    
    rename.. again
    
    * smarten up test names
    
    * formatting
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

--- src/Illuminate/Foundation/Bus/PendingDispatch.php
+++ src/Illuminate/Foundation/Bus/PendingDispatch.php
@@ -7,6 +7,7 @@ use Illuminate\Bus\UniqueLock;
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Bus\Dispatcher;
 use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Contracts\Queue\PreparesForDispatch;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Foundation\Queue\InteractsWithUniqueJobs;
 use Illuminate\Queue\Attributes\DebounceFor;
@@ -206,6 +207,10 @@ class PendingDispatch
      */
     protected function shouldDispatch()
     {
+        if ($this->job instanceof PreparesForDispatch && $this->job->prepareForDispatch() === false) {
+            return false;
+        }
+
         if (! $this->job instanceof ShouldBeUnique) {
             return true;
         }
