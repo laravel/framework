5a24e6c72ef5466c5fe1888e400ecfd9215f1787
Jeremy Nikolic <jeremy@laravel.com>
2026-05-07 13:36:04 +0200

    Add support to scoped filesystem for Cloud

--- src/Illuminate/Foundation/Cloud.php
+++ src/Illuminate/Foundation/Cloud.php
@@ -51,18 +51,26 @@ class Cloud
         $disks = json_decode($_SERVER['LARAVEL_CLOUD_DISK_CONFIG'], true);
 
         foreach ($disks as $disk) {
-            $app['config']->set('filesystems.disks.'.$disk['disk'], [
-                'driver' => 's3',
-                'key' => $disk['access_key_id'],
-                'secret' => $disk['access_key_secret'],
-                'bucket' => $disk['bucket'],
-                'url' => $disk['url'],
-                'endpoint' => $disk['endpoint'],
-                'region' => 'auto',
-                'use_path_style_endpoint' => false,
-                'throw' => false,
-                'report' => false,
-            ]);
+            if ($disk['scoped_disk'] ?? false) {
+                $app['config']->set('filesystems.disks.'.$disk['disk'], [
+                    'driver' => 'scoped',
+                    'disk' => $disk['scoped_disk'],
+                    'prefix' => $disk['prefix'] ?? '',
+                ]);
+            } else {
+                $app['config']->set('filesystems.disks.'.$disk['disk'], [
+                    'driver' => 's3',
+                    'key' => $disk['access_key_id'],
+                    'secret' => $disk['access_key_secret'],
+                    'bucket' => $disk['bucket'],
+                    'url' => $disk['url'],
+                    'endpoint' => $disk['endpoint'],
+                    'region' => 'auto',
+                    'use_path_style_endpoint' => false,
+                    'throw' => false,
+                    'report' => false,
+                ]);
+            }
 
             if ($disk['is_default'] ?? false) {
                 $app['config']->set('filesystems.default', $disk['disk']);
