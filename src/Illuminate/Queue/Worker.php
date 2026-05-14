afa2c57aa67570f47e14f9c91fa70b29c0a93209
Tim MacDonald <hello@timacdonald.me>
2026-05-14 14:25:57 +1000

    Back port #59370

--- src/Illuminate/Queue/Worker.php
+++ src/Illuminate/Queue/Worker.php
@@ -85,6 +85,13 @@ class Worker
     public $shouldQuit = false;
 
     /**
+     * Indicates if the worker lost its connection.
+     *
+     * @var bool
+     */
+    public $lostConnection = false;
+
+    /**
      * Indicates if the worker is paused.
      *
      * @var bool
@@ -304,6 +311,7 @@ class Worker
     protected function stopIfNecessary(WorkerOptions $options, $lastRestart, $startTime = 0, $jobsProcessed = 0, $job = null)
     {
         return match (true) {
+            $this->lostConnection => [static::EXIT_SUCCESS, WorkerStopReason::LostConnection],
             $this->shouldQuit => [static::EXIT_SUCCESS, WorkerStopReason::Interrupted],
             $this->memoryExceeded($options->memory) => [static::EXIT_MEMORY_LIMIT, WorkerStopReason::MaxMemoryExceeded],
             $this->queueShouldRestart($lastRestart) => [static::EXIT_SUCCESS, WorkerStopReason::ReceivedRestartSignal],
@@ -406,7 +414,7 @@ class Worker
     protected function stopWorkerIfLostConnection($e)
     {
         if ($this->causedByLostConnection($e)) {
-            $this->shouldQuit = true;
+            $this->lostConnection = true;
         }
     }
 
