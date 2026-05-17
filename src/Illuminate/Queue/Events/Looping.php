186e00c77a5a272d396264515ef1208e81cc3d5a
Jack <jjbayliss@icloud.com>
2026-05-17 18:58:53 +0100

    13.x-workeroptions-looping

--- src/Illuminate/Queue/Events/Looping.php
+++ src/Illuminate/Queue/Events/Looping.php
@@ -9,10 +9,12 @@ class Looping
      *
      * @param  string  $connectionName  The connection name.
      * @param  string  $queue  The queue name.
+     * @param  \Illuminate\Queue\WorkerOptions|null  $workerOptions  The worker options.
      */
     public function __construct(
         public $connectionName,
         public $queue,
+        public $workerOptions = null,
     ) {
     }
 }
