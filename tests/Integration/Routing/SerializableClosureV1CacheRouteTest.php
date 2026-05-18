a160f7736d6cec74d0775784fcdc7a62a46e9a45
jnoordsij <j.noordsij@sslleiden.nl>
2026-05-18 14:56:48 +0200

    Replace RequiresOperatingSystemFamily attributes with RequiresOperatingSystem

--- tests/Integration/Routing/SerializableClosureV1CacheRouteTest.php
+++ tests/Integration/Routing/SerializableClosureV1CacheRouteTest.php
@@ -7,11 +7,11 @@ use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\Attributes\WithMigration;
 use Orchestra\Testbench\Factories\UserFactory;
 use Orchestra\Testbench\TestCase;
-use PHPUnit\Framework\Attributes\RequiresOperatingSystemFamily;
+use PHPUnit\Framework\Attributes\RequiresOperatingSystem;
 
 use function Illuminate\Filesystem\join_paths;
 
-#[RequiresOperatingSystemFamily('Linux|Darwin')]
+#[RequiresOperatingSystem('Linux|Darwin')]
 #[WithConfig('app.key', 'AckfSECXIvnK5r28GVIWUAxmbBSjTsmF')]
 #[WithMigration]
 class SerializableClosureV1CacheRouteTest extends TestCase
