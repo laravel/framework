1110fd4c07156ea8308cc78a90b1fbc6ce31d089
jradtilbrook <jarryd@tilbrooktech.com>
2026-05-18 14:38:42 +0800

    Output cloud request ID in logs

---
+++ src/Illuminate/Foundation/LaravelCloudJsonFormatter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Illuminate\Foundation;
+
+use Illuminate\Container\Container;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\LogRecord;
+
+class LaravelCloudJsonFormatter extends JsonFormatter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function normalizeRecord(LogRecord $record): array
+    {
+        $normalized = parent::normalizeRecord($record);
+
+        $app = Container::getInstance();
+
+        if ($app->bound('request')) {
+            $request = $app->make('request');
+            $requestId = $request->header('X-Request-ID');
+
+            if ($requestId !== null) {
+                $normalized['cloud_request_id'] = $requestId;
+            }
+        }
+
+        return $normalized;
+    }
+}
