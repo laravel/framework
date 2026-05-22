c533b8b6a15552f02aa9feae242c374b7636635b
Jack Bayliss <jjbayliss@icloud.com>
2026-05-22 10:51:07 +0100

    13.x ensure up/down commands report

--- src/Illuminate/Foundation/Console/DownCommand.php
+++ src/Illuminate/Foundation/Console/DownCommand.php
@@ -67,6 +67,8 @@ class DownCommand extends Command
                 $this->components->info('You may bypass maintenance mode via ['.config('app.url')."/{$downFilePayload['secret']}].");
             }
         } catch (Exception $e) {
+            report($e);
+
             $this->components->error(sprintf(
                 'Failed to enter maintenance mode: %s.',
                 $e->getMessage(),
