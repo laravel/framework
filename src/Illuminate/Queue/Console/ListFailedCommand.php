2627847779c74d48b372ff25b792ab5754c57137
Taylor Otwell <taylor@laravel.com>
2026-05-27 08:24:40 +0900

    use display name

--- src/Illuminate/Queue/Console/ListFailedCommand.php
+++ src/Illuminate/Queue/Console/ListFailedCommand.php
@@ -94,11 +94,19 @@ class ListFailedCommand extends Command
     {
         $payload = json_decode($payload, true);
 
-        if ($payload && (! isset($payload['data']['command']))) {
-            return $payload['job'] ?? null;
-        } elseif ($payload && isset($payload['data']['command'])) {
-            return $this->matchJobName($payload);
+        if (! $payload) {
+            return null;
         }
+
+        if (! isset($payload['data']['command'])) {
+            return $payload['job'] ?? null;
+        }
+
+        if (! empty($payload['displayName']) && is_string($payload['displayName'])) {
+            return $payload['displayName'];
+        }
+
+        return $this->matchJobName($payload);
     }
 
     /**
