48227b0bd231ac822965eb8572eab7f6772d5a48
XananasX <cihbank069@gmail.com>
2026-05-31 22:58:48 +0100

    security: restrict unserialize allowed_classes in InvokeSerializedClosureCommand

--- src/Illuminate/Concurrency/Console/InvokeSerializedClosureCommand.php
+++ src/Illuminate/Concurrency/Console/InvokeSerializedClosureCommand.php
@@ -3,6 +3,7 @@
 namespace Illuminate\Concurrency\Console;
 
 use Illuminate\Console\Command;
+use Laravel\SerializableClosure\SerializableClosure;
 use ReflectionClass;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Throwable;
@@ -44,9 +45,10 @@ class InvokeSerializedClosureCommand extends Command
             $this->output->write(json_encode([
                 'successful' => true,
                 'result' => serialize($this->laravel->call(match (true) {
-                    ! is_null($this->argument('code')) => unserialize($this->argument('code')),
+                    ! is_null($this->argument('code')) => unserialize($this->argument('code'), ['allowed_classes' => [SerializableClosure::class]]),
                     isset($_SERVER['LARAVEL_INVOKABLE_CLOSURE']) => unserialize(
-                        base64_decode($_SERVER['LARAVEL_INVOKABLE_CLOSURE'])
+                        base64_decode($_SERVER['LARAVEL_INVOKABLE_CLOSURE']),
+                        ['allowed_classes' => [SerializableClosure::class]]
                     ),
                     default => fn () => null,
                 })),
