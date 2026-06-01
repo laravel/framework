ede77ce9310c01ef8101b5679f165b43c19f24a8
Amirhf1 <amirhacking85@gmail.com>
2026-06-01 12:12:59 +0330

    test(Benchmark): add test coverage for dd()

--- tests/Support/SupportBenchmarkTest.php
+++ tests/Support/SupportBenchmarkTest.php
@@ -1,9 +1,24 @@
 <?php
 
-namespace Illuminate\Tests\Support;
+namespace Illuminate\Support {
+    function dd(mixed ...$vars): never
+    {
+        throw new \Illuminate\Tests\Support\BenchmarkDdCapturedException($vars[0]);
+    }
+}
+
+namespace Illuminate\Tests\Support {
 
 use Illuminate\Support\Benchmark;
 use PHPUnit\Framework\TestCase;
+
+class BenchmarkDdCapturedException extends \RuntimeException
+{
+    public function __construct(public readonly mixed $result)
+    {
+        parent::__construct();
+    }
+}
 
 class SupportBenchmarkTest extends TestCase
 {
@@ -28,10 +43,48 @@ class SupportBenchmarkTest extends TestCase
 
         $this->assertFalse(Benchmark::hasMacro($macroName));
 
-        // Register a macro to test
         Benchmark::macro($macroName, fn () => true);
 
         $this->assertTrue(Benchmark::hasMacro($macroName));
         $this->assertTrue(Benchmark::$macroName());
     }
+
+    public function testDdWithSingleClosure(): void
+    {
+        try {
+            Benchmark::dd(fn () => 1 + 1);
+            $this->fail('Expected BenchmarkDdCapturedException to be thrown');
+        } catch (BenchmarkDdCapturedException $e) {
+            $this->assertMatchesRegularExpression('/^\d+\.\d{3}ms$/', $e->result);
+        }
+    }
+
+    public function testDdWithArray(): void
+    {
+        try {
+            Benchmark::dd([
+                'first' => fn () => 1 + 1,
+                'second' => fn () => 2 + 2,
+            ]);
+            $this->fail('Expected BenchmarkDdCapturedException to be thrown');
+        } catch (BenchmarkDdCapturedException $e) {
+            $this->assertIsArray($e->result);
+            $this->assertArrayHasKey('first', $e->result);
+            $this->assertArrayHasKey('second', $e->result);
+            $this->assertMatchesRegularExpression('/^\d+\.\d{3}ms$/', $e->result['first']);
+            $this->assertMatchesRegularExpression('/^\d+\.\d{3}ms$/', $e->result['second']);
+        }
+    }
+
+    public function testDdWithIterations(): void
+    {
+        try {
+            Benchmark::dd(fn () => 1 + 1, iterations: 5);
+            $this->fail('Expected BenchmarkDdCapturedException to be thrown');
+        } catch (BenchmarkDdCapturedException $e) {
+            $this->assertMatchesRegularExpression('/^\d+\.\d{3}ms$/', $e->result);
+        }
+    }
+}
+
 }
