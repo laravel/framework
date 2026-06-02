0565084ef4c4019d595d1f531e52df2718f7a394
Jack Bayliss <jjbayliss@icloud.com>
2026-06-02 10:01:57 +0100

    cs2

--- tests/Integration/Filesystem/ServeFileTest.php
+++ tests/Integration/Filesystem/ServeFileTest.php
@@ -58,6 +58,7 @@ class ServeFileTest extends TestCase
 
         $response->assertForbidden();
     }
+
     #[RequiresOperatingSystem('Linux|Darwin')]
     public function testItCanServeAFileWithUriDelimitersInThePath()
     {
