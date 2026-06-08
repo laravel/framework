dbc6ee7cd2f399884858cc333d25c477c2b111b4
jnoordsij <j.noordsij@sslleiden.nl>
2026-06-08 17:12:16 +0200

    Use constructor promoted properties in Address class

--- src/Illuminate/Mail/Mailables/Address.php
+++ src/Illuminate/Mail/Mailables/Address.php
@@ -7,32 +7,17 @@ use InvalidArgumentException;
 class Address
 {
     /**
-     * The recipient's email address.
-     *
-     * @var string
-     */
-    public $address;
-
-    /**
-     * The recipient's name.
-     *
-     * @var string|null
-     */
-    public $name;
-
-    /**
      * Create a new address instance.
      *
      * @param  string  $address
      * @param  string|null  $name
      */
-    public function __construct(string $address, ?string $name = null)
-    {
+    public function __construct(
+        public string $address,
+        public ?string $name = null,
+    ) {
         if (preg_match('/[\r\n]/', $address) > 0) {
             throw new InvalidArgumentException('Email addresses may not contain line break characters.');
         }
-
-        $this->address = $address;
-        $this->name = $name;
     }
 }
