80734e46bc4664d2900b471fbbb3e7295c90b4db
Denys Finchenko <dfinchenko@gmail.com>
2026-06-17 15:05:04 +0200

    [13.x] Add missing @throws \ReflectionException annotations

--- src/Illuminate/Reflection/Reflector.php
+++ src/Illuminate/Reflection/Reflector.php
@@ -82,6 +82,8 @@ class Reflector
      * @param  class-string<TAttribute>  $attribute
      * @param  bool  $includeParents
      * @return ($includeParents is true ? Collection<class-string<contravariant TTarget>, Collection<int, TAttribute>> : Collection<int, TAttribute>)
+     *
+     * @throws \ReflectionException
      */
     public static function getClassAttributes($objectOrClass, $attribute, $includeParents = false)
     {
