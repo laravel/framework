d3a69aa96008869a175765ed70577513b7a14436
Jack Bayliss <jjbayliss@icloud.com>
2026-06-22 13:58:35 +0100

    [13.x] Reduce cache hits when debouncing with maxWait (#60559)
    * Update DebounceLock.php
    
    Update DebounceLock.php
    
    * rename var to match cache key

--- src/Illuminate/Bus/DebounceLock.php
+++ src/Illuminate/Bus/DebounceLock.php
@@ -66,13 +66,15 @@ class DebounceLock
 
         $timestampKey = $key.':first_dispatched_at';
 
-        if (! $cache->has($timestampKey)) {
+        $firstDispatchedAt = $cache->get($timestampKey);
+
+        if (is_null($firstDispatchedAt)) {
             $cache->put($timestampKey, Carbon::now()->getTimestamp(), $ttl);
 
             return false;
         }
 
-        $elapsed = Carbon::now()->getTimestamp() - $cache->get($timestampKey);
+        $elapsed = Carbon::now()->getTimestamp() - $firstDispatchedAt;
 
         if ($elapsed >= $maxWait) {
             $cache->forget($timestampKey);
