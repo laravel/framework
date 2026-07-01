3dce98ee12862dd360cea33e50f23a01b45c45ed
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2026-07-02 00:40:48 +0200

    Fix typo

--- src/Illuminate/Support/Number.php
+++ src/Illuminate/Support/Number.php
@@ -407,7 +407,7 @@ class Number
         }
 
         if ($number > 9) {
-            return implode('', array_map(fn (string $digit) => self::sup((int) $digit), str_split($number)));
+            return implode('', array_map(fn (string $digit) => self::sub((int) $digit), str_split($number)));
         }
 
         return match ($number) {
