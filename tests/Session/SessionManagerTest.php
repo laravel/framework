e55d01dea2afbf9566954749a9e805a281a87575
Jack Bayliss <jjbayliss@icloud.com>
2026-07-07 13:34:20 +0100

    test

--- tests/Session/SessionManagerTest.php
+++ tests/Session/SessionManagerTest.php
@@ -2,10 +2,13 @@
 
 namespace Illuminate\Tests\Session;
 
+use Illuminate\Cache\CacheManager;
 use Illuminate\Config\Repository as Config;
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Redis\Factory as RedisFactory;
 use Illuminate\Session\SessionManager;
 use PHPUnit\Framework\TestCase;
+use Mockery as m;
 
 class SessionManagerTest extends TestCase
 {
@@ -18,6 +21,21 @@ class SessionManagerTest extends TestCase
         $manager->setDefaultDriver(SessionDriverName::Array);
 
         $this->assertSame('array', $app['config']['session.driver']);
+    }
+
+    public function testRedisDriverUsesConfiguredSessionPrefix()
+    {
+        $app = new Container;
+        $app->singleton('config', fn () => new Config([
+            'session' => ['driver' => 'redis', 'lifetime' => 120, 'prefix' => 'some_custom_prefix'],
+            'cache' => ['prefix' => 'cache_prefix', 'stores' => ['redis' => ['driver' => 'redis']]],
+        ]));
+        $app->singleton('cache', fn ($app) => new CacheManager($app));
+        $app->instance('redis', m::mock(RedisFactory::class));
+
+        $manager = new SessionManager($app);
+
+        $this->assertSame('some_custom_prefix', $manager->driver()->getHandler()->getCache()->getStore()->getPrefix());
     }
 }
 
