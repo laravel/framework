7f631749cb85b04b83049918480b72e595e657f0
taylorotwell <463230+taylorotwell@users.noreply.github.com>
2026-07-15 15:42:00 +0000

    Update facade docblocks

--- src/Illuminate/Support/Facades/Image.php
+++ src/Illuminate/Support/Facades/Image.php
@@ -5,15 +5,22 @@ namespace Illuminate\Support\Facades;
 use Illuminate\Image\ImageManager;
 
 /**
- * @method static \Illuminate\Contracts\Image\Driver driver(string|null $name = null)
  * @method static \Illuminate\Image\Image fromBytes(string $contents)
- * @method static \Illuminate\Image\Image fromPath(string $path)
- * @method static \Illuminate\Image\Image fromUrl(string $url)
  * @method static \Illuminate\Image\Image fromBase64(string $base64)
+ * @method static \Illuminate\Image\Image fromPath(string $path)
+ * @method static \Illuminate\Image\Image fromStorage(string $path, string|null $disk = null)
+ * @method static \Illuminate\Image\Image fromUpload(\Illuminate\Http\UploadedFile $file)
+ * @method static \Illuminate\Image\Image fromUrl(string $url)
+ * @method static \Illuminate\Image\ImageManager transformUsing(string $driver, string $transformation, callable $callback)
  * @method static string getDefaultDriver()
+ * @method static mixed driver(\UnitEnum|string|null $driver = null)
  * @method static \Illuminate\Image\ImageManager extend(string $driver, \Closure $callback)
+ * @method static array getDrivers()
+ * @method static \Illuminate\Contracts\Container\Container getContainer()
+ * @method static \Illuminate\Image\ImageManager setContainer(\Illuminate\Contracts\Container\Container $container)
+ * @method static \Illuminate\Image\ImageManager forgetDrivers()
  *
- * @see ImageManager
+ * @see \Illuminate\Image\ImageManager
  */
 class Image extends Facade
 {
