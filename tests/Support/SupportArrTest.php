71a586ed492d97b7a7d874c2bad25e403d5d5db5
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2026-07-24 20:14:08 +0200

    Add missing semicolon

--- tests/Support/SupportArrTest.php
+++ tests/Support/SupportArrTest.php
@@ -1971,7 +1971,7 @@ class SupportArrTest extends TestCase
         $result = [];
 
         foreach (Arr::concurrent(...$input) as $key => [$c, $d]) {
-            array_push($result, ...array_map(fn ($value) => $value * 2), [$c, $d])
+            array_push($result, ...array_map(fn ($value) => $value * 2), [$c, $d]);
         }
         
         $this->assertEquals($output, $result);
