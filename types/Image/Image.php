bad77f9590d60083d789a135e1ea189def0e1457
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2026-07-26 01:36:29 +0200

    Add type tests for some methods of `Image?

---
+++ types/Image/Image.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Str;
+use function PHPStan\Testing\assertType;
+
+$file = UploadedFile::fake()->image('test.jpg', 100, 100);
+$image = new Image(file_get_contents($file->getRealPath()));
+
+rescue(fn () => assertType('never', $image->resize()));
+rescue(fn () => assertType('never', $image->scale()));
+rescue(fn () => assertType('never', $image->optimize('gibberish')));
+rescue(fn () => assertType('never', $image->toFormat('gibberish')));
