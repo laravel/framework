a6c6eacf12f004c3ac0c05067c8ce8cca647c765
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2026-07-26 15:06:45 +0200

    [13.x] Improve types in `Image` class (#60890)
    * Improve types in `Image` class
    
    * Add type tests for some methods of `Image?
    
    * StyleCI
    
    * Fix imports
    
    * StyleCI
    
    * StyleCI
    
    * Fix import
    
    * Fix import (one last time)
    
    * Do not type-check protected method
    
    * conditional never doesn't work with native return type
    
    * Narrow types for `\Illuminate\Image\Transformations\Cover`
    
    * Narrow types of `\Illuminate\Image\Transformations\Contain`
    
    * Narrow types of `\Illuminate\Image\Transformations\Crop`
    
    * Narrow types of `\Illuminate\Image\Transformations\Resize`
    
    * Narrow types of `\Illuminate\Image\Transformations\Scale`
    
    * Narrow types of `\Illuminate\Image\Transformations\Blur`
    
    * Narrow types of `\Illuminate\Image\Transformations\Sharpen`
    
    * Delete `types/Image/Image.php`

--- src/Illuminate/Image/Transformations/Sharpen.php
+++ src/Illuminate/Image/Transformations/Sharpen.php
@@ -6,6 +6,9 @@ use Illuminate\Contracts\Image\Transformation;
 
 class Sharpen implements Transformation
 {
+    /**
+     * @param  positive-int  $amount
+     */
     public function __construct(public readonly int $amount)
     {
         //
