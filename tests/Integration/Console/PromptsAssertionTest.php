15efd602da40375da02192231891c6f1e52012c0
Romain Mezghenna <60466020+romain-mezghenna@users.noreply.github.com>
2026-08-02 22:59:12 +0200

    [13.x] Add NumberPrompt fallback (#60959)
    * fix(prompts): Add missing fallback for NumberPrompt
    
    * fix(prompts): Require laravel/prompts ^0.3.11 for NumberPrompt

--- tests/Integration/Console/PromptsAssertionTest.php
+++ tests/Integration/Console/PromptsAssertionTest.php
@@ -9,6 +9,7 @@ use Orchestra\Testbench\TestCase;
 use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\multisearch;
 use function Laravel\Prompts\multiselect;
+use function Laravel\Prompts\number;
 use function Laravel\Prompts\password;
 use function Laravel\Prompts\pause;
 use function Laravel\Prompts\search;
@@ -86,6 +87,28 @@ class PromptsAssertionTest extends TestCase
             ->artisan('test:textarea')
             ->expectsQuestion('What is your name?', 'Jane')
             ->expectsOutput('Jane');
+    }
+
+    public function testAssertionForNumberPrompt(): void
+    {
+        $this->app[Kernel::class]->registerCommand(
+            new class extends Command
+            {
+                protected $signature = 'test:number';
+
+                public function handle()
+                {
+                    $count = number('How many people?');
+
+                    $this->line("There are {$count} people.");
+                }
+            }
+        );
+
+        $this
+            ->artisan('test:number')
+            ->expectsQuestion('How many people?', 5)
+            ->expectsOutput('There are 5 people.');
     }
 
     public function testAssertionForSuggestPrompt(): void
