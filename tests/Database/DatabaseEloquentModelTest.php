7063440d903f2e79dee0f066ccdd0263453c0da1
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-03 03:06:06 +0500

    [13.x] Fix appended accessors receiving null in toArray() (#60921)

--- tests/Database/DatabaseEloquentModelTest.php
+++ tests/Database/DatabaseEloquentModelTest.php
@@ -2566,6 +2566,15 @@ class DatabaseEloquentModelTest extends TestCase
         $this->assertSame([], $model->toArray());
     }
 
+    public function testToArrayPassesRealValueToAppendedAccessor()
+    {
+        $model = new EloquentModelAppendsWithExistingAttributeStub;
+        $model->price = 100;
+
+        $this->assertSame(100, $model->price);
+        $this->assertSame(100, $model->toArray()['price']);
+    }
+
     public function testMergeAppendsMergesAppends()
     {
         $model = new EloquentModelAppendsStub;
@@ -4304,6 +4313,18 @@ class EloquentModelAppendsStub extends Model
     public function getStudlyCasedAttribute()
     {
         return 'StudlyCased';
+    }
+}
+
+class EloquentModelAppendsWithExistingAttributeStub extends Model
+{
+    protected $guarded = [];
+
+    protected $appends = ['price'];
+
+    protected function price(): Attribute
+    {
+        return Attribute::get(fn ($value) => $value);
     }
 }
 
