2e3154852c135105fe82cad8eb5cf3deaa0ab098
Jess Archer <jess@jessarcher.com>
2026-08-06 16:45:03 +1000

    Configure a socket timeout for the Cloud log driver

--- src/Illuminate/Foundation/Cloud.php
+++ src/Illuminate/Foundation/Cloud.php
@@ -186,6 +186,7 @@ class Cloud
             'with' => [
                 'connectionString' => Cloud::socket(),
                 'persistent' => true,
+                'timeout' => 2.0,
             ],
         ]);
     }
