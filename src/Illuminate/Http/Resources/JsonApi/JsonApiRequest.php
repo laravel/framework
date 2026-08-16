c22fce9dc58516fd4f1e14bac87bbc91754e0382
Sergei Shiriaev <33102548+s-shiryaev@users.noreply.github.com>
2026-08-16 15:50:57 +0300

    Prevent TypeError when JSON:API query parameters are arrays (#61206)

--- src/Illuminate/Http/Resources/JsonApi/JsonApiRequest.php
+++ src/Illuminate/Http/Resources/JsonApi/JsonApiRequest.php
@@ -25,7 +25,7 @@ class JsonApiRequest extends Request
     {
         if (is_null($this->cachedSparseFields)) {
             $this->cachedSparseFields = (new Collection($this->array('fields')))
-                ->transform(fn ($fieldsets) => empty($fieldsets) ? [] : explode(',', $fieldsets))
+                ->transform(fn ($fieldsets) => ! is_string($fieldsets) || empty($fieldsets) ? [] : explode(',', $fieldsets))
                 ->all();
         }
 
@@ -50,7 +50,7 @@ class JsonApiRequest extends Request
     public function sparseIncluded(?string $key = null): ?array
     {
         if (is_null($this->cachedSparseIncluded)) {
-            $included = (string) $this->string('include', '');
+            $included = is_string($included = $this->input('include')) ? $included : '';
 
             $this->cachedSparseIncluded = (new Collection(empty($included) ? [] : explode(',', $included)))
                 ->transform(function ($item) {
