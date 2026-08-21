626bbe8d76a2e3d92fff83b65c33fdc633f6840a
Taylor Otwell <taylor@laravel.com>
2026-08-21 11:55:46 +0200

    Update PhpRedisConnection.php

--- src/Illuminate/Redis/Connections/PhpRedisConnection.php
+++ src/Illuminate/Redis/Connections/PhpRedisConnection.php
@@ -131,6 +131,7 @@ class PhpRedisConnection extends Connection implements ConnectionContract
     public function mget(array $keys)
     {
         $result = $this->command('mget', [$keys]);
+
         if ($result === false) {
             return array_fill(0, count($keys), null);
         }
@@ -185,6 +186,7 @@ class PhpRedisConnection extends Connection implements ConnectionContract
         }
 
         $result = $this->command('hmget', [$key, $dictionary]);
+
         if ($result === false) {
             return array_fill(0, count((array) $dictionary), null);
         }
