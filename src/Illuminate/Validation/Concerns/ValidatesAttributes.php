fcc67a575e085aecfa2e081aedad242989316e2e
KIKOmanasijev <manasijevhristijan@gmail.com>
2026-08-24 19:59:31 +0200

    Use strict comparison for contains validation rule

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -571,7 +571,7 @@ trait ValidatesAttributes
             return false;
         }
 
-        return array_all($parameters, fn ($parameter) => in_array($parameter, $value));
+        return array_all($parameters, fn ($parameter) => in_array($parameter, $value, true));
     }
 
     /**
