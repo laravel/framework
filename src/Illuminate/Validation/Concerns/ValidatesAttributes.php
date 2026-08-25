8f6b2f649f018e94a8f8eb1b435982d88cc84aa6
Taylor Otwell <taylor@laravel.com>
2026-08-25 11:41:31 +0100

    Revert "Use strict comparison for contains validation rule (#61320)" (#61330)
    This reverts commit 87a21fba9eb55441a31558bec80fe9c32630ddd2.

--- src/Illuminate/Validation/Concerns/ValidatesAttributes.php
+++ src/Illuminate/Validation/Concerns/ValidatesAttributes.php
@@ -571,7 +571,7 @@ trait ValidatesAttributes
             return false;
         }
 
-        return array_all($parameters, fn ($parameter) => in_array($parameter, $value, true));
+        return array_all($parameters, fn ($parameter) => in_array($parameter, $value));
     }
 
     /**
