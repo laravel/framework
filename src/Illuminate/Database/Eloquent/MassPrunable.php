eda0b4206d7e3fd6b73542f3b461ce570f0cd22a
Caleb White <cdwhite3@pm.me>
2026-09-03 09:12:36 -0500

    fix: prune soft-deleted mass prunable models

--- src/Illuminate/Database/Eloquent/MassPrunable.php
+++ src/Illuminate/Database/Eloquent/MassPrunable.php
@@ -9,21 +9,17 @@ trait MassPrunable
 {
     /**
      * Prune all prunable models in the database.
-     *
-     * @param  int  $chunkSize
-     * @return int
      */
-    public function pruneAll(int $chunkSize = 1000)
+    public function pruneAll(int $chunkSize = 1000): int
     {
-        $query = tap($this->prunable(), function ($query) use ($chunkSize) {
-            $query->when(! $query->getQuery()->limit, function ($query) use ($chunkSize) {
-                $query->limit($chunkSize);
-            });
+        $softDeletable = static::isSoftDeletable();
+
+        $query = tap($this->prunable(), function ($query) use ($chunkSize, $softDeletable) {
+            $query->when($softDeletable, fn ($query) => $query->withTrashed())
+                ->when(! $query->getQuery()->limit, fn ($query) => $query->limit($chunkSize));
         });
 
         $total = 0;
-
-        $softDeletable = static::isSoftDeletable();
 
         do {
             $total += $count = $softDeletable
