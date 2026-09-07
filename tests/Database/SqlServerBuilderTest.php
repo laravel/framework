61403ebf1babb8687027be22f3fa78a3e88ca854
Yusof Sadat Fakhr <yusofsadat@gmail.com>
2026-09-07 16:05:58 +0330

    Fix file permissions and remove unused import

--- tests/Database/SqlServerBuilderTest.php
+++ tests/Database/SqlServerBuilderTest.php
@@ -3,7 +3,6 @@
 namespace Illuminate\Tests\Database;
 
 use Illuminate\Database\Connection;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Grammars\SqlServerGrammar;
 use Illuminate\Database\Schema\SqlServerBuilder;
 use Mockery;
