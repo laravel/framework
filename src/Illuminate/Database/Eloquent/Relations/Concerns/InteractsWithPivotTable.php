9919eb6e13adf5c3d84f6e39a1c2417f7b22ba34
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2026-09-07 18:50:59 +0600

    Replay recorded closure scopes in newPivotQuery()

--- src/Illuminate/Database/Eloquent/Relations/Concerns/InteractsWithPivotTable.php
+++ src/Illuminate/Database/Eloquent/Relations/Concerns/InteractsWithPivotTable.php
@@ -682,6 +682,12 @@ trait InteractsWithPivotTable
             $query->whereNull(...$arguments);
         }
 
+        foreach ($this->pivotWhereClosures as [$column, $boolean]) {
+            $pivotQuery = $this->newPivotQueryForClosure($column);
+
+            $query->addNestedWhereQuery($pivotQuery->getQuery(), $boolean);
+        }
+
         return $query->where($this->getQualifiedForeignPivotKeyName(), $this->parent->{$this->parentKey});
     }
 
