dc5e5a550cdc5586737846f3b394486d0c7a1439
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2026-09-08 16:43:54 +0600

    Add doesnt_contain tests for non-string-scalar params

--- tests/Validation/ValidationRuleDoesntContainTest.php
+++ tests/Validation/ValidationRuleDoesntContainTest.php
@@ -111,4 +111,23 @@ class ValidationRuleDoesntContainTest extends TestCase
         $v = new Validator($trans, ['flags' => [1]], ['flags' => 'doesnt_contain:1']);
         $this->assertTrue($v->fails());
     }
+
+    public function testDoesntContainRuleMatchesNonStringScalarParametersFromArraySyntax()
+    {
+        $trans = new Translator(new ArrayLoader, 'en');
+
+        $v = new Validator($trans, ['flags' => [1]], ['flags' => [['doesnt_contain', 1]]]);
+        $this->assertTrue($v->fails());
+
+        $v = new Validator($trans, ['flags' => [true]], ['flags' => [['doesnt_contain', true]]]);
+        $this->assertTrue($v->fails());
+    }
+
+    public function testDoesntContainRuleDoesNotThrowForNonScalarParameter()
+    {
+        $trans = new Translator(new ArrayLoader, 'en');
+
+        $v = new Validator($trans, ['roles' => ['admin']], ['roles' => [['doesnt_contain', ['admin']]]]);
+        $this->assertTrue($v->passes());
+    }
 }
