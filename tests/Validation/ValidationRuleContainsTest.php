20474ee36141c366fe8acd95d3000f5d18762d93
iz-ahmad <n.ahmad.web.cit22@gmail.com>
2026-09-08 15:55:16 +0600

    Add coverage for strict, non-string-scalar comparisons in the contains rule

--- tests/Validation/ValidationRuleContainsTest.php
+++ tests/Validation/ValidationRuleContainsTest.php
@@ -8,6 +8,7 @@ use Illuminate\Translation\ArrayLoader;
 use Illuminate\Translation\Translator;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Validator;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
 
 include_once 'Fixtures/Enums.php';
@@ -86,6 +87,28 @@ class ValidationRuleContainsTest extends TestCase
 
         // Test with nullable field
         $v = new Validator($trans, ['roles' => null], ['roles' => ['nullable', Rule::contains('admin')]]);
+        $this->assertTrue($v->passes());
+    }
+
+    #[TestWith([['0e123'], false])]
+    #[TestWith([['0'], true])]
+    public function testContainsRuleDoesNotUseLooseComparisons(array $value, bool $expectation)
+    {
+        $trans = new Translator(new ArrayLoader, 'en');
+
+        $v = new Validator($trans, ['x' => $value], ['x' => ['contains:0']]);
+
+        $this->assertSame($expectation, $v->passes());
+    }
+
+    public function testContainsRuleMatchesNonStringScalarValues()
+    {
+        $trans = new Translator(new ArrayLoader, 'en');
+
+        $v = new Validator($trans, ['flags' => [true]], ['flags' => 'contains:1']);
+        $this->assertTrue($v->passes());
+
+        $v = new Validator($trans, ['flags' => [1]], ['flags' => 'contains:1']);
         $this->assertTrue($v->passes());
     }
 }
