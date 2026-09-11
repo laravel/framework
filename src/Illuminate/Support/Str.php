2a1b0329153726a5ce9e0d08dd26f0cbe282549a
Ray Blair <rayblair06@hotmail.com>
2026-09-11 15:21:24 +0100

    [13.x] Fix Str::camel() not lowercasing multibyte first characters (#61545)

--- src/Illuminate/Support/Str.php
+++ src/Illuminate/Support/Str.php
@@ -239,7 +239,7 @@ class Str
      */
     public static function camel($value)
     {
-        return static::$camelCache[$value] ?? static::$camelCache[$value] = lcfirst(static::studly($value));
+        return static::$camelCache[$value] ?? static::$camelCache[$value] = static::lcfirst(static::studly($value));
     }
 
     /**
