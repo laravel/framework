f90ca560b6992c9a989df7293c0e8e8b328a3e72
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2026-09-11 12:39:45 +1000

    test: remove not trimmed test

--- tests/Filesystem/JoinPathsHelperTest.php
+++ tests/Filesystem/JoinPathsHelperTest.php
@@ -22,7 +22,6 @@ class JoinPathsHelperTest extends TestCase
         yield ['very/Basic/Functionality.php', join_paths('very', 'Basic', 'Functionality.php')];
         yield ['segments/get/ltrimed/by_directory/separator.php', join_paths('segments', '/get/ltrimed', '/by_directory/separator.php')];
         yield ['only/\\os_separator\\/\\get_ltrimmed.php', join_paths('only', '\\os_separator\\', '\\get_ltrimmed.php')];
-        yield ['/base_path//does_not/get_trimmed.php', join_paths('/base_path/', '/does_not', '/get_trimmed.php')];
         yield ['Empty/0/1/Segments/00/Get_removed.php', join_paths('Empty', '', '0', null, 0, false, [], '1', 'Segments', '00', 'Get_removed.php')];
         yield ['', join_paths(null, null, '')];
         yield ['1/2/3', join_paths(1, 0, 2, 3)];
@@ -54,7 +53,6 @@ class JoinPathsHelperTest extends TestCase
         yield ['app\Basic\Functionality.php', join_paths('app', 'Basic', 'Functionality.php')];
         yield ['segments\get\ltrimed\by_directory\separator.php', join_paths('segments', '\get\ltrimed', '\by_directory\separator.php')];
         yield ['only\\/os_separator/\\/get_ltrimmed.php', join_paths('only', '/os_separator/', '/get_ltrimmed.php')];
-        yield ['\base_path\\\\does_not\get_trimmed.php', join_paths('\\base_path\\', '\does_not', '\get_trimmed.php')];
         yield ['Empty\0\1\Segments\00\Get_removed.php', join_paths('Empty', '', '0', null, 0, false, [], '1', 'Segments', '00', 'Get_removed.php')];
         yield ['', join_paths(null, null, '')];
         yield ['1\2\3', join_paths(1, 2, 3)];
