07201ee4bae95a495d702a3001c0dfbae34e8b3b
Taylor Otwell <taylor@laravel.com>
2026-09-15 08:46:07 -0500

    Update Authorizable.php

--- src/Illuminate/Contracts/Auth/Access/Authorizable.php
+++ src/Illuminate/Contracts/Auth/Access/Authorizable.php
@@ -7,7 +7,7 @@ interface Authorizable
     /**
      * Determine if the entity has a given ability.
      *
-     * @param  iterable|\UnitEnum|string  $abilities
+     * @param  \UnitEnum|iterable|string  $abilities
      * @param  mixed  $arguments
      * @return bool
      */
