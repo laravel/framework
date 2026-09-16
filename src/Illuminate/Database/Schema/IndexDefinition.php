96daab9b929788dbeb89e75347bb90ee0663db14
Sander Muller <github@scode.nl>
2026-09-16 13:00:59 +0200

    [13.x] Add inplace() DDL algorithm modifier for index and foreign key operations
    Mirrors the existing ColumnDefinition::instant() modifier on the index side,
    so a migration can assert ALGORITHM=INPLACE instead of letting MySQL fall back
    to a blocking table rebuild. Avoids the IndexDefinition::algorithm() name clash
    (USING BTREE) that closed #58779.

--- src/Illuminate/Database/Schema/IndexDefinition.php
+++ src/Illuminate/Database/Schema/IndexDefinition.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Fluent;
  * @method $this algorithm(string $algorithm) Specify an algorithm for the index (MySQL/PostgreSQL)
  * @method $this deferrable(bool $value = true) Specify that the unique index is deferrable (PostgreSQL)
  * @method $this initiallyImmediate(bool $value = true) Specify the default time to check the unique index constraint (PostgreSQL)
+ * @method $this inplace(bool $value = true) Specify that algorithm=inplace should be used for the index operation (MySQL)
  * @method $this language(string $language) Specify a language for the full text index (PostgreSQL)
  * @method $this lock(('none'|'shared'|'default'|'exclusive') $value) Specify the DDL lock mode for the index operation (MySQL)
  * @method $this nullsNotDistinct(bool $value = true) Specify that the null values should not be treated as distinct (PostgreSQL)
