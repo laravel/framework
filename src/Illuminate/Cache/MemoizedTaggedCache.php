be89dd7073a89ffa33866809be18060295a4db78
Joost de Bruijn <joostdebruijn@users.noreply.github.com>
2026-09-17 22:32:20 +0200

    [13.x] Tagged memoized cache (#61593)
    * feat: tagged memoized cache
    
    * style: apply styleci diff
    
    * test: don't flushall for redis cluster
    
    * fix: improve implementation and add more test cases
    
    * formatting
    
    * formatting
    
    * test fix
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>

---
+++ src/Illuminate/Cache/MemoizedTaggedCache.php
@@ -0,0 +1,310 @@
+<?php
+
+namespace Illuminate\Cache;
+
+use function Illuminate\Support\enum_value;
+
+class MemoizedTaggedCache extends TaggedCache
+{
+    /**
+     * The memoized cache values.
+     *
+     * @var array<string, mixed>
+     */
+    protected $cache = [];
+
+    /**
+     * The tagged cache instance.
+     *
+     * @var \Illuminate\Cache\TaggedCache
+     */
+    protected TaggedCache $taggedCache;
+
+    /**
+     * The memoized store instance.
+     *
+     * @var \Illuminate\Cache\MemoizedStore
+     */
+    protected MemoizedStore $memoizedStore;
+
+    /**
+     * Create a new memoized tagged cache.
+     */
+    public function __construct(TaggedCache $taggedCache, MemoizedStore $memoizedStore)
+    {
+        $this->taggedCache = $taggedCache;
+        $this->memoizedStore = $memoizedStore;
+
+        parent::__construct($taggedCache->getStore(), $taggedCache->getTags());
+
+        $this->config = ['store' => $taggedCache->getName()];
+        $this->default = $taggedCache->getDefaultCacheTime();
+
+        if (! is_null($taggedCache->getEventDispatcher())) {
+            $this->setEventDispatcher($taggedCache->getEventDispatcher());
+        }
+    }
+
+    /**
+     * Retrieve an item from the cache by key.
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    public function get($key, $default = null): mixed
+    {
+        if (is_array($key)) {
+            return $this->many($key);
+        }
+
+        $key = enum_value($key);
+
+        $prefixedKey = $this->itemKey($key);
+
+        if (! array_key_exists($prefixedKey, $this->cache)) {
+            $this->cache[$prefixedKey] = $this->taggedCache->get($key);
+        }
+
+        return is_null($this->cache[$prefixedKey])
+            ? value($default)
+            : $this->cache[$prefixedKey];
+    }
+
+    /**
+     * Retrieve multiple items from the cache by key.
+     *
+     * @param  array  $defaults
+     * @return array
+     */
+    public function many(array $defaults)
+    {
+        $keys = [];
+        $memoized = [];
+        $missing = [];
+
+        foreach ($defaults as $key => $value) {
+            $key = array_is_list($defaults)
+                ? enum_value($value)
+                : enum_value($key);
+
+            $keys[$key] = array_is_list($defaults) ? null : $value;
+
+            $prefixedKey = $this->itemKey($key);
+
+            if (array_key_exists($prefixedKey, $this->cache)) {
+                $memoized[$key] = $this->cache[$prefixedKey];
+            } else {
+                $missing[] = $key;
+            }
+        }
+
+        if (! empty($missing)) {
+            $retrieved = $this->taggedCache->many($missing);
+
+            foreach ($retrieved as $key => $value) {
+                $this->cache[$this->itemKey($key)] = $value;
+            }
+
+            $memoized = array_merge($memoized, $retrieved);
+        }
+
+        $result = [];
+
+        foreach ($keys as $key => $default) {
+            $result[$key] = array_key_exists($key, $memoized) && ! is_null($memoized[$key])
+                ? $memoized[$key]
+                : value($default);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Store an item in the cache for a given number of seconds.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  int  $ttl
+     * @return bool
+     */
+    public function put($key, $value, $ttl = null)
+    {
+        if (is_array($key)) {
+            return $this->putMany($key, $value);
+        }
+
+        $key = enum_value($key);
+
+        unset($this->cache[$this->itemKey($key)]);
+
+        return $this->taggedCache->put($key, $value, $ttl);
+    }
+
+    /**
+     * Store multiple items in the cache for a given number of seconds.
+     *
+     * @param  int  $seconds
+     * @return bool
+     */
+    public function putMany(array $values, $ttl = null)
+    {
+        foreach ($values as $key => $value) {
+            unset($this->cache[$this->itemKey($key)]);
+        }
+
+        return $this->taggedCache->putMany($values, $ttl);
+    }
+
+    /**
+     * Store an item in the cache if the key does not exist.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  int  $ttl
+     * @return bool
+     */
+    public function add($key, $value, $ttl = null)
+    {
+        $key = enum_value($key);
+
+        unset($this->cache[$this->itemKey($key)]);
+
+        return $this->taggedCache->add($key, $value, $ttl);
+    }
+
+    /**
+     * Increment the value of an item in the cache.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return int|bool
+     */
+    public function increment($key, $value = 1)
+    {
+        $key = enum_value($key);
+
+        unset($this->cache[$this->itemKey($key)]);
+
+        return $this->taggedCache->increment($key, $value);
+    }
+
+    /**
+     * Decrement the value of an item in the cache.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return int|bool
+     */
+    public function decrement($key, $value = 1)
+    {
+        $key = enum_value($key);
+
+        unset($this->cache[$this->itemKey($key)]);
+
+        return $this->taggedCache->decrement($key, $value);
+    }
+
+    /**
+     * Store an item in the cache indefinitely.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function forever($key, $value)
+    {
+        $key = enum_value($key);
+
+        unset($this->cache[$this->itemKey($key)]);
+
+        return $this->taggedCache->forever($key, $value);
+    }
+
+    /**
+     * Adjust the expiration time of a cached item.
+     *
+     * @param  string  $key
+     * @param  int  $ttl
+     * @return bool
+     */
+    public function touch($key, $ttl)
+    {
+        $key = enum_value($key);
+
+        unset($this->cache[$this->itemKey($key)]);
+
+        return $this->taggedCache->touch($key, $ttl);
+    }
+
+    /**
+     * Remove an item from the cache.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function forget($key)
+    {
+        $key = enum_value($key);
+
+        unset($this->cache[$this->itemKey($key)]);
+
+        return $this->taggedCache->forget($key);
+    }
+
+    /**
+     * Remove all items from the cache.
+     *
+     * @return bool
+     */
+    public function flush()
+    {
+        $this->memoizedStore->flushTagged();
+
+        return $this->taggedCache->flush();
+    }
+
+    /**
+     * Remove all items from the cache.
+     *
+     * @return bool
+     */
+    public function clear(): bool
+    {
+        return $this->memoizedStore->flush();
+    }
+
+    /**
+     * Remove all memoized items from the cache.
+     *
+     * @return void
+     */
+    public function flushMemoized()
+    {
+        $this->cache = [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function itemKey($key)
+    {
+        return $this->taggedItemKey($this->getPrefix().enum_value($key));
+    }
+
+    /**
+     * Handle dynamic calls into macros or pass missing methods to the tagged cache.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if (static::hasMacro($method)) {
+            return $this->macroCall($method, $parameters);
+        }
+
+        return $this->taggedCache->$method(...$parameters);
+    }
+}
---
+++ tests/Integration/Cache/MemoizedTaggedCacheTest.php
@@ -0,0 +1,368 @@
+<?php
+
+namespace Illuminate\Tests\Integration\Cache;
+
+use Illuminate\Cache\Events\CacheEvent;
+use Illuminate\Cache\Events\CacheFlushed;
+use Illuminate\Cache\Events\CacheFlushing;
+use Illuminate\Cache\Events\CacheMissed;
+use Illuminate\Cache\Events\RetrievingKey;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithRedis;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Redis;
+use Orchestra\Testbench\TestCase;
+
+class MemoizedTaggedCacheTest extends TestCase
+{
+    use InteractsWithRedis;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpRedis();
+
+        Config::set('cache.default', 'redis');
+        Redis::connection(Config::get('cache.stores.redis.connection'))->flushDb();
+        Redis::connection(Config::get('cache.stores.redis.lock_connection'))->flushDb();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->tearDownRedis();
+    }
+
+    public function test_it_can_memoize_with_tags_when_retrieving_single_value()
+    {
+        Cache::tags(['foo', 'bar'])->put('name', 'Tim', 60);
+
+        $live = Cache::tags(['foo', 'bar'])->get('name');
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->get('name');
+        $this->assertSame('Tim', $live);
+        $this->assertSame('Tim', $memoized);
+
+        Cache::tags(['foo', 'bar'])->put('name', 'Taylor', 60);
+
+        $live = Cache::tags(['foo', 'bar'])->get('name');
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->get('name');
+        $this->assertSame('Taylor', $live);
+        $this->assertSame('Tim', $memoized);
+    }
+
+    public function test_it_can_memoize_with_tags_when_retrieving_multiple_values()
+    {
+        Cache::tags(['foo', 'bar'])->put('name.0', 'Tim', 60);
+        Cache::tags(['foo', 'bar'])->put('name.1', 'Taylor', 60);
+
+        $live = Cache::tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $this->assertSame(['name.0' => 'Tim', 'name.1' => 'Taylor'], $live);
+        $this->assertSame(['name.0' => 'Tim', 'name.1' => 'Taylor'], $memoized);
+
+        Cache::tags(['foo', 'bar'])->put('name.0', 'MacDonald', 60);
+        Cache::tags(['foo', 'bar'])->put('name.1', 'Otwell', 60);
+
+        $live = Cache::tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $this->assertSame(['name.0' => 'MacDonald', 'name.1' => 'Otwell'], $live);
+        $this->assertSame(['name.0' => 'Tim', 'name.1' => 'Taylor'], $memoized);
+    }
+
+    public function test_it_can_flush_memoized_values_with_tags()
+    {
+        Cache::tags(['foo', 'bar'])->put('name.0', 'Tim', 60);
+        Cache::tags(['foo', 'bar'])->put('name.1', 'Taylor', 60);
+
+        $live = Cache::tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $this->assertSame(['name.0' => 'Tim', 'name.1' => 'Taylor'], $live);
+        $this->assertSame(['name.0' => 'Tim', 'name.1' => 'Taylor'], $memoized);
+
+        Cache::tags(['foo', 'bar'])->flush();
+
+        $live = Cache::tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $this->assertSame(['name.0' => null, 'name.1' => null], $live);
+        $this->assertSame(['name.0' => 'Tim', 'name.1' => 'Taylor'], $memoized);
+
+        Cache::memo()->tags(['foo', 'bar'])->flush();
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $this->assertSame(['name.0' => null, 'name.1' => null], $memoized);
+    }
+
+    public function test_flushing_tags_forgets_values_memoized_by_overlapping_tag_sets()
+    {
+        Cache::tags(['foo'])->put('name', 'Tim', 60);
+        Cache::tags(['foo', 'bar'])->put('name', 'Taylor', 60);
+
+        $foo = Cache::memo()->tags(['foo']);
+        $fooAndBar = Cache::memo()->tags(['foo', 'bar']);
+
+        $this->assertSame('Tim', $foo->get('name'));
+        $this->assertSame('Taylor', $fooAndBar->get('name'));
+
+        $foo->flush();
+
+        $this->assertNull($fooAndBar->get('name'));
+    }
+
+    public function test_it_can_forget_memoized_values_with_tags()
+    {
+        Cache::tags(['foo', 'bar'])->put('name', 'Tim', 60);
+
+        $live = Cache::tags(['foo', 'bar'])->get('name');
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->get('name');
+        $this->assertSame('Tim', $live);
+        $this->assertSame('Tim', $memoized);
+
+        Cache::memo()->tags(['foo', 'bar'])->forget('name');
+
+        $live = Cache::tags(['foo', 'bar'])->get('name');
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->get('name');
+        $this->assertNull($live);
+        $this->assertNull($memoized);
+    }
+
+    public function test_it_can_increment_and_decrement_memoized_values_with_tags()
+    {
+        Cache::tags(['foo', 'bar'])->put('count', 1, 60);
+
+        $live = Cache::tags(['foo', 'bar'])->get('count');
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->get('count');
+        $this->assertSame('1', $live);
+        $this->assertSame('1', $memoized);
+
+        Cache::memo()->tags(['foo', 'bar'])->increment('count');
+
+        $live = Cache::tags(['foo', 'bar'])->get('count');
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->get('count');
+        $this->assertSame('2', $live);
+        $this->assertSame('2', $memoized);
+
+        Cache::memo()->tags(['foo', 'bar'])->decrement('count');
+
+        $live = Cache::tags(['foo', 'bar'])->get('count');
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->get('count');
+        $this->assertSame('1', $live);
+        $this->assertSame('1', $memoized);
+    }
+
+    public function test_put_many_for_tagged_memo_driver()
+    {
+        Cache::memo()->tags(['foo', 'bar'])->putMany(['name.0' => 'Tim', 'name.1' => 'Taylor'], 60);
+
+        $live = Cache::tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $this->assertSame(['name.0' => 'Tim', 'name.1' => 'Taylor'], $live);
+        $this->assertSame(['name.0' => 'Tim', 'name.1' => 'Taylor'], $memoized);
+
+        Cache::memo()->tags(['foo', 'bar'])->putMany(['name.0' => 'MacDonald', 'name.1' => 'Otwell'], 60);
+
+        $live = Cache::tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $memoized = Cache::memo()->tags(['foo', 'bar'])->getMultiple(['name.0', 'name.1']);
+        $this->assertSame(['name.0' => 'MacDonald', 'name.1' => 'Otwell'], $live);
+        $this->assertSame(['name.0' => 'MacDonald', 'name.1' => 'Otwell'], $memoized);
+    }
+
+    public function test_put_forgets_memoized_value()
+    {
+        Cache::tags(['foo'])->put('name', 'Tim', 60);
+        Cache::memo()->tags(['foo'])->get('name');
+
+        Cache::memo()->tags(['foo'])->put('name', 'Taylor', 60);
+
+        $this->assertSame('Taylor', Cache::memo()->tags(['foo'])->get('name'));
+    }
+
+    public function test_put_supports_an_array_of_values()
+    {
+        Cache::memo()->tags(['foo'])->put(['first' => 'Tim', 'last' => 'MacDonald'], 60);
+
+        $this->assertSame(
+            ['first' => 'Tim', 'last' => 'MacDonald'],
+            Cache::memo()->tags(['foo'])->get(['first', 'last']),
+        );
+    }
+
+    public function test_tagged_memoized_cache_uses_prefixes()
+    {
+        Cache::tags(['foo', 'bar'])->setPrefix('prefix1_');
+
+        $this->assertSame('prefix1_', Cache::tags(['foo', 'bar'])->getPrefix());
+        $this->assertSame('prefix1_', Cache::memo()->tags(['foo', 'bar'])->getPrefix());
+    }
+
+    public function test_memoized_keys_are_prefixed_with_tags()
+    {
+        $redis = Cache::store('redis')->tags(['foo', 'bar']);
+
+        $redis->setPrefix('aaaa');
+        $redis->put('name', 'Tim', 60);
+        $redis->setPrefix('zzzz');
+        $redis->put('name', 'Taylor', 60);
+
+        $redis->setPrefix('aaaa');
+        $value = Cache::memo('redis')->tags(['foo', 'bar'])->get('name');
+        $this->assertSame('Tim', $value);
+
+        $redis->setPrefix('zzzz');
+        $value = Cache::memo('redis')->tags(['foo', 'bar'])->get('name');
+        $this->assertSame('Taylor', $value);
+    }
+
+    public function test_error_thrown_when_tags_not_supported()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage('This cache store does not support tagging.');
+
+        Cache::memo('file')->tags(['foo', 'bar'])->put('name', 'Tim', 60);
+    }
+
+    public function test_it_forwards_methods_supported_by_the_tagged_cache()
+    {
+        $this->assertTrue(Cache::memo()->tags(['foo'])->flushStale());
+    }
+
+    public function test_it_keeps_memoized_values_isolated_between_tag_sets()
+    {
+        Cache::tags(['foo'])->put('name', 'Foo', 60);
+        Cache::tags(['bar'])->put('name', 'Bar', 60);
+
+        $this->assertSame('Foo', Cache::memo()->tags(['foo'])->get('name'));
+        $this->assertSame('Bar', Cache::memo()->tags(['bar'])->get('name'));
+    }
+
+    public function test_scalar_and_array_tag_names_share_the_same_memoized_cache()
+    {
+        Cache::tags(['foo'])->put('name', 'Foo', 60);
+
+        $this->assertSame('Foo', Cache::memo()->tags('foo')->get('name'));
+
+        Cache::tags(['foo'])->put('name', 'Bar', 60);
+
+        $this->assertSame('Foo', Cache::memo()->tags(['foo'])->get('name'));
+    }
+
+    public function test_it_flushes_tagged_memoized_values_when_the_store_is_flushed()
+    {
+        Cache::tags(['foo'])->put('name', 'Foo', 60);
+        Cache::memo()->tags(['foo'])->get('name');
+
+        Cache::memo()->flush();
+
+        $this->assertNull(Cache::memo()->tags(['foo'])->get('name'));
+    }
+
+    public function test_it_resolves_defaults_when_retrieving_multiple_tagged_values()
+    {
+        $this->assertSame(
+            ['missing' => 'fallback'],
+            Cache::memo()->tags(['foo'])->getMultiple(['missing'], fn () => 'fallback'),
+        );
+
+        $this->assertSame(
+            ['missing' => null],
+            Cache::memo()->tags(['foo'])->get(['missing']),
+        );
+    }
+
+    public function test_it_does_not_memoize_default_values()
+    {
+        $cache = Cache::memo()->tags(['foo']);
+
+        $this->assertSame('first', $cache->get('missing', 'first'));
+        $this->assertSame('second', $cache->get('missing', 'second'));
+    }
+
+    public function test_it_supports_enum_keys()
+    {
+        Cache::tags(['foo'])->put(MemoizedTaggedCacheTestKey::Name, 'Tim', 60);
+
+        $this->assertSame('Tim', Cache::memo()->tags(['foo'])->get(MemoizedTaggedCacheTestKey::Name));
+    }
+
+    public function test_mutations_forget_memoized_values()
+    {
+        Cache::tags(['foo'])->put('name', 'Tim', 60);
+        Cache::memo()->tags(['foo'])->get('name');
+
+        Cache::memo()->tags(['foo'])->forever('name', 'Taylor');
+        $this->assertSame('Taylor', Cache::memo()->tags(['foo'])->get('name'));
+
+        Cache::tags(['foo'])->put('name', 'Abigail', 60);
+        Cache::memo()->tags(['foo'])->touch('name', 60);
+        $this->assertSame('Abigail', Cache::memo()->tags(['foo'])->get('name'));
+
+        Cache::tags(['foo'])->put('name', 'Nuno', 60);
+        Cache::memo()->tags(['foo'])->get('name');
+        Cache::tags(['foo'])->put('name', 'Jess', 60);
+        $this->assertFalse(Cache::memo()->tags(['foo'])->add('name', 'Adam', 60));
+        $this->assertSame('Jess', Cache::memo()->tags(['foo'])->get('name'));
+    }
+
+    public function test_clear_forgets_all_memoized_values()
+    {
+        Cache::put('untagged', 'Tim', 60);
+        Cache::tags(['foo'])->put('foo', 'Taylor', 60);
+        Cache::tags(['bar'])->put('bar', 'Jess', 60);
+
+        Cache::memo()->get('untagged');
+        Cache::memo()->tags(['foo'])->get('foo');
+        Cache::memo()->tags(['bar'])->get('bar');
+
+        Cache::memo()->tags(['foo'])->clear();
+
+        $this->assertNull(Cache::memo()->get('untagged'));
+        $this->assertNull(Cache::memo()->tags(['foo'])->get('foo'));
+        $this->assertNull(Cache::memo()->tags(['bar'])->get('bar'));
+    }
+
+    public function test_store_flush_clears_existing_tagged_memoized_cache_instances_without_dispatching_tag_flush_events()
+    {
+        $events = [];
+
+        Event::listen([CacheFlushing::class, CacheFlushed::class], function ($event) use (&$events) {
+            $events[] = $event;
+        });
+
+        Cache::tags(['foo'])->put('name', 'Tim', 60);
+
+        $cache = Cache::memo()->tags(['foo']);
+        $cache->get('name');
+
+        Cache::memo()->flush();
+
+        $this->assertNull($cache->get('name'));
+        $this->assertSame([], $events);
+    }
+
+    public function test_it_dispatches_decorated_driver_events_only()
+    {
+        $events = [];
+
+        Event::listen('*', function ($type, $event) use (&$events) {
+            if ($event[0] instanceof CacheEvent) {
+                $events[] = $event[0];
+            }
+        });
+
+        Cache::memo()->tags(['foo'])->get('name');
+
+        $this->assertCount(2, $events);
+        $this->assertInstanceOf(RetrievingKey::class, $events[0]);
+        $this->assertSame('redis', $events[0]->storeName);
+        $this->assertSame(['foo'], $events[0]->tags);
+        $this->assertInstanceOf(CacheMissed::class, $events[1]);
+        $this->assertSame('redis', $events[1]->storeName);
+        $this->assertSame(['foo'], $events[1]->tags);
+    }
+}
+
+enum MemoizedTaggedCacheTestKey
+{
+    case Name;
+}
