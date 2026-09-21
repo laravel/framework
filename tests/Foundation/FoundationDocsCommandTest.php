b5e3b782258caa7c31c8b565dd5cfc42d02574cc
Jason McCreary <jason@pureconcepts.net>
2026-09-21 10:21:43 -0400

    Fix docs command test env handling

--- tests/Foundation/FoundationDocsCommandTest.php
+++ tests/Foundation/FoundationDocsCommandTest.php
@@ -4,7 +4,6 @@ namespace Illuminate\Tests\Foundation;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Console\DocsCommand;
-use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Http;
 use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\Attributes\WithEnv;
@@ -32,9 +31,6 @@ class FoundationDocsCommandTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-
-        // Testbench disables putenv when it creates the app.
-        Env::enablePutenv();
 
         Http::preventStrayRequests()->fake([
             'https://laravel.com/docs/8.x/index.json' => Http::response(file_get_contents(__DIR__.'/Fixtures/docs.json')),
