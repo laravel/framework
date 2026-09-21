80b403a424610316d38a1119c27dbe8e33fb343c
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-09-21 18:44:30 +0100

    Test Cloud read replica bootstrap wiring

--- tests/Integration/Foundation/CloudBootstrapperTest.php
+++ tests/Integration/Foundation/CloudBootstrapperTest.php
@@ -2,13 +2,14 @@
 
 namespace Illuminate\Tests\Integration\Foundation;
 
+use Illuminate\Foundation\Bootstrap\LoadConfiguration;
 use Illuminate\Foundation\CloudBootstrapper;
 use Orchestra\Testbench\Attributes\WithConfig;
 use Orchestra\Testbench\TestCase;
 
 class CloudBootstrapperTest extends TestCase
 {
-    public function test_it_configures_a_read_replica_connection()
+    public function test_it_configures_a_read_replica_connection_after_configuration_is_loaded()
     {
         $_SERVER['DB_READ_HOST'] = 'read.example.com';
         $this->app['config']->set('database.default', 'mysql');
@@ -17,7 +18,7 @@ class CloudBootstrapperTest extends TestCase
             'host' => 'write.example.com',
         ]);
 
-        CloudBootstrapper::configureReadReplicaConnection($this->app);
+        CloudBootstrapper::bootstrapped($this->app, LoadConfiguration::class);
 
         $this->assertSame('read.example.com', $this->app['config']->get('database.connections.mysql.read.host'));
         $this->assertSame('write.example.com', $this->app['config']->get('database.connections.mysql.host'));
@@ -27,6 +28,8 @@ class CloudBootstrapperTest extends TestCase
 
     public function test_it_does_not_configure_a_read_replica_connection_without_a_read_host()
     {
+        unset($_SERVER['DB_READ_HOST']);
+
         $connection = [
             'driver' => 'mysql',
             'host' => 'write.example.com',
@@ -38,29 +41,6 @@ class CloudBootstrapperTest extends TestCase
         CloudBootstrapper::configureReadReplicaConnection($this->app);
 
         $this->assertSame($connection, $this->app['config']->get('database.connections.mysql'));
-    }
-
-    public function test_it_preserves_existing_read_replica_configuration()
-    {
-        $_SERVER['DB_READ_HOST'] = 'read.example.com';
-        $this->app['config']->set('database.default', 'mysql');
-        $this->app['config']->set('database.connections.mysql', [
-            'driver' => 'mysql',
-            'host' => 'write.example.com',
-            'read' => [
-                'host' => 'old-read.example.com',
-                'username' => 'read-user',
-            ],
-        ]);
-
-        CloudBootstrapper::configureReadReplicaConnection($this->app);
-
-        $this->assertSame([
-            'host' => 'read.example.com',
-            'username' => 'read-user',
-        ], $this->app['config']->get('database.connections.mysql.read'));
-
-        unset($_SERVER['DB_READ_HOST']);
     }
 
     #[WithConfig('database.connections.pgsql', ['host' => 'test-pooler.pg.laravel.cloud', 'username' => 'test-username', 'password' => 'test-password'])]
